9ab7a49b0aa01756c7d99cce09062ada98ff2578
Elliot Saba <staticfloat@gmail.com>
2018-10-16 10:12:32 +0000

    Update XRTServer to have CUDA and XLA capabilities
          *----------------------*
          |  FEAR ME! FOR I AM   |
          |  THE BUILDER OF ALL  |
          | AND DESTROYER OF ALL |
          *----------------------*
         /
       ...
    \_[^_^]_/
       \ /
      *---*

--- XRTServer/build_tarballs.jl
+++ XRTServer/build_tarballs.jl
@@ -4,7 +4,7 @@ ENV["BINARYBUILDER_USE_CCACHE"] = "false"
 using BinaryBuilder
 
 name = "XRTServer"
-version = v"2018.09.26"
+version = v"2018.10.16"
 
 # Collection of sources required
 sources = [
@@ -12,6 +12,10 @@ sources = [
     "11b6ca9bc3506443c5b46f42f193ee7ba6fb1f50",
     "https://github.com/bazelbuild/bazel/releases/download/0.17.2/bazel-0.17.2-linux-x86_64" =>
     "674757d40d4ac0f0175df7fe84cd7250cbf67ac7ebac565e905fdc7e24c0fac5",
+    "https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda_9.1.85_387.26_linux" =>
+    "8496c72b16fee61889f9281449b5d633d0b358b46579175c275d85c9205fe953",
+    "http://developer.download.nvidia.com/compute/redist/cudnn/v7.1.3/cudnn-9.1-linux-x64-v7.1.tgz" =>
+    "dd616d3794167ceb923d706bf73e8d6acdda770751492b921ee6827cdf190228",
     "./bundled",
 ]
 
@@ -19,11 +23,26 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/tensorflow
 
+# Tensorflow can't search for CuDNN without GNU grep. lol.
+apk add grep
+
+# Install CUDA toolkit
+mkdir -p $WORKSPACE/tmp
+/bin/sh $WORKSPACE/srcdir/cuda_*_linux --silent --tmpdir=$WORKSPACE/tmp --toolkit --toolkitpath=$prefix/cuda
+
+# Install CuDNN
+rsync -ra $WORKSPACE/srcdir/cuda/{include,lib64} $prefix/cuda/
+
 # Apply -lrt patch
 atomic_patch -p0 $WORKSPACE/srcdir/patches/link_against_librt.patch
-
 chmod +x $WORKSPACE/srcdir/bazel-*
-$WORKSPACE/srcdir/bazel-* --output_user_root=/workspace/bazel_root build -c opt --verbose_failures //tensorflow/compiler/xrt/utils:xrt_server
+mv $WORKSPACE/srcdir/bazel-* /usr/local/bin/bazel
+
+# Configure to enable CUDA and XLA
+export TF_NEED_CUDA=1
+export CUDA_TOOLKIT_PATH=$prefix/cuda
+export TF_ENABLE_XLA=1
+bazel --output_user_root=/workspace/bazel_root build -c opt --verbose_failures //tensorflow/compiler/xrt/utils:xrt_server
 
 # Install to $prefix/bin
 mkdir -p $prefix/{bin,lib}
