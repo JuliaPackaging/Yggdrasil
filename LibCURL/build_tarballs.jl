c92a0e914d1141ce7f4d9bf4423f696dcec297b8
Elliot Saba <staticfloat@gmail.com>
2019-01-08 20:36:28 +0000

    Better `LibCURL` release

--- LibCURL/build_tarballs.jl
+++ LibCURL/build_tarballs.jl
@@ -16,35 +16,14 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/curl-7.61.0
 ./configure --prefix=$prefix --host=$target --with-mbedtls --disable-manual --without-ssl
-if [[ $target == *-w64-mingw32 ]]; then
-    LDFLAGS="-L$prefix/bin"
-elif [[ $target == x86_64-apple-darwin14 ]]; then
-    LDFLAGS="-L$prefix/lib -Wl,-rpath,$prefix/lib"
-else
-    LDFLAGS="-L$prefix/lib -Wl,-rpath-link,$prefix/lib"
-fi
-make -j${nproc} LDFLAGS="$LDFLAGS"
+make -j${nproc}
 make install
 
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, :glibc),
-    Linux(:x86_64, :glibc),
-    Linux(:aarch64, :glibc),
-    Linux(:armv7l, :glibc, :eabihf),
-    Linux(:powerpc64le, :glibc),
-    Linux(:i686, :musl),
-    Linux(:x86_64, :musl),
-    Linux(:aarch64, :musl),
-    Linux(:armv7l, :musl, :eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64),
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -55,7 +34,7 @@ products(prefix) = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.1/build_Zlib.v1.2.11.jl",
-    "https://github.com/JuliaWeb/MbedTLSBuilder/releases/download/v0.11/build_MbedTLS.v1.0.0.jl"
+    "https://github.com/JuliaWeb/MbedTLSBuilder/releases/download/v0.16.0/build_MbedTLS.v2.13.1.jl",
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
