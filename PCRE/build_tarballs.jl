0d5af60bacac9f71f8c1e4f531f58cd5e4c77ac3
Elliot Saba <staticfloat@gmail.com>
2019-01-09 16:50:51 -0500

    Add PCRE 8.42

---
+++ PCRE/build_tarballs.jl
@@ -0,0 +1,35 @@
+using BinaryBuilder
+
+name = "PCRE"
+version = v"8.42"
+
+# Collection of sources required to build Pcre
+sources = [
+    "https://ftp.pcre.org/pub/pcre/pcre-$(version.major).$(version.minor).tar.bz2" =>
+    "2cd04b7c887808be030254e8d77de11d3fe9d4505c39d4b15d2664ffe8bf9301",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/pcre-*/
+./configure --prefix=$prefix --host=$target
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libpcre", :libpcre)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
