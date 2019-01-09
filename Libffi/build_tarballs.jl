44db1d256e0768dca2f34d73a48374d395d76988
Elliot Saba <staticfloat@gmail.com>
2019-01-09 17:57:26 -0500

    Add `Libffi v3.2.1`

---
+++ Libffi/build_tarballs.jl
@@ -0,0 +1,35 @@
+using BinaryBuilder
+
+name = "Libffi"
+version = v"3.2.1"
+
+# Collection of sources required to build libffi
+sources = [
+    "https://sourceware.org/pub/libffi/libffi-$(version).tar.gz" =>
+    "d06ebb8e1d9a22d19e38d63fdb83954253f39bedc5d46232a05645685722ca37",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/libffi-*/
+./configure --prefix=$prefix --host=$target
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libffi", :libffi)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
