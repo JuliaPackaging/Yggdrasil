9f309cecb94d8b72bce35ec0ef9cc9b10f288d92
Elliot Saba <staticfloat@gmail.com>
2019-02-07 22:31:26 -0500

    Expand GCC versions for SuiteSparse

--- SuiteSparse/build_tarballs.jl
+++ SuiteSparse/build_tarballs.jl
@@ -64,7 +64,7 @@ make "${FLAGS[@]}" install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = expand_gcc_versions(supported_platforms())
 
 # The products that we will ensure are always built
 products(prefix) = [
