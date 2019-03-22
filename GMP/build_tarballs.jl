58ed8ebd32be076a17efa5a4cd04bb0dc0143834
Elliot Saba <staticfloat@gmail.com>
2019-03-21 20:27:55 -0400

    Add `GMP v6.1.2`, now compiling for FreeBSD!

---
+++ GMP/build_tarballs.jl
@@ -0,0 +1,48 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+# Collection of sources required to build GMPBuilder
+name = "GMP"
+version = v"6.1.2"
+sources = [
+    "https://gmplib.org/download/gmp/gmp-6.1.2.tar.bz2" =>
+    "5275bb04f4863a13516b2f39392ac5e272f5e1bb8057b18aec1c9b79d73d8fb2",
+    "./bundled",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/gmp-*
+
+# Patch `configure` to include `$LDFLAGS` in its tests.  This is necessary on FreeBSD.
+atomic_patch -p1 $WORKSPACE/srcdir/patches/configure.patch
+
+flags=(--enable-cxx --enable-shared --disable-static)
+
+# On x86_64 architectures, build fat binary
+if [[ ${proc_family} == intel ]]; then
+    flags+=("--enable-fat")
+fi
+
+./configure --prefix=$prefix --host=$target ${flags[@]}
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products(prefix) = [
+    LibraryProduct(prefix, "libgmp", :libgmp)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
