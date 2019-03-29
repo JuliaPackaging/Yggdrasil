e83d2c7efbd9e397c5051ccb433c001892b9a15d
Elliot Saba <staticfloat@gmail.com>
2019-03-28 21:58:50 -0400

    Eliminate `zlib` from LibGit2 dependency

--- LibGit2/build_tarballs.jl
+++ LibGit2/build_tarballs.jl
@@ -54,10 +54,9 @@ products = prefix -> [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.4/build_Zlib.v1.2.11.jl",
     "https://github.com/JuliaWeb/MbedTLSBuilder/releases/download/v0.16.0/build_MbedTLS.v2.13.1.jl",
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/LibSSH2-v1.8.0-0/build_LibSSH2.v1.8.0.jl",
-    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/LibCURL-v7.61.0-0/build_LibCURL.v7.61.0.jl",
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/LibCURL-v7.61.0-1/build_LibCURL.v7.61.0.jl",
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
