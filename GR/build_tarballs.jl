9f619c4ea1701cdfba535dd4a286f1d6cf7398f9
Elliot Saba <staticfloat@gmail.com>
2019-04-24 20:19:36 -0400

    Add `GR` repackaging script

---
+++ GR/build_tarballs.jl
@@ -0,0 +1,35 @@
+using BinaryBuilder
+
+verbose = "--verbose" in ARGS
+
+# GR already contains a bunch of tarballs for us, so we basically just download
+# and rename them for our own nefarious purposes.
+
+name = "GR"
+version = v"0.39.0"
+name_mapping = Dict(
+    Linux(:x86_64, libc=:glibc) => ("Linux-x86_64", "cb5e1eb4bc94ccf7ddef268679cd4d0853a1e8ecb2f21c2a9025a14f32bfbd79"),
+    Linux(:i686, libc=:glibc) => ("Linux-i386", "fb3fbdb19061224b14fb59a864ddfb5b34744dc77c0d7a6574b3de677c680641"),
+    Windows(:x86_64) => ("Windows-x86_64", "da0ec3e26855de4c05043bd1c49445ddafeba837113580140f25613e25150d9f"),
+    Windows(:i686) => ("Windows-i686", "9e927fcedb784eb60ca3ed682bb056c59b52526d7cfa7cfd1d4e1978d415acc7"),
+    MacOS(:x86_64) => ("Darwin-x86_64", "bf871c66fa3445b49771af803066d9f5cab175b3aa837714375c4b8ef2c067b8"),
+)
+
+# Downoad, unpack, extract, then repackage each of these guys
+mkpath(joinpath(@__DIR__, "build"))
+mkpath(joinpath(@__DIR__, "products"))
+product_hashes = Dict()
+for (platform, (suffix, hash)) in name_mapping
+    extract_dir = joinpath(@__DIR__, "build", triplet(platform))
+    tarball_path = joinpath(@__DIR__, "build", "$(name)-v$(version)-$(triplet(platform)).tar.gz")
+    BinaryBuilder.download_verify_unpack("https://github.com/sciapp/gr/releases/download/v$(version)/gr-$(version)-$(suffix).tar.gz", hash, extract_dir; tarball_path=tarball_path, ignore_existence=true, force=true, verbose=verbose)
+    product_hashes[platform] = BinaryBuilder.package(Prefix(joinpath(extract_dir, "gr")), joinpath(@__DIR__, "products", name), version; platform=platform, verbose=verbose, force=true)
+end
+
+products = [
+    LibraryProduct(Prefix(pwd()), "libGR", :libGR),
+    LibraryProduct(Prefix(pwd()), "libGR3", :libGR3),
+]
+
+bin_path = "https://github.com/$(BinaryBuilder.get_repo_name())/releases/download/$(BinaryBuilder.get_tag_name())"
+BinaryBuilder.print_buildjl(joinpath(@__DIR__, "products"), name, version, products, product_hashes, bin_path)
