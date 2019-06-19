28342e2eabf38f935fecbc69d3938a6bc75ddc14
Elliot Saba <staticfloat@gmail.com>
2019-06-18 23:38:48 -0700

    Bump `hwloc` to `v2.0.4`

--- Hwloc/build_tarballs.jl
+++ Hwloc/build_tarballs.jl
@@ -3,42 +3,25 @@
 using BinaryBuilder
 
 name = "hwloc"
-version = v"2.0.3"
+version = v"2.0.4"
 
 # Collection of sources required to build hwloc
 sources = [
-    "https://download.open-mpi.org/release/hwloc/v2.0/hwloc-2.0.3.tar.bz2" =>
-    "e393aaf39e576b329a2bff3096d9618d4e39f416874390b58e6573349554c725",
-
+    "https://download.open-mpi.org/release/hwloc/v2.0/hwloc-$(version).tar.bz2" =>
+    "653c05742dff16e5ee6ad3343fd40e93be8ba887eaffbd539832b68780d047a9",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd hwloc-2.0.3/
+cd $WORKSPACE/srcdir/hwloc-*
 ./configure --prefix=$prefix --host=$target
 make
 make install
-
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
@@ -47,7 +30,6 @@ products(prefix) = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
