b7721a906ea1bc3d8910d945fed39a5d6c3acffd
Elliot Saba <staticfloat@gmail.com>
2019-08-07 18:46:44 -0700

    Allow CUDA to be built for the big three

--- CUDA/build_tarballs.jl
+++ CUDA/build_tarballs.jl
@@ -8,12 +8,14 @@ sources = [
     "ee395516e85185b47fac340d452d28e107dd18bf36e5af35e8f39ab2a9893f3b",
     "https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.168_mac.dmg" =>
     "a53d17c92b81bb8b8f812d0886a8c2ddf2730be6f5f2659aee11c0da207c2331",
-    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/dmg2img-v1.6.7%2B0/dmg2img.v1.6.7.x86_64-linux-gnu.tar.gz" =>
-    "2157b87982fa45e68f9413875ba41eb6193526c2d1cdf3e389500d888b18a01c",
+    "https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.168_425.25_win10.exe" =>
+    "52450b81a699cb75086e9d3d62abb2a33f823fcf5395444e57ebb5864cc2fd51",
 ]
 
 script = raw"""
 cd ${WORKSPACE}/srcdir
+
+apk add p7zip
 
 # Make temporary space for extraction
 mkdir -p $(pwd)/.tmp
@@ -27,22 +29,46 @@ if [[ ${target} == x86_64-linux-gnu ]]; then
         --no-man-page \
         --tmpdir=$(pwd)/.tmp \
         --no-drm
-elif [[ ${target} == x86_64-apple-darwin* ]]; then
-    dmg2img cuda_*_mac.dmg cuda_mac_converted.img
-fi
+    
+    # Clean up things we don't care about
+    rm -rf ${prefix}/NsightCompute*
+    rm -rf ${prefix}/nsightee*
+    rm -rf ${prefix}/doc
+    rm -rf ${prefix}/samples
+    rm -rf ${prefix}/libnvvp
+    rm -rf ${prefix}/libnsight
+    rm -rf ${prefix}/jre
+elif [[ ${target} == x86_64-w64-mingw32 ]]; then
+    cd .tmp
+    7z x ${WORKSPACE}/srcdir/cuda_*_win10.exe
 
-# Clean up things we don't care about
-rm -rf ${prefix}/NsightCompute*
-rm -rf ${prefix}/nsightee*
-rm -rf ${prefix}/doc
-rm -rf ${prefix}/samples
-rm -rf ${prefix}/libnvvp
-rm -rf ${prefix}/libnsight
-rm -rf ${prefix}/jre
+    # Install things
+    mkdir -p ${prefix}/bin
+    for project in curand cusparse npp cufft cublas cudart cusolver nvrtc; do
+        mv ${project}/bin/* ${prefix}/bin
+    done
+elif [[ ${target} == x86_64-apple-darwin* ]]; then
+    cd .tmp
+    7z x ${WORKSPACE}/srcdir/cuda_*_mac.dmg
+    7z x 5.hfs
+
+    # Extract embedded tarball into prefix
+    tar -C ${prefix} -zxvf CUDAMacOSXInstaller/CUDAMacOSXInstaller.app/Contents/Resources/payload/cuda_mac_installer_tk.tar.gz
+
+    # Clean up things we don't care about
+    rm -rf ${prefix}/usr
+
+    for subdir in bin lib include extras nvvm; do
+        mv ${prefix}/Developer/NVIDIA/CUDA-*/${subdir} ${prefix}/
+    done
+    rm -rf ${prefix}/Developer
+fi
 """
 
 platforms = [
     Linux(:x86_64),
+    Windows(:x86_64),
+    MacOS(:x86_64),
 ]
 
 products(prefix) = [
