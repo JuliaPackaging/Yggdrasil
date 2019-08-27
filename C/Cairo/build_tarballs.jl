eefd9238db11fe58849422d5ee3e5adce3eb3db9
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-08-27 17:27:05 +0200

    [Cairo] Enable freetype2 and start supporting native backends

--- C/Cairo/build_tarballs.jl
+++ C/Cairo/build_tarballs.jl
@@ -7,12 +7,35 @@ version = v"1.14.12"
 sources = [
     "https://www.cairographics.org/releases/cairo-1.14.12.tar.xz" =>
     "8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16",
+    "./bundled"
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/cairo-*/
-LDFLAGS="-L$prefix/lib -L/opt/${target}/${target}/lib" CPPFLAGS="-I$prefix/include" ./configure --prefix=$prefix --host=$target --disable-static --disable-xlib --disable-ft --disable-dependency-tracking
+
+LDFLAGS="${LDFLAGS} -L${prefix}/lib"
+# TODO: find out why we need to manually add "${prefix}/include/freetype2",
+# the pkg-config file for freetype2 should already have this information.
+CFLAGS="-I${prefix}/include -I${prefix}/include/freetype2"
+
+if [[ "${target}" == *-apple-* ]]; then
+    BACKEND_OPTIONS="--disable-xlib --enable-quartz"
+elif [[ "${target}" == *-mingw* ]]; then
+    BACKEND_OPTIONS="--disable-xlib --enable-win32"
+else
+    BACKEND_OPTIONS="--disable-xlib"
+fi
+
+# There is a mess with the version of freetype2.  The following patch changes
+# the required version to the one that is actually found by pkg-config.
+atomic_patch -p1 ../patches/configure.ac.patch
+./autogen.sh --prefix=${prefix} --host=${target}
+./configure --prefix=${prefix} --host=${target} \
+    --disable-static \
+    --enable-ft \
+    --disable-dependency-tracking \
+    ${BACKEND_OPTIONS}
 make -j${nproc}
 make install
 """
@@ -31,7 +54,8 @@ dependencies = [
     "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.4/build_Zlib.v1.2.11.jl",
     "https://github.com/JuliaIO/LibpngBuilder/releases/download/v1.0.3/build_libpng.v1.6.37.jl",
     "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Pixman-v0.36.0-0/build_Pixman.v0.36.0.jl",
-    "https://github.com/JuliaGraphics/FreeTypeBuilder/releases/download/v2.9.1-4/build_FreeType2.v2.10.0.jl"
+    "https://github.com/JuliaGraphics/FreeTypeBuilder/releases/download/v2.9.1-4/build_FreeType2.v2.10.0.jl",
+    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/Bzip2-v1.0.6-2/build_Bzip2.v1.0.6.jl",
 ]
 
 
