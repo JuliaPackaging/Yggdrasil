b394928d031644daa355c3f564c1b32370cea62c
Elliot Saba <staticfloat@gmail.com>
2019-08-31 22:52:53 -0700

    Add `Libpng` from https://github.com/JuliaIO/LibpngBuilder

---
+++ L/Libpng/build_tarballs.jl
@@ -0,0 +1,39 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "libpng"
+version = v"1.6.37"
+
+# Collection of sources required to build libpng
+sources = [
+    "https://vorboss.dl.sourceforge.net/project/libpng/libpng16/$(version)/libpng-$(version).tar.gz" =>
+    "daeb2620d829575513e35fecc83f0d3791a620b9b93d800b763542ece9390fb4",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/libpng-*/
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain ..
+make -j${ncore}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libpng16", :libpng)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "Zlib_jll",
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
