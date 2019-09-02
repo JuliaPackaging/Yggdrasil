6eea17865336cb3eaee5d3609b16a1457bf72c21
Elliot Saba <staticfloat@gmail.com>
2019-09-02 14:34:42 -0700

    Upgrade `Libffi`

--- L/Libffi/build_tarballs.jl
+++ L/Libffi/build_tarballs.jl
@@ -28,8 +28,8 @@ fi
 platforms = supported_platforms()
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libffi", :libffi)
+products = [
+    LibraryProduct("libffi", :libffi)
 ]
 
 # Dependencies that must be installed before this package can be built
