0295f0921648dd803b9e5eb7ecf204a6bf2e3b7e
Elliot Saba <staticfloat@gmail.com>
2019-09-04 17:00:13 -0700

    Don't create a `Libuuid` tarball for `macOS` either

--- L/Libuuid/build_tarballs.jl
+++ L/Libuuid/build_tarballs.jl
@@ -21,7 +21,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [p for p in supported_platforms() if !(p isa Windows)]
+platforms = [p for p in supported_platforms() if !(p isa Union{Windows,MacOS})]
 
 # The products that we will ensure are always built
 products = [
