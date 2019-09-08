56eb078a25e41b625d2a09ccdfe79fe7aa26232f
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-09-08 23:12:25 +0100

    Add Libtiff builder

---
+++ L/Libtiff/build_tarballs.jl
@@ -0,0 +1,45 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "Libtiff"
+version = v"4.0.10"
+
+# Collection of sources required to build Libtiff
+sources = [
+    "https://download.osgeo.org/libtiff/tiff-$(version).tar.gz" =>
+    "2c52d11ccaf767457db0c46795d9c7d1a8d8f76f68b0b800a3dfe45786b996e4"
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/tiff-*/
+CPPFLAGS="-I${prefix}/include"
+
+if [[ "${target}" == *-apple-* ]]; then
+    AR=/opt/${target}/bin/${target}-ar
+fi
+
+./configure --prefix=$prefix --host=$target
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libtiff", :libtiff)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "JpegTurbo_jl",
+    "Zlib_jl",
+    "Zstd_jll",
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
