debb23ac92e202835e7d7fa4c0019252ae030e59
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-09-10 23:20:27 +0100

    Add builder for hicolor-icon-theme

---
+++ H/hicolor_icon_theme/build_tarballs.jl
@@ -0,0 +1,35 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "hicolor_icon_theme"
+version = v"0.17"
+
+# Collection of sources required to build hicolor_icon_theme
+sources = [
+    "https://icon-theme.freedesktop.org/releases/hicolor-icon-theme-$(version.major).$(version.minor).tar.xz" =>
+    "317484352271d18cbbcfac3868eab798d67fff1b8402e740baa6ff41d588a9d8"
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/hicolor-icon-theme-*/
+./configure --prefix=$prefix --host=$target
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    FileProduct("share/icons/hicolor/index.theme", :hicolor_icon_theme)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
