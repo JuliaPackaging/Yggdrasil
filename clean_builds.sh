a6ac55d8948a363f64009014abb79c959a63c61d
Elliot Saba <staticfloat@gmail.com>
2019-09-10 02:01:28 -0700

    Unmount before cleaning, to dodge gazillions of error messages

--- clean_builds.sh
+++ clean_builds.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
+# First, unmount everything
+for mount_dir in $(find . -type d -name mounts); do
+    echo "Unmounting ${mount_dir}/*"
+    sudo umount ${mount_dir}/*
+done
+
 for parent in *; do
     if [[ ! -d "${parent}" ]]; then
         continue
