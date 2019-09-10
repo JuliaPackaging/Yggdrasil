a9a3b0763d964dee471d088ed4b627f7a6c0f841
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-09-11 00:32:30 +0100

    Add builder for libmount

---
+++ L/Libmount/build_tarballs.jl
@@ -0,0 +1,36 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg.BinaryPlatforms
+
+name = "Libmount"
+version = v"2.34"
+
+# Collection of sources required to build FriBidi
+sources = [
+    "https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v$(version.major).$(version.minor)/util-linux-$(version.major).$(version.minor).tar.xz" =>
+    "743f9d0c7252b6db246b659c1e1ce0bd45d8d4508b4dfa427bbb4a3e9b9f62b5"
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/util-linux-*/
+./configure --prefix=$prefix --host=$target --disable-all-programs --enable-libblkid --enable-libmount
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [p for p in supported_platforms() if !(p isa Union{FreeBSD,Windows,MacOS})]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libmount", :libmount)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8")
