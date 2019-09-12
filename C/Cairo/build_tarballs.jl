4e51cfe462d371e9e1367b80f89834590169d914
Mosè Giordano <giordano@users.noreply.github.com>
2019-09-13 00:33:16 +0100

    [Cairo] Enable gobject (#74)

--- C/Cairo/build_tarballs.jl
+++ C/Cairo/build_tarballs.jl
@@ -5,7 +5,7 @@ name = "Cairo"
 version = v"1.14.12"
 
 sources = [
-    "https://www.cairographics.org/releases/cairo-1.14.12.tar.xz" =>
+    "https://www.cairographics.org/releases/cairo-$(version).tar.xz" =>
     "8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16",
     "./bundled"
 ]
@@ -17,7 +17,7 @@ cd $WORKSPACE/srcdir/cairo-*/
 export LDFLAGS="${LDFLAGS} -L${libdir}"
 # TODO: find out why we need to manually add "${prefix}/include/freetype2",
 # the pkg-config file for freetype2 should already have this information.
-export CFLAGS="-I${prefix}/include -I${prefix}/include/freetype2"
+export CPPFLAGS="-I${prefix}/include -I${prefix}/include/freetype2"
 
 if [[ "${target}" == *-apple-* ]]; then
     BACKEND_OPTIONS="--enable-quartz --enable-quartz-image --disable-xcb --disable-xlib"
@@ -35,6 +35,10 @@ atomic_patch -p1 ../patches/configure.ac.patch
     --disable-static \
     --enable-ft \
     --enable-tee \
+    --enable-svg \
+    --enable-ps \
+    --enable-pdf \
+    --enable-gobject \
     --disable-dependency-tracking \
     ${BACKEND_OPTIONS}
 make -j${nproc}
@@ -48,10 +52,12 @@ platforms = supported_platforms()
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libcairo", :libcairo),
+    LibraryProduct("libcairo-gobject", :libcairo_gobject),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    "Glib_jll",
     "Pixman_jll",
     "libpng_jll",
     "Fontconfig_jll",
@@ -64,4 +70,4 @@ dependencies = [
 
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8")
