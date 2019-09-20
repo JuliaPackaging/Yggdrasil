57537efcde2d5b9506f7447c68d9fbd434c41cc0
Mosè Giordano <giordano@users.noreply.github.com>
2019-09-20 04:26:39 +0100

    [Cairo] Remove patch and various hacks not needed anymore (#89)

--- C/Cairo/build_tarballs.jl
+++ C/Cairo/build_tarballs.jl
@@ -7,17 +7,11 @@ version = v"1.14.12"
 sources = [
     "https://www.cairographics.org/releases/cairo-$(version).tar.xz" =>
     "8c90f00c500b2299c0a323dd9beead2a00353752b2092ead558139bd67f7bf16",
-    "./bundled"
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/cairo-*/
-
-export LDFLAGS="${LDFLAGS} -L${libdir}"
-# TODO: find out why we need to manually add "${prefix}/include/freetype2",
-# the pkg-config file for freetype2 should already have this information.
-export CPPFLAGS="-I${prefix}/include -I${prefix}/include/freetype2"
 
 if [[ "${target}" == *-apple-* ]]; then
     BACKEND_OPTIONS="--enable-quartz --enable-quartz-image --disable-xcb --disable-xlib"
@@ -27,10 +21,6 @@ elif [[ "${target}" == *-linux-* ]] || [[ "${target}" == *freebsd* ]]; then
     BACKEND_OPTIONS="--enable-xlib --enable-xcb --enable-xlib-xcb"
 fi
 
-# There is a mess with the version of freetype2.  The following patch changes
-# the required version to the one that is actually found by pkg-config.
-atomic_patch -p1 ../patches/configure.ac.patch
-./autogen.sh --prefix=${prefix} --host=${target}
 ./configure --prefix=${prefix} --host=${target} \
     --disable-static \
     --enable-ft \
