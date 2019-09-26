2198289360408a6ee5c1af9a62bf547d9f036d16
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-09-26 19:16:03 +0100

    [LibCURL] Update for new BinaryBuilder and upgrade to CURL v7.66.0

--- L/LibCURL/build_tarballs.jl
+++ L/LibCURL/build_tarballs.jl
@@ -3,13 +3,12 @@
 using BinaryBuilder
 
 name = "LibCURL"
-version = v"7.61.0"
+version = v"7.66.0"
 
 # Collection of sources required to build LibCURL
 sources = [
-    "https://curl.haxx.se/download/curl-7.61.0.tar.gz" =>
-    "64141f0db4945268a21b490d58806b97c615d3d0c75bf8c335bbe0efd13b45b5",
-
+    "https://curl.haxx.se/download/curl-$(version).tar.bz2" =>
+    "6618234e0235c420a21f4cb4c2dd0badde76e6139668739085a70c4e2fe7a141",
 ]
 
 # Bash recipe for building across all platforms
@@ -19,14 +18,15 @@ cd $WORKSPACE/srcdir/curl-*
 # Holy crow we really configure the bitlets out of this thing
 FLAGS=(
     # Disable....almost everything
-    --without-ssl --without-gnutls --without-gssapi --without-zlib
+    --without-ssl --without-gnutls --without-gssapi
     --without-libidn --without-libidn2 --without-libmetalink --without-librtmp
     --without-nghttp2 --without-nss --without-polarssl
     --without-spnego --without-libpsl --disable-ares --disable-manual
     --disable-ldap --disable-ldaps --without-zsh-functions-dir
+    --disable-static
 
     # Two things we actually enable
-    --with-libssh2=${prefix} --with-mbedtls=${prefix}
+    --with-libssh2=${prefix} --with-mbedtls=${prefix} --with-zlib=${prefix}
 )
 
 # We need to tell it where to find libssh2 on windows
@@ -34,7 +34,7 @@ if [[ ${target} == *mingw* ]]; then
     FLAGS+=(LDFLAGS="${LDFLAGS} -L${prefix}/bin")
 fi
 
-./configure --prefix=$prefix --host=$target "${FLAGS[@]}"
+./configure --prefix=$prefix --host=$target --build=${MACHTYPE} "${FLAGS[@]}"
 make -j${nproc}
 make install
 """
@@ -44,17 +44,17 @@ make install
 platforms = supported_platforms()
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libcurl", :libcurl),
-    ExecutableProduct(prefix, "curl", :curl)
+products = [
+    LibraryProduct("libcurl", :libcurl),
+    ExecutableProduct("curl", :curl)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/JuliaWeb/MbedTLSBuilder/releases/download/v0.16.0/build_MbedTLS.v2.13.1.jl",
-    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/LibSSH2-v1.9.0+0/build_LibSSH2.v1.9.0.jl",
+    "LibSSH2_jll",
+    "MbedTLS_jll",
+    "Zlib_jll",
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
