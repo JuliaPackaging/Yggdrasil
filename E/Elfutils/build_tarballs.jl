6dcbfc4e72c8653c43fafc764706d14b5a057847
Elliot Saba <staticfloat@gmail.com>
2019-09-30 00:42:57 -0400

    Disable Elfutils on non-glibc Linux

--- E/Elfutils/build_tarballs.jl
+++ E/Elfutils/build_tarballs.jl
@@ -14,14 +14,16 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/elfutils-*/
+export CC=gcc
+export CXX=g++
 CFLAGS="-Wno-error=unused-result" CPPFLAGS="-I${prefix}/include" ./configure --prefix=${prefix} --host=${target}
 make -j${nproc}
 make install
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = [p for p in supported_platforms() if p isa Union{Linux,FreeBSD}]
+# Only build for Linux, and disable musl/FreeBSD until we have a custom libargp, perhaps
+# the https://github.com/xhebox/libuargp which is known to be musl-friendly.
+platforms = [p for p in supported_platforms() if p isa Linux && libc(p) != :musl]
 
 # The products that we will ensure are always built
 products = [
