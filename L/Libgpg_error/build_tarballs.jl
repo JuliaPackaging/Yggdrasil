a883e6715c874a44a7a8163b3c585a2557b6f2d9
Elliot Saba <staticfloat@gmail.com>
2019-10-02 16:35:41 -0400

    Upgrade libgpg-error to build on all platforms

--- L/Libgpg_error/build_tarballs.jl
+++ L/Libgpg_error/build_tarballs.jl
@@ -9,19 +9,31 @@ version = v"1.36"
 sources = [
     "https://gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-$(version.major).$(version.minor).tar.bz2" =>
     "babd98437208c163175c29453f8681094bcaf92968a15cafb1a276076b33c97c",
+    "./bundled",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libgpg-error-*/
 
+for p in ${WORKSPACE}/srcdir/patches/*; do
+    atomic_patch -p1 "${p}"
+done
+
+# "Generate" new arch platform definitions because they're actually really simple. :P
+cp -iv src/syscfg/lock-obj-pub.i686-unknown-linux-gnu.h src/syscfg/lock-obj-pub.i686-unknown-linux-musl.h
+cp -iv src/syscfg/lock-obj-pub.aarch64-unknown-linux-gnu.h src/syscfg/lock-obj-pub.aarch64-unknown-linux-musl.h
+cp -iv src/syscfg/lock-obj-pub.arm-unknown-linux-gnueabi.h src/syscfg/lock-obj-pub.arm-unknown-linux-musleabihf.h
+cp -iv src/syscfg/lock-obj-pub.x86_64-unknown-linux-gnu.h src/syscfg/lock-obj-pub.freebsd11.1.h
+
 # Use libgpg-specific mapping for triplets
 TARGET="${target}"
 if [[ "${target}" == x86_64-apple-darwin* ]]; then
     TARGET=x86_64-apple-darwin
-elif [[ "${target}" == x86_64-*-freebsd* ]]; then
-    TARGET=x86_64-unknown-kfreebsd-gnu
 fi
+
+# We need `msgformat`
+apk add gettext
 
 ./configure --prefix=${prefix} --host=${TARGET} --build=${MACHTYPE}
 make -j${nproc}
@@ -29,24 +41,13 @@ make install
 """
 
 # These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    # Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    # Linux(:aarch64, libc=:musl),
-    # Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-]
+# platforms are passed in on the command line.  We are manually disabling
+# many platforms that do not seem to work.
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libgpg-error", :libgpg_error),
+    LibraryProduct(["libgpg-error", "libgpg-error6"], :libgpg_error),
 ]
 
 # Dependencies that must be installed before this package can be built
