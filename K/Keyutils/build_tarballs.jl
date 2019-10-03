4d81030298ed4d5126127417e140dc3f78bc51ac
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-10-03 01:21:28 +0100

    Add builder for keyutils

---
+++ K/Keyutils/build_tarballs.jl
@@ -0,0 +1,44 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "Keyutils"
+version = v"1.6.1"
+
+# Collection of sources required to build keyutils
+sources = [
+    "https://git.kernel.org/pub/scm/linux/kernel/git/dhowells/keyutils.git/snapshot/keyutils-$(version).tar.gz" =>
+    "3c71dcfc6900d07b02f4e061d8fb218a4ae6519c1d283d6a57b8e27718e2f557",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/keyutils-*/
+make -j${nproc}
+make install \
+    DESTDIR="${prefix}/" \
+    SBINDIR="bin" \
+    BINDIR="bin" \
+    LIBDIR="lib" \
+    USRLIBDIR="lib" \
+    INCLUDEDIR="include" \
+    MANDIR="share/man" \
+    SHAREDIR="share/keyutils"
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line.  We are manually disabling
+# many platforms that do not seem to work.
+platforms = [p for p in supported_platforms() if p isa Linux]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libkeyutils", :libkeyutils),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
