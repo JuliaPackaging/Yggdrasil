7e6773954bfe4dd8fd0634868cec4050d2dd0826
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-10-05 22:39:59 +0100

    [CFITSIO] Build with LibCURL

--- C/CFITSIO/build_tarballs.jl
+++ C/CFITSIO/build_tarballs.jl
@@ -2,25 +2,36 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "CFITSIOBuilder"
+name = "CFITSIO"
 version = v"3.47.0"
 
 # Collection of sources required to build CFITSIO
 sources = [
-    "http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-3.47.tar.gz" =>
+    "http://heasarc.gsfc.nasa.gov/FTP/software/fitsio/c/cfitsio-$(version.major).$(version.minor).tar.gz" =>
     "418516f10ee1e0f1b520926eeca6b77ce639bed88804c7c545e74f26b3edf4ef",
+    "./bundled",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/cfitsio*
-if [[ "${target}" == *freebsd* ]]; then
-    CC=/opt/${target}/bin/${target}-gcc
-    LD=/opt/${target}/bin/${target}-ld
+atomic_patch -p1 ../patches/configure_in.patch
+atomic_patch -p1 ../patches/Makefile_in.patch
+autoreconf
+if [[ "${target}" == *-mingw* ]]; then
+    # This is ridiculous: when CURL is enabled, CFITSIO defines a macro,
+    # `TBYTE`, that has the same name as a mingw macro.  The following patch
+    # renames `TBYTE` to `_TBYTE`.
+    atomic_patch -p1 ../patches/tbyte.patch
 fi
 ./configure --prefix=$prefix --host=$target --enable-reentrant
 make -j${nproc} shared
 make install
+# On Windows platforms, we need to move our .dll files to bin
+if [[ "${target}" == *-mingw* ]]; then
+    mkdir -p ${prefix}/bin
+    mv ${prefix}/lib/*.dll ${prefix}/bin
+fi
 """
 
 # These are the platforms we will build for by default, unless further
@@ -28,13 +39,13 @@ make install
 platforms = supported_platforms()
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libcfitsio", :libcfitsio)
+products = [
+    LibraryProduct("libcfitsio", :libcfitsio)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-
+    "LibCURL_jll",
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
