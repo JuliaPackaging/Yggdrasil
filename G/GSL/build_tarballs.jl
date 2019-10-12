d387a1dee2e7c3c49f35e4412fc78f14c9a6316a
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-10-13 00:02:08 +0100

    [GSL] Update to new BB and upgrade to version 2.6

--- G/GSL/build_tarballs.jl
+++ G/GSL/build_tarballs.jl
@@ -3,18 +3,25 @@
 using BinaryBuilder
 
 name = "GSL"
-version = v"2.5.0"
+version = v"2.6"
 
 # Collection of sources required to build GSL
 sources = [
-    "http://ftp.gnu.org/gnu/gsl/gsl-2.5.tar.gz" =>
-    "0460ad7c2542caaddc6729762952d345374784100223995eb14d614861f2258d",
+    "http://ftp.gnu.org/gnu/gsl/gsl-$(version.major).$(version.minor).tar.gz" =>
+    "b782339fc7a38fe17689cb39966c4d821236c28018b6593ddb6fd59ee40786a8",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-AR=/opt/${target}/bin/ar
 cd $WORKSPACE/srcdir/gsl-*/
+
+# We need to massage configure script to convince it to build the shared library
+# for PowerPC.
+if [[ "${target}" == powerpc64le-* ]]; then
+    autoreconf -vi
+fi
+
+update_configure_scripts
 ./configure --prefix=$prefix --host=${target}
 make -j${nproc}
 make install
@@ -25,13 +32,12 @@ make install
 platforms = supported_platforms()
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libgsl", :libgsl)
+products = [
+    LibraryProduct("libgsl", :libgsl)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
