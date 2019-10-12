5646038a0608cd8b9d19a060f88b6b48b1182ee0
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-10-12 16:45:15 +0100

    [GLFW] Update builder to new BinaryBuilder

--- G/GLFW/build_tarballs.jl
+++ G/GLFW/build_tarballs.jl
@@ -1,22 +1,26 @@
 using BinaryBuilder
 
+name = "GLFW"
 version = v"3.3"
 
 # Collection of sources required to build glfw
 sources = [
-    "https://github.com/glfw/glfw.git" =>
-    "b0796109629931b6fa6e449c15a177845256a407",
-
+    "https://github.com/glfw/glfw/releases/download/$(version.major).$(version.minor)/glfw-$(version.major).$(version.minor).zip" =>
+    "36fda4cb173e3eb2928c976b0e9b5014e2e5d12b9b787efa0aa29ffc41c37c4a",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd glfw/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DBUILD_SHARED_LIBS=ON -DGLFW_BUILD_EXAMPLES=false -DGLFW_BUILD_TESTS=false
-make -j${ncore}
+cd $WORKSPACE/srcdir/glfw-*/
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" \
+    -DBUILD_SHARED_LIBS=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DGLFW_BUILD_EXAMPLES=false \
+    -DGLFW_BUILD_TESTS=false \
+    -DGLFW_BUILD_DOCS=OFF
+make -j${nproc}
 make install
-exit
 """
 
 # These are the platforms we will build for by default, unless further
@@ -28,12 +32,12 @@ platforms = [
 ]
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libglfw", :libglfw)
+products = [
+    LibraryProduct(["libglfw", "glfw3"], :libglfw)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = []
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "glfw", version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
