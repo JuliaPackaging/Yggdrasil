f4143ae7dd0efd665f3b10a74351d57f8001ae8d
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-10-12 16:50:58 +0100

    [GLFW] Support all other platforms
    Co-authored-by: Colin Summers <me@colinxsummers.com>

--- G/GLFW/build_tarballs.jl
+++ G/GLFW/build_tarballs.jl
@@ -25,11 +25,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    MacOS(:x86_64),
-    Windows(:x86_64),
-    Windows(:i686),
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
@@ -37,7 +33,10 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = []
+dependencies = [
+    "Libglvnd_jll",
+    "X11_jll",
+]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
