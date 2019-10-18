c2018399b4ccf4a2c61dac80f41010282c2b1214
Martijn Visser <mgvisser@gmail.com>
2019-10-18 22:47:24 +0200

    add GEOS builder
    Based on existing build script at https://github.com/JuliaGeo/GEOSBuilder, but upgraded to the latest GEOS release.
    
    Left out these lines for now, not sure if they are still needed:
    
    https://github.com/JuliaGeo/GEOSBuilder/blob/56e53d1eed1cea8659826652a54aeda5350c6e01/build_tarballs.jl#L13-L17

---
+++ G/GEOS/build_tarballs.jl
@@ -0,0 +1,37 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "GEOS"
+version = v"3.8.0"
+
+# Collection of sources required to build GEOS
+sources = [
+    "http://download.osgeo.org/geos/geos-$src_version.tar.bz2" =>
+    "99114c3dc95df31757f44d2afde73e61b9f742f0b683fd1894cbbee05dda62d5",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/geos-*/
+./configure --prefix=$prefix --host=$target
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = Product[
+    LibraryProduct("libgeos_c", :libgeos),
+    LibraryProduct("libgeos", :libgeos_cpp)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
