9781e14b09cbe4095656f52fc9d96b643f0edc79
haberdashpi <david.frank.little@gmail.com>
2019-10-24 16:32:25 -0400

    added untested FLAC binary

---
+++ F/FLAC/build_tarballs.jl
@@ -0,0 +1,41 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "FLAC"
+version = v"1.3.3"
+
+# Collection of sources required to build FLAC
+sources = [
+    "https://downloads.xiph.org/releases/flac/flac-1.3.3.tar.xz" =>
+    "213e82bd716c9de6db2f98bcadbc4c24c7e2efe8c75939a1a84e28539c4e1748",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/flac-1.3.3/
+./configure --prefix=$prefix --host=$target
+make
+make install
+
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libFLAC", :libflac),
+    ExecutableProduct("metaflac", :metaflac),
+    LibraryProduct("libFLAC++", :libflacpp),
+    ExecutableProduct("flac", :flac)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "Ogg_jll",
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
