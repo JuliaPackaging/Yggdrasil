dfc7a54a666d7fed54f21fcab9f16903c7eadb8c
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-10-26 23:53:15 +0100

    Add builder for Opus

---
+++ O/Opus/build_tarballs.jl
@@ -0,0 +1,36 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "Opus"
+version = v"1.3.1"
+
+# Collection of sources required to build Opus
+sources = [
+    "https://archive.mozilla.org/pub/opus/opus-$(version).tar.gz" =>
+    "65b58e1e25b2a114157014736a3d9dfeaad8d41be1c8179866f144a2fb44ff9d",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/opus-*/
+./configure --prefix=$prefix --host=$target --disable-static --enable-custom-modes
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libopus", :libopus),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
