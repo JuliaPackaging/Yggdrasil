c6ae8ee2e5d713acc381c1fb6b98b94f0c475eb5
Elliot Saba <staticfloat@gmail.com>
2019-10-27 22:36:13 -0400

    Upgrade `Geant4` builder
    * Use prebuilt `Expat_jll` as a dependency
    * Build for everything that isn't Windows
    * Use GCC 7 to dodge older GCC posix_memalign issues

--- G/Geant4/build_tarballs.jl
+++ G/Geant4/build_tarballs.jl
@@ -5,79 +5,63 @@ using BinaryBuilder
 name = "Geant4Builder"
 version = v"0.1.0"
 
-# Collection of sources required to build Geant4Builder
+# Collection of sources required to build
 sources = [
     "https://github.com/Geant4/geant4/archive/v10.5.1.tar.gz" =>
     "443efb0d16e8a5fd195176573d21d2e12415ae7853dd39cc0517171aea243227",
-
-    "https://github.com/libexpat/libexpat/releases/download/R_2_2_8/expat-2.2.8.tar.gz" =>
-    "bd507cba42716ca9afe46dd3687fb0d46c09347517beb9770f53a435d2c67ea0",
-
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd expat-2.2.8/
-./configure --prefix=$prefix --host=$target
-make
-make install
-cd ../geant4-10.5.1/
-mkdir build
-cd build/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain ..
+cd $WORKSPACE/srcdir/geant4-*/
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} ..
 make -j${nproc}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:x86_64, libc=:glibc),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64)
-]
+platforms = filter(p -> !isa(p, Windows), supported_platforms())
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libG4vis_management", :libG4visManagement),
-    LibraryProduct(prefix, "libexpat", :libexpat),
-    LibraryProduct(prefix, "libG4FR", :libG4FR),
-    LibraryProduct(prefix, "libG4event", :libG4Event),
-    LibraryProduct(prefix, "libG4analysis", :libG4Analysis),
-    LibraryProduct(prefix, "libG4digits_hits", :libG4Digits),
-    LibraryProduct(prefix, "libG4run", :libG4Run),
-    LibraryProduct(prefix, "libG4visXXX", :libG4VisXXX),
-    LibraryProduct(prefix, "libG4clhep", :libG4CLHEP),
-    LibraryProduct(prefix, "libG4GMocren", :libG4Mocren),
-    LibraryProduct(prefix, "libG4particles", :libG4Particles),
-    LibraryProduct(prefix, "libG4graphics_reps", :libG4Graphics),
-    LibraryProduct(prefix, "libG4zlib", :libG4Zlib),
-    LibraryProduct(prefix, "libG4geometry", :libG4Geometry),
-    LibraryProduct(prefix, "libG4modeling", :libG4Modeling),
-    LibraryProduct(prefix, "libG4interfaces", :libG4Interfaces),
-    LibraryProduct(prefix, "libG4persistency", :libG4Persistency),
-    LibraryProduct(prefix, "libG4track", :libG4Track),
-    LibraryProduct(prefix, "libG4error_propagation", :libG4ErrorPropagation),
-    LibraryProduct(prefix, "libG4parmodels", :libG4ParModels),
-    LibraryProduct(prefix, "libG4materials", :libG4Material),
-    LibraryProduct(prefix, "libG4physicslists", :libG4PhysicsLists),
-    LibraryProduct(prefix, "libG4VRML", :libG4VRML),
-    LibraryProduct(prefix, "libG4readout", :libG4Readout),
-    LibraryProduct(prefix, "libG4RayTracer", :libG4RayTracer),
-    LibraryProduct(prefix, "libG4visHepRep", :libG4VisHepRep),
-    LibraryProduct(prefix, "libG4Tree", :libG4Tree),
-    LibraryProduct(prefix, "libG4processes", :libG4Processes),
-    LibraryProduct(prefix, "libG4global", :libG4Global),
-    LibraryProduct(prefix, "libG4tracking", :libG4Tracking),
-    LibraryProduct(prefix, "libG4intercoms", :libG4Intercoms),
-    ExecutableProduct(prefix, "xmlwf", :xmlwf)
+products = [
+    LibraryProduct("libG4vis_management", :libG4visManagement),
+    LibraryProduct("libG4FR", :libG4FR),
+    LibraryProduct("libG4event", :libG4Event),
+    LibraryProduct("libG4analysis", :libG4Analysis),
+    LibraryProduct("libG4digits_hits", :libG4Digits),
+    LibraryProduct("libG4run", :libG4Run),
+    LibraryProduct("libG4visXXX", :libG4VisXXX),
+    LibraryProduct("libG4clhep", :libG4CLHEP),
+    LibraryProduct("libG4GMocren", :libG4Mocren),
+    LibraryProduct("libG4particles", :libG4Particles),
+    LibraryProduct("libG4graphics_reps", :libG4Graphics),
+    LibraryProduct("libG4zlib", :libG4Zlib),
+    LibraryProduct("libG4geometry", :libG4Geometry),
+    LibraryProduct("libG4modeling", :libG4Modeling),
+    LibraryProduct("libG4interfaces", :libG4Interfaces),
+    LibraryProduct("libG4persistency", :libG4Persistency),
+    LibraryProduct("libG4track", :libG4Track),
+    LibraryProduct("libG4error_propagation", :libG4ErrorPropagation),
+    LibraryProduct("libG4parmodels", :libG4ParModels),
+    LibraryProduct("libG4materials", :libG4Material),
+    LibraryProduct("libG4physicslists", :libG4PhysicsLists),
+    LibraryProduct("libG4VRML", :libG4VRML),
+    LibraryProduct("libG4readout", :libG4Readout),
+    LibraryProduct("libG4RayTracer", :libG4RayTracer),
+    LibraryProduct("libG4visHepRep", :libG4VisHepRep),
+    LibraryProduct("libG4Tree", :libG4Tree),
+    LibraryProduct("libG4processes", :libG4Processes),
+    LibraryProduct("libG4global", :libG4Global),
+    LibraryProduct("libG4tracking", :libG4Tracking),
+    LibraryProduct("libG4intercoms", :libG4Intercoms),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
+    "Expat_jll",
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7")
