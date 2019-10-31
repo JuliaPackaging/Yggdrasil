8921431fe678c83d8f49525f73348c8b96d3e047
Elliot Saba <staticfloat@gmail.com>
2019-10-31 14:51:51 -0400

    Cleanup `z3`

--- Z/z3/build_tarballs.jl
+++ Z/z3/build_tarballs.jl
@@ -9,64 +9,33 @@ version = v"4.8.6"
 sources = [
     "https://github.com/Z3Prover/z3.git" =>
     "78ed71b8de7d4d089f2799bf2d06f411ac6b9062",
-
+    "./bundled",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd z3/
-cat <<END | patch -p1
-diff --git a/src/util/memory_manager.cpp b/src/util/memory_manager.cpp
-index 3fe71c5e3e..a325afca75 100644
---- a/src/util/memory_manager.cpp
-+++ b/src/util/memory_manager.cpp
-@@ -163,7 +163,7 @@ unsigned long long memory::get_max_used_memory() {
- }
- 
- #if defined(_WINDOWS)
--#include <Windows.h>
-+#include <windows.h>
- #endif
- 
- unsigned long long memory::get_max_memory_size() {
-END
-cd ..
-mkdir z3-build
-cd z3-build/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} ../z3
-make -j20
-make install
+cd $WORKSPACE/srcdir/z3/
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/memory_manager_windows.patch
 
+mkdir z3-build && cd z3-build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} ..
+make -j${nproc}
+make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libz3", :libz3),
-    ExecutableProduct("bin/z3", :z3)
+    ExecutableProduct("z3", :z3)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8")
