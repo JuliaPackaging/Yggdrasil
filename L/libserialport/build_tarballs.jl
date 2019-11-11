9c88cab94574f807f7ff8ae51b759574cb9b8082
Ian <i.r.butterworth@gmail.com>
2019-11-11 16:13:47 -0500

    build libserialport

---
+++ L/libserialport/build_tarballs.jl
@@ -0,0 +1,38 @@
+using BinaryBuilder
+
+# Collection of sources required to build libserialport
+name = "libserialport"
+version = v"0.1.1"
+sources = [
+    "http://sigrok.org/download/source/libserialport/libserialport-$(version).tar.gz" =>
+    "4a2af9d9c3ff488e92fb75b4ba38b35bcf9b8a66df04773eba2a7bbf1fa7529d",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/libserialport-*/
+
+./autogen.sh
+./configure --prefix=$prefix --host=$target --with-include-path=$prefix/include
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# Disable FreeBSD for now, because hogweed needs alloca()?
+platforms = [p for p in platforms if !(typeof(p) <: FreeBSD)]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libserialport", :libnettle)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
