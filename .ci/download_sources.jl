4d9c7f79b64279c296f3104413c5632ee564ede5
Elliot Saba <staticfloat@gmail.com>
2019-11-15 13:17:28 -0800

    Declarative `build_tarballs.jl` parsing!
    This enables us to level up our CI game, namely:
    
    * Download sources once, to avoid clobbering when multiple jobs start at once
    * Run a separate job PER TARGET PLATFORM; not only does this increase parallelism, it also makes debugging easier.

--- .ci/download_sources.jl
+++ .ci/download_sources.jl
@@ -1,11 +1,11 @@
-using JSON, BinaryBuilder
+using BinaryBuilder
 
 # Read in input `.json` file
 json = String(read(ARGS[1]))
 buff = IOBuffer(strip(json))
 objs = []
 while !eof(buff)
-    push!(objs, JSON.parse(buff))
+    push!(objs, BinaryBuilder.JSON.parse(buff))
 end
 
 # Merge the multiple outputs into one
@@ -14,3 +14,8 @@ BinaryBuilder.cleanup_merged_object!(merged)
 
 # Download all sources
 BinaryBuilder.download_sources(merged["sources"]; verbose=true)
+
+# Then export platforms to file
+open(ARGS[2], "w") do io
+    println(io, join(merged["platforms"], " "))
+end
