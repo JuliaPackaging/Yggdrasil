5b3fd55cbcb47f3cc77fcc795ea5c8cf9c69af21
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-11-15 10:31:25 +0000

    [spglib] Adapt to new BinaryBuilder

--- S/spglib/build_tarballs.jl
+++ S/spglib/build_tarballs.jl
@@ -2,52 +2,41 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "Spglib"
-version = v"0.2.0"
+name = "spglib"
+version = v"1.14.1"
 
-# Collection of sources required to build SpglibBuilder
+# Collection of sources required to build spglib
 sources = [
-    "https://github.com/atztogo/spglib/archive/v1.14.1.tar.gz" =>
+    "https://github.com/atztogo/spglib/archive/v$(version).tar.gz" =>
     "9803b0648d9c2d99377f3e1c4cecf712320488403cd674192ec5cbe956bb3c78",
-
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd spglib-1.14.1/
+cd $WORKSPACE/srcdir/spglib-*/
 if [[ ${target} == *-mingw32 ]]; then
     sed -i -e 's/LIBRARY/RUNTIME/' CMakeLists.txt
 fi
 mkdir _build
 cd _build/
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
-      -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
       ../
-make
+make -j${nproc}
 make install VERBOSE=1
 """
 
-
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Windows(:i686),
-    Windows(:x86_64),
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libsymspg", :libsymspg)
+products = [
+    LibraryProduct("libsymspg", :libsymspg)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
