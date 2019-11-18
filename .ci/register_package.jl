2acfa9d0e5d25da653c00cf309564dfaa1655fc3
Elliot Saba <staticfloat@gmail.com>
2019-11-18 14:43:08 -0800

    Improve registration
    This relies upon
    https://github.com/JuliaPackaging/BinaryBuilder.jl/pull/502

--- .ci/register_package.jl
+++ .ci/register_package.jl
@@ -1,15 +1,29 @@
 using BinaryBuilder
 
-if length(ARGS) != 3
-    error("Usage: register_package.jl <name> <version> <dependencies>")
+verbose = "--verbose" in ARGS
+
+# Read in input `.json` file
+json = String(read(ARGS[1]))
+buff = IOBuffer(strip(json))
+objs = []
+while !eof(buff)
+    push!(objs, BinaryBuilder.JSON.parse(buff))
 end
 
-name = ARGS[1]
-version = VersionNumber(ARGS[2])
-dependencies = strip.(split(ARGS[3], " "), '\'')
+# Merge the multiple outputs into one
+merged = BinaryBuilder.merge_json_objects(objs)
+BinaryBuilder.cleanup_merged_object!(merged)
 
 # Determine build version
+name = merged["name"]
+version = merged["version"]
+dependencies = merged["dependencies"]
 build_version = BinaryBuilder.get_next_wrapper_version(name, version)
 
 # Register JLL package using given metadata
+for obj in objs
+    BinaryBuilder.cleanup_merged_object!(obj)
+    BinaryBuilder.rebuild_jll_packages(obj; verbose=verbose)
+end
+BinaryBuilder.push_jll_package(name, build_version)
 BinaryBuilder.register_jll(name, build_version, dependencies)
