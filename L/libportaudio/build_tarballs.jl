c1fdaa166df4238a6196e910f4df4123ccd21398
Spencer Russell <spencer.f.russell@gmail.com>
2019-11-21 17:09:13 -0500

    updates build script for libportaudio

--- L/libportaudio/build_tarballs.jl
+++ L/libportaudio/build_tarballs.jl
@@ -48,8 +48,8 @@ platforms = [
 ]
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libportaudio", :libportaudio)
+products = [
+    LibraryProduct("libportaudio", :libportaudio)
 ]
 
 # Dependencies that must be installed before this package can be built
