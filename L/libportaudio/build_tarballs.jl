46df24420e95db1d7c0fb61ed307f8975ed9b1ff
Spencer Russell <spencer.f.russell@gmail.com>
2019-11-22 10:11:45 -0500

    portaudio: clean up some comments and whitespace

--- L/libportaudio/build_tarballs.jl
+++ L/libportaudio/build_tarballs.jl
@@ -5,7 +5,6 @@ using BinaryBuilder
 name = "libportaudio"
 version = v"19.6.0"
 
-
 # Collection of sources required to build libportaudio. Not all of these
 # are used for all platforms.
 sources = [
@@ -42,22 +41,10 @@ install_license "${WORKSPACE}/srcdir/portaudio/LICENSE.txt"
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-# platforms = [
-#     Linux(:i686, libc=:glibc)
-#     Linux(:x86_64, libc=:glibc)
-#     Linux(:aarch64, libc=:glibc)
-#     Linux(:armv7l, libc=:glibc, call_abi=:eabihf)
-#     Linux(:powerpc64le, libc=:glibc)
-#     MacOS(:x86_64)
-#     Windows(:i686)
-#     Windows(:x86_64)
-# ]
 platforms = supported_platforms()
 
 # The products that we will ensure are always built
-products = [
-    LibraryProduct("libportaudio", :libportaudio)
-]
+products = [ LibraryProduct("libportaudio", :libportaudio) ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = []
