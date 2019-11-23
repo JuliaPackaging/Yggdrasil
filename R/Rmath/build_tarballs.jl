69a3afce29e63d81e00b6c0a5b1a308dbc4435cb
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-11-23 08:27:59 +0000

    [RMath] Fix source URL and use `${libdir}` in the script

--- R/Rmath/build_tarballs.jl
+++ R/Rmath/build_tarballs.jl
@@ -4,20 +4,15 @@ name = "Rmath"
 version = v"0.2.2"
 
 sources = [
-    "https://github.com/JuliaStats/Rmath-julia/archive/$(version).tar.gz" =>
+    "https://github.com/JuliaStats/Rmath-julia/archive/v$(version).tar.gz" =>
         "6544f40e51999469873b0f28d4bdeecdc847d4b24250a65027ae07e7dccb9ccd",
 ]
 
 script = raw"""
 cd $WORKSPACE/srcdir/Rmath-julia-*
 make
-if [[ ${target} == *-mingw32 ]]; then
-    OUTDIR=$DESTDIR/bin
-else
-    OUTDIR=$DESTDIR/lib
-fi
-mkdir -p $OUTDIR
-mv src/libRmath-julia.* $OUTDIR
+mkdir -p "${libdir}"
+mv src/libRmath-julia.* "${libdir}"
 """
 
 platforms = supported_platforms()
