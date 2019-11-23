eb639c06444e85bf5604b0db1efd86f1dc653667
Mikael Slevinsky <Richard.Slevinsky@umanitoba.ca>
2019-11-23 12:08:51 -0600

    add FastTransforms builder (#206)
    * add FastTransforms builder
    
    * expand platforms to MacOS(:x86_64) and Windows(:x86_64)
    
    * remove symlink, tests still fail on Windows(:x86_64)
    
    * update FastTransforms version && idealize build script
    
    * refine yet again
    
    * define missing symbols
    
    * try to help the assembler

---
+++ F/FastTransforms/build_tarballs.jl
@@ -0,0 +1,51 @@
+using BinaryBuilder
+
+# Collection of sources required to build FastTransforms
+name = "FastTransforms"
+version = v"0.2.11"
+sources = [
+    "https://github.com/MikaelSlevinsky/FastTransforms/archive/v$(version).tar.gz" =>
+    "f3d5d7f22af40df36f60ca85cda916d54a1c5fa98df07d6062d02424599938cd",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/FastTransforms-*
+if [[ ${target} == x86_64-w64-mingw32 ]]; then
+    export CFLAGS="-fno-asynchronous-unwind-tables "
+else
+    export CFLAGS
+fi
+if [[ ${nbits} == 64 ]]; then
+    SYMBOL_DEFS=()
+    SYMBOLS=(dgemm dtrmm dtrmv dtrsm sgemm strmm strsm)
+    for sym in ${SYMBOLS[@]}; do
+        SYMBOL_DEFS+=("-Dcblas_${sym}=cblas_${sym}64_")
+    done
+    CFLAGS+=${SYMBOL_DEFS[@]}
+    BLAS=openblas64_
+else
+    BLAS=openblas
+fi
+make lib CC=gcc FT_PREFIX=${prefix} FT_BLAS=${BLAS} FT_FFTW_WITH_COMBINED_THREADS=1
+mv -f libfasttransforms.${dlext} ${libdir}
+"""
+
+platforms = expand_gfortran_versions(supported_platforms())
+platforms = [p for p in platforms if BinaryBuilder.proc_family(p) == :intel]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libfasttransforms", :libfasttransforms),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "CompilerSupportLibraries_jll";
+    "FFTW_jll";
+    "MPFR_jll";
+    "OpenBLAS_jll"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
