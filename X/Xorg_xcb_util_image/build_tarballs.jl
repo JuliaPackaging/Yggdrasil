87794eb0ee3d2fdc6750bb9c0009dc018c4aa3e5
Bart Janssens <bart@bartjanssens.org>
2019-11-28 00:00:36 +0100

    Add some xcb-util based libraries (#242)

---
+++ X/Xorg_xcb_util_image/build_tarballs.jl
@@ -0,0 +1,39 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "Xorg_xcb_util_image"
+version = v"0.4.0"
+
+# Collection of sources required to build libxcb
+sources = [
+    "https://xcb.freedesktop.org/dist/xcb-util-image-$(version).tar.bz2" =>
+    "2db96a37d78831d643538dd1b595d7d712e04bdccf8896a5e18ce0f398ea2ffc",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/xcb-util-image-*/
+CPPFLAGS="-I${prefix}/include"
+# When compiling for things like ppc64le, we need newer `config.sub` files
+update_configure_scripts
+./configure --prefix=${prefix} --host=${target} --enable-malloc0returnsnull=no
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [p for p in supported_platforms() if p isa Union{Linux,FreeBSD}]
+
+products = [
+    LibraryProduct("libxcb-image", :libxcb_image),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "Xorg_xcb_util_jll"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
---
+++ X/Xorg_xcb_util_wm/build_tarballs.jl
@@ -0,0 +1,40 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "Xorg_xcb_util_wm"
+version = v"0.4.1"
+
+# Collection of sources required to build libxcb
+sources = [
+    "https://xcb.freedesktop.org/dist/xcb-util-wm-$(version).tar.bz2" =>
+    "28bf8179640eaa89276d2b0f1ce4285103d136be6c98262b6151aaee1d3c2a3f",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/xcb-util-wm-*/
+CPPFLAGS="-I${prefix}/include"
+# When compiling for things like ppc64le, we need newer `config.sub` files
+update_configure_scripts
+./configure --prefix=${prefix} --host=${target} --enable-malloc0returnsnull=no
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [p for p in supported_platforms() if p isa Union{Linux,FreeBSD}]
+
+products = [
+    LibraryProduct("libxcb-ewmh", :libxcb_ewmh),
+    LibraryProduct("libxcb-icccm", :libxcb_icccm),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "Xorg_xcb_util_jll"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
