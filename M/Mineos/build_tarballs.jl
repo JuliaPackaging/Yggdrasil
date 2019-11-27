1608f24547e112f8bd3007889d7e3af90ccd63b7
Andy Nowacki <a.nowacki@leeds.ac.uk>
2019-11-27 15:45:06 +0000

    [Mineos] Update BinaryBuilder syntax
    Remove unnecessary platforms and turn products into a vector

--- M/Mineos/build_tarballs.jl
+++ M/Mineos/build_tarballs.jl
@@ -23,33 +23,18 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
-platforms = expand_gcc_versions(platforms)
+platforms = expand_gcc_versions(supported_platforms())
 
 # The products that we will ensure are always built
-products(prefix) = [
-    ExecutableProduct(prefix, "simpledit", :simpledit),
-    ExecutableProduct(prefix, "endi", :endi),
-    ExecutableProduct(prefix, "eigcon", :eigcon),
-    ExecutableProduct(prefix, "eigen2asc", :eigen2asc),
-    ExecutableProduct(prefix, "green", :green),
-    ExecutableProduct(prefix, "syndat", :syndat),
-    ExecutableProduct(prefix, "minos_bran", :minos_bran),
-    ExecutableProduct(prefix, "cucss2sac", :cucss2sac)
+products = [
+    ExecutableProduct("simpledit", :simpledit),
+    ExecutableProduct("endi", :endi),
+    ExecutableProduct("eigcon", :eigcon),
+    ExecutableProduct("eigen2asc", :eigen2asc),
+    ExecutableProduct("green", :green),
+    ExecutableProduct("syndat", :syndat),
+    ExecutableProduct("minos_bran", :minos_bran),
+    ExecutableProduct("cucss2sac", :cucss2sac)
 ]
 
 # Dependencies that must be installed before this package can be built
