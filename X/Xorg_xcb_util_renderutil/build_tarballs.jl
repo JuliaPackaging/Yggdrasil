a2383f4fdfeda3f48e6ff3a733373852b0f9db8e
Bart Janssens <bart@bartjanssens.org>
2019-11-27 22:36:35 +0100

    Add some xcb-util based libraries

---
+++ X/Xorg_xcb_util_renderutil/build_tarballs.jl
@@ -0,0 +1,39 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "Xorg_xcb_util_renderutil"
+version = v"0.3.9"
+
+# Collection of sources required to build libxcb
+sources = [
+    "https://xcb.freedesktop.org/dist/xcb-util-renderutil-$(version).tar.bz2" =>
+    "c6e97e48fb1286d6394dddb1c1732f00227c70bd1bedb7d1acabefdd340bea5b",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/xcb-util-renderutil-*/
+CPPFLAGS="-I${prefix}/include"
+# When compiling for things like ppc64le, we need newer `config.sub` files
+update_configure_scripts
+./configure --prefix=${prefix} --host=${target} --enable-malloc0returnsnull=no
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [p for p in supported_platforms() if p isa Union{Linux,FreeBSD}]
+
+products = [
+    LibraryProduct("libxcb-render-util", :libxcb_render_util),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "Xorg_xcb_util_jll"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
