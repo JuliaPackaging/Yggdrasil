a41d5272dbec59fa46cffd6d3cda194105c9c16d
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-11-27 13:11:21 +0000

    [Git] Improve builder so that it runs only for requested platforms

--- G/Git/build_tarballs.jl
+++ G/Git/build_tarballs.jl
@@ -75,8 +75,34 @@ dependencies = [
 
 # Install first for win32, then win64.  This will accumulate files into `products` and also wrappers into the JLL package.
 non_reg_ARGS = filter(arg -> arg != "--register", ARGS)
-build_tarballs(non_reg_ARGS, name, version, sources_w32, script_win, [Windows(:i686)], products, [])
-build_tarballs(non_reg_ARGS, name, version, sources_w64, script_win, [Windows(:x86_64)], products, [])
 
+# Get the list of platforms requested from the command line.  This should be the
+# only argument not prefixed with "--".
+requested_platforms = filter(arg -> !occursin(r"^--.*", arg), ARGS)
+# This returns whether the builder for the given `platform` should be run.
+function run_platform(platform)
+    if isone(length(requested_platforms))
+        # `requested_platforms` has only one element: the comma-separated list
+        # of platform.  We'll run the platform only if it's in the list
+        return platform in split(requested_platforms[1], ",")
+    else
+        # `requested_platforms` doesn't have only one element: if its length is
+        # zero, no platform has been explicitely passed from the command line
+        # and we we'll run all platforms, otherwise we don't know what to do, so
+        # let's return false.
+        return iszero(length(requested_platforms))
+    end
+end
+
+if run_platform("i686-w64-mingw32")
+    build_tarballs(non_reg_ARGS, name, version, sources_w32, script_win, [Windows(:i686)], products, [])
+end
+if run_platform("x86_64-w64-mingw32")
+    build_tarballs(non_reg_ARGS, name, version, sources_w64, script_win, [Windows(:x86_64)], products, [])
+end
 # Then for everything else.  This is the only one that we try to register, and this is the step that will open a PR against General
-build_tarballs(ARGS, name, version, sources_unix, script_unix, filter(p -> !isa(p, Windows), supported_platforms()), products, dependencies)
+platforms = filter!(p -> !isa(p, Windows), supported_platforms())
+if any(run_platform.(triplet.(platforms)))
+    build_tarballs(ARGS, name, version, sources_unix, script_unix, platforms, products, dependencies)
+end
+
