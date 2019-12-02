e2644c8235370a26b7d21a990407dd7f1998036d
Martijn Visser <mgvisser@gmail.com>
2019-12-02 23:01:31 +0100

    Add builder for PROJ (#252)
    * Add builder for PROJ
    
    Adapted from https://github.com/JuliaGeo/PROJBuilder
    
    * fix toolchain file location, use libdir and dlext
    
    * add LibraryProduct synonym for Windows
    
    * take out proj-datumgrid
    
    This is optional and can be added separately as an Artifact.
    
    * strip _path from Product names

---
+++ P/PROJ/build_tarballs.jl
@@ -0,0 +1,71 @@
+using BinaryBuilder
+
+name = "PROJ"
+version = v"6.2.1"
+
+# Collection of sources required to build PROJ
+sources = [
+    "https://download.osgeo.org/proj/proj-$version.tar.gz" =>
+    "7f2e0fe63312f1e766057cceb53dc9585c4a335ff6641de45696dbd40d17c340",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/proj-*/
+
+# sqlite needed to build proj.db, so this should not be the
+# cross-compiled one since it needs to be executed on the host
+apk add sqlite
+
+if [[ ${target} == *mingw* ]]; then
+    SQLITE3_LIBRARY=${libdir}/libsqlite3-0.dll
+else
+    SQLITE3_LIBRARY=${libdir}/libsqlite3.${dlext}
+fi
+
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=$prefix \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DSQLITE3_INCLUDE_DIR=$prefix/include \
+      -DSQLITE3_LIBRARY=$SQLITE3_LIBRARY \
+      -DHAVE_PTHREAD_MUTEX_RECURSIVE_DEFN=1 \
+      -DBUILD_LIBPROJ_SHARED=ON \
+      ..
+cmake --build .
+make install
+"""
+
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct(["libproj", "libproj_$(version.major)_$(version.minor)"], :libproj),
+
+    ExecutableProduct("cct", :cct),
+    ExecutableProduct("cs2cs", :cs2cs),
+    ExecutableProduct("geod", :geod),
+    ExecutableProduct("gie", :gie),
+    ExecutableProduct("proj", :proj),
+    ExecutableProduct("projinfo", :projinfo),
+
+    # complete contents of share/proj, must be kept up to date
+    FileProduct(joinpath("share", "proj", "CH"), :ch),
+    FileProduct(joinpath("share", "proj", "GL27"), :gl27),
+    FileProduct(joinpath("share", "proj", "ITRF2000"), :itrf2000),
+    FileProduct(joinpath("share", "proj", "ITRF2008"), :itrf2008),
+    FileProduct(joinpath("share", "proj", "ITRF2014"), :itrf2014),
+    FileProduct(joinpath("share", "proj", "nad.lst"), :nad_lst),
+    FileProduct(joinpath("share", "proj", "nad27"), :nad27),
+    FileProduct(joinpath("share", "proj", "nad83"), :nad83),
+    FileProduct(joinpath("share", "proj", "null"), :null),
+    FileProduct(joinpath("share", "proj", "other.extra"), :other_extra),
+    FileProduct(joinpath("share", "proj", "proj.db"), :proj_db),
+    FileProduct(joinpath("share", "proj", "world"), :world),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = ["SQLite_jll"]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
