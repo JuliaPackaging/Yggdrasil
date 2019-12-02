11842a657e8036d0e6d360aaf692b2e640bf0d27
Elliot Saba <staticfloat@gmail.com>
2019-12-02 14:16:45 -0800

    Fix registration

--- .ci/register_package.jl
+++ .ci/register_package.jl
@@ -21,7 +21,12 @@ dependencies = merged["dependencies"]
 build_version = BinaryBuilder.get_next_wrapper_version(name, version)
 
 # Register JLL package using given metadata
-BinaryBuilder.init_jll_package(name)
+BinaryBuilder.init_jll_package(
+    name,
+    joinpath(Pkg.devdir(), "$(name)_jll"),
+    "JuliaBinaryWrappers/$(name)_jll.jl",
+)
+
 for obj in objs
     BinaryBuilder.cleanup_merged_object!(obj)
     BinaryBuilder.rebuild_jll_packages(obj; verbose=verbose)
