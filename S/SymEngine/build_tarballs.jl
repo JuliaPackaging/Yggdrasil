3b8d4f8ab415c9fd25495790602c4746a30c5fa9
Isuru Fernando <isuruf@gmail.com>
2019-12-11 18:12:51 -0600

    Don't copy dll and simplify code

--- S/SymEngine/build_tarballs.jl
+++ S/SymEngine/build_tarballs.jl
@@ -26,24 +26,17 @@ cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
 make -j${nproc}
 make install
 
-# On Windows, make sure non-versioned filename exists...
-if [[ ${target} == *mingw* ]]; then
-    cp -v ${prefix}/bin/libsymengine-*.dll ${prefix}/bin/libsymengine.dll
-fi
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-a = supported_platforms()
 # For some reason, on MinGW, using a DLL comppiled with a different gcc
 # segfaults. Expand only on Windows. Other OSes work fine.
-deleteat!(a, findfirst(x->x==Windows(:x86_64), a))
+a = supported_platforms(exclude=[Windows(:x86_64)])
 platforms = BinaryBuilder.expand_gcc_versions(Windows(:x86_64))
 platforms = vcat(platforms, a)
 
 # The products that we will ensure are always built
 products(prefix) = [
-    LibraryProduct("libsymengine", :libsymengine)
+    LibraryProduct(["libsymengine", "libsymengine-$(version.major).$(version.minor)"], :libsymengine)
 ]
 
 # Dependencies that must be installed before this package can be built
