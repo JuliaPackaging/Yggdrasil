60e67e04c5a775441db879da84106cbe46ee9176
Elliot Saba <staticfloat@gmail.com>
2019-12-12 23:45:57 -0800

    Fix shlib installation

--- S/SuiteSparse@4/build_tarballs.jl
+++ S/SuiteSparse@4/build_tarballs.jl
@@ -14,7 +14,7 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/SuiteSparse/
 
-FLAGS=(INSTALL="${prefix}" INSTALL_LIB="${libdir}" INSTALL_INCLUDE="${prefix}/include")
+FLAGS=(INSTALL="${prefix}" INSTALL_LIB="${prefix}/lib" INSTALL_INCLUDE="${prefix}/include")
 
 if [[ ${target} == *mingw32* ]]; then
     FLAGS+=(UNAME=Windows)
@@ -25,7 +25,7 @@ else
     FLAGS+=(LDFLAGS="${LDFLAGS} -L${prefix}/lib")
 fi
 
-if [[ ${nbits} == 64 ]]; then
+if [[ ${nbits} == 64 ]] && [[ ${target} != aarch64* ]]; then
     SUN="-DSUN64 -DLONGBLAS='long long'"
 
     BLASNAME=openblas64_
@@ -53,13 +53,13 @@ for proj in AMD BTF CAMD CCOLAMD COLAMD CHOLMOD LDL KLU UMFPACK RBio SPQR; do
 done
 
 # Manually build shared libraries
-${CC} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/libsuitesparseconfig.a ${NO_WHOLE_ARCHIVE} -o ${prefix}/lib/libsuitesparseconfig.${dlext} ${LDFLAGS} -L${prefix}/lib
+${CC} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/libsuitesparseconfig.a ${NO_WHOLE_ARCHIVE} -o ${libdir}/libsuitesparseconfig.${dlext} ${LDFLAGS} -L${prefix}/lib
 for name in libamd libcolamd libcamd libccolamd; do
-    ${CC} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/${name}.a ${NO_WHOLE_ARCHIVE} -o ${prefix}/lib/${name}.${dlext} ${LDFLAGS} -L${prefix}/lib -lsuitesparseconfig
+        ${CC} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/${name}.a ${NO_WHOLE_ARCHIVE} -o ${libdir}/${name}.${dlext} ${LDFLAGS} -L${prefix}/lib -lsuitesparseconfig
 done
-${CXX} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/libcholmod.a ${NO_WHOLE_ARCHIVE} -o ${prefix}/lib/libcholmod.${dlext} ${LDFLAGS} -L${prefix}/lib -lcolamd -lamd -lcamd -lccolamd -lsuitesparseconfig -l${BLASNAME}
-${CXX} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/libumfpack.a ${NO_WHOLE_ARCHIVE} -o ${prefix}/lib/libumfpack.${dlext} ${LDFLAGS} -L${prefix}/lib -lcholmod -lcolamd -lamd -lsuitesparseconfig -l${BLASNAME}
-${CXX} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/libspqr.a ${NO_WHOLE_ARCHIVE} -o ${prefix}/lib/libspqr.${dlext} ${LDFLAGS} -L${prefix}/lib -lcholmod -lcolamd -lamd -lsuitesparseconfig -l${BLASNAME}
+${CXX} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/libcholmod.a ${NO_WHOLE_ARCHIVE} -o ${libdir}/libcholmod.${dlext} ${LDFLAGS} -L${prefix}/lib -lcolamd -lamd -lcamd -lccolamd -lsuitesparseconfig -l${BLASNAME}
+${CXX} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/libumfpack.a ${NO_WHOLE_ARCHIVE} -o ${libdir}/libumfpack.${dlext} ${LDFLAGS} -L${prefix}/lib -lcholmod -lcolamd -lamd -lsuitesparseconfig -l${BLASNAME}
+${CXX} -shared ${WHOLE_ARCHIVE} ${prefix}/lib/libspqr.a ${NO_WHOLE_ARCHIVE} -o ${libdir}/libspqr.${dlext} ${LDFLAGS} -L${prefix}/lib -lcholmod -lcolamd -lamd -lsuitesparseconfig -l${BLASNAME}
 
 # For now, we'll have to adjust the name of the OpenBLAS library on macOS.
 # Eventually, this should be fixed upstream
