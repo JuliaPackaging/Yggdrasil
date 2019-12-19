4dd0cddb9b3e416ccf19f8bebae353b89d9b0b17
Elliot Saba <staticfloat@gmail.com>
2019-12-19 15:06:00 -0500

    Fix libxml2 problems in LLVM shards

--- 0_RootFS/LLVMBootstrap@7/build_tarballs.jl
+++ 0_RootFS/LLVMBootstrap@7/build_tarballs.jl
@@ -1,9 +1,19 @@
-name = "LLVMBootstrap"
-version = v"7.1.0"
-
 # Include everything common between our LLVM versions (That's a lot)
 include("../llvm_common.jl")
 
 # Build the tarballs, then upload to Yggdrasil releases
+
+# Unfortunately, it looks like these are just too big for GitHub.  :(
+#=
+name = "LLVMBootstrapDebug"
+version = v"7.1.0"
+sources, script, products, dependencies = llvm_build_args(;version=version, llvm_build_type="RelWithDebInfo") 
+build_info = build_tarballs(ARGS, name, version, sources, script, [host_platform], products, dependencies; skip_audit=true)
+upload_and_insert_shards("JuliaPackaging/Yggdrasil", name, version, build_info)
+=#
+
+name = "LLVMBootstrap"
+version = v"7.1.0"
+sources, script, products, dependencies = llvm_build_args(;version=version) 
 build_info = build_tarballs(ARGS, name, version, sources, script, [host_platform], products, dependencies; skip_audit=true)
 upload_and_insert_shards("JuliaPackaging/Yggdrasil", name, version, build_info)
