3cf2ccedee78ff98161e775302b34641dc11114d
Elliot Saba <staticfloat@gmail.com>
2019-12-19 15:29:41 -0800

    post-wizard cleanup

--- I/iperf/build_tarballs.jl
+++ I/iperf/build_tarballs.jl
@@ -9,44 +9,28 @@ version = v"3.7.0"
 sources = [
     "https://github.com/esnet/iperf/archive/3.7.tar.gz" =>
     "c349924a777e8f0a70612b765e26b8b94cc4a97cc21a80ed260f65e9823c8fc5",
-
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd iperf-3.7/
+cd $WORKSPACE/srcdir/iperf-*
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make -j${nproc}
 make install
-
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64)
-]
+platforms = filter(p -> !isa(p, Windows), supported_platforms())
 
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libiperf", :libiperf),
-    ExecutableProduct("iperf3", :iperf)
+    ExecutableProduct("iperf3", :iperf),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
