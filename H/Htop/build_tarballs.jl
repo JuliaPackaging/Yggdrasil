b31d51c9643019f518236b3a597e83614e48c103
lizz <innerlee@users.noreply.github.com>
2019-12-20 02:10:44 +0800

    Wizard recipe: Htop-v2.2.0 (#305)
    * New Recipe: Htop v2.2.0
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl

---
+++ H/Htop/build_tarballs.jl
@@ -0,0 +1,48 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "Htop"
+version = v"2.2.0"
+
+# Collection of sources required to complete build
+sources = [
+    "http://hisham.hm/htop/releases/2.2.0/htop-2.2.0.tar.gz" =>
+    "d9d6826f10ce3887950d709b53ee1d8c1849a70fa38e91d5896ad8cbc6ba3c57",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/htop-*/
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf)
+]
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("htop", :htop)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "Ncurses_jll"
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
