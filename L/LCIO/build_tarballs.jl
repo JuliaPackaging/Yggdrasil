b105ceebd13829e7ec1f9edfa90bdda1cbcd45f3
Jan Strube <jan.strube@gmail.com>
2019-12-27 13:49:02 -0800

    Add LCIO builder (#133)
    * Add LCIO builder
    
    LCIO.jl is a registered package. It depends on binaries that this script builds.
    
    * Iterate on trying to get this to build
    
    Build for all platforms (just to see what happens), also remove some of the workarounds that may not be necessary anymore.  Finally, upgrade builder syntax to new style.
    
    * [LCIO] Reorganise build script
    
    * Update to new version of LCIO
    
    * Be explicit about libc variant for linux
    
    * selecting CxxWrap-compatible gcc version
    
    * Update build_tarballs.jl
    
    * Expanding gcc versions and removing preferred one.
    
    * fixing typo
    
    Co-authored-by: Elliot Saba <staticfloat@gmail.com>
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ L/LCIO/build_tarballs.jl
@@ -0,0 +1,45 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "LCIO"
+version = v"02.13.01"
+
+# Collection of sources required to build LCIO
+sources = [
+    "https://github.com/iLCSoft/LCIO/archive/v02-13-01.tar.gz" =>
+    "aa572e2ba38c0cadd6a92fa933c3ed97e21d016c7982578d3f293901169f4ec0",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/LCIO-*/
+mkdir build && cd build
+
+cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:x86_64, libc=:glibc),
+    MacOS(:x86_64),
+]
+platforms = expand_cxxstring_abis(platforms)
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("liblcio", :liblcio),
+    LibraryProduct("libsio", :libsio)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    "Zlib_jll",
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
