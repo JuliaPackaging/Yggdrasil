d5df683d1914a0fa22613bc15301c9541a320dfe
Viral B. Shah <viral@mayin.org>
2019-12-26 19:57:17 -0500

    fix for openmp

--- S/SuiteSparseGraphBLAS/build_tarballs.jl
+++ S/SuiteSparseGraphBLAS/build_tarballs.jl
@@ -34,6 +34,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    "CompilerSupportLibraries_jll",
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
