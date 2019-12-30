7682a872955131070adc5605271e609cdb9003fb
Gnimuc <qiyupei@gmail.com>
2019-12-30 21:51:39 +0800

    Build on all supported platforms

--- L/libusb/build_tarballs.jl
+++ L/libusb/build_tarballs.jl
@@ -25,9 +25,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:x86_64, libc=:glibc)
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
