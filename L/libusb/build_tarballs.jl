ce2e9ef6dbc56165a4cfb2b90a5bf166685fa1dd
Gnimuc <qiyupei@gmail.com>
2019-12-31 08:54:24 +0800

    Wizard recipe: libusb-v1.0.23 (#348)
    * New Recipe: libusb v1.0.23
    
    * Build on all supported platforms
    
    * Update L/libusb/build_tarballs.jl
    
    * fix-up
    
    * libusb does like FreeBSD
    
    * fix-up
    
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ L/libusb/build_tarballs.jl
@@ -0,0 +1,41 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "libusb"
+version = v"1.0.23"
+
+# Collection of sources required to complete build
+sources = [
+    "https://github.com/libusb/libusb.git" =>
+    "e782eeb2514266f6738e242cdcb18e3ae1ed06fa",
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd libusb/
+./bootstrap.sh 
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-udev
+make -j${nproc}
+make install
+install_license COPYING
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [p for p in supported_platforms() if !isa(p, FreeBSD)]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct(["libusb", "libusb-1", "libusb-1.0"], :libusb)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
