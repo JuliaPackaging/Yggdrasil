5737a1c3341d0301f9596dafa34508b773bff2ce
Gnimuc <qiyupei@gmail.com>
2020-01-01 15:43:43 +0800

    indentation

--- L/librealsense/build_tarballs.jl
+++ L/librealsense/build_tarballs.jl
@@ -55,11 +55,12 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-Linux(:i686, libc=:glibc)
-Linux(:x86_64, libc=:glibc)
-Linux(:aarch64, libc=:glibc)
-Linux(:armv7l, libc=:glibc, call_abi=:eabihf)
-MacOS(:x86_64)]
+    Linux(:i686, libc = :glibc)
+    Linux(:x86_64, libc = :glibc)
+    Linux(:aarch64, libc = :glibc)
+    Linux(:armv7l, libc = :glibc, call_abi = :eabihf)
+    MacOS(:x86_64)
+]
 
 # The products that we will ensure are always built
 products = [
