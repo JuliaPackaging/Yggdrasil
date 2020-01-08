3ee1aa551a1b406fcacdf191c1ebfa5cc8c661fc
Guillaume Fraux <guillaume.fraux@epfl.ch>
2020-01-08 10:48:26 +0100

    Minor improvements

--- C/Chemfiles/build_tarballs.jl
+++ C/Chemfiles/build_tarballs.jl
@@ -7,15 +7,15 @@ version = v"0.9.2"
 
 # Collection of sources required to complete build
 sources = [
-    "https://github.com/chemfiles/chemfiles/archive/0.9.2.tar.gz" =>
+    "https://github.com/chemfiles/chemfiles/archive/$version.tar.gz" =>
     "f0a40c8934cffdc8321bed79ded4cb4de5d5366d1f503a95467e294384521e82",
 ]
 
 # Bash recipe for building across all platforms
-script = raw"""
-cd $WORKSPACE/srcdir/chemfiles-0.9.2/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON .
-make -j4
+script = """
+cd ${WORKSPACE}/srcdir/chemfiles-$version/
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON .
+make -j${nproc}
 make install
 """
 
@@ -44,9 +44,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
+
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
