dee35d1e93021bb4966665cd90b4511571fa1451
Guillaume Fraux <guillaume.fraux@epfl.ch>
2020-01-08 10:53:59 +0100

    Fix interpolation

--- C/Chemfiles/build_tarballs.jl
+++ C/Chemfiles/build_tarballs.jl
@@ -13,9 +13,9 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = """
-cd ${WORKSPACE}/srcdir/chemfiles-$version/
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON .
-make -j${nproc}
+cd \${WORKSPACE}/srcdir/chemfiles-$version/
+cmake -DCMAKE_INSTALL_PREFIX=\${prefix} -DCMAKE_TOOLCHAIN_FILE=\${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON .
+make -j\${nproc}
 make install
 """
 
