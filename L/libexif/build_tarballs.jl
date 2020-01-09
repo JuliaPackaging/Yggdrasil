2dee5363f7461f1293e1feefa4acaee1d6be72c6
lizz <innerlee@users.noreply.github.com>
2020-01-09 10:08:14 +0800

    Try freebsd

--- L/libexif/build_tarballs.jl
+++ L/libexif/build_tarballs.jl
@@ -23,19 +23,7 @@ install_license COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
+platforms = supported_platforms()
 
 
 # The products that we will ensure are always built
