bc49eb6308251faba7b29d836585f54057406953
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-01-09 02:27:48 +0000

    [libexif] Build shared library for PowerPC and FreeBSD

--- L/libexif/build_tarballs.jl
+++ L/libexif/build_tarballs.jl
@@ -1,6 +1,6 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder, Pkg
+using BinaryBuilder
 
 name = "libexif"
 version = v"0.6.21"
@@ -14,11 +14,16 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libexif-*/
+if [[ "${target}" == powerpc64le-* ]] || [[ "${target}" == *-freebsd* ]]; then
+    # Install `autopoint` and other tools needed by `autoreconf`
+    apk add gettext-dev
+    # Rebuild the configure script to convince it to build the shared library
+    autoreconf -vi
+fi
 update_configure_scripts
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make -j${nproc}
 make install
-install_license COPYING
 """
 
 # These are the platforms we will build for by default, unless further
@@ -33,7 +38,6 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
