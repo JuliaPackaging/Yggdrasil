696e5b160c76e3a9c50663c81c2deb18915e8423
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-01-11 23:45:56 +0000

    [Graphviz] Add some patches for Windows

--- G/Graphviz/build_tarballs.jl
+++ G/Graphviz/build_tarballs.jl
@@ -17,26 +17,48 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/graphviz-*/
 
+if [[ "${target}" == *-mingw* ]]; then
+    # We need a `regex.h` header.  MinGW doesn't have one,
+    # let's use `pcreposix.h` instead.
+    cp ${prefix}/include/pcreposix.h ${prefix}/include/regex.h
+
+    # Apply some fun patches
+    atomic_patch -p1 ../patches/0003-sfsetbuf_c_Stat_t_no_st_blksize.patch
+    atomic_patch -p1 ../patches/0004-win32_dllexport_dllimport.patch
+    atomic_patch -p1 ../patches/0005-missing_libs.patch
+    atomic_patch -p1 ../patches/0006-export_neatogen.patch
+    atomic_patch -p1 ../patches/0007-remove_missing_def.patch
+    atomic_patch -p1 ../patches/0008-export_gvc.patch
+
+    export LDFLAGS="-lpcreposix -lexpat"
+    export EXTRA_LDFLAGS="-no-undefined"
+
+    # Remove wrong libtool archives
+    rm ${prefix}/lib/libharfbuzz*.la
+
+    # Rebuild the configure script
+    autoreconf -fiv
+fi
+
 # Apply patch to build a native `mkdefs` utility that can be run within the
 # build environment.
-atomic_patch -p1 ../patches/gvpr-build-native-mkdefs.patch
+atomic_patch -p1 ../patches/0001-gvpr-build-native-mkdefs.patch
 
 # This patch disable generation of dot's configuration
-atomic_patch -p1 ../patches/do-not-build-dot-config.patch
+atomic_patch -p1 ../patches/0002-do-not-build-dot-config.patch
 
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
-make -j${nproc}
+make -j${nproc} LDFLAGS="${LDFLAGS} ${EXTRA_LDFLAGS}"
 make install
+
+if [[ "${target}" == *-mingw* ]]; then
+    # Cover up the traces of the hack
+    rm ${prefix}/include/regex.h
+fi
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-#platforms = [
-#    Linux(:i686, libc=:glibc),
-#    Linux(:x86_64, libc=:glibc),
-#    Linux(:i686, libc=:musl),
-#    Linux(:x86_64, libc=:musl)
-#]   # these are the only ones that worked in the Wizard so far.
 platforms = supported_platforms()
 
 
@@ -51,6 +73,7 @@ dependencies = [
     PackageSpec(name="Cairo_jll", uuid="83423d85-b0ee-5818-9007-b63ccbeb887a")
     PackageSpec(name="Expat_jll", uuid="2e619515-83b5-522b-bb60-26c02a35a201")
     PackageSpec(name="Pango_jll", uuid="36c8627f-9965-5494-a995-c6b170f724f3")
+    PackageSpec(name="PCRE_jll",  uuid="2f80f16e-611a-54ab-bc61-aa92de5b98fc")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
