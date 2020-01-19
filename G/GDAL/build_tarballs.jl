dd30d8088aebd2bf1193542dab1bd1e264cc6bb3
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-01-19 03:00:54 +0000

    [GDAL] Build with GCC v6, for compatibility with GEOS

--- G/GDAL/build_tarballs.jl
+++ G/GDAL/build_tarballs.jl
@@ -79,4 +79,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6")
