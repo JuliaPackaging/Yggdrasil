6b2f84a795f095d3291e77a3d0273ae83d4fd056
Jan Strube <jan.strube@gmail.com>
2020-01-19 19:43:57 -0800

    removing preferred gcc version

--- P/Pythia8/build_tarballs.jl
+++ P/Pythia8/build_tarballs.jl
@@ -39,4 +39,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
