0fb17eac0f3a1a4e44af3b53aa023a20a1289d7f
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-01-20 03:21:48 +0000

    [MPICH] Apply patch to fix configuration of file systems

--- M/MPICH/build_tarballs.jl
+++ M/MPICH/build_tarballs.jl
@@ -5,13 +5,18 @@ version = v"3.3.2"
 sources = [
     "https://www.mpich.org/static/downloads/$(version)/mpich-$(version).tar.gz" =>
     "4bfaf8837a54771d3e4922c84071ef80ffebddbb6971a006038d91ee7ef959b9",
+    "./bundled",
 ]
 
 script = raw"""
 # Enter the funzone
 cd ${WORKSPACE}/srcdir/mpich-*
 
-./configure --prefix=$prefix --host=$target --enable-shared=yes --enable-static=no --disable-dependency-tracking --disable-fortran --docdir=/tmp --with-file-system="testfs ufs nfs"
+atomic_patch -p1 ../patches/0001-romio-Use-tr-for-replacing-to-space-in-list-of-file-.patch
+pushd src/mpi/romio
+autoreconf -vi
+popd
+./configure --prefix=$prefix --host=$target --enable-shared=yes --enable-static=no --disable-dependency-tracking --disable-fortran --docdir=/tmp
 
 # Build the library
 make "${flags[@]}" -j${nproc}
