06fccabe73545e2568ad244fca7090ccfdeff634
Jan Strube <jan.strube@gmail.com>
2020-01-19 19:48:49 -0800

    Fixing capitalization
     to be consistent with upstream project

--- F/Fastjet/build_tarballs.jl
+++ F/Fastjet/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "Fastjet"
+name = "FastJet"
 version = v"3.3.3"
 
 # Collection of sources required to complete build
