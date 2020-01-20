4cdd0164b8d6796a5f548ebb1c3bc30d66182658
Steven G. Johnson <stevenj@mit.edu>
2020-01-20 17:22:27 -0500

    bump ZMQ to 4.3.2 (#396)
    * bump ZMQ to 4.3.2
    
    * [ZeroMQ] Expand C++ string ABIs and apply patch for MinGW
    
    * [ZeroMQ] Apply the patch to fix Musl C library for *86 systems
    
    In this way we are able to build with the oldest GCC version possible
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- Z/ZeroMQ/build_tarballs.jl
+++ Z/ZeroMQ/build_tarballs.jl
@@ -1,19 +1,30 @@
 using BinaryBuilder
 
 name = "ZeroMQ"
-version = v"4.3.1"
+version = v"4.3.2"
 
 # Collection of sources required to build ZMQ
 sources = [
     "https://github.com/zeromq/libzmq.git" =>
-    "2cb1240db64ce1ea299e00474c646a2453a8435b",
+    "a84ffa12b2eb3569ced199660bac5ad128bff1f0",
+    "./bundled"
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 
 cd $WORKSPACE/srcdir/libzmq
-
+if [[ "${target}" == *-mingw* ]]; then
+    # Apply patch from
+    # https://github.com/msys2/MINGW-packages/blob/350ace4617661a4df7b9474c573b08325fa716c3/mingw-w64-zeromq/001-mingw-__except-fixes.patch
+    atomic_patch -p1 ../patches/001-mingw-__except-fixes.patch
+elif [[ "${target}" == *86*-linux-musl* ]]; then
+    pushd /opt/${target}/lib/gcc/${target}/*/include
+    # Fix bug in Musl C library, see
+    # https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/387
+    atomic_patch -p0 $WORKSPACE/srcdir/patches/mm_malloc.patch
+    popd
+fi
 sh autogen.sh
 ./configure --prefix=$prefix --host=${target} \
     --without-docs --disable-libunwind --disable-perf --disable-Werror \
@@ -25,7 +36,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
@@ -37,4 +48,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
