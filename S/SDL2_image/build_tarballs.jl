33b54e477722b557b2ed5689c2b179cc6bbc5c83
Avik Sengupta <avik@sengupta.net>
2020-01-24 19:01:52 +0000

    Windows should build now.

--- S/SDL2_image/build_tarballs.jl
+++ S/SDL2_image/build_tarballs.jl
@@ -22,18 +22,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64)
-]
+platforms = supported_platforms()
 
 
 # The products that we will ensure are always built
