36302bc0ed1203a0ca9dc2918e0464a64114c899
Jan Strube <jan.strube@gmail.com>
2020-01-26 22:18:23 -0800

    avoiding wget to download the binaries

--- F/FastJet_Julia_Wrapper/build_tarballs.jl
+++ F/FastJet_Julia_Wrapper/build_tarballs.jl
@@ -10,19 +10,22 @@ end
 version = VersionNumber(version_number)
 
 # Collection of sources required to build Fjwbuilder
-sources = [
-	   "FastJet_Julia_Wrapper"
+fastjet_sources = [
+	   "https://github.com/jstrube/FastJet_Julia_Wrapper.git" => "f558c8a156edfd00b66b68e544f9a202cb06a463"
 ]
+
+julia_sources = Dict(
+	"x86_64-w64-mingw32" => ["https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/julia-1.0.0-x86_64-w64-mingw32.tar.gz" => "9c58bc0873e52cf6c41108a7a2b100f68419478f10c6fe635197b1bf47eec64d"],
+	"x86_64-linux-gnu" => ["https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/julia-1.0.0-x86_64-linux-gnu.tar.gz" => "34b6e59acf8970a3327cf1603a8f90fa4da8e5ebf09e6624509ac39684a1835d"],
+	"x86_64-linux-musl" => ["https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/julia-1.0.0-x86_64-linux-gnu.tar.gz" => "34b6e59acf8970a3327cf1603a8f90fa4da8e5ebf09e6624509ac39684a1835d"],
+	"x86_64-apple-darwin14" => ["https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/julia-1.0.0-x86_64-apple-darwin14.tar.gz" => "a9537f53306f9cf4f0f376f737c745c16b78e9cf635a0b22fbf0562713454b10"]
+)
 
 # Bash recipe for building across all platforms
 script = raw"""
-wget https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/julia-1.0.0-${target}.tar.gz
-mkdir julia
-cd julia
-tar xf ../julia-1.0.0-${target}.tar.gz
 export PATH=$(pwd)/bin:${PATH}
-ln -s ${WORKSPACE}/srcdir/julia/include/ /opt/${target}/${target}/sys-root/usr/local
-cd ${WORKSPACE}/srcdir
+ln -s ${WORKSPACE}/srcdir/include/ /opt/${target}/${target}/sys-root/usr/local
+cd ${WORKSPACE}/srcdir/FastJet_Julia_Wrapper
 mkdir build && cd build
 export JlCxx_DIR=${prefix}
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
@@ -49,5 +52,13 @@ dependencies = [
 	"FastJet_jll"
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7")
+include("../../fancy_toys.jl")
+
+for p in platforms
+	if should_build_platform(triplet(p))
+		sources = copy(fastjet_sources)
+		append!(sources, julia_sources[triplet(p)])
+		build_tarballs(ARGS, name, version, sources, script, [p], products, dependencies; preferred_gcc_version=v"7")
+
+	end
+end
