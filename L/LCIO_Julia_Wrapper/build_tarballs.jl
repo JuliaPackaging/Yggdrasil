33ce852ec54a38cd6a84e8029af0d0e8c28e487d
Jan Strube <jan.strube@gmail.com>
2020-01-26 22:02:38 -0800

    avoiding wget to download binaries

--- L/LCIO_Julia_Wrapper/build_tarballs.jl
+++ L/LCIO_Julia_Wrapper/build_tarballs.jl
@@ -2,24 +2,26 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "LCIOWrapBuilder"
+name = "LCIO_Julia_Wrapper"
 version = v"0.7"
 
 # Collection of sources required to build LCIOWrapBuilder
-sources = [
-    Dict("https://github.com/jstrube/LCIOWrapBuilder/archive/$(version).tar.gz" => "")
+lcio_sources = [
+	"https://github.com/jstrube/LCIO_Julia_Wrapper.git" => "dea6f83ece60390b17e6c41cfe159f5bddd178e5",
 ]
+julia_sources = Dict(
+	"x86_64-w64-mingw32" => ["https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/julia-1.0.0-x86_64-w64-mingw32.tar.gz" => "9c58bc0873e52cf6c41108a7a2b100f68419478f10c6fe635197b1bf47eec64d"], 
+	"x86_64-linux-gnu" => ["https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/julia-1.0.0-x86_64-linux-gnu.tar.gz" => "34b6e59acf8970a3327cf1603a8f90fa4da8e5ebf09e6624509ac39684a1835d"],
+	"x86_64-apple-darwin14" => ["https://github.com/JuliaPackaging/JuliaBuilder/releases/download/v1.0.0-2/julia-1.0.0-x86_64-apple-darwin14.tar.gz" => "a9537f53306f9cf4f0f376f737c745c16b78e9cf635a0b22fbf0562713454b10"]
+)
 
 # Bash recipe for building across all platforms
 script = raw"""
-	wget https://github.com/Gnimuc/JuliaBuilder/releases/download/v1.3.0/julia-1.3.0-${target}.tar.gz
-	tar xzf julia-1.3.0-${target}.tar.gz
-	export PATH=$(pwd)/juliabin/bin:${PATH}
-	ln -s ${WORKSPACE}/srcdir/juliabin/include/ /opt/${target}/${target}/sys-root/usr/local
-	cd ${WORKSPACE}/srcdir
+	rsync -a ${WORKSPACE}/srcdir/ ${prefix}
+	ln -s ${WORKSPACE}/srcdir/include/ /opt/${target}/${target}/sys-root/usr/local
+	cd ${WORKSPACE}/srcdir/LCIO_Julia_Wrapper
 	mkdir build && cd build
-	cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
-	#cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=/opt/${target}/${target}.toolchain -DCMAKE_FIND_ROOT_PATH=${prefix} -DJulia_PREFIX=${prefix} ..
+	cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_ROOT_PATH=${prefix} -DJulia_PREFIX=${prefix} ..
 	VERBOSE=ON cmake --build . --config Release --target install
 """
 
@@ -40,7 +42,14 @@ dependencies = [
 	"libcxxwrap_julia_jll",
 	"LCIO_jll"
 ]
+include("../../fancy_toys.jl")
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8")
+for p in platforms
+	if should_build_platform(triplet(p))
+		sources = copy(lcio_sources)
+		append!(sources, julia_sources[triplet(p)])
+		build_tarballs(ARGS, name, version, sources, script, [p], products, dependencies; preferred_gcc_version=v"8")
+		
+	end
+end
 
