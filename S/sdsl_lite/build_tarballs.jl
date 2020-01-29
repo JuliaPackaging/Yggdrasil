cd767ec5b2839745e9214d35f99b382b77d1aa5c
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-01-29 23:35:05 +0000

    [sdsl_lite] Rename project to avoid hyphen in the package name

--- S/sdsl_lite/build_tarballs.jl
+++ S/sdsl_lite/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "sdsl-lite"
+name = "sdsl_lite"
 version = v"2.1.1"
 
 # Collection of sources required to complete build
