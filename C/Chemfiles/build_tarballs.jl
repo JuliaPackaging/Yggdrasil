2e1cecb3944689bd6d140e6b07c91a46a06d1c6f
Luthaf <luthaf@luthaf.fr>
2020-02-05 21:47:31 +0100

    Update Chemfiles to 0.9.3

--- C/Chemfiles/build_tarballs.jl
+++ C/Chemfiles/build_tarballs.jl
@@ -3,13 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "Chemfiles"
-version = v"0.9.2"
+version = v"0.9.3"
 
 # Collection of sources required to complete build
 sources = [
     "https://github.com/chemfiles/chemfiles/archive/$version.tar.gz" =>
-    "f0a40c8934cffdc8321bed79ded4cb4de5d5366d1f503a95467e294384521e82",
-    "./bundled",
+    "541878d189886dd52a247be711ab897fc6062c3275c34ee2e76a99b04132c74d",
 ]
 
 # Bash recipe for building across all platforms
@@ -17,7 +16,6 @@ script = raw"""
 cd ${WORKSPACE}/srcdir/chemfiles-*/
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON ..
-atomic_patch -p1 ../../patches/fileno_posix.patch
 make -j${nproc}
 make install
 """
