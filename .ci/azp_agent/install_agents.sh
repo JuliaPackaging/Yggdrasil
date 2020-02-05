5ce813311a8066095115635e05e8805efccfd873
Elliot Saba <staticfloat@gmail.com>
2020-02-05 18:45:11 -0500

    Second draft, now with nested sandboxing!

--- .ci/azp_agent/install_agents.sh
+++ .ci/azp_agent/install_agents.sh
@@ -2,14 +2,71 @@
 
 set -e
 
-NUM_AGENTS=8
+NUM_AGENTS=1
 SRC_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 mkdir -p ${HOME}/.config/systemd/user
+source .env
+
+# Create a nice little rootfs for our agents
+if [[ ! -d "${STORAGE_DIR}/rootfs" ]]; then
+    echo "Setting up rootfs..."
+    mkdir -p "${STORAGE_DIR}/rootfs"
+    #sudo debootstrap --variant=minbase --include=curl,libicu63 buster "${STORAGE_DIR}/rootfs"
+
+    # Remove special `dev` files
+    sudo rm -rf "${STORAGE_DIR}/rootfs/dev/*"
+    # take ownership
+    sudo chown $(id -u):$(id -g) -R "${STORAGE_DIR}/rootfs"
+    # Remove `_apt` user so that `apt` doesn't try to `setgroups()`
+    sed '/_apt:/d' -i "${STORAGE_DIR}/rootfs/etc/passwd"
+
+    # Install Julia into the rootfs
+    echo "Installing Julia..."
+    #JULIA_URL="https://julialang-s3.julialang.org/bin/linux/x64/1.3/julia-1.3.0-linux-x86_64.tar.gz"
+    # until 1.4-rc2 is out
+    JULIA_URL="julialangnightlies-s3.julialang.org/assert_pretesting/linux/x64/1.4/julia-3a22e2fdcf-linux64.tar.gz"
+    curl -# -L "$JULIA_URL" | tar --strip-components=1 -zx -C "${STORAGE_DIR}/rootfs"
+
+    # Install our Julia dependencies, that we bake into our depot
+    export JULIA_DEPOT_PATH="${STORAGE_DIR}/depot"
+    JULIA="${STORAGE_DIR}/rootfs/bin/julia"
+    ${JULIA} --color=yes -e 'using Pkg; Pkg.add([
+                             PackageSpec(name="BinaryProvider", rev="sf/sub_out_to_pkg"),
+                             PackageSpec(name="Registrator", rev="master"),
+                             PackageSpec(name="BinaryBuilder", rev="master")]);
+                             using BinaryBuilder'
+
+    # Install `sandbox` and `run_agent.sh` into the rootfs
+    echo "Installing sandbox..."
+    SANDBOX_URL="https://github.com/JuliaPackaging/Yggdrasil/raw/020391c638b8b1036cb97ef4d7d1ef5620eec654/0_RootFS/Rootfs/bundled/utils/sandbox"
+    curl -# -L "${SANDBOX_URL}" -o "${STORAGE_DIR}/rootfs/sandbox"
+    chmod +x "${STORAGE_DIR}/rootfs/sandbox"
+    cp -a "${SRC_DIR}/run_agent.sh" "${STORAGE_DIR}/rootfs/run_agent.sh"
+
+    # Install agent executable
+    AZP_AGENTPACKAGE_URL=$(
+        curl -LsS -u "user:${AZP_TOKEN}" \
+            -H 'Accept:application/json;api-version=3.0-preview' \
+            "${AZP_URL}/_apis/distributedtask/packages/agent?platform=linux-x64" |
+        jq -r '.value | map([.version.major,.version.minor,.version.patch,.downloadUrl]) | sort | .[length-1] | .[3]'
+    )
+
+    if [ -z "$AZP_AGENTPACKAGE_URL" -o "$AZP_AGENTPACKAGE_URL" == "null" ]; then
+        echo 1>&2 "error: could not determine a matching Azure Pipelines agent - check that account '$AZP_URL' is correct and the token is valid for that account"
+        exit 1
+    fi
+
+    echo "Installing AZP agent..."
+    mkdir -p "${STORAGE_DIR}/rootfs/agent"
+    curl -LsS "$AZP_AGENTPACKAGE_URL" | tar -xz -C "${STORAGE_DIR}/rootfs/agent"
+fi
+
+# Install mknod startup unit
+envsubst "\$SRC_DIR" <"loopback_startup.conf" >"${HOME}/.config/systemd/user/loopback_startup.unit"
 
 for AGENT_IDX in $(seq 1 $NUM_AGENTS); do
-    export SRC_DIR
-    export AGENT_IDX
-    envsubst "\$HOSTNAME \$SRC_DIR \$AGENT_IDX"  <"systemd_startup.conf" >"${HOME}/.config/systemd/user/azp_agent_${AGENT_IDX}.service"
+    export SRC_DIR STORAGE_DIR AGENT_IDX
+    envsubst "\$SRC_DIR \$STORAGE_DIR \$AGENT_IDX"  <"agent_startup.conf" >"${HOME}/.config/systemd/user/azp_agent_${AGENT_IDX}.service"
 done
 
 # Reload systemd user daemon
