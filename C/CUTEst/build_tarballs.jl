77dda51557510645687835c9f32590ef88b8052d
Dominique Orban <dominique.orban@gmail.com>
2020-02-07 18:21:33 -0500

    CUTEst v0.2.0

--- C/CUTEst/build_tarballs.jl
+++ C/CUTEst/build_tarballs.jl
@@ -3,14 +3,14 @@
 using BinaryBuilder
 
 name = "CUTEst"
-version = v"0.1.0"
+version = v"0.2.0"
 
 # Collection of sources required to build ThinASLBuilder
 sources = [
     "https://github.com/ralna/ARCHDefs/archive/v2.0.3x.tar.gz" =>
     "6583e27f84338447767bbdf4335514c8836ae4ad54f5e66280307e8b57189cff",
-    "https://github.com/ralna/SIFDecode/archive/v2.0.2.tar.gz" =>
-    "72936047dbea3695af6a15f9193412c22c119a3bc21d925750abf610c5df181b",
+    "https://github.com/ralna/SIFDecode/archive/v2.0.3.tar.gz" =>
+    "3a4aa817e1bf4e3595d0e4378da6172b65f02861f3a7c39f9da632a5cc31b1b2",
     "https://github.com/ralna/CUTEst/archive/v2.0.3.tar.gz" =>
     "d21a65c975302296f9856c09034cf46edc5da34b6efd96eed6cc94af6d2c8a55",
 ]
@@ -24,17 +24,17 @@ mkdir -p ${prefix}/libexec
 mkdir -p ${bindir}
 mkdir -p ${libdir}
 cp -r ARCHDefs-2.0.3x ${prefix}/libexec/
-cp -r SIFDecode-2.0.2 ${prefix}/libexec/
+cp -r SIFDecode-2.0.3 ${prefix}/libexec/
 cp -r CUTEst-2.0.3 ${prefix}/libexec/
 export ARCHDEFS=${prefix}/libexec/ARCHDefs-2.0.3x
-export SIFDECODE=${prefix}/libexec/SIFDecode-2.0.2
+export SIFDECODE=${prefix}/libexec/SIFDecode-2.0.3
 export CUTEST=${prefix}/libexec/CUTEst-2.0.3
 
 # build SIFDecode
 cd $SIFDECODE
 
 if [[ "${target}" == *-linux* || "${target}" == *-freebsd* ]]; then
-  echo "5" > sifdecode.opts   # PC
+  echo "6" > sifdecode.opts   # PC64
   echo "2" >> sifdecode.opts  # Linux
   echo "5" >> sifdecode.opts  # gfortran
 elif [[ "${target}" == *-apple* ]]; then
@@ -42,9 +42,8 @@ elif [[ "${target}" == *-apple* ]]; then
   echo "2" >> sifdecode.opts  # gfortran
 elif [[ "${target}" == *-mingw* ]]; then
   cd $ARCHDEFS
-  cp compiler.pc64.msys2.gfo compiler.pc.mgw.gfo
   cd $SIFDECODE
-  echo "5" > sifdecode.opts   # PC
+  echo "6" > sifdecode.opts   # PC64
   echo "1" >> sifdecode.opts  # Windows
   echo "3" >> sifdecode.opts  # gfortran
 fi
@@ -54,12 +53,12 @@ echo "nny" >> sifdecode.opts
 # build CUTEst
 cd $CUTEST
 if [[ "${target}" == *-linux* || "${target}" == *-freebsd* ]]; then
-  echo "5" > cutest.opts   # PC
+  echo "6" > cutest.opts   # PC64
   echo "2" >> cutest.opts  # Linux
   echo "5" >> cutest.opts  # gfortran
   echo "2" >> cutest.opts  # build all tools except Matlab
   echo "7" >> cutest.opts  # gcc
-  export MYARCH=pc.lnx.gfo
+  export MYARCH=pc64.lnx.gfo
 elif [[ "${target}" == *-apple* ]]; then
   echo "13" > cutest.opts  # macOS
   echo "2" >> cutest.opts  # gfortran
@@ -67,12 +66,12 @@ elif [[ "${target}" == *-apple* ]]; then
   echo "5" >> cutest.opts  # gcc
   export MYARCH=mac64.osx.gfo
 elif [[ "${target}" == *-mingw* ]]; then
-  echo "5" > cutest.opts   # PC
+  echo "5" > cutest.opts   # PC64
   echo "1" >> cutest.opts  # Windows
   echo "3" >> cutest.opts  # gfortran
   echo "2" >> cutest.opts  # build all tools except Matlab
   echo "5" >> cutest.opts  # gcc
-  export MYARCH=pc.mgw.gfo
+  export MYARCH=pc64.mgw.gfo
 fi
 echo "nnydy" >> cutest.opts
 ./install_cutest < cutest.opts
@@ -87,7 +86,6 @@ if [[ "${target}" == *-apple-* ]]; then
     extra="-Wl,-undefined -Wl,dynamic_lookup -headerpad_max_install_names"
 fi
 cd $CUTEST/objects/$MYARCH/double
-echo "gfortran -fPIC -shared ${extra} -Wl,${all_load} libcutest.a -Wl,${noall_load} -o libcutest_double.${dlext}"
 gfortran -fPIC -shared ${extra} -Wl,${all_load} libcutest.a -Wl,${noall_load} -o libcutest_double.${dlext}
 cd $CUTEST/objects/$MYARCH/single
 gfortran -fPIC -shared ${extra} -Wl,${all_load} libcutest.a -Wl,${noall_load} -o libcutest_single.${dlext}
@@ -95,6 +93,7 @@ gfortran -fPIC -shared ${extra} -Wl,${all_load} libcutest.a -Wl,${noall_load} -o
 ln -s $SIFDECODE/bin/sifdecoder ${bindir}/
 ln -s $SIFDECODE/objects/$MYARCH/double/slct ${bindir}/
 ln -s $SIFDECODE/objects/$MYARCH/double/clsf ${bindir}/
+install_license $CUTEST/lgpl-3.0.txt
 """
 
 # These are the platforms we will build for by default, unless further
