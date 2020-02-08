aa5f4cfbe6987cb1952f2f8b6431bdd72d3765f1
Elliot Saba <staticfloat@gmail.com>
2020-02-07 19:18:53 -0500

    [AZP] Fix `init_jll_package()` race condition
    Because we have multiple workers building at the same time, and because
    we share a Julia package repository, we can occasionally hit race
    conditions such as having 8 workers all try to initialize a JLL package
    directory at the same time.  This should avoid that, and also avoid the
    ENOTFASTFORWARD errors we get from git every now and then when we have a
    dirty JLL package directory lying around.

--- .ci/download_sources.jl
+++ .ci/download_sources.jl
@@ -1,5 +1,5 @@
-using BinaryBuilder
-using BinaryBuilder: download_source, sourcify
+using BinaryBuilder, Pkg
+using BinaryBuilder: download_source, sourcify, init_jll_package
 
 # Read in input `.json` file
 json = String(read(ARGS[1]))
@@ -15,6 +15,14 @@ BinaryBuilder.cleanup_merged_object!(merged)
 
 # Download all sources
 download_source.(sourcify.(merged["sources"]); verbose=true)
+
+# Also initialize JLL package directories
+src_name = merged["name"]
+code_dir = joinpath(Pkg.devdir(), "$(src_name)_jll")
+deploy_repo = "JuliaBinaryWrappers/$(src_name)_jll.jl"
+# Always start from a clean slate
+rm(code_dir; recursive=true, force=true)
+init_jll_package(src_name, code_dir, deploy_repo)
 
 # Then export platforms to file
 open(ARGS[2], "w") do io
