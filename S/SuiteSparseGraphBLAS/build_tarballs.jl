95acd2b938325b235a3e9a7ec9eff957964eb84c
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-09 17:16:55 +0000

    [SuiteSparseGraphBLAS] Use new types for sources and dependencies

--- S/SuiteSparseGraphBLAS/build_tarballs.jl
+++ S/SuiteSparseGraphBLAS/build_tarballs.jl
@@ -5,8 +5,8 @@ version = v"5.6.0"
 
 # Collection of sources required to build SuiteSparse
 sources = [
-    "https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v$(version).tar.gz" =>
-    "76d34d9f6dafc592b69af14f58c1dc59e24853dcd7c2e8f4c98ffa223f6a1adb"
+    FileSource("https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v$(version).tar.gz",
+               "76d34d9f6dafc592b69af14f58c1dc59e24853dcd7c2e8f4c98ffa223f6a1adb")
 ]
 
 # Bash recipe for building across all platforms
@@ -34,7 +34,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "CompilerSupportLibraries_jll",
+    Dependency("CompilerSupportLibraries_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
