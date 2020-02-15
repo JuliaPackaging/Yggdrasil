882686d470c1bd1c096151a23d6ada7dbae20232
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-15 14:32:45 +0000

    [COINBLAS] Add patch to build shared library for PowerPC

--- C/COINBLAS/build_tarballs.jl
+++ C/COINBLAS/build_tarballs.jl
@@ -8,13 +8,20 @@ version = v"1.4.8"
 # Collection of sources required to complete build
 sources = [
     FileSource("https://github.com/coin-or-tools/ThirdParty-Blas/archive/releases/1.4.8.tar.gz", "781283ede62fc58e4eabcd0da00e8853e7001246a0fc4d7dd63207681ef1afff"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/ThirdParty-Blas-*/
 update_configure_scripts
-./get.Blas 
+
+if [[ "${target}" == powerpc64le-linux-gnu ]]; then
+    # Patch configure to be able to build shared library for PowerPC
+    atomic_patch -p1 ../patches/configure_shared_library_powerpc.patch
+fi
+
+./get.Blas
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} \
     --with-pic \
     --disable-pkg-config \
@@ -28,20 +35,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
-platforms = expand_gfortran_versions(platforms)
+platforms = expand_gfortran_versions(filter!(p -> !isa(p, FreeBSD), supported_platforms()))
 
 # The products that we will ensure are always built
 products = [
