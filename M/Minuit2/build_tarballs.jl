43e9002adfacb2e73fd9aa0a6dc78a9525aab2d2
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-16 22:18:57 +0000

    [Minuit2] Build shared library for Windows

--- M/Minuit2/build_tarballs.jl
+++ M/Minuit2/build_tarballs.jl
@@ -18,6 +18,15 @@ mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j ${nproc}
 make install
+
+if [[ "${target}" == *-mingw* ]]; then
+    # We have to manually build the shared library
+    mkdir -p "${libdir}"
+    cd "${prefix}/lib"
+    ar x libMinuit2.dll.a
+    cc -shared -o "${libdir}/libMinuit2.dll" *.o
+    rm *.o
+fi
 """
 
 # These are the platforms we will build for by default, unless further
