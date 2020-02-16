cac625965d745ec5f7f016d340c9a49abe75e99e
Martijn Visser <mgvisser@gmail.com>
2020-02-16 17:28:44 +0100

    New Recipe: Bison v3.5.2

---
+++ B/Bison/build_tarballs.jl
@@ -0,0 +1,48 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Bison"
+version = v"3.5.2"
+
+# Collection of sources required to complete build
+sources = [
+    FileSource("https://ftp.gnu.org/gnu/bison/bison-3.5.2.tar.xz", "24e273db9eb6da8bbb6f0648284d0724a5cbd6268a163db402f961350a4e50dd"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/bison-*
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64)
+]
+
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("bison", :bison_exe)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
