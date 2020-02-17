7b5916cbde532419a2fb41aff034b795e1c27cb2
Jan Strube <jan.strube@gmail.com>
2020-02-16 18:42:18 -0800

    Update build_tarballs.jl

--- M/Minuit2/build_tarballs.jl
+++ M/Minuit2/build_tarballs.jl
@@ -13,6 +13,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/root-*/math/minuit2
+sed -i '/add_library(Minuit2$/a SHARED' src/CMakeLists.txt
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_FLAGS=-DMATHCORE_STANDALONE=1 ..
 make -j ${nproc}
