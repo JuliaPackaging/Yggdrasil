74959feb52e7917dd206ccfde5cf0c00acb191bd
Spencer Russell <spencer.f.russell@gmail.com>
2020-02-17 11:12:26 -0600

    adds patch for ALSA return value to libportaudio (#521)
    * adds patch for ALSA return value
    
    Also:
    - fixes some BinaryBuilder deprecations
    - removes pa_shim.c cruft
    - specifies ALSA lib (cmake was finding the system lib)
    
    * FileSource -> ArchiveSource in libportaudio
    
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>
    
    * libportaudio - sets CMAKE_PREFIX_PATH to find ALSA
    
    * portaudio - sets alsa env vars conditionally
    
    * portaudio - suggested fixes from mose
    
    * portaudio - now trying with CMAKE_FIND_ROOT_PATH
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/libportaudio/build_tarballs.jl
+++ L/libportaudio/build_tarballs.jl
@@ -8,18 +8,18 @@ version = v"19.6.0"
 # Collection of sources required to build libportaudio. Not all of these
 # are used for all platforms.
 sources = [
-    "http://portaudio.com/archives/pa_stable_v190600_20161030.tgz" =>
-    "f5a21d7dcd6ee84397446fa1fa1a0675bb2e8a4a6dceb4305a8404698d8d1513",
+    ArchiveSource("http://portaudio.com/archives/pa_stable_v190600_20161030.tgz",
+               "f5a21d7dcd6ee84397446fa1fa1a0675bb2e8a4a6dceb4305a8404698d8d1513"),
 
-    # This includes the sources for libpa_shim
-    "./bundled",
+    # This includes a patch
+    DirectorySource("./bundled"),
 
     # uncomment the following lines to include ASIO support. To distribute the
     # resulting binaries you'll need to sign the licence agreement included with
     # the SDK at: https://www.steinberg.net/en/company/developers.html
 
-    # "http://www.steinberg.net/sdk_downloads/ASIOSDK2.3.1.zip" =>
-    #     "31074764475059448a9b7a56f103f4723ed60465e0e9d1a9446ca03dcf840f04"
+    # FileSource("http://www.steinberg.net/sdk_downloads/ASIOSDK2.3.1.zip",
+    #            "31074764475059448a9b7a56f103f4723ed60465e0e9d1a9446ca03dcf840f04")
 ]
 
 # Bash recipe for building across all platforms
@@ -31,15 +31,8 @@ if [ -d "asiosdk2.3.1" ]; then
     mv "asiosdk2.3.1 svnrev312937/ASIOSDK2.3.1" asiosdk2.3.1
 fi
 
-# Add the ringbuffer symbols needed by `pa_shim.c` to the windows export lists:
-IDX=80
-for SYM in PaUtil_GetRingBufferWriteAvailable \
-           PaUtil_GetRingBufferReadAvailable \
-           PaUtil_WriteRingBuffer \
-           PaUtil_ReadRingBuffer; do
-    echo "${SYM}    @${IDX}" >> ${WORKSPACE}/srcdir/portaudio/cmake_support/template_portaudio.def
-    IDX=$((IDX+1))
-done
+# apply the patch
+patch -dportaudio -p1 < portaudio_alsa_epipe_v3.diff
 
 # First, build libportaudio
 mkdir build
@@ -47,16 +40,11 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" \
     -DCMAKE_DISABLE_FIND_PACKAGE_PkgConfig=ON \
+    -DCMAKE_FIND_ROOT_PATH=$prefix \
     ../portaudio/
 make
 make install
 install_license "${WORKSPACE}/srcdir/portaudio/LICENSE.txt"
-
-# Next, build libpa_shim.  Note that we explicitly install to `${prefix}/lib` since that's
-# what `portaudio` does, and we need to be together to get auto-moved.
-cd ${WORKSPACE}/srcdir
-SOURCEHASH=$(sha256sum pa_shim.c  | awk '{print $1}')
-${CC} -O2 -fPIC '-DSOURCEHASH="${SOURCEHASH}"' -I${WORKSPACE}/srcdir/portaudio/include -I${WORKSPACE}/srcdir/portaudio/src/common pa_shim.c -lportaudio -o ${prefix}/lib/libpa_shim.${dlext} -shared
 """
 
 # These are the platforms we will build for by default, unless further
@@ -66,13 +54,12 @@ platforms = supported_platforms()
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libportaudio", :libportaudio),
-    LibraryProduct("libpa_shim", :libpa_shim),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
     # Build against `ALSA` on Linux
-    "alsa_jll",
+    Dependency("alsa_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
