f55e0f0d3161567d62b74cefc1adc589e1a718ef
Ryan Curtin <ryan@ratml.org>
2020-02-20 20:40:49 -0500

    Enable LAPACK in Armadillo (#538)
    * Manually enable ARMA_USE_LAPACK so LAPACK functionality is available.
    
    * Update to latest stable version.
    
    * Use ArchiveSource.
    
    * Use LAPACK_LIBRARY instead of hacking after CMake.
    
    * [armadillo] Rename extra symbols also for Windows
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- A/armadillo/build_tarballs.jl
+++ A/armadillo/build_tarballs.jl
@@ -4,10 +4,12 @@
 using BinaryBuilder
 
 name = "armadillo"
-version = v"9.800.3"
+version = v"9.850.1"
 sources = [
-    ("http://sourceforge.net/projects/arma/files/armadillo-9.800.3.tar.xz" =>
-        "a481e1dc880b7cb352f8a28b67fe005dc1117d4341277f12999a2355d40d7599")]
+    ArchiveSource("http://sourceforge.net/projects/arma/files/armadillo-9.850.1.tar.xz",
+                  "d4c389b9597a5731500ad7a2656c11a6031757aaaadbcafdea5cc8ac0fd2c01f")
+]
+
 script = raw"""
 cd ${WORKSPACE}/srcdir/armadillo-*/
 mkdir build && cd build
@@ -19,18 +21,25 @@ FLAGS=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}
 
 if [[ "${nbits}" == 64 ]] && [[ "${target}" != aarch64* ]]; then
     FLAGS+=(-Dopenblas_LIBRARY="${libdir}/libopenblas64_.${dlext}")
+    # Force Armadillo's CMake configuration to accept OpenBLAS as a LAPACK
+    # replacement.
+    FLAGS+=(-DLAPACK_LIBRARY="${libdir}/libopenblas64_.${dlext}")
 
     SYMB_DEFS=()
     for sym in sasum dasum snrm2 dnrm2 sdot ddot sgemv dgemv cgemv zgemv sgemm dgemm cgemm zgemm ssyrk dsyrk cherk zherk; do
         SYMB_DEFS+=("-D${sym}=${sym}_64")
     done
-    if [[ "${target}" == *-apple-* ]]; then
+    if [[ "${target}" == *-apple-* ]] || [[ "${target}" == *-mingw* ]]; then
         FLAGS+=(-DALLOW_OPENBLAS_MACOS=ON)
         for sym in cgbcon cgbsv cgbsvx cgbtrf cgbtrs cgecon cgees cgeev cgeevx cgehrd cgels cgelsd cgemm cgemv cgeqrf cgesdd cgesv cgesvd cgesvx cgetrf cgetri cgetrs cgges cggev cgtsv cgtsvx cheev cheevd cherk clangb clange clanhe clansy cpbtrf cpocon cposv cposvx cpotrf cpotri cpotrs ctrcon ctrsyl ctrtri ctrtrs cungqr dasum ddot dgbcon dgbsv dgbsvx dgbtrf dgbtrs dgecon dgees dgeev dgeevx dgehrd dgels dgelsd dgemm dgemv dgeqrf dgesdd dgesv dgesvd dgesvx dgetrf dgetri dgetrs dgges dggev dgtsv dgtsvx dlahqr dlangb dlange dlansy dlarnv dnrm2 dorgqr dpbtrf dpocon dposv dposvx dpotrf dpotri dpotrs dstedc dsyev dsyevd dsyrk dtrcon dtrevc dtrsyl dtrtri dtrtrs ilaenv sasum sdot sgbcon sgbsv sgbsvx sgbtrf sgbtrs sgecon sgees sgeev sgeevx sgehrd sgels sgelsd sgemm sgemv sgeqrf sgesdd sgesv sgesvd sgesvx sgetrf sgetri sgetrs sgges sggev sgtsv sgtsvx slahqr slangb slange slansy slarnv snrm2 sorgqr spbtrf spocon sposv sposvx spotrf spotri spotrs sstedc ssyev ssyevd ssyrk strcon strevc strsyl strtri strtrs zgbcon zgbsv zgbsvx zgbtrf zgbtrs zgecon zgees zgeev zgeevx zgehrd zgels zgelsd zgemm zgemv zgeqrf zgesdd zgesv zgesvd zgesvx zgetrf zgetri zgetrs zgges zggev zgtsv zgtsvx zheev zheevd zherk zlangb zlange zlanhe zlansy zpbtrf zpocon zposv zposvx zpotrf zpotri zpotrs ztrcon ztrsyl ztrtri ztrtrs zungqr; do
             SYMB_DEFS+=("-D${sym}=${sym}_64")
         done
     fi
     export CXXFLAGS="${SYMB_DEFS[@]}"
+else
+    # Force Armadillo's CMake configuration to accept OpenBLAS as a LAPACK
+    # replacement.
+    FLAGS+=(-DLAPACK_LIBRARY="${libdir}/libopenblas.${dlext}")
 fi
 
 cmake .. "${FLAGS[@]}"
@@ -57,8 +66,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built.
 dependencies = [
-    "OpenBLAS_jll"
+    Dependency("OpenBLAS_jll")
 ]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products,
-        dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
