1f16f7e7c9b72d59345c4a7740f172d1a4531233
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-02-21 15:54:24 -0500

    Test the compilation of NOMAD with GCC 8.1

--- N/NOMAD/build_tarballs.jl
+++ N/NOMAD/build_tarballs.jl
@@ -36,4 +36,4 @@ products = [
 dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8.1.0")
