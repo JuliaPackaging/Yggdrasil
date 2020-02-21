4f720c6f875cf50b9db440e97da039a2d99ff817
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-21 23:47:44 +0000

    [NOMAD] Add patch to build for Musl

--- N/NOMAD/build_tarballs.jl
+++ N/NOMAD/build_tarballs.jl
@@ -5,21 +5,25 @@ version = v"4.0.0"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://gist.github.com/amontoison/06dac8b63424854f754264597af6b09e/raw/8ab9aa6cb54b5491bc4f46977b086ff17bdd2ba8/NOMAD.zip", "84c0f2a01928b8d2c68303709e74173a296d39b014810c658b43662a6a3f93a1")
+    ArchiveSource("https://gist.github.com/amontoison/06dac8b63424854f754264597af6b09e/raw/8ab9aa6cb54b5491bc4f46977b086ff17bdd2ba8/NOMAD.zip", "84c0f2a01928b8d2c68303709e74173a296d39b014810c658b43662a6a3f93a1"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd NOMAD/build
+cd "${WORKSPACE}/srcdir/NOMAD"
+if [[ "${target}" == *-musl* ]]; then
+    atomic_patch -p1 "${WORKSPACE}/srcdir/patches/include_sys_time_missing_timeval_musl.patch"
+fi
+cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
-make
+make -j${nproc}
 make install
-exit
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = expand_cxxstring_abis(supported_platforms())
 
 
 # The products that we will ensure are always built
