f0cc5735a4408ae224fa6d936a89fe0af40f6d01
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-22 21:22:43 +0000

    [PARMETIS] Don't call the useless Makefile

--- P/PARMETIS/build_tarballs.jl
+++ P/PARMETIS/build_tarballs.jl
@@ -7,9 +7,9 @@ version = v"4.0.3"
 # The patch prevents building the source of METIS that ships with PARMETIS;
 # we rely on METIS_jll instead.
 sources = [
-    "http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz" =>
-    "f2d9a231b7cf97f1fee6e8c9663113ebf6c240d407d3c118c55b3633d6be6e5f",
-    "./bundled",
+    ArchiveSource("http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz",
+                  "f2d9a231b7cf97f1fee6e8c9663113ebf6c240d407d3c118c55b3633d6be6e5f"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
@@ -21,7 +21,17 @@ for f in ${WORKSPACE}/srcdir/patches/*.patch; do
   atomic_patch -p1 ${f}
 done
 
-make -j${nproc} config prefix=${prefix} shared=1
+cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DSHARED=1 \
+    -DGKLIB_PATH=$(realpath ../metis/GKlib) \
+    -DMETIS_PATH=$(realpath ../metis) \
+    -DMPI_INCLUDE_PATH="${prefix}/include" \
+    -DMPI_LIBRARIES="mpi"
+make -j${nproc}
 make install
 """
 
@@ -38,8 +48,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-  "METIS_jll",
-  "MPICH_jll"
+    Dependency("METIS_jll"),
+    Dependency("MPICH_jll")
 ]
 
 # Build the tarballs.
