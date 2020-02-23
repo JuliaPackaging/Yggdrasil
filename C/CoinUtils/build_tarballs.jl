8b353623760fb6a539163c9c36ba35140884ff56
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-23 00:14:24 +0000

    [CointUtils] Build for most platforms

--- C/CoinUtils/build_tarballs.jl
+++ C/CoinUtils/build_tarballs.jl
@@ -7,24 +7,56 @@ version = v"2.11.4"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/coin-or/CoinUtils/archive/releases/2.11.4.tar.gz", "d4effff4452e73356eed9f889efd9c44fe9cd68bd37b608a5ebb2c58bd45ef81")
+    ArchiveSource("https://github.com/coin-or/CoinUtils/archive/releases/$(version).tar.gz",
+                  "d4effff4452e73356eed9f889efd9c44fe9cd68bd37b608a5ebb2c58bd45ef81"),
+    ArchiveSource("https://github.com/coin-or-tools/BuildTools/archive/releases/0.8.10.tar.gz",
+                  "6b149acb304bf6fa0d8c468a03b1f67baaf981916b016bc32db018fa512e4f88"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/CoinUtils-*/
-update_configure_scripts 
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+update_configure_scripts
+
+if [[ "${target}" == powerpc64le-* ]] || [[ "${target}" == *-freebsd* ]] ; then
+    # It looks like the directory with the definition of the M4 macros *must* be
+    # called `BuildTools` and stay under the current directory.
+    mv ../BuildTools-releases-0.8.10/ BuildTools
+    # Patch `configure.ac` to look for this directory
+    atomic_patch -p1 ../patches/configure_add_config_macro_dir.patch
+    # Run autoreconf to be able to build the shared libraries for PowerPC and FreeBSD
+    autoreconf -vi
+fi
+
+# Remove wrong libtool files
+rm -f /opt/${target}/${target}/lib64/*.la
+rm -f /opt/${target}/${target}/lib/*.la
+
+if [[ "${nbits}" == 64 ]] && [[ "${target}" != aarch64-* ]]; then
+    OPENBLAS=openblas64_
+else
+    OPENBLAS=openblas
+fi
+
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} \
+    --with-blas-lib="-l${OPENBLAS}"
 make -j${nproc}
 make install
+
+if [[ "${target}" == *-mingw* ]]; then
+    # Manually build the shared library
+    cd "${prefix}/lib"
+    ar x libCoinUtils.a
+    c++ -shared -o "${libdir}/libCoinUtils.${dlext}" *.o -l${OPENBLAS}
+    rm *.o
+fi
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc)
-]
-
+platforms = filter!(p -> !(isa(p, FreeBSD) || p == Linux(:powerpc64le, libc=:glibc)), supported_platforms())
+platforms = expand_gfortran_versions(expand_cxxstring_abis(platforms))
 
 # The products that we will ensure are always built
 products = [
@@ -34,7 +66,6 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="OpenBLAS_jll", uuid="4536629a-c528-5b80-bd46-f80d51c5b363"))
-    Dependency(PackageSpec(name="METIS_jll", uuid="d00139f3-1899-568f-a2f0-47f597d42d70"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
