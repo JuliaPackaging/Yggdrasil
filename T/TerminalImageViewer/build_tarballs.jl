778f5718a13d534160aeac078645d5a94b8efadb
Mosè Giordano <m.giordano@ucl.ac.uk>
2020-02-24 13:55:01 +0000

    [TerminalImageViewer] Rename from `tiv`

--- T/TerminalImageViewer/build_tarballs.jl
+++ T/TerminalImageViewer/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "tiv"
+name = "TerminalImageViewer"
 version = v"1.0.0"
 
 # Collection of sources required to complete build
