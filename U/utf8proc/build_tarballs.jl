ec22a81a11a6ec2fe2642f6dce9900ff4692246f
Elliot Saba <staticfloat@gmail.com>
2020-02-29 01:28:18 -0800

    Add `utf8proc` `v2.4.0`

---
+++ U/utf8proc/build_tarballs.jl
@@ -0,0 +1,33 @@
+using BinaryBuilder
+
+name = "utf8proc"
+version = v"2.4.0"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://github.com/JuliaStrings/utf8proc/archive/v$(version).tar.gz",
+                  "b2e5d547c1d94762a6d03a7e05cea46092aab68636460ff8648f1295e2cdfbd7"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/utf8proc-*
+make -j${nproc}
+make install prefix=${prefix} libdir=${libdir}
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libutf8proc", :libutf8proc),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
