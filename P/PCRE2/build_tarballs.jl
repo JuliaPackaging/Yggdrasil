09ef2e39fae91662013889d7b4ae05204b9ffba9
Elliot Saba <staticfloat@gmail.com>
2020-02-28 23:06:17 -0800

    Fix product name

--- P/PCRE2/build_tarballs.jl
+++ P/PCRE2/build_tarballs.jl
@@ -35,7 +35,7 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libpcre", :libpcre)
+    LibraryProduct("libpcre2", :libpcre)
 ]
 
 # Dependencies that must be installed before this package can be built
