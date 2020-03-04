22f67afb85c29f02c1f9cb83461009e99c79e619
Dominique <dominique.orban@gmail.com>
2020-03-04 05:56:50 -0500

    MUMPS: new builder (#588)

---
+++ M/MUMPS/build_tarballs.jl
@@ -0,0 +1,99 @@
+using BinaryBuilder
+
+name = "MUMPS"
+version = v"5.2.1"
+
+# Collection of sources required to build PARMETIS.
+# The patch prevents building the source of METIS that ships with PARMETIS;
+# we rely on METIS_jll instead.
+sources = [
+  ArchiveSource("http://mumps.enseeiht.fr/MUMPS_5.2.1.tar.gz",
+                "d988fc34dfc8f5eee0533e361052a972aa69cc39ab193e7f987178d24981744a"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+mkdir -p ${libdir}
+cd $WORKSPACE/srcdir/MUMPS_5.2.1
+
+OPENBLAS=(-lopenblas)
+if [[ ${nbits} == 64 ]] && [[ ${target} != aarch64* ]]; then
+  OPENBLAS=(-lopenblas64_)
+fi
+
+makefile="Makefile.G95.PAR"
+cp Make.inc/${makefile} Makefile.inc
+
+make_args+=(OPTF=-O
+            CDEFS=-DAdd_
+            LMETISDIR=/workspace/destdir/lib
+            IMETIS=/workspace/destdir/include
+            LMETIS="-lparmetis -lmetis"
+            ORDERINGSF="-Dpord -Dparmetis"
+            CC="mpicc -fPIC"
+            FC="mpif90 -fPIC"
+            FL="mpif90 -fPIC"
+            SCALAP=-lscalapack
+            INCPAR=  # Let MPI compilers fill in the blanks
+            LIBPAR=-lscalapack
+            LIBBLAS=${OPENBLAS})
+
+if [[ "${target}" == *-apple* ]]; then
+  make_args+=(RANLIB=echo)
+fi
+
+# NB: parallel build fails
+make alllib "${make_args[@]}"
+
+# build shared libs
+all_load="--whole-archive"
+noall_load="--no-whole-archive"
+extra=""
+if [[ "${target}" == *-apple-* ]]; then
+    all_load="-all_load"
+    noall_load="-noall_load"
+    extra="-Wl,-undefined -Wl,dynamic_lookup -headerpad_max_install_names"
+fi
+
+cd lib
+libs=(-lparmetis -lmetis -lscalapack ${OPENBLAS})
+mpif90 -fPIC -shared -Wl,${all_load} libpord.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o libpord.${dlext}
+cp libpord.${dlext} ${libdir}
+
+libs+=(-L${libdir} -lpord)
+mpif90 -fPIC -shared -Wl,${all_load} libmumps_common.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o libmumps_common.${dlext}
+cp libmumps_common.${dlext} ${libdir}
+
+libs+=(-lmumps_common)
+for libname in cmumps dmumps smumps zmumps
+do
+  mpif90 -fPIC -shared -Wl,${all_load} lib${libname}.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o lib${libname}.${dlext}
+done
+cp *.${dlext} ${libdir}
+cd ..
+
+cp include/* ${prefix}/include
+"""
+
+# OpenMPI and MPICH are not precompiled for Windows
+# SCALAPACK doesn't build on PowerPC
+platforms = expand_gfortran_versions(filter!(p -> !isa(p, Windows) && arch(p) != :powerpc64le, supported_platforms()))
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libsmumps", :libsmumps),
+    LibraryProduct("libdmumps", :libdmumps),
+    LibraryProduct("libcmumps", :libcmumps),
+    LibraryProduct("libzmumps", :libzmumps),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("MPICH_jll"),
+    Dependency("METIS_jll"),
+    Dependency("PARMETIS_jll"),
+    Dependency("SCALAPACK_jll"),
+]
+
+# Build the tarballs.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
