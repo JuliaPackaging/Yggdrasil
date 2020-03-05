c49912ee6c48b8c7c4575c1379d4cc66f8bd748b
Mosè Giordano <mose@gnu.org>
2020-03-05 11:50:48 +0000

    [Edlib] Rename to preferred capitalisation

--- E/Edlib/build_tarballs.jl
+++ E/Edlib/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "edlib"
+name = "Edlib"
 version = v"1.2.6"
 
 # Collection of sources required to complete build
