2c61606f674f56609c2f49876ec327f0c87361cf
Mosè Giordano <mose@gnu.org>
2020-03-09 19:47:08 +0000

    [OpenSpecFun] Rebuild JLL package to fix selection of armv7l binaries

--- O/OpenSpecFun/build_tarballs.jl
+++ O/OpenSpecFun/build_tarballs.jl
@@ -43,5 +43,5 @@ dependencies = [
     Dependency("CompilerSupportLibraries_jll"),
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
+# Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
