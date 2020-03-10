09538d2f898fe6984d7065a27dfa9837bf7b91dd
Viral B. Shah <viral@mayin.org>
2020-03-10 14:05:36 -0400

    [SuiteSparseGraphBLAS] Add SuiteSparseGraphBLAS (#336)
    * Add SuiteSparseGraphBLAS
    
    * Update S/SuiteSparseGraphBLAS/build_tarballs.jl
    
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>
    
    * enable shared libraries for windows
    
    * [SuiteSparseGraphBLAS] Manually install shared library for Windows
    
    * fix for openmp
    
    * [SuiteSparseGraphBLAS] Use new types for sources and dependencies
    
    * Update build_tarballs.jl
    
    * [SuiteSparseGraphBLAS] Disable threading support
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    Co-authored-by: Mosè Giordano <m.giordano@ucl.ac.uk>

---
+++ S/SuiteSparseGraphBLAS/build_tarballs.jl
@@ -0,0 +1,41 @@
+using BinaryBuilder
+
+name = "SuiteSparseGraphBLAS"
+version = v"5.6.0"
+
+# Collection of sources required to build SuiteSparse
+sources = [
+    ArchiveSource("https://github.com/DrTimothyAldenDavis/SuiteSparse/archive/v$(version).tar.gz",
+                  "76d34d9f6dafc592b69af14f58c1dc59e24853dcd7c2e8f4c98ffa223f6a1adb")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+# Compile GraphBLAS
+cd $WORKSPACE/srcdir/SuiteSparse-*/GraphBLAS/build
+cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DUSER_NONE=1
+make -j${nproc} install
+if [[ ! -f "${libdir}/libgraphblas.${dlext}" ]]; then
+    # For mysterious reasons, the shared library is not installed
+    # when building for Windows
+    mkdir -p "${libdir}"
+    cp "libgraphblas.${dlext}" "${libdir}"
+fi
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libgraphblas", :libgraphblas),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6")
