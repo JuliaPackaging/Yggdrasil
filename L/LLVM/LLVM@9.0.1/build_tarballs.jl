1daabfd766cce355a3865bd5a213e2c669b9ab86
Mosè Giordano <mose@gnu.org>
2020-03-11 11:46:58 +0000

    Revert "[LLVM@9.0.1] Dummy change number"
    This reverts commit 0d7c42657972b20fa6adf45794eaf44e323f6eef.

--- L/LLVM/LLVM@9.0.1/build_tarballs.jl
+++ L/LLVM/LLVM@9.0.1/build_tarballs.jl
@@ -5,5 +5,5 @@ include("../common.jl")
 platforms = expand_cxxstring_abis(supported_platforms())
 name, sources, script, products = configure(ARGS, version)
 build_tarballs(ARGS, name, version, sources, script,
-               platforms, products, dependencies;
+               platforms, products, dependencies,
                preferred_gcc_version=v"7", preferred_llvm_version=v"8")
