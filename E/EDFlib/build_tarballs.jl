cec7a453d94c76b2c689eef856526ebead66b888
Alex Arslan <ararslan@comcast.net>
2020-03-14 11:14:04 -0700

    Add EDFlib (#620)
    * Add EDFlib
    
    EDFlib is a C library for reading and writing EDF and BDF files.
    
    * Install the README as a license

---
+++ E/EDFlib/build_tarballs.jl
@@ -0,0 +1,29 @@
+using BinaryBuilder
+
+name = "EDFlib"
+version = v"1.16.0"
+
+sources = [
+    ArchiveSource("https://www.teuniz.net/edflib/edflib_116.tar.gz",
+                  "cc9f9cc63869fa5742a7dd7e1aa3ff69fedcd4547f2c56ada43d4a4bfa4c6a4e";
+                  unpack_target="edflib")
+]
+
+script = raw"""
+cd ${WORKSPACE}/srcdir/edflib
+mkdir ${libdir}
+${CC} edflib.c -shared -fPIC -o ${libdir}/libedflib.${dlext}
+# No separate license file, it just lives in the README and in the source files
+install_license README.md
+"""
+
+platforms = supported_platforms()
+
+products = [
+    LibraryProduct("libedflib", :libedflib)
+]
+
+dependencies = [
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
