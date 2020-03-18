1fbfe5eb3abb6687383d0378364b5adb10915920
Michael F. Herbst <info@michael-herbst.com>
2020-03-18 17:27:32 +0100

    Add Libxc 4.3.4 build_tarballs.jl

---
+++ L/Libxc/build_tarballs.jl
@@ -0,0 +1,47 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Libxc"
+version = v"4.3.4"
+
+# Collection of sources required to complete build
+sources = [
+    "https://gitlab.com/libxc/libxc/-/archive/4.3.4/libxc-4.3.4.tar.gz" =>
+    "2d5878dd69f0fb68c5e97f46426581eed2226d1d86e3080f9aa99af604c65647",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd libxc-4.3.4/
+autoreconf -i
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --enable-shared
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64)
+]
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libxc", :libxc)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = []
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
