03775b446815dfd7bcbd3e43b526c38ef2ecdab2
Alex Arslan <ararslan@comcast.net>
2020-03-22 14:02:33 -0700

    Add Luna, software for sleep signal analysis (#634)
    * Add Luna, software for sleep signal analysis
    
    * Point things to the right places, I hope
    
    * [Luna] Fix patch, expand C++ string ABI
    
    * More location fixes
    
    * Add more patches
    
    * Try consolidating patch files
    
    * uggggggh
    
    * Try defining things
    
    * Try to fix macOS
    
    * Try to fix Windows
    
    * Grasping at straws at this point
    
    * mfw
    
    * Disable Windows for now
    
    * Try to fix Windows (guessing, really)
    
    * Reenable Windows
    
    * Explicitly link, use extensions
    
    * Screaming internally
    
    * Fix where things are installed
    
    Co-authored-by: Mosè Giordano <mose@gnu.org>

---
+++ L/Luna/build_tarballs.jl
@@ -0,0 +1,43 @@
+using BinaryBuilder
+
+name = "Luna"
+version = v"0.23.0"
+
+sources = [
+    GitSource("https://bitbucket.org/remnrem/luna-base.git",
+              "6d333a4034f7022ba4d1aa99ce2f8afddfd6832e"),
+    DirectorySource("./bundled"),
+]
+
+script = raw"""
+cd ${WORKSPACE}/srcdir/luna-base
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/fix_it.patch
+mkdir -p ${libdir}
+mkdir -p ${bindir}
+def_windows=""
+if [[ ${target} == *-apple-* ]]; then
+    suspicious_arch="MAC"
+elif [[ ${target} == *-mingw* ]]; then
+    suspicious_arch="WINDOWS"
+    def_windows="WINDOWS=1"
+else
+    suspicious_arch="LINUX"
+fi
+make -j${nproc} ARCH=${suspicious_arch} FFTW=${prefix} PREFIX=${prefix} LIBDIR=${libdir} ${def_windows}
+cp "luna${exeext}" "${bindir}/"
+cp "libluna.${dlext}" "${libdir}/"
+"""
+
+platforms = expand_cxxstring_abis(supported_platforms())
+
+products = [
+    ExecutableProduct("luna", :luna),
+    LibraryProduct("libluna", :libluna),
+]
+
+dependencies = [
+    Dependency("FFTW_jll"),
+    Dependency("Zlib_jll"),
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
