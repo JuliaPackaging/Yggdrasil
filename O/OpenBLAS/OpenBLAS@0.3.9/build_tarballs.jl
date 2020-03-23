1e3754fb49b64618bfd2d84478cd4d52baf7157f
Harmen Stoppels <harmenstoppels@gmail.com>
2020-03-23 17:29:24 +0100

    Use Dependency[]
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>

--- O/OpenBLAS/OpenBLAS@0.3.9/build_tarballs.jl
+++ O/OpenBLAS/OpenBLAS@0.3.9/build_tarballs.jl
@@ -11,8 +11,7 @@ sources = openblas_sources(version)
 script = openblas_script()
 platforms = openblas_platforms()
 products = openblas_products()
-dependencies = []
+dependencies = Dependency[]
 
 # Build the tarballs
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6")
-
