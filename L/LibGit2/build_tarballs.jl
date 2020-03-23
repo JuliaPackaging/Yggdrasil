8d36cec2210b5aa6ad5770ce4d88f2509d8c3828
Mosè Giordano <mose@gnu.org>
2020-03-23 11:40:34 +0000

    [LibGit2] Find header files

--- L/LibGit2/build_tarballs.jl
+++ L/LibGit2/build_tarballs.jl
@@ -5,9 +5,9 @@ version = v"0.99.0"
 
 # Collection of sources required to build libgit2
 sources = [
-   "https://github.com/libgit2/libgit2.git" =>
-   "172239021f7ba04fe7327647b213799853a9eb89",
-   "./bundled",
+    GitSource("https://github.com/libgit2/libgit2.git",
+              "172239021f7ba04fe7327647b213799853a9eb89"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
@@ -34,6 +34,7 @@ elif [[ ${target} == *linux* ]] || [[ ${target} == *freebsd* ]]; then
     # If we're on Linux or FreeBSD, explicitly ask for mbedTLS instead of OpenSSL
     BUILD_FLAGS+=(-DUSE_HTTPS=mbedTLS -DSHA1_BACKEND=CollisionDetection -DCMAKE_INSTALL_RPATH="\$ORIGIN")
 fi
+export CFLAGS="-I${prefix}/include"
 
 mkdir build && cd build
 
@@ -53,8 +54,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "MbedTLS_jll",
-    "LibSSH2_jll",
+    Dependency("MbedTLS_jll"),
+    Dependency("LibSSH2_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
