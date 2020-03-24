debe2af7fde2cdb8aeb84e1ad7f5530ba7c54e3b
Benjamin Lorenz <benlorenz@users.noreply.github.com>
2020-03-24 02:02:01 +0100

    [lrslib] more platforms (#646)
    requires a patch / a few workarounds for the custom makefile

--- L/lrslib/build_tarballs.jl
+++ L/lrslib/build_tarballs.jl
@@ -9,30 +9,49 @@ version = v"0.1.0"
 sources = [
     GitSource("https://github.com/JuliaPolyhedra/lrslib.git",
               "d8b723a2c315614979a8354f9e768d273d14a215"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/lrslib/
-export C_INCLUDE_PATH=$C_INCLUDE_PATH:$prefix/include/
-make install
+cd $WORKSPACE/srcdir/lrslib*
+extraargs=""
+cflags="-O3 -Wall"
+
+# 32bit linux, arm and windows:
+if [[ $target == i686* ]] || [[ $target == arm* ]]; then
+  # no 128 bit ... patch makefile
+  atomic_patch -p1 ../patches/no128bit.patch
+fi
+
+if [[ $target == *apple* ]]; then
+  sed -i -e 's#-Wl,-soname=#-install_name #' makefile
+  extraargs="SONAME=liblrs.0.dylib SHLINK=liblrs.dylib SHLIB=liblrs.0.0.0.dylib"
+elif [[ $target == *freebsd* ]]; then
+  export CC="$CC $LDFLAGS"
+elif [[ $target == *mingw* ]]; then
+  extraargs="SONAME=liblrs-0.dll SHLINK=liblrs.dll SHLIB=liblrs-0-0-0.dll"
+  cflags="$cflags -DSIGNALS -DTIMES"
+fi
+
+make prefix=$prefix INCLUDEDIR=$prefix/include LIBDIR=${libdir} CFLAGS="$cflags" $extraargs -j ${nproc} install
+if [[ $target == *mingw* ]]; then
+  # rename binaries and move libraries
+  for file in ${bindir}/{lrs,lrsnash,redund}; do mv $file $file.exe; done
+  mv ${prefix}/lib/*lrs*.dll ${libdir}/
+fi
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    FreeBSD(:x86_64)
-]
-
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("liblrs", :lrslib)
+    ExecutableProduct("lrs", :lrs)
+    ExecutableProduct("lrsnash", :lrsnash)
+    ExecutableProduct("redund", :redund)
+    LibraryProduct("liblrs", :liblrs)
 ]
 
 # Dependencies that must be installed before this package can be built
