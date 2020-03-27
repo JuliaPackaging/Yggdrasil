3fb6c85b984f2534c761c4dc4274abba18038ffe
Viral B. Shah <viral@mayin.org>
2020-03-27 18:58:22 -0400

    fix

--- X/XGBoost/build_tarballs.jl
+++ X/XGBoost/build_tarballs.jl
@@ -24,7 +24,7 @@ export CXX=g++
 
 # For Linux, build using CMake
 if [[ ${target} == *linux* ]]; then
-    (mkdir build; cd build; cmake .. -DCMAKE_INSTALL_PREFIX=${prefix}) -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}"
+    (mkdir build; cd build; cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}")
     make -C build -j ${nproc}
 else
     if [[ ${target} == *mingw* ]]; then
