8d6dbb63b9aa848d41390d29767e685fad30cea9
Viral B. Shah <viral@mayin.org>
2020-03-28 18:49:42 -0400

    Build GLPK (#676)
    Based on  https://github.com/JuliaOpt/GLPKBuilder/blob/master/build_tarballs.jl

---
+++ G/GLPK/build_tarballs.jl
@@ -0,0 +1,40 @@
+using BinaryBuilder, Pkg
+
+name = "GLPK"
+version = v"4.65"
+
+# Collection of sources required to build GLPKBuilder
+sources = [
+    ArchiveSource("http://ftpmirror.gnu.org/gnu/glpk/glpk-$(version.major).$(version.minor).tar.gz",
+                  "4281e29b628864dfe48d393a7bedd781e5b475387c20d8b0158f329994721a10"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/glpk*
+if [[ ${target} == *mingw* ]]; then
+    export CPPFLAGS="-I${prefix}/include -D__WOE__=1";
+else
+    export CPPFLAGS="-I${prefix}/include";
+fi
+autoreconf -vi
+./configure --prefix=${prefix} --host=${target} --build=${MACHTYPE} --with-gmp
+make -j${nproc}
+make install
+"""
+
+# Build for all platforms
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libglpk", :libglpk)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("GMP_jll")
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
