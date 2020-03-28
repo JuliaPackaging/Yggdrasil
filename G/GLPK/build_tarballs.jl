c41b3c286ca00f927fb37be8b4c78449b6f3a3c2
Viral B. Shah <viral@mayin.org>
2020-03-28 16:43:33 -0400

    fix

--- G/GLPK/build_tarballs.jl
+++ G/GLPK/build_tarballs.jl
@@ -30,8 +30,8 @@ make install
 platforms = supported_platforms()
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libglpk", :libglpk)
+products = [
+    LibraryProduct("libglpk", :libglpk)
 ]
 
 # Dependencies that must be installed before this package can be built
