1a9daf84e1fe92d535e143645578e33c8c604477
Viral B. Shah <viral@mayin.org>
2020-03-27 20:59:20 -0400

    Update XGBoost 0.82 (#657)
    Add xgboost binary as a product
    Depend on CompilerSupportLibraries_jll for openmp support
    Update the build script to be current

--- X/XGBoost/build_tarballs.jl
+++ X/XGBoost/build_tarballs.jl
@@ -4,9 +4,8 @@ using BinaryBuilder, Pkg
 name = "XGBoost"
 version = v"0.82"
 sources = [
-    "https://github.com/dmlc/xgboost.git"=>
-    "3f83dcd50286d7c8d22e552942bd6572547c32b9",
-    "./bundled",
+    GitSource("https://github.com/dmlc/xgboost.git","bf3241368256ddd010d30d98ffc8a0a005f166e9"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
@@ -25,14 +24,14 @@ export CXX=g++
 
 # For Linux, build using CMake
 if [[ ${target} == *linux* ]]; then
-    (mkdir build; cd build; cmake .. -DCMAKE_INSTALL_PREFIX=${prefix})
+    (mkdir build; cd build; cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}")
     make -C build -j ${nproc}
 else
     if [[ ${target} == *mingw* ]]; then
         # Target Windows specifically
         EXTRA_FLAGS=(UNAME=Windows)
     fi
-    
+
     # Otherwise, build with `make`, and do a minimal build
     cp make/minimum.mk config.mk
     make -j ${nproc} USE_OPENMP=1 ${EXTRA_FLAGS[@]}
@@ -43,17 +42,12 @@ mkdir -p ${prefix}/{bin,include,lib}
 cp -ra include/xgboost ${prefix}/include/
 cp -a xgboost ${prefix}/bin/xgboost${exeext}
 
-# Not every platform has a libxgboost.a
-cp -a lib/libxgboost.a ${prefix}/lib || true
-
 # We also need to bundle `libgomp`, so snarf it from the
 # compiler support directory while we copy our main bundle of joy
 if [[ ${target} == *mingw* ]]; then
     cp -a lib/xgboost.dll ${prefix}/bin
-    cp -a /opt/${target}/${target}/lib*/libgomp*.${dlext} ${prefix}/bin
 else
     cp -a lib/libxgboost.${dlext} ${prefix}/lib
-    cp -a /opt/${target}/${target}/lib*/libgomp*.${dlext} ${prefix}/lib
 fi
 """
 
@@ -68,12 +62,13 @@ platforms = [p for p in platforms if !(arch(p) == :powerpc64le)]
 # The products that we will ensure are always built
 products = [
     LibraryProduct(["libxgboost", "xgboost"], :libxgboost),
+    ExecutableProduct("xgboost", :xgboost)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    Dependency("CompilerSupportLibraries_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"5")
