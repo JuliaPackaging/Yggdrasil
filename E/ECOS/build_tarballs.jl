9ab934138e85e65e124df0126fe402ea7b20cecb
Viral B. Shah <viral@mayin.org>
2020-03-28 17:16:23 -0400

    build ecos

---
+++ E/ECOS/build_tarballs.jl
@@ -0,0 +1,38 @@
+using BinaryBuilder
+
+name = "ECOS"
+version = v"2.0.7"
+
+# Collection of sources required to build ECOSBuilder
+sources = [
+    GitSource("https://github.com/embotech/ecos.git", "2954b2a640f2194bf91dbf51e682be17012d7698")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/ecos*
+make shared
+
+if [[ $target = *mingw* ]]; then
+    mkdir $prefix/bin
+    cp libecos.dll $prefix/bin
+else
+    mkdir $prefix/lib
+    cp libecos.${dlext} $prefix/lib;
+fi
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libecos", :ecos)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
