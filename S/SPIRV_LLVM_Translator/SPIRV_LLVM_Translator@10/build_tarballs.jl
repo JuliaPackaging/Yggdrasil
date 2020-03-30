cca3ac96cc3d5c01f9bbf07303e3f1932e074b98
Tim Besard <tim.besard@gmail.com>
2020-03-30 11:43:17 +0200

    Add the SPIRV to LLVM translator.

---
+++ S/SPIRV_LLVM_Translator/SPIRV_LLVM_Translator@10/build_tarballs.jl
@@ -0,0 +1,16 @@
+include("../common.jl")
+
+version = v"10.0"
+
+# Collection of sources required to build attr
+sources = [GitSource(repo, "7743482f2053582be990e93ca46d15239c509c9d")]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    BuildDependency(PackageSpec(name="LLVM_full_jll", version=v"10.0.0")),
+    #Dependency(PackageSpec(name="libLLVM_jll", version=v"10.0.0"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               preferred_gcc_version=v"7")
---
+++ S/SPIRV_LLVM_Translator/common.jl
@@ -0,0 +1,42 @@
+using BinaryBuilder, Pkg
+
+name = "SPIRV_LLVM_Translator"
+repo = "https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git"
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_cxxstring_abis(supported_platforms())
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd SPIRV-LLVM-Translator
+install_license LICENSE.TXT
+
+CMAKE_FLAGS=()
+
+# Release build for best performance
+CMAKE_FLAGS+=(-DCMAKE_BUILD_TYPE=Release)
+
+# Install things into $prefix
+CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix})
+
+# Explicitly use our cmake toolchain file and tell CMake we're cross-compiling
+CMAKE_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN})
+CMAKE_FLAGS+=(-DCMAKE_CROSSCOMPILING:BOOL=ON)
+
+# Tell CMake where LLVM is
+CMAKE_FLAGS+=(-DLLVM_DIR="${prefix}/lib/cmake/llvm")
+
+# Build the library
+CMAKE_FLAGS+=(-DBUILD_SHARED_LIBS=ON)
+
+cmake -B build -S . -GNinja ${CMAKE_FLAGS[@]}
+ninja -C build llvm-spirv install
+install -Dm755 build/tools/llvm-spirv/llvm-spirv${exeext} -t ${bindir}
+"""
+
+# The products that we will ensure are always built
+products = Product[
+    LibraryProduct(["libLLVMSPIRVLib", "LLVMSPIRVLib"], :libLLVMSPIRV),
+    ExecutableProduct("llvm-spirv", :llvm_spirv),
+]
