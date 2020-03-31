2b6df1846d2b60aac6bcee30ae8527fc1e474d76
Ben Fogarty <42045513+fogarty-ben@users.noreply.github.com>
2020-03-31 21:13:15 +0000

    New Recipe: QuantReg v0.1.1 (#683)
    * New Recipe: QuantRegBuilder v0.1.1
    
    * added remedy for incompatibilities across major versions of GCC
    
    * updated QuantRegBuilder recipe
    
    * renamed QuantReg to follow convention
    
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Rename Q/QuantRegBuilder/build_tarballs.jl to Q/QuantReg/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ Q/QuantReg/build_tarballs.jl
@@ -0,0 +1,41 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "QuantReg"
+version = v"0.1.2"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/fogarty-ben/QuantReg.jl-Builder.git", "7800e5d4e4c2dfd89e8d9076d764b11a51f55081")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd QuantReg.jl-Builder/
+mkdir -p ${libdir}
+gfortran -fPIC -shared -std=legacy rqbr.f -o ${libdir}/librqbr.${dlext} 
+gfortran -fPIC -shared -std=legacy rqfnb.f -o ${libdir}/librqfnb.${dlext} 
+install_license LICENSE
+exit
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+platforms = expand_gfortran_versions(platforms)
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("librqfnb", :librqfnb),
+    LibraryProduct("librqbr", :librqbr)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
