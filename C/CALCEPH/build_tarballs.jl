0039243ca493416593221f9fc26eb5b386a511da
Helge Eichhorn <git@helgeeichhorn.de>
2020-03-31 18:16:39 +0200

    Install license

--- C/CALCEPH/build_tarballs.jl
+++ C/CALCEPH/build_tarballs.jl
@@ -22,6 +22,7 @@ autoreconf -fi
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --enable-fortran=no --enable-python=no --disable-static
 make -j${nproc}
 make install
+install_license ${WORKSPACE}/srcdir/COPYING_CECILL_B.LIB
 """
 
 # These are the platforms we will build for by default, unless further
