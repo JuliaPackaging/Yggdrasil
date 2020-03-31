f2268871b69171a60d1a6ac119c78a4941c7631d
Helge Eichhorn <git@helgeeichhorn.de>
2020-03-31 09:58:30 +0200

    New Recipe: CALCEPH v3.4.2

---
+++ C/CALCEPH/build_tarballs.jl
@@ -0,0 +1,41 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "CALCEPH"
+version = v"3.4.2"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://www.imcce.fr/content/medias/recherche/equipes/asd/calceph/calceph-3.4.2.tar.gz", "4bb95979ed77f431c6845b11b7bc49819836e47a8b9ceceff18309683f580c5b")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd calceph-3.4.2/
+if [[ ${target} == i686-w64* ]] || [[ ${target} == x86_64-w64* ]]; then
+    echo 'LT_INIT([win32-dll])' >> configure.ac;
+    sed -i '/^libcalceph_la_LDFLAGS/ s/$/ -no-undefined/' src/Makefile.am;
+fi
+autoreconf -fi
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --enable-fortran=no --enable-python=no --disable-static
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libcalceph", :libcalceph)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
