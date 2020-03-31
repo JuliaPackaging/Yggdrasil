0ea2cc6591f227a05103836e7c301fe9876c3ebc
Helge Eichhorn <git@helgeeichhorn.de>
2020-03-31 19:15:44 +0200

    Install license

--- C/CSPICE/build_tarballs.jl
+++ C/CSPICE/build_tarballs.jl
@@ -26,6 +26,7 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j${nproc}
 make install
+install_license ${WORKSPACE}/srcdir/license/LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
