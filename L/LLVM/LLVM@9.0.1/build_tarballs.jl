28a0287834a492b5a1152744931b7c9341a849e9
Valentin Churavy <v.churavy@gmail.com>
2020-04-02 08:33:02 -0400

    build LLVM and Clang for new libLLVM

--- L/LLVM/LLVM@9.0.1/build_tarballs.jl
+++ L/LLVM/LLVM@9.0.1/build_tarballs.jl
@@ -1,5 +1,5 @@
 name = "LLVM"
-libllvm_version = v"9.0.1+2"
+libllvm_version = v"9.0.1+4"
 
 include("../common.jl")
 build_tarballs(ARGS, configure_extraction(ARGS, libllvm_version, name)...; skip_audit=true)
