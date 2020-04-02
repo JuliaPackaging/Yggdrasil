dfe90ba97a97d76ea7d981c049a3c88034445c85
Valentin Churavy <v.churavy@gmail.com>
2020-04-02 19:57:55 -0400

    rebuild XSLT and Giflib for page alignment

--- G/Giflib/build_tarballs.jl
+++ G/Giflib/build_tarballs.jl
@@ -7,12 +7,12 @@ version = v"5.1.4"
 
 # Collection of sources required to build Giflib
 sources = [
-    "https://downloads.sourceforge.net/project/giflib/giflib-$(version).tar.bz2" =>
-    "df27ec3ff24671f80b29e6ab1c4971059c14ac3db95406884fc26574631ba8d5",
+    ArchiveSource("https://downloads.sourceforge.net/project/giflib/giflib-$(version).tar.bz2",
+                  "df27ec3ff24671f80b29e6ab1c4971059c14ac3db95406884fc26574631ba8d5"),
 
     # Apply security patch
-    "https://deb.debian.org/debian/pool/main/g/giflib/giflib_5.1.4-3.debian.tar.xz" =>
-    "767ea03c1948fa203626107ead3d8b08687a3478d6fbe4690986d545fb1d60bf",
+    ArchiveSource("https://deb.debian.org/debian/pool/main/g/giflib/giflib_5.1.4-3.debian.tar.xz",
+                  "767ea03c1948fa203626107ead3d8b08687a3478d6fbe4690986d545fb1d60bf"),
 ]
 
 # Bash recipe for building across all platforms
