a7130460bb3fa067838824829649ce4ce66ca19d
Valentin Churavy <v.churavy@gmail.com>
2020-04-02 20:14:17 -0400

    rebuild Glib

--- G/Glib/build_tarballs.jl
+++ G/Glib/build_tarballs.jl
@@ -5,8 +5,8 @@ version = v"2.59.0"
 
 # Collection of sources required to build Glib
 sources = [
-    "https://ftp.gnome.org/pub/gnome/sources/glib/$(version.major).$(version.minor)/glib-$(version).tar.xz" =>
-    "664a5dee7307384bb074955f8e5891c7cecece349bbcc8a8311890dc185b428e",
+    ArchiveSource("https://ftp.gnome.org/pub/gnome/sources/glib/$(version.major).$(version.minor)/glib-$(version).tar.xz",
+                  "664a5dee7307384bb074955f8e5891c7cecece349bbcc8a8311890dc185b428e"),
 ]
 
 # Bash recipe for building across all platforms
@@ -25,15 +25,16 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libgio", :libgio),
-    LibraryProduct("libglib", :libglib),
-    LibraryProduct("libgmodule", :libgmodule),
-    LibraryProduct("libgobject", :libgobject),
-    LibraryProduct("libgthread", :libgthread),
+    LibraryProduct(["libgio-2", "libgio-2.0"], :libgio),
+    LibraryProduct(["libglib-2", "libglib-2.0"], :libglib),
+    LibraryProduct(["libgmodule-2", "libgmodule-2.0"], :libgmodule),
+    LibraryProduct(["libgobject-2", "libgobject-2.0"], :libgobject),
+    LibraryProduct(["libgthread-2", "libgthread-2.0"], :libgthread),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    "Libiconv_jll",
     "Libffi_jll",
     "Gettext_jll",
     "PCRE_jll",
