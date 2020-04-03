84ff4bc652668dfecd878135543b0813a7b2789c
jaakkor2 <jaakkor2@users.noreply.github.com>
2020-04-03 07:37:10 +0200

    [Blosc] add recipe
    Compared to old builder https://github.com/stevengj/BloscBuilder/blob/master/build_tarballs.jl
    * Updates version from 0.14.3 to 0.18.1
    * Disables static lib
    * Uses external Zlib, Zstd, Lz4.  Snappy commented out, could be uncommented once available.

---
+++ B/Blosc/build_tarballs.jl
@@ -0,0 +1,48 @@
+using BinaryBuilder
+
+name = "Blosc"
+version = v"1.18.1"
+
+# Collection of sources required to build Blosc
+sources = [
+    "https://github.com/Blosc/c-blosc/archive/v1.18.1.tar.gz" =>
+    "18730e3d1139aadf4002759ef83c8327509a9fca140661deb1d050aebba35afb",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd c-blosc-1.18.1
+mkdir build
+cd build
+CMAKE_FLAGS=(-DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release)
+CMAKE_FLAGS+=(-DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF)
+CMAKE_FLAGS+=(-DBUILD_STATIC=OFF)
+CMAKE_FLAGS+=(-DPREFER_EXTERNAL_ZLIB=ON)
+CMAKE_FLAGS+=(-DPREFER_EXTERNAL_ZSTD=ON)
+CMAKE_FLAGS+=(-DPREFER_EXTERNAL_LZ4=ON)
+#CMAKE_FLAGS+=(-DPREFER_EXTERNAL_SNAPPY=ON)
+cmake ${CMAKE_FLAGS} ..
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libblosc", :libblosc)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+	"Zlib_jll",
+	"Zstd_jll",
+	"Lz4_jll",
+#	"Snappy_jll",
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
