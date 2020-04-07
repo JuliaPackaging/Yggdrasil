41e44a7faae2cad8629c5e0e3e4dd018cbb70cd7
Mosè Giordano <mose@gnu.org>
2020-04-07 10:18:45 +0100

    [Clipper] Rename product variable for consistency

--- C/Clipper/build_tarballs.jl
+++ C/Clipper/build_tarballs.jl
@@ -24,7 +24,7 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libcclipper", :cclipper)
+    LibraryProduct("libcclipper", :libcclipper)
 ]
 
 # Dependencies that must be installed before this package can be built
