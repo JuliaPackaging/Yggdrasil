164025aafa8bc7bc1f7720f0bc4cb2b0f0f7beb1
Viral B. Shah <viral@mayin.org>
2020-04-07 18:08:30 -0400

    [Clp] Add 1.17.3 (#788)
    * [Clp] Add 1.17.3

--- C/Coin-OR/Clp/build_tarballs.jl
+++ C/Coin-OR/Clp/build_tarballs.jl
@@ -1,21 +1,22 @@
 using BinaryBuilder, Pkg
 
 name = "Clp"
-version = v"1.17.5"
+version = v"1.17.3"
 
 # Collection of sources required to build Clp
 sources = [
     GitSource("https://github.com/coin-or/Clp.git", 
-    "29a3d29d94f102e9029eb4be72cde2bfd378d752"), 
+    "27f61df3a85e327b8f7f15f8f40b0f415f4cc4e9"), 
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/Clp*
-update_configure_scripts
 
-# Remove misleading libtool files
-rm -f ${prefix}/lib/*.la
+# Remove misleading libtool files                                                                                            
+rm -f ${prefix}/lib/*.la                                                                                                     
+rm -f /opt/${target}/${target}/lib*/*.la                                                                                     
+update_configure_scripts
 
 mkdir build
 cd build/
@@ -28,9 +29,12 @@ elif [[ ${target} == *linux* ]]; then
     export LDFLAGS="-ldl -lrt"
 fi
 
-../configure --prefix=$prefix --with-pic --disable-pkg-config --build=${MACHTYPE} --host=${target} --enable-shared \
---enable-dependency-linking lt_cv_deplibs_check_method=pass_all \
---with-asl-lib="-lasl" --with-asl-incdir="$prefix/include" \
+if [[ ${target} == *aarch64* ]] || [[ ${target} == *arm* ]]; then
+   export CPPFLAGS="${CPPFLAGS} -D__arm__"
+fi
+
+../configure --prefix=$prefix --with-pic --disable-pkg-config --build=${MACHTYPE} --host=${target} \
+--enable-shared lt_cv_deplibs_check_method=pass_all \
 --with-blas="-lopenblas" --with-lapack="-openblas" \
 --with-metis-lib="-lmetis" \
 --with-coinutils-lib="-lCoinUtils" \
@@ -55,10 +59,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(; name = "CoinUtils_jll", uuid = "be027038-0da8-5614-b30d-e42594cb92df", version = v"2.11.4")),
-    Dependency(PackageSpec(; name = "Osi_jll", uuid = "7da25872-d9ce-5375-a4d3-7a845f58efdd", version = v"0.108.6")),
+    Dependency(PackageSpec(; name = "CoinUtils_jll", uuid = "be027038-0da8-5614-b30d-e42594cb92df", version = v"2.11.3")),
+    Dependency(PackageSpec(; name = "Osi_jll", uuid = "7da25872-d9ce-5375-a4d3-7a845f58efdd", version = v"0.108.5")),
     Dependency(PackageSpec(; name = "METIS_jll", uuid = "d00139f3-1899-568f-a2f0-47f597d42d70", version = v"4.0.3")),
-    Dependency("ASL_jll"),
     Dependency("OpenBLAS32_jll"),
     Dependency("CompilerSupportLibraries_jll"),
 ]
