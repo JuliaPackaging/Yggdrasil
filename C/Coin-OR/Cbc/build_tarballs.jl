7feb4a04b4ef705d13cd6414cf4c6b7bcac72dfa
Viral B. Shah <viral@mayin.org>
2020-04-07 17:38:34 -0400

    [Cbc] Add 2.10.3

--- C/Coin-OR/Cbc/build_tarballs.jl
+++ C/Coin-OR/Cbc/build_tarballs.jl
@@ -1,23 +1,23 @@
 using BinaryBuilder, Pkg
 
 name = "Cbc"
-version = v"2.10.5"
+version = v"2.10.3"
 
 # Collection of sources required to build CbcBuilder
 sources = [
     GitSource("https://github.com/coin-or/Cbc.git",
-               "7b5ccc016f035f56614c8018b20d700978144e9f"),
+               "6fe3addaa76436d479d4431add67b371e11d3e83"),
     DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/Cbc*
-update_configure_scripts
 
-# Remove misleading libtool files
-rm -f ${prefix}/lib/*.la
-rm -f /opt/${target}/${target}/lib*/*.la
+# Remove misleading libtool files                                                                                            
+rm -f ${prefix}/lib/*.la                                                                                                     
+rm -f /opt/${target}/${target}/lib*/*.la                                                                                     
+update_configure_scripts
 
 # Apply patch related to https://github.com/JuliaOpt/Cbc.jl/issues/117 and https://github.com/coin-or/Cbc/issues/267
 (cd Cbc/src && atomic_patch -p0 $WORKSPACE/srcdir/patches/no_lp.patch)
@@ -25,17 +25,16 @@ rm -f /opt/${target}/${target}/lib*/*.la
 mkdir build
 cd build/
 
-export CPPFLAGS="-I${prefix}/include -I${prefix}/include/coin"
-export CXXFLAGS="-std=c++11"
+export CPPFLAGS="${CPPFLAGS} -I${prefix}/include -I$prefix/include/coin"
+export CXXFLAGS="${CXXFLAGS} -std=c++11"
 if [[ ${target} == *mingw* ]]; then
     export LDFLAGS="-L$prefix/bin"
 elif [[ ${target} == *linux* ]]; then
     export LDFLAGS="-ldl -lrt"
 fi
 
-../configure --prefix=$prefix --with-pic --disable-pkg-config  --build=${MACHTYPE} --host=${target} --enable-shared \
---enable-dependency-linking lt_cv_deplibs_check_method=pass_all \
---with-asl-lib="-lasl" --with-asl-incdir="$prefix/include/asl" \
+../configure --prefix=$prefix --with-pic --disable-pkg-config  --build=${MACHTYPE} --host=${target} \
+--enable-shared lt_cv_deplibs_check_method=pass_all \
 --with-blas-lib="-lopenblas" --with-lapack-lib="-lopenblas" \
 --with-metis-lib="-lmetis" \
 --with-coinutils-lib="-lCoinUtils" \
@@ -63,12 +62,11 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(; name = "Clp_jll", uuid = "06985876-5285-5a41-9fcb-8948a742cc53", version = v"1.17.5")),
-    Dependency(PackageSpec(; name = "Cgl_jll", uuid = "3830e938-1dd0-5f3e-8b8e-b3ee43226782", version = v"0.60.3")),
-    Dependency(PackageSpec(; name = "Osi_jll", uuid = "7da25872-d9ce-5375-a4d3-7a845f58efdd", version = v"0.108.6")),
-    Dependency(PackageSpec(; name = "CoinUtils_jll", uuid = "be027038-0da8-5614-b30d-e42594cb92df", version = v"2.11.4")),
-    Dependency(PackageSpec(; name = "METIS_jll", uuid = "d00139f3-1899-568f-a2f0-47f597d42d70", version = v"4.0.3")),
-    Dependency("ASL_jll"),
+    Dependency(PackageSpec(; name = "Clp_jll", uuid = "06985876-5285-5a41-9fcb-8948a742cc53", version = v"1.17.3")),
+    Dependency(PackageSpec(; name = "Cgl_jll", uuid = "3830e938-1dd0-5f3e-8b8e-b3ee43226782", version = v"0.60.2")),
+    Dependency(PackageSpec(; name = "Osi_jll", uuid = "7da25872-d9ce-5375-a4d3-7a845f58efdd", version = v"0.108.5")),
+    Dependency(PackageSpec(; name = "CoinUtils_jll", uuid = "be027038-0da8-5614-b30d-e42594cb92df", version = v"2.11.3")),
+#    Dependency(PackageSpec(; name = "METIS_jll", uuid = "d00139f3-1899-568f-a2f0-47f597d42d70", version = v"4.0.3")),
     Dependency("OpenBLAS32_jll"),
     Dependency("CompilerSupportLibraries_jll"),
 ]
