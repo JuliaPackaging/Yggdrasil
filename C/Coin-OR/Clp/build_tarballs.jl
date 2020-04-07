406c0af06c762795eef300988f8635334a4b9282
Viral B. Shah <viral@mayin.org>
2020-04-07 13:02:22 -0400

    [Clp] Add 1.17.3
    Add support for mumps

--- C/Coin-OR/Clp/build_tarballs.jl
+++ C/Coin-OR/Clp/build_tarballs.jl
@@ -1,7 +1,7 @@
 using BinaryBuilder, Pkg
 
 name = "Clp"
-version = v"1.17.5"
+version = v"1.17.3"
 
 # Collection of sources required to build Clp
 sources = [
@@ -12,15 +12,16 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/Clp*
-update_configure_scripts
 
-# Remove misleading libtool files
-rm -f ${prefix}/lib/*.la
+# Remove misleading libtool files                                                                                            
+rm -f ${prefix}/lib/*.la                                                                                                     
+rm -f /opt/${target}/${target}/lib*/*.la                                                                                     
+update_configure_scripts
 
 mkdir build
 cd build/
 
-export CPPFLAGS="${CPPFLAGS} -I${prefix}/include -I$prefix/include/coin"
+export CPPFLAGS="${CPPFLAGS} -I${prefix}/include -I$prefix/include/coin -I$prefix/include/mumps_seq"
 export CXXFLAGS="${CXXFLAGS} -std=c++11"
 if [[ ${target} == *mingw* ]]; then	
     export LDFLAGS="-L$prefix/bin"
@@ -28,10 +29,11 @@ elif [[ ${target} == *linux* ]]; then
     export LDFLAGS="-ldl -lrt"
 fi
 
-../configure --prefix=$prefix --with-pic --disable-pkg-config --build=${MACHTYPE} --host=${target} --enable-shared \
---enable-dependency-linking lt_cv_deplibs_check_method=pass_all \
---with-asl-lib="-lasl" --with-asl-incdir="$prefix/include" \
+../configure --prefix=$prefix --with-pic --disable-pkg-config --build=${MACHTYPE} --host=${target} \
+--enable-shared lt_cv_deplibs_check_method=pass_all \
+--with-asl-lib="-lasl" \
 --with-blas="-lopenblas" --with-lapack="-openblas" \
+--with-mumps-lib="-L${libdir} -ldmumps -lmpiseq -lmumps_common -lopenblas -lpord" \
 --with-metis-lib="-lmetis" \
 --with-coinutils-lib="-lCoinUtils" \
 --with-osi-lib="-lOsi -lCoinUtils" 
@@ -55,10 +57,11 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(; name = "CoinUtils_jll", uuid = "be027038-0da8-5614-b30d-e42594cb92df", version = v"2.11.4")),
-    Dependency(PackageSpec(; name = "Osi_jll", uuid = "7da25872-d9ce-5375-a4d3-7a845f58efdd", version = v"0.108.6")),
+    Dependency(PackageSpec(; name = "CoinUtils_jll", uuid = "be027038-0da8-5614-b30d-e42594cb92df", version = v"2.11.3")),
+    Dependency(PackageSpec(; name = "Osi_jll", uuid = "7da25872-d9ce-5375-a4d3-7a845f58efdd", version = v"0.108.5")),
     Dependency(PackageSpec(; name = "METIS_jll", uuid = "d00139f3-1899-568f-a2f0-47f597d42d70", version = v"4.0.3")),
     Dependency("ASL_jll"),
+    Dependency("MUMPS_seq_jll"),
     Dependency("OpenBLAS32_jll"),
     Dependency("CompilerSupportLibraries_jll"),
 ]
