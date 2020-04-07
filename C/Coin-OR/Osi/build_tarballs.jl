78bfae439c69435acb98091fd71e24e5e727314e
Viral B. Shah <viral@mayin.org>
2020-04-07 12:46:18 -0400

    [Osi] Add v0.108.5 (#784)

--- C/Coin-OR/Osi/build_tarballs.jl
+++ C/Coin-OR/Osi/build_tarballs.jl
@@ -1,32 +1,36 @@
 using BinaryBuilder, Pkg
 
 name = "Osi"
-version = v"0.108.6"
+version = v"0.108.5"
 
 # Collection of sources required to build OsiBuilder
 sources = [
     GitSource("https://github.com/coin-or/Osi.git",
-              "dfa6449d6756fdd96912cf96e168d0be07b1d37c")
+              "2bd34ae6b8c93d342d54fd19d4d773f07194583c")
 ]
 
 script = raw"""
 cd $WORKSPACE/srcdir/Osi*
+
+# Remove misleading libtool files                                                                                            
+rm -f ${prefix}/lib/*.la                                                                                                     
+rm -f /opt/${target}/${target}/lib*/*.la                                                                                     
 update_configure_scripts
 
 mkdir build
 cd build/
 
-export CPPFLAGS="${CPPFLAGS} -I${prefix}/include"
-export CXXFLAGS="${CXXFLAGS} -std=c++11"
+export CPPFLAGS="${CPPFLAGS} -I${prefix}/include -I${prefix}/include/coin"                                                   
+export CXXFLAGS="${CXXFLAGS} -std=c++11"                                                                                     
 if [[ ${target} == *mingw* ]]; then	
     export LDFLAGS="-L$prefix/bin"
 elif [[ ${target} == *linux* ]]; then
     export LDFLAGS="-ldl -lrt"
 fi
 
-../configure --prefix=$prefix --with-pic --disable-pkg-config --build=${MACHTYPE} --host=${target} --enable-shared \
---enable-dependency-linking lt_cv_deplibs_check_method=pass_all \
---with-coinutils-lib="-lCoinUtils" --with-coinutils-incdir="$prefix/include/coin" \
+../configure --prefix=$prefix --with-pic --disable-pkg-config --build=${MACHTYPE} --host=${target} \
+--enable-shared lt_cv_deplibs_check_method=pass_all \
+--with-coinutils-lib="-lCoinUtils" \
 --with-blas-lib="-lopenblas" --with-lapack-lib="-lopenblas"
 
 make -j${nproc}
@@ -41,13 +45,13 @@ platforms = [p for p in platforms if !(arch(p) == :powerpc64le)]
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct(["libOsi", "libOsi-1"], :libOsi), 
-    LibraryProduct(["libOsiCommonTests", "libOsiCommonTests-1"], :libOsiCommonTests),
+    LibraryProduct("libOsi", :libOsi), 
+    LibraryProduct("libOsiCommonTests", :libOsiCommonTests),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(; name = "CoinUtils_jll", uuid = "be027038-0da8-5614-b30d-e42594cb92df", version = v"2.11.4")),
+    Dependency(PackageSpec(; name = "CoinUtils_jll", uuid = "be027038-0da8-5614-b30d-e42594cb92df", version = v"2.11.3")),
     Dependency("OpenBLAS32_jll"),
     Dependency("CompilerSupportLibraries_jll"),
 ]
