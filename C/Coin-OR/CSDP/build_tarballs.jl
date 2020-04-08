f87a0043c65da2fee875ad4dd83fafa41c25d81a
Alexis <35051714+amontoison@users.noreply.github.com>
2020-04-08 14:23:44 -0400

    [CSDP] Version 6.2.0 (#795)
    * [CSDP] Version 6.2.0
    
    Co-authored-by: Viral B. Shah <viral@juliacomputing.com>
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ C/Coin-OR/CSDP/build_tarballs.jl
@@ -0,0 +1,66 @@
+include("../coin-or-common.jl")
+
+name = "CSDP"
+version = v"6.2.0"
+
+# Collection of sources required to build Clp
+sources = [
+    GitSource("https://github.com/coin-or/Csdp.git",
+    "e1586e0413ef236b19abe5202f7e8392f3dd4614"),
+    DirectorySource("./bundled"), 
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/Csdp*
+
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/makefile.patch"
+
+if [[ ${nbits} == 32 ]]; then
+    atomic_patch -p1 "${WORKSPACE}/srcdir/patches/32bits_platforms.patch"
+fi
+
+if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]]; then
+    atomic_patch -p1 "${WORKSPACE}/srcdir/patches/mac_freebsd.patch"
+fi
+
+if [[ "${target}" == powerpc* ]]; then
+    atomic_patch -p1 "${WORKSPACE}/srcdir/patches/powerpc.patch"
+fi
+
+if [[ "${target}" == arm* ]]; then
+    atomic_patch -p1 "${WORKSPACE}/srcdir/patches/arm.patch"
+fi
+
+if [[ "${target}" == aarch* ]]; then
+    atomic_patch -p1 "${WORKSPACE}/srcdir/patches/aarch.patch"
+fi
+
+make -j${nproc}
+make install
+mkdir -p ${bindir}
+cp /usr/local/bin/csdp ${bindir}/csdp
+
+if [[ "${target}" == *-mingw* ]]; then
+    mv "${bindir}/csdp" "${bindir}/csdp${exeext}"
+fi
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("csdp", :csdp),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("OpenBLAS32_jll"),
+    Dependency("CompilerSupportLibraries_jll"),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
+               preferred_gcc_version=gcc_version)
