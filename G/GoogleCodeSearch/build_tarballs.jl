e4bd418cfec637b60fbeec2ed9a8a341cd728499
tan <tanmaykm@gmail.com>
2020-04-08 12:09:04 +0530

    New Recipe: GoogleCodeSearch v1.2.0

---
+++ G/GoogleCodeSearch/build_tarballs.jl
@@ -0,0 +1,43 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "GoogleCodeSearch"
+version = v"1.2.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/google/codesearch.git", "8ba29bd255b740aee4eb4e4ddb5d7ec0b4d9f23e")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+mkdir -p ${prefix}/share/licenses
+cp codesearch/LICENSE $prefix/share/licenses/
+mkdir gsc
+cd gsc
+go mod init github.com/m
+go get -d -v github.com/google/codesearch@v1.2.0
+mkdir -p $prefix/bin
+go build -o $prefix/bin/csearch github.com/google/codesearch/cmd/csearch
+go build -o $prefix/bin/cindex github.com/google/codesearch/cmd/cindex
+exit
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("csearch", :csearch),
+    ExecutableProduct("cindex", :cindex)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers = [:go, :c])
