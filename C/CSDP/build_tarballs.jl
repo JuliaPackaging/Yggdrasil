74540298c448f8f51795232c6e3035776716d859
Alexis Montoison <alexis.montoison@polymtl.ca>
2020-04-07 20:25:56 -0400

    [CSDP] Version 6.2.0

---
+++ C/CSDP/build_tarballs.jl
@@ -0,0 +1,38 @@
+using BinaryBuilder
+
+name = "CSDP"
+version = v"6.2.0"
+
+# Collection of sources required to build Clp
+sources = [
+    ArchiveSource("https://github.com/coin-or/Csdp/archive/releases/6.2.0.tar.gz",
+    "3d341974af1f8ed70e1a37cc896e7ae4a513375875e5b46db8e8f38b7680b32f"),
+    DirectorySource("./bundled"), 
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/Csdp-releases-6.2.0/
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/makefile.patch"
+make -j${nproc}
+make install
+cp /usr/local/bin/csdp $WORKSPACE/destdir/csdp
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("csdp", :csdp),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("OpenBLAS32_jll"),
+    Dependency("CompilerSupportLibraries_jll"),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
