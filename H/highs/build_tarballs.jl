1aeb3ac8c7509599c7c8b2895aa4e34d9409a499
Mathieu Besançon <mathieu.besancon@gmail.com>
2020-04-11 21:09:10 +0100

    Update H/highs/build_tarballs.jl
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>

--- H/highs/build_tarballs.jl
+++ H/highs/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "highs"
+name = "HiGHS"
 version = v"0.1.1"
 
 # Collection of sources required to complete build
