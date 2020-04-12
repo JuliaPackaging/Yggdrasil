ac59d0af4bfde60dda3dacd2f972a228d25041f1
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-12 12:43:48 -0700

    use target instead of arch

--- P/PETSc/build_tarballs.jl
+++ P/PETSc/build_tarballs.jl
@@ -27,12 +27,12 @@ else
 fi
 
 opt_flags="--with-debugging=0 COPTFLAGS='-O3 -march=native -mtune=native' -CXXOPTFLAGS='-O3 -march=native -mtune=native' FOPTFLAGS='-O3 -march=native -mtune=native"
-./configure --prefix=$prefix $opt_flags CC=$CC FC=$FC CXX=$CXX --with-batch --PETSC_ARCH=$BUILD_AR  --with-blaslapack-lib=$BLAS_LAPACK_LIB --with-blaslapack-suffix=$BLAS_LAPACK_SUFFIX --known-64-bit-blas-indices=$blas_64 --with-mpi=0
+./configure --prefix=$prefix $opt_flags CC=$CC FC=$FC CXX=$CXX --with-batch --PETSC_ARCH=$target  --with-blaslapack-lib=$BLAS_LAPACK_LIB --with-blaslapack-suffix=$BLAS_LAPACK_SUFFIX --known-64-bit-blas-indices=$blas_64 --with-mpi=0
 
 # Generates some errors when mpi is included. These flags detect it properly
 # --with-mpi-lib="${libdir}/libmpi.${dlext}" --with-mpi-include="$includedir"
 
-make PETSC_DIR=$prefix PETSC_ARCH=$BUILD_AR
+make PETSC_DIR=$prefix PETSC_ARCH=$target
 
 # Move libraries to ${libdir} on Windows
 if [[ "${target}" == *-mingw* ]]; then
