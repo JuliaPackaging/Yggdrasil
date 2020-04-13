a329ce6fb0ed4e1ef74047e45992f0babfcc392f
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-12 18:46:52 -0700

    add dependency for MKL

--- O/OSQP/build_tarballs.jl
+++ O/OSQP/build_tarballs.jl
@@ -33,6 +33,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
+    Dependency("MKL_jll"),
+    Dependency("CompilerSupportLibraries_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
