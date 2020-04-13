60b66a3bfcdc642dec5649082fb2c3e7cbff29ab
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-13 09:18:40 -0700

    update link and native options

--- P/PETSc/build_tarballs.jl
+++ P/PETSc/build_tarballs.jl
@@ -6,7 +6,7 @@ version = v"3.13.0"
 # Collection of sources required to build PETSc. Avoid using the git repository, it will
 # require building SOWING which fails in all non-linux platforms.
 sources = [
-    ArchiveSource("http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.13.0.tar.gz",
+    ArchiveSource("https://www.mcs.anl.gov/petsc/mirror/release-snapshots/petsc-3.13.0.tar.gz",
     "df2ff7cb0341bb534a18c7dbea37aa2e2c543a440bf63c24977a605d9b5f8324"),
     DirectorySource("./bundled"),
 ]
@@ -32,7 +32,7 @@ if [[ ${target} != *darwin* ]]; then
     export CFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib -Wl,-rpath-link,/opt/${target}/${target}/lib64"
 fi
 
-opt_flags="--with-debugging=0 COPTFLAGS='-O3 -march=native -mtune=native' -CXXOPTFLAGS='-O3 -march=native -mtune=native' FOPTFLAGS='-O3 -march=native -mtune=native'"
+opt_flags="--with-debugging=0 COPTFLAGS='-O3' -CXXOPTFLAGS='-O3' FOPTFLAGS='-O3'"
 ./configure --prefix=${prefix} $opt_flags \
     CC=$CC \
     FC=$FC \
