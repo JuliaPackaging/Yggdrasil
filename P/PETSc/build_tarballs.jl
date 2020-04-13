2a5809d23430e7d061785e20336bacd1ce282c24
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-12 17:02:52 -0700

    add comment about sowing

--- P/PETSc/build_tarballs.jl
+++ P/PETSc/build_tarballs.jl
@@ -3,7 +3,8 @@ using BinaryBuilder
 name = "PETSc"
 version = v"3.13.0"
 
-# Collection of sources required to build Sundials
+# Collection of sources required to build PETSc. Avoid using the git repository, it will
+# require building SOWING which fails in all non-linux platforms.
 sources = [
     ArchiveSource("http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-3.13.0.tar.gz",
     "df2ff7cb0341bb534a18c7dbea37aa2e2c543a440bf63c24977a605d9b5f8324"),
@@ -26,6 +27,11 @@ else
   blas_64=0
 fi
 
+if [[ ${target} != *darwin* ]]; then
+    # Needed to find libgfortran for OpenBLAS.
+    export CFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib -Wl,-rpath-link,/opt/${target}/${target}/lib64"
+fi
+
 opt_flags="--with-debugging=0 COPTFLAGS='-O3 -march=native -mtune=native' -CXXOPTFLAGS='-O3 -march=native -mtune=native' FOPTFLAGS='-O3 -march=native -mtune=native"
 ./configure --prefix=$prefix $opt_flags CC=$CC FC=$FC CXX=$CXX --with-batch --PETSC_ARCH=$target  --with-blaslapack-lib=$BLAS_LAPACK_LIB --with-blaslapack-suffix=$BLAS_LAPACK_SUFFIX --known-64-bit-blas-indices=$blas_64 --with-mpi=0 --with-sowing=0"
 
@@ -34,11 +40,6 @@ opt_flags="--with-debugging=0 COPTFLAGS='-O3 -march=native -mtune=native' -CXXOP
 
 make PETSC_DIR=$PWD PETSC_ARCH=$target all
 make PETSC_DIR=$PWD PETSC_ARCH=$target DEST_DIR=$prefix install
-
-if [[ ${target} != *darwin* ]]; then
-    # Needed to find libgfortran for OpenBLAS.
-    export CFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib -Wl,-rpath-link,/opt/${target}/${target}/lib64"
-fi
 
 # Move libraries to ${libdir} on Windows
 if [[ "${target}" == *-mingw* ]]; then
