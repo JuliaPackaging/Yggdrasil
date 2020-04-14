800edf4ae4008f449c7ebaf4bd3a32ae62caa29f
Mathieu Besançon <mathieu.besancon@gmail.com>
2020-04-14 02:04:06 +0100

    HiGHS: expanding platforms with patch (#860)
    * expanding platforms with patch
    
    * add patch dir

--- H/HiGHS/build_tarballs.jl
+++ H/HiGHS/build_tarballs.jl
@@ -3,16 +3,24 @@
 using BinaryBuilder, Pkg
 
 name = "HiGHS"
-version = v"0.1.2"
+version = v"0.1.3"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/ERGO-Code/HiGHS.git", "0dc437abd75ba9a56d24e4f4f5a60bd89a2839a5"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
+if [[ "${target}" == *86*-linux-musl* ]]; then
+    pushd /opt/${target}/lib/gcc/${target}/*/include
+    # Fix bug in Musl C library, see
+    # https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/387
+    atomic_patch -p0 $WORKSPACE/srcdir/patches/mm_malloc.patch
+    popd
+fi
 mkdir -p HiGHS/build
 cd HiGHS/build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
@@ -22,12 +30,10 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64)
-]
+platforms = filter!(
+    p -> !isa(p, Windows) && (arch(p) != :powerpc64le),
+    supported_platforms()
+)
 platforms = expand_gfortran_versions(expand_cxxstring_abis(platforms))
 
 # The products that we will ensure are always built
