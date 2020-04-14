e3791fe6ba11c48ddaaf0e41d87addc88b616d65
Chris de Graaf <chrisadegraaf@gmail.com>
2020-04-13 22:21:19 -0400

    Build LLNL Elemental fork

--- E/Elemental/build_tarballs.jl
+++ E/Elemental/build_tarballs.jl
@@ -3,40 +3,50 @@
 using BinaryBuilder
 
 name = "Elemental"
-version = v"0.87.7"
+version = v"1.3.3"
 
 # Collection of sources required to build Elemental
 sources = [
-    GitSource("https://github.com/elemental/Elemental.git",
-              "477e503a7a840cc1a75173552711b980505a0b06"),
+    GitSource("https://github.com/LLNL/Elemental.git",
+              "38505fbf3b9f4a511fb35b0776144f336616fe38"),
+    DirectorySource("bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-if [[ $nbits == 64 ]] && [[ "$target" != aarch64-* ]]; then
-  BLAS_LAPACK_LIB="$libdir/libopenblas64_.$dlext"
-  BLAS_LAPACK_SUFFIX="_64_"
+cd Elemental
+
+atomic_patch -p1 ../patches/01-fix-suffix.patch
+
+mkdir build
+cd build
+
+if [[ "$nbits" == 64 ]]; then
+  IS_64="ON"
 else
-  BLAS_LAPACK_LIB="$libdir/libopenblas.$dlext"
-  BLAS_LAPACK_SUFFIX=""
+  IS_64="OFF"
 fi
 
-mkdir "$WORKSPACE/srcdir/$SRC_NAME/build"
-cd "$WORKSPACE/srcdir/$SRC_NAME/build"
+if [[ "$nbits" == 64 ]] && [[ "$target" != aarch64-* ]]; then
+  BLAS_LAPACK_LIB="$libdir/libopenblas64_.$dlext"
+  IS_BLAS_64="ON"
+else
+  BLAS_LAPACK_LIB="$libdir/libopenblas.$dlext"
+  IS_BLAS_64="OFF"
+fi
 
 cmake \
   -DCMAKE_INSTALL_PREFIX="$prefix" \
   -DCMAKE_TOOLCHAIN_FILE="$CMAKE_TARGET_TOOLCHAIN" \
   -DCMAKE_BUILD_TYPE="Release" \
-  -DEL_DISABLE_PARMETIS="ON" \
-  -DMETIS_TEST_RUNS_EXITCODE="0" \
-  -DMETIS_TEST_RUNS_EXITCODE__TRYRUN_OUTPUT="" \
-  -DMATH_LIBS="$BLAS_LAPACK_LIB" \
+  -DBUILD_SHARED_LIBS="ON" \
   -DBLAS_LIBRARIES="$BLAS_LAPACK_LIB" \
   -DLAPACK_LIBRARIES="$BLAS_LAPACK_LIB" \
-  -DEL_BLAS_SUFFIX="$BLAS_LAPACK_SUFFIX" \
-  -DEL_LAPACK_SUFFIX="$BLAS_LAPACK_SUFFIX" \
-  "$WORKSPACE/srcdir/$SRC_NAME"
+  -DHydrogen_ENABLE_TESTING="OFF" \
+  -DHydrogen_USE_OpenBLAS="ON" \
+  -DHydrogen_USE_64BIT_INTS="$IS_64" \
+  -DHydrogen_USE_64BIT_BLAS_INTS="$IS_BLAS_64" \
+  ..
 
 make "-j$nproc"
 make install
@@ -50,13 +60,11 @@ filter!(p -> !(p isa Windows), platforms)
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libEl", :libEl),
-    LibraryProduct("libElSuiteSparse", :libElSuiteSparse),
+    LibraryProduct("libHydrogen_CXX", :libHydrogen),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("METIS_jll"),
     Dependency("MPICH_jll"),
     Dependency("OpenBLAS_jll"),
 ]
