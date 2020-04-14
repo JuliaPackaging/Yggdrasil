29f5f391e61893e1c0d4f00070f079d5da6fe75d
Mosè Giordano <giordano@users.noreply.github.com>
2020-04-14 14:19:04 +0100

    [fakechroot] Fix directory of the library

--- F/fakechroot/build_tarballs.jl
+++ F/fakechroot/build_tarballs.jl
@@ -32,7 +32,7 @@ platforms = [
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libfakechroot", :libfakechroot, ["lib/fakeroot"])
+    LibraryProduct("libfakechroot", :libfakechroot, ["lib/fakechroot"])
 ]
 
 # Dependencies that must be installed before this package can be built
