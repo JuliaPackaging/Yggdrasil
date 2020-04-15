2aa0f7c96214d885d477eea8996ab50b85ef6984
Dhairya Gandhi <dhairya@juliacopmuting.com>
2020-04-15 15:28:43 +0530

    fix cuda_jll path

--- T/Torch/build_tarballs.jl
+++ T/Torch/build_tarballs.jl
@@ -19,16 +19,16 @@ cd $WORKSPACE/srcdir
 mv libtorch cudnn $prefix
 mkdir -p /usr/local/cuda/lib64
 cd /usr/local/cuda/lib64
-ln -s ${prefix}/lib64/libcudart.so libcudart.so
-ln -s ${prefix}/lib64/libnvToolsExt.so libnvToolsExt.so
+ln -s ${prefix}/cuda/lib64/libcudart.so libcudart.so
+ln -s ${prefix}/cuda/lib64/libnvToolsExt.so libnvToolsExt.so
 
 cd $WORKSPACE/srcdir/Torch.jl/build
 mkdir build && cd build
-cmake -DCMAKE_PREFIX_PATH=$prefix -DTorch_DIR=$prefix/libtorch/share/cmake/Torch -DCUDA_TOOLKIT_ROOT_DIR=$prefix -DCUDNN_LIBRARY_PATH=$prefix/cudnn/lib/libcudnn.so  -DCUDNN_INCLUDE_DIR=$prefix/cudnn/include ..
+cmake -DCMAKE_PREFIX_PATH=$prefix -DTorch_DIR=$prefix/libtorch/share/cmake/Torch -DCUDA_TOOLKIT_ROOT_DIR=$prefix/cuda -DCUDNN_LIBRARY_PATH=$prefix/cudnn/lib/libcudnn.so  -DCUDNN_INCLUDE_DIR=$prefix/cudnn/include ..
 cmake --build .
 mkdir -p "${libdir}"
 cp -r $WORKSPACE/srcdir/Torch.jl/build/build/*.${dlext} "${libdir}"
-cp $WORKSPACE/srcdir/Torch.jl/build/*.cpp $WORKSPACE/srcdir/Torch.jl/build/*.h "${prefix}/include"
+# cp $WORKSPACE/srcdir/Torch.jl/build/*.cpp $WORKSPACE/srcdir/Torch.jl/build/*.h "${prefix}/include"
 """
 
 # These are the platforms we will build for by default, unless further
