cb6c332baddd08ed7a09166bc277d9e99786713e
jaakkor2 <jaakkor2@users.noreply.github.com>
2020-04-16 09:11:42 +0200

    update the build script

--- L/LibSpatialIndex/build_tarballs.jl
+++ L/LibSpatialIndex/build_tarballs.jl
@@ -1,21 +1,20 @@
-# Note that this script can accept some limited command-line arguments, run
-# `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-version = v"1.8.5" # also change in raw script string
+name = "LibSpatialIndex"
+version = v"1.8.5"
 
-# Collection of sources required to build LibSpatialIndexBuilder
+# Collection of sources required to build LibSpatialIndex
 sources = [
-    "http://download.osgeo.org/libspatialindex/spatialindex-src-$version.tar.bz2" =>
-    "31ec0a9305c3bd6b4ad60a5261cba5402366dd7d1969a8846099717778e9a50a",
-    "./patches"
+    ArchiveSource("http://download.osgeo.org/libspatialindex/spatialindex-src-1.8.5.tar.bz2",
+        "31ec0a9305c3bd6b4ad60a5261cba5402366dd7d1969a8846099717778e9a50a"),
+    DirectorySource("./patches"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 
-cd spatialindex-src-1.8.5/
+cd spatialindex-src-*
 
 patch < ${WORKSPACE}/srcdir/makefile.patch
 rm Makefile.am.orig
@@ -31,9 +30,10 @@ automake --add-missing --foreign
 # Show options in the log
 ./configure --help
 
-./configure --prefix=$prefix --host=$target
+./configure --host=$target --enable-static=no
 make
 make install
+install_license ../COPYING
 """
 
 # These are the platforms we will build for by default, unless further
@@ -41,14 +41,13 @@ make install
 platforms = supported_platforms()
 
 # The products that we will ensure are always built
-products(prefix) = [
-    LibraryProduct(prefix, "libspatialindex_c", :libspatialindex_c),
-    LibraryProduct(prefix, "libspatialindex", :libspatialindex)
+products = [
+    LibraryProduct("libspatialindex_c", :libspatialindex_c),
+    LibraryProduct("libspatialindex", :libspatialindex),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = []
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, "SpatialIndex", version, sources, script, platforms, products, dependencies)
-
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
