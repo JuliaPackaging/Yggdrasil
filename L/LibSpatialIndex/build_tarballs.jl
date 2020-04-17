63f51f9acb1d8cc8e2f80cbd453f46377110a0ff
jaakkor2 <jaakkor2@users.noreply.github.com>
2020-04-17 06:36:57 +0200

    Update L/LibSpatialIndex/build_tarballs.jl
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>

--- L/LibSpatialIndex/build_tarballs.jl
+++ L/LibSpatialIndex/build_tarballs.jl
@@ -38,7 +38,7 @@ install_license COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
