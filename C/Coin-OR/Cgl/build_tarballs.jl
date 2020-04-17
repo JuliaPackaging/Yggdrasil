ba3909f8804541bc06f454919e250dd5060b045b
Viral B. Shah <viral@mayin.org>
2020-04-17 17:37:23 -0400

    Update build_tarballs.jl

--- C/Coin-OR/Cgl/build_tarballs.jl
+++ C/Coin-OR/Cgl/build_tarballs.jl
@@ -28,7 +28,8 @@ elif [[ ${target} == *linux* ]]; then
     export LDFLAGS="-ldl -lrt"
 fi
 
-../configure --prefix=$prefix --with-pic --disable-pkg-config --build=${MACHTYPE} --host=${target} \
+../configure --prefix=$prefix --build=${MACHTYPE} --host=${target} \
+--with-pic --disable-pkg-config --disable-debug \
 --enable-shared lt_cv_deplibs_check_method=pass_all \
 --with-coinutils-lib="-lCoinUtils" \
 --with-osi-lib="-lOsi -lCoinUtils" \
