bc115bdd703da3a8e4902ecb311accaa745f270c
Viral B. Shah <viral@mayin.org>
2020-04-17 17:22:14 -0400

    [Cbc] Avoid setting CXXFLAGS

--- C/Coin-OR/Cbc/build_tarballs.jl
+++ C/Coin-OR/Cbc/build_tarballs.jl
@@ -26,7 +26,6 @@ mkdir build
 cd build/
 
 export CPPFLAGS="${CPPFLAGS} -DNDEBUG -I${prefix}/include -I$prefix/include/coin"
-export CXXFLAGS="${CXXFLAGS} -std=c++11"
 if [[ ${target} == *mingw* ]]; then
     export LDFLAGS="-L$prefix/bin"
 elif [[ ${target} == *linux* ]]; then
