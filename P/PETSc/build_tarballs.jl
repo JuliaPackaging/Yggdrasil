d64712a0dde0e62b04aae3b49aee13621bf851f1
Jose Daniel Lara <jdlara@berkeley.edu>
2020-04-20 14:11:40 -0700

    remove improper file reference

--- P/PETSc/build_tarballs.jl
+++ P/PETSc/build_tarballs.jl
@@ -8,7 +8,7 @@ version = v"3.13.0"
 sources = [
     ArchiveSource("https://www.mcs.anl.gov/petsc/mirror/release-snapshots/petsc-3.13.0.tar.gz",
     "df2ff7cb0341bb534a18c7dbea37aa2e2c543a440bf63c24977a605d9b5f8324"),
-    DirectorySource(joinpath("/Users/jdlara/cache/Yggdrasil/P/PETSc/","bundled")),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
