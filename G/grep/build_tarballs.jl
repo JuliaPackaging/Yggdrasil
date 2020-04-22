9d7f65227fb5cca72fd05a132e2b79500e83908f
simeonschaub <simeondavidschaub99@gmail.com>
2020-04-22 12:51:49 +0200

    add dependency on `Libiconv_jll`

--- G/grep/build_tarballs.jl
+++ G/grep/build_tarballs.jl
@@ -29,6 +29,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
+    Dependency("Libiconv_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
