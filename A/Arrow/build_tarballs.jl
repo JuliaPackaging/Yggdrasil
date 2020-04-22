9e4bbc5a93e7c27d730390e5c2e4c3be3ee01ece
David Anthoff <anthoff@berkeley.edu>
2020-04-22 15:05:56 +0000

    Enable build on all platforms

--- A/Arrow/build_tarballs.jl
+++ A/Arrow/build_tarballs.jl
@@ -22,6 +22,10 @@ if [[ "${target}" == *-apple-* ]]; then
     FLAGS+=(-DARROW_CXXFLAGS="-mmacosx-version-min=10.9")
 fi
 
+if [[ ${target} == arm-* ]]; then
+    FLAGS+=(-DARROW_SIMD_LEVEL=NONE)
+fi
+
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DARROW_JEMALLOC=OFF ${FLAGS[@]}
 
 make -j${nproc}
@@ -30,21 +34,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    # Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    # Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
+platforms = supported_platforms()
 
 platforms = expand_cxxstring_abis(platforms)
 
