90093a44e0e7833372221fbfdd26a3a47da57748
Mosè Giordano <mose@gnu.org>
2020-04-22 17:25:22 +0100

    [Poppler] Fix OpenJPEGTurbo troubles

--- P/Poppler/build_tarballs.jl
+++ P/Poppler/build_tarballs.jl
@@ -8,30 +8,52 @@ version = v"0.87.0"
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://poppler.freedesktop.org/poppler-0.87.0.tar.xz", "6f602b9c24c2d05780be93e7306201012e41459f289b8279a27a79431ad4150e")
-    ArchiveSource("https://poppler.freedesktop.org/poppler-data-0.4.9.tar.gz", "1f9c7e7de9ecd0db6ab287349e31bf815ca108a5a175cf906a90163bdbe32012")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/poppler-0.87.0/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DCMAKE_BUILD_TYPE=Release -DBUILD_GTK_TESTS=OFF -DENABLE_CMS=lcms2 \
-    -DENABLE_GLIB=OFF -DENABLE_QT5=OFF -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
-    -DWITH_GObjectIntrospection=OFF
+cd $WORKSPACE/srcdir/poppler-*/
+
+# Create link ${bindir} before starting.  `OpenJPEGTargets.cmake` will try to
+# look for some executables in `sys-root/usr/local/bin`
+ln -s ${bindir} /opt/${target}/${target}/sys-root/usr/local/bin
+export CXXFLAGS="-I${prefix}/include/openjpeg-2.3"
+
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_GTK_TESTS=OFF \
+    -DENABLE_CMS=lcms2 \
+    -DENABLE_GLIB=OFF \
+    -DENABLE_QT5=OFF \
+    -DENABLE_UNSTABLE_API_ABI_HEADERS=ON \
+    -DWITH_GObjectIntrospection=OFF \
+    ..
 make -j${nproc}
 make install
-
-cd $WORKSPACE/srcdir/poppler-data-0.4.9/
-make prefix=$prefix install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
-products = Product[
+products = [
+    LibraryProduct("libpoppler-cpp", :libpoppler_cpp),
+    LibraryProduct("libpoppler", :libpoppler),
+    ExecutableProduct("pdfattach", :pdfattach),
+    ExecutableProduct("pdfdetach", :pdfdetach),
+    ExecutableProduct("pdffonts", :pdffonts),
+    ExecutableProduct("pdfimages", :pdfimages),
+    ExecutableProduct("pdfinfo", :pdfinfo),
+    ExecutableProduct("pdfseparate", :pdfseparate),
+    ExecutableProduct("pdftocairo", :pdftocairo),
+    ExecutableProduct("pdftohtml", :pdftohtml),
+    ExecutableProduct("pdftoppm", :pdftoppm),
+    ExecutableProduct("pdftops", :pdftops),
+    ExecutableProduct("pdftotext", :pdftotext),
+    ExecutableProduct("pdfunite", :pdfunite),
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -48,4 +70,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"5")
