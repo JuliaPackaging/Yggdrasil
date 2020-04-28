5af1e80ced3fb0798b3af6170844ab4518ab08d6
Mosè Giordano <giordano@users.noreply.github.com>
2020-04-28 15:32:38 +0100

    [CovidSim] Build also for Windows (#943)

--- C/CovidSim/build_tarballs.jl
+++ C/CovidSim/build_tarballs.jl
@@ -32,8 +32,7 @@ cp "CovidSim${exeext}" "${bindir}/"
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [p for p in supported_platforms() if !isa(p, Windows)]
-
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
