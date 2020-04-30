06a5b257e1a7a2821ad5a9907e87d79796f86ecd
Avik Sengupta <avik@sengupta.net>
2020-04-30 20:04:21 +0100

    user versioned cacert download
    Co-authored-by: Elliot Saba <staticfloat@gmail.com>

--- L/LibCURL/build_tarballs.jl
+++ L/LibCURL/build_tarballs.jl
@@ -4,13 +4,15 @@ using BinaryBuilder
 
 name = "LibCURL"
 version = v"7.70.0"
+cacert_version = "2020-01-01"
 
 # Collection of sources required to build LibCURL
 sources = [
     ArchiveSource("https://curl.haxx.se/download/curl-$(version).tar.gz", 
     "ca2feeb8ef13368ce5d5e5849a5fd5e2dd4755fecf7d8f0cc94000a4206fb8e7"), 
-    FileSource("https://curl.haxx.se/ca/cacert.pem", 
-    "adf770dfd574a0d6026bfaa270cb6879b063957177a991d453ff1d302c02081f")
+    FileSource("https://curl.haxx.se/ca/cacert-$cacert_version.pem", 
+    "adf770dfd574a0d6026bfaa270cb6879b063957177a991d453ff1d302c02081f",
+    filename="cacert.pem")
 ]
 
 # Bash recipe for building across all platforms
@@ -22,13 +24,14 @@ FLAGS=(
     # Disable....almost everything
     --without-ssl --without-gnutls --without-gssapi
     --without-libidn --without-libidn2 --without-libmetalink --without-librtmp
-    --without-nghttp2 --without-nss --without-polarssl
+    --without-nss --without-polarssl
     --without-spnego --without-libpsl --disable-ares --disable-manual
     --disable-ldap --disable-ldaps --without-zsh-functions-dir
     --disable-static
 
     # Two things we actually enable
     --with-libssh2=${prefix} --with-mbedtls=${prefix} --with-zlib=${prefix}
+    --with-nghttp2=${prefix}
 )
 
 # We need to tell it where to find libssh2 on windows
@@ -40,7 +43,7 @@ fi
 make -j${nproc}
 make install
 install_license COPYING
-cp ../cacert.pem $prefix/share/
+cp ../cacert.pem $prefix/share/cacert.pem
 """
 
 # These are the platforms we will build for by default, unless further
@@ -59,6 +62,7 @@ dependencies = [
     Dependency("LibSSH2_jll"),
     Dependency("MbedTLS_jll"),
     Dependency("Zlib_jll"),
+    Dependency("nghttp2_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
