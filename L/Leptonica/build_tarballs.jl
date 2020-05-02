ed97e9ca5b4b37ce625bb9ec5b02d92a6369c388
Joshua Gentry <pixel@deathbysoftware.com>
2020-05-02 16:20:09 -0400

    Need to define CPPFLAGS so configure will find gif_lib.h.  Otherwise gif support is excluded from the compilation.

--- L/Leptonica/build_tarballs.jl
+++ L/Leptonica/build_tarballs.jl
@@ -14,6 +14,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/leptonica-*/
+export CPPFLAGS="-I$prefix/include"
 ./configure --prefix=$prefix --host=$target
 make -j${nproc}
 make install
