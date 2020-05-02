b9db9278f9fdb988d22e7fec6370e8666d435a25
pixel27 <pixel@deathbysoftware.com>
2020-05-01 20:51:57 -0400

    [Leptonica] Upgrade to v1.79.0 (#968)
    Co-authored-by: Mosè Giordano <mose@gnu.org>

--- L/Leptonica/build_tarballs.jl
+++ L/Leptonica/build_tarballs.jl
@@ -1,14 +1,14 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "Leptonica"
-version = v"1.78.0"
+version = v"1.79.0"
 
 # Collection of sources required to build Leptonica
 sources = [
     ArchiveSource("http://www.leptonica.org/source/leptonica-$(version).tar.gz",
-                  "e2ed2e81e7a22ddf45d2c05f0bc8b9ae7450545d995bfe28517ba408d14a5a88"),
+                  "045966c9c5d60ebded314a9931007a56d9d2f7a6ac39cb5cc077c816f62300d8"),
 ]
 
 # Bash recipe for building across all platforms
@@ -41,12 +41,12 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "Giflib_jll",
-    "JpegTurbo_jll",
-    "libpng_jll",
-    "Libtiff_jll",
-    "libwebp_jll",
-    "Zlib_jll",
+    Dependency(PackageSpec(name="Giflib_jll", uuid="59f7168a-df46-5410-90c8-f2779963d0ec"))
+    Dependency(PackageSpec(name="JpegTurbo_jll", uuid="aacddb02-875f-59d6-b918-886e6ef4fbf8"))
+    Dependency(PackageSpec(name="libpng_jll", uuid="b53b4c65-9356-5827-b1ea-8c7a1a84506f"))
+    Dependency(PackageSpec(name="Libtiff_jll", uuid="89763e89-9b03-5906-acba-b20f662cd828"))
+    Dependency(PackageSpec(name="libwebp_jll", uuid="c5f90fcd-3b7e-5836-afba-fc50a0988cb2"))
+    Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a"))
 ]
 
 # Build the tarballs.
