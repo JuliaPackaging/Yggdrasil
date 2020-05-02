7f70fbafac11e171ac0ea9e87971eee2ea4b9e06
Mosè Giordano <mose@gnu.org>
2020-05-02 01:57:08 +0100

    [libwebp] Upgrade to v1.1.0 and remove duplicate library product

--- L/libwebp/build_tarballs.jl
+++ L/libwebp/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder
 
 name = "libwebp"
-version = v"1.0.3"
+version = v"1.1.0"
 
 # Collection of sources required to build libwebp
 sources = [
-    "https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-$(version).tar.gz" =>
-    "e20a07865c8697bba00aebccc6f54912d6bc333bb4d604e6b07491c1a226b34f",
+    ArchiveSource("https://storage.googleapis.com/downloads.webmproject.org/releases/webp/libwebp-$(version).tar.gz",
+                  "98a052268cc4d5ece27f76572a7f50293f439c17a98e67c4ea0c7ed6f50ef043"),
 ]
 
 # Bash recipe for building across all platforms
@@ -19,7 +19,7 @@ export CPPFLAGS="-I${prefix}/include"
 if [[ "${target}" == *-freebsd* ]]; then
     export LDFLAGS="-L${libdir}"
 fi
-./configure --prefix=$prefix --host=$target \
+./configure --prefix=${prefix} --host=${target} \
     --enable-swap-16bit-csp \
     --enable-experimental \
     --enable-libwebp{mux,demux,decoder,extras} \
@@ -34,11 +34,10 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libwebp", :libwebp),
+    LibraryProduct("libwebpdecoder", :libwebpdecoder),
     LibraryProduct("libwebpdemux", :libwebpdemux),
     LibraryProduct("libwebpmux", :libwebpmux),
-    LibraryProduct("libwebpdecoder", :libwebpdecoder),
-    LibraryProduct("libwebpmux", :libwebpmux),
+    LibraryProduct("libwebp", :libwebp),
     ExecutableProduct("cwebp", :cwebp),
     ExecutableProduct("dwebp", :dwebp),
     ExecutableProduct("gif2webp", :gif2webp),
@@ -49,11 +48,11 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "Giflib_jll",
-    "JpegTurbo_jll",
-    "libpng_jll",
-    "Libtiff_jll",
-    "Libglvnd_jll",
+    Dependency("Giflib_jll"),
+    Dependency("JpegTurbo_jll"),
+    Dependency("libpng_jll"),
+    Dependency("Libtiff_jll"),
+    Dependency("Libglvnd_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
