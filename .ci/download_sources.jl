2aad1c6dc13d4b5b22d0de478847592982effce9
Mosè Giordano <mose@gnu.org>
2020-05-05 22:14:16 +0100

    [AZP] Revert temporary work around in `.ci/download_sources.jl`
    [skip ci]

--- .ci/download_sources.jl
+++ .ci/download_sources.jl
@@ -12,7 +12,6 @@ end
 # Merge the multiple outputs into one
 merged = BinaryBuilder.merge_json_objects(objs)
 BinaryBuilder.cleanup_merged_object!(merged)
-merged["platforms"] = [p == UnknownPlatform() ? AnyPlatform() : p for p in merged["platforms"]]
 
 # Download all sources
 download_source.(merged["sources"]; verbose=true)
