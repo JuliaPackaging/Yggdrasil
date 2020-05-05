a88c54a78bc17a4104cb536261acd7cc04f74e1a
Jose Daniel Lara <jdlara@berkeley.edu>
2020-05-05 10:19:25 -0700

    update petsc version

--- P/PETSc/build_tarballs.jl
+++ P/PETSc/build_tarballs.jl
@@ -6,8 +6,8 @@ version = v"3.13.0"
 # Collection of sources required to build PETSc. Avoid using the git repository, it will
 # require building SOWING which fails in all non-linux platforms.
 sources = [
-    ArchiveSource("https://www.mcs.anl.gov/petsc/mirror/release-snapshots/petsc-3.13.0.tar.gz",
-    "df2ff7cb0341bb534a18c7dbea37aa2e2c543a440bf63c24977a605d9b5f8324"),
+    ArchiveSource("http://ftp.mcs.anl.gov/pub/petsc/release-snapshots/petsc-lite-3.13.1.tar.gz",
+    "74a895e44e2ff1146838aaccb7613e7626d99e0eed64ca032c87c72d084efac3"),
     DirectorySource("./bundled"),
 ]
 
