809c16ad4f8f406f7016033f839808fdaa7b0810
Jan Strube <jan.strube@gmail.com>
2020-05-06 13:36:35 -0700

    [FastJet] updating version
    Also attempting to build plugins. The plugin "Makefile" is a shell script and not very portable, since it calls a fastjet config binary.
    The plugins are independent, though, so I'm attempting something extremely simple.

--- F/FastJet/build_tarballs.jl
+++ F/FastJet/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "FastJet"
-version = v"3.3.3"
+version = v"3.3.4"
 
 # Collection of sources required to complete build
 sources = [
-    "http://fastjet.fr/repo/fastjet-$(version).tar.gz" =>
-    "30b0a0282ce5aeac9e45862314f5966f0be941ce118a83ee4805d39b827d732b",
+    ArchiveSource("http://fastjet.fr/repo/fastjet-3.3.4.tar.gz", "432b51401e1335697c9248519ce3737809808fc1f6d1644bfae948716dddfc03"),
+    ArchiveSource("http://fastjet.hepforge.org/contrib/downloads/fjcontrib-1.044.tar.gz", "de3f45c2c1bed6d7567483e4a774575a504de8ddc214678bac7f64e9d2e7e7a7")
 ]
 
 # Bash recipe for building across all platforms
@@ -31,6 +31,16 @@ fi
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-auto-ptr
 make -j ${nprocs} "${FLAGS[@]}"
 make install
+
+cd $WORKSPACE/srcdir/fjcontrib-*/
+for name in ClusteringVetoPlugin ConstituentSubtractor EnergyCorrelator FlavorCone GenericSubtractor JetCleanser JetFFMoments JetsWithoutJets LundPlane Nsubjettiness QCDAwarePlugin RecursiveTools ScJet SoftKiller SubjetCounting ValenciaPlugin VariableR
+do
+    cd $name
+    rm example*
+    $CXX -fPIC -shared -O3 -Wall [A-Z]*.cc -o lib${name}.o
+    mv lib${name}.o $prefix/lib
+    cd ..
+done
 """
 
 # These are the platforms we will build for by default, unless further
@@ -38,12 +48,30 @@ make install
 platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
+# The products that we will ensure are always built
 products = [
-    LibraryProduct("libsiscone", :libsiscone),
-    LibraryProduct("libfastjetplugins", :libfastjetplugins),
+    LibraryProduct("libConstituentSubtractor", :libClusteringVetoPlugin),
+    LibraryProduct("libQCDAwarePlugin", :libQCDAwarePlugin),
+    LibraryProduct("libJetFFMoments", :libJetFFMoments),
+    LibraryProduct("libLundPlane", :libLundPlane),
+    LibraryProduct("libJetsWithoutJets", :libJetsWithoutJets),
+    LibraryProduct("libEnergyCorrelator", :libEnergyCorrelator),
     LibraryProduct("libfastjettools", :libfastjettools),
+    LibraryProduct("libSoftKiller", :libSoftKiller),
+    LibraryProduct("libRecursiveTools", :libRecursiveTools),
+    LibraryProduct("libfastjet", :libfastjet),
+    LibraryProduct("libValenciaPlugin", :libValenciaPlugin),
+    LibraryProduct("libsiscone", :libsiscone),
+    LibraryProduct("libNsubjettiness", :libNsubjettiness),
+    LibraryProduct("libfastjetplugins", :libfastjetplugins),
+    LibraryProduct("libVariableR", :libVariableR),
     LibraryProduct("libsiscone_spherical", :libsiscone_spherical),
-    LibraryProduct("libfastjet", :libfastjet)
+    LibraryProduct("libJetCleanser", :libJetCleanser),
+    LibraryProduct("libFlavorCone", :libFlavorCone),
+    LibraryProduct("libGenericSubtractor", :libGenericSubtractor),
+    LibraryProduct("libScJet", :libScJet),
+    LibraryProduct("libSubjetCounting", :libSubjetCounting),
+    LibraryProduct("libClusteringVetoPlugin", :libClusteringVetoPlugin)
 ]
 
 # Dependencies that must be installed before this package can be built
