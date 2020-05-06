6dfc9214069ecd209dec4edf962270802ed32c17
Tim Besard <tim.besard@gmail.com>
2020-05-06 08:26:08 +0200

    Revert "[OpenCL] Use AnyPlatform for the headers JLL."

--- O/OpenCL_Headers/build_tarballs.jl
+++ O/OpenCL_Headers/build_tarballs.jl
@@ -22,7 +22,7 @@ mv CL ${prefix}/include/
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [AnyPlatform()]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
