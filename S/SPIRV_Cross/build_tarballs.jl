d59182f967b69ae9f8fbb47602873d165476d7b7
Filippo Vicentini <filippovicentini@gmail.com>
2020-05-07 02:23:11 +0200

    Update S/SPIRV_Cross/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- S/SPIRV_Cross/build_tarballs.jl
+++ S/SPIRV_Cross/build_tarballs.jl
@@ -41,7 +41,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms() # build on all supported platforms
+platforms = expand_cxxstring_abis(supported_platforms()) # build on all supported platforms
 
 
 # The products that we will ensure are always built
