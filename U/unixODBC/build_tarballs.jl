897cc95372c0d2861a52b60fdfc770e16234531a
Jacob Quinn <quinn.jacobd@gmail.com>
2020-05-08 08:46:06 -0600

    Wizard recipe: unixODBC-v2.3.7 (#1008)
    * New Recipe: unixODBC v2.3.7
    
    * [unixODBC] Add `Libiconv_jll` to the dependencies
    
    Co-authored-by: Mosè Giordano <mose@gnu.org>

---
+++ U/unixODBC/build_tarballs.jl
@@ -0,0 +1,44 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "unixODBC"
+version = v"2.3.7"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("http://www.unixodbc.org/unixODBC-2.3.7.tar.gz", "45f169ba1f454a72b8fcbb82abd832630a3bf93baa84731cf2949f449e1e3e77")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/unixODBC-2.3.7/
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libodbc", :libodbc),
+    ExecutableProduct("odbc_config", :odbc_config),
+    LibraryProduct("libodbcinst", :libodbcinst),
+    ExecutableProduct("isql", :isql),
+    ExecutableProduct("iusql", :iusql),
+    ExecutableProduct("odbcinst", :odbcinst),
+    LibraryProduct("libodbccr", :libodbccr),
+    ExecutableProduct("slencheck", :slencheck),
+    ExecutableProduct("dltest", :dltest)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("Libiconv_jll"),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
