1368270f1beea85bf6af69d0fc8b28450966cfac
lizz <innerlee@users.noreply.github.com>
2020-05-08 10:13:50 +0800

    Update build_tarballs.jl

--- D/datamash/build_tarballs.jl
+++ D/datamash/build_tarballs.jl
@@ -21,7 +21,7 @@ install_license COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = filter(p -> !(p isa Windows), supported_platforms())
 
 # The products that we will ensure are always built
 products = [
