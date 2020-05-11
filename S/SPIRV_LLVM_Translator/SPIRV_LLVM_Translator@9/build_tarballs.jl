e498dd755c29f3e2e335686219064f2a14c894f8
Mosè Giordano <giordano@users.noreply.github.com>
2020-05-11 21:54:29 +0100

    [SPIRV_LLVM_Translator] Rebuild to fix wrappers on macOS (#1017)

--- S/SPIRV_LLVM_Translator/SPIRV_LLVM_Translator@9/build_tarballs.jl
+++ S/SPIRV_LLVM_Translator/SPIRV_LLVM_Translator@9/build_tarballs.jl
@@ -11,6 +11,6 @@ dependencies = [
     #Dependency(PackageSpec(name="libLLVM_jll", version=v"9.0.1"))
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
+# Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                preferred_gcc_version=v"7")
