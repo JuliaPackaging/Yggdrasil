428fc2eb06ef111b1c2c001ce1d70089cd9f4d6a
aviks <avik@sengupta.net>
2020-05-14 14:35:42 -0400

    Add patches to fix windows build

--- P/Popt/build_tarballs.jl
+++ P/Popt/build_tarballs.jl
@@ -7,7 +7,8 @@ version = v"1.16.0"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("http://archive.ubuntu.com/ubuntu/pool/main/p/popt/popt_1.16.orig.tar.gz", "e728ed296fe9f069a0e005003c3d6b2dde3d9cad453422a10d6558616d304cc8")
+    ArchiveSource("http://archive.ubuntu.com/ubuntu/pool/main/p/popt/popt_1.16.orig.tar.gz", "e728ed296fe9f069a0e005003c3d6b2dde3d9cad453422a10d6558616d304cc8"),
+    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
@@ -15,25 +16,26 @@ script = raw"""
 cd $WORKSPACE/srcdir
 cd popt-1.16/
 update_configure_scripts
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/0001-nl_langinfo.mingw32.patch"
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/197416.all.patch"
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/217602.all.patch"
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/278402-manpage.all.patch"
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/318833.all.patch"
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/356669.all.patch"
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/367153-manpage.all.patch"
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/get-w32-console-maxcols.mingw32.patch"
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/no-uid-stuff-on.mingw32.patch"
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-static
 make -j${nproc}
 make install
 install_license COPYING
+false
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64)
-]
+platforms = supported_platforms()
+
 
 
 # The products that we will ensure are always built
