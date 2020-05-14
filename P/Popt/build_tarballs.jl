125da4eccad56b7548866c140628ca6e7de87d77
Mosè Giordano <mose@gnu.org>
2020-05-15 00:28:01 +0100

    [Popt] Add `Libiconv_jll` to the dependencies

--- P/Popt/build_tarballs.jl
+++ P/Popt/build_tarballs.jl
@@ -49,7 +49,8 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
+dependencies = [
+    Dependency("Libiconv_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
