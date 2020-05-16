49c74195d9be8cbd57c50ca8e8a95b8f33a0d17b
Elliot Saba <staticfloat@gmail.com>
2020-05-16 13:21:30 -0400

    Updates to installation

--- .ci/azp_agent/install_agents.sh
+++ .ci/azp_agent/install_agents.sh
@@ -22,7 +22,7 @@ if [[ ! -d "${STORAGE_DIR}/rootfs" ]]; then
 
     # Set up the one true locale
     echo "en_US.UTF-8 UTF-8" >> ${STORAGE_DIR}/rootfs/etc/locale.gen
-    sudo chroot /data/staticfloat/yggstorage2/rootfs locale-gen
+    sudo chroot ${STORAGE_DIR}/rootfs locale-gen
 
     # Install Julia into the rootfs
     echo "Installing Julia..."
