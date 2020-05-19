d70d942a4c0da4cddccb97271c04da5c79c532db
Mosè Giordano <mose@gnu.org>
2020-05-19 10:35:54 +0100

    [Zlib] Use build type Release, which enables `-O3` optimisation flag

--- Z/Zlib/build_tarballs.jl
+++ Z/Zlib/build_tarballs.jl
@@ -7,8 +7,8 @@ version = v"1.2.11"
 
 # Collection of sources required to build zlib
 sources = [
-    "https://zlib.net/zlib-$(version).tar.gz" =>
-    "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1",
+    ArchiveSource("https://zlib.net/zlib-$(version).tar.gz",
+                  "c3e5e9fdd5004dcb542feda5ee4f0ff0744628baf8ed2dd5d66f8ca1197cb1a1"),
 ]
 
 # Bash recipe for building across all platforms
@@ -17,8 +17,12 @@ cd $WORKSPACE/srcdir/zlib-*
 mkdir build && cd build
 
 # We use `-DUNIX=true` to ensure that it is always named `libz` instead of `libzlib` or something ridiculous like that.
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" -DUNIX=true ..
-make install -j${nproc} ${EXTRA_MAKE_FLAGS}
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DUNIX=true \
+    ..
+make install -j${nproc}
 install_license ../README
 """
 
@@ -31,7 +35,7 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
+dependencies = Dependency[
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
