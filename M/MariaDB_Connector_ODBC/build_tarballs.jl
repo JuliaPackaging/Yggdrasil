a832db2409d494cf958b023696afff4e29033dba
Mosè Giordano <giordano@users.noreply.github.com>
2020-05-20 01:25:52 +0100

    [MariaDB_Connector_ODBC] Add builder (#1055)

---
+++ M/MariaDB_Connector_ODBC/build_tarballs.jl
@@ -0,0 +1,107 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "MariaDB_Connector_ODBC"
+version = v"3.1.7"
+
+# Collection of sources required to build MariaDB_Connector_ODBC
+sources = [
+    ArchiveSource("https://downloads.mariadb.com/Connectors/odbc/connector-odbc-3.1.7/mariadb-connector-odbc-3.1.7-ga-src.tar.gz",
+                  "699c575e169d770ccfae1c1e776aa7725d849046476bf6579d292c89e8c8593e"),
+    FileSource("https://downloads.mariadb.com/Connectors/odbc/connector-odbc-3.1.7/mariadb-connector-odbc-3.1.7-win64.msi",
+               "ef1ad796cc4aba67aa6606b35239302c579b7894604239d1caaa1f2d10623bd2"; filename = "x86_64-w64-mingw32.msi"),
+    FileSource("https://downloads.mariadb.com/Connectors/odbc/connector-odbc-3.1.7/mariadb-connector-odbc-3.1.7-win32.msi",
+               "a60b74dc3af0b450d892731c2973037beef6fc972e0813f3db9f28f083a63402"; filename = "i686-w64-mingw32.msi"),
+    ## Keep the patches just in case some day we decide to build for Windows
+    ## from source
+    # DirectorySource("./bundled"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+install_license $WORKSPACE/srcdir/mariadb-connector*/COPYING
+
+if [[ "${target}" == *-mingw* ]]; then
+    # For Windows just use the prebuilt library
+    mkdir -p "${libdir}"
+    cd $WORKSPACE/srcdir
+    apk add p7zip
+    7z x "${target}.msi"
+    cp mariadb_odbc_dll "${libdir}/mariadb_odbc.dll"
+    exit
+fi
+
+cd $WORKSPACE/srcdir/mariadb-connector*/
+
+# Skip building of macOS package
+sed -i 's/ADD_SUBDIRECTORY(osxinstall)/# ADD_SUBDIRECTORY(osxinstall)/' CMakeLists.txt
+
+## Keep this for reference in case we decide to build for Windows from source
+# if [[ "${target}" == *-mingw* ]]; then
+#     # Disable dialog plugin
+#     sed -i 's/-DPLUGINS_LIB_DIR.*:dialog.*//' CMakeLists.txt
+#     atomic_patch -p1 "${WORKSPACE}/srcdir/patches/0001-fix-case-headers-windows.patch"
+#     # The following is not really a patch: Microsoft Visual C++ thinks that it's
+#     # a good idea to put some stray characters in the first bits of the file,
+#     # making it hard to digest for MinGW.  We can't diff the file as the
+#     # original one appears to be binary, because of the stray character.
+#     cp "${WORKSPACE}/srcdir/patches/resource.h" dsn/
+# fi
+
+mkdir build && cd build
+
+export CFLAGS="-I${includedir}/mariadb"
+export LDFLAGS="-L${libdir}/mariadb"
+if [[ "${target}" == *-apple-* ]]; then
+    # Link to the right OpenSSL and Zlib libraries
+    export LDFLAGS="${LDFLAGS} ${libdir}/libssl.${dlext} ${libdir}/libcrypto.${dlext} -lz"
+fi
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DWITH_OPENSSL=ON \
+    -DWITH_SSL=OPENSSL \
+    -DOPENSSL_FOUND=ON \
+    -DOPENSSL_CRYPTO_LIBRARY=${libdir}/libcrypto.${dlext} \
+    -DOPENSSL_SSL_LIBRARY=${libdir}/libssl.${dlext} \
+    -DICONV_LIBRARIES=${libdir}/libiconv.${dlext} \
+    -DICONV_INCLUDE_DIR=${includedir} \
+    ..
+make -j${nproc}
+make install
+"""
+
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    # Linux(:aarch64, libc=:glibc),
+    # Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    # Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    # Linux(:aarch64, libc=:musl),
+    # Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    MacOS(:x86_64),
+    # FreeBSD(:x86_64),
+    Windows(:i686),
+    Windows(:x86_64),
+]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct(["libmaodbc", "mariadb_odbc"], :libmaodbc),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    BuildDependency("MariaDB_Connector_C_jll"),
+    Dependency("iODBC_jll"),
+    Dependency("Libiconv_jll"),
+    Dependency("unixODBC_jll"),
+    Dependency("OpenSSL_jll"),
+    Dependency("Zlib_jll"),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
---
+++ M/MariaDB_Connector_ODBC/bundled/patches/resource.h
@@ -0,0 +1,113 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by odbc_dsn.rc
+//
+#define PB_NEXT                         3
+#define IDCANCEL3                       4
+#define PB_PREV                         5
+#define IDCANCEL4                       6
+#define IDD_DIALOG1                     101
+#define Tab_DSN                         102
+#define Page_0                          103
+#define Tab_Cursor                      104
+#define IDB_BITMAP1                     105
+#define Tab_Metadata                    106
+#define Tab_Connection                  108
+#define Tab_Connect                     109
+#define Page_1                          110
+#define Page_2                          111
+#define Page_3                          112
+#define Page_4                          114
+#define Page_5                          115
+#define DSN_Name                        1002
+#define DSN_Description                 1003
+#define DSN_Database                    1005
+#define DSN_UserName                    1013
+#define DSN_Password                    1014
+#define DSN_Test                        1015
+#define IDC_CHECK2                      1017
+#define rbTCP                           1018
+#define IDC_CHECK3                      1019
+#define rbPipe                          1020
+#define IDC_CHECK4                      1021
+#define txtServerName                   1022
+#define IDC_CHECK5                      1023
+#define lblServerName                   1024
+#define txtPort                         1026
+#define lblPort                         1028
+#define IDC_CHECK8                      1029
+#define IDC_CHECK9                      1030
+#define txtServer                       1031
+#define txtUserName                     1032
+#define IDC_EDIT4                       1033
+#define txtPassword                     1034
+#define pbTestDSN                       1035
+#define IDC_COMBO1                      1036
+#define cbDatabase                      1037
+#define txtInitCmd                      1038
+#define ckReconnect                     1039
+#define cbCharset                       1040
+#define ckConnectPrompt                 1041
+#define IDC_EDIT1                       1042
+#define txtConnectionTimeOut            1043
+#define txtDriver                       1200
+#define ckEnableDC                      1045
+#define ckReconnect2                    1046
+#define ckDisableDriverCursorSupport    1047
+#define ckNoCacheForwardOnly            1048
+#define ckPrefetch                      1049
+#define IckUseIS                        1050
+#define ckUseIS                         1051
+#define ckIgnoreSchema                  1052
+#define txtDsnName                      1053
+#define txtDSNDescription               1054
+#define ckEnableDynamicCursor           1100
+#define ckDisableDriverCursor           1101
+#define ckDontCacheForwardCursor        1102
+#define ckForwardCursorOnly             1103
+#define ckReturnMatchedRows             1104
+#define ckEnableSQLAutoIsNull           1105
+#define ckPadCharFullLength             1106
+#define ckNullDate                      1107
+#define ckPrefetchRows                  1108
+#define ckCompressed                    1109
+#define ckDebug                         1110
+#define ckPrefetchRows2                 1111
+#define ckIgnoreSpace                   1112
+#define ckMultiStmt                     1113
+#define ckMatchedRows                   1114
+#define txtPluginDir                    1115
+#define pbPlugindirBrowse               1116
+#define txtSslKey                       1117
+#define txtSslCert                      1118
+#define txtSslCertAuth                  1119
+#define txtSslCaPath                    1120
+#define txtSslCipher                    1121
+#define cbSslVerify                     1122
+#define pbCaPathBrowse                  1123
+#define pbKeyBrowse                     1124
+#define pbCertBrowse                    1125
+#define pbCaCertBrowse                  1126
+#define ckUseMycnf                      1127
+/* TLSVERSION checkbox group. Id determines bit */
+#define cbTls11                         1128
+#define cbTls12                         1129
+#define cbTls13                         1131
+/* End of TLSVERSION */
+#define cbForceTls                      1300
+#define txtServerKey                    1200
+#define pbServerKeyBrowse               1201
+#define txtTlsPeerFp                    1210
+#define txtTlsPeerFpList                1211
+#define pbFpListBrowse                  1212
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        107
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1044
+#define _APS_NEXT_SYMED_VALUE           113
+#endif
+#endif
