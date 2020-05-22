55c0edca351ba3cd335f4539c676fb231286c4f5
Wiktor Phillips <wsphillips@gmail.com>
2020-05-22 14:44:11 +0200

    syntax fix

--- C/CImPlot/build_tarballs.jl
+++ C/CImPlot/build_tarballs.jl
@@ -46,8 +46,8 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
-    "CImGui_jll",
+dependencies = [
+    Dependency("CImGui_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
