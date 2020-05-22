f558178e737bf52b70332b3b5c01c067701aa96c
Wiktor Phillips <wsphillips@gmail.com>
2020-05-22 14:39:31 +0200

    revised binary dependencies

--- C/CImPlot/build_tarballs.jl
+++ C/CImPlot/build_tarballs.jl
@@ -3,18 +3,18 @@
 using BinaryBuilder
 
 name = "CImPlot"
-version = v"1.76.0"
+version = v"0.0.1"
 
 # Collection of sources required to build CImGui
 sources = [
-    GitSource("https://github.com/ocornut/imgui.git",
-              "5503c0a12e0c929e84b3f61b2cb4bb9177ea3da1"),
-
     GitSource("https://github.com/cimgui/cimplot.git",
               "8799c69b20081a744f44bb1ff9d39f6c8d8f23a1"),
 
     GitSource("https://github.com/epezent/implot.git",
               "1353014bce7d330e612529cb6193d811281eabac"),
+
+    GitSource("https://github.com/ocornut/imgui.git",
+              "58b3e02b95b4c7c5bb9128a28c6d55546501bf93"),
 
     DirectorySource("./bundled"),
 ]
@@ -22,16 +22,17 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-mv cmake/CMakeLists.txt ./
+mv cmake/CMakeLists.txt ./cimplot/
 mv implot cimplot/
+cd cimplot/
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
 make -j${nproc}
 make install
 mkdir -p ${prefix}/share/licenses/${SRC_NAME}
-mv ../cimplot/LICENSE ${prefix}/share/licenses/${SRC_NAME}/LICENSE_cimplot
-mv ../cimplot/implot/LICENSE ${prefix}/share/licenses/${SRC_NAME}/LICENSE_implot
-mv ../imgui/LICENSE.txt ${prefix}/share/licenses/${SRC_NAME}/LICENSE_imgui
+mv ../LICENSE ${prefix}/share/licenses/${SRC_NAME}/LICENSE_cimplot
+mv ../implot/LICENSE ${prefix}/share/licenses/${SRC_NAME}/LICENSE_implot
+mv ../../imgui/LICENSE.txt ${prefix}/share/licenses/${SRC_NAME}/LICENSE_imgui
 """
 
 # These are the platforms we will build for by default, unless further
@@ -46,6 +47,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
+    "CImGui_jll",
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
