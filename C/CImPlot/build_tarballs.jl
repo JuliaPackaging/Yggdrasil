68153f1cdf58de5032230cb7e66643c5c74707df
Wiktor Phillips <wsphillips@gmail.com>
2020-05-22 16:11:50 +0200

    [CImPlot] initial commit (#1058)
    * CImPlot_jll initial commit
    
    * license installation fix
    
    * typo fix
    
    * revised binary dependencies
    
    * syntax fix
    
    * spec for CImGui_jll at most recent build
    
    * version spec and automatic header import for imgui
    
    * file moved

---
+++ C/CImPlot/build_tarballs.jl
@@ -0,0 +1,50 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+using Pkg
+name = "CImPlot"
+version = v"0.0.1"
+
+# Collection of sources required to build CImGui
+sources = [
+    GitSource("https://github.com/cimgui/cimplot.git",
+              "8799c69b20081a744f44bb1ff9d39f6c8d8f23a1"),
+
+    GitSource("https://github.com/epezent/implot.git",
+              "1353014bce7d330e612529cb6193d811281eabac"),
+
+    DirectorySource("./bundled"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+mv cmake/CMakeLists.txt ./cimplot/
+mv implot cimplot/
+cd cimplot/
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
+make -j${nproc}
+make install
+mkdir -p ${prefix}/share/licenses/${SRC_NAME}
+mv ../LICENSE ${prefix}/share/licenses/${SRC_NAME}/LICENSE_cimplot
+mv ../implot/LICENSE ${prefix}/share/licenses/${SRC_NAME}/LICENSE_implot
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libcimplot", :libcimplot),
+    FileProduct("share/compile_commands.json", :compile_commands)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+                Dependency(PackageSpec(name="CImGui_jll", version=v"1.75.0")),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
