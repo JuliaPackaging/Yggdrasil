80189e0c2d5f2fea1f9557ba87858442b0e5ca48
Xiaoliang Pan <panxl@users.noreply.github.com>
2020-05-23 21:34:19 -0500

    Remove Windows support

--- L/libcint/build_tarballs.jl
+++ L/libcint/build_tarballs.jl
@@ -28,12 +28,9 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:x86_64, libc=:musl),
-    Windows(:x86_64),
+    Linux(:x86_64),
     MacOS(:x86_64),
-    Linux(:x86_64, libc=:glibc)
 ]
-
 
 # The products that we will ensure are always built
 products = [
