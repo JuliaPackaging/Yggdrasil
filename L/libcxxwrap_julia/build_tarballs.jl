79744765abbdf217e5a0451d51886ff567bc133d
Viral B. Shah <ViralBShah@users.noreply.github.com>
2020-05-25 18:48:30 -0400

    Update L/libcxxwrap_julia/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/libcxxwrap_julia/build_tarballs.jl
+++ L/libcxxwrap_julia/build_tarballs.jl
@@ -55,7 +55,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-	Dependency("Julia_jll")
+    BuildDependency("Julia_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
