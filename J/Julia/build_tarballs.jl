d1e3fa2ece308144c47479d525a1018f24f00672
Viral B. Shah <viral@mayin.org>
2020-05-25 18:34:00 -0400

    update hashes.

--- J/Julia/build_tarballs.jl
+++ J/Julia/build_tarballs.jl
@@ -18,21 +18,21 @@ platforms = [
 
 sources = [
     ArchiveSource("https://julialang2.s3.amazonaws.com/bin/linux/aarch64/1.4/julia-$(version)-linux-aarch64.tar.gz",
-                  "788dc1e79344b52f65358ce4406dc5304bafd82c6af50bfa92a6ee5ea998e678"; unpack_target="aarch64-linux-gnu-libgfortran4-cxx11"),
-    ArchiveSource("https://julialang2.s3.amazonaws.com/bin/linux/armv7l/1.4/julia-$(version)-linux-armv7l.tar.gz",
+                  "f124d1b9fa68c3049d4ffe2349454f8ba1753d17d6578bc6e7cb916aed7cff4a"; unpack_target="aarch64-linux-gnu-libgfortran4-cxx11"),
+#    ArchiveSource("https://julialang2.s3.amazonaws.com/bin/linux/armv7l/1.4/julia-$(version)-linux-armv7l.tar.gz",
                   "bdcf24e0365f16092838daf7059bf5c0036bff9dc418511010e79249d9f71e96"; unpack_target="armv7l-linux-gnueabihf-libgfortran4-cxx11"),
     ArchiveSource("https://julialang2.s3.amazonaws.com/bin/linux/x64/1.4/julia-$(version)-linux-x86_64.tar.gz",
-                  "fd6d8cadaed678174c3caefb92207a3b0e8da9f926af6703fb4d1e4e4f50610a"; unpack_target="x86_64-linux-gnu-libgfortran4-cxx11"),
+                  "d77311be23260710e89700d0b1113eecf421d6cf31a9cebad3f6bdd606165c28"; unpack_target="x86_64-linux-gnu-libgfortran4-cxx11"),
     ArchiveSource("https://julialang2.s3.amazonaws.com/bin/linux/x86/1.4/julia-$(version)-linux-i686.tar.gz",
-                  "765e614b2754b20d50bae475dd9f3b794f445915084afa42523fd1b14e4c91fe"; unpack_target="i686-linux-gnu-libgfortran4-cxx11"),
+                  "ce821b6671a137dc7c2ccbf40ff08471a6791ea8af80a30d6716806608e72dab"; unpack_target="i686-linux-gnu-libgfortran4-cxx11"),
     ArchiveSource("https://julialang2.s3.amazonaws.com/bin/mac/x64/1.4/julia-$(version)-mac64.tar.gz",
-                  "78afd9c6769d645f7c30ad14dacadfd826cc5b1b3227c46fd7592ecde8af2fc3"; unpack_target="x86_64-apple-darwin14-libgfortran4-cxx11"),
+                  "660ecd64db812e642e2b9483966c61f7fec3ee88c84a1eb26831bfce2aa51ac9"; unpack_target="x86_64-apple-darwin14-libgfortran4-cxx11"),
     ArchiveSource("https://julialang2.s3.amazonaws.com/bin/winnt/x64/1.4/julia-$(version)-win64.tar.gz",
                   "c4c1ae109f7eed5e9791d871b3e29003309d61ffa55a05cfd23184447ec8cfbe"; unpack_target="x86_64-w64-mingw32-libgfortran4-cxx11"),
     ArchiveSource("https://julialang2.s3.amazonaws.com/bin/winnt/x86/1.4/julia-$(version)-win32.tar.gz",
                   "04d1abb1e28d643c6d049a4b241483ced155aebaf3eea8fb2d995299543645c5"; unpack_target="i686-w64-mingw32-libgfortran4-cxx11"),
     ArchiveSource("https://julialang-s3.julialang.org/bin/freebsd/x64/1.4/julia-$(version)-freebsd-x86_64.tar.gz",
-                  "f7a6545953f8843201acec16ea66ba0d0ced8145439b6cebfcb893e9671cf7af"; unpack_target="x86_64-unknown-freebsd11.1-libgfortran4-cxx11"),
+                  "29892e81d663dc081281b8530fa2e949306da933dcf2d6d28a5dec165a92aa24"; unpack_target="x86_64-unknown-freebsd11.1-libgfortran4-cxx11"),
 ]
 script = raw"""
 cp -rva ${WORKSPACE}/srcdir/${bb_full_target}/julia-*/* ${prefix}/
