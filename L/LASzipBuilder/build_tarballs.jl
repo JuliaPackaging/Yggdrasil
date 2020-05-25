c9982eeb8476109199128c090ffe345e66b970a1
Maarten Pronk <evetion@users.noreply.github.com>
2020-05-25 16:08:12 +0000

    Apply suggestions from code review
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/LASzipBuilder/build_tarballs.jl
+++ L/LASzipBuilder/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "LASzipBuilder"
+name = "LASzip"
 version = v"3.4.3"
 
 # Collection of sources required to complete build
@@ -15,18 +15,14 @@ script = raw"""
 cd $WORKSPACE/srcdir
 
 # Patch to find unordered_map from tr1, logic there doesn't work for MINGW32.
-if [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]; 
-    then sed -i '/add_definitions(-DUNORDERED)/d' LASzip/src/CMakeLists.txt;  
+if [[ "${target}" == *-mingw* ]]; then
+    sed -i '/add_definitions(-DUNORDERED)/d' LASzip/src/CMakeLists.txt;  
 fi
 
 mkdir LASzip/build
 cd LASzip/build/
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_CXX_FLAGS="-std=c++11" -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=RelWithDebInfo -DCMAKE_VERBOSE_MAKEFILE=OFF ..
 cmake --build . --target install --config Release
-
-# Rename versioned library so BinaryBuilder picks it up
-if [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]; then mv $prefix/bin/liblaszip3.dll $prefix/bin/liblaszip.dll;  fi
-exit
 """
 
 # These are the platforms we will build for by default, unless further
