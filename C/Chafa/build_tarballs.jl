80fe904a4ed0aecfb4f813b663dc0de0c83375cc
Alex Arslan <ararslan@comcast.net>
2020-05-28 09:45:27 -0700

    Add Chafa, a library for images in the terminal

---
+++ C/Chafa/build_tarballs.jl
@@ -0,0 +1,30 @@
+using BinaryBuilder
+
+name = "Chafa"
+version = v"1.4.1"
+
+sources = [
+    ArchiveSource("https://hpjansson.org/chafa/releases/chafa-$(version).tar.xz",
+                  "46d34034f4c96d120e0639f87a26590427cc29e95fe5489e903a48ec96402ba3"),
+]
+
+script = raw"""
+cd ${WORKSPACE}/srcdir/chafa-*/
+./autogen.sh
+make -j${nproc}
+make install
+"""
+
+platforms = supported_platforms()
+
+products = [
+    LibraryProduct("libchafa", :libchafa),
+    ExecutableProduct("chafa", :chafa),
+]
+
+dependencies = [
+    Dependency("Glib_jll"),
+    Dependency("ImageMagick_jll"),
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
