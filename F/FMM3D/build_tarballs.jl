9fd9dc9bdd62af3c5afa0153afdca87b80f7b6e7
Travis Askham <askhamwhat@gmail.com>
2020-05-29 12:12:06 -0400

    Update F/FMM3D/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- F/FMM3D/build_tarballs.jl
+++ F/FMM3D/build_tarballs.jl
@@ -75,9 +75,7 @@ exit
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-platforms = expand_gfortran_versions(platforms)
-platforms = expand_cxxstring_abis(platforms)
+platforms = expand_gfortran_versions(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
