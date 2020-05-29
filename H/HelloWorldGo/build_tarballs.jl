792e6f47df1a097b114132918554daf605a4c98c
Mosè Giordano <mose@gnu.org>
2020-05-29 12:16:11 +0100

    [HelloWorldGo] Third attempt to update the wrappers
    `[skip ci]`

--- H/HelloWorldGo/build_tarballs.jl
+++ H/HelloWorldGo/build_tarballs.jl
@@ -26,5 +26,5 @@ products = [
 dependencies = Dependency[
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
+# Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers=[:c, :go])
