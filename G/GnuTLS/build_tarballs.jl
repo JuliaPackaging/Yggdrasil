e436b70a62a699b1b10fb85b58cc2bcdc316c852
Elliot Saba <staticfloat@gmail.com>
2020-05-28 23:03:34 -0700

    [GnuTLS]: Bump to v3.6.13
    Also update to new BB syntax

--- G/GnuTLS/build_tarballs.jl
+++ G/GnuTLS/build_tarballs.jl
@@ -1,12 +1,12 @@
 using BinaryBuilder
 
 name = "GnuTLS"
-version = v"3.6.5"
+version = v"3.6.13"
 
 # Collection of sources required to build GnuTLS
 sources = [
-    "https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.5.tar.xz" =>
-    "073eced3acef49a3883e69ffd5f0f0b5f46e2760ad86eddc6c0866df4e7abb35",
+    ArchiveSource("https://www.gnupg.org/ftp/gcrypt/gnutls/v3.6/gnutls-3.6.13.tar.xz" =>
+                  "32041df447d9f4644570cf573c9f60358e865637d69b7e59d1159b7240b52f38"),
 ]
 
 # Bash recipe for building across all platforms
@@ -14,9 +14,9 @@ script = raw"""
 cd $WORKSPACE/srcdir/gnutls-*/
 
 # Grumble-grumble apple grumble-grumble broken linkers...
-if [[ ${target} == *-apple-* ]]; then
-    export AR=/opt/${target}/bin/ar
-fi
+#if [[ ${target} == *-apple-* ]]; then
+#    export AR=/opt/${target}/bin/ar
+#fi
 
 ./configure --prefix=${prefix} --host=${target} \
     --with-included-libtasn1 \
@@ -38,15 +38,15 @@ platforms = [p for p in platforms if !(typeof(p) <: FreeBSD)]
 platforms = [p for p in platforms if !(typeof(p) <: Windows)]
 
 # The products that we will ensure are always built
-products = prefix -> [
-    LibraryProduct(prefix, "libgnutls", :libgnutls),
+products = Product[
+    LibraryProduct("libgnutls", :libgnutls),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "https://github.com/bicycle1885/ZlibBuilder/releases/download/v1.0.3/build_Zlib.v1.2.11.jl",
-    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/GMP-v6.1.2-1/build_GMP.v6.1.2.jl",
-    "https://github.com/JuliaPackaging/Yggdrasil/releases/download/NettleHogweed-v3.4.1-0/build_Nettle.v3.4.1.jl",
+    "Zlib_jll",
+    "GMP_jll",
+    "Nettle_jll",
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
