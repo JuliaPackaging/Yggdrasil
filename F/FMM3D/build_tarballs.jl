747606a23e7f17640a3ecb08ebff37520efdea1a
Travis Askham <askhamwhat@gmail.com>
2020-05-29 12:47:12 -0400

    add initial build_tarballs for FMM3D (#1092)
    * add initial build_tarballs for FMM3D
    
    * [FMM3D] fix darwin and mingw (expected dylib/dll)
    turn off fast_ker on certain platforms
    
    * [FMM3D] attempt fix of invalid register error for mingw
    
    * [FMM3D] restore flags for mingw but keep fast_ker off
    
    * [FMM3D] disable fast kernels for now, various issues
    
    * [FMM3D] attempt fix at registry issue in mingw
    
    * [FMM3D] remove march native, restore openmp linking on apple and
    mingw (requires gcc on apple and freebsd), clean up file
    
    * Update F/FMM3D/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Update F/FMM3D/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ F/FMM3D/build_tarballs.jl
@@ -0,0 +1,90 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+
+# author: Travis Askham (askhamwhat@gmail.com)
+#
+#TODO: there are a number of stumbling points for the
+# FAST_KER=ON option when cross-compiling. it is completely
+# disabled for now. Some issues:
+# - on musl, darwin, and freebsd various standard libraries
+#   are not found
+# - on mingw there is an invalid regiser error on x86_64 arch
+# - generally, these seem to require a later version of gcc
+#   but we would like to compile for various libgfortran
+#   major versions
+
+using BinaryBuilder, Pkg
+
+name = "FMM3D"
+version = v"0.0.1"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/flatironinstitute/FMM3D.git", "2fd129984e48abe437fddd12013dc434563a773a")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd FMM3D/
+
+touch make.inc
+
+# clang has some issue with openmp (lomp not found)
+# force gcc
+if [[ ${target} = *apple* || ${target} = *freebsd* ]]; then
+  export CC="gcc"
+fi
+
+echo "CC=${CC}" >> make.inc;
+echo "CXX=${CXX}" >> make.inc;
+echo "FC=${FC}" >> make.inc;
+
+if [[ ${target} = *mingw* ]]; then
+    echo "FFLAGS= -fPIC -O3 -fno-asynchronous-unwind-tables -funroll-loops" >> make.inc;
+else
+    echo "FFLAGS= -fPIC -O3 -funroll-loops" >> make.inc;
+fi
+
+export OMPFLAGS="-fopenmp"
+export OMPLIBS="-lgomp"
+echo "OMPFLAGS= ${OMPFLAGS}" >> make.inc;
+echo "OMPLIBS= ${OMPLIBS}" >> make.inc;
+
+export LIBS="-lm -lgfortran"
+echo "LIBS = ${LIBS}" >> make.inc;
+
+make -j${nproc} lib OMP=ON
+
+echo "${TARGET}"
+
+# build the correct type of dynamic library for
+# current target
+
+export SHAREFLAGS="-shared -fPIC"
+
+cd lib-static
+ar x libfmm3d.a
+${CC} ${SHAREFLAGS} ${OMPFLAGS} *.o -o "${libdir}/libfmm3d.${dlext}" ${LIBS} ${OMPLIBS}
+cd ..
+
+install_license ${WORKSPACE}/srcdir/FMM3D/LICENSE
+rm make.inc
+exit
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_gfortran_versions(supported_platforms())
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libfmm3d", :libfmm3d)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;)
