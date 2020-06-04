da4758dc8f9fa6c528d345b53b183307a020d39d
aviks <avik@sengupta.net>
2020-06-04 10:15:51 -0400

    Capitalise name

--- H/Hwloc/build_tarballs.jl
+++ H/Hwloc/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "hwloc"
+name = "Hwloc"
 version = v"2.2.0"
 
 # Collection of sources required to build hwloc
