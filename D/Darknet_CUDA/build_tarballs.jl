4fdbd1d80a5a1ee376b25d403c5af634f07b6996
Ian <i.r.butterworth@gmail.com>
2020-06-06 18:56:50 -0400

    tidy up bad commit

--- D/Darknet_CUDA/build_tarballs.jl
+++ D/Darknet_CUDA/build_tarballs.jl
@@ -7,22 +7,22 @@ version = v"2020.6.5"
 sources = [
     ArchiveSource("https://github.com/AlexeyAB/darknet/archive/3708b2e47d355ba0a206fd7a06bbc5a6e38af4ff.zip", "e18a6374822fe3c9b95f2b6a4086decbdfbd1c589f2481ce5704a4384044ea6f"),
     ArchiveSource("https://github.com/JuliaGPU/CUDABuilder/releases/download/v0.3.0/CUDNN+CUDA10.1.v7.6.5.x86_64-linux-gnu.tar.gz", "79de5b5085a33bc144b87028e998a1d295a15c3424d6d45b25defe500f616974", unpack_target = "cudnn"),
+    DirectorySource("./bundled"),
 ]
-
-
 
 script = raw"""
 cd $WORKSPACE/srcdir
 
 ## CUDA setup
-
-mv cudnn $prefix                # Note - when MacOS is added, Darknet expects cudnn in cuda's dir: https://github.com/AlexeyAB/darknet/blob/master/Makefile#L112
 mkdir -p /usr/local
-ln -s ${prefix}/cudnn /usr/local/cudnn
+ln -s "${WORKSPACE}/srcdir/cudnn" /usr/local/cudnn
 ln -s ${prefix}/cuda /usr/local/cuda
-export PATH=$PATH:/usr/local/cuda
+
+export PATH="/usr/local/cuda/bin:${PATH}"
 
 cd $WORKSPACE/srcdir/darknet-*
+
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/makefile-fix-cuda-paths.patch"
 
 ## Required for OPENMP=1
 if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]]; then
@@ -56,7 +56,9 @@ make OPTS="" -j${nproc} libdarknet.${dlext} \
 
 mkdir -p "${libdir}"
 cp libdarknet.${dlext} "${libdir}"
-rm -rf $prefix/cuda
+install_license LICENSE
+
+rm -rf "${prefix}/cuda"
 """
 
 # These are the platforms we will build for by default, unless further
@@ -67,7 +69,7 @@ platforms = [
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libdarknet", :libdarknet),
+    LibraryProduct("libdarknet", :libdarknet; dont_dlopen = true),
 ]
 
 # Dependencies that must be installed before this package can be built
