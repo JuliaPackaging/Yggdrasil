82fbf52507373f659b2b27c9ae47627585e64da6
aviks <avik@sengupta.net>
2020-06-07 21:35:34 -0400

    add windows patch from mingw-packages

--- G/Gdbm/build_tarballs.jl
+++ G/Gdbm/build_tarballs.jl
@@ -7,13 +7,19 @@ version = v"1.18.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("ftp://ftp.gnu.org/gnu/gdbm/gdbm-1.18.1.tar.gz", "86e613527e5dba544e73208f42b78b7c022d4fa5a6d5498bf18c8d6f745b91dc")
+    ArchiveSource("ftp://ftp.gnu.org/gnu/gdbm/gdbm-1.18.1.tar.gz", "86e613527e5dba544e73208f42b78b7c022d4fa5a6d5498bf18c8d6f745b91dc"),
+    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd gdbm-1.18.1/
+
+if [[ "${target}" == *-mingw* ]]; then
+    atomic_patch -p1 ../patches/gdbm-1.15-win32.patch
+fi
+
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-static --with-libiconv-prefix=${prefix}
 make -j${nproc}
 make install
@@ -21,17 +27,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    FreeBSD(:x86_64)
-]
+platforms = supported_platforms()
 
 
 # The products that we will ensure are always built
