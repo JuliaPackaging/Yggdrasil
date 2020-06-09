95f17e9c371779f3912207ef50edeae616b42375
aviks <avik@sengupta.net>
2020-06-09 09:11:08 -0400

    Add mingw patches. Add gdbm

--- C/CyrusSASL/build_tarballs.jl
+++ C/CyrusSASL/build_tarballs.jl
@@ -14,9 +14,23 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/cyrus-sasl-*/
+if [[ "${target}" == *-mingw* ]]; then
+  atomic_patch -p1 ../patches/02-exeext.patch
+  atomic_patch -p1 ../patches/03-fix-plugins.patch
+  atomic_patch -p1 ../patches/04-manpage-paths.patch
+  atomic_patch -p1 ../patches/14-MinGW-w64-add-LIBSASL_API-to-function-definitions.patch
+  atomic_patch -p1 ../patches/15-MinGW-w64-define-LIBSASL_EXPORTS_eq_1-for-sasldb.patch
+  atomic_patch -p1 ../patches/16-MinGW-w64-define-WIN32_LEAN_AND_MEAN-avoiding-handle_t-redef.patch
+  atomic_patch -p1 ../patches/17-MinGW-w64-define-S_IRUSR-and-S_IWUSR.patch
+  atomic_patch -p1 ../patches/19-paths-relocation.patch
+  atomic_patch -p1 ../patches/20-mingw-tchar.patch
+  atomic_patch -p1 ../patches/21-fix-getopt-guard.patch
+
+  cp win32/include/md5global.h include/md5global.h
+fi
 atomic_patch -p1 ../patches/macos-shared-lib-extension.patch
 autoreconf -vi
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-openssl=${prefix} --enable-ntlm --disable-gssapi --with-dblib=none
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-openssl=${prefix} --with-sqlite3=${prefix} --oldincludedir=${prefix}/include --enable-ntlm --disable-gssapi --with-dblib=gdbm --disable-static --disable-ldapdb --without-saslauthd --without-pwcheck --without-des --without-authdemond --disable-sample --with-plugindir=${prefix}/lib/sasl2 --with-configdir=${prefix}/etc/sasl2:${prefix}/etc/sasl:${prefix}/lib/sasl2
 make -j${nproc}
 make install
 install_license COPYING
@@ -42,6 +56,8 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"))
+    Dependency(PackageSpec(name="SQLite_jll", uuid="76ed43ae-9a5d-5a62-8c75-30186b810ce8"))
+    Dependency(PackageSpec(name="Gdbm_jll", uuid="54ca2031-c8dd-5cab-9ed4-295edde1660f"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
