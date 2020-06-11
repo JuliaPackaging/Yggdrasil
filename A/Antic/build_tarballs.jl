870578b0593078653044d9eb9f34e92f3fc08da1
Tommy Hofmann <thofma@gmail.com>
2020-06-11 14:35:15 +0200

    New Recipe: Antic v0.2.0+0

---
+++ A/Antic/build_tarballs.jl
@@ -0,0 +1,47 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Antic"
+version = v"0.2.0+0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/wbhart/antic.git", "c02176789fe2d5d28cf928eafa63c3765117cc7c")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd antic/
+
+if [[ ${target} == *musl* ]]; then
+   export CFLAGS=-D_GNU_SOURCE=1;
+elif [[ ${target} == *mingw* ]]; then
+   sed -i -e "s#/lib\>#/$(basename ${libdir})#g" configure
+   extraflags=--build=MINGW${nbits};
+fi
+
+./configure --prefix=$prefix --disable-static --enable-shared --with-gmp=$prefix --with-mpfr=$prefix --with-flint=$prefix ${extraflags}
+make -j${nproc}
+make install LIBDIR=$(basename ${libdir})
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libantic", :libarb)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="FLINT_jll", uuid="e134572f-a0d5-539d-bddf-3cad8db41a82"))
+    Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"))
+    Dependency(PackageSpec(name="MPFR_jll", uuid="3a97d323-0669-5f0c-9066-3539efd106a3"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
