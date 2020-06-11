6e9bcd0998826c896b1d001340532d551dcae80a
thofma <thofma@gmail.com>
2020-06-11 13:56:50 +0200

    Update build_tarballs.jl

--- A/Arb/build_tarballs.jl
+++ A/Arb/build_tarballs.jl
@@ -27,15 +27,18 @@ make install LIBDIR=$(basename ${libdir})
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64)
-]
+
+platforms = supported_platforms()
+
+#platforms = [
+#    Linux(:i686, libc=:glibc),
+#    Linux(:x86_64, libc=:glibc),
+#    Linux(:aarch64, libc=:glibc),
+#    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+#    Linux(:powerpc64le, libc=:glibc),
+#    MacOS(:x86_64),
+#    FreeBSD(:x86_64)
+#]
 
 
 # The products that we will ensure are always built
