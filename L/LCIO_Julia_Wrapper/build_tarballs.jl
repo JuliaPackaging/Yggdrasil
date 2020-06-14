fa7bfc56156ad015eda4a35f09c81eee4ce7af47
Jan Strube <jan.strube@gmail.com>
2020-06-14 16:13:53 -0700

    [LCIO_Julia_Wrapper] version bump to follow dependencies (#1151)
    * [LCIO_Julia_Wrapper] version bump to follow dependencies
    
    * [LCIO_Julia_Wrapper] update source version
    
    * [LCIO_Julia_Wrapper] using Pkg
    
    * [LCIO_Julia_Wrapper] setting string abi explicitly

--- L/LCIO_Julia_Wrapper/build_tarballs.jl
+++ L/LCIO_Julia_Wrapper/build_tarballs.jl
@@ -1,20 +1,21 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
+using Pkg
 
 name = "LCIO_Julia_Wrapper"
-version = v"0.8.2"
+version = v"0.9.0"
 
 # Collection of sources required to build LCIOWrapBuilder
 sources = [
-	GitSource("https://github.com/jstrube/LCIO_Julia_Wrapper.git", "d3563a8b29086e264e9483c92a26b7d29faf7c70")
+	GitSource("https://github.com/jstrube/LCIO_Julia_Wrapper.git", "a1c201bd7a15c236f221f4f343dd795874794379")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/LCIO_Julia_Wrapper/
 mkdir build && cd build
-cmake -DCMAKE_PREFIX_PATH=${libdir}/cmake -DJulia_PREFIX=${prefix} -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_FIND_ROOT_PATH=${libdir}/cmake -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DJlCxx_DIR=${libdir}/cmake/JlCxx ..
+cmake -DJulia_PREFIX=${prefix} -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_FIND_ROOT_PATH=${libdir}/cmake -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j${nproc}
 make install
 install_license $WORKSPACE/srcdir/LCIO_Julia_Wrapper/LICENSE
@@ -23,10 +24,9 @@ install_license $WORKSPACE/srcdir/LCIO_Julia_Wrapper/LICENSE
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = Platform[
-    Linux(:x86_64, libc=:glibc),
-    MacOS(:x86_64)
+    Linux(:x86_64; libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    MacOS(:x86_64; compiler_abi=CompilerABI(cxxstring_abi=:cxx11))
 ]
-#platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -35,9 +35,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-	Dependency("libcxxwrap_julia_jll"),
-	Dependency("LCIO_jll"),
-	BuildDependency("Julia_jll")
+        Dependency(PackageSpec(name="libcxxwrap_julia_jll",version=v"0.8")),
+        Dependency("LCIO_jll"),
+        BuildDependency(PackageSpec(name="Julia_jll",version=v"1.4.1"))
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7")
