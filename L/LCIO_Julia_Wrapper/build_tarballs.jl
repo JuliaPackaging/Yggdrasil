590aaa857b1abf63122e792f988e0389a69370a7
Jan Strube <jan.strube@gmail.com>
2020-06-14 14:20:21 -0700

    [LCIO_Julia_Wrapper] source version bump and using builddependency (#1146)
    * [LCIO_Julia_Wrapper] source version bump and using builddependency
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * [LCIO_Julia_Wrapper] trying to fix cmake env
    
    * [LCIO_Julia_Wrapper] cmake env try 5
    
    * [LCIO_Julia_Wrapper] cmake env try 6
    
    * [LCIO_Julia_Wrapper] debugging CMake vars try 6
    
    * [LCIO_Julia_Wrapper] removing cxx03 versions

--- L/LCIO_Julia_Wrapper/build_tarballs.jl
+++ L/LCIO_Julia_Wrapper/build_tarballs.jl
@@ -3,29 +3,18 @@
 using BinaryBuilder
 
 name = "LCIO_Julia_Wrapper"
-version = v"0.8.1"
+version = v"0.8.2"
 
 # Collection of sources required to build LCIOWrapBuilder
 sources = [
-	GitSource("https://github.com/jstrube/LCIO_Julia_Wrapper.git", "0cebd2e94e739e086ee49b2fcce4839786a39b0a"),
-	ArchiveSource("https://julialang-s3.julialang.org/bin/linux/x64/1.3/julia-1.3.1-linux-x86_64.tar.gz", "faa707c8343780a6fe5eaf13490355e8190acf8e2c189b9e7ecbddb0fa2643ad"; unpack_target="julia-x86_64-linux-gnu"),
-        ArchiveSource("https://github.com/Gnimuc/JuliaBuilder/releases/download/v1.3.0/julia-1.3.0-x86_64-apple-darwin14.tar.gz", "f2e5359f03314656c06e2a0a28a497f62e78f027dbe7f5155a5710b4914439b1"; unpack_target="julia-x86_64-apple-darwin14"),
+	GitSource("https://github.com/jstrube/LCIO_Julia_Wrapper.git", "d3563a8b29086e264e9483c92a26b7d29faf7c70")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-case "$target" in
-	arm-linux-gnueabihf|x86_64-linux-gnu)
-        Julia_PREFIX=${WORKSPACE}/srcdir/julia-$target/julia-1.3.1
-        ;;
-    x86_64-apple-darwin14|x86_64-w64-mingw32)
-        Julia_PREFIX=${WORKSPACE}/srcdir/julia-$target/juliabin
-        ;;
-esac
-
 cd ${WORKSPACE}/srcdir/LCIO_Julia_Wrapper/
 mkdir build && cd build
-cmake -DJulia_PREFIX=${Julia_PREFIX} -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_FIND_ROOT_PATH=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_PREFIX_PATH=${libdir}/cmake -DJulia_PREFIX=${prefix} -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_FIND_ROOT_PATH=${libdir}/cmake -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DJlCxx_DIR=${libdir}/cmake/JlCxx ..
 make -j${nproc}
 make install
 install_license $WORKSPACE/srcdir/LCIO_Julia_Wrapper/LICENSE
@@ -37,7 +26,7 @@ platforms = Platform[
     Linux(:x86_64, libc=:glibc),
     MacOS(:x86_64)
 ]
-platforms = expand_cxxstring_abis(platforms)
+#platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -47,7 +36,8 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
 	Dependency("libcxxwrap_julia_jll"),
-	Dependency("LCIO_jll")
+	Dependency("LCIO_jll"),
+	BuildDependency("Julia_jll")
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7")
