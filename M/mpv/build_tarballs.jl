f5b173343a249aa829e610e4b167b5e8ad014d9a
aviks <avik@sengupta.net>
2020-06-13 20:47:14 -0400

    disable FreeBSD

--- M/mpv/build_tarballs.jl
+++ M/mpv/build_tarballs.jl
@@ -44,7 +44,7 @@ fi
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
-
+platforms = [p for p in platforms if !(typeof(p) <: FreeBSD)]
 
 # The products that we will ensure are always built
 products = [
