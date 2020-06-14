fa87e7b548cddc79ff86104b70b8f1f615f7c91c
Mosè Giordano <giordano@users.noreply.github.com>
2020-06-14 16:12:24 +0100

    [pandoc] Use permanet URL for the copyright files (#1148)
    [AZP] Fix detection of [skip build] cookie

--- P/pandoc/build_tarballs.jl
+++ P/pandoc/build_tarballs.jl
@@ -10,8 +10,8 @@ sources = [
     ArchiveSource("https://github.com/jgm/pandoc/releases/download/$pandoc_ver/pandoc-$pandoc_ver-macOS.zip", "c4847f7a6e6a02a7d1b8dc17505896d8a6e4c2ee9e8b325e47a0468036675307"; unpack_target = "x86_64-apple-darwin14"),
     ArchiveSource("https://github.com/jgm/pandoc/releases/download/$pandoc_ver/pandoc-$pandoc_ver-windows-i386.zip", "db5a8533b7e2ef38114e9788e56530bb6be330c326731692f236218682017d4d"; unpack_target = "i686-w64-mingw32"),
     ArchiveSource("https://github.com/jgm/pandoc/releases/download/$pandoc_ver/pandoc-$pandoc_ver-windows-x86_64.zip", "223f7ef1dd926394ee57b6b5893484e51100be8527bd96eec26e284774863084"; unpack_target = "x86_64-w64-mingw32"),
-    FileSource("https://raw.githubusercontent.com/jgm/pandoc/master/COPYING.md", "2b0d4dda4bf8034e1506507a67f80f982131137afe62bf144d248f9faea31da4"),
-    FileSource("https://raw.githubusercontent.com/jgm/pandoc/master/COPYRIGHT", "39be98cc4d2906dd44abf8573ab91557e0b6d51f503d3a889dab0e8bcca1c43f"),
+    FileSource("https://raw.githubusercontent.com/jgm/pandoc/16889a01b95a1b897abdfa2da191c1338f0333b2/COPYING.md", "2b0d4dda4bf8034e1506507a67f80f982131137afe62bf144d248f9faea31da4"),
+    FileSource("https://raw.githubusercontent.com/jgm/pandoc/16889a01b95a1b897abdfa2da191c1338f0333b2/COPYRIGHT", "39be98cc4d2906dd44abf8573ab91557e0b6d51f503d3a889dab0e8bcca1c43f"),
 ]
 
 # Bash recipe for building across all platforms
