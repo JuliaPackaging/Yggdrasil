eed20f9ce51d58f8c2999c85ced3ab31689dd23a
MattDavis <matthew.davis010@gmail.com>
2020-06-15 11:31:37 -0400

    New Recipe: tblis v1.2.0

---
+++ T/tblis/build_tarballs.jl
@@ -0,0 +1,43 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "tblis"
+version = v"1.2.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/devinamatthews/tblis.git", "3e4c4b82943726c443b6f408c9c9791dcad7a847")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd tblis/
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make -j${nproc}
+cp lib/.libs/libtblis.so.0.0.0 /workspace/destdir/lib/libtblis.so
+cp src/external/tci/lib/.libs/libtci.so.0.0.0 /workspace/destdir/lib/libtci.so
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:x86_64, libc=:glibc),
+    Linux(:x86_64, libc=:musl)
+]
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libtci", :tci),
+    LibraryProduct("libtblis", :tblis)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="Hwloc_jll", uuid="e33a78d0-f292-5ffc-b300-72abe9b543c8"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"7.1.0")
