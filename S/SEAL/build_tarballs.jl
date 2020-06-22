9f24f5ccdf2f541e30c988abc7fdc5574a4c091b
Michael Schlottke-Lakemper <michael@sloede.com>
2020-06-22 20:26:14 +0200

    Reduce preferred GCC version to 7.1.0 (was: 9.1.0) to hopefully fix build errors

--- S/SEAL/build_tarballs.jl
+++ S/SEAL/build_tarballs.jl
@@ -81,4 +81,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"7.1.0")
