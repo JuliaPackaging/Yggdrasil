2a36fd144892df2406fd622e800fc1cd65534d16
Michael Schlottke-Lakemper <michael@sloede.com>
2020-06-22 13:04:59 +0200

    Fix name by removing `_jll` suffix from directory and package name

--- S/SEAL/build_tarballs.jl
+++ S/SEAL/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "SEAL_jll"
+name = "SEAL"
 version = v"3.5.4"
 
 # Collection of sources required to complete build
