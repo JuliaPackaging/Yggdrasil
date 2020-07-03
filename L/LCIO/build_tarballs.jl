df0083bba264268d861df17540245c7a1cc1e6c1
Jan Strube <jan.strube@gmail.com>
2020-07-03 16:39:32 -0700

    Update build_tarballs.jl

--- L/LCIO/build_tarballs.jl
+++ L/LCIO/build_tarballs.jl
@@ -18,8 +18,7 @@ mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release
-make -j${nproc}
-make install
+cmake --build .
 """
 
 # These are the platforms we will build for by default, unless further
