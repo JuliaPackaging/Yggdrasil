e527ec295a1d2f38ff56e551de8497232b69a118
Jan Strube <jan.strube@gmail.com>
2020-07-03 16:49:47 -0700

    make gcc version consistent with the Julia Wrapper jll

--- L/LCIO/build_tarballs.jl
+++ L/LCIO/build_tarballs.jl
@@ -18,14 +18,14 @@ mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release
-cmake --build .
+cmake --build . --target install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:x86_64, libc=:glibc),
-    MacOS(:x86_64),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    MacOS(:x86_64, compiler_abi=CompilerABI(cxxstring_abi=:cxx11))
 ]
 platforms = expand_cxxstring_abis(platforms)
 # The products that we will ensure are always built
@@ -40,4 +40,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies, preferred_gcc_version=v"7")
