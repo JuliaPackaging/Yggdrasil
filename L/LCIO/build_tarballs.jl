c9f8eb3897a8e1ded875a60b24d709e8afdf35a7
Jan Strube <jan.strube@gmail.com>
2020-07-03 17:13:14 -0700

    [LCIO] v2.14.01 (#1285)
    * [LCIO] v2.14.01
    
    * Update build_tarballs.jl
    
    * make gcc version consistent with the Julia Wrapper jll

--- L/LCIO/build_tarballs.jl
+++ L/LCIO/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder
 
 name = "LCIO"
-version = v"02.13.03"
+version = v"02.14.01"
 
 # Collection of sources required to build LCIO
 sources = [
-    GitSource("https://github.com/iLCSoft/LCIO.git", "0ac5c384661ed4804c2bde99e774cab96e65740b"),
+    GitSource("https://github.com/iLCSoft/LCIO.git", "e18ec38f569c85ad878f6404fa8f6c24ca08353d"),
 ]
 
 # Bash recipe for building across all platforms
@@ -18,15 +18,14 @@ mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release
-make -j${nproc}
-make install
+cmake --build . --target install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Linux(:x86_64, libc=:glibc),
-    MacOS(:x86_64),
+    Linux(:x86_64, libc=:glibc, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    MacOS(:x86_64, compiler_abi=CompilerABI(cxxstring_abi=:cxx11))
 ]
 platforms = expand_cxxstring_abis(platforms)
 # The products that we will ensure are always built
@@ -41,4 +40,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies, preferred_gcc_version=v"7")
