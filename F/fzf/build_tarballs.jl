ea8b01782d0149442e9a27bb53082ca7b59eb021
Moelf <proton@jling.dev>
2020-07-04 18:17:31 -0400

    New Recipe: fzf v0.21.1

---
+++ F/fzf/build_tarballs.jl
@@ -0,0 +1,35 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "fzf"
+version = v"0.21.1"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/junegunn/fzf.git", "4b518e50a3fce78eebff136dec2f1c8bee7c611c")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd fzf/
+mkdir -p ${bindir}
+go build -o ${bindir}
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("fzf", :fzf)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers = [:go])
