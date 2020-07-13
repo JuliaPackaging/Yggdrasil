03771b0583d45961ba5cf3c8e05ba5a55d8d04b7
Gnimuc <qiyupei@gmail.com>
2020-07-13 08:55:25 +0800

    fix-up

--- L/librealsense/build_tarballs.jl
+++ L/librealsense/build_tarballs.jl
@@ -68,7 +68,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
-    "libusb_jll",
+    Dependency("libusb_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
