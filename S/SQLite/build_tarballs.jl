44345e4448f674657283ce547f2c959808a106c3
Mosè Giordano <mose@gnu.org>
2020-07-14 17:43:47 +0100

    [SQLite] Fix version number

--- S/SQLite/build_tarballs.jl
+++ S/SQLite/build_tarballs.jl
@@ -3,13 +3,13 @@
 using BinaryBuilder, Pkg
 
 name = "SQLite"
-version = v"3.31.1"
+version = v"3.32.3"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://sqlite.org/2020/sqlite-autoconf-3320300.tar.gz", "a31507123c1c2e3a210afec19525fd7b5bb1e19a6a34ae5b998fbd7302568b66"),
     FileSource("https://git.archlinux.org/svntogit/packages.git/plain/trunk/license.txt?h=packages/sqlite&id=33cad63ddb1ba86b7c5a47430c98083ce2b4d86b",
-                  "4e57d9ac979f1c9872e69799c2597eeef4c6ce7224f3ede0bf9dc8d217b1e65d"),
+               "4e57d9ac979f1c9872e69799c2597eeef4c6ce7224f3ede0bf9dc8d217b1e65d"; filename="LICENSE"),
 ]
 
 # Bash recipe for building across all platforms
@@ -18,9 +18,7 @@ cd $WORKSPACE/srcdir/sqlite-autoconf-*/
 ./configure --prefix=$prefix --host=$target
 make -j${nproc}
 make install
-# Manually install the license with a super funky name
-mkdir -p "${prefix}/share/licenses/${SRC_NAME}"
-cp $WORKSPACE/srcdir/*id=* "${prefix}/share/licenses/${SRC_NAME}/LICENSE"
+install_license "${WORKSPACE}/srcdir/LICENSE"
 """
 
 # These are the platforms we will build for by default, unless further
