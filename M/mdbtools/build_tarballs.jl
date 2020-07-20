5d7f1abddc90fba117892a0fbe907ee82af32036
Dheepak Krishnamurthy <me@kdheepak.com>
2020-07-20 16:35:46 -0600

    Change to using supported platforms and add exclude windows

--- M/mdbtools/build_tarballs.jl
+++ M/mdbtools/build_tarballs.jl
@@ -22,22 +22,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64),
-]
-
+platforms = supported_platforms(exclude = [Windows(:i686), Windows(:x86_64)])
 
 # The products that we will ensure are always built
 products = [
