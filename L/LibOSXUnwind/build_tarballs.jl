69d9d9ad04c32c3939eb406ec9c2505ea1bc07b2
Jameson Nash <vtjnash+github@gmail.com>
2020-07-20 18:00:55 -0400

    Update libosxunwind version to v0.0.6 (#1321)
    * Update libosxunwind version
    
    * Update build_tarballs.jl

--- L/LibOSXUnwind/build_tarballs.jl
+++ L/LibOSXUnwind/build_tarballs.jl
@@ -1,33 +1,27 @@
 using BinaryBuilder
 
 name = "LibOSXUnwind"
-version = v"0.0.5"
+version = v"0.0.6"
 
-# Collection of sources required to build libffi
+# Collection of sources required to build libosxunwind
 sources = [
     ArchiveSource("https://github.com/JuliaLang/libosxunwind/archive/v$(version).tar.gz",
-                  "4ba7b3e24988053870d811afdff58ff103929e7531f156e663f3cf25416c9f46"),
+                  "c3943fdd063c2c8c249778326cbfecdc62c804a0509c4ae4799604e1817f9058"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libosxunwind*/
 
-EXTRA_CFLAGS="-ggdb3 -O0"
+EXTRA_CFLAGS="-ggdb3 -O2"
 
 FLAGS=(
     CC="$CC"
     CXX="$CXX"
-    CFLAGS="${CFLAGS} -ggdb3 -O0"
-    CXXFLAGS="${CXXFLAGS} -ggdb3 -O0"
-    # We lie aobut this because Apple version numbers have nothing to do with
-    # upstream LLVM version numbers.  Sigh.
-    CLANG_MAJOR_VERSION=10
+    CFLAGS="${CFLAGS} -ggdb3 -O2"
+    CXXFLAGS="${CXXFLAGS} -ggdb3 -O2"
     PREFIX="${prefix}"
 )
-
-# Comment out CLANG_MAJOR_VERSION setting, since it is broken in BB due to non-apple clang output
-sed -i.bak -e 's/^CLANG_MAJOR_VERSION := .*$//g' Makefile
 
 # When all you have is a hammer...
 make -j${nproc} "${FLAGS[@]}"
