f398e3789ae0ec72f1ce3c15fa996e1ad2749ef1
Nicolau Leal Werneck <nwerneck@gmail.com>
2020-08-03 13:34:28 +0200

    Update A/accerionsensorapi/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- A/accerionsensorapi/build_tarballs.jl
+++ A/accerionsensorapi/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "accerionsensorapi"
+name = "AccerionSensorAPI"
 version = v"0.1.0"
 
 # Collection of sources required to complete build
