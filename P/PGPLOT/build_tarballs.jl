f39cae723598579f10c89f767ea27f569d1383b5
Alexander <alexander@plav.in>
2020-08-03 18:08:16 +0300

    add PGPLOT recipe (#1389)
    * add PGPLOT recipe
    
    * add PGPLOT license
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * remove "make clean"
    
    * fix paths
    
    * don't expand gfortran versions
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * remove dependencies that are likely unnecessary
    
    * add libX11 dependency
    
    * make libX11 a build-time dependency
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * remove unsupported i686-musl build platform
    
    * build library only, remove unnecessary dependency
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * [PGPLOT] Expand gfortran version, add CSL to deps
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ P/PGPLOT/build_tarballs.jl
@@ -0,0 +1,34 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "PGPLOT"
+version = v"5.2"
+
+sources = [ArchiveSource("ftp://ftp.astro.caltech.edu/pub/pgplot/pgplot5.2.tar.gz", "a5799ff719a510d84d26df4ae7409ae61fe66477e3f1e8820422a9a4727a5be4")]
+
+script = raw"""
+cd $WORKSPACE/srcdir
+mkdir pgplot_build && cd pgplot_build/
+cat ../pgplot/drivers.list | sed 's|! PSDRIV|  PSDRIV|g' | sed 's|! GIDRIV|  GIDRIV|g' > drivers.list
+../pgplot/makemake ../pgplot/ linux g77_gcc
+sed -i 's|FCOMPL=g77|FCOMPL=gfortran|' makefile
+make lib SHARED_LD="${FC} -shared  -o libpgplot.${dlext}"
+cp libpgplot.so "${libdir}/libpgplot.so"
+install_license ../pgplot/copyright.notice
+"""
+
+platforms = [
+    Linux(:x86_64, libc=:musl),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:i686, libc=:glibc),
+]
+platform = expand_gfortran_versions(platforms)
+
+products = [LibraryProduct("libpgplot", :libpgplot)]
+
+dependencies = [
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
