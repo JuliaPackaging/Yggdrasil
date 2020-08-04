2413faba8013771a52bc89fb486b09f42f39a6e3
fkguo <62633089+fkguo@users.noreply.github.com>
2020-08-04 23:09:14 +0800

    LoopTools build_tarballs

---
+++ L/LoopTools/build_tarballs.jl
@@ -0,0 +1,39 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "LoopTools"
+version = v"2.15"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("http://www.feynarts.de/looptools/LoopTools-2.15.tar.gz", "a065ffdc4fe6882aa3bb926134ba8ec875d6c0a633c3d4aa5f70db26542713f2"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ${WORKSPACE}/srcdir/LoopTools-*
+
+./configure --prefix=$prefix --build=${MACHTYPE} --host=${target}
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+# platforms = expand_gfortran_versions(supported_platforms())
+#
+platforms = supported_platforms()
+# platforms = expand_cxxstring_abis(platforms)
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("liblooptools", :liblooptools)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
