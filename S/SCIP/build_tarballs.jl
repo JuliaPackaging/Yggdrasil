c0cffe1c549cac982cb030443bc9eeaada19ee2b
Mathieu Besançon <mathieu.besancon@gmail.com>
2020-08-04 12:59:19 +0200

    off is zero

--- S/SCIP/build_tarballs.jl
+++ S/SCIP/build_tarballs.jl
@@ -16,7 +16,7 @@ cd $WORKSPACE/srcdir
 cd scipoptsuite-7.0.1/
 mkdir build
 cd build/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DPAPILO=0 -DZIMPL=off -DBOOST=off ..
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DPAPILO=0 -DZIMPL=0 -DBOOST=off ..
 make -j${nproc}
 make install
 """
