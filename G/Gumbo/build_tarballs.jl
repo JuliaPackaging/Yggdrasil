e9a69bd5574b651e99bbd3fb860d3eb5b431b41c
Avik Sengupta <avik@sengupta.net>
2020-08-07 11:52:06 +0100

    try gumbo on all platforms

--- G/Gumbo/build_tarballs.jl
+++ G/Gumbo/build_tarballs.jl
@@ -6,6 +6,8 @@ name = "Gumbo"
 version = v"0.10.1"
 
 # Collection of sources required to complete build
+#This is commit dated Jun 28, 2016 which is currently master as of Aug 5, 2020
+# v0.10.1 is the last release, so we keep that version number. 
 sources = [
     "https://github.com/google/gumbo-parser.git" =>
     "aa91b27b02c0c80c482e24348a457ed7c3c088e0",
@@ -24,21 +26,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
+platforms =  supported_platforms()
 
 # The products that we will ensure are always built
 products = [
