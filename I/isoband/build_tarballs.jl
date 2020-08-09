333934c1e1a9153acd68755ab1c58e236cfec513
jkrumbiegel <22495855+jkrumbiegel@users.noreply.github.com>
2020-08-09 12:22:35 +0200

    New Recipe: isoband v0.2.2 (#1430)
    * New Recipe: isoband v0.2.2
    
    * mkdir -p
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * libisoband.${dlext}
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Update I/isoband/build_tarballs.jl
    
    expand_cxxstring_abis
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * libisoband
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ I/isoband/build_tarballs.jl
@@ -0,0 +1,34 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "isoband"
+version = v"0.2.2"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/jkrumbiegel/isoband.git", "8c0050541c02d0cc5b3dd737c78cbf81ee61f802")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+mkdir -p ${libdir}
+$CXX -shared -std=c++11 -O3 -fPIC -o ${libdir}/libisoband.${dlext} isoband/src/isoband.cpp
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_cxxstring_abis(supported_platforms())
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libisoband", :libisoband)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
