ada6ed93a83435707dbd0e0c662924fc78eddbfe
Elliot Saba <staticfloat@gmail.com>
2020-08-14 10:48:20 -0700

    [adwaita_icon_theme]: Convert to AnyPlatform (#1476)

--- A/adwaita_icon_theme/build_tarballs.jl
+++ A/adwaita_icon_theme/build_tarballs.jl
@@ -7,8 +7,8 @@ version = v"3.33.92"
 
 # Collection of sources required to build adwaita-icon-theme
 sources = [
-    "https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/archive/$(version)/adwaita-icon-theme-$(version).tar.bz2" =>
-    "9e2078bf9e4d28f2a921fa88159733fe83a1fd37f8cbd768a5de3b83f44f0973"
+    ArchiveSource("https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/archive/$(version)/adwaita-icon-theme-$(version).tar.bz2",
+                  "9e2078bf9e4d28f2a921fa88159733fe83a1fd37f8cbd768a5de3b83f44f0973"),
 ]
 
 # Bash recipe for building across all platforms
@@ -25,7 +25,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = [AnyPlatform()]
 
 # The products that we will ensure are always built
 products = Product[
