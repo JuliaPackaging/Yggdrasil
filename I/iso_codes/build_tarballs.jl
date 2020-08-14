3634138e7e5560d77f0ad87eafb725a7b3d018d5
Elliot Saba <staticfloat@gmail.com>
2020-08-14 03:02:08 -0700

    Switch iso_codes to AnyPlatform

--- I/iso_codes/build_tarballs.jl
+++ I/iso_codes/build_tarballs.jl
@@ -7,8 +7,8 @@ version = v"4.3"
 
 # Collection of sources required to build iso-codes
 sources = [
-    "https://salsa.debian.org/iso-codes-team/iso-codes/-/archive/iso-codes-$(version.major).$(version.minor)/iso-codes-iso-codes-$(version.major).$(version.minor).tar.bz2" =>
-    "6b539f915d02c957c45fce8133670811f1c36a1f1535d5af3dd95dc519d3c386"
+    ArchiveSource("https://salsa.debian.org/iso-codes-team/iso-codes/-/archive/iso-codes-$(version.major).$(version.minor)/iso-codes-iso-codes-$(version.major).$(version.minor).tar.bz2",
+                  "6b539f915d02c957c45fce8133670811f1c36a1f1535d5af3dd95dc519d3c386"),
 ]
 
 # Bash recipe for building across all platforms
@@ -22,7 +22,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = [AnyPlatform()]
 
 # The products that we will ensure are always built
 products = Product[
