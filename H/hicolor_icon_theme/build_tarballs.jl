b403dcf54a9a0386b489981ccb827bde9bf4a3b5
Elliot Saba <staticfloat@gmail.com>
2020-08-14 10:41:04 -0700

    [hicolor_icon_theme]: Convert to AnyPlatform

--- H/hicolor_icon_theme/build_tarballs.jl
+++ H/hicolor_icon_theme/build_tarballs.jl
@@ -7,8 +7,8 @@ version = v"0.17"
 
 # Collection of sources required to build hicolor_icon_theme
 sources = [
-    "https://icon-theme.freedesktop.org/releases/hicolor-icon-theme-$(version.major).$(version.minor).tar.xz" =>
-    "317484352271d18cbbcfac3868eab798d67fff1b8402e740baa6ff41d588a9d8"
+    ArchiveSource("https://icon-theme.freedesktop.org/releases/hicolor-icon-theme-$(version.major).$(version.minor).tar.xz",
+                  "317484352271d18cbbcfac3868eab798d67fff1b8402e740baa6ff41d588a9d8"),
 ]
 
 # Bash recipe for building across all platforms
@@ -20,7 +20,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = [AnyPlatform()]
 
 # The products that we will ensure are always built
 products = [
