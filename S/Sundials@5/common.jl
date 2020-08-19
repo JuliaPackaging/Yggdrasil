0cadda907e106b66807e53e01d9526000cecf251
Chris de Graaf <me@cdg.dev>
2020-08-19 15:05:16 -0400

    Split Sundials and Sundials32

--- S/Sundials@5/common.jl
+++ S/Sundials@5/common.jl
@@ -1,53 +1,13 @@
 using BinaryBuilder
 
-name = "Sundials"
 version = v"5.2.0"
 
 # Collection of sources required to build Sundials
 sources = [
     GitSource("https://github.com/LLNL/sundials.git",
               "b16d3d3995668c9a13c9f4bee8b0113ff6a9cf6d"),
-    DirectorySource("./bundled"),
+    DirectorySource("../bundled"),
 ]
-
-# Bash recipe for building across all platforms
-script = raw"""
-cd $WORKSPACE/srcdir/sundials*
-
-# Set up CFLAGS
-if [[ "${target}" == *-mingw* ]]; then
-    atomic_patch -p1 $WORKSPACE/srcdir/patches/Sundials_windows.patch
-    # Work around https://github.com/LLNL/sundials/issues/29
-    export CFLAGS="${CFLAGS} -DBUILD_SUNDIALS_LIBRARY"
-elif [[ "${target}" == powerpc64le-* ]]; then
-    export CFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib64"
-fi
-
-# Set up LAPACK
-LAPACK_LIBRARIES="-lgfortran ${libdir}/libopenblas.${dlext}"
-if [[ "${target}" == i686-* ]] || [[ "${target}" == x86_64-* ]]; then
-    LAPACK_LIBRARIES="${LAPACK_LIBRARIES} -lquadmath"
-elif [[ "${target}" == powerpc64le-* ]]; then
-    LAPACK_LIBRARIES="${LAPACK_LIBRARIES} -lgomp -ldl -lm -lpthread"
-fi
-
-# Build
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" \
-    -DEXAMPLES_ENABLE_C=OFF \
-    -DKLU_ENABLE=ON -DKLU_INCLUDE_DIR="$prefix/include" -DKLU_LIBRARY_DIR="$libdir" \
-    -DLAPACK_ENABLE=ON -DLAPACK_LIBRARIES:STRING="${LAPACK_LIBRARIES}" \
-    ..
-make -j${nproc}
-make install
-
-# Move libraries to ${libdir} on Windows
-if [[ "${target}" == *-mingw* ]]; then
-    mv ${prefix}/lib/libsundials_*.${dlext} "${libdir}"
-fi
-"""
 
 # We attempt to build for all defined platforms
 platforms = supported_platforms()
@@ -80,11 +40,5 @@ products = [
     LibraryProduct("libsundials_sunnonlinsolnewton", :libsundials_sunnonlinsolnewton),
 ]
 
-dependencies = [
-    Dependency("CompilerSupportLibraries_jll"),
-    Dependency("OpenBLAS32_jll"),
-    BuildDependency("SuiteSparse32_jll"),
-]
-
 # Build the tarballs.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"6")
+build() = build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"6")
