5c1319fe822cb3bdf76422b3d4d6f933cd1f9f89
Tim Besard <tim.besard@gmail.com>
2020-08-20 10:15:25 +0200

    Update libigc to 1.0.4560.

--- L/libigc/build_tarballs.jl
+++ L/libigc/build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder
 
 name = "libigc"
-version = v"1.0.4241"
+version = v"1.0.4560"
 
 # IGC depends on LLVM, a custom Clang, and a Khronos tool. Instead of building these pieces
 # separately, taking care to match versions and apply Intel-specific patches where needed
@@ -12,13 +12,14 @@ version = v"1.0.4241"
 
 # Collection of sources required to build IGC
 sources = [
-    GitSource("https://github.com/intel/intel-graphics-compiler.git", "9a456d81355b266ac60b26c1865935b4a266d6e2"),
+    GitSource("https://github.com/intel/intel-graphics-compiler.git", "c622f2ffab847a00616a593e92057f4ed31a5c83"),
     # use LLVM 10 as provided by the official packages for Ubuntu 18.04
     GitSource("https://github.com/llvm/llvm-project.git", "d32170dbd5b0d54436537b6b75beaf44324e0c28"), # v10.0.0
-    GitSource("https://github.com/intel/opencl-clang.git", "92f3f7f1a06f25fb13708f87c26b0fbf50924c96"), # v10.0.0-2
-    GitSource("https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git", "4d43f68a30a510b4e7607351caab3df8e7426a6b"),
+    GitSource("https://github.com/intel/opencl-clang.git", "6a9cd2c7dc37f168dae327564a98cab7c4382a2c"),
+    GitSource("https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git", "424e375edc4b915218ab5d1f08670a8d1e92c9d3"),
+    GitSource("https://github.com/intel/vc-intrinsics.git", "d7c5f99bd910a681b907815ebd44ef319ff417c4"),
     # patches
-    GitSource("https://github.com/intel/llvm-patches.git", "0e35a4a02de23c235f28505f52c1222731667d17"),
+    GitSource("https://github.com/intel/llvm-patches.git", "c4a03454d55ed786138128bfd73fba491b31ca7c"),
     DirectorySource("./bundled"),
 ]
 
@@ -53,22 +54,24 @@ mv opencl-clang llvm-project/llvm/projects/opencl-clang
 mv SPIRV-LLVM-Translator llvm-project/llvm/projects/llvm-spirv
 mv llvm-patches llvm_patches
 
-cd intel-graphics-compiler
-install_license LICENSE.md
-
 # Work around compilation failures
+atomic_patch -p0 patches/cmake.patch
 # https://gcc.gnu.org/bugzilla/show_bug.cgi?id=86678
-atomic_patch -p1 ../patches/gcc-constexpr_assert_bug.patch
+atomic_patch -p0 patches/gcc-constexpr_assert_bug.patch
 if [[ "${target}" == *86*-linux-musl* ]]; then
-    atomic_patch -p1 ../patches/musl-concat.patch
-    atomic_patch -p1 ../patches/musl-inttypes.patch
+    atomic_patch -p0 patches/musl-concat.patch
+    atomic_patch -p0 patches/musl-inttypes.patch
+    atomic_patch -p0 patches/musl-deepbind.patch
     # https://github.com/JuliaPackaging/Yggdrasil/issues/739
-    sed -i '/-fstack-protector/d' IGC/CMakeLists.txt
+    sed -i '/-fstack-protector/d' intel-graphics-compiler/IGC/CMakeLists.txt
     # https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/387
     pushd /opt/${target}/lib/gcc/${target}/*/include
     atomic_patch -p0 $WORKSPACE/srcdir/patches/musl-malloc.patch
     popd
 fi
+
+cd intel-graphics-compiler
+install_license LICENSE.md
 
 CMAKE_FLAGS=()
 
