714adc12721da7f69e165584059ef9a7bbc32410
Tim Besard <tim.besard@gmail.com>
2020-08-20 16:58:29 +0200

    Split the oneAPI Level Zero package, pt. 1

---
+++ O/oneAPI_Level_Zero/common.jl
@@ -0,0 +1,8 @@
+
+version = v"1.0"
+
+# Collection of sources required to build this package
+sources = [
+    GitSource("https://github.com/oneapi-src/level-zero.git",
+              "fcc7b7aceacf3cbfabaf3c0952ae0cc02d083592"),
+]
