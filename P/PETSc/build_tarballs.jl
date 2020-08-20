d16f945d78e7d1e11273a9d16f04a7a5ed4efbde
Jose Daniel Lara <jdlara@berkeley.edu>
2020-08-20 11:20:40 -0700

    [PETSc] Add builder (#845)
    * add build file
    
    * add patch
    
    * modify application of patch
    
    * improve build
    
    * expand gfortran versions
    
    * address PR coments
    
    * use target instead of arch
    
    * fix petsc dir
    
    * add make install call
    
    * add sowing configuration
    
    * change source type
    
    * add hash
    
    * add comment about sowing
    
    * Update P/PETSc/build_tarballs.jl
    
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>
    
    * [PETSc] Fix quotes
    
    * [PETSc] Fix lapack suffix
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * update link and native options
    
    * run make with one core
    
    * try again for windows
    
    * fix typo
    
    * fix bad comparison
    
    * try to get the logs
    
    * Update P/PETSc/build_tarballs.jl
    
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>
    
    * remove no-shared flags in windows
    
    * [PETSc] Fix detection of `direct.h` header file
    
    On case-sensitive filesystems the capitalisation of the file is important and
    the header file is correctly called `direct.h`, with lowercase `d`, see
    https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/mkdir-wmkdir?view=vs-2019
    
    * update tarballs
    
    * Update P/PETSc/build_tarballs.jl
    
    Co-Authored-By: Mosè Giordano <giordano@users.noreply.github.com>
    
    * [PETSc] Set CPPFLAGS for Windows, fix more header cases
    
    * [PETSc] Move libraries for Windows to the correct place
    
    * [PETSc] Force use of `-fPIC` for PowerPC
    
    * try using openmpi flags
    
    * change specification of openmpi dirs
    
    * change MPI versions
    
    * update build
    
    * update build
    
    * remove mpiexec
    
    * remove improper file reference
    
    * using other compilers didnt work
    
    * link libmpifort
    
    * [PETSc] Configure Windows to use MicrosoftMPI
    
    * [PETSc] Remove workaround not needed anymore
    
    * update petsc version
    
    * comment the header patches
    
    * exclude win 32 bits
    
    * naive try
    
    * switch to openblas 32
    
    * add flag for MPI indexing
    
    * remove unused patch
    
    * add missing space an SuperLU place holder
    
    * fix blas suffix
    
    * try addint superlu MT
    
    * use PETSc's superlu. SuperLU MT not compatible
    
    * remove superlu
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    Co-authored-by: Mosè Giordano <mose@gnu.org>

---
+++ P/PETSc/build_tarballs.jl
@@ -0,0 +1,90 @@
+using BinaryBuilder
+
+name = "PETSc"
+version = v"3.13.4"
+
+# Collection of sources required to build PETSc. Avoid using the git repository, it will
+# require building SOWING which fails in all non-linux platforms.
+sources = [
+    ArchiveSource("https://www.mcs.anl.gov/petsc/mirror/release-snapshots/petsc-3.13.4.tar.gz",
+    "2cf8abd70ec332510b17bff7dc8a465bf1eb053e17e2a0451a4a0256385657d8"),
+    DirectorySource("./bundled"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/petsc*
+includedir="${prefix}/include"
+atomic_patch -p1 $WORKSPACE/srcdir/patches/petsc_name_mangle.patch
+
+BLAS_LAPACK_LIB="${libdir}/libopenblas.${dlext}"
+
+if [[ "${target}" == *-mingw* ]]; then
+    #atomic_patch -p1 $WORKSPACE/srcdir/patches/fix-header-cases.patch
+    MPI_LIBS="${libdir}/msmpi.${dlext}"
+else
+    MPI_LIBS="[${libdir}/libmpi.${dlext},libmpifort.${dlext}]"
+fi
+
+./configure --prefix=${prefix} \
+    CC=${CC} \
+    FC=${FC} \
+    CXX=${CXX} \
+    COPTFLAGS='-O3' \
+    CXXOPTFLAGS='-O3' \
+    FOPTFLAGS='-O3' \
+    --with-64-bit-indices=0 \
+    --with-debugging=0 \
+    --with-batch \
+    --PETSC_ARCH=$target \
+    --with-blaslapack-lib=$BLAS_LAPACK_LIB \
+    --with-blaslapack-suffix="" \
+    --known-64-bit-blas-indices=0 \
+    --with-mpi-lib="${MPI_LIBS}" \
+    --known-mpi-int64_t=0 \
+    --with-mpi-include="${includedir}" \
+    --with-sowing=0
+
+if [[ "${target}" == *-mingw* ]]; then
+    export CPPFLAGS="-Dpetsc_EXPORTS"
+elif [[ "${target}" == powerpc64le-* ]]; then
+    export CFLAGS="-fPIC"
+    export FFLAGS="-fPIC"
+fi
+
+make -j${nproc} \
+    PETSC_DIR="${PWD}" \
+    PETSC_ARCH="${target}" \
+    CPPFLAGS="${CPPFLAGS}" \
+    CFLAGS="${CFLAGS}" \
+    FFLAGS="${FFLAGS}" \
+    DEST_DIR="${prefix}" \
+    all
+
+make PETSC_DIR=$PWD PETSC_ARCH=$target DEST_DIR=$prefix install
+
+if [[ "${target}" == *-mingw* ]]; then
+    # Move library to ${libdir} on Windows,
+    # changing the extension from so to dll.
+    mv ${prefix}/lib/libpetsc.so.*.*.* "${libdir}/libpetsc.${dlext}"
+    # Remove useless links
+    rm ${prefix}/lib/libpetsc.*
+fi
+"""
+
+# We attempt to build for all defined platforms
+platforms = expand_gfortran_versions(supported_platforms(exclude=[Windows(:i686)]))
+
+products = [
+    LibraryProduct("libpetsc", :libpetsc),
+]
+
+dependencies = [
+    Dependency("OpenBLAS32_jll"),
+    Dependency("MPICH_jll"),
+    Dependency("MicrosoftMPI_jll"),
+    Dependency("CompilerSupportLibraries_jll"),
+]
+
+# Build the tarballs.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"6")
