80edcc955b2f3f08abb8d3de57aaa46f30263be3
Helge Eichhorn <git@helgeeichhorn.de>
2020-08-26 11:23:08 +0200

    Try building all platforms

--- G/GMAT/build_tarballs.jl
+++ G/GMAT/build_tarballs.jl
@@ -39,22 +39,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
-]
-platforms = expand_cxxstring_abis(platforms)
-
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
