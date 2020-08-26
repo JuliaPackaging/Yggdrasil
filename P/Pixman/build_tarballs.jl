ee8181dd81c4056d2eca4c6283f62326b33694cc
Páll Haraldsson <PHaraldsson@symmetryinvestments.com>
2020-08-26 15:20:24 +0000

    Bump JLL wrapper: Pixman_jll
    [skip build]

--- P/Pixman/build_tarballs.jl
+++ P/Pixman/build_tarballs.jl
@@ -32,3 +32,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6")
+
