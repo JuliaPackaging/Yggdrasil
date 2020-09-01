3539d4299cd448b013c5159d2bad9e834e9d92b2
Mosè Giordano <mose@gnu.org>
2020-09-01 19:57:04 +0200

    [AZP] Update BBB to master

--- H/HelloWorldCxx/build_tarballs.jl
+++ H/HelloWorldCxx/build_tarballs.jl
@@ -1,7 +1,7 @@
 using BinaryBuilder
 
 name = "HelloWorldCxx"
-version = v"1.0.2"
+version = v"1.0.3"
 
 # No sources, we're just building the testsuite
 sources = [
@@ -28,4 +28,4 @@ dependencies = [
 ]
 
 # Build the tarballs
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6")
