43b62c11191d2bb6c11b404d75c978dfb1d78bd1
Elliot Saba <staticfloat@gmail.com>
2020-09-01 17:27:30 -0700

    Update build_tarballs.jl

--- O/OpenLibm/build_tarballs.jl
+++ O/OpenLibm/build_tarballs.jl
@@ -46,4 +46,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; lock_microarchitecture=false)
