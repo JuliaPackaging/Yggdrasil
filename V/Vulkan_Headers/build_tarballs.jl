f1b0cc56376f3cd90a59cb02c7ca5d310845545a
Cédric Belmant <belmant@etud.insa-toulouse.fr>
2020-09-02 14:19:50 +0200

    New recipe for Vulkan_Headers (#1634)
    * New recipe for Vulkan_Headers
    
    * Remove postfix unix (refactor)
    
    Co-authored-by: belmant <cedric.bel@hotmail.fr>

---
+++ V/Vulkan_Headers/build_tarballs.jl
@@ -0,0 +1,42 @@
+using BinaryBuilder
+
+name = "Vulkan_Headers"
+version = v"1.2.151"
+
+source = "https://github.com/KhronosGroup/Vulkan-Headers.git"
+commit = "99638d8d7fc64ae9c3fc6a396ec034abd40e675d" # August 19th, 2020
+
+sources = [
+    GitSource(source, commit)
+]
+
+script = raw"""
+cd Vulkan-Headers
+install_license LICENSE.txt
+
+CMAKE_FLAGS=()
+
+# Install things into $prefix
+CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix})
+
+# Install things into $prefix
+CMAKE_FLAGS+=(-DCMAKE_INSTALL_LIBDIR=${libdir})
+
+cmake -B build -S . -GNinja ${CMAKE_FLAGS[@]}
+ninja -C build -j ${nproc} install
+"""
+
+# platforms = supported_platforms()
+platforms = [AnyPlatform()]
+
+
+# The products that we will ensure are always built
+products = Product[
+    FileProduct("share/vulkan/registry/vk.xml", :vk_xml)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
