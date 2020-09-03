c7bcdad11725af62eb17ed2a0a4fb3db1ba3411c
Mikael Öhman <micketeer@gmail.com>
2020-09-04 00:11:18 +0200

    Add HSA ROC runtime (#1500)
    * Add HSA ROC runtime
    
    * Add missing expand_cxxstring_abis to ROCR runtime
    
    * Update to 3.7.0, but, new dependency is added which needs to be dealt with first.
    
    * Disable image library in hsa_rocr until deps are in place.
    
    * Fix silly typo in build script

---
+++ H/hsa_rocr/build_tarballs.jl
@@ -0,0 +1,52 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "hsa_rocr"
+version = v"3.7.0"
+
+# Collection of sources required to build
+sources = [
+    ArchiveSource("https://github.com/RadeonOpenCompute/ROCR-Runtime/archive/rocm-$(version).tar.gz",
+                  "0071d14431f73ce74574e61d0786f2b7cf34b14ea898a1f54b6e1b06b2d468c0")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ${WORKSPACE}/srcdir/ROCR-Runtime*/
+
+mkdir build && cd build
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_INSTALL_PREFIX=${prefix} \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_SKIP_RPATH=ON \
+      -DIMAGE_SUPPORT=OFF \
+      ../src
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+# Only supports Linux, seemingly only 64bit
+platforms = [
+    Linux(:x86_64, libc=:glibc),
+]
+platforms = expand_cxxstring_abis(platforms)
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct(["libhsa-runtime64"], :libhsa_runtime64),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("hsakmt_roct_jll"),
+    Dependency("NUMA_jll"),
+    Dependency("Zlib_jll"),
+    Dependency("Elfutils_jll"),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies,
+               preferred_gcc_version=v"8") 
