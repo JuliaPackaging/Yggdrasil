904ee2496287c046eeffcaf143ff5e47ed1f8843
kalmarek <kalmar@amu.edu.pl>
2020-09-03 18:42:42 +0200

    SCS_GPU: build libscsgpuindir against CUDA_full_jll (#1294)
    * build libscsgpuindir against CUDA_full_jll
    
    * use `libdir` instead of `${prefix}/lib`
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * change name to SCS_GPU
    
    * only Linux CUDA is supported
    
    * fix CUDA_full_jll to v9.0
    
    * update SCS to 2.1.2
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ S/SCS_GPU/build_tarballs.jl
@@ -0,0 +1,46 @@
+using Pkg
+using BinaryBuilder
+
+name = "SCS_GPU"
+version = v"2.1.2"
+
+# Collection of sources required to build SCSBuilder
+sources = [
+    GitSource("https://github.com/cvxgrp/scs.git", "4ed6c2abf28399c01a0417ff3456b2639560afa6")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/scs*
+flags="DLONG=0 USE_OPENMP=0 BLAS64=1 BLASSUFFIX=_64_"
+blasldflags="-L${libdir} -lopenblas64_"
+
+CUDA_PATH=$prefix/cuda make BLASLDFLAGS="${blasldflags}" ${flags} out/libscsgpuindir.${dlext}
+
+mkdir -p ${libdir}
+cp out/libscs*.${dlext} ${libdir}
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+
+platforms = [
+    Linux(:x86_64),
+]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libscsgpuindir", :libscsgpuindir, dont_dlopen=true)
+]
+
+# Dependencies that must be installed before this package can be built
+
+cuda_version = v"9.0.176"
+
+dependencies = [
+    Dependency("OpenBLAS_jll"),
+    BuildDependency(PackageSpec(name="CUDA_full_jll", version=cuda_version))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
