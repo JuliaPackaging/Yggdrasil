c2d223936b522671e39847104fb266c644e4f1d9
Matthew Koski <maccam912@gmail.com>
2020-09-04 19:35:07 -0500

    Add commas in to list

--- A/Arrow/build_tarballs.jl
+++ A/Arrow/build_tarballs.jl
@@ -29,19 +29,19 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
- Linux(:i686, libc=:glibc)
- Linux(:x86_64, libc=:glibc)
- #Linux(:aarch64, libc=:glibc)
- #Linux(:armv7l, libc=:glibc, call_abi=:eabihf)
- Linux(:powerpc64le, libc=:glibc)
- Linux(:i686, libc=:musl)
- Linux(:x86_64, libc=:musl)
- #Linux(:aarch64, libc=:musl)
- #Linux(:armv7l, libc=:musl, call_abi=:eabihf)
- MacOS(:x86_64)
- FreeBSD(:x86_64)
- #Windows(:i686)
- #Windows(:x86_64)
+ Linux(:i686, libc=:glibc),
+ Linux(:x86_64, libc=:glibc),
+ #Linux(:aarch64, libc=:glibc),
+ #Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+ Linux(:powerpc64le, libc=:glibc),
+ Linux(:i686, libc=:musl),
+ Linux(:x86_64, libc=:musl),
+ #Linux(:aarch64, libc=:musl),
+ #Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+ MacOS(:x86_64),
+ FreeBSD(:x86_64),
+ #Windows(:i686),
+ #Windows(:x86_64),
 ]
 
 platforms = expand_cxxstring_abis(platforms)
