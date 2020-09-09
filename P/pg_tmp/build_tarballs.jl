bcf66f79b7a0531448404ce2e04130e5d68202aa
morris25 <sam.morrison@invenia.ca>
2020-09-09 15:48:09 -0500

    New Recipe: pg_tmp v3.0.0

---
+++ P/pg_tmp/build_tarballs.jl
@@ -0,0 +1,50 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "pg_tmp"
+version = v"3.0.0"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("http://eradman.com/ephemeralpg/code/ephemeralpg-3.0.tar.gz", "70ef314e31c5547f353ea7b2787faafa07adc32dcfaea6f4f1475512c23b0fc8")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+export PREFIX=$prefix
+cd ephemeralpg-3.0
+make
+make install
+exit
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl),
+    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64)
+]
+
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("getsocket", :getsocket)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
