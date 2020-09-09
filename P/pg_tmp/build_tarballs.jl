fbd5d5fc95cca46176fe03f36998a3f6271ba2a8
morris25 <sam.morrison@invenia.ca>
2020-09-09 16:11:39 -0500

    Add missing executables

--- P/pg_tmp/build_tarballs.jl
+++ P/pg_tmp/build_tarballs.jl
@@ -39,7 +39,9 @@ platforms = [
 
 # The products that we will ensure are always built
 products = [
-    ExecutableProduct("getsocket", :getsocket)
+    ExecutableProduct("pg_tmp", :pg_tmp),
+    ExecutableProduct("getsocket", :getsocket),
+    ExecutableProduct("ddl_compare", :ddl_compare),
 ]
 
 # Dependencies that must be installed before this package can be built
