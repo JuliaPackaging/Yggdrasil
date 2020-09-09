b6b3011a114f3077df83c590b4da082a1758e661
Sam Kaplan <Sam.Kaplan@chevron.com>
2020-09-09 15:29:19 +0000

    copy-paste error

--- A/AzStorage/build_tarballs.jl
+++ A/AzStorage/build_tarballs.jl
@@ -32,3 +32,22 @@ else
     cp libAzStorage.so ${libdir}/libAzStorage.so
 fi
 """
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+# TODO - add libgomp dependency
+products = [
+    LibraryProduct("libAzStorage", :libAzStorage)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("CompilerSupportLibraries_jll"),
+    Dependency("LibCURL_jll")
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
