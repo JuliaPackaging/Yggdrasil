5e6994e3616da40d91914f9562515777e60822c0
Scott Mishra <scottmishra@chevron.com>
2020-09-14 10:50:52 -0500

    adding using Pkg to the top of the tar build file

--- C/CvxCompress/build_tarballs.jl
+++ C/CvxCompress/build_tarballs.jl
@@ -1,6 +1,6 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "CvxCompress"
 version = v"1.0.0"
