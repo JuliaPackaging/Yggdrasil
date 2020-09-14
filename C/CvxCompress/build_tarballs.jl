e853d1bb407726fa7c67889cdb2e7e89ed4eaddd
Mosè Giordano <mose@gnu.org>
2020-09-14 23:47:30 +0100

    [CvxCompress] Build for all x86-64 Unices

--- C/CvxCompress/build_tarballs.jl
+++ C/CvxCompress/build_tarballs.jl
@@ -10,13 +10,21 @@ sources = [
     GitSource(
         "https://github.com/ChevronETC/CvxCompress.git",
         "55e072862196e917e3ecece81f014e93d60cdf81"
-    )
+    ),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/CvxCompress
-make -j${nproc}
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/cross-compile-cvxcompress-gencode.patch
+${CXX_FOR_BUILD} -O2 -o CvxCompress_GenCode Wavelet_Transform_Slow.cpp CvxCompress_GenCode.cpp
+./CvxCompress_GenCode
+FLAGS=()
+if [[ "${target}" == *-apple-* ]]; then
+    FLAGS=(LDFLAGS="-fopenmp -lm")
+fi
+make -j${nproc} "${FLAGS[@]}"
 mkdir -p ${libdir}
 cp libcvxcompress.so ${libdir}/libcvxcompress.${dlext}
 """
@@ -24,10 +32,7 @@ cp libcvxcompress.so ${libdir}/libcvxcompress.${dlext}
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 # TODO update make to adjust the intrinsic code generation to support more platforms
-platforms = [
-  Linux(:x86_64, libc=:glibc),
-  Linux(:x86_64, libc=:musl)
-]
+platforms = [p for p in supported_platforms() if arch(p) === :x86_64 && !Sys.iswindows(p)]
 
 # The products that we will ensure are always built
 products = [
