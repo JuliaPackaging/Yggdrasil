9b334ef00d0ef975061f862acecb7fa3d860f65f
RuQing Xu <r-xu@gcc.u-tokyo.ac.jp>
2020-09-23 07:37:06 +0900

    New Recipe: blis v0.7.0+0

---
+++ B/blis/build_tarballs.jl
@@ -0,0 +1,91 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "blis"
+version = v"0.7.0+0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/flame/blis.git", "68b88aca6692c75a9f686187e6c4a4e196ae60a9")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd blis/
+
+for i in ./config/*/*.mk; do
+
+    # Building in container forbids -march options
+    sed -i "s/-march[^ ]*//g" $i
+    # Building in container forbids unsafe optimization.
+    sed -i "s/-ffast-math//g" $i
+    sed -i "s/-funsafe-math-optimizations//g" $i
+
+done
+
+case ${target} in
+
+    *"x86_64"*"linux"*) 
+        export BLI_CONFIG=x86_64
+        export BLI_THREAD=openmp
+        ;;
+    *"x86_64"*"w64"*) 
+        # MinGW doesn't support savexmm instructions
+        # Build only for AMD processors.
+        export BLI_CONFIG=amd64
+        export BLI_THREAD=openmp
+        ;;
+    *"x86_64"*"apple"*) 
+        export BLI_CONFIG=x86_64
+        export BLI_THREAD=none
+        ;;
+    *"x86_64"*"freebsd"*) 
+        export BLI_CONFIG=x86_64
+        export BLI_THREAD=none
+        ;;
+    *"aarch64"*"linux"*) 
+        # Aarch64 has no metaconfiguration support yet.
+        # Use Cortex-A57 for the moment.
+        export BLI_CONFIG=cortexa57
+        export BLI_THREAD=openmp
+        ;;
+    *"arm"*"linux"*) 
+        export BLI_CONFIG=cortexa9
+        export BLI_THREAD=none
+        ;;
+    *)
+        ;; 
+
+esac
+
+./configure -p ${prefix} -t ${BLI_THREAD} ${BLI_CONFIG}
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:x86_64, libc=:musl),
+    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    Windows(:x86_64),
+    MacOS(:x86_64),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    FreeBSD(:x86_64)
+]
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libblis", :blis)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8.1.0")
