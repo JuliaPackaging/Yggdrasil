1fcf4467c768d64577f39dc347d8df86026b2802
Valentin Churavy <v.churavy@gmail.com>
2020-09-23 14:23:02 -0400

    [LLVM] prep LLVM 11

---
+++ L/LLVM/LLVM_full@11.0.0/build_tarballs.jl
@@ -0,0 +1,6 @@
+version = v"11.0.0-rc3"
+
+include("../common.jl")
+
+build_tarballs(ARGS, configure_build(ARGS, version)...;
+                     preferred_gcc_version=v"7", preferred_llvm_version=v"8")
