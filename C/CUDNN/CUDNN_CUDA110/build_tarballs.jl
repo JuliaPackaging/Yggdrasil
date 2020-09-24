9c00d994c4dcbb1cd236fafd10910ce9a9699d8b
Tim Besard <tim.besard@gmail.com>
2020-09-24 15:28:13 +0200

    Update CUDNN to 8.0.3.

--- C/CUDNN/CUDNN_CUDA110/build_tarballs.jl
+++ C/CUDNN/CUDNN_CUDA110/build_tarballs.jl
@@ -1,18 +1,18 @@
 using BinaryBuilder
 
-cuda_version = v"11.0.2"   # NOTE: could be less specific
+cuda_version = v"11.0.3"   # NOTE: could be less specific
 
 sources_linux_x64 = [
-    ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.0.2.39/11.0_20200626/cudnn-11.0-linux-x64-v8.0.2.39.tgz",
-                  "672f46288b8edd98f8d156a4f1ff518201ca6de0cff67915ceaa37f6d6d86345")
+    ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.0.3.33/11.0_20200825/cudnn-11.0-linux-x64-v8.0.3.33.tgz",
+                  "8924bcc4f833734bdd0009050d110ad0c8419d3796010cf7bc515df654f6065a")
 ]
 sources_linux_ppc64le = [
-    ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.0.2.39/11.0_20200626/cudnn-11.0-linux-ppc64le-v8.0.2.39.tgz",
-                  "b7c1ce5b1191eb007ba3455ea5f497fdce293a646545d8a6ed93e9bb06d7f057")
+    ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.0.3.33/11.0_20200825/cudnn-11.0-linux-ppc64le-v8.0.3.33.tgz",
+                  "c2d0519831137b43d0eebe07522edb4ef5d62320e65e5d5fa840a9856f25923d")
 ]
 sources_windows = [
-    ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.0.2.39/11.0_20200626/cudnn-11.0-windows-x64-v8.0.2.39.zip",
-                  "3e340edba0b9ce7ad35d8e77cbaee868bba3e4394a3435bd4fa27c81f261ca4c")
+    ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.0.3.33/11.0_20200825/cudnn-11.0-windows-x64-v8.0.3.33.zip",
+                  "67f64109a677fb43ddbf730b1fbf8f9ba9bf0214d3a9e1027ac546255d4ae3c6")
 ]
 
 include("../common.jl")
