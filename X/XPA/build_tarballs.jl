1ecb3b15d8e8d7603073cfa64d026ff35852b344
Miles Lucas <mdlucas@hawaii.edu>
2020-09-24 09:40:32 -0500

    fix typo

--- X/XPA/build_tarballs.jl
+++ X/XPA/build_tarballs.jl
@@ -17,7 +17,7 @@ cd $WORKSPACE/srcdir/xpa
 make -j$(nproc)
 if [[ ${target} == *mingw* ]]; then
     make mingw-dll
-    cp libxpa.dll ${libdir}/bin
+    cp libxpa.dll ${libdir}
 fi
 make install
 """
