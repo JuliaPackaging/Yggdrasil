105f268fdf1805cf72547112726cec9c02194240
Tim Besard <tim.besard@gmail.com>
2020-09-24 12:12:02 +0200

    Add CUDA 11.1 (full).

---
+++ C/CUDA/CUDA_full@11.1/build_tarballs.jl
@@ -0,0 +1,114 @@
+using BinaryBuilder
+
+include("../../../fancy_toys.jl")
+
+name = "CUDA_full"
+version = v"11.1.0"
+
+sources_linux = [
+    FileSource("https://developer.download.nvidia.com/compute/cuda/11.1.0/local_installers/cuda_11.1.0_455.23.05_linux.run",
+               "858cbab091fde94556a249b9580fadff55a46eafbcb4d4a741d2dcd358ab94a5", "installer.run")
+]
+sources_linux_ppc64le = [
+    FileSource("https://developer.download.nvidia.com/compute/cuda/11.1.0/local_installers/cuda_11.1.0_455.23.05_linux_ppc64le.run",
+               "a561e6f7f659bc4100e4713523b0b8aad6b36aa77fac847f6423e7780c750064", "installer.run")
+]
+sources_win10 = [
+    FileSource("https://developer.download.nvidia.com/compute/cuda/11.1.0/local_installers/cuda_11.1.0_456.43_win10.exe",
+               "759b6cc5d24da6a4a49fc708d7d37b164918433ac59248ae31c7e95bc8a2e1d5", "installer.exe")
+]
+
+script = raw"""
+cd ${WORKSPACE}/srcdir
+
+# use a temporary directory to avoid running out of tmpfs
+temp=${WORKSPACE}/tmpdir
+mkdir ${temp}
+
+mkdir ${prefix}/cuda
+if [[ ${target} == *-linux-gnu ]]; then
+    # don't run the embedded installer (libc issue, doesn't work on ppc),
+    # but instruct makeself to uncompress the embedded archive.
+    sh installer.run --noexec --target "${temp}"
+    cd ${temp}/builds
+    find .
+
+    cp cuda_documentation/EULA.txt ${prefix}/cuda
+
+    for project in cuda_cudart cuda_cuobjdump cuda_cupti cuda_gdb cuda_memcheck \
+                   cuda_nvcc cuda_nvdisasm cuda_nvml_dev cuda_nvprof cuda_nvprune \
+                   cuda_nvrtc cuda_nvtx cuda_sanitizer_api \
+                   libcublas libcufft libcurand libcusolver libcusparse \
+                   libnpp libnvjpeg; do
+        [[ -d ${project} ]] || { echo "${project} does not exist!"; exit 1; }
+        cp -a ${project}/* ${prefix}/cuda
+    done
+
+    cp -a integration/Sanitizer/* ${prefix}/cuda/bin
+elif [[ ${target} == x86_64-w64-mingw32 ]]; then
+    apk add p7zip
+
+    7z x installer.exe -o${temp}
+    cd ${temp}
+    find .
+
+    mv cuda_documentation/Doc/EULA.txt ${prefix}/cuda
+
+    for project in cuda_cudart cuda_cuobjdump cuda_cupti cuda_memcheck \
+                   cuda_nvcc cuda_nvdisasm cuda_nvml_dev cuda_nvprof cuda_nvprune \
+                   cuda_nvrtc cuda_nvtx cuda_sanitizer_api \
+                   libcublas libcufft libcurand libcusolver libcusparse  \
+                   libnpp libnvjpeg; do
+        [[ -d ${project} ]] || { echo "${project} does not exist!"; exit 1; }
+        cp -a ${project}/*/* ${prefix}/cuda
+    done
+
+    # NVIDIA Tools Extension Library
+    7z x "nsight_nvtx/nsight_nvtx/NVIDIA NVTX Installer.x86_64".*.msi -o${temp}/nvtx_installer
+    find nvtx_installer
+    for file in nvtx_installer/*.*_*; do
+        mv $file $(echo $file | sed 's/\.\(\w*\)_.*/.\1/')
+    done
+    mv nvtx_installer/*.dll ${prefix}/cuda/bin
+    mv nvtx_installer/*64_*.lib ${prefix}/cuda/lib/x64
+    mv nvtx_installer/*32_*.lib ${prefix}/cuda/lib/Win32
+    mv nvtx_installer/*.h ${prefix}/cuda/include
+
+    # fixup
+    chmod +x ${prefix}/cuda/bin/*.exe
+
+    # clean-up
+    rm ${prefix}/cuda/*.nvi
+fi
+
+rm -rf ${temp}
+
+cd ${prefix}/cuda
+install_license EULA.txt
+"""
+
+products = Product[
+    # this JLL isn't meant for use by Julia packages, but only as build dependency
+]
+
+dependencies = []
+
+non_reg_ARGS = filter(arg -> arg != "--register", ARGS)
+
+if should_build_platform("x86_64-linux-gnu")
+    build_tarballs(non_reg_ARGS, name, version, sources_linux, script,
+                   [Linux(:x86_64)], products, dependencies;
+                   skip_audit=true)
+end
+
+if should_build_platform("powerpc64le-linux-gnu")
+    build_tarballs(non_reg_ARGS, name, version, sources_linux_ppc64le, script,
+                   [Linux(:powerpc64le)], products, dependencies;
+                   skip_audit=true)
+end
+
+if should_build_platform("x86_64-w64-mingw32")
+    build_tarballs(ARGS, name, version, sources_win10, script,
+                   [Windows(:x86_64)], products, dependencies;
+                   skip_audit=true)
+end
