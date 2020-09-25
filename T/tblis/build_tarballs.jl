4b500a0f49eb61280da78ec864f41d7572ccacab
RuQing Xu <r-xu@g.ecc.u-tokyo.ac.jp>
2020-09-26 04:40:14 +0900

    Update recipe: tblis-v1.2.0 more Tier-1 platforms (#1691)
    * Update recipe: tblis-v1.2.0 more Tier-1 platforms
    
    Previous script seems not compiling on containers provided by current master.
    Container build does not allow specifying -march options.
    
    This update:
    - Patches scripts before compiling to remove incompatible options;
    - Adds support for macOS and FreeBSD;
    
    Limitations:
    - Support for MUSL Linux is dropped due to seemingly lack of standard library files;
      (<complex> not found)
    - Windows is not supported yet due to the current autotool's not giving
      dynamic libraries under MinGW.
    
    * Update recipe: tblis-v1.2.0 - Edit 1
    
    This is workaround for the 2 limitations in the original commit:
    
    - A workaround for misconfigured(?) x86_64-linux-musl containers;
    - Update Autoconf & library renaming for Windows MinGW;
    - posix_memalign wrapper for Windows MinGW;
    
    * Update recipe: tblis-v1.2.0 - Edit 2
    
    Change MinGW source code patch style.
    Otherwise deployment will be broken.
    
    * Update recipe: tblis-v1.2.0 - Edit 3
    
    Encode Source Patch with Base64.
    
    * Update recipe: tblis-v1.2.0 - Edit 4
    
    Pipeline failure was caused by typo.
    Stupid me.
    
    * Update recipe: tblis-v1.2.0 - Edit 5
    
    Switched compiler version back to default.
    This does not require any additional processing on linux-musl.
    
    * Update recipe: tblis-v1.2.0 - Edit 6
    
    Switched to v5.
    v4.8 does not support C++14.
    
    * Update recipe: tblis-v1.2.0 - Edit 7
    
    Revert to e36b923e4144dd3a2447dd2ffbdcf78492f6a148, because:
    - GCC 5 drops support for macOS (and probably FreeBSD);
    
    Linux-MUSL: Uses Clang/Clang++ for seemingly misconfigured G++ include path.
    - Clang always uses C++11 ABI hence drop C++03 for now;
    - LLVM has no OpenMP hence use pthreads for threading;
    
    * Update recipe: tblis-v1.2.0 - Edit 8
    
    MinGW already has its dll naming correctly resolved.
    No need for renaming.
    
    * Update recipe: tblis-v1.2.0 - Edit 9
    
    - Use DirectorySource to provide MinGW posix_memalign patch as patch
      file instead of base64.
    - Source list now becomes multiple so the 3-Clause BSD LICENSE file
      is copied explicitly.
    
    * [tblis] Use `install_license`
    
    Co-authored-by: RuQing Xu <r-xu@gcc.u-tokyo.ac.jp>
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- T/tblis/build_tarballs.jl
+++ T/tblis/build_tarballs.jl
@@ -8,23 +8,89 @@ version = v"1.2.0"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/devinamatthews/tblis.git", "3e4c4b82943726c443b6f408c9c9791dcad7a847")
+    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd tblis/
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+
+for i in ./Makefile.* ./configure*; do
+
+    # Building in container forbids -march options
+    sed -i "s/-march[^ ]*//g" $i
+
+done
+
+case ${target} in
+    # Unlike stated in Wiki, 
+    # TBLIS automatically detects threading model.
+    *"x86_64"*"linux"*"gnu"*) 
+        export BLI_CONFIG=x86
+        export BLI_THREAD=openmp
+        ;;
+    *"x86_64"*"linux"*"musl"*)
+        export BLI_CONFIG=x86
+        export BLI_THREAD=pthreads
+        export CC=clang
+        export CXX=clang++
+        ;;
+    *"x86_64"*"w64"*)
+        # Windows lacks support for some instructions.
+        # Building only for AMD processors.
+        export BLI_CONFIG=amd
+        export BLI_THREAD=openmp
+        # Wrapper for posix_memalign calls.
+        patch src/memory/aligned_allocator.hpp < ${WORKSPACE}/srcdir/patches/aligned_allocator.hpp.mingw.patch
+        # Additional linking parameter needed for MinGW Autoconf.
+        # Update Autoconf parameters and refresh.
+        cd src/external/tci
+        echo "AM_LDFLAGS=-no-undefined" >> Makefile.am
+        echo "lib_libtci_la_LDFLAGS=-no-undefined" >> Makefile.am
+        update_configure_scripts --reconf
+        cd ../../..
+        sed -i "s/.*AM_LDFLAGS.*/AM_LDFLAGS=-lpthread -no-undefined/" Makefile.am
+        echo "lib_libtblis_la_LDFLAGS=-no-undefined" >> Makefile.am
+        update_configure_scripts --reconf
+        ;;
+    *"x86_64"*"apple"*) 
+        export BLI_CONFIG=x86
+        export BLI_THREAD=openmp
+        export CC=gcc
+        export CXX=g++
+        ;;
+    *"x86_64"*"freebsd"*) 
+        export BLI_CONFIG=x86
+        export BLI_THREAD=openmp
+        export CC=gcc
+        export CXX=g++
+        ;;
+    *)
+        ;; 
+
+esac
+
+CFG_OPTION_POSIX="--prefix=${prefix} --build=${MACHTYPE} --host=${target}"
+# ./configure will warn about --enable-thread-model but it is actually effective.
+CFG_OPTION_TBLIS="--enable-config=${BLI_CONFIG} --enable-thread-model=${BLI_THREAD}"
+
+./configure ${CFG_OPTION_TBLIS} ${CFG_OPTION_POSIX}
 make -j${nproc}
-cp lib/.libs/libtblis.so.0.0.0 ${libdir}/libtblis.so.0
-cp src/external/tci/lib/.libs/libtci.so.0.0.0 ${libdir}/libtci.so.0
+make install
+
+# Copy license file
+install_license LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
     Linux(:x86_64, libc=:glibc),
-    Linux(:x86_64, libc=:musl)
+    Linux(:x86_64, libc=:musl, compiler_abi=CompilerABI(cxxstring_abi=:cxx11)),
+    MacOS(:x86_64),
+    FreeBSD(:x86_64),
+    Windows(:x86_64)
 ]
 platforms = expand_cxxstring_abis(platforms)
 
