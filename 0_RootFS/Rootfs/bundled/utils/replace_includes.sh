5f27f36e36703cf25f2903fe0d32a19ac8b15350
Elliot Saba <staticfloat@gmail.com>
2020-09-28 14:34:32 -0700

    Update replace_includes.sh

--- 0_RootFS/Rootfs/bundled/utils/replace_includes.sh
+++ 0_RootFS/Rootfs/bundled/utils/replace_includes.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
+# grep options:
 # -r = recursive
 # -l = files-with-matches
+# -E = use extended regex
+#
+# sed options:
 # -i = in-place
-grep -r -l "#\s*include\s*<$1.h>" . | xargs sed -i "s/#\s*include\s*<$1.h>/#include <$2.h>/"
+# use `&` as the regex delimiter for `sed` so that it doesn't conflict with anything in $1 or $2
+FILES=$(grep -r -l -E "#\s*include\s*<$1>" .)
+for f in ${FILES}; do
+    echo " --> ${f}"
+    sed -i -e "s&#\s*include\s*<$1>&#include <$2>&" "${f}"
+done
