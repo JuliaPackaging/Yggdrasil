d3a02e5ac274bbec843a7416bd29bd8999f1fa31
john verzani <jverzani@gmail.com>
2020-09-28 12:17:13 -0400

    Update build_tarballs.jl
    Thanks Mosè!

--- T/TtH/build_tarballs.jl
+++ T/TtH/build_tarballs.jl
@@ -23,7 +23,7 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    
+    ExecutableProduct("tth", :tth), 
 ]
 
 # Dependencies that must be installed before this package can be built
