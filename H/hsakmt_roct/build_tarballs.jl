434a491a4b14467634cb13c64ebe04e203dfb0e5
Julian P Samaroo <jpsamaroo@jpsamaroo.me>
2020-10-03 15:54:27 -0500

    [hsakmt_roct] Added musl support

--- H/hsakmt_roct/build_tarballs.jl
+++ H/hsakmt_roct/build_tarballs.jl
@@ -8,12 +8,16 @@ version = v"3.7.0"
 # Collection of sources required to build ROCT-Thunk-Interface
 sources = [
     ArchiveSource("https://github.com/RadeonOpenCompute/ROCT-Thunk-Interface/archive/rocm-$(version).tar.gz",
-                  "b357fe7f425996c49f41748923ded1a140933de7564a70a828ed6ded6d896458")
+                  "b357fe7f425996c49f41748923ded1a140933de7564a70a828ed6ded6d896458"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/ROCT-Thunk-Interface*/
+
+# fix for musl (but works on glibc too)
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/0001-Build-correctly-on-musl.patch"
 
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
@@ -30,6 +34,7 @@ make install
 # ROCT-Thunk-Interface only supports Linux, seemingly only 64bit
 platforms = [
     Linux(:x86_64, libc=:glibc),
+    Linux(:x86_64, libc=:musl),
 ]
 
 # The products that we will ensure are always built
