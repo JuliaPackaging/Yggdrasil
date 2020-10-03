f1bb7c721c2b78efe07ca98945aa98157801d404
Gnimuc <qiyupei@gmail.com>
2020-10-03 21:42:25 +0800

    [GLFW] Update build script

--- G/GLFW/build_tarballs.jl
+++ G/GLFW/build_tarballs.jl
@@ -5,22 +5,23 @@ version = v"3.3.2"
 
 # Collection of sources required to build glfw
 sources = [
-    "https://github.com/glfw/glfw/releases/download/$(version)/glfw-$(version).zip" =>
-    "08a33a512f29d7dbf78eab39bd7858576adcc95228c9efe8e4bc5f0f3261efc7",
+    ArchiveSource("https://github.com/glfw/glfw/releases/download/$(version)/glfw-$(version).zip",
+    "08a33a512f29d7dbf78eab39bd7858576adcc95228c9efe8e4bc5f0f3261efc7")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/glfw-*/
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" \
-    -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DGLFW_BUILD_EXAMPLES=false \
-    -DGLFW_BUILD_TESTS=false \
-    -DGLFW_BUILD_DOCS=OFF
+CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release"
+CMAKE_FLAGS="${CMAKE_FLAGS} -DBUILD_SHARED_LIBS=ON"
+CMAKE_FLAGS="${CMAKE_FLAGS} -DGLFW_BUILD_EXAMPLES=false"
+CMAKE_FLAGS="${CMAKE_FLAGS} -DGLFW_BUILD_TESTS=false"
+CMAKE_FLAGS="${CMAKE_FLAGS} -DGLFW_BUILD_DOCS=OFF"
+cmake ${CMAKE_FLAGS} ..
 make -j${nproc}
 make install
+install_license ../LICENSE.md
 """
 
 # These are the platforms we will build for by default, unless further
@@ -34,11 +35,11 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    "Libglvnd_jll",
-    "Xorg_libXcursor_jll",
-    "Xorg_libXi_jll",
-    "Xorg_libXinerama_jll",
-    "Xorg_libXrandr_jll",
+    Dependency("Libglvnd_jll"),
+    Dependency("Xorg_libXcursor_jll"),
+    Dependency("Xorg_libXi_jll"),
+    Dependency("Xorg_libXinerama_jll"),
+    Dependency("Xorg_libXrandr_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
