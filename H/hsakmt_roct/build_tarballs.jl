586dfb311f8a55f672432b9c9003a66f3c97d81c
Mosè Giordano <giordano@users.noreply.github.com>
2020-10-03 23:15:46 +0100

    Update H/hsakmt_roct/build_tarballs.jl

--- H/hsakmt_roct/build_tarballs.jl
+++ H/hsakmt_roct/build_tarballs.jl
@@ -33,8 +33,8 @@ make install
 # platforms are passed in on the command line
 # ROCT-Thunk-Interface only supports Linux, seemingly only 64bit
 platforms = [
-    Platform("x86_64", "linux"; libc=:glibc),
-    Platform("x86_64", "linux"; libc=:musl),
+    Platform("x86_64", "linux"; libc="glibc"),
+    Platform("x86_64", "linux"; libc="musl"),
 ]
 
 # The products that we will ensure are always built
