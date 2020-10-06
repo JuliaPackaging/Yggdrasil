b70dcb44b520f0b99943f4887903856d21a5d547
Michael Schlottke-Lakemper <michael@sloede.com>
2020-10-07 00:33:46 +0200

    P4est: Enable BLAS and disable (unused) static libraries (#1749)
    * P4est: Enable BLAS and disable (unused) static libraries
    
    * [P4est] Work around bug in x86_64-linux-musl platform
    
    Co-authored-by: Mosè Giordano <mose@gnu.org>

--- P/P4est/build_tarballs.jl
+++ P/P4est/build_tarballs.jl
@@ -13,31 +13,28 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd p4est-2.2/
+cd $WORKSPACE/srcdir/p4est-2.2/
 if [[ "${target}" == *-freebsd* ]]; then
   export LIBS="-lm"
+elif [[ "${target}" == x86_64-linux-musl ]]; then
+    # We can't run Fortran programs for the native platform, so a check that the
+    # Fortran compiler works would fail.  Small hack: swear that we're
+    # cross-compiling.  See:
+    # https://github.com/JuliaPackaging/BinaryBuilderBase.jl/issues/50.
+    sed -i 's/cross_compiling=no/cross_compiling=yes/' configure
+    sed -i 's/cross_compiling=no/cross_compiling=yes/' sc/configure
 fi
-CFLAGS="-I${prefix}/include" LDFLAGS="-L${prefix}/lib" ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --without-blas
+export CPPFLAGS="-I${includedir}"
+export LDFLAGS="-L${libdir}"
+export BLAS_LIBS="${libdir}/libopenblas.${dlext}"
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-static
 make -j${nproc}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Linux(:i686, libc=:glibc),
-    Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
-    MacOS(:x86_64),
-    FreeBSD(:x86_64)
-]
+platforms = supported_platforms(; exclude=Sys.iswindows)
 
 
 # The products that we will ensure are always built
@@ -49,6 +46,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a"))
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
