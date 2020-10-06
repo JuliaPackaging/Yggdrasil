35de54d4c44a9c89b1112e5d13a06b05c8be30ef
Jan Strube <jan.strube@gmail.com>
2020-10-05 21:43:32 -0700

    [HepMC3] name change

--- H/HepMC3/build_tarballs.jl
+++ H/HepMC3/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "hepmc3"
+name = "HepMC3"
 version = v"3.2.2"
 
 # Collection of sources required to complete build
