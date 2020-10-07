7c50a7226b4f7ed77266c8fec24818c43b78d7ed
Viral B. Shah <viralbshah@antarctic.juliacomputing.io>
2020-10-07 13:50:17 -0400

    OpenBLAS32 and OpenBLASHighCoreCount at 0.3.10

---
+++ O/OpenBLAS/OpenBLASHighCoreCount@0.3.10/build_tarballs.jl
@@ -0,0 +1,16 @@
+using BinaryBuilder
+
+include("../common.jl")
+
+# Collection of sources required to build OpenBLAS
+name = "OpenBLAS"
+version = v"0.3.10"
+
+sources = openblas_sources(version)
+script = openblas_script(num_64bit_threads=128)
+platforms = openblas_platforms()
+products = openblas_products()
+dependencies = openblas_dependencies()
+
+# Build the tarballs
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", lock_microarchitecture=false)
