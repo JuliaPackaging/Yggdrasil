a01b12b15d3177f2ef54024919647289e990cba0
David Widmann <dev+git@devmotion.de>
2020-10-09 09:58:01 +0200

    Add LibYAML

---
+++ L/LibYAML/build_tarballs.jl
@@ -0,0 +1,41 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "LibYAML"
+version = v"0.2.5"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("http://pyyaml.org/download/libyaml/yaml-$version.tar.gz", "c642ae9b75fee120b2d96c712538bd2cf283228d2337df2cf2988e3c02678ef4")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ${WORKSPACE}/srcdir/yaml-*/
+
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make -j${nproc}
+make install
+
+# Move library on Windows
+if [[ "${target}" == *-mingw* ]]; then
+    mv "${libdir}"/libyaml-*.${dlext} "${libdir}/libyaml.${dlext}"
+fi
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libyaml", :libyaml)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
