85b5ec8b6242b5e51fa857ff62ca5d780a224d2d
Mosè Giordano <mose@gnu.org>
2020-10-10 23:05:58 +0100

    [ripgrep] Add builder

---
+++ R/ripgrep/build_tarballs.jl
@@ -0,0 +1,37 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "ripgrep"
+version = v"12.1.1"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://github.com/BurntSushi/ripgrep/archive/$(version).tar.gz",
+                  "2513338d61a5c12c8fea18a0387b3e0651079ef9b31f306050b1f0aaa926271e"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/ripgrep-*/
+cargo build --release --features 'pcre2'
+mkdir -p "${bindir}"
+cp "target/${rust_target}/release/rg${exeext}" "${bindir}/."
+install_license COPYING LICENSE-MIT UNLICENSE
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("rg", :rg),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers=[:c, :rust], lock_microarchitecture=false)
