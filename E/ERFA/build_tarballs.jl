ad767f24e94722c669bba6f2b0a610dbf52b597d
Helge Eichhorn <git@helgeeichhorn.de>
2020-10-10 07:13:38 +0200

    Update ERFA to 1.7.1

--- E/ERFA/build_tarballs.jl
+++ E/ERFA/build_tarballs.jl
@@ -3,17 +3,17 @@
 using BinaryBuilder, Pkg
 
 name = "ERFA"
-version = v"1.7.0"
+version = v"1.7.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/liberfa/erfa/releases/download/v1.7.0/erfa-1.7.0.tar.gz", "f0787e30e848750c0cbfc14827de6fc7f69a2d5ef0fc653504e74b8967a764e0")
+    ArchiveSource("https://github.com/liberfa/erfa/releases/download/v1.7.1/erfa-1.7.1.tar.gz", "80239ae9ba4bf7f328f7f64baec7fc38e587058b3ef9cea9331c751de0b98783")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd erfa-1.7.0/
+cd erfa-1.7.1/
 if [[ ${target} == i686-w64* ]] || [[ ${target} == x86_64-w64* ]]; then
     sed -i 's/LT_INIT/LT_INIT([win32-dll])/' configure.ac;
     sed -i 's/liberfa_la_LDFLAGS = -version-info \$(VI_ALL)/liberfa_la_LDFLAGS = -no-undefined -version-info $(VI_ALL)/' src/Makefile.am;
