c0cdeef02e1aa476c377b992a263bf3d61988d80
Mosè Giordano <giordano@users.noreply.github.com>
2020-10-10 09:22:13 +0100

    Apply suggestions from code review

--- E/ERFA/build_tarballs.jl
+++ E/ERFA/build_tarballs.jl
@@ -7,13 +7,13 @@ version = v"1.7.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/liberfa/erfa/releases/download/v1.7.1/erfa-1.7.1.tar.gz", "80239ae9ba4bf7f328f7f64baec7fc38e587058b3ef9cea9331c751de0b98783")
+    ArchiveSource("https://github.com/liberfa/erfa/releases/download/v$(version)/erfa-$(version).tar.gz", "80239ae9ba4bf7f328f7f64baec7fc38e587058b3ef9cea9331c751de0b98783")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd erfa-1.7.1/
+cd erfa-*/
 if [[ ${target} == i686-w64* ]] || [[ ${target} == x86_64-w64* ]]; then
     sed -i 's/LT_INIT/LT_INIT([win32-dll])/' configure.ac;
     sed -i 's/liberfa_la_LDFLAGS = -version-info \$(VI_ALL)/liberfa_la_LDFLAGS = -no-undefined -version-info $(VI_ALL)/' src/Makefile.am;
