0840b89ad3f0226e815fc79413a476332a2b64ac
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2020-10-14 10:58:18 +0200

    [libpolymake_julia] BuildDependency needs a different syntax for fixing the version

--- L/libpolymake_julia/build_tarballs.jl
+++ L/libpolymake_julia/build_tarballs.jl
@@ -52,8 +52,8 @@ dependencies = [
     BuildDependency(PackageSpec(name="Julia_jll", version=v"1.4.1")),
     Dependency("libcxxwrap_julia_jll"),
     Dependency(PackageSpec(name="polymake_jll", version=VersionSpec("4.2.0-4.2"))),
-    BuildDependency("GMP_jll", v"6.1.2"),
-    BuildDependency("MPFR_jll", v"4.0.2"),
+    BuildDependency(PackageSpec(name="GMP_jll", version=v"6.1.2")),
+    BuildDependency(PackageSpec(name="MPFR_jll", version=v"4.0.2")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
