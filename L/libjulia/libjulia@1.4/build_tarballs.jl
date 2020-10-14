aa9a251d35d67d31d05f6e033e0bd8317bfd1607
Max Horn <max@quendi.de>
2020-10-14 15:52:40 +0200

    HACK: drop libjulia 1.4 so it doesn't get build on this PR

--- L/libjulia/libjulia@1.4/build_tarballs.jl
+++
@@ -1,7 +0,0 @@
-include("../common.jl")
-
-name, version, sources, script, platforms, products, dependencies = configure(v"1.4.2")
-
-# Build the tarballs.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7", lock_microarchitecture=false)
-
