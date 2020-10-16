4ac5b0ef260a54877cdae8601697b9783ed3bab0
Mosè Giordano <giordano@users.noreply.github.com>
2020-10-16 19:46:36 +0100

    JLL bump: HarfBuzz_jll (#1810)
    [skip build]

--- H/HarfBuzz/build_tarballs.jl
+++ H/HarfBuzz/build_tarballs.jl
@@ -55,3 +55,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"5")
+
