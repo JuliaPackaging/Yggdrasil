704d81bae64948b499d6501928cdbea749340958
Mosè Giordano <mose@gnu.org>
2020-10-16 19:37:17 +0100

    Bump JLL wrapper: Librsvg_jll
    [skip build]

--- L/Librsvg/build_tarballs.jl
+++ L/Librsvg/build_tarballs.jl
@@ -96,3 +96,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers=[:c, :rust])
+
