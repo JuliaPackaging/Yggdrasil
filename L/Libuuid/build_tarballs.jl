eeaf1fc6d980601c453471bc516ed7c0e5c85e7c
Mosè Giordano <mose@gnu.org>
2020-10-16 19:32:35 +0100

    [Librsvg] Move under `L` directory [Libuuid] `ismacos` -> `isapple`
    [skip ci]

--- L/Libuuid/build_tarballs.jl
+++ L/Libuuid/build_tarballs.jl
@@ -22,7 +22,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [p for p in supported_platforms() if !(Sys.iswindows(p) || Sys.ismacos(p))]
+platforms = [p for p in supported_platforms() if !(Sys.iswindows(p) || Sys.isapple(p))]
 
 # The products that we will ensure are always built
 products = [
