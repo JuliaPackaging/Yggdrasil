58b3f2326ab9fff02ad65573e6260ecd09b904bc
Mosè Giordano <mose@gnu.org>
2020-10-16 19:38:27 +0100

    Bump JLL wrapper: Fontconfig_jll
    [skip build]

--- F/Fontconfig/build_tarballs.jl
+++ F/Fontconfig/build_tarballs.jl
@@ -83,4 +83,3 @@ build_tarballs(ARGS, name, version, sources, script, platforms, products, depend
 ENV["FONTCONFIG_FILE"] = get(ENV, "FONTCONFIG_FILE", fonts_conf)
     ENV["FONTCONFIG_PATH"] = get(ENV, "FONTCONFIG_PATH", dirname(ENV["FONTCONFIG_FILE"]))
 """)
-
