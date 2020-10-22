84e5db20909f0360c69db1769fb822004e7ec9e0
Elliot Saba <staticfloat@gmail.com>
2020-10-21 17:47:38 -0700

    [p7zip] Enable experimental platforms (#1960)
    * [p7zip] Enable experimental platforms
    
    * [p7zip] Fix misisng license error and configure for aarch64-darwin

--- P/p7zip/build_tarballs.jl
+++ P/p7zip/build_tarballs.jl
@@ -49,6 +49,7 @@ else
             arm-*linux*)         echo makefile.linux_cross_arm;;
             x86_64-*freebsd*)    echo makefile.freebsd6+;;
             x86_64-*darwin*)     echo makefile.macosx_llvm_64bits;;
+            aarch64-*darwin*)    echo makefile.macosx_llvm_64bits;;
         esac
     }
     cp $(target_makefile) makefile.machine
@@ -58,15 +59,16 @@ else
         CXXFLAGS="${CXXFLAGS} -Wno-c++11-narrowing"
     fi
 
+    install_license DOC/License.txt
+
     make -j${nproc} 7za CC="${CC} ${CFLAGS}" CXX="${CXX} ${CXXFLAGS}"
     mkdir -p ${prefix}/bin
     cp -a bin/7za ${prefix}/bin/7z
 fi
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = supported_platforms()
+# We enable experimental platforms as this is a core Julia dependency
+platforms = supported_platforms(;experimental=true)
 
 # The products that we will ensure are always built
 products = [
