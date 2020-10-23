0255518a123781071d7974b8e1190213cf652cf0
Max Horn <max@quendi.de>
2020-10-23 17:33:28 +0200

    GMP 6.1.2: correct Julia compat (#1968)
    [skip build]

--- G/GMP@6.1.2/build_tarballs.jl
+++ G/GMP@6.1.2/build_tarballs.jl
@@ -58,5 +58,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", julia_compat="~1.0, ~1.1, ~1.2, ~1.3, ~1.4, ~1.5")
 
