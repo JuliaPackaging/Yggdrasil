f95501feee12faef61deb398fc6413e4bcbdf409
Chris Foster <chris42f@gmail.com>
2020-10-30 05:35:13 +1000

    Wizard recipe for rclone-1.53.2 (#2009)
    * Wizard recipe for rclone-1.53.2
    
    * Fix executable product path
    
    * Update R/Rclone/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ R/Rclone/build_tarballs.jl
@@ -0,0 +1,39 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Rclone"
+version = v"1.53.2"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/rclone/rclone.git", "30dae752c9258f6aca9521a205696648fde4a98e")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd rclone/
+
+make
+
+# install manually as `make install` doesn't include $exeext
+install -d ${bindir}
+install -t ${bindir} ${GOPATH}/bin/rclone${exeext}
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("rclone", :rclone)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers = [:go, :c])
