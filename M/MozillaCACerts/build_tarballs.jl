e2a232e65666882debfca3859396fe7acb3f0635
Max Horn <max@quendi.de>
2020-10-30 00:49:28 +0100

    Update MozillaCACerts (#2012)

--- M/MozillaCACerts/build_tarballs.jl
+++ M/MozillaCACerts/build_tarballs.jl
@@ -4,13 +4,13 @@ using BinaryBuilder
 
 name = "MozillaCACerts"
 # Info and new versions here: https://curl.haxx.se/docs/caextract.html
-cacert_version = "2020-07-22"
+cacert_version = "2020-10-14"
 version = VersionNumber(replace(cacert_version, '-'=>'.'))
 
 # Collection of sources required to build MozillaCACerts
 sources = [
     FileSource("https://curl.haxx.se/ca/cacert-$cacert_version.pem", 
-    "2782f0f8e89c786f40240fc1916677be660fb8d8e25dede50c9f6f7b0c2c2178",
+    "bb28d145ed1a4ee67253d8ddb11268069c9dafe3db25a9eee654974c4e43eee5",
     filename="cacert.pem"),
     DirectorySource("./bundled")
 ]
