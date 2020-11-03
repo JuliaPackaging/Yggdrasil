13c3951e2c4bca0fa6f2f42168a38d666fb4af89
Elliot Saba <staticfloat@gmail.com>
2020-11-03 01:54:05 +0000

    [LibSSH2] Rebuild with fake version 1.9.1 for v1.6+

--- L/LibSSH2/build_tarballs.jl
+++ L/LibSSH2/build_tarballs.jl
@@ -33,7 +33,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(;experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -45,6 +45,7 @@ dependencies = [
     "MbedTLS_jll",
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
+# Note: we explicitly lie about this because we don't have the new
+# versioning APIs worked out in BB yet.
+version = v"1.9.1"
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
