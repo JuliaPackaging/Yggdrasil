1dc84dd71c2ed634f996399fcbae864ee6007b5a
Elliot Saba <staticfloat@gmail.com>
2020-11-02 17:27:39 -0800

    [GMP] Rebuild 6.2.0 v1.6+ experimental platforms (#2034)

--- G/GMP@6.2.0/build_tarballs.jl
+++ G/GMP@6.2.0/build_tarballs.jl
@@ -54,9 +54,5 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
-]
-
-# Build the tarballs, and possibly a `build.jl` as well.
+dependencies = []
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", julia_compat="1.6")
-
