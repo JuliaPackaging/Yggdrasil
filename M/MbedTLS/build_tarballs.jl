8e646ab190ec4dc9133e7fa016ac736161638a86
Elliot Saba <staticfloat@gmail.com>
2020-11-02 17:43:25 -0800

    Build for experimental platforms, and restrict to v1.6+

--- M/MbedTLS/build_tarballs.jl
+++ M/MbedTLS/build_tarballs.jl
@@ -36,7 +36,7 @@ make -j${nproc} && make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(;experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -50,4 +50,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
