979c153d0a62efaf564ffa42b0cba2b824d299fc
Elliot Saba <staticfloat@gmail.com>
2020-11-02 18:36:57 -0800

    Another batch of v1.6 JLLs (#2036)

--- S/SuiteSparse/SuiteSparse/build_tarballs.jl
+++ S/SuiteSparse/SuiteSparse/build_tarballs.jl
@@ -105,5 +105,7 @@ dependencies = [
 #    Dependency("METIS_jll"),
 ]
 
-# Build the tarballs
+# Note: we explicitly lie about this because we don't have the new
+# versioning APIs worked out in BB yet.
+version = v"5.4.1"
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
