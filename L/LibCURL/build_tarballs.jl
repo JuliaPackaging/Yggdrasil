f9659434845ada3a6fe208426d05d99f7891cfce
Elliot Saba <staticfloat@gmail.com>
2020-11-04 12:29:07 -0800

    [LibCURL] Enable experimental platforms (#2054)

--- L/LibCURL/build_tarballs.jl
+++ L/LibCURL/build_tarballs.jl
@@ -52,7 +52,7 @@ install_license COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(;experimental=true)
 
 # The products that we will ensure are always built
 products = [
