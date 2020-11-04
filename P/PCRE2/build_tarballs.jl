b88285834866c9748d5b0f45c3291b2c0316c3a6
Elliot Saba <staticfloat@gmail.com>
2020-11-03 17:34:58 -0800

    build for experimental platforms and set v1.6+

--- P/PCRE2/build_tarballs.jl
+++ P/PCRE2/build_tarballs.jl
@@ -40,7 +40,7 @@ fi
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(;experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -54,5 +54,5 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
 
