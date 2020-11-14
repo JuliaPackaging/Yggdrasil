fd1f770cc4b153f7eb980e7fef70c4e7a9d259fb
David Widmann <devmotion@users.noreply.github.com>
2020-11-14 21:39:43 +0100

    Remove conditionals

--- L/Libtask/bundled/task.c
+++ L/Libtask/bundled/task.c
@@ -15,14 +15,12 @@ jl_task_t *vanilla_get_current_task(void)
 
 jl_task_t *jl_enable_stack_copying(jl_task_t *t)
 {
-#if JULIA_VERSION_MAJOR == 1 && JULIA_VERSION_MINOR >= 1
     if (!t->copy_stack) {
         jl_ptls_t ptls = jl_get_ptls_states();
         t->copy_stack = 1;
         t->bufsz = 0;
         memcpy(&t->ctx, &ptls->base_ctx, sizeof(t->ctx));
     }
-#endif
     return t;
 }
 
@@ -51,24 +49,12 @@ jl_task_t *jl_clone_task(jl_task_t *t)
     newt->started = t->started;  // TODO: need testing
 
 
-#if JULIA_VERSION_MAJOR == 1 && JULIA_VERSION_MINOR >= 1
     newt->copy_stack = t->copy_stack;
     memcpy((void*)newt->ctx.uc_mcontext, (void*)t->ctx.uc_mcontext, sizeof(jl_jmp_buf));
-#if JULIA_VERSION_MINOR >= 2
     newt->queue = t->queue;
     newt->sticky = t->sticky;
-#endif
-#else
-    newt->parent = ptls->current_task;
-    newt->current_module = t->current_module;
-    newt->ssize = t->ssize;  // size of saved piece
-    memcpy((void*)newt->ctx, (void*)t->ctx, sizeof(jl_jmp_buf));
-#endif
 
     if (t->stkbuf) {
-#if !(JULIA_VERSION_MAJOR == 1 && JULIA_VERSION_MINOR >= 1)
-        newt->ssize = t->ssize;  // size of saved piece
-#endif
         // newt->stkbuf = allocb(t->bufsz);
         // newt->bufsz = t->bufsz;
         // memcpy(newt->stkbuf, t->stkbuf, t->bufsz);
@@ -77,9 +63,6 @@ jl_task_t *jl_clone_task(jl_task_t *t)
         newt->bufsz = 0;
         newt->stkbuf = t->stkbuf;
     } else {
-#if !(JULIA_VERSION_MAJOR == 1 && JULIA_VERSION_MINOR >= 1)
-        newt->ssize = 0;
-#endif
         newt->bufsz = 0;
         newt->stkbuf = NULL;
     }
