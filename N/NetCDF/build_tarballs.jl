694974e9d9e7fffc900c5750b5e1d5de9a81c39c
Martijn Visser <mgvisser@gmail.com>
2020-11-18 13:09:38 +0100

    using Pkg

--- N/NetCDF/build_tarballs.jl
+++ N/NetCDF/build_tarballs.jl
@@ -1,6 +1,6 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "NetCDF"
 version = v"4.7.4"
