bf3286682fe0280369cb30af1001ca9c1310ce96
Viral B. Shah <viral@mayin.org>
2020-12-01 16:47:40 -0500

    fix

--- Z/ZeroMQ/ZeroMQ@4.0/build_tarballs.jl
+++ Z/ZeroMQ/ZeroMQ@4.0/build_tarballs.jl
@@ -13,18 +13,21 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/zeromq-*
-if [[ "${target}" == *-mingw* ]]; then
-    # Apply patch from
-    # https://github.com/msys2/MINGW-packages/blob/350ace4617661a4df7b9474c573b08325fa716c3/mingw-w64-zeromq/001-mingw-__except-fixes.patch
-    atomic_patch -p1 ../patches/001-mingw-__except-fixes.patch
-elif [[ "${target}" == *86*-linux-musl* ]]; then
-    pushd /opt/${target}/lib/gcc/${target}/*/include
-    # Fix bug in Musl C library, see
-    # https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/387
-    atomic_patch -p0 $WORKSPACE/srcdir/patches/mm_malloc.patch
-    popd
-fi
+# if [[ "${target}" == *-mingw* ]]; then
+#     # Apply patch from
+#     # https://github.com/msys2/MINGW-packages/blob/350ace4617661a4df7b9474c573b08325fa716c3/mingw-w64-zeromq/001-mingw-__except-fixes.patch
+#     atomic_patch -p1 ../patches/001-mingw-__except-fixes.patch
+# elif [[ "${target}" == *86*-linux-musl* ]]; then
+#     pushd /opt/${target}/lib/gcc/${target}/*/include
+#     # Fix bug in Musl C library, see
+#     # https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/387
+#     atomic_patch -p0 $WORKSPACE/srcdir/patches/mm_malloc.patch
+#     popd
+# fi
 sh autogen.sh
+update_configure_scripts
+
+export LDFLAGS="-lstdc++"
 ./configure --prefix=$prefix \
     --host=${target} \
     --without-docs \
@@ -55,5 +58,4 @@ products = [
 dependencies = Dependency[
 ]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies,
-               preferred_gcc_version=v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
