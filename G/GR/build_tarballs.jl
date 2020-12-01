3d60bfd1792baa682ae18b659d1e69628c345f6d
Viral B. Shah <ViralBShah@users.noreply.github.com>
2020-12-01 09:36:39 -0500

    Update build_tarballs.jl

--- G/GR/build_tarballs.jl
+++ G/GR/build_tarballs.jl
@@ -22,7 +22,6 @@ if [[ $target == *"mingw"* ]]; then
     winflags=-DCMAKE_C_FLAGS="-D_WIN32_WINNT=0x0f00"
     tifflags=""
     update_configure_scripts
-    update_configure_scripts
 else
     tifflags=-DTIFF_LIBRARY=$libdir/libtiff.$dlext
 fi
