d0c1de12117ccb0adbb1e8dbcc22dfea0ff30867
Valentin Churavy <vchuravy@users.noreply.github.com>
2020-12-03 12:49:31 -0500

    bump Clang and LLVM (#2218)

--- L/LLVM/LLVM@11.0.0/build_tarballs.jl
+++ L/LLVM/LLVM@11.0.0/build_tarballs.jl
@@ -1,6 +1,6 @@
 name = "LLVM"
-llvm_full_version = v"11.0.0+5"
-libllvm_version = v"11.0.0+1"
+llvm_full_version = v"11.0.0+6"
+libllvm_version = v"11.0.0+2"
 
 # Include common LLVM stuff
 include("../common.jl")
