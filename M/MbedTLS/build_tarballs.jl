01bb13ae856660291f01e39ad76f05cc73c65826
Jacob Quinn <quinn.jacobd@gmail.com>
2020-12-12 17:12:44 -0700

    Bump mbedtls (#2246)
    * Bump mbedtls
    
    * [MbedTLS] Remove patch included in new version and add new patch for macOS
    
    Co-authored-by: Mosè Giordano <mose@gnu.org>

--- M/MbedTLS/build_tarballs.jl
+++ M/MbedTLS/build_tarballs.jl
@@ -1,12 +1,12 @@
 using BinaryBuilder
 
 name = "MbedTLS"
-version = v"2.24.0"
+version = v"2.25.0"
 
 # Collection of sources required to build MbedTLS
 sources = [
     GitSource("https://github.com/ARMmbed/mbedtls.git",
-              "523f0554b6cdc7ace5d360885c3f5bbcc73ec0e8"),
+              "1c54b5410fd48d6bcada97e30cac417c5c7eea67"),
     DirectorySource("./bundled"),
 ]
 
@@ -14,12 +14,11 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/mbedtls
 
-atomic_patch -p1 ../patches/fix_incorrect_EOF_check.patch
-
 # llvm-ranlib gets confused, use the binutils one
 if [[ "${target}" == *apple* ]]; then
     ln -sf /opt/${target}/bin/${target}-ranlib /opt/bin/ranlib
     ln -sf /opt/${target}/bin/${target}-ranlib /opt/bin/${target}-ranlib
+    atomic_patch -p1 ../patches/0001-Remove-flags-not-sopported-by-ranlib.patch
 fi
 
 # enable MD4
@@ -31,7 +30,8 @@ cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_C_STANDARD=99 \
     -DUSE_SHARED_MBEDTLS_LIBRARY=On \
     ..
-make -j${nproc} && make install
+make -j${nproc}
+make install
 
 if [[ "${target}" == *mingw* ]]; then
     # For some reason, the build system doesn't set the `.dll` files as
