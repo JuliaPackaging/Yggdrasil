400c7f428c8c0e7006687b06c60e526d993c07d6
jonas-kr <30567171+jonas-kr@users.noreply.github.com>
2020-12-16 09:46:59 +0100

    Update to latest stable release v2.0.2

--- F/finufft/build_tarballs.jl
+++ F/finufft/build_tarballs.jl
@@ -3,19 +3,20 @@
 using BinaryBuilder, Pkg
 
 name = "finufft"
-version = v"1.1.2"
+version = v"2.0.2"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/flatironinstitute/finufft.git", "5b92daf4244c92844dd2640ef80457c030e1bf25")
+    ArchiveSource("https://github.com/flatironinstitute/finufft/archive/v2.0.2.zip", "fe83dbc22a1d13799e9db48568d8417d3028e7b831800f2a626ea19b4f0461fe")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/finufft/
+cd $WORKSPACE/srcdir/finufft-2.0.2/
 # Overwrite LIBSFFT such that we do not require fftw3_threads or fftw3_omp for OMP support. Since the libraries in FFTW_jll already provide for threading, we do not loose anything.
-make lib -j${nproc} CFLAGS="-fPIC -O3 -funroll-loops -fcx-limited-range" LIBSFFT="-lfftw3 -lfftw3f -lm"
-cp lib/libfinufft.so "${libdir}/libfinufft.${dlext}"
+# Make use of the -DFFTW_PLAN_SAFE flag to allow for multiple threads using finufft at the same time.
+make lib CFLAGS="-fPIC -O3 -funroll-loops -fcx-limited-range -Iinclude" CXXFLAGS="-fPIC -O3 -funroll-loops -fcx-limited-range -Iinclude -std=c++14 -DFFTW_PLAN_SAFE" LIBSFFT="-lfftw3 -lfftw3f -lm"
+mv lib/libfinufft.so "${libdir}/libfinufft.${dlext}"
 """
 
 # These are the platforms we will build for by default, unless further
@@ -30,7 +31,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a")),
+    Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
 
