859567571278fa74d8d0f8f6fa18875fbed30b1b
Jan Strube <jan.strube@gmail.com>
2020-12-16 14:36:17 -0800

    [LCIO_Julia_Wrapper] add more platforms (#2273)
    * [LCIO_Julia_Wrapper] add more platforms
    
    * removing musl

--- L/LCIO_Julia_Wrapper/build_tarballs.jl
+++ L/LCIO_Julia_Wrapper/build_tarballs.jl
@@ -24,10 +24,13 @@ install_license $WORKSPACE/srcdir/LCIO_Julia_Wrapper/LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = Platform[
-    Platform("x86_64", "linux"; libc="glibc"),
-    Platform("x86_64", "macos")
-]
+platforms = supported_platforms()
+filter!(!Sys.isfreebsd, platforms)
+filter!(!Sys.iswindows, platforms)
+filter!(p -> arch(p) != "armv7l", platforms)
+# skip i686 musl builds (not supported by libjulia_jll)
+filter!(p -> !(Sys.islinux(p) && libc(p) == "musl" && arch(p) == "i686"), platforms)
+
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
