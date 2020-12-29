8201d2a67d3cd9085245ff759f168c039f267edd
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2020-12-29 20:40:03 +0100

    review changes

--- G/gmsh/build_tarballs.jl
+++ G/gmsh/build_tarballs.jl
@@ -16,25 +16,27 @@ cd ${WORKSPACE}/srcdir/gmsh
 install_license LICENSE.txt
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DENABLE_BUILD_DYNAMIC=1 ..
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=Release \ 
+      -DENABLE_BUILD_DYNAMIC=1 \ 
+      ..
 make
 make install
-exit
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
     LibraryProduct(["libgmsh", "gmsh"], :libgmsh),
-#   ExecutableProduct(prefix, "gmsh", :gmsh)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-   #"https://github.com/JuliaLinearAlgebra/OpenBLASBuilder/releases/download/v0.3.0-3/build_OpenBLAS.v0.3.0.jl" 
+
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
