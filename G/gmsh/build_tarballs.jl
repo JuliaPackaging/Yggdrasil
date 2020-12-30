4a06858cd917fc5bd8b3d45530fa1dbb4ae3566e
Maximilian Köhler <maximilian.koehler@ruhr-uni-bochum.de>
2020-12-30 01:50:32 +0100

    Gmsh (#2322)
    * add gmsh
    
    * rename Gmsh folder to gmsh
    
    * review changes
    
    * remove whitespace at eol
    
    * another eol whitespace try
    
    * lets try gcc 7

---
+++ G/gmsh/build_tarballs.jl
@@ -0,0 +1,44 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "gmsh"
+version = v"4.7.1"
+
+# Collection of sources required to build Gmsh
+sources = [
+    GitSource("https://gitlab.onelab.info/gmsh/gmsh.git", "8417af5701df5fb2d4e208424f1477be21f65c3c"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ${WORKSPACE}/srcdir/gmsh
+install_license LICENSE.txt
+mkdir build
+cd build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DENABLE_BUILD_DYNAMIC=1 \
+      ..
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_cxxstring_abis(supported_platforms())
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct(["libgmsh", "gmsh"], :libgmsh),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies,  preferred_gcc_version=v"7")
+
