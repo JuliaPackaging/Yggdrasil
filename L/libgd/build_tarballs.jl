aeb0238cb635ee7f05a81d8262866cc782eae7bf
Chetan Vardhan <51269425+clad26@users.noreply.github.com>
2021-01-01 16:53:53 +0530

    Create build_tarballs.jl

---
+++ L/libgd/build_tarballs.jl
@@ -0,0 +1,44 @@
+using BinaryBuilder, Pkg
+
+name = "libgd"
+version = v"2.3.0"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://github.com/libgd/libgd/releases/download/gd-2.3.0/libgd-2.3.0.tar.gz")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/libgd-*/
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("pngtogd2", :pngtogd2),
+    ExecutableProduct("webpng", :webpng)
+    ExecutableProduct("pngtogd", :pngtogd)
+    ExecutableProduct("gdtopng", :gdtopng)
+    ExecutableProduct("gdcmpgif", :gdcmpgif)
+    ExecutableProduct("gd2topng", :gd2topng)
+    ExecutableProduct("gdparttopng", :gdparttopng)
+    ExecutableProduct("gd2copypal", :gd2copypal)
+    LibraryProduct("libgd.so.3.0.8", :libgd),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="JpegTurbo_jll", "aacddb02-875f-59d6-b918-886e6ef4fbf8")),
+    Dependency(PackageSpec(name="Zlib_jll", uuid = "83775a58-1f1d-513f-b197-d71354ab007a")),
+    Dependency(PackageSpec(name="libpng_jll", uuid="b53b4c65-9356-5827-b1ea-8c7a1a84506f")),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
