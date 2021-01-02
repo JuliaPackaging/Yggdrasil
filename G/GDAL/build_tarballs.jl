1ef110731424c1afe656fd61571281181e7635c2
Crghilardi <Crghilardi@yahoo.com>
2021-01-01 17:41:58 -0800

    [GDAL] change export LDFLAG over to use -rpath-link (#2334)
    * change export LDFLAG over to use -rpath-link
    
    * [GDAL] Pass same flag also to configure GEOS
    
    Co-authored-by: Mosè Giordano <mose@gnu.org>

--- G/GDAL/build_tarballs.jl
+++ G/GDAL/build_tarballs.jl
@@ -21,12 +21,15 @@ if [[ ${target} == *mingw* ]]; then
     autoreconf -vi
     export PROJ_LIBS="proj_7_2"
 elif [[ "${target}" == *-linux-* ]]; then
-    # Make sure GEOS is linked against libstdc++
-     atomic_patch -p1 "$WORKSPACE/srcdir/patches/geos-m4-extra-libs.patch"
-     atomic_patch -p1 "$WORKSPACE/srcdir/patches/configure_ac_curl_libs.patch"
-    export EXTRA_GEOS_LIBS="-lstdc++"
-    export EXTRA_CURL_LIBS="-lstdc++"
-    export LDFLAGS="$LDFLAGS -lstdc++"
+    # Hint to find libstdc++, required to link against C++ libs when using C compiler
+    if [[ "${nbits}" == 32 ]]; then
+        export CFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib"
+    else
+        export CFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib64"
+    fi
+    # Use same flags also for GEOS
+    atomic_patch -p1 "$WORKSPACE/srcdir/patches/geos-m4-extra-cflags.patch"
+    export EXTRA_GEOS_CFLAGS="${CFLAGS}"
     if [[ "${target}" == powerpc64le-* ]]; then
         atomic_patch -p1 "$WORKSPACE/srcdir/patches/sqlite3-m4-extra-libs.patch"
         export EXTRA_GEOS_LIBS="${EXTRA_GEOS_LIBS} -lm"
