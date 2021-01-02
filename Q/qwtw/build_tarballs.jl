4f3eec2795be07160a62445886ef4199f97c1e9a
ig-or <19777752+ig-or@users.noreply.github.com>
2021-01-02 05:25:51 -0800

    [qwtw] maps added with KDE Marble support (#2337)

--- Q/qwtw/build_tarballs.jl
+++ Q/qwtw/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "qwtw"
-version = v"2.0.6"
+version = v"2.1.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/ig-or/qwtw.git", "ca79480ec11855c9f2c09df39eb5a9f4cbb62657")
+    GitSource("https://github.com/ig-or/qwtw.git", "e5c38174da3c75e2673c001f7b270a4ec210f9e5")
 ]
 
 # Bash recipe for building across all platforms
@@ -36,7 +36,8 @@ platforms = expand_cxxstring_abis(platforms)
 products = [
     LibraryProduct("libqwtw", :qwtw),
     ExecutableProduct("qwproc", :qwproc),
-    ExecutableProduct("qttest", :qttest)
+    ExecutableProduct("qttest", :qttest),
+    ExecutableProduct("qwtest2", :qwtest2)
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -47,6 +48,8 @@ dependencies = [
     Dependency(PackageSpec(name="qwt_jll", uuid="ed0789fa-10db-50b3-94da-03266d70be0f"))
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
     Dependency(PackageSpec(name="FreeType2_jll", uuid="d7e528f0-a631-5988-bf34-fe36492bcfd7"))
+    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"))
+    Dependency(PackageSpec(name="marble_jll", uuid="678d7417-9a84-558b-a975-2deb8d71bebc"))
 ]
 
 
