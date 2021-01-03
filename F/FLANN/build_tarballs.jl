41b26080ee5b6d5d2b552cbf64aa2329e604d740
ASUS1 <crghilardi@yahoo.com>
2021-01-03 10:32:06 -0800

    fix patch path issue,  add CompilerSupportLibraries to deps

--- F/FLANN/build_tarballs.jl
+++ F/FLANN/build_tarballs.jl
@@ -18,7 +18,7 @@ cd $WORKSPACE/srcdir/flann-*
 
 #CMake doesn't work straight from clone, see https://github.com/mariusmuja/flann/issues/369 for source of workaround
 
-atomic_patch -p1 ../patches/cmake_empty_target.patch
+atomic_patch -p1 "$WORKSPACE/srcdir/patches/cmake_empty_target2.patch"
 
 touch src/cpp/empty.cpp
 
@@ -46,7 +46,9 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[]
+dependencies = Dependency[
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
