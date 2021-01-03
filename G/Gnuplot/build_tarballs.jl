ee45af060a1182afb09d7de5de44a845663382c8
Chetan Vardhan <51269425+clad26@users.noreply.github.com>
2021-01-03 22:32:27 +0530

    Update build_tarballs.jl

--- G/Gnuplot/build_tarballs.jl
+++ G/Gnuplot/build_tarballs.jl
@@ -29,7 +29,7 @@ platforms = supported_platforms()
 # The products that we will ensure are always built
 products = [
     ExecutableProduct("gnuplot", :gnuplot),
-    ExecutableProduct("gnuplot_qt", :gnuplot_qt, "libexec"),
+    ExecutableProduct("gnuplot_x11", :gnuplot_x11, "libexec"),
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -37,7 +37,7 @@ dependencies = [
     BuildDependency(PackageSpec(name="Xorg_xorgproto_jll", uuid = "c4d99508-4286-5418-9131-c86396af500b")),
     Dependency(PackageSpec(name="Libcerf_jll", uuid="af83a40a-c4c4-57a0-81df-2309fbd279e3")),
     Dependency(PackageSpec(name="LibGD_jll", uuid="16339573-6216-525a-b38f-30b6f6b71b5f")),
-    Dependency(PackageSpec(name="Qt_jll", uuid="ede63266-ebff-546c-83e0-1c6fb6d0efc8")),
+    Dependency(PackageSpec(name="X11_jll", uuid="546b0b6d-9ca3-5ba2-8705-1bc1841d8479")),
     Dependency(PackageSpec(name="Lua_jll", uuid="a4086b1d-a96a-5d6b-8e4f-2030e6f25ba6")),
     Dependency(PackageSpec(name="Cairo_jll", uuid="83423d85-b0ee-5818-9007-b63ccbeb887a")),
     Dependency(PackageSpec(name="Pango_jll", uuid="36c8627f-9965-5494-a995-c6b170f724f3")),
