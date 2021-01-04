ec43aa3971872bc4f28a77e2bfd944b788b0fc2f
Bart Janssens <bart@bartjanssens.org>
2021-01-04 22:56:25 +0100

    [Qt] Build tools

--- Q/Qt/build_tarballs.jl
+++ Q/Qt/build_tarballs.jl
@@ -31,7 +31,7 @@ commonoptions=" \
 -skip qtactiveqt -skip qtandroidextras -skip qtcanvas3d -skip qtconnectivity -skip qtdatavis3d -skip qtdoc -skip qtgamepad \
 -skip qtnetworkauth -skip qtpurchasing -skip qtremoteobjects -skip qtscript -skip qtscxml -skip qtsensors -skip qtserialbus \
 -skip qtserialport -skip qtspeech -skip qtvirtualkeyboard -skip qtlocation -skip qtwayland -skip qtwebchannel -skip qtwebengine \
--skip qtwebglplugin -skip qtwebsockets -skip qtwebview  -skip qttools  -openssl-linked  -nomake examples -release \
+-skip qtwebglplugin -skip qtwebsockets -skip qtwebview -openssl-linked  -nomake examples -release \
 "
 
 export OPENSSL_LIBS="-L${libdir} -lssl -lcrypto"
