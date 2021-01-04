7d73c530af9b7cc7f5c2465c566ff7c8af853350
Chetan Vardhan <51269425+clad26@users.noreply.github.com>
2021-01-04 23:47:32 +0530

    Update build_tarballs.jl

--- W/wxWidgets/build_tarballs.jl
+++ W/wxWidgets/build_tarballs.jl
@@ -12,6 +12,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/wxWidgets-*/
+apk add g++ linux-headers samurai
 if [[ "${target}" == *-linux-musl ]]; then
     # Delete libexpat to prevent it from being picked up by mistake
     rm /usr/lib/libexpat.so*
