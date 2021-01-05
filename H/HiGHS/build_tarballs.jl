81c1e6ee8c41bca6b1e2da79ea8880110fa47e76
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-01-05 17:42:28 +0100

    second commit

--- H/HiGHS/build_tarballs.jl
+++ H/HiGHS/build_tarballs.jl
@@ -49,3 +49,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"4.9")
+
