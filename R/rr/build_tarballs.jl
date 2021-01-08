59a7a126360dd9c587b8f71771c07201ce722951
Keno Fischer <keno@juliacomputing.com>
2021-01-08 01:40:26 -0500

    Bump rr (#2366)
    * Bump rr
    
    Updates https://github.com/rr-debugger/rr/pull/2772 to the latest version of the PR.
    Carries https://github.com/rr-debugger/rr/pull/2785 and https://github.com/rr-debugger/rr/pull/2787, which should help https://github.com/JuliaLang/julia/issues/39068.
    
    * Fix build with ancient libc

--- R/rr/build_tarballs.jl
+++ R/rr/build_tarballs.jl
@@ -8,7 +8,7 @@ version = v"5.4.1"
 # Collection of sources required to build rr
 sources = [
     GitSource("https://github.com/Keno/rr.git",
-              "f17f62ab0f68394073d8a04f47ccec38afbd515a")
+              "dc50efb04f29e21890c92534781ce11b5c91372a")
 ]
 
 # Bash recipe for building across all platforms
