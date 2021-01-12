ecea5e062f50197475aabc1456bfcb93d34967f5
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-01-12 21:56:44 +0100

    cross compiling bliss

--- S/SCIP/build_tarballs.jl
+++ S/SCIP/build_tarballs.jl
@@ -17,7 +17,7 @@ cd $WORKSPACE/srcdir
 cd bliss
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_BUILD_TYPE=Release ..
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j${nproc}
 make install
 cd ../../scipoptsuite*
