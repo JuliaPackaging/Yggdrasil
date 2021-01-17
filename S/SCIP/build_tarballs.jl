3840d12267ec32dba9954a00168ec023083a62aa
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-01-17 14:44:39 +0100

    add bliss symmetry

--- S/SCIP/build_tarballs.jl
+++ S/SCIP/build_tarballs.jl
@@ -22,14 +22,14 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j${nproc}
 make install
-cd ../..
+cd $WORKSPACE/srcdir
 """
 
 script = raw"""
 cd scipoptsuite*
 mkdir build
 cd build/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DPAPILO=0 -DZIMPL=OFF -DGCG=0 -DREADLINE=OFF -DBOOST=off ..
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DPAPILO=0 -DZIMPL=OFF -DGCG=0 -DREADLINE=OFF -DBOOST=off -DSYM=bliss ..
 make -j${nproc}
 make install
 """
