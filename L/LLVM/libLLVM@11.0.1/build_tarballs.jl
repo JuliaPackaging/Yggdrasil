5ca325877a12d7e4c27041531ccf756a4dcbba78
Valentin Churavy <vchuravy@users.noreply.github.com>
2021-01-17 12:24:15 -0500

    build libLLVM for LLVM 11.0.1 (#2403)

---
+++ L/LLVM/libLLVM@11.0.1/build_tarballs.jl
@@ -0,0 +1,6 @@
+name = "libLLVM"
+version = v"11.0.1+0"
+
+# Include common tools.
+include("../common.jl")
+build_tarballs(ARGS, configure_extraction(ARGS, version, name; experimental_platforms=true)...; skip_audit=true, julia_compat="1.6")
