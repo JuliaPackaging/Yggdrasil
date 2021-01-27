0b3ff2125be5888a9cf66b9b69340081afdaeb05
Curtis Vogt <curtis.vogt@gmail.com>
2021-01-27 16:21:37 -0600

    [Zstd] Update to version 1.4.8

--- Z/Zstd/build_tarballs.jl
+++ Z/Zstd/build_tarballs.jl
@@ -1,11 +1,11 @@
 using BinaryBuilder
 
 name = "Zstd"
-version = v"1.4.5"
+version = v"1.4.8"
 
 sources = [
     ArchiveSource("https://github.com/facebook/zstd/releases/download/v$version/zstd-$version.tar.gz",
-                  "98e91c7c6bf162bf90e4e70fdbc41a8188b9fa8de5ad840c401198014406ce9e"),
+                  "32478297ca1500211008d596276f5367c54198495cf677e9439f4791a4c69f24"),
 ]
 
 script = raw"""
