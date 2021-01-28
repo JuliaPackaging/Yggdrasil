7443856c690db2f3dfc0bd0b2738555c1c34bf15
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2021-01-28 09:26:37 +0100

    simplify the build
    (thanks to @giordano again)

--- S/SBML/build_tarballs.jl
+++ S/SBML/build_tarballs.jl
@@ -10,25 +10,17 @@ sources = [
 ]
 
 script = raw"""
-cd ${WORKSPACE}/srcdir
-SBMLNAME=libsbml-*
-
-cd ${WORKSPACE}/srcdir/${SBMLNAME}
+cd ${WORKSPACE}/srcdir/libsbml-*
 mkdir build
 cd build
 cmake \
   -DCMAKE_INSTALL_PREFIX=${prefix} \
   -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-  -DCMAKE_LIBRARY_PATH=${prefix}/lib \
-  -DCMAKE_INCLUDE_PATH="${prefix}/include/libxml2;${prefix}/include" \
+  -DCMAKE_LIBRARY_PATH=${libdir} \
+  -DCMAKE_INCLUDE_PATH="${includedir}/libxml2;${includedir}" \
   ..
 make -j${nproc}
 make install
-
-LICENSEDIR=${prefix}/share/licenses/SBML
-mkdir -p $LICENSEDIR
-cp ${WORKSPACE}/srcdir/${SBMLNAME}/LICENSE.txt $LICENSEDIR/LICENSE
-cp ${WORKSPACE}/srcdir/${SBMLNAME}/COPYING.txt $LICENSEDIR/COPYING
 """
 
 platforms = expand_cxxstring_abis(supported_platforms())
