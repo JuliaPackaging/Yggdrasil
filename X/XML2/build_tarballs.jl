834cef932d21536b8b32399b1d16af8601b82784
Curtis Vogt <curtis.vogt@gmail.com>
2021-01-29 21:55:49 -0600

    [XML2] Build on experimental platforms

--- X/XML2/build_tarballs.jl
+++ X/XML2/build_tarballs.jl
@@ -28,7 +28,7 @@ rm -rf ${prefix}/share/{doc/libxml2-*,gtk-doc}
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(;experimental=true)
 
 # The products that we will ensure are always built
 products = [
