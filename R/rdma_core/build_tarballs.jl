d78744e6a8c787129bd657f41dc89d1bd7974666
Valentin Churavy <v.churavy@gmail.com>
2021-01-30 19:27:51 -0500

    [RDMA-CORE] Build v33.1

---
+++ R/rdma_core/build_tarballs.jl
@@ -0,0 +1,47 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "rdma_core"
+version = v"33.1.0"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://github.com/linux-rdma/rdma-core/releases/download/v33.1/rdma-core-33.1.tar.gz", "d179b102bec551ce62265ed463d1095fb2ae9baff604261ad63327fcd20650e5")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd rdma-core-*
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DNO_MAN_PAGES=ON
+make -j
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Linux(:i686, libc=:glibc),
+    Linux(:x86_64, libc=:glibc),
+    Linux(:aarch64, libc=:glibc),
+    Linux(:powerpc64le, libc=:glibc),
+    Linux(:i686, libc=:musl),
+    Linux(:x86_64, libc=:musl),
+    Linux(:aarch64, libc=:musl)
+]
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libibverbs", :libibverbs),
+    LibraryProduct("librdmacm", :lbrdmacm)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="libnl_jll", uuid="7c700954-19d3-5208-81e2-8fa5fe7c0bd8"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"5.2.0")
