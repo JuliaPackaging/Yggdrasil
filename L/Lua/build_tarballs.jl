33d6d4af4807ba64e04f796098dd74f27619a83b
Valentin Churavy <v.churavy@gmail.com>
2021-02-09 22:41:38 -0500

    bump Lua patch version

--- L/Lua/build_tarballs.jl
+++ L/Lua/build_tarballs.jl
@@ -1,11 +1,11 @@
 using BinaryBuilder
 
 name = "Lua"
-version = v"5.3.5"
+version = v"5.3.6"
 
 sources = [
-    "https://www.lua.org/ftp/lua-5.3.5.tar.gz" =>
-        "0c2eed3f960446e1a3e4b9a1ca2f3ff893b6ce41942cf54d5dd59ab4b3b058ac",
+    "https://www.lua.org/ftp/lua-$(version).tar.gz" =>
+        "fc5fd69bb8736323f026672b1b7235da613d7177e72558893a0bdcd320466d60",
     "./bundled",
 ]
 
