0a3fa11dc23938f03e3f088162a2e829bb021a16
wsmoses <wsmoses@users.noreply.github.com>
2021-02-10 22:45:24 +0000

    Upgrade enzyme to refs/pull/136/merge

--- E/Enzyme/Enzyme@9/build_tarballs.jl
+++ E/Enzyme/Enzyme@9/build_tarballs.jl
@@ -1,7 +1,45 @@
-include("../common.jl")
+using BinaryBuilder, Pkg
 
-# Dependencies that must be installed before this package can be built
+name = "Enzyme"
+repo = "https://github.com/wsmoses/Enzyme.git"
 
+auto_version = "refs/pull/136/merge"
+version = VersionNumber(split(auto_version, "/")[end])
+
+# Collection of sources required to build attr
+sources = [GitSource(repo, "6e0a8ddd57f58274048bbf51c1e299c880a20ba8")]
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_cxxstring_abis(supported_platforms())
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd Enzyme
+# install_license LICENSE.TXT
+CMAKE_FLAGS=()
+# Release build for best performance
+CMAKE_FLAGS+=(-DENZYME_EXTERNAL_SHARED_LIB=ON)
+CMAKE_FLAGS+=(-DCMAKE_BUILD_TYPE=RelWithDebInfo)
+# Install things into $prefix
+CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix})
+# Explicitly use our cmake toolchain file and tell CMake we're cross-compiling
+CMAKE_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN})
+CMAKE_FLAGS+=(-DCMAKE_CROSSCOMPILING:BOOL=ON)
+# Tell CMake where LLVM is
+CMAKE_FLAGS+=(-DLLVM_DIR="${prefix}/lib/cmake/llvm")
+# Force linking against shared lib
+CMAKE_FLAGS+=(-DLLVM_LINK_LLVM_DYLIB=ON)
+# Build the library
+CMAKE_FLAGS+=(-DBUILD_SHARED_LIBS=ON)
+cmake -B build -S enzyme -GNinja ${CMAKE_FLAGS[@]}
+ninja -C build -j ${nproc} install
+"""
+
+# The products that we will ensure are always built
+products = Product[
+    LibraryProduct(["libEnzyme-9", "libEnzyme"], :libEnzyme),
+]
 
 dependencies = [
     BuildDependency(PackageSpec(name="LLVM_full_jll", version=v"9.0.1")),
