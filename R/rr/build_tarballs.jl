c58a5cc0d87e8f578c3f09d3a7e72271e593e2b2
Keno Fischer <keno@juliacomputing.com>
2021-02-09 19:49:04 -0500

    Bump rr
    Should fix our traces coming back with broken symlinks

--- R/rr/build_tarballs.jl
+++ R/rr/build_tarballs.jl
@@ -8,7 +8,7 @@ version = v"5.4.1"
 # Collection of sources required to build rr
 sources = [
     GitSource("https://github.com/Keno/rr.git",
-              "03ffb97f74dc9cc7b5bed1102cbf83d25f5e5644")
+              "162dc87419f349e5af53f99acc473251f58a4f9d")
 ]
 
 # Bash recipe for building across all platforms
