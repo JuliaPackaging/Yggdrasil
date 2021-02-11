8e80d5245462d39e8471a4045c9c2b048faea7c0
Venkateshprasad <vensadk@gmail.com>
2021-02-11 12:54:56 +0530

    With alternate deps
    Builds fully locally

--- T/Trilinos/build_tarballs.jl
+++ T/Trilinos/build_tarballs.jl
@@ -3,21 +3,22 @@
 using BinaryBuilder, Pkg
 
 name = "Trilinos"
-version = v"13.0.1"
+version = v"12.12.1"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/trilinos/Trilinos.git", "4796b92fb0644ba8c531dd9953e7a4878b05c62d")
+    GitSource("https://github.com/trilinos/Trilinos.git", "512a9e81183c609ab16366a9b09d70d37c6af8d4")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
+mkdir build
+cd build
 install_license ${WORKSPACE}/srcdir/Trilinos/LICENSE
-mkdir buildt
-cd buildt
+SRCDIR="/workspace/srcdir/Trilinos"
 FLAGS="-O3 -fPIC"
-cmake /workspace/srcdir/Trilinos -G "Unix Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DTrilinos_ENABLE_Fortran=OFF -DCMAKE_CXX_FLAGS="$FLAGS" -DCMAKE_C_FLAGS="$FLAGS" -DCMAKE_MAKE_PROGRAM="make" -DBUILD_SHARED_LIBS=ON -DTrilinos_ENABLE_NOX=ON -DNOX_ENABLE_LOCA=ON -DTrilinos_ENABLE_EpetraExt=ON -DEpetraExt_BUILD_BTF=ON -DEpetraExt_BUILD_EXPERIMENTAL=ON -DEpetraExt_BUILD_GRAPH_REORDERINGS=ON -DTrilinos_ENABLE_TrilinosCouplings=ON -DTrilinos_ENABLE_Ifpack=ON -DTrilinos_ENABLE_Isorropia=ON -DTrilinos_ENABLE_AztecOO=ON -DTrilinos_ENABLE_Teuchos=ON -DTeuchos_ENABLE_COMPLEX=ON -DTrilinos_ENABLE_Amesos=ON -DAmesos_ENABLE_KLU=ON -DTrilinos_ENABLE_Sacado=ON -DTrilinos_ENABLE_Kokkos=OFF -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=OFF -DTrilinos_ENABLE_CXX11=ON -DTPL_ENABLE_AMD=ON -DAMD_LIBRARY_DIRS="/$prefix/lib" -DTPL_AMD_INCLUDE_DIRS="$prefix/include/" -DTPL_BLAS_LIBRARIES='${prefix}/lib/' -DTPL_LAPACK_LIBRARIES="/$prefix/bin/" -DTPL_BLAS_INCLUDE_DIRS='${prefix}/include/cblas;${prefix}/include/f77blas' -DTPL_LAPACK_INCLUDE_DIRS="$prefix/include/lapack" -DTrilinos_SET_INSTALL_RPATH=FALSE -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=FALSE -DCMAKE_INSTALL_RPATH=$prefix/lib -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_BUILD_TYPE=Release
+cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_CXX_FLAGS="$FLAGS" -DCMAKE_C_FLAGS="$FLAGS" -DCMAKE_Fortran_FLAGS="$FLAGS" -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_MAKE_PROGRAM="make" -DTrilinos_ENABLE_NOX=ON   -DNOX_ENABLE_LOCA=ON -DTrilinos_ENABLE_EpetraExt=ON   -DEpetraExt_BUILD_BTF=ON   -DEpetraExt_BUILD_EXPERIMENTAL=ON   -DEpetraExt_BUILD_GRAPH_REORDERINGS=ON -DTrilinos_ENABLE_TrilinosCouplings=ON -DTrilinos_ENABLE_Ifpack=ON -DTrilinos_ENABLE_Isorropia=ON -DTrilinos_ENABLE_AztecOO=ON -DTrilinos_ENABLE_Belos=ON -DTrilinos_ENABLE_Teuchos=ON   -DTeuchos_ENABLE_COMPLEX=ON -DTrilinos_ENABLE_Amesos=ON   -DAmesos_ENABLE_KLU=ON -DTrilinos_ENABLE_Sacado=ON -DTrilinos_ENABLE_Kokkos=OFF -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=OFF -DTrilinos_ENABLE_CXX11=ON -DTPL_ENABLE_AMD=ON -DAMD_LIBRARY_DIRS="/${libdir}" -DTPL_ENABLE_BLAS=ON -DTPL_ENABLE_LAPACK=ON -DBLAS_LIBRARY_DIRS="${prefix}/lib" -DBLAS_LIBRARY_NAMES="libopenblas.so" -DLAPACK_LIBRARY_DIRS="${prefix}/lib" -DLAPACK_LIBRARY_NAMES="libopenblas.so" -DTrilinos_SET_INSTALL_RPATH=FALSE -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=FALSE -DCMAKE_INSTALL_RPATH=$prefix -DCMAKE_BUILD_TYPE=Release $SRCDIR
 make -j${nprocs}
 make install
 """
@@ -29,38 +30,42 @@ platforms = [
 ]
 
 platforms = expand_cxxstring_abis(platforms)
+platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libaztecoo", :libaztecoo),
-    LibraryProduct("libnoxepetra", :libnoxepetra),
-    LibraryProduct("libloca", :libloca),
     LibraryProduct("liblocalapack", :liblocalapack),
-    LibraryProduct("liblocaepetra", :liblocaepetra),
-    LibraryProduct("libepetra", :libepetra),
-    LibraryProduct("libteuchosremainder", :libteuchosremainder),
-    LibraryProduct("libsimpi", :libsimpi),
-    LibraryProduct("libnoxlapack", :libnoxlapack),
-    LibraryProduct("libteuchosparameterlist", :libteuchosparameterlist),
-    LibraryProduct("libteuchoscore", :libteuchoscore),
-    LibraryProduct("libtrilinoscouplings", :libtrilinoscouplings),
-    LibraryProduct("libnox", :libnox),
-    LibraryProduct("libteuchosparser", :libteuchosparser),
-    LibraryProduct("libteuchosnumerics", :libteuchosnumerics),
-    LibraryProduct("libtrilinosss", :libtrilinosss),
-    LibraryProduct("libisorropia", :libisorropia),
-    LibraryProduct("libzoltan", :libzoltan),
-    LibraryProduct("libepetraext", :libepetraext),
     LibraryProduct("libifpack", :libifpack),
-    LibraryProduct("libtriutils", :libtriutils),
-    LibraryProduct("libsacado", :libsacado),
+    LibraryProduct("libloca", :libloca),
+    LibraryProduct("libzoltan", :libzoltan),
+    LibraryProduct("libisorropia", :libisorropia),
+    LibraryProduct("libbelos", :libbelos),
+    LibraryProduct("libteuchosparameterlist", :libteuchosparameterlist),
+    LibraryProduct("libnoxlapack", :libnoxlapack),
+    LibraryProduct("libnox", :libnox),
+    LibraryProduct("libteuchoscore", :libteuchoscore),
+    LibraryProduct("libsimpi", :libsimpi),
+    LibraryProduct("libteuchosremainder", :libteuchosremainder),
+    LibraryProduct("liblocaepetra", :liblocaepetra),
+    LibraryProduct("libteuchosnumerics", :libteuchosnumerics),
     LibraryProduct("libteuchoscomm", :libteuchoscomm),
+    LibraryProduct("libtrilinoscouplings", :libtrilinoscouplings),
+    LibraryProduct("libepetraext", :libepetraext),
+    LibraryProduct("libtriutils", :libtriutils),
+    LibraryProduct("libbelosepetra", :libbelosepetra),
+    LibraryProduct("libepetra", :libepetra),
+    LibraryProduct("libtrilinosss", :libtrilinosss),
+    LibraryProduct("libnoxepetra", :libnoxepetra),
+    LibraryProduct("libsacado", :libsacado),
     LibraryProduct("libamesos", :libamesos)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="SuiteSparse_jll", uuid="bea87d4a-7f5b-5778-9afe-8cc45184846c"))
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
+    Dependency(PackageSpec(name="SuiteSparse32_jll", uuid="ca45d3f4-326b-53b0-9957-23b75aacb3f2"))
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
