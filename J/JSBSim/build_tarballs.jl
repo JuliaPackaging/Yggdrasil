ac515d21044684b55f4bb0592d8a43f83b40f3ec
Mosè Giordano <mose@gnu.org>
2021-02-13 16:02:55 +0000

    [JSBSim] Reorganise script a bit

--- J/JSBSim/build_tarballs.jl
+++ J/JSBSim/build_tarballs.jl
@@ -2,28 +2,42 @@ using BinaryBuilder
 
 name = "JSBSim"
 version = v"1.1.6"
+julia_version = v"1.5.3"
 
 # Collection of sources required to build JSBSim
 sources = [
-    GitSource("https://github.com/bcoconni/jsbsim.git", "a31ecb8727361aa74dee4ea7158c944848f78fb6"),
+    GitSource("https://github.com/bcoconni/jsbsim.git",
+              "a31ecb8727361aa74dee4ea7158c944848f78fb6"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/jsbsim
 mkdir build && cd build
-if [ ${target} = "x86_64-w64-mingw32" ]; then
-  cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_CXXFLAGS_RELEASE="-D_POSIX_C_SOURCE" -DCMAKE_BUILD_TYPE=Release -DBUILD_DOCS=OFF -DBUILD_PYTHON_MODULE=OFF ..
-else
-  cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_DOCS=OFF -DBUILD_PYTHON_MODULE=OFF ..
+FLAGS=()
+if [[ "${target}" == *-mingw* ]]; then
+    FLAGS+=(-DCMAKE_CXXFLAGS_RELEASE="-D_POSIX_C_SOURCE")
 fi
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_DOCS=OFF \
+    -DBUILD_PYTHON_MODULE=OFF \
+    -DBUILD_JULIA_PACKAGE=ON \
+    -DJulia_PREFIX="$prefix" \
+    "${FLAGS[@]}" \
+    ..
 make -j${nproc}
 cp julia/*JSBSimJL* $prefix/.
 cp ../julia/JSBSim.jl $prefix/.
 """
 
-# Skip platforms with GCC 4.x because it does not support C++14
-platforms = supported_platforms()
+ARGS
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+include("../../L/libjulia/common.jl")
+platforms = libjulia_platforms(julia_version)
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
@@ -32,8 +46,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    BuildDependency(PackageSpec(name="libjulia_jll", version=julia_version)),
     Dependency("libcxxwrap_julia_jll"),
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-    preferred_gcc_version=v"5")
+               preferred_gcc_version=v"5")
