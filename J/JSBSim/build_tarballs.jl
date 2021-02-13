68a98c91c3b6ac1f9c2a8f84be9937d3383a3a3c
Bertrand Coconnier <bcoconni@users.noreply.github.com>
2021-02-13 13:59:28 +0100

    Skip GCC 4.x because it does not support C++14

--- J/JSBSim/build_tarballs.jl
+++ J/JSBSim/build_tarballs.jl
@@ -3,7 +3,7 @@ using BinaryBuilder
 name = "JSBSim"
 version = v"1.1.6"
 
-# Collection of sources required to build ZMQ
+# Collection of sources required to build JSBSim
 sources = [
     GitSource("https://github.com/bcoconni/jsbsim.git", "a31ecb8727361aa74dee4ea7158c944848f78fb6"),
 ]
@@ -18,9 +18,8 @@ cp julia/*JSBSimJL* $prefix/.
 cp ../julia/JSBSim.jl $prefix/.
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = supported_platforms()
+# Skip platforms with GCC 4.x because it does not support C++14
+platforms = [p for p in supported_platforms() if compiler_abi(p).gcc_version != :gcc4]
 
 # The products that we will ensure are always built
 products = [
