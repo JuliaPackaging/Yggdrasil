3cc67de83f020cac825938fcfbb15977ea028235
Galabova <galabovaa@gmail.com>
2021-02-15 16:34:44 +0000

    remove fortran

--- H/HiGHS/build_tarballs.jl
+++ H/HiGHS/build_tarballs.jl
@@ -37,9 +37,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_gfortran_versions(
-    expand_cxxstring_abis(supported_platforms())
-)
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
