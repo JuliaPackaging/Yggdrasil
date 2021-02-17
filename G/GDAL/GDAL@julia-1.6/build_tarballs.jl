d036724aeb11c2f797ebc159b28fc99283efcd19
Martijn Visser <mgvisser@gmail.com>
2021-02-17 01:05:44 +0100

    [GDAL] build for julia 1.6+ (#2555)
    Follow up of #2554.

---
+++ G/GDAL/GDAL@julia-1.6/build_tarballs.jl
@@ -0,0 +1,19 @@
+include("../common.jl")
+
+# Offset to add to the version number
+version_offset = v"0.2.0"
+# Minimum Julia version supported: this is important to decide which versions of
+# the dependencies to use, in particular the JLL stdlibs.
+min_julia_version = v"1.6"
+# Fix to minor PROJ version; also update PROJ_LIBS
+# needed for Windows because of https://github.com/OSGeo/PROJ/blob/949171a6e/cmake/ProjVersion.cmake#L40-L46
+# to avoid this problem https://github.com/JuliaGeo/GDAL.jl/pull/102
+# Note that this currently fixes it to the exact JLL version that is used,
+# this is issue https://github.com/JuliaPackaging/BinaryBuilderBase.jl/issues/89.
+# Ideally we could by default allow any version X??.Y?? here, i.e. the PROJ minor version.
+proj_jll_version = "700.202"
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, configure(version_offset, min_julia_version, proj_jll_version)...;
+    julia_compat="1.6",
+    preferred_gcc_version=v"6")
