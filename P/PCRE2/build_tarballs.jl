9bb6d2b201a8b7d3528b0233b42ba7293c36217f
Elliot Saba <staticfloat@gmail.com>
2021-02-17 09:22:27 -0800

    [PCRE2] bump for codesigning (#2574)

--- P/PCRE2/build_tarballs.jl
+++ P/PCRE2/build_tarballs.jl
@@ -57,6 +57,5 @@ products = [
 dependencies = Dependency[
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
 
