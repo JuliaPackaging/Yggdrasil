c57227e4861d2803e17f54027362febaff19dd2c
Elliot Saba <staticfloat@gmail.com>
2021-02-18 00:47:07 +0000

    [OpenBLAS] Opt-in to ILP64 on OpenBLAS v0.3.13

--- O/OpenBLAS/OpenBLAS@0.3.13/build_tarballs.jl
+++ O/OpenBLAS/OpenBLAS@0.3.13/build_tarballs.jl
@@ -7,7 +7,7 @@ name = "OpenBLAS"
 version = v"0.3.13"
 
 sources = openblas_sources(version)
-script = openblas_script()
+script = openblas_script(;aarch64_ilp64=true)
 platforms = openblas_platforms(;experimental=true)
 products = openblas_products()
 dependencies = openblas_dependencies()
