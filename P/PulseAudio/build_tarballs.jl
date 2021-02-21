5ad778dd109299ebbfe0f2bd5de6ca01a936d91d
bramtayl <brandon.taylor221@gmail.com>
2021-02-21 10:06:34 -0500

    debug

--- P/PulseAudio/build_tarballs.jl
+++ P/PulseAudio/build_tarballs.jl
@@ -32,9 +32,7 @@ mkdir build
 cd build
 # I can't figure out how to build tdb, use gdbm instead
 # BlueZ requires systemd, which I'm also stuck on
-meson ..  -Ddatabase="gdbm" -Dbluez5="false" --cross-file=${MESON_TARGET_TOOLCHAIN}
-# debug
-tail meson-logs/meson-log.txt
+meson ..  -Ddatabase="gdbm" -Dbluez5="false" --cross-file=${MESON_TARGET_TOOLCHAIN} --debug
 # on musl, meson seems to be able to find dependencies, but isn't passing that info on to ninja?
 sed -i -e "s~/opt/bin/x86_64-linux-musl-cc~/opt/bin/x86_64-linux-musl-cc -I${includedir} -L${libdir}~" build.ninja
 ninja
