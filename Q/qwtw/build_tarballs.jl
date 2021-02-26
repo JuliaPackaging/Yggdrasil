9df50bddbf32b510f6ee050da87c7b112db8d6d5
Igor Sandler <isandler@topcon.com>
2021-02-25 18:45:34 -0800

    [qwtw] updated up to the version 2.3.0

--- Q/qwtw/build_tarballs.jl
+++ Q/qwtw/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "qwtw"
-version = v"2.2.1"
+version = v"2.3.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/ig-or/qwtw.git", "ccc8b2b1c00db11b2b57c6081a013a011807204c")
+    GitSource("https://github.com/ig-or/qwtw.git", "62540aee4aedbae34b814a178515914548f33e45")
 ]
 
 # Bash recipe for building across all platforms
