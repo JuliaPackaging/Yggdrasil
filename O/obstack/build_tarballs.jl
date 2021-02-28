0ddea28fd6f96d23d729458e6316ad5618834fa6
Julian Samaroo <jpsamaroo@jpsamaroo.me>
2021-02-28 11:19:28 -0600

    [obstack] Add obstack for musl (#2628)
    * [obstack] Add obstack for musl
    
    * Only build for musl
    
    * Update O/obstack/build_tarballs.jl
    
    * Use version 1.1.0
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ O/obstack/build_tarballs.jl
@@ -0,0 +1,36 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "obstack"
+version = v"1.1.0"
+
+# Collection of sources required to build obstack
+sources = [
+    ArchiveSource("https://github.com/pullmoll/musl-obstack/archive/v$(version.major).$(version.minor).tar.gz",
+                  "52a216613e7d55e8725e43d017bb2d49a4b1ffa1e06da472f03c7f9875df7d0d"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/musl-obstack-*/
+./bootstrap.sh
+CFLAGS="-fPIC" ./configure --prefix=${prefix} --host=${target}
+make -j${nproc}
+make install
+"""
+
+# Select Unix platforms
+platforms = [p for p in supported_platforms() if Sys.islinux(p) && libc(p) == "musl"]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libobstack", :libobstack),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
