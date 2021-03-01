f2b9bbf587db3a89817d8dc883f8fabd921552f4
Mosè Giordano <giordano@users.noreply.github.com>
2021-03-01 19:39:12 +0000

    Update build_tarballs.jl

--- H/Help2man/build_tarballs.jl
+++ H/Help2man/build_tarballs.jl
@@ -3,10 +3,11 @@ using BinaryBuilder
 # Collection of sources required to build Gettext
 name = "Help2man"
 version = v"1.48.1"
+
 sources = [
-               ArchiveSource("ftp://ftp.gnu.org/gnu/help2man/help2man-1.48.1.tar.xz",
-                              "de8a74740bd058646567b92ab4ecdeb9da9f1a07cc7c4f607a3c14dd38d10799"),
-               ]
+    ArchiveSource("https://ftp.gnu.org/gnu/help2man/help2man-$(version).tar.xz",
+                  "de8a74740bd058646567b92ab4ecdeb9da9f1a07cc7c4f607a3c14dd38d10799"),
+]
 
 # Bash recipe for building across all platforms
 script = raw"""
@@ -22,13 +23,12 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-                FileProduct("help2man", :help2man),
-
-                ]
+    FileProduct("bin/help2man", :help2man),
+]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
-                  ]
+dependencies = Dependency[
+]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
