60b71c40b86fb23ee12337d9cd3be20a145beedb
bramtayl <brandon.taylor221@gmail.com>
2021-02-28 19:44:29 -0500

    Update G/GStreamer/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- G/GStreamer/build_tarballs.jl
+++ G/GStreamer/build_tarballs.jl
@@ -46,7 +46,7 @@ dependencies = [
     Dependency(PackageSpec(name="Glib_jll", uuid="7746bdde-850d-59dc-9ae8-88ece973131d"))
     Dependency(PackageSpec(name="LibUnwind_jll", uuid="745a5e78-f969-53e9-954f-d19f2f74f4e3"))
     Dependency(PackageSpec(name="Elfutils_jll", uuid="ab5a07f8-06af-567f-a878-e8bb879eba5a"))
-    Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"))
+    Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"), v"6.1.2")
     Dependency(PackageSpec(name="GSL_jll", uuid="1b77fbbe-d8ee-58f0-85f9-836ddc23a7a4"))
     Dependency(PackageSpec(name="libcap_jll", uuid="eef66a8b-8d7a-5724-a8d2-7c31ae1e29ed"))
 ]
