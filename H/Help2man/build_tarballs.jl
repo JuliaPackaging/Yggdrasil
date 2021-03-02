5b183e6571704bf33db99d360acad0ca6289aae3
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2021-03-02 10:51:33 +0530

    Update H/Help2man/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- H/Help2man/build_tarballs.jl
+++ H/Help2man/build_tarballs.jl
@@ -19,7 +19,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = [AnyPlatform()]
 
 # The products that we will ensure are always built
 products = [
