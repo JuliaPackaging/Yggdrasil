88148deaf97b7ad7a5599a57c8d8368aa091579e
Valentin Churavy <v.churavy@gmail.com>
2021-03-02 09:45:42 -0500

    fixup! bump Clang and LLVM

--- L/LLVM/LLVM_assert@11.0.1/build_tarballs.jl
+++ L/LLVM/LLVM_assert@11.0.1/build_tarballs.jl
@@ -4,4 +4,4 @@ libllvm_version = v"11.0.1+3"
 
 # Include common LLVM stuff
 include("../common.jl")
-build_tarballs(ARGS, configure_extraction(ARGS, llvm_full_version, name, libllvm_version; experimental_platforms=true, assert=true)...; skip_audit=true)
+build_tarballs(ARGS, configure_extraction(ARGS, llvm_full_version, name, libllvm_version; experimental_platforms=true, assert=true)...; skip_audit=true,  julia_compat="1.6")
