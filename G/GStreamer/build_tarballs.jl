c0128ba55deda8f91c42543d4e1420f73477d22a
bramtayl <brandon.taylor221@gmail.com>
2021-03-02 09:48:12 -0500

    Update build_tarballs.jl

--- G/GStreamer/build_tarballs.jl
+++ G/GStreamer/build_tarballs.jl
@@ -25,7 +25,7 @@ ninja install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(exclude=[Platform("i686", "linux", libc = "musl"), Platform("powerpc64le", "linux")])
 
 # The products that we will ensure are always built
 products = [
