11d7f4dae696940cc1a77cdfcb9e971a27bfd671
Venkateshprasad <vensadk@gmail.com>
2021-03-04 19:01:08 +0530

    Builds for Linux ([musl, glibc]) and the patch

--- X/Xyce/build_tarballs.jl
+++ X/Xyce/build_tarballs.jl
@@ -7,48 +7,47 @@ version = v"7.2.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/westes/flex.git", "d69a58075169410324fe49666f6641ba6a9d1f91"),
-    GitSource("https://github.com/Xyce/Xyce.git", "a61faef4bfb2f36f1aa7cc44264bbbb66fbaac11"),
-    GitSource("https://github.com/trilinos/Trilinos.git", "512a9e81183c609ab16366a9b09d70d37c6af8d4")
-]
+            GitSource("https://github.com/Xyce/Xyce.git", "a61faef4bfb2f36f1aa7cc44264bbbb66fbaac11"),
+            GitSource("https://github.com/westes/flex.git", "d69a58075169410324fe49666f6641ba6a9d1f91"),
+            DirectorySource("./bundled")
+          ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd flex
 apk add texinfo
-apk add help2man
 ./autogen.sh
-./configure --prefix=${prefix}
-make -j${nprocs}
-make install
-cd ..
-mkdir trilbuild
-cd trilbuild
-SRCDIR="/workspace/srcdir/Trilinos"
-FLAGS="-O3 -fPIC"
-cmake -G "Unix Makefiles" -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_Fortran_COMPILER=gfortran -DCMAKE_CXX_FLAGS="$FLAGS" -DCMAKE_C_FLAGS="$FLAGS" -DCMAKE_Fortran_FLAGS="$FLAGS" -DBUILD_SHARED_LIBS=ON -DCMAKE_INSTALL_PREFIX="$prefix" -DCMAKE_MAKE_PROGRAM="make" -DTrilinos_ENABLE_NOX=ON -DNOX_ENABLE_LOCA=ON -DTrilinos_ENABLE_EpetraExt=ON   -DEpetraExt_BUILD_BTF=ON   -DEpetraExt_BUILD_EXPERIMENTAL=ON -DEpetraExt_BUILD_GRAPH_REORDERINGS=ON -DTrilinos_ENABLE_TrilinosCouplings=ON -DTrilinos_ENABLE_Ifpack=ON -DTrilinos_ENABLE_Isorropia=ON -DTrilinos_ENABLE_AztecOO=ON -DTrilinos_ENABLE_Belos=ON -DTrilinos_ENABLE_Teuchos=ON -DTeuchos_ENABLE_COMPLEX=ON -DTrilinos_ENABLE_Amesos=ON -DAmesos_ENABLE_KLU=ON -DTrilinos_ENABLE_Sacado=ON -DTrilinos_ENABLE_Kokkos=OFF -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=OFF -DTrilinos_ENABLE_CXX11=ON -DTPL_ENABLE_AMD=ON -DAMD_LIBRARY_DIRS="/${libdir}" -DAMD_LIBRARY_NAMES="libsuitesparseconfig.${dlext};libamd.${dlext};libklu.${dlext};libcolamd.${dlext};libbtf.${dlext}" -DAMD_INCLUDE_DIRS="${prefix}/include" -DTPL_ENABLE_UMFPACK=ON -DUMFPACK_LIBRARY_DIRS="/${libdir}" -DAMD_LIBRARY_NAMES="libumfpack.${dlext}" -DTPL_UMFPACK_INCLUDE_DIRS="${prefix}/include" -DTPL_ENABLE_BLAS=ON -DTPL_ENABLE_LAPACK=ON -DBLAS_LIBRARY_DIRS="${prefix}/lib" -DBLAS_LIBRARY_NAMES="libopenblas.${dlext}" -DLAPACK_LIBRARY_DIRS="${prefix}/lib" -DLAPACK_LIBRARY_NAMES="libopenblas.${dlext}" -DCMAKE_BUILD_TYPE=Release $SRCDIR
+./configure --prefix=${prefix} --host=${target}
 make -j${nprocs}
 make install
 cd ..
 cd Xyce
+if [[ $target != *glibc ]] ; then
+        atomic_patch -p1 ${WORKSPACE}/srcdir/patches/cross.patch
+fi
 ./bootstrap
 cd ..
 mkdir buildx
 cd buildx
-/workspace/srcdir/Xyce/./configure --enable-shared --disable-mpi --prefix=${prefix} LDFLAGS="-L${libdir} -lopenblas" CPPFLAGS="-I/$prefix/include"
+/workspace/srcdir/Xyce/./configure --enable-shared --disable-mpi --prefix=${prefix} LDFLAGS="-L${libdir} -lopenblas" CPPFLAGS="-I/$prefix/include" --host=${target}
 cd ..
 cd /workspace/destdir/
-mkdir x86_64-linux-gnu
-cd x86_64-linux-gnu
+mkdir ${target}
+cd ${target}
 mkdir lib
-mkdir lib64
-cd /opt/x86_64-linux-gnu/x86_64-linux-gnu/lib64
-cp libquadmath.a /workspace/destdir/x86_64-linux-gnu/lib64
-cp libquadmath.so /workspace/destdir/x86_64-linux-gnu/lib64
-cp libquadmath.la /workspace/destdir/x86_64-linux-gnu/lib64
-cp libquadmath.so.0.0.0 /workspace/destdir/x86_64-linux-gnu/lib64
-cp libquadmath.so.0 /workspace/destdir/x86_64-linux-gnu/lib64
+if [[ $target == x86_64-* ]] ; then
+    mkdir lib64
+    libname="lib64"
+elif [[ $target == i686-* ]]; then
+    libname="lib"
+fi
+cd /opt/${target}/${target}/${libname}
+cp libquadmath.a /workspace/destdir/${target}/${libname}
+cp libquadmath.${dlext} /workspace/destdir/${target}/${libname}
+cp libquadmath.la /workspace/destdir/${target}/${libname}
+cp libquadmath.${dlext}.0.0.0 /workspace/destdir/${target}/${libname}
+cp libquadmath.${dlext}.0 /workspace/destdir/${target}/${libname}
 cd /workspace/srcdir/buildx
 make -j${nprocs}
 make install
@@ -56,10 +55,16 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("x86_64", "linux"; libc="glibc"),
-]
+platforms = [   
+                Platform("i686", "linux"; libc="glibc"),
+                Platform("x86_64", "linux"; libc="glibc"),
+                Platform("i686", "linux"; libc="musl"),
+                Platform("x86_64", "linux"; libc="musl"),
+    
+            ]
 
+platforms = expand_cxxstring_abis(platforms)
+platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -71,12 +76,14 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="SuiteSparse_jll", uuid="bea87d4a-7f5b-5778-9afe-8cc45184846c"))
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
-    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
-    Dependency(PackageSpec(name="Gettext_jll", uuid="78b55507-aeef-58d4-861c-77aaff3498b1"))
-    Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
-]
+                    Dependency(PackageSpec(name="Trilinos_jll", uuid="b6fd3212-6f87-5999-b9ea-021e9cd21b17"))
+                    Dependency(PackageSpec(name="SuiteSparse_jll", uuid="bea87d4a-7f5b-5778-9afe-8cc45184846c"))
+                    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
+                    Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
+                    Dependency(PackageSpec(name="Help2man_jll", uuid="b065d96c-5f5a-5e02-95cc-818e6b3b761f"))
+                    Dependency(PackageSpec(name="Gettext_jll", uuid="78b55507-aeef-58d4-861c-77aaff3498b1"))
+                    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+                ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"6.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"7.1.0")
