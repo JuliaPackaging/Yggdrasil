aa054d2e5b7f7a8ea5b8ce6582fe6f8df9d497fa
Venkateshprasad <vensadk@gmail.com>
2021-03-06 12:39:36 +0530

    no more 'flex'ing [as a source]

--- X/Xyce/build_tarballs.jl
+++ X/Xyce/build_tarballs.jl
@@ -8,20 +8,13 @@ version = v"7.2.0"
 # Collection of sources required to complete build
 sources = [
             GitSource("https://github.com/Xyce/Xyce.git", "a61faef4bfb2f36f1aa7cc44264bbbb66fbaac11"),
-            GitSource("https://github.com/westes/flex.git", "d69a58075169410324fe49666f6641ba6a9d1f91"),
             DirectorySource("./bundled")
           ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd flex
-apk add texinfo
-./autogen.sh
-./configure --prefix=${prefix} --host=${target}
-make -j${nprocs}
-make install
-cd ..
+apk add flex-dev
 cd Xyce
 if [[ $target != *gnu ]] ; then
         atomic_patch -p1 ${WORKSPACE}/srcdir/patches/cross.patch
@@ -30,7 +23,7 @@ fi
 cd ..
 mkdir buildx
 cd buildx
-/workspace/srcdir/Xyce/./configure --enable-shared --disable-mpi --prefix=${prefix} LDFLAGS="-L${libdir} -lopenblas" CPPFLAGS="-I/$prefix/include" --host=${target}
+/workspace/srcdir/Xyce/./configure --enable-shared --disable-mpi --prefix=${prefix} LDFLAGS="-L${libdir} -lopenblas" CPPFLAGS="-I/$prefix/include -I/usr/include" --host=${target}
 make -j${nprocs}
 make install
 """
@@ -38,10 +31,7 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
    
-platforms = filter(p -> (!Sys.iswindows(p) &&
-                         !Sys.isapple(p) &&
-                         !Sys.isfreebsd(p) &&
-                         arch(p) ∉ ("armv7l", "powerpc64le", "aarch64")),
+platforms = filter(p -> (!Sys.iswindows(p) && !Sys.isapple(p)),
                    supported_platforms())
     
 platforms = expand_cxxstring_abis(platforms)
@@ -61,8 +51,6 @@ dependencies = [
                     Dependency(PackageSpec(name="SuiteSparse_jll", uuid="bea87d4a-7f5b-5778-9afe-8cc45184846c"))
                     Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
                     Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
-                    Dependency(PackageSpec(name="Help2man_jll", uuid="b065d96c-5f5a-5e02-95cc-818e6b3b761f"))
-                    Dependency(PackageSpec(name="Gettext_jll", uuid="78b55507-aeef-58d4-861c-77aaff3498b1"))
                     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
                 ]
 
