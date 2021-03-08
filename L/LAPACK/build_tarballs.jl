d1ed561aae143f4f11c2202ca685a62e615202aa
Viral B. Shah <ViralBShah@users.noreply.github.com>
2021-03-07 19:03:09 -0500

    Update build_tarballs.jl

--- L/LAPACK/build_tarballs.jl
+++ L/LAPACK/build_tarballs.jl
@@ -36,6 +36,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
         Dependency("CompilerSupportLibraries_jll")
+        Dependency("libblastrampoline_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
