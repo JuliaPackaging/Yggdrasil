5e30bcdec3a17c3bf94d9b791cb16523d51abf8a
Rik Huijzer <rikhuijzer@pm.me>
2021-03-16 17:03:19 +0100

    Copy hash from CI

--- P/pandoc/build_tarballs.jl
+++ P/pandoc/build_tarballs.jl
@@ -11,7 +11,7 @@ sources = [
     ArchiveSource("$(url_prefix)-macOS.zip", "13b8597860afa6ab802993a684b340be3f31f4d2a06c50b6601f9e726cf76f71"; unpack_target = "x86_64-apple-darwin14"),
     ArchiveSource("$(url_prefix)-windows-x86_64.zip", "ee1b0c4d0f539ee8316d6cebb29f6aa709aa3a72be2b2b4ab6e9e4a77a01a50b"; unpack_target = "x86_64-w64-mingw32"),
     FileSource("https://raw.githubusercontent.com/jgm/pandoc/54d8c6959cc53e78cfea2093b33504672f81ed74/COPYING.md", "2b0d4dda4bf8034e1506507a67f80f982131137afe62bf144d248f9faea31da4"),
-    FileSource("https://raw.githubusercontent.com/jgm/pandoc/54d8c6959cc53e78cfea2093b33504672f81ed74/COPYRIGHT", "39be98cc4d2906dd44abf8573ab91557e0b6d51f503d3a889dab0e8bcca1c43f"),
+    FileSource("https://raw.githubusercontent.com/jgm/pandoc/54d8c6959cc53e78cfea2093b33504672f81ed74/COPYRIGHT", "7087ebeaa4c3131b7a989ea4310da1442610e9e2874c3bc9d3e8a52d618fd97a"),
 ]
 
 # Bash recipe for building across all platforms
