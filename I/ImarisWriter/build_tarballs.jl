6feebd7c03cba369236b12af3f915b14cbc83fae
Mark Kittisopikul <markkitt@gmail.com>
2021-03-22 01:49:01 -0400

    ImarisWriter: Add keyword skip=Sys.isbsd to expand_cxxstring_abis

--- I/ImarisWriter/build_tarballs.jl
+++ I/ImarisWriter/build_tarballs.jl
@@ -33,7 +33,7 @@ platforms = [
     Windows(:i686),
     Windows(:x86_64)
 ]
-platforms = expand_cxxstring_abis(platforms)
+platforms = expand_cxxstring_abis(platforms; skip=Sys.isbsd)
 
 
 # The products that we will ensure are always built
