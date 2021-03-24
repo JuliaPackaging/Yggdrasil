c47bfa98144803ed169cd41e7e061bbc212c8bfd
Keno Fischer <keno@juliacomputing.com>
2021-03-24 15:08:16 -0400

    Upgrade ngspice to ngspice-34 (#2716)

--- N/ngspice/build_tarballs.jl
+++ N/ngspice/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "ngspice"
-version = v"32.2.0"
+version = v"34"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/imr/ngspice.git", "a0389bb5471a43bc828b08b81bd3fc1693200130"),
+    GitSource("https://github.com/imr/ngspice.git", "279edff5d9877b13ddd7708285553ea20d6f14cd"),
     DirectorySource("./bundled")
 ]
 
@@ -43,4 +43,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies, preferred_gcc_version=v"8")
