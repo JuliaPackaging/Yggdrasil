140037ee976b0a4faabef054b7b0d262dbfee178
Alex Arslan <ararslan@comcast.net>
2021-03-24 09:30:08 -0700

    Add AMICA

---
+++ A/AMICA/build_tarballs.jl
@@ -0,0 +1,27 @@
+using BinaryBuilder
+
+name = "AMICA"
+version = v"2021.3.24"  # AMICA has no tagged versions
+
+sources = [
+    GitSource("https://github.com/japalmer29/amica.git",
+              "b4328096805826c3fcc228ca71561027968b10ae"),
+]
+
+script = raw"""
+mkdir -p ${bindir}
+cd ${WORKSPACE}/srcdir/amica*
+mpif90 -qopenmp -O3 -fpp amica15.f90 funmod2.f90 -o ${bindir}/amica
+"""
+
+platforms = supported_platforms()
+
+products = [
+    ExecutableProduct("amica", :amica),
+]
+
+dependencies = [
+    Dependency("MPICH_jll"),
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
