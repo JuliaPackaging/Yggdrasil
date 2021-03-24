3850d9e0db48eaed5ed1ba686cf16bf7c96f3bb1
John Brennan <john.brennan@ichec.ie>
2021-03-24 19:02:17 +0000

    Add flow cutter tarballs build script

---
+++ F/FlowCutterPACE17/build_tarballs.jl
@@ -0,0 +1,39 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "FlowCutterPACE17"
+version = v"0.0.1"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/kit-algo/flow-cutter-pace17.git", "fd405a2b003c9a93838e0488728addc0610a769f")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd flow-cutter-pace17/
+$CXX -c -Wall -std=c++11 -O3 -DNDEBUG src/*.cpp
+$CXX -o flow_cutter_pace17 *.o
+install_license LICENSE
+mkdir $bindir 
+mv flow_cutter_pace17 $bindir/flow_cutter_pace17
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+platforms = expand_cxxstring_abis(platforms)
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("flow_cutter_pace17", :flow_cutter_pace17)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
