cc2fc26bb4f03250ef1a2d145b626ba48d9a8da6
bramtayl <brandon.taylor221@gmail.com>
2021-03-28 18:08:21 -0400

    Add plugin dir

--- A/alsa/build_tarballs.jl
+++ A/alsa/build_tarballs.jl
@@ -14,7 +14,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/alsa-lib*/
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-plugindir='\$ORIGIN/alsa-lib'
 make
 make install
 """
