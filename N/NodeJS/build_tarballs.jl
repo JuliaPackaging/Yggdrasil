271b21874cab0a1eef4b5595407925ba652fda75
Mike <michaelhatherly@gmail.com>
2021-03-29 10:35:24 +0100

    Try split windows from other platforms.

--- N/NodeJS/build_tarballs.jl
+++ N/NodeJS/build_tarballs.jl
@@ -9,6 +9,8 @@ sources = [
     ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "2b78771550f8a3e6e990d8e60e9ade82c7a9e2738b6222e92198bcd5ea857ea6"; unpack_target = "aarch64-linux-gnu"),
     ArchiveSource("$(url_prefix)-linux-ppc64le.tar.gz", "2339b4b1a8db39348cb1877b0cfdee3b2ef2b730f461ef7263610cbaaea5232a"; unpack_target = "powerpc64le-linux-gnu"),
     ArchiveSource("$(url_prefix)-darwin-x64.tar.gz", "14ec767e376d1e2e668f997065926c5c0086ec46516d1d45918af8ae05bd4583"; unpack_target = "x86_64-apple-darwin14"),
+]
+win_sources = [
     ArchiveSource("$(url_prefix)-win-x64.zip", "716045c2f16ea10ca97bd04cf2e5ef865f9c4d6d677a9bc25e2ea522b594af4f"; unpack_target = "x86_64-w64-mingw32"),
     ArchiveSource("$(url_prefix)-win-x86.zip", "9699067581e0d333b13158d4ebb27b6357444564548aaa220d821cdc6d840bd2"; unpack_target = "i686-w64-mingw32"),
 ]
@@ -17,12 +19,11 @@ sources = [
 script = raw"""
 cd ${WORKSPACE}/srcdir/
 cp -r ${target}/*/* ${prefix}/.
+cd ${prefix}
 if [[ "${target}" == *-mingw* ]]; then
-    mkdir ${prefix}/bin
-    mv ${prefix}/node.exe ${prefix}/bin
-    chmod +x ${prefix}/bin/node.exe
+    chmod +x {node.exe,npm,npm.cmd,npx,npx.cmd}
 fi
-install_license ${prefix}/LICENSE
+install_license LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
@@ -32,6 +33,8 @@ platforms = [
     Platform("aarch64", "linux"),
     Platform("powerpc64le", "linux"),
     Platform("x86_64", "macos"),
+]
+win_platforms =  [
     Platform("x86_64", "windows"),
     Platform("i686", "windows"),
 ]
@@ -39,11 +42,23 @@ platforms = [
 # The products that we will ensure are always built
 products = [
     ExecutableProduct("node", :node),
+    ExecutableProduct("npm", :npm),
+    ExecutableProduct("npx", :npx),
+]
+win_products = [
+    ExecutableProduct("node", :node, "."),
+    ExecutableProduct("npm", :npm, "."),
+    ExecutableProduct("npx", :npx, "."),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
 ]
 
+include("../../fancy_toys.jl")
+
 # Build the tarballs, and possibly a `build.jl` as well.
+if should_build_platform("x86_64-w64-mingw32") || should_build_platform("i686-w64-mingw32")
+    build_tarballs(ARGS, name, version, win_sources, script, win_platforms, win_products, dependencies)
+end
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
