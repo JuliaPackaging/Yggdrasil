961ce8422f44666bb01482e5aca2fe2aa4b8ed3d
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-04-02 15:37:09 -0400

    Remove Windows platforms

--- S/StarPU/build_tarballs.jl
+++ S/StarPU/build_tarballs.jl
@@ -24,7 +24,7 @@ install_license ../COPYING.LGPL
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_gfortran_versions(supported_platforms())
+platforms = expand_gfortran_versions(filter!(!Sys.iswindows, supported_platforms()))
 
 
 # The products that we will ensure are always built
