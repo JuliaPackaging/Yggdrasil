8f0933740d8bc4b6296d7e7c1ebbc06431fd6609
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-04-04 07:01:07 -0400

    libsodium + M1, take I (#2768)

--- L/libsodium/build_tarballs.jl
+++ L/libsodium/build_tarballs.jl
@@ -6,7 +6,7 @@ name = "libsodium"
 # Note: upstream stopped giving version numbers to new releases.  "Releases" are
 # commits to the "stable" branch.  Here we just invent new versions numbers
 # because we need having different versions.
-version = v"1.0.19"
+version = v"1.0.20"
 
 # Collection of sources required to complete build
 sources = [
@@ -25,7 +25,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -37,4 +37,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat = "1.6")
