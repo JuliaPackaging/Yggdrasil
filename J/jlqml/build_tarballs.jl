6dbfd55ae27773f3e51dc01ba11355e490d017ee
Bart Janssens <bart@bartjanssens.org>
2021-04-11 00:04:35 +0200

    [jlqml] Version 0.2

--- J/jlqml/build_tarballs.jl
+++ J/jlqml/build_tarballs.jl
@@ -2,14 +2,14 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-julia_version = v"1.5.3"
+julia_version = v"1.6.0"
 
 name = "jlqml"
-version = v"0.1.5"
+version = v"0.2"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/barche/jlqml.git", "a36226c77ad712611ae142b9a5855e33eaddbb53"),
+    GitSource("https://github.com/barche/jlqml.git", "4bc5d39bdc261eff12e3b7d1cd8b28ff0519e316"),
 ]
 
 # Bash recipe for building across all platforms
@@ -57,12 +57,13 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("libcxxwrap_julia_jll"),
-    Dependency("Qt_jll"),
+    Dependency("Qt5Declarative_jll"),
+    Dependency("Qt5Svg_jll"),
     BuildDependency("Libglvnd_jll"),
     BuildDependency(PackageSpec(name="libjulia_jll", version=julia_version)),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-    preferred_gcc_version=v"8",
+    preferred_gcc_version=v"9",
     julia_compat = "$(julia_version.major).$(julia_version.minor)")
