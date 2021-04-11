eada6c6bf07f4003b0b82577d5dfc618753641a3
Wimmerer <wrkimmerer@outlook.com>
2021-04-11 00:23:44 -0400

    First pass, no build on powerpc linux

---
+++ N/npth/build_tarballs.jl
@@ -0,0 +1,40 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "npth"
+version = v"1.6"
+
+# Collection of sources required to build libgcrypt
+sources = [
+    ArchiveSource("https://gnupg.org/ftp/gcrypt/npth/npth-1.6.tar.bz2",
+                  "1393abd9adcf0762d34798dc34fdcf4d0d22a8410721e76f1e3afcd1daa4e2d1"),
+]
+
+# Bash recipe for building across all platforms
+
+# Tried -no-undefined but still couldn't build for windows
+script = raw"""
+cd $WORKSPACE/srcdir/npth-*/
+./configure --prefix=${prefix} --host=${target} --build=${MACHTYPE}
+make -j${nproc}
+make install
+install_license ${WORKSPACE}/srcdir/npth-*/COPYING.LIB
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line.  We are manually disabling
+# many platforms that do not seem to work.
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libnpth", :libnpth),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
