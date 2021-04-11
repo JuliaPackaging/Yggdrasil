b17ad194aef270c750a95eab4a9b6f8d6676d0f6
Wimmerer <wrkimmerer@outlook.com>
2021-04-11 16:16:59 -0400

    Properly cased name

--- N/nPth/build_tarballs.jl
+++ N/nPth/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "npth"
+name = "nPth"
 version = v"1.6"
 
 # Collection of sources required to build libgcrypt
