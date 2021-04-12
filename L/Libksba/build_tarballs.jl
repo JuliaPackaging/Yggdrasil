58ec8a47da9442dff04b1f9bcbcc6552f93226d0
Mosè Giordano <mose@gnu.org>
2021-04-12 22:56:54 +0100

    [Libksba] More properly build the shared library for Windows

--- L/Libksba/build_tarballs.jl
+++ L/Libksba/build_tarballs.jl
@@ -9,6 +9,7 @@ version = v"1.5.1"
 sources = [
     ArchiveSource("https://gnupg.org/ftp/gcrypt/libksba/libksba-$(version).tar.bz2",
                   "b0f4c65e4e447d9a2349f6b8c0e77a28be9531e4548ba02c545d1f46dc7bf921"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
@@ -16,15 +17,16 @@ sources = [
 # Tried -no-undefined but still couldn't build for windows
 script = raw"""
 cd $WORKSPACE/srcdir/libksba-*/
+if [[ "${target}" == x86_64-*-mingw* ]]; then
+    # `gpgrt-config` for this platform returns garbage results.  We replace it with
+    # a simple wrapper around `pkg-config`, so that we can easily build the shared library.
+    FLAGS=(GPG_ERROR_CONFIG="../gpgrt-config.sh" ac_cv_path_GPGRT_CONFIG="../gpgrt-config.sh")
+fi
 export CPPFLAGS="-I${includedir}"
-./configure --prefix=${prefix} --host=${target} --build=${MACHTYPE} --disable-static
+./configure --prefix=${prefix} --host=${target} --build=${MACHTYPE} "${FLAGS[@]}"
 make -j${nproc}
 make install
-if [[ "${target}" == x86_64-*-mingw* ]]; then
-    # We have to manually build the shared library for Windows
-    cc -shared -fPIC -o "${libdir}/libksba-8.${dlext}" -Wl,$(flagon --whole-archive) "${prefix}/lib/libksba.a" -Wl,$(flagon --no-whole-archive)  -lgpg-error
-    rm "${prefix}/lib/libksba.a"
-fi
+install_license COPYING*
 """
 
 # These are the platforms we will build for by default, unless further
