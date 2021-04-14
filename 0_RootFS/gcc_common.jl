32b851b1a09445e4f92d5af4212cb8cc3867ea17
Elliot Saba <staticfloat@gmail.com>
2021-04-14 03:18:29 +0000

    Manually disable `clock_gettime` in GCC builds for Darwin
    GCC on Darwin can't properly deal with certain feature detection macros,
    so we'll help it out by manually disabling them for it.

--- 0_RootFS/gcc_common.jl
+++ 0_RootFS/gcc_common.jl
@@ -357,6 +357,9 @@ function gcc_script(compiler_target::Platform)
 
         # On darwin, cilk doesn't build on 5.X-7.X.  :(
         export enable_libcilkrts=no
+
+        # GCC doesn't know how to use availability macros properly, so tell it not to use `clock_gettime()`
+        export ac_cv_func_clock_gettime=no
     fi
 
     # Link dependent packages into gcc build root:
