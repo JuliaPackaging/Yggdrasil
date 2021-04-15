ec8e7018830d69f1d151393c81a7007b162990a5
Morten Piibeleht <morten.piibeleht@gmail.com>
2021-04-15 12:33:06 +1200

    Try building HelFEM for 1.6

---
+++ H/HelFEM/HelFEM@1.6/build_tarballs.jl
@@ -0,0 +1,3 @@
+version = v"0.1.6"
+julia_version = v"1.6.0"
+include("../common.jl")
--- H/HelFEM/common.jl
+++ H/HelFEM/common.jl
@@ -1,9 +1,6 @@
 using BinaryBuilder, Pkg
 
-julia_version = v"1.5.3"
-
 name = "HelFEM"
-version = v"0.1.5"
 sources = [
     GitSource("https://github.com/mortenpi/HelFEM.git", "a4d3b2e6f16f7f7953afd5f69a44257a65c5b131"),
 ]
