050d53daf01a0e0bae7d6b023202f8da1eb135f6
Morten Piibeleht <morten.piibeleht@gmail.com>
2021-04-15 13:57:45 +1200

    Whoops

--- H/HelFEM/common.jl
+++ H/HelFEM/common.jl
@@ -62,9 +62,9 @@ products = [
 dependencies = [
     BuildDependency(PackageSpec(name="libjulia_jll", version=julia_version)),
     Dependency("libcxxwrap_julia_jll"),
-    Dependency(PackageSpec(name = "armadillo_jll", compat = "9.850.1")),
-    Dependency(PackageSpec(name = "GSL_jll", compat = "2.6.0")),
-    Dependency(PackageSpec(name = "OpenBLAS_jll", compat = "0.3.9")),
+    Dependency("armadillo_jll", compat = "9.850.1"),
+    Dependency("GSL_jll", compat = "2.6.0"),
+    Dependency("OpenBLAS_jll", compat = "0.3.9"),
 ]
 
 # preferred_gcc_version = v"8" is a requirement from libcxxwrap_julia_jll
