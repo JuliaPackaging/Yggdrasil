34c11e705d7a014090e99680dac4c2e136db249c
Bart Janssens <bart@bartjanssens.org>
2021-04-18 03:47:06 +0200

    [Qt] Qt6Base builder for all platforms (#2856)
    * [Qt] Qt6Base builder for all platforms
    
    * Update Q/Qt6Base/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- Q/Qt6Base/build_tarballs.jl
+++ Q/Qt6Base/build_tarballs.jl
@@ -1,6 +1,6 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "Qt6Base"
 version = v"6.0.3"
@@ -9,8 +9,8 @@ version = v"6.0.3"
 sources = [
     ArchiveSource("https://download.qt.io/official_releases/qt/$(version.major).$(version.minor)/$version/submodules/qtbase-everywhere-src-$version.tar.xz",
                   "1a45b61c2a349964625c50e3ea40cbb309e269762dd0786397e0e18e7e10d394"),
-    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
-                  "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f")
+    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/11.0-11.1/MacOSX11.1.sdk.tar.xz",
+                  "9b86eab03176c56bb526de30daa50fa819937c54b280364784ce431885341bf6")
 ]
 
 script = raw"""
@@ -28,31 +28,57 @@ commonoptions=" \
 -openssl-linked  -nomake examples -release \
 "
 
+commoncmakeoptions="-DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DQT_HOST_PATH=$host_prefix -DQT_FEATURE_openssl_linked=ON"
+
+export LD_LIBRARY_PATH=$host_libdir:$LD_LIBRARY_PATH
 export OPENSSL_LIBS="-L${libdir} -lssl -lcrypto"
 
 sed -i 's/-march=haswell/-mavx2/' $qtsrcdir/cmake/QtCompilerOptimization.cmake
+sed -i 's/.*Ws2_32 Crypt32.*//' $qtsrcdir/cmake/FindWrapOpenSSL.cmake
+sed -i "s!Qt::GuiPrivate!Qt::GuiPrivate $libdir/clang/11.0.1/lib/darwin/libclang_rt.osx.a!" $qtsrcdir/src/plugins/platforms/cocoa/CMakeLists.txt
 
 case "$target" in
 
     x86_64-linux-musl*)
         ../qtbase-everywhere-src-*/configure -prefix $prefix $commonoptions -fontconfig -- -DCMAKE_PREFIX_PATH=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_HOST_TOOLCHAIN}
     ;;
+
+    *mingw*)
+        ../qtbase-everywhere-src-*/configure -prefix $prefix $commonoptions -opengl dynamic -- $commoncmakeoptions
+    ;;
+
+    *apple-darwin*)
+        # Install 11.1 SDK
+        cd $WORKSPACE/srcdir/MacOSX11.1.sdk
+        rm -rf /opt/$target/$target/sys-root/System
+        rm -rf /opt/$target/$target/sys-root/usr/include/libxml2/
+        rsync -a usr/* /opt/$target/$target/sys-root/usr/
+        cp -a System /opt/$target/$target/sys-root/
+        cd $WORKSPACE/srcdir/build
+
+        ../qtbase-everywhere-src-*/configure -prefix $prefix $commonoptions -- $commoncmakeoptions -DCMAKE_FRAMEWORK_PATH=/opt/$target/$target/sys-root/System/Library/Frameworks -DCMAKE_OSX_DEPLOYMENT_TARGET=10.14 -DCUPS_INCLUDE_DIR=/opt/$target/$target/sys-root/usr/include -DCUPS_LIBRARIES=/opt/$target/$target/sys-root/usr/lib/libcups.tbd
+    ;;
+
+    *freebsd*)
+        sed -i 's/-Wl,--no-undefined//' $qtsrcdir/mkspecs/freebsd-clang/qmake.conf
+        sed -i 's/-Wl,--no-undefined//' $qtsrcdir/cmake/QtFlagHandlingHelpers.cmake
+        ../qtbase-everywhere-src-*/configure -prefix $prefix $commonoptions -fontconfig -- $commoncmakeoptions -DQT_PLATFORM_DEFINITION_DIR=$host_prefix/mkspecs/freebsd-clang
+    ;;
+
+    *)
+        ../qtbase-everywhere-src-*/configure -prefix $prefix $commonoptions -fontconfig -- $commoncmakeoptions
+    ;;
 esac
 
-cmake --build . --parallel
+cmake --build . --parallel ${nproc}
 cmake --install .
-$BUILD_STRIP $bindir/qmake
 install_license $WORKSPACE/srcdir/qtbase-everywhere-src-*/LICENSE.LGPLv3
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms_linux = [
-    Platform("x86_64", "linux"; libc="musl"),
-]
-platforms_linux = expand_cxxstring_abis(platforms_linux)
-# platforms_win = expand_cxxstring_abis([Platform("x86_64", "windows"), Platform("i686", "windows")])
-# platforms_macos = [ Platform("x86_64", "macos") ]
+platforms = expand_cxxstring_abis(filter(!Sys.isapple, supported_platforms()))
+platforms_macos = [ Platform("x86_64", "macos") ]
 
 # The products that we will ensure are always built
 products = [
@@ -85,6 +111,7 @@ products_macos = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    HostBuildDependency("Qt6Base_jll"),
     BuildDependency("Xorg_libX11_jll"),
     Dependency("Xorg_libXext_jll"),
     BuildDependency("Xorg_xproto_jll"),
@@ -104,16 +131,14 @@ dependencies = [
     Dependency("Zlib_jll"),
     Dependency("CompilerSupportLibraries_jll"),
     Dependency("OpenSSL_jll"),
+    BuildDependency(PackageSpec(name="LLVM_full_jll", version=v"11.0.1")),
 ]
 
 include("../../fancy_toys.jl")
 
-if any(should_build_platform.(triplet.(platforms_linux)))
-    build_tarballs(ARGS, name, version, sources, script, platforms_linux, products, dependencies; preferred_gcc_version = v"8")
+if any(should_build_platform.(triplet.(platforms_macos)))
+    build_tarballs(ARGS, name, version, sources, script, platforms_macos, products_macos, dependencies; preferred_gcc_version = v"8", julia_compat="1.6")
 end
-# if any(should_build_platform.(triplet.(platforms_win)))
-#     build_tarballs(ARGS, name, version, sources, script, platforms_win, products, dependencies; preferred_gcc_version = v"8")
-# end
-# if any(should_build_platform.(triplet.(platforms_macos)))
-#     build_tarballs(ARGS, name, version, sources, script, platforms_macos, products_macos, dependencies; preferred_gcc_version = v"8")
-# end
+if any(should_build_platform.(triplet.(platforms)))
+    build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8", julia_compat="1.6")
+end
