645bce883d99108a88e9507b78d7f78025bd2e5d
bzinberg <bzinberg@mit.edu>
2021-04-21 18:44:07 -0400

    [lrslib] Add LibraryProduct for `liblrsnash` (#2862)
    * Add LibraryProduct for `liblrsnash`
    
    * instead of outputting here and copying to there, just output there
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * use GCC toolchain in apple and freebsd platforms
    
    First attempt to duct-tape https://github.com/JuliaPackaging/Yggdrasil/pull/2862#issuecomment-822892005
    
    * don't refer to $LDFLAGS, it's not set
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/lrslib/build_tarballs.jl
+++ L/lrslib/build_tarballs.jl
@@ -16,7 +16,7 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/lrslib*
 extraargs=""
-cflags="-O3 -Wall"
+cflags="-fPIC -O3 -Wall"
 
 # 32bit linux, arm and windows:
 if [[ $target == i686* ]] || [[ $target == arm* ]]; then
@@ -25,10 +25,11 @@ if [[ $target == i686* ]] || [[ $target == arm* ]]; then
 fi
 
 if [[ $target == *apple* ]]; then
+  export CC=gcc
   sed -i -e 's#-Wl,-soname=#-install_name #' makefile
   extraargs="SONAME=liblrs.0.dylib SHLINK=liblrs.dylib SHLIB=liblrs.0.0.0.dylib"
 elif [[ $target == *freebsd* ]]; then
-  export CC="$CC $LDFLAGS"
+  export CC="gcc"
 elif [[ $target == *mingw* ]]; then
   extraargs="SONAME=liblrs-0.dll SHLINK=liblrs.dll SHLIB=liblrs-0-0-0.dll"
   cflags="$cflags -DSIGNALS -DTIMES"
@@ -40,6 +41,8 @@ if [[ $target == *mingw* ]]; then
   for file in ${bindir}/{lrs,lrsnash,redund}; do mv $file $file.exe; done
   mv ${prefix}/lib/*lrs*.dll ${libdir}/
 fi
+
+${CC} -shared ${cflags} -o "${libdir}/liblrsnash.${dlext}" lrsnashlib.c -L${libdir} -llrs -lgmp -Wl,-rpath,${libdir} -DMA -DGMP -DLRS_QUIET -I${includedir}
 """
 
 # These are the platforms we will build for by default, unless further
@@ -52,6 +55,7 @@ products = [
     ExecutableProduct("lrsnash", :lrsnash)
     ExecutableProduct("redund", :redund)
     LibraryProduct("liblrs", :liblrs)
+    LibraryProduct("liblrsnash", :liblrsnash)
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -61,4 +65,3 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
-
