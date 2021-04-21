1a4f2398986662ceed0c15e5979a28ba68186bd3
bramtayl <brandon.taylor221@gmail.com>
2021-04-21 13:17:53 -0400

    Update build_tarballs.jl

--- A/alsa/build_tarballs.jl
+++ A/alsa/build_tarballs.jl
@@ -9,11 +9,14 @@ version = v"1.2.4"
 sources = [
     ArchiveSource("https://www.alsa-project.org/files/pub/lib/alsa-lib-$version.tar.bz2",
                   "f7554be1a56cdff468b58fc1c29b95b64864c590038dd309c7a978c7116908f7"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/alsa-lib*/
+# TODO: remove for version >= 1.2.5
+atomic_patch -p1 $WORKSPACE/srcdir/patches/plugin_dir_no_DL.patch
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make
 make install
