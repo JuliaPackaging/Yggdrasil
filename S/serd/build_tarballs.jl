fda0719fa160223027aea9558d093dd82c8d594b
Evan Patterson <evan@epatters.org>
2021-04-22 10:06:01 -0700

    Fix name of Serd library product.
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- S/serd/build_tarballs.jl
+++ S/serd/build_tarballs.jl
@@ -40,7 +40,7 @@ platforms = [
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libserd", :libserd)
+    LibraryProduct("libserd-0", :libserd)
 ]
 
 # Dependencies that must be installed before this package can be built
