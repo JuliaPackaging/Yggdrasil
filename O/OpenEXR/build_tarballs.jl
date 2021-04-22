693fb7d73c71c1dd415a7d7af2f1bd88963a8ac7
Tracy Wadleigh <tracy@compoundeye.com>
2021-04-22 09:43:00 -0700

    [OpenEXR] new package (#2879)
    * Add OpenEXR libraries.
    
    * Added Imath_jll as dependency.

---
+++ O/OpenEXR/build_tarballs.jl
@@ -0,0 +1,44 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "OpenEXR"
+version = v"3.0.1"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/AcademySoftwareFoundation/openexr.git", "4a84390282584957ffaf1697c411f9ae7e420f2f")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+mkdir build
+cd build
+cmake -DOPENEXR_INSTALL_EXAMPLES=OFF -DOPENEXR_INSTALL_TOOLS=OFF -DOPENEXR_BUILD_UTILS=OFF -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ../openexr/
+make -j
+make install
+install_license ../openexr/LICENSE.md 
+exit
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_cxxstring_abis(supported_platforms())
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libOpenEXRUtil-3_0", :libOpenEXRUtil),
+    LibraryProduct("libOpenEXR-3_0", :libOpenEXR),
+    LibraryProduct("libIlmThread-3_0", :libIlmThread),
+    LibraryProduct("libIex-3_0", :libIex)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="Imath_jll", uuid="905a6f67-0a94-5f89-b386-d35d92009cd1")),
+    Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"7.1.0")
