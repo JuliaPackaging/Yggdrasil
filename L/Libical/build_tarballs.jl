a7a04e61b86497829436e0f4271ba1253af3c78f
bramtayl <brandon.taylor221@gmail.com>
2021-04-22 14:33:47 -0400

    Reset LDFLAGS

--- L/Libical/build_tarballs.jl
+++ L/Libical/build_tarballs.jl
@@ -54,6 +54,7 @@ if [[ "${target}" == *-linux-* ]]; then
     fi
 fi
 
+export LDFLAGS="-L${libdir}"
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
