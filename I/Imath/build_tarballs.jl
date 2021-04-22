387cc19d64f48eeebaa847ace751b8f43513d8eb
Tracy Wadleigh <tracy@compoundeye.com>
2021-04-21 22:28:29 -0700

    Add Imath library.

---
+++ I/Imath/build_tarballs.jl
@@ -0,0 +1,39 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Imath"
+version = v"3.0.1"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/AcademySoftwareFoundation/Imath.git", "73c2cdfcaf2a22880ddf42a866ebd4614d424410")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+mkdir build
+cd build/
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ../Imath
+make -j
+make install
+install_license ../Imath/LICENSE.md 
+exit
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libImath-3_0", :libImath)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"7.1.0")
