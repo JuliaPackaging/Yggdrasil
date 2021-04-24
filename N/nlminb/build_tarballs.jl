65dd449bf2f3139248190b5fb5c4c06847b2090c
Dongdong Kong <kongdd@users.noreply.github.com>
2021-04-25 07:50:06 +0800

    use openblas
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- N/nlminb/build_tarballs.jl
+++ N/nlminb/build_tarballs.jl
@@ -9,40 +9,26 @@ sources = [
               "6b2d460b866911e8bfca32b1217c08fb78c3111f"), # v0.1.0
 ]
 
-# copied from SCALAPACK
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/nlminb.f
-cd lib && tar -xzf blas-3.8.0.tgz \
-    && cd BLAS-3.8.0 && make \
-    && cd ../.. 
 
 ## compile by hand
-if [[ ${target} == *aarch64* ]] || [[ ${target} == *arm* ]]; then
-    FLAGS=""
-else
+if [[ "${proc_family}" == "intel" ]]; then
     FLAGS="-mfpmath=sse -msse2 -mstackrealign"
 fi
-CCFLAGS="-fPIC -DNDEBUG  -Iinclude -O2 -Wall -std=gnu99 ${FLAGS}"
-FCFLAGS="-fPIC -fno-optimize-sibling-calls -O2 ${FLAGS}"
+CFLAGS="-fPIC -DNDEBUG  -Iinclude -O2 -Wall -std=gnu99 ${FLAGS}"
+FFLAGS="-fPIC -fno-optimize-sibling-calls -O2 ${FLAGS}"
 
-gfortran ${FCFLAGS} -c src/portsrc.f -o src/portsrc.o 
-gfortran ${FCFLAGS} -c src/d1mach.f -o src/d1mach.o 
-gcc ${CCFLAGS} -c src/port2.c -o src/port.o
-gfortran -shared -static-libgcc -lm -o ${libdir}/libnlminb.${dlext} src/portsrc.o src/d1mach.o src/port.o lib/BLAS-3.8.0/blas_LINUX.a
+gfortran ${FFLAGS} -c src/portsrc.f -o src/portsrc.o 
+gfortran ${FFLAGS} -c src/d1mach.f -o src/d1mach.o 
+gcc ${CFLAGS} -c src/port2.c -o src/port.o
+gfortran -shared -static-libgcc -lm -o ${libdir}/libnlminb.${dlext} src/portsrc.o src/d1mach.o src/port.o -lopenblas
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc="glibc"),
-    Platform("x86_64", "macos"),
-    Platform("x86_64", "windows"),
-    Platform("i686", "windows"),
-]
-platforms = expand_gfortran_versions(platforms)
-platforms = platforms[2:end]
+platforms = expand_gfortran_versions(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
@@ -51,7 +37,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("CompilerSupportLibraries_jll")
+    Dependency("CompilerSupportLibraries_jll"),
+    Dependency("OpenBLAS32_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
