fab001485ff9bdd7b0a3bf89ffd924684c9caa4b
lizz <innerlee@users.noreply.github.com>
2021-04-25 12:26:12 +0800

    Update build_tarballs.jl

--- Z/zimg/build_tarballs.jl
+++ Z/zimg/build_tarballs.jl
@@ -22,7 +22,7 @@ install_license COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(;experimental=true)
+platforms = supported_platforms()
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
