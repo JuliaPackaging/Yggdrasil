ba59dd47b2563766e631daef851e72c88f7d64e2
lizz <innerlee@users.noreply.github.com>
2021-04-25 23:48:55 +0800

    Update N/nasm/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- N/nasm/build_tarballs.jl
+++ N/nasm/build_tarballs.jl
@@ -22,7 +22,7 @@ install_license LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
