5700543ad8c34c421220c124824458ce51e37a07
lizz <innerlee@users.noreply.github.com>
2021-04-25 23:43:27 +0800

    Update Z/zimg/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- Z/zimg/build_tarballs.jl
+++ Z/zimg/build_tarballs.jl
@@ -22,7 +22,7 @@ install_license COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter!(p -> arch(p) != "aarch64", supported_platforms())
+platforms = filter!(p -> arch(p) != "aarch64", supported_platforms(; experimental=true))
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
