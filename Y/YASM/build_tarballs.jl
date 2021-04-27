c142464d0866acd9e1e9e908e057cf4ba4b6cfb7
lizz <innerlee@users.noreply.github.com>
2021-04-27 13:22:54 +0800

    Update build_tarballs.jl

--- Y/YASM/build_tarballs.jl
+++ Y/YASM/build_tarballs.jl
@@ -12,6 +12,11 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
+# Set BINARY=32 on i686 platforms and armv7l
+if [[ ${nbits} == 32 ]]; then
+    flags="${flags} BINARY=32"
+fi
+
 cd $WORKSPACE/srcdir/yasm-*
 autoreconf -f -i
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
