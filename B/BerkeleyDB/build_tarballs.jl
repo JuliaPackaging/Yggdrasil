c8cbb40a5e5413c867e23f45ad3b753c9898623a
bramtayl <brandon.taylor221@gmail.com>
2021-04-27 23:29:33 -0400

    Exclude problematic platforms

--- B/BerkeleyDB/build_tarballs.jl
+++ B/BerkeleyDB/build_tarballs.jl
@@ -24,8 +24,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-
+platforms = filter!(platform -> !(Sys.iswindows(platform)), supported_platforms(exclude=[Platform("powerpc64le", "linux"]))
 
 # The products that we will ensure are always built
 products = [
