b9838efc1d2943393759770edcdfcaaca33f59a1
lizz <innerlee@users.noreply.github.com>
2021-04-29 19:55:18 +0800

    Update build_tarballs.jl

--- L/libbluray/build_tarballs.jl
+++ L/libbluray/build_tarballs.jl
@@ -23,7 +23,7 @@ install_license COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
@@ -42,4 +42,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
