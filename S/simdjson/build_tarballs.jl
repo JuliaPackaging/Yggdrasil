41ba647c1c40aae6b646217abe6ff133035f58ac
Miguel Raz Guzmán Macedo <miguelraz@ciencias.unam.mx>
2021-04-29 20:13:15 -0500

    Update S/simdjson/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- S/simdjson/build_tarballs.jl
+++ S/simdjson/build_tarballs.jl
@@ -17,7 +17,7 @@ cd simdjson/
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DSIMDJSON_ENABLE_THREADS=ON ..
-make -j ${nprocs}
+make -j ${nproc}
 make install
 exit
 """
