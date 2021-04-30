12245192b9436233f2b760090145980b4d02018c
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-04-30 19:04:08 -0400

    Use lrt.patch for MacOSX

--- S/SPRAL/build_tarballs.jl
+++ S/SPRAL/build_tarballs.jl
@@ -15,15 +15,15 @@ sources = [
 script = raw"""
 cd ${WORKSPACE}/srcdir/spral
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/clong.patch
-if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]] || [[ "${target}" == *-w64-* ]]; then
-    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/timespec.patch
-fi
-if [[ "${target}" == *-w64-* ]]; then
-    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/lrt.patch
-fi
 if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]]; then
     CC=gcc
     CXX=g++
+fi
+if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]] || [[ "${target}" == *-w64-* ]]; then
+    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/timespec.patch
+fi
+if [[ "${target}" == *-apple-* ]] || [[ "${target}" == *-w64-* ]]; then
+    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/lrt.patch
 fi
 ./autogen.sh
 update_configure_scripts
