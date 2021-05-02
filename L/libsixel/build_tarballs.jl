8b9ad1f3c0c503d884094fe1a530324683f04ae7
Johnny Chen <johnnychen94@hotmail.com>
2021-05-02 22:51:04 +0800

    apply suggestions
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/libsixel/build_tarballs.jl
+++ L/libsixel/build_tarballs.jl
@@ -13,14 +13,19 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libsixel/
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --includedir=$WORKSPACE/destdir/include --libdir=$WORKSPACE/destdir/lib --enable-python=no
+./configure --prefix=${prefix} \
+    --build=${MACHTYPE} \
+    --host=${target} \
+    --includedir=$WORKSPACE/destdir/include \
+    --libdir=$WORKSPACE/destdir/lib \
+    --enable-python=no
 make -j${nproc}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -32,4 +37,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
