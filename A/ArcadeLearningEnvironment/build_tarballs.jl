15e40fa40f6d120ed5c5e0ce6841882fd601df7e
jbrea <jbrea@users.noreply.github.com>
2021-05-03 11:23:39 +0200

    Update build_tarballs.jl

--- A/ArcadeLearningEnvironment/build_tarballs.jl
+++ A/ArcadeLearningEnvironment/build_tarballs.jl
@@ -8,11 +8,18 @@ version = v"0.6.1"
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://github.com/mgbellemare/Arcade-Learning-Environment/archive/v0.6.1.tar.gz", "8059a4087680da03878c1648a8ceb0413a341032ecaa44bef4ef1f9f829b6dde"),
+    FileSource("http://www.atarimania.com/roms/Roms.rar", "4e35879fbd3da7d008f80f8d3a48360b9513859aa6c694164e67d5a82daca498"),
+    FileSource("https://raw.githubusercontent.com/mgbellemare/Arcade-Learning-Environment/v0.6.1/md5.txt", "218673cbeba56f3a7066293c259ae7b31ebece686bf3ff4ae4fe746e7d58a51e"),
     DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
+cd $WORKSPACE/srcdir/
+unrar e Roms.rar
+unzip ROMS.zip
+mkdir roms
+for f in ROMS/*; do md5=`md5sum "$f" | awk '{print $1}'`; newname=`grep $md5 md5.txt | awk '{print $2}'`; if [[ $newname != "" ]]; then cp "$f" roms/$newname; fi; done
 cd $WORKSPACE/srcdir/Arcade-Learning-Environment-*/
 atomic_patch -p1 ../patches/fix-dlext-macos.patch
 atomic_patch -p1 ../patches/cmake-install-for-windows.patch
@@ -38,7 +45,8 @@ platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libale_c", :libale_c)
+    LibraryProduct("libale_c", :libale_c),
+    FileProduct("roms", :roms)
 ]
 
 # Dependencies that must be installed before this package can be built
