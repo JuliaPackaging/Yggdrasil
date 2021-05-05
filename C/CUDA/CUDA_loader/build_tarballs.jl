75f427b997e03e0a2a8111ce15122263c6577e62
Tim Besard <tim.besard@gmail.com>
2021-05-05 18:26:58 +0200

    [CUDA_loader] Add forgotten CUDA 10.1. (#2937)

--- C/CUDA/CUDA_loader/build_tarballs.jl
+++ C/CUDA/CUDA_loader/build_tarballs.jl
@@ -5,7 +5,7 @@ include("../../../fancy_toys.jl")
 name = "CUDA_loader"
 version = v"0.1"
 
-cuda_versions = [v"9.0", v"9.2", v"10.0", v"10.2", v"11.0", v"11.1", v"11.2", v"11.3"]
+cuda_versions = [v"9.0", v"9.2", v"10.0", v"10.1", v"10.2", v"11.0", v"11.1", v"11.2", v"11.3"]
 for cuda_version in cuda_versions
     cuda_tag = "$(cuda_version.major).$(cuda_version.minor)"
     include("build_$(cuda_tag).jl")
