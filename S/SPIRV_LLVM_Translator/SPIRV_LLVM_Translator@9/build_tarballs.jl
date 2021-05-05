e97cb6485f688f501bdaaceb356250519a56b0ba
Tim Besard <tim.besard@gmail.com>
2021-05-05 12:09:29 +0200

    [SPIRV LLVM Translator] Switch to a fork with backported fixes. (#2935)

--- S/SPIRV_LLVM_Translator/SPIRV_LLVM_Translator@9/build_tarballs.jl
+++ S/SPIRV_LLVM_Translator/SPIRV_LLVM_Translator@9/build_tarballs.jl
@@ -3,7 +3,7 @@ include("../common.jl")
 version = v"9.0"
 
 # Collection of sources required to build attr
-sources = [GitSource(repo, "8442f41802d8afaff9fd2b95396e9f82c8b5bf05")]
+sources = [GitSource(repo, "6faf15fdf46814034808a26fc0108e41d89ff1a4")]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
