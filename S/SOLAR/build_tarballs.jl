bf352a29794833e2914452a59a3262d228546658
Alexis Montoison <alexis.montoison@polymtl.ca>
2021-05-06 20:16:36 -0400

    solar -> SOLAR

--- S/SOLAR/build_tarballs.jl
+++ S/SOLAR/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "solar"
+name = "SOLAR"
 version = v"0.3.0"
 
 # Collection of sources required to complete build
