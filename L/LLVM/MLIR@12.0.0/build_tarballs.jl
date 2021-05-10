ae24ddab87a4bd09625e4a31aac991affbf1ed31
Valentin Churavy <v.churavy@gmail.com>
2021-05-10 11:30:24 -0400

    don't build MLIR for now

--- L/LLVM/MLIR@12.0.0/build_tarballs.jl
+++
@@ -1,7 +0,0 @@
-name = "MLIR"
-llvm_full_version = v"12.0.0+0"
-libllvm_version = v"12.0.0+0"
-
-# Include common LLVM stuff
-include("../common.jl")
-build_tarballs(ARGS, configure_extraction(ARGS, llvm_full_version, name, libllvm_version; experimental_platforms=true)...; skip_audit=true,  julia_compat="1.7")
