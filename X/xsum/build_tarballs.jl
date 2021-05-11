5fed195f20b4b1ea904d2701869d47c06c8e4916
Steven G. Johnson <stevenj@mit.edu>
2021-05-11 09:05:34 -0400

    update xsum to latest version (#2963)
    * update xsum to latest version
    
    Includes new routines to add one accumulator to another, among other things.
    
    * fix date, and pbinary.c is only used for debugging output
    
    * require Julia 1.6 in the build
    
    * [xsum] Build for new platforms
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- X/xsum/build_tarballs.jl
+++ X/xsum/build_tarballs.jl
@@ -1,12 +1,12 @@
 using BinaryBuilder
 
 name = "xsum"
-version = v"1.0"
+version = v"1.1"
 
 # Collection of sources required to build Xsum
 sources = [
     GitSource("https://gitlab.com/radfordneal/xsum.git",
-              "63bd4c13e4a8da4932ded362e2921ed99079f432"), # version 2018-12-02 "1.0"
+              "e3212fdcff9928f0b9e571a89fa08b2d2d634a81"), # version 2021-05-10 "1.1"
 ]
 
 # Bash recipe for building across all platforms
@@ -16,12 +16,12 @@ mkdir -p ${libdir}
 if [[ $target == i686-* ]]; then
     xsumfpmath="-mfpmath=sse"
 fi
-${CC} -shared -fPIC -O3 -std=c99 ${xsumfpmath} -fno-inline-functions -o ${libdir}/libxsum.${dlext} xsum.c pbinary.c
+${CC} -shared -fPIC -O3 -std=c99 ${xsumfpmath} -fno-inline-functions -o ${libdir}/libxsum.${dlext} xsum.c
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms() # build on all supported platforms
+platforms = supported_platforms(; experimental=true) # build on all supported platforms
 
 # The products that we will ensure are always built
 products = [
@@ -33,4 +33,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
