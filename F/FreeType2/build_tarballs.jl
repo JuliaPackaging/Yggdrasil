aab959a9b92c3c1c252e13fca83443dd63875cc3
Mosè Giordano <mose@gnu.org>
2021-05-16 00:27:33 +0100

    [FreeTyp2] Upgrade to v2.10.4 and build for experimental platforms

--- F/FreeType2/build_tarballs.jl
+++ F/FreeType2/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder
 
 name = "FreeType2"
-version = v"2.10.1"
+version = v"2.10.4"
 
 # Collection of sources required to build FreeType2
 sources = [
-    ArchiveSource("https://download.savannah.gnu.org/releases/freetype/freetype-$(version).tar.gz",
-                  "3a60d391fd579440561bf0e7f31af2222bc610ad6ce4d9d7bd2165bca8669110")
+    ArchiveSource("https://download.savannah.gnu.org/releases/freetype/freetype-$(version).tar.xz",
+                  "86a854d8905b19698bbc8f23b860bc104246ce4854dcea8e3b0fb21284f75784")
 ]
 
 # Bash recipe for building across all platforms
@@ -17,11 +17,12 @@ cd $WORKSPACE/srcdir/freetype-*/
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --enable-shared --disable-static
 make -j${nproc}
 make install
+install_license docs/{FTL,GPLv2,LICENSE}.TXT
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -30,11 +31,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    # Future versions of bzip2 should allow a more relaxed compat because the
-    # soname of the macOS library shouldn't change at every patch release.
-    Dependency("Bzip2_jll", v"1.0.6"; compat="=1.0.6"),
+    Dependency("Bzip2_jll", v"1.0.8"; compat="1.0.8"),
     Dependency("Zlib_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
