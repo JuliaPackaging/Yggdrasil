efb545ebae050c579895bb9349ca6fc351fec72f
Mosè Giordano <giordano@users.noreply.github.com>
2021-05-16 21:03:51 +0100

    [ERFA] Require Julia v1.6 and build for experimental platforms

--- E/ERFA/build_tarballs.jl
+++ E/ERFA/build_tarballs.jl
@@ -26,7 +26,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -38,4 +38,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
