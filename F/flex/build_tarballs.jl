ed95d5cc6cb6c7889610c0670c8d075125c8c123
Wimmerer <wrkimmerer@outlook.com>
2021-05-16 19:35:32 -0400

    Excluding windows, Still can't find gettext_jll

--- F/flex/build_tarballs.jl
+++ F/flex/build_tarballs.jl
@@ -15,23 +15,16 @@ sources = [
 script = raw"""
 apk add gettext-dev
 cd ${WORKSPACE}/srcdir/flex-*
-if [[ "${target}" == *-mingw* ]]; then
-	cp ${includedir}/pcreposix.h ${includedir}/regex.h
-	export LDFLAGS="-lpcreposix-0 -L${libdir}"
-fi
 ./autogen.sh
+export CPPFLAGS="${CPPFLAGS} -I${includedir}"
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-static --enable-shared --disable-bootstrap
 make -j${nproc}
 make install
-if [[ "${target}" == *-mingw* ]]; then
-    # Cover up the traces of the hack
-    rm ${includedir}/regex.h
-fi
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(;experimental=true))
+platforms = expand_cxxstring_abis(supported_platforms(;exclude=Sys.iswindows, experimental=true))
 
 # The products that we will ensure are always built
 products = [
@@ -42,9 +35,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    HostBuildDependency("Gettext_jll"),
-    Dependency("PCRE_jll")
+    HostBuildDependency("Gettext_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
