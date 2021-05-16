b8d53b4ca3713f6da3d6832b53e3cfbb266aa4dd
Wimmerer <wrkimmerer@outlook.com>
2021-05-16 11:26:53 -0400

    Try HostBuildDep

--- F/flex/build_tarballs.jl
+++ F/flex/build_tarballs.jl
@@ -31,7 +31,8 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
+dependencies = [
+    HostBuildDependency("Gettext_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
