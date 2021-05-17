8594c1d277c24efae8443261078eeba395c929d7
Erik Schnetter <schnetter@gmail.com>
2021-05-17 18:25:17 -0400

    Make ADIOS2 build on Windows

--- A/ADIOS2/build_tarballs.jl
+++ A/ADIOS2/build_tarballs.jl
@@ -15,27 +15,32 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd ADIOS2-2.7.1
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/shlwapi.patch
 mkdir build
 cd build
 if [[ "$target" == *-apple-* ]]; then
-    # Set up a wrapper script for the assembler
+    # Set up a wrapper script for the assembler. GCC's assembler
+    # output isn't accepted by the LLVM assembler.
     as=$(which as)
     mv "$as" "$as.old"
     export AS="$as.old"
     ln -s "$WORKSPACE/srcdir/scripts/as.llvm" "$as"
 fi
 mpiopts=
-sstopts=
-# if [[ "$target" == x86_64-w64-mingw32 ]]; then
-#     mpiopts="-DMPI_HOME=$prefix -DMPI_GUESS_LIBRARY_NAME=MSMPI -DMPI_C_LIBRARIES=msmpi64 -DMPI_CXX_LIBRARIES=msmpi64"
-#     sstopts="-DADIOS2_USE_SST=OFF"
-# elif [[ "$target" == *-mingw* ]]; then
-#     mpiopts="-DMPI_HOME=$prefix -DMPI_GUESS_LIBRARY_NAME=MSMPI"
-#     sstopts="-DADIOS2_USE_SST=OFF"
-# fi
+winopts=
+if [[ "$target" == x86_64-w64-mingw32 ]]; then
+    # cmake's auto-detection doesn't work on Windows.
+    # The SST and Table ADIOS2 components don't build on Windows
+    # (reported in <https://github.com/ornladios/ADIOS2/issues/2705>)
+    mpiopts="-DMPI_HOME=$prefix -DMPI_GUESS_LIBRARY_NAME=MSMPI -DMPI_C_LIBRARIES=msmpi64 -DMPI_CXX_LIBRARIES=msmpi64"
+    winopts="-DADIOS2_USE_SST=OFF -DADIOS2_USE_Table=OFF"
+elif [[ "$target" == *-mingw* ]]; then
+    mpiopts="-DMPI_HOME=$prefix -DMPI_GUESS_LIBRARY_NAME=MSMPI"
+    winopts="-DADIOS2_USE_SST=OFF -DADIOS2_USE_Table=OFF"
+fi
 # We disable Fortran because this would require building many more
 # versions of the library
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN%.*}_gcc.cmake -DCMAKE_BUILD_TYPE=Release -DADIOS2_USE_Fortran=OFF -DADIOS2_BUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF ${mpiopts} ${sstopts} ..
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN%.*}_gcc.cmake -DCMAKE_BUILD_TYPE=Release -DADIOS2_USE_Fortran=OFF -DADIOS2_BUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF ${mpiopts} ${winopts} ..
 make -j${nproc}
 make -j${nproc} install
 """
@@ -49,6 +54,7 @@ platforms = [
     Platform("x86_64", "linux"; libc="glibc"),
     Platform("x86_64", "linux"; libc="musl"),
     Platform("x86_64", "macos"),
+    Platform("x86_64", "windows"),
 
     # These platforms fail:
 
@@ -58,18 +64,13 @@ platforms = [
     #FAIL Platform("armv7l", "linux"; libc="glibc"),
     #FAIL Platform("i686", "linux"; libc="glibc"),
     #TODO Platform("i686", "linux"; libc="musl"),
+    #TODO Platform("i686", "windows"),
 
     # [22:40:43] /workspace/srcdir/ADIOS2-2.7.1/source/adios2/toolkit/profiling/taustubs/tautimer.cpp:208:31: error: ‘__NR_gettid’ was not declared in this scope
     # [22:40:43]      mytid = (uint64_t)syscall(__NR_gettid);
     # (Likely the respective syscall does not exist on FreeBSD;
     # reported as <https://github.com/ornladios/ADIOS2/issues/2705>.)
     #FAIL Platform("x86_64", "freebsd"),
-
-    # [13:22:26] /workspace/srcdir/ADIOS2-2.7.1/source/adios2/engine/table/TableWriter.cpp:201:16: error: ‘AvailableIpAddresses’ is not a member of ‘adios2::helper’
-    # [13:22:26]      auto ips = helper::AvailableIpAddresses();
-    # (Reported as <https://github.com/ornladios/ADIOS2/issues/2705>.)
-    #FAIL Platform("x86_64", "windows"),
-    #TODO Platform("i686", "windows"),
 ]
 # Apparently, macOS doesn't use different C++ string APIs
 platforms = expand_cxxstring_abis(platforms; skip=Sys.isapple)
@@ -86,7 +87,7 @@ dependencies = [
     Dependency(PackageSpec(name="Bzip2_jll")),
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
     Dependency(PackageSpec(name="MPICH_jll")),
-    # Dependency(PackageSpec(name="MicrosoftMPI_jll")),
+    Dependency(PackageSpec(name="MicrosoftMPI_jll")),
     Dependency(PackageSpec(name="Python_jll")),
     Dependency(PackageSpec(name="ZeroMQ_jll")),
     Dependency(PackageSpec(name="libpng_jll")),
@@ -95,4 +96,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+# GCC 4 is too old for Windows; it doesn't have <regex.h>
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"5")
