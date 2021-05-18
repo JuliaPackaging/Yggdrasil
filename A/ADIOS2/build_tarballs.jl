edb404708bcd66e03d297e26c89642e96f2ecdf3
Erik Schnetter <schnetter@gmail.com>
2021-05-17 20:21:15 -0400

    Make ADIOS2 build on macOS and Windows (#3019)
    * ADIOS2: Support macOS
    
    Use a wrapper script for the assembler on macOS.
    
    Also enable compression by depending on compression libraries.
    
    * Make ADIOS2 build on Windows
    
    * Update A/ADIOS2/build_tarballs.jl
    
    * Incorporate @giordano's suggestions
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- A/ADIOS2/build_tarballs.jl
+++ A/ADIOS2/build_tarballs.jl
@@ -7,26 +7,43 @@ version = v"2.7.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/ornladios/ADIOS2/archive/refs/tags/v2.7.1.tar.gz", "c8e237fd51f49d8a62a0660db12b72ea5067512aa7970f3fcf80b70e3f87ca3e")
+    ArchiveSource("https://github.com/ornladios/ADIOS2/archive/refs/tags/v2.7.1.tar.gz", "c8e237fd51f49d8a62a0660db12b72ea5067512aa7970f3fcf80b70e3f87ca3e"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd ADIOS2-2.7.1
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/shlwapi.patch
 mkdir build
 cd build
-# if [[ "$target" == x86_64-w64-mingw32 ]]; then
-#     mpiopts="-DMPI_HOME=$prefix -DMPI_GUESS_LIBRARY_NAME=MSMPI -DMPI_C_LIBRARIES=msmpi64 -DMPI_CXX_LIBRARIES=msmpi64"
-# elif [[ "$target" == *-mingw* ]]; then
-#     mpiopts="-DMPI_HOME=$prefix -DMPI_GUESS_LIBRARY_NAME=MSMPI"
-# else
-#     mpiopts=
-# fi
+if [[ "$target" == *-apple-* ]]; then
+    # Set up a wrapper script for the assembler. GCC's assembler
+    # output isn't accepted by the LLVM assembler.
+    as=$(which as)
+    mv "$as" "$as.old"
+    export AS="$as.old"
+    ln -s "$WORKSPACE/srcdir/scripts/as.llvm" "$as"
+fi
 mpiopts=
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN%.*}_gcc.cmake -DCMAKE_BUILD_TYPE=Release -DADIOS2_USE_Fortran=OFF -DADIOS2_BUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF ${mpiopts} ..
+winopts=
+if [[ "$target" == x86_64-w64-mingw32 ]]; then
+    # cmake's auto-detection doesn't work on Windows.
+    # The SST and Table ADIOS2 components don't build on Windows
+    # (reported in <https://github.com/ornladios/ADIOS2/issues/2705>)
+    mpiopts="-DMPI_HOME=$prefix -DMPI_GUESS_LIBRARY_NAME=MSMPI -DMPI_C_LIBRARIES=msmpi64 -DMPI_CXX_LIBRARIES=msmpi64"
+    winopts="-DADIOS2_USE_SST=OFF -DADIOS2_USE_Table=OFF"
+elif [[ "$target" == *-mingw* ]]; then
+    mpiopts="-DMPI_HOME=$prefix -DMPI_GUESS_LIBRARY_NAME=MSMPI"
+    winopts="-DADIOS2_USE_SST=OFF -DADIOS2_USE_Table=OFF"
+fi
+# We disable Fortran because this would require building many more
+# versions of the library
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN%.*}_gcc.cmake -DCMAKE_BUILD_TYPE=Release -DADIOS2_USE_Fortran=OFF -DADIOS2_BUILD_EXAMPLES=OFF -DBUILD_TESTING=OFF ${mpiopts} ${winopts} ..
 make -j${nproc}
 make -j${nproc} install
+install_license ../Copyright.txt ../LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
@@ -37,13 +54,10 @@ platforms = [
     Platform("powerpc64le", "linux"; libc="glibc"),
     Platform("x86_64", "linux"; libc="glibc"),
     Platform("x86_64", "linux"; libc="musl"),
+    Platform("x86_64", "macos"),
+    Platform("x86_64", "windows"),
 
     # These platforms fail:
-
-    # [21:20:50] /tmp/ccaoehHe.s:73869:2: error: ambiguous instructions require an explicit suffix (could be 'filds', or 'fildl')
-    # [21:20:50]         fild    14(%rsp)
-    # (Maybe using a different assembler would help?)
-    #FAIL Platform("x86_64", "macos"),
 
     # [22:03:24] /workspace/srcdir/ADIOS2-2.7.1/source/adios2/engine/ssc/SscReader.cpp:420:71: error: narrowing conversion of ‘18446744073709551613ull’ from ‘long long unsigned int’ to ‘unsigned int’ inside { } [-Wnarrowing]
     # [22:03:24]                  m_IO.DefineVariable<T>(b.name, {adios2::LocalValueDim});       \
@@ -51,18 +65,13 @@ platforms = [
     #FAIL Platform("armv7l", "linux"; libc="glibc"),
     #FAIL Platform("i686", "linux"; libc="glibc"),
     #TODO Platform("i686", "linux"; libc="musl"),
+    #TODO Platform("i686", "windows"),
 
     # [22:40:43] /workspace/srcdir/ADIOS2-2.7.1/source/adios2/toolkit/profiling/taustubs/tautimer.cpp:208:31: error: ‘__NR_gettid’ was not declared in this scope
     # [22:40:43]      mytid = (uint64_t)syscall(__NR_gettid);
     # (Likely the respective syscall does not exist on FreeBSD;
     # reported as <https://github.com/ornladios/ADIOS2/issues/2705>.)
     #FAIL Platform("x86_64", "freebsd"),
-
-    # [10:00:48] /workspace/srcdir/ADIOS2-2.7.1/thirdparty/dill/dill/x86_64_rt.c:4:22: fatal error: sys/mman.h: No such file or directory
-    # [10:00:48]  #include "sys/mman.h"
-    # (Windows is not supported.)
-    #FAIL Platform("x86_64", "windows"),
-    #TODO Platform("i686", "windows"),
 ]
 # Apparently, macOS doesn't use different C++ string APIs
 platforms = expand_cxxstring_abis(platforms; skip=Sys.isapple)
@@ -75,10 +84,19 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    Dependency(PackageSpec(name="Blosc_jll")),
+    # We don't want to use Bzip2 because this would lock us into Julia ≥1.6
+    # Dependency(PackageSpec(name="Bzip2_jll")),
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
     Dependency(PackageSpec(name="MPICH_jll")),
-    # Dependency(PackageSpec(name="MicrosoftMPI_jll")),
+    Dependency(PackageSpec(name="MicrosoftMPI_jll")),
+    Dependency(PackageSpec(name="ZeroMQ_jll")),
+    Dependency(PackageSpec(name="libpng_jll")),
+    Dependency(PackageSpec(name="zfp_jll")),
+    # We cannot use HDF5 because we need a HDF5 configurion with MPI support
+    # Dependency(PackageSpec(name="HDF5_jll")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+# GCC 4 is too old for Windows; it doesn't have <regex.h>
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"5")
