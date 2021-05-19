e836269fcaed499c00f1aec1edebfb4bcb0aded7
Mark Kittisopikul <markkitt@gmail.com>
2021-05-19 13:52:55 -0400

    Bump GCC version to 5.2 for __AMD_NEON and use Zlib, Zstd, and Lz4 deps

--- B/Blosc2/build_tarballs.jl
+++ B/Blosc2/build_tarballs.jl
@@ -5,7 +5,7 @@ using BinaryBuilder, Pkg
 name = "Blosc2"
 version = v"1.9.9"
 
-# Collection of sources required to complete build
+# Collection of sources required to build Blosc2
 sources = [
     GitSource("https://github.com/Blosc/c-blosc2.git", "f06319ce93654e7cada64a75778fb50e8ddc6667")
 ]
@@ -16,7 +16,14 @@ cd $WORKSPACE/srcdir
 cd c-blosc2/
 mkdir build
 cd build/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF -DBUILD_EXAMPLES=OFF -DBUILD_STATIC=OFF ..
+CMAKE_FLAGS=(-DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release)
+CMAKE_FLAGS+=(-DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF)
+CMAKE_FLAGS+=(-DBUILD_EXAMPLES=OFF -DBUILD_STATIC=OFF)
+CMAKE_FLAGS+=(-DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS="")
+CMAKE_FLAGS+=(-DPREFER_EXTERNAL_ZLIB=ON)
+CMAKE_FLAGS+=(-DPREFER_EXTERNAL_ZSTD=ON)
+CMAKE_FLAGS+=(-DPREFER_EXTERNAL_LZ4=ON)
+cmake ${CMAKE_FLAGS[@]} ..
 make -j${nproc}
 make install
 install_license ../LICENSES/*.txt
@@ -34,7 +41,10 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
+	Dependency("Zlib_jll"),
+	Dependency("Zstd_jll"),
+	Dependency("Lz4_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", lock_microarchitecture=false)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", lock_microarchitecture=false, preferred_gcc_version=v"5.2")
