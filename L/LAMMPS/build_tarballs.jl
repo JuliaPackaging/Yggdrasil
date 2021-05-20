8ccb524ef9f93d3aaa5cfb9553377d143cb95e74
Valentin Churavy <vchuravy@users.noreply.github.com>
2021-05-20 11:55:42 -0400

    Update L/LAMMPS/build_tarballs.jl

--- L/LAMMPS/build_tarballs.jl
+++ L/LAMMPS/build_tarballs.jl
@@ -27,6 +27,7 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
+filter!(p -> libc(p) != "musl", platforms)
 
 # The products that we will ensure are always built
 products = [
