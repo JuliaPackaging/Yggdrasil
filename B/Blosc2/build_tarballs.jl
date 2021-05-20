aac8a80a500a38170785d18d918e9f6cb002bb19
Mosè Giordano <giordano@users.noreply.github.com>
2021-05-20 02:37:27 +0100

    [Blosc2] Build for experimental platforms

--- B/Blosc2/build_tarballs.jl
+++ B/Blosc2/build_tarballs.jl
@@ -8,44 +8,45 @@ version = v"1.9.9"
 # Collection of sources required to build Blosc2
 sources = [
     GitSource("https://github.com/Blosc/c-blosc2.git", "f06319ce93654e7cada64a75778fb50e8ddc6667"),
-    DirectorySource("bundled")
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd c-blosc2/
-git apply ../patches/blosc_armv7l.patch
-mkdir build
-cd build/
-CMAKE_FLAGS=(-DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release)
-CMAKE_FLAGS+=(-DBUILD_TESTS=OFF -DBUILD_BENCHMARKS=OFF)
-CMAKE_FLAGS+=(-DBUILD_EXAMPLES=OFF -DBUILD_STATIC=OFF)
-CMAKE_FLAGS+=(-DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS="")
-CMAKE_FLAGS+=(-DPREFER_EXTERNAL_ZLIB=ON)
-CMAKE_FLAGS+=(-DPREFER_EXTERNAL_ZSTD=ON)
-CMAKE_FLAGS+=(-DPREFER_EXTERNAL_LZ4=ON)
-cmake ${CMAKE_FLAGS[@]} ..
+cd $WORKSPACE/srcdir/c-blosc2/
+atomic_patch -p1 ../patches/blosc_armv7l.patch
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_TESTS=OFF \
+    -DBUILD_BENCHMARKS=OFF \
+    -DBUILD_EXAMPLES=OFF \
+    -DBUILD_STATIC=OFF \
+    -DCMAKE_SHARED_LIBRARY_LINK_C_FLAGS="" \
+    -DPREFER_EXTERNAL_ZLIB=ON) \
+    -DPREFER_EXTERNAL_ZSTD=ON \
+    -DPREFER_EXTERNAL_LZ4=ON \
+    ..
 make -j${nproc}
 make install
 install_license ../LICENSES/*.txt
-exit
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libblosc2", :libblosc2)
+    LibraryProduct("libblosc2", :libblosc2),
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
-	Dependency("Zlib_jll"),
-	Dependency("Zstd_jll"),
-	Dependency("Lz4_jll"),
+dependencies = [
+    Dependency("Zlib_jll"),
+    Dependency("Zstd_jll"),
+    Dependency("Lz4_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
