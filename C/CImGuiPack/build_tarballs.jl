4ad4a0ae3a158876dd891a35e3e7391d75d37ded
Gnimuc <qiyupei@gmail.com>
2021-05-22 01:17:39 +0900

    Run submodule update

--- C/CImGuiPack/build_tarballs.jl
+++ C/CImGuiPack/build_tarballs.jl
@@ -13,8 +13,8 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd cimgui-pack
+cd $WORKSPACE/srcdir/cimgui-pack
+git submodule update --init --recursive
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
 make -j${nproc}
