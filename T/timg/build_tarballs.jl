60ac120983726b4bfc5a9dc9450e6192aba0ad5b
Jerry Ling <proton@jling.dev>
2021-05-22 16:54:33 -0400

    Update build_tarballs.jl

--- T/timg/build_tarballs.jl
+++ T/timg/build_tarballs.jl
@@ -12,10 +12,9 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd timg/
+cd $WORKSPACE/srcdir/timg
 echo "add_definitions(-D__STDC_FORMAT_MACROS)"|cat - CMakeLists.txt > tmp_out && mv tmp_out CMakeLists.txt 
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DWITH_VIDEO_DECODING=Off -DWITH_VIDEO_DEVICE=Off -DWITH_OPENSLIDE_SUPPORT=Off -DWITH_GRAPHICSMAGICK=Off -DTIMG_VERSION_FROM_GIT=Off
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DWITH_VIDEO_DECODING=On -DWITH_VIDEO_DEVICE=Off -DWITH_OPENSLIDE_SUPPORT=On
 sed -i '$ s/$/ -lrt/' ./src/CMakeFiles/timg.dir/link.txt 
 make
 make install
@@ -49,6 +48,9 @@ dependencies = [
     Dependency(PackageSpec(name="FFMPEG_jll", uuid="b22a6f82-2f65-5046-a5b2-351ab43fb4e5"))
     Dependency(PackageSpec(name="JpegTurbo_jll", uuid="aacddb02-875f-59d6-b918-886e6ef4fbf8"))
     Dependency(PackageSpec(name="libexif_jll", uuid="cdeeb48b-bcdf-5b3f-98c4-7a29487f695f"))
+    Dependency(PackageSpec(name="OpenSlide_jll", uuid="becf559c-afb9-529d-9a2d-92566e0304eb"))
+    Dependency(PackageSpec(name = "libgraphicsmagic_jll",  uuid = "3e975b7b-ab84-5bf4-b4b6-586754a53ef6"))
+
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
