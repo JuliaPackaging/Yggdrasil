c0cfa9d4f9063119f1e08193fce4e513be85c5e9
Mosè Giordano <giordano@users.noreply.github.com>
2021-05-27 00:30:45 +0100

    [NOMAD] Do not expand C++ string ABI for FreeBSD

--- N/NOMAD/build_tarballs.jl
+++ N/NOMAD/build_tarballs.jl
@@ -22,7 +22,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(); skip=Sys.isapple)
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
