238bd628ce9279b44e502044405957f762b0b62a
Jéssica Milaré <jessymilare@gmail.com>
2021-05-27 15:06:33 -0300

    Remove unsupported Windows entries

--- L/LibMPDec/build_tarballs.jl
+++ L/LibMPDec/build_tarballs.jl
@@ -32,8 +32,8 @@ platforms = [
     Platform("aarch64", "linux"; libc = "musl"),
     Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
     Platform("x86_64", "freebsd"; ),
-    Platform("i686", "windows"; ),
-    Platform("x86_64", "windows"; )
+    # Platform("i686", "windows"; ),
+    # Platform("x86_64", "windows"; )
 ]
 
 
