eddc73af09336b488ea7fac456d8136f57f5a60e
Alexander <alexander@plav.in>
2021-05-30 11:00:34 +0300

    update hash

--- D/difmap/build_tarballs.jl
+++ D/difmap/build_tarballs.jl
@@ -6,7 +6,7 @@ name = "difmap"
 # difmap version is 2.5k, mapped here to numerical value of 2.5.11
 version = v"2.5.11"
 
-sources = [ArchiveSource("ftp://ftp.astro.caltech.edu/pub/difmap/difmap2.5k.tar.gz", "457cd77c146e22b5332403c19b29485388a863ec494fff87137176396fc6a9ff")]
+sources = [ArchiveSource("ftp://ftp.astro.caltech.edu/pub/difmap/difmap2.5k.tar.gz", "d754dcc094a758a817677fe493bde13408cc270910f6bb7bbc3487e002cc184e")]
 
 script = raw"""
 cd $WORKSPACE/srcdir
