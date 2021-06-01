57112f2bf9f13ed8b0b0d3681123302165da2853
Paul <paul.berg@etu.utc.fr>
2021-06-01 14:30:28 +0200

    [Deno_jll] update to version 1.10.3

--- D/Deno/build_tarballs.jl
+++ D/Deno/build_tarballs.jl
@@ -3,15 +3,15 @@
 using BinaryBuilder, Pkg
 
 name = "Deno"
-version = v"1.10.2"
+version = v"1.10.3"
 
 release_url = "https://github.com/denoland/deno/releases/download/v$version"
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("$release_url/deno-x86_64-unknown-linux-gnu.zip", "ec6f1e50df9dc32f493d53b3d5befe5d42e7eeba87ae9be75c331f900c3f2453"; unpack_target = "x86_64-linux-gnu"),
-    ArchiveSource("$release_url/deno-x86_64-apple-darwin.zip", "ae8dde90c83de9b12ca110db324cf29f87b471ddfc06be1df9c18dcbd8a1413c"; unpack_target = "x86_64-apple-darwin14"),
-    ArchiveSource("$release_url/deno-x86_64-pc-windows-msvc.zip", "03bd14a0e506929769367b0bb604404dc37afad881db8f39d1b24df031856f2a"; unpack_target = "x86_64-w64-mingw32"),
-    ArchiveSource("$release_url/deno_src.tar.gz", "16c32ecff8c8e6169bcfd79f4a7ebd8fc3c79bba88cfd970befd4020c2ca1e34"),
+    ArchiveSource("$release_url/deno-x86_64-unknown-linux-gnu.zip", "7b278d41356ad0b36e79f9f444bd3e6b8e308fd08cb4ea381e085390825db22d"; unpack_target = "x86_64-linux-gnu"),
+    ArchiveSource("$release_url/deno-x86_64-apple-darwin.zip", "22aafee051291725ac9c02301eaa861f5c3375d0bdc41b2475d6745d94eb7907"; unpack_target = "x86_64-apple-darwin14"),
+    ArchiveSource("$release_url/deno-x86_64-pc-windows-msvc.zip", "0836022ddc35fe931884e182fa72614d927d851629f590c19d58a39f31d6cda7"; unpack_target = "x86_64-w64-mingw32"),
+    ArchiveSource("$release_url/deno_src.tar.gz", "056c2ad93df0b1a19ef1c6d168f12f48bdf0e58a5c7f11ed7a3bb3646789c7b7"),
 ]
 
 # Bash recipe for building across all platforms
