9bc500787a2d0b11c20e595f7771dae1f454ac40
Ryan Curtin <ryan@ratml.org>
2021-06-08 17:04:51 -0400

    Patch categorical support for mlpack.

--- M/mlpack/build_tarballs.jl
+++ M/mlpack/build_tarballs.jl
@@ -9,11 +9,18 @@ name = "mlpack"
 version = v"3.4.2"
 sources = [
     ArchiveSource("https://www.mlpack.org/files/mlpack-$(version).tar.gz",
-                  "9e5c4af5c276c86a0dcc553289f6fe7b1b340d61c1e59844b53da0debedbb171")
+                  "9e5c4af5c276c86a0dcc553289f6fe7b1b340d61c1e59844b53da0debedbb171"),
+    DirectorySource("./bundled"),
 ]
 
 script = raw"""
 cd ${WORKSPACE}/srcdir/mlpack-*/
+
+# Apply any patches that are needed.
+for f in ${WORKSPACE}/srcdir/patches/*.patch;
+do
+    atomic_patch -p1 ${f};
+done
 
 mkdir build && cd build
 
