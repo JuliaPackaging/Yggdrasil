55777a3d0eecb57e7f1362cef33da805c20c9aa2
Tim Besard <tim.besard@gmail.com>
2021-06-10 11:16:23 +0200

    Update LLVM to SPIRV translator, add LLVM 12 build. (#3148)

--- S/SPIRV_LLVM_Translator/SPIRV_LLVM_Translator@11/build_tarballs.jl
+++ S/SPIRV_LLVM_Translator/SPIRV_LLVM_Translator@11/build_tarballs.jl
@@ -3,7 +3,7 @@ include("../common.jl")
 version = v"11.0"
 
 # Collection of sources required to build attr
-sources = [GitSource(repo, "d3c8d0e0379202b124c0ff5454a05ab0fc1153f7")]
+sources = [GitSource(repo, "52ba8426e7d2839807337339e17222b91a3b8e35")]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
@@ -13,4 +13,4 @@ dependencies = [
 
 # Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               preferred_gcc_version=v"7", julia_compat="1.6")
+               preferred_gcc_version=v"7", julia_compat="~1.6")
