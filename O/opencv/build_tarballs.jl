8643381f0bddde371468dbedcf2586335f95cc6d
Archit Rungta <architrungta120@gmail.com>
2021-06-14 22:52:17 +0530

    add more platforms

--- O/opencv/build_tarballs.jl
+++ O/opencv/build_tarballs.jl
@@ -20,19 +20,31 @@ cd build
 cmake -DCMAKE_FIND_ROOT_PATH=$prefix       -DJulia_PREFIX=${prefix}       -DWITH_JULIA=ON    -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}       -DCMAKE_BUILD_TYPE=Release       -DWITH_QT=ON   -DOPENCV_EXTRA_MODULES_PATH=../opencv_contrib/modules   -DBUILD_LIST=core,imgproc,imgcodecs,highgui,videoio,dnn,features2d,objdetect,calib3d,julia       ../opencv/
 make -j${nproc}
 make install
-cp lib/libopencv_julia.so ${prefix}/lib
-tar cvf ${prefix}/OpenCV.jl.tar OpenCV
+cp lib/libopencv_julia.* ${prefix}/lib
+cp -R OpenCV ${prefix}
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
+
 platforms = [
-    # Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc")#,
-    # Platform("aarch64", "linux"; libc = "glibc"),
-    # Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    # Platform("powerpc64le", "linux"; libc = "glibc")
+    FreeBSD(:x86_64),
+    Linux(:armv7l; libc=:glibc),
+    Linux(:aarch64; libc=:glibc),
+    Linux(:x86_64; libc=:glibc),
+    Linux(:i686; libc=:glibc),
+    MacOS(:x86_64),
+    Windows(:x86_64),
+    Windows(:i686),
 ]
+
+# platforms = [
+#     Platform("i686", "linux"; libc = "glibc"),
+#     Platform("x86_64", "linux"; libc = "glibc"),
+#     Platform("aarch64", "linux"; libc = "glibc"),
+#     Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
+#     Platform("powerpc64le", "linux"; libc = "glibc")
+# ]
 
 
 # The products that we will ensure are always built
@@ -47,13 +59,13 @@ products = [
     LibraryProduct("libopencv_imgproc", :libopencv_imgproc),
     LibraryProduct("libopencv_features2d", :libopencv_features2d),
     LibraryProduct("libopencv_videoio", :libopencv_videoio),
-    LibraryProduct("libopencv_julia", :libopencv_julia),
-    FileProduct("OpenCV.jl.tar", :OpenCV_jl)
+    LibraryProduct("libopencv_julia", :libopencv_julia)#,
+    # FileProduct("OpenCV.jl.tar", :OpenCV_jl)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Qt_jll", uuid="ede63266-ebff-546c-83e0-1c6fb6d0efc8"))
+    Dependency(PackageSpec(name="Qt5Base_jll", uuid="ea2cea3b-5b76-57ae-a6ef-0a8af62496e1"))
     Dependency(PackageSpec(name="Libglvnd_jll", uuid="7e76a0d4-f3c7-5321-8279-8d96eeed0f29"))
     BuildDependency(PackageSpec(name="libjulia_jll", version=julia_version))
     Dependency(PackageSpec(name="libcxxwrap_julia_jll"))
