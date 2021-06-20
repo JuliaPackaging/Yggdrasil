38fbe5b9406b1b1c72c6174ca1bb4a0180620654
Mosè Giordano <giordano@users.noreply.github.com>
2021-06-20 17:40:26 +0100

    [QPALM] Build for experimental platforms

--- Q/QPALM/build_tarballs.jl
+++ Q/QPALM/build_tarballs.jl
@@ -61,7 +61,7 @@ install_license LICENSE.LADEL
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = Product[
