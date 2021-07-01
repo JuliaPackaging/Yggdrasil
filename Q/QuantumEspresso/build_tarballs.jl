4fd2c92e3c03286590fd500ee52937a4b172121c
Michael F. Herbst <info@michael-herbst.com>
2021-07-01 18:00:52 +0200

    Build with Libxc only on 64-bit platforms

--- Q/QuantumEspresso/build_tarballs.jl
+++ Q/QuantumEspresso/build_tarballs.jl
@@ -3,9 +3,6 @@ using BinaryBuilder, Pkg
 name = "QuantumEspresso"
 version = v"6.7.0"
 
-# TODO This build still suffers from some limitations:
-#      - Missing SCALAPACK support
-
 sources = [
     ArchiveSource("https://github.com/QEF/q-e/releases/download/qe-6.7.0/qe-6.7-ReleasePack.tgz",
                   "8f06ea31ae52ad54e900a2f51afd5c70f78096d9dcf39c86c2b17dccb1ec9c87"),
@@ -17,25 +14,33 @@ sources = [
 script = raw"""
     cd qe-*
     atomic_patch -p1 ../patches/0000-pass-host-to-configure.patch
-    atomic_patch -p1 ../patches/0001-libxc-prefix.patch
 
     export BLAS_LIBS="-L${libdir} -lopenblas"
     export LAPACK_LIBS="-L${libdir} -lopenblas"
-    export FFTW_INCLUDE=${includedir} FFT_LIBS="-L${libdir} -lfftw3"
-    export MPIF90=mpif90
+    export SCALAPACK_LIBS="-L${libdir} -lscalapack"
+    export FFTW_INCLUDE=${includedir}
+    export FFT_LIBS="-L${libdir} -lfftw3"
+    export FC=mpif90
     export CC=mpicc
-    ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} \
-        --enable-parallel=yes --with-scalapack=no \
-        --with-libxc=yes --with-libxc-prefix=${prefix}
-    make all "${make_args[@]}" -j $nproc MPIF90=mpif90 CC=mpicc
+
+    flags=(--enable-parallel=yes --with-scalapack=yes)
+    if [ ${nbits} -eq 64 ]; then
+        # Enable Libxc support only on 64-bit platforms
+        atomic_patch -p1 ../patches/0001-libxc-prefix.patch
+        flags+=(--with-libxc=yes --with-libxc-prefix=${prefix})
+    fi
+
+    ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} ${flags[@]}
+    make all "${make_args[@]}" -j $nproc
     make install
+    exit 1
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_gfortran_versions(supported_platforms())
 platforms = filter!(!Sys.iswindows, platforms)
-platforms = filter!(!Sys.isapple, platforms)
+platforms = filter!(!Sys.isapple,   platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -52,6 +57,7 @@ dependencies = [
     Dependency("Libxc_jll"),
     Dependency("MPICH_jll"),
     Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2")),
+    Dependency("SCALAPACK_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well
