bc25dee8b8c4cc6311e128793b49ace1fcc1a017
Mosè Giordano <mose@gnu.org>
2021-07-04 23:50:52 +0100

    [SLATEC] Bundle license and build for experimental platforms
    Use license file from
    https://aur.archlinux.org/cgit/aur.git/tree/LICENSE?h=slatec&id=53040fc2756b7daee6dd920e66d805ba2d0d6cf8

--- S/SLATEC/build_tarballs.jl
+++ S/SLATEC/build_tarballs.jl
@@ -6,15 +6,15 @@ name = "SLATEC"
 version = v"4.1.0"
 
 # Collection of sources required to complete build
-sources = [GitSource("https://github.com/sabjohnso/slatec.git",
-                  "417db9e31c49eba4aee5ab9bb719093f6886bcee")
+sources = [
+    GitSource("https://github.com/sabjohnso/slatec.git",
+              "417db9e31c49eba4aee5ab9bb719093f6886bcee"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/slatec
-
-cd src
+cd $WORKSPACE/srcdir/slatec/src
 
 # Manually set LAPACK and BLAS libraries
 echo "list( REMOVE_ITEM SLATEC_SOURCE_FILES \${BLAS_SOURCE_FILES} )" >> CMakeLists.txt
@@ -26,23 +26,48 @@ echo "add_library( slatec_shared SHARED \${SLATEC_SOURCE_FILES} )" >> CMakeLists
 echo "target_link_libraries( slatec_shared \${LAPACK_LIBRARIES} \${OpenBLAS_LIBRARIES})" >> CMakeLists.txt
 echo "set_target_properties( slatec_shared PROPERTIES OUTPUT_NAME slatec )" >> CMakeLists.txt
 echo "install( TARGETS slatec_shared LIBRARY DESTINATION lib )" >> CMakeLists.txt
-  
-cd ..
 
-mkdir build
-cd build/
+if [[ "${target}" == aarch64-apple-* ]]; then
+    # Fix issue due to GCC 10+.
+    #     [  2%] Building Fortran object src/CMakeFiles/slatec_shared.dir/dwnlsm.f.o
+    #     cd /workspace/srcdir/slatec/build/src && /opt/bin/aarch64-apple-darwin20-libgfortran5-cxx11/aarch64-apple-darwin20-gfortran --sysroot=/opt/aarch64-apple-darwin20/aarch64-apple-darwin20/sys-root -Dslatec_shared_EXPORTS  -O3 -DNDEBUG -O3 -fPIC   -c /workspace/srcdir/slatec/src/dwnlsm.f -o CMakeFiles/slatec_shared.dir/dwnlsm.f.o
+    #     /workspace/srcdir/slatec/src/dwnlsm.f:440:28:
+    #
+    #       119 |       CALL DCOPY (N, 1.D0, 0, D, 1)
+    #           |                     2
+    #     ......
+    #       440 |          CALL DCOPY (NSOLN, Z, 1, X, 1)
+    #           |                            1
+    #     Error: Rank mismatch between actual argument at (1) and actual argument at (2) (scalar and rank-1)
+    #     /workspace/srcdir/slatec/src/dwnlsm.f:608:25:
+    #
+    #       119 |       CALL DCOPY (N, 1.D0, 0, D, 1)
+    #           |                     2
+    #     ......
+    #       608 |       CALL DCOPY (NSOLN, Z, 1, X, 1)
+    #           |                         1
+    #     Error: Rank mismatch between actual argument at (1) and actual argument at (2) (scalar and rank-1)
+    export FFLAGS="-fallow-argument-mismatch"
+fi
+
+mkdir ../build
+cd ../build/
 # Above on the fly added CMake code buils shared library with OpenBLAS
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-        -DCMAKE_BUILD_TYPE=Release -DUSE_OPTIMIZED_BLAS=0 -DBUILD_SHARED_LIBRARY=0 ..
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DUSE_OPTIMIZED_BLAS=0 \
+    -DBUILD_SHARED_LIBRARY=0 \
+    ..
 make -j${nproc}
 make install
 
+install_license ../../LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-platforms = expand_gfortran_versions(platforms)
+platforms = expand_gfortran_versions(supported_platforms(; experimental=true))
 
 # The products that we will ensure are always built
 products = [
