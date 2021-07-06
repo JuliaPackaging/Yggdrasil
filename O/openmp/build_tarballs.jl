5ee32cb89b09c4c43ef8b459bebf7dd27fa508c7
Clouds <Clouds.Flowing@gmail.com>
2021-07-06 11:01:38 +0800

    [openmp] Add initial recipe.

---
+++ O/openmp/build_tarballs.jl
@@ -0,0 +1,38 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "openmp"
+version = v"12.0.0"
+
+# Collection of sources required to build mpg123
+sources = [
+    ArchiveSource(
+        "https://github.com/llvm/llvm-project/releases/download/llvmorg-$version/openmp-$version.src.tar.xz",
+        "eb1b7022a247332114985ed155a8fb632c28ce7c35a476e2c0caf865150f167d"
+    ),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/openmp-*/
+cmake . -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DLIBOMP_INSTALL_ALIASES=OFF
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libomp", :libomp),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8", perferred_llvm_version=v"12")
