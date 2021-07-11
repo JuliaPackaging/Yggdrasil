5aecae882d8011fea24a59698a9b6e17f67a589e
Gnimuc <qiyupei@gmail.com>
2021-07-11 22:27:07 +0900

    fix-up

--- L/libclangex/build_tarballs.jl
+++ L/libclangex/build_tarballs.jl
@@ -1,6 +1,7 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
+using Base.BinaryPlatforms
 
 include("../../fancy_toys.jl")
 
