10cf4e72210e9d1202834113456e9e5d6c889b5e
Gnimuc <qiyupei@gmail.com>
2021-07-12 00:52:13 +0900

    Fix `julia_compat`

--- L/libclangex/build_tarballs.jl
+++ L/libclangex/build_tarballs.jl
@@ -67,5 +67,5 @@ for (julia_version, llvm_version, clang_version) in supported
 
     # Build the tarballs.
     build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-                   preferred_gcc_version=v"8", julia_compat="1.6")
+                   preferred_gcc_version=v"8", julia_compat=string(julia_version))
 end
