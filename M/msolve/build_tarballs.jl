e78e6b6d82059c901da60c3d12b53b881439efa6
ederc <ederc@mathematik.uni-kl.de>
2021-07-23 12:45:06 +0200

    updates build recipe to use git repo and restrict FLINT version

--- M/msolve/build_tarballs.jl
+++ M/msolve/build_tarballs.jl
@@ -7,7 +7,8 @@ version = v"0.1.2"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://www.mathematik.uni-kl.de/~ederc/msolve-0.1.2.tar.gz", "ce6454b28477cb3b5670042faf7b3282e234fe1e0ee5a62c184d0512ef4126e1")
+    GitSource("https://gitlab.lip6.fr/safey/msolve.git", "e62794b7bd3b7c34fca5364017c278e116abc1ab"),
+    #= ArchiveSource("https://www.mathematik.uni-kl.de/~ederc/msolve-0.1.2.tar.gz", "ce6454b28477cb3b5670042faf7b3282e234fe1e0ee5a62c184d0512ef4126e1") =#
 ]
 
 # Bash recipe for building across all platforms
@@ -40,7 +41,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"), v"6.1.2"; compat="6.1.2")
-    Dependency(PackageSpec(name="FLINT_jll", uuid="e134572f-a0d5-539d-bddf-3cad8db41a82"))
+    Dependency(PackageSpec(name="FLINT_jll"), compat = "~200.700"),
     Dependency(PackageSpec(name="MPFR_jll", uuid="3a97d323-0669-5f0c-9066-3539efd106a3"))
 ]
 
