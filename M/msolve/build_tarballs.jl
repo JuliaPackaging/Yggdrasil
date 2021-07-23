f10f4fed59048400fddd652f379073b1093bcf6f
ederc <ederc@mathematik.uni-kl.de>
2021-07-23 13:50:54 +0200

    [msolve] use git repo and restrict FLINT version (#3370)
    * updates build recipe to use git repo and restrict FLINT version
    
    * fixes typo
    
    * fixes build folder
    
    * adds autotools step

--- M/msolve/build_tarballs.jl
+++ M/msolve/build_tarballs.jl
@@ -7,13 +7,15 @@ version = v"0.1.2"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://www.mathematik.uni-kl.de/~ederc/msolve-0.1.2.tar.gz", "ce6454b28477cb3b5670042faf7b3282e234fe1e0ee5a62c184d0512ef4126e1")
+    GitSource("https://gitlab.lip6.fr/safey/msolve.git", "e62794b7bd3b7c34fca5364017c278e116abc1ab"),
+    #= ArchiveSource("https://www.mathematik.uni-kl.de/~ederc/msolve-0.1.2.tar.gz", "ce6454b28477cb3b5670042faf7b3282e234fe1e0ee5a62c184d0512ef4126e1") =#
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd msolve-0.1.2/
+cd msolve/
+./autogen.sh
 ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes ./configure --with-gnu-ld --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-openmp
 make -j${nprocs}
 make install
@@ -40,7 +42,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"), v"6.1.2"; compat="6.1.2")
-    Dependency(PackageSpec(name="FLINT_jll", uuid="e134572f-a0d5-539d-bddf-3cad8db41a82"))
+    Dependency(PackageSpec(name="FLINT_jll"), compat = "~200.700")
     Dependency(PackageSpec(name="MPFR_jll", uuid="3a97d323-0669-5f0c-9066-3539efd106a3"))
 ]
 
