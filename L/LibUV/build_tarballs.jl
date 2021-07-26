45fbb18a945e15d591c4c9c99f2e6eff762b0490
Jameson Nash <vtjnash+github@gmail.com>
2021-07-26 15:18:18 -0400

    Upgrade libuv to julia-uv2-1.42.0

--- L/LibUV/build_tarballs.jl
+++ L/LibUV/build_tarballs.jl
@@ -6,7 +6,7 @@ version = v"2"
 # Collection of sources required to build libuv
 sources = [
     GitSource("https://github.com/JuliaLang/libuv.git",
-              "fb3e3364c33ae48c827f6b103e05c3f0e78b79a9"),
+              "c6869fba163a1e04af64ede438a8fd0191e75e9e"),
 ]
 
 # Bash recipe for building across all platforms
