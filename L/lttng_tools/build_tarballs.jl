968ff032e88d41430deecd8b5eb22f45cf41ea73
hros <2613469+hros@users.noreply.github.com>
2021-07-28 03:09:06 +0300

    Update L/lttng_tools/build_tarballs.jl
    supported_platforms
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/lttng_tools/build_tarballs.jl
+++ L/lttng_tools/build_tarballs.jl
@@ -20,12 +20,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc")
-]
+platforms = filter!(Sys.islinux, supported_platforms())
 
 
 # The products that we will ensure are always built
