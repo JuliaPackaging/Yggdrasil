e61cf00d51df61007390a386a93a9fbf65b91305
Mosè Giordano <mose@gnu.org>
2021-07-28 22:25:57 +0100

    [msquic] Some reorganisation

--- M/msquic/build_tarballs.jl
+++ M/msquic/build_tarballs.jl
@@ -7,8 +7,8 @@ version = v"1.5.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/microsoft/msquic.git", "a2c19d3ac386b8bb923e14d2607d42bffcfca0c7"),
-    DirectorySource("./bundled")
+    GitSource("https://github.com/microsoft/msquic.git",
+              "a2c19d3ac386b8bb923e14d2607d42bffcfca0c7"),
 ]
 
 # Bash recipe for building across all platforms
@@ -16,26 +16,26 @@ script = raw"""
 cd $WORKSPACE/srcdir/msquic
 git submodule update --init --recursive
 mkdir build && cd build
-cmake -G 'Unix Makefiles' ..
-cmake --build .
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+make -j${nproc}
+make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    
-]
-
+platforms = filter!(Sys.islinux, supported_platforms())
 
 # The products that we will ensure are always built
-products = [
-
+products = Product[
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="lttng_ust_jll", uuid="a2826780-45ff-53db-9dda-fd961bc58de1"))
-    Dependency(PackageSpec(name="lttng_tools_jll", uuid="37591b3e-a3c2-50e0-b676-8bc36018f336"))
+    Dependency(PackageSpec(name="lttng_ust_jll", uuid="a2826780-45ff-53db-9dda-fd961bc58de1")),
+    Dependency(PackageSpec(name="lttng_tools_jll", uuid="37591b3e-a3c2-50e0-b676-8bc36018f336")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
