5e95e7c4240a380a1587325c6857f540801e329e
Mosè Giordano <mose@gnu.org>
2021-07-28 15:21:28 +0100

    [GTK3] Update to v3.24.30 and build for experimental platforms

--- G/GTK3/build_tarballs.jl
+++ G/GTK3/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder
 
 name = "GTK3"
-version = v"3.24.29"
+version = v"3.24.30"
 
 # Collection of sources required to build GTK
 sources = [
     ArchiveSource("http://ftp.gnome.org/pub/gnome/sources/gtk+/$(version.major).$(version.minor)/gtk+-$(version).tar.xz",
-                  "f57ec4ade8f15cab0c23a80dcaee85b876e70a8823d9105f067ce335a8268caa"),
+                  "ba75bfff320ad1f4cfbee92ba813ec336322cc3c660d406aad014b07087a3ba9"),
     DirectorySource("./bundled"),
 ]
 
@@ -19,36 +19,20 @@ cd $WORKSPACE/srcdir/gtk+-*/
 # We need to run some commands with a native Glib
 apk add glib-dev gtk+3.0
 
-if [[ "${target}" == *-linux-* ]]; then
-    # We need to run `wayland-scanner` on the build system only when Wayland is
-    # available, i.e. only on Linux
-    apk add wayland-dev
-fi
-
 # This is awful, I know
 ln -sf /usr/bin/glib-compile-resources ${prefix}/bin/glib-compile-resources
 ln -sf /usr/bin/glib-compile-schemas ${prefix}/bin/glib-compile-schemas
 ln -sf /usr/bin/gdk-pixbuf-pixdata ${prefix}/bin/gdk-pixbuf-pixdata
-# Remove wayland-scanner when present, so that we can call the native one
-rm -f ${prefix}/bin/wayland-scanner
+# Remove gio-2.0 pkgconfig file so that it isn't picked up by post-install script.
+rm ${prefix}/lib/pkgconfig/gio-2.0.pc
 
-# Patch upstreamed: https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/3526
-atomic_patch -p1 $WORKSPACE/srcdir/patches/gdkwindow-quartz_c.patch
 atomic_patch -p1 $WORKSPACE/srcdir/patches/meson_build.patch
-# Patch upstreamed: https://gitlab.gnome.org/GNOME/gtk/-/merge_requests/3527
-atomic_patch -p1 $WORKSPACE/srcdir/patches/c_standard_c99.patch
 
 FLAGS=()
 if [[ "${target}" == *-apple-* ]]; then
     FLAGS+=(-Dx11_backend=false -Dwayland_backend=false)
 elif [[ "${target}" == *-freebsd* ]]; then
     FLAGS+=(-Dwayland_backend=false)
-fi
-
-if [[ "${target}" == *-mingw* ]]; then
-    # Despite what's declared in their meson.build, they require Meson 0.54:
-    # https://gitlab.gnome.org/GNOME/gtk/-/issues/3927
-    apk add meson --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing
 fi
 
 mkdir build-gtk && cd build-gtk
@@ -68,7 +52,7 @@ rm ${prefix}/bin/gdk-pixbuf-pixdata ${prefix}/bin/glib-compile-{resources,schema
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = filter!(p -> arch(p) != "armv6l", supported_platforms(; experimental=true))
 
 # The products that we will ensure are always built
 products = Product[
@@ -79,16 +63,18 @@ products = Product[
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    # Need a host Wayland for wayland-scanner
+    HostBuildDependency("Wayland_jll"),
     BuildDependency("Xorg_xorgproto_jll"),
-    Dependency("Glib_jll", v"2.59.0"; compat="2.59.0"),
+    Dependency("Glib_jll"; compat="2.68.1"),
     Dependency("Cairo_jll"),
-    Dependency("Pango_jll", v"1.42.4"; compat="1.42.4"),
+    Dependency("Pango_jll"; compat="1.47.0"),
     Dependency("FriBidi_jll"),
     Dependency("FreeType2_jll"),
     Dependency("gdk_pixbuf_jll"),
     Dependency("Libepoxy_jll"),
     # Gtk 3.24.29 requires ATK 2.35.1
-    Dependency("ATK_jll", v"2.36"; compat="2.35.1"),
+    Dependency("ATK_jll", v"2.36.1"; compat="2.35.1"),
     Dependency("HarfBuzz_jll"),
     Dependency("xkbcommon_jll"),
     Dependency("iso_codes_jll"),
@@ -108,4 +94,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
