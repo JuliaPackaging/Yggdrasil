d6a0fc54243014f6892318621d8a7913f3b69451
Chris de Graaf <me@cdg.dev>
2021-07-31 21:08:58 +0000

    Build yq

---
+++ Y/yq/build_tarballs.jl
@@ -0,0 +1,33 @@
+using BinaryBuilder
+
+name = "yq"
+
+version = v"0.5.0"
+
+# Collection of sources required to build yq
+sources = [
+    GitSource("https://github.com/mikefarah/yq", "f89a1335589031ef845f8c7829ee709b15ab65d5"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/yq
+mkdir -p ${bindir}
+go build -o ${bindir}
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("yq", :yq),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers=[:c, :go])
