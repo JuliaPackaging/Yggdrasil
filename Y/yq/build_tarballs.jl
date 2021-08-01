db5adbfc75cb27ea773128ef29b9c8d9f2f33e5a
Mosè Giordano <giordano@users.noreply.github.com>
2021-08-01 15:52:41 +0100

    [yq] Build also for experimental platforms

--- Y/yq/build_tarballs.jl
+++ Y/yq/build_tarballs.jl
@@ -18,7 +18,7 @@ go build -o ${bindir}
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
