204222be7f2c7360a3c7390090023ac2d6b4144f
Tim Besard <tim.besard@gmail.com>
2021-08-05 17:05:06 +0200

    Upgrade CUDNN to v8.2.2. (#3433)

--- C/CUDNN/build_11.jl
+++ C/CUDNN/build_11.jl
@@ -1,11 +1,14 @@
 platforms_and_sources = Dict(
     Platform("x86_64", "linux") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.0.53/11.3_04222021/cudnn-11.3-linux-x64-v8.2.0.53.tgz",
-                      "7a195dc93a7cda2bdd4d9b73958d259c784be422cd941a9a625aab75309f19dc")],
+        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.2/11.4_07062021/cudnn-11.4-linux-x64-v8.2.2.26.tgz",
+                      "fbc631ce19688e87d7d2420403b20db97885b17f718f0f51d7e9fc0905d86e07")],
     Platform("powerpc64le", "linux") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.0.53/11.3_04222021/cudnn-11.3-linux-ppc64le-v8.2.0.53.tgz",
-                      "cfe06735671a41a5e25fc7542d740177ac8eab1ab146bd30f19e0fa836895611")],
+        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.2/11.4_07062021/cudnn-11.4-linux-ppc64le-v8.2.2.26.tgz",
+                      "b11b9e515a86978dc21ab50a7d2320bfb505cbce9dffa25480225c597c682b43")],
+    Platform("aarch64", "linux") => [
+        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.2/11.4_07062021/cudnn-11.4-linux-aarch64sbsa-v8.2.2.26.tgz",
+                      "e240d45d79eecb2257fcb8a219324f19d8e2d6e145fbd035a38d267580d65e9a")],
     Platform("x86_64", "windows") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.0.53/11.3_04222021/cudnn-11.3-windows-x64-v8.2.0.53.zip",
-                      "c8f108f8b646fd22ab35da7a7d73e2d977f671a1abd3364d372292102e0d7429")],
+        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.2/11.4_07062021/cudnn-11.4-windows-x64-v8.2.2.26.zip",
+                      "fce18f1f515a480e33fa3df8bfd8616463ee44b101e6e149d46f29a66c1c306e")],
 )
