07133bd66460e2d818ca74c513d580b4a2033f06
Mosè Giordano <giordano@users.noreply.github.com>
2021-08-06 18:11:50 +0100

    Update Y/Yosys/build_tarballs.jl

--- Y/Yosys/build_tarballs.jl
+++ Y/Yosys/build_tarballs.jl
@@ -33,6 +33,7 @@ make install PREFIX=${prefix}
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = filter!(p -> Sys.isapple(p) || Sys.islinux(p), supported_platforms())
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = Product[
