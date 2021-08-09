6dbbd1a5b9dac56e452145607bf222baabde548d
Bart Janssens <bart@bartjanssens.org>
2021-08-09 20:19:23 +0200

    Libjulia 1.5.4 (#3418)
    * Add Julia 1.5.4
    
    * Build libjulia 1.5.4
    
    * Fix macOS crash
    
    * Keep both libjulia 1.5.4 and 1.4.3 hashes
    
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- L/libjulia/common.jl
+++ L/libjulia/common.jl
@@ -28,6 +28,7 @@ function build_julia(ARGS, version::VersionNumber)
         v"1.3.1" => "3d9037d281fb41ad67b443f42d8a8e400b016068d142d6fafce1952253ae93db",
         v"1.4.2" => "76a94e06e68fb99822e0876a37c2ed3873e9061e895ab826fd8c9fc7e2f52795",
         v"1.5.3" => "be19630383047783d6f314ebe0bf5e3f95f82b0c203606ec636dced405aab1fe",
+        v"1.5.4" => "852122bf1bdefd39307b1dd2aa546e3885d76ede7c07cb04d90814b9510ea9f9",
         v"1.6.0" => "c253360d29abb9a3a9e6e01493e0f7bb537d88014fd58ac561b5ba30fcb44cad",
         v"1.7.0-beta3" => "d2f30bd0b25b9cb8c837276342710643dd9420baaa4d27bcb583a3c7f66563f4",
     )
@@ -84,9 +85,9 @@ function build_julia(ARGS, version::VersionNumber)
     # a HostDependency, now that we have those
     LLVM_CXXFLAGS="-I${prefix}/include -fno-exceptions -fno-rtti -D_GNU_SOURCE -D__STDC_CONSTANT_MACROS -D__STDC_FORMAT_MACROS -D__STDC_LIMIT_MACROS"
     if [[ "${version}" == 1.[0-5].* ]]; then
-        LLVM_CXXFLAGS="${LLVM_CXXFLAGS} -std=c++11""
+        LLVM_CXXFLAGS="${LLVM_CXXFLAGS} -std=c++11"
     else
-        LLVM_CXXFLAGS="${LLVM_CXXFLAGS} -std=c++14""
+        LLVM_CXXFLAGS="${LLVM_CXXFLAGS} -std=c++14"
     fi
     LLVM_LDFLAGS="-L${prefix}/lib"
     LDFLAGS="-L${prefix}/lib"
@@ -269,7 +270,6 @@ function build_julia(ARGS, version::VersionNumber)
 
     dependencies = BinaryBuilder.AbstractDependency[
         Dependency("LibUnwind_jll"),
-        Dependency("LibUV_jll"),
         BuildDependency("OpenLibm_jll"),
         BuildDependency("dSFMT_jll"),
         BuildDependency("utf8proc_jll"),
@@ -286,16 +286,17 @@ function build_julia(ARGS, version::VersionNumber)
         push!(dependencies, Dependency("LibOSXUnwind_jll", compat="0.0.5"))
     elseif version < v"1.7"
         push!(dependencies, Dependency("LibOSXUnwind_jll", compat="0.0.6"))
+        push!(dependencies, Dependency("LibUV_jll"))
     end
 
     if version < v"1.6"
-        push!(dependencies, BuildDependency("SuiteSparse_jll", compat="5.4.0"))
+        push!(dependencies, BuildDependency(PackageSpec(name="SuiteSparse_jll", version="5.4.0")))
     else
         push!(dependencies, BuildDependency("SuiteSparse_jll"))
     end
 
     if version < v"1.7"
-        push!(dependencies, BuildDependency("PCRE2_jll", compat="10.31"))
+        push!(dependencies, BuildDependency(PackageSpec(name="PCRE2_jll", version="10.31")))
     #else
     #    push!(dependencies, BuildDependency("PCRE2_jll", compat="10.36"))
     end
@@ -310,9 +311,9 @@ function build_julia(ARGS, version::VersionNumber)
         push!(dependencies, Dependency("libLLVM_jll", compat="8.0.1"))
         push!(dependencies, BuildDependency("LibGit2_jll", compat="0.28.2"))
     elseif version.major == 1 && version.minor == 5
-        push!(dependencies, BuildDependency("OpenBLAS_jll", compat="0.3.9"))
+        push!(dependencies, BuildDependency(PackageSpec(name="OpenBLAS_jll", version="0.3.9")))
         push!(dependencies, Dependency("libLLVM_jll", compat="9.0.1"))
-        push!(dependencies, BuildDependency("LibGit2_jll", compat="0.28.2"))
+        push!(dependencies, BuildDependency(PackageSpec(name="LibGit2_jll", version="0.28.2")))
     elseif version.major == 1 && version.minor == 6
         push!(dependencies, BuildDependency("OpenBLAS_jll", compat="0.3.10"))
         push!(dependencies, Dependency("libLLVM_jll", compat="11.0.0"))
@@ -335,7 +336,7 @@ function build_julia(ARGS, version::VersionNumber)
         error("Unsupported Julia version")
     end
 
-    julia_compat = version ≥ v"1.7" ? "1.6" : "1.0"
+    julia_compat = version ≥ v"1.6" ? "1.6" : "1.0"
 
     build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                    preferred_gcc_version=v"7", lock_microarchitecture=false, julia_compat)
