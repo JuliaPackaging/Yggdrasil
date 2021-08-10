85fdefdb6db3d47eb32345e518fdc871de2e6657
ig-or <19777752+ig-or@users.noreply.github.com>
2021-08-10 11:50:45 -0700

    [qwtw] updated to version 2.6.0

--- Q/qwtw/build_tarballs.jl
+++ Q/qwtw/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "qwtw"
-version = v"2.5.0"
+version = v"2.6.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/ig-or/qwtw.git", "de764350ba1fe57926349fe0d4d0f817bfdaee38")
+    GitSource("https://github.com/ig-or/qwtw.git", "7dcf526775c80c19e158449e858e496a0f6692cf")
 ]
 
 # Bash recipe for building across all platforms
