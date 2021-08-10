574fc0326d4c32111d9708cf80e0747851348bcb
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-08-10 08:33:52 +0200

    Update P/PaPILO/build_tarballs.jl

--- P/PaPILO/build_tarballs.jl
+++ P/PaPILO/build_tarballs.jl
@@ -22,7 +22,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
     ..
-make -j${nprocs}
+make
 make install
 """
 
