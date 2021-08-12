613287275e61cfdd5817909e3a7081375cf579fe
Alex Arslan <ararslan@comcast.net>
2021-08-12 14:13:28 -0700

    [GMP] Touch build_tarballs.jl to trigger a rebuild (#3479)
    This touches GMP 6.2.0 (for Julia 1.6 + MPFR) and 6.2.1 (newer Julia).
    It leaves 6.1.2 alone, as it's incompatble with Julia < 1.6.

--- G/GMP/GMP@6.2.1/build_tarballs.jl
+++ G/GMP/GMP@6.2.1/build_tarballs.jl
@@ -2,7 +2,7 @@ version = v"6.2.1"
 
 include("../common.jl")
 
-# Build the tarballs
+# Build the tarballs!
 build_tarballs(ARGS, configure(version)...;
                preferred_gcc_version=v"6", julia_compat="1.7")
 
