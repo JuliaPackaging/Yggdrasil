87c07cf254e910db2ec76cfa8dabdc98167e8657
WooKyoung Noh <wookay.noh@gmail.com>
2021-08-13 01:22:51 +0900

    Update H/H3/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- H/H3/build_tarballs.jl
+++ H/H3/build_tarballs.jl
@@ -39,7 +39,7 @@ ls $WORKSPACE/destdir/lib
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
