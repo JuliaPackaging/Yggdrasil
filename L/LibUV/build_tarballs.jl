88f6dc126b95a8920e09fbbb7023db3cd8db29c5
Alex Arslan <ararslan@comcast.net>
2021-08-12 13:27:38 -0700

    [LibUV] Touch build_tarballs.jl to trigger a rebuild (#3476)

--- L/LibUV/build_tarballs.jl
+++ L/LibUV/build_tarballs.jl
@@ -19,7 +19,7 @@ touch -c Makefile.in
 touch -c configure
 
 # `--with-pic` isn't enough; we really really need -fPIC and -DPIC everywhere...
-# everywhere, especially on FreeBSD
+# everywhere, especially on FreeBSD. In the end, isn't FreeBSD all that matters?
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-pic CFLAGS="${CFLAGS} -DPIC -fPIC" CXXFLAGS="${CXXFLAGS} -DPIC -fPIC"
 make -j${nproc} V=1
 make install
