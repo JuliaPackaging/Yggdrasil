853acd5f848c537ea521b0bc8efc06bbd9ab302d
Alex Arslan <ararslan@comcast.net>
2021-08-12 18:18:06 -0700

    [OpenBLAS] Touch build_tarballs.jl to trigger a rebuild (#3486)
    0.3.10 and 0.3.13 only

--- O/OpenBLAS/OpenBLAS@0.3.13/build_tarballs.jl
+++ O/OpenBLAS/OpenBLAS@0.3.13/build_tarballs.jl
@@ -12,8 +12,6 @@ platforms = openblas_platforms(;experimental=true)
 products = openblas_products()
 dependencies = openblas_dependencies()
 
-
 # Build the tarballs
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                preferred_gcc_version=v"6", lock_microarchitecture=false, julia_compat="1.7")
-
