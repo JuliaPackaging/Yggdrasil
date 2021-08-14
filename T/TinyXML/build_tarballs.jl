c16caa67f45252b4ab5015fa67d95490b1afb98d
ASUS1 <crghilardi@yahoo.com>
2021-08-14 12:14:56 -0700

    use FileProduct instead of ExecutableProduct

--- T/TinyXML/build_tarballs.jl
+++ T/TinyXML/build_tarballs.jl
@@ -38,7 +38,7 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    ExecutableProduct("xmltest", :xmltest)
+    FileProduct("bin/xmltest", :xmltest)
 ]
 
 # Dependencies that must be installed before this package can be built
