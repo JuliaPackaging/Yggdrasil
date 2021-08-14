6e2c22c3505c2c0f98a9d3ad0495391931589544
Mosè Giordano <giordano@users.noreply.github.com>
2021-08-14 22:23:42 +0100

    Update build_tarballs.jl

--- T/TinyXML/build_tarballs.jl
+++ T/TinyXML/build_tarballs.jl
@@ -7,38 +7,35 @@ version = v"2.6.2"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://downloads.sourceforge.net/project/tinyxml/tinyxml/2.6.2/tinyxml_2_6_2.tar.gz", "15bdfdcec58a7da30adc87ac2b078e4417dbe5392f3afb719f9ba6d062645593")
+    ArchiveSource("https://downloads.sourceforge.net/project/tinyxml/tinyxml/$(version)/tinyxml_$(version.major)_$(version.minor)_$(version.patch).tar.gz",
+                  "15bdfdcec58a7da30adc87ac2b078e4417dbe5392f3afb719f9ba6d062645593"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-
 cd $WORKSPACE/srcdir/tinyxml
 
-g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3   tinyxml.cpp -o tinyxml.o
-g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 tinyxmlparser.cpp -o tinyxmlparser.o
-g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 xmltest.cpp -o xmltest.o
-g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3   tinyxmlerror.cpp -o tinyxmlerror.o
-g++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3   tinystr.cpp -o tinystr.o
-g++ -o xmltest tinyxml.o tinyxmlparser.o xmltest.o tinyxmlerror.o tinystr.o  
-
-mkdir -p ${bindir}
-cp xmltest ${bindir}
+c++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 tinyxml.cpp -o tinyxml.o
+c++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 tinyxmlparser.cpp -o tinyxmlparser.o
+c++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 xmltest.cpp -o xmltest.o
+c++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 tinyxmlerror.cpp -o tinyxmlerror.o
+c++ -c -Wall -Wno-unknown-pragmas -Wno-format -O3 tinystr.cpp -o tinystr.o
+mkdir -p "${bindir}"
+c++ -o "${bindir}/xmltest${exeext}" tinyxml.o tinyxmlparser.o xmltest.o tinyxmlerror.o tinystr.o  
 
 install_license readme.txt
 
-mkdir -p ${includedir}
-cp *.h ${includedir}
+mkdir -p "${includedir}"
+cp *.h "${includedir}/."
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
-    FileProduct("bin/xmltest", :xmltest)
+    ExecutableProduct("xmltest", :xmltest)
 ]
 
 # Dependencies that must be installed before this package can be built
