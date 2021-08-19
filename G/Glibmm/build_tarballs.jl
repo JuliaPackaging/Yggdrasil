736d9a6d0d348c04559a119c413444ff5bc9ef2e
Pepijn de Vos <pepijndevos@gmail.com>
2021-08-19 16:05:41 +0200

    Update build_tarballs.jl

--- G/Glibmm/build_tarballs.jl
+++ G/Glibmm/build_tarballs.jl
@@ -24,6 +24,7 @@ ninja install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
