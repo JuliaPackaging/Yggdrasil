df9f4e6d275c320c6519559a19ad2b33ed4610e6
Mosè Giordano <giordano@users.noreply.github.com>
2021-08-19 22:43:46 +0100

    [Glibmm] Fix library names

--- G/Glibmm/build_tarballs.jl
+++ G/Glibmm/build_tarballs.jl
@@ -7,35 +7,34 @@ version = v"2.68.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://download.gnome.org/sources/glibmm/2.68/glibmm-2.68.1.tar.xz", "6664e27c9a9cca81c29e35687f49f2e0d173a2fc9e98c3428311f707db532f8c")
+    ArchiveSource("https://download.gnome.org/sources/glibmm/$(version.major).$(version.minor)/glibmm-$(version).tar.xz",
+                  "6664e27c9a9cca81c29e35687f49f2e0d173a2fc9e98c3428311f707db532f8c")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
+cd $WORKSPACE/srcdir/glibmm*/
 apk add doxygen graphviz libxslt
-cd glibmm-2.68.1/
 meson --cross-file=${MESON_TARGET_TOOLCHAIN} --libdir lib builddir .
 cd builddir/
-ninja
+ninja -j${nproc}
 ninja install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-platforms = expand_cxxstring_abis(platforms)
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libglibmm", :glibmm),
-    LibraryProduct("libglibmm_generate_extra_defs", :glibmm_gen),
-    LibraryProduct("libgiomm", :giomm)
+    LibraryProduct("libglibmm-$(version.major).$(version.minor)", :libglibmm),
+    LibraryProduct("libglibmm_generate_extra_defs-$(version.major).$(version.minor)", :libglibmm_generate_extra_defs),
+    LibraryProduct("libgiomm-$(version.major).$(version.minor)", :libgiomm)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Glib_jll", uuid="7746bdde-850d-59dc-9ae8-88ece973131d"))
+    Dependency(PackageSpec(name="Glib_jll", uuid="7746bdde-850d-59dc-9ae8-88ece973131d"); compat=version)
     Dependency(PackageSpec(name="libsigcpp_jll", uuid="8855df87-3ca1-5a3d-a68b-4f0ddc198ba8"))
 ]
 
