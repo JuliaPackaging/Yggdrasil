31d9463ebae72fee4bc407940cbb599a709a60ef
Pepijn de Vos <pepijndevos@gmail.com>
2021-08-20 16:54:46 +0200

    only intel for now

--- S/scopehal/build_tarballs.jl
+++ S/scopehal/build_tarballs.jl
@@ -33,6 +33,7 @@ install_license scopehal/LICENSE
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = filter!(p -> BinaryBuilder.proc_family(p) == "intel", platforms)
 platforms = expand_cxxstring_abis(platforms; skip=p->false)
 
 
