3aebc98f79c90c8c15b117f5e8a9045a29780eab
Pepijn de Vos <pepijndevos@gmail.com>
2021-08-20 09:57:51 +0200

    Update build_tarballs.jl

--- P/Pangomm/build_tarballs.jl
+++ P/Pangomm/build_tarballs.jl
@@ -23,26 +23,12 @@ ninja install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("x86_64", "macos"; ),
-    Platform("x86_64", "freebsd"; ),
-    Platform("i686", "windows"; ),
-    Platform("x86_64", "windows"; )
-]
-platforms = expand_cxxstring_abis(platforms)
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
 
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libpangomm", :pangomm)
+    LibraryProduct("libpangomm-2.48", :pangomm)
 ]
 
 # Dependencies that must be installed before this package can be built
