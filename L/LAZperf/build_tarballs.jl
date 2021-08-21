6be648d19834eae6ed790499c530cfc8f07e71af
ASUS1 <crghilardi@yahoo.com>
2021-08-21 08:49:01 -0700

    change folder and variable naming to LAZperf

--- L/LAZperf/build_tarballs.jl
+++ L/LAZperf/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "laz_perf"
+name = "LAZperf"
 version = v"2.1.0"
 
 # Collection of sources required to complete build
