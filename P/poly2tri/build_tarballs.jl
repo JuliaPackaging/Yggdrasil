ca8cfdf7951f9e57d40d400382351f9ec8f00769
Mosè Giordano <giordano@users.noreply.github.com>
2021-08-22 23:06:10 +0100

    Update build_tarballs.jl

--- P/poly2tri/build_tarballs.jl
+++ P/poly2tri/build_tarballs.jl
@@ -26,13 +26,12 @@ cmake -GNinja . \
 -DP2T_BUILD_TESTBED=OFF
 
 cmake --build .
-ninja install
-
+ninja -j${nproc} install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms())
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
 
 # The products that we will ensure are always built
 products = [
