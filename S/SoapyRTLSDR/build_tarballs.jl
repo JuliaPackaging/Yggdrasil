e3221c7ab042339f68ac96cd3515744815d91728
Steve Kelly <kd2cca@gmail.com>
2021-08-25 14:08:33 -0400

    [SoapyRTLSDR] rename
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- S/SoapyRTLSDR/build_tarballs.jl
+++ S/SoapyRTLSDR/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "soapyrtlsdr"
+name = "SoapyRTLSDR"
 version = v"0.3.2"
 
 # Collection of sources required to complete build
