8c653160ac864460b7aa86ba64e7858d90d6018c
Alex Arslan <ararslan@comcast.net>
2021-08-27 11:22:52 -0700

    [LLVM_full@12.0.1] Crank GCC up to 11

--- L/LLVM/LLVM_full@12.0.1/build_tarballs.jl
+++ L/LLVM/LLVM_full@12.0.1/build_tarballs.jl
@@ -4,4 +4,4 @@ include("../common.jl")
 
 # Go forth and build
 build_tarballs(ARGS, configure_build(ARGS, version; experimental_platforms=true)...;
-               preferred_gcc_version=v"7", preferred_llvm_version=v"9", julia_compat="1.7")
+               preferred_gcc_version=v"11", preferred_llvm_version=v"9", julia_compat="1.7")
