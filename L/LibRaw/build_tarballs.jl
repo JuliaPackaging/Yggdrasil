30c6d900adc7bbbe2f4bf080e428e58d2ae00f8c
Tracy Wadleigh <tracy@compoundeye.com>
2021-08-31 12:07:50 -0700

    Add extra dependencies.
    The build currently breaks with JasPer enabled so it is currently
    explicitly disabled.

--- L/LibRaw/build_tarballs.jl
+++ L/LibRaw/build_tarballs.jl
@@ -14,8 +14,8 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd LibRaw-*
-autoreconf
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+autoreconf --install
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-jasper
 make -j${nproc}
 make install
 exit
@@ -28,14 +28,28 @@ platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
+    ExecutableProduct("mem_image", :mem_image),
+    ExecutableProduct("postprocessing_benchmark", :postprocessing_benchmark),
+    LibraryProduct("libraw", :libraw),
+    ExecutableProduct("raw-identify", :raw_identify),
+    ExecutableProduct("4channels", :_4channels),
+    ExecutableProduct("half_mt", :half_mt),
+    ExecutableProduct("multirender_test", :multirender_test),
     LibraryProduct("libraw_r", :libraw_r),
-    LibraryProduct("libraw", :libraw)
+    ExecutableProduct("dcraw_emu", :dcraw_emu),
+    ExecutableProduct("simple_dcraw", :simple_dcraw),
+    ExecutableProduct("unprocessed_raw", :unprocessed_raw),
+    ExecutableProduct("rawtextdump", :rawtextdump),
+    ExecutableProduct("dcraw_half", :dcraw_half)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a")),
+    Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a"))
+    Dependency(PackageSpec(name="JpegTurbo_jll", uuid="aacddb02-875f-59d6-b918-886e6ef4fbf8"))
+    Dependency(PackageSpec(name="JasPer_jll", uuid="f6ca3df5-1ae7-521a-b96b-cb83f57f3f1f"))
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+    Dependency(PackageSpec(name="LittleCMS_jll", uuid="d3a379c0-f9a3-5b72-a4c0-6bf4d2e8af0f"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
