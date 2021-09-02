635a545cdec1d0c753b0aa71d80eef8ab728cc0d
XUE Cheng <xuecheng@nuaa.edu.cn>
2021-09-02 21:35:07 +0800

    fix hash mismatch

--- G/gmsh/build_tarballs.jl
+++ G/gmsh/build_tarballs.jl
@@ -7,7 +7,7 @@ version = v"4.8.4"
 
 # Collection of sources required to build Gmsh
 sources = [
-    ArchiveSource("https://gmsh.info/src/gmsh-$version-source.tgz", "d5038f5f25ae85973536fb05cc886feb1bd7d67e2605a9d8789bcc2528fa8b35")
+    ArchiveSource("https://gmsh.info/src/gmsh-$version-source.tgz", "760dbdc072eaa3c82d066c5ba3b06eacdd3304eb2a97373fe4ada9509f0b6ace")
 ]
 
 # Bash recipe for building across all platforms
