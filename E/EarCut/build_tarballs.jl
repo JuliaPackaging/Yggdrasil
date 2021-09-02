61104cd1ae1a40cf8dc5511d8201b48f08ac3d0b
SimonDanisch <sdanisch@protonmail.com>
2021-09-02 11:04:52 +0200

    clean up EarCut sources + m1 support

--- E/EarCut/build_tarballs.jl
+++ E/EarCut/build_tarballs.jl
@@ -3,23 +3,26 @@
 using BinaryBuilder
 
 name = "EarCut"
-version = v"2.1.5"
-# Collection of sources required to build Clipper
+# Version is a bit unclear with the git wrapper, but commit says 2.2.3:
+# https://github.com/mapbox/earcut.hpp/commit/966abaa052cafbdcc7fdc9c61eceabfd8b8d38c4
+version = v"2.2.3"
 sources = [
-    GitSource("https://github.com/SimonDanisch/EarCutBuilder.git",
-              "cfa4233e26ac785a89954a72b4e2e84312b389c2"),
+    GitSource("https://github.com/mapbox/earcut.hpp.git",
+              "966abaa052cafbdcc7fdc9c61eceabfd8b8d38c4"),
+    DirectorySource("./bundled")
+
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/EarCutBuilder/
+cp $WORKSPACE/srcdir/earcut.hpp/include/mapbox/earcut.hpp ./earcut.h
 mkdir "${libdir}"
 ${CXX} -std=c++11 -fPIC -shared -o "${libdir}/libearcut.${dlext}" cwrapper.cpp
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -31,4 +34,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
