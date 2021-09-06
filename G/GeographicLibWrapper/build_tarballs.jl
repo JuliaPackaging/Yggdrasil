8770d0357a69ccd2a117d861db64f5cd0c9b7145
Tracy Wadleigh <tracy@compoundeye.com>
2021-09-06 12:16:44 -0700

    Add GeographicLibWrapper-0.1.0

---
+++ G/GeographicLibWrapper/build_tarballs.jl
@@ -0,0 +1,53 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "GeographicLibWrapper"
+version = v"0.1.0"
+
+julia_version = v"1.6"
+
+sources = [DirectorySource("./src")]
+
+script = raw"""
+mkdir -p build
+cd build
+cmake \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_FIND_ROOT_PATH=${prefix} \
+    -DJulia_PREFIX=${prefix} \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+cmake --build . --config Release --target install -- -j${nproc}
+install_license /usr/share/licenses/MIT
+"""
+
+include("../../L/libjulia/common.jl")
+platforms = expand_cxxstring_abis(libjulia_platforms(julia_version))
+
+products = [LibraryProduct("libGeographicLibWrapper", :libGeographicLibWrapper)]
+
+dependencies = [
+    Dependency(
+        PackageSpec(;
+            name="GeographicLib_jll",
+            uuid="a660ed4b-f19b-5b77-bc0d-0bc97335ea30",
+            url="https://github.com/twadleigh/GeographicLib_jll.jl",
+        ),
+    ),
+    Dependency("libcxxwrap_julia_jll"),
+    BuildDependency(PackageSpec(; name="libjulia_jll", version=julia_version)),
+]
+
+build_tarballs(
+    ARGS,
+    name,
+    version,
+    sources,
+    script,
+    platforms,
+    products,
+    dependencies;
+    preferred_gcc_version=v"7",
+)
