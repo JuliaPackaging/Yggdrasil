0bf84535b7a7a1a5d7c7a6f85f70c0949a9de97e
Max Horn <max@quendi.de>
2021-09-06 12:12:25 +0200

    Create JLLs for some GAP packages
    Unfortunately GAP_jll produce an unusable `gac` script, and also
    `sysinfo.gap` is problematic. This will be fixed in a future version of
    GAP_jll; for now it was easiest to include fixed versions of these
    generated files in this patch.

---
+++ G/GAP_pkg/GAP_pkg_ferret/build_tarballs.jl
@@ -0,0 +1,43 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+include("../common.jl")
+
+gap_version = v"400.1190.200"
+name = "ferret"
+upstream_version = v"1.0.5" # when you increment this, reset offset to v"0.0.0"
+offset = v"0.0.0" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
+version = offset_version(upstream_version, offset)
+
+# Collection of sources required to build libsingular-julia
+sources = [
+    ArchiveSource("https://github.com/gap-packages/$(name)/releases/download/v$(upstream_version)/$(name)-$(upstream_version).tar.gz",
+                  "08ae9cd65c5e086962ca9025d5d51e03b1cc1ccaa39f52aeeff47bf79c6c17e8"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ferret*
+
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/0001-Replace-u_int32_t-uint32_t.patch
+
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-gaproot=${prefix}/share/gap/
+make -j${nproc}
+
+# copy just the loadable module
+mkdir -p ${prefix}/lib/gap/
+cp bin/*/*.so ${prefix}/lib/gap/
+
+install_license LICENSE
+"""
+
+name = gap_pkg_name(name)
+platforms, dependencies = setup_gap_package(gap_version; uses_cxx = true)
+
+# The products that we will ensure are always built
+products = [
+    FileProduct("lib/gap/ferret.so", :ferret),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               preferred_gcc_version=v"7")
