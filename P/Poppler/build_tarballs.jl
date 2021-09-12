a29d03cc7c00c385d71e3aa1a10ecc71dffba31b
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2021-09-12 13:56:36 +0200

    Fix keyword argument

--- P/Poppler/build_tarballs.jl
+++ P/Poppler/build_tarballs.jl
@@ -38,7 +38,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(; experimental_platforms=true))
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
 
 # The products that we will ensure are always built
 products = [
