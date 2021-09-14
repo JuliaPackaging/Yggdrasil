b3f30588d2a7d51de6e1a729f46c0a0a6ab216d1
Jerry Ling <proton@jling.dev>
2021-09-13 22:07:54 -0400

    Update build_tarballs.jl

--- Z/zlibng/build_tarballs.jl
+++ Z/zlibng/build_tarballs.jl
@@ -20,12 +20,7 @@ make install -j
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("x86_64", "macos"; ),
-    Platform("x86_64", "freebsd"; )
-]
+platforms = supported_platforms(;experimental=true)
 
 
 # The products that we will ensure are always built
