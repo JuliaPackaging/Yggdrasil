eaaf11cee1bb87966208efd62b1a18d3a699e9aa
Gabriel Wu <wuzihua@pku.edu.cn>
2021-09-14 09:18:34 +0800

    New Recipe: TMatrix v0.1.0

---
+++ T/TMatrix/build_tarballs.jl
@@ -0,0 +1,36 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "TMatrix"
+version = v"0.1.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/JuliaRemoteSensing/TMatrix-fortran.git", "87a098629bfe40ac28d6aa925f8884d7d7fef5c0")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/TMatrix-fortran/
+mkdir build && cd build
+meson --cross-file=${MESON_TARGET_TOOLCHAIN}
+meson install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libtmatrixro", :tmatrix_random_orientation),
+    LibraryProduct("libtmatrixfo", :tmatrix_fixed_orientation)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
