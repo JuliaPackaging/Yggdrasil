e63db72cc62a5574a25767f8d16a2a6353fdbbe4
Kimmo Linna <kimmo.linna@gmail.com>
2021-09-30 22:47:41 +0300

    Updated DuckDB to version 0.2.9

--- D/DuckDB/build_tarballs.jl
+++ D/DuckDB/build_tarballs.jl
@@ -3,32 +3,20 @@
 using BinaryBuilder, Pkg
 
 name = "DuckDB"
-version = v"0.2.1"
+version = v"0.2.9"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/cwida/duckdb.git", "d9bceddc7209a7e0b5c0402958d1191e19a491e7")
+    ArchiveSource("https://github.com/duckdb/duckdb/archive/refs/tags/v0.2.9.tar.gz", "ede74256ca8811ae37001d238c4ffb156861c408453e95e02096773465619ccf")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd duckdb/
-if [[ "${target}" == *-mingw32 ]]; then
-    sed -i -E "/add_executable\(duckdb_rest_server server.cpp\)$/aif\(\$\{WIN32\}\)\n  set\(LINK_EXTRA -lwsock32 -lws2_32\)\nendif\(\)\n" tools/rest/CMakeLists.txt
-fi
-
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DDISABLE_UNITY=TRUE -DENABLE_SANITIZER=FALSE -DBUILD_UNITTESTS=FALSE ..
+cd duckdb-0.2.9/
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
 make -j${nproc}
 make install
-
-
-if [[ "${target}" == *-mingw32 ]]; then
-    mkdir -p ${libdir}
-    cp src/libduckdb.${dlext} ${libdir}
-fi
 """
 
 # These are the platforms we will build for by default, unless further
@@ -46,4 +34,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"6.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
