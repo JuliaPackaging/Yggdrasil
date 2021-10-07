8dd57f7b4c7e919958f9be5acb42365254888481
Rik Huijzer <rikhuijzer@pm.me>
2021-10-07 13:13:08 +0200

    Add Windows and MacOS

--- T/tectonic/build_tarballs.jl
+++ T/tectonic/build_tarballs.jl
@@ -7,6 +7,8 @@ url_prefix = "https://github.com/tectonic-typesetting/tectonic/releases/download
 sources = [
     ArchiveSource("$(url_prefix)-x86_64-unknown-linux-gnu.tar.gz", "3a128a4f2302033c350f9a18dc35a48f3034173d9e02b80c00ae4f83ee506cde"; unpack_target="x86_64-linux-gnu"),
     ArchiveSource("$(url_prefix)-x86_64-unknown-linux-musl.tar.gz", "e46767ab9edc8cd83666a293020c290c984470d984f6f6e2f53454c0869c3dd4"; unpack_target="x86_64-linux-musl"),
+    ArchiveSource("$(url_prefix)-x86_64-pc-windows-gnu.zip", "96659ede3eaeab5070bc518810a369d4113deaa53262b3d48d220c2f77c55072"; unpack_target="x86_64-w64-mingw32"),
+    ArchiveSource("$(url_prefix)-x86_64-apple-darwin.tar.gz", "4311c5f77d89f11be2f56c14997c8b850f748038f679b8e73c5fe8601a50cba4"; unpack_target="x86_64-apple-darwin14"),
     FileSource("https://raw.githubusercontent.com/tectonic-typesetting/tectonic/tectonic%40$(version)/LICENSE", "814a258f76e420b25cb3c07172eb2b3956f34cefbf0a650413b78e65c425f306"),
 ]
 
@@ -22,6 +24,8 @@ install_license LICENSE
 platforms = [
     Platform("x86_64", "linux"),
     Platform("x86_64", "linux"; libc="musl"),
+    Platform("x86_64", "windows"),
+    Platform("x86_64", "macos")
 ]
 platforms = expand_cxxstring_abis(platforms)
 
