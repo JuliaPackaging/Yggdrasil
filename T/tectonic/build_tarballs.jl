5231aa242029d5e375d344044d45ddc8062e5045
Rik Huijzer <rikhuijzer@pm.me>
2021-10-07 12:33:59 +0200

    Use binaries for Tectonic 0.7.1

--- T/tectonic/build_tarballs.jl
+++ T/tectonic/build_tarballs.jl
@@ -1,40 +1,27 @@
-# Note that this script can accept some limited command-line arguments, run
-# `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder, Pkg
+using BinaryBuilder
 
 name = "tectonic"
-version = v"0.1.15"
+version = v"0.7.1"
 
-# Collection of sources required to build tar
+url_prefix = "https://github.com/tectonic-typesetting/tectonic/releases/download/tectonic%40$(version)/tectonic-$(version)"
 sources = [
-    ArchiveSource(
-        "https://github.com/tectonic-typesetting/tectonic/archive/tectonic@$(version).tar.gz",
-        "0e55188eafc1b58f3660a303fcdd6adc071051b9eb728119837fbeed2309914f"
-    )
+    ArchiveSource("$(url_prefix)-x86_64-unknown-linux-gnu.tar.gz", "3a128a4f2302033c350f9a18dc35a48f3034173d9e02b80c00ae4f83ee506cde"; unpack_target="x86_64-linux-gnu"),
+    ArchiveSource("$(url_prefix)-x86_64-unknown-linux-musl.tar.gz", "e46767ab9edc8cd83666a293020c290c984470d984f6f6e2f53454c0869c3dd4"; unpack_target="x86_64-linux-musl"),
+    FileSource("https://raw.githubusercontent.com/tectonic-typesetting/tectonic/tectonic%40$(version)/LICENSE", "814a258f76e420b25cb3c07172eb2b3956f34cefbf0a650413b78e65c425f306"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/tectonic-*/
-cargo build --release -j${nproc}
-cp target/${rust_target}/release/tectonic${exeext} ${bindir}/
+cd ${WORKSPACE}/srcdir/
+mkdir -p "${bindir}"
+dirprefix="${target}/"
+install -m 755 "${dirprefix}tectonic${exeext}" "${bindir}/tectonic${exeext}"
+install_license LICENSE
 """
 
-# Some platforms disabled for now due issues with rust and musl cross compilation. See #1673.
 platforms = [
-    Platform("x86_64", "freebsd"),
-    Platform("aarch64", "linux"; libc="glibc"),
-    # Platform("aarch64", "linux"; libc="musl"),
-    Platform("armv7l", "linux"; libc="glibc"),
-    # Platform("armv7l", "linux"; libc="musl"),
-    Platform("i686", "linux"; libc="glibc"),
-    # Platform("i686", "linux"; libc="musl"),
-    Platform("powerpc64le", "linux"; libc="glibc"),
-    Platform("x86_64", "linux"; libc="glibc"),
-    # Platform("x86_64", "linux"; libc="musl"),
-    Platform("x86_64", "macos"),
-    # Platform("i686", "windows"),
-    Platform("x86_64", "windows"),
+    Platform("x86_64", "linux"),
+    Platform("x86_64", "linux"; libc="musl"),
 ]
 platforms = expand_cxxstring_abis(platforms)
 
@@ -45,15 +32,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("Fontconfig_jll"),
-    Dependency("FreeType2_jll"),
-    Dependency("Graphite2_jll"),
-    Dependency("HarfBuzz_jll"),
-    Dependency(PackageSpec(; name="ICU_jll", version=v"67.1.0")),
-    Dependency("OpenSSL_jll"),
-    Dependency("Zlib_jll"),
-    Dependency("libpng_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers=[:c, :rust], preferred_gcc_version=v"7", lock_microarchitecture=false)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
