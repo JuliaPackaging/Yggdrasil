8f4cacae22955c49bc0751dda98f07e09c77a9f1
Max Horn <max@quendi.de>
2021-10-08 09:50:20 +0200

    Rebuild libsingular_jll for Julia 1.8 support
    Also stop building for Julia before 1.6

--- L/libsingular_julia/build_tarballs.jl
+++ L/libsingular_julia/build_tarballs.jl
@@ -1,11 +1,12 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder
-import Pkg: PackageSpec
-import Pkg.Types: VersionSpec
+using BinaryBuilder, Pkg
+using Base.BinaryPlatforms
 
 name = "libsingular_julia"
-version = VersionNumber(0, 18, julia_version.minor)
+version = v"0.8.10"
+
+julia_versions = [v"1.6.0", v"1.7.0", v"1.8.0"]
 
 # Collection of sources required to build libsingular-julia
 sources = [
@@ -32,7 +33,7 @@ install_license LICENSE.md
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 include("../../L/libjulia/common.jl")
-platforms = libjulia_platforms(julia_version)
+platforms = vcat(libjulia_platforms.(julia_versions)...)
 filter!(!Sys.iswindows, platforms) # Singular does not support Windows
 # disable experimental platforms for now: support would require rebuilding
 # all dependencies; and it would require them to have julia_compat >= 1.6
@@ -49,15 +50,14 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency(PackageSpec(name="libjulia_jll", version=julia_version)),
-    # Work around dependency bugs
-    BuildDependency(PackageSpec(name="GMP_jll", version=julia_version < v"1.6" ? v"6.1.2" : v"6.2.0")),
-    BuildDependency(PackageSpec(name="MPFR_jll", version=julia_version <v"1.6" ? v"4.0.2" : v"4.1.1")),
-    Dependency("libcxxwrap_julia_jll", VersionNumber(0, 8, julia_version.minor)),
+    BuildDependency("libjulia_jll"),
+    BuildDependency("GMP_jll"),
+    BuildDependency("MPFR_jll"),
+    Dependency("libcxxwrap_julia_jll"),
     Dependency("Singular_jll", compat = "~402.100.101"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
     preferred_gcc_version=v"8",
-    julia_compat = "$(julia_version.major).$(julia_version.minor)")
+    julia_compat = "1.6")
