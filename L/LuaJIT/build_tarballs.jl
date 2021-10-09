ae39259cc5068b714cba29437bb0a4309636b70e
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2021-10-10 02:32:30 +0530

    Update build_tarballs.jl

--- L/LuaJIT/build_tarballs.jl
+++ L/LuaJIT/build_tarballs.jl
@@ -3,29 +3,29 @@
 using BinaryBuilder, Pkg
 
 name = "LuaJIT"
-version = v"2.0.5"
+version = v"2.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://luajit.org/download/LuaJIT-$(version).tar.gz", "874b1f8297c697821f561f9b73b57ffd419ed8f4278c82e05b48806d30c1e979")
+    ArchiveSource("https://github.com/LuaJIT/LuaJIT/archive/refs/heads/v$(version).zip", "9f06038cc7b74672a479cbbfc4a7560ea35b2776fffa85358de4e2fbbb3d6dfe")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/LuaJIT*
+cd $WORKSPACE/srcdir/LuaJIT-*
 make
 make install DESTDIR=/workspace/destdir
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms();
 
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libluajit-5.1", :libluajit, "usr/local/lib"),
-    ExecutableProduct("luajit-$(version)", :luajit, "usr/local/bin")
+    LibraryProduct("libluajit", :libluajit, "usr/local/lib"),
+    ExecutableProduct("luajit-2.1.0-beta3", :luajit, "usr/local/bin")
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -34,3 +34,4 @@ dependencies = Dependency[
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+```
