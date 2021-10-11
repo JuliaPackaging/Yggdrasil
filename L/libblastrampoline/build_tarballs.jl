7954b778dd5da95f515a56f4cce73c272f7f9767
Viral B. Shah <ViralBShah@users.noreply.github.com>
2021-10-11 15:29:03 -0400

    [LBT] Set up LBT forwards for Julia 1.6 (#3700)
    * [LBT] Set up LBT forwards for Julia 1.6
    
    * Apply suggestions from code review
    
    Co-authored-by: Elliot Saba <staticfloat@gmail.com>
    
    * Have a version for Julia 1.6 and one for Julia 1.7 onwards
    
    * Remove + from directory name
    
    * fix for 1.6
    
    * Rename directories
    
    * Update build_tarballs.jl
    
    * fix
    
    * Update L/libblastrampoline/build_tarballs.jl
    
    * [libblastrampoline] Fix indentation of init block
    
    [skip build]
    
    Co-authored-by: Elliot Saba <staticfloat@gmail.com>
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/libblastrampoline/build_tarballs.jl
+++ L/libblastrampoline/build_tarballs.jl
@@ -33,4 +33,12 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6",
+               init_block = """
+               @static if VERSION < v"1.7.0-DEV.641"
+                       ccall((:lbt_forward, libblastrampoline), Int32, (Cstring, Int32, Int32),
+                             Libdl.dlpath(Base.libblas_name) , 1, 0)
+                   end
+               """
+)
