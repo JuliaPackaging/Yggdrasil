c8db3cce5df82d8db0878c365293fb94d92893b2
Viral B. Shah <ViralBShah@users.noreply.github.com>
2021-10-11 14:24:09 -0400

    Update build_tarballs.jl

--- L/libblastrampoline/libblastrampoline@julia1.6/build_tarballs.jl
+++ L/libblastrampoline/libblastrampoline@julia1.6/build_tarballs.jl
@@ -4,6 +4,7 @@ include("../common.jl")
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="~1.6",
                init_block = """
-                 ccall((:lbt_forward, libblastrampoline), Int32, (Cstring, Int32, Int32), path, 1, 0)
+                 using Libdl
+                 ccall((:lbt_forward, libblastrampoline), Int32, (Cstring, Int32, Int32), Libdl.dlpath(Base.libblas_name), 1, 0)
                """
 )
