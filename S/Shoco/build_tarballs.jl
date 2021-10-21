7ffac2548963a7c2979b88fc4081f5905a8029a8
Alex Arslan <ararslan@comcast.net>
2021-10-21 09:32:49 -0700

    Set Julia compatibility to 1.6

--- S/Shoco/build_tarballs.jl
+++ S/Shoco/build_tarballs.jl
@@ -30,4 +30,5 @@ products = [
 
 dependencies = []
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6")
