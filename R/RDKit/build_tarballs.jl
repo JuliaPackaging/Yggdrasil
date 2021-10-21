649d6f62cc64f427adcf64036bd5a54490883ec6
Eloy Felix <eloyfelix@gmail.com>
2021-10-22 00:22:37 +0100

    [RDKit] use gcc6

--- R/RDKit/build_tarballs.jl
+++ R/RDKit/build_tarballs.jl
@@ -52,4 +52,4 @@ dependencies = [
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
-    preferred_gcc_version=v"5", julia_compat="^$(julia_version.major).$(julia_version.minor)")
+    preferred_gcc_version=v"6", julia_compat="^$(julia_version.major).$(julia_version.minor)")
