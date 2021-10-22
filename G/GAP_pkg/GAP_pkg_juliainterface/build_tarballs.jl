c04b84f4310e6cb76e214f662bbdd16bc8b4e3a4
Max Horn <max@quendi.de>
2021-10-22 11:00:31 +0200

    Update GAP package JLLs, add a new one for JuliaInterface (#3785)
    * GAP_pkg_*: undo HACKs so they don't pollute the final JLL
    
    * GAP_pkg_*: allow specifying GAP_lib version
    
    * Update GAP package 'cvec' to 2.7.5
    
    * Update GAP package 'io' to 4.7.2
    
    * Rebuild GAP package 'ferret' for latest GAP_jll
    
    * Add GAP package 'JuliaInterface'

--- G/GAP_pkg/GAP_pkg_juliainterface/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_juliainterface/build_tarballs.jl
@@ -1,41 +1,58 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
+using Base.BinaryPlatforms
 include("../common.jl")
 
 gap_version = v"400.1191.001"
 gap_lib_version = v"400.1191.000"
-name = "cvec"
-upstream_version = v"2.7.5" # when you increment this, reset offset to v"0.0.0"
+name = "JuliaInterface"
+upstream_version = v"0.7.0" # when you increment this, reset offset to v"0.0.0"
 offset = v"0.0.0" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
 version = offset_version(upstream_version, offset)
 
+julia_versions = [v"1.6.0", v"1.7.0", v"1.8.0"]
+
 # Collection of sources required to build libsingular-julia
 sources = [
-    ArchiveSource("https://github.com/gap-packages/$(name)/releases/download/v$(upstream_version)/$(name)-$(upstream_version).tar.bz2",
-                  "b9b51356e88f5a4bbdb25edaa41738338567dcdaa0b8a04e4b2807803953e5c5"),
+    GitSource("https://github.com/oscar-system/GAP.jl", "02de69a677054e4bad52fe1bc612071d1aeeb18a"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd cvec*
-./configure ${prefix}/share/gap
+cd GAP.jl/pkg/JuliaInterface
+./configure --with-gaproot=${prefix}/share/gap
 make -j${nproc}
 
 # copy the loadable module
 mkdir -p ${prefix}/lib/gap/
 cp bin/*/*.so ${prefix}/lib/gap/
 
-install_license LICENSE
+# copy the sources, too, so that we can later compare them
+cp -r src ${prefix}/
+
+install_license ../../LICENSE
 """
 
 name = gap_pkg_name(name)
 platforms, dependencies = setup_gap_package(gap_version, gap_lib_version)
 
+# expand julia platforms
+julia_platforms = []
+for p in platforms
+    for jv in julia_versions
+        p = deepcopy(p)
+        BinaryPlatforms.add_tag!(p.tags, "julia_version", string(jv))
+        push!(julia_platforms, p)
+    end
+end
+
+push!(dependencies, BuildDependency("libjulia_jll"))
+
 # The products that we will ensure are always built
 products = [
-    FileProduct("lib/gap/cvec.so", :cvec),
+    FileProduct("lib/gap/JuliaInterface.so", :JuliaInterface),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+build_tarballs(ARGS, name, version, sources, script, julia_platforms, products, dependencies;
                julia_compat="1.6", preferred_gcc_version=v"7")
