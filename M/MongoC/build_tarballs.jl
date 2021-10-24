6034908519590003affb795b53903743efdaf5fd
Max Horn <max@quendi.de>
2021-10-24 23:37:18 +0200

    Update MongoC to 1.19.1, add Apple M1 support

--- M/MongoC/build_tarballs.jl
+++ M/MongoC/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "MongoC"
-version = v"1.16.2"
+version = v"1.19.1"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://github.com/mongodb/mongo-c-driver/releases/download/$(version)/mongo-c-driver-$(version).tar.gz",
-                  "0a722180e5b5c86c415b9256d753b2d5552901dc5d95c9f022072c3cd336887e")
+                  "1732251e3f65bc02ce05c04ce34ef2819b154479108df669f0c045486952521d")
 ]
 
 # Bash recipe for building across all platforms
@@ -29,7 +29,22 @@ elif [[ "${target}" != *-apple-* ]]; then
     export CFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib64"
 fi
 
-cmake  -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF -DCMAKE_BUILD_TYPE=Release -DENABLE_SSL=OPENSSL -DENABLE_SASL=OFF -DENABLE_EXAMPLES=OFF -DENABLE_TESTS=OFF -DENABLE_UNINSTALL=OFF -DENABLE_STATIC=OFF -DENABLE_SNAPPY=ON -DENABLE_MONGOC=ON -DENABLE_BSON=ON -DOPENSSL_ROOT_DIR=$prefix -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} ..
+cmake \
+    -DENABLE_AUTOMATIC_INIT_AND_CLEANUP=OFF \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DENABLE_SSL=OPENSSL \
+    -DENABLE_SASL=OFF \
+    -DENABLE_EXAMPLES=OFF \
+    -DENABLE_TESTS=OFF \
+    -DENABLE_UNINSTALL=OFF \
+    -DENABLE_STATIC=OFF \
+    -DENABLE_SNAPPY=ON \
+    -DENABLE_MONGOC=ON \
+    -DENABLE_BSON=ON \
+    -DOPENSSL_ROOT_DIR=$prefix \
+    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    ..
 make -j${nproc}
 make install
 
@@ -43,7 +58,7 @@ fi
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -60,4 +75,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat = "1.6")
