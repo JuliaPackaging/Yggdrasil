fa9e03414c4bfb71b37397e842229fa629816c57
Will Kimmerer <wrkimmerer@outlook.com>
2021-10-24 19:17:49 -0400

    Remove string abi
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- K/Kissat/build_tarballs.jl
+++ K/Kissat/build_tarballs.jl
@@ -23,7 +23,7 @@ cp build/libkissat.so "$libdir/libkissat.${dlext}"
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
+platforms = supported_platforms(; experimental=true)
 # The products that we will ensure are always built
 products = Product[
 ]
