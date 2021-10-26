b1b086d01faf61e94c71e62022a7365e6647b4d2
Tim Besard <tim.besard@gmail.com>
2021-10-26 16:33:29 +0200

    Update CUDA_compat to 11.5.
    Switch to using the RPMs since driver 495 isn't released yet.

--- C/CUDA/CUDA_compat/build_tarballs.jl
+++ C/CUDA/CUDA_compat/build_tarballs.jl
@@ -9,24 +9,34 @@ using BinaryBuilder, Pkg
 include("../../../fancy_toys.jl")
 
 name = "CUDA_compat"
-version = v"0.0.1"
+version = v"11.5.50"
+
+cuda_version = "$(version.major)-$(version.minor)"
+driver_version = "495.29.05"
+build = 1
+
+sources_linux_x86 = [
+    FileSource("https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-compat-$(cuda_version)-$(driver_version)-$(build).x86_64.rpm",
+               "d8eabf75a651266a706c9b00edb75ce7f4097dd1a9185c20ecfaf5375e06ebf9", "compat.rpm")
+]
+sources_linux_ppc64le = [
+    FileSource("https://developer.download.nvidia.com/compute/cuda/repos/rhel8/ppc64le/cuda-compat-$(cuda_version)-$(driver_version)-$(build).ppc64le.rpm",
+               "6a830001cd0ae318b2b0f5b1970ecb799f42b311d6483fff58b332073e19e647", "compat.rpm")
+]
+sources_linux_aarch64 = [
+    FileSource("https://developer.download.nvidia.com/compute/cuda/repos/rhel8/sbsa/cuda-compat-$(cuda_version)-$(driver_version)-$(build).aarch64.rpm",
+               "6aebc19cdab34f36b914875a45bda2ce398485a95e156b37ee7b2699e4bd4276", "compat.rpm")
+]
 
 dependencies = []
 
 script = raw"""
-sh installer.run -x
-cd NVIDIA-*
-find .
-
-install_license LICENSE
+apk add rpm2cpio
+rpm2cpio compat.rpm | cpio -idmv
 
 mkdir -p ${libdir}
 
-ln -s libcuda.so.* libcuda.so
-mv libcuda.so* ${libdir}
-
-ln -s libnvidia-ptxjitcompiler.so.* libnvidia-ptxjitcompiler.so
-mv libnvidia-ptxjitcompiler.so* ${libdir}
+mv usr/local/cuda-*/compat/* ${libdir}
 """
 
 products = [
@@ -36,8 +46,20 @@ products = [
 
 non_reg_ARGS = filter(arg -> arg != "--register", ARGS)
 
-cuda_versions = [v"11.4"]
-for cuda_version in cuda_versions
-    cuda_tag = "$(cuda_version.major).$(cuda_version.minor)"
-    include("build_$(cuda_tag).jl")
+if should_build_platform("x86_64-linux-gnu")
+    build_tarballs(non_reg_ARGS, name, version, sources_linux_x86, script,
+                   [Platform("x86_64", "linux")], products, dependencies;
+                   lazy_artifacts=true, skip_audit=true)
+end
+
+if should_build_platform("powerpc64le-linux-gnu")
+    build_tarballs(non_reg_ARGS, name, version, sources_linux_ppc64le, script,
+                   [Platform("powerpc64le", "linux")], products, dependencies;
+                   lazy_artifacts=true, skip_audit=true)
+end
+
+if should_build_platform("aarch64-linux-gnu")
+    build_tarballs(ARGS, name, version, sources_linux_aarch64, script,
+                   [Platform("aarch64", "linux")], products, dependencies;
+                   lazy_artifacts=true, skip_audit=true)
 end
