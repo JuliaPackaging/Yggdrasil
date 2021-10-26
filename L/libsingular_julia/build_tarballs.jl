1e729c8a91f0197d38fabf31703b379f7335b5ce
Max Horn <max@quendi.de>
2021-10-26 21:42:09 +0200

    Update libsingular_julia, add Apple M1 support (#3799)
    * Update libsingular_julia, add Apple M1 support
    
    * Update L/libsingular_julia/build_tarballs.jl
    
    Co-authored-by: dan <tthsqe12@gmail.com>
    
    Co-authored-by: dan <tthsqe12@gmail.com>

--- L/libsingular_julia/build_tarballs.jl
+++ L/libsingular_julia/build_tarballs.jl
@@ -4,13 +4,13 @@ using BinaryBuilder, Pkg
 using Base.BinaryPlatforms
 
 name = "libsingular_julia"
-version = v"0.18.10"
+version = v"0.19.00"
 
 julia_versions = [v"1.6.0", v"1.7.0", v"1.8.0"]
 
 # Collection of sources required to build libsingular-julia
 sources = [
-    GitSource("https://github.com/oscar-system/libsingular-julia.git", "2887b9c452936c82e7de1acc2da1b9f099bc086d"),
+    GitSource("https://github.com/oscar-system/libsingular-julia.git", "d3f30b1c11d390c734da1dcbdbad265df2e90418"),
 ]
 
 # Bash recipe for building across all platforms
@@ -35,12 +35,6 @@ install_license LICENSE.md
 include("../../L/libjulia/common.jl")
 platforms = vcat(libjulia_platforms.(julia_versions)...)
 filter!(!Sys.iswindows, platforms) # Singular does not support Windows
-# disable experimental platforms for now: support would require rebuilding
-# all dependencies; and it would require them to have julia_compat >= 1.6
-# (so either we drop support for all older Julia versions, or have to build
-# those dependencies twice)
-filter!(p -> !(Sys.isapple(p) && arch(p) == "aarch64"), platforms) # M1
-filter!(p -> arch(p) != "armv6l", platforms)
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
@@ -54,7 +48,7 @@ dependencies = [
     BuildDependency("GMP_jll"),
     BuildDependency("MPFR_jll"),
     Dependency("libcxxwrap_julia_jll"),
-    Dependency("Singular_jll", compat = "~402.100.101"),
+    Dependency("Singular_jll", compat = "~402.101.100"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
