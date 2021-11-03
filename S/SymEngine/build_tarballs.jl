f56bee83e284473da0d9ace46ad0f54512b7bae7
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2021-11-03 23:23:56 +0100

    [SymEngine] M1

--- S/SymEngine/build_tarballs.jl
+++ S/SymEngine/build_tarballs.jl
@@ -4,7 +4,7 @@ using BinaryBuilder
 
 # Collection of sources required to build SymEngine
 name = "SymEngine"
-version = v"0.8.0"
+version = v"0.8.1"
 sources = [
     ArchiveSource("https://github.com/symengine/symengine/releases/download/v$(version)/symengine-$(version).tar.gz",
                   "ebe71d3c42661d12fa991040097ff383e193006af4e76d1adaabb8cee45cf94c"),
@@ -28,7 +28,7 @@ make -j${nproc}
 make install
 """
 
-platforms = expand_cxxstring_abis(supported_platforms())
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
 
 # The products that we will ensure are always built
 products = [
@@ -37,10 +37,10 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("GMP_jll", v"6.1.2"),
-    Dependency("MPFR_jll", v"4.0.2"),
-    Dependency("MPC_jll", v"1.1.0"),
+    Dependency("GMP_jll", v"6.2.1"),
+    Dependency("MPFR_jll", v"4.1.0"),
+    Dependency("MPC_jll", v"1.2.1"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
