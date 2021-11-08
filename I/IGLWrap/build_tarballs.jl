967207420c7a2550c79a7afeb7b57022e08b16f2
Mosè Giordano <giordano@users.noreply.github.com>
2021-11-08 21:50:59 +0000

    Update build_tarballs.jl

--- I/IGLWrap/build_tarballs.jl
+++ I/IGLWrap/build_tarballs.jl
@@ -12,8 +12,6 @@ sources = [
 	# 2.3.0 stable release:
 	GitSource("https://github.com/libigl/libigl.git",
 		"e60423e28c86b6aa2a3f6eb0112e8fd881f96777"),
-	FileSource("https://www.gnu.org/licenses/gpl-3.0.txt",
-		"3972dc9744f6499f0f9b2dbf76696f2ae7ad8af9b23dde66d6af86c9dfb36986"),
 ]
 
 script = raw"""
@@ -24,6 +22,7 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_MODULE_PATH=${WORKSPACE}/srcdir/libigl/cmake\; \
+    `# We can't run try_run in cross-compilation mode, pass the exit code and stdout of the program (the value of __cplusplus macro)` \
     -DCGAL_test_cpp_version_RUN_RES=0 \
     -DCGAL_test_cpp_version_RUN_RES__TRYRUN_OUTPUT="$(c++ -xc++ - -E -dM < /dev/null | grep -oP '__cplusplus \K.*')" \
     ..
@@ -48,11 +47,11 @@ platforms = expand_cxxstring_abis(platforms)
 
 
 dependencies = [
-  Dependency("boost_jll", compat="~1.71"),
-	Dependency("GMP_jll"),
-	Dependency("MPFR_jll"),
-	Dependency("OpenBLAS_jll"),
- ]
+    Dependency("boost_jll", compat="~1.71"),
+    Dependency("GMP_jll"),
+    Dependency("MPFR_jll"),
+    Dependency("OpenBLAS_jll"),
+]
 
 build_tarballs(ARGS, "IGLWrap", version,
     sources,
