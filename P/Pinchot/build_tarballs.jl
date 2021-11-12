16944aebd8e98e66aaded78b61d0bed560085a6e
Joey Nelson <joey@joescan.com>
2021-11-12 10:53:45 -0800

    Referenced new commit that fixes compiler issues with older GCCs.

--- P/Pinchot/build_tarballs.jl
+++ P/Pinchot/build_tarballs.jl
@@ -7,7 +7,7 @@ version = v"13.0.1"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/joeynelson/pinchot-c-api.git", "54cb91757513145b3bf3358be9d2818095b128ca")
+    GitSource("https://github.com/joeynelson/pinchot-c-api.git", "cee8ff854a6c3135aa861094cf1b27dc6e01a393")
 ]
 
 # Bash recipe for building across all platforms
@@ -16,7 +16,7 @@ cd $WORKSPACE/srcdir/pinchot-c-api
 mkdir build
 cd build/
 cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
-make
+make -j${nproc}
 make install
 install_license ${WORKSPACE}/srcdir/pinchot-c-api/LICENSE.txt
 """
@@ -36,4 +36,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"11.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
