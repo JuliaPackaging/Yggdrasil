e6ba8b83aa08976e6b1c417810280ec52fca6115
Mosè Giordano <mose@gnu.org>
2021-11-14 16:14:19 +0000

    [qwt] Revert `qmake` dance and use again `Qt_jll`

--- Q/qwt/build_tarballs.jl
+++ Q/qwt/build_tarballs.jl
@@ -1,6 +1,5 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-
 using BinaryBuilder, Pkg
 
 name = "qwt"
@@ -13,11 +12,6 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-prefixqmake=$(which qmake)
-realqmake=$(readlink $prefixqmake)
-rm $prefixqmake
-cp $bindir/$realqmake $prefixqmake
-
 cd $WORKSPACE/srcdir/qwt
 qmake
 make -j${nproc}
@@ -39,8 +33,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Qt5Base_jll", uuid="ea2cea3b-5b76-57ae-a6ef-0a8af62496e1"))
-    Dependency(PackageSpec(name="Qt5Svg_jll", uuid="3af4ccab-a251-578e-a514-ea85a0ba79ee"))
+    # TODO: in the future replace `Qt_jll` with `Qt5Base_jll` + `Qt5Svg_jll`
+    Dependency(PackageSpec(name="Qt_jll", uuid="ede63266-ebff-546c-83e0-1c6fb6d0efc8"))
     Dependency(PackageSpec(name="GLEW_jll", uuid="bde7f898-03f7-559e-8810-194d950ce600"))
     Dependency(PackageSpec(name="GLFW_jll", uuid="0656b61e-2033-5cc2-a64a-77c0f6c09b89"))
     Dependency(PackageSpec(name="GLU_jll", uuid="bd17208b-e95e-5925-bf81-e2f59b3e5c61"))
