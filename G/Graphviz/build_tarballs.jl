3e418b7e4a62b3ba725f39d3bf4a96a50b8c9e42
Keno Fischer <keno@alumni.harvard.edu>
2021-11-17 22:37:20 -0500

    Add darwin config6

--- G/Graphviz/build_tarballs.jl
+++ G/Graphviz/build_tarballs.jl
@@ -63,8 +63,8 @@ if [[ "${target}" == *-linux* ]]; then
     cp ../config6-linux ${prefix}/lib/graphviz/config6
 # elif [[ "${target}" == *-mingw* ]]; then
     # TODO: Add config6 for mingw
-# elif [[ "${target}" == *-darwin* ]]; then
-    # TODO: Add config6 for darwin
+elif [[ "${target}" == *-darwin* ]]; then
+    cp ../config6-darwin ${prefix}/lib/graphviz/config6
 fi
 """
 
