4b617737b2a906b539756a5ece74909744e0cf28
Mosè Giordano <mose@gnu.org>
2021-11-19 00:26:11 +0000

    [OpenFOAM] Use MPICH

--- O/OpenFOAM/build_tarballs.jl
+++ O/OpenFOAM/build_tarballs.jl
@@ -12,19 +12,42 @@ sources = [
     DirectorySource("./bundled"),
 ]
 
+# In order to set up OpenFOAM, we need to know the version of some of the
+# dependencies.
+const SCOTCH_VERSION = "6.1.0"
+
 # Bash recipe for building across all platforms
-script = raw"""
+script = "SCOTCH_VERSION=$(SCOTCH_VERSION)\n" * raw"""
 cd ${WORKSPACE}/srcdir/OpenFOAM*
 atomic_patch -p1 ../patches/etc-bashrc.patch
+
 # Set rpaths in all C/C++ compilers
 LDFLAGS=""
 for dir in "" "/dummy" "/openmpi-system"; do
     LDFLAGS="${LDFLAGS} -Wl,-rpath=\$\$ORIGIN${dir} -Wl,-rpath-link=${PWD}/platforms/linux64GccDPInt32Opt/lib${dir}"
 done
 sed -i "s?-m64?-m64 ${LDFLAGS}?g" wmake/rules/*/c*
-# Allow failures, sigh
-source etc/bashrc || true
-./Allwmake -j${nproc}
+
+# Set version of Scotch
+echo "export SCOTCH_VERSION=${SCOTCH_VERSION}" > etc/config.sh/scotch
+echo "export SCOTCH_ARCH_PATH=${prefix}"      >> etc/config.sh/scotch
+
+# Set up to use our MPI (MPICH)
+sed -i 's/WM_MPLIB=SYSTEMOPENMPI/WM_MPLIB=SYSTEMMPI/g' etc/bashrc
+export MPI_ROOT="${prefix}"
+export MPI_ARCH_FLAGS=""
+export MPI_ARCH_INC="-I${includedir}"
+export MPI_ARCH_LIBS="-L${libdir} -lmpi"
+
+# HACK!  Explanation: sourcing `etc/bashrc` would result in (non-fatal) errors, that would
+# spring our traps.  As a workaround, we run the rest of script in a subshell (sigh), allow
+# failures in `source`, but exit if other commands fail.  We also don't mess up our system
+# by sourcing the script.  Inspired by
+# https://aur.archlinux.org/cgit/aur.git/tree/PKGBUILD?h=openfoam&id=c8374a4890add0117e972629cf556c49b9a8ec36
+
+bash -c "
+  source etc/bashrc
+  ./Allwmake -j${nproc} || exit 1"
 """
 
 # These are the platforms we will build for by default, unless further
@@ -37,11 +60,9 @@ products = Product[
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    # TODO: Try to use MPICH_jll, like PTSCHOT_jll, otherwise we have a conflict
-    # between the two dependencies.
-    Dependency("OpenMPI_jll"),
+    Dependency("MPICH_jll"),
     Dependency("flex_jll"),
-    Dependency("SCOTCH_jll"),
+    Dependency("SCOTCH_jll"; compat=SCOTCH_VERSION),
     Dependency("PTSCOTCH_jll"),
     Dependency("METIS_jll"),
     Dependency("Zlib_jll"),
