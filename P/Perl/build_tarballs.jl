02c8dbbf67a783e8da126242e0580f0c9b4d3f3f
Mosè Giordano <giordano@users.noreply.github.com>
2021-11-21 17:06:09 +0000

    Update P/Perl/build_tarballs.jl

--- P/Perl/build_tarballs.jl
+++ P/Perl/build_tarballs.jl
@@ -136,7 +136,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("Readline_jll", v"8.1.1"; compat="~8.1.1")
+    Dependency("Readline_jll"; compat="~8.1.1")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
