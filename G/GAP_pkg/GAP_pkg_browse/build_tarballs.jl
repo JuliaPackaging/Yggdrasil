300a1680799613b3488d13ef4965e7632b4a1287
Max Horn <max@quendi.de>
2021-11-22 23:18:16 +0100

    Update GAP_pkg_browse to 1.8.13

--- G/GAP_pkg/GAP_pkg_browse/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_browse/build_tarballs.jl
@@ -5,28 +5,22 @@ include("../common.jl")
 gap_version = v"400.1191.001"
 gap_lib_version = v"400.1191.000"
 name = "Browse"
-upstream_version = v"1.8.12" # when you increment this, reset offset to v"0.0.0"
+upstream_version = v"1.8.13" # when you increment this, reset offset to v"0.0.0"
 offset = v"0.0.0" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
 version = offset_version(upstream_version, offset)
 
 # Collection of sources required to build libsingular-julia
 sources = [
     ArchiveSource("https://www.math.rwth-aachen.de/~Browse/$(name)-$(upstream_version).tar.bz2",
-                  "6ee674b3f1877e550bacb302335fca7c707f4eb742428d8be7781f8aa77c8b9b"),
+                  "118d1f7fb67044c103cf44673ade8de57e4289a906dd01e15d7562dd0aface5b"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd Browse*
 
-# HACK to fool the Browse build system
-ln -s sysinfo.gap ${prefix}/share/gap/sysinfo.gap-default64
-
 ./configure ${prefix}/share/gap
 make -j${nproc} CFLAGS="-I$prefix/include/ncurses"
-
-# revert the HACK
-rm -f ${prefix}/share/gap/sysinfo.gap-default64
 
 # copy the loadable module
 mkdir -p ${prefix}/lib/gap
