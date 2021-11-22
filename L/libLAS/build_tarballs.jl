bc0a25d19dd1631d9b8f4c11665ba58823594897
ASUS1 <crghilardi@yahoo.com>
2021-11-21 19:21:00 -0800

    I forgot GDAL and PROJ don't have experimental platforms yet

--- L/libLAS/build_tarballs.jl
+++ L/libLAS/build_tarballs.jl
@@ -48,7 +48,7 @@ fi
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(; experimental = true))
+platforms = expand_cxxstring_abis(supported_platforms())
 
 
 # The products that we will ensure are always built
