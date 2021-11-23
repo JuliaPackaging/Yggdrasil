8d39c8f612a903ad0fc05862431eb51946a22fdf
Mosè Giordano <giordano@users.noreply.github.com>
2021-11-23 16:53:30 +0000

    Update build_tarballs.jl

--- F/fzf/build_tarballs.jl
+++ F/fzf/build_tarballs.jl
@@ -32,4 +32,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers = [:go])
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers = [:go], julia_compat="1.6")
