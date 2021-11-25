320e4045bf0fc215b26092a68f817182ebda145d
Tim Besard <tim.besard@gmail.com>
2021-11-25 11:35:25 +0100

    Remove Julia compat from LLVM_full.

--- L/LLVM/LLVM_full_assert@12.0.1/build_tarballs.jl
+++ L/LLVM/LLVM_full_assert@12.0.1/build_tarballs.jl
@@ -3,5 +3,5 @@ version = v"12.0.1"
 include("../common.jl")
 
 build_tarballs(ARGS, configure_build(ARGS, version; experimental_platforms=true, assert=true)...;
-                     preferred_gcc_version=v"7", preferred_llvm_version=v"9", julia_compat="1.7")
+                     preferred_gcc_version=v"7", preferred_llvm_version=v"9", julia_compat="1.6")
 
