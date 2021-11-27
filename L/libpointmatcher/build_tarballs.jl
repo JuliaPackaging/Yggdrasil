46c4afdcde312d5100759d43b6b726e8f5b351bf
Mosè Giordano <mose@gnu.org>
2021-11-27 19:10:00 +0000

    [libpointmatcher] Live patch libnabo cmake file

--- L/libpointmatcher/build_tarballs.jl
+++ L/libpointmatcher/build_tarballs.jl
@@ -8,7 +8,7 @@ version = v"0.1.0"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/ethz-asl/libpointmatcher.git", "fc7cd5da39b24665d65e6a570124621b4e1d3ef2"),
-    DirectorySource("./bundled")
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
@@ -19,21 +19,26 @@ atomic_patch -p1 ${WORKSPACE}/srcdir/patches/comment-cmake-alias.patch
 
 mkdir build && cd build
 
+# Fix path of libnabo.  TODO: This has to be fixed in libnabo_jll.
+sed -i "s?${prefix}/lib?${libdir}?" ${prefix}/share/libnabo/cmake/libnaboConfig.cmake
+
 cmake .. \
--DCMAKE_INSTALL_PREFIX=${prefix} \
--DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
--DCMAKE_BUILD_TYPE=Release \
--DBUILD_TESTS=OFF \
--DPOINTMATCHER_BUILD_EXAMPLES=OFF \
--DPOINTMATCHER_BUILD_EVALUATIONS=OFF \
--DBUILD_PYTHON_MODULE=OFF \
--DBUILD_SHARED_LIBS=ON
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_TESTS=OFF \
+    -DPOINTMATCHER_BUILD_EXAMPLES=OFF \
+    -DPOINTMATCHER_BUILD_EVALUATIONS=OFF \
+    -DBUILD_PYTHON_MODULE=OFF \
+    -DBUILD_SHARED_LIBS=ON
 
 make -j${nproc}
 make install
 
 install_license ../debian/copyright
 
+# Delete modified file from libnabo
+rm ${prefix}/share/libnabo/cmake/libnaboConfig.cmake
 """
 
 # These are the platforms we will build for by default, unless further
@@ -42,7 +47,6 @@ install_license ../debian/copyright
 platforms = expand_cxxstring_abis(supported_platforms())
 #only supports 64 bit compilers
 filter!(p -> nbits(p) != 32, platforms)
-
 
 
 # The products that we will ensure are always built
@@ -55,6 +59,7 @@ dependencies = [
     BuildDependency(PackageSpec(name="Eigen_jll", uuid="bc6bbf8a-a594-5541-9c57-10b0d0312c70"))
     Dependency("boost_jll"; compat="=1.76.0")
     Dependency(PackageSpec(name="libnabo_jll", uuid="569caa04-3db7-54b1-b5fe-10f3ec93054b"))
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
