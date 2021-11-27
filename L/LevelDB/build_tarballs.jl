05a3c810831e133ab3f2b5479a62b49006e05031
Mosè Giordano <mose@gnu.org>
2021-11-27 01:09:19 +0000

    [LevelDB] Several fixes
    * Fix version number
    * Remove useless patch
    * Simplify build script
    * Do not build with latest GCC version, it's not good for compatibility, we
      should use instead the oldest one possible
    * Try to build for all platforms
    * Expand C++ string ABIs

--- L/LevelDB/build_tarballs.jl
+++ L/LevelDB/build_tarballs.jl
@@ -3,48 +3,33 @@
 using BinaryBuilder, Pkg
 
 name = "LevelDB"
-version = v"0.1.0"
+version = v"1.23"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/google/leveldb.git", "99b3c03b3284f5886f9ef9a4ef703d57373e61be"),
-    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-for f in ${WORKSPACE}/srcdir/patches/*.patch; do
-    atomic_patch -p1 ${f}
-done
-ls
-cd leveldb/
-ls
+cd $WORKSPACE/srcdir/leveldb*/
 git submodule update --init --recursive
-mkdir build
-cd build
-ls
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DBUILD_GMOCK=OFF -DLEVELDB_BUILD_TESTS=OFF LEVELDB_BUILD_BENCHMARKS=OFF ..
-cmake --build .
-make 
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_GMOCK=OFF \
+    -DLEVELDB_BUILD_TESTS=OFF \
+    -DLEVELDB_BUILD_BENCHMARKS=OFF \
+    ..
+make -j${nproc}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("x86_64", "freebsd"; )
-]
-
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
 
 # The products that we will ensure are always built
 products = [
@@ -56,4 +41,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"11.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
