b39c1e2b0993bf4a929e2c6568fbd18ca614a9ae
Mosè Giordano <giordano@users.noreply.github.com>
2021-11-28 23:46:32 +0000

    Update build_tarballs.jl

--- Y/yaml_cpp/build_tarballs.jl
+++ Y/yaml_cpp/build_tarballs.jl
@@ -16,7 +16,7 @@ script = raw"""
 cd $WORKSPACE/srcdir/yaml-cpp*/
 mkdir build && cd build
 cmake .. \
--DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
     -DYAML_BUILD_SHARED_LIBS=ON \
