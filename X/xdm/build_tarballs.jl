ca8720772696993922df533280e5b30bc64fa2de
Keno Fischer <keno@juliacomputing.com>
2021-12-01 00:07:57 -0500

    rm boostpython workaround

--- X/xdm/build_tarballs.jl
+++ X/xdm/build_tarballs.jl
@@ -16,7 +16,6 @@ script = raw"""
 cd $WORKSPACE/srcdir/xdm
 atomic_patch -p1 ../patches/pyinterp.patch
 atomic_patch -p1 ../patches/musl.patch
-mv $prefix/lib/libboost_python.$dlext /workspace/destdir/lib/libboost_python38.$dlext || true
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBoost_NO_BOOST_CMAKE=ON
 make -j$nproc
 make install
