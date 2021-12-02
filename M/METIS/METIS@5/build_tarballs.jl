3af711a8305988c615b66dff836ac3d3c6d07b44
odow <o.dowson@gmail.com>
2021-12-03 11:23:48 +1300

    [METIS] build on experimental platforms

--- M/METIS/METIS@5/build_tarballs.jl
+++ M/METIS/METIS@5/build_tarballs.jl
@@ -33,7 +33,7 @@ make -j${nproc} install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(;experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -44,4 +44,4 @@ products = [
 dependencies = Dependency[]
 
 # Build the tarballs
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
