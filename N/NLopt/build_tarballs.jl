1ea8205b07c2bcc63ca33017ba63fd7a21878407
Mosè Giordano <giordano@users.noreply.github.com>
2021-12-03 18:45:14 +0000

    [NLopt] Build for experimental platforms

--- N/NLopt/build_tarballs.jl
+++ N/NLopt/build_tarballs.jl
@@ -21,7 +21,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms()) # build on all supported platforms
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true)) # build on all supported platforms
 
 # The products that we will ensure are always built
 products = [
@@ -33,4 +33,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
