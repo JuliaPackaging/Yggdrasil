51235aa89483446a295f8417332ca3756ba559f9
Mosè Giordano <mose@gnu.org>
2021-12-03 01:51:26 +0000

    [Librsvg] Try to build for all platforms
    Musl-based platforms seem to have some troubles

--- L/Librsvg/build_tarballs.jl
+++ L/Librsvg/build_tarballs.jl
@@ -29,7 +29,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -47,12 +47,13 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    # We need to run `gdk-pixbuf-query-loaders`
+    HostBuildDependency("gdk_pixbuf_jll"),
     BuildDependency("Xorg_xorgproto_jll"),
     Dependency("gdk_pixbuf_jll"),
-    Dependency("Pango_jll"),
+    Dependency("Pango_jll"; compat="1.47.0"),
     Dependency("Libcroco_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers=[:c, :rust])
-
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", compilers=[:c, :rust])
