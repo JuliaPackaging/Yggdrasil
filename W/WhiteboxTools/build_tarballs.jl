0c0d2b317107699f825a67515506297e75c0d68c
Mosè Giordano <giordano@users.noreply.github.com>
2021-12-04 13:40:03 +0000

    Update W/WhiteboxTools/build_tarballs.jl

--- W/WhiteboxTools/build_tarballs.jl
+++ W/WhiteboxTools/build_tarballs.jl
@@ -43,4 +43,4 @@ products = [
 dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", compilers = [:rust, :c], lock_microarchitecture = false)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", compilers = [:rust, :c])
