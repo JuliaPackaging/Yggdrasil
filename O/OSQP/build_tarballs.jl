a713f3224c5b33e7fba13c128f4ae817f68871d2
Oscar Dowson <odow@users.noreply.github.com>
2021-12-05 23:57:43 +1300

    [OSQP] build on experimental platforms (#4030)
    * [OSQP] build on experimental platforms
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl

--- O/OSQP/build_tarballs.jl
+++ O/OSQP/build_tarballs.jl
@@ -2,29 +2,37 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "OSQP"
-version = v"0.6.2"
+# To distinguish between upstream changes and changes to the JLL, we use:
+#     version = 100 * upstream version + offset
+# See C/Coin-OR/coin-or-common.jl for more details.
+version = v"0.600.200"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://dl.bintray.com/bstellato/generic/OSQP/$(version)/osqp-$(version).tar.gz",
-                  "2026ec67784344fbe062708947a68420911f8feb040e07ff09fba35254032b1f")
+    GitSource(
+        "https://github.com/osqp/osqp.git",
+        "f9fc23d3436e4b17dd2cb95f70cfa1f37d122c24",
+    ),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd osqp-*
+cd $WORKSPACE/srcdir/osqp
+git submodule update --init --recursive
 mkdir build
 cd build/
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
+cmake \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
 make
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(;experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -37,4 +45,14 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(
+    ARGS,
+    "OSQP",
+    version,
+    sources,
+    script,
+    platforms,
+    products,
+    dependencies;
+    julia_compat = "1.6",
+)
