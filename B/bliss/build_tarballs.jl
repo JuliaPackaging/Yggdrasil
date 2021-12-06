c14b27e0ec4237f63d5aea67d2f72df2fd9eb0b5
Mathieu Besançon <mathieu.besancon@gmail.com>
2021-12-06 17:35:42 +0100

    build exists

--- B/bliss/build_tarballs.jl
+++ B/bliss/build_tarballs.jl
@@ -14,9 +14,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd bliss-*
-mkdir build
-cd build
+cd bliss-*/build
 
 cmake -DCMAKE_INSTALL_PREFIX=$prefix\
   -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}\
