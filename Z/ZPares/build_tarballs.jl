e6c1d45b37426cfebe5a95bda8d8a684eb204f76
cometscome <cometscome@gmail.com>
2021-12-08 11:39:07 +0900

    script is changed

--- Z/ZPares/build_tarballs.jl
+++ Z/ZPares/build_tarballs.jl
@@ -24,39 +24,11 @@ if [[ $target == *"aarch64-apple-darwin"* ]]; then
     Rankmismatch="-fallow-argument-mismatch"
 fi
 
-
-if [[ $target == *"apple-darwin"* ]]; then
-    cp Makefile.inc/make.inc.gfortran.seq ./make.inc
-    make BLAS="-L$libdir -lopenblas" USE_MPI="0" FFLAG="-O3 $Rankmismatch -dynamiclib -L$libdir -lopenblas" LAPACK="-L./"
-    gfortran -O3 -dynamiclib -fPIC zpares_wrapper.f90 -I./include -L./lib -lzpares -L$libdir -lopenblas -o libzpares.dylib
-    cp libzpares.dylib lib/
-    cp lib/libzpares.dylib $prefix/lib/
-    cp include/zpares.mod $prefix/include/
-    cp zpares_wrapper.mod $prefix/include/
-    cp lib/libzpares.a $prefix/lib/
-elif [[ $target == *"w64-mingw32"* ]]; then 
-    cp Makefile.inc/make.inc.gfortran.seq ./make.inc
-    make BLAS="-L$libdir -lopenblas" LAPACK="-L$libdir -lopenblas" FFLAG="-O3 $Rankmismatch -shared -fPIC"
-    gfortran -O3 -shared -fPIC zpares_wrapper.f90 -I./include -L./lib -lzpares  -L$libdir -lopenblas -o zpares_wrapper.a
-    ld -shared -o zpares.so --whole-archive zpares_wrapper.a 
-    cp lib/* $prefix/bin/
-    cp zpares.so lib/
-    cp lib/zpares.so $prefix/lib/libzpares.dll
-    cp include/zpares.mod $prefix/include/
-    cp zpares_wrapper.mod $prefix/include/
-    cp lib/libzpares.a $prefix/lib/
-else
-    cp Makefile.inc/make.inc.gfortran.seq ./make.inc
-    make BLAS="-L$libdir -lopenblas" LAPACK="-L$libdir -lopenblas" FFLAG="-O3 $Rankmismatch -shared -fPIC"
-    gfortran -O3 -shared -fPIC zpares_wrapper.f90 -I./include -L./lib -lzpares  -o zpares_wrapper.a
-    ld -shared -o zpares.so --whole-archive zpares_wrapper.a 
-    cp zpares_wrapper.a $prefix/lib/
-    cp zpares.so lib/
-    cp lib/zpares.so $prefix/lib/libzpares.so
-    cp include/zpares.mod $prefix/include/
-    cp zpares_wrapper.mod $prefix/include/
-    cp lib/libzpares.a $prefix/lib/
-fi
+cp Makefile.inc/make.inc.gfortran.seq ./make.inc
+make BLAS="-L${libdir} -lopenblas" USE_MPI="0" FFLAG="-O3 ${Rankmismatch} -shared -L${libdir} -lopenblas" LAPACK="-L./"
+gfortran -O3 -shared -fPIC zpares_wrapper.f90 -I./include -L./lib -lzpares -L${libdir} -lopenblas -o "${libdir}/libzpares.${dlext}"
+cp include/zpares.mod "${includedir}"
+cp zpares_wrapper.mod "${includedir}"
 """
 
 # These are the platforms we will build for by default, unless further
