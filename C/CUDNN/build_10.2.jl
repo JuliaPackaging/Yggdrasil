2f288b3745b42da0b0c595f1fe363ac8f9980b78
Tim Besard <tim.besard@gmail.com>
2021-12-09 16:54:20 +0100

    Bump CUDNN to v8.3.1.

--- C/CUDNN/build_10.2.jl
+++ C/CUDNN/build_10.2.jl
@@ -1,8 +1,8 @@
 platforms_and_sources = Dict(
     Platform("x86_64", "linux") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.2/10.2_07062021/cudnn-10.2-linux-x64-v8.2.2.26.tgz",
-                      "b4a2067774f509e65a1d8ba3bd86162b9e09de5946bb636887b4cc605dddeb6e")],
+        ArchiveSource("https://developer.nvidia.com/compute/cudnn/secure/8.3.1/local_installers/10.2/cudnn-linux-x86_64-8.3.1.22_cuda10.2-archive.tar.xz",
+                      "5982bb96c2a720268fa44b908feb5258d060ad47f1f6e6030e760d13195ea964")],
     Platform("x86_64", "windows") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/cudnn/secure/8.2.2/10.2_07062021/cudnn-10.2-windows10-x64-v8.2.2.26.zip",
-                      "27dee1094c49f4993754ddecd1e3bf4a14b1d4d43951cc5956d2375eb3bcb9f8")],
+        ArchiveSource("https://developer.nvidia.com/compute/cudnn/secure/8.3.1/local_installers/10.2/cudnn-windows-x86_64-8.3.1.22_cuda10.2-archive.zip",
+                      "83797cac5c1e6c59bcf6bcd13ae23a3d00f909b19295819fb50063ea3356fadc")],
 )
