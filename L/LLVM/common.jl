5ed9f050a70b2e87ec12bc138fb8e270f89c8494
Valentin Churavy <vchuravy@users.noreply.github.com>
2021-12-09 17:06:29 -0500

    Update L/LLVM/common.jl

--- L/LLVM/common.jl
+++ L/LLVM/common.jl
@@ -426,7 +426,6 @@ function configure_extraction(ARGS, LLVM_full_version, name, libLLVM_version=not
         script = mlirscript
         products = [
             LibraryProduct("libMLIR", :libMLIR, dont_dlopen=true),
-            LibraryProduct("libMLIRPublicAPI", :libMLIRPublicAPI, dont_dlopen=true),
         ]
         if v"12" == version
             push!(products, LibraryProduct("libMLIRPublicAPI", :libMLIRPublicAPI, dont_dlopen=true))
