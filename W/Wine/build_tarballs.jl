86af74504ca2887d6180010716ef72a278bf3542
Keno Fischer <keno@juliacomputing.com>
2021-12-11 01:28:14 -0500

    Build glibc also

--- W/Wine/build_tarballs.jl
+++ W/Wine/build_tarballs.jl
@@ -8,8 +8,10 @@ using BinaryBuilder
 # Wine, install and bundle that version up, then resume the 64-bit build.
 
 # We also don't support passing explicit targets in
-SAFE_ARGS = [a for a in ARGS if startswith(a, "-")]
+SAFE_ARGS = [a for a in ARGS if startswith(a, "-") && !startswith(a, "--deploy")]
+platform_spec = [a for a in ARGS if !startswith(a, "-")]
 is_meta = any(a->startswith(a, "--meta-json"), ARGS)
+requested_platforms = map(x->parse(Platform, p; validate_strict=true), platform_spec)
 
 version = v"7.0-rc1"
 sources = Any[
@@ -29,47 +31,75 @@ dependencies = [
     Dependency("GnuTLS_jll"),
 ]
 
-platform32 = Platform("i686", "linux"; libc="musl")
-platform64 = Platform("x86_64", "linux"; libc="musl")
+platform32_musl = Platform("i686", "linux"; libc="musl")
+platform64_musl = Platform("x86_64", "linux"; libc="musl")
 
-if !is_meta
-wine64_build_script = raw"""
-# First, build wine64, making the actual build directory itself the thing we will install.
-mkdir $WORKSPACE/destdir/wine64
-cd $WORKSPACE/destdir/wine64
-$WORKSPACE/srcdir/wine/configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --without-x --without-freetype --enable-win64
-make -j${nproc}
-"""
+platform32_glibc = Platform("i686", "linux")
+platform64_glibc = Platform("x86_64", "linux")
 
-# package up the wine64 build directory itself:
-product_hashes = build_tarballs(copy(SAFE_ARGS), "Wine64Build", version, sources, wine64_build_script, [platform64], products, dependencies; skip_audit=true, preferred_gcc_version=v"11.1")
-
-# Include that tarball as one of the sources we need to include:
-wine64_build_path, wine64_build_hash = product_hashes[platform64]
-push!(sources, ArchiveSource(joinpath("products", wine64_build_path), wine64_build_hash))
-
-# Next, build wine32, without wine64, then use those tools to build wine32 WITH wine64
-wine32_script = raw"""
-# Next, build wine32, linking against the previously included wine64 stuff:
-mkdir $WORKSPACE/srcdir/wine32_only
-cd $WORKSPACE/srcdir/wine32_only
-$WORKSPACE/srcdir/wine/configure --host=${target}  --without-x --without-freetype
-make -j${nproc}
-
-mkdir $prefix/wine32
-cd $prefix/wine32
-$WORKSPACE/srcdir/wine/configure --prefix=${prefix}/wine32 --host=${target} --without-x --without-freetype --with-wine64=$WORKSPACE/srcdir/wine64 --with-wine-tools=$WORKSPACE/srcdir/wine32_only
-make -j${nproc}
-make -j${nproc} install
-"""
-
-wine32_products = Product[
-   ExecutableProduct("wine", :wine, "wine32/loader"),
+all_platforms = [
+    platform64_musl,
+    platform64_glibc
 ]
 
-product_hashes = build_tarballs(copy(SAFE_ARGS), "Wine32", version, sources, wine32_script, [platform32], wine32_products, dependencies; skip_audit=true, preferred_gcc_version=v"11.1")
-wine32_path, wine32_hash = product_hashes[platform32]
-push!(sources, ArchiveSource(joinpath("products", wine32_path), wine32_hash))
+platform_map = Dict(
+    platform64_musl => platform32_musl,
+    platform64_glibc => platform32_glibc
+)
+
+# In meta mode, note all supported platforms.
+platforms = all_platforms
+
+if !is_meta
+    # TODO: Make it possible to build multiple platforms in one invocation.
+    if isempty(requested_platforms)
+        @info "No platform specified; defaulting to musl."
+        platform64 = platform64_musl
+    elseif length(requested_platforms) != 1
+        error("This build script is special and only supports one platform at a time.")
+    else
+        platform64 = requested_platforms[1]
+    end
+
+    platform32 = platform_map[platform64]
+
+    wine64_build_script = raw"""
+    # First, build wine64, making the actual build directory itself the thing we will install.
+    mkdir $WORKSPACE/destdir/wine64
+    cd $WORKSPACE/destdir/wine64
+    $WORKSPACE/srcdir/wine/configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --without-x --without-freetype --enable-win64
+    make -j${nproc}
+    """
+
+    # package up the wine64 build directory itself:
+    product_hashes = build_tarballs(copy(SAFE_ARGS), "Wine64Build", version, sources, wine64_build_script, [platform64], products, dependencies; skip_audit=true, preferred_gcc_version=v"11.1")
+
+    # Include that tarball as one of the sources we need to include:
+    wine64_build_path, wine64_build_hash = product_hashes[platform64]
+    push!(sources, ArchiveSource(joinpath("products", wine64_build_path), wine64_build_hash))
+
+    # Next, build wine32, without wine64, then use those tools to build wine32 WITH wine64
+    wine32_script = raw"""
+    # Next, build wine32, linking against the previously included wine64 stuff:
+    mkdir $WORKSPACE/srcdir/wine32_only
+    cd $WORKSPACE/srcdir/wine32_only
+    $WORKSPACE/srcdir/wine/configure --host=${target}  --without-x --without-freetype
+    make -j${nproc}
+
+    mkdir $prefix/wine32
+    cd $prefix/wine32
+    $WORKSPACE/srcdir/wine/configure --prefix=${prefix}/wine32 --host=${target} --without-x --without-freetype --with-wine64=$WORKSPACE/srcdir/wine64 --with-wine-tools=$WORKSPACE/srcdir/wine32_only
+    make -j${nproc}
+    make -j${nproc} install
+    """
+
+    wine32_products = Product[
+        ExecutableProduct("wine", :wine, "wine32/loader"),
+    ]
+
+    product_hashes = build_tarballs(copy(SAFE_ARGS), "Wine32", version, sources, wine32_script, [platform32], wine32_products, dependencies; skip_audit=true, preferred_gcc_version=v"11.1")
+    wine32_path, wine32_hash = product_hashes[platform32]
+    push!(sources, ArchiveSource(joinpath("products", wine32_path), wine32_hash))
 end
 
 # Finally, install both:
@@ -86,4 +116,4 @@ final_products = Product[
     ExecutableProduct("wine", :wine),
 ]
 
-build_tarballs(copy(SAFE_ARGS), "Wine", version, sources, script, [platform64], final_products, dependencies, preferred_gcc_version=v"11.1")
+build_tarballs(copy(SAFE_ARGS), "Wine", version, sources, script, platforms, final_products, dependencies, preferred_gcc_version=v"11.1")
