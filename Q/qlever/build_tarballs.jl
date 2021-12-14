728b5a0491c761ec311a5f230235b51b2d29ca12
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2021-12-14 16:24:54 -0600

    Fix formatting

--- Q/qlever/build_tarballs.jl
+++ Q/qlever/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "qlever"
+name = "QLever"
 version = v"0.0.1"
 
 # Collection of sources required to complete build
