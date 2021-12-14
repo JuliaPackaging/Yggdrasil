dc2c4c7818d97ef01e8883eed5911d444ff60ee1
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2021-12-14 12:04:51 -0600

    Add qlever build script

---
+++ Q/qlever/build_tarballs.jl
@@ -0,0 +1,46 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "qlever"
+version = v"0.0.1"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/ad-freiburg/qlever.git", "1d5503c65c604ce8fb5869da7f4f249a28be9dba"),
+    GitSource("https://github.com/joka921/stxxl.git", "b9e44f0ecba7d7111fbb33f3330c3e53f2b75236"),
+    ArchiveSource("https://github.com/google/googletest/archive/refs/tags/release-1.11.0.tar.gz",
+                  "5d44d52cf7ae7ab6ad1208d6ed563f3dc73df10e31a71024a1f9b7ba7dd71427"),
+    GitSource("https://github.com/google/re2.git", "0dade9ff39bb6276f18dd6d4bc12d3c20479ee24"),
+    GitSource("https://github.com/abseil/abseil-cpp.git", "215105818dfde3174fe799600bb0f3cae233d0bf"),
+    GitSource("https://github.com/antlr/antlr4.git", "e4c1a74c66bd5290364ea2b36c97cd724b247357")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+mv $WORKSPACE/srcdir/stxxl/stxxl $WORKSPACE/srcdir/googletest/googletest $WORKSPACE/srcdir/re2/re2 $WORKSPACE/srcdir/abseil-cpp/abseil-cpp $WORKSPACE/srcdir/antlr4/antlr4/ $WORKSPACE/srcdir/qlever/third_party/
+
+cd $WORKSPACE/srcdir/qlever/
+cmake -DCMAKE_BUILD_TYPE=Release -DLOGLEVEL=DEBUG -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DUSE_PARALLEL=true -GNinja . && ninja
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms(; experimental=true)
+
+# The products that we will ensure are always built
+products = Product[
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="Libuuid_jll", uuid="38a345b3-de98-5d2b-a5d3-14cd9215e700"))
+    Dependency(PackageSpec(name="Git_jll", uuid="f8c6e375-362e-5223-8a59-34ff63f689eb"))
+    Dependency(PackageSpec(name="Zstd_jll", uuid="3161d3a3-bdf6-5164-811a-617609db77b4"))
+    Dependency(PackageSpec(name="boost_jll", uuid="28df3c45-c428-5900-9ff8-a3135698ca75"))
+    Dependency(PackageSpec(name="ICU_jll", uuid="a51ab1cf-af8e-5615-a023-bc2c838bba6b"))
+    Dependency(PackageSpec(name="jemalloc_jll", uuid="454a8cc1-5e0e-5123-92d5-09b094f0e876"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"11.1.0")
