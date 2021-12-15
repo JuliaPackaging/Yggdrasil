37fa9ff76571106d5d772113259ba989ba154362
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2021-12-14 18:12:11 -0600

    Are we done yet?

--- Q/qlever/build_tarballs.jl
+++ Q/qlever/build_tarballs.jl
@@ -26,7 +26,6 @@ mkdir build && cd build
 # -DSTXXL_TRY_COMPILE_HEADERS=ON ..
 cmake -DCMAKE_BUILD_TYPE=Release -DLOGLEVEL=DEBUG -DUSE_PARALLEL=true -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DABSL_PROPAGATE_CXX_STD=ON -GNinja .. && ninja
 
-make -j ${nproc}
 """
 
 # These are the platforms we will build for by default, unless further
