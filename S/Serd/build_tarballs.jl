78ec269c65016e1eb1ee320a93a179cf335fa8e5
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2021-12-16 12:33:13 -0600

    Bump version

--- S/Serd/build_tarballs.jl
+++ S/Serd/build_tarballs.jl
@@ -3,17 +3,19 @@
 using BinaryBuilder, Pkg
 
 name = "Serd"
+# <-- this is a lie, we're building v0.30.10, but we need to bump version to build for julia v1.6
+version_fake = v"0.30.11"
 version = v"0.30.10"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("http://download.drobilla.net/serd-0.30.10.tar.bz2", "affa80deec78921f86335e6fc3f18b80aefecf424f6a5755e9f2fa0eb0710edf")
+    ArchiveSource("http://download.drobilla.net/serd-$(version).tar.bz2", "affa80deec78921f86335e6fc3f18b80aefecf424f6a5755e9f2fa0eb0710edf")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd serd-0.30.10/
+cd serd-${version}/
 install_license COPYING
 ./waf configure --prefix=$prefix
 ./waf
@@ -35,4 +37,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies, julia_compat="1.6")
+build_tarballs(ARGS, name, version_fake, sources, script, platforms, products, dependencies, julia_compat="1.6")
