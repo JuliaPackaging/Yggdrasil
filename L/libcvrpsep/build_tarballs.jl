23c3ee3ff7ad0375abe237851d5f20e678284865
Changhyun Kwon <chkwon@users.noreply.github.com>
2021-12-19 02:12:23 +0900

    Update L/libcvrpsep/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/libcvrpsep/build_tarballs.jl
+++ L/libcvrpsep/build_tarballs.jl
@@ -13,6 +13,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/CVRPSEP/
+autoreconf -fiv
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make -j${nproc}
 make install
