63bcd2ce192627bb62885990d806a8987d7faa17
Mosè Giordano <giordano@users.noreply.github.com>
2021-12-19 22:07:21 +0000

    Edit to retrigger build

--- C/cargo_license/build_tarballs.jl
+++ C/cargo_license/build_tarballs.jl
@@ -22,7 +22,6 @@ install_license $WORKSPACE/srcdir/cargo-license/LICENSE
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms(; experimental=true)
-
 # Rust toolchain for i686 Windows is unusable
 filter!(p -> !Sys.iswindows(p) || arch(p) != "i686", platforms)
 
