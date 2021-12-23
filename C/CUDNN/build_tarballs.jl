94abc5afa716f68bdaf7a5f99f800e5526763d86
Tim Besard <tim.besard@gmail.com>
2021-12-23 09:01:26 +0100

    Add a custom zlib to CUDNN on Windows. (#4104)

--- C/CUDNN/build_tarballs.jl
+++ C/CUDNN/build_tarballs.jl
@@ -35,6 +35,8 @@ elif [[ ${target} == x86_64-w64-mingw32 ]]; then
     mv bin/cudnn*64_*.dll ${libdir}
     mv include/* ${prefix}/include
 
+    mv ../dll_x64/zlibwapi.dll ${libdir}
+
     # fixup
     chmod +x ${libdir}/*.dll
 fi
@@ -57,6 +59,11 @@ for cuda_version in cuda_versions
     for (platform, sources) in platforms_and_sources
         augmented_platform = Platform(arch(platform), os(platform); cuda=cuda_tag)
         should_build_platform(triplet(augmented_platform)) || continue
+        if platform == Platform("x86_64", "windows")
+            push!(sources,
+                ArchiveSource("http://www.winimage.com/zLibDll/zlib123dllx64.zip",
+                              "fd324c6923aa4f45a60413665e0b68bb34a7779d0861849e02d2711ff8efb9a4"))
+        end
         build_tarballs(ARGS, name, version, sources, script, [augmented_platform],
                        products, dependencies; lazy_artifacts=true)
     end
