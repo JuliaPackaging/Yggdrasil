50dab9932d5f4177b479f37f08d89bcd71e87533
ASUS1 <crghilardi@yahoo.com>
2021-12-23 19:23:12 -0800

    try bumping gcc version to fix mingw

--- P/ParseGen/build_tarballs.jl
+++ P/ParseGen/build_tarballs.jl
@@ -44,4 +44,4 @@ dependencies = Dependency[
 
 # Build the tarballs, and possibly a `build.jl` as well.
 #this uses std filesystem, so we need gcc 8 at least
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"9")
