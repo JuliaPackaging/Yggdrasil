99aa99274b64a0424e61bbf026038023af7e7136
Mosè Giordano <giordano@users.noreply.github.com>
2021-12-28 12:34:00 +0000

    Update build_tarballs.jl

--- P/protoc/build_tarballs.jl
+++ P/protoc/build_tarballs.jl
@@ -36,4 +36,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"9")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"9", julia_compat="1.6")
