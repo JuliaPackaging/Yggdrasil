ea43648af8c98c3e526e5cf51d05e254472155cc
Tracy Wadleigh <tracy.wadleigh@gmail.com>
2021-12-28 10:00:51 -0800

    Update O/ONNX/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- O/ONNX/build_tarballs.jl
+++ O/ONNX/build_tarballs.jl
@@ -32,8 +32,7 @@ cmake --build . --config Release --target install -- -j${nproc}
 Sadly, no windows support yet. It looks to me like the upstream treats
 `defined(_WIN32)` as synonymous with `build with MSVC`.
 =#
-platforms =
-    filter(p -> p.tags["os"] != "windows", expand_cxxstring_abis(supported_platforms()))
+platforms = expand_cxxstring_abis(supported_platforms(; experimental=true, exclude=Sys.iswindows))
 
 products = [
     FileProduct("lib/libonnx.a", :libonnx),
