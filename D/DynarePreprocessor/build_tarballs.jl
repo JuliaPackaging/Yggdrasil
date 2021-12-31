e99376a3ef6a29c553fc3156b81160bc66c9d891
MichelJuillard <michel.juillard@mjui.fr>
2021-12-31 10:59:12 +0100

    revert to clang for apple and freebsd + patch EquationsTags.hh

--- D/DynarePreprocessor/build_tarballs.jl
+++ D/DynarePreprocessor/build_tarballs.jl
@@ -5,6 +5,7 @@ name = "DynarePreprocessor"
 version = v"4.8.0"
 sources = [
     GitSource("https://git.dynare.org/Dynare/preprocessor.git", "a8fce06dc46ca09329e6899e1fde47f2cd81809b"),
+    DirectorySource("./bundled")
 ]
 
 script = raw"""
@@ -14,13 +15,9 @@ cd ${WORKSPACE}/srcdir/preprocessor
 # remove -lstdc++fs in Makefile.am
 sed s/-lstdc++fs// -i src/Makefile.am
 
-autoreconf -si
+atomic_patch -p1 "./bundled/patches/equationstags.patch"
 
-# use non default gcc for apple and freebsdn
-if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]]; then
-    CC=gcc
-    CXX=g++
-fi
+autoreconf -si
 
 update_configure_scripts
 ./configure --prefix=$prefix  --build=${MACHTYPE} --host=${target} --disable-doc
