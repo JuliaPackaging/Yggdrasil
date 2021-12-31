2fd5a82b774859fbcddb5226b10731a9e71f253b
Mosè Giordano <giordano@users.noreply.github.com>
2021-12-31 11:17:37 +0100

    Update S/sed/build_tarballs.jl
    Co-authored-by: Benjamin Lorenz <benlorenz@users.noreply.github.com>

--- S/sed/build_tarballs.jl
+++ S/sed/build_tarballs.jl
@@ -18,7 +18,7 @@ if [[ "${target}" == *-mingw* ]]; then
     #    sed/sed-compile.o: In function `sprintf':
     #    /opt/x86_64-w64-mingw32/x86_64-w64-mingw32/sys-root/include/stdio.h:366: undefined reference to `__chk_fail'
     # See https://github.com/msys2/MINGW-packages/issues/5868#issuecomment-544107564
-    export LDFLAGS="-lssp"
+    export LIBS="-lssp"
 fi
 
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
