1c2e489c6ce601719a1a0bc77bf8b6838593c1ad
Mosè Giordano <mose@gnu.org>
2022-01-01 20:10:16 +0100

    [DynarePreprocessor] Use macOS SDK 10.15 for `std::filesystem`

--- D/DynarePreprocessor/build_tarballs.jl
+++ D/DynarePreprocessor/build_tarballs.jl
@@ -5,7 +5,9 @@ name = "DynarePreprocessor"
 version = v"4.8.0"
 sources = [
     GitSource("https://git.dynare.org/Dynare/preprocessor.git", "a8fce06dc46ca09329e6899e1fde47f2cd81809b"),
-    DirectorySource("./bundled")
+    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
+                  "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
+    DirectorySource("./bundled"),
 ]
 
 script = raw"""
@@ -21,6 +23,22 @@ rm -f /usr/bin/flex
 # https://github.com/JuliaPackaging/Yggdrasil/issues/3949
 if [[ "${target}" == *-freebsd* ]]; then
     export CPPFLAGS="-I${includedir}"
+elif [[ "${target}" == x86_64-apple-darwin* ]]; then
+    # Install a newer SDK which supports `std::filesystem` to work around the issue
+    #     Undefined symbols for architecture x86_64:
+    #       "__ZNKSt3__14__fs10filesystem4path10__filenameEv", referenced from:
+    #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-ModelTree.o
+    #           __ZNK11StaticModel15writeStaticFileERKNSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEEbbbS8_RKNS0_4__fs10filesystem4pathESD_b in dynare_preprocessor-StaticModel.o
+    #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-StaticModel.o
+    #           __ZNK12DynamicModel16writeDynamicFileERKNSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEEbbbS8_RKNS0_4__fs10filesystem4pathESD_b in dynare_preprocessor-DynamicModel.o
+    #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-DynamicModel.o
+    #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-ModFile.o
+    #           _main in dynare_preprocessor-DynareMain.o
+    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
+    rm -rf /opt/${target}/${target}/sys-root/System
+    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
+    cp -ra System "/opt/${target}/${target}/sys-root/."
+    popd
 fi
 
 atomic_patch -p1 "../patches/patches.patch"
@@ -34,6 +52,7 @@ make install
 mkdir -p "${bindir}"
 strip "src/dynare-preprocessor${exeext}"
 cp "src/dynare-preprocessor${exeext}" "${bindir}"
+install_license COPYING
 """
 
 platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
