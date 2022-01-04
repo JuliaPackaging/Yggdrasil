a1512041d29a3a567f7bcfb6715dba225c659075
Abhaya Parthy <abhayap@gmail.com>
2022-01-04 10:27:17 -0800

    Add compilation support for MacOS AArch64

--- F/FastTransforms/build_tarballs.jl
+++ F/FastTransforms/build_tarballs.jl
@@ -57,4 +57,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"5")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"5", julia_compat="1.6")
