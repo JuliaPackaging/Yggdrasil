3b556643f0220f8e918938b5a203cba96beb4dc1
Abhaya Parthy <abhayap@gmail.com>
2022-01-04 04:22:37 -0800

    Updated libportaudio to v19.7.0 and added support for MacOS AArch64 (#4158)
    * Updated libportaudio to v19.7.0 and added support for MacOS AArch64
    
    * Added upstream patch to fix compile on win32
    
    * Add alternate cmake for windows i686
    
    * Fix cmake path
    
    * [libportaudio] Vendor 32-bit Windows patch
    
    * [libportaudio] Use `alsa_jll` only on Linux
    
    * [libportaudio] No tabs
    
    Co-authored-by: Abhaya Parthy <aparthy@apple.com>
    Co-authored-by: Mosè Giordano <mose@gnu.org>

--- L/libportaudio/build_tarballs.jl
+++ L/libportaudio/build_tarballs.jl
@@ -3,13 +3,13 @@
 using BinaryBuilder
 
 name = "libportaudio"
-version = v"19.6.0"
+version = v"19.7.0"
 
 # Collection of sources required to build libportaudio. Not all of these
 # are used for all platforms.
 sources = [
-    ArchiveSource("http://portaudio.com/archives/pa_stable_v190600_20161030.tgz",
-               "f5a21d7dcd6ee84397446fa1fa1a0675bb2e8a4a6dceb4305a8404698d8d1513"),
+    ArchiveSource("http://files.portaudio.com/archives/pa_stable_v190700_20210406.tgz", 
+                  "47efbf42c77c19a05d22e627d42873e991ec0c1357219c0d74ce6a2948cb2def"),
 
     # This includes a patch
     DirectorySource("./bundled"),
@@ -24,7 +24,7 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
+cd $WORKSPACE/srcdir/portaudio
 
 # move the ASIO SDK to where CMake can find it
 if [ -d "asiosdk2.3.1" ]; then
@@ -32,24 +32,25 @@ if [ -d "asiosdk2.3.1" ]; then
 fi
 
 # apply the patch
-patch -dportaudio -p1 < portaudio_alsa_epipe_v3.diff
+atomic_patch -p1 ../patches/win_ds_fix_warning.patch
+atomic_patch -p1 ../patches/wasapi-32-bit-windows.patch
 
 # First, build libportaudio
-mkdir build
-cd build
+mkdir build-bb
+cd build-bb
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" \
     -DCMAKE_DISABLE_FIND_PACKAGE_PkgConfig=ON \
     -DCMAKE_FIND_ROOT_PATH=$prefix \
-    ../portaudio/
+    ..
 make
 make install
-install_license "${WORKSPACE}/srcdir/portaudio/LICENSE.txt"
+install_license "../LICENSE.txt"
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; experimental=true)
 
 # The products that we will ensure are always built
 products = [
@@ -59,8 +60,8 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     # Build against `ALSA` on Linux
-    Dependency("alsa_jll"),
+    Dependency("alsa_jll"; platforms=filter(Sys.islinux, platforms)),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
