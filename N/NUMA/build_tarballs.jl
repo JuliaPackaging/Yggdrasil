b5e58a0f881351b8027c503e0d25d10371590dcf
Mark Kittisopikul <mkitti@users.noreply.github.com>
2022-01-04 16:16:52 -0500

    Update NUMA to 2.0.14

--- N/NUMA/build_tarballs.jl
+++ N/NUMA/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "NUMA"
-version = v"2.0.13"
+version = v"2.0.14"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/numactl/numactl/releases/download/v2.0.13/numactl-2.0.13.tar.gz",
-                  "991e254b867eb5951a44d2ae0bf1996a8ef0209e026911ef6c3ef4caf6f58c9a"),
+    ArchiveSource("https://github.com/numactl/numactl/releases/download/v2.0.14/numactl-2.0.14.tar.gz",
+                  "826bd148c1b6231e1284e42a4db510207747484b112aee25ed6b1078756bcff6"),
 ]
 
 # Bash recipe for building across all platforms
