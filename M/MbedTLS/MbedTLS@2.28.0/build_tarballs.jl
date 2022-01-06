a63f20118dd15cce832b8f98007d814bd62582e6
Mark Kittisopikul <mkitti@users.noreply.github.com>
2022-01-06 15:44:27 -0500

    Update M/MbedTLS/MbedTLS@2.28.0/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- M/MbedTLS/MbedTLS@2.28.0/build_tarballs.jl
+++ M/MbedTLS/MbedTLS@2.28.0/build_tarballs.jl
@@ -1,4 +1,4 @@
 version = v"2.28.0"
 include("../common.jl")
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.7")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.8")
