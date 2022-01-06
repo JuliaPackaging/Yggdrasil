e047e801037a38a6180e3f6e5d83fba7369df761
Abhaya Parthy <abhayap@gmail.com>
2022-01-06 08:31:24 -0800

    Upgrade libogg to v1.3.5 (#4181)

--- O/Ogg/build_tarballs.jl
+++ O/Ogg/build_tarballs.jl
@@ -1,14 +1,12 @@
 using BinaryBuilder
 
 name = "Ogg"
-version = v"1.3.4"
+version = v"1.3.5"
 
 sources = [
     ArchiveSource("https://ftp.osuosl.org/pub/xiph/releases/ogg/libogg-$(version).tar.xz",
-                  "c163bc12bc300c401b6aa35907ac682671ea376f13ae0969a220f7ddf71893fe"),
+                  "c4d91be36fc8e54deae7575241e03f4211eb102afb3fc0775fbbc1b740016705"),
 ]
-
-version = v"1.3.5" # <-- This version number is a lie to build for experimental platforms
 
 # Bash recipe for building across all platforms
 script = raw"""
@@ -23,7 +21,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
