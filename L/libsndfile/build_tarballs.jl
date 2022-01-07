236e055d4a63cd044aa2ec92703768faa837f081
Mosè Giordano <mose@gnu.org>
2022-01-07 18:27:00 +0100

    [libsndfile] Use CMake instead of autoconf
    This lets us sidestep problem with missing libtool archive for libogg.

--- L/libsndfile/build_tarballs.jl
+++ L/libsndfile/build_tarballs.jl
@@ -14,8 +14,13 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libsndfile-*/
-export CFLAGS="-I${includedir}" 
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-static
+export CFLAGS="-I${includedir}"
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    ..
 make -j${nproc}
 make install
 """
