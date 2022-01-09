b1f9e98f916a34767d6d8b3ed3d0080f915d388b
Seyoon Ko <kos@ucla.edu>
2022-01-09 11:23:37 -0800

    change to ArchiveSource

--- B/BVLS/build_tarballs.jl
+++ B/BVLS/build_tarballs.jl
@@ -3,13 +3,15 @@ using BinaryBuilder, Pkg
 name = "BVLS"
 version = v"1990.03.19"
 sources = [
-    DirectorySource("./bundled/"),
+    ArchiveSource("https://cran.r-project.org/src/contrib/bvls_1.4.tar.gz", "a22e1951d280c7d281ffb6d4440938dad83b59a810739e8a1a7ad632e7714aa1")
 ]
 
 script = raw"""
-cd ${WORKSPACE}/srcdir/
+cd ${WORKSPACE}/srcdir/bvls
 mkdir -p "${libdir}"
-gfortran -shared -fPIC -o $libdir/libbvls.${dlext} bvls.f
+gfortran -shared -fPIC -o $libdir/libbvls.${dlext} src/bvls.f
+install_license inst/COPYRIGHTS
+sed -n 10,19p src/bvls.f > LICENSE
 install_license LICENSE
 """
 
