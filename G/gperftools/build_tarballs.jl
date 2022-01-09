65bed9d21cc82bc95350c52df259c452f555f32c
Mosè Giordano <giordano@users.noreply.github.com>
2022-01-09 23:41:03 +0100

    Update G/gperftools/build_tarballs.jl

--- G/gperftools/build_tarballs.jl
+++ G/gperftools/build_tarballs.jl
@@ -44,6 +44,7 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms(; exclude=Sys.iswindows))
+filter!(p -> !(Sys.islinux(p) && arch(p) = 64 && libc(p) == "musl"), platforms)
 
 # The products that we will ensure are always built
 products = [
