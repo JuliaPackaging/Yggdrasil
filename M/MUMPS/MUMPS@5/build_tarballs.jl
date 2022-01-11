a64e9369afe58ce42f78081702011a75c2a70d4a
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-01-11 22:19:06 +0100

    Remove SCOTCH ordering

--- M/MUMPS/MUMPS@5/build_tarballs.jl
+++ M/MUMPS/MUMPS@5/build_tarballs.jl
@@ -18,22 +18,31 @@ atomic_patch -p1 ${WORKSPACE}/srcdir/patches/mumps_int32.patch
 makefile="Makefile.G95.PAR"
 cp Make.inc/${makefile} Makefile.inc
 
-make_args+=(OPTF="-O -DGEMMT_AVAILABLE" \
+if [[ "${target}" == aarch64-apple-darwin* ]]; then
+    # Fix the error:
+    #     Type mismatch in argument ‘s’ at (1); passed INTEGER(4) to LOGICAL(4)
+    FFLAGS=("-fallow-argument-mismatch")
+fi
+
+make_args+=(OPTF=-O \
             CDEFS=-DAdd_ \
             LMETISDIR=${prefix} \
             IMETIS="-I${includedir}" \
             LMETIS="-L${libdir} -lparmetis -lmetis" \
-            LSCOTCHDIR=${prefix} \
-            ISCOTCH="-I${includedir}" \
-            LSCOTCH="-L${libdir} -lesmumps -lscotch -lscotcherr" \
-            ORDERINGSF="-Dpord -Dparmetis -Dscotch" \
+            ORDERINGSF="-Dpord -Dparmetis" \
             CC="mpicc -fPIC" \
-            FC="mpif90 -fPIC" \
+            FC="mpif90 -fPIC ${FFLAGS[@]}" \
             FL="mpif90 -fPIC" \
             SCALAP="${libdir}/scalapack32.${dlext} ${libdir}/libopenblas.${dlext}" \
             INCPAR= \
-            LIBPAR=-lmpich \
+            LIBPAR=-lscalapack32 \
             LIBBLAS=-lopenblas)
+
+# Options for SCOTCH
+# LSCOTCHDIR=${prefix}
+# ISCOTCH="-I${includedir}"
+# LSCOTCH="-L${libdir} -lesmumps -lscotch -lscotcherr"
+# ORDERINGSF="-Dpord -Dparmetis -Dscotch"
 
 if [[ "${target}" == *-apple* ]]; then
   make_args+=(RANLIB=echo)
@@ -81,19 +90,19 @@ products = [
     LibraryProduct("libsmumps", :libsmumps),
     LibraryProduct("libdmumps", :libdmumps),
     LibraryProduct("libcmumps", :libcmumps),
-    LibraryProduct("libzmumps", :libzmumps),
-    LibraryProduct("libmumps_common", :libmumps_common)
+    LibraryProduct("libzmumps", :libzmumps)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("CompilerSupportLibraries_jll"),
-    Dependency("MPICH_jll"),
-    Dependency("METIS_jll"),
-    Dependency("SCOTCH_jll"),
-    Dependency("PARMETIS_jll"),
-    Dependency("SCALAPACK32_jll"),
-    Dependency("OpenBLAS32_jll"),
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+    Dependency(PackageSpec(name="MPICH_jll", uuid="7cb0a576-ebde-5e09-9194-50597f1243b4")),
+    Dependency(PackageSpec(name="METIS_jll", uuid="d00139f3-1899-568f-a2f0-47f597d42d70")),
+    Dependency(PackageSpec(name="SCOTCH_jll", uuid="a8d0f55d-b80e-548d-aff6-1a04c175f0f9")),
+    Dependency(PackageSpec(name="PARMETIS_jll", uuid="b247a4be-ddc1-5759-8008-7e02fe3dbdaa")),
+    Dependency(PackageSpec(name="SCALAPACK32_jll", uuid="aabda75e-bfe4-5a37-92e3-ffe54af3c273")),
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2")),
+    # Dependency(PackageSpec(name="SCOTCH_jll", uuid="a8d0f55d-b80e-548d-aff6-1a04c175f0f9"))
 ]
 
 # Build the tarballs
