e31e28153921be2dcc9b0364d959085e5c0c86e3
Elliot Saba <staticfloat@gmail.com>
2022-01-12 14:02:24 -0800

    [Glibc] build 1/4 (#4245)

--- G/Glibc/Glibc@2.12.2/build_tarballs.jl
+++ G/Glibc/Glibc@2.12.2/build_tarballs.jl
@@ -10,7 +10,4 @@ script = glibc_script()
 platforms = glibc_platforms(version)
 products = glibc_products()
 dependencies = glibc_dependencies()
-
-# Build the tarballs
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", lock_microarchitecture=false)
-
