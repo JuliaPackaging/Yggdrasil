2768e91e58ff25666f2ea81ab770de19b0f86282
Kimmo Linna <kimmo.linna@gmail.com>
2022-01-12 22:57:42 +0200

    DuckDB: Parquet extension support (#4242)
    * DuckDB:Support for Parquet extension
    
    * Parquet support with a proper version number
    
    * DuckDB: manual entry for version number
    
    * [DuckDB] Add patch to fix case of Windows header file
    
    Co-authored-by: Mosè Giordano <mose@gnu.org>

--- D/DuckDB/build_tarballs.jl
+++ D/DuckDB/build_tarballs.jl
@@ -7,17 +7,21 @@ version = v"0.3.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/duckdb/duckdb/archive/refs/tags/v$(version).tar.gz", "ae2367d0a393be59e137ffa975f48f60b113b2a72aacc24fbc59afa0cbc3a511")
+    ArchiveSource("https://github.com/duckdb/duckdb/archive/refs/tags/v$(version).tar.gz", "ae2367d0a393be59e137ffa975f48f60b113b2a72aacc24fbc59afa0cbc3a511"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/duckdb*/
 
+atomic_patch -p1 ../patches/0001-right-case-for-winsock2.h.patch
+
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
       -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DBUILD_PARQUET_EXTENSION=TRUE \
       -DCMAKE_BUILD_TYPE=Release -DDISABLE_UNITY=TRUE \
       -DENABLE_SANITIZER=FALSE -DBUILD_UNITTESTS=FALSE ..
 make -j${nproc}
