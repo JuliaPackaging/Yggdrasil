1c381b10858a5f75ab93ee3c04e35bd6961a4af1
Mosè Giordano <giordano@users.noreply.github.com>
2022-01-15 21:11:03 +0000

    More dummy edit

--- T/Taskflow/build_tarballs.jl
+++ T/Taskflow/build_tarballs.jl
@@ -16,9 +16,9 @@ cd $WORKSPACE/srcdir/taskflow-*
 mkdir build && cd build
 
 cmake .. \
--DCMAKE_INSTALL_PREFIX=$prefix \
--DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
--DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release
 
 make -j${nproc}
 make install
