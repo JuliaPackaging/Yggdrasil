3566160ccc71d106652e42305b694cb64d6bb64e
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-01-18 18:11:14 +0100

    [GoogleTest] V1.11.0

--- G/GoogleTest/build_tarballs.jl
+++ G/GoogleTest/build_tarballs.jl
@@ -2,12 +2,12 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "GoogleTest"
-version = v"1.10.0"
+ name = "GoogleTest"
+ version = v"1.11.0"
 
-# Collection of sources required to complete build
-sources = [
-    GitSource("https://github.com/google/googletest.git", "703bd9caab50b139428cea1aaff9974ebee5742e")
+ # Collection of sources required to complete build
+ sources = [
+     GitSource("https://github.com/google/googletest.git", "e2239ee6043f73722e7aa812a459f54a28552929")
 ]
 
 # Bash recipe for building across all platforms
