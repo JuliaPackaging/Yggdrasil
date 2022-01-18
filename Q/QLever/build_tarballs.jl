633bc1fb41233e7ad30776345952f4f33789afe2
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-01-18 13:36:21 +0000

    Add libunwind

--- Q/QLever/build_tarballs.jl
+++ Q/QLever/build_tarballs.jl
@@ -99,6 +99,7 @@ dependencies = [
     Dependency(PackageSpec(name="boost_jll", uuid="28df3c45-c428-5900-9ff8-a3135698ca75"); compat="~1.76")
     Dependency(PackageSpec(name="ICU_jll", uuid="a51ab1cf-af8e-5615-a023-bc2c838bba6b"); compat = "~69.1")
     Dependency(PackageSpec(name="jemalloc_jll", uuid="454a8cc1-5e0e-5123-92d5-09b094f0e876"))
+    Dependency(PackageSpec(name="LibUnwind_jll", uuid="745a5e78-f969-53e9-954f-d19f2f74f4e3"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
