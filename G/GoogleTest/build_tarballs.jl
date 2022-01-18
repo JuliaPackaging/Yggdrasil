08443fe003eecf9c3fd22a410fa9322759410d91
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-01-18 22:05:48 +0100

    Add min gcc version

--- G/GoogleTest/build_tarballs.jl
+++ G/GoogleTest/build_tarballs.jl
@@ -36,9 +36,6 @@ make install
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
 
-# Only CXX 11 is supported
-filter!(x -> cxxstring_abi(x) != "cxx03", platforms)
-
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libgtest_main", :libgtest_main),
@@ -50,4 +47,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"5")
