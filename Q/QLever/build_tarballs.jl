67e83864bea938d3aa7b188b04cc525717b4bb43
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-01-18 12:06:48 +0000

    Deactivate mac

--- Q/QLever/build_tarballs.jl
+++ Q/QLever/build_tarballs.jl
@@ -9,8 +9,8 @@ version = v"0.0.1"
 sources = [
     GitSource("https://github.com/ad-freiburg/qlever.git", "f338a371ca81938fbbd059df5654cd39b145fefb"),
     DirectorySource("./bundled"),
-    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
-                  "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
+    # ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
+    #               "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
 ]
 
 
