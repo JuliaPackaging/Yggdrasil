d89cb4ffdc8e96ad39b6242b3574c59561c1b546
Tim Besard <tim.besard@gmail.com>
2022-01-19 11:55:09 +0100

    Bump SPIRV LLVM translator. (#4281)
    Includes build against LLVM 13 for Julia 1.8.

--- S/SPIRV_LLVM_Translator/SPIRV_LLVM_Translator@12/build_tarballs.jl
+++ S/SPIRV_LLVM_Translator/SPIRV_LLVM_Translator@12/build_tarballs.jl
@@ -3,12 +3,12 @@ include("../common.jl")
 version = v"12.0"
 
 # Collection of sources required to build attr
-sources = [GitSource(repo, "67d3e271a28287b2c92ecef2f5e98c49134e5946")]
+sources = [GitSource(repo, "3ed66fc7524d648dec5a71c5e09985d5aff5bd99")]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency(PackageSpec(name="LLVM_full_jll", version=v"12.0.0")),
-    #Dependency(PackageSpec(name="libLLVM_jll", version=v"12.0.0"))
+    BuildDependency(PackageSpec(name="LLVM_full_jll", version=v"12.0.1")),
+    #Dependency(PackageSpec(name="libLLVM_jll", version=v"12.0.1"))
 ]
 
 # Build the tarballs.
