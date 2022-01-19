0b1d931f33640b48e612cb65ab65ad94c1e65edf
Sharan Yalburgi <sharanyalburgi@gmail.com>
2022-01-19 20:13:21 +0530

    Update build_tarballs.jl

--- F/FMUComplianceChecker/build_tarballs.jl
+++ F/FMUComplianceChecker/build_tarballs.jl
@@ -19,13 +19,14 @@ mkdir ${bindir}/
 if [[ "${target}" == *linux* ]]; then
     cd FMUChecker-2.0.4-linux64/
     mv ./fmuCheck.linux64 ${bindir}/fmuCheck
-    chmod +x ${bindir}/*
 fi
 
 if [[ "${target}" == *mingw* ]]; then
     cd FMUChecker-2.0.4-win64/
     mv ./fmuCheck.win64.exe ${bindir}/fmuCheck.exe
 fi
+
+chmod +x ${bindir}/*
 
 LIC_DIR="${prefix}/share/licenses/${SRC_NAME}"
 mkdir -p "${LIC_DIR}"
@@ -43,7 +44,7 @@ platforms = [
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("fmuCheck", :libFMUCheck)
+    ExecutableProduct("fmuCheck", :libFMUCheck)
 ]
 
 # Dependencies that must be installed before this package can be built
