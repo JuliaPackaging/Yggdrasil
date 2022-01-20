db0389129b55aadbfcdf46d1f581e68bcdf8bcef
Mosè Giordano <giordano@users.noreply.github.com>
2022-01-20 22:55:37 +0000

    [FMUComplianceChecker] Remove extra stuff from the tarball

--- F/FMUComplianceChecker/build_tarballs.jl
+++ F/FMUComplianceChecker/build_tarballs.jl
@@ -27,6 +27,8 @@ mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j${nproc}
 install -Dm755 fmuCheck.* "${bindir}/fmuCheck${exeext}"
+# Delete expat stuff
+rm -rf "${includedir}" "${prefix}/lib"
 install_license "../LICENSE"
 """
 
