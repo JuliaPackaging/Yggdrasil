27475fde53bacc6603ca5a695c23f47b67a8d535
Mosè Giordano <mose@gnu.org>
2022-01-21 13:03:29 +0100

    [libblastrampoline] Fix julia compat to v1.8
    [skip build] [skip ci]

--- L/libblastrampoline/build_tarballs.jl
+++ L/libblastrampoline/build_tarballs.jl
@@ -34,7 +34,7 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6",
+               julia_compat="1.8",
                init_block = """
                @static if VERSION < v"1.7.0-DEV.641"
                        ccall((:lbt_forward, libblastrampoline), Int32, (Cstring, Int32, Int32, Ptr{Cvoid}),
