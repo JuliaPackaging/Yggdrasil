e263ef72827d2f824bbef622a9820338b0bae426
Erik Schnetter <schnetter@gmail.com>
2022-01-22 15:45:13 -0500

    libfyaml: Correct package name

--- L/libfyaml/build_tarballs.jl
+++ L/libfyaml/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "libyfaml"
+name = "libfyaml"
 version = v"0.7.12"
 
 # Collection of sources required to complete build
