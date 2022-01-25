718125f59be0f54a1c6a9bb4276808247a5a2e67
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-01-25 21:12:06 +0100

    Bump boost gcc to test for qlever

--- B/boost/build_tarballs.jl
+++ B/boost/build_tarballs.jl
@@ -103,4 +103,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"11.1.0", julia_compat="1.6")
