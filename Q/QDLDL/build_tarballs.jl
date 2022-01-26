adc6363662c01731fac01ec2a31b862ce98bfad4
Mosè Giordano <mose@gnu.org>
2022-01-26 20:45:52 +0000

    [QDLDL] Rename from `qdldl` to match styling of upstream project

--- Q/QDLDL/build_tarballs.jl
+++ Q/QDLDL/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "qdldl"
+name = "QDLDL"
 version = v"0.1.5"
 
 # Collection of sources required to complete build
