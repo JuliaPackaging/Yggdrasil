8f57eee74f7c151d712b8fd9d4df22f034ca58fa
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2022-01-27 21:23:44 +0100

    Rough vegafusion build script

---
+++ V/vegafusion/build_tarballs.jl
@@ -0,0 +1,32 @@
+using BinaryBuilder
+
+name = "Tokei"
+version = v"12.1.2"
+
+sources = [
+    GitSource("https://github.com/vegafusion/vegafusion.git",
+                  "4f261d0aea9eaf1c220de29c5f396d95025e4af2"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ${WORKSPACE}/srcdir/vegafusion/vegafusion-core/
+cargo build --release
+install_license ../LICENSE
+"""
+
+platforms = supported_platforms()
+# Our Rust toolchain for i686 Windows is unusable
+filter!(p -> !Sys.iswindows(p) || arch(p) != "i686", platforms)
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("vegafusion-core", :vegafusioncore),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers=[:c, :rust]; julia_compat="1.6")
