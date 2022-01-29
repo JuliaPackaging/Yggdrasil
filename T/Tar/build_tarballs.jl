0d271f112869adac5477ce0e0f97890f1cb9bc71
Mosè Giordano <mose@gnu.org>
2022-01-29 00:40:11 +0000

    [Tar] Add MinGW patches for Windows

--- T/Tar/build_tarballs.jl
+++ T/Tar/build_tarballs.jl
@@ -9,11 +9,22 @@ version = v"1.34"
 sources = [
     ArchiveSource("https://ftp.gnu.org/gnu/tar/tar-$(version.major).$(version.minor).tar.xz",
                   "63bebd26879c5e1eea4352f0d03c991f966aeb3ddeb3c7445c902568d5411d28"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/tar-*/
+
+if [[ "${target}" == *-mingw* ]]; then
+    # Apply patches for MinGW:
+    # https://github.com/msys2/MSYS2-packages/tree/master/tar
+    atomic_patch -p1 ../patches/gnulib-weak-symbols.patch
+    atomic_patch -p1 ../patches/tar-1.33-msys2.patch
+    atomic_patch -p1 ../patches/tar-1.33-textmount.patch
+    autoreconf -fv
+fi
+
 export FORCE_UNSAFE_CONFIGURE=1
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make -j${nproc}
@@ -21,9 +32,8 @@ make install
 """
 
 # These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line.  We are manually disabling
-# many platforms that do not seem to work.
-platforms = filter!(!Sys.iswindows, supported_platforms(; experimental=true))
+# platforms are passed in on the command line.
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
@@ -32,7 +42,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("Attr_jll"),
+    Dependency("Attr_jll"; platforms=filter(Sys.islinux, platforms)),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
