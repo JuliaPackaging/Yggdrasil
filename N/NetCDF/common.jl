8a40b46f4b8fc50a345af2e91260ed738ecf1488
gbaraldi <baraldigabriel@gmail.com>
2022-01-29 10:38:30 -0300

    New try

--- N/NetCDF/common.jl
+++ N/NetCDF/common.jl
@@ -72,7 +72,9 @@ nc-config --all
         Platform("x86_64", "windows"),
         Platform("i686", "windows"),
     ]
-
+    if min_julia_version == v"1.6"
+        push!(platforms, Platform("aarch64","macos"))
+    end
     # The products that we will ensure are always built
     products = [
         LibraryProduct("libnetcdf", :libnetcdf),
