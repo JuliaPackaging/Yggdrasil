05f1f4c7effab42d29a19dc622b37826b5643ab8
Wimmerer <wrkimmerer@outlook.com>
2022-01-29 21:50:09 -0500

    init

---
+++ L/lapackpp/build_tarballs.jl
@@ -0,0 +1,43 @@
+using BinaryBuilder
+
+name = "blaspp"
+version = v"2021.04.01"
+
+# Collection of sources required to build PETSc. Avoid using the git repository, it will
+# require building SOWING which fails in all non-linux platforms.
+sources = [
+    GitSource("https://bitbucket.org/icl/lapackpp.git", "31d969200a9f65390f56ac2ea48888bd10a13397")
+]
+
+# Bash recipe for building across all platforms
+
+# Needs to add -Dcapi eventually once it's added to the cmake build system. Note yet available under CMAKAE toolchain.
+script = raw"""
+cd lapackpp
+mkdir build && cd build
+cmake \
+  -DCMAKE_INSTALL_PREFIX=${prefix} \
+  -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+  -DCMAKE_BUILD_TYPE="Release" \
+  -DLAPACK_LIBRARIES="-lblastrampoline" \
+  -DBLAS_LIBRARIES="-lblastrampoline" \
+  -DCMAKE_CXX_FLAGS="-I${includedir}/libblastrampoline/LP64/${target}" \
+  -Dbuild_tests=no \
+  ..
+make -j${nproc}
+make install
+"""
+
+# We attempt to build for all defined platforms
+platforms = expand_cxxstring_abis(supported_platforms(;experimental=true))
+products = [
+    LibraryProduct("libblaspp", :libblaspp)
+]
+
+dependencies = [
+    Dependency("CompilerSupportLibraries_jll"),
+    Dependency("blaspp_jll"),
+]
+
+# Build the tarballs.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"7")
