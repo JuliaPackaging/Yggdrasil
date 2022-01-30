8d76002e57f8d5776390ebab4a4a25e873dc04e7
Simon Byrne <simonbyrne@gmail.com>
2022-01-30 14:18:53 -0800

    [TempestRemap] Update binary compatibility (#4340)
    * [TempestRemap] Update binary compatibility
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * Update T/TempestRemap/build_tarballs.jl
    
    * Update T/TempestRemap/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- T/TempestRemap/build_tarballs.jl
+++ T/TempestRemap/build_tarballs.jl
@@ -73,7 +73,7 @@ products = [
 
 dependencies = [
     Dependency("OpenBLAS32_jll"),
-    Dependency("NetCDF_jll"),
+    Dependency("NetCDF_jll", compat="400.802.101"),
     Dependency("HDF5_jll"),
     # The following is adapted from NetCDF_jll
     BuildDependency(PackageSpec(; name="MbedTLS_jll", version=v"2.24.0")),
