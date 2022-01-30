2aabbb52c5a88e9ea1e5f8416fb6b397cfb6949f
Mosè Giordano <mose@gnu.org>
2022-01-30 23:32:31 +0000

    [NetCDFF] Update to v4.5.4 and link to NetCDF v4.8

--- N/NetCDFF/build_tarballs.jl
+++ N/NetCDFF/build_tarballs.jl
@@ -3,11 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "NetCDFF"
-version = v"4.5.3"
+version = v"4.5.4"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v4.5.3.tar.gz", "c6da30c2fe7e4e614c1dff4124e857afbd45355c6798353eccfa60c0702b495a")
+    ArchiveSource("https://github.com/Unidata/netcdf-fortran/archive/refs/tags/v$(version).tar.gz",
+                  "1a8613cb639e83e2df5a8e6c21fa48a0c64b053c244abddecec66cfcac03a48a"),
 ]
 
 # Bash recipe for building across all platforms
@@ -42,7 +43,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="NetCDF_jll", uuid="7243133f-43d8-5620-bbf4-c2c921802cf3"); compat="400.701.400 - 400.799"),
+    Dependency(PackageSpec(name="NetCDF_jll", uuid="7243133f-43d8-5620-bbf4-c2c921802cf3"); compat="400.802.101"),
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
     # `MbedTLS_jll` is an indirect dependency through NetCDF, we need to specify
     # a compatible build version for this to work.
@@ -50,4 +51,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
