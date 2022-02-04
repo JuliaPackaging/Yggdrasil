d34d72bd1498a37ec992ef075f195f0cb051ed4a
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-02-04 13:21:30 +0100

    fix constexpr issue

--- Q/QLever/build_tarballs.jl
+++ Q/QLever/build_tarballs.jl
@@ -7,7 +7,7 @@ version = v"0.0.1"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/joka921/QLever.git", "73fdce940d589dcb40de6846b7f9bc82ba48ec28"),
+    GitSource("https://github.com/joka921/QLever.git", "d98cf52bc430c0a132ffd898ad748fcc1822583b"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
                   "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
 ]
