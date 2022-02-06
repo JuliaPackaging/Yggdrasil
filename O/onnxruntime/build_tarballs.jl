4403c5489b45d9cf232ded03af40d559310035e9
Jesper Stemann Andersen <jsa@ihp.dk>
2022-02-06 18:14:33 +0100

    Re-introduced the onnxruntime python-based build script
    Includes:
    * Building aarch64-apple-darwin from source
    * Added workaround for arm-linux-gnueabihf
    * Disabled build of unit tests
    * Replaced custom protoc with protoc_jll
    * Restricted expansion of CXX string ABIs to Linux
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- O/onnxruntime/build_tarballs.jl
+++ O/onnxruntime/build_tarballs.jl
@@ -8,47 +8,50 @@ version = v"1.10.0"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/microsoft/onnxruntime.git", "0d9030e79888d1d5828730b254fedc53c7b640c1"),
-    ArchiveSource("https://github.com/protocolbuffers/protobuf/releases/download/v3.16.1/protoc-3.16.1-linux-x86_64.zip", "dffb7209d31b7e87e8e8ba2d5d869ceda5a5cea8883c4b13a726611a0dbd8a7c"; unpack_target = "protoc"),
     ArchiveSource("https://github.com/microsoft/onnxruntime/releases/download/v$version/onnxruntime-win-x64-$version.zip", "a0c6db3cff65bd282f6ba4a57789e619c27e55203321aa08c023019fe9da50d7"),
-    ArchiveSource("https://github.com/microsoft/onnxruntime/releases/download/v$version/onnxruntime-win-x86-$version.zip", "fd1680fa7248ec334efc2564086e9c5e0d6db78337b55ec32e7b666164bdb88c"),
-    ArchiveSource("https://github.com/microsoft/onnxruntime/releases/download/v$version/onnxruntime-osx-arm64-$version.tgz", "1dbf1b0aed50849a58ae74d6790c35aaffb2362eaff64a8c9bc6fc39c2545357")
+    ArchiveSource("https://github.com/microsoft/onnxruntime/releases/download/v$version/onnxruntime-win-x86-$version.zip", "fd1680fa7248ec334efc2564086e9c5e0d6db78337b55ec32e7b666164bdb88c")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 
-if [[ $target == *-w64-mingw32 || $target == aarch64-apple-darwin ]]; then
-    if [[ $target == *-w64-mingw32 ]]; then
-        if [[ $target == x86_64-w64-mingw32 ]]; then
-            dist_name=onnxruntime-win-x64
-        elif [[ $target == i686-w64-mingw32 ]]; then
-            dist_name=onnxruntime-win-x86
-        fi
-        chmod 755 $dist_name*/lib/*
-    elif [[ $target == aarch64-apple-darwin ]]; then
-        dist_name=onnxruntime-osx-arm64
+if [[ $target == *-w64-mingw32* ]]; then
+    if [[ $target == x86_64-w64-mingw32* ]]; then
+        dist_name=onnxruntime-win-x64
+    elif [[ $target == i686-w64-mingw32* ]]; then
+        dist_name=onnxruntime-win-x86
     fi
-
+    chmod 755 $dist_name*/lib/*
     mkdir -p $includedir $libdir
     cp -a $dist_name*/include/* $includedir
     cp -a $dist_name*/lib/* $libdir
     install_license $dist_name*/LICENSE
 else
+    # aarch64-apple-darwin fix, cf. https://github.com/microsoft/onnxruntime/issues/6573#issuecomment-900877035
+    if [[ $target == aarch64-apple-darwin* ]]; then
+        cmake_extra_defines=CMAKE_OSX_ARCHITECTURES='arm64'
+    # Workaround for https://github.com/microsoft/onnxruntime/issues/2152
+    elif [[ $target == arm-linux-gnueabihf* ]]; then
+        cmake_extra_defines="onnxruntime_DEV_MODE=OFF"
+    fi
+
     cd onnxruntime
-    git submodule update --init --recursive
-    mkdir -p build
-    cd build
-    cmake $WORKSPACE/srcdir/onnxruntime/cmake \
-        -DCMAKE_INSTALL_PREFIX=$prefix \
-        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-        -DCMAKE_BUILD_TYPE=Release \
-        -DONNX_CUSTOM_PROTOC_EXECUTABLE=$WORKSPACE/srcdir/protoc/bin/protoc \
-        -Donnxruntime_BUILD_SHARED_LIB=ON \
-        -Donnxruntime_BUILD_UNIT_TESTS=OFF \
-        -Donnxruntime_CROSS_COMPILING=ON \
-        $CMAKE_EXTRA_ARGS
-    make -j $nproc
+    python3 tools/ci_build/build.py \
+        --build \
+        --build_dir $WORKSPACE/srcdir/onnxruntime/build \
+        --build_shared_lib \
+        --cmake_extra_defines \
+            CMAKE_INSTALL_PREFIX=$prefix \
+            CMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+            onnxruntime_BUILD_UNIT_TESTS=OFF \
+            $cmake_extra_defines \
+        --config Release \
+        --parallel $nproc \
+        --path_to_protoc_exe $host_bindir/protoc \
+        --skip_tests \
+        --update
+    cd build/Release
     make install
     install_license $WORKSPACE/srcdir/onnxruntime/LICENSE
 fi
@@ -58,11 +61,11 @@ fi
 # platforms are passed in on the command line
 function platform_exclude_filter(p::Platform)
     libc(p) == "musl" ||
-    p == Platform("i686", "Linux") ||
+    p == Platform("i686", "Linux") || # No binary - and source build fails linking CXX shared library libonnxruntime.so
     Sys.isfreebsd(p)
 end
 platforms = supported_platforms(; exclude=platform_exclude_filter)
-platforms = expand_cxxstring_abis(platforms)
+platforms = expand_cxxstring_abis(platforms; skip=!Sys.islinux)
 
 # The products that we will ensure are always built
 products = [
@@ -70,7 +73,9 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[]
+dependencies = [
+    HostBuildDependency(PackageSpec("protoc_jll", Base.UUID("c7845625-083e-5bbe-8504-b32d602b7110"), v"3.16.1"))
+]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
