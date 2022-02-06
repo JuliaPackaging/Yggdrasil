9e01e747b4648467b4d2db3b1d7fd1d5c93ba4a2
Mosè Giordano <giordano@users.noreply.github.com>
2022-02-06 14:48:59 +0000

    [protoc] Build v3.16.1 (#4376)

--- P/protoc/build_tarballs.jl
+++ P/protoc/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "protoc"
-version = v"3.19.1"
+version = v"3.16.1"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/protocolbuffers/protobuf.git", "7c40b2df1fdf6f414c1c18c789715a9c948a0725"),
+    GitSource("https://github.com/protocolbuffers/protobuf.git", "791a4355c365bd92720160671a7491be168055cb"),
 ]
 
 # Bash recipe for building across all platforms
@@ -21,7 +21,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
