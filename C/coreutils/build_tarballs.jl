ad870a189511fb34ebbd28a23c8582abb67f64b8
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-02-07 14:12:35 +0100

    Update C/coreutils/build_tarballs.jl

--- C/coreutils/build_tarballs.jl
+++ C/coreutils/build_tarballs.jl
@@ -26,7 +26,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; exclude=p -> !(Sys.islinux(p) | Sys.isbsd(p)))
+platforms = supported_platforms(; exclude=p -> !(Sys.islinux(p) | Sys.isfreebsd(p)))
 
 # The products that we will ensure are always built
 products = [
