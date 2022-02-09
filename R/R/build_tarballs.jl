c6c921cd4c8e97abf9e401cc4148e95b05586acc
Mosè Giordano <mose@gnu.org>
2022-02-09 12:13:16 +0000

    [R] Set CPPFLAGS for musl

--- R/R/build_tarballs.jl
+++ R/R/build_tarballs.jl
@@ -14,7 +14,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/R-*
-
+export CPPFLAGS="-I${includedir}"
 ./configure --prefix=${prefix} \
     --build=${MACHTYPE} \
     --host=${target} \
