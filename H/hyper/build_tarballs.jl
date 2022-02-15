aa88de08754977f967b28c5e7813b61bf270ed3c
Mosè Giordano <mose@gnu.org>
2022-02-15 20:04:10 +0000

    [hyper] Add builder

---
+++ H/hyper/build_tarballs.jl
@@ -0,0 +1,35 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "hyper"
+version = v"0.14.17"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://github.com/hyperium/hyper/archive/refs/tags/v$(version).tar.gz",
+                  "64420fd550f43af09b0722b3504d4fd919de642d63f01ad54108aa854f5f5470"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/hyper*/
+cargo build --release
+install -Dm 755 "target/${rust_target}/release/libhyper.${dlext}" "${libdir}/libhyper.${dlext}"
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libhyper", :libhyper),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers=[:c, :rust], julia_compat="1.6")
