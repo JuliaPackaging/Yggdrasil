e1411633e12eb280532e6638584c4ecdaa76addd
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-02-16 11:29:32 +0100

    Patch over sw_vers fix

--- P/ParseGen/build_tarballs.jl
+++ P/ParseGen/build_tarballs.jl
@@ -9,7 +9,8 @@ version = v"2.0.0"
 sources = [
     ArchiveSource("https://github.com/sandialabs/parsegen-cpp/archive/refs/tags/v$(version).tar.gz", "c6c7c4958d1c6ab77bf0970b5aacae4b63603f702666492638ee8c0bdf3125c8"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
-    "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62")
+    "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
+    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
@@ -27,7 +28,7 @@ if [[ "${target}" == x86_64-apple-darwin* ]]; then
     rm -rf /opt/${target}/${target}/sys-root/System
     cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
     cp -ra System "/opt/${target}/${target}/sys-root/."
-
+    cp sw_vers "${bindir}/."
     popd
 elif [[ "${target}" == aarch64-apple-darwin* ]]; then
     # TODO: we need to fix this in the compiler wrappers
@@ -42,7 +43,6 @@ cmake .. \
 
 make -j${nproc}
 make install
-
 """
 
 # These are the platforms we will build for by default, unless further
