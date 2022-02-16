b0839848ad46e344f1ceffd0a020a63ee8469862
Elliot Saba <staticfloat@gmail.com>
2022-02-16 22:47:22 +0000

    Add utilities for dealing with artifacts that target a specific platform

--- fancy_toys.jl
+++ fancy_toys.jl
@@ -1,7 +1,7 @@
 # This is a collection of toys under the Yggdrasil tree for the good <s>kids</s>
 # developers.  These utilities can be employed in builder files.
 
-using BinaryBuilder, Pkg, LibGit2
+using BinaryBuilder, Pkg, LibGit2, Base.BinaryPlatforms
 
 """
     should_build_platform(platform) -> Bool
@@ -111,4 +111,68 @@ function get_addable_spec(name::AbstractString, version::VersionNumber)
         tree_hash=tree_hash_sha1,
         repo=repo,
     )
+end
+
+"""
+    encode_target_platform(target_platform::Platform)
+
+Encode a target as tags within a host platform object.  This allows recipes such as
+GCC to provide multiple "target" artifacts, while all still properly identifying themselves
+as having a single "host" architecture.
+"""
+function encode_target_platform(target_platform::Platform;
+                               host_platform::Platform = Platform("x86_64", "linux"; libc="musl"))
+    p = deepcopy(host_platform)
+    for (tag, value) in tags(target_platform)
+        p["target_"*tag] = value
+    end
+    return p
+end
+
+"""
+    decode_target_triplet(encoded_platform::Platform)
+
+Decode a target from tags within `encoded_platform`.
+"""
+function decode_target_platform(encoded_platform::Platform)
+    target_tags = Dict(k => v for (k, v) in tags(p) if startswith(k, "target_"))
+
+    arch = pop!(target_tags, "arch")
+    os = pop!(target_tags, "os")
+    return Platform(arch, os; target_tags...)
+end
+
+"""
+    bash_parse_encoded_target_triplet()
+
+This generates bash code to parse out an encoded target triplet from 
+"""
+function bash_parse_encoded_target_triplet()
+    return raw"""
+    # Figure out target arch from full target triplet
+    function parse_platform_tag() {
+        tr '-' '\n' <<< "${bb_full_target}" | grep "${1}" | cut -d'+' -f2-
+    }
+    encoded_arch=$(parse_platform_tag "target_arch")
+    encoded_os=$(parse_platform_tag "target_os")
+    encoded_libc=$(parse_platform_tag "target_libc")
+    encoded_callabi=$(parse_platform_tag "target_callabi")
+    if [[ "${encoded_libc}" == "glibc" ]] || [[ "${encoded_os}${encoded_libc}" == "linux" ]]; then
+        encoded_libc="gnu"
+    fi
+
+    # Re-assemble `encoded_target` according to some simplified target generation rules
+    if [[ "${encoded_os}" == "freebsd" ]]; then
+        encoded_target="${encoded_arch}-unknown-${encoded_os}"
+    elif [[ "${encoded_os}" == "darwin" ]]; then
+        encoded_target="${encoded_arch}-apple-${encoded_os}"
+    elif [[ "${encoded_os}" == "windows" ]]; then
+        encoded_target="${encoded_arch}-w64-mingw32"
+    elif [[ "${encoded_os}" == "linux" ]]; then
+        encoded_target="${encoded_arch}-${encoded_os}-${encoded_libc}${encoded_callabi}"
+    else
+        echo "ERROR: Unknown encoded OS '${encoded_os}'!" >&2
+        exit 1
+    fi
+    """
 end
