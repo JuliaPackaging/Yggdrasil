3fd5578d84da24a30158241238e9aa356ea16a93
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-02-16 12:08:06 +0100

    [skip ci] Indentation

--- P/ParseGen/build_tarballs.jl
+++ P/ParseGen/build_tarballs.jl
@@ -30,10 +30,10 @@ if [[ "${target}" == x86_64-apple-darwin* ]]; then
 fi
 
 cmake .. \
--DCMAKE_INSTALL_PREFIX=${prefix} \
--DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
--DCMAKE_BUILD_TYPE=Release \
--DBUILD_SHARED_LIBS=ON
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON
 
 make -j${nproc}
 make install
