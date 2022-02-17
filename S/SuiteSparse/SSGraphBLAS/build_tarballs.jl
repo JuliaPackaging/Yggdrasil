bd76a209bf4138cc1bcb0e0e8d72a763dd3bbc2b
Wimmerer <wrkimmerer@outlook.com>
2022-02-17 01:41:19 -0500

    version bump, some upstream fixes to build system

--- S/SuiteSparse/SSGraphBLAS/build_tarballs.jl
+++ S/SuiteSparse/SSGraphBLAS/build_tarballs.jl
@@ -1,30 +1,22 @@
 using BinaryBuilder, Pkg
 
 name = "SSGraphBLAS"
-version = v"6.1.3"
+version = v"6.2.0"
 
 # Collection of sources required to build SuiteSparse:GraphBLAS
 sources = [
     GitSource("https://github.com/DrTimothyAldenDavis/GraphBLAS.git",
-        "8144c2dc53acca8236dc1fa04d1df8218b3e2b1d"),
-    DirectorySource("./bundled")
+        "4e6ad10e942265ce049afc553e7da25554a13523")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 # Compile GraphBLAS
 cd $WORKSPACE/srcdir/GraphBLAS
-atomic_patch -p1 "${WORKSPACE}/srcdir/cpu_features.patch"
 
-make -j${nproc} CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DGBCOMPACT=1"
+make -j${nproc} CMAKE_OPTIONS="-DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}"
 make install
 install_license LICENSE
-if [[ ! -f "${libdir}/libgraphblas.${dlext}" ]]; then
-    # For mysterious reasons, the shared library is not installed
-    # when building for Windows
-    mkdir -p "${libdir}"
-    cp "build/libgraphblas.${dlext}" "${libdir}"
-fi
 """
 
 # These are the platforms we will build for by default, unless further
