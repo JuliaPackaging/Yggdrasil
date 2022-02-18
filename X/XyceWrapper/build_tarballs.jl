3f57a2f7b0f68786923fec210d3ace7a4ef5b2e7
Steve Kelly <kd2cca@gmail.com>
2022-02-18 14:15:27 -0500

    XyceWrapper: 1.6 -> 1.6.3

--- X/XyceWrapper/build_tarballs.jl
+++ X/XyceWrapper/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-julia_versions = [v"1.6", v"1.7"]
+julia_versions = [v"1.6.3", v"1.7"]
 
 name = "XyceWrapper"
 version = v"0.2.0"
