71c2504fe570de93b14662d736f00e4b55fe1a36
Jesper Stemann Andersen <jsa@ihp.dk>
2022-02-21 16:22:44 +0100

    Renamed Torch to TorchCUDA

--- T/Torch/TorchCUDA/build_tarballs.jl
+++ T/Torch/TorchCUDA/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "Torch"
+name = "TorchCUDA"
 version = v"1.4.0"
 
 # Collection of sources required to complete build
