50041b1161fd2fac2dffe1f3d3a44cfcdb964082
Mosè Giordano <giordano@users.noreply.github.com>
2022-02-21 11:40:13 +0000

    Update M/MSTM/build_tarballs.jl

--- M/MSTM/build_tarballs.jl
+++ M/MSTM/build_tarballs.jl
@@ -17,7 +17,7 @@ install_license ${WORKSPACE}/srcdir/MSTM/LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_gfortran_versions(filter!(!Sys.iswindows, supported_platforms(; experimental=true)))
+platforms = expand_gfortran_versions(filter!(!Sys.iswindows, supported_platforms()))
 
 # The products that we will ensure are always built
 products = [
