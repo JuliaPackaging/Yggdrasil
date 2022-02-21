7c31a18db1b0c7b320955161988fb665a6794a67
Zihua Wu <qqbbnease1004@126.com>
2022-02-22 02:03:09 +0800

    Update build_tarballs.jl

--- M/MSTM/build_tarballs.jl
+++ M/MSTM/build_tarballs.jl
@@ -23,7 +23,7 @@ elif [[ "$target" == *-mingw* ]]; then
     cp $WORKSPACE/destdir/src/mpi.f90 .
     gfortran -DWIN32 -DINT_PTR_KIND=8 -fno-range-check mpi.f90 || true
     cd $WORKSPACE/srcdir/MSTM/code
-    echo "void __guard_check_icall_fptr(unsigned long ptr) { }\nvoid __security_check_cookie(void) { }" > cfg_stub.c
+    echo "void __guard_check_icall_fptr(unsigned long ptr) { } void __security_check_cookie(void) { }" > cfg_stub.c
     gcc -c cfg_stub.c
     gfortran -O2 -fno-range-check mpidefs-parallel.f90 mstm-intrinsics.f90 mstm-v4.0.f90 cfg_stub.o -L${WORKSPACE}/destdir/lib -I${WORKSPACE}/destdir/include -lmsmpifec -lmsmpi -o "${bindir}/mstm${exeext}"
 else
