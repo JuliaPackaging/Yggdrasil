0f7623b06ddeb5a32d4ec7fada453dcb884f7006
Tim Besard <tim.besard@gmail.com>
2022-02-23 16:25:28 +0100

    Bump version.

--- C/CMake/build_tarballs.jl
+++ C/CMake/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder
 
 name = "CMake"
-version = v"3.19.2"
+version = v"3.22.2"
 
 # Collection of sources required to build CMake
 sources = [
     ArchiveSource("https://github.com/Kitware/CMake/releases/download/v$(version)/cmake-$(version).tar.gz",
-                  "e3e0fd3b23b7fb13e1a856581078e0776ffa2df4e9d3164039c36d3315e0c7f0"),
+                  "3c1c478b9650b107d452c5bd545c72e2fad4e37c09b89a1984b9a2f46df6aced"),
 ]
 
 # Bash recipe for building across all platforms
