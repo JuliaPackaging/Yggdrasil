4d5740276d8a07da71cf96ce6f0b91c94feaed2a
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-02-25 20:35:33 +0100

    Update build_tarballs.jl

--- V/vegafusion/build_tarballs.jl
+++ V/vegafusion/build_tarballs.jl
@@ -13,7 +13,7 @@ script = raw"""
 export TARGET_CC=$CC
 if [[ "$bb_full_target" == armv7l-linux-* ]]; then
     export TARGET_CFLAGS="-O3 -ffunction-sections -fdata-sections -fPIC -mfpu=vfpv3-d16"
-    export CFLAGS_armv7-unknown-linux-gnueabihf="-O3 -ffunction-sections -fdata-sections -fPIC -mfpu=vfpv3-d16"
+    export CFLAGS="-O3 -ffunction-sections -fdata-sections -fPIC -mfpu=vfpv3-d16"
 
 fi
 
