2d0a5e25471b9d52f92a6c056b4471f94630ec52
Simeon David Schaub <schaub@mit.edu>
2022-02-26 13:17:36 -0500

    wip

--- Q/Qemu/build_tarballs.jl
+++ Q/Qemu/build_tarballs.jl
@@ -1,27 +1,30 @@
 using BinaryBuilder
 
 name = "Qemu"
-version = v"4.1.0"
+version = v"6.2.0"
 
 # Collection of sources required to build libffi
 sources = [
-    GitSource("https://gitlab.com/virtio-fs/qemu.git",
-              "bf5775237ee563b4baa1c7f3c1a65b7c93b93fca"),
+    ArchiveSource("https://download.qemu.org/qemu-6.2.0.tar.xz",
+                  "68e15d8e45ac56326e0b9a4afa8b49a3dfe8aba3488221d098c84698bca65b45"),
     DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/qemu
+cd $WORKSPACE/srcdir/qemu-*
 
 # Patch out usage of MADV_NOHUGEPAGE which does not exist in glibc 2.12.X
 atomic_patch -p1 "${WORKSPACE}/srcdir/patches/qemu_madv_nohugepage.patch"
 
-# Patch in adapter for `clock_gettime()` on macOS 10.12-
-atomic_patch -p1 "${WORKSPACE}/srcdir/patches/qemu_clock_gettime.patch"
+# Patch to include `falloc` header in `strace.c`
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/qemu_falloc.patch"
 
-# Patch to fix pointer mismatch between `size_t` and `uint64_t`
-atomic_patch -p1 "${WORKSPACE}/srcdir/patches/qemu_size_uint64.patch"
+## Patch in adapter for `clock_gettime()` on macOS 10.12-
+#atomic_patch -p1 "${WORKSPACE}/srcdir/patches/qemu_clock_gettime.patch"
+#
+## Patch to fix pointer mismatch between `size_t` and `uint64_t`
+#atomic_patch -p1 "${WORKSPACE}/srcdir/patches/qemu_size_uint64.patch"
 
 # Configure, ignoring some warnings that we don't need, etc...
 ./configure --host-cc="${HOSTCC}" --extra-cflags="-I${prefix}/include -Wno-unused-result" --disable-cocoa --prefix=$prefix
@@ -40,10 +43,12 @@ platforms = [
     Platform("x86_64", "linux"; libc="glibc"),
     Platform("x86_64", "macos"),
 ]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
     ExecutableProduct("qemu-system-x86_64", :qemu_system_x86_64),
+    ExecutableProduct("qemu-system-aarch64", :qemu_system_aarch64),
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -52,9 +57,9 @@ dependencies = [
     Dependency("Glib_jll", v"2.59.0"; compat="2.59.0"),
     Dependency("PCRE_jll"),
     # TOOD: verify Gettext is actually needed at runtime
-    Dependency("Gettext_jll", v"0.20.1"; compat="=0.20.1"),
+    Dependency("Gettext_jll"),
     Dependency("libcap_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8")
