5032ba888e686e9230db26f48835cfeed1c14973
Valentin Churavy <v.churavy@gmail.com>
2022-03-03 13:30:33 -0500

    Add MPI platform tag

---
+++ platforms/mpi.jl
@@ -0,0 +1,59 @@
+module MPI
+
+const tag_name = "mpi"
+
+const augment = """
+    # Can't use Preferences since we might be running this very early with a non-existing Manifest
+    MPIPreferences_UUID = Base.UUID("3da0fdf6-3ccc-4f1b-acd9-58baa6c99267")
+    const preferences = Base.get_preferences(MPIPreferences_UUID)
+
+    # Keep logic in sync with MPIPreferences.jl
+    const binary = get(preferences, "binary", Sys.iswindows() ? "MicrosoftMPI_jll" : "MPICH_jll")
+
+    const abi = if binary == "system"
+        get(preferences, "abi")
+    elseif binary == "MicrosoftMPI_jll"
+        "MicrosoftMPI"
+    elseif binary == "MPICH_jll"
+        "MPICH"
+    elseif binary == "OpenMPI_jll"
+        "OpenMPI"
+    elseif binary == "MPItrampoline_jll"
+        "MPIwrapper"
+    else
+        error("Unknown binary: $binary")
+    end
+
+    function augment_mpi!(platform)
+        if !haskey(platform, "mpi")
+            platform["mpi"] = abi
+        end
+        return platform
+    end
+"""
+
+using BinaryBuilder, Pkg
+using Base.BinaryPlatforms
+
+mpi_abis = (
+    ("MPICH", PackageSpec(name="MPICH_jll"), "", !Sys.iswindows) ,
+    ("OpenMPI", PackageSpec(name="OpenMPI"), "", !Sys.iswindows),
+    ("MicrosoftMPI", PackageSpec(name="MicrosoftMPI_jll"), "", Sys.iswindows),
+    ("MPIwrapper", PackageSpec(name="MPItrampoline_jll"), "2", !Sys.iswindows)
+)
+
+function augment_platforms(platforms)
+    all_platforms = AbstractPlatform[]
+    dependencies = []
+    for (abi, pkg, compat, f) in mpi_abis
+        pkg_platforms = deepcopy(filter(f, platforms))
+        foreach(pkg_platforms) do p
+            p[tag_name] = abi
+        end
+        append!(all_platforms, pkg_platforms)
+        push!(dependencies, Dependency(pkg; compat, platforms=pkg_platforms))
+    end
+    return all_platforms, dependencies
+end
+
+end
