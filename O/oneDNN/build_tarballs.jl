79bed7c2231befa7de5b7751790280232a008ab6
Jesper Stemann Andersen <jsa@ihp.dk>
2022-03-05 15:13:15 +0100

    Random CMAKE_SYSTEM_PROCESSOR to all-caps AARCH64

--- O/oneDNN/build_tarballs.jl
+++ O/oneDNN/build_tarballs.jl
@@ -16,6 +16,9 @@ script = raw"""
 cd $WORKSPACE/srcdir/oneDNN
 
 mkdir build && cd build/
+if [[ $target == aarch64* ]]; then
+    cmake_extra_args="-DCMAKE_SYSTEM_PROCESSOR=AARCH64"
+fi
 cmake \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
