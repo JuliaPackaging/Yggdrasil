a528909e68f9069cb2a48df1ea12d5f81b9abedf
Jesper Stemann Andersen <jsa@ihp.dk>
2022-03-06 23:59:54 +0100

    Updated PThreadPool to v0.0.20200302 - fixing build on mingw64

--- P/PThreadPool/build_tarballs.jl
+++ P/PThreadPool/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "PThreadPool"
-version = v"0.0.20191029"
+version = v"0.0.20200302"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/Maratyszcza/pthreadpool.git", "d465747660ecf9ebbaddf8c3db37e4a13d0c9103"),
+    GitSource("https://github.com/Maratyszcza/pthreadpool.git", "ebd50d0cfa3664d454ffdf246fcd228c3b370a11"),
     GitSource("https://github.com/Maratyszcza/FXdiv.git", "b742d1143724d646cd0f914646f1240eacf5bd73"),
 ]
 
@@ -27,13 +27,15 @@ cmake \
     ..
 cmake --build . -- -j $nproc
 make install
+if [[ $target == *-w64-mingw32 ]]; then
+    install -Dvm 755 libpthreadpool.dll "${libdir}/libpthreadpool.dll"
+fi
 install_license ../LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
-filter!(p -> !Sys.iswindows(p), platforms) # Windows fails to build
 
 # The products that we will ensure are always built
 products = [
