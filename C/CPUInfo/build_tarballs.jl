0eac1b9fb33029f6de4acc6abde1a8814a7bf4d7
Jesper Stemann Andersen <jesper@sait.dk>
2022-03-06 19:32:29 +0100

    Simplified filtering of platforms
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- C/CPUInfo/build_tarballs.jl
+++ C/CPUInfo/build_tarballs.jl
@@ -1,7 +1,6 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
-using BinaryBuilderBase: os
 
 name = "CPUInfo"
 version = v"0.0.20200122"
@@ -38,8 +37,8 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
-filter!(p -> arch(p) != "aarch64" || os(p) != "macos", platforms) # aarch64-macos unsupported
-filter!(p -> os(p) != "freebsd", platforms) # FreeBSD unsupported
+filter!(p -> !(Sys.isapple(p) && arch(p) == "aarch64"), platforms) # aarch64-macos unsupported
+filter!(p -> !Sys.isfreebsd(p), platforms) # FreeBSD unsupported
 
 # The products that we will ensure are always built
 products = [
