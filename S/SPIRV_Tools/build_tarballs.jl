d5fc376c33d974940cc943763cd694b0cdc704ca
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-03-07 12:10:01 +0100

    [SPIRV_Tools] [skip ci] Drop experimental flag (#4564)

--- S/SPIRV_Tools/build_tarballs.jl
+++ S/SPIRV_Tools/build_tarballs.jl
@@ -47,7 +47,7 @@ ninja -C build -j ${nproc} install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
