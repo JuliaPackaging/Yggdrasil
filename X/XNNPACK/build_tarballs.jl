e1b48ad3a1a966b8c7734f13728cc1b60132b290
Jesper Stemann Andersen <jsa@ihp.dk>
2022-03-07 01:27:17 +0100

    Set preferred GCC version to 9

--- X/XNNPACK/build_tarballs.jl
+++ X/XNNPACK/build_tarballs.jl
@@ -44,5 +44,5 @@ dependencies = Dependency[
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-    preferred_gcc_version=v"5",
+    preferred_gcc_version=v"9",
     julia_compat="1.6")
