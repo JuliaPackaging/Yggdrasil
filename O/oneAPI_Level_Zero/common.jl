59b91eb7767f2dcb17a0cad316371f78ccb916b6
Julian Samaroo <jpsamaroo@jpsamaroo.me>
2022-03-07 16:43:40 -0600

    [oneAPI] Add musl support (#4573)

--- O/oneAPI_Level_Zero/common.jl
+++ O/oneAPI_Level_Zero/common.jl
@@ -6,7 +6,7 @@ api_version = v"1.3.7"
 #
 # The level zero repository contains both the API headers and a loader,
 # which are versioned independently.
-sources = [
+sources = BinaryBuilder.AbstractSource[
     GitSource("https://github.com/oneapi-src/level-zero.git",
               "78f08cc338d70ac60a8f61084ad194fa0dbc90b0"),
 ]
