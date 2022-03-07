b3359886fc83c95ba212a4386b70fad69099843c
Jesper Stemann Andersen <jsa@ihp.dk>
2022-03-07 15:40:00 +0100

    Building shared lib

--- G/Gloo/build_tarballs.jl
+++ G/Gloo/build_tarballs.jl
@@ -19,6 +19,7 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
     ..
 cmake --build . -- -j $nproc
 make install
