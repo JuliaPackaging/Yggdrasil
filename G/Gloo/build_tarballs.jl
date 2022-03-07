ea90708376b1156e5de9b3544dd65bff4c6c2166
Jesper Stemann Andersen <jsa@ihp.dk>
2022-03-07 15:44:54 +0100

    Gloo can only be built on 64-bit systems

--- G/Gloo/build_tarballs.jl
+++ G/Gloo/build_tarballs.jl
@@ -28,6 +28,7 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+filter!(p -> nbits(p) == 64, platforms) # Gloo can only be built on 64-bit systems
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
