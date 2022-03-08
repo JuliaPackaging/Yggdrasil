a51d9988f5f450c918dff59e4cac500f0f0d6772
ederc <ederc@mathematik.uni-kl.de>
2022-03-08 09:44:47 +0100

    bump msolve to v0.2.2

--- M/msolve/build_tarballs.jl
+++ M/msolve/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "msolve"
-version = v"0.2.1"
+version = v"0.2.2"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://gitlab.lip6.fr/safey/msolve.git", "ca3d395b2fa9c5fda4499ae055601a96dacd1d92")
+    GitSource("https://gitlab.lip6.fr/safey/msolve.git", "469a4e74a172a109e4cdc8ff2057943b915866b5")
 ]
 
 # Bash recipe for building across all platforms
