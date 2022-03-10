9f5b0e793ae9b210354f57ef6401012a9fff9ef9
Mosè Giordano <mose@gnu.org>
2022-03-10 00:33:12 +0000

    [Emoslib] Rename from libemos and add some fixes

--- E/Emoslib/build_tarballs.jl
+++ E/Emoslib/build_tarballs.jl
@@ -1,6 +1,6 @@
 using BinaryBuilder
 
-name = "libemos"
+name = "Emoslib"
 version = v"4.5.9"
 sources = [
     ArchiveSource("https://confluence.ecmwf.int/download/attachments/3473472/libemos-$version-Source.tar.gz", "e57e02c636dc8f5ccb862a103789b1e927bc985b1e0f2b05abf4f64e86d2f67f"),
@@ -8,14 +8,18 @@ sources = [
 
 script = raw"""
 cd libemos-*-Source
+
+# This is a cross build, we can't do native compilation.
+sed -i 's/-mtune=native//' CMakeLists.txt
+
 mkdir build ; cd build
 cmake \
-    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
     -DENABLE_ECCODES=ON \
-    -DGRIB_API_LIBRARIES=${libdir}/libeccodes.so \
-    -DGRIB_API_INCLUDE_DIRS=${prefix}/include \
+    -DGRIB_API_LIBRARIES=${libdir}/libeccodes.${dlext} \
+    -DGRIB_API_INCLUDE_DIRS=${includedir} \
     ..
 make -j${nproc}
 make install
@@ -25,8 +29,8 @@ install_license ../LICENSE
 platforms = supported_platforms()
 
 products = [
-    # LibraryProduct("libemos", :emos),
-    FileProduct("lib/libemos.a", :emos), # libemos is not designed to be build as a shared library (see libemos-$version-Source/CMakeLists.txt:39)
+    # libemos is not designed to be build as a shared library (see libemos-$version-Source/CMakeLists.txt:39)
+    FileProduct("lib/libemos.a", :emos),
 ]
 
 dependencies = [
