526db9f653d3ccf07f9a5b15362c22f0a0785dd1
James Hester <jamesrhester@gmail.com>
2022-03-11 11:59:01 +1100

    Update C/CBFlib_small/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- C/CBFlib_small/build_tarballs.jl
+++ C/CBFlib_small/build_tarballs.jl
@@ -21,21 +21,7 @@ install_license ${WORKSPACE}/srcdir/cbflib/LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("x86_64", "freebsd"; ),
-    Platform("x86_64", "windows"; ),
-    Platform("i686", "windows"; )
-]
-
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
