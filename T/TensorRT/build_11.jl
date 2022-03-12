30c75a2bdba7d264b7e408d41d73ef98a7439d26
Jesper Stemann Andersen <jsa@ihp.dk>
2022-03-12 13:37:34 +0100

    Updated TensorRT to v8.0.3

--- T/TensorRT/build_11.jl
+++ T/TensorRT/build_11.jl
@@ -1,14 +1,8 @@
 platforms_and_sources = Dict(
     Platform("x86_64", "linux") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.0.1/tars/TensorRT-8.0.1.6.Linux.x86_64-gnu.cuda-11.3.cudnn8.2.tar.gz",
-                      "def6a5ee50bed25a68a9c9e22ec671a8f29ee5414bde47c5767bd279e5596f88")],
-    Platform("ppc64le", "linux") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.0.1/tars/TensorRT-8.0.1.6.CentOS-8.3.ppc64le-gnu.cuda-11.3.cudnn8.2.tar.gz",
-                      "fd33a32085c468f638505e2603936fa4e3f2a3fa46989570fa0b9e31a9e6914a")],
-    Platform("aarch64", "linux") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.0.1/tars/TensorRT-8.0.1.6.Ubuntu-20.04.aarch64-gnu.cuda-11.3.cudnn8.2.tar.gz",
-                      "ea322da72b1b1ca6b8d0715ab14668c54f7d00ad22695d41a85a7055df9f63e1")],
+        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.0.3/tars/TensorRT-8.0.3.4.Linux.x86_64-gnu.cuda-11.3.cudnn8.2.tar.gz",
+                      "3177435024ff4aa5a6dba8c1ed06ab11cc0e1bf3bb712dfa63a43422f41313f3")],
     Platform("x86_64", "windows") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.0.1/zip/TensorRT-8.0.1.6.Windows10.x86_64.cuda-11.3.cudnn8.2.zip",
-                      "e51b382e931ae9032e431fff218cd2cf2d2b7a7c66c7a6bdf453557612466ae1")],
+        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.0.3/zip/TensorRT-8.0.3.4.Windows10.x86_64.cuda-11.3.cudnn8.2.zip",
+                      "a347d6e7981d0497ba60c5de78716101d73105946e1ff745f0f426f51ea691b0")],
 )
