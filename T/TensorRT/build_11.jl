eff17a40035a32d250cb2048d64d5481108944e1
Jesper Stemann Andersen <jsa@ihp.dk>
2022-03-12 13:37:34 +0100

    Updated TensorRT to v8.2.2

--- T/TensorRT/build_11.jl
+++ T/TensorRT/build_11.jl
@@ -1,11 +1,11 @@
 platforms_and_sources = Dict(
     Platform("x86_64", "linux") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.2.1/tars/TensorRT-8.2.1.8.Linux.x86_64-gnu.cuda-11.4.cudnn8.2.tar.gz",
-                      "3e9a9cc4ad0e5ae637317d924dcddf66381f4db04e2571f0f2e6ed5a2a51f247")],
+        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.2.2/tars/TensorRT-8.2.2.1.Linux.x86_64-gnu.cuda-11.4.cudnn8.2.tar.gz",
+                      "da130296ac6636437ff8465812eb55dbab0621747d82dc4fe9b9376f00d214af")],
     Platform("aarch64", "linux") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.2.1/tars/TensorRT-8.2.1.8.Ubuntu-20.04.aarch64-gnu.cuda-11.4.cudnn8.2.tar.gz",
-                      "7c21312bf552904339d5f9270dc40c39321558e5993d93e4f94a0ed47d9a8a79")],
+        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.2.2/tars/TensorRT-8.2.2.1.Ubuntu-20.04.aarch64-gnu.cuda-11.4.cudnn8.2.tar.gz",
+                      "ed3bea21f44da7b43e93803a1e0ce0f4d68678afe5c7c0393b3e41d5c099555c")],
     Platform("x86_64", "windows") => [
-        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.2.1/zip/TensorRT-8.2.1.8.Windows10.x86_64.cuda-11.4.cudnn8.2.zip",
-                      "a900840f3839ae14fbd9dc837eb6335d3cb4f217f1f29604ef72fa88e8994bcd")],
+        ArchiveSource("https://developer.nvidia.com/compute/machine-learning/tensorrt/secure/8.2.2/zip/TensorRT-8.2.2.1.Windows10.x86_64.cuda-11.4.cudnn8.2.zip",
+                      "9efd246b1f518314f8912c6997fe8064ee9d557ae01665c2c4cb4f1a11ed8865")],
 )
--- T/TensorRT/build_tarballs.jl
+++ T/TensorRT/build_tarballs.jl
@@ -4,32 +4,21 @@ using Base.BinaryPlatforms: arch, os
 include("../../fancy_toys.jl")
 
 name = "TensorRT"
-version = v"8.2.1"
+version = v"8.2.2"
 
 script = raw"""
 cd ${WORKSPACE}/srcdir
 
-if [[ ${bb_full_target} == aarch64-linux-gnu*cuda+10.2 ]]; then
-    apk add dpkg
-    ls *.deb | xargs -Ideb_file dpkg-deb -x deb_file tmp
-    mkdir -p $bindir $includedir $libdir
-    install -Dv --mode=755 tmp/usr/src/tensorrt/bin/* $bindir
-    install -Dv --mode=644 tmp/usr/include/$target/* $includedir
-    install -Dv --mode=755 tmp/usr/lib/$target/*.so* $libdir
-    install_license tmp/usr/share/doc/libnvinfer8/copyright
+mkdir -p ${bindir} ${libdir} ${includedir}
+cd TensorRT*
+mv bin/* ${bindir}
+mv include/* ${includedir}
+mv lib/*.${dlext}* ${libdir}
+if [[ ${target} == x86_64-w64-mingw32 ]]; then
+    chmod +x ${bindir}/*.{dll,exe}
+    install_license doc/TensorRT-SLA.pdf 
 else
-    mkdir -p ${bindir} ${libdir} ${includedir}
-    cd TensorRT*
-    mv bin/* ${bindir}
-    mv include/* ${includedir}
-    mv lib/*.${dlext}* ${libdir}
-
-    if [[ ${target} == x86_64-w64-mingw32 ]]; then
-        chmod +x ${bindir}/*.{dll,exe}
-        install_license doc/TensorRT-SLA.pdf 
-    else
-        install_license doc/pdf/TensorRT-SLA.pdf 
-    fi
+    install_license doc/pdf/TensorRT-SLA.pdf 
 fi
 """
 
@@ -55,7 +44,6 @@ for cuda_version in cuda_versions
     for (platform, sources) in platforms_and_sources
         augmented_platform = Platform(arch(platform), os(platform); cuda=cuda_tag)
         should_build_platform(triplet(augmented_platform)) || continue
-        arch(platform) != "aarch64" || cuda_version == v"10.2" || cuda_version == v"11.4" || continue # AArch64 only support CUDA v10.2 and v11.4
         build_tarballs(ARGS, name, version, sources, script, [augmented_platform],
                        products, dependencies; lazy_artifacts=true)
     end
