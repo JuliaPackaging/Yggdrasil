83c5e099710f39fda3c9b43fdf3e3dd325a306aa
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2022-03-13 18:59:12 +0100

    Update build_tarballs.jl
    using OpenBLAS32 instead of LAPACKE & use a new version of MAGEMin that has a fix for windows mkdir issue

--- M/MAGEMin/build_tarballs.jl
+++ M/MAGEMin/build_tarballs.jl
@@ -7,27 +7,29 @@ version = v"1.0.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/ComputationalThermodynamics/MAGEMin.git", "032116744f3df05b5a2999c2eb39689c831cf4c7"),
-    ArchiveSource("https://github.com/Reference-LAPACK/lapack/archive/refs/tags/v3.10.0.tar.gz", "328c1bea493a32cac5257d84157dc686cc3ab0b004e2bea22044e0a59f6f8a19"),
-    DirectorySource("./bundled")
+    GitSource("https://github.com/ComputationalThermodynamics/MAGEMin.git", "76c550c5dab9c9ca76440b4600a6d7eb4cc71344"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/lapack-*/LAPACKE/
-cmake ../ -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DLAPACKE=ON -DBUILD_SHARED_LIBS=ON
-make lapacke -j${nproc}
-make install
 
-cd ../../MAGEMin/
+cd MAGEMin
 if [[ "${target}" == *-mingw* ]]; then
     MPI_LIBS="-lmsmpi"
 else
     MPI_LIBS="-lmpi"
 fi
-make CC=$CC CCFLAGS="-O3 -g -fPIC -std=c99" LIBS="-L${libdir} -lm -llapacke -lnlopt ${MPI_LIBS}" INC="-I${includedir}" lib
+
+# compile binary
+make CC=$CC CCFLAGS="-O3 -g -fPIC -std=c99" LIBS="-L${libdir} -lm -lopenblas -lnlopt ${MPI_LIBS}" INC="-I${includedir}" all
+
+# Compile library:
+make CC=$CC CCFLAGS="-O3 -g -fPIC -std=c99" LIBS="-L${libdir} -lm -lopenblas -lnlopt ${MPI_LIBS}" INC="-I${includedir}" lib
+
 install -Dvm 755 libMAGEMin.dylib "${libdir}/libMAGEMin.${dlext}"
 install -Dvm 644 src/*.h "${includedir}"
+install MAGEMin "${bindir}"
+
 install_license LICENSE
 """
 
@@ -38,15 +40,17 @@ platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libMAGEMin", :MAGEMin_library)
+    LibraryProduct("libMAGEMin", :libMAGEMin)
+    ExecutableProduct("MAGEMin", :MAGEMin)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="MPICH_jll", uuid="7cb0a576-ebde-5e09-9194-50597f1243b4"); platforms=filter(!Sys.iswindows, platforms))
-    Dependency(PackageSpec(name="NLopt_jll", uuid="079eb43e-fd8e-5478-9966-2cf3e3edb778"))
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
     Dependency(PackageSpec(name="MicrosoftMPI_jll", uuid="9237b28f-5490-5468-be7b-bb81f5f5e6cf"); platforms=filter(Sys.iswindows, platforms))
+    Dependency(PackageSpec(name="NLopt_jll", uuid="079eb43e-fd8e-5478-9966-2cf3e3edb778"))
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
