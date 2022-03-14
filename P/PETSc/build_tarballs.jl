7cb359c5c1a8ed8bbd580e77ce6e02c8730e4ac7
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2022-03-14 10:44:31 +0100

    Update build_tarballs.jl
    update

--- P/PETSc/build_tarballs.jl
+++ P/PETSc/build_tarballs.jl
@@ -1,7 +1,9 @@
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "PETSc"
 version = v"3.16.5"
+
+# SuiteSparse does not support 64bit indices in 32bit (pointer) mode.
 
 # Collection of sources required to build PETSc. Avoid using the git repository, it will
 # require building SOWING which fails in all non-linux platforms.
@@ -25,7 +27,7 @@ if [[ "${target}" == *-mingw* ]]; then
 else
     MPI_LIBS="[${libdir}/libmpifort.${dlext},${libdir}/libmpi.${dlext}]"
 fi
-mkdir $libdir/petsc
+mkdir ${libdir}/petsc
 build_petsc()
 {
 
@@ -34,8 +36,12 @@ build_petsc()
     else
         USE_INT64=0
     fi
+
     USE_MUMPS=1
+    
     if [[ "${1}" == "single" ]]; then
+        USE_SUITESPARSE=0
+    elif [[ "${1}" == "single" ]] && [[ "${3}" == "Int64" ]]; then
         USE_SUITESPARSE=0
     else
         USE_SUITESPARSE=1
@@ -70,7 +76,6 @@ build_petsc()
         --PETSC_ARCH=${target}_${1}_${2}_${3}
 
     if [[ "${target}" == *-mingw* ]]; then
-
         export CPPFLAGS="-Dpetsc_EXPORTS"
     elif [[ "${target}" == powerpc64le-* ]]; then
         export CFLAGS="-fPIC"
@@ -115,22 +120,22 @@ products = [
     # Current default build, equivalent to Float64_Real_Int32
     LibraryProduct("libpetsc_double_real_Int32", :libpetsc, "\$libdir/petsc/double_real_Int32/lib")
     LibraryProduct("libpetsc_double_real_Int32", :libpetsc_Float64_Real_Int32, "\$libdir/petsc/double_real_Int32/lib")
-    LibraryProduct("libpetsc_double_real_Int64", :libpetsc_Float64_Real_Int64, "\$libdir/petsc/double_real_Int64/lib")
-    LibraryProduct("libpetsc_single_real_Int64", :libpetsc_Float32_Real_Int64, "\$libdir/petsc/single_real_Int64/lib")
-    LibraryProduct("libpetsc_double_complex_Int64", :libpetsc_Float64_Complex_Int64, "\$libdir/petsc/double_complex_Int64/lib")
-    LibraryProduct("libpetsc_single_complex_Int64", :libpetsc_Float32_Complex_Int64, "\$libdir/petsc/single_complex_Int64/lib")
+    
     LibraryProduct("libpetsc_single_real_Int32", :libpetsc_Float32_Real_Int32, "\$libdir/petsc/single_real_Int32/lib")
     LibraryProduct("libpetsc_double_complex_Int32", :libpetsc_Float64_Complex_Int32, "\$libdir/petsc/double_complex_Int32/lib")
     LibraryProduct("libpetsc_single_complex_Int32", :libpetsc_Float32_Complex_Int32, "\$libdir/petsc/single_complex_Int32/lib")
+    LibraryProduct("libpetsc_double_real_Int64", :libpetsc_Float64_Real_Int64, "\$libdir/petsc/double_real_Int64/lib")
+    LibraryProduct("libpetsc_single_real_Int64", :libpetsc_Float32_Real_Int64, "\$libdir/petsc/single_real_Int64/lib")
+    
+    LibraryProduct("libpetsc_double_complex_Int64", :libpetsc_Float64_Complex_Int64, "\$libdir/petsc/double_complex_Int64/lib")
+    LibraryProduct("libpetsc_single_complex_Int64", :libpetsc_Float32_Complex_Int64, "\$libdir/petsc/single_complex_Int64/lib")
 ]
 
 dependencies = [
-    Dependency("OpenBLAS32_jll"),
-    Dependency("MPICH_jll"; platforms=filter(!Sys.iswindows, platforms)),
-    Dependency("MicrosoftMPI_jll"; platforms=filter(Sys.iswindows, platforms)),
-    Dependency("CompilerSupportLibraries_jll"),
-    Dependency("SCALAPACK32_jll"),
-    Dependency("CMake_jll"),
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2")),
+    Dependency(PackageSpec(name="MPICH_jll", uuid="7cb0a576-ebde-5e09-9194-50597f1243b4"); platforms=filter(!Sys.iswindows, platforms)),
+    Dependency(PackageSpec(name="MicrosoftMPI_jll", uuid="9237b28f-5490-5468-be7b-bb81f5f5e6cf"); platforms=filter(Sys.iswindows, platforms)),
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
 
 # Build the tarballs.
