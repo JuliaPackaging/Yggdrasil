d97e671dbb441160f406db65888a36ba6623b777
Joshua Higginbotham <jhigginbotham64@gmail.com>
2022-03-14 17:21:11 -0500

    Update G/glm/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- G/glm/build_tarballs.jl
+++ G/glm/build_tarballs.jl
@@ -31,5 +31,4 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"11.1.0")
-
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
