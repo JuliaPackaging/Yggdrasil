c76faf6dfdc3f4a9faa7dac4ab0b527f45d93a11
BenjaminDoran <20494558+BenjaminDoran@users.noreply.github.com>
2022-03-15 21:58:26 -0500

    add recipe for seq-gen

---
+++ S/seqgen/build_tarballs.jl
@@ -0,0 +1,48 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "SeqGen"
+version = v"1.3.4"
+
+# Collection of sources required to build SFML
+sources = [
+    GitSource("https://github.com/rambaut/Seq-Gen.git", "bc9d8070b2cd1f1352f74282e5b209302eae38a1")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ${WORKSPACE}/srcdir/Seq-Gen/source
+head -39 seq-gen.c > license
+install_license ${WORKSPACE}/srcdir/Seq-Gen/source/license
+
+# compile seq-gen
+make -j${nproc}
+
+# move to bindir
+mkdir ${bindir}
+
+if [[ "${target}" == *-mingw32* ]]; then
+    # not entirely sure this will work on windows
+    # but it is wourth a shot
+    cp seq-gen ${bindir}/seq-gen.exe
+else
+    cp seq-gen ${bindir}/seq-gen
+fi
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("seq-gen", :seqgen),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
