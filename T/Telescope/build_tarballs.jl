18ccc755ea43a6624c0f00b93b697d951e2142b1
Joshua Higginbotham <jhigginbotham64@gmail.com>
2022-03-15 22:29:33 -0500

    Update build_tarballs.jl

--- T/Telescope/build_tarballs.jl
+++ T/Telescope/build_tarballs.jl
@@ -28,7 +28,13 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; exclude=p->!(Sys.islinux(p) && nbits(p) == "64"))
+platforms = [
+    Platform("x86_64", "linux"; libc = "glibc"),
+    Platform("aarch64", "linux"; libc = "glibc"),
+    Platform("powerpc64le", "linux"; libc = "glibc"),
+    Platform("x86_64", "linux"; libc = "musl"),
+    Platform("aarch64", "linux"; libc = "musl")
+]
 
 platforms = expand_cxxstring_abis(platforms)
 
