9ec3778bc311879a9f67ad817941c32944fe1489
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2022-03-17 17:53:00 +0100

    [MAGEMin]: update version to 1.0.2 (#4633)
    updated version

--- M/MAGEMin/build_tarballs.jl
+++ M/MAGEMin/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "MAGEMin"
-version = v"1.0.1"
+version = v"1.0.2"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/ComputationalThermodynamics/MAGEMin.git", "431fe8972a29cc70af002da9fd72000da7052c8c"),
+    GitSource("https://github.com/ComputationalThermodynamics/MAGEMin.git", "6d9ebb1575ea48477380f1cf7b97e41df8b76f3b"),
 ]
 
 # Bash recipe for building across all platforms
