66ff7e6e6a325bc3b560805c7b8ba700928a75c9
Mosè Giordano <mose@gnu.org>
2022-03-22 22:10:37 +0000

    [finufft] Augment platforms by setting the microarchitecture

--- F/finufft/build_tarballs.jl
+++ F/finufft/build_tarballs.jl
@@ -3,6 +3,8 @@
 using BinaryBuilder, Pkg
 using BinaryBuilderBase
 
+include(joinpath(@__DIR__, "..", "..", "platforms", "microarchitectures.jl"))
+
 name = "finufft"
 version = v"2.0.4"
 julia_compat = "1.6"
@@ -17,20 +19,30 @@ script = raw"""
 cd $WORKSPACE/srcdir/finufft*/
 # Overwrite LIBSFFT such that we do not require fftw3_threads or fftw3_omp for OMP support. Since the libraries in FFTW_jll already provide for threading, we do not loose anything.
 # Make use of the -DFFTW_PLAN_SAFE flag to allow for multiple threads using finufft at the same time.
-make lib CFLAGS="-fopenmp -fPIC -O3 -funroll-loops -fcx-limited-range -Iinclude" CXXFLAGS="-fopenmp -fPIC -O3 -funroll-loops -fcx-limited-range -Iinclude -std=c++14 -DFFTW_PLAN_SAFE" LIBSFFT="-lfftw3 -lfftw3f -lm"
-mv lib/libfinufft.so "${libdir}/libfinufft.${dlext}"
+make lib \
+    CFLAGS="-fopenmp -fPIC -O3 -funroll-loops -fcx-limited-range -Iinclude" \
+    CXXFLAGS="-fopenmp -fPIC -O3 -funroll-loops -fcx-limited-range -Iinclude -std=c++14 -DFFTW_PLAN_SAFE" \
+    LIBSFFT="-lfftw3 -lfftw3f -lm"
+install -Dvm 0755 lib/libfinufft.so "${libdir}/libfinufft.${dlext}"
 """
-
-# Build for all supported platforms
-platforms_x86_64 = filter(p -> p.tags["arch"]=="x86_64", supported_platforms())
-platforms_other  = filter(p -> p.tags["arch"]!="x86_64", supported_platforms())
 
 # Expand for microarchitectures on x86_64 (library doesn't have CPU dispatching)
 # Tests on Linux/x86_64 yielded a slow binary with avx512 for some reason, so disable that
-platforms_x86_64 = expand_microarchitectures(platforms_x86_64)
-platforms_x86_64 = filter(p -> p.tags["march"] != "avx512", platforms_x86_64)
+platforms = expand_cxxstring_abis(expand_microarchitectures(supported_platforms(), ["x86_64", "avx", "avx2"]); skip=!Sys.iswindows)
 
-platforms = vcat(platforms_x86_64, platforms_other)
+augment_platform_block = """
+    $(MicroArchitectures.augment)
+
+    function augment_platform!(platform::Platform)
+        # We augment only x86_64
+        @static if Sys.ARCH === :x86_64
+            augment_microarchitecture!(platform)
+        else
+            platform
+        end
+    end
+    """
+
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libfinufft", :libfinufft)
@@ -38,9 +50,12 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+    Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a")),
+    # TODO: we should use clang as compiler on BSD systems and use
+    # `LLVMOpenMP_jll` to provide the OpenMP implementation.
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8", julia_compat=julia_compat)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               preferred_gcc_version = v"8", julia_compat, augment_platform_block)
