92a060b62ecd565bac4728611535917a8873f665
Mosè Giordano <giordano@users.noreply.github.com>
2022-03-22 00:04:19 +0000

    [z3] Do not build for non-libcxxwrap platforms (#4664)
    It's a useless complication

--- Z/z3/build_tarballs.jl
+++ Z/z3/build_tarballs.jl
@@ -15,7 +15,7 @@ julia_versions = [v"1.6.3", v"1.7.0", v"1.8.0", v"1.9.0"]
 sources = [
     GitSource("https://github.com/Z3Prover/z3.git", "df8f9d7dcb8b9f9b3de1072017b7c2b7f63f0af8"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
-        "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
+                  "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
 ]
 
 macfix = raw"""
@@ -35,7 +35,7 @@ fi
 """
 
 # Bash recipe for building across all platforms
-script_libcxxwrap = macfix * raw"""
+script = macfix * raw"""
 cd $WORKSPACE/srcdir/z3/
 
 mkdir z3-build && cd z3-build
@@ -51,39 +51,16 @@ make install
 install_license ${WORKSPACE}/srcdir/z3/LICENSE.txt
 """
 
-script = macfix * raw"""
-cd $WORKSPACE/srcdir/z3/
-
-mkdir z3-build && cd z3-build
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
-    -DCMAKE_FIND_ROOT_PATH="${prefix}" \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    ..
-make -j${nproc}
-make install
-install_license ${WORKSPACE}/srcdir/z3/LICENSE.txt
-"""
-
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 include("../../L/libjulia/common.jl")
-platforms_libcxxwrap = vcat(libjulia_platforms.(julia_versions)...)
-
-platforms = filter(x->!(x in platforms_libcxxwrap), supported_platforms())
-
-platforms_libcxxwrap = expand_cxxstring_abis(platforms_libcxxwrap)
+platforms = vcat(libjulia_platforms.(julia_versions)...)
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
-products_libcxxwrap = [
-    LibraryProduct("libz3", :libz3),
-    LibraryProduct("libz3jl", :libz3jl),
-    ExecutableProduct("z3", :z3)
-]
-
 products = [
     LibraryProduct("libz3", :libz3),
+    LibraryProduct("libz3jl", :libz3jl),
     ExecutableProduct("z3", :z3)
 ]
 
@@ -93,17 +70,6 @@ dependencies = [
     Dependency("libcxxwrap_julia_jll")
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
-non_reg_ARGS = filter(arg -> arg != "--register", ARGS)
-
-include("../../fancy_toys.jl")
-
-if any(should_build_platform.(triplet.(platforms_libcxxwrap)))
-    build_tarballs(non_reg_ARGS, name, version, sources, script_libcxxwrap, platforms_libcxxwrap, 
-        products_libcxxwrap, dependencies; preferred_gcc_version=v"9", 
-        julia_compat="1.6")
-end
-if any(should_build_platform.(triplet.(platforms)))
-    build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-        preferred_gcc_version=v"9", julia_compat="1.6")
-end
+build_tarballs(ARGS, name, version, sources, script, platforms,
+               products, dependencies; preferred_gcc_version=v"9",
+               julia_compat="1.6")
