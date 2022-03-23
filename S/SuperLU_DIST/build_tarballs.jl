34356f13e99ace6ff1990d20f2db27373006c648
Wimmerer <kimmerer@mit.edu>
2022-03-23 15:32:10 -0400

    fix name

--- S/SuperLU_DIST/build_tarballs.jl
+++ S/SuperLU_DIST/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "SuperLU"
+name = "SuperLU_DIST"
 version = v"7.2.0"
 
 # Collection of sources required to complete build
