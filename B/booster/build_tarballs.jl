fd7b0caee622cd796a804f0cfda751b80abe1eb8
BenjaminDoran <20494558+BenjaminDoran@users.noreply.github.com>
2022-03-28 15:10:40 -0500

    update /B/booster/build_tarballs.jl

--- B/booster/build_tarballs.jl
+++ B/booster/build_tarballs.jl
@@ -33,7 +33,6 @@ products = [
 dependencies = [
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
-    Dependency("OpenMPI_jll", v"4.1.2"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
