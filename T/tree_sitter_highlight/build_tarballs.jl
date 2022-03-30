ac920b28fedc16a73fc7666b502d82cd1aea7697
Mosè Giordano <giordano@users.noreply.github.com>
2022-03-31 00:19:34 +0100

    Update build_tarballs.jl

--- T/tree_sitter_highlight/build_tarballs.jl
+++ T/tree_sitter_highlight/build_tarballs.jl
@@ -13,13 +13,12 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-ls
 cd $WORKSPACE/srcdir/
 
 # Use cargo-rustc to build tree-sitter-highlight as a dynamic library here
 cargo rustc --release --manifest-path highlight/Cargo.toml -- --crate-type=cdylib
 
-install -Dm 755 target/${rust_target}/release/deps/*tree_sitter_highlight-*.${dlext} "${libdir}/libtree_sitter_highlight.${dlext}"
+install -Dvm 755 target/${rust_target}/release/deps/*tree_sitter_highlight-*.${dlext} "${libdir}/libtree_sitter_highlight.${dlext}"
 """
 
 # These are the platforms we will build for by default, unless further
