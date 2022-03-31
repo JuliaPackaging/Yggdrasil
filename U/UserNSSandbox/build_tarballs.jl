520f51a1737f426611c5a7ad52fac8c41a0d1198
Elliot Saba <staticfloat@gmail.com>
2022-03-30 22:36:29 -0700

    [UserNSSandbox] Bump to include signal handling

--- U/UserNSSandbox/build_tarballs.jl
+++ U/UserNSSandbox/build_tarballs.jl
@@ -1,12 +1,12 @@
 using BinaryBuilder
 
 name = "UserNSSandbox"
-version = v"2022.03.10"
+version = v"2022.03.30"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/staticfloat/Sandbox.jl.git",
-              "9023307c1eed13058a517afe66197cf559cf454d"),
+              "4a82de7b33326acffc75336364c71f06ea84ec8c"),
 ]
 
 # Bash recipe for building across all platforms
