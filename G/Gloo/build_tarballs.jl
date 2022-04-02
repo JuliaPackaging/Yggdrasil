023d2f229ddcf025ae1e7be9e3700c675f2dc5b1
Jesper Stemann Andersen <jsa@ihp.dk>
2022-04-02 15:45:22 +0200

    Unsuccesful hacking to configure for CUDA (10.2)

--- G/Gloo/build_tarballs.jl
+++ G/Gloo/build_tarballs.jl
@@ -21,10 +21,15 @@ atomic_patch -p1 ../patches/musl-caddr.patch
 mkdir build
 cd build
 if [[ $target != *w64-mingw32* ]]; then
-    cmake_extra_args+="-DUSE_LIBUV=ON"
+    cmake_extra_args+="-DUSE_LIBUV=ON "
 fi
 if [[ $bb_full_target == *cuda* ]]; then
-    cmake_extra_args+="-DUSE_CUDA=ON -DCUDA_TOOLKIT_ROOT_DIR=$prefix/cuda -DCUDA_INCLUDE_DIRS=$prefix/cuda/include -DCUDA_CUDART_LIBRARY=$prefix/lib64/libcudart.so"
+    cmake_extra_args+="-DUSE_CUDA=ON -DCUDA_TOOLKIT_ROOT_DIR=$prefix/cuda -DCMAKE_CUDA_FLAGS='-cudart shared' -DCUDA_INCLUDE_DIRS=$prefix/cuda/include -DCUDA_CUDART_LIBRARY=$prefix/lib64/libcudart.so "
+    export PATH=$PATH:$prefix/cuda/bin
+    export CUDACXX=nvcc
+    export CUDAHOSTCXX=$CXX
+    apk del cmake
+    apk add 'cmake<3.17' --repository=http://dl-cdn.alpinelinux.org/alpine/v3.11/main
 fi
 cmake \
     -DCMAKE_INSTALL_PREFIX=$prefix \
@@ -43,7 +48,7 @@ platforms = supported_platforms()
 filter!(p -> nbits(p) == 64, platforms) # Gloo can only be built on 64-bit systems
 
 cuda_platforms = Platform[]
-for cuda_version in [v"10.2", v"11.0", v"11.1", v"11.2", v"11.3", v"11.4", v"11.5", v"11.6"]
+for cuda_version in [v"10.2"]
     cuda_platform = Platform("x86_64", "linux"; cuda = "$(cuda_version.major).$(cuda_version.minor)")
     push!(platforms, cuda_platform)
     push!(cuda_platforms, cuda_platform)
@@ -60,7 +65,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     BuildDependency("LibUV_jll"),
-    BuildDependency("CUDA_full_jll"; platforms = cuda_platforms),
+    BuildDependency(PackageSpec("CUDA_full_jll", Base.UUID("4f82f1eb-248c-5f56-a42e-99106d144614"), v"10.2.89"); platforms = cuda_platforms),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
