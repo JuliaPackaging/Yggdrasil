7ab46304e0f57dc5a5487979c63286064aad8f15
Mosè Giordano <mose@gnu.org>
2022-04-05 23:30:01 +0200

    [RDKit] Update to v2022.03.1 and build for Windows

--- R/RDKit/build_tarballs.jl
+++ R/RDKit/build_tarballs.jl
@@ -1,41 +1,53 @@
 using BinaryBuilder, Pkg
 
-julia_version = v"1.6.0"
-
 name = "RDKit"
-version = v"2021.09.2"
+version = v"2022.03.1"
 
 sources = [
-    GitSource("https://github.com/rdkit/rdkit.git", "333fa5ce222627583052ddffb2c27265994a3950"),
+    GitSource("https://github.com/rdkit/rdkit.git", "7e205e0d93a3046c1eaab37120c9f6971194ddf2"),
 ]
 
 script = raw"""
 cd ${WORKSPACE}/srcdir/rdkit
+FLAGS=()
+if [[ "${target}" == *-mingw* ]]; then
+    FLAGS+=(-DRDK_BUILD_THREADSAFE_SSS=OFF -DRDK_USE_URF=OFF)
+fi
+if [[ "${proc_family}" == intel ]]; then
+    FLAGS+=(-DRDK_OPTIMIZE_POPCNT=ON)
+else
+    # For some reasons they think unconditionally enabling this option by
+    # default on all platforms is a good idea, so we have to manually disable it
+    # on non-Intel platforms
+    FLAGS+=(-DRDK_OPTIMIZE_POPCNT=OFF)
+fi
 mkdir build
 cd build
 cmake \
--DCMAKE_BUILD_TYPE=Release \
--DRDK_INSTALL_INTREE=OFF \
--DRDK_BUILD_INCHI_SUPPORT=ON \
--DRDK_BUILD_PYTHON_WRAPPERS=OFF \
--DRDK_BUILD_CFFI_LIB=ON \
--DRDK_BUILD_FREETYPE_SUPPORT=ON \
--DRDK_BUILD_CPP_TESTS=OFF \
--RDK_BUILD_SLN_SUPPORT=OFF \
--DCMAKE_INSTALL_PREFIX=${prefix} \
--DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
--DCMAKE_PREFIX_PATH=${prefix} \
-..
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DRDK_INSTALL_INTREE=OFF \
+    -DRDK_BUILD_INCHI_SUPPORT=ON \
+    -DRDK_BUILD_PYTHON_WRAPPERS=OFF \
+    -DRDK_BUILD_CFFI_LIB=ON \
+    -DRDK_BUILD_FREETYPE_SUPPORT=ON \
+    -DRDK_BUILD_CPP_TESTS=OFF \
+    -DRDK_BUILD_SLN_SUPPORT=OFF \
+    -DRDK_TEST_MULTITHREADED=OFF \
+    "${FLAGS[@]}" \
+    ..
 make -j${nproc}
 make install
 """
 
 platforms = [
-    Platform("x86_64", "linux"),
-    Platform("i686", "linux"),
-    Platform("aarch64", "linux"),
+    Platform("x86_64", "linux"; libc="glibc"),
+    Platform("i686", "linux"; libc="glibc"),
+    Platform("aarch64", "linux"; libc="glibc"),
     Platform("x86_64", "macos"),
     Platform("aarch64", "macos"),
+    Platform("x86_64", "windows"),
 ]
 
 platforms = expand_cxxstring_abis(platforms)
@@ -52,4 +64,4 @@ dependencies = [
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
-    preferred_gcc_version=v"6", julia_compat="^$(julia_version.major).$(julia_version.minor)")
+               preferred_gcc_version=v"7", julia_compat="1.6")
