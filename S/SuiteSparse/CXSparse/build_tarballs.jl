c7512250056ba3281280d9ec8d1c913e3a6e2c22
Valentin Churavy <vchuravy@users.noreply.github.com>
2022-04-05 10:27:42 -0400

    Update S/SuiteSparse/CXSparse/build_tarballs.jl

--- S/SuiteSparse/CXSparse/build_tarballs.jl
+++ S/SuiteSparse/CXSparse/build_tarballs.jl
@@ -35,9 +35,9 @@ function configure_extraction(ARGS, name, SuiteSparse_version=nothing; experimen
     ]
 
     ctx = Pkg.Types.Context()
-    name = "SuiteSparse_jll"
+    libname = "SuiteSparse_jll"
     uuid = Base.UUID("bea87d4a-7f5b-5778-9afe-8cc45184846c")
-    Pkg.Types.registry_resolve!(ctx.registries, Pkg.Types.PackageSpec(;name, uuid))
+    Pkg.Types.registry_resolve!(ctx.registries, Pkg.Types.PackageSpec(;name=libname, uuid))
     push!(dependencies, BuildDependency(get_addable_spec("SuiteSparse_jll", SuiteSparse_version ;ctx)))
 
     return name, version, [], script, platforms, products, dependencies
