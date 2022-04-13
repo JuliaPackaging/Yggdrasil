6635bf12727d85ee55ea369fa70b7804b0c45f93
doriantsolak <dorian.tsolak@uni-bielefeld.de>
2022-04-13 20:30:14 +0200

    Fix dirname

--- G/gflags/build_tarballs.jl
+++ G/gflags/build_tarballs.jl
@@ -13,7 +13,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd gflags.git/
+cd gflags/
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON ..
