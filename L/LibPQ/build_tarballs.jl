4e3633ab68f4cda08445e98633d74c824addf7c9
Thomas Poulsen <ta.poulsen@gmail.com>
2022-04-14 17:55:17 +0200

    revert adding krb5 directly

--- L/LibPQ/build_tarballs.jl
+++ L/LibPQ/build_tarballs.jl
@@ -17,38 +17,24 @@ sources = [
         "https://data.iana.org/time-zones/releases/tzcode$tzcode_version.tar.gz",
         "584666393a5424d13d27ec01183da17703273664742e049d4f62f62dab631775",
     ),
-    ArchiveSource(
-        "https://kerberos.org/dist/krb5/1.19/krb5-1.19.3.tar.gz",
-        "56d04863cfddc9d9eb7af17556e043e3537d41c6e545610778676cf551b9dcd0",
-    ),
 ]
 
 # Bash recipe for building across all platforms
 # NOTE: readline and zlib are not used by libpq
 script = raw"""
-cd $WORKSPACE/srcdir
-make CC=$BUILD_CC VERSION_DEPS= zic
-export ZIC=$WORKSPACE/srcdir/zic
-if [[ "${target}" == *"linux"* ]]; then
-    ## We can only compile gss-api for linux
-    cd krb5-1.19.3/src
-    krb5_cv_attr_constructor_destructor=yes,yes  ac_cv_printf_positional=yes ac_cv_func_regcomp=yes ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
-    make
-    make install
+    cd $WORKSPACE/srcdir
+    make CC=$BUILD_CC VERSION_DEPS= zic
+    export ZIC=$WORKSPACE/srcdir/zic
     cd $WORKSPACE/srcdir/postgresql-*/
     if [[ "${target}" == i686-linux-musl ]]; then
-        # Small hack: swear that we're cross-compiling.  Our `i686-linux-musl` is
-        # bugged and it can run only a few programs, with the result that the
-        # configure test to check whether we're cross-compiling returns that we're
-        # doing a native build, but then it fails to run a bunch of programs during
-        # other tests.
-        sed -i 's/cross_compiling=no/cross_compiling=yes/' configure
+            # Small hack: swear that we're cross-compiling.  Our `i686-linux-musl` is
+            # bugged and it can run only a few programs, with the result that the
+            # configure test to check whether we're cross-compiling returns that we're
+            # doing a native build, but then it fails to run a bunch of programs during
+            # other tests.
+            sed -i 's/cross_compiling=no/cross_compiling=yes/' configure
     fi
-        ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-includes=$prefix/include --with-libraries=$prefix/lib --without-readline --without-zlib --with-openssl --with-gssapi
-else
-    cd /postgresql-*/
     ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-includes=$prefix/include --with-libraries=$prefix/lib --without-readline --without-zlib --with-openssl
-fi
 make -C src/interfaces/libpq install
 
 install_license COPYRIGHT
