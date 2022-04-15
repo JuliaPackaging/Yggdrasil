99c8f854f629555879cd619b93bfabed3a4ee0ca
Dorian <dorian.tsolak@gmail.com>
2022-04-15 10:59:42 +0200

    Update G/gflags/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- G/gflags/build_tarballs.jl
+++ G/gflags/build_tarballs.jl
@@ -12,12 +12,15 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd gflags/
+cd $WORKSPACE/srcdir/gflags/
 mkdir build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON ..
-make
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    ..
+make -j${nproc}
 make install
 """
 
