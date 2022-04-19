c1b0b54677e75b80921a5c23fede4071b411cf3c
Jorge Brito <101952709+dylanxyz@users.noreply.github.com>
2022-04-19 14:44:41 -0300

    New recipe: Pathfinder-v0.5.0 (#4778)
    * New recipe: Pathfinder-v0.5.0
    
    * Update P/Pathfinder/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Update P/Pathfinder/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Update P/Pathfinder/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ P/Pathfinder/build_tarballs.jl
@@ -0,0 +1,70 @@
+using BinaryBuilder
+
+name = "Pathfinder"
+version = v"0.5.0"
+sources = [
+    GitSource("https://github.com/servo/pathfinder.git", "7281a607a80dacc5470d7d3ec208341b6f0b0727"),
+    DirectorySource("./bundled")
+]
+
+script = raw"""
+cd ${WORKSPACE}/srcdir/pathfinder*
+
+# by default, pathfinder will compile to a static library,
+# so we change "staticlib" to "cdylib" to compile to a dynamic library
+atomic_patch -p1 ../patches/Compile-to-dynamic-lib.patch
+
+# we need cbindgen to generate the header file
+cargo install cbindgen --force --target "${rust_host}"
+
+cd c
+
+# generate the header file
+cbindgen \
+    --lang c \
+    --crate pathfinder_c \
+    --config cbindgen.toml \
+    --output "${includedir}/pathfinder.h"
+
+# build pathfinder
+cargo build --release --lib --target=${rust_target}
+
+# On windows the generated .dll doesn't start with lib
+if [[ "${target}" == *-mingw* ]]; then
+    cp \
+        ../target/${rust_target}/release/pathfinder.dll \
+        ../target/${rust_target}/release/libpathfinder.dll
+fi
+
+# install the library
+install -D -m 755 "../target/${rust_target}/release/libpathfinder.${dlext}" "${libdir}/libpathfinder.${dlext}"
+
+# install the licenses
+install_license ../LICENSE-APACHE ../LICENSE-MIT
+"""
+
+platforms = supported_platforms()
+
+# font-kit fails to compile on arm and i686 linux
+filter!(p -> arch(p) ∉ ("armv6l", "armv7l"), platforms)
+filter!(p -> !Sys.islinux(p) || arch(p) != "i686", platforms)
+# cdylib apparently doesn't support musl
+filter!(p -> libc(p) != "musl", platforms)
+# Rust toolchain for i686 Windows is unusable
+filter!(p -> !Sys.iswindows(p) || arch(p) != "i686", platforms)
+
+products = [
+    LibraryProduct("libpathfinder", :libpathfinder),
+]
+
+not_windows = filter(!Sys.iswindows, platforms)
+
+dependencies = [
+    BuildDependency("Fontconfig_jll", platforms=not_windows),
+    BuildDependency("FreeType2_jll",  platforms=not_windows),
+    BuildDependency("HarfBuzz_jll",   platforms=not_windows),
+]
+
+compilers = [:rust, :c]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers, julia_compat="1.6")
