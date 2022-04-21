3f27e730ef9d55c4d5c8056b1e8bb8745402851b
Benoît Legat <benoit.legat@gmail.com>
2022-04-21 10:09:00 -0400

    Add Julia v1.6 requirement for MUMPS_seq@4 (#4794)
    * Add Julia v1.6 requirement for MUMPS_seq@4
    
    * [MUMPS_seq@4] Fix issue with GCC 10+
    
    Co-authored-by: Mosè Giordano <mose@gnu.org>

--- M/MUMPS/MUMPS_seq@4/build_tarballs.jl
+++ M/MUMPS/MUMPS_seq@4/build_tarballs.jl
@@ -1,10 +1,27 @@
 using BinaryBuilder, Pkg
 
+# The version of this JLL is decoupled from the upstream version.
+# Whenever we package a new upstream release, we initially map its
+# version X.Y.Z to X00.Y00.Z00 (i.e., multiply each component by 100).
+# So for example version 2.6.3 would become 200.600.300.
+#
+# Together, this allows to increment the patch level of the JLL for minor tweaks.
+# If a rebuild of the JLL is needed which keeps the upstream version identical
+# but breaks ABI compatibility for any reason, one can increment the minor or major
+# version (depending on whether package using this JLL use `~` or `^` compat entries)
+# e.g. go from 200.600.300 to 200.601.300 or 201.600.300
+# Similar tricks can also be used to package prerelease versions; e.g. one might
+# map a prerelease of 2.7.0 to 200.690.000.
+
 name = "MUMPS_seq"
-version = v"4.10.0"
+upstream_version = v"4.10.0"
+version_offset = v"0.0.0" # reset to 0.0.0 once the upstream version changes
+version = VersionNumber(upstream_version.major * 100 + version_offset.major,
+                        upstream_version.minor * 100 + version_offset.minor,
+                        upstream_version.patch * 100 + version_offset.patch)
 
 sources = [
-  ArchiveSource("http://mumps.enseeiht.fr/MUMPS_4.10.0.tar.gz",
+  ArchiveSource("http://mumps.enseeiht.fr/MUMPS_$(upstream_version).tar.gz",
                 "d0f86f91a74c51a17a2ff1be9c9cee2338976f13a6d00896ba5b43a5ca05d933"),
   DirectorySource("bundled")
 ]
@@ -20,7 +37,12 @@ cd $WORKSPACE/srcdir/MUMPS_4.10.0
 
 cp Make.inc/Makefile.gfortran.SEQ Makefile.inc
 
-make_args+=(OPTF=-O3
+FFLAGS=()
+if [[ "${target}" == aarch64-apple-* ]]; then
+    FFLAGS+=(-fallow-argument-mismatch)
+fi
+
+make_args=(OPTF=-O3
             CDEFS=-DAdd_
             LMETISDIR=${prefix}/lib
             IMETIS=-I${prefix}/include
@@ -73,4 +95,4 @@ dependencies = [
 
 # Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               preferred_gcc_version=v"6")
+               julia_compat="1.6", preferred_gcc_version=v"6")
