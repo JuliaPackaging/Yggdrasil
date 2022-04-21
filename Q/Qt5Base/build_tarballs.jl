3a1ee8e4a4b5b0b53195895608c4a3c768ba1692
Mosè Giordano <mose@gnu.org>
2022-04-21 18:46:43 +0200

    [Qt5Base] Upgrade to v5.15.3 and fix some stuff for FreeBSD

--- Q/Qt5Base/build_tarballs.jl
+++ Q/Qt5Base/build_tarballs.jl
@@ -3,17 +3,15 @@
 using BinaryBuilder
 
 name = "Qt5Base"
-version = v"5.15.2"
+version = v"5.15.3"
 
 # Collection of sources required to build qt5
 sources = [
-    ArchiveSource("https://download.qt.io/official_releases/qt/$(version.major).$(version.minor)/$version/submodules/qtbase-everywhere-src-$version.tar.xz",
-                  "909fad2591ee367993a75d7e2ea50ad4db332f05e1c38dd7a5a274e156a4e0f8"),
+    ArchiveSource("https://download.qt.io/official_releases/qt/$(version.major).$(version.minor)/$(version)/submodules/qtbase-everywhere-opensource-src-$(version).tar.xz",
+                  "26394ec9375d52c1592bd7b689b1619c6b8dbe9b6f91fdd5c355589787f3a0b6"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
                   "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f")
 ]
-
-version = v"5.15.3" # This version number is a lie to build for experimental platforms
 
 script = raw"""
 cd $WORKSPACE/srcdir
@@ -111,7 +109,7 @@ EOT
 
     *x86_64-unknown-freebsd*)
         sed -i 's/load(qt_config)//' ../qtbase-everywhere-src-*/mkspecs/freebsd-g++/qmake.conf
-        grep -A11 QMAKE_CC ../qtbase-everywhere-src-*/mkspecs/linux-aarch64-gnu-g++/qmake.conf | sed -e 's/aarch64-linux-gnu/x86_64-unknown-freebsd11.1/' >> ../qtbase-everywhere-src-*/mkspecs/freebsd-g++/qmake.conf
+        grep -A11 QMAKE_CC ../qtbase-everywhere-src-*/mkspecs/linux-aarch64-gnu-g++/qmake.conf | sed -e "s/aarch64-linux-gnu/${target}/" >> ../qtbase-everywhere-src-*/mkspecs/freebsd-g++/qmake.conf
         echo "QMAKE_CFLAGS_ARCH_HASWELL =" >> ../qtbase-everywhere-src-*/mkspecs/freebsd-g++/qmake.conf
 
         ../qtbase-everywhere-src-*/configure -platform musl -xplatform freebsd-g++ -device-option CROSS_COMPILE=${BIN_DIR}/$target- \
