4e911d4f6919eed45c85e1de4efd41131af02027
Mosè Giordano <giordano@users.noreply.github.com>
2022-04-22 09:09:19 +0100

    Update build_tarballs.jl

--- L/librdkafka/build_tarballs.jl
+++ L/librdkafka/build_tarballs.jl
@@ -7,28 +7,33 @@ version = v"1.8.2"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/edenhill/librdkafka/archive/v1.8.2.tar.gz", "6a747d293a7a4613bd2897e28e8791476fbe1ae7361f2530a876e0fd483482a6")
+    ArchiveSource("https://github.com/edenhill/librdkafka/archive/v$(version).tar.gz",
+                  "6a747d293a7a4613bd2897e28e8791476fbe1ae7361f2530a876e0fd483482a6"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd librdkafka*
+cd $WORKSPACE/srcdir/librdkafka*
 if [[ "${target}" != *-freebsd* ]]; then
-rm -f /opt/${target}/${target}/sys-root/usr/lib/libcrypto.*
-rm -f /opt/${target}/${target}/sys-root/usr/lib/libssl.*
-rm -f /opt/${target}/${target}/sys-root/usr/lib/libsasl2.*
+    rm -f /opt/${target}/${target}/sys-root/usr/lib/libcrypto.*
+    rm -f /opt/${target}/${target}/sys-root/usr/lib/libssl.*
+    rm -f /opt/${target}/${target}/sys-root/usr/lib/libsasl2.*
 fi
 mkdir build
 cd build/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DRDKAFKA_BUILD_EXAMPLES=OFF -DRDKAFKA_BUILD_TESTS=OFF ..
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DRDKAFKA_BUILD_EXAMPLES=OFF \
+    -DRDKAFKA_BUILD_TESTS=OFF \
+    ..
 make -j${nproc}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = expand_cxxstring_abis(supported_platforms())
 
 
 # The products that we will ensure are always built
@@ -46,4 +51,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-    julia_compat="1.6")
+               julia_compat="1.6")
