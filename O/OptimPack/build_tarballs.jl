33e6832fa5c735488f121a665b29dd40edfd3a0c
Mosè Giordano <giordano@users.noreply.github.com>
2022-04-22 22:17:58 +0100

    Update build_tarballs.jl

--- O/OptimPack/build_tarballs.jl
+++ O/OptimPack/build_tarballs.jl
@@ -7,8 +7,8 @@ version = v"3.2.0"
 
 # Collection of sources required to build OptimPAck
 sources = [
-    "https://github.com/emmt/OptimPack/releases/download/v$(version)/optimpack-$(version).tar.gz" =>
-    "3df032cde5ba6a3ffa04f0b0fd95d0694b75af82c8d2ddff0666c61e86c3ef5a",
+    ArchiveSource("https://github.com/emmt/OptimPack/releases/download/v$(version)/optimpack-$(version).tar.gz",
+                  "3df032cde5ba6a3ffa04f0b0fd95d0694b75af82c8d2ddff0666c61e86c3ef5a"),
 ]
 
 # Bash recipe for building across all platforms
@@ -37,8 +37,8 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
+dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
