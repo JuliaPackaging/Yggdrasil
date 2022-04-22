f7362ba475e0d3334980800fb784df03bbfbbc96
tcarion <tristan.carion@gmail.com>
2022-04-22 12:11:19 +0200

    remove flex_extract

--- F/FLEXPART/build_tarballs.jl
+++ F/FLEXPART/build_tarballs.jl
@@ -9,7 +9,6 @@ version = v"10.4"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://www.flexpart.eu/gitmob/flexpart", "3d7eebf7c4909f59db5ec32c524f88fb846e9fe5"),
-    GitSource("https://www.flexpart.eu/gitmob/flex_extract", "e0005c99ac81d12faa45a8ff799debbd592b0dc0"),
     DirectorySource("./bundled"),
 ]
 
@@ -24,13 +23,6 @@ cd src
 make ncf=yes
 cp FLEXPART $bindir
 install_license ../LICENSE
-
-cd ../../flex_extract
-atomic_patch -p1 /workspace/srcdir/patches/flexextract-makefile.patch
-cd Source/Fortran/
-make -f makefile_local_gfortran
-cp calc_etadot_fast.out $bindir/calc_etadot
-install_license ../../LICENSE.md
 """
 
 platforms = [
@@ -41,14 +33,12 @@ platforms = expand_gfortran_versions(platforms)
 # The products that we will ensure are always built
 products = [
     ExecutableProduct("FLEXPART", :FLEXPART),
-    ExecutableProduct("calc_etadot", :calc_etadot),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("CompilerSupportLibraries_jll"),
     Dependency("eccodes_jll"),
-    Dependency("Emoslib_jll"),
     Dependency("NetCDFF_jll"),
 ]
 
