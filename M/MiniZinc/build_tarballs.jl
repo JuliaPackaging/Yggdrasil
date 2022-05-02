7b1a9ed2728bf8c37ffcddeaf9c71b965d8a3ec2
odow <o.dowson@gmail.com>
2022-05-03 11:52:11 +1200

    [MiniZinc] WIP: begin a builder for MiniZinc

---
+++ M/MiniZinc/build_tarballs.jl
@@ -0,0 +1,63 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "MiniZinc"
+
+version = v"2.6.2"
+
+sources = [
+    GitSource(
+        "https://github.com/MiniZinc/libminizinc.git",
+        "a56602765b4294b796c063664733b28f5a663af7",
+    ),
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir
+
+mkdir -p libminizinc/build
+cd libminizinc/build
+
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=${BUILD_SHARED} \
+    ..
+
+if [[ "${target}" == *-linux-* ]]; then
+        make -j ${nproc}
+else
+    if [[ "${target}" == *-mingw* ]]; then
+        cmake --build . --config Release
+    else
+        cmake --build . --config Release --parallel
+    fi
+fi
+make install
+"""
+
+products = [
+    ExecutableProduct("minizinc", :minizinc),
+]
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+dependencies = [
+    Dependency("CompilerSupportLibraries_jll"),
+]
+
+build_tarballs(
+    ARGS,
+    name,
+    version,
+    sources,
+    script,
+    platforms,
+    products,
+    dependencies;
+    preferred_gcc_version = v"6",
+    julia_compat = "1.6",
+)
