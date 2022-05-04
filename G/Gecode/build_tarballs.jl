153c92b13240febc8bbc2f7fd896d9d6f625c1ae
odow <o.dowson@gmail.com>
2022-05-04 12:31:48 +1200

    [Gecode] add builder

---
+++ G/Gecode/build_tarballs.jl
@@ -0,0 +1,63 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Gecode"
+
+version = v"6.2.0"
+
+sources = [
+    GitSource(
+        "https://github.com/Gecode/gecode.git",
+        "2925392ac7a0c8a7b33aaee51721d84725605919",
+    ),
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir/gecode
+
+mkdir -p build
+cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
+
+if [[ "${target}" == *-linux-* ]]; then
+        make -j ${nproc}
+else
+    if [[ "${target}" == *-mingw* ]]; then
+        cmake --build . --config Release
+    else
+        cmake --build . --config Release --parallel
+    fi
+fi
+make install
+"""
+
+products = [
+    ExecutableProduct("fzn-gecode", :fzngecode),
+    ExecutableProduct("mzn-gecode", :mzngecode),
+]
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_cxxstring_abis(supported_platforms())
+
+dependencies = [
+    Dependency("CompilerSupportLibraries_jll"),
+]
+
+build_tarballs(
+    ARGS,
+    name,
+    version,
+    sources,
+    script,
+    platforms,
+    products,
+    dependencies;
+    preferred_gcc_version = v"6",
+    julia_compat = "1.6",
+)
