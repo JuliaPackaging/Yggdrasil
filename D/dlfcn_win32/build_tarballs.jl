810f9a8b138ee772c39b6ed250546f4e625122ba
Steve Kelly <kd2cca@gmail.com>
2022-05-06 20:10:25 +0000

    [dlfcn-win32] init

---
+++ D/dlfcn_win32/build_tarballs.jl
@@ -0,0 +1,38 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "dlfcn_win32"
+version = v"1.3.1"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/dlfcn-win32/dlfcn-win32.git", "9d0ef119d9fcb9139f831adc224857b791c81140")
+]
+
+dependencies = [
+
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd dlfcn-win32
+
+mkdir build
+cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = filter!(p -> Sys.iswindows(p), supported_platforms())
+
+# The products that we will ensure are always built
+products = Product[
+    LibraryProduct("libdl", :libdl)
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
