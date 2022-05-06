b90c76c9fcf41e0e04fcfc33983fb2d0afc1b1a7
Steve Kelly <kd2cca@gmail.com>
2022-05-06 22:20:58 +0000

    [yosys] bump to v0.16

--- Y/Yosys/build_tarballs.jl
+++ Y/Yosys/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "Yosys"
-version = v"0.11.0"
+version = v"0.16.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/YosysHQ/yosys.git", "360fed8e4d611fa725a4526cf960383b8e6c6e64")
+    GitSource("https://github.com/YosysHQ/yosys.git", "b63e0a0caeb74f7323130fd36ee77b738cf8461b")
 ]
 
 dependencies = [
@@ -15,29 +15,35 @@ dependencies = [
     Dependency("Readline_jll"; compat="8.1.1"),
     Dependency("Tcl_jll"; compat="8.6.11"),
     Dependency("Zlib_jll"; compat="1.2.11"),
-    Dependency("Libffi_jll"; compat="~3.2.2")
+    Dependency("Libffi_jll"; compat="~3.2.2"),
+    Dependency(PackageSpec(;name="dlfcn_win32_jll", uuid = "c4b69c83-5512-53e3-94e6-de98773c479f", path="/home/sjkelly/.julia/dev/dlfcn_win32_jll"); compat="1.3.1"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd yosys
+
 if [[ "${target}" == *-apple-* ]] || [[ "${target}" == *-freebsd* ]]; then
     CONFIG=clang
     OS=Darwin
-elif [[ "${target}" == *-x86_64-w64-mingw32* ]]; then
+elif [[ "${target}" == *x86_64-w64-mingw32* ]]; then
     CONFIG=msys2-64
     OS=Windows
-elif [[ "${target}" == *-i686-w64-mingw32* ]]; then
+elif [[ "${target}" == *i686-w64-mingw32* ]]; then
     CONFIG=msys2-32
     OS=Windows
 else
     CONFIG=gcc
     OS=Linux
 fi
-# TODO: we need mingw-dlfcn wrappers for windows plugin support
+
 # TODO: ABC does not build on windows
-make ENABLE_LIBYOSYS=1 OS=${OS} CONFIG=${CONFIG} PREFIX=${prefix} LIBDIR=${libdir} -j${nproc}
-make install ENABLE_LIBYOSYS=1 OS=${OS} PREFIX=${prefix} LIBDIR=${libdir}
+if [[ "${target}" == *x86_64-w64-mingw32* ]] || [[ "${target}" == *i686-w64-mingw32* ]]; then
+    make install ENABLE_LIBYOSYS=1 ENABLE_ABC=0 ENABLE_TCL=0 OS=${OS} CONFIG=${CONFIG} PREFIX=${prefix} LIBDIR=${libdir} -j${nproc}
+else
+    make install ENABLE_LIBYOSYS=1 OS=${OS} CONFIG=${CONFIG} PREFIX=${prefix} LIBDIR=${libdir} -j${nproc}
+fi
+
 if [[ "${target}" == *-apple-* ]]; then
     mv ${prefix}/lib/libyosys.so  ${prefix}/lib/libyosys.dylib
 fi
@@ -45,7 +51,7 @@ fi
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter!(p -> arch(p) != "armv6l" && (Sys.isapple(p) || Sys.islinux(p)), supported_platforms(;experimental=true))
+platforms = filter!(p -> arch(p) != "armv6l", supported_platforms())
 platforms = expand_cxxstring_abis(platforms)
 # For some reasons, building for CXX03 string ABI doesn't actually work, skip it
 filter!(x -> cxxstring_abi(x) != "cxx03", platforms)
