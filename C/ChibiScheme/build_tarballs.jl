8b02ddc2b0fec0fc55aed6f2062c96c72db90cbc
Steven G. Johnson <stevenj@mit.edu>
2022-05-11 12:51:55 -0400

    Update C/ChibiScheme/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- C/ChibiScheme/build_tarballs.jl
+++ C/ChibiScheme/build_tarballs.jl
@@ -12,6 +12,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/chibi-scheme
+apk add chibi-scheme
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
