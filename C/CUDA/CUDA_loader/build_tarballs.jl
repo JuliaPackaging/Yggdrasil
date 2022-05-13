76aa1e3fabc04efb1497db6e6cb1bd04dfc9a214
Tim Besard <tim.besard@gmail.com>
2022-05-13 22:43:09 +0200

    Update CUDA loader versions 11.4-11.7. (#4909)

--- C/CUDA/CUDA_loader/build_tarballs.jl
+++ C/CUDA/CUDA_loader/build_tarballs.jl
@@ -7,7 +7,7 @@ version = v"0.2.1"
 
 cuda_versions = [v"9.0", v"9.2",
                  v"10.0", v"10.1", v"10.2",
-                 v"11.0", v"11.1", v"11.2", v"11.3", v"11.4", v"11.5", v"11.6"]
+                 v"11.0", v"11.1", v"11.2", v"11.3", v"11.4", v"11.5", v"11.6", v"11.7"]
 for cuda_version in cuda_versions
     cuda_tag = "$(cuda_version.major).$(cuda_version.minor)"
     include("build_$(cuda_tag).jl")
