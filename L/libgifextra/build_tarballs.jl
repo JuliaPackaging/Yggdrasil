a4ef39518faf61dc5f3f900a2a4802d053f4f113
ash <ab669522@gmail.com>
2022-05-23 21:01:48 +0530

    added gitsource and supported platforms increase test, fix license

--- L/libgifextra/build_tarballs.jl
+++ L/libgifextra/build_tarballs.jl
@@ -2,39 +2,25 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "Giflibextra"
+name = "GifLibExtra"
 version = v"0.0.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/ashwani-rathee/io-project/raw/main/gifwrapper/gen/libgifextra.zip", "4e1d3e0d1e9df0f30e0dc6c907bb2d574d4c4efec79fb1558451d4c24fd3430b")
+    GitSource("https://github.com/ashwani-rathee/GifLibExtra.git", "a5c74d671dbede5f4be5448f80a1fd6950ae38b7")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd libgifextra/
-make
+cd $WORKSPACE/srcdir/GifLibExtra.git/lib
+make -j${nproc}
 make install
 exit
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl")
-]
-
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
