86961d7d7e20a4e143b473867dd8e3510150c051
Tyler Thomas <36181311+tylerjthomas9@users.noreply.github.com>
2022-05-23 09:24:03 -0700

    Update MacOSX SDK for x86_64-apple-darwin
    The default MacOSX SDK, 10.12, does not include `___cxa_deleted_virtual`

--- X/XGBoost/build_tarballs.jl
+++ X/XGBoost/build_tarballs.jl
@@ -6,6 +6,8 @@ version = v"1.6.1"
 sources = [
     GitSource("https://github.com/dmlc/xgboost.git","5d92a7d936fc3fad4c7ecb6031c3c1c7da882a14"), 
     DirectorySource("./bundled"),
+    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
+    "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62")
 ]
 
 # Bash recipe for building across all platforms
@@ -20,8 +22,19 @@ git submodule update --init
 if [[ ${target} == *linux* ]] ||  [[ ${target} == *mingw* ]]; then
     (mkdir build; cd build; cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}")
     make -C build -j ${nproc}
-# For Mac and FreeBSD, build without openmp
-elif [[ ${target} == *darwin* ]] ||  [[ ${target} == *freebsd* ]]; then
+# For ARM64 Mac and FreeBSD, build without openmp
+elif [[ ${target} == aarch64-apple-darwin* ]] ||  [[ ${target} == *freebsd* ]]; then
+    (mkdir build; cd build; cmake .. -DUSE_OPENMP=OFF -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}")
+    make -C build -j ${nproc}
+# For x86_64 Mac, build without openmp
+elif [[ "${target}" == x86_64-apple-darwin* ]]; then
+    export MACOSX_DEPLOYMENT_TARGET=10.15
+    #install a newer SDK which supports `___cxa_deleted_virtual`
+    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
+    rm -rf /opt/${target}/${target}/sys-root/System
+    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
+    cp -ra System "/opt/${target}/${target}/sys-root/."
+    popd
     (mkdir build; cd build; cmake .. -DUSE_OPENMP=OFF -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}")
     make -C build -j ${nproc}
 fi
