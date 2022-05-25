5072d40c733174c14005a569844604d7c7a889bc
anand jain <anandj@uchicago.edu>
2022-05-25 20:29:06 +0000

    [libcellml] add libcellml recipe

---
+++ L/libcellml/build_tarballs.jl
@@ -0,0 +1,36 @@
+#!/usr/bin/env julia
+using BinaryBuilder, Pkg
+
+name = "libcellml"
+version = v"0.2.0"
+sources = [
+    GitSource(
+        "https://github.com/cellml/libcellml",
+        "9948e5fb6159bbe50bbe0f4bec883ed7190a51f7"),
+]
+
+script = raw"""
+cd ${WORKSPACE}/srcdir/libcellml
+
+mv libcellml source
+mkdir build
+mkdir install
+cd build
+cmake -DINSTALL_PREFIX=../install -S "../source" -B=.
+
+make -j${nproc}
+make install
+
+"""
+
+platforms = expand_cxxstring_abis(supported_platforms())
+
+products = [
+    LibraryProduct("libcellml", :libcellml)
+]
+
+dependencies = [
+    Dependency("XML2_jll")
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
