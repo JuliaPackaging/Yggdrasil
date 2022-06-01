67caf9e50846a73ed8708ca3c381c0aca60b2769
Martijn Visser <mgvisser@gmail.com>
2022-06-01 20:42:37 +0000

    no need to increase compat, libtiff soversion is the same

--- L/libgeotiff/build_tarballs.jl
+++ L/libgeotiff/build_tarballs.jl
@@ -42,7 +42,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("PROJ_jll"; compat="~900.0"),
-    Dependency("Libtiff_jll"; compat="4.4"),
+    Dependency("Libtiff_jll"; compat="4.3"),
     Dependency("LibCURL_jll"),
 ]
 
