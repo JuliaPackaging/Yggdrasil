3216b2e03d2b0563a6943d197ef3ef4759693e8e
Martijn Visser <mgvisser@gmail.com>
2022-06-01 19:57:50 +0000

    [libgeotiff] build version 1.7.1
    The patch is no longer needed: https://github.com/OSGeo/libgeotiff/blob/1.7.1/libgeotiff/CMakeLists.txt#L242.
    
    And locally at least the build compiles fine without the libstdc++ and -lcurl modifications.

--- L/libgeotiff/build_tarballs.jl
+++ L/libgeotiff/build_tarballs.jl
@@ -1,13 +1,12 @@
 using BinaryBuilder, Pkg
 
 name = "libgeotiff"
-version = v"1.7.0"
+version = v"1.7.1"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://github.com/OSGeo/libgeotiff/releases/download/$version/libgeotiff-$version.tar.gz",
-                  "fc304d8839ca5947cfbeb63adb9d1aa47acef38fc6d6689e622926e672a99a7e"),
-    DirectorySource("./bundled"),
+                  "05ab1347aaa471fc97347d8d4269ff0c00f30fa666d956baba37948ec87e55d6"),
 ]
 
 # Bash recipe for building across all platforms
@@ -15,25 +14,7 @@ script = raw"""
 
 cd $WORKSPACE/srcdir/libgeotiff-*/
 
-# Who knows why they want to install Windows binaries not build with MSVC to
-# ${prefix} instead of ${prefix}/bin
-atomic_patch -p1 ../patches/more-reasonable-default-bin-subdir.patch
-
 mkdir build && cd build
-
-# Hint to find libstc++, required to link against C++ libs when using C compiler
-if [[ "${target}" == *-linux-* ]]; then
-    if [[ "${nbits}" == 32 ]]; then
-        export CFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib"
-    else
-        export CFLAGS="-Wl,-rpath-link,/opt/${target}/${target}/lib64"
-    fi
-fi
-
-# same fix as used for PROJ
-if [[ "${target}" == x86_64-linux-musl* ]]; then
-    export LDFLAGS="$LDFLAGS -lcurl"
-fi
 
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
       -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
@@ -60,8 +41,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("PROJ_jll"; compat="~800.200"),
-    Dependency("Libtiff_jll"; compat="4.3"),
+    Dependency("PROJ_jll"; compat="~900.0"),
+    Dependency("Libtiff_jll"; compat="4.4"),
     Dependency("LibCURL_jll"),
 ]
 
