9ed826643d467caa963a40f83e914faa33f8b0d5
Wimmerer <kimmerer@mit.edu>
2022-06-06 09:32:57 -0400

    Update to 2022 release

--- S/SLATE/build_tarballs.jl
+++ S/SLATE/build_tarballs.jl
@@ -1,12 +1,12 @@
 using BinaryBuilder
 
 name = "SLATE"
-version = v"2021.05.02"
+version = v"2022.05.00"
 
 # Collection of sources required to build PETSc. Avoid using the git repository, it will
 # require building SOWING which fails in all non-linux platforms.
 sources = [
-    GitSource("https://bitbucket.org/icl/slate.git", "859efbd6ad7dfc3efc190701676d2e0a0d8987fb")
+    GitSource("https://bitbucket.org/icl/slate.git", "5fb57877effa06d2ef090402a39341ebeb44f180")
 ]
 
 # Bash recipe for building across all platforms
@@ -26,17 +26,15 @@ cmake \
   -DMPI_RUN_RESULT_CXX_libver_mpi_normal__TRYRUN_OUTPUT="" \
   -Drun_result="0" \
   -Drun_result__TRYRUN_OUTPUT="ok" \
+  -Dc_api=yes \
   ..
 make -j${nproc}
 make install
 """
 
 # We attempt to build for all defined platforms
-platforms = expand_gfortran_versions(expand_cxxstring_abis(supported_platforms(;experimental=true, exclude=Sys.iswindows)))
-platforms = filter(p -> !(Sys.iswindows(p) ||Sys.isapple(p)), platforms)
-platforms = filter(!Sys.isfreebsd, platforms)
-platforms = expand_gfortran_versions(platforms)
-platforms = filter(p -> libgfortran_version(p) ≠ v"3", platforms)
+platforms = expand_cxxstring_abis(supported_platforms(;experimental=true, exclude=Sys.iswindows))
+
 products = [
     LibraryProduct("libslate", :libslate),
     LibraryProduct("libslate_lapack_api", :libslate_lapack_api)
@@ -46,8 +44,7 @@ products = [
 dependencies = [
     Dependency("CompilerSupportLibraries_jll"),
     Dependency("OpenBLAS32_jll"),
-    Dependency("MPICH_jll"),
-    Dependency("SCALAPACK_jll")
+    Dependency("MPICH_jll")
 ]
 
 # Build the tarballs.
