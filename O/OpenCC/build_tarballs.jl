6ee3ea804125904f51b2ac1681d092a32612ea02
Yuanqing Wu <69360763+yqwu905@users.noreply.github.com>
2022-06-08 01:45:38 +0800

    Fix reviewe problems.
    I fix other problems according to your instruction, except:
    * I set `cxxstring_abi="cxx11"` for `platforms`.
    
    Also, I remove some products and specification of gcc compiler in `build_tarballs`.

--- O/OpenCC/build_tarballs.jl
+++ O/OpenCC/build_tarballs.jl
@@ -1,28 +1,30 @@
 using BinaryBuilder
 
-name = "libopencc"
+name = "OpenCC"
 version = v"1.1.3"
 sources = [
-  ArchiveSource("https://github.com/BYVoid/OpenCC/archive/refs/tags/ver.1.1.3.tar.gz", "99a9af883b304f11f3b0f6df30d9fb4161f15b848803f9ff9c65a96d59ce877f")
+    ArchiveSource("https://github.com/BYVoid/OpenCC/archive/refs/tags/ver.$(version).tar.gz", "99a9af883b304f11f3b0f6df30d9fb4161f15b848803f9ff9c65a96d59ce877f")
 ]
 
 script = raw"""
-cd ${WORKSPACE}/srcdir/OpenCC-ver.1.1.3
-cmake -S. -Bbuild -DCMAKE_INSTALL_PREFIX:PATH=${prefix}
-cmake --build build --config Release --target install
+cd ${WORKSPACE}/srcdir/OpenCC*
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_DOCUMENTATION:BOOL=OFF \
+    ..
+make -j${nproc}
+make install
 """
 
 platforms = [
-  Platform("x86_64", "linux"),
+  Platform("x86_64", "linux", cxxstring_abi="cxx11"),
 ]
 
 products = [
   LibraryProduct("libopencc", :libopencc),
-  FileProduct("share/opencc/TSCharacters.ocd2", :opencc_char),
-  FileProduct("share/opencc/TSPhrases.ocd2", :opencc_phra),
-  FileProduct("share/opencc/t2s.json", :opencc_conf)
 ]
 
-dependcies = []
+dependcies = Dependency[]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependcies, preferred_gcc_version = v"11.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependcies)
