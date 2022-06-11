4b02518db5275cea9e9e869d18f135253410414b
Kiran Shila <me@kiranshila.com>
2022-06-11 12:52:10 -0700

    Apply upstream patch to fix a few builds
    https://github.com/casacore/casacore/commit/6f4d5783453e5e3516a134bda3b55e0a50a27aa0

--- C/casacore/build_tarballs.jl
+++ C/casacore/build_tarballs.jl
@@ -8,12 +8,16 @@ version = v"3.4.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/casacore/casacore.git", "a735bf5f31ea012ac4f7f7378a1f89c1fc136a06")
+    GitSource("https://github.com/casacore/casacore.git", "a735bf5f31ea012ac4f7f7378a1f89c1fc136a06"),
+    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/casacore
+
+# Apply upstream patch that fixes i386 builds
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/0001-Fix-compilation-of-RefTable-on-i386-platform-1129.patch
 
 mkdir build && cd build
 CMAKE_FLAGS=()
