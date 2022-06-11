9a2abf8d867ae28d611a31edfe17096ca3444495
Kiran Shila <me@kiranshila.com>
2022-06-11 12:23:44 -0700

    Conditionally link against librt

--- C/casacore/build_tarballs.jl
+++ C/casacore/build_tarballs.jl
@@ -15,31 +15,41 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/casacore
 
-if [[ "${nbits}" == 64 ]] && [[ "${target}" != aarch64* ]]; then
-    OPENBLAS="${libdir}/libopenblas64_.${dlext}"
-else
-    OPENBLAS="${libdir}/libopenblas.${dlext}"
+mkdir build && cd build
+CMAKE_FLAGS=()
+
+# Release build for best performance
+CMAKE_FLAGS+=(-DCMAKE_BUILD_TYPE=Release)
+
+# Install things into $prefix
+CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix})
+
+# Explicitly use our cmake toolchain file and tell CMake we're cross-compiling
+CMAKE_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN})
+CMAKE_FLAGS+=(-DCMAKE_CROSSCOMPILING:BOOL=ON)
+
+# Don't build python hooks
+CMAKE_FLAGS+=(-DBUILD_PYTHON=no)
+
+# Stock FFTW3 has threads already baked in
+CMAKE_FLAGS+=(-DFFTW3_DISABLE_THREADS=yes)
+
+# linux builds need to link against rt (they don't do this for some reason)
+if [[ ${target} == *-linux-* ]]; then
+    CMAKE_FLAGS+=(-DCASACORE_ARCH_LIBS="-lrt")
 fi
 
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix \
-      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DBUILD_PYTHON=no \
-      -DFFTW3_DISABLE_THREADS=yes \
-      -DBLAS_LIBRARIES=${OPENBLAS} \
-      -DLAPACK_LIBRARIES=${OPENBLAS} \
-      -DCASACORE_ARCH_LIBS="-lrt" \
-      ..
+# Congire
+cmake ${CMAKE_FLAGS[@]} ..
 
+# Make and install
 make -j${nproc}
 make install
 exit
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = supported_platforms()
+# Exclude windows, casacore needs pread and pwrite, which are POSIX-only
+platforms = supported_platforms(exclude=Sys.iswindows)
 # expand gfortran versions as well
 platforms = expand_gfortran_versions(platforms)
 
@@ -81,7 +91,7 @@ products = Product[
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="OpenBLAS_jll", uuid="4536629a-c528-5b80-bd46-f80d51c5b363"))
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
     Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
     Dependency(PackageSpec(name="CFITSIO_jll", uuid="b3e40c51-02ae-5482-8a39-3ace5868dcf4"))
     Dependency(PackageSpec(name="WCS_jll", uuid="550c8279-ae0e-5d1b-948f-937f2608a23e"))
@@ -90,4 +100,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"5.2.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.7", preferred_gcc_version = v"5.2.0")
