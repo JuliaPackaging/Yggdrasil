811bbc9fc444ddcbf7911e3a0616f68507321f84
Kiran Shila <me@kiranshila.com>
2022-06-11 10:47:33 -0700

    Update C/casacore/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- C/casacore/build_tarballs.jl
+++ C/casacore/build_tarballs.jl
@@ -32,7 +32,7 @@ exit
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 # expand gfortran versions as well
 platforms = expand_gfortran_versions(platforms)
 
