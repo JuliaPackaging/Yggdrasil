6bc5506d57e861438142878c6193a31dadfb0419
Kiran Shila <me@kiranshila.com>
2022-06-13 15:35:00 -0700

    Update C/casacorewrapper/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- C/casacorewrapper/build_tarballs.jl
+++ C/casacorewrapper/build_tarballs.jl
@@ -12,8 +12,7 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd casascorewrapper/
+cd $WORKSPACE/srcdir/casascorewrapper/
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j${nproc}
