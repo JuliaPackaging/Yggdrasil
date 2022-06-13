3f9f71e9ba31765e27f101886b9512f317e72c59
Kiran Shila <me@kiranshila.com>
2022-06-13 15:35:04 -0700

    Update C/casacorewrapper/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- C/casacorewrapper/build_tarballs.jl
+++ C/casacorewrapper/build_tarballs.jl
@@ -17,7 +17,6 @@ mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j${nproc}
 make install
-exit
 """
 
 # Use the same platforms from casacore
