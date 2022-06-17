fd4f35ef6159910a603bd89f8cae6050b5f1d664
agerlach <599421+agerlach@users.noreply.github.com>
2022-06-17 07:33:01 -0400

    Update F/Flowstar/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- F/Flowstar/build_tarballs.jl
+++ F/Flowstar/build_tarballs.jl
@@ -13,7 +13,6 @@ name = "Flowstar"
 version = v"2.1.0"
 sources = [
     ArchiveSource("https://www.cs.colorado.edu/~xich8622/src/flowstar-$version.tar.gz", "642b17a55c6725d4bfc5b98900802e3b82f37fbbe9fb9028f1110c669a5afc86")
-    FileSource("https://www.gnu.org/licenses/gpl-3.0.txt", "3972dc9744f6499f0f9b2dbf76696f2ae7ad8af9b23dde66d6af86c9dfb36986"; filename = "LICENSE")
 ]
 
 begin
