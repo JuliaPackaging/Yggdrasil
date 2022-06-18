3c877e18dd9bb9b2e79415e00f661a7e37b2aea9
Mosè Giordano <giordano@users.noreply.github.com>
2022-06-18 10:56:40 +0100

    [LibUnwind] Add patch to fix warnings in aarch64 header file (#5040)

--- L/LibUnwind/LibUnwind@1.5.0/build_tarballs.jl
+++ L/LibUnwind/LibUnwind@1.5.0/build_tarballs.jl
@@ -1,15 +1,13 @@
 using BinaryBuilder
 
 name = "LibUnwind"
-version = v"1.5.0"
-
-# XXX: The git tag for libunwind v1.5.0 is just v1.5 rather than v1.5.0. Check this
-# when updating to the next libunwind version.
-version_short = "1.5"
+# XXX: The git tag is sometimes something like `v1.5` rather than `v1.5.0`.
+version_string = "1.5"
+version = VersionNumber(version_string)
 
 # Collection of sources required to build libffi
 sources = [
-    ArchiveSource("https://github.com/libunwind/libunwind/releases/download/v$(version_short)/libunwind-$(version).tar.gz",
+    ArchiveSource("https://github.com/libunwind/libunwind/releases/download/v$(version_string)/libunwind-$(version).tar.gz",
                   "90337653d92d4a13de590781371c604f9031cdb50520366aa1e3a91e1efb1017"),
     DirectorySource("./bundled"),
 ]
@@ -18,25 +16,19 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/libunwind*/
 
-# We need to massage configure script to convince it to build the shared library
-# for PowerPC.
-if [[ "${target}" == powerpc64le-* ]]; then
-    autoreconf -vi
-fi
-
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/libunwind-prefer-extbl.patch
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/libunwind-static-arm.patch
 atomic_patch -p0 ${WORKSPACE}/srcdir/patches/libunwind-configure-ppc64le.patch
 atomic_patch -p0 ${WORKSPACE}/srcdir/patches/libunwind-configure-static-lzma.patch
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/libunwind-cfa-rsp.patch
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/libunwind-dwarf-table.patch
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/libunwind-non-empty-structs.patch
 
-CFLAGS="${CFLAGS} -DPI -fPIC -I${prefix}/include"
+export CFLAGS="-DPI -fPIC"
 ./configure \
     --prefix=${prefix} \
     --build=${MACHTYPE} \
     --host=${target} \
-    CFLAGS="${CFLAGS}" \
     --libdir=${libdir} \
     --enable-minidebuginfo \
     --enable-zlibdebuginfo \
@@ -55,7 +47,7 @@ ar -qc ${prefix}/lib/libunwind.a unpacked/**/*
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line.  libunwind is only used
 # on Linux or FreeBSD (e.g. ELF systems)
-platforms = filter(p -> Sys.islinux(p) || Sys.isfreebsd(p), supported_platforms(;experimental=true))
+platforms = filter(p -> Sys.islinux(p) || Sys.isfreebsd(p), supported_platforms())
 
 # The products that we will ensure are always built
 products = [
