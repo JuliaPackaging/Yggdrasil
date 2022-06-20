3aca061874c2924c7f33cc0763ee3211dc1cd002
Oscar Dowson <odow@users.noreply.github.com>
2022-06-20 12:36:07 +1200

    Update build_tarballs.jl

--- C/CasADi/build_tarballs.jl
+++ C/CasADi/build_tarballs.jl
@@ -44,6 +44,7 @@ products = [
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
+platforms = supported_platforms()
 platforms = expand_cxxstring_abis(platforms)
 
 dependencies = [
