bc515e0103af4a9ee97899ea1601ab2332e703aa
Mosè Giordano <giordano@users.noreply.github.com>
2022-06-21 19:13:03 +0100

    Apply suggestions from code review

--- S/SoapyMultiSDR/build_tarballs.jl
+++ S/SoapyMultiSDR/build_tarballs.jl
@@ -34,11 +34,11 @@ install_license ../LICENSE_1_0.txt
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter!(p -> arch(p) != "armv6l", supported_platforms(;experimental=true))
+platforms = filter!(p -> arch(p) != "armv6l", supported_platforms())
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
-products = Product[
+products = [
     LibraryProduct("libMultiSDRSupport", :librtlsdrSupport, ["lib/SoapySDR/modules0.8/"])
 ]
 
