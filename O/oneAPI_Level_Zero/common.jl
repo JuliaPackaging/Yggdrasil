408f91e780a14bba14188b9bbabde0e4600b4a5b
Tim Besard <tim.besard@gmail.com>
2022-06-27 15:43:13 +0200

    Bump oneAPI Loader to v1.8.1.

--- O/oneAPI_Level_Zero/common.jl
+++ O/oneAPI_Level_Zero/common.jl
@@ -1,5 +1,4 @@
-
-version = v"1.7.15"
+version = v"1.8.1"
 api_version = v"1.3.7"
 
 # Collection of sources required to build this package
@@ -8,5 +7,5 @@ api_version = v"1.3.7"
 # which are versioned independently.
 sources = BinaryBuilder.AbstractSource[
     GitSource("https://github.com/oneapi-src/level-zero.git",
-              "bb7fff05b801e26c3d7858e03e509d1089914d59"),
+              "108c4ded66ee9bcd225358e723fe9173c15171d3"),
 ]
