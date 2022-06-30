cb781153b7dd7871779c6b7ceafdc3561726fa96
Mosè Giordano <giordano@users.noreply.github.com>
2022-06-30 17:31:16 +0100

    [FMM3D] Simplify recipe

--- F/FMM3D/build_tarballs.jl
+++ F/FMM3D/build_tarballs.jl
@@ -25,59 +25,38 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd FMM3D/
+cd $WORKSPACE/srcdir/FMM3D/
 
-touch make.inc
-
+FFLAGS="-fPIC -O3 -funroll-loops -std=legacy"
+if [[ ${target} = *mingw* ]]; then
+    FFLAGS="${FFLAGS} -fno-asynchronous-unwind-tables"
+fi
+OMPFLAGS="-fopenmp"
+LIBS="-lm"
 
 echo "CC=${CC}" >> make.inc;
 echo "CXX=${CXX}" >> make.inc;
 echo "FC=${FC}" >> make.inc;
-
-if [[ ${target} = *mingw* ]]; then
-    echo "FFLAGS= -fPIC -O3 -fno-asynchronous-unwind-tables -funroll-loops -std=legacy" >> make.inc;
-else
-    echo "FFLAGS= -fPIC -O3 -funroll-loops -std=legacy" >> make.inc;
-fi
-
-export OMPFLAGS="-fopenmp"
-export OMPLIBS="-lgomp"
-
-# openmp library name is different on bsd systems
-if [[ ${target} = *apple* || ${target} = *freebsd* ]]; then
-  export OMPLIBS="-lomp"
-fi
-
-
+echo "FFLAGS= ${FFLAGS}" >> make.inc;
 echo "OMPFLAGS= ${OMPFLAGS}" >> make.inc;
-echo "OMPLIBS= ${OMPLIBS}" >> make.inc;
-
-export LIBS="-lm -lgfortran"
 echo "LIBS = ${LIBS}" >> make.inc;
 
 make -j${nproc} lib OMP=ON
 
-echo "${TARGET}"
-
 # build the correct type of dynamic library for
 # current target
-
-export SHAREFLAGS="-shared -fPIC"
+SHAREFLAGS="-shared -fPIC"
 
 cd lib-static
 ar x libfmm3d.a
-${FC} ${SHAREFLAGS} ${OMPFLAGS} *.o -o "${libdir}/libfmm3d.${dlext}" ${LIBS} ${OMPLIBS}
-cd ..
+${FC} ${SHAREFLAGS} ${OMPFLAGS} *.o -o "${libdir}/libfmm3d.${dlext}" ${LIBS}
 
 install_license ${WORKSPACE}/srcdir/FMM3D/LICENSE
-rm make.inc
-exit
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_gfortran_versions(supported_platforms(; experimental=true))
+platforms = expand_gfortran_versions(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
