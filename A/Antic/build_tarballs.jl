b7a948e0e983646ebf7b150c091f5112ddf16bc1
Mosè Giordano <giordano@users.noreply.github.com>
2022-06-30 18:03:56 +0100

    Update build_tarballs.jl

--- A/Antic/build_tarballs.jl
+++ A/Antic/build_tarballs.jl
@@ -52,7 +52,7 @@ make install LIBDIR=$(basename ${libdir})
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=build_for_julia16_or_newer)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
