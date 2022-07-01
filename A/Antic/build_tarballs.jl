ed1311f17e8ce2b7f56f62193edff486bb0cf8fd
Mosè Giordano <giordano@users.noreply.github.com>
2022-07-01 10:57:54 +0100

    [Antic] Dummy edit to trigger rebuild

--- A/Antic/build_tarballs.jl
+++ A/Antic/build_tarballs.jl
@@ -57,7 +57,7 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libantic", :libantic)
+    LibraryProduct("libantic", :libantic),
 ]
 
 # Dependencies that must be installed before this package can be built
