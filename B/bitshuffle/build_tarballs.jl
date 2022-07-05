7eeda2bfa1dec1b95b9a507a237295a6f815b07d
James.Hester <jxh@ansto.gov.au>
2022-07-05 13:59:57 +1000

    Add support for lz4 and zstd compression during shuffle.

--- B/bitshuffle/build_tarballs.jl
+++ B/bitshuffle/build_tarballs.jl
@@ -7,14 +7,16 @@ version = v"0.4.2"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/kiyo-masui/bitshuffle.git", "a60471d37a8cbbd8265dc8cfa83a9320abdcb590")
+    GitSource("https://github.com/kiyo-masui/bitshuffle.git", "a60471d37a8cbbd8265dc8cfa83a9320abdcb590"),
+    GitSource("https://github.com/facebook/zstd.git", "e47e674cd09583ff0503f0f6defd6d23d8b718d3")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/bitshuffle/src
+cd $WORKSPACE/srcdir/bitshuffle
 mkdir -p "${libdir}"
-cc -O3 -std=c99 -fPIC --shared -o "${libdir}/libbitshuffle.${dlext}" iochain.c bitshuffle_core.c
+zstd_srcs="../zstd/lib/common/*.c ../zstd/lib/compress/*.c ../zstd/lib/decompress/*.c ../zstd/lib/decompress/*.S"
+cc -O3 -std=c99 -Ilz4 -Isrc -I../zstd/lib -fPIC --shared -o "${libdir}/libbitshuffle.${dlext}" src/bitshuffle.c src/iochain.c src/bitshuffle_core.c lz4/lz4.c ${zstd_srcs}
 """
 
 # These are the platforms we will build for by default, unless further
