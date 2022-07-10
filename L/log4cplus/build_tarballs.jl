8f9fea2a7bf9a5c127b1ec1c51d8a0c778336891
Mosè Giordano <giordano@users.noreply.github.com>
2022-07-10 22:59:24 +0100

    Apply suggestions from code review

--- L/log4cplus/build_tarballs.jl
+++ L/log4cplus/build_tarballs.jl
@@ -26,13 +26,13 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = expand_cxxstring_abis(supported_platforms())
 
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("liblog4cplusU", :liblog4cplusU),
-    LibraryProduct("liblog4cplus", :liblog4cplus)
+    LibraryProduct(["liblog4cplusU", "liblog4cplusU-2-0"], :liblog4cplusU),
+    LibraryProduct(["liblog4cplus", "liblog4cplus-2-0"], :liblog4cplus),
 ]
 
 # Dependencies that must be installed before this package can be built
