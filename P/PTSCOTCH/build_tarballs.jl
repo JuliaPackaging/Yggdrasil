b5ca2233e07a848b808f49b6eebafe921c3f9858
Erik Schnetter <schnetter@gmail.com>
2022-07-14 10:04:45 -0400

    PTSCOTCH: Add patch for MPI constants

--- P/PTSCOTCH/build_tarballs.jl
+++ P/PTSCOTCH/build_tarballs.jl
@@ -1,6 +1,9 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
+using Base.BinaryPlatforms
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 
 name = "PTSCOTCH"
 version = v"6.1.3"
@@ -22,6 +25,7 @@ fi
 if [[ "${target}" == *mingw* ]]; then
     atomic_patch -p1 "${WORKSPACE}/srcdir/patches/Windows.patch"
 fi
+atomic_patch -p1 "${WORKSPACE}/srcdir/patches/mpi-constants.patch"
 cd src
 make ptscotch
 make ptesmumps
@@ -30,9 +34,27 @@ cp ../include/p* ${includedir}
 install_license ../LICENSE_en.txt
 """
 
+augment_platform_block = """
+    using Base.BinaryPlatforms
+    $(MPI.augment)
+    augment_platform!(platform::Platform) = augment_mpi!(platform)
+"""
+
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms(; exclude=Sys.iswindows)
+
+platforms, platform_dependencies = MPI.augment_platforms(platforms)
+
+# There is a build error with OpenMPI (this could probably be circumvented)
+platforms = filter(p -> p["mpi"] ≠ "openmpi", platforms)
+
+# Avoid platforms where the MPI implementation isn't supported
+# OpenMPI
+platforms = filter(p -> !(p["mpi"] == "openmpi" && arch(p) == "armv6l" && libc(p) == "glibc"), platforms)
+# MPItrampoline
+platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && libc(p) == "musl"), platforms)
+platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && Sys.isfreebsd(p)), platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -46,9 +68,10 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a")),
-    Dependency("MPICH_jll"),
     Dependency("SCOTCH_jll", v"6.1.3")
 ]
+append!(dependencies, platform_dependencies)
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9.1.0", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               augment_platform_block, julia_compat="1.6", preferred_gcc_version=v"9.1.0")
