efa54fd2b7020395c03ca852993586389e28fa2c
Erik Schnetter <schnetter@gmail.com>
2022-07-16 20:31:57 -0400

    SCALAPACK: Works with MPICH

--- S/SCALAPACK/build_tarballs.jl
+++ S/SCALAPACK/build_tarballs.jl
@@ -8,8 +8,10 @@ version = v"2.2.0"
 scalapack_version = v"2.2.0"
 
 sources = [
-  ArchiveSource("http://www.netlib.org/scalapack/scalapack-$(scalapack_version).tgz",
-                "40b9406c20735a9a3009d863318cb8d3e496fb073d201c5463df810e01ab2a57"),
+  # ArchiveSource("http://www.netlib.org/scalapack/scalapack-$(scalapack_version).tgz",
+  #               "40b9406c20735a9a3009d863318cb8d3e496fb073d201c5463df810e01ab2a57"),
+  ArchiveSource("https://github.com/Reference-ScaLAPACK/scalapack/archive/refs/tags/v$(scalapack_version).tar.gz",
+                "8862fc9673acf5f87a474aaa71cd74ae27e9bbeee475dbd7292cec5b8bcbdcf3"),
   DirectorySource("./bundled")
 ]
 
@@ -24,55 +26,47 @@ for f in ${WORKSPACE}/srcdir/patches/*.patch; do
   atomic_patch -p1 ${f}
 done
 
-CMAKE_FLAGS=(-DCMAKE_INSTALL_PREFIX=${prefix}
-             -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}"
-             -DCMAKE_BUILD_TYPE=Release
-             -DBUILD_SHARED_LIBS=ON)
-
-# if [[ "${target}" == i686-*  ]] || [[ "${target}" == x86_64-*  ]]; then
-#   CMAKE_FLAGS+=(-DCMAKE_EXE_LINKER_FLAGS="-lgfortran -lquadmath")
-# else
-#   if [[ "${target}" == powerpc64le-linux-gnu ]]; then
-#     # special case for CMake to discover MPI_Fortran
-#     CMAKE_FLAGS+=(-DCMAKE_EXE_LINKER_FLAGS="-lgfortran -L/opt/${target}/${target}/sys-root/usr/lib64 -lpthread -lrt" \
-#                   -DCMAKE_SHARED_LINKER_FLAGS="-lgfortran -L/opt/${target}/${target}/sys-root/usr/lib64 -lpthread -lrt" \
-#                   -DMPI_Fortran_LINK_FLAGS="-Wl,-rpath -Wl,/workspace/destdir/lib -Wl,--enable-new-dtags -L/workspace/destdir/lib -Wl,-L/opt/${target}/${target}/sys-root/usr/lib64 -Wl,-lpthread -Wl,-lrt")
-#   else
-#     CMAKE_FLAGS+=(-DCMAKE_EXE_LINKER_FLAGS="-lgfortran")
-#   fi
-# fi
-# CMAKE_FLAGS+=(-DMPI_BASE_DIR=${prefix})
-
-OPENBLAS=(-lopenblas)
-CPPFLAGS=()
-FFLAGS=(-cpp -ffixed-line-length-none)
-
-# TODO: Follow the official instruction to build in ILP64 mode; see <https://github.com/Reference-ScaLAPACK/scalapack/pull/19>
-if [[ ${nbits} == 64 ]] && [[ ${target} != aarch64* ]]; then
-  OPENBLAS=(-lopenblas64_)
-  if [[ "${target}" == powerpc64le-linux-gnu ]]; then
-    OPENBLAS+=(-lgomp)
-  fi
-  syms=(caxpy cbdsqr ccopy cdotc cdotu cgbmv cgbtrf cgemm cgemv cgerc cgeru cgesv cgetrf cgetrs chbmv chemm chemv cher cher2 cher2k cherk chetrd clacgv clacpy cladiv clanhs clarfg clartg claset clasr classq claswp cpbtrf cpotrf cpttrf crot csbmv cscal csscal cswap csymm csyr2k csyrk ctbtrs ctrmm ctrmv ctrsm ctrsv ctrtrs dasum daxpy dbdsqr dcopy ddot dgbmv dgbtrf dgemm dgemv dger dgesv dgetrf dgetrs dhbmv disnan dlabad dlacpy dlae2 dlaebz dlaed4 dlaev2 dlagtf dlagts dlahqr dlamc3 dlamch dlange dlanst dlanv2 dlapy2 dlapy3 dlaqr0 dlaqr1 dlaqr3 dlaqr4 dlarfg dlarfx dlarnv dlarra dlarrb dlarrc dlarrd dlarrk dlarrv dlartg dlaruv dlascl dlaset dlasq2 dlasr dlasrt dlassq dlaswp dlasy2 dnrm2 dpbtrf dpotrf dpttrf drot dsbmv dscal dstedc dsteqr dsterf dswap dsymm dsymv dsyr dsyr2 dsyr2k dsyrk dsytrd dtbtrs dtrmm dtrmv dtrsm dtrsv dtrtrs dzasum dznrm2 dzsum1 icamax icmax1 idamax ieeeck ilaenv isamax izamax izmax1 lsame lsamen sasum saxpy sbdsqr scasum scnrm2 scopy scsum1 sdot sgbmv sgbtrf sgemm sgemv sger sgesv sgetrf sgetrs shbmv sisnan slabad slacpy slae2 slaebz slaed4 slaev2 slagtf slagts slahqr slamc3 slamch slange slanst slanv2 slapy2 slapy3 slaqr0 slaqr1 slaqr3 slaqr4 slarfg slarfx slarnv slarra slarrb slarrc slarrd slarrk slarrv slartg slaruv slascl slaset slasq2 slasr slasrt slassq slaswp slasy2 snrm2 spbtrf spotrf spttrf srot ssbmv sscal sstedc ssteqr ssterf sswap ssymm ssymv ssyr ssyr2 ssyr2k ssyrk ssytrd stbtrs strmm strmv strsm strsv strtrs xerbla zaxpy zbdsqr zcopy zdbmv zdotc zdotu zdscal zgbmv zgbtrf zgemm zgemv zgerc zgeru zgesv zgetrf zgetrs zhbmv zhemm zhemv zher zher2 zher2k zherk zhetrd zlacgv zlacpy zladiv zlanhs zlarfg zlartg zlaset zlasr zlassq zlaswp zpbtrf zpotrf zpttrf zrot zscal zswap zsymm zsyr2k zsyrk ztbtrs ztrmm ztrmv ztrsm ztrsv ztrtrs)
-  for sym in ${syms[@]}
-  do
-    CPPFLAGS+=("-D${sym}=${sym}_64")
-    CPPFLAGS+=("-D${sym}_=${sym}_64_")  # due to some evil #defines in SCALAPACK
-    CPPFLAGS+=("-D${sym^^}=${sym}_64")
-  done
-
-  CMAKE_FLAGS+=(-DCMAKE_Fortran_FLAGS=\"${CPPFLAGS[*]} ${FFLAGS[*]}\" \
-                -DCMAKE_C_FLAGS=\"${CPPFLAGS[*]}\")
+cppflags=()
+cflags=()
+fflags=(-cpp -ffixed-line-length-none)
+openblas=(-lopenblas)
+if [[ ${nbits} == 64 ]]; then
+    cppflags+=(-DInt=long)
+    fflags+=(-fdefault-integer-8)
+    syms=(caxpy cbdsqr ccopy cdotc cdotu cgbmv cgbtrf cgemm cgemv cgerc cgeru cgesv cgetrf cgetrs chbmv chemm chemv cher cher2 cher2k cherk chetrd clacgv clacpy cladiv clanhs clarfg clartg claset clasr classq claswp cpbtrf cpotrf cpttrf crot csbmv cscal csscal cswap csymm csyr2k csyrk ctbtrs ctrmm ctrmv ctrsm ctrsv ctrtrs dasum daxpy dbdsqr dcopy ddot dgbmv dgbtrf dgemm dgemv dger dgesv dgetrf dgetrs dhbmv disnan dlabad dlacpy dlae2 dlaebz dlaed4 dlaev2 dlagtf dlagts dlahqr dlamc3 dlamch dlange dlanst dlanv2 dlapy2 dlapy3 dlaqr0 dlaqr1 dlaqr3 dlaqr4 dlarfg dlarfx dlarnv dlarra dlarrb dlarrc dlarrd dlarrk dlarrv dlartg dlaruv dlascl dlaset dlasq2 dlasr dlasrt dlassq dlaswp dlasy2 dnrm2 dpbtrf dpotrf dpttrf drot dsbmv dscal dstedc dsteqr dsterf dswap dsymm dsymv dsyr dsyr2 dsyr2k dsyrk dsytrd dtbtrs dtrmm dtrmv dtrsm dtrsv dtrtrs dzasum dznrm2 dzsum1 icamax icmax1 idamax ieeeck ilaenv isamax izamax izmax1 lsame lsamen sasum saxpy sbdsqr scasum scnrm2 scopy scsum1 sdot sgbmv sgbtrf sgemm sgemv sger sgesv sgetrf sgetrs shbmv sisnan slabad slacpy slae2 slaebz slaed4 slaev2 slagtf slagts slahqr slamc3 slamch slange slanst slanv2 slapy2 slapy3 slaqr0 slaqr1 slaqr3 slaqr4 slarfg slarfx slarnv slarra slarrb slarrc slarrd slarrk slarrv slartg slaruv slascl slaset slasq2 slasr slasrt slassq slaswp slasy2 snrm2 spbtrf spotrf spttrf srot ssbmv sscal sstedc ssteqr ssterf sswap ssymm ssymv ssyr ssyr2 ssyr2k ssyrk ssytrd stbtrs strmm strmv strsm strsv strtrs xerbla zaxpy zbdsqr zcopy zdbmv zdotc zdotu zdscal zgbmv zgbtrf zgemm zgemv zgerc zgeru zgesv zgetrf zgetrs zhbmv zhemm zhemv zher zher2 zher2k zherk zhetrd zlacgv zlacpy zladiv zlanhs zlarfg zlartg zlaset zlasr zlassq zlaswp zpbtrf zpotrf zpttrf zrot zscal zswap zsymm zsyr2k zsyrk ztbtrs ztrmm ztrmv ztrsm ztrsv ztrtrs)
+    for sym in ${syms[@]}; do
+        cppflags+=("-D${sym}=${sym}_64")
+        cppflags+=("-D${sym}_=${sym}_64_")   # due to some evil #defines in SCALAPACK
+        cppflags+=("-D${sym^^}=${sym}_64")
+    done
+    openblas=(-lopenblas64_)
 fi
 
-# We attach our MPI flags here
-CMAKE_FLAGS+=(-DBLAS_LIBRARIES=\"${OPENBLAS[*]} -lmpifort -lmpi\" \
-              -DLAPACK_LIBRARIES=\"${OPENBLAS[*]}\")
+mpilibs=()
+if grep -q MPICH "${prefix}/include/mpi.h"; then
+    mpilibs=(-lmpifort -lmpi)
+elif grep -q MPItrampoline "${prefix}/include/mpi.h"; then
+    mpilibs=(-lmpitrampoline)
+elif grep -q OMPI_MAJOR_VERSION $prefix/include/mpi.h; then
+    mpilibs=(-lmpi_usempif08 -lmpi_usempi_ignore_tkr -lmpi_mpifh -lmpi)
+fi
+
+cmake_flags=(-DCMAKE_INSTALL_PREFIX=${prefix}
+             -DCMAKE_FIND_ROOT_PATH=${prefix}
+             -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}"
+             -DCMAKE_Fortran_FLAGS="${cppflags[*]} ${fflags[*]}"
+             -DCMAKE_C_FLAGS="${cppflags[*]} ${cflags[*]}"
+             -DCMAKE_BUILD_TYPE=Release
+             -DBLAS_LIBRARIES="${openblas[*]} ${mpilibs[*]}"
+             -DLAPACK_LIBRARIES="${openblas[*]}"
+             -DSCALAPACK_BUILD_TESTS=OFF
+             -DBUILD_SHARED_LIBS=ON)
 
 export CDEFS="Add_"
 
-mkdir build && cd build
-cmake .. "${CMAKE_FLAGS[@]}"
+mkdir build
+cd build
+cmake .. "${cmake_flags[@]}"
 
 make -j${nproc} all
 make install
