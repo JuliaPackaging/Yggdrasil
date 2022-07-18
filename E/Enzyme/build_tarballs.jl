86d53331ca142a7bd3f61bb92832095aa5103c66
William S. Moses <gh@wsmoses.com>
2022-07-18 16:42:04 -0400

    Fix scope

--- E/Enzyme/build_tarballs.jl
+++ E/Enzyme/build_tarballs.jl
@@ -14,7 +14,7 @@ version = VersionNumber(split(auto_version, "/")[end])
 llvm_versions = [v"11.0.1", v"12.0.1", v"13.0.1", v"14.0.2"]
 
 # Collection of sources required to build attr
-sources = [GitSource(repo, "95a666626f93f976a587d26113b4924ceeeeb668")]
+sources = [GitSource(repo, "6d649c700aa992a2741c7da29959ea5665f8d6ce")]
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
@@ -40,12 +40,11 @@ cmake -B build-native -S enzyme -GNinja "${NATIVE_CMAKE_FLAGS[@]}"
 
 # Only build blasheaders and tblgen
 ninja -C build-native -j ${nproc} blasheaders enzyme-tblgen
-
 # 2. Cross-compile
 CMAKE_FLAGS=()
 CMAKE_FLAGS+=(-DENZYME_EXTERNAL_SHARED_LIB=ON)
 CMAKE_FLAGS+=(-DBC_LOAD_HEADER=`pwd`/build-native/BCLoad/gsl/blas_headers.h)
-CMAKE_FLAGS+=(-DEnzyme_TABLEGEN_EXE=`pwd`/build-native/bin/enzyme-tblgen)
+CMAKE_FLAGS+=(-DEnzyme_TABLEGEN_EXE=`pwd`/build-native/tools/enzyme-tblgen/enzyme-tblgen)
 CMAKE_FLAGS+=(-DENZYME_CLANG=OFF)
 # RelWithDebInfo for decent performance, with debugability
 CMAKE_FLAGS+=(-DCMAKE_BUILD_TYPE=RelWithDebInfo)
