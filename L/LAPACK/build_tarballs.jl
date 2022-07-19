1167a6dc514503fe1b89bf64da01af7408729082
Viral B. Shah <ViralBShah@users.noreply.github.com>
2022-07-18 22:19:08 -0400

    Update build_tarballs.jl

--- L/LAPACK/build_tarballs.jl
+++ L/LAPACK/build_tarballs.jl
@@ -41,7 +41,7 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_gfortran_versions(supported_platforms())
-filter!(p -> !(arch(p) == "aarch64" && Sys.islinux(p) && libgfortran_version(p)) ≠ v"3", platforms)
+filter!(p -> !(arch(p) == "aarch64" && Sys.islinux(p) && libgfortran_version(p) != v"3"), platforms)
 
 # The products that we will ensure are always built
 products = [
