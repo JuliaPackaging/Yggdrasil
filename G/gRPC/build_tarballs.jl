f70392e1c9a83f96f7bcfd1c1080d1b716907406
Rory Linehan <rorylinehan@protonmail.com>
2022-07-24 11:58:49 -0500

    switch to build dependency

--- G/gRPC/build_tarballs.jl
+++ G/gRPC/build_tarballs.jl
@@ -64,7 +64,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("OpenSSL_jll")
+    BuildDependency("OpenSSL_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
