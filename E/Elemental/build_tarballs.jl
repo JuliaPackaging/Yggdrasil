554b3ad0b67426dc3396f7c856ae163a297d6b68
Erik Schnetter <schnetter@gmail.com>
2022-07-24 19:58:17 -0400

    Increase version number since MPItrampoline 5 is required
    Require GCC 7 to avoid ICEs on aarch64

--- E/Elemental/build_tarballs.jl
+++ E/Elemental/build_tarballs.jl
@@ -6,10 +6,10 @@ const YGGDRASIL_DIR = "../.."
 include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 
 name = "Elemental"
-# This is not really version 0.87.8, but taken from the master branch
+# This is not really version 0.87.9, but taken from the master branch
 # after 0.87.7. This is necessary to prevent C++ template
 # instantiation errors with newer GCC versions.
-version = v"0.87.8"
+version = v"0.87.9"
 
 # Collection of sources required to build Elemental
 sources = [
@@ -77,6 +77,19 @@ platforms = expand_gfortran_versions(platforms)
 
 filter!(!Sys.iswindows, platforms)
 
+platforms, platform_dependencies = MPI.augment_platforms(platforms)
+# Avoid platforms where the MPI implementation isn't supported
+# OpenMPI
+platforms = filter(p -> !(p["mpi"] == "openmpi" && arch(p) == "armv6l" && libc(p) == "glibc"), platforms)
+# With MPItrampoline, select only those platforms where MPItrampoline is actually built
+platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && (Sys.iswindows(p) || libc(p) == "musl")), platforms)
+platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && Sys.isfreebsd(p)), platforms)
+
+# ICE aarch64-linux-gnu-libgfortran4-cxx11-mpi+mpitrampoline
+# ICE aarch64-linux-musl-libgfortran3-cxx11-mpi+mpich
+# ICE aarch64-linux-musl-libgfortran3-cxx11-mpi+mpich
+# ICE aarch64-linux-musl-libgfortran4-cxx03-mpi+openmpi
+
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libEl", :libEl),
@@ -89,17 +102,11 @@ dependencies = [
     Dependency("OpenBLAS_jll"),
 ]
 
-platforms, platform_dependencies = MPI.augment_platforms(platforms)
-# Avoid platforms where the MPI implementation isn't supported
-# OpenMPI
-platforms = filter(p -> !(p["mpi"] == "openmpi" && arch(p) == "armv6l" && libc(p) == "glibc"), platforms)
-# With MPItrampoline, select only those platforms where MPItrampoline is actually built
-platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && (Sys.iswindows(p) || libc(p) == "musl")), platforms)
-platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && Sys.isfreebsd(p)), platforms)
 append!(dependencies, platform_dependencies)
 
 # Build the tarballs, and possibly a `build.jl` as well.
 # GCC 4 does not support -std=c++14
 # GCC 5 does not support the OpenMP function `omp_get_num_places`
+# GCC 6 reports ICEs on aarch64
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               augment_platform_block, julia_compat="1.6", preferred_gcc_version=v"6")
+               augment_platform_block, julia_compat="1.6", preferred_gcc_version=v"7")
