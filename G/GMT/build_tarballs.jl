bf360b198b0852adf9a78e70ae965c59f1558356
Boris Kaus <kaus@uni-mainz.de>
2022-07-24 13:29:24 +0200

    update version

--- G/GMT/build_tarballs.jl
+++ G/GMT/build_tarballs.jl
@@ -57,7 +57,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="LibCURL_jll", uuid="deac9b47-8bc7-5906-a0fe-35ac56dc84c0"); compat="7.73.0")
-    Dependency("NetCDF_jll", v"4.7.4")
+    Dependency("NetCDF_jll", compat="400.702.402")
     Dependency(PackageSpec(name="GDAL_jll", uuid="a7073274-a066-55f0-b90d-d619367d196c"))
     Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
     Dependency(PackageSpec(name="PCRE_jll", uuid="2f80f16e-611a-54ab-bc61-aa92de5b98fc"))
