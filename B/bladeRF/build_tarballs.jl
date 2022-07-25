c73d6baee2295a5d26a8d4a0ea3cbd03f14e1368
Steve Kelly <kd2cca@gmail.com>
2022-07-25 11:27:05 -0400

    [bladerf] drop experimental

--- B/bladeRF/build_tarballs.jl
+++ B/bladeRF/build_tarballs.jl
@@ -28,7 +28,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter!(p -> !Sys.iswindows(p) && !in(arch(p),("armv7l","armv6l")), supported_platforms(;experimental=true))
+platforms = filter!(p -> !Sys.iswindows(p) && !in(arch(p),("armv7l","armv6l")), supported_platforms())
 
 # The products that we will ensure are always built
 products = Product[]
