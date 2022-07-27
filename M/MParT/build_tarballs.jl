247e0961542bb349983c79da86d21cf515cc47a8
Danny Sharp <dannys4@mit.edu>
2022-07-27 08:37:44 -0400

    Make mpart v2

--- M/MParT/build_tarballs.jl
+++ M/MParT/build_tarballs.jl
@@ -2,16 +2,27 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
+# See https://github.com/JuliaLang/Pkg.jl/issues/2942
+# Once this Pkg issue is resolved, this must be removed
+uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
+delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
+
+julia_versions = [v"1.6.3", v"1.7.0", v"1.8.0", v"1.9.0"]
 name = "MParT"
-version = v"0.1.0"
+version = v"0.2.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/MeasureTransport/MParT.git", "64e4b8ee7637382ab7f9aa091ace41ed6a5cbcc6")
+    GitSource("https://github.com/MeasureTransport/MParT.git", "62dd75302c5c4700998fc918f122f66b9428a8e0")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
+# Override compiler ID to silence the horrible "No features found" cmake error
+if [[ $target == *"apple-darwin"* ]]; then
+  macos_extra_flags="-DCMAKE_CXX_COMPILER_ID=AppleClang -DCMAKE_CXX_COMPILER_VERSION=10.0.0 -DCMAKE_CXX_STANDARD_COMPUTED_DEFAULT=11"
+fi
+
 cd $WORKSPACE/srcdir
 mkdir MParT/build && cd MParT/build
 
@@ -19,13 +30,21 @@ if [[ "${target}" == *-freebsd* ]]; then
     export LDFLAGS="-lexecinfo"
 fi
 
+ls ${prefix}/include/eigen3 1>&2
+
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
   -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+  -DCMAKE_CXX_FLAGS="-I${prefix}/include/eigen3 -fopenmp" \
   -DCMAKE_BUILD_TYPE=Release \
   -DMPART_BUILD_TESTS=OFF \
-  -DMPART_PYTHON=OFF ..
-make -j${nprocs} install
+  -DMPART_PYTHON=OFF \
+  -DMPART_JULIA=ON \
+  -DJulia_PREFIX=${prefix} \
+  $macos_extra_flags \
+  ..
+make install
 """
+include("../../L/libjulia/common.jl")
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
@@ -39,10 +58,12 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Kokkos_jll", uuid="c1216c3d-6bb3-5a2b-bbbf-529b35eba709"); compat="=3.6.0")
-    Dependency(PackageSpec(name="Eigen_jll", uuid="bc6bbf8a-a594-5541-9c57-10b0d0312c70"))
+    Dependency("libcxxwrap_julia_jll"),
+    Dependency(PackageSpec(name="Kokkos_jll", uuid="c1216c3d-6bb3-5a2b-bbbf-529b35eba709"); compat="=3.6.0"),
+    Dependency(PackageSpec(name="Eigen_jll", uuid="bc6bbf8a-a594-5541-9c57-10b0d0312c70")),
+    BuildDependency("libjulia_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"8.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"9")
 
