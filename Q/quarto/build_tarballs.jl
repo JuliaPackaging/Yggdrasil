4f995718b8717cd64971299eecf851a906ada31f
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-08-03 19:26:13 +0200

    Add quarto

---
+++ Q/quarto/build_tarballs.jl
@@ -0,0 +1,36 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "quarto"
+version = v"1.1.29"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/quarto-dev/quarto-cli.git", "6a42361a388433ad8b52ab71759a50e778b48ba0"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ${WORKSPACE}/srcdir/quarto-cli
+source configuration
+export QUARTO_BIN_DIR=/workspace/destdir/bin
+/workspace/destdir/bin/deno run --unstable --allow-env --allow-read --allow-write --allow-run --allow-net --allow-ffi --importmap=/workspace/srcdir/quarto-cli/src/dev_import_map.json /workspace/srcdir/quarto-cli/package/src/bld.ts $@
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("quarto", :quarto)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("Deno_jll")
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
