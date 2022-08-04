87250b24bc76d70526e940d55e2c5b92351cb8b6
Christopher Doris <github.com/cjdoris>
2022-08-04 19:09:33 +0100

    forgot to update the hash

--- I/iso_codes/build_tarballs.jl
+++ I/iso_codes/build_tarballs.jl
@@ -19,7 +19,7 @@ end
 # Collection of sources required to build iso-codes
 sources = [
     ArchiveSource("https://salsa.debian.org/iso-codes-team/iso-codes/-/archive/$tag/iso-codes-$tag.tar.bz2",
-                  "a33140be9915330d8826783bcda8d4120f02c301c16af6df9a15a8bc657cfb3d"),
+                  "5aed05f5053080fe55c3fb47f46677691efb59dfb215bd41484a391a7c4df9c5"),
 ]
 
 # Bash recipe for building across all platforms
