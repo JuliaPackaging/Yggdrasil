772eed53485631d81cb65c2df798c7d887ae5c2f
Jun Tian <tianjun.cpp@gmail.com>
2022-08-07 12:43:29 +0800

    filter supported platforms

--- F/FastNoise2/build_tarballs.jl
+++ F/FastNoise2/build_tarballs.jl
@@ -25,7 +25,18 @@ install_license ${WORKSPACE}/srcdir/FastNoise2/LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms())
+platforms = [
+    Platform("x86_64", "linux"; libc="glibc"),
+    Platform("i686", "linux"; libc="glibc"),
+    Platform("x86_64", "linux"; libc="musl"),
+    Platform("i686", "linux"; libc="musl"),
+    Platform("x86_64", "macos"),
+    Platform("x86_64", "freebsd"),
+    Platform("i686", "freebsd"),
+    Platform("x86_64", "windows"),
+    Platform("i686", "windows"),
+]
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
