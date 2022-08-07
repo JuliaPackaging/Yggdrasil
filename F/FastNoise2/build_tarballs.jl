1d685109d4c5119bf289a14970cc7fd6925a384b
Jun Tian <tianjun.cpp@gmail.com>
2022-08-07 11:54:25 +0800

    correct lib name

--- F/FastNoise2/build_tarballs.jl
+++ F/FastNoise2/build_tarballs.jl
@@ -29,7 +29,7 @@ platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libfastnoise2", :libfastnoise2),
+    LibraryProduct("libFastNoise", :libFastNoise),
 ]
 
 # Dependencies that must be installed before this package can be built
