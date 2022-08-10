77285ec9c194b579b029094a543d44ec4a4f024b
Zihua Wu <qqbbnease1004@126.com>
2022-08-10 09:22:22 +0800

    Update V/VDT/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- V/VDT/build_tarballs.jl
+++ V/VDT/build_tarballs.jl
@@ -14,8 +14,7 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/vdt-*
 sed -i "s/-Ofast//" CMakeLists.txt
-mv Licence.txt LICENSE
-install_license LICENSE
+install_license Licence.txt
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
 make -j`nproc` && make install
