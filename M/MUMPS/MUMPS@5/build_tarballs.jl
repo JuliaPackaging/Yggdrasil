fed1963dfbf37986d8670e765c67f1d1ab8dd1d1
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-08-12 23:09:57 -0400

    [MUMPS] Version 5.5.1

--- M/MUMPS/MUMPS@5/build_tarballs.jl
+++ M/MUMPS/MUMPS@5/build_tarballs.jl
@@ -1,19 +1,20 @@
 using BinaryBuilder, Pkg
 
 name = "MUMPS"
-version = v"5.4.2" # <-- This is a lie, we're bumping to 5.4.2 to create a Julia v1.6+ release with experimental platforms
+version = v"5.5.1"
 
 sources = [
-  ArchiveSource("http://mumps.enseeiht.fr/MUMPS_5.4.1.tar.gz",
-                "93034a1a9fe0876307136dcde7e98e9086e199de76f1c47da822e7d4de987fa8"),
-  DirectorySource("./bundled"),
+  ArchiveSource("https://graal.ens-lyon.fr/MUMPS/MUMPS_$(version).tar.gz",
+                "1abff294fa47ee4cfd50dfd5c595942b72ebfcedce08142a75a99ab35014fa15"),
+  DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 mkdir -p ${libdir}
 cd $WORKSPACE/srcdir/MUMPS*
-atomic_patch -p1 ${WORKSPACE}/srcdir/patches/mumps_int32.patch
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/pord.patch
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/Makefile.patch
 
 makefile="Makefile.G95.PAR"
 cp Make.inc/${makefile} Makefile.inc
@@ -28,15 +29,24 @@ if [[ "${target}" == *-apple* ]]; then
   CFLAGS=("-fno-stack-check")
 fi
 
+if [[ "${target}" == *apple* ]]; then
+    SONAME="-install_name"
+else
+    SONAME="-soname"
+fi
+
 make_args+=(OPTF=-O \
             CDEFS=-DAdd_ \
             LMETISDIR=${prefix} \
             IMETIS="-I${includedir}" \
             LMETIS="-L${libdir} -lparmetis -lmetis" \
             ORDERINGSF="-Dpord -Dparmetis" \
+            LIBEXT_SHARED=".${dlext}" \
+            SONAME="${SONAME}" \
             CC="mpicc -fPIC ${CFLAGS[@]}" \
             FC="mpif90 -fPIC ${FFLAGS[@]}" \
             FL="mpif90 -fPIC" \
+            RANLIB="echo" \
             SCALAP="${libdir}/scalapack32.${dlext} ${libdir}/libopenblas.${dlext}" \
             INCPAR= \
             LIBPAR=-lscalapack32 \
@@ -48,53 +58,23 @@ make_args+=(OPTF=-O \
 # LSCOTCH="-L${libdir} -lesmumps -lscotch -lscotcherr"
 # ORDERINGSF="-Dpord -Dparmetis -Dscotch"
 
-if [[ "${target}" == *-apple* ]]; then
-  make_args+=(RANLIB=echo)
-fi
+make -j${nproc} allshared "${make_args[@]}"
 
-# NB: parallel build fails
-make all "${make_args[@]}"
-
-# build shared libs
-all_load="--whole-archive"
-noall_load="--no-whole-archive"
-extra=""
-if [[ "${target}" == *-apple-* ]]; then
-    all_load="-all_load"
-    noall_load="-noall_load"
-    extra="-Wl,-undefined -Wl,dynamic_lookup -headerpad_max_install_names"
-fi
-
-cd lib
-libs=(-lparmetis -lmetis -lscalapack32 -lopenblas)
-mpif90 -fPIC -shared -Wl,${all_load} libpord.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o libpord.${dlext}
-cp libpord.${dlext} ${libdir}
-
-libs+=(-L${libdir} -lpord)
-mpif90 -fPIC -shared -Wl,${all_load} libmumps_common.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o libmumps_common.${dlext}
-cp libmumps_common.${dlext} ${libdir}
-
-libs+=(-lmumps_common)
-for libname in cmumps dmumps smumps zmumps
-do
-  mpif90 -fPIC -shared -Wl,${all_load} lib${libname}.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o lib${libname}.${dlext}
-done
-cp *.${dlext} ${libdir}
-cd ..
-
-cp include/* ${prefix}/include
+cp include/*.h ${includedir}
+cp lib/*.${dlext} ${libdir}
 """
 
 # OpenMPI and MPICH are not precompiled for Windows
 # MUMPS doesn't build on PowerPC
-platforms = expand_gfortran_versions(filter!(p -> !Sys.iswindows(p) && arch(p) != "powerpc64le", supported_platforms()))
+platforms = expand_gfortran_versions(filter!(p -> !Sys.iswindows(p), supported_platforms()))
 
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libsmumps", :libsmumps),
     LibraryProduct("libdmumps", :libdmumps),
     LibraryProduct("libcmumps", :libcmumps),
-    LibraryProduct("libzmumps", :libzmumps)
+    LibraryProduct("libzmumps", :libzmumps),
+    LibraryProduct("libmumps_common", :libmumps_common),
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -105,9 +85,8 @@ dependencies = [
     Dependency(PackageSpec(name="SCOTCH_jll", uuid="a8d0f55d-b80e-548d-aff6-1a04c175f0f9")),
     Dependency(PackageSpec(name="PARMETIS_jll", uuid="b247a4be-ddc1-5759-8008-7e02fe3dbdaa")),
     Dependency(PackageSpec(name="SCALAPACK32_jll", uuid="aabda75e-bfe4-5a37-92e3-ffe54af3c273")),
-    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2")),
-    # Dependency(PackageSpec(name="SCOTCH_jll", uuid="a8d0f55d-b80e-548d-aff6-1a04c175f0f9"))
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
 ]
 
 # Build the tarballs
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies, julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies, julia_compat="1.6", preferred_gcc_version=v"6")
