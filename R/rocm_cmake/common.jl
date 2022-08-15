a875207ef62e0f2acebe507b5a70f8830dfe84de
Anton Smirnov <tonysmn97@gmail.com>
2022-08-15 15:06:08 +0300

    Actually use common build script for 'rocm_cmake'

--- R/rocm_cmake/common.jl
+++ R/rocm_cmake/common.jl
@@ -1,10 +1,13 @@
 const ROCM_GIT = "https://github.com/RadeonOpenCompute/rocm-cmake/"
 const ROCM_TAGS = Dict(
     v"4.2.0" => "299e190ec3d38c2279d9aec762469628f0b2b1867adc082edc5708d1ac785c3b",
-    v"4.5.2" => "85f2ef51327e4b09d81a221b4ad31c97923dabc1bc8ff127dd6c570742185751")
+    v"4.5.2" => "85f2ef51327e4b09d81a221b4ad31c97923dabc1bc8ff127dd6c570742185751",
+)
 const ROCM_PLATFORMS = [
     Platform("x86_64", "linux"; libc="glibc", cxxstring_abi="cxx11"),
-    Platform("x86_64", "linux"; libc="musl", cxxstring_abi="cxx11")]
+    Platform("x86_64", "linux"; libc="musl", cxxstring_abi="cxx11"),
+]
+
 const BUILDSCRIPT = raw"""
 cd ${WORKSPACE}/srcdir/rocm-cmake*/
 
@@ -18,15 +21,13 @@ make -j${nproc}
 make install
 """
 
+const PRODUCTS = [FileProduct("share/rocm/cmake", :cmake_dir)]
+const NAME = "rocm_cmake"
+
 function configure_build(version)
     sources = [
         ArchiveSource(
             ROCM_GIT * "archive/rocm-$(version).tar.gz", ROCM_TAGS[version]),
     ]
-    products = [FileProduct("share/rocm/cmake", :cmake_dir)]
-    name = "rocm_cmake"
-    dependencies = []
-    (
-        name, version, sources, BUILDSCRIPT,
-        ROCM_PLATFORMS, products, dependencies)
+    NAME, version, sources, BUILDSCRIPT, ROCM_PLATFORMS, PRODUCTS, []
 end
--- R/rocm_cmake/rocm_cmake@4.5.2/build_tarballs.jl
+++ R/rocm_cmake/rocm_cmake@4.5.2/build_tarballs.jl
@@ -1,32 +1,7 @@
 using Pkg
 using BinaryBuilder
 
-name = "rocm_cmake"
-version = v"4.5.2"
-
-sources = [
-    ArchiveSource(
-        "https://github.com/RadeonOpenCompute/rocm-cmake/archive/rocm-$(version).tar.gz",
-        "85f2ef51327e4b09d81a221b4ad31c97923dabc1bc8ff127dd6c570742185751"),
-]
-
-script = raw"""
-cd ${WORKSPACE}/srcdir/rocm-cmake*/
-
-mkdir build && cd build
-cmake \
-    -DCMAKE_INSTALL_PREFIX=${prefix} \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    ..
-
-make -j${nproc}
-make install
-"""
-
-platforms = [Platform("x86_64", "linux"; libc="glibc", cxxstring_abi="cxx11")]
-products = [FileProduct("share/rocm/cmake", :cmake_dir)]
-dependencies = []
-
+include("../common.jl")
 build_tarballs(
-    ARGS, name, version, sources, script, platforms, products, dependencies;
+    ARGS, configure_build(v"4.5.2")...;
     preferred_gcc_version=v"7", preferred_llvm_version=v"9", julia_compat="1.8")
