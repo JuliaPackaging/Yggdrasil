d7d5cf906a2529827143fe3e75cdbe5a30fc3998
Keno Fischer <keno@juliacomputing.com>
2022-08-16 12:34:26 -0400

    FlangClassic: Split Compiler and RTLib (#5316)

---
+++ F/FlangClassic/FlangClassic/build_tarballs.jl
@@ -0,0 +1,20 @@
+name = "FlangClassic"
+version = v"13.0.0"
+
+include("../common.jl")
+
+# The products that we will ensure are always built
+products = Product[
+    ExecutableProduct("flang", :flang),
+    ExecutableProduct("flang1", :flang1),
+    ExecutableProduct("flang2", :flang2)
+]
+
+# We only build the compiler for musl. The rtlibs get built for all platforms in the RTLib jll
+platforms = [
+    Platform("x86_64", "linux"; libc = "musl"),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, flang_script(true), platforms, products, dependencies;
+    julia_compat="1.6", preferred_gcc_version=v"10", lock_microarchitecture=false)
