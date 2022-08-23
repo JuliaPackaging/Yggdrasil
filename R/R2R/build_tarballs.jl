dbb69c22361ea0a1598ac89743f0ce5ffcf55db7
Marco Matthies <marco.matthies@gmail.com>
2022-08-23 14:36:59 +0200

    new package R2R-1.0.6

---
+++ R/R2R/build_tarballs.jl
@@ -0,0 +1,53 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "R2R"
+version = v"1.0.6"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://sourceforge.net/projects/weinberg-r2r/files/R2R-1.0.6.tgz",
+                  "1ba8f51db92866ebe1aeb3c056f17489bceefe2f67c5c0bbdfbddc0eee17743d"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/R2R-*/
+
+update_configure_scripts
+./configure \
+    --prefix=${prefix} --build=${MACHTYPE} --host=${target} \
+    --enable-nlopt
+
+make -j${nproc}
+make install
+
+# install manual
+# mkdir "${prefix}/doc"
+# cp R2R-manual.pdf "${prefix}/doc"
+
+# install demos
+# cp -r demo/ "${prefix}"
+
+install_license r2r-gpl-text.txt
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+platforms = expand_cxxstring_abis(platforms)
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("r2r", :r2r),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="NLopt_jll", uuid="079eb43e-fd8e-5478-9966-2cf3e3edb778")),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6")
