9e75982d9461b33a09fed1d836d9643b2fb5a98a
Maarten Pronk <evetion@users.noreply.github.com>
2022-08-24 14:08:28 +0200

    No experimental platforms

--- L/LASzip/build_tarballs.jl
+++ L/LASzip/build_tarballs.jl
@@ -28,7 +28,7 @@ cmake --build . --target install --config Release
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(experimental=false)
 
 
 # The products that we will ensure are always built
