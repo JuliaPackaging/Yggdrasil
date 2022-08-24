29d854b7dc0fe7ab045f80f5b4fe073ee8460379
Alex Arslan <ararslan@comcast.net>
2022-08-24 10:48:21 -0700

    add Julia compat

--- Z/ZipFlow/build_tarballs.jl
+++ Z/ZipFlow/build_tarballs.jl
@@ -19,4 +19,5 @@ products = [LibraryProduct("libzipflow", :libzipflow)]
 
 dependencies = [Dependency("Zlib_jll")]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6")
