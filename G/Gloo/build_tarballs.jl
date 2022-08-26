2a2ce8fed126e26a928604446e183a0c2a5289ce
Jesper Stemann Andersen <jsa@ihp.dk>
2022-08-26 08:23:39 +0200

    Added mingw32 patch to link with ws2_32

--- G/Gloo/build_tarballs.jl
+++ G/Gloo/build_tarballs.jl
@@ -16,6 +16,7 @@ script = raw"""
 cd $WORKSPACE/srcdir/gloo
 atomic_patch -p1 ../patches/mingw32.patch
 atomic_patch -p1 ../patches/mingw-lowercase-include.patch
+atomic_patch -p1 ../patches/mingw32-link-with-ws2_32.patch
 atomic_patch -p1 ../patches/musl-caddr.patch
 mkdir build
 cd build
