51e0d0637f1a02902bbbcc2ca47ab5a2fee2d525
Alexis Montoison <alexis.montoison@polymtl.ca>
2022-08-31 18:19:25 -0400

    Recompile MUMPS 5.5.1

--- M/MUMPS/MUMPS_seq@5/build_tarballs.jl
+++ M/MUMPS/MUMPS_seq@5/build_tarballs.jl
@@ -14,88 +14,19 @@ using BinaryBuilder, Pkg
 # map a prerelease of 2.7.0 to 200.690.000.
 
 name = "MUMPS_seq"
-upstream_version = v"5.4.1"
+upstream_version = v"5.5.1"
 version_offset = v"0.0.0" # reset to 0.0.0 once the upstream version changes
 version = VersionNumber(upstream_version.major * 100 + version_offset.major,
                         upstream_version.minor * 100 + version_offset.minor,
                         upstream_version.patch * 100 + version_offset.patch)
 upstream_version
 sources = [
-  ArchiveSource("https://graal.ens-lyon.fr/MUMPS/MUMPS_$(upstream_version).tar.gz","93034a1a9fe0876307136dcde7e98e9086e199de76f1c47da822e7d4de987fa8"), # v5.4.1
-  # ArchiveSource("https://graal.ens-lyon.fr/MUMPS/MUMPS_$(upstream_version).tar.gz","1abff294fa47ee4cfd50dfd5c595942b72ebfcedce08142a75a99ab35014fa15"), # v5.5.1
+  ArchiveSource("http://mumps.enseeiht.fr/MUMPS_$(upstream_version).tar.gz","1abff294fa47ee4cfd50dfd5c595942b72ebfcedce08142a75a99ab35014fa15"),
   DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-mkdir -p ${libdir}
-cd $WORKSPACE/srcdir/MUMPS*
-atomic_patch -p1 ${WORKSPACE}/srcdir/patches/mumps_int32.patch
-
-makefile="Makefile.G95.SEQ"
-cp Make.inc/${makefile} Makefile.inc
-
-if [[ "${target}" == aarch64-apple-darwin* ]]; then
-    # Fix the error:
-    #     Type mismatch in argument ‘s’ at (1); passed INTEGER(4) to LOGICAL(4)
-    FFLAGS=("-fallow-argument-mismatch")
-fi
-
-make_args+=(OPTF=-O3
-            OPTL=-O3
-            OPTC=-O3
-            CDEFS=-DAdd_
-            LMETISDIR=${libdir}
-            IMETIS=-I${includedir}
-            LMETIS="-L${libdir} -lmetis"
-            ORDERINGSF="-Dpord -Dmetis"
-            CC="$CC -fPIC ${CFLAGS[@]}"
-            FC="gfortran -fPIC ${FFLAGS[@]}"
-            FL="gfortran -fPIC"
-            RANLIB="echo"
-            LIBBLAS="-L${libdir} -lblastrampoline"
-            LAPACK="-L${libdir} -lblastrampoline")
-
-# NB: parallel build fails
-make all "${make_args[@]}"
-
-# build shared libs
-all_load="--whole-archive"
-noall_load="--no-whole-archive"
-extra=""
-if [[ "${target}" == *-apple-* ]]; then
-    all_load="-all_load"
-    noall_load="-noall_load"
-    extra="-Wl,-undefined -Wl,dynamic_lookup -headerpad_max_install_names"
-fi
-
-cd libseq
-gfortran -fPIC -shared -Wl,${all_load} libmpiseq.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o libmpiseq.${dlext}
-cp libmpiseq.${dlext} ${libdir}
-
-cd ../lib
-libs=(-L${libdir} -lmetis -lblastrampoline -lmpiseq)
-gfortran -fPIC -shared -Wl,${all_load} libpord.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o libpord.${dlext}
-cp libpord.${dlext} ${libdir}
-
-libs+=(-lpord)
-gfortran -fPIC -shared -Wl,${all_load} libmumps_common.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o libmumps_common.${dlext}
-cp libmumps_common.${dlext} ${libdir}
-
-libs+=(-lmumps_common)
-for libname in cmumps dmumps smumps zmumps
-do
-  gfortran -fPIC -shared -Wl,${all_load} lib${libname}.a ${libs[@]} -Wl,${noall_load} ${extra[@]} -o lib${libname}.${dlext}
-done
-cp *.${dlext} ${libdir}
-cd ..
-
-cp include/*.h ${includedir}
-cp libseq/*.h ${includedir}
-"""
-
-# Recipe for v5.5.1
-script2 = raw"""
 mkdir -p ${libdir}
 cd $WORKSPACE/srcdir/MUMPS*
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/pord.patch
@@ -134,7 +65,8 @@ make_args+=(OPTF=-O3
             LIBBLAS="-L${libdir} -lblastrampoline"
             LAPACK="-L${libdir} -lblastrampoline")
 
-make -j${nproc} allshared "${make_args[@]}"
+# Add again the option -j${nproc} if the issue with LBT is solved
+make allshared "${make_args[@]}"
 
 cp include/*.h ${includedir}
 cp libseq/*.h ${includedir}
@@ -156,9 +88,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("CompilerSupportLibraries_jll"),
-    Dependency("METIS_jll"),
-    Dependency("libblastrampoline_jll")
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+    Dependency(PackageSpec(name="METIS_jll", uuid="d00139f3-1899-568f-a2f0-47f597d42d70")),
+    Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"))
 ]
 
 # Build the tarballs
