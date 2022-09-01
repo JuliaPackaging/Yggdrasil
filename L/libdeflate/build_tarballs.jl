999d660ec7cefe1ecf9cc46ebfd47a6876822af3
Mosè Giordano <giordano@users.noreply.github.com>
2022-09-01 09:59:40 +0100

    Update build_tarballs.jl

--- L/libdeflate/build_tarballs.jl
+++ L/libdeflate/build_tarballs.jl
@@ -3,18 +3,18 @@
 using BinaryBuilder, Pkg
 
 name = "libdeflate"
-version = v"1.13.0"
+version_string = "1.13"
+version = VersionNumber(version_string)
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/ebiggers/libdeflate/archive/refs/tags/v1.13.tar.gz", 
+    ArchiveSource("https://github.com/ebiggers/libdeflate/archive/refs/tags/v$(version_string).tar.gz", 
                   "0d81f197dc31dc4ef7b6198fde570f4e8653c77f4698fcb2163d820a9607c838"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd libdeflate-*/
+cd $WORKSPACE/srcdir/libdeflate-*/
 make PROG_SUFFIX=$exeext PREFIX=${prefix} LIBDIR=${libdir} DISABLE_ZLIB=true
 make PROG_SUFFIX=$exeext PREFIX=${prefix} LIBDIR=${libdir} install
 """
