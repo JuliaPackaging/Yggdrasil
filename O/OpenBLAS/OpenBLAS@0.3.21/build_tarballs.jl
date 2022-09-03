6a5468852bb2be92d7239c4ebcc77a0823576513
Mosè Giordano <mose@gnu.org>
2022-09-03 11:19:41 +0100

    [OpenBLAS] Need to push the new platform before using `platforms`

--- O/OpenBLAS/OpenBLAS@0.3.21/build_tarballs.jl
+++ O/OpenBLAS/OpenBLAS@0.3.21/build_tarballs.jl
@@ -9,10 +9,9 @@ version = v"0.3.21"
 sources = openblas_sources(version)
 script = openblas_script(;aarch64_ilp64=true, num_64bit_threads=512)
 platforms = openblas_platforms(;experimental=true)
+push!(platforms, Platform("x86_64", "linux"; sanitize="memory"))
 products = openblas_products()
 dependencies = openblas_dependencies(platforms)
-
-push!(platforms, Platform("x86_64", "linux"; sanitize="memory"))
 
 # Build the tarballs
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
