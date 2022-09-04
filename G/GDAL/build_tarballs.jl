ddcfbab1ca429222d2573407693b373ddff40bfd
Martijn Visser <mgvisser@gmail.com>
2022-09-04 10:13:06 +0200

    Revert "Add postgres support"
    It seems to find the wrong postgres:
    
    ```
    [07:34:13] In file included from /workspace/srcdir/gdal-3.5.1/ogr/ogrsf_frmts/pg/ogr_pg.h:35,
    [07:34:13]                  from /workspace/srcdir/gdal-3.5.1/ogr/ogrsf_frmts/pg/ogrpgdatasource.cpp:31:
    [07:34:13] /opt/aarch64-linux-gnu/aarch64-linux-gnu/sys-root/usr/local/include/libpq-fe.h:29:10: fatal error: postgres_ext.h: No such file or directory
    [07:34:13]  #include "postgres_ext.h"
    [07:34:13]           ^~~~~~~~~~~~~~~~
    ```
    
    This reverts commit da9909dc7466078d8553d9798d0538a1d54849f0.

--- G/GDAL/build_tarballs.jl
+++ G/GDAL/build_tarballs.jl
@@ -34,7 +34,6 @@ cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DGDAL_USE_GEOTIFF=ON \
     -DGDAL_USE_GEOS=ON \
     -DGDAL_USE_OPENJPEG=ON \
-    -DGDAL_USE_POSTGRESQL=ON \
     -DGDAL_USE_SQLITE3=ON \
     -DGDAL_USE_TIFF=ON \
     -DGDAL_USE_ZLIB=ON \
@@ -85,7 +84,6 @@ dependencies = [
     Dependency("Libtiff_jll"; compat="4.3"),
     Dependency("libgeotiff_jll"; compat="100.700.100"),
     Dependency("LibCURL_jll"; compat="7.73"),
-    Dependency("LibPQ_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
