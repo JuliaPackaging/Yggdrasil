5882988519a7eb1ca7483c4021d791d7c8165aad
Maarten Pronk <evetion@users.noreply.github.com>
2022-09-04 18:23:49 +0200

    Bump

--- M/MDAL/build_tarballs.jl
+++ M/MDAL/build_tarballs.jl
@@ -56,13 +56,11 @@ fi
 cmake . ${CMAKE_FLAGS[@]}
 make -j${nproc}
 make install
-
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
-
 
 # The products that we will ensure are always built
 products = [
