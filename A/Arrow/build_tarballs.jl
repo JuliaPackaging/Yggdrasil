00ec791828bc0e2f4ac00b112e0ca689f267aba9
Maarten Pronk <git@evetion.nl>
2022-09-05 23:15:00 +0200

    Let's try a hack.

--- A/Arrow/build_tarballs.jl
+++ A/Arrow/build_tarballs.jl
@@ -8,12 +8,20 @@ version = v"9.0.0"
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://github.com/apache/arrow/archive/refs/tags/apache-arrow-9.0.0.zip", "fb2469e9bfeb3e9a45f1e3086f536a329d59a0efd3c905436d3b6e8dd030be41")
+    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 
-cd $WORKSPACE/srcdir/arrow-apache-arrow-*/cpp
+cd $WORKSPACE/srcdir/arrow-apache-arrow-*
+
+# Set toolchain for building external deps
+for f in ${WORKSPACE}/srcdir/patches/*.patch; do
+    atomic_patch -p1 ${f}
+done
+
+cd cpp && mkdir build_dir && cd build_dir
 
 CMAKE_FLAGS=(-DCMAKE_INSTALL_PREFIX=$prefix
 -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}
@@ -29,9 +37,20 @@ CMAKE_FLAGS=(-DCMAKE_INSTALL_PREFIX=$prefix
 -DARROW_WITH_SNAPPY=ON
 -DARROW_JEMALLOC_USE_SHARED=ON
 -DARROW_PARQUET=ON
-)
+-DARROW_SIMD_LEVEL=NONE
+-DARROW_USE_XSIMD=OFF
+-Dxsimd_SOURCE=AUTO
+-Dre2_SOURCE=AUTO)
 
-cmake . ${CMAKE_FLAGS[@]}
+echo $CMAKE_FLAGS
+
+# CMake is doubling the suffixes...
+if [[ "${target}" == *-mingw32 ]]; then
+    ln -s ${prefix}/lib/libthrift.dll.a ${prefix}/lib/libthrift.a.dll.a
+    ln -s ${prefix}/lib/libutf8proc.a ${prefix}/lib/libutf8proc.dll.a.a
+fi
+
+cmake .. "${CMAKE_FLAGS[@]}"
 
 make -j${nproc}
 make install
@@ -57,8 +76,8 @@ dependencies = [
     Dependency("jemalloc_jll")
     Dependency("Thrift_jll")
     Dependency("snappy_jll")
+    Dependency("utf8proc_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
-b
