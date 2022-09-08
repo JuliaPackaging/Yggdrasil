7a6f3bd3083b1e8eaf0081837a70ee6b2df447e0
Boris Kaus <kaus@uni-mainz.de>
2022-09-08 11:46:53 +0200

    bugfix, which caused the wrong executable to be generated

--- M/MAGEMin/build_tarballs.jl
+++ M/MAGEMin/build_tarballs.jl
@@ -10,9 +10,9 @@ version = v"1.2.4"
 
 # Collection of sources required to complete build
 sources = [GitSource("https://github.com/ComputationalThermodynamics/MAGEMin", 
-                    "5b7b999dd12ed28c0a6d18af70777a8170724185")
+                    "ac3786e88aeb190ad6388ffdf3552bfc7bbdcaf8")
         ]
-
+        
 # Bash recipe for building across all platforms
 script = raw"""
 cd MAGEMin*
@@ -38,7 +38,7 @@ make -j${nproc} CC="${CC}" CCFLAGS="${CCFLAGS}" LIBS="${LIBS}" INC="${INC}" lib
 make -j${nproc} CC="${CC}" CCFLAGS="${CCFLAGS}" LIBS="${LIBS}" INC="${INC}" all
 
 install -Dvm 755 libMAGEMin.dylib "${libdir}/libMAGEMin.${dlext}"
-install -Dvm 755 MAGEMin* "${bindir}/MAGEMin${exeext}"
+install -Dvm 755 MAGEMin${exeext} "${bindir}/MAGEMin${exeext}"
 
 # store files
 install -vm 644 src/*.h "${includedir}"
