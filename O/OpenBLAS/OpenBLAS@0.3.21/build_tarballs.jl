f1c0a9db59c1b41b1728a8d293b342bdf646bc0d
Gabriel Baraldi <baraldigabriel@gmail.com>
2022-09-09 17:57:22 -0300

    Julia expects a libgfortran version so add it

--- O/OpenBLAS/OpenBLAS@0.3.21/build_tarballs.jl
+++ O/OpenBLAS/OpenBLAS@0.3.21/build_tarballs.jl
@@ -9,7 +9,7 @@ version = v"0.3.21"
 sources = openblas_sources(version)
 script = openblas_script(;aarch64_ilp64=true, num_64bit_threads=512)
 platforms = openblas_platforms(;experimental=true)
-push!(platforms, Platform("x86_64", "linux"; sanitize="memory"))
+push!(platforms, Platform("x86_64", "linux"; sanitize="memory", ibgfortran_version=v"5.0.0"))
 products = openblas_products()
 dependencies = openblas_dependencies(platforms)
 
