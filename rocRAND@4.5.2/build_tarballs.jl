da4c490f320d95bd5007190f13a54f117a454766
Anton Smirnov <tonysmn97@gmail.com>
2022-09-09 14:58:54 +0300

    Remove 4.5.2 for now

--- rocRAND@4.5.2/build_tarballs.jl
+++
@@ -1,7 +0,0 @@
-using Pkg
-using BinaryBuilder
-
-include("../common.jl")
-build_tarballs(
-    ARGS, configure_build(v"4.5.2")...;
-    preferred_gcc_version=v"7", preferred_llvm_version=v"9", julia_compat="1.8")
