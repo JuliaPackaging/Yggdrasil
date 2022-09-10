ab0e0ce11c3d7bc82ae89872df229363894fff70
cmhamel <cmhamel32@gmail.com>
2022-09-10 13:10:38 -0400

    adding a script to look around

--- E/Exodus/build_tarballs.jl
+++ E/Exodus/build_tarballs.jl
@@ -13,6 +13,19 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
+SEARCH_PATH=$PATH  # or any colon-delimited list of paths
+IFS=':' read -r -a PathDirs <<< "$SEARCH_PATH"
+echo "##[debug] Found directories"
+for element in "${PathDirs[@]}"; do
+    echo "$element"
+done;
+echo;
+echo;  
+echo "##[debug] Found files"
+for element in "${PathDirs[@]}"; do
+    find "$element" -type f
+done
+
 # install TPLs first
 #
 cd $WORKSPACE/srcdir/seacas && ACCESS=`pwd`
