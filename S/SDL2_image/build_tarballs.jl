2de22c5c340b5a7a8a6df63dd78336bd366f9057
Mosè Giordano <mose@gnu.org>
2022-09-16 19:18:24 +0100

    [SDL2_image] More modern recipe

--- S/SDL2_image/build_tarballs.jl
+++ S/SDL2_image/build_tarballs.jl
@@ -3,18 +3,17 @@
 using BinaryBuilder, Pkg
 
 name = "SDL2_image"
-version = v"2.0.5"
+version = v"2.6.2"
 
 # Collection of sources required to complete build
 sources = [
-    "https://www.libsdl.org/projects/SDL_image/release/SDL2_image-2.6.2.zip" =>
-    "efe3c229853d0d40c35e5a34c3f532d5d9728f0abc623bc62c962bcef8754205",
+    ArchiveSource("https://www.libsdl.org/projects/SDL_image/release/SDL2_image-$(version).zip",
+                  "efe3c229853d0d40c35e5a34c3f532d5d9728f0abc623bc62c962bcef8754205"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/SDL2_image-*/
-export CPPFLAGS="-I${prefix}/include"
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-pic
 make -j${nproc}
 make install
@@ -30,7 +29,6 @@ fi
 # platforms are passed in on the command line
 platforms = supported_platforms()
 
-
 # The products that we will ensure are always built
 products = [
     LibraryProduct(["libSDL2_image", "SDL2_image"], :libsdl2_image)
@@ -38,11 +36,11 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    PackageSpec(name="SDL2_jll", uuid="ab825dc5-c88e-5901-9575-1e5e20358fcf")
-    PackageSpec(name="libwebp_jll", uuid="c5f90fcd-3b7e-5836-afba-fc50a0988cb2")
-    PackageSpec(name="JpegTurbo_jll", uuid="aacddb02-875f-59d6-b918-886e6ef4fbf8")
-    PackageSpec(name="libpng_jll", uuid="b53b4c65-9356-5827-b1ea-8c7a1a84506f")
-    PackageSpec(name="Libtiff_jll", uuid="89763e89-9b03-5906-acba-b20f662cd828")
+    Dependency(PackageSpec(name="SDL2_jll", uuid="ab825dc5-c88e-5901-9575-1e5e20358fcf"))
+    Dependency(PackageSpec(name="libwebp_jll", uuid="c5f90fcd-3b7e-5836-afba-fc50a0988cb2"))
+    Dependency(PackageSpec(name="JpegTurbo_jll", uuid="aacddb02-875f-59d6-b918-886e6ef4fbf8"))
+    Dependency(PackageSpec(name="libpng_jll", uuid="b53b4c65-9356-5827-b1ea-8c7a1a84506f"))
+    Dependency(PackageSpec(name="Libtiff_jll", uuid="89763e89-9b03-5906-acba-b20f662cd828"))
 ]
 
 # Build the tarballs.
