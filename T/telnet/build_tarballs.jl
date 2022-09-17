e1ecacf9868917936da73c666feb44200796fa80
Mosè Giordano <mose@gnu.org>
2022-09-18 00:14:40 +0100

    [telnet] Use `libxcrypt_legacy_jll` to provide `libcrypt.so.1`

--- T/telnet/build_tarballs.jl
+++ T/telnet/build_tarballs.jl
@@ -14,20 +14,6 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/inetutils-*
-# Remove OpenSSL from the sysroot to avoid confusion
-rm -f /opt/${target}/${target}/sys-root/usr/lib/libcrypto.*
-rm -f /opt/${target}/${target}/sys-root/usr/lib/libssl.*
-rm -f /lib/libcrypto.so*
-rm -f /usr/lib/libcrypto.so*
-
-conf_args=()
-if [[ "${target}" == *-linux-gnu* ]]; then
-    # We use very old versions of glibc which used to have `libcrypt.so.1`, but modern
-    # glibcs have `libcrypt.so.2`, so if we link to `libcrypt.so.1` most users would
-    # have troubles running the programs at runtime.
-    conf_args+=(ac_cv_lib_crypt_crypt=no)
-fi
-
 ./configure --prefix=${prefix} \
     --build=${MACHTYPE} \
     --host=${target} \
@@ -62,7 +48,7 @@ products = [
 dependencies = [
     Dependency(PackageSpec(name="Ncurses_jll", uuid="68e3532b-a499-55ff-9963-d1c0c0748b3a"))
     Dependency(PackageSpec(name="Readline_jll", uuid="05236dd9-4125-5232-aa7c-9ec0c9b2c25a"))
-    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95")) 
+    Dependency(PackageSpec(name="libxcrypt_legacy_jll", uuid="5ef642bb-a58b-5208-ae37-583168b2c491"); platforms=filter(p -> libc(p) == "glibc", platforms))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
