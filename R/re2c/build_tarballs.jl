712f0ddcf837266cf0c3b22baf4cafb5227ad322
Zihua Wu <qqbbnease1004@126.com>
2022-09-17 15:54:07 +0800

    Update build_tarballs.jl

--- R/re2c/build_tarballs.jl
+++ R/re2c/build_tarballs.jl
@@ -21,7 +21,7 @@ install_license LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
@@ -32,6 +32,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
