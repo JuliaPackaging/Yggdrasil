8579c850ae5cf062b4d2d42a0bafd0e380f3c1dc
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-09-19 14:18:42 +0200

    Update build_tarballs.jl

--- S/squashfs_tools/build_tarballs.jl
+++ S/squashfs_tools/build_tarballs.jl
@@ -19,8 +19,7 @@ cp mksquashfs unsquashfs ${bindir}
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-# platforms = filter(p -> Sys.islinux(p) && libc(p) == "glibc", supported_platforms())
+platforms = filter(p -> Sys.islinux(p) && libc(p) == "glibc", supported_platforms())
 
 
 # The products that we will ensure are always built
