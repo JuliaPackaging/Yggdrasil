ae48addcf44c73e53f78dd27a27f4bc71e1c9128
Zihua Wu <qqbbnease1004@126.com>
2022-09-23 23:16:41 +0800

    Update D/DynamO/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- D/DynamO/build_tarballs.jl
+++ D/DynamO/build_tarballs.jl
@@ -20,14 +20,7 @@ install_license DynamO-*/LICENSE.txt
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc")
-]
+platforms = supported_platforms(; exclude=p -> libc(p) != "glibc")
 
 platforms = expand_cxxstring_abis(platforms)
 
