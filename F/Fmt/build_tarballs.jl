8b35a611a4fb703ba9977b1b3b0b319e90078fc8
cmhamel <cmhamel32@gmail.com>
2022-09-23 18:02:17 -0400

    adding build for fmt

---
+++ F/Fmt/build_tarballs.jl
@@ -0,0 +1,58 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Fmt"
+version = v"9.1.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/fmtlib/fmt.git", "a33701196adfad74917046096bf5a2aa0ab0bb50")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+INSTALL_PATH=${prefix}
+cd $WORKSPACE/srcdir/fmt
+mkdir build
+cd build
+cmake \
+-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+-DBUILD_SHARED_LIBS=True \
+-DCMAKE_INSTALL_PREFIX=${INSTALL_PATH} \
+..
+make
+make install
+
+# the license file isn't found automatically for some reason
+# perhaps it's the .rst extension?
+#
+mkdir ${prefix}/share
+mkdir ${prefix}/share/licenses
+mkdir ${prefix}/share/licenses/Fmt
+cp $WORKSPACE/srcdir/fmt/LICENSE.rst ${prefix}/share/licenses/Fmt/LICENSE
+"""
+
+platforms = [
+    Platform("x86_64", "linux"; libc = "glibc"),
+    Platform("aarch64", "linux"; libc = "glibc"),
+    Platform("x86_64", "macOs"),
+    Platform("aarch64", "macOs"),
+    Platform("x86_64", "windows"),
+    Platform("i686", "windows")
+]
+
+platforms = expand_cxxstring_abis(platforms)
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libfmt", :libfmt),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+
