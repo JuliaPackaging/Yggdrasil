a78fb4151d4a999c741df1d8e9173584e113df33
Mosè Giordano <giordano@users.noreply.github.com>
2022-09-24 13:31:42 +0100

    Update X/xapian/build_tarballs.jl

--- X/xapian/build_tarballs.jl
+++ X/xapian/build_tarballs.jl
@@ -21,17 +21,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("x86_64", "macos"; ),
-    Platform("aarch64", "macos"; ),
-    Platform("x86_64", "freebsd"; )
-]
+platforms = supported_platforms(; exclude=p -> Sys.iswindows(p) || libc(p) == "musl")
 
 platforms = expand_cxxstring_abis(platforms)
 
