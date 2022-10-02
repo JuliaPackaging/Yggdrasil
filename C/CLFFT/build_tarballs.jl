0a40a752cbd5451055aad9444e8b3b0b290ad2c8
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-10-02 15:32:41 -0300

    Update C/CLFFT/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- C/CLFFT/build_tarballs.jl
+++ C/CLFFT/build_tarballs.jl
@@ -23,7 +23,7 @@ cmake --build ./clFFT/build --target install -j${nproc}
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
-
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
