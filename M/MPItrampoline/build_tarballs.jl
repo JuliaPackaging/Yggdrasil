52bd707bc7b63408deff07b85584e0b35a1aead1
Valentin Churavy <v.churavy@gmail.com>
2022-10-03 10:24:49 -0400

    Rebuild MPItrampoline with MPIPreferences as a top-level dependency

--- M/MPItrampoline/build_tarballs.jl
+++ M/MPItrampoline/build_tarballs.jl
@@ -264,7 +264,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"), v"0.5.2"),
-    Dependency(PackageSpec(name="MPIPreferences", uuid="3da0fdf6-3ccc-4f1b-acd9-58baa6c99267"); compat="0.1"),
+    Dependency(PackageSpec(name="MPIPreferences", uuid="3da0fdf6-3ccc-4f1b-acd9-58baa6c99267"); compat="0.1", top_level=true),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
