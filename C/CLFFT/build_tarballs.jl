bc4dee270037cd60fe1c7670a94dc9ad5282f781
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-10-05 20:09:50 -0300

    Update C/CLFFT/build_tarballs.jl

--- C/CLFFT/build_tarballs.jl
+++ C/CLFFT/build_tarballs.jl
@@ -46,9 +46,9 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     BuildDependency(PackageSpec(; name="OpenCL_Headers_jll", version=v"2022.09.23")),
-    BuildDependency(PackageSpec(; name="OpenCL_jll", version=v"2022.09.23")),
-    BuildDependency(PackageSpec(; name="FFTW_jll", version=v"3.3.10")),
-    BuildDependency(PackageSpec(; name="boost_jll", version=v"1.76.0"))
+    Dependency(PackageSpec(; name="OpenCL_jll", version=v"2022.09.23")),
+    Dependency(PackageSpec(; name="FFTW_jll", version=v"3.3.10")),
+    Dependency(PackageSpec(; name="boost_jll", version=v"1.76.0"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
