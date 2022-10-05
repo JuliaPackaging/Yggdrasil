a253ed74febbbc6c3dbb024941dbda6fc7e6cf24
Júlio Hoffimann <julio.hoffimann@gmail.com>
2022-10-05 14:37:59 -0300

    Apply suggestions from code review

--- C/CLFFT/build_tarballs.jl
+++ C/CLFFT/build_tarballs.jl
@@ -7,7 +7,9 @@ version = v"2.12.2"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/clMathLibraries/clFFT.git", "1e4833f060976971c4df4b54b1b9ad1620aaf1fb")
+    GitSource("https://github.com/clMathLibraries/clFFT.git", "1e4833f060976971c4df4b54b1b9ad1620aaf1fb"),
+    FileSource("https://patch-diff.githubusercontent.com/raw/clMathLibraries/clFFT/pull/245.patch",
+               "b78e80258bd5dd41572dfad8f4c8fe3b0192d7d4f3bdaa85aae2d59f61184a88")
 ]
 
 # Bash recipe for building across all platforms
@@ -16,13 +18,24 @@ cd $WORKSPACE/srcdir
 
 install_license ./clFFT/LICENSE
 
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -S ./clFFT/src -B ./clFFT/build
+patch ./clFFT/src/CMakeLists.txt 245.patch
+
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_EXAMPLES=OFF -S ./clFFT/src -B ./clFFT/build
 cmake --build ./clFFT/build --target install -j${nproc}
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = [
+    Platform("x86_64", "linux"; libc = "glibc"),
+    Platform("aarch64", "linux"; libc = "glibc"),
+    Platform("powerpc64le", "linux"; libc = "glibc"),
+    Platform("x86_64", "linux"; libc = "musl"),
+    Platform("aarch64", "linux"; libc = "musl"),
+    Platform("aarch64", "macos"),
+    Platform("x86_64", "macos")
+]
+
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
