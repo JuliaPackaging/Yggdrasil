eb29d834be76bbc849214b159c6b787e768c3e07
zxygentoo <zxygentoo@users.noreply.github.com>
2022-10-07 16:19:16 +0800

    Update for cjieba
    - Use new commit from author with explicit license.
    - Some suggested change for build script from Yggdrasil maintainer.
    - Rename some FileProduct.

--- C/CJieba/build_tarballs.jl
+++ C/CJieba/build_tarballs.jl
@@ -4,20 +4,17 @@ name = "CJieba"
 version = v"0.3.0"
 
 sources = [
-    ArchiveSource(
-        "https://github.com/yanyiwu/cjieba/archive/refs/tags/v$(version).tar.gz",
-        "1f5ca82c5d19c38485c8fd50db8711602d339b1e85f15fb864fc116c7f770f12"
-    )
+    GitSource("https://github.com/yanyiwu/cjieba.git",
+              "1db462d33255aff08802c248b6d6e59202b5619e"),
 ]
 
 script = raw"""
 cd $WORKSPACE/srcdir
 mkdir $libdir $prefix/share
-curl https://raw.githubusercontent.com/yanyiwu/cppjieba/master/LICENSE > LICENSE
+cd cjieba
 install_license LICENSE 
-cd cjieba-0.3.0/
-g++ -o "$libdir/libcjieba.$dlext" lib/jieba.cpp -shared -fPIC -I./deps/ -std=c++11
-cp -a dict $prefix/share/
+c++ -o "$libdir/libcjieba.$dlext" lib/jieba.cpp -shared -fPIC -I./deps/ -std=c++11
+cp -va dict $prefix/share/
 """
 
 platforms = supported_platforms()
@@ -27,9 +24,9 @@ products = [
     LibraryProduct("libcjieba", :libcjieba),
     FileProduct("share/dict/hmm_model.utf8", :hmm_model),
     FileProduct("share/dict/idf.utf8", :idf),
-    FileProduct("share/dict/jieba.dict.utf8", :jiebadict),
-    FileProduct("share/dict/stop_words.utf8", :stopwords),
-    FileProduct("share/dict/user.dict.utf8", :userdict)
+    FileProduct("share/dict/jieba.dict.utf8", :jieba_dict),
+    FileProduct("share/dict/stop_words.utf8", :stop_words),
+    FileProduct("share/dict/user.dict.utf8", :user_dict)
 ]
 
 dependencies = Dependency[
