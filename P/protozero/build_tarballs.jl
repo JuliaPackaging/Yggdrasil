7c9f3ca3466b037ef491952a2bb58d6db5f46c4b
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-10-09 21:05:14 +0200

    Drop flag

--- P/protozero/build_tarballs.jl
+++ P/protozero/build_tarballs.jl
@@ -19,8 +19,7 @@ mkdir build && cd build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_TESTS=OFF
+    -DCMAKE_BUILD_TYPE=Release
 
 make -j${nproc}
 make install
