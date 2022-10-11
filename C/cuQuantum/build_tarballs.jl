7a049ce07c60b162a7098a8284e7a6313bd3ee0e
Tim Besard <tim.besard@gmail.com>
2022-10-11 11:50:00 +0200

    [CUDA] Upgrade cuQuantum to v22.07.1.14

--- C/cuQuantum/build_tarballs.jl
+++ C/cuQuantum/build_tarballs.jl
@@ -1,38 +1,40 @@
 using BinaryBuilder
 
+include("../../fancy_toys.jl")
+
 name = "cuQuantum"
-version_string = "0.1.0.30"
+version_string = "22.07.1.14"
 version = let
     maj, min, patch, extra = parse.(Int, split(version_string, '.'))
     VersionNumber(maj, min, patch * 100 + extra)
 end
 
-sources = [
-    ArchiveSource("https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-x86_64/cuquantum-linux-x86_64-0.1.0.30-archive.tar.xz",
-                  "8ad8e98f14275ffe0de02574be5c86224af1c657c41baf02c16440301ffe0aae";
-                  unpack_target="x86_64-linux-gnu"),
-    ArchiveSource("https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-sbsa/cuquantum-linux-sbsa-0.1.0.30-archive.tar.xz",
-                  "3dd04cf08f1323318e0e63a7e28bc904c426ced3367aca809fba7e7beef94063";
-                  unpack_target="aarch64-linux-gnu"),
-    ArchiveSource("https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-ppc64le/cuquantum-linux-ppc64le-0.1.0.30-archive.tar.xz",
-                  "0eb84eef619a1cfab5870fb585200a9869a197866685252e4ca8187322809554";
-                  unpack_target="powerpc64le-linux-gnu"),
-]
+platforms_and_sources = Dict(
+    Platform("x86_64", "linux") => [
+        ArchiveSource("https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-x86_64/cuquantum-linux-x86_64-$(version_string)-archive.tar.xz",
+                      "4c4931096498451593ad553b6cb7a107bb6d6cedea65c80d5376d0cfbb647f8e")],
+    Platform("powerpc64le", "linux") => [
+        ArchiveSource("https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-ppc64le/cuquantum-linux-ppc64le-$(version_string)-archive.tar.xz",
+                      "2d35dba3c739e8de51591bcd2d7edf1a3c9995850af6525ae08e9b5c3798cf9a")],
+    Platform("aarch64", "linux") => [
+        ArchiveSource("https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-sbsa/cuquantum-linux-sbsa-$(version_string)-archive.tar.xz",
+                      "0c8fb14bf8916170e15a2ae7cfa950e23af13c9cb0915bad88d754221ad60116")],
+)
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd ${WORKSPACE}/srcdir/${target}/cuquantum-*
+cd ${WORKSPACE}/srcdir/cuquantum-*
 
 mkdir -p ${prefix}
-cp -var lib/ include/ ${prefix}/.
-cp -var pkg-config/ ${prefix}/lib/.
+cp -var lib/ include/ ${prefix}
+cp -var pkg-config/ ${prefix}/lib
 
 # Fixup pkg-config files
 sed -i \
     -e "s?^cudaroot=.*?cudaroot=${prefix}?" \
     -e "s?^libdir=.*?libdir=${libdir}?" \
     -e "s?^includedir=.*?includedir=${includedir}?" \
-    ${prefix}/lib/pkg-config/*.pc
+    ${libdir}/pkg-config/*.pc
 
 # Remove static libraries
 rm ${libdir}/*.a
@@ -41,14 +43,6 @@ rm ${libdir}/*.a
 install_license LICENSE docs/*
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = [
-    Platform("x86_64", "linux"; libc="glibc"),
-    Platform("aarch64", "linux"; libc="glibc"),
-    Platform("powerpc64le", "linux"; libc="glibc"),
-]
-
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libcustatevec", :libcustatevec; dont_dlopen=true),
@@ -56,8 +50,11 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
-]
+dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+for (platform, sources) in platforms_and_sources
+    should_build_platform(triplet(platform)) || continue
+    build_tarballs(ARGS, name, version, sources, script, [platform],
+                   products, dependencies; lazy_artifacts=true)
+end
