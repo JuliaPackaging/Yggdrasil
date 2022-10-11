95c31a12fa6651d196e352e1017db1b76a2b3ce3
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-10-11 13:26:27 +0200

    Fix posix_memalign issue with gcc floor

--- P/protozero/build_tarballs.jl
+++ P/protozero/build_tarballs.jl
@@ -37,4 +37,4 @@ products = Product[]
 dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"6")
