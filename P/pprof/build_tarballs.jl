d6716e2cb4716c31006345d8752c56ac9168770a
Valentin Churavy <vchuravy@users.noreply.github.com>
2022-10-11 10:24:46 -0400

    add Graphviz as dependency to PProf (#5652)
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- P/pprof/build_tarballs.jl
+++ P/pprof/build_tarballs.jl
@@ -32,7 +32,8 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
+dependencies = [
+    Dependency("Graphviz_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
