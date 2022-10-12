d20f527fc98c610f71ced9a44a2ae79b395fa6e0
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-10-12 19:41:13 +0000

    Header only

--- L/libosmium/build_tarballs.jl
+++ L/libosmium/build_tarballs.jl
@@ -24,10 +24,11 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = [AnyPlatform()]
 
 # The products that we will ensure are always built
-products = Product[
+products = [
+    FileProduct("include/osmium/version.hpp", :osmium_version),
 ]
 
 # Dependencies that must be installed before this package can be built
