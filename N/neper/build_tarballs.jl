5ed96bd535681278835e6d87367ffcbe6ab14889
Fredrik Ekre <ekrefredrik@gmail.com>
2022-10-14 09:56:15 +0200

    Use SCOTCH_jll

--- N/neper/build_tarballs.jl
+++ N/neper/build_tarballs.jl
@@ -10,7 +10,12 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/neper-*
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ../src
+CXXFLAGS="-lz"
+cmake -DCMAKE_INSTALL_PREFIX=$prefix \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DCMAKE_CXX_FLAGS="${CXXFLAGS}" \
+      ../src
 make -j${nproc}
 make install
 """
@@ -48,8 +53,7 @@ dependencies = [
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="GSL_jll", uuid="1b77fbbe-d8ee-58f0-85f9-836ddc23a7a4")),
     Dependency(PackageSpec(name="NLopt_jll", uuid="079eb43e-fd8e-5478-9966-2cf3e3edb778")),
-    # /opt/aarch64-linux-gnu/bin/../lib/gcc/aarch64-linux-gnu/12.1.0/../../../../aarch64-linux-gnu/bin/ld: /opt/aarch64-linux-gnu/aarch64-linux-gnu/sys-root/usr/local/lib/libscotch.so: undefined reference to `gzwrite'
-    # Dependency(PackageSpec(name="SCOTCH_jll", uuid="a8d0f55d-b80e-548d-aff6-1a04c175f0f9")),
+    Dependency(PackageSpec(name="SCOTCH_jll", uuid="a8d0f55d-b80e-548d-aff6-1a04c175f0f9")),
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"5")
