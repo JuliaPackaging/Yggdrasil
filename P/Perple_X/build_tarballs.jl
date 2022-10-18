594909de17e6dece00cc3d0de5eb4a7363736bc1
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2022-10-18 18:33:44 -0300

    Perple_X (#5694)
    * initial build file
    
    * deal with windows extensions
    
    * parallel compilation
    
    * Update P/Perple_X/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * build shared libraries as well; simplified build script
    
    * bugfix
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ P/Perple_X/build_tarballs.jl
@@ -0,0 +1,94 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Perple_X"
+version = v"6.9.1"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/jadconnolly/Perple_X.git", "b0eb132a34de94b768ba69c8244ceaf4efa698a7")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/Perple_X/sources/
+
+FilesArray=("vertex"  "build"  "actcor"  "convex" "ctransf" "fluids" "frendly" "meemum" "pspts" "pssect" "pstable" "psvdraw" "pt2curv" "werami")
+
+# 1) compile binaries
+make -j${nproc} -f makefile_691 EXT=${exeext}
+
+# deploy binaries & libraries
+for file in ${FilesArray[*]}; do
+    install -Dvm 755 $file${exeext} "${bindir}/$file${exeext}"
+done;
+
+# 2) compile shared libraries
+make -f makefile_691 clean
+make -j${nproc} -f makefile_691 EXT=.${dlext} FLINK='-shared' FFLAGS='-fPIC -O3'
+
+# deploy 
+for file in ${FilesArray[*]}; do
+    install -Dvm 755 $file.${dlext} "${libdir}/lib$file.${dlext}"
+done;
+
+install_license LICENSE
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+platforms = expand_gfortran_versions(platforms)
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("fluids", :fluids),
+    LibraryProduct("libfluids", :libfluids),
+    
+    ExecutableProduct("meemum", :meemum),
+    LibraryProduct("libmeemum", :libmeemum),
+    
+    ExecutableProduct("pt2curv", :pt2curv),
+    LibraryProduct("libpt2curv", :libpt2curv),
+    
+    ExecutableProduct("pspts", :pspts),
+    LibraryProduct("libpspts", :libpspts),
+
+    ExecutableProduct("actcor", :actcor),
+    LibraryProduct("libactcor", :libactcor),
+    
+    ExecutableProduct("ctransf", :ctransf),
+    LibraryProduct("libctransf", :libctransf),
+    
+    ExecutableProduct("frendly", :frendly),
+    LibraryProduct("libfrendly", :libfrendly),
+    
+    ExecutableProduct("vertex", :vertex),
+    LibraryProduct("libvertex", :libvertex),
+    
+    ExecutableProduct("build", :build),
+    LibraryProduct("libbuild", :libbuild),
+    
+    ExecutableProduct("pstable", :pstable),
+    LibraryProduct("libpstable", :libpstable),
+    
+    ExecutableProduct("psvdraw", :psvdraw),
+    LibraryProduct("libpsvdraw", :libpsvdraw),
+    
+    ExecutableProduct("pssect", :pssect),
+    LibraryProduct("libpssect", :libpssect),
+    
+    ExecutableProduct("werami", :werami),
+    LibraryProduct("libwerami", :libwerami),
+    
+    ExecutableProduct("convex", :convex),
+    LibraryProduct("libconvex", :libconvex)
+
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
