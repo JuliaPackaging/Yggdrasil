1f06f84508ce5c0dc1b8ecabc6103a29f6ef47d8
Tim Besard <tim.besard@gmail.com>
2022-10-19 16:25:02 +0200

    [CUDA] Support loading a runtime artifact without a driver when we have a preference. (#5712)

--- C/CUDA/CUDA_Runtime/platform_augmentation.jl
+++ C/CUDA/CUDA_Runtime/platform_augmentation.jl
@@ -5,17 +5,8 @@ using Base: thismajor, thisminor
 try
     using CUDA_Driver_jll
 catch err
-    # During package installation, Pkg installs all packages in parallel, so CUDA_Driver may
-    # not be available yet. In that case, postpone the decision until we are able to load
-    # the driver and accurately determine which CUDA Runtime to use.
-    #
-    # XXX: this is not a future-proof solution, as it shouldn't be possible to load any
-    # non-stdlib dependencies from a package hook at any point. A better solution would be
-    # to inline CUDA_Driver_jll here (while possibly renaming this artifact to CUDA_jll), so
-    # that we can use the driver artifact during package installation to determine which
-    # runtime artifacts to use. But that requires changes to BinaryBuilder so that we can
-    # bind multiple artifacts, as well as the guarantee that it should be possible to load
-    # lazy artifacts from package augmentation hooks (see JuliaLang/Pkg.jl#3225).
+    # during initial package installation, CUDA_Driver_jll may not be available.
+    # in that case, we just won't select an artifact (unless the user has set a preference).
 end
 
 # Can't use Preferences for the same reason
@@ -23,24 +14,28 @@ const CUDA_Runtime_jll_uuid = Base.UUID("76a88914-d11a-5bdc-97e0-2f5a05c973a2")
 const preferences = Base.get_preferences(CUDA_Runtime_jll_uuid)
 
 function toolkit_version(cuda_toolkits)
-    if !@isdefined(CUDA_Driver_jll) ||          # see above
-       !isdefined(CUDA_Driver_jll, :libcuda)    # no driver found
-        return nothing
-    end
-
-    # check if the user prefers a specific version
-    cuda_version_override = if haskey(preferences, "version")
-        VersionNumber(preferences["version"])
-    else
-        nothing
+    # check if the user requested a specific version
+    if haskey(preferences, "version")
+        cuda_version_override = VersionNumber(preferences["version"])
     end
     Base.record_compiletime_preference(CUDA_Runtime_jll_uuid, "version")
 
+    # if not, we need to be able to use the driver to determine the version.
+    # note that we only require this when there's no override, to support
+    # precompiling with a fixed version without having the driver available.
+    if !@isdefined(cuda_version_override)
+        if !@isdefined(CUDA_Driver_jll) ||          # see above
+           !isdefined(CUDA_Driver_jll, :libcuda)    # no driver found
+            return nothing
+        end
+
+        cuda_driver = CUDA_Driver_jll.libcuda_version
+    end
+
     # "[...] applications built against any of the older CUDA Toolkits always continued
     #  to function on newer drivers due to binary backward compatibility"
-    cuda_driver = CUDA_Driver_jll.libcuda_version
     filter!(cuda_toolkits) do toolkit
-        if cuda_version_override !== nothing
+        if @isdefined(cuda_version_override)
             toolkit == cuda_version_override
         elseif cuda_driver >= v"11"
             # enhanced compatibility
