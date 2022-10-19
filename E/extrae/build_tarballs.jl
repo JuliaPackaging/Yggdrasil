dec30d61328e92a109cb3471d053eeb73860814c
Mirek Kratochvil <miroslav.kratochvil@uni.lu>
2022-10-19 13:52:27 +0200

    new recipe: extrae

---
+++ E/extrae/build_tarballs.jl
@@ -0,0 +1,42 @@
+#!/usr/bin/env julia
+using BinaryBuilder, Pkg
+
+name = "extrae"
+version = v"4.0.1"
+sources = [
+    ArchiveSource(
+        "https://github.com/bsc-performance-tools/extrae/archive/refs/tags/$(version).tar.gz",
+        "e6765eb087be3f3c162e08d65f425de5f26912811392527d56ecd75d4fb6b99d"),
+]
+
+script = raw"""
+cd ${WORKSPACE}/srcdir/extrae-*
+autoreconf -fvi
+./configure \
+    --prefix=$prefix \
+    --without-binutils \
+    --without-dyninst \
+    --without-mpi \
+    --without-papi \
+    --without-unwind
+
+make -j${nproc}
+make install
+"""
+
+platforms = expand_cxxstring_abis([
+    Platform("i686", "Linux"),
+    Platform("x86_64", "Linux"),
+])
+
+products = [
+    LibraryProduct("libseqtrace", :libseqtrace),
+    LibraryProduct("libomptrace", :libomptrace),
+    LibraryProduct("libnanostrace", :libnanostrace),
+]
+
+dependencies = [
+    Dependency("XML2_jll"),
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
