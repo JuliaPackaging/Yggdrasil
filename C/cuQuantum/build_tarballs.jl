ee37bf608728f983b4eddcfe2d7a7fe978189e52
Tim Besard <tim.besard@gmail.com>
2022-10-20 10:06:20 +0200

    [CUDA] Update cuQuantum to use CUDA_Runtime_jll. (#5715)
    Also includes a version revert, until CUDA.jl is compatible.

--- C/cuQuantum/build_tarballs.jl
+++ C/cuQuantum/build_tarballs.jl
@@ -1,9 +1,12 @@
-using BinaryBuilder
+using BinaryBuilder, Pkg
+using Base.BinaryPlatforms: arch, os
 
-include("../../fancy_toys.jl")
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
+include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "cuQuantum"
-version_string = "22.07.1.14"
+version_string = "0.1.0.30"
 version = let
     maj, min, patch, extra = parse.(Int, split(version_string, '.'))
     VersionNumber(maj, min, patch * 100 + extra)
@@ -12,13 +15,13 @@ end
 platforms_and_sources = Dict(
     Platform("x86_64", "linux") => [
         ArchiveSource("https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-x86_64/cuquantum-linux-x86_64-$(version_string)-archive.tar.xz",
-                      "4c4931096498451593ad553b6cb7a107bb6d6cedea65c80d5376d0cfbb647f8e")],
+                      "8ad8e98f14275ffe0de02574be5c86224af1c657c41baf02c16440301ffe0aae")],
     Platform("powerpc64le", "linux") => [
         ArchiveSource("https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-ppc64le/cuquantum-linux-ppc64le-$(version_string)-archive.tar.xz",
-                      "2d35dba3c739e8de51591bcd2d7edf1a3c9995850af6525ae08e9b5c3798cf9a")],
+                      "0eb84eef619a1cfab5870fb585200a9869a197866685252e4ca8187322809554")],
     Platform("aarch64", "linux") => [
         ArchiveSource("https://developer.download.nvidia.com/compute/cuquantum/redist/cuquantum/linux-sbsa/cuquantum-linux-sbsa-$(version_string)-archive.tar.xz",
-                      "0c8fb14bf8916170e15a2ae7cfa950e23af13c9cb0915bad88d754221ad60116")],
+                      "3dd04cf08f1323318e0e63a7e28bc904c426ced3367aca809fba7e7beef94063")],
 )
 
 # Bash recipe for building across all platforms
@@ -43,18 +46,26 @@ rm ${libdir}/*.a
 install_license LICENSE docs/*
 """
 
-# The products that we will ensure are always built
-products = [
-    LibraryProduct("libcustatevec", :libcustatevec; dont_dlopen=true),
-    LibraryProduct("libcutensornet", :libcutensornet; dont_dlopen=true),
+augment_platform_block = CUDA.augment
+
+dependencies = [
+    RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll")),
+    RuntimeDependency(PackageSpec(name="CUTENSOR_jll"), compat="~1.4")
 ]
 
-# Dependencies that must be installed before this package can be built
-dependencies = Dependency[]
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libcustatevec", :libcustatevec),
+    LibraryProduct("libcutensornet", :libcutensornet),
+]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 for (platform, sources) in platforms_and_sources
-    should_build_platform(triplet(platform)) || continue
-    build_tarballs(ARGS, name, version, sources, script, [platform],
-                   products, dependencies; lazy_artifacts=true)
+    augmented_platform = Platform(arch(platform), os(platform);
+                                  cuda=CUDA.platform(v"11"))
+    should_build_platform(triplet(augmented_platform)) || continue
+    build_tarballs(ARGS, name, version, sources, script, [augmented_platform],
+                   products, dependencies; lazy_artifacts=true,
+                   julia_compat="1.6", augment_platform_block,
+                   skip_audit=true, dont_dlopen=true)
 end
