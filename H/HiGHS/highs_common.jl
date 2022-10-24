5f4b85566ab5ffa18c13d214f51b4a00d2f9b697
Oscar Dowson <odow@users.noreply.github.com>
2022-10-25 10:24:00 +1300

    [HiGHS] update to v1.3.0 (#5713)
    * [HiGHS] make a v101.200.200 release from a master branch commit
    
    * Add patch
    
    * Attempt to patch
    
    * Update H/HiGHS/highs_common.jl
    
    * Switch to explicit detection of popcnt
    
    * Update H/HiGHS/HiGHS/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- H/HiGHS/highs_common.jl
+++ H/HiGHS/highs_common.jl
@@ -4,13 +4,14 @@ using BinaryBuilder, Pkg
 
 name = "HiGHS"
 
-version = v"1.2.2"
+version = v"1.3.0"
 
 sources = [
     GitSource(
         "https://github.com/ERGO-Code/HiGHS.git",
-        "3f0a74587ff25009f12a73efcfbf5dd8aef45a4a",
+        "e5004072be62335eebe96149eabf3879d8d82635",
     ),
+    DirectorySource("../bundled"),
 ]
 
 # These are the platforms we will build for by default, unless further
@@ -19,22 +20,24 @@ platforms = supported_platforms()
 
 function build_script(; shared_libs::String)
     return "BUILD_SHARED=$(shared_libs)\n" * raw"""
-cd $WORKSPACE/srcdir
+cd $WORKSPACE/srcdir/HiGHS
 
-mkdir -p HiGHS/build
-cd HiGHS/build
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/v1.3.0.patch
+
+mkdir -p build
+cd build
 
 # Do fully static build only on Windows
 if [[ "${BUILD_SHARED}" == "OFF" ]] && [[ "${target}" == *-mingw* ]]; then
     export CXXFLAGS="-static"
 fi
+
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=${BUILD_SHARED} \
     -DFAST_BUILD=ON \
-    -DJULIA=ON \
-    -DIPX=ON ..
+    -DJULIA=ON ..
 
 if [[ "${target}" == *-linux-* ]]; then
         make -j ${nproc}
