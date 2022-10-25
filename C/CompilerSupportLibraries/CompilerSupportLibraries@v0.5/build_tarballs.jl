6536964c175f6d2f8d3605033d39cd88bf575f30
Mosè Giordano <mose@gnu.org>
2022-10-25 11:04:42 +0100

    [CompilerSupportLibraries] Add also `libgcc_s.a` on Windows

--- C/CompilerSupportLibraries/CompilerSupportLibraries@v0.5/build_tarballs.jl
+++ C/CompilerSupportLibraries/CompilerSupportLibraries@v0.5/build_tarballs.jl
@@ -2,4 +2,4 @@ include("../common.jl")
 
 build_csl(ARGS, v"0.5.3"; preferred_gcc_version=v"11", windows_staticlibs=true, julia_compat="1.9")
 
-# Build trigger: 0
+# Build trigger: 1
--- C/CompilerSupportLibraries/common.jl
+++ C/CompilerSupportLibraries/common.jl
@@ -83,7 +83,7 @@ if [[ ${target} == *mingw* ]]; then
     cp -uav /opt/${target}/${target}/sys-root/bin/*.${dlext}* ${libdir}/
     if [[ "${WINDOWS_STATICLIBS}" == "true" ]]; then
         # Install also some static libraries, needed for linking
-        for lib in libmsvcrt.a libgcc.a libssp.a; do
+        for lib in libmsvcrt.a libgcc.a libgcc_s.a libssp.a; do
             qfind "/opt/${target}" -name "${lib}" -exec install -Dvm 0644 '{}' "${prefix}/lib/${lib}" \;
         done
     fi
@@ -148,6 +148,7 @@ install_license /usr/share/licenses/GPL-3.0+
                 vcat(common_products,
                      [FileProduct("lib/libmsvcrt.a", :libmsvcrt_a),
                       FileProduct("lib/libgcc.a", :libgcc_a),
+                      FileProduct("lib/libgcc_s.a", :libgcc_s_a),
                       FileProduct("lib/libssp.a", :libssp_a),
                       ])
             else
