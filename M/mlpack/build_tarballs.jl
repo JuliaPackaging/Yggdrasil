f4c6b383b92594e870021a5da98a7ff35acc1064
Mosè Giordano <giordano@users.noreply.github.com>
2022-10-25 23:01:31 +0100

    [mlpack] Use GCC 7 to avoid ICE on `aarch64-linux-musl-cxx11`

--- M/mlpack/build_tarballs.jl
+++ M/mlpack/build_tarballs.jl
@@ -193,4 +193,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", julia_compat="1.7")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7", julia_compat="1.7")
