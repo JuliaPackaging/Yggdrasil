04ad128b22f67167b4e0c8f274058694af3c835b
Mosè Giordano <giordano@users.noreply.github.com>
2022-10-29 01:54:45 +0100

    [CompilerSupportLibraries] Rebuild to get fixed RUNPATH (#5760)

--- C/CompilerSupportLibraries/CompilerSupportLibraries@v0.5/build_tarballs.jl
+++ C/CompilerSupportLibraries/CompilerSupportLibraries@v0.5/build_tarballs.jl
@@ -1,5 +1,5 @@
 include("../common.jl")
 
-build_csl(ARGS, v"0.5.3"; preferred_gcc_version=v"11", windows_staticlibs=true, julia_compat="1.9")
+build_csl(ARGS, v"0.5.4"; preferred_gcc_version=v"11", windows_staticlibs=true, julia_compat="1.9")
 
 # Build trigger: 1
