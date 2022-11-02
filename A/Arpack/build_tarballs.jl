111ed3344c4c230b84578f35fd5df8d9a67adb71
Elliot Saba <staticfloat@gmail.com>
2022-11-02 16:47:22 +0000

    Update `Arpack_jll` to require Julia v1.8+
    When building against the latest libblastrampoline, we embed an
    SOVERSION dependency on `libblastrampoline.so.5` that is only
    compatible with Julia v1.8+.  I have fixed this in General in
    this PR [0], so updating this without bumping the Arpack version number
    should be fine.
    
    [0] https://github.com/JuliaRegistries/General/pull/71500

--- A/Arpack/build_tarballs.jl
+++ A/Arpack/build_tarballs.jl
@@ -123,4 +123,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", julia_compat="1.7")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", julia_compat="1.8")
