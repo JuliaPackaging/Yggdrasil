a8748787f9a05fd90597f3e15534b1e5b5242b12
MichelJuillard <michel.juillard@mjui.fr>
2022-11-03 14:53:05 +0100

    v6.0.2: sparse storage for derivatives

--- D/DynarePreprocessor/build_tarballs.jl
+++ D/DynarePreprocessor/build_tarballs.jl
@@ -2,11 +2,11 @@ using BinaryBuilder
 
 
 name = "DynarePreprocessor"
-version = v"6.0.0"
+version = v"6.0.2"
 sources = [
-    GitSource("https://git.dynare.org/Dynare/preprocessor.git", "285a4389a3a368774124c9ae66ed7fc367592946"),
-    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
-                  "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
+    GitSource("https://git.dynare.org/Dynare/preprocessor.git", "00fd9dadb6dde5929c224c74e0a43715554a2c05"),
+    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz",
+                  "cd4f08a75577145b8f05245a2975f7c81401d75e9535dcffbb879ee1deefcbf4"),
     DirectorySource("./bundled"),
 ]
 
@@ -21,9 +21,14 @@ rm -f /usr/bin/flex
 
 # Help FreeBSD find header files.  See
 # https://github.com/JuliaPackaging/Yggdrasil/issues/3949
+if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]]; then
+    CC=gcc
+    CXX=g++
+    LDFLAGS=-static-libgcc
+fi
 if [[ "${target}" == *-freebsd* ]]; then
     export CPPFLAGS="-I${includedir}"
-elif [[ "${target}" == x86_64-apple-darwin* ]]; then
+    #elif [[ "${target}" == x86_64-apple-darwin* ]]; then
     # Install a newer SDK which supports `std::filesystem` to work around the issue
     #     Undefined symbols for architecture x86_64:
     #       "__ZNKSt3__14__fs10filesystem4path10__filenameEv", referenced from:
@@ -34,14 +39,14 @@ elif [[ "${target}" == x86_64-apple-darwin* ]]; then
     #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-DynamicModel.o
     #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-ModFile.o
     #           _main in dynare_preprocessor-DynareMain.o
-    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
-    rm -rf /opt/${target}/${target}/sys-root/System
+    pushd $WORKSPACE/srcdir/MacOSX11.*.sdk
+    #    rm -rf /opt/${target}/${target}/sys-root/System
     cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
     cp -ra System "/opt/${target}/${target}/sys-root/."
     popd
 fi
 
-atomic_patch -p1 "../patches/patches.patch"
+#atomic_patch -p1 "../patches/patches.patch"
 
 autoreconf -si
 
@@ -56,6 +61,7 @@ install_license COPYING
 """
 
 platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
+platforms = expand_cxxstring_abis(platforms)
 
 products = [
     ExecutableProduct("dynare-preprocessor", :dynare_preprocessor),
@@ -67,4 +73,4 @@ dependencies = [
     HostBuildDependency("flex_jll"),
 ]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"9")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"13")
