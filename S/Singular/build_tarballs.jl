23c4fc91c6c00e1dc91f3423e7ba80829cd4ce1d
dan <tthsqe12@gmail.com>
2022-11-03 17:18:01 +0100

    [Singular] update to 4.3.1-3 (#5793)

--- S/Singular/build_tarballs.jl
+++ S/Singular/build_tarballs.jl
@@ -27,7 +27,7 @@ import Pkg.Types: VersionSpec
 # to all components.
 #
 name = "Singular"
-upstream_version = v"4.3.1-2" # 4.3.1 plus some changes
+upstream_version = v"4.3.1-3" # 4.3.1 plus some changes
 version_offset = v"0.0.0"
 version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + version_offset.major,
                         upstream_version.patch * 100 + version_offset.minor,
@@ -35,7 +35,7 @@ version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + 
 
 # Collection of sources required to build normaliz
 sources = [
-    GitSource("https://github.com/Singular/Singular.git", "352380b2ec8d43ddf0176a9f70720e6e0e259361"),
+    GitSource("https://github.com/Singular/Singular.git", "2ea97f9b11bc251f21ac6d89ae9453eb5074a95d"),
     #ArchiveSource("https://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/$(upstream_version.major)-$(upstream_version.minor)-$(upstream_version.patch)/singular-$(upstream_version).tar.gz",
     #              "5b0f6c036b4a6f58bf620204b004ec6ca3a5007acc8352fec55eade2fc9d63f6"),
     #DirectorySource("./bundled")
