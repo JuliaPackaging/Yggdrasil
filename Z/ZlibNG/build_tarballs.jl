0fa7fb7385eb2907c87051a93f17929b85e2e558
Tomáš Drvoštěp <tomas.drvostep@gmail.com>
2022-11-04 13:50:26 +0100

    Update Z/ZlibNG/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- Z/ZlibNG/build_tarballs.jl
+++ Z/ZlibNG/build_tarballs.jl
@@ -17,6 +17,7 @@ mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
     -DZLIB_ENABLE_TESTS=OFF \
     ..
 make -j${nproc}
