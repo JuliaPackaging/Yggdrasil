70b2b5a2a5bfbaf05e8443ed7f2291c3fdf917d9
Anton Smirnov <tonysmn97@gmail.com>
2022-11-07 11:28:37 +0200

    Disable musl

--- R/rocBLAS/common.jl
+++ R/rocBLAS/common.jl
@@ -9,7 +9,7 @@ const GIT_TAGS = Dict(
 
 const ROCM_PLATFORMS = [
     Platform("x86_64", "linux"; libc="glibc", cxxstring_abi="cxx11"),
-    Platform("x86_64", "linux"; libc="musl", cxxstring_abi="cxx11"),
+    # Platform("x86_64", "linux"; libc="musl", cxxstring_abi="cxx11"),
 ]
 const PRODUCTS = [LibraryProduct(["librocblas"], :librocblas, ["lib"])]
 
