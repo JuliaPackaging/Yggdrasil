90a5a9a85dccdf2eb7ba1c051ffe53874ecfc95e
Mosè Giordano <mose@gnu.org>
2022-11-09 01:03:19 +0000

    [CBQN] More improvements

--- C/CBQN/build_tarballs.jl
+++ C/CBQN/build_tarballs.jl
@@ -7,19 +7,28 @@ version = v"2022.11.8"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/dzaima/CBQN.git", "4f9af9965c8c8fbb90b79168fa529ebe4124622c")
+    GitSource("https://github.com/dzaima/CBQN.git",
+              "4f9af9965c8c8fbb90b79168fa529ebe4124622c"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/CBQN/
 
+atomic_patch -p1 ../patches/library-extension.patch
+
 # Yes, the build systems fiddles with git remotes names, so we need to make it happy.  Sigh.
 git remote rename origin bb
 git remote add origin https://github.com/dzaima/CBQN.git
 git fetch origin
 
-make shared-o3 C_INCLUDE_PATH="${prefix}/lib/libffi-3.2.1/include"
+FLAGS=()
+if [[ "${target}" == *-apple-* ]]; then
+    FLAGS+=(NO_DYNAMIC_LIST=1)
+fi
+
+make shared-o3 C_INCLUDE_PATH="${includedir}" "${FLAGS[@]}"
 install -Dvm 0755 "libcbqn.${dlext}" "${libdir}/libcbqn.${dlext}"
 """
 
@@ -34,7 +43,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Libffi_jll", uuid="e9f186c6-92d2-5b65-8a66-fee21dc1b490"))
+    Dependency(PackageSpec(name="Libffi_jll", uuid="e9f186c6-92d2-5b65-8a66-fee21dc1b490"); compat="3.4.4"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
