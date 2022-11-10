d8e4116c15df5b615a092a576eb6b80a252c2544
Tim Besard <tim.besard@gmail.com>
2022-11-10 15:37:09 +0100

    fts: build for experimental platforms. (#5842)

--- F/fts/build_tarballs.jl
+++ F/fts/build_tarballs.jl
@@ -23,7 +23,8 @@ install_license ./COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter!(Sys.islinux, supported_platforms())
+platforms = supported_platforms(; experimental=true)
+filter!(Sys.islinux, platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -35,4 +36,5 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6")
