ec735697bea3ea5c25c7559e3b86042d86fe691d
MichelJuillard <michel.juillard@mjui.fr>
2022-11-15 22:31:02 +0100

    new source + clang patches

--- D/DynarePreprocessor/build_tarballs.jl
+++ D/DynarePreprocessor/build_tarballs.jl
@@ -1,10 +1,9 @@
 using BinaryBuilder
 
-
 name = "DynarePreprocessor"
-version = v"6.0.2"
+version = v"6.1.0"
 sources = [
-    GitSource("https://git.dynare.org/Dynare/preprocessor.git", "00fd9dadb6dde5929c224c74e0a43715554a2c05"),
+    GitSource("https://git.dynare.org/Dynare/preprocessor.git", "4807a6c8806b650094f8a1fc8bc7435e94d38305"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz",
                   "cd4f08a75577145b8f05245a2975f7c81401d75e9535dcffbb879ee1deefcbf4"),
     DirectorySource("./bundled"),
@@ -21,32 +20,19 @@ rm -f /usr/bin/flex
 
 # Help FreeBSD find header files.  See
 # https://github.com/JuliaPackaging/Yggdrasil/issues/3949
-if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]]; then
-    CC=gcc
-    CXX=g++
-    LDFLAGS=-static-libgcc
-fi
 if [[ "${target}" == *-freebsd* ]]; then
     export CPPFLAGS="-I${includedir}"
-    #elif [[ "${target}" == x86_64-apple-darwin* ]]; then
-    # Install a newer SDK which supports `std::filesystem` to work around the issue
-    #     Undefined symbols for architecture x86_64:
-    #       "__ZNKSt3__14__fs10filesystem4path10__filenameEv", referenced from:
-    #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-ModelTree.o
-    #           __ZNK11StaticModel15writeStaticFileERKNSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEEbbbS8_RKNS0_4__fs10filesystem4pathESD_b in dynare_preprocessor-StaticModel.o
-    #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-StaticModel.o
-    #           __ZNK12DynamicModel16writeDynamicFileERKNSt3__112basic_stringIcNS0_11char_traitsIcEENS0_9allocatorIcEEEEbbbS8_RKNS0_4__fs10filesystem4pathESD_b in dynare_preprocessor-DynamicModel.o
-    #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-DynamicModel.o
-    #           __ZNSt3__14__fs10filesystem4pathdVERKS2_ in dynare_preprocessor-ModFile.o
-    #           _main in dynare_preprocessor-DynareMain.o
+elif [[ "${target}" == x86_64-apple-darwin* ]]; then
     pushd $WORKSPACE/srcdir/MacOSX11.*.sdk
-    #    rm -rf /opt/${target}/${target}/sys-root/System
+    rm -rf /opt/${target}/${target}/sys-root/System
+    rm -rf /opt/x86_64-apple-darwin14/x86_64-apple-darwin14/sys-root/usr/include/libxml2/libxml
     cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
     cp -ra System "/opt/${target}/${target}/sys-root/."
+    export MACOSX_DEPLOYMENT_TARGET=11.1
     popd
 fi
 
-#atomic_patch -p1 "../patches/patches.patch"
+atomic_patch -p1 "../patches/patches.patch"
 
 autoreconf -si
 
