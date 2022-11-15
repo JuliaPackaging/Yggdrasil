12d0a0f75a0b72e325d9c2e51f751836ea8002a5
Tim Besard <tim.besard@gmail.com>
2022-11-15 22:52:44 +0100

    Fixes.

--- P/pthread_win32/build_tarballs.jl
+++ P/pthread_win32/build_tarballs.jl
@@ -7,30 +7,32 @@ version = v"3.0.3"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/GerHobbelt/pthread-win32.git", "512a38decec4d2007d4b336f5f24f7e4afa23bd0")
+    GitSource("https://github.com/GerHobbelt/pthread-win32.git",
+              "512a38decec4d2007d4b336f5f24f7e4afa23bd0")
 ]
 
-dependencies = [
-]
+dependencies = []
 
 # Bash recipe for building across all platforms
 script = raw"""
+apk add binutils    # for objdump
+
 cd pthread-win32
-apk add binutils
+install_license /usr/share/licenses/APL2
+
 autoheader
 autoconf
-echo ${target}
 if [[ "${target}" == *x86_64-w64-mingw32* ]]; then
     ./configure --host=x86-w64-mingw32
 elif [[ "${target}" == *i686-w64-mingw32* ]]; then
     ./configure --host=i686-w64-mingw32
 fi
+
 make realclean
-make GC
-make install DLLDEST=${libdir} LIBDEST=${libdir} HDRDEST=${includedir}
-#mkdir -p {bindir}/bin
-#cp pthreadGC3.dll ${bindir}/pthread.dll
-install_license /usr/share/licenses/APL2
+make GC GC-static
+
+chmod +x install-sh
+make install DESTDIR=${prefix} prefix=
 """
 
 # These are the platforms we will build for by default, unless further
@@ -39,8 +41,9 @@ platforms = filter!(p -> Sys.iswindows(p), supported_platforms())
 
 # The products that we will ensure are always built
 products = Product[
-    LibraryProduct("pthread", :pthread)
+    LibraryProduct("pthreadGC3", :pthread)
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6")
