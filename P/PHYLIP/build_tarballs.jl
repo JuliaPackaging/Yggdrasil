fed93824c3ebb5762ad7c6b5440085a251422757
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-18 00:18:37 +0100

    disable macos, freebsd

--- P/PHYLIP/build_tarballs.jl
+++ P/PHYLIP/build_tarballs.jl
@@ -54,8 +54,8 @@ platforms = [
     Platform("aarch64", "linux"; libc = "musl"),
     Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
     Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("x86_64", "macos"; ),
-    Platform("x86_64", "freebsd"; ),
+    #Platform("x86_64", "macos"; ),
+    #Platform("x86_64", "freebsd"; ),
     Platform("i686", "windows"; ),
     Platform("x86_64", "windows"; )
 ]
