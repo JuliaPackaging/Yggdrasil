a81620c752c29083ef2ae8c97c55f7d63dfb4902
Max Horn <max@quendi.de>
2022-11-18 16:12:23 +0100

    [XyceWrapper] rebuild for Julia 1.10 support (#5886)

--- X/XyceWrapper/build_tarballs.jl
+++ X/XyceWrapper/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-julia_versions = [v"1.6.3", v"1.7", v"1.8"]
+julia_versions = [v"1.6.3", v"1.7", v"1.8", v"1.9", v"1.10"]
 
 name = "XyceWrapper"
 version = v"0.3.0"
