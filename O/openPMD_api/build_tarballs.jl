1f61ae59e3dbf50cf17911a694bb98aa44c8b490
Erik Schnetter <schnetter@gmail.com>
2022-11-18 22:35:29 -0500

    openPMD-api: Disable Julia 1.7 build to reduce number of build jobs

--- O/openPMD_api/build_tarballs.jl
+++ O/openPMD_api/build_tarballs.jl
@@ -10,7 +10,8 @@ version = v"0.15.3"
 openpmi_api_version = "v.0.14.5" # This is really the `dev` branch after version 0.14.5
 
 # `v"1.6.3"` fails to build
-julia_versions = [v"1.7", v"1.8", v"1.9", v"1.10"]
+# We disable `v"1.7"` because there would otherwise be too many (> 256) build jobs
+julia_versions = [v"1.8", v"1.9", v"1.10"]
 
 # Collection of sources required to complete build
 sources = [
