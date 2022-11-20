35b32955ad1355456fbc038f6052f1d66fb29c17
Christopher Doris <github.com/cjdoris>
2022-11-20 18:01:11 +0000

    change to immutable conda url

--- M/micromamba/build_tarballs.jl
+++ M/micromamba/build_tarballs.jl
@@ -4,26 +4,27 @@ using BinaryBuilder
 
 name = "micromamba"
 version = v"1.0.0"
+build = "1"
 
 # Collection of sources required to build micromamba
 # These are actually just the conda packages for each platform
 sources = [
-    FileSource("https://micro.mamba.pm/api/micromamba/linux-64/$version",
+    FileSource("https://conda.anaconda.org/conda-forge/linux-64/micromamba-$version-$build.tar.bz2",
         "41bda425dd9c44b59cc35781c1b4031465c36648791e8598f7826f65ef09e387",
         filename="micromamba-x86_64-linux-gnu.tar.bz2"),
-    FileSource("https://micro.mamba.pm/api/micromamba/linux-aarch64/$version",
+    FileSource("https://conda.anaconda.org/conda-forge/linux-aarch64/micromamba-$version-$build.tar.bz2",
         "57934f137d326aaaebfd9788ddd0e112135d33b95a7dd3e9292517ce025b4d5a",
         filename="micromamba-aarch64-linux-gnu.tar.bz2"),
-    FileSource("https://micro.mamba.pm/api/micromamba/linux-ppc64le/$version",
+    FileSource("https://conda.anaconda.org/conda-forge/linux-ppc64le/micromamba-$version-$build.tar.bz2",
         "196831fad6384fc7520ae2bf2eddaae6b3a256d80f607efb5eb1d97d3cc0c4e0",
         filename="micromamba-powerpc64le-linux-gnu.tar.bz2"),
-    FileSource("https://micro.mamba.pm/api/micromamba/osx-64/$version",
+    FileSource("https://conda.anaconda.org/conda-forge/osx-64/micromamba-$version-$build.tar.bz2",
         "2141288a06b520c81724eb1fad2e99395133c7d64ca9c776ae8d6ccc7edfd8e0",
         filename="micromamba-x86_64-apple-darwin14.tar.bz2"),
-    FileSource("https://micro.mamba.pm/api/micromamba/osx-arm64/$version",
+    FileSource("https://conda.anaconda.org/conda-forge/osx-arm64/micromamba-$version-$build.tar.bz2",
         "ffb87b3923a01f22d299d21f8da2b57b0a22bf1be4f59f634f449ffa431d7d18",
         filename="micromamba-aarch64-apple-darwin20.tar.bz2"),
-    FileSource("https://micro.mamba.pm/api/micromamba/win-64/$version",
+    FileSource("https://conda.anaconda.org/conda-forge/win-64/micromamba-$version-$build.tar.bz2",
         "e8b02c5b51eecb71953a460846f37acd6066eb3cef6cd3aebc60835048f37ad3",
         filename="micromamba-x86_64-w64-mingw32.tar.bz2"),
 ]
