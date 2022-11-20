1e7bcf337a3dd7babc9598820be9d4ae1c826363
Simon Byrne <simonbyrne@gmail.com>
2022-11-20 14:57:24 -0800

    add linux limits patch

--- J/JuliaNVTXCallbacks/build_tarballs.jl
+++ J/JuliaNVTXCallbacks/build_tarballs.jl
@@ -8,12 +8,14 @@ version = v"0.1"
 sources = [
     GitSource("https://github.com/NVIDIA/NVTX",
               "e170594ac7cf1dac584da473d4ca9301087090c1"), # v3.1.0
+    DirectorySource("./bundled"),
     GitSource("https://github.com/simonbyrne/NVTX.jl",
               "54c215bec0d2e71b43d6ccab5d57e9fb6dd9615f"),          
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
+atomic_patch -p1 $WORKSPACE/srcdir/patches/nvtx-linux-limits.patch
 cd ${WORKSPACE}/srcdir/NVTX.jl/deps
 mkdir -p ${libdir}
 ${CC} -std=c99 -O2 -fPIC -shared -I${WORKSPACE}/srcdir/NVTX/c/include -o ${libdir}/libjulia_nvtx_callbacks.${dlext} callbacks.c 
