5bf229ace313d58ac9b54355eb0431028e36799f
Simon Byrne <simonbyrne@gmail.com>
2022-11-21 15:49:26 -0800

    [NVTX] remove pragma hide (#5905)
    * [NVTX] remove pragma hide
    
    * Update nvtx-pragma-hidden.patch
    
    * Update N/NVTX/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- N/NVTX/build_tarballs.jl
+++ N/NVTX/build_tarballs.jl
@@ -14,9 +14,14 @@ sources = [
 script = raw"""
 atomic_patch -p1 $WORKSPACE/srcdir/patches/nvtx-linux-limits.patch
 atomic_patch -p1 $WORKSPACE/srcdir/patches/nvtx-windows-case.patch
+atomic_patch -p1 $WORKSPACE/srcdir/patches/nvtx-pragma-hidden.patch
 cd ${WORKSPACE}/srcdir/
 mkdir -p ${libdir}
-${CC} -std=c99 -O2 -fPIC -shared -I${WORKSPACE}/srcdir/NVTX/c/include -o ${libdir}/libnvToolsExt.${dlext} nvtx.c 
+if [[ "${target}" == *-linux-* ]]; then
+    CFLAGS="-fPIC"
+    LIBS="-ldl"
+fi
+${CC} -std=c99 -O2 ${CFLAGS} -shared ${LIBS} -I${WORKSPACE}/srcdir/NVTX/c/include -o ${libdir}/libnvToolsExt.${dlext} nvtx.c
 install_license ${WORKSPACE}/srcdir/NVTX/LICENSE.txt
 """
 
