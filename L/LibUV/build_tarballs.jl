b5fef352621f293f0e0b1091cbe86135a3408277
Tim Besard <tim.besard@gmail.com>
2022-11-25 12:08:55 +0100

    Bump libuv. (#5918)

--- L/LibUV/build_tarballs.jl
+++ L/LibUV/build_tarballs.jl
@@ -6,7 +6,7 @@ version = v"2"
 # Collection of sources required to build libuv
 sources = [
     GitSource("https://github.com/JuliaLang/libuv.git",
-              "a9b8cea503ea7350ec9f4926a60544ac5807f038"),
+              "2723e256e952be0b015b3c0086f717c3d365d97e"),
 ]
 
 # Bash recipe for building across all platforms
