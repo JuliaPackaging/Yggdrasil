2fe1ed9f200df83e7cd6a003544f3096c8faa860
Jorge Fernandez-de-Cossio-Diaz <cossio@users.noreply.github.com>
2022-11-28 20:01:27 +0100

    fix string

--- T/TMscore/build_tarballs.jl
+++ T/TMscore/build_tarballs.jl
@@ -22,6 +22,7 @@ install_license LICENSE_1_0.txt
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
