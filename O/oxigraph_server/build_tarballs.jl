7079babd6f5cc863ad71d06eae9077dee28d5685
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-11-30 15:51:56 +0100

    Add dependency

--- O/oxigraph_server/build_tarballs.jl
+++ O/oxigraph_server/build_tarballs.jl
@@ -40,6 +40,7 @@ products = Product[
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("OpenSSL_jll"),
+    BuildDependency("LLVMCompilerRT_jll"), 
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
