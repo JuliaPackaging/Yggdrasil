c71ac58640e91aab4782309b0b2bbc7841b388dc
Tim Besard <tim.besard@gmail.com>
2022-12-02 17:13:37 +0100

    rr: build 32bits objects for use on 64bits systems.

--- R/rr/build_tarballs.jl
+++ R/rr/build_tarballs.jl
@@ -19,10 +19,23 @@ cd ${WORKSPACE}/srcdir/rr/
 mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-      -Ddisable32bit=ON -DBUILD_TESTS=OFF -DWILL_RUN_TESTS=OFF -Dstaticlibs=ON ..
+      -DBUILD_TESTS=OFF -DWILL_RUN_TESTS=OFF -Dstaticlibs=ON ..
 make -j${nproc}
 make install
 """
+
+augment_platform_block = """
+    using Base.BinaryPlatforms
+
+    function augment_platform!(platform::Platform)
+        if Sys.islinux()
+            real_arch = chomp(read(`uname -m`, String))
+            remaining_tags = copy(tags(platform))
+            delete!(remaining_tags, "arch")
+            delete!(remaining_tags, "os")
+            Platform(String(real_arch), os(platform), remaining_tags)
+        end
+    end"""
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
@@ -50,5 +63,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies,
-               preferred_gcc_version=v"10")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               augment_platform_block, julia_compat="1.6", preferred_gcc_version=v"10")
