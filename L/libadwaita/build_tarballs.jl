addd5f21e18d618f3230c95024ac4f6d94962f6f
Jared Wahlstrand <jwahlstrand@gmail.com>
2022-12-04 18:58:19 -0500

    Update L/libadwaita/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/libadwaita/build_tarballs.jl
+++ L/libadwaita/build_tarballs.jl
@@ -46,7 +46,7 @@ glib-compile-schemas ${prefix}/share/glib-2.0/schemas
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter!(p -> arch(p) != "armv6l", supported_platforms(; experimental=true))
+platforms = filter!(p -> arch(p) != "armv6l", supported_platforms())
 
 # The products that we will ensure are always built
 products = [
