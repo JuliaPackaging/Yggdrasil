56b9a6d38ce926f8c7c09a92a1d36cc4f4805f42
notinaboat <notinaboat@icloud.com>
2022-12-05 20:33:55 +1100

    liburing: build_tarballs.jl

---
+++ L/Liburing/build_tarballs.jl
@@ -0,0 +1,47 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Liburing"
+version = v"2.4.0"
+
+# Collection of sources required to complete build
+sources = [
+    # PR743: https://github.com/axboe/liburing/pull/743
+    GitSource("https://github.com/cmazakas/liburing.git",
+              "9d2ee254e7411b7e683a9a5d3b61f9b3400747ee"),
+
+    # FIXME revert to axboe/master when PR is merged...
+    # GitSource("https://github.com/axboe/liburing.git", "??"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd liburing/
+./configure --prefix=${prefix}
+make -C src install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+#platforms = [
+#    Platform("x86_64", "linux"; libc = "glibc"),
+#    Platform("aarch64", "linux"; libc = "glibc")
+#]
+platforms = supported_platforms(;exclude=x->
+    startswith(arch(x), r"arm|power") ||
+    !Sys.islinux(x))
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("liburing-ffi", :liburing)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"10.2.0")
