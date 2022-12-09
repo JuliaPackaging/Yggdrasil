9adecaff289ff04b8e52086b6cd50dda68b34485
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2022-12-09 22:56:40 +0100

    [xz] Bump version to 5.2.9

--- X/XZ/build_tarballs.jl
+++ X/XZ/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder
 
 name = "XZ"
-version = v"5.2.7"
+version = v"5.2.9"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://tukaani.org/xz/xz-$(version).tar.xz",
-                  "8712e9acb0b6b49a97d443458a3067dc5c08a025e02dc5f773176c51dd7cfc69"),
+                  "287ef163e7e57561e9de590b2a9037457af24f03a46bbd12bf84f3263679e8d2"),
 ]
 
 # Bash recipe for building across all platforms
@@ -21,7 +21,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(;experimental=true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
