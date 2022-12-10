109d27d293f3b7a0fff77800cd059a5b7dc14f20
Mosè Giordano <giordano@users.noreply.github.com>
2022-12-10 23:44:00 +0000

    [XZ] Protect expansion of variables

--- X/XZ/build_tarballs.jl
+++ X/XZ/build_tarballs.jl
@@ -18,12 +18,12 @@ BUILD_FLAGS=(--prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-pic)
 
 # i686 error "configure works but build fails at crc32_x86.S"
 # See 4.3 from https://git.tukaani.org/?p=xz.git;a=blob_plain;f=INSTALL;hb=HEAD
-if [[ $target == i686-linux-* ]]; then
+if [[ "${target}" == i686-linux-* ]]; then
     BUILD_FLAGS+=(--disable-assembler)
 fi
 
-if [[ ${target} != *-gnu* ]]; then
-    ./configure ${BUILD_FLAGS[@]}
+if [[ "${target}" != *-gnu* ]]; then
+    ./configure "${BUILD_FLAGS[@]}"
     make -j${nproc}
     make install
 else
@@ -35,8 +35,8 @@ else
     #      Use either --disable-shared or --disable-static to build one type
     #      of library at a time. If both types are needed, build one at a time,
     #      possibly picking only src/liblzma/.libs/liblzma.a from the static build.
-    for TOGGLE in ${STATIC_SHARED_TOGGLE[@]} ; do
-        ./configure ${BUILD_FLAGS[@]} ${TOGGLE[@]} 
+    for TOGGLE in "${STATIC_SHARED_TOGGLE[@]}"; do
+        ./configure "${BUILD_FLAGS[@]}" "${TOGGLE[@]}"
         make -j${nproc}
         make install
     done
