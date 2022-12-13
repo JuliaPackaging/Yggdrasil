18b17cf78f95992e7d7a032dc099839efccb9040
Tyler Thomas <36181311+tylerjthomas9@users.noreply.github.com>
2022-12-13 09:53:14 -0700

    [XGBoost] Add GPU support (#5892)
    * initial gpu build
    
    * x86_64-linux-gnu cuda builds
    
    * Fix No space left on device for linux
    
    * remove powerpc from cuda build
    
    * initial windows cuda build
    
    * fix i686-w64 build
    
    * separate cuda/non-cuda builds, drop windows cuda
    
    * remove commented powerpc64le, aarch64 builds
    
    * add NCCL to cuda build
    
    * only build vs cuda v11, add cuda_runtime_jll

--- X/XGBoost/build_tarballs.jl
+++ X/XGBoost/build_tarballs.jl
@@ -3,6 +3,10 @@ using BinaryBuilder, Pkg
 name = "XGBoost"
 version = v"1.7.1"
 
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
+include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
+
 # Collection of sources required to build XGBoost
 sources = [
     GitSource("https://github.com/dmlc/xgboost.git","534c940a7ea50ab3b8a827546ac9908f859379f2"), 
@@ -18,8 +22,24 @@ git submodule update --init
 (cd dmlc-core; atomic_patch -p1 "../../patches/dmlc_windows.patch")
 
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}"
-make -j${nproc}
+if  [[ "${target}" == *linux-gnu-cuda* ]]; then
+    # nvcc writes to /tmp, which is a small tmpfs in our sandbox.
+    # make it use the workspace instead
+    export TMPDIR=${WORKSPACE}/tmpdir
+    mkdir ${TMPDIR}
+    
+    export CUDA_HOME=${WORKSPACE}/destdir/cuda
+    export PATH=$PATH:$CUDA_HOME/bin
+    cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} \
+            -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" \
+            -DUSE_CUDA=ON \
+            -DBUILD_WITH_CUDA_CUB=ON \
+            -DUSE_NCCL=ON
+    make -j${nproc}
+else
+    cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" 
+    make -j${nproc}
+fi
 
 # Manual installation, to avoid installing dmlc
 cd ..
@@ -37,9 +57,19 @@ fi
 install_license LICENSE
 """
 
+
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
+cuda_platforms = expand_cxxstring_abis(Platform("x86_64", "linux"))
+
+cuda_versions = [v"11.0"]
+
+cuda_full_versions = Dict(
+    v"11.0" => v"11.0.3",
+)
+
+augment_platform_block = CUDA.augment
 
 # The products that we will ensure are always built
 products = [
@@ -57,3 +87,20 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8", julia_compat="1.6")
+
+# build cuda tarballs
+for cuda_version in cuda_versions, platform in cuda_platforms
+    augmented_platform = Platform(arch(platform), os(platform);
+                                  cuda=CUDA.platform(cuda_version))
+    should_build_platform(triplet(augmented_platform)) || continue
+
+    cuda_deps = [
+        BuildDependency(PackageSpec(name="CUDA_full_jll",
+                                    version=cuda_full_versions[cuda_version])),
+        RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll")),
+        RuntimeDependency(PackageSpec(name="NCCL_jll", version=v"2.15.1")),
+    ]
+
+    build_tarballs(ARGS, name, version, sources, script, [augmented_platform], products, [dependencies; cuda_deps];
+                   preferred_gcc_version=v"8", lazy_artifacts=true, julia_compat="1.6", augment_platform_block)
+end
