14ef69757c5f4b5b091dc765999f65959bdc2de2
Tyler Thomas <36181311+tylerjthomas9@users.noreply.github.com>
2022-12-13 14:37:53 -0700

    enable non-cuda builds

--- X/XGBoost/build_tarballs.jl
+++ X/XGBoost/build_tarballs.jl
@@ -86,7 +86,7 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-# build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8", julia_compat="1.6")
 
 # build cuda tarballs
 for cuda_version in cuda_versions, platform in cuda_platforms
