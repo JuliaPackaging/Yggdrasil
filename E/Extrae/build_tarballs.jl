8e158995b7657858ed73e2a3037e6b40f170697b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez@bsc.es>
2022-12-15 18:01:44 +0100

    Add CUDA support to Extrae

--- E/Extrae/build_tarballs.jl
+++ E/Extrae/build_tarballs.jl
@@ -1,5 +1,9 @@
 using BinaryBuilder
 
+include(joinpath(dirname(dirname(@__DIR__)), "platforms", "cuda.jl"))
+include(joinpath(dirname(dirname(@__DIR__)), "fancy_toys.jl"))
+
+
 name = "Extrae"
 version = v"4.0.2" # NOTE this is version 4.0.2rc1. its release candidate, but we needed a patch for Binutils 2.39
 sources = [
@@ -17,6 +21,10 @@ if [ $(uname -m) == "aarch64" ]; then
     export ENABLE_ARM64=1
 fi
 
+if [ -d "$prefix/cuda" ]; then
+    export ENABLE_CUDA=1
+fi
+
 autoreconf -fvi
 ./configure \
     --prefix=${prefix} \
@@ -27,6 +35,7 @@ autoreconf -fvi
     --disable-nanos \
     --disable-smpss \
     --without-mpi \
+    ${ENABLE_CUDA:+--with-cuda=$prefix/cuda} \
     --with-binutils=$prefix \
     --with-unwind=$prefix \
     --with-xml-prefix=$prefix \
@@ -42,6 +51,16 @@ platforms = [
     Platform("aarch64", "linux"; libc="glibc"),
 ]
 
+platforms = expand_cxxstring_abis(platforms)
+
+cuda_version = v"10.2.89"
+cuda_platforms = map(platforms) do platform
+    Platform(arch(platform), os(platform); libc="glibc", cuda=CUDA.platform(cuda_version))
+end
+
+# filter aarch64 + cuda
+cuda_platforms = filter(==("x86_64") ∘ arch, platforms)
+
 products = [
     LibraryProduct("libseqtrace", :libseqtrace),
     LibraryProduct("libpttrace", :libpttrace),
@@ -50,11 +69,23 @@ products = [
     ExecutableProduct("extrae-loader", :extrae_loader),
 ]
 
+cuda_product = [
+    LibraryProduct("libcudatrace", :libcudatrace),
+]
+
 dependencies = [
     Dependency("Binutils_jll"),
     Dependency("LibUnwind_jll"),
     Dependency("PAPI_jll"),
     Dependency("XML2_jll"),
+    BuildDependency(PackageSpec(name="CUDA_full_jll", version=cuda_version), platforms=cuda_platforms),
+    RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll", version=cuda_version), platforms=cuda_platforms),
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+
+for platform in cuda_platforms
+    should_build_platform(triplet(platform)) || continue
+
+    build_tarballs(ARGS, name, version, sources, script, [platform], vcat(products, cuda_product), dependencies; julia_compat="1.6", lazy_artifacts=true, CUDA.augment)
+end
