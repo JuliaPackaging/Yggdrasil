0a9e9c1593f4d76dc38f705c6e6b310fcf1cf2ea
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2022-12-17 04:47:59 +0530

    Update build_tarballs.jl

--- L/libboxer/build_tarballs.jl
+++ L/libboxer/build_tarballs.jl
@@ -18,10 +18,7 @@ cargo build --release
 install -Dvm 0755 "target/${rust_target}/release/libBoxer.${dlext}" "${libdir}/libBoxer.${dlext}"
 install_license LICENSE
 """ * """
-curl "https://github.com/feenkcom/libboxer/releases/download/v($version)/boxer.h"
-""" * raw"""
-install -Dvm 0755 "boxer.h" "${includedir}/boxer.h"
-"""
+curl "https://github.com/feenkcom/libboxer/releases/download/v($version)/boxer.h" --output """ * raw""" "${includedir}/boxer.h" --create-dirs"""
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
