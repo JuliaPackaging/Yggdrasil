2d72ee1874592cf3bfa250638b25004869349d37
Martijn Visser <mgvisser@gmail.com>
2022-12-20 13:14:46 +0100

    [GDAL] build version 3.6.1 (#6010)
    * [GDAL] build version 3.6.1
    
    3.6.0 is retracted, details in https://github.com/OSGeo/gdal/blob/v3.6.1/NEWS.md
    
    * update hash
    
    * install_license LICENSE.TXT

--- G/GDAL/build_tarballs.jl
+++ G/GDAL/build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "GDAL"
-upstream_version = v"3.6.0"
+upstream_version = v"3.6.1"
 version_offset = v"1.0.0"
 version = VersionNumber(upstream_version.major * 100 + version_offset.major,
                         upstream_version.minor * 100 + version_offset.minor,
@@ -12,7 +12,7 @@ version = VersionNumber(upstream_version.major * 100 + version_offset.major,
 # Collection of sources required to build GDAL
 sources = [
     ArchiveSource("https://github.com/OSGeo/gdal/releases/download/v$upstream_version/gdal-$upstream_version.tar.gz",
-        "0d6a79eec0c6afb97c7172e429ec05c9d15d3917987ad686a914b292c83531db"),
+        "2e2bf9116ec76100a7de6d2a2f939466291dead007b905089eb37ffdbce593e1"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
         "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
 ]
@@ -20,6 +20,8 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/gdal-*/
+install_license LICENSE.TXT
+
 mkdir build
 cd build
 
