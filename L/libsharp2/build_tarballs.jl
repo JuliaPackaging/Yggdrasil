6ee049ddf854b29ef8facdf57202068f0ee92f5b
Mosè Giordano <mose@gnu.org>
2022-12-27 19:55:24 +0100

    [libsharp2] Enable OpenMP on macOS

--- L/libsharp2/build_tarballs.jl
+++ L/libsharp2/build_tarballs.jl
@@ -23,17 +23,17 @@ if [[ "${target}" == x86_64-* ]] && [[ "${target}" != *-apple-* ]] && [[ "${targ
     # Enable runtime multiarch, but not for macOS nor Windows. For Mac OS X, see
     # https://gitlab.mpcdf.mpg.de/mtr/libsharp/-/blob/6374a3a1ffb935443c56f09b371b11cf982a7e28/COMPILE
     # For Windows, see https://github.com/ziotom78/Libsharp.jl/issues/3
-    export CFLAGS="-DMULTIARCH"
+    export CPPFLAGS="-DMULTIARCH"
 fi
-export CFLAGS="${CFLAGS} -O3"
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+export CFLAGS="-std=c99 -O3"
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --enable-openmp
 make -j${nproc}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
@@ -42,7 +42,10 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
+    # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
+    Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
