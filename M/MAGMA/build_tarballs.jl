70a7eba6db99a5080ef8f8036ec060ffe52e277b
Will Kimmerer <kimmerer@mit.edu>
2022-12-28 15:52:03 -0500

    build both 10.x and 11.x cuda

--- M/MAGMA/build_tarballs.jl
+++ M/MAGMA/build_tarballs.jl
@@ -10,30 +10,31 @@ version = v"2.7.0"
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("http://icl.utk.edu/projectsfiles/magma/downloads/magma-2.7.0.tar.gz", "fda1cbc4607e77cacd8feb1c0f633c5826ba200a018f647f1c5436975b39fd18"),
-    DirectorySource("./bundled")
+    DirectorySource("./bundled"),
+    ArchiveSource("https://github.com/JuliaBinaryWrappers/CUDA_full_jll.jl/releases/download/CUDA_full-v10.2.89%2B5/CUDA_full.v10.2.89.x86_64-linux-gnu.tar.gz", "60e6f614db3b66d955b7e6aa02406765e874ff475c69e2b4a04eb95ba65e4f3b"; unpack_target = "CUDA_full.v10.2"),
+    ArchiveSource("https://github.com/JuliaBinaryWrappers/CUDA_full_jll.jl/releases/download/CUDA_full-v11.3.1%2B1/CUDA_full.v11.3.1.x86_64-linux-gnu.tar.gz", "9ae00d36d39b04e8e99ace63641254c93a931dcf4ac24c8eddcdfd4625ab57d6"; unpack_target = "CUDA_full.v11.3")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/magma-*
-
+cuda_version=`echo $bb_full_target | sed -E -e 's/.*cuda\+([0-9]+\.[0-9]+).*/\1/'`
+cuda_version_major=`echo $cuda_version | cut -d . -f 1`
+cuda_version_minor=`echo $cuda_version | cut -d . -f 2`
+cuda_full_path="$WORKSPACE/srcdir/CUDA_full.v$cuda_version/cuda"
+export PATH=$PATH:${cuda_full_path}/bin
+export CUDA_DIR=${cuda_full_path}
 cp ../make.inc .
 make -j${nproc} sparse-shared
 make install prefix=${prefix}
 install_license COPYRIGHT
 """
 
-# Dependencies that must be installed before this package can be built
-cuda_full_versions = Dict(
-    v"11.0" => v"11.0.3",
-)
-cuda_version = v"11.0"
-
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-# platforms = expand_cxxstring_abis(supported_platforms())
-platforms = expand_cxxstring_abis(Platform("x86_64", "linux"; 
-                                        cuda=CUDA.platform(cuda_version)))
+cuda_platforms = [
+    Platform("x86_64", "Linux"; cuda = "10.2"),
+    Platform("x86_64", "Linux"; cuda = "11.3"),
+]
+platforms = expand_cxxstring_abis(cuda_platforms)
 
 
 # The products that we will ensure are always built
@@ -46,7 +47,6 @@ products = [
 dependencies = [
     # You can only specify one cuda version in the deps. To build against more than 
     # one cuda version, you have to include them as Archive Sources. (see Torch_jll)
-    BuildDependency(PackageSpec(name="CUDA_full_jll", version=cuda_full_versions[cuda_version])),
     RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll")),
     Dependency("libblastrampoline_jll")
 ]
