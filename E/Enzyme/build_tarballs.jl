c3b6ff0737eaf58fe8ab8b5df692b506cdae31cb
vchuravy <vchuravy@users.noreply.github.com>
2022-12-28 17:35:35 +0000

    Upgrade enzyme to refs/tags/v.0.0.47

--- E/Enzyme/build_tarballs.jl
+++ E/Enzyme/build_tarballs.jl
@@ -8,13 +8,13 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
 name = "Enzyme"
 repo = "https://github.com/EnzymeAD/Enzyme.git"
 
-auto_version = "refs/tags/v0.0.46"
+auto_version = "refs/tags/v.0.0.47"
 version = VersionNumber(split(auto_version, "/")[end])
 
 llvm_versions = [v"11.0.1", v"12.0.1", v"13.0.1", v"14.0.2"]
 
 # Collection of sources required to build attr
-sources = [GitSource(repo, "ed65775ced38742b2617caabef88302e96adf000")]
+sources = [GitSource(repo, "d54daa7b40e9ec2d1db743064f4f0211c26272eb")]
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
