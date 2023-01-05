a4d529dbfa27fd29d2126be1a5105f68715eb1e8
Sam Kaplan <sam.kaplan@chevron.com>
2023-01-05 15:58:39 -0600

    use CC and dlext vars in build

--- A/AzStorage/build_tarballs.jl
+++ A/AzStorage/build_tarballs.jl
@@ -3,13 +3,13 @@
 using BinaryBuilder, Pkg
 
 name = "AzStorage"
-version = v"0.6.0"
+version = v"0.6.1"
 
 # Collection of sources required to build AzStorage
 sources = [
     GitSource(
         "https://github.com/ChevronETC/AzStorage.jl.git",
-        "4a0d10bd58334f2b44a8615f518d5b4b634f4633"
+        "f888c178ab0fa8bdbcc26694550109ad7fadbc03"
     )
 ]
 
@@ -22,7 +22,7 @@ if [[ ${target} == *mingw* ]]; then
     export LDFLAGS="-L${libdir}"
 fi
 
-make
+make yggdrasil
 
 install -Dvm 755 libAzStorage.so "${libdir}/libAzStorage.${dlext}"
 install -Dvm 644 AzStorage.h "${includedir}/AzStorage.h"
