452f5b0648c0fb080dec41caee7bfacffd489cbe
Handy Kurniawan <handykhandy@gmail.com>
2023-01-13 13:48:04 +0200

    Update build_tarballs.jl
    fix build script

--- Q/QuEST/build_tarballs.jl
+++ Q/QuEST/build_tarballs.jl
@@ -17,11 +17,8 @@ mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS=-std=c99 ..
 make
-mkdir ${prefix}/quest
-mkdir ${prefix}/quest/include
-mkdir ${prefix}/quest/lib
-cp $WORKSPACE/srcdir/QuEST-3.5.0/QuEST/include/* ${prefix}/quest/include/
-cp $WORKSPACE/srcdir/QuEST-3.5.0/build/QuEST/libQuEST.so ${prefix}/quest/lib/
+cp $WORKSPACE/srcdir/QuEST-3.5.0/QuEST/include/* ${prefix}/include/
+cp $WORKSPACE/srcdir/QuEST-3.5.0/build/QuEST/libQuEST.so ${prefix}/lib/
 exit
 """
 
@@ -38,7 +35,7 @@ platforms = [
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libQuEST", :libquest, "quest/lib")
+    LibraryProduct("libQuEST", :libquest)
 ]
 
 # Dependencies that must be installed before this package can be built
