b5caa8cfe21c5eb42ad38ad17be6a0a216e8c8ae
Brian Chen <ToucheSir@users.noreply.github.com>
2023-01-14 13:46:34 -0800

    Build MLIR-C on LLVM >= 14

--- L/LLVM/common.jl
+++ L/LLVM/common.jl
@@ -111,6 +111,9 @@ if [[ "${LLVM_MAJ_VER}" -gt "11" ]]; then
 else
     CMAKE_FLAGS+=(-DLLVM_ENABLE_PROJECTS='llvm;clang;clang-tools-extra')
 fi
+if [[ "${LLVM_MAJ_VER}" -ge "14" ]]; then
+    CMAKE_FLAGS+=(-DMLIR_BUILD_MLIR_C_DYLIB:BOOL=ON)
+fi
 CMAKE_FLAGS+=(-DCMAKE_CROSSCOMPILING=False)
 CMAKE_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_HOST_TOOLCHAIN})
 
@@ -542,6 +545,9 @@ function configure_build(ARGS, version; experimental_platforms=false, assert=fal
     if version >= v"12.0.1"
         push!(products, LibraryProduct(["MLIR", "libMLIR"], :mlir, dont_dlopen=true))
     end
+    if version >= v"14"
+        push!(products, LibraryProduct(["MLIR-C", "libMLIR-C"], :mlir_c, dont_dlopen=true))
+    end
     if version >= v"12"
         push!(products, LibraryProduct("libclang-cpp", :libclang_cpp, dont_dlopen=true))
         push!(products, ExecutableProduct("lld", :lld, "tools"))
@@ -610,6 +616,9 @@ function configure_extraction(ARGS, LLVM_full_version, name, libLLVM_version=not
         ]
         if v"12" <= version < v"13"
             push!(products, LibraryProduct("libMLIRPublicAPI", :libMLIRPublicAPI, dont_dlopen=true))
+        end
+        if version >= v"14"
+            push!(products, LibraryProduct(["MLIR-C", "libMLIR-C"], :mlir_c, dont_dlopen=true))
         end
     elseif name == "LLD"
         script = lldscript
