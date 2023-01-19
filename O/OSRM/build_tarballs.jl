f7b2080df1b638986ebc6f89e706e4dba34c7e69
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-01-19 16:38:25 +0100

    Add expand_cxxstring_abis

--- O/OSRM/build_tarballs.jl
+++ O/OSRM/build_tarballs.jl
@@ -40,6 +40,7 @@ cp libosrm_* ${libdir}
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = Product[
