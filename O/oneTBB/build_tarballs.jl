9242737c47bef2a82ff178cad54446025b57853d
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-01-19 17:04:26 +0100

    [oneTBB] Version bump

--- O/oneTBB/build_tarballs.jl
+++ O/oneTBB/build_tarballs.jl
@@ -3,11 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "oneTBB"
-version = v"2021.5.0"
+version_string = v"2021.8.0"
+version = VersionNumber(version_string)
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/oneapi-src/oneTBB/archive/refs/tags/v2021.5.0.tar.gz", "e5b57537c741400cf6134b428fc1689a649d7d38d9bb9c1b6d64f092ea28178a"),
+    ArchiveSource("https://github.com/oneapi-src/oneTBB/archive/refs/tags/v$version_string.tar.gz", "eee380323bb7ce864355ed9431f85c43955faaae9e9bce35c62b372d7ffd9f8b"),
     DirectorySource("./bundled"),
 ]
 
@@ -17,19 +18,6 @@ cd $WORKSPACE/srcdir/oneTBB*
 
 # We can't do Link-Time-Optimization with Clang, disable it.
 atomic_patch -p1 ../patches/clang-no-lto.patch
-
-if [[ ${target} == *-linux-musl* ]]; then
-    # Adapt patch from https://github.com/oneapi-src/oneTBB/pull/203
-    atomic_patch -p1 ../patches/musl.patch
-
-elif [[ ${target} == *-mingw* ]]; then
-    #derived from https://github.com/oneapi-src/oneTBB/commit/ce476173772f289c66ba98089618c1ff767ecea4, can hopefully be removed next release
-    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/lowercase-windows-include.patch
-    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/NOMINMAX-defines.patch
-    # `CreateSemaphoreEx` requires at least Windows Vista/Server 2008:
-    # https://docs.microsoft.com/en-us/windows/win32/api/winbase/nf-winbase-createsemaphoreexa
-    export CXXFLAGS="-D_WIN32_WINNT=0x0600"
-fi
 
 mkdir build && cd build/
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
@@ -50,7 +38,7 @@ platforms = expand_cxxstring_abis(supported_platforms())
 filter!(p -> arch(p) ∉ ("armv6l", "armv7l"), platforms)
 
 #i686 mingw fails with errors about _control87
-filter!(p -> !Sys.iswindows(p) || arch(p) != "i686", platforms)
+# filter!(p -> !Sys.iswindows(p) || arch(p) != "i686", platforms)
 
 # The products that we will ensure are always built
 products = [
