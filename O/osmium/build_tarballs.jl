b88e9c0378b686f819acc73648e7f4f0246d2bf7
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-01-20 17:27:49 +0000

    fix cxx 17 error

--- O/osmium/build_tarballs.jl
+++ O/osmium/build_tarballs.jl
@@ -19,7 +19,8 @@ mkdir build && cd build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17
 
 make -j${nproc}
 make install
