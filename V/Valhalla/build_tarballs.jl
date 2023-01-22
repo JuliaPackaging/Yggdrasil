665c52d5b4383da930dae7c89a73b46b341b6173
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-01-22 21:52:41 +0000

    gcc note

--- V/Valhalla/build_tarballs.jl
+++ V/Valhalla/build_tarballs.jl
@@ -66,3 +66,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"9")
+# probably could use 6/7/8, work way down if it builds..
