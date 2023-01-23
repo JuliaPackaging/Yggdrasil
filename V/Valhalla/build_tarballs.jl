d36d242daf5e8c3a66383efb3096c103a11c24d6
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-01-23 08:57:02 +0100

    add cmake prefix command

--- V/Valhalla/build_tarballs.jl
+++ V/Valhalla/build_tarballs.jl
@@ -18,7 +18,10 @@ git submodule update --init --recursive
 
 mkdir build && cd build
 
-cmake .. -DCMAKE_BUILD_TYPE=Release \
+cmake ..
+    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=ON \
     -DENABLE_DATA_TOOLS=OFF \
     -DENABLE_PYTHON_BINDINGS=OFF \
