89c33a80d8c9d7e0a9293b1aa06ffc5b26563e89
Michael F. Herbst <info@michael-herbst.com>
2023-01-27 22:49:52 +0100

    Avoid pulling in CUDA 12 in Libxc (#6160)
    * Avoid pulling in CUDA 12 in Libxc
    
    * Update build_tarballs.jl

--- L/Libxc/Libxc_GPU/build_tarballs.jl
+++ L/Libxc/Libxc_GPU/build_tarballs.jl
@@ -55,24 +55,25 @@ dependencies = [
 
 # XXX: support only specifying major/minor version (JuliaPackaging/BinaryBuilder.jl#/1212)
 cuda_full_versions = Dict(
-    v"10.2" => v"10.2.89",
-    v"11.0" => v"11.0.3"
+    v"11.0" => v"11.0.3",
+    v"12.0" => v"12.0.0"
 )
 
 # Build Libxc for all supported CUDA toolkits
 #
-# The library doesn't have specific CUDA requirements, so we only build for CUDA 10.2,
-# the oldest version supported by CUDA.jl, and 11.0, which (per semantic versioning)
-# should support every CUDA 11.x version.
+# The library doesn't have specific CUDA requirements, so we only build for CUDA 11.0 and 12.0,
+# which (per semantic versioning) should support every CUDA 11.x and 12.x version.
 #
-for cuda_version in [v"10.2", v"11.0"], platform in platforms
+# TODO Note: 12 not yet fully rolled out.
+for cuda_version in [v"11.0"], platform in platforms
     augmented_platform = Platform(arch(platform), os(platform); cuda=CUDA.platform(cuda_version))
     should_build_platform(triplet(augmented_platform)) || continue
 
     cuda_deps = [
         BuildDependency(PackageSpec(name="CUDA_full_jll",
                                     version=cuda_full_versions[cuda_version])),
-        RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll")),
+        RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll",
+                                      version=v"0.2")),  # avoid pulling in CUDA 12 for now.
     ]
 
     build_tarballs(ARGS, name, version, sources, script, [augmented_platform],
