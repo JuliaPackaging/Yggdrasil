f062bb1ecb8ea9da8390c803345e030e9339e408
Mosè Giordano <mose@gnu.org>
2023-01-28 00:46:36 +0000

    [libcellml] Use macOS 10.14 SDK for x86_64 darwin

--- L/libcellml/build_tarballs.jl
+++ L/libcellml/build_tarballs.jl
@@ -7,10 +7,23 @@ sources = [
     GitSource(
         "https://github.com/hsorby/libcellml",
         "bfd2537cc123b7d4b0521caa5d21b3e1c9638b83"),
+    ArchiveSource(
+        "https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
+        "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f"),
 ]
 
 # https://libcellml.org/documentation/guides/latest/installation/build_from_source
 script = raw"""
+# This requires macOS 10.14
+if [[ "${target}" == x86_64-apple-darwin* ]]; then
+    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
+    rm -rf /opt/${target}/${target}/sys-root/System
+    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
+    cp -ra System "/opt/${target}/${target}/sys-root/."
+    export MACOSX_DEPLOYMENT_TARGET=10.14
+    popd
+fi
+
 cd libcellml
 mkdir build && cd build
 cmake -DINSTALL_PREFIX=${prefix} \
@@ -23,6 +36,7 @@ cmake -DINSTALL_PREFIX=${prefix} \
     ..
 make -j${nproc}
 make install
+install_license ../LICENSE
 """
 
 # It doesn't look like this works with 32-bit systems
@@ -34,7 +48,7 @@ products = [
 
 dependencies = [
     Dependency("XML2_jll"),
-    Dependency("Zlib_jll")
+    Dependency("Zlib_jll"),
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"7")
