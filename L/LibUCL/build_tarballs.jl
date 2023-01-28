8582e948a2dfa4c5e62579df0d7580880d408679
Mosè Giordano <mose@gnu.org>
2023-01-28 18:18:41 +0000

    [LibUCL] Fix Windows build

--- L/LibUCL/build_tarballs.jl
+++ L/LibUCL/build_tarballs.jl
@@ -3,15 +3,24 @@ using BinaryBuilder
 name = "LibUCL"
 version = v"0.8.2"
 
-sources = [GitSource("https://github.com/vstakhov/libucl.git",
-                     "a615938cec3ae35c70aa2fb9845c9c12e5c7326f")]
+sources = [
+    GitSource("https://github.com/vstakhov/libucl.git",
+              "a615938cec3ae35c70aa2fb9845c9c12e5c7326f"),
+    DirectorySource("./bundled"),
+]
 
 script = raw"""
 cd ${WORKSPACE}/srcdir/libucl*
 ./autogen.sh
+FLAGS=()
 if [[ "${target}" == *-mingw* ]]; then
     cp "${includedir}/pcreposix.h" "${includedir}/regex.h"
     sed -i 's/-lregex/-lpcreposix-0/' configure
+    export LIBS="-lpcreposix-0"
+    export LDFLAGS="-L${libdir}"
+    # Patch upstreamed: https://github.com/vstakhov/libucl/pull/270
+    atomic_patch -p1 ../patches/win32.patch
+    FLAGS+=(LDFLAGS="${LDFLAGS} -no-undefined")
 fi
 ./configure \
     --enable-urls \
@@ -20,7 +29,7 @@ fi
     --prefix="${prefix}" \
     --build="${MACHTYPE}" \
     --host="${target}"
-make -j${nproc}
+make -j${nproc} "${FLAGS[@]}"
 make install
 
 if [[ "${target}" == *-mingw* ]]; then
