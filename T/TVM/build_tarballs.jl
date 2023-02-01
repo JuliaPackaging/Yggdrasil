4bbc0a044f74a6f122ac6f8e29b8701b2856c304
Gabriel Wu <wuzihua@pku.edu.cn>
2023-02-01 20:20:22 +0800

    expand LLVM versions

--- T/TVM/build_tarballs.jl
+++ T/TVM/build_tarballs.jl
@@ -2,8 +2,13 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
+include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
+
 name = "TVM"
 version = v"0.10.0"
+llvm_versions = [v"12.0.1", v"13.0.1", v"14.0.5"]
 
 # Collection of sources required to complete build
 sources = [
@@ -29,18 +34,22 @@ if [[ "$target" == *darwin* ]]; then
     cp -ra System "/opt/${target}/${target}/sys-root/."
     popd
 
-    export LLVM_LIBS="${libdir}/libLLVM-14.0.dylib"
+    export LLVM_LIBS="${libdir}/libLLVM-#LLVM_VER#.0.dylib"
 else
     if [[ "$target" == *mingw* ]]; then
-        export LLVM_LIBS="${bindir}/libLLVM-14jl.dll"
+        export LLVM_LIBS="${bindir}/libLLVM-#LLVM_VER#jl.dll"
+    else
+        export LLVM_LIBS="${libdir}/libLLVM-#LLVM_VER#jl.so"
     fi
 fi
 
 cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} \
          -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
          -DCMAKE_BUILD_TYPE=Release \
-         -DUSE_LLVM=ON \
+         -DUSE_LLVM="/opt/${target}/${target}/sys-root/tools/llvm-config --link-static" \
          -DLLVM_LIBS=${LLVM_LIBS} \
+         -DLLVM_INCLUDE_DIRS=${includedir} \
+         -DTVM_LLVM_VERSION=#LLVM_VER#0 \
          -G Ninja
 
 ninja
@@ -59,18 +68,54 @@ platforms = [
 ]
 platforms = expand_cxxstring_abis(platforms)
 
-# The products that we will ensure are always built
-products = [
-    LibraryProduct("libtvm", :libtvm),
-    LibraryProduct("libtvm_runtime", :libtvm_runtime),
-]
+augment_platform_block = """
+    using Base.BinaryPlatforms
+    $(LLVM.augment)
+    function augment_platform!(platform::Platform)
+        augment_llvm!(platform)
+    end"""
 
-# Dependencies that must be installed before this package can be built
-dependencies = Dependency[
-    Dependency(PackageSpec(name="Zlib_jll")),
-    Dependency(PackageSpec(name="LLVM_jll")),
-    Dependency(PackageSpec(name="MLIR_jll")),
-]
+# determine exactly which tarballs we should build
+builds = []
+for llvm_version in llvm_versions, llvm_assertions in (false, true)
+    # Dependencies that must be installed before this package can be built
+    llvm_name = llvm_assertions ? "LLVM_full_assert_jll" : "LLVM_full_jll"
+    dependencies = [
+        Dependency(PackageSpec(name="Zlib_jll")),
+        BuildDependency(PackageSpec(name=llvm_name, version=llvm_version)),
+        BuildDependency(PackageSpec(name="MLIR_jll", version=llvm_version)),
+    ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"8.1.0")
+    # The products that we will ensure are always built
+    products = [
+        LibraryProduct("libtvm", :libtvm),
+        LibraryProduct("libtvm_runtime", :libtvm_runtime),
+    ]
+
+    for platform in platforms
+        augmented_platform = deepcopy(platform)
+        augmented_platform[LLVM.platform_name] = LLVM.platform(llvm_version, llvm_assertions)
+
+        should_build_platform(triplet(augmented_platform)) || continue
+        push!(builds, (;
+            dependencies, products,
+            platforms=[augmented_platform],
+            script=replace(script, "#LLVM_VER#" => string(llvm_version.major))
+        ))
+    end
+end
+
+# don't allow `build_tarballs` to override platform selection based on ARGS.
+# we handle that ourselves by calling `should_build_platform`
+non_platform_ARGS = filter(arg -> startswith(arg, "--"), ARGS)
+
+# `--register` should only be passed to the latest `build_tarballs` invocation
+non_reg_ARGS = filter(arg -> arg != "--register", non_platform_ARGS)
+
+for (i, build) in enumerate(builds)
+    build_tarballs(i == lastindex(builds) ? non_platform_ARGS : non_reg_ARGS,
+        name, version, sources, build.script,
+        build.platforms, build.products, build.dependencies;
+        preferred_gcc_version=v"9", julia_compat="1.6",
+        augment_platform_block, lazy_artifacts=true)
+end
