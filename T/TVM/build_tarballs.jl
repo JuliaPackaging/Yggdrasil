b932406810ba052b4a85af2d92a24a4d335e9d73
Gabriel Wu <wuzihua@pku.edu.cn>
2023-02-01 13:07:26 +0800

    use MacOSX10.15 SDK

--- T/TVM/build_tarballs.jl
+++ T/TVM/build_tarballs.jl
@@ -7,7 +7,9 @@ version = v"0.10.0"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://dlcdn.apache.org/tvm/tvm-v$(version)/apache-tvm-src-v$(version).tar.gz", "2da001bf847636b32fc7a34f864abf01a46c69aaef0ff37cfbfbcc2eb5b0fce4")
+    ArchiveSource("https://dlcdn.apache.org/tvm/tvm-v$(version)/apache-tvm-src-v$(version).tar.gz", "2da001bf847636b32fc7a34f864abf01a46c69aaef0ff37cfbfbcc2eb5b0fce4"),
+    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
+        "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
 ]
 
 # Bash recipe for building across all platforms
@@ -18,7 +20,15 @@ mkdir build && cd build
 
 # setup LLVM_LIBS manually for non-Linux OS
 if [[ "$target" == *darwin* ]]; then
-    export LDFLAGS="-lc++abi"
+    # Work around "'value' is unavailable"
+    export MACOSX_DEPLOYMENT_TARGET=10.15
+    # ...and install a newer SDK which supports `std::filesystem`
+    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
+    rm -rf /opt/${target}/${target}/sys-root/System
+    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
+    cp -ra System "/opt/${target}/${target}/sys-root/."
+    popd
+
     export LLVM_LIBS="${libdir}/libLLVM-14.0.dylib"
 else
     if [[ "$target" == *mingw* ]]; then
@@ -30,8 +40,8 @@ cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} \
          -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
          -DCMAKE_BUILD_TYPE=Release \
          -DUSE_LLVM=ON \
-	 -DLLVM_LIBS=${LLVM_LIBS} \
-	 -G Ninja
+         -DLLVM_LIBS=${LLVM_LIBS} \
+         -G Ninja
 
 ninja
 ninja install
