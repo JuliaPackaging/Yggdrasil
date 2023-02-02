4cc8d0907a1022f4295c1b38f57847087442cc9a
Michael F. Herbst <info@michael-herbst.com>
2023-02-02 07:17:56 +0100

    Libxc GPU build for CUDA 12

--- L/Libxc/Libxc_GPU/build_tarballs.jl
+++ L/Libxc/Libxc_GPU/build_tarballs.jl
@@ -65,15 +65,14 @@ cuda_full_versions = Dict(
 # which (per semantic versioning) should support every CUDA 11.x and 12.x version.
 #
 # TODO Note: 12 not yet fully rolled out.
-for cuda_version in [v"11.0"], platform in platforms
+for cuda_version in [v"11.0", v"12.0"], platform in platforms
     augmented_platform = Platform(arch(platform), os(platform); cuda=CUDA.platform(cuda_version))
     should_build_platform(triplet(augmented_platform)) || continue
 
     cuda_deps = [
         BuildDependency(PackageSpec(name="CUDA_full_jll",
                                     version=cuda_full_versions[cuda_version])),
-        RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll",
-                                      version=v"0.2"), compat="0.2"),  # avoid pulling in CUDA 12 for now.
+        RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll")),
     ]
 
     build_tarballs(ARGS, name, version, sources, script, [augmented_platform],
