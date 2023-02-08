980c021caf59c482fe57dfbefa09929ccde3442b
Stephan Buchert <stephanb007@gmail.com>
2023-02-08 16:56:18 +0100

    NRLMSIS platform updated

--- N/NRLMSIS/build_tarballs.jl
+++ N/NRLMSIS/build_tarballs.jl
@@ -24,7 +24,8 @@ $FC $FFLAGS msis_constants.F90 msis_utils.F90 msis_init.F90 msis_gfn.F90 msis_tf
 
 # For testing with local deployment:
 # platforms = [Platform("x86_64", "linux", libgfortran_version="5.0.0")]
-platforms = [Platform("x86_64", "linux")]
+# platforms = [Platform("x86_64", "linux")]
+platforms = supported_platforms()
 platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
