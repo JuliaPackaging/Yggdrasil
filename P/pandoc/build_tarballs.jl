4c232d7055189fdb246b72239294df4147233aa5
Dheepak Krishnamurthy <me@kdheepak.com>
2023-02-09 03:18:20 -0500

    Fix linux-arm64 hash

--- P/pandoc/build_tarballs.jl
+++ P/pandoc/build_tarballs.jl
@@ -11,7 +11,7 @@ sources = [
     ArchiveSource("$(url_prefix)-macOS.zip", "5769f3aa42860d39059f971341fd813d0dbb78f52d2594edb29999f430c87af6"; unpack_target = "x86_64-apple-darwin14"),
     ArchiveSource("$(url_prefix)-macOS.zip", "5769f3aa42860d39059f971341fd813d0dbb78f52d2594edb29999f430c87af6"; unpack_target = "aarch64-apple-darwin20"),
     ArchiveSource("$(url_prefix)-windows-x86_64.zip", "32c8de0bd205eaf411d1990a1ecf6d9a6f67f8836bd39de80ea5f6226028d9fc"; unpack_target = "x86_64-w64-mingw32"),
-    ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "43f364915b9da64905fc3f6009f5542f224e54fb24f71043ef5154540f1a3983"; unpack_target = "aarch64-linux-gnu"),
+    ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "4e3441ed831f63eb624ba4080a1fbdaa4697390d6494973d0c4f2e5ba109e573"; unpack_target = "aarch64-linux-gnu"),
     FileSource("https://raw.githubusercontent.com/jgm/pandoc/$(pandoc_ver)/COPYRIGHT", "bec2a2261d16b5ffddde7e7f2f51d2131a1686006f32a61475a4054415d7e367"),
     FileSource("https://raw.githubusercontent.com/jgm/pandoc/$(pandoc_ver)/COPYING.md", "e7ea3adeab955103a837b692ca0017cb3abbed0d3dccbfa499d6b2b825d698c3"),
 ]
