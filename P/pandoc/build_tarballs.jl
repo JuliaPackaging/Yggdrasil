0be2c89590ceebf4930d3ae921fffee4b9fe4575
Dheepak Krishnamurthy <me@kdheepak.com>
2023-02-09 03:11:06 -0500

    Fix macos hash

--- P/pandoc/build_tarballs.jl
+++ P/pandoc/build_tarballs.jl
@@ -8,8 +8,8 @@ version = v"3.0.1"
 url_prefix = "https://github.com/jgm/pandoc/releases/download/$(pandoc_ver)/pandoc-$(pandoc_ver)"
 sources = [
     ArchiveSource("$(url_prefix)-linux-amd64.tar.gz", "b8b0051a3c27ab5802bb2a091c8dd5cdb6588ce7356a6d5c4e64fbf02225da04"; unpack_target = "x86_64-linux-gnu"),
-    ArchiveSource("$(url_prefix)-macOS.zip", "af0cda69e31e42f01ba6adc0aa779d3e5853e6c092beeb420a4fc22712d2110b"; unpack_target = "x86_64-apple-darwin14"),
-    ArchiveSource("$(url_prefix)-macOS.zip", "af0cda69e31e42f01ba6adc0aa779d3e5853e6c092beeb420a4fc22712d2110b"; unpack_target = "aarch64-apple-darwin20"),
+    ArchiveSource("$(url_prefix)-macOS.zip", "5769f3aa42860d39059f971341fd813d0dbb78f52d2594edb29999f430c87af6"; unpack_target = "x86_64-apple-darwin14"),
+    ArchiveSource("$(url_prefix)-macOS.zip", "5769f3aa42860d39059f971341fd813d0dbb78f52d2594edb29999f430c87af6"; unpack_target = "aarch64-apple-darwin20"),
     ArchiveSource("$(url_prefix)-windows-x86_64.zip", "e7a0c92b4af6cad31d9899a8b92a3992e18634320349bbf56b729bbbcf71bb99"; unpack_target = "x86_64-w64-mingw32"),
     ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "43f364915b9da64905fc3f6009f5542f224e54fb24f71043ef5154540f1a3983"; unpack_target = "aarch64-linux-gnu"),
     FileSource("https://raw.githubusercontent.com/jgm/pandoc/$(pandoc_ver)/COPYRIGHT", "bec2a2261d16b5ffddde7e7f2f51d2131a1686006f32a61475a4054415d7e367"),
