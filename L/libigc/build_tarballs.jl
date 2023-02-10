3afc0176a4c63df33bfe2b81a0265d1a06f02787
Tim Besard <tim.besard@gmail.com>
2023-02-10 20:39:30 +0100

    IGC: bump to v1.0.12812. (#6241)

--- L/libigc/build_tarballs.jl
+++ L/libigc/build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder
 
 name = "libigc"
-version = v"1.0.12504"#.5
+version = v"1.0.12812"#.26
 
 # IGC depends on LLVM, a custom Clang, and a Khronos tool. Instead of building these pieces
 # separately, taking care to match versions and apply Intel-specific patches where needed
@@ -12,15 +12,24 @@ version = v"1.0.12504"#.5
 
 # Collection of sources required to build IGC
 # NOTE: these hashes are taken from the release notes in GitHub,
-#       https://github.com/intel/intel-graphics-compiler/releases
+#       https://github.com/intel/intel-graphics-compiler/releases.
+#
+#       however, it seems like their Ubuntu build instrictions,
+#       as well as the CI build infrastructure, uses way newer
+#       sources, directly checking out upstream branches
+#       see https://github.com/intel/intel-graphics-compiler/blob/master/documentation/build_ubuntu.md
+#
+#       only the SPIRV-Tools and SPIRV-Headers versions are hard-coded,
+#       see https://github.com/intel/intel-graphics-compiler/blob/master/.github/workflows/build-IGC.yml
+#
 sources = [
-    GitSource("https://github.com/intel/intel-graphics-compiler.git", "b8fa033f17b86949d69451643563307dab596e47"),
-    GitSource("https://github.com/intel/opencl-clang.git", "363a5262d8c7cff3fb28f3bdb5d85c8d7e91c1bb"),
-    GitSource("https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git", "4ef524240833abfeee1c5b9fff6b1bd53f4806b3"),
-    GitSource("https://github.com/KhronosGroup/SPIRV-Tools.git", "45dd184c790d6bfc78a5a74a10c37e888b1823fa" #= sdk-1.3.204.1 =#),
-    GitSource("https://github.com/KhronosGroup/SPIRV-Headers.git", "b42ba6d92faf6b4938e6f22ddd186dbdacc98d78" #= sdk-1.3.204.1 =#),
-    GitSource("https://github.com/intel/vc-intrinsics.git", "b305ba31c063baa6257826cf133b5f31f9b12dfe" #= v0.7.1 =#),
-    GitSource("https://github.com/llvm/llvm-project.git", "1fdec59bffc11ae37eb51a1b9869f0696bfd5312" #= llvmorg-11.1.0 =#),
+    GitSource("https://github.com/intel/intel-graphics-compiler.git", "492c11b739568f3ef5f5a33952cfd841a44ae8b5"),
+    GitSource("https://github.com/intel/opencl-clang.git", "ee31812ea8b89d08c2918f045d11a19bd33525c5" #= branch ocl-open-110 =#),
+    GitSource("https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git", "9991d09a15f6cc0d331d800e6ddefe6c778e5fb6" #= branch llvm_release_110 =#),
+    GitSource("https://github.com/KhronosGroup/SPIRV-Tools.git", "45dd184c790d6bfc78a5a74a10c37e888b1823fa" #= tag sdk-1.3.204.1 =#),
+    GitSource("https://github.com/KhronosGroup/SPIRV-Headers.git", "b42ba6d92faf6b4938e6f22ddd186dbdacc98d78" #= tag sdk-1.3.204.1 =#),
+    GitSource("https://github.com/intel/vc-intrinsics.git", "dd72efa3b4aafdbbf599e6f3c6f8c55450e348de" #= latest version: v0.11.0 =#),
+    GitSource("https://github.com/llvm/llvm-project.git", "1fdec59bffc11ae37eb51a1b9869f0696bfd5312" #= branch llvmorg-11.1.0 =#),
     # patches
     DirectorySource("./bundled"),
 ]
