e6b5f66967397b4e842fdc099822419891398193
Phillip Alday <palday@users.noreply.github.com>
2023-02-10 20:46:07 +0000

    update pandoc_crossref for pandoc 3.0 (#6246)

--- P/pandoc_crossref/build_tarballs.jl
+++ P/pandoc_crossref/build_tarballs.jl
@@ -3,15 +3,15 @@ using BinaryBuilder
 # Collection of pre-build pandoc binaries
 name = "pandoc_crossref"
 
-crossref_ver = "0.3.14.0"
-version = v"0.3.14"
+crossref_ver = "0.3.15.0"
+version = v"0.3.15"
 
 url_prefix = "https://github.com/lierdakil/pandoc-crossref/releases/download/v$(crossref_ver)/pandoc-crossref"
 sources = [
-    ArchiveSource("$(url_prefix)-Linux.tar.xz", "9ad4aff57d58198b9d97fef71319c5b060ebcfc570055d87aa95b4117b40db56"; unpack_target = "x86_64-linux-gnu"),
-    ArchiveSource("$(url_prefix)-macOS.tar.xz", "b29ebe076ecd462bf567f7d0774f70177aa109999a3c0fa542a6c3564ad50e2f"; unpack_target = "x86_64-apple-darwin14"),
-    ArchiveSource("$(url_prefix)-macOS.tar.xz", "b29ebe076ecd462bf567f7d0774f70177aa109999a3c0fa542a6c3564ad50e2f"; unpack_target = "aarch64-apple-darwin20"),
-    FileSource("$(url_prefix)-Windows.7z", "f07e7e64e7a2260d43e93941387aa320914f3e161697bce305622d4250040cb3"; filename = "x86_64-w64-mingw32"),
+    ArchiveSource("$(url_prefix)-Linux.tar.xz", "07951a4dd43ec221863b6ee8a3721c7354a07ecb1e6066fa405de522ec5987fd"; unpack_target = "x86_64-linux-gnu"),
+    ArchiveSource("$(url_prefix)-macOS.tar.xz", "4ddadd2e6a5ed4abbbe1d58ac9ff235789615f6b4e69e8bc5f3dbe50ff3eda8b"; unpack_target = "x86_64-apple-darwin14"),
+    ArchiveSource("$(url_prefix)-macOS.tar.xz", "4ddadd2e6a5ed4abbbe1d58ac9ff235789615f6b4e69e8bc5f3dbe50ff3eda8b"; unpack_target = "aarch64-apple-darwin20"),
+    FileSource("$(url_prefix)-Windows.7z", "fcfa2be7f2a07875deb21a84fa1ec0b3399f23ad4758caf8d8e463663260fb9e"; filename = "x86_64-w64-mingw32"),
     FileSource("https://raw.githubusercontent.com/lierdakil/pandoc-crossref/v$(crossref_ver)/LICENSE", "39db8f9acf036595a2566ea3fe560bc7bd65d8749f088e0f4a4ef2f8a6cb4b34"),
 ]
 
