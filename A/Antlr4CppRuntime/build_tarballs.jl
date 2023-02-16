1b2e2c0eb4ea28998961bd531161b68d881bbabf
George Kastrinis <george.kast@gmail.com>
2023-02-16 12:05:59 +0200

    Trying to fix the build script

--- A/Antlr4CppRuntime/build_tarballs.jl
+++ A/Antlr4CppRuntime/build_tarballs.jl
@@ -15,7 +15,6 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/antlr4*/runtime/Cpp
-atomic_patch -p1 ../../../patches/forward-cmake-toolchain.patch
 mkdir build && cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
 make -j${nproc}
