869987881cc2a72b23c6987a90bbb8955b7c8d6a
jochalek <68436920+jochalek@users.noreply.github.com>
2023-02-16 21:40:32 -0500

    [JpegTurbo] Update to 2.1.91

--- J/JpegTurbo/build_tarballs.jl
+++ J/JpegTurbo/build_tarballs.jl
@@ -1,12 +1,12 @@
 using BinaryBuilder
 
 name = "JpegTurbo"
-version = v"2.1.2"
+version = v"2.1.91"
 
 # Collection of sources required to build Ogg
 sources = [
     ArchiveSource("https://github.com/libjpeg-turbo/libjpeg-turbo/archive/$(version).tar.gz",
-                  "e7fdc8a255c45bc8fbd9aa11c1a49c23092fcd7379296aeaeb14d3343a3d1bed"),
+                  "e08989cfe6dfd27fba24696f2f2255402bef0c8d987000b96d28266714d4ec94"),
 ]
 
 # Bash recipe for building across all platforms
@@ -16,7 +16,7 @@ cd $WORKSPACE/srcdir/libjpeg-turbo-*/
 mkdir build
 cd build
 
-cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}"
+cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" -DCMAKE_POSITION_INDEPENDENT_CODE=ON
 make -j${nproc}
 make install
 """
