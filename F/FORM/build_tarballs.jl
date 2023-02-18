73b0fd83f97df4179efa2d85f7e2fab9d6b3824f
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2023-02-18 23:06:19 +0800

    add `MicrosoftMPI_jll` for Windows.

--- F/FORM/build_tarballs.jl
+++ F/FORM/build_tarballs.jl
@@ -21,17 +21,18 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("x86_64", "macos"; ),
-    Platform("aarch64", "macos"; ),
-    Platform("x86_64", "freebsd"; )
-]
+# platforms = [
+#     Platform("x86_64", "linux"; libc = "glibc"),
+#     Platform("aarch64", "linux"; libc = "glibc"),
+#     Platform("powerpc64le", "linux"; libc = "glibc"),
+#     Platform("x86_64", "linux"; libc = "musl"),
+#     Platform("aarch64", "linux"; libc = "musl"),
+#     Platform("x86_64", "macos"; ),
+#     Platform("aarch64", "macos"; ),
+#     Platform("x86_64", "freebsd"; )
+# ]
 
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
@@ -44,7 +45,8 @@ products = [
 dependencies = [
     Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"))
     Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a"))
-    Dependency(PackageSpec(name="MPICH_jll", uuid="7cb0a576-ebde-5e09-9194-50597f1243b4"))
+    Dependency(PackageSpec(name="MPICH_jll", uuid="7cb0a576-ebde-5e09-9194-50597f1243b4"); platforms=filter(!Sys.iswindows, platforms))
+    Dependency(PackageSpec(name="MicrosoftMPI_jll", uuid="9237b28f-5490-5468-be7b-bb81f5f5e6cf"); platforms=filter(Sys.iswindows, platforms))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
