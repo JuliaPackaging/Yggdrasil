8037b4b7169aa4436f286a6d2e6f6e2fbe63ce79
Mark Kittisopikul <mkitti@users.noreply.github.com>
2023-02-22 18:26:04 -0500

    Build libaec for all platforms (#6299)

--- L/libaec/build_tarballs.jl
+++ L/libaec/build_tarballs.jl
@@ -27,7 +27,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; exclude=!Sys.islinux)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
