a262ca99585392347a4034eeb01576e02b56ff5a
Devansh Ramgopal Agrawal <devansh@umich.edu>
2023-02-23 16:16:01 -0500

    removed windows from platforms

--- D/DecompUtil/build_tarballs.jl
+++ D/DecompUtil/build_tarballs.jl
@@ -21,7 +21,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = [p for p in supported_platforms() if p.tags["os"] != "windows"]
 
 # The products that we will ensure are always built
 products = [
