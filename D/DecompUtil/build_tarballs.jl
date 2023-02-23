b6b5df1bf244332413454d50f18f29caf66bdc8e
Devansh Ramgopal Agrawal <devansh@umich.edu>
2023-02-23 16:06:26 -0500

    missing dollar sign

--- D/DecompUtil/build_tarballs.jl
+++ D/DecompUtil/build_tarballs.jl
@@ -15,7 +15,7 @@ script = raw"""
 cd $WORKSPACE/srcdir/DecompUtil_C
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release .. 
-make -j{nproc}
+make -j${nproc}
 make install
 """
 
