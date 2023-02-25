857b932484c7f37c340c33881b5b4c42e116005a
Pere Mato <peremato@users.noreply.github.com>
2023-02-25 23:31:41 +0100

    [Xerces] Upgrade to version 3.2.4 (#6308)
    * Upgrade version
    
    * julia_compat
    
    * fixes
    
    * fix warning
    
    * undo
    
    * Update X/Xerces/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Update X/Xerces/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Update X/Xerces/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- X/Xerces/build_tarballs.jl
+++ X/Xerces/build_tarballs.jl
@@ -3,23 +3,28 @@
 using BinaryBuilder, Pkg
 
 name = "Xerces"
-version = v"3.2.3"
+version = v"3.2.4"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://apache.mirror.digionline.de//xerces/c/3/sources/xerces-c-3.2.3.tar.gz", "fb96fc49b1fb892d1e64e53a6ada8accf6f0e6d30ce0937956ec68d39bd72c7e"),
+    ArchiveSource("https://dlcdn.apache.org/xerces/c/3/sources/xerces-c-$(version).tar.gz", "3d8ec1c7f94e38fee0e4ca5ad1e1d9db23cbf3a10bba626f6b4afa2dedafe5ab"),
     DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd xerces-c-3.2.3/
+cd $WORKSPACE/srcdir/xerces-*/
 atomic_patch -p1 "${WORKSPACE}/srcdir/ThreadTest.patch"
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -Dnetwork:BOOL=OFF
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=$prefix \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -Dnetwork:BOOL=OFF \
+      ..
 make -j${nproc}
 make install
-install_license ${WORKSPACE}/srcdir/xerces-c-3.2.3/LICENSE
+
+install_license ../LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
@@ -36,4 +41,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
