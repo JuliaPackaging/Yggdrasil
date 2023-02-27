6c580f6dcf573992ccdc539434e88ff4ec40a21b
Mark Kittisopikul <mkitti@users.noreply.github.com>
2023-02-27 04:56:47 -0500

    Add MbedTLS 2.28.2, compat Julia 1.9 (#6323)
    * Add MbedTLS 2.28.2, compat Julia 1.9
    
    * Fix typo
    
    * Use GitSource instead of ArchiveSource

--- M/MbedTLS/common.jl
+++ M/MbedTLS/common.jl
@@ -5,28 +5,38 @@ name = "MbedTLS"
 # Collection of sources required to build MbedTLS
 sources_by_version = Dict(
     v"2.24.0" => [
-        GitSource("https://github.com/ARMmbed/mbedtls.git",
+        GitSource("https://github.com/Mbed-TLS/mbedtls.git",
                   "523f0554b6cdc7ace5d360885c3f5bbcc73ec0e8"),
         DirectorySource("./bundled"; follow_symlinks=true),
     ],
     v"2.25.0" => [
-        GitSource("https://github.com/ARMmbed/mbedtls.git",
+        GitSource("https://github.com/Mbed-TLS/mbedtls.git",
                   "1c54b5410fd48d6bcada97e30cac417c5c7eea67"),
         DirectorySource("./bundled"; follow_symlinks=true),
     ],
     v"2.26.0" => [
-        GitSource("https://github.com/ARMmbed/mbedtls.git",
+        GitSource("https://github.com/Mbed-TLS/mbedtls.git",
                   "e483a77c85e1f9c1dd2eb1c5a8f552d2617fe400"),
         DirectorySource("./bundled"; follow_symlinks=true),
     ],
     v"2.27.0" => [
-        GitSource("https://github.com/ARMmbed/mbedtls.git",
+        GitSource("https://github.com/Mbed-TLS/mbedtls.git",
                   "f71e2878084126737cc39083e1e15afc459bd93d"),
         DirectorySource("./bundled"; follow_symlinks=true),
     ],
     v"2.28.0" => [
-        GitSource("https://github.com/ARMmbed/mbedtls.git",
+        GitSource("https://github.com/Mbed-TLS/mbedtls.git",
                   "8b3f26a5ac38d4fdccbc5c5366229f3e01dafcc0"),
+        DirectorySource("./bundled"; follow_symlinks=true),
+    ],
+    v"2.28.1" => [
+        GitSource("https://github.com/Mbed-TLS/mbedtls.git",
+                  "dd79db10014d85b26d11fe57218431f2e5ede6f2"),
+        DirectorySource("./bundled"; follow_symlinks=true),
+    ],
+    v"2.28.2" => [
+        GitSource("https://github.com/Mbed-TLS/mbedtls.git",
+                  "89f040a5c938985c5f30728baed21e49d0846a53"),
         DirectorySource("./bundled"; follow_symlinks=true),
     ],
 )
