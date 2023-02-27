5c29fb903975b0d8ff674fb0c89e6ad6e756dc5b
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-02-27 02:23:36 +0100

    [FAMSA] prefer gcc version 10

--- F/FAMSA/build_tarballs.jl
+++ F/FAMSA/build_tarballs.jl
@@ -82,4 +82,4 @@ dependencies = Dependency[
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version = v"11")
+               julia_compat="1.6", preferred_gcc_version = v"10")
