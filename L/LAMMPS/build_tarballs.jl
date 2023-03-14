45d7b6a42aabe10aa0c5ad40597cdf57f6f2b633
Valentin Churavy <vchuravy@users.noreply.github.com>
2023-03-14 11:09:47 -0400

    Update L/LAMMPS/build_tarballs.jl

--- L/LAMMPS/build_tarballs.jl
+++ L/LAMMPS/build_tarballs.jl
@@ -13,7 +13,7 @@ version = v"2.3.2" # Equivalent to 23Jun2022_update3
 # 2.0.0 -> https://github.com/lammps/lammps/releases/tag/stable_29Sep2021
 # 2.2.0 -> https://github.com/lammps/lammps/releases/tag/stable_29Sep2021_update2
 # 2.3.0 -> https://github.com/lammps/lammps/releases/tag/stable_23Jun2022_update1
-# 2.3.3 -> https://github.com/lammps/lammps/releases/tag/stable_23Jun2022_update3
+# 2.3.2 -> https://github.com/lammps/lammps/releases/tag/stable_23Jun2022_update3
 
 # Collection of sources required to complete build
 sources = [
