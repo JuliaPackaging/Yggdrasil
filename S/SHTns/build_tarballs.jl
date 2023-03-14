a97d00a215e5f53d93b8fa3d1d49940b81989196
Felix Gerick <felixgerick@gmail.com>
2023-03-14 18:25:45 +0100

    [SHTns] bump v3.5.2, build microarchitectures
    also hack around fftw_omp requirement of shtns to allow SHTns_jll to use FFTW_jll threads.

--- S/SHTns/build_tarballs.jl
+++ S/SHTns/build_tarballs.jl
@@ -3,34 +3,36 @@
 using BinaryBuilder, Pkg
 
 name = "SHTns"
-version = v"3.5.1"
+version = v"3.5.2"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://bitbucket.org/nschaeff/shtns/downloads/shtns-$(version).tar.gz", "77f8a33b94df8786d2ce9b95cbfbe548f00443625b310b8c64012b22c8a7394f")
+    ArchiveSource("https://bitbucket.org/nschaeff/shtns/downloads/shtns-$(version).tar.gz", "dc4ac08c09980e47c71d79d38696c5d1d631f86c2af1ce8aad5d21f7fd2c05b9")
 ]
 
 # Bash recipe for building across all platforms
+#if [[ "${target}" == *-apple-* ]]; then
+    # For some reasons, `configure` insists on setting CC2=gcc, also on macOS
+#    sed -i -e 's/gcc/cc/' -e 's/ -fno-tree-loop-distribute-patterns//' Makefile
+#fi
+#
 script = raw"""
 cd $WORKSPACE/srcdir/shtns/
-export CFLAGS="-fPIC"
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
-if [[ "${target}" == *-apple-* ]]; then
-    # For some reasons, `configure` insists on setting CC2=gcc, also on macOS
-    sed -i -e 's/gcc/cc/' -e 's/ -fno-tree-loop-distribute-patterns//' Makefile
-fi
+export CFLAGS=" -DFFTW_PLAN_SAFE"
+sed -i -e '4908,4957d' configure #removes openmp fftw checks, this is fine as we link to FFTW_jll
+./configure --prefix=${prefix} --host=${target} --enable-openmp
 make -j${nproc}
 make install
 mkdir -p ${libdir}
-cc -shared -o "${libdir}/libshtns.${dlext}" *.o -lfftw3
-rm "${prefix}/lib/libshtns.a"
+cc -shared -o "${libdir}/libshtns.${dlext}" *.o -lfftw3 
+#rm "${prefix}/lib/libshtns_omp.a"
 install_license LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-
+platforms = expand_microarchitectures(supported_platforms())
+#platforms = expand_cxxstring_abis(expand_microarchitectures(supported_platforms(), ["x86_64", "avx", "avx2", "avx512"]))
 
 # The products that we will ensure are always built
 products = [
@@ -43,4 +45,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6") #, preferred_gcc_version=v"11")
+
