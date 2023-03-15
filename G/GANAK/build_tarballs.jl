0832f936baa1824799ba1068bc028d21d14e3d66
Benjamin Lorenz <lorenz@math.tu-berlin.de>
2023-03-15 09:26:20 +0100

    [GANAK] support more platforms
    link to all dependencies
    make logtable signed everywhere
    disable pcc for platforms other than x86_64-linux

--- G/GANAK/build_tarballs.jl
+++ G/GANAK/build_tarballs.jl
@@ -8,16 +8,34 @@ version = v"0.1.0"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/meelgroup/ganak.git", "36fa4b35c8e3cb588a3a76a87624d97ae7f18285")
+    DirectorySource("bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd ganak/
+
+# add all dependencies for linking without 'allow undefined'
+atomic_patch -p1 ../patches/alllibs.patch
+
+# make sure char in LogTable is signed on all platforms (needed for storing -1)
+atomic_patch -p1 ../patches/logtab_signed.patch
+
 mkdir build; cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_SKIP_BUILD_RPATH=TRUE ..
+
+if [[ ${target} != x86_64-linux* ]]; then
+  # clhash only works on x86_64 linux platforms
+  disablepcc=-DDOPCC=OFF
+fi
+
+cmake $disablepcc \
+      -DCMAKE_INSTALL_PREFIX=$prefix \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_SKIP_BUILD_RPATH=TRUE \
+      ..
 make -j${nproc}
 install -vc ganak -Dt $bindir 
-install -vc src/libganak.${dlext}.* -Dt $libdir
+install -vc src/libganak* -Dt $libdir
 cp -d src/libganak.${dlext} $libdir
 install -vc src/clhash/libclhash* -Dt $libdir
 install -vc src/component_types/libcomponent_types* -Dt $libdir
@@ -25,10 +43,7 @@ install -vc src/component_types/libcomponent_types* -Dt $libdir
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "musl")
-]
+platforms = filter(p->(Sys.islinux(p) || Sys.isapple(p)), supported_platforms())
 platforms = expand_cxxstring_abis(platforms)
 
 
