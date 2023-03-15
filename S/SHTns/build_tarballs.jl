1f2bf9d2f03e29c45487282bdf44cc4c042de45c
Felix Gerick <felixgerick@gmail.com>
2023-03-15 18:54:21 +0100

    use openmp properly and remove gcc.
    removing lfftw3_omp reference from configure file seems to work to get around issue

--- S/SHTns/build_tarballs.jl
+++ S/SHTns/build_tarballs.jl
@@ -13,17 +13,19 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/shtns/
-export CC="gcc"
 export CFLAGS="-fPIC -O3" #only -fPIC produces slow code on linux x86 and MacOS x86 (maybe others)
-# I remove openmp fftw checks, because FFTW_jll doesn't provide -lfftw3_omp
-# propaly not the safest way!
-sed -i -e '4908,4957d' configure 
+
+#remove lfftw3_omp library references, as FFTW_jll does not provide it
+sed -i -e 's/lfftw3_omp/lfftw3/' configure
+
 ./configure --prefix=${prefix} --host=${target} --enable-openmp
 make -j${nproc}
 make install
+
 mkdir -p ${libdir}
-gcc -fopenmp -shared -o "${libdir}/libshtns.${dlext}" *.o -lfftw3     
+cc -fopenmp -shared -o "${libdir}/libshtns.${dlext}" *.o -lfftw3     
 rm "${prefix}/lib/libshtns_omp.a"
+
 install_license LICENSE
 """
 
@@ -39,12 +41,14 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a")),
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
+    # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
+    Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
                     julia_compat="1.6", 
-                    # preferred_gcc_version=v"12", 
                     lock_microarchitecture=false,
                     )
