f3560274b80b56c0589433310fb3c27ba8f16bb7
Mosè Giordano <giordano@users.noreply.github.com>
2023-03-15 22:19:33 +0000

    Apply suggestions from code review
    Co-authored-by: Benjamin Lorenz <benlorenz@users.noreply.github.com>

--- G/GANAK/build_tarballs.jl
+++ G/GANAK/build_tarballs.jl
@@ -54,7 +54,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"); compat="6.1.2")
+    Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"); compat="6.2.0")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
