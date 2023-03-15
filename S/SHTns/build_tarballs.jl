0545a0255530a6b32a3f8f4bfecf618e5a50f317
Felix Gerick <felixgerick@gmail.com>
2023-03-15 16:27:25 +0100

    gcc and -O3
    switch to gcc compilation completely to make -fopenmp work and add -O3 flag to CFLAGS because somehow only -fPIC produced much slower code on the two tested machines (linux x86 and intel apple x86).

--- S/SHTns/build_tarballs.jl
+++ S/SHTns/build_tarballs.jl
@@ -11,28 +11,25 @@ sources = [
 ]
 
 # Bash recipe for building across all platforms
-#if [[ "${target}" == *-apple-* ]]; then
-    # For some reasons, `configure` insists on setting CC2=gcc, also on macOS
-#    sed -i -e 's/gcc/cc/' -e 's/ -fno-tree-loop-distribute-patterns//' Makefile
-#fi
-#
 script = raw"""
 cd $WORKSPACE/srcdir/shtns/
-export CFLAGS="-fPIC -DFFTW_PLAN_SAFE"
-sed -i -e '4908,4957d' configure #removes openmp fftw checks, this is fine as we link to FFTW_jll
+export CC="gcc"
+export CFLAGS="-fPIC -O3" #only -fPIC produces slow code on linux x86 and MacOS x86 (maybe others)
+# I remove openmp fftw checks, because FFTW_jll doesn't provide -lfftw3_omp
+# propaly not the safest way!
+sed -i -e '4908,4957d' configure 
 ./configure --prefix=${prefix} --host=${target} --enable-openmp
 make -j${nproc}
 make install
 mkdir -p ${libdir}
-cc -shared -o "${libdir}/libshtns.${dlext}" *.o -lfftw3 
-#rm "${prefix}/lib/libshtns_omp.a"
+gcc -fopenmp -shared -o "${libdir}/libshtns.${dlext}" *.o -lfftw3     
+rm "${prefix}/lib/libshtns_omp.a"
 install_license LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_microarchitectures(supported_platforms())
-#platforms = expand_cxxstring_abis(expand_microarchitectures(supported_platforms(), ["x86_64", "avx", "avx2", "avx512"]))
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
@@ -41,9 +38,13 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
+    Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a")),
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6") #, preferred_gcc_version=v"11")
-
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
+                    julia_compat="1.6", 
+                    # preferred_gcc_version=v"12", 
+                    lock_microarchitecture=false,
+                    )
