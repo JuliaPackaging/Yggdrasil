c74afb22c75ffe19b70424a33cf8950c328e6573
Johannes Markert <johannes.markert@dlr.de>
2023-03-16 15:26:58 +0100

    Updated build recipe for t8code to support more build targets

--- T/t8code/build_tarballs.jl
+++ T/t8code/build_tarballs.jl
@@ -3,19 +3,31 @@
 using BinaryBuilder, Pkg
 
 name = "t8code"
-version = v"1.1.0"
+version = v"1.1.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/DLR-AMR/t8code/releases/download/v$(version)/t8code_v$(version).tar.gz", "01ceae6dd0ed596cf1abb5bc78caa77b5da94ca60d080aae3ccf0ba3c6464deb")
+    ArchiveSource("https://github.com/DLR-AMR/t8code/releases/download/v$(version)/t8code_v$(version).tar.gz", "fcacc0ab511b38a607e05ec6b7c28333937bee86e1472107078382f22b03938e")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd t8code/
-./configure --enable-mpi CFLAGS=-O3 CXXFLAGS=-O3 CC=mpicc CXX=mpicxx --prefix=${prefix} --build=${MACHTYPE} --host=${target}
-make -j
+if [[ "${target}" == *-freebsd* ]]; then
+  export LIBS="-lm"
+fi
+# Set default preprocessor and linker flags
+export CPPFLAGS="-I${includedir}"
+export LDFLAGS="-L${libdir}"
+# Use MPI including MPI I/O on all other platforms
+export CC="mpicc"
+export CXX="mpicxx"
+mpiopts="--enable-mpi"
+# Configure, build, install
+# Note: BLAS is disabled since it is only needed for SC if it is used outside of p4est
+./configure --disable-static --without-blas ${mpiopts} CFLAGS='-O3 -std=c99' CXXFLAGS='-O3 -std=c++11' --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make -j${nproc}
 make install
 """
 
@@ -27,7 +39,15 @@ platforms = [
     Platform("aarch64", "linux"; libc = "glibc"),
     Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
     Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc")
+    Platform("powerpc64le", "linux"; libc = "glibc"),
+    Platform("i686", "linux"; libc = "musl"),
+    Platform("x86_64", "linux"; libc = "musl"),
+    Platform("aarch64", "linux"; libc = "musl"),
+    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
+    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
+    Platform("x86_64", "macos"; ),
+    Platform("aarch64", "macos"; ),
+    Platform("x86_64", "freebsd"; )
 ]
 
 
@@ -41,8 +61,6 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="MPICH_jll", uuid="7cb0a576-ebde-5e09-9194-50597f1243b4"))
-    Dependency(PackageSpec(name="MPItrampoline_jll", uuid="f1f71cc9-e9ae-5b93-9b94-4fe0e1ad3748"))
-    Dependency(PackageSpec(name="MicrosoftMPI_jll", uuid="9237b28f-5490-5468-be7b-bb81f5f5e6cf"))
     Dependency(PackageSpec(name="OpenMPI_jll", uuid="fe0851c0-eecd-5654-98d4-656369965a5c"))
     Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a"))
 ]
