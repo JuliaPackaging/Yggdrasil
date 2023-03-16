2507712753588b1670cd0cbfdc288c5cfa15caa6
Felix <felixgerick@gmail.com>
2023-03-16 15:59:26 +0100

    gcc v7

--- S/SHTns/build_tarballs.jl
+++ S/SHTns/build_tarballs.jl
@@ -66,6 +66,6 @@ dependencies = [
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
                 julia_compat="1.6", 
-                preferred_gcc_version=v"12",
+                preferred_gcc_version=v"7",
                 augment_platform_block)
 
