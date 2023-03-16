fb6fa946a13feda7320c443a5a0e364861d0d6e6
Mosè Giordano <giordano@users.noreply.github.com>
2023-03-16 17:21:38 +0000

    Update build_tarballs.jl

--- S/SuperLU_DIST/build_tarballs.jl
+++ S/SuperLU_DIST/build_tarballs.jl
@@ -108,7 +108,6 @@ dependencies = [
     # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
-
 ]
 append!(dependencies, platform_dependencies)
 
