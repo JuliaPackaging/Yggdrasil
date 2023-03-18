98c60505858b3cb27c2101223b537d7cc75cb450
Norman <norman.chen.git@gmail.com>
2023-03-18 00:53:14 -0700

    Remove the redundant flags

--- C/CeresSolver/build_tarballs.jl
+++ C/CeresSolver/build_tarballs.jl
@@ -41,7 +41,7 @@ CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix}
 
 cd $WORKSPACE/srcdir/ceres-solver/
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ${CMAKE_FLAGS[@]}
+cmake .. ${CMAKE_FLAGS[@]} -DCMAKE_BUILD_TYPE=Release
 make -j${nproc}
 make install
 
