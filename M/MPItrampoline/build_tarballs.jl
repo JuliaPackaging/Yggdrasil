857f2f80c8d4f12779ec92e0b5600c070c5fa67b
Mosè Giordano <giordano@users.noreply.github.com>
2023-03-18 15:39:56 +0000

    Apply suggestions from code review

--- M/MPItrampoline/build_tarballs.jl
+++ M/MPItrampoline/build_tarballs.jl
@@ -139,11 +139,8 @@ if [[ "${target}" == aarch64-apple-* ]]; then
 fi
 
 # Do not install doc and man files which contain files which clashing names on
-
 # case-insensitive file systems:
-
 # * https://github.com/JuliaPackaging/Yggdrasil/pull/315
-
 # * https://github.com/JuliaPackaging/Yggdrasil/issues/6344
 ./configure \
     --build=${MACHTYPE} \
