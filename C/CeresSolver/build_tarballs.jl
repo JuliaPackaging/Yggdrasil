ba80d6496fba7d950b1755a547fca91302541747
Norman <norman.chen.git@gmail.com>
2023-03-19 11:19:03 -0700

    Try GCC version 8

--- C/CeresSolver/build_tarballs.jl
+++ C/CeresSolver/build_tarballs.jl
@@ -50,4 +50,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8", julia_compat="1.6")
