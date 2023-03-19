1bb6f60c2d8fdc16f1ee810cdbfa894a63aae237
Norman <norman.chen.git@gmail.com>
2023-03-18 21:20:11 -0700

    Use Eigen v3.3.9 on powerpc64le

--- C/CeresSolver/build_tarballs.jl
+++ C/CeresSolver/build_tarballs.jl
@@ -19,14 +19,6 @@ CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix}
               -DBLAS_LIBRARIES=${libdir}/libopenblas.${dlext}
               -DLAPACK_LIBRARIES=${libdir}/libopenblas.${dlext}
               -DMETIS_LIBRARY=${libdir}/libmetis.${dlext}
-              -DSUITESPARSE_INCLUDE_DIR_HINTS=${prefix}/include
-              -DAMD_LIBRARY="${libdir}/libamd.${dlext} ${libdir}/libsuitesparseconfig.${dlext}"
-              -DCAMD_LIBRARY="${libdir}/libcamd.${dlext} ${libdir}/libsuitesparseconfig.${dlext}"
-              -DCCOLAMD_LIBRARY="${libdir}/libccolamd.${dlext} ${libdir}/libsuitesparseconfig.${dlext}"
-              -DCHOLMOD_LIBRARY="${libdir}/libcholmod.${dlext} ${libdir}/libsuitesparseconfig.${dlext}"
-              -DCOLAMD_LIBRARY="${libdir}/libcolamd.${dlext} ${libdir}/libsuitesparseconfig.${dlext}"
-              -DSUITESPARSEQR_LIBRARY="${libdir}/libspqr.${dlext} ${libdir}/libsuitesparseconfig.${dlext}"
-              -DSUITESPARSE_CONFIG_LIBRARY="${libdir}/libsuitesparseconfig.${dlext}"
               )
 
 cd $WORKSPACE/srcdir/ceres-solver/
@@ -46,21 +38,25 @@ products = Product[
     LibraryProduct("libceres", :libceres),
 ]
 
+ispowerpc64le(x) = arch(x) == "powerpc64le"
+
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    # CeresSolver prefers CXX_THREADS over OpenMP and OpenMP is not used
     # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
     # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll",
-        uuid="e66e0078-7015-5450-92f7-15fbd957f2ae");
-        platforms=filter(!Sys.isbsd, platforms)),
-    Dependency(PackageSpec(name="LLVMOpenMP_jll",
-        uuid="1d63c593-3942-5779-bab2-d838dc0a180e");
-        platforms=filter(Sys.isbsd, platforms)),
-    BuildDependency("Eigen_jll"),
+    #Dependency(PackageSpec(name="CompilerSupportLibraries_jll",
+    #    uuid="e66e0078-7015-5450-92f7-15fbd957f2ae");
+    #    platforms=filter(!Sys.isbsd, platforms)),
+    #Dependency(PackageSpec(name="LLVMOpenMP_jll",
+    #    uuid="1d63c593-3942-5779-bab2-d838dc0a180e");
+    #    platforms=filter(Sys.isbsd, platforms)),
+    BuildDependency("Eigen_jll"; platforms=filter(!ispowerpc64le, platforms)),
+    # Eigen v3.4.0 does not work on powerpc64le
+    BuildDependency("Eigen_jll", v"3.3.9"; platforms=filter(ispowerpc64le, platforms)),
     Dependency("glog_jll"),
     Dependency("METIS_jll"),
     Dependency("OpenBLAS32_jll"),
-    Dependency("OpenBLAS_jll"),
     Dependency("SuiteSparse_jll", v"5.10.1")
 ]
 
