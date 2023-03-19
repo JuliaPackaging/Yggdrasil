1730bdcca812f2821ee983db1ee81b7c0b569e5c
Norman <norman.chen.git@gmail.com>
2023-03-18 20:41:28 -0700

    Fix the version specification

--- C/CeresSolver/build_tarballs.jl
+++ C/CeresSolver/build_tarballs.jl
@@ -60,7 +60,7 @@ dependencies = [
     Dependency("glog_jll"),
     Dependency("METIS_jll"),
     Dependency("OpenBLAS_jll"),
-    Dependency("SuiteSparse_jll", build_version=v"5.10.1")
+    Dependency("SuiteSparse_jll", v"5.10.1")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
