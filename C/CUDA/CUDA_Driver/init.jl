195755540723a33f25f591df1e30e3ec03d476ea
Tim Besard <tim.besard@gmail.com>
2023-03-20 17:00:24 +0100

    Revert "[CUDA_Driver] Initialize globals to nothing (#6398)" (#6422)
    This reverts commit 47fded2821094f19cbb2fe6a37b219e4a0c6fadc.

--- C/CUDA/CUDA_Driver/init.jl
+++ C/CUDA/CUDA_Driver/init.jl
@@ -1,7 +1,7 @@
 # global variables we will set
-global libcuda = nothing
-global libcuda_version = nothing
-global libcuda_original_version = nothing
+global libcuda
+global libcuda_version
+global libcuda_original_version
 # compat_version is set in build_tarballs.jl
 
 # minimal API call wrappers we need
