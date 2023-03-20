2a07282872a592fb26792d8c1118079cbbffc55e
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-03-20 23:14:10 +0100

    [Diamond] Add new package Diamond v2.1.6 (#6414)
    * [Diamond] Add new package diamond v2.1.6
    
    * disable 32-bit platforms
    
    * Disable expand_cxxstring_abis on x86_64 darwin
    
    * Fix expand_cxxstring_abis call
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * [Diamond] Fix source
    
    * [Diamond] Add zstd
    
    * [Diamond] Add CSL dependency
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    Co-authored-by: Mosè Giordano <mose@gnu.org>

---
+++ D/Diamond/build_tarballs.jl
@@ -0,0 +1,49 @@
+using BinaryBuilder, Pkg
+
+name = "Diamond"
+version = v"2.1.6"
+
+# url = "https://github.com/bbuchfink/diamond"
+# description = "Accelerated BLAST-compatible local sequence aligner"
+
+# TODO
+# - WITH_AVX512
+# - build failures
+#   - x86_64-w64-mingw32-cxx11
+#     error during compilation: unknown identifier __cpuidex
+
+sources = [
+    # v2.1.6
+    GitSource("https://github.com/bbuchfink/diamond.git",
+              "0540f86d3d5965cf9a6ef8871068999ef61211dc"),
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir/diamond*
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=RELEASE \
+    -DWITH_ZSTD=ON \
+    -DZSTD_LIBRARY="${libdir}/libzstd.${dlext}"
+make -j${nproc}
+make install
+install_license ../LICENSE
+"""
+
+platforms = supported_platforms(exclude = p -> Sys.iswindows(p) || nbits(p) == 32)
+platforms = expand_cxxstring_abis(platforms)
+
+products = [
+    ExecutableProduct("diamond", :diamond)
+]
+
+dependencies = [
+    Dependency("Zlib_jll"),
+    Dependency("Zstd_jll"),
+    Dependency("CompilerSupportLibraries_jll"),
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", preferred_gcc_version = v"7")
