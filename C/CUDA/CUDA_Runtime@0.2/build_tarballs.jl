26cb7884d648d6e0dd58a801c80573ec797e77c9
Valentin Churavy <vchuravy@users.noreply.github.com>
2023-03-21 12:16:01 -0400

    Update C/CUDA/CUDA_Runtime@0.2/build_tarballs.jl

--- C/CUDA/CUDA_Runtime@0.2/build_tarballs.jl
+++ C/CUDA/CUDA_Runtime@0.2/build_tarballs.jl
@@ -5,7 +5,7 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "CUDA_Runtime"
-version = v"0.2.3"
+version = v"0.2.4"
 
 cuda_versions = [v"10.2", v"11.0", v"11.1", v"11.2", v"11.3", v"11.4", v"11.5", v"11.6", v"11.7", v"11.8"]
 
