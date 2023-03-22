92032a4ec54c7d70c57f28dc5ac9306547c66c8e
Devansh <devanshinspace@gmail.com>
2023-03-22 18:07:42 -0400

    Update D/DecompUtil/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- D/DecompUtil/build_tarballs.jl
+++ D/DecompUtil/build_tarballs.jl
@@ -21,7 +21,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [p for p in supported_platforms() if triplet(p) != "powerpc64le-linux-gnu"]
+platforms = supported_platforms(; exclude=p->arch(p)=="powerpc64le")
 
 # The products that we will ensure are always built
 products = [
