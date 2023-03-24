84c8f39436239d98115ad35382b2baebbaed5763
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-03-24 18:35:02 +0100

    [llama_cpp] bump version, also install library and header files

--- L/llama_cpp/build_tarballs.jl
+++ L/llama_cpp/build_tarballs.jl
@@ -1,7 +1,7 @@
 using BinaryBuilder, Pkg
 
 name = "llama_cpp"
-version = v"0.0.3"  # fake version number
+version = v"0.0.4"  # fake version number
 
 # url = "https://github.com/ggerganov/llama.cpp"
 # description = "Port of Facebook's LLaMA model in C/C++"
@@ -16,12 +16,12 @@ version = v"0.0.3"  # fake version number
 # 0.0.1           20.03.2023       master-074bea2    https://github.com/ggerganov/llama.cpp/releases/tag/master-074bea2
 # 0.0.2           21.03.2023       master-8cf9f34    https://github.com/ggerganov/llama.cpp/releases/tag/master-8cf9f34
 # 0.0.3           22.03.2023       master-d5850c5    https://github.com/ggerganov/llama.cpp/releases/tag/master-d5850c5
+# 0.0.4           24.03.2023       master-31572d9    https://github.com/ggerganov/llama.cpp/releases/tag/master-31572d9
 
 sources = [
-    # 2023.03.22, https://github.com/ggerganov/llama.cpp/releases/tag/master-d5850c5
-    # fake version = 0.0.3
+    # fake version = 0.0.4
     GitSource("https://github.com/ggerganov/llama.cpp.git",
-              "d5850c53ca179b9674b98f35d359763416a3cc11"),
+              "31572d966531f7d768eb773322016ab78eb6e835"),
     DirectorySource("./bundled"),
 ]
 
@@ -40,14 +40,35 @@ cmake .. \
     -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=RELEASE \
+    -DBUILD_SHARED_LIBS=ON \
+    -DLLAMA_BUILD_TESTS=OFF \
+    -DLLAMA_BUILD_EXAMPLES=OFF \
+    -DLLAMA_OPENBLAS=OFF \
     -DLLAMA_NATIVE=OFF \
     $EXTRA_CMAKE_ARGS
 make -j${nproc}
 
 # `make install` doesn't work (2023.03.21)
 # make install
+
+# executables
 for prg in main quantize; do
     install -Dvm 755 "./bin/${prg}${exeext}" "${bindir}/${prg}${exeext}"
+done
+
+# libs
+for lib in libllama; do
+    if [[ "${target}" == *-w64-mingw32* ]]; then
+        install -Dvm 644 "./bin/${lib}.${dlext}" "${libdir}/${lib}.${dlext}"
+    else
+        install -Dvm 644 "./${lib}.${dlext}" "${libdir}/${lib}.${dlext}"
+    fi
+done
+
+
+# header files
+for hdr in llama.h ggml.h; do
+    install -Dvm 644 "../${hdr}" "${includedir}/${hdr}"
 done
 
 install_license ../LICENSE
@@ -59,6 +80,7 @@ platforms = expand_cxxstring_abis(platforms)
 products = [
     ExecutableProduct("main", :main),
     ExecutableProduct("quantize", :quantize),
+    LibraryProduct("libllama", :libllama),
 ]
 
 dependencies = Dependency[
