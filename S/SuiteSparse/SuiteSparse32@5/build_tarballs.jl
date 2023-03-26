0273d966be326bad99e4a4f6e98e51e3363d0e81
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-03-25 21:07:50 -0400

    Add julia_compat for SuiteSparse32@5

--- S/SuiteSparse/SuiteSparse32@5/build_tarballs.jl
+++ S/SuiteSparse/SuiteSparse32@5/build_tarballs.jl
@@ -56,4 +56,4 @@ dependencies = [
 ]
 
 # Build the tarballs
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
