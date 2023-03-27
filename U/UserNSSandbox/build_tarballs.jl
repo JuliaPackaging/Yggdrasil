0e6b2fb73a5d3c5fcafb6e8dbb4443c1032f52d6
Elliot Saba <staticfloat@gmail.com>
2023-03-27 16:47:21 -0700

    Update build_tarballs.jl

--- U/UserNSSandbox/build_tarballs.jl
+++ U/UserNSSandbox/build_tarballs.jl
@@ -24,7 +24,8 @@ platforms = filter(p -> Sys.islinux(p), supported_platforms())
 
 # The products that we will ensure are always built
 products = [
-    ExecutableProduct("sandbox", :sandbox),
+    ExecutableProduct("userns_sandbox", :sandbox),
+    ExecutableProduct("userns_overlay_probe", :overlay_probe),
 ]
 
 # Dependencies that must be installed before this package can be built
