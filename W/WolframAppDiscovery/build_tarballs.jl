7c42c505133ca7257d3dcebd7145a7ccc146e81a
Simon Byrne <simonbyrne@gmail.com>
2023-03-26 20:39:04 -0700

    LICENSE with an S

--- W/WolframAppDiscovery/build_tarballs.jl
+++ W/WolframAppDiscovery/build_tarballs.jl
@@ -12,7 +12,7 @@ script = raw"""
 cd ${WORKSPACE}/srcdir/wolfram-app-discovery-rs/
 cargo build --release --bin wolfram-app-discovery --features=cli
 install -D -m 755 "target/${rust_target}/release/wolfram-app-discovery${exeext}" "${bindir}/wolfram-app-discovery${exeext}"
-install_license LICENCE-MIT LICENCE-APACHE
+install_license LICENSE-MIT LICENSE-APACHE
 """
 
 platforms = supported_platforms()
