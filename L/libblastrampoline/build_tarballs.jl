853a29d9b2cb0d451c2dc887816254b48effbe9b
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-03-27 09:42:53 -0400

    Update gcc version

--- L/libblastrampoline/build_tarballs.jl
+++ L/libblastrampoline/build_tarballs.jl
@@ -41,5 +41,5 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.9",
+               julia_compat="1.9", preferred_gcc_version = v"6"
 )
