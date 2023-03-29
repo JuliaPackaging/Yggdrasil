ae64a9f0cf411cf0eab1b4e4de8149b6b7bbe9ff
Andy Ferris <ferris.andy@gmail.com>
2023-03-29 21:38:09 +1000

    New Recipe: libsasl2 v2.1.28

---
+++ L/libsasl2/build_tarballs.jl
@@ -0,0 +1,54 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "libsasl2"
+version = v"2.1.28"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://github.com/cyrusimap/cyrus-sasl/archive/refs/tags/cyrus-sasl-$(version).tar.gz", "3e38933a30b9ce183a5488b4f6a5937a702549cde0d3287903d80968ad4ec341")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd cyrus-sasl-cyrus-sasl-2.1.28/
+./autogen.sh --prefix=${prefix} --build=${MACHTYPE} --host=${target} ac_cv_gssapi_supports_spnego=yes
+make
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Platform("i686", "linux"; libc = "glibc"),
+    Platform("x86_64", "linux"; libc = "glibc"),
+    Platform("aarch64", "linux"; libc = "glibc"),
+    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
+    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
+    Platform("powerpc64le", "linux"; libc = "glibc"),
+    Platform("x86_64", "macos"; ),
+    Platform("aarch64", "macos"; ),
+    Platform("x86_64", "freebsd"; )
+]
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libsasl2", :libsasl2),
+    LibraryProduct("libcrammd5", :libcrammd5, "lib/sasl2"),
+    LibraryProduct("libanonymous", :libanonymous, "lib/sasl2"),
+    LibraryProduct("libscram", :libscram, "lib/sasl2"),
+    LibraryProduct("libplain", :libplain, "lib/sasl2"),
+    LibraryProduct("libdigestmd5", :libdigestmd5, "lib/sasl2"),
+    LibraryProduct("libotp", :libopt, "lib/sasl2")
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
