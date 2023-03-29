18ed5b17ab07421849ba1344b35556817162490c
Mosè Giordano <giordano@users.noreply.github.com>
2023-03-29 23:12:55 +0100

    Update L/libsasl2/build_tarballs.jl

--- L/libsasl2/build_tarballs.jl
+++ L/libsasl2/build_tarballs.jl
@@ -13,7 +13,7 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/cyrus-sasl-cyrus-sasl*
+cd $WORKSPACE/srcdir/cyrus-sasl*
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} ac_cv_gssapi_supports_spnego=yes
 make -j{nproc}
 make install
