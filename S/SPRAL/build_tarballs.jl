59666831bd5b4dc785c9ca3925bfa87f031349a3
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2023-03-29 18:41:02 -0400

    Update SPRAL and compile it with LBT for all platforms (#6474)
    * Update SPRAL and compile it with LBT for all platforms
    
    * Update how we use flagon
    
    * Apply suggestions from code review
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Use LBT variable instead of BLAS_LAPACK
    
    * FCLAGS -> FCFLAGS
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- S/SPRAL/build_tarballs.jl
+++ S/SPRAL/build_tarballs.jl
@@ -3,36 +3,46 @@
 using BinaryBuilder, Pkg
 
 name = "SPRAL"
-version = v"0.1.0"
+version = v"2023.03.29"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/ralna/spral.git", "cd2d2e817275f16d586cf72767631b3c7472ce02")
+    GitSource("https://github.com/ralna/spral.git", "b1d6a7409d41d575bff7106a4e9d9a60b8935c2e")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/spral
+
+if [[ "${target}" == *mingw* ]]; then
+  LBT="-lblastrampoline-5"
+else
+  LBT="-lblastrampoline"
+fi
+
 if [[ "${target}" == *-freebsd* ]] || [[ "${target}" == *-apple-* ]]; then
     CC=gcc
     CXX=g++
 fi
+
 ./autogen.sh
 mkdir build
 cd build
-CFLAGS=-fPIC CPPFLAGS=-fPIC CXXFLAGS=-fPIC FFLAGS=-fPIC FCFLAGS=-fPIC \
-    ../configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} \
-    --with-blas="-L${libdir} -lopenblas" --with-lapack="-L${libdir} -lopenblas" \
-    --with-metis="-L${libdir} -lmetis" --with-metis-inc-dir="${prefix}/include"
+export CFLAGS="-O3 -fPIC"
+export CXXFLAGS="-O3 -fPIC"
+export FFLAGS="-O3 -fPIC"
+export FCFLAGS="-O3 -fPIC"
+../configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} \
+    --with-blas="-L${libdir} ${LBT}" --with-lapack="-L${libdir} ${LBT}" \
+    --with-metis="-L${libdir} -lmetis" --with-metis-inc-dir="${includedir}"
 make
-gfortran -fPIC -shared -Wl,$(flagon --whole-archive) libspral.a -Wl,$(flagon --no-whole-archive | cut -d' ' -f1) -lgomp -lopenblas -lhwloc -lmetis -lstdc++ -o ${libdir}/libspral.${dlext}
+gfortran -fPIC -shared $(flagon -Wl,--whole-archive) libspral.a $(flagon -Wl,--no-whole-archive) -lgomp ${LBT} -lhwloc -lmetis -lstdc++ -o ${libdir}/libspral.${dlext}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_gfortran_versions(supported_platforms())
-
 
 # The products that we will ensure are always built
 products = [
@@ -42,11 +52,11 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="METIS_jll", uuid="d00139f3-1899-568f-a2f0-47f597d42d70"))
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
-    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
-    Dependency(PackageSpec(name="Hwloc_jll", uuid="e33a78d0-f292-5ffc-b300-72abe9b543c8"))
+    Dependency(PackageSpec(name="METIS_jll", uuid="d00139f3-1899-568f-a2f0-47f597d42d70")),
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+    Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"), compat="5.4.0"),
+    Dependency(PackageSpec(name="Hwloc_jll", uuid="e33a78d0-f292-5ffc-b300-72abe9b543c8")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9.1.0", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9.1.0", julia_compat="1.9")
