46f4ffdc9e0f7ace0b316a1a0f1cf4c3a64f2c0d
Quan-feng WU <wuquanfeng@ihep.ac.cn>
2023-03-31 09:15:19 +0800

    switch to `CLN_jll.jl`

--- G/GiNaC/build_tarballs.jl
+++ G/GiNaC/build_tarballs.jl
@@ -7,19 +7,11 @@ version = v"1.8.6"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://www.ginac.de/ginac-$(version).tar.bz2", "00b320b1116cae5b7b43364dbffb7912471d171f484d82764605d715858d975b"),
-    GitSource("git://www.ginac.de/cln.git", "d4621667b173aa197a2b23d63f561648c0ee2968")
+    ArchiveSource("https://www.ginac.de/ginac-$(version).tar.bz2", "00b320b1116cae5b7b43364dbffb7912471d171f484d82764605d715858d975b")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-mkdir $WORKSPACE/srcdir/cln-build/
-cd $WORKSPACE/srcdir/cln-build/
-
-cmake -GNinja -DCMAKE_CXX_STANDARD=11 -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release $WORKSPACE/srcdir/cln
-cmake --build .
-cmake --build . -t install
-
 cd $WORKSPACE/srcdir/ginac-*.*.*/
 ./configure --prefix=$prefix --build=${MACHTYPE} --host=${target}
 make -j${nproc}
@@ -40,10 +32,10 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"))
-    Dependency(PackageSpec(name="Python_jll", uuid="93d3a430-8e7c-50da-8e8d-3dfcfb3baf05"))
     Dependency(PackageSpec(name="Bison_jll", uuid="0f48145f-aea8-549d-8864-7f251ac1e6d0"))
+    Dependency(PackageSpec(name="CLN_jll", uuid="b3974076-79ef-58d3-b5c7-5ef926e97925"))
     Dependency(PackageSpec(name="flex_jll", uuid="48a596b8-cc7a-5e48-b182-65f75e8595d0"))
+    Dependency(PackageSpec(name="Python_jll", uuid="93d3a430-8e7c-50da-8e8d-3dfcfb3baf05"))
     Dependency(PackageSpec(name="Readline_jll", uuid="05236dd9-4125-5232-aa7c-9ec0c9b2c25a"))
 ]
 
