3287a737d296a2607dd0a6e93c2cfcf8e5058cd3
Erik Schnetter <schnetter@gmail.com>
2023-04-04 13:00:01 -0400

    Blosc2: Trigger pipeline

--- B/Blosc2/build_tarballs.jl
+++ B/Blosc2/build_tarballs.jl
@@ -48,6 +48,6 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-# TODO: GCC 9 not tested for powerpc
+# TODO: GCC 8, 9 not tested for powerpc.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.6", preferred_gcc_version=v"10")
