a59aba486a18395319f397c2f9ad028811330f08
Erik Schnetter <schnetter@gmail.com>
2023-04-03 21:26:03 -0400

    Blosc2: Re-enable powerpc and arm

--- B/Blosc2/build_tarballs.jl
+++ B/Blosc2/build_tarballs.jl
@@ -48,4 +48,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"5.2")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", preferred_gcc_version=v"6")
