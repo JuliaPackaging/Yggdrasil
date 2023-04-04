1761f7c8de4b07b2a4cbd0ff624eedbf459c829c
Michel Juillard <michel.juillard@mjui.fr>
2023-04-04 04:43:37 +0200

    Dynare julia (#6487)
    * checking whether github is working better
    
    * checking whether github is working better
    
    * upstream update + fix derivatives wr parameters

--- D/DynarePreprocessor/build_tarballs.jl
+++ D/DynarePreprocessor/build_tarballs.jl
@@ -1,9 +1,9 @@
 using BinaryBuilder
 
 name = "DynarePreprocessor"
-version = v"6.2.0"
+version = v"6.3.0"
 sources = [
-    GitSource("https://git.dynare.org/Dynare/preprocessor.git", "b0cb1a728cc25f574879042971e94bc49834d96e"),
+    GitSource("https://git.dynare.org/Dynare/preprocessor.git", "07f5de36795bf62807e63f0f66f1841a70172d4a"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz",
                   "cd4f08a75577145b8f05245a2975f7c81401d75e9535dcffbb879ee1deefcbf4"),
 ]
