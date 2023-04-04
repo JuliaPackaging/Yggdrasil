7ad31c6269571684a1d0b3b45bccd3033b837cfa
Josef Heinen <jheinen@users.noreply.github.com>
2023-04-04 09:22:53 +0200

    [GR] bump version (#6511)
    bump GR version to 0.72.0

--- G/GR/build_tarballs.jl
+++ G/GR/build_tarballs.jl
@@ -9,7 +9,7 @@ version = v"0.72.0"
 sources = [
     GitSource("https://github.com/sciapp/gr.git", "eff7161c08df40e298cb16ac429bbaeae13cb3b1"),
     FileSource("https://github.com/sciapp/gr/releases/download/v$version/gr-$version.js",
-        "30bd4eb18107b7e80360d7f25e65a0f21e2eeba2a52d1921da9b61b21c918038", "gr.js")
+               "30bd4eb18107b7e80360d7f25e65a0f21e2eeba2a52d1921da9b61b21c918038", "gr.js")
 ]
 
 # Bash recipe for building across all platforms
