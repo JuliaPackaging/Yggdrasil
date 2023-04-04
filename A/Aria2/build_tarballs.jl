1fb190690a61543270f72d516cf58f2f1e647124
Mark Kittisopikul <mkitti@users.noreply.github.com>
2023-04-04 05:45:45 -0400

    [aria2] Set MbedTLS build dep to 2.28.0, set openssl compat to 1.1.10 (#6517)
    * [aria2] Set MbedTLS build dep to 2.28.0, set openssl compat to 1.1.10
    
    [skip build]
    
    * Fix compat keyword
    
    * Fix version
    
    [skip ci]

--- A/Aria2/build_tarballs.jl
+++ A/Aria2/build_tarballs.jl
@@ -49,8 +49,8 @@ dependencies = [
     # are getting a newer version in the build than the one
     # `LibSSH2_jll` was compiled with.  So we explicitly select the
     # right version here.
-    BuildDependency(PackageSpec(name="MbedTLS_jll", version="2.24")),
-    Dependency(PackageSpec(name="OpenSSL_jll")),
+    BuildDependency(PackageSpec(name="MbedTLS_jll", version=v"2.28")),
+    Dependency(PackageSpec(name="OpenSSL_jll"); compat = "1.1.10"),
     Dependency(PackageSpec(name="XML2_jll")),
     Dependency(PackageSpec(name="Zlib_jll")),
 ]
