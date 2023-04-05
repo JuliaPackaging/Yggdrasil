c382a7615e55fd20d699e3afb5f5c6d6c9d9c98d
Thomas van Doornmalen <60822431+Taaitaaiger@users.noreply.github.com>
2023-04-05 23:03:41 +0200

    JlrsLedger: Build for all supported platforms, address other style recommendations. (#6529)
    * Build for all supported platforms, address other style recommendations.
    
    * Remove trailing comma.
    
    * Update J/JlrsLedger/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- J/JlrsLedger/build_tarballs.jl
+++ J/JlrsLedger/build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "JlrsLedger"
-version = v"0.0.1"
+version = v"0.0.2"
 
 # Collection of sources required to complete build
 sources = [
@@ -15,33 +15,28 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/jlrs_ledger
 
-if [[ "${target}" == *-musl ]]; then 
+if [[ "${target}" == *-musl* ]]; then
     rust_flags="-C target-feature=-crt-static"
 fi
 
 RUSTFLAGS=$rust_flags cargo build --release --verbose
-
 install_license LICENSE
-if [[ "${target}" == *-mingw* ]]; then
-    install -Dvm 0755 "target/${rust_target}/release/jlrs_ledger.${dlext}" "${libdir}/libjlrs_ledger.${dlext}"
-else
-    install -Dvm 0755 "target/${rust_target}/release/libjlrs_ledger.${dlext}" "${libdir}/libjlrs_ledger.${dlext}"
-fi
+install -Dvm 0755 "target/${rust_target}/release/"*jlrs_ledger".${dlext}" "${libdir}/libjlrs_ledger.${dlext}"
 """
 
-platforms = supported_platforms()
-
-is_supported(p) = arch(p) ∈ ("i686", "x86_64") &&
-                  (Sys.islinux(p) || nbits(p) == 64 && Sys.iswindows(p))
-
-filter!(is_supported, platforms)
+# Rust toolchain for i686 Windows is unusable
+is_excluded(p) = Sys.iswindows(p) && nbits(p) != 64
+platforms = supported_platforms(; exclude=is_excluded)
 
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libjlrs_ledger", :libjlrs_ledger; dlopen_flags=[:RTLD_GLOBAL]),
 ]
 
-dependencies = Dependency[]
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("Libiconv_jll"; platforms=filter(Sys.isapple, platforms)),
+]
 
 # Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
