d155811d6f2134cc9b7e7897b958aee5d284ff92
Quan-feng WU <32387646+Fenyutanchan@users.noreply.github.com>
2023-04-06 14:21:31 +0800

    try for all mpis

--- F/FireFly/build_tarballs.jl
+++ F/FireFly/build_tarballs.jl
@@ -1,6 +1,9 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
+using Base.BinaryPlatforms
+const YGGDRASIL_DIR = (dirname∘dirname∘dirname)(@__FILE__)
+(include∘joinpath)(YGGDRASIL_DIR, "platforms", "mpi.jl")
 
 name = "FireFly"
 version = v"2.0.3"
@@ -30,11 +33,24 @@ cmake --build . -j${nproc} -t install
 install_license ${WORKSPACE}/srcdir/firefly/LICENSE
 """
 
+argument_platform_block = """
+    using Base.BinaryPlatforms
+    $(MPI.augment)
+    argument_platform!(platform::Platform) = augment_mpi!(platform)
+"""
+
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; exclude=Sys.iswindows)
+platforms = supported_platforms()
 platforms = expand_cxxstring_abis(platforms)
+platforms, platform_dependencies = MPI.augment_platforms(platforms)
 
+# Avoid platforms where the MPI implementation isn't supported
+# OpenMPI
+platforms = filter(p -> !(p["mpi"] == "openmpi" && arch(p) == "armv6l" && libc(p) == "glibc"), platforms)
+# MPItrampoline
+platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && libc(p) == "musl"), platforms)
+platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && Sys.isfreebsd(p)), platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -46,14 +62,14 @@ dependencies = [
     Dependency(PackageSpec(name="FLINT_jll", uuid="e134572f-a0d5-539d-bddf-3cad8db41a82"))
     Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"); compat="6.2.0")
     Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a"))
-    Dependency(PackageSpec(name="MPICH_jll", uuid="7cb0a576-ebde-5e09-9194-50597f1243b4"))
     Dependency(PackageSpec(name="jemalloc_jll", uuid="454a8cc1-5e0e-5123-92d5-09b094f0e876"))
 ]
+append!(dependencies, platform_dependencies)
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
     julia_compat="1.6",
     # preferred_gcc_version = v"5.2.0" # for std=c++14
-    # preferred_gcc_version = v"6.1.0" # for making the target example
-    preferred_gcc_version = v"7.1.0" # for avoiding unexpected segmentation fault on x86_64-linux-gnu-cxx11 @ Buildkite.com
+    preferred_gcc_version = v"6.1.0" # for making the target example
+    # preferred_gcc_version = v"7.1.0" # for avoiding unexpected segmentation fault on x86_64-linux-gnu-cxx11 @ Buildkite.com
 )
