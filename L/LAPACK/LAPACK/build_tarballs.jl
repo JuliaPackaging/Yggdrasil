572784cdb54d3bda12f3f972e853d1c0177da903
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-04-09 11:43:00 -0700

    Update LAPACK to 3.11

--- L/LAPACK/LAPACK/build_tarballs.jl
+++ L/LAPACK/LAPACK/build_tarballs.jl
@@ -1,9 +1,5 @@
 name = "LAPACK"
 
 include("../common.jl")
-
 script = lapack_script(lapack32=false)
-
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.8", preferred_gcc_version=v"6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
--- L/LAPACK/LAPACK32/build_tarballs.jl
+++ L/LAPACK/LAPACK32/build_tarballs.jl
@@ -1,9 +1,5 @@
 name = "LAPACK32"
 
 include("../common.jl")
-
 script = lapack_script(lapack32=true)
-
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.8", preferred_gcc_version=v"6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
