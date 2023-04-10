0d640853167ed91faab06a80a48ad19d917dd2c1
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-04-10 14:23:43 -0400

    gcc 6

--- L/LAPACK/LAPACK/build_tarballs.jl
+++ L/LAPACK/LAPACK/build_tarballs.jl
@@ -2,4 +2,4 @@ name = "LAPACK"
 
 include("../common.jl")
 script = lapack_script(lapack32=false)
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"6")
