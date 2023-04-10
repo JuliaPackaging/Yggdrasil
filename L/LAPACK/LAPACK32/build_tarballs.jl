498bd69f6af00f12b28013672d65f144a4263fcb
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-04-10 14:16:49 -0400

    min julia_compat as 1.6

--- L/LAPACK/LAPACK32/build_tarballs.jl
+++ L/LAPACK/LAPACK32/build_tarballs.jl
@@ -2,4 +2,4 @@ name = "LAPACK32"
 
 include("../common.jl")
 script = lapack_script(lapack32=true)
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
