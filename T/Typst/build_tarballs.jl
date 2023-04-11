40198b9cb1c9c1eee10e13b7a29a9c12937705d9
Gabriel Wu <qqbbnease1004@126.com>
2023-04-12 07:46:42 +0800

    Re-enable Windows build

--- T/Typst/build_tarballs.jl
+++ T/Typst/build_tarballs.jl
@@ -20,7 +20,7 @@ install_license LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter(!Sys.iswindows, supported_platforms())
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = Product[
