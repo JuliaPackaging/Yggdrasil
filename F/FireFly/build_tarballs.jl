b8ff846c4d0447eb0488343eec1809916a394eb9
Quan-feng WU <32387646+Fenyutanchan@users.noreply.github.com>
2023-04-13 19:23:57 +0800

    [FireFly] fix missing augment (#6564)
    * fix missing augment
    
    * add `CompilerSupportLibraries_jll`
    ensure that the correct version of `libatomic`
    when building for `x86_64-linux-gnu-cxx11-mpi+openmpi`
    
    * Apply suggestions from code review
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- F/FireFly/build_tarballs.jl
+++ F/FireFly/build_tarballs.jl
@@ -34,7 +34,7 @@ cmake --build . -j${nproc} -t install
 install_license ${WORKSPACE}/srcdir/firefly/LICENSE
 """
 
-argument_platform_block = """
+augment_platform_block = """
     using Base.BinaryPlatforms
     $(MPI.augment)
     argument_platform!(platform::Platform) = augment_mpi!(platform)
@@ -61,6 +61,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")) # ensure that the correct version of libatomic @ x86_64-linux-gnu-cxx11-mpi+openmpi
     Dependency(PackageSpec(name="FLINT_jll", uuid="e134572f-a0d5-539d-bddf-3cad8db41a82"))
     Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"); compat="6.2.0")
     Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a"))
@@ -70,6 +71,7 @@ append!(dependencies, platform_dependencies)
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+    augment_platform_block,
     julia_compat="1.6",
     # preferred_gcc_version = v"5.2.0" # for std=c++14
     # preferred_gcc_version = v"6.1.0" # for making the target example
