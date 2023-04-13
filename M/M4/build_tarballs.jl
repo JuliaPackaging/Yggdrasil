94f3af76d04d6571061b0637e00c0c8a9ba12860
Elliot Saba <staticfloat@gmail.com>
2023-04-13 14:15:23 -0700

    Update M/M4/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- M/M4/build_tarballs.jl
+++ M/M4/build_tarballs.jl
@@ -26,7 +26,7 @@ products = [
     ExecutableProduct("m4", :m4),
 ]
 
-dependencies = [
+dependencies = Dependency[
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
