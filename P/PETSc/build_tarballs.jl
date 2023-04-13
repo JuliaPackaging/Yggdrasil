e2712a372160cdc70bf23254d66f9816f356018a
Boris Kaus <kaus@uni-mainz.de>
2023-04-13 13:59:29 +0200

    no longer necessary

--- P/PETSc/build_tarballs.jl
+++ P/PETSc/build_tarballs.jl
@@ -125,9 +125,6 @@ build_petsc()
 
     if [[ "${target}" == *-apple* ]]; then 
         # on a mac, there might otherwise be confusion with clang
-      #  CC=gcc
-      #  FC=gfortran
-      #  CXX=g++
         LIBFLAGS="-L${libdir} -framework Accelerate" 
     fi
 
