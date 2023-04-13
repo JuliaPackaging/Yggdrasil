e26b2f45825ba6a32b991b257c273865445d3dce
Elliot Saba <staticfloat@gmail.com>
2023-04-13 16:15:21 -0700

    Update P/patch/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- P/patch/build_tarballs.jl
+++ P/patch/build_tarballs.jl
@@ -5,7 +5,7 @@ version = v"2.7.6"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://ftp.gnu.org/gnu/patch/patch-2.7.6.tar.xz",
+    ArchiveSource("https://ftp.gnu.org/gnu/patch/patch-$(version).tar.xz",
                   "ac610bda97abe0d9f6b7c963255a11dcb196c25e337c61f94e4778d632f1d8fd"),
 ]
 
