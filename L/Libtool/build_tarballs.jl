a199c3617c18ef2207db4810203df45cbebed8a9
Elliot Saba <staticfloat@gmail.com>
2023-04-14 16:06:05 -0700

    [Libtool] Clean up, add more executable products (#6581)
    Update build_tarballs.jl
    
    Update build_tarballs.jl
    
    `libtool` and `libtoolize` are shell scripts
    
    So they wouldn't have a `.exe` extension.  Heh.

--- L/Libtool/build_tarballs.jl
+++ L/Libtool/build_tarballs.jl
@@ -7,31 +7,33 @@ version = v"2.4.6"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("http://gnu.mirrors.hoobly.com/libtool/libtool-2.4.6.tar.gz", "e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3")
+    ArchiveSource("https://ftpmirror.gnu.org/libtool/libtool-$(version).tar.gz",
+                  "e3bd4d5d3d025a36c21dd6af7ea818a2afcd4dfc1ea5a17b39d7854bcd0c06e3")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd libtool-2.4.6
+cd $WORKSPACE/srcdir/libtool-*
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make
 make install
-exit
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+# TODO: in next release include the "experimental" platforms
+filter!(p -> !(Sys.isapple(p) && arch(p) == "aarch64") && !(arch(p) == "armv6l"), platforms)
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libltdl", :libltdl)
+    FileProduct("bin/libtool", :libtool),
+    FileProduct("bin/libtoolize", :libtoolize),
+    LibraryProduct("libltdl", :libltdl),
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
-]
+dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
