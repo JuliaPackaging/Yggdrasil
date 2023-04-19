4783dc1733f6c4f54fc719f470c2c1e8e7b65a2f
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-04-19 15:00:54 -0400

    Unify blis and blis32

---
+++ B/blis/blis/build_tarballs.jl
@@ -0,0 +1,9 @@
+name = "blis"
+
+include("../common.jl")
+
+script = blis_script(blis32=false)
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               preferred_gcc_version=v"11", lock_microarchitecture=false, julia_compat="1.6")
