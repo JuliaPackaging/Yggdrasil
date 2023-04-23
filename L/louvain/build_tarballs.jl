82e2846e0b6d108263c2de4a8ad503f2c6dc17b0
Lukasz Krainski <lukasz.krainski123@gmail.com>
2023-04-23 22:29:47 +0200

    Adding macOS aarch64

--- L/louvain/build_tarballs.jl
+++ L/louvain/build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "louvain"
-version = v"0.1.0"
+version = v"0.2.0" # Rebuilding louvain_jll to include macOS aarch64
 
 # Collection of sources required to complete build
 sources = [
