f05954a00dedd0d2c143d4ec0593cb537812ea1c
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-04-24 10:53:41 +0200

    [llama_cpp] Bump version (#6631)
    * [llama_cpp] Bump version
    
    * Add compiler defines for Windows 10

--- L/llama_cpp/build_tarballs.jl
+++ L/llama_cpp/build_tarballs.jl
@@ -1,12 +1,16 @@
 using BinaryBuilder, Pkg
 
 name = "llama_cpp"
-version = v"0.0.6"  # fake version number
+version = v"0.0.7"  # fake version number
 
 # url = "https://github.com/ggerganov/llama.cpp"
 # description = "Port of Facebook's LLaMA model in C/C++"
 
 # NOTES
+# - _mm_loadu_si64 needs at least gcc-11.3.0
+#   https://stackoverflow.com/questions/72837929/mm-loadu-si32-not-recognized-by-gcc-on-ubuntu
+#   gcc-11.1.0 will silently produce wrong code, so we have to use gcc-12
+#   https://gcc.gnu.org/bugzilla/show_bug.cgi?id=99754
 # - missing architectures: powerpc64le, armv6l, arm7vl
 
 # versions: fake_version to github_version mapping
@@ -18,11 +22,12 @@ version = v"0.0.6"  # fake version number
 # 0.0.4           25.03.2023       master-1972616    https://github.com/ggerganov/llama.cpp/releases/tag/master-1972616
 # 0.0.5           30.03.2023       master-3bcc129    https://github.com/ggerganov/llama.cpp/releases/tag/master-3bcc129
 # 0.0.6           03.04.2023       master-437e778    https://github.com/ggerganov/llama.cpp/releases/tag/master-437e778
-# 0.0.7           16.04.2023       master-47f61aa    https://github.com/ggerganov/llama.cpp/releases/tag/master-47f61aa
+# 0.0.6+1         16.04.2023       master-47f61aa    https://github.com/ggerganov/llama.cpp/releases/tag/master-47f61aa
+# 0.0.7           24.04.2023       master-c4fe84f    https://github.com/ggerganov/llama.cpp/releases/tag/master-c4fe84f
 
 sources = [
     GitSource("https://github.com/ggerganov/llama.cpp.git",
-              "47f61aaa5f76d04286792e2fbd0c95b659ab2af0"),
+              "c4fe84fb0d28851a5c10e5a633f82ae2ba3b7fae"),
     DirectorySource("./bundled"),
 ]
 
@@ -40,6 +45,20 @@ if [[ "${target}" == *-linux-* ]]; then
 fi
 
 mkdir build && cd build
+
+# On Windows, we target Windows 10 for mmap support
+# Julia-1.6 and above require Windows 10 or later
+# Windows 10 corresponds to 0x0A00
+# https://learn.microsoft.com/en-us/cpp/porting/modifying-winver-and-win32-winnt?view=msvc-170
+#
+# Can be removed after
+#     https://github.com/JuliaPackaging/BinaryBuilderBase.jl/pull/308
+# has been merged and released
+if [[ "${target}" == *-w64-mingw32* ]]; then
+    export CFLAGS="-DWINVER=0x0A00 -D_WIN32_WINNT=0x0A00"
+    export CXXFLAGS="-DWINVER=0x0A00 -D_WIN32_WINNT=0x0A00"
+fi
+
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
@@ -56,7 +75,7 @@ make -j${nproc}
 # make install
 
 # executables
-for prg in embedding main perplexity quantize; do
+for prg in embedding main perplexity q8dot quantize quantize-stats vdot; do
     install -Dvm 755 "./bin/${prg}${exeext}" "${bindir}/${prg}${exeext}"
 done
 
@@ -68,7 +87,6 @@ for lib in libllama; do
         install -Dvm 755 "./${lib}.${dlext}" "${libdir}/${lib}.${dlext}"
     fi
 done
-
 
 # header files
 for hdr in llama.h ggml.h; do
@@ -85,7 +103,10 @@ products = [
     ExecutableProduct("embedding", :embedding),
     ExecutableProduct("main", :main),
     ExecutableProduct("perplexity", :perplexity),
+    ExecutableProduct("q8dot", :q8dot),
     ExecutableProduct("quantize", :quantize),
+    ExecutableProduct("quantize-stats", :quantize_stats),
+    ExecutableProduct("vdot", :vdot),
     LibraryProduct("libllama", :libllama),
 ]
 
@@ -93,4 +114,4 @@ dependencies = Dependency[
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version = v"8.1.0")
+               julia_compat="1.6", preferred_gcc_version = v"12")
