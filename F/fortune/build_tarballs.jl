5801ca5128c13a50e78e5bdcc2026e21cd100b86
Simon Christ <SimonChrist@gmx.de>
2023-04-25 08:38:33 +0200

    Apply suggestions from code review
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- F/fortune/build_tarballs.jl
+++ F/fortune/build_tarballs.jl
@@ -12,30 +12,14 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd gortune/
+cd $WORKSPACE/srcdir/gortune/
 mkdir -p ${bindir}
 go build -o ${bindir}
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("x86_64", "freebsd"; ),
-    Platform("i686", "windows"; ),
-    Platform("x86_64", "windows"; )
-]
+platforms = supported_platforms()
 
 
 # The products that we will ensure are always built
