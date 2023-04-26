73ed969e1bf2cca789d3751b9f6697c50a211dca
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-04-26 10:26:08 +0200

    Remove extra whitespace

--- K/kalign/build_tarballs.jl
+++ K/kalign/build_tarballs.jl
@@ -49,7 +49,7 @@ products = [
 ]
 
 dependencies = Dependency[
-    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD  
+    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
     # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae");
                platforms=filter(!Sys.isbsd, platforms)),
