280d8b8f517f0a8e211332bc1eebbe3f121a732f
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-04-26 10:21:15 +0200

    Fix OpenMP dependency filters
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- K/kalign/build_tarballs.jl
+++ K/kalign/build_tarballs.jl
@@ -54,7 +54,7 @@ dependencies = Dependency[
     # other libraries from `CompilerSupportLibraries_jll` are needed on x86_64 macOS and
     # FreeBSD
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae");
-               platforms=filter(p -> !(Sys.isapple(p) && arch(p) == "aarch64"), platforms)),
+               platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e");
                platforms=filter(Sys.isbsd, platforms)),
 ]
