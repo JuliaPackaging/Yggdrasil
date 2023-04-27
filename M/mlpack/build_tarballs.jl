baec709505da1dbf2753cb657870993dd9340ac4
Ryan Curtin <ryan@ratml.org>
2023-04-27 11:51:21 -0400

    Update mlpack to version 4.1.0.

--- M/mlpack/build_tarballs.jl
+++ M/mlpack/build_tarballs.jl
@@ -6,11 +6,11 @@ using BinaryBuilder
 
 # Set sources and other environment variables.
 name = "mlpack"
-source_version = v"4.0.1"
+source_version = v"4.1.0"
 version = source_version
 sources = [
     ArchiveSource("https://www.mlpack.org/files/mlpack-$(source_version).tar.gz",
-                  "4c746936ed9da9f16744240ed7b9f2815d3abb90c904071a1d1a628a9bbfb3a5"),
+                  "e0c760baf15fd0af5601010b7cbc536e469115e9dd45f96712caa3b651b1852a"),
 ]
 
 script = raw"""
