ce794a4eb142db070a8161b3d6c75c79c200cb95
Mosè Giordano <giordano@users.noreply.github.com>
2023-04-27 14:18:52 +0100

    Update build_tarballs.jl

--- T/Typst/build_tarballs.jl
+++ T/Typst/build_tarballs.jl
@@ -32,4 +32,5 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", compilers=[:rust, :c])
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", compilers=[:rust, :c], lock_microarchitecture=false)
