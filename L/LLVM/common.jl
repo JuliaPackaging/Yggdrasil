ede2417875b943d2af8142372731b8bba1986871
Valentin Churavy <vchuravy@users.noreply.github.com>
2023-05-03 04:15:10 -0400

    [MLIR] Build julia-15.0.7-5 and MLIR-C (#6683)

--- L/LLVM/common.jl
+++ L/LLVM/common.jl
@@ -15,7 +15,7 @@ const llvm_tags = Dict(
     v"12.0.1" => "980d2f60a8524c5546397db9e8bbb7d6ea56c1b7", # julia-12.0.1-4
     v"13.0.1" => "8a2ae8c8064a0544814c6fac7dd0c4a9aa29a7e6", # julia-13.0.1-3
     v"14.0.6" => "5c82f5309b10fab0adf6a94969e0dddffdb3dbce", # julia-14.0.6-3
-    v"15.0.7" => "fcc8566589dd38181281316a65a96ea1e8b6b599", # julia-15.0.7-4
+    v"15.0.7" => "3089b0b526eed060bc1c0ed582dabe2a06801226", # julia-15.0.7-5
 )
 
 const buildscript = raw"""
