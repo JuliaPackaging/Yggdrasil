e256a78c27d1bb156c07fd8de9a14fd2e2310f58
Cody Tapscott <topolarity@tapscott.me>
2023-05-03 05:41:55 -0400

    [ATKmm] Bump to v2.36.2
    This is the latest version with libsigc++-3.0, for use with GTK4.

--- A/ATKmm/ATKmm@2.36.2/build_tarballs.jl
+++ A/ATKmm/ATKmm@2.36.2/build_tarballs.jl
@@ -3,26 +3,26 @@
 using BinaryBuilder, Pkg
 
 name = "ATKmm"
-version = v"2.36.1"
+version = v"2.36.2"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://download.gnome.org/sources/atkmm/$(version.major).$(version.minor)/atkmm-$(version).tar.xz",
-                  "e11324bfed1b6e330a02db25cecc145dca03fb0dff47f0710c85e317687da458")
+                  "6f62dd99f746985e573605937577ccfc944368f606a71ca46342d70e1cdae079")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/atkmm*/
 mkdir output && cd output/
-meson --cross-file=${MESON_TARGET_TOOLCHAIN} ..
+meson --cross-file=${MESON_TARGET_TOOLCHAIN%.*}_gcc.meson ..
 ninja -j${nproc}
 ninja install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = expand_cxxstring_abis(supported_platforms(); skip=Returns(false))
 
 # The products that we will ensure are always built
 products = [
@@ -31,8 +31,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="ATK_jll", uuid="7b86fcea-f67b-53e1-809c-8f1719c154e8"); compat=string(version))
-    Dependency(PackageSpec(name="Glibmm_jll", uuid="5d85a9da-21f7-5855-afec-cdc5039c46e8"))
+    Dependency(PackageSpec(name="ATK_jll", uuid="7b86fcea-f67b-53e1-809c-8f1719c154e8"); compat="^2.38.0")
+    Dependency(PackageSpec(name="Glibmm_jll", uuid="5d85a9da-21f7-5855-afec-cdc5039c46e8"); compat="^2.76.0")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
