a18100a340c78e6ea7a82e04022c7fa679fc0aa4
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-05-03 13:54:56 -0700

    Filter out failing builds

--- L/LAPACK/LAPACK/build_tarballs.jl
+++ L/LAPACK/LAPACK/build_tarballs.jl
@@ -4,6 +4,10 @@ include("../common.jl")
 
 script = lapack_script(lapack32=false)
 
+products = [
+    LibraryProduct(["liblapack"], :liblapack),
+]
+
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.10", preferred_gcc_version=v"6")
--- L/LAPACK/common.jl
+++ L/LAPACK/common.jl
@@ -332,11 +332,7 @@ end
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_gfortran_versions(supported_platforms())
-
-# The products that we will ensure are always built
-products = [
-    LibraryProduct(["liblapack32", "liblapack"], :liblapack),
-]
+filter!(p -> !(arch(p) == "aarch64" && Sys.islinux(p) && libgfortran_version(p) == v"4"), platforms)
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
