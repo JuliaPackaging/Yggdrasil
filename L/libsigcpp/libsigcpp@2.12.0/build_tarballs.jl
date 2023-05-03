be38dbc3e7e5683a620ee45d53e46deb943ad523
Cody Tapscott <84105208+topolarity@users.noreply.github.com>
2023-05-03 10:06:34 -0400

    [libsigc++] Expand C++ string ABI for all systems (#6691)
    * [libsigc++] Expand C++ string ABI for all systems
    
    * Update L/libsigcpp/libsigcpp@2.12.0/build_tarballs.jl
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/libsigcpp/libsigcpp@2.12.0/build_tarballs.jl
+++ L/libsigcpp/libsigcpp@2.12.0/build_tarballs.jl
@@ -23,7 +23,7 @@ ninja install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
+platforms = expand_cxxstring_abis(supported_platforms(); skip=Returns(false))
 
 # The products that we will ensure are always built
 products = [
