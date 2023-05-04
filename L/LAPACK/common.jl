3dc9515785f663848a395302e50e6fd71c69694d
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-05-04 07:48:40 -0700

    fix

--- L/LAPACK/common.jl
+++ L/LAPACK/common.jl
@@ -13,7 +13,11 @@ sources = [
 # Bash recipe for building across all platforms
 
 function lapack_script(;lapack32::Bool=false)
-    script = raw"""
+    script = """
+    LAPACK32=$(lapack32)
+    """
+
+    script *= raw"""
     LAPACK32=$(lapack32)
     cd $WORKSPACE/srcdir/lapack*
     FFLAGS=(-cpp -ffixed-line-length-none -DUSE_ISNAN)
