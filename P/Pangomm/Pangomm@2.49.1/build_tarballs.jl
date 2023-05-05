f298a413fb9fd83d3e2673d5b81112ccdaa7da3e
Cody Tapscott <84105208+topolarity@users.noreply.github.com>
2023-05-05 04:40:53 -0400

    [Pangomm] Bump to v2.49.1 (#6704)
    This is the latest version with libsigc++-3.0, for use with GTK4.

--- P/Pangomm/Pangomm@2.49.1/build_tarballs.jl
+++ P/Pangomm/Pangomm@2.49.1/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "Pangomm"
-version = v"2.49.1"
+version = v"2.50.1"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://download.gnome.org/sources/pangomm/$(version.major).$(version.minor)/pangomm-$(version).tar.xz",
-                  "a2272883152618fddea016a62f50eb23b9b056ab3c08f3b64422591e6a507bd5")
+                  "ccc9923413e408c2bff637df663248327d72822f11e394b423e1c5652b7d9214")
 ]
 
 # Bash recipe for building across all platforms
@@ -19,27 +19,30 @@ if [[ "${target}" == "${MACHTYPE}" ]]; then
     rm /usr/lib/libexpat*
 fi
 mkdir output && cd output
-meson --cross-file=${MESON_TARGET_TOOLCHAIN} ..
+meson --cross-file=${MESON_TARGET_TOOLCHAIN%.*}_gcc.meson ..
 ninja -j${nproc}
 ninja install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
-# We don't have armv6l Pango at the moment
-filter!(p -> arch(p) != "armv6l", platforms)
+platforms = expand_cxxstring_abis(supported_platforms(); skip=Returns(true))
+
+# These platforms are not supported by Cairo_jll/Pango_jll yet
+platforms = filter!(p -> arch(p) != "armv6l", supported_platforms())
+
+platforms = expand_cxxstring_abis(platforms; skip=Returns(false))
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct(["libpangomm-$(version.major)", "libpangomm-$(version.major).48"], :libpangomm)
+    LibraryProduct(["libpangomm-2", "libpangomm-2.48"], :libpangomm)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Cairomm_jll", uuid="af74c99f-f0eb-54aa-aecc-a10e8fc65c17"); compat="~1.16.1")
-    Dependency(PackageSpec(name="Glibmm_jll", uuid="5d85a9da-21f7-5855-afec-cdc5039c46e8"); compat="~2.68.1")
-    Dependency(PackageSpec(name="Pango_jll", uuid="36c8627f-9965-5494-a995-c6b170f724f3"); compat="1.47.0")
+    Dependency(PackageSpec(name="Cairomm_jll", uuid="af74c99f-f0eb-54aa-aecc-a10e8fc65c17"); compat="^1.16.2")
+    Dependency(PackageSpec(name="Glibmm_jll", uuid="5d85a9da-21f7-5855-afec-cdc5039c46e8"); compat="^2.76.0")
+    Dependency(PackageSpec(name="Pango_jll", uuid="36c8627f-9965-5494-a995-c6b170f724f3"); compat="^1.50.9")
     BuildDependency(PackageSpec(name="Xorg_xorgproto_jll", uuid="c4d99508-4286-5418-9131-c86396af500b"))
 ]
 
