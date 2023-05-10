3678821af50e4c514a6c26b3e9c0c047c5597be9
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-05-09 22:33:15 -0400

    Trigger the recompilation of blis

--- B/blis/blis/build_tarballs.jl
+++ B/blis/blis/build_tarballs.jl
@@ -8,6 +8,5 @@ products = [
     LibraryProduct("libblis", :blis)
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                preferred_gcc_version=v"11", lock_microarchitecture=false, julia_compat="1.6")
