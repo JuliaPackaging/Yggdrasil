2055261da659881e4a85a9e517e55b7bc4c58cf3
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2023-05-09 21:39:57 -0400

    [blis32] Add an LP64 version of blis (#6608)

---
+++ B/blis/blis32/build_tarballs.jl
@@ -0,0 +1,13 @@
+name = "blis32"
+
+include("../common.jl")
+
+script = blis_script(blis32=true)
+
+products = [
+    LibraryProduct("libblis32", :blis32)
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               preferred_gcc_version=v"11", lock_microarchitecture=false, julia_compat="1.6")
