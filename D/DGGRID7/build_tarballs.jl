5e5d5da227b20ad1c8ac42f64e74c418713e84c9
Daniel Loos <dloos@bgc-jena.mpg.de>
2023-05-11 14:09:32 +0200

    Fix GDAL and bump version of DGGRID7

--- D/DGGRID7/build_tarballs.jl
+++ D/DGGRID7/build_tarballs.jl
@@ -7,24 +7,28 @@ version = v"0.9.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/allixender/DGGRID.git", "0dabdfd3f8fd6ac27626064a94c483e610d578bb")
+    GitSource("https://github.com/sahrk/DGGRID.git", "4f24e9de46f9e669af4cba94d9d148b557a1de11")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/DGGRID/src/
+cd $WORKSPACE/srcdir/DGGRID
+mkdir build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
 if [[ "${target}" == x86_64-linux-musl ]]; then
     # Remove libexpat to avoid it being picked up by mistake
     rm /usr/lib/libexpat.so*
 fi
 make -j${nproc} CCOMP="${CC}" CPPCOMP="${CXX}"
-cp "apps/dggrid/dggrid${exeext}" "${bindir}/."
+mkdir -p ${bindir}
+cp "src/apps/dggrid/dggrid${exeext}" "${bindir}/."
 """
+
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
-
 
 # The products that we will ensure are always built
 products = [
@@ -37,4 +41,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8.1.0", julia_compat="v1.6.0")
