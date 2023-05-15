5680f6e60fb2515cb8419fd54333f361098f672b
Tyler Thomas <tyler@saguarocm.com>
2023-05-14 17:30:01 -0700

    [XGBoost] initial CUDA 12 build

--- X/XGBoost/build_tarballs.jl
+++ X/XGBoost/build_tarballs.jl
@@ -1,7 +1,7 @@
-using BinaryBuilder, Pkg
+using BinaryBuilder, Pkg, BinaryBuilderBase
 
 name = "XGBoost"
-version = v"1.7.4"
+version = v"1.7.5"
 
 const YGGDRASIL_DIR = "../.."
 include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
@@ -9,7 +9,7 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 # Collection of sources required to build XGBoost
 sources = [
-    GitSource("https://github.com/dmlc/xgboost.git","36ad160501251336bfe69b602acc37ab3ec32d69"), 
+    GitSource("https://github.com/dmlc/xgboost.git","21d95f3d8f23873a76f8afaad0fee5fa3e00eafe"), 
     DirectorySource("./bundled"),
 ]
 
@@ -57,21 +57,22 @@ fi
 install_license LICENSE
 """
 
+versions_to_build = [
+    nothing,
+    v"11.0",
+    v"12.0", 
+]
+
+# XXX: support only specifying major/minor version (JuliaPackaging/BinaryBuilder.jl#/1212)
 cuda_full_versions = Dict(
     v"11.0" => v"11.0.3",
+    v"12.0" => v"12.0.1",
 )
-cuda_version = v"11.0"
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms())
-cuda_platforms = expand_cxxstring_abis(Platform("x86_64", "linux"; 
-                                        cuda=CUDA.platform(cuda_version)))
-
-for p in cuda_platforms
-    push!(platforms, p)
-end
-
+cuda_archs = Dict(
+    v"11.0" => "60;70;75;80",
+    v"12.0" => "60;70;75;80;89;90",
+)
 
 # The products that we will ensure are always built
 products = [
@@ -79,21 +80,35 @@ products = [
     ExecutableProduct("xgboost", :xgboost)
 ]
 
-# Dependencies that must be installed before this package can be built
-dependencies = [
-    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
-    # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
-    Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
+for cuda_version in versions_to_build
 
-    # You can only specify one cuda version in the deps. To build against more than 
-    # one cuda version, you have to include them as Archive Sources. (see Torch_jll)
-    BuildDependency(PackageSpec(name="CUDA_full_jll", version=cuda_full_versions[cuda_version]), platforms=cuda_platforms),
-    RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll"), platforms=cuda_platforms),
-]
+    if isnothing(cuda_version)
+        platforms = expand_cxxstring_abis(supported_platforms())
+        dependencies = [
+            # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
+            # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
+            Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
+            Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
+        ]
+        preamble = ""
+    else
+        platforms = expand_cxxstring_abis(Platform("x86_64", "linux"; 
+                            cuda=CUDA.platform(cuda_version)))
+        dependencies = [
+            Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
+            BuildDependency(PackageSpec(name="CUDA_full_jll", version=cuda_full_versions[cuda_version]), platforms=platforms),
+            RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll"), platforms=platforms),
+        ]
+        preamble = """
+        CUDA_ARCHS="$(cuda_archs[cuda_version])"
+        """
+        preamble = ""
+    end
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
-                preferred_gcc_version=v"8", 
-                julia_compat="1.6",
-                augment_platform_block=CUDA.augment)
+    # Build the tarballs, and possibly a `build.jl` as well.
+    build_tarballs(ARGS, name, version, sources,  preamble*script, platforms, products, dependencies; 
+                    preferred_gcc_version=v"8", 
+                    julia_compat="1.6",
+                    augment_platform_block=CUDA.augment)
+
+end
