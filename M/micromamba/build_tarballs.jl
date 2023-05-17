67e6dc90e58579df8ae3012faf9bb86a3e91aa81
Tyler Thomas <36181311+tylerjthomas9@users.noreply.github.com>
2023-05-17 08:45:16 -0700

    [micromamba] Update to v1.4.3 (#6753)

--- M/micromamba/build_tarballs.jl
+++ M/micromamba/build_tarballs.jl
@@ -3,29 +3,29 @@
 using BinaryBuilder
 
 name = "micromamba"
-version = v"1.4.1"
+version = v"1.4.3"
 build = "0"
 
 # Collection of sources required to build micromamba
 # These are actually just the conda packages for each platform
 sources = [
     FileSource("https://conda.anaconda.org/conda-forge/linux-64/micromamba-$version-$build.tar.bz2",
-        "3cf53fee4650a5b1e6fc1817b1b2c43c415d50af1efeaf0db0a0aee6820dd2cf",
+        "faf0a6af6d0676050a7ec535a3d10c50c9c03bbf0bb554732151cf62f3417379",
         filename="micromamba-x86_64-linux-gnu.tar.bz2"),
     FileSource("https://conda.anaconda.org/conda-forge/linux-aarch64/micromamba-$version-$build.tar.bz2",
-        "e7144aa2a18bfb65d1ec899041b2c3cbea34c8c95e7bebc5a866337e7f3af85b",
+        "1044323557ea4677c54c2cb643f0f36786da7085e1c2930a9f36521aae686388",
         filename="micromamba-aarch64-linux-gnu.tar.bz2"),
     FileSource("https://conda.anaconda.org/conda-forge/linux-ppc64le/micromamba-$version-$build.tar.bz2",
-        "283fb4da81426b7fdad2efa51a3708cec8b952d73bc7d0dd97958cc9e43ea36c",
+        "6157e877bc63f8f3081c0904075ef47952a86106870aa6f94f6116a6e951a50d",
         filename="micromamba-powerpc64le-linux-gnu.tar.bz2"),
     FileSource("https://conda.anaconda.org/conda-forge/osx-64/micromamba-$version-$build.tar.bz2",
-        "5d5f088d93843734598daa30cfbf4eb5bb7621b2cb80a81898b75883984a30df",
+        "36c437a03c7cc72b4366d5225afa86d65e21f5100bd30cdb5b602465e812a02a",
         filename="micromamba-x86_64-apple-darwin14.tar.bz2"),
     FileSource("https://conda.anaconda.org/conda-forge/osx-arm64/micromamba-$version-$build.tar.bz2",
-        "4670ec5715af1c26e3a5c19a9afe5d10a48c8cd3966f733b1c586640330a4be2",
+        "93dee34f603cc189e9d1ac99ed2938bedd8de81fab350fce128bd453a04bd73b",
         filename="micromamba-aarch64-apple-darwin20.tar.bz2"),
     FileSource("https://conda.anaconda.org/conda-forge/win-64/micromamba-$version-$build.tar.bz2",
-        "d9ee76ffbecb53e4e87c88f6824d6c4bf6ee8b8a0cd234551dc387a70663d0cf",
+        "173d2a8dd8e324611fa7331992896ebf7ea2f953c61559c10772ee377af27d05",
         filename="micromamba-x86_64-w64-mingw32.tar.bz2"),
 ]
 
