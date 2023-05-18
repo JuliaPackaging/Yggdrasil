b1ac4a41696ad28702d6381ab4d207c573d60c2d
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-05-18 12:51:35 +0200

    Bump to mac 10.14 for x86

--- A/ArcadeLearningEnvironment/build_tarballs.jl
+++ A/ArcadeLearningEnvironment/build_tarballs.jl
@@ -8,12 +8,26 @@ version = v"0.8.1"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/mgbellemare/Arcade-Learning-Environment.git", "ba84c1480008aa606ebc1efd7a04a7a7729796d4"),
+    ArchiveSource(
+        "https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
+        "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
+# This requires macOS 10.14
+if [[ "${target}" == x86_64-apple-darwin* ]]; then
+    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
+    rm -rf /opt/${target}/${target}/sys-root/System
+    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
+    cp -ra System "/opt/${target}/${target}/sys-root/."
+    export MACOSX_DEPLOYMENT_TARGET=10.14
+    popd
+fi
+
 cd $WORKSPACE/srcdir/Arcade-Learning-Environment/
 mkdir build && cd build
+
 cmake -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
