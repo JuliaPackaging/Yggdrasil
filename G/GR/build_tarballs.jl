60ceb6dc7578e4be977b82cb6f2f5626b479ebed
Josef Heinen <jheinen@users.noreply.github.com>
2023-05-18 07:40:32 +0200

    Update G/GR/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- G/GR/build_tarballs.jl
+++ G/GR/build_tarballs.jl
@@ -43,7 +43,6 @@ if [[ "${target}" == x86_64-apple-darwin* ]]; then
 fi
 
 if [[ "${target}" == *apple* ]]; then
-    export MACOSX_DEPLOYMENT_TARGET=10.14
     make -C 3rdparty/zeromq ZEROMQ_EXTRA_CONFIGURE_FLAGS="--host=${target}"
 fi
 
