28cd8e0ebe8c508f0203cf7a0c679dd450ec4f2f
Mosè Giordano <mose@gnu.org>
2023-05-20 18:44:01 +0100

    Exclude `libmodplug` from the lot, it can't be rebuilt on macOS

--- L/libmodplug/build_tarballs.jl
+++ L/libmodplug/build_tarballs.jl
@@ -7,14 +7,13 @@ version = v"0.8.9"
 
 # Collection of sources required to build libmodplug
 sources = [
-    ArchiveSource("https://downloads.sourceforge.net/modplug-xmms/libmodplug-$(version).0.tar.gz",
-                  "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de"),
+    "https://downloads.sourceforge.net/modplug-xmms/libmodplug-$(version).0.tar.gz" =>
+    "457ca5a6c179656d66c01505c0d95fafaead4329b9dbaa0f997d00a3508ad9de",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libmodplug-*/
-sed -i 's/-ffast-math//g' configure # This should be a real patch, but `sed` is quicker
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make -j${nproc}
 make install
@@ -23,7 +22,6 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
-filter!(p->arch(p) != "armv6l" && !(Sys.isapple(p) && arch(p) == "aarch64"), platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -31,7 +29,7 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
+dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
