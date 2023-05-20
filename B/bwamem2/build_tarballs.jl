16b0e438a3a777cb8f2ebb23292581029fcfe27a
Malte Siemers <malte.siemers@mailbox.org>
2023-05-20 18:12:32 +0200

    build from dedicated build dir
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- B/bwamem2/build_tarballs.jl
+++ B/bwamem2/build_tarballs.jl
@@ -13,7 +13,11 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/bwa-mem2-*/ext/safestringlib/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
+mkdir build && cd build
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    ..
 cd ../../
 make
 mkdir -p "${libdir}" "${bindir}"
