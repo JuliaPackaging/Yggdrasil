a661ca1a185a76e3892b079417846bdb6c6d26be
Mosè Giordano <giordano@users.noreply.github.com>
2023-05-20 18:47:22 +0100

    Rebuild more packages to automatically pick up license files (#6772)

--- L/lm_Sensors/build_tarballs.jl
+++ L/lm_Sensors/build_tarballs.jl
@@ -7,20 +7,21 @@ version = v"3.5.0"
 
 # Collection of sources required to build lm_sensors
 sources = [
-    "https://github.com/lm-sensors/lm-sensors/archive/V$(version.major)-$(version.minor)-$(version.patch).tar.gz" =>
-    "f671c1d63a4cd8581b3a4a775fd7864a740b15ad046fe92038bcff5c5134d7e0",
+    GitSource("https://github.com/lm-sensors/lm-sensors.git",
+              "e8afbda10fba571c816abddcb5c8180afc435bba"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/lm-sensors-*/
+cd $WORKSPACE/srcdir/lm-sensors*/
 make -j${nproc} PREFIX=${prefix}
 make install PREFIX=${prefix}
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [p for p in supported_platforms() if Sys.islinux(p)]
+platforms = supported_platforms(; exclude=!Sys.islinux)
+filter!(p->arch(p) != "armv6l", platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -29,7 +30,7 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
+dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
