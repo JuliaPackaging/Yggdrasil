5bb6895b489a19ad2d82521a4930f393215cdbeb
Mosè Giordano <mose@gnu.org>
2023-05-22 23:29:45 +0100

    [isa_l] Exclude aarch64 Darwin

--- I/isa_l/build_tarballs.jl
+++ I/isa_l/build_tarballs.jl
@@ -32,6 +32,12 @@ filter!(p -> arch(p) != "i686", platforms)
 # YASM v1.3.0 (latest stable version as of 2023-05-22) doesn't seem to
 # understand AVX512 opcodes.
 filter!(!Sys.iswindows, platforms)
+# Compilation for aarch64 Darwin fails with error
+#     /tmp/crc16_t10dif_pmull-69b5ed.s:209:9: error: unknown AArch64 fixup kind!
+#             ldr q_fold_const, fold_constant
+#             ^
+#     make[1]: *** [Makefile:3623: crc/aarch64/crc16_t10dif_pmull.lo] Error 1
+filter!(p -> !(Sys.isapple(p) && arch(p) == "aarch64"), platforms)
 
 # The products that we will ensure are always built
 products = [
