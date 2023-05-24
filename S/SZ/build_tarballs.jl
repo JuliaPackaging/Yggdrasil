04c3178c606eeae45839f6c8738afc6068a99a87
Erik Schnetter <schnetter@gmail.com>
2023-05-23 22:45:50 -0400

    Set compat bounds for HDF5_jll for all direct users of HDF5_jll [skip build] (#6707)
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    [skip ci]

--- S/SZ/build_tarballs.jl
+++ S/SZ/build_tarballs.jl
@@ -80,7 +80,8 @@ dependencies = [
                platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e");
                platforms=filter(Sys.isbsd, platforms)),
-    Dependency("HDF5_jll"; platforms=hdf5_platforms),
+    # Updating to a newer HDF5 version is likely possible without problems but requires rebuilding this package
+    Dependency("HDF5_jll"; compat="~1.12", platforms=hdf5_platforms),
     Dependency("NetCDF_jll"; platforms=hdf5_platforms),
     Dependency("Zlib_jll"),
     Dependency("Zstd_jll"),
