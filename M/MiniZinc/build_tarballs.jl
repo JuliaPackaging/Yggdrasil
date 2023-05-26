23eb45a07a0a7211b6b5eb39c86a97ed3722e5ad
Oscar Dowson <odow@users.noreply.github.com>
2023-05-27 11:44:21 +1200

    Update build_tarballs.jl

--- M/MiniZinc/build_tarballs.jl
+++ M/MiniZinc/build_tarballs.jl
@@ -28,7 +28,7 @@ cd build
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CXX_FLAGS="-I${includedir}/highs" \
+    -DCMAKE_CXX_FLAGS="-I${includedir}/highs -L${libdir}" \
     ..
 
 if [[ "${target}" == *-linux-* ]]; then
