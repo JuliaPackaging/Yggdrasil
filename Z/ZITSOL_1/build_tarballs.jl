5ab34f821740efef238e8c2cda903a308227a893
Mosè Giordano <giordano@users.noreply.github.com>
2023-05-27 17:46:59 +0100

    Update Z/ZITSOL_1/build_tarballs.jl

--- Z/ZITSOL_1/build_tarballs.jl
+++ Z/ZITSOL_1/build_tarballs.jl
@@ -26,7 +26,7 @@ install_license ../{COPYRIGHT,LGPL}
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
