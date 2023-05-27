5a1b36aad95b044844e1a3f1d60b1ed05c52e0f8
Christian Dengler <sumo_spider@yahoo.de>
2023-05-27 18:45:34 +0200

    New Recipe: mosquitto_client v2.0.15

---
+++ M/mosquitto_client/build_tarballs.jl
@@ -0,0 +1,39 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "mosquitto_client"
+version = v"2.0.15"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/eclipse/mosquitto.git", "dc75fec606d5c266cff5b3cf2259beee4523cda7")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+export OPENSSL_ROOT_DIR=/workspace/destdir/lib64/
+cd mosquitto/
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -D DOCUMENTATION=OFF -D WITH_CJSON=OFF -D WITH_BROKER=OFF -D WITH_APPS=OFF -D WITH_PLUGINS=OFF .
+make
+make install
+install_license LICENSE.txt
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libmosquitto", :libmosquitto)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
