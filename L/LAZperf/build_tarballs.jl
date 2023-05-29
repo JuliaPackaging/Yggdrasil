51b1f795be5d3c5143f4db37144e63c3e0a00a06
Mosè Giordano <giordano@users.noreply.github.com>
2023-05-29 16:17:05 +0100

    Update build_tarballs.jl

--- L/LAZperf/build_tarballs.jl
+++ L/LAZperf/build_tarballs.jl
@@ -7,29 +7,29 @@ version = v"3.3.0"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/hobu/laz-perf/archive/refs/tags/$version.tar.gz", "029e830758d2a0d873110b714e7f918c1a8fbad6f6538f0af9f41c3fec9de0c0")
+    GitSource("https://github.com/hobu/laz-perf.git",
+              "8dc8d055a8f9a5f86bde6ea4d45722febf9c432c"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
+cd $WORKSPACE/srcdir/laz-perf*
 
-cd $WORKSPACE/srcdir/laz-perf-*
-
-cmake . \
--DCMAKE_INSTALL_PREFIX=$prefix \
--DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
--DCMAKE_BUILD_TYPE=Release \
--DEMSCRIPTEN=OFF \
--DWITH_TESTS=OFF
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DEMSCRIPTEN=OFF \
+    -DWITH_TESTS=OFF
 
 make -j${nproc}
 make install
-
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
+platforms = expand_cxxstring_abis(supported_platforms())
 
 
 # The products that we will ensure are always built
