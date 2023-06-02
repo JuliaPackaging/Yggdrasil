c642b3470fb87ed0abaf3ea2ca0e1b8fc0fa0ee7
Michael Persico <michael.a.persico@gmail.com>
2023-06-02 19:11:18 -0400

    [MUSCLE] add recipe (#6837)
    * [MUSCLE] add recipe
    
    [MUSCLE](https://github.com/rcedgar/muscle) v5.2 (MacOS, Linux builds)
    
    * Attempt fix for failed MacOS build
    
    * Linux-only build for now
    
    * [MUSCLE] additional tweaks
    
    * [MUSCLE] replace patch with variable overwrite

---
+++ M/MUSCLE/build_tarballs.jl
@@ -0,0 +1,35 @@
+using BinaryBuilder, Pkg
+
+name = "MUSCLE"
+version = v"5.2"
+
+sources = [
+    GitSource("https://github.com/rcedgar/muscle.git",
+                  "6c601163998616bb88991931e443c645858e162c"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ${WORKSPACE}/srcdir/muscle/src/
+make -j${nproc} CXXFLAGS="-O3 -fopenmp"
+install -Dvm 755 "Linux/muscle" "${bindir}/muscle"
+install_license ${WORKSPACE}/srcdir/muscle/LICENSE
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = filter!(Sys.islinux, supported_platforms())
+platforms = expand_cxxstring_abis(platforms)
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("muscle", :muscle),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"6")
