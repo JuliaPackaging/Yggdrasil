9bfd65d2407ab37fc8f616ca35540914c915dc0e
Erik Schnetter <schnetter@gmail.com>
2023-06-06 10:28:22 -0400

    Update B/Blosc/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- B/Blosc/build_tarballs.jl
+++ B/Blosc/build_tarballs.jl
@@ -35,7 +35,7 @@ install_license ../LICENSES/*.txt
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental = true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
