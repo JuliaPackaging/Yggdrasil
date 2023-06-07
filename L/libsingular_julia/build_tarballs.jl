d8da2bdabcfa4078877200e68ddaa3d620627fb5
Max Horn <max@quendi.de>
2023-06-07 16:13:49 +0200

    [libsingular_julia] various updates
    - switch to new repository
    - store tree hash of the source code
    - update Singular_jll dependency to latest

--- L/libsingular_julia/build_tarballs.jl
+++ L/libsingular_julia/build_tarballs.jl
@@ -9,7 +9,7 @@ uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "libsingular_julia"
-version = v"0.32.1"
+version = v"0.33.0"
 
 # reminder: change the above version if restricting the supported julia versions
 julia_versions = [v"1.6.3", v"1.7", v"1.8", v"1.9", v"1.10"]
@@ -17,12 +17,12 @@ julia_compat = join("~" .* string.(getfield.(julia_versions, :major)) .* "." .* 
 
 # Collection of sources required to build libsingular-julia
 sources = [
-    GitSource("https://github.com/oscar-system/libsingular-julia.git", "eff1f172c070325887361c63b85b614de7e6b675"),
+    GitSource("https://github.com/oscar-system/Singular.jl.git", "14fdb93f83a7bc5a1ba396b85cf1a4fa9eff49d4"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd libsingular-julia
+cd Singular.jl/deps/src
 cmake . -B build \
    -DJulia_PREFIX="$prefix" \
    -DSingular_PREFIX="$prefix" \
@@ -33,6 +33,9 @@ cmake . -B build \
    -DCMAKE_BUILD_TYPE=Release
 
 VERBOSE=ON cmake --build build --config Release --target install -- -j${nproc}
+
+# store tree hash of the source directory
+git ls-tree HEAD .. | cut -c13-52 > ${libdir}/libsingular_julia.treehash
 
 install_license LICENSE.md
 """
@@ -55,7 +58,7 @@ dependencies = [
     BuildDependency("GMP_jll"),
     BuildDependency("MPFR_jll"),
     Dependency("libcxxwrap_julia_jll"),
-    Dependency("Singular_jll", compat = "~403.202.200"),
+    Dependency("Singular_jll", compat = "~403.203.200"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
