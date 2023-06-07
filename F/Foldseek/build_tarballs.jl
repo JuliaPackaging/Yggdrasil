eb6e3f9b6f6ac51d0ebcf9789445772df836d7e0
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-06-08 00:14:03 +0200

    [Foldseek] update to v6, take 2

--- F/Foldseek/build_tarballs.jl
+++ F/Foldseek/build_tarballs.jl
@@ -1,3 +1,18 @@
+using BinaryBuilder, Pkg
+
+name = "Foldseek"
+version = v"6"
+
+# url = "https://github.com/steineggerlab/foldseek"
+# description = "Fast and sensitive comparisons of large protein structure sets"
+
+sources = [
+    # Foldseek 6-29e2557
+    GitSource("https://github.com/steineggerlab/foldseek",
+              "29e2557970c39c8e689601ecaae2279fff4faa17"),
+    DirectorySource("./bundled"),
+]
+
 # TODO
 # - use Zstd_jll ? (static lib?) (now uses builtin zstd lib)
 
@@ -8,31 +23,14 @@
 #   /opt/x86_64-unknown-freebsd12.2/x86_64-unknown-freebsd12.2/sys-root//usr/include/sys/time.h
 #   see: https://github.com/JuliaPackaging/Yggdrasil/pull/6195#issuecomment-1416227398
 
-using BinaryBuilder, Pkg
-
-name = "Foldseek"
-
-# foldseek seem to use as versioning scheme of "major version + first 7
-# characters of the tagged commit"
-version = v"5"
-# version_commitprefix = "53465f0"
-
-# url = "https://github.com/steineggerlab/foldseek"
-# description = "Fast and sensitive comparisons of large protein structure sets"
-
-sources = [
-    # Foldseek 5-53465f0
-    # Note: this is the same commit as v"4" in Yggdrasil
-    GitSource("https://github.com/steineggerlab/foldseek",
-              "53465f07cdeed1f7fda08ee7f188327cb57c37ba"),
-    DirectorySource("./bundled"),
-]
-
 script = raw"""
 cd $WORKSPACE/srcdir/foldseek*/
 
 # patch lib/mmseqs/CMakeLists.txt so it doesn't set -march unnecessarily on ARM
 atomic_patch -p1 ../patches/mmseqs-arm-simd-march-cmakefile.patch
+
+# remove rustup check in corrosion's FindRust.cmake
+atomic_patch -p1 ../patches/corrosion-remove-rustup-check.patch
 
 ARCH_FLAGS=
 if [[ "${target}" == x86_64-* || "${target}" == i686-* ]]; then
@@ -43,17 +41,37 @@ elif [[ "${target}" == aarch64-* ]]; then
     ARCH_FLAGS="-DHAVE_ARM8=1"
 fi
 
+# hack around corrosion (cmake cargo interop package) using a wrong
+# target string when cross-compiling
+CARGO_TARGET=
+if [[ "${target}" == *-linux-* ]]; then
+    # cargo uses an extra 'unknown' in the target string,
+    # e.g. 'x86_64-linux-gnu' becomes 'x86_64-unknown-linux-gnu' in cargo
+    CARGO_TARGET=$(echo ${target} | cut -d- -f1)-unknown-$(echo ${target} | cut -d- -f2-)
+else
+    CARGO_TARGET=${target}
+fi
+
+export RUSTFLAGS=
+if [[ "${target}" == *-musl* ]]; then
+    # avoid 'cannot create cdylib' error on musl targets
+    # see https://github.com/rust-lang/cargo/issues/8607
+    #     https://github.com/rust-lang/rust/issues/59302
+    export RUSTFLAGS="-C target-feature=-crt-static"
+fi
+
 mkdir build && cd build
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=RELEASE \
-    -DNATIVE_ARCH=0 ${ARCH_FLAGS}
+    -DNATIVE_ARCH=0 ${ARCH_FLAGS} \
+    -DRust_COMPILER=$(which ${RUSTC}) -DRust_CARGO_TARGET=${CARGO_TARGET}
 make -j${nproc}
 make install
 
 install_license ../LICENSE.md
 """
 
-platforms = supported_platforms(; exclude = p -> Sys.iswindows(p) || Sys.isfreebsd(p) || arch(p) == "i686")
+platforms = supported_platforms(; exclude = p -> Sys.iswindows(p) || Sys.isfreebsd(p) || nbits(p) == 32)
 platforms = expand_cxxstring_abis(platforms)
 
 products = [
@@ -72,4 +90,4 @@ dependencies = Dependency[
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version = v"8")
+               julia_compat="1.6", preferred_gcc_version = v"8", compilers=[:c, :rust])
