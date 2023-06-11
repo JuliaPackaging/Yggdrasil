d7d077fbf1da19189e672b79ea5b9adea167b9c1
aviks <avik@sengupta.net>
2023-06-11 18:21:22 -0400

    build for all platforms

--- P/protobuf-c/build_tarballs.jl
+++ P/protobuf-c/build_tarballs.jl
@@ -20,13 +20,8 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "musl")
-]
 
-# platforms = supported_platforms()
+platforms = supported_platforms()
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
