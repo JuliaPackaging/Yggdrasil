510be3597b40944dc9a638bb739b54f1c8b55c03
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-06-12 10:52:34 +0200

    Fix RNAxplorer build on windows (w64-mingw32), and add some notes
    Windows fixes for RNAxplorer for missing POSIX functionality:
    
    - work around missing regex.h by using PCRE
    
    - work around missing `sysconf(_SC_NPROCESSORS_ONLN)` by using `GetActiveProcessorCount(ALL_PROCESSOR_GROUPS)`
    
    - work around missing getline by using the NetBSD implementation

--- V/ViennaRNA/build_tarballs.jl
+++ V/ViennaRNA/build_tarballs.jl
@@ -10,13 +10,15 @@ sources = [
     DirectorySource("./bundled")
 ]
 
-# Build issues and notes
-# - windows: disabled RNAxplorer build, regex.h not found
+# Build issues
 # - powerpc64le: build fails in vectorisation routines of dlib
-# - windows and gcc-8: internal compiler error during lto on gcc-8
-# - can we conditionally have
-#       ExecutableProduct("RNAxplorer", :RNAxplorer)
-#   for non-windows platforms?
+# - windows and gcc-8: internal compiler error during lto on gcc-8,
+#   which is why we use gcc-9
+
+# Notes
+# - I think only RNAxplorer needs to link to blas, lapack (and pcre on windows)
+#   also the generated shared lib doesn't need to link to these libs
+
 
 script = raw"""
 cd $WORKSPACE/srcdir/ViennaRNA*/
@@ -35,6 +37,7 @@ export LDFLAGS="-L${libdir}"
 
 # fix compile fail on musl
 # ViennaRNA PR: https://github.com/ViennaRNA/ViennaRNA/pull/187
+# should be included in next release (ViennaRNA-2.6.1)
 atomic_patch -p1 ../patches/fix-no-uint-typedef-on-musl.patch
 
 if [[ $target == *-w64-mingw32* ]]; then
@@ -45,16 +48,26 @@ if [[ $target == *-w64-mingw32* ]]; then
     # fix for missing nonstandard strcasestr function on windows
     atomic_patch -p1 ../patches/windows-strcasestr-fix.patch
 
+    # fix for missing sysconf on windows
+    atomic_patch -p1 ../patches/RNAxplorer-windows-fix-missing-sysconf.patch
+
+    # fix for missing getline on windows, we use the one from NetBSD
+    atomic_patch -p1 ../patches/RNAxplorer-windows-fix-missing-getline.patch
+
     # needed for compilation of dlib on windows
     export LIBS="-lwinmm"
 
-    # disable RNAxplorer
-    CONFIGURE_FLAGS="--without-rnaxplorer"
+    # work around there not being a regex.h on w64-mingw32
+    cp "${includedir}/pcreposix.h" "${includedir}/regex.h"
+    export LIBS="$LIBS -lpcreposix-0"
+
+    # BLAS/LAPACK support
+    export LIBS="$LIBS -lblastrampoline-5"
 else
-    # enable RNAxplorer
-    CONFIGURE_FLAGS="--with-rnaxplorer --with-blas=${libdir}/libopenblas.${dlext} --with-lapack=${libdir}/liblapack.${dlext}"
-    export LIBS="-lblastrampoline"
+    # BLAS/LAPACK support
+    export LIBS="$LIBS -lblastrampoline"
 fi
+
 
 # configure script fails (only in sandbox) without
 # setting ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes
@@ -64,7 +77,7 @@ ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes \
     --with-pic --disable-c11 \
     --with-mpfr --with-json --with-svm --with-gsl \
     --enable-openmp \
-    $CONFIGURE_FLAGS \
+    --with-rnaxplorer --with-blas="${libdir}/libopenblas.${dlext}" --with-lapack="${libdir}/liblapack.${dlext}" \
     --with-cluster --with-kinwalker \
     --without-perl --without-python --without-python2 \
     --without-doc --without-tutorial --without-cla
@@ -91,17 +104,25 @@ fi
     -Wl,$(flagon --whole-archive) ./src/ViennaRNA/libRNA.a -Wl,$(flagon --no-whole-archive) \
     $libs
 
+# install licenses
 cp src/cthreadpool/LICENSE LICENSE-cthreadpool
 cp src/dlib-*/LICENSE.txt LICENSE-dlib
 cp src/json/LICENSE LICENSE-json
 cp src/libsvm-*/COPYRIGHT COPYRIGHT-libsvm
 cp src/RNAforester/COPYING COPYING-RNAforester
+cp src/RNAxplorer/COPYING COPYING-RNAxplorer
 install_license LICENSE-cthreadpool
 install_license LICENSE-dlib
 install_license LICENSE-json
 install_license COPYRIGHT-libsvm
 install_license COPYING-RNAforester
+install_license COPYING-RNAxplorer
 install_license COPYING
+if [[ $target == *-w64-mingw32* ]]; then
+    # this code/license is only added on windows via a patch
+    cp src/RNAxplorer/COPYING-getline COPYING-RNAxplorer-getline
+    install_license COPYING-RNAxplorer-getline
+fi
 """
 
 platforms = supported_platforms(; exclude = p -> arch(p) == "powerpc64le")
@@ -142,8 +163,7 @@ products = [
     ExecutableProduct("RNAsnoop", :RNAsnoop),
     ExecutableProduct("RNAsubopt", :RNAsubopt),
     ExecutableProduct("RNAup", :RNAup),
-    # not available on windows
-    # ExecutableProduct("RNAxplorer", :RNAxplorer),
+    ExecutableProduct("RNAxplorer", :RNAxplorer),
     LibraryProduct("libRNA", :libRNA),
 ]
 
@@ -156,11 +176,11 @@ dependencies = [
                platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e");
                platforms=filter(Sys.isbsd, platforms)),
-    # we need BLAS/LAPACK for RNAxplorer, but this fails to build on windows
-    Dependency(PackageSpec(name="LAPACK_jll");
-               platforms=filter(!Sys.iswindows, platforms)),
-    Dependency(PackageSpec(name="OpenBLAS_jll");
-               platforms=filter(!Sys.iswindows, platforms)),
+    # we need BLAS/LAPACK for RNAxplorer
+    Dependency("LAPACK_jll"),
+    Dependency("OpenBLAS_jll"),
+    # windows POSIX regex replacement via PCRE
+    Dependency("PCRE_jll"; platforms=filter(Sys.iswindows, platforms)),
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
