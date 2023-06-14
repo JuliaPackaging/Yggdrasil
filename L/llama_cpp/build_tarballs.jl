88dd9f44b7948e6a349877813deedfe391ee1c11
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-06-14 14:56:17 +0200

    Use gcc-8

--- L/llama_cpp/build_tarballs.jl
+++ L/llama_cpp/build_tarballs.jl
@@ -123,4 +123,4 @@ dependencies = Dependency[
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version = v"12")
+               julia_compat="1.6", preferred_gcc_version = v"8")
