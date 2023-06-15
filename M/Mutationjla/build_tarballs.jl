070ecc8a6b47c502448691ffe5a50c4a077ead22
Ahmad Peyvan <115842305+apey236@users.noreply.github.com>
2023-06-15 11:54:10 -0400

    Update M/Mutationjla/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- M/Mutationjla/build_tarballs.jl
+++ M/Mutationjla/build_tarballs.jl
@@ -25,9 +25,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("aarch64", "macos"; )
-]
+platforms = supported_platforms()
 
 
 # The products that we will ensure are always built
