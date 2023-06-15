3c375a57866035f1eee9be743360477c80f8bfd5
Ahmad Peyvan <115842305+apey236@users.noreply.github.com>
2023-06-15 11:56:56 -0400

    Update build_tarballs.jl
    Changed the name of the library

--- M/Mutationjla/build_tarballs.jl
+++ M/Mutationjla/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "Mutationjla"
+name = "Mutationjl"
 version = v"1.0.5"
 
 # Collection of sources required to complete build
