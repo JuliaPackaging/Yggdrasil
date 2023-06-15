e79eb226a14906b2bba1c96290223eda704e6f2f
Ahmad Peyvan <115842305+apey236@users.noreply.github.com>
2023-06-15 11:53:43 -0400

    Update M/Mutationjla/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- M/Mutationjla/build_tarballs.jl
+++ M/Mutationjla/build_tarballs.jl
@@ -14,7 +14,11 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd Mutationpp/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
+mkdir build && cd build
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release
 make 
 make install
 """
