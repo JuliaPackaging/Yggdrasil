c41d1a7e7b205b13b4e6cf97b3f7fae4d88159ca
Michael Buche <mrbuche@sandia.gov>
2023-06-22 11:19:01 -0600

    Update build_tarballs.jl

--- P/Polymers/build_tarballs.jl
+++ P/Polymers/build_tarballs.jl
@@ -1,15 +1,15 @@
 using BinaryBuilder, Pkg
 
-name = "polymers"
+name = "Polymers"
 version = v"0.3.6"
 sources = [
     GitSource(
-        "https://github.com/sandialabs/polymers.git",
+        "https://github.com/sandialabs/Polymers.git",
         "c977b84d921019b3731b2c6cce2537e1d03ebcef",
     ),
 ]
 script = raw"""
-cd $WORKSPACE/srcdir/polymers
+cd $WORKSPACE/srcdir/Polymers
 cargo build --release --features extern
 install -Dvm 755 "target/${rust_target}/release/libpolymers.so" "${libdir}/libpolymers.so"
 """
