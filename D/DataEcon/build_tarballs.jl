e943d8ec6f892d5daae09b9cf1599f1ba0c53eff
Boyan Bejanov <bbejanov@bankofcanada.ca>
2023-06-24 14:57:38 -0400

    new recipe: DataEcon (#6937)
    * new recipe: DataEcon
    
    * Apply suggestions from code review
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Update D/DataEcon/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ D/DataEcon/build_tarballs.jl
@@ -0,0 +1,41 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "DataEcon"
+version = v"0.1.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/bankofcanada/DataEcon.git",
+        "c99b6b8ea8a5d500ba8d091b384d58a815b7db01")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/DataEcon/
+make -j${nproc} all LIBDE="bin/libdaec.${dlext}"
+install_license LICENSE.md
+install -Dvm 755 "bin/sqlite3${exeext}" "${bindir}/sqlite3${exeext}"
+install -Dvm 755 "bin/libdaec.${dlext}" "${libdir}/libdaec.${dlext}"
+install -Dvm 644 src/daec.h "${includedir}/daec.h"
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct(["libdaec", "daec"], :libdaec),
+    ExecutableProduct("sqlite3", :sqlite3shell),
+    FileProduct("include/daec.h", :daec_header),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"10.2.0")
