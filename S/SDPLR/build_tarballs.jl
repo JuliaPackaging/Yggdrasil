71d3d7a2f0eb7f696935c775e66189a55aa4bb6f
Benoît Legat <benoit.legat@gmail.com>
2023-06-28 16:18:45 +0200

    Add SDPLR (#6941)
    * Add SDPLR
    
    * install instead of cp
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Remove ext
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Add debug
    
    * Revert "Remove ext"
    
    This reverts commit 9a2700ba1408265b5056e51dded03e1e6053bbbb.
    
    * GCC v5
    
    * GCC v6
    
    * Branch on the .exe
    
    * Fix
    
    * https
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * for loop
    
    * Add CompilerSupportLibraries
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ S/SDPLR/build_tarballs.jl
@@ -0,0 +1,46 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "SDPLR"
+version = v"1.0.3"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://sburer.github.io/files/SDPLR-1.03-beta.zip", "f1f945734f72e008fd7be8544b27341b179292c3304226563d9c0f6cf503b2eb")
+]
+
+# Bash recipe for building across all platforms
+# Even if the `Makefile` specifies `-o ../sdplr`,
+# On Windows with `libgfortran5`, `.exe` is added.
+# On Windows with `libgfortran3` or `libgfortran4`, nothing is added though.
+# so we try both
+script = raw"""
+cd $WORKSPACE/srcdir/SDPLR*
+make LAPACK_LIB=-lopenblas BLAS_LIB=
+for executable in sdplr sdplr${exeext}
+do
+    if [[ -f ${executable} ]]; then
+        install -Dvm 755 ${executable} "${bindir}/sdplr${exeext}"
+    fi
+done
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = expand_gfortran_versions(supported_platforms())
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("sdplr", :sdplr)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency("OpenBLAS32_jll"),
+    Dependency("CompilerSupportLibraries_jll"),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+# Use GCC v6 to fix OpenMP issues on PowerPC
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", julia_compat="1.6")
