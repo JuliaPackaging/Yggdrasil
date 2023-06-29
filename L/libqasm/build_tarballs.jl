f562b44d94be07c0b5dbcb775921efc620a69ffb
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2023-06-30 00:42:13 +0200

    Extend `platforms` to all supported platforms

--- L/libqasm/build_tarballs.jl
+++ L/libqasm/build_tarballs.jl
@@ -28,11 +28,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "musl")
-]
+platforms = supported_platforms()
 
 
 # The products that we will ensure are always built
