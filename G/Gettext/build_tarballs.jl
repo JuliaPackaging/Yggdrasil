2ac4c392592d6b2c711d16ace3659f8d64e99a59
Steven G. Johnson <stevenj@mit.edu>
2023-06-28 20:28:43 -0400

    Update build_tarballs.jl

--- G/Gettext/build_tarballs.jl
+++ G/Gettext/build_tarballs.jl
@@ -48,4 +48,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"8.1")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", preferred_gcc_version=v"8")
