79cadba8fc3fb908b4994b3035317643b32608c3
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-07-03 15:47:49 +0200

    Change name to STRIDE, change repo source with clear license

--- S/STRIDE/build_tarballs.jl
+++ S/STRIDE/build_tarballs.jl
@@ -1,30 +1,28 @@
 using BinaryBuilder, Pkg
 using BinaryBuilderBase: Dependency
 
-name = "Stride"
+name = "STRIDE"
 version = v"1.0.0"
 
-# url = "https://github.com/MDAnalysis/stride"
+# url = "https://github.com/josch/stride/"
 # description = "Protein secondary structure assignment from atomic coordinates"
 
 sources = [
-    GitSource("https://github.com/MDAnalysis/stride",
-              "867a5eb0f2479cb16615512a53ee472c54649505"),
+    GitSource("https://github.com/josch/stride/",
+              "79aeff9a7e875eee17f3b34364a1db5da2dc222d"),
 ]
 
 script = raw"""
-cd $WORKSPACE/srcdir/stride*/
-
-cd src
+cd $WORKSPACE/srcdir/
 
 make CC="${CC} -g -O2 -fPIC -Wall"
 install -Dvm 755 "stride" "${bindir}/stride${exeext}"
 
 # build shared library
 mkdir -p "${libdir}"
-"$CC" -shared -O2 -g -o "${libdir}"/libstride.${dlext} *.o
+"${CC}" -shared -O2 -g -o "${libdir}"/libstride.${dlext} *.o
 
-install_license ../LICENSE
+install_license doc/stride.doc
 """
 
 platforms = supported_platforms()
