53c1db823389cd539dc341294b657cc617dcbb44
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-07-05 15:02:18 +0200

    Don't use libcifpp_jll, build it as a static library here

--- D/DSSP/build_tarballs.jl
+++ D/DSSP/build_tarballs.jl
@@ -12,10 +12,21 @@ sources = [
               "b87ef206a071e6f086c8dc01551afd5e9b23eb43"),
     GitSource("https://github.com/mhekkel/libmcfp",
               "4aa95505ded43e663fd9dae61c49b08fdc6cce0c"), # v1.2.4
+    GitSource("https://github.com/PDB-REDO/libcifpp",
+              "836aed6ea9a227b37e5b0d9cbcb1253f545d0778"), # v5.1.0 (git-tag v5.1.0.1)
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
                   "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
     DirectorySource("./bundled"),
 ]
+
+# NOTE
+# We don't use libcifpp_jll, as that is currently built incorrectly on
+# windows.  Instead, we build libcifpp here as well and use it as a
+# static library, as that seems to be the default way to build
+# libcifpp and dssp.
+#
+# Ref: https://github.com/JuliaPackaging/Yggdrasil/pull/7001
+
 
 # The tests only pass with the correct cxxabi (-cxx11), so we create a
 # MACHTYPE_FULL variable to pass to the shell script which can there
@@ -32,8 +43,8 @@ MACHTYPE_FULL=$MACHTYPE_FULL
 """ * raw"""
 cd $WORKSPACE/srcdir/
 
-# Install a newer MacOS SDK
-# fixes compilation of dssp
+# Install a newer MacOS SDK on x86_64-apple-darwin
+# Fixes compilation of libcifpp and dssp
 # - cmake fails on checking for std::filesystem
 # - compile error: 'any_cast<std::basic_string<char>>' is unavailable: introduced in macOS 10.14
 if [[ "${target}" == x86_64-apple-darwin* ]]; then
@@ -45,8 +56,16 @@ if [[ "${target}" == x86_64-apple-darwin* ]]; then
     popd
 fi
 
+# build the tests if we are building for the build host platform
+CFG_TESTING="-DENABLE_TESTING=OFF"
+if [[ "${bb_full_target}" == "${MACHTYPE_FULL}" ]]; then
+    CFG_TESTING="-DENABLE_TESTING=ON"
+fi
 
-# install header-only libmcfp command-line argument parser
+
+###########
+# libmcfp: install header-only libmcfp command-line argument parser
+###########
 cd libmcfp
 mkdir build && cd build
 cmake .. \
@@ -60,18 +79,62 @@ install_license LICENSE-libmcfp
 cd ../..
 
 
-# now compile dssp proper
+###########
+# libcifpp: install libcifpp static library
+###########
+cd libcifpp
+
+# mingw doesn't have ioctl
+# upstream PR: https://github.com/PDB-REDO/libcifpp/pull/45
+atomic_patch -p1 ../patches/libcifpp-mingw-no-ioctl.patch
+
+# fixes for clang and libc++ on macos for missing C++20 features
+# (missing std::set::contains and operator<=>)
+# Upstream issue: https://github.com/PDB-REDO/libcifpp/issues/39
+if [[ "${target}" == *-apple-darwin* ]]; then
+    atomic_patch -p1 ../patches/libcifpp-clang-libc++-fixes.patch
+fi
+
+mkdir build && cd build
+# Set cmake cache vars because test programs can't be run during
+# cross-compilation
+#
+# -DSTD_REGEX_RUNNING=0               # std::regex works
+# -D_CXX_ATOMIC_BUILTIN_EXITCODE=0    # std::atomic works
+# -D_CXX_ATOMIC_BUILTIN_EXITCODE__TRYRUN_OUTPUT=0
+#
+cmake .. \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_FOR_CCP4=OFF \
+    -DCIFPP_DOWNLOAD_CCD=OFF \
+    -DCIFPP_INSTALL_UPDATE_SCRIPT=OFF \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DSTD_REGEX_RUNNING=OFF \
+    -D_CXX_ATOMIC_BUILTIN_EXITCODE=0 \
+    -D_CXX_ATOMIC_BUILTIN_EXITCODE__TRYRUN_OUTPUT=0 \
+    ${CFG_TESTING}
+
+make -j${nproc}
+if [[ "${bb_full_target}" == "${MACHTYPE_FULL}" ]]; then
+    # run the tests on the build host platform
+    make test
+fi
+make install
+cp ../LICENSE LICENSE-libcifpp
+install_license LICENSE-libcifpp
+cd ../..
+
+
+###########
+# dssp: now compile dssp proper
+###########
 cd dssp
 
 # fix libcifpp linkage
 # Ref: https://github.com/PDB-REDO/dssp/commit/49306a57098f1eaebb700eed29025e6f472a799a
-atomic_patch -p1 ../patches/cmake-fix-libcifpp-linkage.patch
-
-CFG_TESTING="-DENABLE_TESTING=OFF"
-if [[ "${bb_full_target}" == "${MACHTYPE_FULL}" ]]; then
-    # build the tests if we are building for the build host platform
-    CFG_TESTING="-DENABLE_TESTING=ON"
-fi
+atomic_patch -p1 ../patches/dssp-cmake-fix-libcifpp-linkage.patch
 
 mkdir build && cd build
 cmake .. \
@@ -84,19 +147,11 @@ cmake .. \
     ${CFG_TESTING}
 
 make -j${nproc}
-
 if [[ "${bb_full_target}" == "${MACHTYPE_FULL}" ]]; then
     # run the tests on the build host platform
     make test
 fi
-
 make install
-
-# build and install shared library libdssp
-"${CXX}" -shared -o "${libdir}/libdssp.${dlext}" \
-    -Wl,$(flagon --whole-archive) "${libdir}/libdssp.a" -Wl,$(flagon --no-whole-archive) \
-    -lcifpp
-
 install_license ../LICENSE
 """
 
@@ -105,16 +160,16 @@ platforms = expand_cxxstring_abis(platforms)
 
 products = [
     ExecutableProduct("mkdssp", :mkdssp),
-    LibraryProduct("libdssp", :libdssp),
-    FileProduct("lib/libdssp.a", :libdssp_a),
+    # LibraryProduct("libdssp", :libdssp),
+    # FileProduct("lib/libdssp.a", :libdssp_a),
 ]
 
 dependencies = [
+    BuildDependency("Eigen_jll"),
     Dependency("Zlib_jll"),
-    Dependency("libcifpp_jll"),
-    # needed for make test, which we only run on the `default_host_platform`
+    # boost is needed for `make test`, which we only run on `default_host_platform`
     HostBuildDependency("boost_jll"; platforms=[default_host_platform]),
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version = v"10")
+               julia_compat="1.6", preferred_gcc_version = v"9")
