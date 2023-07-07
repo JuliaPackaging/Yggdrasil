d74b0cacd0520198232fa83be549bf63945663f2
Michael Persico <michael.a.persico@gmail.com>
2023-07-07 13:58:51 -0400

    [argtable] reattempt libargtable2 build (#7021)
    [libargtable](https://sourceforge.net/projects/argtable/) v2.13.
    
    Possible merge conflict was found when pulling both versions to the General registry (see [#86914](https://github.com/JuliaRegistries/General/pull/86914)),
    most likely due to trying to register two versions of the same package at once (my bad :c).

--- A/argtable/argtable@2.13/build_tarballs.jl
+++ A/argtable/argtable@2.13/build_tarballs.jl
@@ -24,7 +24,7 @@ make -j${nproc}
 make install
 """
 
-# These are the platforms we will build for by default, unless further
+# These are the platforms that we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms(; exclude = Sys.iswindows)
 platforms = expand_cxxstring_abis(platforms)
