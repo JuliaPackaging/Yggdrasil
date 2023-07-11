ebe20029a7573528d70228807bc47e93c231f60e
Leandro Martínez <31046348+lmiq@users.noreply.github.com>
2023-07-11 19:00:21 -0300

    Dependency(PackageSpec(name="CompilerSupportLibraries_jll

--- M/MDLovoFit/build_tarballs.jl
+++ M/MDLovoFit/build_tarballs.jl
@@ -32,6 +32,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
