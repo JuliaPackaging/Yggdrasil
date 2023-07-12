649fcda2d30563c077fd57fe3980c7c9d1e5b33e
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-07-12 11:00:34 +0000

    Tweak platforms

--- O/oxigraph_server/build_tarballs.jl
+++ O/oxigraph_server/build_tarballs.jl
@@ -29,8 +29,7 @@ install_license LICENSE.txt
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-platforms = expand_cxxstring_abis(platforms)
+platforms = supported_platforms(; exclude=p -> (Sys.isbsd(p) || libc(p) == "musl" || nbits(p) != 64 || arch(p) == "powerpc64le"))
 
 # Rust toolchain for i686 Windows is unusable
 filter!(p -> !Sys.iswindows(p) || arch(p) != "i686", platforms)
