ae620a0dbb9e26e0eb65a690dfd7d78e732b1f84
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-07-15 12:46:13 +0200

    Update build_tarballs.jl

--- L/LibPQ/build_tarballs.jl
+++ L/LibPQ/build_tarballs.jl
@@ -29,6 +29,7 @@ export ZIC=$WORKSPACE/srcdir/zic
 # Fix "cannot find openssl" under Windows
 if [[ ${target} == x86_64-w64-mingw32 ]]; then
     export OPENSSL_ROOT_DIR=${prefix}/lib64/
+    export OPENSSL_LIBS="-L${libdir} -lssl -lcrypto"
 fi
 
 cd $WORKSPACE/srcdir/postgresql-*/
@@ -83,4 +84,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"5")
