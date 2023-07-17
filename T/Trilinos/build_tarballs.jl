a77f8922e8a8e3f658a95f73fa5d8d1f62c5804c
Viral B. Shah <ViralBShah@users.noreply.github.com>
2023-07-17 18:21:43 -0400

    Update build_tarballs.jl

--- T/Trilinos/build_tarballs.jl
+++ T/Trilinos/build_tarballs.jl
@@ -4,7 +4,7 @@ using BinaryBuilder, Pkg
 
 name = "Trilinos"
 # Not a real version - this is 12.12.1, but needed a bump to change julia compat
-version = v"12.13.2"
+version = v"12.13.3"
 
 # Collection of sources required to complete build
 sources = [
@@ -78,7 +78,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="SuiteSparse_jll", uuid="bea87d4a-7f5b-5778-9afe-8cc45184846c"))
-    Dependency(PackageSpec(name="libblastrampoline_jll")
+    Dependency(PackageSpec(name="libblastrampoline_jll"))
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
 
