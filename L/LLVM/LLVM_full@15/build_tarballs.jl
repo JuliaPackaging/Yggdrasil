8ff9bbc51311f39c8edec5b3dffe53c26170155d
Tim Besard <tim.besard@gmail.com>
2023-07-21 20:36:35 +0200

    Bump LLVM to include yet another patch. (#7080)

--- L/LLVM/LLVM_full@15/build_tarballs.jl
+++ L/LLVM/LLVM_full@15/build_tarballs.jl
@@ -4,3 +4,5 @@ include("../common.jl")
 
 build_tarballs(ARGS, configure_build(ARGS, version; experimental_platforms=true)...;
                preferred_gcc_version=v"7", preferred_llvm_version=v"12", julia_compat="1.10")
+
+# rebuild
--- L/LLVM/common.jl
+++ L/LLVM/common.jl
@@ -15,7 +15,7 @@ const llvm_tags = Dict(
     v"12.0.1" => "980d2f60a8524c5546397db9e8bbb7d6ea56c1b7", # julia-12.0.1-4
     v"13.0.1" => "8a2ae8c8064a0544814c6fac7dd0c4a9aa29a7e6", # julia-13.0.1-3
     v"14.0.6" => "5c82f5309b10fab0adf6a94969e0dddffdb3dbce", # julia-14.0.6-3
-    v"15.0.7" => "e42aaf192bf5a8a2b55ac520cad9a70f5aab3348", # julia-15.0.7-6
+    v"15.0.7" => "e010657e399d312a9440732a8a67125ecd0e2298", # julia-15.0.7-7
 )
 
 const buildscript = raw"""
