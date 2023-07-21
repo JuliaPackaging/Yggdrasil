164a3858abed27b3bb72a66d330539603ccd3df0
Dheepak Krishnamurthy <me@kdheepak.com>
2023-07-21 17:06:55 -0400

    New Recipe: libcrossterm v0.2.0 (#7081)
    * feat: Add libcrossterm ✨
    
    * feat: Update to v0.2.0 ✨
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ L/libcrossterm/build_tarballs.jl
@@ -0,0 +1,44 @@
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "libcrossterm"
+version = v"0.2.0"
+
+sources = [
+  GitSource(
+    "https://github.com/kdheepak/libcrossterm.git",
+    "65b89e0f18fc30e9d4a812327507dd9b468c1597"
+  )
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir/libcrossterm/
+
+cargo build --release -j${nproc} --target=${rust_target}
+
+if [[ "${target}" == *-w64-mingw32* ]]; then
+    # Windows generates .dlls without the lib prefix
+    install -Dvm 0755 "target/${rust_target}/release/crossterm.${dlext}" "${libdir}/libcrossterm.${dlext}"
+else
+    install -Dvm 0755 "target/${rust_target}/release/libcrossterm.${dlext}" "${libdir}/libcrossterm.${dlext}"
+fi
+
+install -Dvm 0755 "include/crossterm.h" "${includedir}/crossterm.h"
+
+install_license LICENSE
+"""
+
+platforms = supported_platforms()
+# `cdylib` apparently doesn't support musl
+filter!(p -> libc(p) != "musl", platforms)
+# Our Rust toolchain for i686 Windows is unusable
+filter!(p -> !Sys.iswindows(p) || arch(p) != "i686", platforms)
+
+products = [
+  LibraryProduct("libcrossterm", :libcrossterm),
+  FileProduct("include/crossterm.h", :crossterm_h),
+]
+
+dependencies = Dependency[]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers=[:c, :rust], julia_compat="1.6")
