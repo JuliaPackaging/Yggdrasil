e55ddabbe658990955739a859f86724e450b2e54
Alex Arslan <ararslan@comcast.net>
2023-07-28 10:14:20 -0700

    [p7zip] Touch to trigger a rebuild

--- P/p7zip/build_tarballs.jl
+++ P/p7zip/build_tarballs.jl
@@ -82,7 +82,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency("LLVMCompilerRT_jll",platforms=[Platform("x86_64", "linux"; sanitize="memory")]),
+    BuildDependency("LLVMCompilerRT_jll"; platforms=[Platform("x86_64", "linux"; sanitize="memory")]),
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
