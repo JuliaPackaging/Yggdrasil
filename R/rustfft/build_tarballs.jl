093dae8b439c8cda82ab98deb7011cf8a3ec1991
Thomas van Doornmalen <thomas.vandoornmalen@gmail.com>
2023-07-28 20:35:20 +0200

    [rustfft] version 0.2.0-bis: disable Julia 1.10 support to avoid issues on Windows

--- R/rustfft/build_tarballs.jl
+++ R/rustfft/build_tarballs.jl
@@ -9,7 +9,7 @@ delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "rustfft"
 version = v"0.2.0"
-julia_versions = [v"1.6.3", v"1.7", v"1.8", v"1.9", v"1.10"]
+julia_versions = [v"1.6.3", v"1.7", v"1.8", v"1.9"]
 
 # Collection of sources required to complete build
 sources = [
