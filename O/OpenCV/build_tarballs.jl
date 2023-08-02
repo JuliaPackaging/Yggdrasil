7f12e1afce93a0030a29cefe844a5d48c9ecc6d2
Jesper Stemann Andersen <jesper@sait.dk>
2023-08-02 10:52:00 +0200

    [OpenCV] Rebuild JLL for 1.10 compatibility
    Cf. https://github.com/JuliaImages/OpenCV.jl/issues/23

--- O/OpenCV/build_tarballs.jl
+++ O/OpenCV/build_tarballs.jl
@@ -130,3 +130,5 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"9")
+
+# Build trigger
