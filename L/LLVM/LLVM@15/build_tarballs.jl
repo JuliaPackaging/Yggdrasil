daf97ac883e87690443d78011c098b918ea10524
Alex Arslan <ararslan@comcast.net>
2023-08-07 15:53:02 -0700

    Rebuild LLVM and Clang 15 (#7185)
    * [LLVM@15] Rebuild using LLVM_full and libLLVM 15.0.7+8
    
    * [Clang@15] Rebuild using LLVM_full and libLLVM 15.0.7+8

--- L/LLVM/LLVM@15/build_tarballs.jl
+++ L/LLVM/LLVM@15/build_tarballs.jl
@@ -1,6 +1,6 @@
 name = "LLVM"
-llvm_full_version = v"15.0.7+7"
-libllvm_version = v"15.0.7+7"
+llvm_full_version = v"15.0.7+8"
+libllvm_version = v"15.0.7+8"
 
 using BinaryBuilder, Pkg
 using Base.BinaryPlatforms
