8c5e3f0c674024a5e6e008fe8915113a1584e2ec
Tim Besard <tim.besard@gmail.com>
2023-08-18 10:10:31 +0200

    CUDA: Re-build CUDNN JLL using redist sources. (#7217)

--- C/CUDA/common.jl
+++ C/CUDA/common.jl
@@ -1,10 +1,22 @@
 using JSON3, Downloads
 using BinaryBuilder
 
-function get_sources(product::String="cuda", components::Vector{String}=["cuda_cudart"];
-                     version::Union{VersionNumber,String}="12.2.1", platform::Platform=Platform("x86_64", "linux"))
+function get_sources(product::String, components::Vector{String};
+                     version::Union{VersionNumber,String}, platform::Platform,
+                     variant::Union{Nothing,String}=nothing)
     root = "https://developer.download.nvidia.com/compute/$product/redist"
 
+    url = "$root/redistrib_$(version).json"
+    json = sprint(io->Downloads.download(url, io))
+    parse_sources(json, product, components; version, platform)
+end
+# XXX: split, for now, so that we can use this with manual JSON
+function parse_sources(json::String, product::String, components::Vector{String};
+                       version::Union{VersionNumber,String}, platform::Platform,
+                       variant::Union{Nothing,String}=nothing)
+    root = "https://developer.download.nvidia.com/compute/$product/redist"
+
+    redist = JSON3.read(json)
     architecture = if Sys.islinux(platform)
         libc(platform) == "glibc" || error("Only glibc is supported on Linux")
         if arch(platform) == "x86_64"
@@ -22,23 +34,27 @@ function get_sources(product::String="cuda", components::Vector{String}=["cuda_c
         "windows-x86_64"
     end
 
-    url = "$root/redistrib_$(version).json"
-    json = sprint(io->Downloads.download(url, io))
-    redistrib = JSON3.read(json)
-
     sources = []
     for component in components
-        if !haskey(redistrib, component)
+        if !haskey(redist, component)
             error("No such component $component for $product $version")
         end
-        data = redistrib[component]
+        data = redist[component]
 
         if !haskey(data, architecture)
             error("No $architecture binaries for $component in $product $version")
         end
-        arch_data = data[architecture]
+        data = data[architecture]
+
+        if variant !== nothing
+            if !haskey(data, variant)
+                error("No $variant variant for $architecture $component in $product $version")
+            end
+            data = data[variant]
+        end
+
         push!(sources, ArchiveSource(
-            "$root/$(arch_data["relative_path"])", arch_data["sha256"]
+            "$root/$(data["relative_path"])", data["sha256"]
         ))
     end
     sources
