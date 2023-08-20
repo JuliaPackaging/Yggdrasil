c3b3e1a84871fe4eae1e06c1a614e3a52ffead08
Jamie Mair <JamieMair@users.noreply.github.com>
2023-08-20 10:02:40 +0100

    Updated mujoco build script based on PR comments

--- M/MuJoCo/build_tarballs.jl
+++ M/MuJoCo/build_tarballs.jl
@@ -8,31 +8,36 @@ version = v"2.3.7"
 # Collection of sources required to build mujoco
 sources = [
     GitSource("https://github.com/deepmind/mujoco", "c9246e1f5006379d599e0bcddf159a8616d31441"),
-    DirectorySource(joinpath(@__DIR__, "patches"))
+    DirectorySource("bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/mujoco
 if [[ "${target}" == *-mingw* ]]; then
-    atomic_patch -p1 ../mingw.patch
+    atomic_patch -p1 ../patches/mingw.patch
 elif [[ "${target}" == *-apple-darwin* ]]; then
-    atomic_patch -p1 ../macos.patch
+    atomic_patch -p1 ../patches/macos.patch
 else
-    atomic_patch -p1 ../other.patch
+    atomic_patch -p1 ../patches/other.patch
 fi
 CPPFLAGS="-I${prefix}/include"
 CXXFLAGS="-I${prefix}/include"
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release .
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DMUJOCO_BUILD_EXAMPLES=OFF -DMUJOCO_BUILD_SIMULATE=OFF -DMUJOCO_BUILD_TESTS=OFF -DMUJOCO_TEST_PYTHON_UTIL=OFF .
 cmake --build .
 cmake --install .
 """
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-function platform_filer(p)
-    return contains(p.tags["arch"], "64") && (!haskey(p.tags, "libc") || p.tags["libc"] != "musl") && (!Sys.isbsd(p) || Sys.isapple(p))
-end
-platforms = [p for p in supported_platforms() if platform_filer(p)]
+platforms = [
+    Platform("x86_64", "linux", libc="glibc"),
+    Platform("aarch64", "linux", libc="glibc"),
+    Platform("powerpc64le", "linux", libc="glibc"),
+    Platform("x86_64", "macos"),
+    Platform("aarch64", "macos"),
+    Platform("x86_64", "windows")
+]
+
 
 products = [
     LibraryProduct("libmujoco", :libmujoco)
