c353b95a7735ca9e7dc830d3e55a2b6b105ce412
Jamie Mair <JamieMair@users.noreply.github.com>
2023-08-21 11:20:29 +0100

    Cleaned up windows and macos patches for mujoco

--- M/MuJoCo/build_tarballs.jl
+++ M/MuJoCo/build_tarballs.jl
@@ -29,10 +29,15 @@ cmake --install .
 """
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
+# platforms = [
+#     Platform("x86_64", "linux", libc="glibc"),
+#     Platform("aarch64", "linux", libc="glibc"),
+#     Platform("powerpc64le", "linux", libc="glibc"),
+#     Platform("x86_64", "macos"),
+#     Platform("aarch64", "macos"),
+#     Platform("x86_64", "windows")
+# ]
 platforms = [
-    Platform("x86_64", "linux", libc="glibc"),
-    Platform("aarch64", "linux", libc="glibc"),
-    Platform("powerpc64le", "linux", libc="glibc"),
     Platform("x86_64", "macos"),
     Platform("aarch64", "macos"),
     Platform("x86_64", "windows")
