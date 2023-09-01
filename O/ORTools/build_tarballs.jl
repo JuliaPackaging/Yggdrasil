2df6358e15f73ba2ae99059dacc117b51b2d12b8
Thibaut Cuvelier <dourouc05@gmail.com>
2023-09-02 01:36:29 +0200

    Add builder for OR-Tools

---
+++ O/ORTools/build_tarballs.jl
@@ -0,0 +1,51 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "ORTools"
+version = v"9.7"
+
+# Collection of sources required to build this package
+sources = [
+    GitSource("https://github.com/google/or-tools.git",
+              "35d56a4b07db8ea135b83762289fbc0e7d229221"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/or-tools*
+mkdir build
+cmake -S. -Bbuild \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DBUILD_DEPS:BOOL=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_EXAMPLES:BOOL=OFF \
+    -DBUILD_SAMPLES:BOOL=OFF \
+    -DUSE_SCIP:BOOL=OFF \
+    -DUSE_HIGHS:BOOL=OFF
+cmake --build build
+cmake --build build --target install
+"""
+
+# TODO: generate with ProtoBuf.jl
+
+platforms = [
+    Platform("x86_64", "linux"),
+    Platform("aarch64", "linux"),
+    Platform("x86_64", "macos"),
+    Platform("aarch64", "macos"),
+    Platform("x86_64", "freebsd"),
+    Platform("x86_64", "windows")
+]
+platforms = expand_cxxstring_abis(platforms)
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libortools", :libortools),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"10", julia_compat="1.6")
