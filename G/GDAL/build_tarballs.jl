6f9b1ae42460b91b57f7eb64f3b0c9609587032e
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-09-15 10:50:16 +0200

    Bump NetCDF

--- G/GDAL/build_tarballs.jl
+++ G/GDAL/build_tarballs.jl
@@ -143,7 +143,7 @@ dependencies = [
     Dependency("Libtiff_jll"; compat="4.3"),
     Dependency("libgeotiff_jll"; compat="100.700.100"),
     Dependency("LibCURL_jll"; compat="7.73,8"),
-    Dependency("NetCDF_jll"; compat="400.902.5", platforms=hdf5_platforms),
+    Dependency("NetCDF_jll"; compat="400.902.208", platforms=hdf5_platforms),
     Dependency("HDF5_jll"; compat="~1.14", platforms=hdf5_platforms),
     Dependency("Arrow_jll"; compat="10"),
 ]
