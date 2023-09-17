7094ea9ac0a93bd911c8b67ff3bf4738933605cf
Mosè Giordano <giordano@users.noreply.github.com>
2023-09-17 20:28:05 +0100

    [MozillaCACerts] Update to v2023-08-22 (#7351)

--- M/MozillaCACerts/build_tarballs.jl
+++ M/MozillaCACerts/build_tarballs.jl
@@ -4,13 +4,13 @@ using BinaryBuilder
 
 name = "MozillaCACerts"
 # Info and new versions here: https://curl.haxx.se/docs/caextract.html
-cacert_version = "2023-01-10"
+cacert_version = "2023-08-22"
 version = VersionNumber(replace(cacert_version, '-'=>'.'))
 
 # Collection of sources required to build MozillaCACerts
 sources = [
     FileSource("https://curl.haxx.se/ca/cacert-$cacert_version.pem", 
-               "fb1ecd641d0a02c01bc9036d513cb658bbda62a75e246bedbc01764560a639f0",
+               "23c2469e2a568362a62eecf1b49ed90a15621e6fa30e29947ded3436422de9b9",
                filename="cacert.pem"),
 ]
 
