8469029241e47ea802881819c6eb41c1b5eb4976
Julian Samaroo <jpsamaroo@jpsamaroo.me>
2023-09-19 12:29:21 -0500

    [HIP] Add 5.4.4 (properly) (#7363)

--- H/HIP/HIP@5.4.4/build_tarballs.jl
+++ H/HIP/HIP@5.4.4/build_tarballs.jl
@@ -4,5 +4,5 @@ using BinaryBuilder
 include("../common.jl")
 
 build_tarballs(
-    ARGS, configure_build(v"5.2.3")...;
+    ARGS, configure_build(v"5.4.4")...;
     preferred_gcc_version=v"7", preferred_llvm_version=v"9", julia_compat="1.9")
