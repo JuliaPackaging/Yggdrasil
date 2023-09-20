64cd2790905cc3f0062a606673bfbae818be92b5
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-09-20 11:46:26 +0200

    Syntax update

--- O/OpenJpeg/build_tarballs.jl
+++ O/OpenJpeg/build_tarballs.jl
@@ -25,7 +25,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
