63affd8b4239b71b05f98ceeb03044fe47c527c1
Mosè Giordano <giordano@users.noreply.github.com>
2023-09-21 23:31:27 +0000

    [aws_c_cal] [s2n_tls] Use GCC 7 to link static lib

--- A/aws_c_cal/build_tarballs.jl
+++ A/aws_c_cal/build_tarballs.jl
@@ -43,4 +43,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"5")
+               julia_compat="1.6", preferred_gcc_version=v"7")
