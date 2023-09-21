7c109e8815ed2eeeb41d2a60bd4e478dafd50094
Keno Fischer <keno@juliacomputing.com>
2023-09-21 13:49:57 +0000

    Fix Teko build

--- T/Trilinos/build_tarballs.jl
+++ T/Trilinos/build_tarballs.jl
@@ -26,7 +26,8 @@ fi
 rm /usr/bin/cmake
 
 cd Trilinos
-atomic_patch -p1 $WORKSPACE/srcdir/patches/*.patch
+atomic_patch -p1 $WORKSPACE/srcdir/patches/kokkostpl.patch
+atomic_patch -p1 $WORKSPACE/srcdir/patches/tekoepetraguard.patch
 
 mkdir trilbuild
 cd trilbuild
@@ -36,7 +37,7 @@ FLAGS='-O3 -fPIC'
 CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}"
 # Trilinos package enables
 CMAKE_FLAGS="${CMAKE_FLAGS}
-    -DTrilinos_ENABLE_NOX=ON
+    -DTrilinos_ENABLE_NOX=ON -DNOX_ENABLE_ABSTRACT_IMPLEMENTATION_EPETRA=ON
     -DNOX_ENABLE_LOCA=ON
     -DTrilinos_ENABLE_EpetraExt=ON -DEpetraExt_BUILD_BTF=ON -DEpetraExt_BUILD_EXPERIMENTAL=ON -DEpetraExt_BUILD_GRAPH_REORDERINGS=ON
     -DTrilinos_ENABLE_TrilinosCouplings=ON
@@ -44,16 +45,18 @@ CMAKE_FLAGS="${CMAKE_FLAGS}
     -DTrilinos_ENABLE_Isorropia=ON
     -DTrilinos_ENABLE_AztecOO=ON
     -DTrilinos_ENABLE_Belos=ON
-    -DTrilinos_ENABLE_Teuchos=ON -DTeuchos_ENABLE_COMPLEX=ON
+    -DTrilinos_ENABLE_Teuchos=ON
     -DTrilinos_ENABLE_Amesos=ON -DAmesos_ENABLE_KLU=ON
     -DTrilinos_ENABLE_Sacado=ON
-    -DTrilinos_ENABLE_Kokkos=OFF
+    -DTrilinos_ENABLE_Tpetra=ON
+    -DTrilinos_ENABLE_Teko=ON
     "
 # Global Trilinos FLAGS
 CMAKE_FLAGS="${CMAKE_FLAGS}
     -DBUILD_SHARED_LIBS=ON
     -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES=OFF
     -DTrilinos_ENABLE_CXX11=ON -DCMAKE_BUILD_TYPE=Release
+    -DTrilinos_ENABLE_OpenMP=ON -DTrilinos_ENABLE_COMPLEX_DOUBLE=ON
     "
 # SuiteSparse config
 CMAKE_FLAGS="${CMAKE_FLAGS} -DTPL_ENABLE_AMD=ON -DAMD_LIBRARY_DIRS=\"/${libdir}\"
@@ -73,12 +76,13 @@ CMAKE_FLAGS="${CMAKE_FLAGS}
     -DHAVE_GCC_ABI_DEMANGLE_EXITCODE=0
     -DHAVE_TEUCHOS_BLASFLOAT_EXITCODE=0 -DHAVE_TEUCHOS_BLASFLOAT_DOUBLE_RETURN_EXITCODE=0
     -DCXX_COMPLEX_BLAS_WORKS_EXITCODE=0 -DLAPACK_SLAPY2_WORKS_EXITCODE=0
-    -DHAVE_TEUCHOS_LAPACKLARND_EXITCODE=0
+    -DHAVE_TEUCHOS_LAPACKLARND_EXITCODE=0 -DKK_BLAS_RESULT_AS_POINTER_ARG_EXITCODE=0
     -DHAVE_GCC_ABI_DEMANGLE_EXITCODE__TRYRUN_OUTPUT=''
     -DHAVE_TEUCHOS_BLASFLOAT_EXITCODE__TRYRUN_OUTPUT=''
     -DLAPACK_SLAPY2_WORKS_EXITCODE__TRYRUN_OUTPUT=''
     -DCXX_COMPLEX_BLAS_WORKS_EXITCODE__TRYRUN_OUTPUT=''
     -DHAVE_TEUCHOS_LAPACKLARND_EXITCODE__TRYRUN_OUTPUT=''
+    -DKK_BLAS_RESULT_AS_POINTER_ARG_EXITCODE__TRYRUN_OUTPUT=''
     "
 
 cmake -G "Unix Makefiles" ${CMAKE_FLAGS} -DCMAKE_CXX_FLAGS="${FLAGS}" -DCMAKE_C_FLAGS="${FLAGS}" -DCMAKE_Fortran_FLAGS="${FLAGS}" $SRCDIR
@@ -139,4 +143,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"7.1.0", julia_compat="1.10")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9", julia_compat="1.10")
