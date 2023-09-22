62254e4ea2c1598e4db23116af921cd358320323
Mosè Giordano <mose@gnu.org>
2023-09-22 23:28:04 +0100

    Revert changes to aws_c_http

--- A/aws_c_http/build_tarballs.jl
+++ A/aws_c_http/build_tarballs.jl
@@ -24,7 +24,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DBUILD_TESTING=OFF \
     -DCMAKE_BUILD_TYPE=Release \
-    -DBUILD_SHARED_LIBS=ON \
+    -DBUILD_SHARED_LIBS=OFF \
     ..
 cmake --build . -j${nproc} --target install
 """
@@ -36,15 +36,19 @@ filter!(p -> !(Sys.iswindows(p) && arch(p) == "i686"), platforms)
 
 # The products that we will ensure are always built
 products = [
-    FileProduct("libaws-c-http", :libaws_c_http),
+    FileProduct("lib/libaws-c-http.a", :libaws_c_http),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("aws_c_compression_jll"; compat="0.2.17"),
-    Dependency("aws_c_io_jll"; compat="0.13.32"),
+    BuildDependency("aws_c_cal_jll"),
+    BuildDependency("aws_c_compression_jll"),
+    BuildDependency("aws_c_common_jll"),
+    BuildDependency("aws_c_io_jll"),
+    BuildDependency("aws_lc_jll"),
+    BuildDependency("s2n_tls_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"5")
+               julia_compat="1.6", preferred_gcc_version = v"5")
