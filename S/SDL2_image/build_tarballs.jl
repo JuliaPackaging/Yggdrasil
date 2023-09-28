e037b9ba179b6989c96c4ee89cd37e8981851693
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-09-29 01:16:57 +0200

    [SDL2_image] Bump v2.3.2 (#7446)
    * [SDL2_image] Bump v2.3.2
    
    * fix folder name

--- S/SDL2_image/build_tarballs.jl
+++ S/SDL2_image/build_tarballs.jl
@@ -3,20 +3,26 @@
 using BinaryBuilder, Pkg
 
 name = "SDL2_image"
-version = v"2.6.2"
+version = v"2.6.3"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/libsdl-org/SDL_image.git",
-              "a861543ab78f5d5e18e6af43964ce8df60cb30b3"),
+              "d3c6d5963dbe438bcae0e2b6f3d7cfea23d02829"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/SDL*/
-mkdir build
-cd build
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DSDL2_IMAGE_WEBP=ON -DSDL2_IMAGE_TIF=ON SDL2IMAGE_BACKEND_IMAGEIO=OFF ..
+cd $WORKSPACE/srcdir/SDL_image
+
+mkdir build && cd build
+
+cmake -DCMAKE_INSTALL_PREFIX=$prefix \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DSDL2_IMAGE_WEBP=ON \
+      -DSDL2_IMAGE_TIF=ON \
+      -DSDL2IMAGE_BACKEND_IMAGEIO=OFF ..
 make -j${nproc}
 make install
 install_license ../LICENSE.txt
@@ -34,11 +40,11 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="SDL2_jll", uuid="ab825dc5-c88e-5901-9575-1e5e20358fcf"))
-    Dependency(PackageSpec(name="libwebp_jll", uuid="c5f90fcd-3b7e-5836-afba-fc50a0988cb2"))
+    Dependency("libwebp_jll"; compat="1.2.4")
     Dependency(PackageSpec(name="JpegTurbo_jll", uuid="aacddb02-875f-59d6-b918-886e6ef4fbf8"))
     Dependency(PackageSpec(name="libpng_jll", uuid="b53b4c65-9356-5827-b1ea-8c7a1a84506f"))
     Dependency(PackageSpec(name="Libtiff_jll", uuid="89763e89-9b03-5906-acba-b20f662cd828"))
 ]
 
 # Build the tarballs.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
