49af86d7e3efbae21f1eb10d3cc60009dd907770
Tim Besard <tim.besard@gmail.com>
2023-09-29 17:43:19 +0200

    Bump LLVMExtra. (#7452)

--- L/LLVMExtra/build_tarballs.jl
+++ L/LLVMExtra/build_tarballs.jl
@@ -7,14 +7,14 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
 
 name = "LLVMExtra"
 repo = "https://github.com/maleadt/LLVM.jl.git"
-version = v"0.0.25"
+version = v"0.0.26"
 
 llvm_versions = [v"13.0.1", v"14.0.6", v"15.0.7"]
 
 
 # Collection of sources required to build LLVMExtra
 sources = [
-    GitSource(repo, "51094f89c6b84bca1198c75d22c16b032ee98f2d"),
+    GitSource(repo, "b4aef3da952ce70997114c036de44525d8e96626"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
     "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f"),
 ]
