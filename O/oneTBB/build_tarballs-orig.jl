9092d8d10170a39cffe0000377cf12a2ad847521
Erik Schnetter <schnetter@gmail.com>
2023-10-01 16:44:08 -0400

    oneTBB: Update to 2021.9.0

--- O/oneTBB/build_tarballs-orig.jl
+++ O/oneTBB/build_tarballs-orig.jl
@@ -1,21 +1,20 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "oneTBB"
-version = v"2021.9.0"
+version = v"2021.8.0"
 
-# Collection of sources required to build hwloc
+# Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/oneapi-src/oneTBB", "a00cc3b8b5fb4d8115e9de56bf713157073ed68c"),
+    GitSource("https://github.com/oneapi-src/oneTBB.git",
+    "c9497714821c3d443ee44c732609eb6850195ffb"),
     DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd ${WORKSPACE}/srcdir/oneTBB
-
-rm -f /usr/share/cmake/Modules/Compiler/._*.cmake
+cd $WORKSPACE/srcdir/oneTBB*
 
 if [[ ${target} == *mingw* ]]; then
     atomic_patch -p1 "${WORKSPACE}/srcdir/patches/mingw.patch"
@@ -25,21 +24,18 @@ if [[ ${target} == *mingw* ]]; then
     export CXXFLAGS="-D_WIN32_WINNT=0x0600"
 fi
 
-cmake -B build -S . \
-    -DBUILD_SHARED_LIBS=ON \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_FIND_ROOT_PATH=${prefix} \
-    -DCMAKE_INSTALL_PREFIX=${prefix} \
+mkdir build && cd build/
+
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DTBB_TEST=OFF \
     -DTBB_EXAMPLES=OFF \
-    -DTBB_STRICT=OFF \
-    -DTBB_TEST=OFF
-cmake --build build --parallel ${nproc}
-cmake --build build --parallel ${nproc} --target install
+    ..
+make -j${nproc}
+make install
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
@@ -54,5 +50,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"9")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"9")
