2ca69fe3bc739f21e4cef1dc8d1e61e8dfc45b1e
Erik Schnetter <schnetter@gmail.com>
2023-10-03 11:32:10 -0400

    obstack: Build for armv6l

--- O/obstack/build_tarballs.jl
+++ O/obstack/build_tarballs.jl
@@ -33,5 +33,5 @@ products = [
 dependencies = Dependency[
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
+# Build the tarballs, and possibly a `build.jl` as well
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
