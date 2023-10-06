0aeb4a7732032bef5034afe7e334a25beb165d77
Max Horn <max@quendi.de>
2023-10-06 07:06:46 +0200

    [GAP_pkg_juliainterface] rebuild for Julia 1.10.0-beta3 (#7494)

--- G/GAP_pkg/GAP_pkg_juliainterface/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_juliainterface/build_tarballs.jl
@@ -54,7 +54,7 @@ for p in platforms
     end
 end
 
-push!(dependencies, BuildDependency(PackageSpec(;name="libjulia_jll", version=v"1.10.5")))
+push!(dependencies, BuildDependency(PackageSpec(;name="libjulia_jll", version=v"1.10.6")))
 
 # The products that we will ensure are always built
 products = [
