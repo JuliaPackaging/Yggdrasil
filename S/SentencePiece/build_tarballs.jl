e81a5cc230ed0133f5641809867ec6c6faba7ca5
Mosè Giordano <mose@gnu.org>
2023-10-10 00:40:21 +0100

    [SentencePiece] Build shared libraries on Windows

--- S/SentencePiece/build_tarballs.jl
+++ S/SentencePiece/build_tarballs.jl
@@ -7,16 +7,24 @@ version = v"0.1.99"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/google/sentencepiece", "3863f7648e5d8edb571ac592f3ac4f5f0695275a")
+    GitSource("https://github.com/google/sentencepiece",
+              "3863f7648e5d8edb571ac592f3ac4f5f0695275a"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/sentencepiece*
 
+# There is no reason for not building the shared libraries on Windows.
+atomic_patch -p1 ../patches/build_shared_library_windows.patch
+
 mkdir build && cd build/
 
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=17 ..
+cmake -DCMAKE_INSTALL_PREFIX=$prefix \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=Release \
+      ..
 make -j${nproc}
 make install
 """
@@ -27,15 +35,13 @@ platforms = supported_platforms()
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
-lib_products = [
+products = [
     LibraryProduct("libsentencepiece_train", :libsentencepiece_train),
-    LibraryProduct("libsentencepiece", :libsentencepiece)
-]
-exe_products = [
     ExecutableProduct("spm_normalize", :spm_normalize),
     ExecutableProduct("spm_encode", :spm_encode),
     ExecutableProduct("spm_export_vocab", :spm_export_vocab),
     ExecutableProduct("spm_train", :spm_train),
+    LibraryProduct("libsentencepiece", :libsentencepiece),
     ExecutableProduct("spm_decode", :spm_decode)
 ]
 
@@ -47,5 +53,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, filter(Sys.iswindows, platforms), exe_products, dependencies; julia_compat = "1.6",preferred_gcc_version=v"8")
-build_tarballs(ARGS, name, version, sources, script, filter(!Sys.iswindows, platforms), [lib_products; exe_products], dependencies; julia_compat = "1.6",preferred_gcc_version=v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat = "1.6", preferred_gcc_version=v"8")
