52cf19ccded289182a7ed7d0171eb1fe3d700571
Hugh Sorby <h.sorby@auckland.ac.nz>
2023-10-10 12:10:46 +1300

    Update build_tarballs.jl for libcellml 0.5.0

--- L/libcellml/build_tarballs.jl
+++ L/libcellml/build_tarballs.jl
@@ -6,24 +6,11 @@ version = v"0.4.0"
 sources = [
     GitSource(
         "https://github.com/cellml/libcellml",
-        "697e0b2d07e5e68d147a42c63305d7f72ce26c78"),
-    ArchiveSource(
-        "https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
-        "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f"),
+        "9482382"),
 ]
 
 # https://libcellml.org/documentation/guides/latest/installation/build_from_source
 script = raw"""
-# This requires macOS 10.14
-if [[ "${target}" == x86_64-apple-darwin* ]]; then
-    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
-    rm -rf /opt/${target}/${target}/sys-root/System
-    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
-    cp -ra System "/opt/${target}/${target}/sys-root/."
-    export MACOSX_DEPLOYMENT_TARGET=10.14
-    popd
-fi
-
 cd libcellml
 mkdir build && cd build
 cmake -DINSTALL_PREFIX=${prefix} \
