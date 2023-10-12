121dd4f14d239299087b4b5f51854db80c90e56d
Matt Bhagat-Conway <matt@indicatrix.org>
2023-10-12 09:53:45 -0400

    (libspatialindex) fix build system to not use MSVC flags

--- L/LibSpatialIndex/build_tarballs.jl
+++ L/LibSpatialIndex/build_tarballs.jl
@@ -6,7 +6,8 @@ version = v"1.9.3"
 # Collection of sources required to build LibSpatialIndex
 sources = [
     ArchiveSource("https://github.com/libspatialindex/libspatialindex/releases/download/1.9.3/spatialindex-src-1.9.3.tar.bz2",
-        "4a529431cfa80443ab4dcd45a4b25aebbabe1c0ce2fa1665039c80e999dcc50a")
+        "4a529431cfa80443ab4dcd45a4b25aebbabe1c0ce2fa1665039c80e999dcc50a"),
+    DirectorySource("./patches")
     ]
 
 # Bash recipe for building across all platforms
@@ -15,12 +16,11 @@ cd $WORKSPACE/srcdir
 
 cd spatialindex-src-*
 
-# patch < ${WORKSPACE}/srcdir/makefile.patch
-# rm Makefile.am.orig
-
-# if [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]; then
-#   patch < ${WORKSPACE}/srcdir/header-check.patch
-# fi
+# apply https://github.com/libspatialindex/libspatialindex/pull/185 for mingw builds
+# to succeed
+if [ $target = "x86_64-w64-mingw32" ] || [ $target = "i686-w64-mingw32" ]; then
+   patch src/CMakeLists.txt < ${WORKSPACE}/srcdir/0001-fix-mingw-build-185.patch
+fi
 
 
 mkdir build
