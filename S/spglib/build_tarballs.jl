8c464644fe8425676cf9306251b3649230230cd1
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2023-10-12 14:29:20 -0400

    Update `spglib` to v2.1.0 (#7310)
    * Update `spglib` to v2.1.0
    
    * Update spglib/build_tarballs.jl
    
    * Update spglib/build_tarballs.jl
    
    * Update spglib/build_tarballs.jl
    
    * Update spglib/build_tarballs.jl
    
    * Update spglib/build_tarballs.jl
    
    * Update spglib/build_tarballs.jl
    
    * Update spglib/build_tarballs.jl
    
    * Update spglib/build_tarballs.jl
    
    Remove all dependencies

--- S/spglib/build_tarballs.jl
+++ S/spglib/build_tarballs.jl
@@ -1,29 +1,28 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "spglib"
-version = v"2.0.2"
+version = v"2.1.0"
 
-# Collection of sources required to build spglib
+# Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/spglib/spglib.git", "42cf4f6bea3419f720308713deb5ddb81350bcd9"),
+    GitSource("https://github.com/spglib/spglib.git", "59bea8a7df30c8f2202ed0ee1033be0d98d9ed5e")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/spglib
-if [[ ${target} == *-mingw32 ]]; then
-    sed -i -e 's/LIBRARY/RUNTIME/' CMakeLists.txt
+args=""
+if [[ ! -z "${CMAKE_TARGET_TOOLCHAIN}" ]]; then
+  args="${args} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}"
 fi
-mkdir _build
-cd _build
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
-      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-      -DCMAKE_BUILD_TYPE=Release \
-      ..
-make -j${nproc}
-make install VERBOSE=1
+cmake -B ./build \
+      -DCMAKE_INSTALL_PREFIX=${prefix} \
+      -DSPGLIB_WITH_TESTS=OFF \
+      ${args}
+cmake --build ./build -j${nproc}
+cmake --install ./build
 """
 
 # These are the platforms we will build for by default, unless further
@@ -36,12 +35,8 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
-    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
-    # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
-    Dependency("CompilerSupportLibraries_jll"; platforms=filter(!Sys.isbsd, platforms)),
-    Dependency("LLVMOpenMP_jll"; platforms=filter(Sys.isbsd, platforms)),
+dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies, julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"12.1.0")
