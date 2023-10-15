6bdf5d0c891603ce812f0565b41d765b1156a43e
Keno Fischer <keno@juliacomputing.com>
2023-10-14 21:33:24 -0400

    Kokkos: Also enable serial execution space (#7401)
    For downstream users that may not want to require OpenMP

--- K/Kokkos/build_tarballs.jl
+++ K/Kokkos/build_tarballs.jl
@@ -27,7 +27,8 @@ cmake .. \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_CXX_STANDARD=17 \
-    -DKokkos_ENABLE_OPENMP=ON
+    -DKokkos_ENABLE_OPENMP=ON \
+    -DKokkos_ENABLE_SERIAL=ON
 
 make -j${nproc}
 make install
