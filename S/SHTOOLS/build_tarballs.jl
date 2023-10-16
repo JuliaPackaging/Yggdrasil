f10b2fd0dc71a81b8cfaab5ad2c6fe613c216613
Erik Schnetter <schnetter@gmail.com>
2023-10-16 17:12:13 -0400

    [SHTOOLS] New version 4.10.4 (#7540)

--- S/SHTOOLS/build_tarballs.jl
+++ S/SHTOOLS/build_tarballs.jl
@@ -2,15 +2,19 @@ using BinaryBuilder
 
 # Collection of sources required to build SHTOOLS
 name = "SHTOOLS"
-version = v"4.10.2"
+version = v"4.10.4"
 sources = [
     ArchiveSource("https://github.com/SHTOOLS/SHTOOLS/releases/download/v$(version)/SHTOOLS-$(version).tar.gz",
-                  "0caece67d65ddde19a79ec79bc6244f447f6fa878e5b2dc3f635cae2a3d1ee8c"),
+                  "f31ab09e960d85ad23d046fa427b692ffb80915a2c96773725bb83ad90bdec20"),
+    DirectorySource("bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/SHTOOLS-*
+
+# Add missing underscore to call to `dggglm`
+atomic_patch -p1 ../patches/dggglm.patch
 
 # Build and install static libraries
 make fortran -j${nproc} F95FLAGS="-fPIC -O3 -std=gnu"
