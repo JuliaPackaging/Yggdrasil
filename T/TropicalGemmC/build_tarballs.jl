340e57fb97a4e322f14c9097301244d1105b2567
xzgao <xz.gao@connect.ust.hk>
2023-10-18 04:04:58 +0000

    using CUDA.required_dependencies()

--- T/TropicalGemmC/build_tarballs.jl
+++ T/TropicalGemmC/build_tarballs.jl
@@ -1,6 +1,7 @@
 using BinaryBuilder
 using BinaryBuilderBase
 using Pkg
+using Base.BinaryPlatforms: arch, os
 
 name = "TropicalGemmC"
 version = v"0.1.1"
@@ -40,19 +41,6 @@ done
 install_license /usr/share/licenses/MIT
 """
 
-augment_platform_block = CUDA.augment
-
-versions_to_build = [
-    v"11.4"
-    v"11.5"
-    v"11.6"
-    v"11.7"
-    v"11.8"
-    v"12.0"
-    v"12.1"
-    v"12.2"
-]
-
 # The products that we will ensure are always built
 products = [
     LibraryProduct(["lib_PlusMul_FP32"], :lib_PlusMul_FP32),
@@ -70,39 +58,21 @@ products = [
     LibraryProduct(["lib_TropicalMinPlus_FP64"], :lib_TropicalMinPlus_FP64),
 ]
 
-platforms = [
-    Platform("x86_64", "linux"),
-]
+platforms = CUDA.supported_platforms()
+filter!(p -> arch(p) == "x86_64", platforms)
 
-for cuda_version in versions_to_build, platform in platforms
+for platform in platforms
+    should_build_platform(triplet(platform)) || continue
 
-    cuda_platform = (os(platform) == "linux") && (arch(platform) in ["x86_64"])
-    if !isnothing(cuda_version) && !cuda_platform
-        continue
-    end
-    
-    if cuda_platform
-        augmented_platform = Platform(arch(platform), os(platform);
-            cxxstring_abi = cxxstring_abi(platform),
-            cuda=isnothing(cuda_version) ? "none" : CUDA.platform(cuda_version)
-        )
-    else
-        augmented_platform = deepcopy(platform)
-    end
-    should_build_platform(triplet(augmented_platform)) || continue
+    cuda_deps = CUDA.required_dependencies(platform, static_sdk=true)
 
     dependencies = AbstractDependency[
-        Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, [augmented_platform])),
+        Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+        cuda_deps...
     ]
 
-    if !isnothing(cuda_version)
-        push!(dependencies, BuildDependency(PackageSpec(name="CUDA_SDK_jll", version=CUDA.full_version(cuda_version))))
-        push!(dependencies, BuildDependency(PackageSpec(name="CUDA_SDK_static_jll", version=CUDA.full_version(cuda_version))))
-        push!(dependencies, RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll")))
-    end
-
-    build_tarballs(ARGS, name, version, sources,  script, [augmented_platform], products, dependencies;
-                    preferred_gcc_version=v"8",
-                    julia_compat="1.6",
-                    augment_platform_block)
+    build_tarballs(ARGS, name, version, sources,  script, [platform], products, dependencies;
+    preferred_gcc_version=v"8",
+    julia_compat="1.6",
+    augment_platform_block=CUDA.augment)
 end
