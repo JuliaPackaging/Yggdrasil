a5d53b450e465a1d263bf7f949a5995aab5e7994
Jakob Peters <jakobj.peters@gmail.com>
2023-10-19 18:10:09 -0700

    Exclude `powerpc64le` from the list of platforms

--- T/Typst/build_tarballs.jl
+++ T/Typst/build_tarballs.jl
@@ -20,7 +20,9 @@ install_license LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter(p -> !(Sys.iswindows(p) && arch(p) == "i686"), supported_platforms())
+platforms = filter(supported_platforms()) do p
+    !(Sys.iswindows(p) && arch(p) == "i686" || Sys.islinux(p) && arch(p) == "powerpc64le")
+end
 
 # The products that we will ensure are always built
 products = [
