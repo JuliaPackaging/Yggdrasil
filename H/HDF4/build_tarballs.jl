1967defa10dde06effe4a3308d8acfc8534dd752
Fabian Gans <fabiancoding@gmail.com>
2023-10-20 16:19:46 +0200

    Update H/HDF4/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- H/HDF4/build_tarballs.jl
+++ H/HDF4/build_tarballs.jl
@@ -29,6 +29,10 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+# /opt/x86_64-unknown-freebsd13.2/x86_64-unknown-freebsd13.2/sys-root/usr/include/rpc/xdr.h:125:2: error: unknown type name 'u_int'
+filter!(!Sys.isfreebsd, platforms)
+# /workspace/srcdir/hdf-4.2.16-2/mfhdf/xdr/types.h:68:18: error: conflicting types for ‘u_quad_t’
+filter!(p -> !(libc(p) == "musl" && nbits(p) == 64), platforms)
 
 # The products that we will ensure are always built
 products = [
