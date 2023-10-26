47207a8b72a445559776cb33927456c862999c5f
Philip Tellis <ptellis@akamai.com>
2023-10-26 15:39:56 -0400

    Update CyrusSASL to use OpenSSL 3.0 (#7575)
    * Update CyrusSASL to use OpenSSL 3.0
    
    * Different versions for jll and upstream
    
    * relax openssl dep to 3.0.8
    
    * [CyrusSASL] Set `LDFLAGS` to help Windows linker
    
    * Update build_tarballs.jl
    
    * [CyrusSASL] Fix value of `LDFLAGS` for x86_64 Windows
    
    * [CyrusSASL] Clearer comment
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- C/CyrusSASL/build_tarballs.jl
+++ C/CyrusSASL/build_tarballs.jl
@@ -3,11 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "CyrusSASL"
-version = v"2.1.28"
+version = v"2.1.29"          # We need to bump the version of the jll to compile with OpenSSL 3.0 instead of 1.1.10
+library_version = v"2.1.28"  # But keep the CyrusSASL version as-is
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-$(version)/cyrus-sasl-$(version).tar.gz", "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"),
+    ArchiveSource("https://github.com/cyrusimap/cyrus-sasl/releases/download/cyrus-sasl-$(library_version)/cyrus-sasl-$(library_version).tar.gz", "7ccfc6abd01ed67c1a0924b353e526f1b766b21f42d4562ee635a8ebfc5bb38c"),
     DirectorySource("./bundled"),
 ]
 
@@ -41,6 +42,11 @@ if [[ "${target}" == *-mingw* ]]; then
     # ...and don't regenerate a wrong one with `make.  This patch needs to be
     # applied _after_ autoreconf, which seems to somehow revert the changes :-(
     atomic_patch -p1 ../patches/31-do-not-make-mdf5global_h.patch
+
+    if [[ "${target}" == x86_64-* ]]; then
+        # On x86_64 mingw32 the import libraries of OpenSSL are in `lib64/`.
+        export LDFLAGS="-L${prefix}/lib64"
+    fi
 fi
 ./configure --prefix=${prefix} \
     --build=${MACHTYPE} --host=${target} \
@@ -81,7 +87,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"); compat="1.1.10")
+    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"); compat="3.0.8")
     Dependency(PackageSpec(name="SQLite_jll", uuid="76ed43ae-9a5d-5a62-8c75-30186b810ce8"))
     Dependency(PackageSpec(name="Gdbm_jll", uuid="54ca2031-c8dd-5cab-9ed4-295edde1660f"))
 ]
