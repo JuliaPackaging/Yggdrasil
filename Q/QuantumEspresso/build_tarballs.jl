3f1ca93de10e9947d3731b20a240903e07945ec6
Qi Zhang <25192197+singularitti@users.noreply.github.com>
2023-10-26 17:01:10 -0400

    Update build_tarballs.jl for QuantumEspresso

--- Q/QuantumEspresso/build_tarballs.jl
+++ Q/QuantumEspresso/build_tarballs.jl
@@ -108,4 +108,4 @@ append!(dependencies, platform_dependencies)
 
 # Build the tarballs, and possibly a `build.jl` as well
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-    augment_platform_block, julia_compat="1.6", preferred_gcc_version=v"12.1.0")
+    augment_platform_block, julia_compat="1.6", preferred_gcc_version=v"10.2.0")
