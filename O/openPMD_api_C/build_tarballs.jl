608db0d3717fabd41725793477884e90a3402dce
Erik Schnetter <schnetter@gmail.com>
2023-10-26 09:50:10 -0400

    openPMD_api_C: Update upstream version

--- O/openPMD_api_C/build_tarballs.jl
+++ O/openPMD_api_C/build_tarballs.jl
@@ -11,7 +11,7 @@ version = v"0.15.2" # This is really the branch `eschnett/c-bindings` after vers
 # Collection of sources required to complete build
 sources = [
     # We use a feature branch instead of a released version because the C bindings are not released yet
-    GitSource("https://github.com/eschnett/openPMD-api.git", "137488c1914aa1e4b22c8b638935a41355f07739"),
+    GitSource("https://github.com/eschnett/openPMD-api.git", "58f99165451e39bab63f5d98177276d1e15e787c"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
                   "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
 ]
