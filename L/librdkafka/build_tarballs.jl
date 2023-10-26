d7234885a32243e38ebc03a0524d053ea82ef7ae
Philip Tellis <ptellis@akamai.com>
2023-10-26 15:43:22 -0400

    require the lowest version of CyrusSASL that uses OpenSSL 3

--- L/librdkafka/build_tarballs.jl
+++ L/librdkafka/build_tarballs.jl
@@ -49,7 +49,7 @@ dependencies = [
     Dependency(PackageSpec(name="Zstd_jll", uuid="3161d3a3-bdf6-5164-811a-617609db77b4")),
     Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a")),
     Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"); compat="3.0.8"),
-    Dependency(PackageSpec(name="CyrusSASL_jll", uuid="6422fedd-75a7-50c2-a7c3-a11dad25a896")),
+    Dependency(PackageSpec(name="CyrusSASL_jll", uuid="6422fedd-75a7-50c2-a7c3-a11dad25a896"); compat="2.1.29"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
