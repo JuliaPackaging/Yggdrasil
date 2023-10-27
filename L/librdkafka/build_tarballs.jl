f20d83bdcdd32dfdbb8bf619fd945c7c7d0b6afb
Philip Tellis <ptellis@akamai.com>
2023-10-27 17:57:44 -0400

    Update to librdkafka 2.3.0 and openssl 3.0 (#7574)
    * Update to librdkafka 2.3.0 and openssl 3.0
    
    This MR bumps the librdkafka version to 2.3.0 and also bumps the openssl dependency to 3.0.
    
    * relax to openssl 3.0.8
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * require the lowest version of CyrusSASL that uses OpenSSL 3
    
    * Set more compat entries
    
    * relax some lib versions
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * compat is more relaxed than build_version
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * Create 01-add-stdlib.patch
    
    * Add stdlib before building
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * Delete L/librdkafka/bundles/01-add-stdlib.patch
    
    * Exclude FreeBSD
    
    * Update build_tarballs.jl
    
    * Exclude only freebsd
    
    Co-authored-by: Mark Kittisopikul <mkitti@users.noreply.github.com>
    
    * Update build_tarballs.jl
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    Co-authored-by: Mark Kittisopikul <mkitti@users.noreply.github.com>

--- L/librdkafka/build_tarballs.jl
+++ L/librdkafka/build_tarballs.jl
@@ -3,14 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "librdkafka"
-version = v"2.0.3" # note: bumped from 2.0.2 for SASL integration
+version = v"2.3.0"
 
 # Collection of sources required to complete build
 sources = [
-    # git rev-list -n 1 v2.0.2
-    GitSource("https://github.com/confluentinc/librdkafka.git",
-        "292d2a66b9921b783f08147807992e603c7af059",
-    ),
+    # git rev-list -n 1 v2.3.0
+    GitSource("https://github.com/confluentinc/librdkafka.git", "95a542c87c61d2c45b445f91c73dd5442eb04f3c",),
 ]
 
 # Bash recipe for building across all platforms
@@ -35,7 +33,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms())
+platforms = expand_cxxstring_abis(supported_platforms(exclude = Sys.isfreebsd))
 
 
 # The products that we will ensure are always built
@@ -45,11 +43,11 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Lz4_jll", uuid="5ced341a-0733-55b8-9ab6-a4889d929147")),
-    Dependency(PackageSpec(name="Zstd_jll", uuid="3161d3a3-bdf6-5164-811a-617609db77b4")),
-    Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a")),
-    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"); compat="1.1.10"),
-    Dependency(PackageSpec(name="CyrusSASL_jll", uuid="6422fedd-75a7-50c2-a7c3-a11dad25a896")),
+    Dependency(PackageSpec(name="Lz4_jll", uuid="5ced341a-0733-55b8-9ab6-a4889d929147"); compat="1.9.4"),
+    Dependency(PackageSpec(name="Zstd_jll", uuid="3161d3a3-bdf6-5164-811a-617609db77b4"); compat="1.4.2"),
+    Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a"); compat="1.2.11"),
+    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"); compat="3.0.8"),
+    Dependency(PackageSpec(name="CyrusSASL_jll", uuid="6422fedd-75a7-50c2-a7c3-a11dad25a896"); compat="2.1.29"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
