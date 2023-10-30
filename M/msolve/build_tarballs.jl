3a9929561a0d85effab13e5958485280762d127f
ederc <ederc@mathematik.uni-kl.de>
2023-10-30 12:09:12 +0100

    msolve v0.6.0 (#7595)

--- M/msolve/build_tarballs.jl
+++ M/msolve/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "msolve"
-version = v"0.4.9"
+version = v"0.6.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/algebraic-solving/msolve.git", "c547cb83ac3e876a2d72a2cc8ee191b543287d85")
+    GitSource("https://github.com/algebraic-solving/msolve.git", "e041d4aa140a8a4c89ee7b917cefee0aafd1697c")
 ]
 
 # Bash recipe for building across all platforms
