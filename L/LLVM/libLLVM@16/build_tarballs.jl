96f6331d31108d3be2b0563268baddf453397e54
Gabriel Baraldi <baraldigabriel@gmail.com>
2023-11-01 13:17:12 -0300

    bump!

--- L/LLVM/libLLVM@16/build_tarballs.jl
+++ L/LLVM/libLLVM@16/build_tarballs.jl
@@ -20,7 +20,7 @@ augment_platform_block = """
 # determine exactly which tarballs we should build
 builds = []
 for llvm_assertions in (false, true)
-    # Dependencies that must be installed before this package can be built
+    # Dependencies that must be installed before this package can be built!
     llvm_name, uuid = llvm_assertions ? ("LLVM_full_assert_jll", "6ec703ca-3f29-566b-9bb1-b5c9e844abaf") : ("LLVM_full_jll", "a3ccf953-465e-511d-b87f-60a6490c289d")
     dependencies = [
         BuildDependency(PackageSpec(;name=llvm_name, uuid, version))
