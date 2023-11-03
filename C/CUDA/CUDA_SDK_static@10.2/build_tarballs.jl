7358369b8cda4aa051608afb2426aeae51650190
Jesper Stemann Andersen <jsa@ihp.dk>
2023-11-03 12:53:25 +0100

    Re-factored CUDA_SDK@10.2 into CUDA_SDK@10.2 and CUDA_SDK_static@10.2

--- C/CUDA/CUDA_SDK_static@10.2/build_tarballs.jl
+++ C/CUDA/CUDA_SDK_static@10.2/build_tarballs.jl
@@ -6,7 +6,7 @@ const YGGDRASIL_DIR = "../../.."
 include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
-name = "CUDA_SDK"
+name = "CUDA_SDK_static"
 version = CUDA.full_version(v"10.2")
 
 platforms = [Platform("x86_64", "linux"),
@@ -14,4 +14,4 @@ platforms = [Platform("x86_64", "linux"),
              Platform("x86_64", "macos"),
              Platform("x86_64", "windows")]
 
-build_sdk(name, version, platforms; static=false)
+build_sdk(name, version, platforms; static=true)
