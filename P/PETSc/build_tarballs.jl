92e0ce880c0758ef727e6c44257102eeb7d16189
Alexis Montoison <alexis.montoison@polymtl.ca>
2023-11-04 21:45:26 -0500

    [PETSc] Update the compat entry for MUMPS_jll

--- P/PETSc/build_tarballs.jl
+++ P/PETSc/build_tarballs.jl
@@ -6,7 +6,7 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 name = "PETSc"
 version = v"3.18.6"
 petsc_version = v"3.18.6"
-MUMPS_COMPAT_VERSION = "5.5.1"
+MUMPS_COMPAT_VERSION = "5.5.1 - 5.6.1"
 SUITESPARSE_COMPAT_VERSION = "5.10.1"
 SUPERLUDIST_COMPAT_VERSION = "8.1.2"   
 MPItrampoline_compat_version="5.2.1"    
