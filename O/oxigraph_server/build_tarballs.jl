7dd124c36b9c49390dca6061b581db65e07109a8
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2023-11-06 02:46:07 -0600

    [oxigraph_sever] Bump v0.3.20 (#7631)

--- O/oxigraph_server/build_tarballs.jl
+++ O/oxigraph_server/build_tarballs.jl
@@ -3,17 +3,17 @@
 using BinaryBuilder, Pkg
 
 name = "oxigraph_server"
-version = v"0.3.19"
+version = v"0.3.20"
 
 url_prefix = "https://github.com/oxigraph/oxigraph/releases/download/v$version/oxigraph_server_v$version"
 
 # Collection of sources required to complete build
 sources = [
-    FileSource("$(url_prefix)_aarch64_apple", "a53629afdbd5e34e0024abcc42ed559bd2d4d8456ad1851a5098b1a7bc10c66b"; filename = "oxigraph_server-aarch64-apple-darwin20"),
-    FileSource("$(url_prefix)_x86_64_apple", "6d3cdb5c06746bb09cff3ba892e00ecfb5faaf6ac98c0e35bd06d04ee305a1b3"; filename = "oxigraph_server-x86_64-apple-darwin14"),
-    FileSource("$(url_prefix)_aarch64_linux_gnu", "3ebc68013762b8377a2caadc2846aafcaa15830bec0e2368c091a5d99551552b"; filename = "oxigraph_server-aarch64-linux-gnu"),
-    FileSource("$(url_prefix)_x86_64_linux_gnu", "e93d4a8640f8356acddc6bd8a039b7decbca3852d4c310c396552a8d50ebe05a"; filename = "oxigraph_server-x86_64-linux-gnu"),
-    FileSource("$(url_prefix)_x86_64_windows_msvc.exe", "ea65b7e283593937e7d932f077e35f865ce408e9a19ee798dd26a745e0cd600f"; filename = "oxigraph_server-x86_64-w64-mingw32"),
+    FileSource("$(url_prefix)_aarch64_apple", "cc9e47f43cf546eb16a0300972de37ddc699565340c2ee5fa82c96bb57c8b19e"; filename = "oxigraph_server-aarch64-apple-darwin20"),
+    FileSource("$(url_prefix)_x86_64_apple", "fa3d2dae62063d68c00dd2b0787ac5514ac2d914c9fd31fa9fd3f8246363dc8a"; filename = "oxigraph_server-x86_64-apple-darwin14"),
+    FileSource("$(url_prefix)_aarch64_linux_gnu", "67021a33618af8a4418186cd3ee9d2be1e3de7b2a26f7e0a1a4e4bbd4f4cfefc"; filename = "oxigraph_server-aarch64-linux-gnu"),
+    FileSource("$(url_prefix)_x86_64_linux_gnu", "bd1905a38014fde4b2983c9a6c13992c64797b1c4cd7e272c1217876d077f2a9"; filename = "oxigraph_server-x86_64-linux-gnu"),
+    FileSource("$(url_prefix)_x86_64_windows_msvc.exe", "d8e836562f3b0d762a6410b06c736f0ccb2c9a0da95a2921487b7b63f62f8c9d"; filename = "oxigraph_server-x86_64-w64-mingw32"),
     FileSource("https://raw.githubusercontent.com/oxigraph/oxigraph/v$version/LICENSE-MIT", "1f4f6736adc52ebfda18bb84947e0ef492bd86a408c0e83872efb75ed5e02838"; filename = "LICENSE.txt")
 ]
 
