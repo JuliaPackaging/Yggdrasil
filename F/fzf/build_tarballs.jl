ec53a86a604b54977372d4aa9c242dac6a502b58
Jerry Ling <proton@jling.dev>
2023-11-07 05:36:33 -0500

    Update build_tarballs.jl

--- F/fzf/build_tarballs.jl
+++ F/fzf/build_tarballs.jl
@@ -31,4 +31,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers = [:gcc, :go], julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; compilers = [:c, :go], julia_compat="1.6")
