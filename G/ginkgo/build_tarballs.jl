4fdc32a745e80f98b6f758739be1eeacaa840f3a
You Wu <82619264+youwuyou@users.noreply.github.com>
2023-11-08 10:25:58 -0500

    Update G/ginkgo/build_tarballs.jl
    resolving gcc 4/5 version boundary warning
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- G/ginkgo/build_tarballs.jl
+++ G/ginkgo/build_tarballs.jl
@@ -48,7 +48,8 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
+dependencies = [
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
