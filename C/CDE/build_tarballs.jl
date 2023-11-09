5c1cc33ff0762b92cf0f178695f6595fbd2f31bb
joegilkes <j.gilkes@warwick.ac.uk>
2023-11-09 16:17:14 +0000

    Fixed Windows-specific build issues

--- C/CDE/build_tarballs.jl
+++ C/CDE/build_tarballs.jl
@@ -5,7 +5,12 @@ version = v"1.0.0"
 sources = [GitSource("https://github.com/HabershonLab/cde", "049cd0d2da73a0c5bb340ebdb9a78e35348fe49a")]
 
 script = raw"""
-export LDFLAGS="-L${libdir}"
+if [[ ${target} == *w64* ]]; then
+    exe_out="${bindir}/cde.exe"
+else
+    exe_out="${bindir}/cde"
+fi
+
 cd cde
 gfortran -O3 -c src/constants.f90 -o src/constants.o -J src
 gfortran -O3 -c src/globaldata.f90 -o src/globaldata.o -J src
@@ -17,14 +22,15 @@ gfortran -O3 -c src/rpath.f90 -o src/rpath.o -J src
 gfortran -O3 -c src/pathopt.f90 -o src/pathopt.o -J src
 gfortran -O3 -c src/pathfinder.f90 -o src/pathfinder.o -J src
 gfortran -O3 -c src/main.f90 -o src/main.o -J src
-mkdir $bindir
-gfortran -O3 -o "${bindir}/cde_exe" src/constants.o src/globaldata.o src/functions.o src/io.o src/structure.o src/pes.o src/rpath.o src/pathopt.o src/pathfinder.o src/main.o -L"${libdir}" -lopenblas
+
+mkdir -p $bindir
+gfortran -O3 -o $exe_out src/constants.o src/globaldata.o src/functions.o src/io.o src/structure.o src/pes.o src/rpath.o src/pathopt.o src/pathfinder.o src/main.o -L"${libdir}" -lopenblas
 """
 
 platforms = supported_platforms()
 platforms = expand_gfortran_versions(platforms)
 
-products = [ExecutableProduct("cde_exe", :cde, "bin")]
+products = [ExecutableProduct("cde", :run_cde)]
 
 dependencies = [
     Dependency("OpenBLAS32_jll"),
