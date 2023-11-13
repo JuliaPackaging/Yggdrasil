e4eb3ee297e3919764f98bd4bda29419973de781
joegilkes <j.gilkes@warwick.ac.uk>
2023-11-13 16:22:53 +0000

    [CDE] Moved to compiling with Makefile

--- C/CDE/build_tarballs.jl
+++ C/CDE/build_tarballs.jl
@@ -5,22 +5,9 @@ version = v"1.0.0"
 sources = [GitSource("https://github.com/HabershonLab/cde", "049cd0d2da73a0c5bb340ebdb9a78e35348fe49a")]
 
 script = raw"""
-exe_out="${bindir}/cde${exeext}"
-
 cd cde
-gfortran -O3 -c src/constants.f90 -o src/constants.o -J src
-gfortran -O3 -c src/globaldata.f90 -o src/globaldata.o -J src
-gfortran -O3 -c src/functions.f90 -o src/functions.o -J src
-gfortran -O3 -c src/io.f90 -o src/io.o -J src
-gfortran -O3 -c src/structure.f90 -o src/structure.o -J src
-gfortran -O3 -c src/pes.f90 -o src/pes.o -J src
-gfortran -O3 -c src/rpath.f90 -o src/rpath.o -J src
-gfortran -O3 -c src/pathopt.f90 -o src/pathopt.o -J src
-gfortran -O3 -c src/pathfinder.f90 -o src/pathfinder.o -J src
-gfortran -O3 -c src/main.f90 -o src/main.o -J src
-
 mkdir -p $bindir
-gfortran -O3 -o $exe_out src/constants.o src/globaldata.o src/functions.o src/io.o src/structure.o src/pes.o src/rpath.o src/pathopt.o src/pathfinder.o src/main.o -L"${libdir}" -lopenblas
+make -j1 EXE="${bindir}/cde${exeext}" COMPILE_STATIC=false 
 """
 
 platforms = supported_platforms()
