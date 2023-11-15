ac9f80255a49d33e5f5a7224b4dd5b48607798d5
Marco Matthies <71844+marcom@users.noreply.github.com>
2023-11-16 00:51:58 +0100

    [Cares] Update to v1.22.0 (#7686)

--- C/Cares/build_tarballs.jl
+++ C/Cares/build_tarballs.jl
@@ -1,18 +1,15 @@
-# Note that this script can accept some limited command-line arguments, run
-# `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
 name = "Cares"
-version = v"1.19.1"
+version = v"1.22.0"
 
 # url = "https://c-ares.org/"
 # description = "C library for asynchronous DNS requests (including name resolves)"
 
 sources = [
     ArchiveSource("https://c-ares.org/download/c-ares-$(version).tar.gz",
-                  "321700399b72ed0e037d0074c629e7741f6b2ec2dda92956abe3e9671d3e268e"),
+                  "ad2e205088083317147c9f9eab5f24b82c3d50927c381a7c963deeb1182dbc21"),
 ]
-
 
 script = raw"""
 cd $WORKSPACE/srcdir/c-ares-*/
@@ -32,7 +29,6 @@ install_license ../LICENSE.md
 platforms = supported_platforms()
 
 products = [
-    ExecutableProduct("acountry", :acountry),
     ExecutableProduct("adig", :adig),
     ExecutableProduct("ahost", :ahost),
     LibraryProduct("libcares", :libcares),
