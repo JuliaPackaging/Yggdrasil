8cd273b482fd74938d3a4ac709ade32e9da2fbe6
Erik Schnetter <schnetter@gmail.com>
2023-11-17 09:47:56 -0500

    ADIOS2: New version 2.9.2 (#7687)
    * ADIOS2: New version 2.9.2
    
    * ADIOS2: Explicitly require shared libraries
    
    * ADIOS2: Use Blosc2 shared libraries
    
    * ADIOS2: Declare arm8_rt_call_link
    
    * ADIOS2: Include <arpa/inet.h>
    
    * ADIOS2: Correct patch file name
    
    * ADIOS2: Remove unnecessary file
    
    * ADIOS2: Mention https://github.com/ornladios/ADIOS2/issues/3924

--- A/ADIOS2/build_tarballs.jl
+++ A/ADIOS2/build_tarballs.jl
@@ -6,12 +6,12 @@ const YGGDRASIL_DIR = "../.."
 include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 
 name = "ADIOS2"
-adios2_version = v"2.9.1"
-version = v"2.9.2"
+adios2_version = v"2.9.2"
+version = v"2.9.3"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/ornladios/ADIOS2.git", "d143154f3bbebfdd7ac3c46b4e31fdfda7e8e79c"),
+    GitSource("https://github.com/ornladios/ADIOS2.git", "83cb06ae66700a8d5cc37f3c1af5c9e917a1236f"),
     DirectorySource("./bundled"),
 ]
 
@@ -21,17 +21,27 @@ cd $WORKSPACE/srcdir
 cd ADIOS2
 # Don't define clock_gettime on macOS
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/clock_gettime.patch
-
-mkdir build
-cd build
+# Declare `arm8_rt_call_link`. See <https://github.com/ornladios/ADIOS2/issues/3925>.
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/arm8_rt_call_link.patch
+# Declare `htons`. See <https://github.com/ornladios/ADIOS2/issues/3926>.
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/htons.patch
 
 if [[ ${target} == x86_64-linux-musl ]]; then
     # HDF5 needs libcurl, and it needs to be the BinaryBuilder libcurl, not the system libcurl.
+    # MPI needs libevent, and it needs to be the BinaryBuilder libevent, not the system libevent.
     rm /usr/lib/libcurl.*
+    rm /usr/lib/libevent*
     rm /usr/lib/libnghttp2.*
 fi
 
 archopts=()
+
+if grep -q MPICH_NAME $prefix/include/mpi.h && ls /usr/include/*/sys/queue.hh >/dev/null 2>&1; then
+    # This feature only works with MPICH
+    archopts+=(-DADIOS2_HAVE_MPI_CLIENT_SERVER_EXITCODE=0 -DADIOS2_HAVE_MPI_CLIENT_SERVER_EXITCODE__TRYRUN_OUTPUT=)
+else
+    archopts+=(-DADIOS2_HAVE_MPI_CLIENT_SERVER_EXITCODE=1 -DADIOS2_HAVE_MPI_CLIENT_SERVER_EXITCODE__TRYRUN_OUTPUT=)
+fi
 
 if grep -q MSMPI_VER ${includedir}/mpi.h; then
     # Microsoft MPI
@@ -48,21 +58,17 @@ else
     archopts+=(-DADIOS2_USE_DataMan=ON -DADIOS2_USE_HDF5=ON -DADIOS2_USE_SST=ON)
 fi
 
-if grep -q MPICH_NAME $prefix/include/mpi.h && ls /usr/include/*/sys/queue.hh >/dev/null 2>&1; then
-    # This feature only works with MPICH
-    archopts+=(-DADIOS2_HAVE_MPI_CLIENT_SERVER_EXITCODE=0 -DADIOS2_HAVE_MPI_CLIENT_SERVER_EXITCODE__TRYRUN_OUTPUT=)
-else
-    archopts+=(-DADIOS2_HAVE_MPI_CLIENT_SERVER_EXITCODE=1 -DADIOS2_HAVE_MPI_CLIENT_SERVER_EXITCODE__TRYRUN_OUTPUT=)
-fi
-
 # Fortran is not supported with Clang
-cmake \
+# We need `-DADIOS2_Blosc2_PREFER_SHARED=ON` because of <https://github.com/ornladios/ADIOS2/issues/3924>.
+cmake -B build -S . \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_FIND_ROOT_PATH=$prefix \
+    -DBUILD_SHARED_LIBS=ON \
     -DBUILD_TESTING=OFF \
     -DADIOS2_BUILD_EXAMPLES=OFF \
     -DADIOS2_HAVE_ZFP_CUDA=OFF \
     -DADIOS2_USE_Blosc2=ON \
+    -DADIOS2_Blosc2_PREFER_SHARED=ON \
     -DADIOS2_USE_CUDA=OFF \
     -DADIOS2_USE_Fortran=OFF \
     -DADIOS2_USE_MPI=ON \
@@ -72,11 +78,10 @@ cmake \
     -DMPI_HOME=$prefix \
     ${archopts[@]} \
     -DADIOS2_INSTALL_GENERATE_CONFIG=OFF \
-    -DCMAKE_INSTALL_PREFIX=$prefix \
-    ..
-cmake --build . --config RelWithDebInfo --parallel $nproc
-cmake --build . --config RelWithDebInfo --parallel $nproc --target install
-install_license ../Copyright.txt ../LICENSE
+    -DCMAKE_INSTALL_PREFIX=$prefix
+cmake --build build --config RelWithDebInfo --parallel $nproc
+cmake --build build --config RelWithDebInfo --parallel $nproc --target install
+install_license Copyright.txt LICENSE
 """
 
 augment_platform_block = """
@@ -100,7 +105,7 @@ platforms, platform_dependencies = MPI.augment_platforms(platforms; MPItrampolin
 
 # Avoid platforms where the MPI implementation isn't supported
 # OpenMPI
-platforms = filter(p -> !(p["mpi"] == "openmpi" && arch(p) == "armv6l" && libc(p) == "glibc"), platforms)
+platforms = filter(p -> !(p["mpi"] == "openmpi" && Sys.isfreebsd(p)), platforms)
 # MPItrampoline
 platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && libc(p) == "musl"), platforms)
 platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && Sys.isfreebsd(p)), platforms)
