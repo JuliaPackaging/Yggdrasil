44f39469f3e571fc6f6e126b213765fd9e009bac
Anton Smirnov <tonysmn97@gmail.com>
2023-11-22 01:10:54 +0200

    Trigger 5.6.1

--- R/ROCmDeviceLibs/ROCmDeviceLibs@5.6.1/build_tarballs.jl
+++ R/ROCmDeviceLibs/ROCmDeviceLibs@5.6.1/build_tarballs.jl
@@ -4,4 +4,5 @@ using BinaryBuilder
 include("../common.jl")
 build_tarballs(
     ARGS, configure_build(v"5.6.1")...;
-    preferred_gcc_version=v"7", preferred_llvm_version=v"9", julia_compat="1.9")
+    preferred_gcc_version=v"7", preferred_llvm_version=v"9",
+    julia_compat="1.9")
