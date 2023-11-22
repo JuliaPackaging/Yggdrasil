592ecb1993cc179339cca381eecefc725c403ee7
Zhanibek <zhanibek.om@gmail.com>
2023-11-22 14:30:41 +0900

    remove white space

--- S/SEAL/build_tarballs.jl
+++ S/SEAL/build_tarballs.jl
@@ -80,4 +80,3 @@ dependencies = Dependency[
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                preferred_gcc_version = v"7.1.0")
-
