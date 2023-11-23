009e718d4e78eb892501e3051f7c52c3f2839243
Zhanibek <zhanibek.om@gmail.com>
2023-11-23 16:37:44 +0900

    bump gcc version

--- S/SEAL/build_tarballs.jl
+++ S/SEAL/build_tarballs.jl
@@ -79,4 +79,4 @@ dependencies = Dependency[
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               preferred_gcc_version = v"7.1.0")
+               preferred_gcc_version = v"10.2.0")
