92f8063bf87f541ae1714763f4688df65eab161d
Bart Janssens <bart@bartjanssens.org>
2023-11-26 21:26:12 +0100

    [libcxxwrap-julia] Version 0.11.2

--- L/libcxxwrap_julia/build_tarballs.jl
+++ L/libcxxwrap_julia/build_tarballs.jl
@@ -8,7 +8,7 @@ uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "libcxxwrap_julia"
-version = v"0.11.1"
+version = v"0.11.2"
 
 julia_versions = [v"1.6.3", v"1.7", v"1.8", v"1.9", v"1.10", v"1.11"]
 
@@ -16,7 +16,7 @@ git_repo = "https://github.com/JuliaInterop/libcxxwrap-julia.git"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource(git_repo, "48886070845cccb676caaf9f41667c0492f8aecc"),
+    GitSource(git_repo, "f1c42d9602e32f566847c253270b4fd536db6554"),
 ]
 
 # Bash recipe for building across all platforms
@@ -49,7 +49,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency(PackageSpec(;name="libjulia_jll", version=v"1.10.6")),
+    BuildDependency(PackageSpec(;name="libjulia_jll", version=v"1.10.7")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
