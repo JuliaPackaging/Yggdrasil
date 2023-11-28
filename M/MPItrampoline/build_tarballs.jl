3f05b4559128b86fe0385e024598de98afc1be32
Erik Schnetter <schnetter@gmail.com>
2023-11-28 14:17:33 -0500

    MPItrampoline: Require GCC 5

--- M/MPItrampoline/build_tarballs.jl
+++ M/MPItrampoline/build_tarballs.jl
@@ -261,4 +261,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               augment_platform_block, julia_compat="1.6")
+               augment_platform_block, julia_compat="1.6", preferred_gcc_version=v"5")
