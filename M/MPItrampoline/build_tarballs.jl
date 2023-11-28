daa1d06483b034cb324695948fa37d9b9ccbbb33
Erik Schnetter <schnetter@gmail.com>
2023-11-28 16:30:45 -0500

    MPItrampoline: Update checkout

--- M/MPItrampoline/build_tarballs.jl
+++ M/MPItrampoline/build_tarballs.jl
@@ -14,7 +14,7 @@ mpich_version_str = "4.1.2"
 # Collection of sources required to complete build
 sources = [
     # This is really the development version before version 6.0.0
-    GitSource("https://github.com/eschnett/MPItrampoline", "8205eeb58180ebbca827419ce4a280ea27768d7b"),
+    GitSource("https://github.com/eschnett/MPItrampoline", "b425b3fa0cfaf6cced0d9543f1be875052571e6c"),
     ArchiveSource("https://www.mpich.org/static/downloads/$(mpich_version_str)/mpich-$(mpich_version_str).tar.gz",
                   "3492e98adab62b597ef0d292fb2459b6123bc80070a8aa0a30be6962075a12f0"),
 ]
