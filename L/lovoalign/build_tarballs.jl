a3567e206eba6a73511960cd454ea833b82410f0
Leandro Martínez <31046348+lmiq@users.noreply.github.com>
2023-11-29 16:15:55 -0300

    blastrampoline-5 for windows

--- L/lovoalign/build_tarballs.jl
+++ L/lovoalign/build_tarballs.jl
@@ -14,8 +14,9 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd lovoalign
+if [[ $target == *"w64-mingw32" ]]; then  sed -i 's/blastrampoline/blastrampoline-5/' CMakeLists.txt ; fi
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release .
-make
+make 
 make install
 exit
 """
