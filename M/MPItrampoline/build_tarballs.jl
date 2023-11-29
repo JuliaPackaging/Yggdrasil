e3460c23a958ef09fc764c702888f2e4eb12b644
Erik Schnetter <schnetter@gmail.com>
2023-11-28 20:25:25 -0500

    MPItrampoline: Point to mpiexec

--- M/MPItrampoline/build_tarballs.jl
+++ M/MPItrampoline/build_tarballs.jl
@@ -121,7 +121,7 @@ if ar t ${prefix}/lib/mpich/lib/libpmpi.a | grep -q setbotf.o; then
 fi
 
 # Ensure that `bin/mpiexec` exists
-if test -e ${prefix}/lib/mpich/bin/mpiexec; then
+if ! test -e ${prefix}/lib/mpich/bin/mpiexec; then
     test -e ${prefix}/lib/mpich/bin/mpiexec.hydra
     ln -s mpiexec.hydra ${prefix}/lib/mpich/bin/mpiexec
 fi
