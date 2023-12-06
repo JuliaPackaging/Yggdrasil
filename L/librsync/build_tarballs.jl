63dc5ea386c5610c02a9d1cc07e978a684a8cf43
Erik Schnetter <schnetter@gmail.com>
2023-12-05 21:22:07 -0500

    librsync: Build rdiff

--- L/librsync/build_tarballs.jl
+++ L/librsync/build_tarballs.jl
@@ -14,7 +14,7 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 cd librsync/
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_RDIFF=ON
 make -j${nproc}
 make install
 install_license COPYING
