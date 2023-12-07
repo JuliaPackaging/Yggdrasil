1a859cc82735a8b53fd5ffc946f448138a3e2513
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2023-12-07 23:13:25 +0100

    Fix type of `dependencies`
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- X/XLA/build_tarballs.jl
+++ X/XLA/build_tarballs.jl
@@ -38,7 +38,7 @@ platforms = [
 products = Product[]
 
 # Dependencies that must be installed before this package can be built
-dependencies = []
+dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"10")
