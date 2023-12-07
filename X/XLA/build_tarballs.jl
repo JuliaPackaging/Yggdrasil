e9dc56ff288f8848673a5bc9456460aa082d801b
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-12-07 21:04:07 +0100

    Fix empty products array initialization

--- X/XLA/build_tarballs.jl
+++ X/XLA/build_tarballs.jl
@@ -35,7 +35,7 @@ platforms = [
 ]
 
 # The products that we will ensure are always built
-products = []
+products = Product[]
 
 # Dependencies that must be installed before this package can be built
 dependencies = []
