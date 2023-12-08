d8afd9e3b31d1d8754e3343b0b09c4784be112de
Mosè Giordano <giordano@users.noreply.github.com>
2023-12-08 11:14:39 +0000

    !fixup Debug

--- X/XLA/build_tarballs.jl
+++ X/XLA/build_tarballs.jl
@@ -24,7 +24,7 @@ export TF_DOWNLOAD_CLANG=0
 export TF_CUDA_CLANG=0 
 yes "" | ./configure
 
-bazel build --test_output=all --spawn_strategy=local --verbose_failures //xla/... || df -HT
+bazel build --test_output=all --spawn_strategy=local --verbose_failures //xla/... || df -hT
 """
 
 # These are the platforms we will build for by default, unless further
