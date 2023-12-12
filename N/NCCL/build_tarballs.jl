97aa510ca05f32859d54f239349c3e968bf81c4f
Simon Byrne <simonbyrne@gmail.com>
2023-12-11 16:20:14 -0800

    add coreutils as build dep

--- N/NCCL/build_tarballs.jl
+++ N/NCCL/build_tarballs.jl
@@ -35,6 +35,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    BuildDependency("coreutils_jll"), # requires fmt
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
 ]
 
