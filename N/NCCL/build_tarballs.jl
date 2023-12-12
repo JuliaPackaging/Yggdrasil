07212a1d0591fdc6bfa259ffcdd490fca264c223
Simon Byrne <simonbyrne@gmail.com>
2023-12-12 11:15:02 -0800

    HostBuildDependency

--- N/NCCL/build_tarballs.jl
+++ N/NCCL/build_tarballs.jl
@@ -35,7 +35,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency("coreutils_jll"), # requires fmt
+    HostBuildDependency("coreutils_jll"), # requires fmt
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
 ]
 
