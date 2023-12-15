0b6de39109e0e189163b08906ca9e94f6b6e2bd3
Aron Zingler <aron.zingler@rwth-aachen.de>
2023-12-15 19:44:31 +0100

    Delete unused patch and cleanup build_tarballs.jl.
    Add comments explaining why certain platforms are not yet build.
    Add expansion over cxxstring and libgfortran 4 and 5.

--- M/MAiNGO/build_tarballs.jl
+++ M/MAiNGO/build_tarballs.jl
@@ -8,7 +8,6 @@ version = v"0.7.2"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://git.rwth-aachen.de/avt-svt/public/maingo.git", "252733413a29dbe5b84a4cdaf53e60e9934f372f"),
-    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
@@ -23,7 +22,7 @@ git submodule update -j 1
 if [[ "${target}" == x86_64-apple-darwin* ]]; then
     export MACOSX_DEPLOYMENT_TARGET=10.15
     export CXXFLAGS="-mmacosx-version-min=10.15"
-fi    
+fi
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DMAiNGO_build_standalone=True -DMAiNGO_build_shared_c_api=True -DMAiNGO_build_parser=True -DMAiNGO_use_cplex=False -DMAiNGO_use_melon=False  ..
 cmake --build . --config Release --parallel ${nproc} > make_out.txt
 mkdir -p ${libdir}
@@ -36,11 +35,21 @@ exit
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("x86_64", "windows"; ),
-    Platform("x86_64", "linux"; libc = "musl")
-]
 
+
+#Auditor complains about avx1.
+#Without march the Auditor detects avx2
+#but with march="avx2" avx512 is detected, so we build without march
+#We expand only to gfortan 4 and 5 (others seem not to have std::variant)
+#MacOS is in principle supported but requires newer SDK
+#see https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/1263 (wee need std::shared_ptr and std::weak_ptr with array support that needs XCode 12.0)
+#FreeBsd builds only with gcc, that platform has not yet been sufficiently tested for inclusion.
+platforms = [
+    Platform("x86_64", "linux", libgfortran_version=v"4"),
+    Platform("x86_64", "linux", libgfortran_version=v"5"),
+    Platform("x86_64", "Windows", libgfortran_version=v"4"),
+    Platform("x86_64", "Windows", libgfortran_version=v"5")]
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
