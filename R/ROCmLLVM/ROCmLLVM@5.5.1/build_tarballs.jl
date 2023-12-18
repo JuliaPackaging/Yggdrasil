307c7b7fc202e2672c9d017fae45cd4b4617f6f0
Erik Schnetter <schnetter@gmail.com>
2023-12-17 20:29:48 -0500

    ROCmLLVM: Update comments

--- R/ROCmLLVM/ROCmLLVM@5.5.1/build_tarballs.jl
+++ R/ROCmLLVM/ROCmLLVM@5.5.1/build_tarballs.jl
@@ -2,7 +2,7 @@ using Pkg
 using BinaryBuilder
 
 include("../common.jl")
-# original: GCC 7, Clang 9
+# RoCMLLVM 5.5.1 is built on LLVM 16 which is not what Julia 1.9 uses
 build_tarballs(
     ARGS, configure_build(v"5.5.1")...;
     preferred_gcc_version=v"8", preferred_llvm_version=v"9", julia_compat="1.9")
