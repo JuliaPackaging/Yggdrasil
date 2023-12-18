f125d1edcdb267981527712cf79c455f30da1831
Erik Schnetter <schnetter@gmail.com>
2023-12-17 19:10:00 -0500

    ROCmLLVM: Use newer GCC

--- R/ROCmLLVM/ROCmLLVM@5.5.1/build_tarballs.jl
+++ R/ROCmLLVM/ROCmLLVM@5.5.1/build_tarballs.jl
@@ -2,6 +2,7 @@ using Pkg
 using BinaryBuilder
 
 include("../common.jl")
+# original: GCC 7, Clang 9
 build_tarballs(
     ARGS, configure_build(v"5.5.1")...;
-    preferred_gcc_version=v"7", preferred_llvm_version=v"9", julia_compat="1.9")
+    preferred_gcc_version=v"8", preferred_llvm_version=v"9", julia_compat="1.9")
