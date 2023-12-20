60a4e3c3038dd682b1f8d4af9d71db478aff6318
Victor Hugo Cano Gil <49959310+vcanogil@users.noreply.github.com>
2023-12-20 14:10:59 -0500

    Update build_tarballs.jl

--- O/obabel/build_tarballs.jl
+++ O/obabel/build_tarballs.jl
@@ -23,6 +23,7 @@ exit
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
