4f24457603e8f2bc395572b08718898cff8b1e55
Michael Persico <michael.a.persico@gmail.com>
2023-12-20 21:38:56 -0500

    remove Pkg

--- H/Hugo/build_tarballs.jl
+++ H/Hugo/build_tarballs.jl
@@ -1,6 +1,6 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message
-using BinaryBuilder, Pkg
+using BinaryBuilder
 
 name = "Hugo"
 version = v"0.118.2"
