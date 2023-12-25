2d0efec0f7c3dbf7c748065bb6350f58738409f1
JamesWrigley <james@puiterwijk.org>
2023-12-25 01:05:24 +0100

    [Doxygen] Update to 1.9.8

--- D/Doxygen/build_tarballs.jl
+++ D/Doxygen/build_tarballs.jl
@@ -3,24 +3,41 @@
 using BinaryBuilder, Pkg
 
 name = "Doxygen"
-version = v"1.9.2"
+version = v"1.9.8"
 
 # Notes
 # - compile error on g++ < 8
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/doxygen/doxygen/archive/refs/tags/Release_$(version.major)_$(version.minor)_$(version.patch).tar.gz",
-                  "40f429241027ea60f978f730229d22e971786172fdb4dc74db6406e7f6c034b3"),
+    ArchiveSource("https://github.com/doxygen/doxygen/releases/download/Release_$(version.major)_$(version.minor)_$(version.patch)/doxygen-$(version.major).$(version.minor).$(version.patch).src.tar.gz",
+                  "05e3d228e8384b5f3af9c8fd6246d22804acb731a3a24ce285c8986ed7e14f62"),
     DirectorySource("./bundled"),
+    ArchiveSource(
+        "https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.13.sdk.tar.xz",
+        "a3a077385205039a7c6f9e2c98ecdf2a720b2a819da715e03e0630c75782c1e4",
+    )
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
+if [[ "${target}" == x86_64-apple-darwin* ]]; then
+    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
+    rm -rf /opt/${target}/${target}/sys-root/System
+    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
+    cp -ra System "/opt/${target}/${target}/sys-root/."
+    export MACOSX_DEPLOYMENT_TARGET=10.13
+    popd
+fi
+
 cd $WORKSPACE/srcdir/doxygen*/
 if [[ "${target}" == *-linux-* ]] || [[ "${target}" == *-freebsd* ]]; then
     atomic_patch -p1 ../patches/skip-iconv-in-glibc-test.patch
 fi
+if [[ "${target}" == *-w64-* ]]; then
+    atomic_patch -p1 ../patches/convert-path-to-std-string.patch
+fi
+
 mkdir build
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release ..
