1f5a446af8195431af6c0318573eec56618a5c65
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2023-12-27 11:51:09 +0100

    Fix Git version

--- L/LLVM/LLVM_full_assert@17/build_tarballs.jl
+++ L/LLVM/LLVM_full_assert@17/build_tarballs.jl
@@ -2,6 +2,6 @@ version = v"17.0.6+"
 
 include("../common.jl")
 
-build_tarballs(ARGS, configure_build(ARGS, version; assert=true, experimental_platforms=true)...;
+build_tarballs(ARGS, configure_build(ARGS, version; assert=true, experimental_platforms=true, git_ver=llvm_tags[v"17.0.6"])...;
                preferred_gcc_version=v"10", preferred_llvm_version=v"16", julia_compat="1.11")
 # It's building time!!
