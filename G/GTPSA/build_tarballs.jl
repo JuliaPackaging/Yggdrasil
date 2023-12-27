0e1b2d59322cfc3eecd665bf1ba3091e996eda10
Matt Signorelli <mgs255@cornell.edu>
2023-12-27 11:34:20 -0500

    Up Julia compat v1.9

--- G/GTPSA/build_tarballs.jl
+++ G/GTPSA/build_tarballs.jl
@@ -50,4 +50,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"11.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.9", preferred_gcc_version = v"11.1.0")
