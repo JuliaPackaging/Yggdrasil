cb6ddc8c877d0e280b526366ab29b61a9ffdf9e5
bkaus <kaus@uni-mainz.de>
2023-12-28 21:46:22 +0100

    change julia compat

--- P/PETSc/build_tarballs.jl
+++ P/PETSc/build_tarballs.jl
@@ -7,7 +7,7 @@ name = "PETSc"
 version = v"3.18.6"
 petsc_version = v"3.18.6"
 MUMPS_COMPAT_VERSION = "5.5.1"
-SUITESPARSE_COMPAT_VERSION = "7.2.1"  # copmpiled with this version; compat removed below
+SUITESPARSE_COMPAT_VERSION = "7.3.0" 
 SUPERLUDIST_COMPAT_VERSION = "8.1.2"   
 MPItrampoline_compat_version="5.2.1"    
 
@@ -296,4 +296,4 @@ ENV["MPITRAMPOLINE_DELAY_INIT"] = "1"
 
 # Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               augment_platform_block, julia_compat="1.6", preferred_gcc_version = v"9")
+               augment_platform_block, julia_compat="1.10", preferred_gcc_version = v"9")
