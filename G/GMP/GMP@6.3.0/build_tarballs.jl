eb5648437e6362464a16eca2da0411f66e528736
Erik Schnetter <schnetter@gmail.com>
2023-12-29 15:18:36 -0500

    GMP: Don't use clang_use_lld

--- G/GMP/GMP@6.3.0/build_tarballs.jl
+++ G/GMP/GMP@6.3.0/build_tarballs.jl
@@ -4,5 +4,5 @@ include("../common.jl")
 
 # Build the tarballs!
 build_tarballs(ARGS, configure(version)...;
-               preferred_gcc_version=v"6", julia_compat="1.6")
+               clang_use_lld=false, julia_compat="1.6", preferred_gcc_version=v"6")
 
