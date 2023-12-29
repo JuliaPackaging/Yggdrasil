5141603bd37e102cfaa417b9347b31e3d1931c80
Michael Schlottke-Lakemper <michael@sloede.com>
2023-12-29 13:13:50 +0100

    Add OpenFHE

---
+++ O/OpenFHE/build_tarballs.jl
@@ -0,0 +1,43 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "OpenFHE"
+version = v"1.1.2"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/openfheorg/openfhe-development.git", "b2869aef5cf61afd364b3eaea748dcc8a7020b9c")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/openfhe-development/
+mkdir build && cd build
+cmake .. -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DWITH_BE2=ON -DWITH_BE4=ON
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+# platforms = [
+#     Platform("x86_64", "linux"; libc = "glibc")
+# ]
+platforms = expand_cxxstring_abis(platforms)
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libOPENFHEbinfhe", :libOPENFHEbinfhe),
+    LibraryProduct("libOPENFHEpke", :libOPENFHEpke),
+    LibraryProduct("libOPENFHEcore", :libOPENFHEcore)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"10.2.0")
