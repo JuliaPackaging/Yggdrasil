b0dbbe6a6bb18cc8a20199fa390295d545757d46
Matt Signorelli <mgs255@cornell.edu>
2023-12-30 16:44:37 -0500

    Remove ReferenceBLAS_jll, use OpenBLAS32_jll, no more ln

--- G/GTPSA/build_tarballs.jl
+++ G/GTPSA/build_tarballs.jl
@@ -7,32 +7,19 @@ version = v"1.1.1"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/mattsignorelli/gtpsa.git", "7916354009343356f5c44b017fded044b4aaaa53")
+    GitSource("https://github.com/mattsignorelli/gtpsa.git", "12162c188c16cd9c64a43442b704509e8d106a22")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd gtpsa/
-if [[ $target == *"-apple-"* ]]
-then
-CC=gcc
-CXX=g++
-ln /workspace/destdir/lib/liblapack32.dylib /workspace/destdir/lib/liblapack.dylib
-elif [[ $target == *"linux"* ]]
-then
-ln /workspace/destdir/lib/liblapack32.so /workspace/destdir/lib/liblapack.so
-elif [[ $target == *"w64-min"* ]]
-then
-ln /workspace/destdir/bin/liblapack32.dll /workspace/destdir/bin/liblapack.dll
-fi
 cmake . -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN%.*}_gcc.cmake -DCMAKE_BUILD_TYPE=Release
 make
 make install
 """
 
 # These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
 # The code does not compile on FreeBSD due to an error with __builtin_tgmath
 platforms = supported_platforms()
 filter!(!Sys.isfreebsd, platforms)
@@ -45,9 +32,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="ReferenceBLAS_jll", uuid="ee697234-451c-51c9-b102-303d89a9c3a0"))
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
     Dependency(PackageSpec(name="LAPACK32_jll", uuid="17f450c3-bd24-55df-bb84-8c51b4b939e3"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.9", preferred_gcc_version = v"11.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"11.1.0")
