bb5afc3ca1a460ab305c321b542d501d9bae0241
Marco Matthies <71844+marcom@users.noreply.github.com>
2024-01-02 11:42:39 +0100

    [Cares] Update to v1.24.0 (#7885)

--- C/Cares/build_tarballs.jl
+++ C/Cares/build_tarballs.jl
@@ -1,14 +1,14 @@
 using BinaryBuilder, Pkg
 
 name = "Cares"
-version = v"1.23.0"
+version = v"1.24.0"
 
 # url = "https://c-ares.org/"
 # description = "C library for asynchronous DNS requests (including name resolves)"
 
 sources = [
     ArchiveSource("https://c-ares.org/download/c-ares-$(version).tar.gz",
-                  "cb614ecf78b477d35963ebffcf486fc9d55cc3d3216f00700e71b7d4868f79f5"),
+                  "c517de6d5ac9cd55a9b72c1541c3e25b84588421817b5f092850ac09a8df5103"),
 ]
 
 script = raw"""
