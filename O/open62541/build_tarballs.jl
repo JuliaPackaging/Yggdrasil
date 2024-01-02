54d32a16f4165c1608c04d245dcfcd26d48ae8c8
Thomas Vetter <80452158+thomvet@users.noreply.github.com>
2024-01-02 14:15:29 +0100

    add MbedTLS_jll dependency

--- O/open62541/build_tarballs.jl
+++ O/open62541/build_tarballs.jl
@@ -45,7 +45,8 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
+dependencies = [
+    Dependency("MbedTLS_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
