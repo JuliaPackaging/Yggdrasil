39ec8dfd7538db933c0d159b8818df104790a154
Jeremiah Lewis <--get>
2024-01-03 23:58:09 +0100

    add product

--- R/RocksDB/build_tarballs.jl
+++ R/RocksDB/build_tarballs.jl
@@ -40,9 +40,11 @@ ninja install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = Product[
+    LibraryProduct("librocksdb", :librocksdb),
 ]
 
 # Dependencies that must be installed before this package can be built
