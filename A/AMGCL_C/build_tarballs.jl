7e6e33c67cfc732923f359e2a683c7fa51c1af25
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2024-01-04 15:44:18 +0100

    [AMGCL_C] pin boost version
    * Not using much of boost though  - just  boost/property_tree/json_parser.hpp, boost/property_tree/ptree.hpp
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- A/AMGCL_C/build_tarballs.jl
+++ A/AMGCL_C/build_tarballs.jl
@@ -75,7 +75,7 @@ dependencies = [
     # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
-    Dependency(PackageSpec(name="boost_jll", uuid="28df3c45-c428-5900-9ff8-a3135698ca75"),v"1.79.0")
+    Dependency(PackageSpec(name="boost_jll", uuid="28df3c45-c428-5900-9ff8-a3135698ca75"); compat="=1.79.0")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
