9c40eaf63f5963d2d465e77e12cf43c2c054234e
Nick Robinson <npr251@gmail.com>
2024-01-08 13:21:17 +0000

    Add recipe for object_store_ffi

---
+++ O/object_store_ffi/build_tarballs.jl
@@ -0,0 +1,40 @@
+using BinaryBuilder
+
+name = "object_store_ffi"
+version = v"0.1.0"
+
+sources = [
+    # https://github.com/RelationalAI/object_store_ffi/commit/c03a55e65512136c91b6f74787191d34ce2c48b3
+    GitSource("https://github.com/RelationalAI/object_store_ffi.git", "c03a55e65512136c91b6f74787191d34ce2c48b3")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd ${WORKSPACE}/srcdir/
+cargo rustc --release --lib --crate-type=cdylib
+install -Dvm 755 "target/${rust_target}/release/libobject_store_ffi.${dlext}" "${libdir}/libobject_store_ffi.${dlext}"
+"""
+
+# We could potentially support more platforms, if required.
+# Except perhaps i686 Windows and Musl systems.
+platforms = [
+    Platform("aarch64", "macos"),
+    Platform("x86_64",  "linux"),
+    Platform("x86_64",  "macos"),
+    Platform("aarch64", "linux"),
+]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libobject_store_ffi", :libobject_store_ffi),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+]
+
+# Build the tarballs
+build_tarballs(
+    ARGS, name, version, sources, script, platforms, products, dependencies;
+    compilers=[:c, :rust], julia_compat="1.6"
+)
