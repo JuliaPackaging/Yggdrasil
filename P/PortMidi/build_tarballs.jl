422112dc17e9a69d2b21f0f4d79e8013f87d1419
Steffen <steffen.plunder@web.de>
2024-01-13 02:46:21 +0900

    Update build_tarballs.jl

--- P/PortMidi/build_tarballs.jl
+++ P/PortMidi/build_tarballs.jl
@@ -45,7 +45,10 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="alsa_jll", uuid="45378030-f8ea-5b20-a7c7-1a9d95efb90e"))
+    Dependency(
+        PackageSpec(name="alsa_jll", uuid="45378030-f8ea-5b20-a7c7-1a9d95efb90e"), 
+        platforms=[Platform("i686", "windows"; ),Platform("x86_64", "windows"; )]
+    )
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
