1e9d40668e814f12c6cf40e8ffac8d98522cc7d3
Steffen <steffen.plunder@web.de>
2024-01-13 17:54:48 +0900

    Update P/PortMidi/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- P/PortMidi/build_tarballs.jl
+++ P/PortMidi/build_tarballs.jl
@@ -12,8 +12,7 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd portmidi/
+cd $WORKSPACE/srcdir/portmidi/
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
 make 
 make install
