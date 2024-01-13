bb2947c6069db486acea80955a0db672ffda6e3e
Erik Schnetter <schnetter@gmail.com>
2024-01-13 18:29:53 -0500

    squashfs_tools: Build on Apple as well (#7946)
    * squashfs_tools: Build on all platforms
    
    * squashfs_tools: Disable OS xattr support on FreeBSD and Windows
    
    * squashfs_tools: Correct shell syntax
    
    * squashfs_tools: Correct configuration settings
    
    * squashfs_tools: Patch support for XATTR_OS_SUPPORT=0
    
    * squashfs_tools: Patch support for XATTR_OS_SUPPORT=0
    
    * squashfs_tools: Disable FreeBSD and Windows

--- S/squashfs_tools/build_tarballs.jl
+++ S/squashfs_tools/build_tarballs.jl
@@ -12,15 +12,20 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/squashfs-tools/squashfs-tools/
-make XZ_SUPPORT=1 LZO_SUPPORT=1 LZ4_SUPPORT=1 ZSTD_SUPPORT=1 -j${nproc}
+cd $WORKSPACE/srcdir/squashfs-tools/squashfs-tools
+
+env CONFIG=1 make XZ_SUPPORT=1 LZO_SUPPORT=1 LZ4_SUPPORT=1 ZSTD_SUPPORT=1 -j${nproc}
 cp mksquashfs unsquashfs ${bindir}
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter(p -> Sys.islinux(p) && libc(p) == "glibc", supported_platforms())
-
+platforms = supported_platforms()
+# FreeBSD and Windows don't support extended attributes.
+# (The master branch of squashfs-tools already supports disabling host
+# support for them, version 4.6.1 does not yet.  See
+# `XATTR_OS_SUPPORT`.)
+filter!(p -> !Sys.isfreebsd(p) && !Sys.iswindows(p), platforms)
 
 # The products that we will ensure are always built
 products = [
