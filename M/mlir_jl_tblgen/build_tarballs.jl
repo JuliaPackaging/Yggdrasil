567e205198ec3844777cf31e19815f3c19c7cd49
Valentin Churavy <vchuravy@users.noreply.github.com>
2024-01-16 14:51:52 -0500

    Build mlir-jl-tblgen 0.0.2 (#7957)

--- M/mlir_jl_tblgen/build_tarballs.jl
+++ M/mlir_jl_tblgen/build_tarballs.jl
@@ -7,12 +7,12 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
 
 name = "mlir_jl_tblgen"
 repo = "https://github.com/JuliaLabs/MLIR.jl.git"
-version = v"0.0.1"
+version = v"0.0.2"
 
 llvm_versions = [v"14.0.6", v"15.0.7", v"16.0.6"]
 
 sources = [
-    GitSource(repo, "ff7e7e9f342b5fd1932efbee506d19bec6cf3159")
+    GitSource(repo, "72abb740e205fdce1fafcd6177d876864d7b78fb")
 ]
 
 # Bash recipe for building across all platforms
