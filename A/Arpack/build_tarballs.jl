e0a0fad22c36a1ba09e1c28c2473f00713dc2723
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-01-16 10:21:10 -0500

    Update A/Arpack/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- A/Arpack/build_tarballs.jl
+++ A/Arpack/build_tarballs.jl
@@ -23,21 +23,18 @@ mkdir build
 cd build
 
 if [[ ${nbits} == 64 ]]; then
-    cmake .. -DCMAKE_INSTALL_PREFIX="$prefix" \
-        -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" -DCMAKE_BUILD_TYPE=Release \
-        -DBLAS_LIBRARIES=${LBT} -DBLAS_LINKER_FLAGS="-L${libdir}" \
-        -DLAPACK_LIBRARIES=${LBT} -DLAPACK_LINKER_FLAGS="-L${libdir}" \
-        -DEXAMPLES=OFF \
-        -DBUILD_SHARED_LIBS=ON \
-        -DINTERFACE64=1 -DSYMBOLSUFFIX=_64
-else
-    cmake .. -DCMAKE_INSTALL_PREFIX="$prefix" \
-        -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" -DCMAKE_BUILD_TYPE=Release \
-        -DBLAS_LIBRARIES=${LBT} -DBLAS_LINKER_FLAGS="-L${libdir}" \
-        -DLAPACK_LIBRARIES=${LBT} -DLAPACK_LINKER_FLAGS="-L${libdir}" \
-        -DEXAMPLES=OFF \
-        -DBUILD_SHARED_LIBS=ON
+    FLAGS=(-DINTERFACE64=1 -DSYMBOLSUFFIX=_64)
 fi
+cmake .. -DCMAKE_INSTALL_PREFIX="$prefix" \
+    -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBLAS_LIBRARIES=${LBT} \
+    -DBLAS_LINKER_FLAGS="-L${libdir}" \
+    -DLAPACK_LIBRARIES=${LBT} \
+    -DLAPACK_LINKER_FLAGS="-L${libdir}" \
+    -DEXAMPLES=OFF \
+    -DBUILD_SHARED_LIBS=ON \
+    "${FLAGS[@]}"
 
 make -j${nproc}
 make install
