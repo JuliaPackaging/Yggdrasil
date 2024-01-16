ef4e63c7ba6acc1a3400f5a7402e3aa74fafdac5
Mosè Giordano <giordano@users.noreply.github.com>
2024-01-16 00:04:11 +0000

    [OpenBLAS@0.3.26] Build with GCC 11 to get SVE kernels (#7952)

--- O/OpenBLAS/OpenBLAS@0.3.26/build_tarballs.jl
+++ O/OpenBLAS/OpenBLAS@0.3.26/build_tarballs.jl
@@ -15,7 +15,7 @@ dependencies = openblas_dependencies(platforms)
 
 # Build the tarballs
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               preferred_gcc_version=v"6", lock_microarchitecture=false,
+               preferred_gcc_version=v"11", lock_microarchitecture=false,
                julia_compat="1.11", preferred_llvm_version=v"13.0.1")
 
 # Build trigger: 1
