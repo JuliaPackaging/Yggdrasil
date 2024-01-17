ab979d98fabed99b38e775fced5434fe2a3cccc7
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2024-01-17 15:13:51 -0500

    [GTKmm3] remove extra symlinks

--- G/GTKmm3/build_tarballs.jl
+++ G/GTKmm3/build_tarballs.jl
@@ -32,6 +32,9 @@ mkdir output && cd output
 meson --cross-file=${MESON_TARGET_TOOLCHAIN%.*}_gcc.meson .. -Dbuild-demos=false -Dbuild-tests=false
 ninja -j${nproc}
 ninja install
+
+# Remove temporary links
+rm ${prefix}/bin/gdk-pixbuf-pixdata ${prefix}/bin/glib-compile-{resources,schemas}
 """
 
 # These are the platforms we will build for by default, unless further
