84580dae2d207ee73d528312311e3db7223606b4
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2024-01-17 16:07:56 +0100

    [Singular] update to 4.3.2-14

--- S/Singular/build_tarballs.jl
+++ S/Singular/build_tarballs.jl
@@ -28,8 +28,8 @@ import Pkg.Types: VersionSpec
 #
 name = "Singular"
 
-upstream_version = v"4.3.2-12" # 4.3.2p12
-version_offset = v"0.12.2"
+upstream_version = v"4.3.2-14" # 4.3.2p14
+version_offset = v"0.14.0"
 
 version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + version_offset.major,
                         upstream_version.patch * 100 + version_offset.minor,
@@ -37,7 +37,7 @@ version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + 
 
 # Collection of sources required to build normaliz
 sources = [
-    GitSource("https://github.com/Singular/Singular.git", "99a6af79b57a36052bcbeb36b895628ae3344600"),
+    GitSource("https://github.com/Singular/Singular.git", "9413e2181e45f7e7c2764bcb39f0487b97f4d3ba"),
     #ArchiveSource("https://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/$(upstream_version.major)-$(upstream_version.minor)-$(upstream_version.patch)/singular-$(upstream_version).tar.gz",
     #              "5b0f6c036b4a6f58bf620204b004ec6ca3a5007acc8352fec55eade2fc9d63f6"),
     #DirectorySource("./bundled")
