005519b49208bf5e3ff3b66d335652e824e36cf7
Martijn Visser <mgvisser@gmail.com>
2024-01-23 09:30:22 +0100

    try clang_use_lld=false to fix lld linker issues on BSD

--- M/Modflow6/build_tarballs.jl
+++ M/Modflow6/build_tarballs.jl
@@ -46,4 +46,5 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"5.2.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+    julia_compat="1.6", preferred_gcc_version = v"5.2.0", clang_use_lld=false)
