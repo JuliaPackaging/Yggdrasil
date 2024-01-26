7312de97fc29b75a7751ae6d2a76338a28f3e5ac
Michael Schlottke-Lakemper <michael@sloede.com>
2024-01-26 16:31:37 +0100

    gfortran v9 seems to be too old; we will try v10 next

--- S/smesh/build_tarballs.jl
+++ S/smesh/build_tarballs.jl
@@ -46,4 +46,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9.1.0", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"10.1.0", julia_compat="1.6")
