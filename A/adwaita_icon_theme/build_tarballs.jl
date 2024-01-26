6010b50849fb06a00a0e5618f1ed29de5c140b14
Nathan Zimmerberg <39104088+nhz2@users.noreply.github.com>
2024-01-25 20:48:16 -0500

    [adwaita_icon_theme] rebuild

--- A/adwaita_icon_theme/build_tarballs.jl
+++ A/adwaita_icon_theme/build_tarballs.jl
@@ -4,16 +4,24 @@ using BinaryBuilder
 
 name = "adwaita_icon_theme"
 version = v"3.33.93" # new patch version to build for all platforms
+tag = v"3.33.92"
 
 # Collection of sources required to build adwaita-icon-theme
 sources = [
-    ArchiveSource("https://github.com/JuliaBinaryWrappers/adwaita_icon_theme_jll.jl/releases/download/adwaita_icon_theme-v3.33.92+4/adwaita_icon_theme.v3.33.92.any.tar.gz",
-    "f50f3c85710f7dfbd6959bfaa6cc3a940195cd09dadddefb3b5ae9a2f97adad3"),
+    ArchiveSource("https://gitlab.gnome.org/GNOME/adwaita-icon-theme/-/archive/$(tag)/adwaita-icon-theme-$(tag).tar.bz2",
+                  "9e2078bf9e4d28f2a921fa88159733fe83a1fd37f8cbd768a5de3b83f44f0973"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-rsync -a $WORKSPACE/srcdir/share ${prefix}
+cd $WORKSPACE/srcdir/adwaita-icon-theme-*/
+
+# Install native gtk+3.0 so that we get `gtk-encode-symbolic-svg`
+apk add gtk+3.0 librsvg
+./autogen.sh --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make -j${nproc}
+make install
 """
 
 # These are the platforms we will build for by default, unless further
