5dda2a2d50fce489d97b2997b871ac9fddb160af
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-01 18:20:57 +0100

    Fix type used for versioning dependencies

--- S/StableHLO/build_tarballs.jl
+++ S/StableHLO/build_tarballs.jl
@@ -43,8 +43,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("MLIR_jll", compat=v"17.0.6"),
-    Dependency("LLVM_jll", compat=v"17.0.6"),
+    Dependency("MLIR_jll", compat="17.0.6"),
+    Dependency("LLVM_jll", compat="17.0.6"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
