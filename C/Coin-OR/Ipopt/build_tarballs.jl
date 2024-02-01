7c98931137a5dc26bfc7955e6610d66adda9ead4
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-02-01 17:22:43 -0500

    [Ipopt] Update to v3.14.14 (#8055)

--- C/Coin-OR/Ipopt/build_tarballs.jl
+++ C/Coin-OR/Ipopt/build_tarballs.jl
@@ -1,7 +1,7 @@
 include("../coin-or-common.jl")
 
 name = "Ipopt"
-version = Ipopt_version
+version = Ipopt_version  # v3.14.14
 
 sources = [
     GitSource("https://github.com/coin-or/Ipopt.git", Ipopt_gitsha)
