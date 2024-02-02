46738f5f664a402fe1a04ba1756aab693b06d18f
Dongdong Kong <kongdd@users.noreply.github.com>
2024-02-03 00:04:12 +0800

    New Package: Hydrological Model VIC5 (#8056)
    * New Package: Hydrological Model VIC5
    
    * [VIC5] add julia_compat
    
    * [VIC5] fix bug
    
    * Update V/VIC5/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Update V/VIC5/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

---
+++ V/VIC5/build_tarballs.jl
@@ -0,0 +1,38 @@
+using BinaryBuilder
+
+name = "VIC5"
+version = v"0.1.1"
+
+# Collection of sources required to build
+sources = [
+    GitSource("https://github.com/CUG-hydro/VIC5.c.git",
+    "339d9666ce17e9638e16193c43637e453e3b03ef"), # v0.1.1
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/VIC5.c/vic
+
+mkdir -p "${libdir}"
+mkdir -p "${bindir}"
+
+target=${libdir}/libvic5_classic.${dlext} make dll CC=${CC} -j${nproc}
+install_license ${WORKSPACE}/srcdir/VIC5.c/LICENSE.txt
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libvic5_classic", :libvic5_classic),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    # Dependency("CompilerSupportLibraries_jll")
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
