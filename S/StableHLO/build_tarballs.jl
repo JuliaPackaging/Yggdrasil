150759691da364b399bb56c938971261b6398faf
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-02-02 16:11:07 +0100

    Set minimum GCC version to v8

--- S/StableHLO/build_tarballs.jl
+++ S/StableHLO/build_tarballs.jl
@@ -45,4 +45,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.10", preferred_llvm_version=v"17")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.10", preferred_gcc_version=v"8", preferred_llvm_version=v"17")
