eef7625c934a11115bf4f009d662de08245d9a8c
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2024-02-05 15:46:36 +0100

    Update S/sox/build_tarballs.jl

--- S/sox/build_tarballs.jl
+++ S/sox/build_tarballs.jl
@@ -14,7 +14,46 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/sox/
 if [[ "${target}" == *-apple-darwin* ]]; then # Apply patch from https://github.com/chirlu/sox/pull/2
-    echo "diff --git a/src/skelform.c b/src/skelform.c
+    echo "diff --git a/src/flac.c b/src/flac.c
+index 4abcc580..6729f5b2 100644
+--- a/src/flac.c
++++ b/src/flac.c
+@@ -582,7 +582,7 @@ static int stop_write(sox_format_t * const ft)
+ 
+ 
+ 
+-static int seek(sox_format_t * ft, uint64_t offset)
++static int seek(sox_format_t * ft, sox_uint64_t offset)
+ {
+   priv_t * p = (priv_t *)ft->priv;
+   p->seek_offset = offset;
+diff --git a/src/mp3.c b/src/mp3.c
+index 1fd144d1..b75e13b5 100644
+--- a/src/mp3.c
++++ b/src/mp3.c
+@@ -559,7 +559,7 @@ static int stopread(sox_format_t * ft)
+   return SOX_SUCCESS;
+ }
+ 
+-static int sox_mp3seek(sox_format_t * ft, uint64_t offset)
++static int sox_mp3seek(sox_format_t * ft, sox_uint64_t offset)
+ {
+   priv_t   * p = (priv_t *) ft->priv;
+   size_t   initial_bitrate = p->Frame.header.bitrate;
+diff --git a/src/raw.c b/src/raw.c
+index 6c0eaa1b..e7cbcd45 100644
+--- a/src/raw.c
++++ b/src/raw.c
+@@ -19,7 +19,7 @@ typedef sox_int16_t sox_int13_t;
+ #define SOX_SAMPLE_TO_ULAW_BYTE(d,c) sox_14linear2ulaw(SOX_SAMPLE_TO_UNSIGNED(14,d,c) - 0x2000)
+ #define SOX_SAMPLE_TO_ALAW_BYTE(d,c) sox_13linear2alaw(SOX_SAMPLE_TO_UNSIGNED(13,d,c) - 0x1000)
+ 
+-int lsx_rawseek(sox_format_t * ft, uint64_t offset)
++int lsx_rawseek(sox_format_t * ft, sox_uint64_t offset)
+ {
+   return lsx_offset_seek(ft, (off_t)ft->data_start, (off_t)offset);
+ }
+diff --git a/src/skelform.c b/src/skelform.c
 index 793c767e..412bea5d 100644
 --- a/src/skelform.c
 +++ b/src/skelform.c
@@ -27,6 +66,19 @@ index 793c767e..412bea5d 100644
  {
    /* Seek relative to current position. */
    return SOX_SUCCESS;
+diff --git a/src/smp.c b/src/smp.c
+index bf8ac632..90197f38 100644
+--- a/src/smp.c
++++ b/src/smp.c
+@@ -172,7 +172,7 @@ static int writetrailer(sox_format_t * ft, struct smptrailer *trailer)
+         return(SOX_SUCCESS);
+ }
+ 
+-static int sox_smpseek(sox_format_t * ft, uint64_t offset)
++static int sox_smpseek(sox_format_t * ft, sox_uint64_t offset)
+ {
+     uint64_t new_offset;
+     size_t channel_block, alignment;
 diff --git a/src/sox_i.h b/src/sox_i.h
 index c8552f97..301bb5e8 100644
 --- a/src/sox_i.h
