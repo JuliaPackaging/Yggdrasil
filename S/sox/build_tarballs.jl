1090ab1cef9604ca82ce74ffa2d7209b7627c6b9
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2024-02-05 11:05:27 +0100

    Update S/sox/build_tarballs.jl

--- S/sox/build_tarballs.jl
+++ S/sox/build_tarballs.jl
@@ -13,6 +13,9 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/sox/
+if [[ "${target}" != *-apple-darwin* ]]; then # Apply patch from https://github.com/chirlu/sox/pull/2
+    sed -i '141s/uint64_t/sox_uint64_t/' src/formats_i.c # Replace uint64_t by sox_uint64_t at line 141
+fi
 autoreconf -i
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make -j${nproc}
