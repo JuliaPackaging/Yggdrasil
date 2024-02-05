70ba3175c77f7b77e345381b72048646dbc50e9c
Gabriel Baraldi <baraldigabriel@gmail.com>
2024-02-05 11:43:37 -0300

    Rebuild dependent libs

--- L/LLVM/LLVM@16/build_tarballs.jl
+++ L/LLVM/LLVM@16/build_tarballs.jl
@@ -1,6 +1,6 @@
 name = "LLVM"
-llvm_full_version = v"16.0.6+3"
-libllvm_version = v"16.0.6+3"
+llvm_full_version = v"16.0.6+4"
+libllvm_version = v"16.0.6+4"
 
 using BinaryBuilder, Pkg
 using Base.BinaryPlatforms
