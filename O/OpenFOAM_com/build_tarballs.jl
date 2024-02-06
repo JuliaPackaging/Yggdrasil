74f8e9e0133d06e214c37b03f7d92f0544e27887
thealanjason <40885942+thealanjason@users.noreply.github.com>
2024-02-06 01:30:08 +0100

    Use gcc v9

--- O/OpenFOAM_com/build_tarballs.jl
+++ O/OpenFOAM_com/build_tarballs.jl
@@ -573,4 +573,4 @@ append!(dependencies, platform_dependencies)
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
-    preferred_gcc_version = v"8")
+    preferred_gcc_version = v"9")
