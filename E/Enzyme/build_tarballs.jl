532b7b1eed781cb5a65f21b179902eec37fba9e5
William Moses <gh@wsmoses.com>
2024-02-08 10:41:06 -0500

    Update build_tarballs.jl

--- E/Enzyme/build_tarballs.jl
+++ E/Enzyme/build_tarballs.jl
@@ -15,7 +15,7 @@ llvm_versions = [v"11.0.1", v"12.0.1", v"13.0.1", v"14.0.2", v"15.0.7", v"16.0.6
 
 # Collection of sources required to build attr
 sources = [
-    GitSource(repo, "ea07b775faeca2d63ab71df74d8269c6ef9f4ef6"),
+    GitSource(repo, "21053d4122e90cbac3a121286302864627050067"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
                   "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f"),
 ]
