bd3fa87825da171c7ed5663e234eab4b04ae5a95
Erik Schnetter <schnetter@gmail.com>
2024-02-14 21:23:25 -0500

    libpcap: Disable Windows and aarch64-apple

--- L/libpcap/build_tarballs.jl
+++ L/libpcap/build_tarballs.jl
@@ -13,7 +13,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd libpcap/
+cd libpcap
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make
 make install
@@ -22,19 +22,9 @@ exit
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-# platforms = [
-#     Platform("i686", "linux"; libc = "glibc"),
-#     Platform("x86_64", "linux"; libc = "glibc"),
-#     Platform("aarch64", "linux"; libc = "glibc"),
-#     Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-#     Platform("powerpc64le", "linux"; libc = "glibc"),
-#     Platform("i686", "linux"; libc = "musl"),
-#     Platform("x86_64", "linux"; libc = "musl"),
-#     Platform("aarch64", "linux"; libc = "musl"),
-#     Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-#     Platform("x86_64", "freebsd"; )
-# ]
 platforms = supported_platforms()
+filter!(p -> !(Sys.isapple(p) && arch(p) == "aarch64"), platforms)
+filter!(!Sys.iswindows, platforms)
 
 # The products that we will ensure are always built
 products = [
