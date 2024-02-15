4a569565fe36abebdd383849ff3aadf138f14715
Erik Schnetter <schnetter@gmail.com>
2024-02-14 19:45:55 -0500

    libzip: New version 1.10.1

--- L/libzip/build_tarballs.jl
+++ L/libzip/build_tarballs.jl
@@ -3,17 +3,18 @@
 using BinaryBuilder, Pkg
 
 name = "libzip"
-version = v"1.9.2"
+version = v"1.10.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://libzip.org/download/libzip-$(version).tar.gz", "fd6a7f745de3d69cf5603edc9cb33d2890f0198e415255d0987a0cf10d824c6f")
+    ArchiveSource("https://libzip.org/download/libzip-$(version).tar.gz",
+                  "fd6a7f745de3d69cf5603edc9cb33d2890f0198e415255d0987a0cf10d824c6f")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd libzip-1.9.2/
+cd libzip-*
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
 make
 make install
@@ -37,7 +38,8 @@ dependencies = [
     Dependency(PackageSpec(name="Bzip2_jll", uuid="6e34b625-4abd-537c-b88f-471c36dfa7a0"))
     Dependency(PackageSpec(name="XZ_jll", uuid="ffd25f8a-64ca-5728-b0f7-c24cf3aae800"))
     Dependency(PackageSpec(name="Zstd_jll", uuid="3161d3a3-bdf6-5164-811a-617609db77b4"))
-    Dependency(PackageSpec(name="GnuTLS_jll", uuid="0951126a-58fd-58f1-b5b3-b08c7c4a876d"); platforms=filter(p -> Sys.islinux(p) || Sys.isfreebsd(p), platforms))
+    Dependency(PackageSpec(name="GnuTLS_jll", uuid="0951126a-58fd-58f1-b5b3-b08c7c4a876d");
+               platforms=filter(p -> Sys.islinux(p) || Sys.isfreebsd(p), platforms))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
