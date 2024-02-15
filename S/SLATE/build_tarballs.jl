9d2550791f2f6c36ad64e982537a853b4d394c64
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-02-15 18:33:41 +0100

    Update build_tarballs.jl

--- S/SLATE/build_tarballs.jl
+++ S/SLATE/build_tarballs.jl
@@ -70,4 +70,4 @@ append!(dependencies, platform_dependencies)
 
 # Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               augment_platform_block, julia_compat="1.6", preferred_gcc_version = v"11.1.0")
+               augment_platform_block, julia_compat="1.6", preferred_gcc_version = v"7")
