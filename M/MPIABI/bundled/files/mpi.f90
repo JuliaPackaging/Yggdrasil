7a1d0636a9d8aca89230ff15519a6fbb786eca90
Erik Schnetter <schnetter@gmail.com>
2024-02-16 10:59:14 -0500

    MPIABI: Simplify Fortran ABI

--- M/MPIABI/bundled/files/mpi.f90
+++ M/MPIABI/bundled/files/mpi.f90
@@ -1,20 +1,8 @@
-! gfortran -fcray-pointer -c hello_mpif.f90
-! gfortran -o hello_mpif hello_mpif.o
 module mpi
   implicit none
   public
 
-  private ::MPI_DUMMY_VAR
-  private :: MPI_BOTTOM_PTR
-  private :: MPI_IN_PLACE_PTR
-  private :: MPI_BUFFER_AUTOMATIC_PTR
-  private :: MPI_ARGV_NULL_PTR
-  private :: MPI_ARGVS_NULL_PTR
-  private :: MPI_ERRCODES_IGNORE_PTR
-  private :: MPI_STATUS_IGNORE_PTR
-  private :: MPI_STATUSES_IGNORE_PTR
-  private :: MPI_UNWEIGHTED_PTR
-  private :: MPI_WEIGHTS_EMPTY_PTR
+  private :: MPI_DUMMY_VAR
 
   include "mpif.h"
 end module mpi
