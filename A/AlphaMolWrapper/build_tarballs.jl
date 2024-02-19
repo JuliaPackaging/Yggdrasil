aa79c5a90570a2473723ee81c8a6affe1fdabf9d
IvanSpirandelli <ivan@spirandelli.de>
2024-02-19 11:47:01 +0100

    [AlphaMolWrapper_jll] Update (#8128)
    * Updating to latest upstream version.
    
    * fixing libcxxwrap_julia_jll at version 0.11.2
    
    * Increasing version

--- A/AlphaMolWrapper/build_tarballs.jl
+++ A/AlphaMolWrapper/build_tarballs.jl
@@ -1,7 +1,7 @@
 using BinaryBuilder, Pkg
 
 name = "AlphaMolWrapper"
-version = v"0.2"
+version = v"0.2.1"
 julia_versions = [v"1.7", v"1.8", v"1.9", v"1.10"]
 julia_compat = join("~" .* string.(getfield.(julia_versions, :major)) .* "." .* string.(getfield.(julia_versions, :minor)), ", ")
 
@@ -27,7 +27,7 @@ products = [
 
 dependencies = [
     BuildDependency("libjulia_jll"),
-    Dependency("libcxxwrap_julia_jll"),
+    Dependency("libcxxwrap_julia_jll"; compat="0.11.2"),
     Dependency("GMP_jll"; compat="6.2.1"),
 ]
 
