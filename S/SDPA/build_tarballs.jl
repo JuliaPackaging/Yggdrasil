ec94a228154a8d7fdb8f461339105a98ac9a01f0
Benoît Legat <benoit.legat@gmail.com>
2024-02-19 14:02:57 +0100

    Remove hack

--- S/SDPA/build_tarballs.jl
+++ S/SDPA/build_tarballs.jl
@@ -67,8 +67,6 @@ export CPPFLAGS="${CPPFLAGS} -I${prefix}/include -I$prefix/include/coin"
 export CXXFLAGS="${CXXFLAGS} -std=c++11"
 if [[ ${target} == *mingw* ]]; then
     export LDFLAGS="-L$prefix/bin"
-    # work around missing strtoll strtoull, see https://github.com/JuliaLang/julia/issues/48081
-    cp /opt/*-w64-mingw32/*-w64-mingw32/sys-root/lib/libmsvcrt.a ./usr/lib/libmsvcrt.a
 elif [[ ${target} == *linux* ]]; then
     export LDFLAGS="-ldl -lrt"
 fi
