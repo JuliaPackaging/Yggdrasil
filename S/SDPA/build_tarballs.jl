3a71f725a0e7821a27fd2a65d320ff6dada99fe9
Benoît Legat <benoit.legat@gmail.com>
2024-02-21 10:09:14 +0100

    Remove ls

--- S/SDPA/build_tarballs.jl
+++ S/SDPA/build_tarballs.jl
@@ -67,9 +67,8 @@ export CXXFLAGS="${CXXFLAGS} -std=c++11"
 if [[ ${target} == *mingw* ]]; then
     # Needed for https://github.com/JuliaLang/julia/issues/48081
     echo $MACHTYPE
-    ls /opt/i686-w64-mingw32/i686-w64-mingw32/sys-root/lib/libmsvcrt.a
-    ls /opt/$MACHTYPE/$MACHTYPE/sys-root/lib/libmsvcrt.a
-    export LDFLAGS="-L$prefix/bin -L/opt/i686-w64-mingw32/i686-w64-mingw32/sys-root/lib -lmsvcrt"
+    echo $proc_family
+    export LDFLAGS="-L$prefix/bin -L/opt/i686-w64-mingw32/i686-w64-mingw32/sys-root/lib -L/opt/x86_64-w64-mingw32/x86_64-w64-mingw32/sys-root/lib -lmsvcrt"
 elif [[ ${target} == *linux* ]]; then
     export LDFLAGS="-ldl -lrt"
 fi
