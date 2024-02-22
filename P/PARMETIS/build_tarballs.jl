7460264f1c609195061ab6afc79c9e5c8114aee2
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-02-22 21:38:35 +0100

    Update build_tarballs.jl

--- P/PARMETIS/build_tarballs.jl
+++ P/PARMETIS/build_tarballs.jl
@@ -88,7 +88,9 @@ platforms, platform_dependencies = MPI.augment_platforms(platforms; MPItrampolin
 
 # Avoid platforms where the MPI implementation isn't supported
 # OpenMPI
-platforms = filter(p -> !(p["mpi"] == "openmpi" && arch(p) == "armv6l" && libc(p) == "glibc"), platforms)
+platforms = filter(p -> !(p["mpi"] == "openmpi" && nbits(p) == 32), platforms)
+platforms = filter(p -> !(p["mpi"] == "openmpi" && Sys.isfreebsd(p)), platforms)
+platforms = filter(p -> !(p["mpi"] == "openmpi" && Sys.iswindows(p)), platforms)
 # MPItrampoline
 platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && libc(p) == "musl"), platforms)
 platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && Sys.isfreebsd(p)), platforms)
