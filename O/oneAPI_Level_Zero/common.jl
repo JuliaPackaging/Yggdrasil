f1450c9c08bc41fb93c193d29b1449f1da6fcfb7
Tim Besard <tim.besard@gmail.com>
2024-02-22 10:50:37 +0100

    oneAPI Level Zero: bump to headers to v1.16.1 (#8145)

--- O/oneAPI_Level_Zero/common.jl
+++ O/oneAPI_Level_Zero/common.jl
@@ -1,5 +1,5 @@
-version = v"1.13.1"
-api_version = v"1.7.0"
+version = v"1.16.1"
+api_version = v"1.9.1"
 
 # Collection of sources required to build this package
 #
@@ -7,5 +7,5 @@ api_version = v"1.7.0"
 # which are versioned independently.
 sources = BinaryBuilder.AbstractSource[
     GitSource("https://github.com/oneapi-src/level-zero.git",
-              "11c3649a05cf346157bbc6d93a330c33bb7ff7f4"),
+              "ac99dbfb937f0715171eb39f83b5fadf20474b68"),
 ]
