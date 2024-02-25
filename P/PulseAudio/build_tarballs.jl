6745e76b73e1c3960d48ed4406ae29d8e6a6ec67
Mosè Giordano <mose@gnu.org>
2024-02-25 18:44:59 +0000

    [PulseAudio] Disable armv6l

--- P/PulseAudio/build_tarballs.jl
+++ P/PulseAudio/build_tarballs.jl
@@ -39,6 +39,8 @@ ninja install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms(; exclude=!Sys.islinux)
+# Many dependencies are missing for armv6l
+filter!(p->arch(p)!="armv6l", platforms)
 
 # The products that we will ensure are always built
 products = [
