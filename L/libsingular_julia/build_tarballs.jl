2f4a645e430e5858223be8bece6ba972fa2b8174
Max Horn <max@quendi.de>
2024-02-25 20:35:38 +0100

    Use latest libjulia

--- L/libsingular_julia/build_tarballs.jl
+++ L/libsingular_julia/build_tarballs.jl
@@ -50,7 +50,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency(PackageSpec(;name="libjulia_jll", version=v"1.10.8")),
+    BuildDependency(PackageSpec(;name="libjulia_jll", version=v"1.10.9")),
     BuildDependency("GMP_jll"),
     BuildDependency("MPFR_jll"),
     Dependency("libcxxwrap_julia_jll"; compat = "~0.11.2"),
