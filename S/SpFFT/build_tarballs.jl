0d3c391ecc5299c62c06a66f6610286d713aabc0
Augustin Bussy <augustin.bussy@cscs.ch>
2024-02-26 18:33:24 +0100

    [SpFFT] fix build_tarballs.jl (#8190)

--- S/SpFFT/build_tarballs.jl
+++ S/SpFFT/build_tarballs.jl
@@ -89,4 +89,4 @@ append!(dependencies, platform_dependencies)
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version = v"8")
+               augment_platform_block, julia_compat="1.6", preferred_gcc_version = v"8")
