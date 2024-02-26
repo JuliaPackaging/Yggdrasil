f25b78a117df693d4d8cca335c8a64b7e65219c1
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-02-25 23:08:26 -0500

    Update build_tarballs.jl

--- O/OpenJpeg/build_tarballs.jl
+++ O/OpenJpeg/build_tarballs.jl
@@ -46,4 +46,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", clang_use_lld=false)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"6")
