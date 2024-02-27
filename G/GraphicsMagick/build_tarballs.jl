c31ffcb63d1a413048a054f6d8a9493eab066fc9
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-02-28 00:07:29 +0100

    [GraphicsMagick] Bump to 1.3.42 (#7442)
    * [GraphicsMagick] Bump to 1.3.42
    
    * update hash
    
    * Drop patches (upstream is fixed)
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * Add back realtime patch
    
    * Fix patch
    
    * Drop duplicate lib

--- G/GraphicsMagick/build_tarballs.jl
+++ G/GraphicsMagick/build_tarballs.jl
@@ -8,12 +8,13 @@ version = v"1.3.42"
 sources = [
     ArchiveSource("https://sourceforge.net/projects/graphicsmagick/files/graphicsmagick/$(version)/GraphicsMagick-$(version).tar.xz",
                   "484fccfd2b2faf6c2ba9151469ece5072bcb91ba4ed73e75ed3d8e46c759d557"),
-    DirectorySource("bundled"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/GraphicsMagick*
+
 # Don't use `clock_realtime` if it isn't available
 atomic_patch -p1 ../patches/check-have-clock-realtime.patch
 
@@ -83,7 +84,7 @@ dependencies = [
     Dependency("Zstd_jll"),
     Dependency("gperftools_jll"),
     Dependency("libpng_jll"),
-    Dependency("libwebp_jll"),
+    Dependency("libwebp_jll"; compat="1.2.4"),
     # TODO:
     # - ralcgm <http://www.agocg.ac.uk/train/cgm/ralcgm.htm>
     # - cdraw <https://www.dechifro.org/dcraw/>
