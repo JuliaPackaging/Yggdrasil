c973e9e132e00c607974154b92ce4df9a1dc00b8
Erik Schnetter <schnetter@gmail.com>
2024-02-27 12:55:35 -0500

    ZeroMQ: Remove oudated patch

--- Z/ZeroMQ/build_tarballs.jl
+++ Z/ZeroMQ/build_tarballs.jl
@@ -12,11 +12,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libzmq
-if [[ "${target}" == *-mingw* ]]; then
-    # Apply patch from
-    # https://github.com/msys2/MINGW-packages/blob/66c0195ad84836161c48797241a1c7611ac4a435/mingw-w64-zeromq/001-testutil_different_signedness-fix.patch
-    atomic_patch -p1 ../patches/001-testutil_different_signedness-fix.patch
-elif [[ "${target}" == *86*-linux-musl* ]]; then
+if [[ "${target}" == *86*-linux-musl* ]]; then
     # Fix bug in Musl C library, see
     # https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/387
     atomic_patch -d /opt/${target}/lib/gcc/${target}/*/include -p0 $WORKSPACE/srcdir/patches/mm_malloc.patch
@@ -53,4 +49,5 @@ dependencies = Dependency[
     Dependency("libsodium_jll")
 ]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               clang_use_lld=false, julia_compat="1.6")
