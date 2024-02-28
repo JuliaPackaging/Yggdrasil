8a98a430067a8da4e4c550fef1d2ad03058070a8
Daniel <43151183+dannys4@users.noreply.github.com>
2024-02-27 23:57:06 -0500

    Update build_tarballs.jl

--- M/MParT/build_tarballs.jl
+++ M/MParT/build_tarballs.jl
@@ -9,7 +9,7 @@ delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 julia_versions = [v"1.6.3", v"1.7", v"1.8", v"1.9", v"1.10"]
 name = "MParT"
-version = v"2.2.2"
+version = v"2.2.3"
 
 # Collection of sources required to complete build
 sources = [
