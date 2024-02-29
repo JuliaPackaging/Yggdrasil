25ccc8c5deee553020f72e78e40aed2dc3b0ec72
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-02-29 22:16:50 +0100

    [OpenSpiel] drop Julia versions definition (#8222)

--- O/OpenSpiel/build_tarballs.jl
+++ O/OpenSpiel/build_tarballs.jl
@@ -7,7 +7,6 @@ using BinaryBuilder, Pkg
 uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
-julia_versions = [v"1.6.3", v"1.7", v"1.8", v"1.9", v"1.10"]
 name = "OpenSpiel"
 version = v"1.2"
 
