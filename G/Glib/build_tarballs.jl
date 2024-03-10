aafa83a1d74f95781d43a857024b80975b781e15
Erik Schnetter <schnetter@gmail.com>
2024-03-10 16:06:30 -0400

    Glib: Don't use lld

--- G/Glib/build_tarballs.jl
+++ G/Glib/build_tarballs.jl
@@ -85,4 +85,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               clang_use_lld=false, julia_compat="1.6", preferred_gcc_version=v"6")
