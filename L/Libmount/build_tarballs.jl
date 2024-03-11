2fc452c8b247f8636c727fcd0be6f159c97869cb
Erik Schnetter <schnetter@gmail.com>
2024-03-11 06:45:53 -0400

    Libmount: New version 2.39.3 (#8276)
    * Libmount: New version 2.39.3
    
    * Libmount: Use GCC 5
    
    * Libmount: Represent version as string

--- L/Libmount/build_tarballs.jl
+++ L/Libmount/build_tarballs.jl
@@ -3,12 +3,13 @@
 using BinaryBuilder
 
 name = "Libmount"
-version = v"2.35"
+version_string = "2.39.3"
+version = VersionNumber(version_string)
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v$(version.major).$(version.minor)/util-linux-$(version.major).$(version.minor).tar.xz",
-                  "b3081b560268c1ec3367e035234e91616fa7923a0afc2b1c80a2a6d8b9dfe2c9")
+    ArchiveSource("https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v$(version.major).$(version.minor)/util-linux-$(version_string).tar.xz",
+                  "7b6605e48d1a49f43cc4b4cfc59f313d0dd5402fa40b96810bd572e167dfed0f")
 ]
 
 # Bash recipe for building across all platforms
@@ -21,7 +22,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter!(Sys.islinux, supported_platforms(; experimental=true))
+platforms = filter!(Sys.islinux, supported_platforms())
 
 # The products that we will ensure are always built
 products = [
@@ -33,4 +34,5 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", preferred_gcc_version=v"5")
