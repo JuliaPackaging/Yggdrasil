ae34b0e350aeacab4318c88b6c8def4da0867e65
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2024-03-13 18:56:32 +0530

    Update L/libdatachannel/build_tarballs.jl
    Co-authored-by: Ian McInerney <mcianster@gmail.com>

--- L/libdatachannel/build_tarballs.jl
+++ L/libdatachannel/build_tarballs.jl
@@ -26,7 +26,7 @@ cmake --install build
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
-
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
