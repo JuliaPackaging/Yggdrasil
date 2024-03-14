7afa60bfb8036563b92461d083fb3926aee6fb63
Chetan Vardhan <51269425+VarLad@users.noreply.github.com>
2024-03-14 17:25:53 +0530

    Update compiler version

--- L/libdatachannel/build_tarballs.jl
+++ L/libdatachannel/build_tarballs.jl
@@ -58,4 +58,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"13.2.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"8.2.0")
