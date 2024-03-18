c2925e6f0465b918403b5a6a405c084a490e5d91
Valentin Churavy <vchuravy@users.noreply.github.com>
2024-03-18 19:22:28 -0400

    Manually bump Enzyme_jll to 0.0.103 (#8304)

--- E/Enzyme/build_tarballs.jl
+++ E/Enzyme/build_tarballs.jl
@@ -8,14 +8,14 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
 name = "Enzyme"
 repo = "https://github.com/EnzymeAD/Enzyme.git"
 
-auto_version = "refs/tags/v0.0.102"
+auto_version = "refs/tags/v0.0.103"
 version = VersionNumber(split(auto_version, "/")[end])
 
 llvm_versions = [v"11.0.1", v"12.0.1", v"13.0.1", v"14.0.2", v"15.0.7", v"16.0.6"]
 
 # Collection of sources required to build attr
 sources = [
-    GitSource(repo, "c0c1070533767e11c3e97e3337724f309c60331f"),
+    GitSource(repo, "880fb3272e4e34a8535a36f0d0a0fb232630a2b9"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
                   "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f"),
 ]
