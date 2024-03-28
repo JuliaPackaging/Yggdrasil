2f6751e43ffa8cf62710273104107d6293018765
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-03-28 15:56:43 -0400

    [oneAPI_Support_Headers] Upgrade to v2024.1.0 (#8376)

--- O/oneAPI_Support_Headers/build_tarballs.jl
+++ O/oneAPI_Support_Headers/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "oneAPI_Support_Headers"
-version = v"2024.0.0"
+version = v"2024.1.0"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://anaconda.org/intel/mkl-devel-dpcpp/2024.0.0/download/linux-64/mkl-devel-dpcpp-2024.0.0-intel_49656.tar.bz2",
-                  "ba52047546ced5a6b2060dd6c59384af1ab9aefaa47fdc202fbbde2d07602658")
+    ArchiveSource("https://anaconda.org/intel/mkl-devel-dpcpp/2024.1.0/download/linux-64/mkl-devel-dpcpp-2024.1.0-intel_691.tar.bz2",
+                  "abae8c0903e438bce8acfdf2b790d10863669490a87f19a908942268d5fabc82")
 ]
 
 # Bash recipe for building across all platforms
