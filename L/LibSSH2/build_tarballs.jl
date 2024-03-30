dbee0b6951a0cae5f37f2de8e51139a012650159
FX Coudert <fxcoudert@gmail.com>
2024-03-30 17:33:01 +0100

    Migrate libcurl/libgit2/libssh2 to OpenSSL (#8377)
    * LibSSH2: use OpenSSL
    
    * LibGit2: use OpenSSL
    
    Also, disable linking to mbedTLS
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * LibCURL: use OpenSSL
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/LibSSH2/build_tarballs.jl
+++ L/LibSSH2/build_tarballs.jl
@@ -9,24 +9,25 @@ version = v"1.11.0"
 sources = [
     ArchiveSource("https://github.com/libssh2/libssh2/releases/download/libssh2-$(version)/libssh2-$(version).tar.gz",
                   "3736161e41e2693324deb38c26cfdc3efe6209d634ba4258db1cecff6a5ad461"),
-    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libssh2*/
 
-# Apply patch from https://github.com/libssh2/libssh2/pull/1054
-atomic_patch -p1 ../patches/0001-mbedtls-use-more-size_t-to-sync-up-with-crypto.h.patch
-
 if [[ ${bb_full_target} == *-sanitize+memory* ]]; then
     # Install msan runtime (for clang)
     cp -rL ${libdir}/linux/* /opt/x86_64-linux-musl/lib/clang/*/lib/linux/
 fi
 
+# Necessary for cmake to find openssl on Windows
+if [[ ${target} == x86_64-*-mingw* ]]; then
+    export OPENSSL_ROOT_DIR=${prefix}/lib64
+fi
+
 BUILD_FLAGS=(
     -DCMAKE_BUILD_TYPE=Release
-    -DCRYPTO_BACKEND=mbedTLS
+    -DCRYPTO_BACKEND=OpenSSL
     -DBUILD_SHARED_LIBS=ON
     -DBUILD_STATIC_LIBS=OFF
     -DBUILD_EXAMPLES=OFF
@@ -57,7 +58,7 @@ llvm_version = v"13.0.1"
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("MbedTLS_jll"; compat="~2.28.0"),
+    Dependency("OpenSSL_jll"; compat="3.0.8"),
     BuildDependency(PackageSpec(name="LLVMCompilerRT_jll", uuid="4e17d02c-6bf5-513e-be62-445f41c75a11", version=llvm_version);
                     platforms=filter(p -> sanitize(p)=="memory", platforms)),
 ]
