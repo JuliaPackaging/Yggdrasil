4bfee50f75475bce6fc5da6d42027f799d89d384
Claudio Contardo <ccontard@gmail.com>
2024-03-31 13:29:29 -0400

    [CutBranching] new wrapper
    CutBranching_jll provides a julia wrapper for the CutBranching C++ solver for maximum independent set by Demian Hespe, Sebastian Lamm, and Christian Schorr

---
+++ C/CutBranching/build_tarballs.jl
@@ -0,0 +1,44 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "CutBranching"
+version = v"0.1.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/claud10cv/CutBranching.git", "9a1f5745eab88759aa6c3180030eb739a5d4f403")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/CutBranching/
+Julia_PREFIX=${prefix}
+cmake -B build -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DJulia_PREFIX=${Julia_PREFIX} -DJlCxx_DIR=$prefix/lib/cmake/JlCxx
+cmake --build build --parallel ${nproc}
+cmake --install build
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+filter!(x -> libc(x) != "musl", platforms)
+#platforms = [
+#    Platform("x86_64", "linux"; libc = "glibc"),
+#]
+
+
+# The products that we will ensure are always built
+products = Product[
+	LibraryProduct(["libmiscb", "miscblib"], :miscb)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+	Dependency("libcxxwrap_julia_jll"),
+	Dependency("libjulia_jll")
+	
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"9.1.0")
