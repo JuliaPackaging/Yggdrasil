ba89fcb03c637f43235e941496f9ab28dd079cea
Claudio Contardo <ccontard@gmail.com>
2024-04-01 17:26:06 -0400

    Update build_tarballs.jl

--- C/CutBranching/build_tarballs.jl
+++ C/CutBranching/build_tarballs.jl
@@ -7,27 +7,29 @@ version = v"0.1.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/claud10cv/CutBranching.git", "46c14849e75e647045eac97764d31815d9a2f9d2")
+    GitSource("https://github.com/claud10cv/CutBranching.git", "892d48d21b815b6fb55580b33a99e7f251b37e54")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/CutBranching/
 Julia_PREFIX=${prefix}
-cmake -B build -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DJulia_PREFIX=${Julia_PREFIX} -DJlCxx_DIR=$prefix/lib/cmake/JlCxx
+cmake -B build \
+-DCMAKE_INSTALL_PREFIX=${prefix} \
+-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+-DCMAKE_BUILD_TYPE=Release \
+-DJulia_PREFIX=${Julia_PREFIX} \
+-DJlCxx_DIR=$prefix/lib/cmake/JlCxx
 cmake --build build --parallel ${nproc}
 cmake --install build
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
+include("../../L/libjulia/common.jl") 
 platforms = supported_platforms()
 filter!(x -> libc(x) != "musl", platforms)
 filter!(x -> os(x) != "windows", platforms)
-#platforms = [
-#    Platform("x86_64", "linux"; libc = "glibc"),
-#]
-
 
 # The products that we will ensure are always built
 products = Product[
@@ -36,8 +38,8 @@ products = Product[
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
-	Dependency("libcxxwrap_julia_jll"),
-	Dependency("libjulia_jll")
+	Dependency("libcxxwrap_julia_jll"; compat = "~0.12.2"),
+	BuildDependency(PackageSpec(; name = "libjulia_jll", version = v"1.10.9"))
 	
 ]
 
