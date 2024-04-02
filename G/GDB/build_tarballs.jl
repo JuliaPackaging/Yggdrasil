4b496764e37e0f6aa94f7f1ba4201ad2dd7b8bfd
Gabriel Baraldi <baraldigabriel@gmail.com>
2024-04-02 11:12:12 -0300

    Build gdb 14 with a newer python

--- G/GDB/build_tarballs.jl
+++ G/GDB/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "GDB"
-version = v"12.1"
+version = v"14.2"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://ftp.gnu.org/gnu/gdb/gdb-$(version.major).$(version.minor).tar.xz",
-                  "0e1793bf8f2b54d53f46dea84ccfd446f48f81b297b28c4f7fc017b818d69fed"),
+                  "2d4dd8061d8ded12b6c63f55e45344881e8226105f4d2a9b234040efa5ce7772"),
     DirectorySource("./bundled")
 ]
 
@@ -55,7 +55,7 @@ products = [
 dependencies = [
     Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d")),
     Dependency("Expat_jll"),
-    Dependency("Python_jll"; compat="~3.8.8"),
+    Dependency("Python_jll"; compat="~3.10.14"),
     Dependency("Zlib_jll")
 ]
 
