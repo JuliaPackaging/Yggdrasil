0247830bdf1d364e9e34abf277b8e5e1e5dbcd32
Gabriel Baraldi <baraldigabriel@gmail.com>
2024-04-02 11:18:14 -0300

    Add MPFR as a dependency

--- G/GDB/build_tarballs.jl
+++ G/GDB/build_tarballs.jl
@@ -54,6 +54,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d")),
+    Dependency(PackageSpec(name="MPFR_jll", uuid="3a97d323-0669-5f0c-9066-3539efd106a3")),
     Dependency("Expat_jll"),
     Dependency("Python_jll"; compat="~3.10.14"),
     Dependency("Zlib_jll")
