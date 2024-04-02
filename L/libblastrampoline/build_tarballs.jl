d531de9dea22b4f0d6b8be24ab226b7b1fe1db0c
Ian McInerney <i.mcinerney17@imperial.ac.uk>
2024-04-02 15:36:40 +0100

    [libblastrampoline] Fix sanitize build to use LLVM 13

--- L/libblastrampoline/build_tarballs.jl
+++ L/libblastrampoline/build_tarballs.jl
@@ -34,12 +34,15 @@ products = [
     LibraryProduct("libblastrampoline", :libblastrampoline)
 ]
 
+llvm_version = v"13.0.1"
+
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency("LLVMCompilerRT_jll"; platforms=[Platform("x86_64", "linux"; sanitize="memory")]),
+    BuildDependency(PackageSpec(name="LLVMCompilerRT_jll", uuid="4e17d02c-6bf5-513e-be62-445f41c75a11", version=llvm_version);
+    platforms=filter(p -> sanitize(p)=="memory", platforms)),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.10"
+               julia_compat="1.10",  preferred_llvm_version=llvm_version
 )
