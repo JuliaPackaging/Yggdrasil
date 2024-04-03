41629177032d171c9e8520200d28a2b0aebd3067
Mathieu Besançon <mathieu.besancon@gmail.com>
2024-04-03 18:40:20 +0200

    [SCIP_PaPILO] Update version to scip 9 (#8224)
    Co-authored-by: Ian McInerney <i.mcinerney17@imperial.ac.uk>

--- S/SCIP_PaPILO/build_tarballs.jl
+++ S/SCIP_PaPILO/build_tarballs.jl
@@ -4,13 +4,15 @@ using BinaryBuilder, Pkg
 
 name = "SCIP_PaPILO"
 
-version = v"800.100.000"
+version = v"900.000.000"
 
+# Collection of sources required to complete build
 sources = [
     ArchiveSource(
-        "https://scipopt.org/download/release/scipoptsuite-8.1.0.tgz",
-        "a3c1b45220252865d4cedf41d6327b6023608feb360d463f2e68ec4ac41cda06"
+        "https://scipopt.org/download/release/scipoptsuite-9.0.0.tgz",
+        "c49a0575003322fcbfe2d3765de7e3e60ff7c08d1e8b17d35409be40476cb98a"
     ),
+    DirectorySource("./bundled/")
 ]
 
 # Bash recipe for building across all platforms
@@ -20,8 +22,12 @@ cd scipoptsuite*
 # for soplex threadlocal
 export CXXFLAGS="-DTHREADLOCAL=''"
 
-# can be removed for scip 805
-echo "target_link_libraries(clusol gfortran)" >> papilo/CMakeLists.txt
+# Enable large file support on mingw
+if [[ "${target}" == *-mingw* ]]; then
+    export CXXFLAGS="-Wa,-mbig-obj"
+fi
+
+atomic_patch -p0 $WORKSPACE/srcdir/patches/papilo_cmake.patch
 
 mkdir build
 cd build/
@@ -74,9 +80,9 @@ dependencies = [
     Dependency(PackageSpec(name="Bzip2_jll", uuid="6e34b625-4abd-537c-b88f-471c36dfa7a0"); compat="1.0.8"),
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
     Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"); compat="6.2.1"),
-    Dependency(PackageSpec(name="Ipopt_jll", uuid="9cc047cb-c261-5740-88fc-0cf96f7bdcc7"); compat="=300.1400.1302"),
+    Dependency(PackageSpec(name="Ipopt_jll", uuid="9cc047cb-c261-5740-88fc-0cf96f7bdcc7"); compat="=300.1400.1400"),
     Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"); compat="0.3.10"),
-    Dependency(PackageSpec(name="oneTBB_jll", uuid="1317d2d5-d96f-522e-a858-c73665f53c3e"); compat="2021.8.0"),
+    Dependency(PackageSpec(name="oneTBB_jll", uuid="1317d2d5-d96f-522e-a858-c73665f53c3e"); compat="2021.9.0"),
     Dependency(PackageSpec(name="Readline_jll", uuid="05236dd9-4125-5232-aa7c-9ec0c9b2c25a")),
     Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a")),
 ]
