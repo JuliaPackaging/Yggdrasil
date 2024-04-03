3481149636dce785af247223fac82295ef21c54a
Francois-Xavier Coudert <fxcoudert@gmail.com>
2024-04-03 22:54:11 +0200

    [GDAL] Rebuild with libcurl with unversioned symbols

--- G/GDAL/build_tarballs.jl
+++ G/GDAL/build_tarballs.jl
@@ -151,8 +151,8 @@ dependencies = [
     Dependency("Libtiff_jll"; compat="~4.5.1"),
     Dependency("libgeotiff_jll"; compat="100.701.100"),
     Dependency("LibCURL_jll"; compat="7.73,8"),
-    Dependency("NetCDF_jll"; compat="400.902.208", platforms=hdf5_platforms),
-    Dependency("HDF5_jll"; compat="~1.14", platforms=hdf5_platforms),
+    Dependency("NetCDF_jll"; compat="400.902.210", platforms=hdf5_platforms),
+    Dependency("HDF5_jll"; compat="~1.14.3", platforms=hdf5_platforms),
     Dependency("Arrow_jll"; compat="10"),
     BuildDependency(PackageSpec(; name="OpenMPI_jll", version=v"4.1.6"); platforms=filter(p -> nbits(p)==32, platforms)),
 ]
