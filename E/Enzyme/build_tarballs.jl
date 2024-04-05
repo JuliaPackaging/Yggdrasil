228551cee477356bffcd61a91f89cd1d680874c8
William Moses <gh@wsmoses.com>
2024-04-05 15:13:27 -0700

    Bump Enzyme to 0.0.104 (#8449)

--- E/Enzyme/build_tarballs.jl
+++ E/Enzyme/build_tarballs.jl
@@ -8,14 +8,14 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
 name = "Enzyme"
 repo = "https://github.com/EnzymeAD/Enzyme.git"
 
-auto_version = "refs/tags/v0.0.103"
+auto_version = "refs/tags/v0.0.104"
 version = VersionNumber(split(auto_version, "/")[end])
 
 llvm_versions = [v"11.0.1", v"12.0.1", v"13.0.1", v"14.0.2", v"15.0.7", v"16.0.6"]
 
 # Collection of sources required to build attr
 sources = [
-    GitSource(repo, "880fb3272e4e34a8535a36f0d0a0fb232630a2b9"),
+    GitSource(repo, "58b322ebbc50b5f2d585e72a10e77893d6634690"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
                   "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f"),
 ]
