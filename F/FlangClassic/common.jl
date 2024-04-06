42336f0435ba40f9c7443f5bad88a78018c5f54a
Erik Schnetter <schnetter@gmail.com>
2024-04-06 11:30:54 -0400

    FlangClassic: Remove no-src patch

--- F/FlangClassic/common.jl
+++ F/FlangClassic/common.jl
@@ -54,7 +54,6 @@ cd flang-build
 atomic_patch -p1 -d ../flang ../patches/musl-patches.patch
 atomic_patch -p1 -d ../flang ../patches/no-fastmath.patch
 atomic_patch -p1 -d ../flang ../patches/flang2-install-dir.patch
-# TODO atomic_patch -p1 -d ../flang ../patches/flang-no-src-root.patch
 
 ## Create Compiler wrapper for flang
 if [[ ${build_compiler} == 1 ]]; then
