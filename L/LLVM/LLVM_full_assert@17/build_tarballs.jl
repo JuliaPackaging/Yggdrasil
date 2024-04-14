3019abbe26b72eeec7790c5ad79fd4175d0907fa
Zentrik <Zentrik@users.noreply.github.com>
2024-04-14 12:03:00 +0100

    Fix build script for assert build

--- L/LLVM/LLVM_full_assert@17/build_tarballs.jl
+++ L/LLVM/LLVM_full_assert@17/build_tarballs.jl
@@ -2,6 +2,8 @@ version = v"17.0.6"
 
 include("../common.jl")
 
-build_tarballs(ARGS, configure_build(ARGS, version; assert=true, experimental_platforms=true)...;
+for configurations in configure_build(ARGS, version; assert=true, experimental_platforms=true)
+    build_tarballs(ARGS, configurations...;
                preferred_gcc_version=v"10", preferred_llvm_version=v"16", julia_compat="1.12")
+end
 # Build trigger: 4
