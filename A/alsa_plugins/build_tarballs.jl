d27b02a8b69694422d5252b4c65c0a9c8e528b42
Albert R. Gnadt <20693045+gnadt@users.noreply.github.com>
2024-04-16 14:51:17 -0400

    [alsa_plugins] Remove LDFLAGS environment variable

--- A/alsa_plugins/build_tarballs.jl
+++ A/alsa_plugins/build_tarballs.jl
@@ -14,7 +14,6 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/alsa-plugins-*
-export LDFLAGS="-L${libdir}/alsa-lib"
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make
 make install
