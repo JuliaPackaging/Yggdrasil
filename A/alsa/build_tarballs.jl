b2857950f9c096f979c9d85321d7e12ce5a47250
Albert R. Gnadt <20693045+gnadt@users.noreply.github.com>
2024-04-16 13:10:16 -0400

    Revert "[alsa]  Specify config and plugin directories"
    This reverts commit a7d1982b3c457c850af452ab1b3de6ed69e074e0.

--- A/alsa/build_tarballs.jl
+++ A/alsa/build_tarballs.jl
@@ -14,7 +14,7 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/alsa-lib*/
-./configure --prefix=${prefix} --with-configdir=/usr/share/alsa --with-plugindir=/usr/lib/alsa-lib --build=${MACHTYPE} --host=${target}
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make
 make install
 """
