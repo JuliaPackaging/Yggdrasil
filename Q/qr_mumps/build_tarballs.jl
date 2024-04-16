66aaf24fd0611f48c61912b3636ffe192d13926c
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-04-16 18:19:15 -0400

    Use an older version of Clang

--- Q/qr_mumps/build_tarballs.jl
+++ Q/qr_mumps/build_tarballs.jl
@@ -66,4 +66,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat = "1.6", preferred_gcc_version = v"9.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"9.1.0", clang_use_lld=false)
