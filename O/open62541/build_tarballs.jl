63f84663577c92e83854f6b483b38fcde1583f36
Thomas Vetter <80452158+thomvet@users.noreply.github.com>
2024-04-19 00:19:03 +0200

    [open62541] Add OpenSSL encryption, update Julia compat and increase version number (1.4.0) (#7889)
    * [open62541] Add TLS encryption to v1.3.9 build
    
    * add MbedTLS_jll dependency
    
    * Add compat for MbedTLS
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>
    
    * Update build_tarballs.jl
    
    * Use OpenSSL instead of MbedTLS; update to open62541 v1.4.0
    
    * adjust version requirement for OpenSSL_jll
    
    * correct typo
    
    * check 1.4 independently of openssl
    
    * try without amalgamation
    
    * try
    
    * revert
    
    * try another OpenSSL version
    
    * Set OPENSSL_ROOT_DIR to fix build on x86 Windows
    
    * hotfix attempt
    
    * revert
    
    * patch attempt
    
    * patch attempt 2
    
    * actually applying the patch would be good.
    
    * fix directory mistake
    
    * typo
    
    * Update O/open62541/build_tarballs.jl
    
    ---------
    
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- O/open62541/build_tarballs.jl
+++ O/open62541/build_tarballs.jl
@@ -3,17 +3,27 @@
 using BinaryBuilder, Pkg
 
 name = "open62541"
-version = v"1.3.9"
+version = v"1.4.0"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/open62541/open62541.git",
-              "70ff3501ddecd7e7594ebc63e2365994d59e010d")
+              "84347820c8550b5750f2cd581c14ab201611c579"),
+    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
+# Necessary for cmake to find openssl on Windows 
+if [[ ${target} == x86_64-*-mingw* ]]; then 
+    export OPENSSL_ROOT_DIR=${prefix}/lib64 
+fi 
+
 cd $WORKSPACE/srcdir/open62541/
+if [[ "${target}" == *-freebsd* ]]; then
+    # https://github.com/open62541/open62541/issues/6414
+    atomic_patch -p1 ../0001-freebsd.patch
+fi
 mkdir build && cd build/
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
@@ -23,7 +33,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DUA_ENABLE_METHODCALLS=ON \
     -DUA_ENABLE_PARSING=ON \
     -DUA_ENABLE_NODEMANAGEMENT=ON \
-    -DUA_ENABLE_AMALGAMATION=ON \
+    -DUA_ENABLE_ENCRYPTION=OPENSSL \
     -DUA_ENABLE_IMMUTABLE_NODES=ON \
     -DUA_ENABLE_HISTORIZING=ON \
     -DBUILD_SHARED_LIBS=ON \
@@ -44,7 +54,8 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
+dependencies = [
+    Dependency("OpenSSL_jll"; compat="3.0.8")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
