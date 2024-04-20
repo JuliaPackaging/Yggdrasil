ebc25c3239a4e4887ec24e1f9f936a45eeb24e9f
Albert R. Gnadt <20693045+gnadt@users.noreply.github.com>
2024-04-20 10:47:15 -0400

    Update A/alsa_plugins/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- A/alsa_plugins/build_tarballs.jl
+++ A/alsa_plugins/build_tarballs.jl
@@ -21,7 +21,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter(p -> Sys.islinux(p) & (arch(p) != "armv6l"), supported_platforms(;experimental=true))
+platforms = filter(p -> Sys.islinux(p) & (arch(p) != "armv6l"), supported_platforms())
 
 # The products that we will ensure are always built
 products = [
