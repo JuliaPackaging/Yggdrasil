38a018fc9e0b811bec2822bfc6e3d472bcc86b8b
Erik Schnetter <schnetter@gmail.com>
2024-04-22 04:42:10 -0400

    MKL_Headers: New version 2024.1.0 (#8507)

--- M/MKL_Headers/build_tarballs.jl
+++ M/MKL_Headers/build_tarballs.jl
@@ -2,68 +2,53 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-# Note: 2024.0.0 has already been built, but 2023.2.0 is being built to fix a bug in the packaging where the CMake/pkgconfig scripts were missing.
-# When updating to the next 2024.x.x release, the following must be done again (in addition to updating the sources):
-# * Bump the Julia compat to 1.6
-# * Remove the macos x86_64 platform and source files
-
 name = "MKL_Headers"
-version = v"2023.2.0"
+version = v"2024.1.0"
 
 # Collection of sources required to complete build
 sources = [
     # Archives for the headers
     ArchiveSource(
-        "https://anaconda.org/intel/mkl-include/2023.2.0/download/win-32/mkl-include-2023.2.0-intel_49496.tar.bz2",
-        "0ed907ecc2eaae0ed8c280814392b5b80cc19df78838d9688273a12bd72c7bf8";
+        "https://anaconda.org/intel/mkl-include/2024.1.0/download/win-32/mkl-include-2024.1.0-intel_692.tar.bz2",
+        "8994e1c5b5599934e83eb964a136be98dc5a6355f3f5b35cab44cdc0e8b970dd";
         unpack_target = "mkl-include-i686-w64-mingw32"
     ),
     ArchiveSource(
-        "https://anaconda.org/intel/mkl-include/2023.2.0/download/win-64/mkl-include-2023.2.0-intel_49496.tar.bz2",
-        "daa93c899e6c7627232fa60e67a2b6079cd29752e8ba1251ae895a57e51defa7";
+        "https://anaconda.org/intel/mkl-include/2024.1.0/download/win-64/mkl-include-2024.1.0-intel_692.tar.bz2",
+        "28229844aa6c19870531452e5805ab876da4a5df896a9e753e6b481da2d389cb";
         unpack_target = "mkl-include-x86_64-w64-mingw32"
     ),
     ArchiveSource(
-        "https://anaconda.org/intel/mkl-include/2023.2.0/download/linux-32/mkl-include-2023.2.0-intel_49495.tar.bz2",
-        "b4433c6839bb7f48951b2dcf409dec7306aee3649c539ee0513d8bfb1a1ea283";
+        "https://anaconda.org/intel/mkl-include/2024.1.0/download/linux-32/mkl-include-2024.1.0-intel_691.tar.bz2",
+        "88529f8bea2498e88b2cf8dc7aa3735f46f348cf5047006dfc6455f8e2bbdd30";
         unpack_target = "mkl-include-i686-linux-gnu"
     ),
     ArchiveSource(
-        "https://anaconda.org/intel/mkl-include/2023.2.0/download/linux-64/mkl-include-2023.2.0-intel_49495.tar.bz2",
-        "0dfb6ca3c17d99641f20877579c78155cf95aa0b22363bcc91b1d57df4646318";
+        "https://anaconda.org/intel/mkl-include/2024.1.0/download/linux-64/mkl-include-2024.1.0-intel_691.tar.bz2",
+        "e36b2e74f5c28ff91565abe47a09dc246c9cf725e0d05b5fb08813b4073ea68b";
         unpack_target = "mkl-include-x86_64-linux-gnu"
-    ),
-    ArchiveSource(
-        "https://anaconda.org/intel/mkl-include/2023.2.0/download/osx-64/mkl-include-2023.2.0-intel_49499.tar.bz2",
-        "c3940a33498df821821c28dc292f7d7a739b11892856fd9fbbc3de5cf0990b00";
-        unpack_target = "mkl-include-x86_64-apple-darwin14"
     ),
 
     # Archives for the CMake/pkgconfig files
     ArchiveSource(
-        "https://anaconda.org/intel/mkl-devel/2023.2.0/download/win-32/mkl-devel-2023.2.0-intel_49496.tar.bz2",
-        "15653969e64579e7bfb098fcc3e994590b6f7f7fcf9f2ac6447884e8f0ffb027";
+        "https://anaconda.org/intel/mkl-devel/2024.1.0/download/win-32/mkl-devel-2024.1.0-intel_692.tar.bz2",
+        "845156ebe08b1d3ac519da1a56d1b98a6f818158eaa88bd4bbaebb9b28aab6cd";
         unpack_target = "mkl-devel-i686-w64-mingw32"
     ),
     ArchiveSource(
-        "https://anaconda.org/intel/mkl-devel/2023.2.0/download/win-64/mkl-devel-2023.2.0-intel_49496.tar.bz2",
-        "caaf632783636e8434216f37978b0942e0aa18c9fd3a4bb4a0444aa48ac07323";
+        "https://anaconda.org/intel/mkl-devel/2024.1.0/download/win-64/mkl-devel-2024.1.0-intel_692.tar.bz2",
+        "d656781f53513be1cde1d33fd84fcd43a746453347afc2fcf1f61218b2d08783";
         unpack_target = "mkl-devel-x86_64-w64-mingw32"
     ),
     ArchiveSource(
-        "https://anaconda.org/intel/mkl-devel/2023.2.0/download/linux-32/mkl-devel-2023.2.0-intel_49495.tar.bz2",
-        "09302cd45ff9252e862abe8bc01cefc1f4afa8339237129f847620784f1fd93e";
+        "https://anaconda.org/intel/mkl-devel/2024.1.0/download/linux-32/mkl-devel-2024.1.0-intel_691.tar.bz2",
+        "2145f2ae4f383cc46cf4e5f516b7709f727d21865f21d445454c52175c8fafd1";
         unpack_target = "mkl-devel-i686-linux-gnu"
     ),
     ArchiveSource(
-        "https://anaconda.org/intel/mkl-devel/2023.2.0/download/linux-64/mkl-devel-2023.2.0-intel_49495.tar.bz2",
-        "f3e2b7063b28b280602fea4005408ee74cf6a376bc99c0e05fc67531f2c03ace";
+        "https://anaconda.org/intel/mkl-devel/2024.1.0/download/linux-64/mkl-devel-2024.1.0-intel_691.tar.bz2",
+        "def8ca30d0560a712e5f010f26da26d723c6bc9148124d8a63f6d2fb64fd3e38";
         unpack_target = "mkl-devel-x86_64-linux-gnu"
-    ),
-    ArchiveSource(
-        "https://anaconda.org/intel/mkl-devel/2023.2.0/download/osx-64/mkl-devel-2023.2.0-intel_49499.tar.bz2",
-        "0f1f7da2fb79d40257c9a15496c84036771cb265c6e3c82ed8b5852c64bdeed0";
-        unpack_target = "mkl-devel-x86_64-apple-darwin14"
     ),
 ]
 
@@ -92,7 +77,6 @@ fi
 platforms = [
     Platform("x86_64", "linux"; libc="glibc"),
     Platform("i686", "linux"; libc="glibc"),
-    Platform("x86_64", "macos"),
     Platform("i686", "windows"),
     Platform("x86_64", "windows"),
 ]
@@ -107,4 +91,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
