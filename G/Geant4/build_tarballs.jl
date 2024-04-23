887a6590984f352c511f0eddccdebcc78955321d
Pere Mato <pere.mato@cern.ch>
2024-04-23 20:35:36 +0200

    Suggestions from Mosè

--- G/Geant4/build_tarballs.jl
+++ G/Geant4/build_tarballs.jl
@@ -109,4 +109,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"11", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"10", julia_compat="1.6")
