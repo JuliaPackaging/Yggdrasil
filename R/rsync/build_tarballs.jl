4fc0654f0102087da102957c4c3ddd228a58e32a
Erik Schnetter <schnetter@gmail.com>
2024-04-23 18:50:11 -0400

    rsync: Correct configure options

--- R/rsync/build_tarballs.jl
+++ R/rsync/build_tarballs.jl
@@ -15,7 +15,7 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 
-cd rsync-*/
+cd rsync-*
 install_license COPYING
 
 CONFIGURE_FLAGS=(--prefix=${prefix} --build=${MACHTYPE} --host=${target})
@@ -28,8 +28,13 @@ CONFIGURE_FLAGS+=(--disable-iconv-open)
 # don't include debug symbols
 CONFIGURE_FLAGS+=(--disable-debug)
 
-./configure ${CONFIGURE_FLAGS[@]}
-make -j${nproc} install
+# The default settings for `includedir`, `libdir`, and `bindir`
+# contain an unexpanded `${prefix}`. This is not expanded in our `cc`
+# wrapper. This means that dependencies are not found. (This may
+# happen only on some architectures?)
+./configure ${CONFIGURE_FLAGS[@]} includedir=${includedir} libdir=${libdir} bindir=${bindir}
+make -j${nproc}
+make install
 """
 
 # These are the platforms we will build for by default, unless further
@@ -46,12 +51,12 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a")),
-    Dependency(PackageSpec(name="Popt_jll", uuid="e80236cf-ab1d-5f5d-8534-1d1285fe49e8")),
     Dependency(PackageSpec(name="Lz4_jll", uuid="5ced341a-0733-55b8-9ab6-a4889d929147")),
+    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"); compat="3.0.13"),
+    Dependency(PackageSpec(name="Popt_jll", uuid="e80236cf-ab1d-5f5d-8534-1d1285fe49e8")),
+    Dependency(PackageSpec(name="Zlib_jll", uuid="83775a58-1f1d-513f-b197-d71354ab007a")),
     Dependency(PackageSpec(name="Zstd_jll", uuid="3161d3a3-bdf6-5164-811a-617609db77b4")),
     Dependency(PackageSpec(name="xxHash_jll", uuid="5fdcd639-92d1-5a06-bf6b-28f2061df1a9")),
-    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"); compat="3.0.13"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
