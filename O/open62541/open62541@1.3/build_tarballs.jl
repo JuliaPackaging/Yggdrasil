9123d05b5615909e6675e807dfc475934acdb899
Thomas Vetter <80452158+thomvet@users.noreply.github.com>
2024-04-24 11:41:44 +0200

    remove experimental
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- O/open62541/open62541@1.3/build_tarballs.jl
+++ O/open62541/open62541@1.3/build_tarballs.jl
@@ -32,7 +32,7 @@ install_license ../LICENSE
 """
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libopen62541", :libopen62541)
