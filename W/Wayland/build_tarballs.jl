9bd15e930c10e7a2eb97022341266ae60aa16de6
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2024-04-26 15:03:56 +0200

    Update build_tarballs.jl

--- W/Wayland/build_tarballs.jl
+++ W/Wayland/build_tarballs.jl
@@ -18,7 +18,7 @@ cd $WORKSPACE/srcdir/wayland/
 # We need to run `wayland-scanner` of the same version on the host system Alpine v3.18 has v1.22
 apk add wayland-dev --repository=http://dl-cdn.alpinelinux.org/alpine/v3.20/main
 
-# ln -s `which wayland-scanner` $bindir
+ln -s `which wayland-scanner` $bindir
 # cp $prefix/libdata/pkgconfig/* $prefix/lib/pkgconfig || true
 
 mkdir build-wayland
