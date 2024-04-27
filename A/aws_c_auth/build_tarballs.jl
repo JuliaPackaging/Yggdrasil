896deef28b6a3e55d0e398899fb854e1ad371fd8
Jacob Quinn <quinn.jacobd@gmail.com>
2024-04-27 14:02:02 -0600

    Bump aws-c-auth to 0.7.18 (#8549)
    * Bump aws-c-auth to 0.7.18
    
    * Also bump aws-c-http dep version

--- A/aws_c_auth/build_tarballs.jl
+++ A/aws_c_auth/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_auth"
-version = v"0.7.3"
+version = v"0.7.18"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/awslabs/aws-c-auth.git",
-              "df370cabd6bdfee7ce7d550daca19b6d14eb54b4"),
+              "277c8fbcb3f5341adb51000624b55ace301cfe82"),
 ]
 
 # Bash recipe for building across all platforms
@@ -38,7 +38,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("aws_c_cal_jll"; compat="0.6.2"),
-    Dependency("aws_c_http_jll"; compat="0.7.12"),
+    Dependency("aws_c_http_jll"; compat="0.8.1"),
     Dependency("aws_c_sdkutils_jll"; compat="0.1.12"),
     BuildDependency("aws_lc_jll"),
 ]
