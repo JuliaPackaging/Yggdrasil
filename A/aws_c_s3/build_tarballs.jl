43e7e0d17932c3aa3bef2cf91750a1a98e904681
Octogonapus <me@irrational.engineering>
2024-05-01 17:24:19 -0400

    Update aws-c-s3 to 0.5.7 (#8583)

--- A/aws_c_s3/build_tarballs.jl
+++ A/aws_c_s3/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_s3"
-version = v"0.3.17"
+version = v"0.5.7"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/awslabs/aws-c-s3.git",
-              "1fe34646f6bb2282491077b1d2407c724c0d5471"),
+              "3334843eb4e0e56c2565e481b2ef853d1cadde78"),
 ]
 
 # Bash recipe for building across all platforms
@@ -38,8 +38,8 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("aws_checksums_jll"; compat="0.1.17"),
-    Dependency("aws_c_http_jll"; compat="0.7.12"),
-    Dependency("aws_c_auth_jll"; compat="0.7.3"),
+    Dependency("aws_c_http_jll"; compat="0.8.1"),
+    Dependency("aws_c_auth_jll"; compat="0.7.18"),
     BuildDependency("aws_lc_jll"),
 ]
 
