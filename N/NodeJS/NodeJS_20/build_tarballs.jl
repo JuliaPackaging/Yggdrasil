84a4824f82f1e8ce5ccc2229ffae6549ab285f79
Anshul Singhvi <asinghvi17@simons-rock.edu>
2024-05-03 11:18:34 -0400

    [NodeJS_20_jll] Update version (#8595)
    * [NodeJS_20_jll] Update version + make all products executable
    
    This fixes https://github.com/LuxDL/DocumenterVitepress.jl/issues/122 where the PATH was not adjusted appropriately.
    
    * Revert executableproduct change

--- N/NodeJS/NodeJS_20/build_tarballs.jl
+++ N/NodeJS/NodeJS_20/build_tarballs.jl
@@ -1,24 +1,24 @@
 using BinaryBuilder
 
 name = "NodeJS_20"
-version = v"20.11.1"
+version = v"20.12.2"
 
 url_prefix = "https://nodejs.org/dist/v$version/node-v$version"
 sources = [
-    ArchiveSource("$(url_prefix)-linux-x64.tar.gz", "bf3a779bef19452da90fb88358ec2c57e0d2f882839b20dc6afc297b6aafc0d7"; unpack_target = "x86_64-linux-gnu"),
-    ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "e34ab2fc2726b4abd896bcbff0250e9b2da737cbd9d24267518a802ed0606f3b"; unpack_target = "aarch64-linux-gnu"),
-    ArchiveSource("$(url_prefix)-linux-ppc64le.tar.gz", "9823305ac3a66925a9b61d8032f6bbb4c3e33c28e7f957ebb27e49732feffb23"; unpack_target = "powerpc64le-linux-gnu"),
-    ArchiveSource("$(url_prefix)-linux-armv7l.tar.gz", "e42791f76ece283c7a4b97fbf716da72c5128c54a9779f10f03ae74a4bcfb8f6"; unpack_target = "arm-linux-gnueabihf"),
+    ArchiveSource("$(url_prefix)-linux-x64.tar.gz", "f8f9b6877778ed2d5f920a5bd853f0f8a8be1c42f6d448c763a95625cbbb4b0d"; unpack_target = "x86_64-linux-gnu"),
+    ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "2dc8ffa0da135bf493f881d2d38aac610772c801bb7b6208fcc5de9350f119f7"; unpack_target = "aarch64-linux-gnu"),
+    ArchiveSource("$(url_prefix)-linux-ppc64le.tar.gz", "c33968d78e06af64bd8d89a74781fef71ff126f862f7ed0ff2417d612dd64abb"; unpack_target = "powerpc64le-linux-gnu"),
+    ArchiveSource("$(url_prefix)-linux-armv7l.tar.gz", "5861b891815ae8d42835db52bc57191858f348e0521b162c670c8ed4df417f1c"; unpack_target = "arm-linux-gnueabihf"),
     
-    ArchiveSource("$(url_prefix)-linux-x64.tar.gz", "bf3a779bef19452da90fb88358ec2c57e0d2f882839b20dc6afc297b6aafc0d7"; unpack_target = "x86_64-linux-musl"),
-    ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "e34ab2fc2726b4abd896bcbff0250e9b2da737cbd9d24267518a802ed0606f3b"; unpack_target = "aarch64-linux-musl"),
-    ArchiveSource("$(url_prefix)-linux-armv7l.tar.gz", "e42791f76ece283c7a4b97fbf716da72c5128c54a9779f10f03ae74a4bcfb8f6"; unpack_target = "arm-linux-musleabihf"),
+    ArchiveSource("$(url_prefix)-linux-x64.tar.gz", "f8f9b6877778ed2d5f920a5bd853f0f8a8be1c42f6d448c763a95625cbbb4b0d"; unpack_target = "x86_64-linux-musl"),
+    ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "2dc8ffa0da135bf493f881d2d38aac610772c801bb7b6208fcc5de9350f119f7"; unpack_target = "aarch64-linux-musl"),
+    ArchiveSource("$(url_prefix)-linux-armv7l.tar.gz", "5861b891815ae8d42835db52bc57191858f348e0521b162c670c8ed4df417f1c"; unpack_target = "arm-linux-musleabihf"),
     
-    ArchiveSource("$(url_prefix)-darwin-x64.tar.gz", "c52e7fb0709dbe63a4cbe08ac8af3479188692937a7bd8e776e0eedfa33bb848"; unpack_target = "x86_64-apple-darwin14"),
-    ArchiveSource("$(url_prefix)-darwin-arm64.tar.gz", "e0065c61f340e85106a99c4b54746c5cee09d59b08c5712f67f99e92aa44995d"; unpack_target = "aarch64-apple-darwin20"),
+    ArchiveSource("$(url_prefix)-darwin-x64.tar.gz", "cd5e9a80a38ccffc036a87b232a5402339c7bf8fa9a494ae0731a1a671687718"; unpack_target = "x86_64-apple-darwin14"),
+    ArchiveSource("$(url_prefix)-darwin-arm64.tar.gz", "98eb624b52efec2530079e1d11296ec0ac20771b94b087d21649250339cf5332"; unpack_target = "aarch64-apple-darwin20"),
     
-    ArchiveSource("$(url_prefix)-win-x64.zip", "bc032628d77d206ffa7f133518a6225a9c5d6d9210ead30d67e294ff37044bda"; unpack_target = "x86_64-w64-mingw32"),
-    ArchiveSource("$(url_prefix)-win-x86.zip", "b98e95f78416d1359b647cfa09ba2a48b76d41b56a776df822bf36ffe8e76a2d"; unpack_target = "i686-w64-mingw32"),
+    ArchiveSource("$(url_prefix)-win-x64.zip", "66dda1717cae30a13be6bb17ad96ee54b69f2c23c85acd9c3299b095fa26b452"; unpack_target = "x86_64-w64-mingw32"),
+    ArchiveSource("$(url_prefix)-win-x86.zip", "acf7d7fedf3a50aaa12c4e2bf0aa6220727b22eb24ad1b37264d46e12421d03d"; unpack_target = "i686-w64-mingw32"),
 ]
 
 # Bash recipe for building across all platforms
