7d76a6f70b02087d6508a0fce7a0b5b4db36656e
Erik Schnetter <schnetter@gmail.com>
2024-05-04 11:20:49 -0400

    MPItrampoline: Use GCC5 for ligbfortran3 architectures (#8610)

--- M/MPItrampoline/build_tarballs.jl
+++ M/MPItrampoline/build_tarballs.jl
@@ -262,5 +262,6 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
+# We use GCC 5 to ensure Fortran module files are readable by all `libgfortran3` architectures. GCC 4 would use an older format.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               augment_platform_block, julia_compat="1.6", clang_use_lld=false)
+               augment_platform_block, julia_compat="1.6", clang_use_lld=false, preferred_gcc_version=v"5")
