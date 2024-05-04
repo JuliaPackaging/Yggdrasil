083e90a820f16f8384c26355cda9a091ac764f75
William S. Moses <gh@wsmoses.com>
2024-05-03 20:49:35 -0400

    Reactant: another try at augment platform block

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -269,11 +269,11 @@ platforms = filter(p -> !(Sys.isfreebsd(p)), platforms)
 # platforms = filter(p -> !(Sys.isapple(p)), platforms)
 # platforms = filter(p -> cxxstring_abi(p) == "cxx11", platforms)
 
+augment_platform_block=CUDA.augment::String
 
 for platform in platforms
     augmented_platform = deepcopy(platform)
     cuda_deps = []
-    augment_platform_block=""
 
     prefix=""
     platform_sources = BinaryBuilder.AbstractSource[sources...]
@@ -292,14 +292,13 @@ for platform in platforms
         push!(cuda_deps, BuildDependency(PackageSpec(name="CUDNN_jll")))
         push!(cuda_deps, BuildDependency(PackageSpec(name="TensorRT_jll")))
         push!(cuda_deps, BuildDependency(PackageSpec(name="CUDA_full_jll")))
-        augment_platform_block=CUDA.augment::String
         prefix = "export CUDA_VERSION=\"\"\n"
     end
 
     should_build_platform(triplet(augmented_platform)) || continue
     push!(builds, (;
                    dependencies=[dependencies; cuda_deps], products, sources=platform_sources,
-        platforms=[augmented_platform], augment_platform_block=augment_platform_block::String, script=prefix*script
+        platforms=[augmented_platform], script=prefix*script
     ))
 end
 
@@ -315,6 +314,6 @@ for (i,build) in enumerate(builds)
                    name, version, build.sources, build.script,
                    build.platforms, build.products, build.dependencies;
                    preferred_gcc_version=v"10", julia_compat="1.6",
-                   augment_platform_block=build.augment_platform_block::String, lazy_artifacts=true)
+                   augment_platform_block, lazy_artifacts=true)
 end
 
