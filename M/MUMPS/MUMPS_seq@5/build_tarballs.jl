da1a9cb8f19ab8a00b3dab48372548eac20e20dc
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-05-04 22:06:58 -0400

    Try to compile MUMPS with the release 5.9.0 of LBT

--- M/MUMPS/MUMPS_seq@5/build_tarballs.jl
+++ M/MUMPS/MUMPS_seq@5/build_tarballs.jl
@@ -93,7 +93,7 @@ products = [
 dependencies = [
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
     Dependency(PackageSpec(name="METIS_jll", uuid="d00139f3-1899-568f-a2f0-47f597d42d70")),
-    Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"), compat="5.4.0"),
+    Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"), compat="5.9.0"),
 ]
 
 # Build the tarballs
