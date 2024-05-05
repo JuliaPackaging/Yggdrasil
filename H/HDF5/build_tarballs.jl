9a59617f428a205874478c0a52809c3d3a24073d
Erik Schnetter <schnetter@gmail.com>
2024-05-05 11:38:07 -0400

    HDF5: Update compat bounds

--- H/HDF5/build_tarballs.jl
+++ H/HDF5/build_tarballs.jl
@@ -301,7 +301,7 @@ dependencies = [
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); compat="1.0.0"),
     Dependency("LibCURL_jll"; compat="7.73.0, 8"),
     Dependency("OpenSSL_jll", compat="3.0.8"),
-    Dependency("Zlib_jll"; compat="1.2.11"),
+    Dependency("Zlib_jll"; compat="1.2.12"),
     # Dependency("dlfcn_win32_jll"; compat="1.3.1", platforms=filter(Sys.iswindows, platforms)),
     Dependency("libaec_jll"; compat="1.0.6"), # This is the successor of szlib
 ]
