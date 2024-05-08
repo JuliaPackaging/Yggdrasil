68a579840cfcf06b1a50f8a65dbecd0ff5e9e5fb
Mosè Giordano <mose@gnu.org>
2024-05-08 10:47:26 +0200

    [Extrae] Rebuild fixing compat with PAPI

--- E/Extrae/build_tarballs.jl
+++ E/Extrae/build_tarballs.jl
@@ -62,19 +62,24 @@ make -j${nproc}
 make install
 """
 
-platforms = [
+non_cuda_platforms = [
     Platform("x86_64", "linux"; libc="glibc"),
-    Platform("powerpc64le", "linux"; libc="glibc"),
     Platform("aarch64", "linux"; libc="glibc"),
+    Platform("powerpc64le", "linux"; libc="glibc"),
 ]
-platforms = expand_cxxstring_abis(platforms)
+non_cuda_platforms = expand_cxxstring_abis(non_cuda_platforms)
+# (Almost) Same as PAPI
+cuda_platforms = CUDA.supported_platforms(; max_version=v"12.2")
+cuda_platforms = expand_cxxstring_abis(cuda_platforms)
 
-# some platforms need glibc 2.19+, because the default one is too old
-glibc_platforms = filter(platforms) do p
+# Concatenate the platforms _after_ the C++ string ABI expansion, otherwise the
+# `platform in non_cuda_platforms` test below is meaningless.
+all_platforms = [non_cuda_platforms; cuda_platforms]
+
+# Some platforms need glibc 2.19+, because the default one is too old
+glibc_platforms = filter(all_platforms) do p
     libc(p) == "glibc" && proc_family(p) in ("intel", "power")
 end
-
-cuda_versions_to_build = Any[v"11.4", v"12", nothing]
 
 products = [
     LibraryProduct("libseqtrace", :libseqtrace),
@@ -89,39 +94,37 @@ cuda_products = [
     LibraryProduct("libcudatrace", :libcudatrace, dont_dlopen=true),
 ]
 
-dependencies = BinaryBuilder.AbstractDependency[
+dependencies = [
     # `MADV_HUGEPAGE` and `MAP_HUGE_SHIFT` require glibc 2.19, but we only
-    # package glibc 2.17.
+    # package glibc 2.17 on some architectures.
     BuildDependency(PackageSpec(name = "Glibc_jll", version = v"2.19");
                     platforms=glibc_platforms),
-    Dependency("Binutils_jll"; compat="~2.39"),
+    Dependency("Binutils_jll"; compat="~2.41"),
     Dependency("LibUnwind_jll"),
-    Dependency("PAPI_jll"),
-    Dependency("XML2_jll"),
+    Dependency("PAPI_jll"; compat="~7.1"),
+    Dependency("XML2_jll"; compat="2.12.0"),
     RuntimeDependency("CUDA_Runtime_jll"),
 ]
 
-for cuda_version in cuda_versions_to_build, platform in platforms
-    # powerpc64le not supported until CUDA 11.0
-    if arch(platform) == "powerpc64le" && !isnothing(cuda_version) && cuda_version <= v"11.0"
-        continue
+for platform in all_platforms
+    # Only for the non-CUDA platforms, add the cuda=none tag, if necessary.
+    if platform in non_cuda_platforms && CUDA.is_supported(platform)
+        platform["cuda"] = "none"
     end
 
-    # TODO PAPI_jll 7.0.0+2 does not support CUDA on aarch64 (only x86_64, powerpc64le with glibc)
-    if arch(platform) == "aarch64" && !isnothing(cuda_version)
-        continue
+    should_build_platform(triplet(platform)) || continue
+
+    _dependencies = if !haskey(platform, "cuda") || platform["cuda"] == "none"
+        dependencies
+    else
+        [
+            dependencies;
+            BuildDependency(PackageSpec(name="CUDA_full_jll", version=CUDA.full_version(VersionNumber(platform["cuda"]))))
+        ]
     end
 
-    platform_tags = [Symbol(k) => v for (k, v) in tags(platform) if k ∉ ("arch", "os")]
-    augmented_platform = Platform(arch(platform), os(platform);
-        platform_tags...,
-        cuda=isnothing(cuda_version) ? "none" : CUDA.platform(cuda_version)
-    )
-    should_build_platform(triplet(augmented_platform)) || continue
-
-    if !isnothing(cuda_version)
-        push!(dependencies, BuildDependency(PackageSpec(name="CUDA_full_jll", version=CUDA.full_version(cuda_version))))
-    end
-
-    build_tarballs(ARGS, name, version, sources, script, [augmented_platform], products, dependencies; julia_compat="1.6", CUDA.augment)
+    build_tarballs(ARGS, name, version, sources, script, [platform],
+                   products, _dependencies; lazy_artifacts=true,
+                   julia_compat="1.6", augment_platform_block=CUDA.augment,
+                   preferred_gcc_version=v"5")
 end
