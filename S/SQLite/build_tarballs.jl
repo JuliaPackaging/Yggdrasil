99af9933aa0c113efd9108275080d4cbaa8103dc
Erik Schnetter <schnetter@gmail.com>
2024-05-10 12:13:12 -0400

    SQLite: New version 3.45.3 (#8555)
    * SQLite: New version 3.45.3
    
    * SQLite: Add Readline dependency
    
    * SQLite: Remove Readline dependency again

--- S/SQLite/build_tarballs.jl
+++ S/SQLite/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "SQLite"
-version = v"3.45.0"
+version = v"3.45.3"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://sqlite.org/2024/sqlite-autoconf-3450000.tar.gz",
-                  "72887d57a1d8f89f52be38ef84a6353ce8c3ed55ada7864eb944abd9a495e436"),
+    ArchiveSource("https://sqlite.org/2024/sqlite-autoconf-3450300.tar.gz",
+                  "b2809ca53124c19c60f42bf627736eae011afdcc205bb48270a5ee9a38191531"),
     FileSource("https://raw.githubusercontent.com/archlinux/svntogit-community/cf0a3337bd854104252dc1ff711e95cc8bc7ffb3/trunk/license.txt",
                "4e57d9ac979f1c9872e69799c2597eeef4c6ce7224f3ede0bf9dc8d217b1e65d";
                filename="LICENSE"),
@@ -16,7 +16,7 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/sqlite-autoconf-*/
+cd $WORKSPACE/srcdir/sqlite-autoconf-*
 
 # Use same flags as
 # https://github.com/archlinux/svntogit-packages/blob/packages/sqlite/trunk/PKGBUILD
@@ -47,7 +47,7 @@ install_license "${WORKSPACE}/srcdir/LICENSE"
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
