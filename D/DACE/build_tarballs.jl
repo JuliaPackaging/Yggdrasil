6935ebaf5aa1756f99286cefa65dc8de8d9e9592
Alberto Fossà <49210322+afossa@users.noreply.github.com>
2024-05-10 07:53:55 -0500

    [DACE] bump version to 0.2.0 (#8638)

--- D/DACE/build_tarballs.jl
+++ D/DACE/build_tarballs.jl
@@ -8,11 +8,11 @@ uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "DACE"
-version = v"0.1.0"
+version = v"0.2.0"
 
 # Collection of sources required to build DACE
 sources = [
-    GitSource("https://github.com/a-ev/dace.git", "9fe534f9b27c147a171bce1ad7dc8b4706a9457e"),
+    GitSource("https://github.com/a-ev/dace.git", "1d3b35beac7389feeef14364c0cc2f63541eff4e"),
     DirectorySource("./bundled"),
 ]
 
@@ -52,7 +52,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     BuildDependency(PackageSpec(name="libjulia_jll")),
-    Dependency("libcxxwrap_julia_jll"; compat = "~0.12.2"),
+    Dependency("libcxxwrap_julia_jll"; compat = "~0.12.3"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
