c0317b869ff3304c9b28ce7cd4aef3cda2e066be
Erik Schnetter <schnetter@gmail.com>
2024-05-10 18:57:29 -0400

    Pixman: Trigger CI

--- P/Pixman/build_tarballs.jl
+++ P/Pixman/build_tarballs.jl
@@ -51,4 +51,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"6", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", preferred_gcc_version=v"6")
