02063643c5cce6f4f3628e1bba387c863b60f4f7
Erik Schnetter <schnetter@gmail.com>
2024-05-14 14:08:01 -0400

    dav1d: Make meson more tolerant

--- D/dav1d/build_tarballs.jl
+++ D/dav1d/build_tarballs.jl
@@ -9,11 +9,16 @@ version = v"1.4.1"
 sources = [
     ArchiveSource("https://code.videolan.org/videolan/dav1d/-/archive/$(version)/dav1d-$(version).tar.bz2",
                   "ab02c6c72c69b2b24726251f028b7cb57d5b3659eeec9f67f6cecb2322b127d8"),
+    DirectorySource("bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/dav1d-*
+
+# Avoid problems with `-Werror=unused-command-line-argument`
+sed -i -e 's!/opt/bin/.*-clang!'${WORKSPACE}/srcdir/files/cc'!' ${MESON_TARGET_TOOLCHAIN}
+sed -i -e 's!/opt/bin/.*-clang[+][+]!'${WORKSPACE}/srcdir/files/c++'!' ${MESON_TARGET_TOOLCHAIN}
 
 mkdir build && cd build
 meson setup --cross-file="${MESON_TARGET_TOOLCHAIN}" --buildtype=release -Denable_tests=false ..
@@ -37,5 +42,6 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
+# Need at least GCC 6 for Atomics and to avoid ICEs
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               clang_use_lld=false, julia_compat="1.6", preferred_gcc_version=v"5")
+               clang_use_lld=false, julia_compat="1.6", preferred_gcc_version=v"6")
