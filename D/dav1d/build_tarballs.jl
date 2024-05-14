77196081f54520b8cff72b66f20087f2c30c8c29
Erik Schnetter <schnetter@gmail.com>
2024-05-14 13:35:34 -0400

    dav1d: Debug

--- D/dav1d/build_tarballs.jl
+++ D/dav1d/build_tarballs.jl
@@ -37,4 +37,9 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6",
+               #BAD 4
+               #GOOD 12
+               preferred_gcc_version=v"6",
+               )
