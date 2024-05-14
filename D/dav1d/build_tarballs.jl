4c030324a05f57a6a0e9d2104594d785c6d35be4
Erik Schnetter <schnetter@gmail.com>
2024-05-14 13:41:40 -0400

    dav1d: Use GCC 6

--- D/dav1d/build_tarballs.jl
+++ D/dav1d/build_tarballs.jl
@@ -38,8 +38,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6",
-               #BAD 4
-               #GOOD 12
-               preferred_gcc_version=v"6",
-               )
+               julia_compat="1.6", preferred_gcc_version=v"6")
