cd64153b688630f6c4e9e440c0526e0faeff1c74
Erik Schnetter <schnetter@gmail.com>
2024-05-16 17:01:57 -0400

    z5: Correct dependencies

--- Z/z5/build_tarballs.jl
+++ Z/z5/build_tarballs.jl
@@ -12,17 +12,9 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/z5
+cd ${WORKSPACE}/srcdir/z5
 
-# CMAKE_FLAGS=()
-# if [[ ${target} = arm-* ]]; then
-#    # Not even GCC 13 can compile for 32-bit ARM
-#    CMAKE_FLAGS+=(-DAOM_TARGET_CPU=generic)
-# fi
-
-# ${CMAKE_FLAGS[@]}
-
-cmake -B build-dir -G Ninja \
+cmake -B build -G Ninja \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
@@ -30,34 +22,34 @@ cmake -B build-dir -G Ninja \
     -DBUILD_Z5PY=OFF \
     -DWITH_BLOSC=ON \
     -DWITH_BZIP2=ON \
-    -DWITH_GCS=ON \
     -DWITH_LZ4=ON \
     -DWITH_MARRAY=ON \
     -DWITH_XZ=ON \
     -DWITH_ZLIB=ON
 # Other possible options:
+#     WITH_GCS
 #     WITH_S3
 
-cmake --build build-dir --parallel ${nproc}
-cmake --install build-dir
+cmake --build build --parallel ${nproc}
+cmake --install build
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-platforms = expand_cxxstring_abis(platforms)
+platforms = [AnyPlatform()]
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libz5", :libz5)
+    # This is a header-only library without any binary products
+    FileProduct("include/z5/z5.hxx", :z5)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency(PackageSpec(name="nlohman_json_jll", uuid="7c7c7bd4-5f1c-5db3-8b3f-fcf8282f06da")),
-    BuildDependency(PackageSpec(name="xsimd_jll", uuid="9e151094-6d0c-5a5c-8bf5-2fcad2e63db9")),
-    BuildDependency(PackageSpec(name="xtensor_jll", uuid="e692580d-a894-5e11-adde-c3d453d9283f")),
-    BuildDependency(PackageSpec(name="xtl_jll", uuid="ba385536-920d-53da-b5db-5183e89bfaf8")),
+    BuildDependency("nlohmann_json_jll"),
+    BuildDependency("xsimd_jll"),
+    BuildDependency("xtensor_jll"),
+    BuildDependency("xtl_jll"),
     Dependency("Blosc_jll"),
     Dependency("Bzip2_jll"),
     Dependency("Lz4_jll"),
