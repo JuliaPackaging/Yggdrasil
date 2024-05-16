56ed0da872c6583c3fbd7e77561203a22a109a5b
Erik Schnetter <schnetter@gmail.com>
2024-05-16 12:04:22 -0400

    xtl: Add some FileProducts

--- X/xtl/build_tarballs.jl
+++ X/xtl/build_tarballs.jl
@@ -27,12 +27,44 @@ platforms = [AnyPlatform()]
 
 # The products that we will ensure are always built
 products = Product[
-    # This is a header-only library without any products
+    # This is a header-only library without any binary products
+    FileProduct("include/xtl/xany.hpp", :xany),
+    FileProduct("include/xtl/xbase64.hpp", :xbase64),
+    FileProduct("include/xtl/xbasic_fixed_string.hpp", :xbasic_fixed_string),
+    FileProduct("include/xtl/xclosure.hpp", :xclosure),
+    FileProduct("include/xtl/xcompare.hpp", :xcompare),
+    FileProduct("include/xtl/xcomplex.hpp", :xcomplex),
+    FileProduct("include/xtl/xcomplex_sequence.hpp", :xcomplex_sequence),
+    FileProduct("include/xtl/xdynamic_bitset.hpp", :xdynamic_bitset),
+    FileProduct("include/xtl/xfunctional.hpp", :xfunctional),
+    FileProduct("include/xtl/xhalf_float.hpp", :xhalf_float),
+    FileProduct("include/xtl/xhalf_float_impl.hpp", :xhalf_float_impl),
+    FileProduct("include/xtl/xhash.hpp", :xhash),
+    FileProduct("include/xtl/xhierarchy_generator.hpp", :xhierarchy_generator),
+    FileProduct("include/xtl/xiterator_base.hpp", :xiterator_base),
+    FileProduct("include/xtl/xjson.hpp", :xjson),
+    FileProduct("include/xtl/xmasked_value.hpp", :xmasked_value),
+    FileProduct("include/xtl/xmasked_value_meta.hpp", :xmasked_value_meta),
+    FileProduct("include/xtl/xmeta_utils.hpp", :xmeta_utils),
+    FileProduct("include/xtl/xmultimethods.hpp", :xmultimethods),
+    FileProduct("include/xtl/xoptional.hpp", :xoptional),
+    FileProduct("include/xtl/xoptional_meta.hpp", :xoptional_meta),
+    FileProduct("include/xtl/xoptional_sequence.hpp", :xoptional_sequence),
+    FileProduct("include/xtl/xplatform.hpp", :xplatform),
+    FileProduct("include/xtl/xproxy_wrapper.hpp", :xproxy_wrapper),
+    FileProduct("include/xtl/xsequence.hpp", :xsequence),
+    FileProduct("include/xtl/xspan.hpp", :xspan),
+    FileProduct("include/xtl/xspan_impl.hpp", :xspan_impl),
+    FileProduct("include/xtl/xsystem.hpp", :xsystem),
+    FileProduct("include/xtl/xtl_config.hpp", :xtl_config),
+    FileProduct("include/xtl/xtype_traits.hpp", :xtype_traits),
+    FileProduct("include/xtl/xvariant.hpp", :xvariant),
+    FileProduct("include/xtl/xvariant_impl.hpp", :xvariant_impl),
+    FileProduct("include/xtl/xvisitor.hpp", :xvisitor),
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
-]
+dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
