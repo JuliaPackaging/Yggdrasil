803cd5ec574de2a985ab60f16df3b040b18e888f
Jan Weidner <jw3126@gmail.com>
2024-05-21 15:29:48 +0200

    Update M/manifold/build_tarballs.jl
    Co-authored-by: Ian McInerney <mcianster@gmail.com>

--- M/manifold/build_tarballs.jl
+++ M/manifold/build_tarballs.jl
@@ -49,6 +49,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
+    Dependency("Clipper2_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
