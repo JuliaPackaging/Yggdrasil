a8b3618b5691eedc9536512759d9c67c0c8616b2
Tim Besard <tim.besard@gmail.com>
2024-05-22 13:08:38 +0200

    CUDA: Update to CUDA 12.5. (#8736)

--- C/CUDA/CUDA_Runtime/build_tarballs.jl
+++ C/CUDA/CUDA_Runtime/build_tarballs.jl
@@ -7,14 +7,13 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "CUDA_Runtime"
-version = v"0.13.0"
+version = v"0.14.0"
 
 augment_platform_block = """
     $(read(joinpath(@__DIR__, "platform_augmentation.jl"), String))
     const cuda_toolkits = $(CUDA.cuda_full_versions)"""
 
 platforms = [Platform("x86_64", "linux"),
-             Platform("powerpc64le", "linux"),
              Platform("aarch64", "linux"; cuda_platform="jetson"),
              Platform("aarch64", "linux"; cuda_platform="sbsa"),
              Platform("x86_64", "windows")]
@@ -136,7 +135,7 @@ for version in CUDA.cuda_full_versions
 
         if arch(platform) == "aarch64"
             # CUDA 10.x: our CUDA 10.2 build recipe for arm64 only provides jetson binaries
-            if thisminor(version) == "10.2" && platform["cuda_platform"] != "jetson"
+            if thisminor(version) == v"10.2" && platform["cuda_platform"] != "jetson"
                 continue
             end
 
@@ -153,14 +152,14 @@ for version in CUDA.cuda_full_versions
 
         if Base.thisminor(version) == v"10.2"
             push!(builds,
-                (; dependencies=[Dependency("CUDA_Driver_jll"; compat="0.8"),
+                (; dependencies=[Dependency("CUDA_Driver_jll"; compat="0.9"),
                                  BuildDependency(PackageSpec(name="CUDA_SDK_jll", version=v"10.2.89"))],
                    script=get_script(), platforms=[augmented_platform], products=get_products(platform),
                    sources=[]
             ))
         else
             push!(builds,
-                (; dependencies=[Dependency("CUDA_Driver_jll"; compat="0.8")],
+                (; dependencies=[Dependency("CUDA_Driver_jll"; compat="0.9")],
                    script, platforms=[augmented_platform], products=get_products(platform),
                    sources=get_sources("cuda", components; version, platform)
             ))
