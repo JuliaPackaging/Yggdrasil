fc8fe21caae2a45c43ad122ca342a96fdbc63af0
Tim Besard <tim.besard@gmail.com>
2024-05-22 10:20:37 +0200

    CUDA: Upgrade driver to v12.5. (#8735)

--- C/CUDA/CUDA_Driver/build_tarballs.jl
+++ C/CUDA/CUDA_Driver/build_tarballs.jl
@@ -9,24 +9,20 @@ using BinaryBuilder, Pkg
 include("../../../fancy_toys.jl")
 
 name = "CUDA_Driver"
-version = v"0.8.1"
+version = v"0.9.0"
 
-cuda_version = v"12.4"
+cuda_version = v"12.5"
 cuda_version_str = "$(cuda_version.major)-$(cuda_version.minor)"
-driver_version_str = "550.54.15"
+driver_version_str = "555.42.02"
 build = 1
 
 sources_linux_x86 = [
     FileSource("https://developer.download.nvidia.com/compute/cuda/repos/rhel8/x86_64/cuda-compat-$(cuda_version_str)-$(driver_version_str)-$(build).x86_64.rpm",
-               "3e2727fb76285978ac4a1e0c8b7ca5d88ece4bf124dfb2a3d0a486829bf315d3", "compat.rpm")
-]
-sources_linux_ppc64le = [
-    FileSource("https://developer.download.nvidia.com/compute/cuda/repos/rhel8/ppc64le/cuda-compat-$(cuda_version_str)-$(driver_version_str)-$(build).ppc64le.rpm",
-               "1d487161589b3d91a3648ca271046fa451072ceddb070e4aaeb7ed377d990b98", "compat.rpm")
+               "b5f70dfd2149ef41a3decda1a398b805430d7129e487f17e9a4f154384be660e", "compat.rpm")
 ]
 sources_linux_aarch64 = [
     FileSource("https://developer.download.nvidia.com/compute/cuda/repos/rhel8/sbsa/cuda-compat-$(cuda_version_str)-$(driver_version_str)-$(build).aarch64.rpm",
-               "bafe0831dd92a5ba47d790709f0d642d4f30cf2ade5d32371d012ae09b5c2512", "compat.rpm")
+               "bce9fe958f8e0811bb5cc40cc0235de8453c6a4e89b12e8c0458d78768af908a", "compat.rpm")
 ]
 
 dependencies = []
@@ -69,12 +65,6 @@ non_reg_ARGS = filter(arg -> arg != "--register", ARGS)
 if should_build_platform("x86_64-linux-gnu")
     build_tarballs(non_reg_ARGS, name, version, sources_linux_x86, script,
                    [Platform("x86_64", "linux")], products, dependencies;
-                   lazy_artifacts=true, skip_audit=true, init_block)
-end
-
-if should_build_platform("powerpc64le-linux-gnu")
-    build_tarballs(non_reg_ARGS, name, version, sources_linux_ppc64le, script,
-                   [Platform("powerpc64le", "linux")], products, dependencies;
                    lazy_artifacts=true, skip_audit=true, init_block)
 end
 
