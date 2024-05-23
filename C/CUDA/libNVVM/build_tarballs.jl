ac48add11cd221511eecce946e786d7303c26a3a
Tim Besard <tim.besard@gmail.com>
2024-05-23 12:52:52 +0200

    CUDA: Bump NVVM to CUDA 12.5.

--- C/CUDA/libNVVM/build_tarballs.jl
+++ C/CUDA/libNVVM/build_tarballs.jl
@@ -6,8 +6,8 @@ const YGGDRASIL_DIR = "../../.."
 include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "libNVVM"
-version = v"4.0.2"
-cuda_version = v"12.4.1"
+version = v"4.0.3"
+cuda_version = v"12.5.0"
 
 script = raw"""
 cd ${WORKSPACE}/srcdir/cuda_nvcc-*
