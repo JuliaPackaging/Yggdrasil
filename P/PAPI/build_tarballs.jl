ebb4248549cb1c1e94561641b82f7c07a18406bd
Erik Schnetter <schnetter@gmail.com>
2024-05-24 18:55:09 -0400

    PAPI: Enable additional platforms

--- P/PAPI/build_tarballs.jl
+++ P/PAPI/build_tarballs.jl
@@ -63,16 +63,19 @@ make -j ${nproc}
 make install
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
+# These are the platforms we will build for by default
 platforms = [
     Platform("x86_64", "linux"; libc = "glibc"),
+    Platform("i686", "linux"; libc = "glibc"),
     Platform("aarch64", "linux"; libc = "glibc"),
+    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
     Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
     Platform("powerpc64le", "linux"; libc = "glibc"),
     Platform("x86_64", "linux"; libc = "musl"),
+    Platform("i686", "linux"; libc = "musl"),
     Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl")
+    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
+    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
 ]
 
 
