d81152b2ca154d06d26c8855d1807692d4276bdb
Yupei Qi <qiyupei@gmail.com>
2024-05-27 01:41:38 +0900

    apply suggestion
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- L/libCppInterOp/build_tarballs.jl
+++ L/libCppInterOp/build_tarballs.jl
@@ -33,7 +33,7 @@ install_license ../LICENSE.txt
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
+platforms = expand_cxxstring_abis(supported_platforms())
 filter!(p -> libc(p) != "musl", platforms) # LLVM_full+asserts isn't available for musl
 
 augment_platform_block = """
