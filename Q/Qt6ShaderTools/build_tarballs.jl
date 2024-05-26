386f38a3b5f66bce4c54b42e1691956061695204
Bart Janssens <bart@bartjanssens.org>
2024-05-26 23:16:45 +0200

    [Qt6ShaderTools] Version 6.7.1 (targets build) (#8771)
    * [Qt6ShaderTools] Version 6.7.1 (targets build)
    
    * Fix macOS builds

--- Q/Qt6ShaderTools/build_tarballs.jl
+++ Q/Qt6ShaderTools/build_tarballs.jl
@@ -8,7 +8,7 @@ version = v"6.7.1"
 # Set this to true first when updating the version. It will build only for the host (linux musl).
 # After that JLL is in the registyry, set this to false to build for the other platforms, using
 # this same package as host build dependency.
-const host_build = true
+const host_build = false
 
 # Collection of sources required to build qt6
 sources = [
@@ -56,7 +56,7 @@ case "$bb_full_target" in
     ;;
 
     *)
-        cmake -DQT_HOST_PATH=$host_prefix -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_FIND_ROOT_PATH=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release $qtsrcdir
+        cmake -G Ninja -DQT_HOST_PATH=$host_prefix -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_FIND_ROOT_PATH=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DQT_NO_APPLE_SDK_AND_XCODE_CHECK=ON -DCMAKE_BUILD_TYPE=Release $qtsrcdir
     ;;
 
 esac
