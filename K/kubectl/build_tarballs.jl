8b932601251f5c378d0fc6c2aff3941a31f0ffc5
Curtis Vogt <curtis.vogt@gmail.com>
2024-05-28 16:48:14 -0500

    [kubectl_jll] Build version v1.30.1

--- K/kubectl/build_tarballs.jl
+++ K/kubectl/build_tarballs.jl
@@ -1,16 +1,16 @@
 using BinaryBuilder
 
 name = "kubectl"
-version = v"1.29.5"
+version = v"1.30.1"
 
 # Release history can be found at: https://kubernetes.io/releases/
 # Links to downloads can found at: https://kubernetes.io/docs/tasks/tools/
 sources = [
-    FileSource("https://dl.k8s.io/release/v$(version)/bin/linux/amd64/kubectl", "603c8681fc0d8609c851f9cc58bcf55eeb97e2934896e858d0232aa8d1138366"; filename="x86_64-linux-gnu-kubectl"),
-    FileSource("https://dl.k8s.io/release/v$(version)/bin/linux/arm64/kubectl", "9ee9168def12ac6a6c0c6430e0f73175e756ed262db6040f8aa2121ad2c1f62e"; filename="aarch64-linux-gnu-kubectl"),
-    FileSource("https://dl.k8s.io/release/v$(version)/bin/darwin/amd64/kubectl", "395082ef84594ea4cb170d599056406ed2cf39555b53e92e0caee013c1ed5cdf"; filename="x86_64-apple-darwin14-kubectl"),
-    FileSource("https://dl.k8s.io/release/v$(version)/bin/darwin/arm64/kubectl", "23b09c126c0a0b71b58cc725a32cf84f1753242b3892dfd762511f2da6cce165"; filename="aarch64-apple-darwin20-kubectl"),
-    FileSource("https://dl.k8s.io/release/v$(version)/bin/windows/amd64/kubectl.exe", "8de419ccecdde90172345e7d12a63de42c217d28768d84c2398d932b44d73489"; filename="x86_64-w64-mingw32-kubectl"),
+    FileSource("https://dl.k8s.io/release/v$(version)/bin/linux/amd64/kubectl", "5b86f0b06e1a5ba6f8f00e2b01e8ed39407729c4990aeda961f83a586f975e8a"; filename="x86_64-linux-gnu-kubectl"),
+    FileSource("https://dl.k8s.io/release/v$(version)/bin/linux/arm64/kubectl", "d90446719b815e3abfe7b2c46ddf8b3fda17599f03ab370d6e47b1580c0e869e"; filename="aarch64-linux-gnu-kubectl"),
+    FileSource("https://dl.k8s.io/release/v$(version)/bin/darwin/amd64/kubectl", "eaefb69cf908b7473d2dce0ba894c956b7e1ad5a4987a96d68a279f5597bb22d"; filename="x86_64-apple-darwin14-kubectl"),
+    FileSource("https://dl.k8s.io/release/v$(version)/bin/darwin/arm64/kubectl", "55dec3c52702bd68488a5c1ab840b79ea9e73e4b9f597bcf75b201c55d0bd280"; filename="aarch64-apple-darwin20-kubectl"),
+    FileSource("https://dl.k8s.io/release/v$(version)/bin/windows/amd64/kubectl.exe", "f7391a2de0491caadedb5178ac2485cbf104189b2e0f3d6c577bd6ea1892898f"; filename="x86_64-w64-mingw32-kubectl"),
     FileSource("https://raw.githubusercontent.com/kubernetes/kubernetes/v$(version)/LICENSE", "cfc7749b96f63bd31c3c42b5c471bf756814053e847c10f3eb003417bc523d30")
 ]
 
