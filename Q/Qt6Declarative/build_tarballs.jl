969ff1ad02fcb4a3770b45facaf10dfd0f15fc53
Bart Janssens <bart@bartjanssens.org>
2024-05-30 00:33:41 +0200

    [Qt6Declarative] Version 6.7.1 (targets) (#8804)

--- Q/Qt6Declarative/build_tarballs.jl
+++ Q/Qt6Declarative/build_tarballs.jl
@@ -8,7 +8,7 @@ version = v"6.7.1"
 # Set this to true first when updating the version. It will build only for the host (linux musl).
 # After that JLL is in the registyry, set this to false to build for the other platforms, using
 # this same package as host build dependency.
-const host_build = true
+const host_build = false
 
 # Collection of sources required to build qt6
 sources = [
@@ -67,13 +67,14 @@ case "$bb_full_target" in
     *apple-darwin*)
         apple_sdk_root=$WORKSPACE/srcdir/MacOSX13.3.sdk
         sed -i "s!/opt/x86_64-apple-darwin14/x86_64-apple-darwin14/sys-root!$apple_sdk_root!" $CMAKE_TARGET_TOOLCHAIN
-        cmake -DQT_HOST_PATH=$host_prefix \
+        cmake -G Ninja -DQT_HOST_PATH=$host_prefix \
         -DPython_ROOT_DIR=/usr \
         -DCMAKE_INSTALL_PREFIX=${prefix} \
         -DCMAKE_PREFIX_PATH=$host_prefix \
         -DCMAKE_FIND_ROOT_PATH=$prefix \
         -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
         -DCMAKE_SYSROOT=$apple_sdk_root -DCMAKE_FRAMEWORK_PATH=$apple_sdk_root/System/Library/Frameworks -DCMAKE_OSX_DEPLOYMENT_TARGET=11 \
+        -DQT_NO_APPLE_SDK_AND_XCODE_CHECK=ON \
         -DCMAKE_BUILD_TYPE=Release \
         $qtsrcdir
     ;;
