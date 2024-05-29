a91a151e92620ec5ba58e89e78340266d76f967f
Ryan Curtin <ryan@ratml.org>
2024-05-29 08:43:39 -0400

    Bump to GCC 8 for C++17 support.

--- M/mlpack/build_tarballs.jl
+++ M/mlpack/build_tarballs.jl
@@ -187,4 +187,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7", julia_compat="1.7")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8", julia_compat="1.7")
