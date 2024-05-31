db436efaebe2c39ea5205f378f25d450d0d987ff
Jacob Quinn <quinn.jacobd@gmail.com>
2024-05-30 20:23:44 -0600

    Use gcc 8 for aws-lc to re-enable 32-bit (#8818)

--- A/aws_lc/build_tarballs.jl
+++ A/aws_lc/build_tarballs.jl
@@ -40,7 +40,7 @@ ninja install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 # Disable 32-bit because it's too much for the time being
-platforms = expand_cxxstring_abis(supported_platforms(; exclude=p->Sys.iswindows(p) || Sys.isapple(p) || arch(p) == "i686"))
+platforms = expand_cxxstring_abis(supported_platforms(; exclude=p->Sys.iswindows(p) || Sys.isapple(p)))
 
 # The products that we will ensure are always built
 products = [
@@ -57,4 +57,4 @@ dependencies = Dependency[
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"7")
+               julia_compat="1.6", preferred_gcc_version=v"8")
