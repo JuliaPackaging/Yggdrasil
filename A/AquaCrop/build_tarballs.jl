9709e7bc5b0a73bd34f5156e98f56c04c5ddef6d
Marco Matthies <71844+marcom@users.noreply.github.com>
2024-06-06 18:19:52 +0200

    [AquaCrop] Add version v7.1

---
+++ A/AquaCrop/build_tarballs.jl
@@ -0,0 +1,40 @@
+using BinaryBuilder, Pkg
+
+name = "AquaCrop"
+version = v"7.1"
+
+# url = "https://github.com/KUL-RSDA/AquaCrop"
+# description = "FAO AquaCrop model of plant growth"
+
+sources = [
+    GitSource("https://github.com/KUL-RSDA/AquaCrop",
+              "33ae89706ff82c2c119930e47899cd3fad519d6f"),
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir/AquaCrop*/src/
+
+sed -i -e 's/-march=native//g' Makefile
+
+export FC
+make -j${nproc}
+
+install -Dvm 755 aquacrop${exeext} "${bindir}"/aquacrop${exeext}
+install -Dvm 755 libaquacrop.${dlext} "${libdir}"/libaquacrop.${dlext}
+
+install_license ../LICENSE
+"""
+
+# we link against libgfortran
+platforms = expand_gfortran_versions(supported_platforms())
+
+products = [
+    ExecutableProduct("aquacrop", :aquacrop),
+    LibraryProduct("libaquacrop", :libaquacrop),
+]
+
+dependencies = Dependency[
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6")
