d67f7f9ddfde032191b19f254ec4d9120a72c2c9
Bart Janssens <bart@bartjanssens.org>
2024-06-09 17:29:56 +0200

    [libcxxwrap-julia] Version 0.13

--- L/libcxxwrap_julia/build_tarballs.jl
+++ L/libcxxwrap_julia/build_tarballs.jl
@@ -11,13 +11,13 @@ delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 include("../../L/libjulia/common.jl")
 
 name = "libcxxwrap_julia"
-version = v"0.12.5"
+version = v"0.13.0"
 
 git_repo = "https://github.com/JuliaInterop/libcxxwrap-julia.git"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource(git_repo, "93768c16d1beec0734dc951917e84372888d4422"),
+    GitSource(git_repo, "b00e22b6b703be86d9be710f530107eae9b7401a"),
 ]
 
 # Bash recipe for building across all platforms
