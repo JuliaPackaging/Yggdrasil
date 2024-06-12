b79cc12262b71ec2399eda2635fbd892d1263726
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-06-12 17:24:40 +0200

    Disable binaries

--- S/StableHLO/build_tarballs.jl
+++ S/StableHLO/build_tarballs.jl
@@ -23,36 +23,36 @@ cd $WORKSPACE/srcdir/stablehlo
 # ln -s ${host_prefix}/bin/mlir-tblgen ${bindir}/mlir-tblgen
 # ln -s ${host_prefix}/bin/mlir-pdll ${bindir}/mlir-pdll
 
-CMAKE_FLAGS=()
-CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix})
-CMAKE_FLAGS+=(-DCMAKE_BUILD_TYPE=Release)
-CMAKE_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN})
-CMAKE_FLAGS+=(-DCMAKE_CROSSCOMPILING:BOOL=ON)
+# CMAKE_FLAGS=()
+# CMAKE_FLAGS+=(-DCMAKE_INSTALL_PREFIX=${prefix})
+# CMAKE_FLAGS+=(-DCMAKE_BUILD_TYPE=Release)
+# CMAKE_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN})
+# CMAKE_FLAGS+=(-DCMAKE_CROSSCOMPILING:BOOL=ON)
 
-CMAKE_FLAGS+=(-DLLVM_DIR=${prefix}/lib/cmake/llvm)
-CMAKE_FLAGS+=(-DLLVM_ENABLE_ASSERTIONS=OFF)
-CMAKE_FLAGS+=(-DLLVM_LINK_LLVM_DYLIB=ON)
-CMAKE_FLAGS+=(-DLLVM_ENABLE_LLD="OFF")
+# CMAKE_FLAGS+=(-DLLVM_DIR=${prefix}/lib/cmake/llvm)
+# CMAKE_FLAGS+=(-DLLVM_ENABLE_ASSERTIONS=OFF)
+# CMAKE_FLAGS+=(-DLLVM_LINK_LLVM_DYLIB=ON)
+# CMAKE_FLAGS+=(-DLLVM_ENABLE_LLD="OFF")
 
-CMAKE_FLAGS+=(-DMLIR_DIR=${prefix}/lib/cmake/mlir)
+# CMAKE_FLAGS+=(-DMLIR_DIR=${prefix}/lib/cmake/mlir)
 
-# building shared libraries crashes on linking, so build static libraries and link them manually
-CMAKE_FLAGS+=(-DBUILD_SHARED_LIBS=OFF)
+# # building shared libraries crashes on linking, so build static libraries and link them manually
+# CMAKE_FLAGS+=(-DBUILD_SHARED_LIBS=OFF)
 
-cmake -B build -S . -GNinja ${CMAKE_FLAGS[@]}
-ninja -C build -j 1 all
+# cmake -B build -S . -GNinja ${CMAKE_FLAGS[@]}
+# ninja -C build -j 1 all
 
-# NOTE no idea why they are not installed by default
-install -v build/bin/stablehlo-opt build/bin/stablehlo-translate build/bin/stablehlo-lsp-server ${bindir}
+# # NOTE no idea why they are not installed by default
+# install -v build/bin/stablehlo-opt build/bin/stablehlo-translate build/bin/stablehlo-lsp-server ${bindir}
 
-# build shared library from static libraries
-cd build/lib
-for i in *.a; do
-    echo $i
-    ar x $i
-done
-c++ -shared -o libStablehlo.${dlext} -lLLVM -lMLIR *.cpp.o
-install -v libStablehlo.${dlext} ${libdir}
+# # build shared library from static libraries
+# cd build/lib
+# for i in *.a; do
+#     echo $i
+#     ar x $i
+# done
+# c++ -shared -o libStablehlo.${dlext} -lLLVM -lMLIR *.cpp.o
+# install -v libStablehlo.${dlext} ${libdir}
 
 # generate Julia bindings of the HLO dialects
 mkdir -p ${prefix}/share/stablehlo_jll/
@@ -85,10 +85,10 @@ filter!(!Sys.iswindows, platforms)
 platforms = expand_cxxstring_abis(platforms)
 
 products = [
-    ExecutableProduct("stablehlo-opt", :stablehlo_opt),
-    ExecutableProduct("stablehlo-translate", :stablehlo_translate),
-    ExecutableProduct("stablehlo-lsp-server", :stablehlo_lsp_server),
-    LibraryProduct("libStablehlo", :libStablehlo),
+    # ExecutableProduct("stablehlo-opt", :stablehlo_opt),
+    # ExecutableProduct("stablehlo-translate", :stablehlo_translate),
+    # ExecutableProduct("stablehlo-lsp-server", :stablehlo_lsp_server),
+    # LibraryProduct("libStablehlo", :libStablehlo),
     FileProduct("share/stablehlo_jll/stablehlo.inc.jl", :stablehlo_inc_jl),
     FileProduct("share/stablehlo_jll/chlo.inc.jl", :chlo_inc_jl),
     FileProduct("share/stablehlo_jll/vhlo.inc.jl", :vhlo_inc_jl),
@@ -105,8 +105,8 @@ builds = []
 for llvm_version in llvm_versions
 
     dependencies = [
-        Dependency("MLIR_jll", llvm_version),
-        BuildDependency(PackageSpec(name="LLVM_full_jll", version=llvm_version)),
+        # Dependency("MLIR_jll", llvm_version),
+        # BuildDependency(PackageSpec(name="LLVM_full_jll", version=llvm_version)),
         HostBuildDependency(PackageSpec(name="MLIR_jll", version=llvm_version)),
         HostBuildDependency(PackageSpec(name="mlir_jl_tblgen_jll", version=v"0.0.8+0")),
     ]
