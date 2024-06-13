9f617703361a1f96dded525614742b86262020f6
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2024-06-13 16:04:39 +0200

    ADOLC: remove superfluous empty line
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- A/ADOLC/build_tarballs.jl
+++ A/ADOLC/build_tarballs.jl
@@ -77,5 +77,3 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"8", preferred_llvm_version = v"11.0.1")
-
-
