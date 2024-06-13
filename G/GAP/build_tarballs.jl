0fdb3fc0e13cec2df919fa6fd661bc6f83fee1ac
Max Horn <max@quendi.de>
2024-06-13 16:03:19 +0200

    [GAP] version 4.13.1
    Also fix GAC_LDFLAGS on macOS, drop a redundant filter!

--- G/GAP/build_tarballs.jl
+++ G/GAP/build_tarballs.jl
@@ -26,13 +26,13 @@ uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "GAP"
-upstream_version = v"4.13.0"
-version = v"400.1300.000"
+upstream_version = v"4.13.1"
+version = v"400.1300.100"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://github.com/gap-system/gap/releases/download/v$(upstream_version)/gap-$(upstream_version)-core.tar.gz",
-                  "6e6433b56c43ac4b2dab098bfb146dae1cb0dab62ae48a1a2144354af239c121"),
+                  "3bd0b5e52ea6984c22d6f6003b2a5805a843659ddbfd8da6ee50609338703451"),
     DirectorySource("./bundled"),
 ]
 
@@ -64,7 +64,7 @@ julia_version=$(./julia_version)
 # configure GAP
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} \
     JULIA_VERSION="$julia_version" \
-    CPPFLAGS="$CPPFLAGS -DUSE_GAP_INSIDE_JULIA=1" \
+    CPPFLAGS="$CPPFLAGS -DUSE_GAP_INSIDE_JULIA=1 -DREQUIRE_PRECISE_MARKING=1" \
     --with-gmp=${prefix} \
     --with-readline=${prefix} \
     --with-zlib=${prefix} \
@@ -110,7 +110,6 @@ install_license LICENSE
 
 include("../../L/libjulia/common.jl")
 platforms = vcat(libjulia_platforms.(julia_versions)...)
-filter!(!Sys.iswindows, platforms)
 
 # we only care about 64bit builds
 filter!(p -> nbits(p) == 64, platforms)
