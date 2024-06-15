f9c584edff264ede0e76d896fbc5bfd59963e1a5
Jeremiah Lewis <4462211+jeremiahpslewis@users.noreply.github.com>
2024-06-15 21:14:45 +0000

    Fix zlib?

--- O/OSRM/build_tarballs.jl
+++ O/OSRM/build_tarballs.jl
@@ -54,8 +54,7 @@ CMAKE_FLAGS+=(-DENABLE_CCACHE=OFF)
 CMAKE_FLAGS+=(-Wno-dev)
 CMAKE_FLAGS+=(-DENABLE_MASON=OFF)
 CMAKE_FLAGS+=(-DZLIB_INCLUDE_DIRS=${includedir})
-CMAKE_FLAGS+=(-DZLIB_LIBRARY="${libdir}/libz.${dlext}")
-CMAKE_FLAGS+=(-DZLIB_LIBRARIES="${libdir}/libz.${dlext}")
+CMAKE_FLAGS+=(-DZLIB_LIBRARY=${libdir}/libz.${dlext})
 
 if [[ ${target} == *mingw* ]]; then
     CMAKE_FLAGS+=(-DLUA_INCLUDE_DIR=${includedir})
