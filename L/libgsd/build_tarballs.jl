4f43a80cda17c2a97b4da0706bcd7f5edc5bb3ab
ywitzky <134135819+ywitzky@users.noreply.github.com>
2024-06-18 15:41:31 +0200

    Update build_tarballs.jl
    Accepting the change request for the pull request

--- L/libgsd/build_tarballs.jl
+++ L/libgsd/build_tarballs.jl
@@ -7,12 +7,11 @@ version = v"3.2.1"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/glotzerlab/gsd/releases/download/v$(version)/gsd-$(version).tar.gz", "49a0e16276789b6953ece7e5a678bd22c128f8cc57cb967f4228ff6487770995")
+    GitSource("https://github.com/glotzerlab/gsd/releases/download/v$(version)/gsd-$(version).tar.gz", "49a0e16276789b6953ece7e5a678bd22c128f8cc57cb967f4228ff6487770995")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
 cd ${WORKSPACE}/srcdir/gsd-*
 mkdir -p "${libdir}"
 ${CC} -std=c99 -fPIC gsd/gsd.c -shared -o "${libdir}/libgsd.${dlext}"
@@ -20,22 +19,9 @@ ${CC} -std=c99 -fPIC gsd/gsd.c -shared -o "${libdir}/libgsd.${dlext}"
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("x86_64", "macos"; ),
-    Platform("aarch64", "macos"; ),
-    Platform("x86_64", "freebsd"; )
-]
+platforms = supported_platforms()
+# Windows cant be build since the source code implicitly requires commands from unistd.h which only exist for unix and unix-like systems
+platforms = filter!(!Sys.iswindows, platforms)
 
 
 # The products that we will ensure are always built
