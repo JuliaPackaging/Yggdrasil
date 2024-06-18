de75ce99f5c552862063d0a3cf79d37f43a80333
NicolasRiel <a.nicolas.riel@gmail.com>
2024-06-18 23:41:59 +0200

    MAGEMin v1.4.9

--- M/MAGEMin/build_tarballs.jl
+++ M/MAGEMin/build_tarballs.jl
@@ -6,13 +6,13 @@ const YGGDRASIL_DIR = "../.."
 include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 
 name = "MAGEMin"
-version = v"1.4.8"
+version = v"1.4.9"
 
 MPItrampoline_compat_version="5.2.1"  
 
 # Collection of sources required to complete build
 sources = [GitSource("https://github.com/ComputationalThermodynamics/MAGEMin", 
-                    "0c96d794aa2db79c2f77167b5e17b469bda3bb5c")                 ]
+                    "f35c559ac24accb1462572681cc92e62adae831d")                 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
