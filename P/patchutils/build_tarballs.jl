71f735f7791233f11a74bec6df52f8e09bfd2c62
Elliot Saba <staticfloat@gmail.com>
2024-06-19 13:32:35 -0700

    [patchutils] Package more executable products

--- P/patchutils/build_tarballs.jl
+++ P/patchutils/build_tarballs.jl
@@ -20,8 +20,11 @@ make install
 
 # Windows needs some help
 platforms = filter(!Sys.iswindows, supported_platforms())
-products = [
-    ExecutableProduct("lsdiff", :lsdiff),
+exe_names = [
+    "combine", "dehtml", "edit", "esp", "filter",
+    "flip", "git", "grep", "ls", "inter", "recount",
+    "re", "split", "svn", "unwrap",
 ]
+products = [ExecutableProduct(string(name,"diff"), Symbol(string(name,"diff"))) for name in exe_names]
 dependencies = Dependency[]
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
