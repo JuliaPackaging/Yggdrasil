216d00172e879dfaa411fef61c750138fb289dbc
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-06-20 16:00:23 +0200

    Fixup

--- S/strobealign/build_tarballs.jl
+++ S/strobealign/build_tarballs.jl
@@ -36,4 +36,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"11", julia_compat="1.6")
