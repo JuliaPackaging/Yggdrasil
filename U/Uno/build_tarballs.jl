72a15348f13ed36c4b4302abcb632dbbb8e78662
Oscar Dowson <odow@users.noreply.github.com>
2024-06-22 09:30:27 +1200

    Apply suggestions from code review
    Co-authored-by: Ian McInerney <mcianster@gmail.com>

--- U/Uno/build_tarballs.jl
+++ U/Uno/build_tarballs.jl
@@ -22,6 +22,7 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
+    -DBLA_VENDOR="libblastrampoline" \
     ..
 
 make -j${nproc}
@@ -37,7 +38,14 @@ products = [
 dependencies = [
     Dependency("ASL_jll", compat="0.1.3"),
     Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"), compat="5.4.0"),
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
+    # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
+    Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
+
+    # We need at least 3.29 (Ygg version), or 3.30 upstream version
+    # for LBT support, so always pull the most recent CMake version.
+    HostBuildDependency(PackageSpec(; name="CMake_jll")),
 ]
 
 build_tarballs(
