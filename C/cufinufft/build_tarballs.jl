04aa213fafc23d0dca979ead0d09631dada959d5
Ludvig af Klinteberg <ludvigak@users.noreply.github.com>
2024-06-26 12:09:33 +0200

    Update C/cufinufft/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- C/cufinufft/build_tarballs.jl
+++ C/cufinufft/build_tarballs.jl
@@ -46,7 +46,7 @@ cmake --install .
 # Build for all supported CUDA > v11
 platforms = expand_cxxstring_abis(CUDA.supported_platforms(min_version=v"11.0"))
 # Cmake toolchain breaks on aarch64, so only x86_64 for now
-filter!(p -> p["arch"]=="x86_64", platforms)
+filter!(p -> arch(p)=="x86_64", platforms)
 
 # The products that we will ensure are always built
 products = [
