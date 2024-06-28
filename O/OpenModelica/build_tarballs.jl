3aabedfc9b24077a8ad1e0b888ffbb2891d83faa
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-06-28 15:46:14 -0400

    Update build_tarballs.jl

--- O/OpenModelica/build_tarballs.jl
+++ O/OpenModelica/build_tarballs.jl
@@ -52,4 +52,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; lazy_artifacts=true, julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; lazy_artifacts=true, julia_compat="1.10")
