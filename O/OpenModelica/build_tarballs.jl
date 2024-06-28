b9389a0d51c499a3f468d563fa3f131b756ce183
Viral B. Shah <viral@mayin.org>
2024-06-28 19:33:19 +0000

    update

--- O/OpenModelica/build_tarballs.jl
+++ O/OpenModelica/build_tarballs.jl
@@ -13,8 +13,14 @@ script = raw"""
 cd $WORKSPACE/srcdir
 mkdir -p "${libdir}"
 
+apk add openjdk17
+
 cd OpenModelica
-cmake -S . -B build_cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
+cmake -S . -B build_cmake -DCMAKE_INSTALL_PREFIX=$prefix \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=Release \
+      -DBLAS_LIBRARIES="${libdir}/libblastrampoline.${dlext}" \
+      -DOM_ENABLE_GUI_CLIENTS=OFF 
 cmake --build build_cmake --parallel 10 --target install
 
 install_license OSMC-license.txt
@@ -23,7 +29,7 @@ install_license OSMC-license.txt
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = [
-    Platform("x86_64", "linux"; libc="glibc"),
+    Platform("x86_64", "linux"; libc="glibc", cxxstring_abi="cxx11"),
 ]
 
 # The products that we will ensure are always built
@@ -35,8 +41,14 @@ products = [
 dependencies = [
     Dependency("CompilerSupportLibraries_jll"),
     Dependency("libblastrampoline_jll"),
+    Dependency("Ipopt_jll"),
+    Dependency("LibCURL_jll"),
+    Dependency("util_linux_jll"),
+    Dependency("OpenCL_jll"),
     BuildDependency("flex_jll"),
     BuildDependency("boost_jll"),
+    BuildDependency("LLVMOpenMP_jll"),
+    BuildDependency("OpenCL_Headers_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
