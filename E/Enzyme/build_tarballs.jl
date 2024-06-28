a0b9eeebeb009a6baa4f34447da44f0549e1cc75
William Moses <gh@wsmoses.com>
2024-06-29 00:36:47 +0200

    Enzyme: is third time the charm?
    @vchuravy @giordano @staticfloat 
    
    I'm doing nothing differently here, but I can update the commit and maybe it'll be happy after some time off

--- E/Enzyme/build_tarballs.jl
+++ E/Enzyme/build_tarballs.jl
@@ -15,7 +15,7 @@ llvm_versions = [v"11.0.1", v"12.0.1", v"13.0.1", v"14.0.2", v"15.0.7", v"16.0.6
 
 # Collection of sources required to build attr
 sources = [
-    GitSource(repo, "f3c1fda941bf8acf8dbc985920df28fd27b9c024"),
+    GitSource(repo, "3a12490a3a07b1d0828007daa60e57a874a81480"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
                   "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f"),
 ]
