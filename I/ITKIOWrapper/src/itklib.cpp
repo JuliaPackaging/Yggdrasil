e4b3b594675c81e06820c150f5c4d7fa637b9485
hurtbadly <divita2004@gmail.com>
2024-06-29 23:05:44 +0530

    [ITKIOWrapper-1.0.0] Refined wrapper c++ script with generalized function names

--- I/ITKIOWrapper/src/itklib.cpp
+++ I/ITKIOWrapper/src/itklib.cpp
@@ -1,18 +1,17 @@
 #include "jlcxx/jlcxx.hpp"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "itkNiftiImageIO.h"
 
 using ImageType = itk::Image<float, 3>;
 
-ImageType::Pointer read_nifti_image(const std::string& filename) {
+ImageType::Pointer loadImage(const std::string& filename) {
     auto reader = itk::ImageFileReader<ImageType>::New();
     reader->SetFileName(filename);
     reader->Update();
     return reader->GetOutput();
 }
 
-void write_nifti_image(ImageType::Pointer image, const std::string& filename) {
+void writeImage(ImageType::Pointer image, const std::string& filename) {
     auto writer = itk::ImageFileWriter<ImageType>::New();
     writer->SetFileName(filename);
     writer->SetInput(image);
@@ -20,6 +19,6 @@ void write_nifti_image(ImageType::Pointer image, const std::string& filename) {
 }
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod) {
-    mod.method("read_nifti_image", read_nifti_image);
-    mod.method("write_nifti_image", write_nifti_image);
+    mod.method("loadImage", loadImage);
+    mod.method("writeImage", writeImage);
 }
