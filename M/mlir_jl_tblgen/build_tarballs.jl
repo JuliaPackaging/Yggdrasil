3a2b842f018f8b3a6ddc4447d1c8532e1fde145a
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-06-29 23:34:22 +0200

    [mlir_jl_tblgen] Build with MLIR 18 too

--- M/mlir_jl_tblgen/build_tarballs.jl
+++ M/mlir_jl_tblgen/build_tarballs.jl
@@ -9,7 +9,7 @@ name = "mlir_jl_tblgen"
 repo = "https://github.com/JuliaLabs/MLIR.jl.git"
 version = v"0.0.8"
 
-llvm_versions = [v"14.0.6", v"15.0.7", v"16.0.6", v"17.0.6"]
+llvm_versions = [v"14.0.6", v"15.0.7", v"16.0.6", v"17.0.6", "18.1.7"]
 
 sources = [
     GitSource(repo, "5c35af320732e6d3363cdf47e4d6f156b100756e")
