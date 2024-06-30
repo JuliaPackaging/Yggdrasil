c3a81a73672415f5c5765f0e6185c7790a02a486
Laslo Hunhold <hunhold@uni-koeln.de>
2024-06-30 11:26:32 +0200

    New Recipe: libtakum v0.1.0

---
+++ L/libtakum/build_tarballs.jl
@@ -0,0 +1,34 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "libtakum"
+version = v"0.1.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/takum-arithmetic/libtakum.git", "a7360b5f3e0c9e936bce69b32ad430001f94951d")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/libtakum
+./configure
+make PREFIX=${prefix} LDCONFIG= -j${nproc} install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libtakum", :libtakum)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
