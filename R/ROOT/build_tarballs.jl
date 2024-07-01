7be07aac2d8107eebaffd26f922d501e78bcb6ab
Philippe Gras <philippe.gras@cern.ch>
2024-07-01 14:02:44 +0200

    R/ROOT WIP
    . Works for x86_64-linux-musl, but of little use, due to the general
    dependency library loard issue for .jll packages with musl
    
    . Fails for x86_64-linux-gnu: misses to instruct the cross-compiled
      rootcling_stage1 to use the header files of the sysroot of the
      target host.

--- R/ROOT/build_tarballs.jl
+++ R/ROOT/build_tarballs.jl
@@ -1,3 +1,4 @@
+
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
@@ -9,49 +10,122 @@ rootgithash = Dict(v"6.30.4" => "ebd5b65997c3dff10fc38472a40ddffc26fb982a")
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/root-project/root.git", rootgithash[version])
+    GitSource("https://github.com/root-project/root.git", rootgithash[version]),
+    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-
-if echo "$target" | grep musl; then #build wih musl library
+cd $WORKSPACE
+cat > script.sh <<EOF
+if echo "\$target" | grep -q musl; then #build wih musl library
 # Disabling what is not working with musl:
-    CMAKE_EXTRA_OPTS="-Ddavix=OFF -Dxrootd=OFF -Dssl=OFF"
+    CMAKE_EXTRA_OPTS="-Ddavix=OFF -Dxrootd=OFF -Dssl=OFF -Druntime_cxxmodules=OFF -Droofit=OFF"
+else
+    CMAKE_EXTRA_OPTS=-Druntime_cxxmodules=OFF
 fi
+
+#Uncomment for a minimal build for debugging purposes
+#CMAKE_EXTRA_OPTS="\$CMAKE_EXTRA_OPTS -Dclad=OFF -Dhtml=OFF -Dwebgui=OFF -Dcxxmodules=OFF -Dproof=OFF -Dtmva=OFF -Drootfit=OFF -Dxproofd=OFF -Dxrootd=OFF -Dssl=OFF -Dpyroot=OFF -Dtesting=OFF -Droot7=OFF -Dspectrum=OFF -Dunfold=OFF -Dasimage=OFF -Dgviz=OFF -Dfitiso=OFF -Dcocoa=OFF -Dopengl=OFF -Dproof=OFF -Dxml=OFF -Dgfal=OFF -Dmpi=OFF"
+
+export SYSTEM_INCLUDE_PATH="\`\$BUILD_CXX --sysroot="/opt/\$target/\$target/sys-root" -E -x c++ -v /dev/null  2>&1  | awk '{gsub("^ ", "")} /End of search list/{a=0} {if(a==1){s=s d \$0;d=":"}} /#include <...> search starts here/{a=1} END{print s}'\`"
 
 # build-in compilation of the libAfterImage library needs this directory
 mkdir -p /tmp/user/0
 
-cd $WORKSPACE/srcdir
+cd /
+[ -f /opt/bin/x86_64-linux-gnu-libgfortran5-cxx11/x86_64-linux-gnu-g++ ] && atomic_patch -p1 \${WORKSPACE}/srcdir/patches/x86_64-linux-gnu-g++.patch
+
+cd "\$WORKSPACE"
 
 # For the cross-compiling, LLVM needs to compile the llvm-tblgen tool
 # for the host. The ROOT LLVM code tree does not include the test and
 # benchmark directories cmake will need to configure for the host for
-# this bootstrap. We need to disable the build of the test and benchmarks
+# this bootstrap. Therefore, we need to disable the build of the test and
+# benchmarks.
 sed -i 's/\(option(LLVM_INCLUDE_TESTS[[:space:]].*[[:space:]]\)on)\(.*\)/\1OFF)\2/i
-        s/\(option(LLVM_INCLUDE_BENCHMARKS[[:space:]].*[[:space:]]\)on)\(.*\)/\1OFF)\2/i' \
-        root/interpreter/llvm-project/llvm/CMakeLists.txt
+        s/\(option(LLVM_INCLUDE_BENCHMARKS[[:space:]].*[[:space:]]\)on)\(.*\)/\1OFF)\2/i' \\
+        srcdir/root/interpreter/llvm-project/llvm/CMakeLists.txt
+echo "set(CXX_STANDARD 17)" >> srcdir/root/interpreter/llvm-project/llvm/CMakeLists.txt
 
 # N llvm links. LLVM link command needs 15GB
-LLVM_PARALLEL_LINK_JOBS=`grep MemTot /proc/meminfo  | awk '{a=int($2/15100000); if(a>'"$nproc"') a='"$nproc"'; if(a<1) a=1; print a;}'`
+njobs=\$((2*\`nproc\`))
+LLVM_PARALLEL_LINK_JOBS=\`grep MemTot /proc/meminfo  | awk '{a=int(\$2/15100000); if(a>'"\$njobs"') a='"\$njobs"'; if(a<1) a=1; print a;}'\`
 
-# For the rootcling execution performed during the build:
-echo "include_directories(SYSTEM /opt/$target/$target/sys-root/usr/include)" >> ${CMAKE_TARGET_TOOLCHAIN}
+#DEBUG# # For the rootcling execution performed during the build:
+#DEBUG# echo "include_directories(SYSTEM /opt/\$target/\$target/sys-root/usr/include)" >> \${CMAKE_TARGET_TOOLCHAIN}
 
 # Build with debug info, while we are debugging this script:
 BUILD_TYPE=Debug
 
-cmake -GNinja \
-      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-      -DCMAKE_INSTALL_PREFIX=$prefix \
-      -DLLVM_PARALLEL_LINK_JOBS=$LLVM_PARALLEL_LINK_JOBS \
-      $CMAKE_EXTRA_OPTS \
-      -DCMAKE_BUILD_TYPE=$BUILD_TYPE -DLLVM_BUILD_TYPE=$BUILD_TYPE \
-      -B build -S root
+# Support to run programs linked with glibc
+#if [ $target = x86_64-linux-gnu ]; then
+#   ln -sf /opt/x86_64-linux-gnu/x86_64-linux-gnu/sys-root/lib64/ld-linux-x86-64.so.2 /lib64/ld-linux-x86-64.so.2
+#fi
 
-cmake --build build -j${nproc} 
-cmake --install build --prefix $prefix
+#For gcc 9.1.0 (bug fixed with 9.3.0)
+#(cd / && atomic_patch -p1 \${WORKSPACE}/srcdir/patches/\$target-variant.patch)
+
+if [ $target != $MACHTYPE ]; then #cross compilation
+
+   (cd srcdir
+   # patch to fix cross-compilation with rootcling
+   #atomic_patch -p1 patches/rootcling-cross-compile.patch
+   atomic_patch -p1 patches/rootcling-cross-compile-v3.patch
+
+   # patch to fix cross-compilation for afterimage
+   atomic_patch -p1 patches/afterimage-cross-compile.patch
+   )
+
+   #sed -i 's@set(command \${CMAKE_COMMAND} -E env "LD_LIBRARY_PATH=\${CMAKE_BINARY_DIR}/lib:.*" \$<TARGET_FILE:rootcling_stage1>)@set(command \${CMAKE_COMMAND} -E env "LD_LIBRARY_PATH=\${CMAKE_BINARY_DIR}/lib:\$ENV{WORKSPACE}/x86_64-linux-gnu-libgfortran5-cxx11/destdir/lib" LLVM_SYMBOLIZER_PATH=\${CMAKE_BINARY_BUILD}/interpreter/llvm-project/llvm/NATIVE/tools/llvm-symbolizer \$<TARGET_FILE:rootcling_stage1>)@' \${WORKSPACE}/srcdir/root/cmake/modules/RootMacros.cmake
+
+   # Compile for the host binary used in the build process
+   mkdir NATIVE
+   cmake -GNinja \\
+         -DCMAKE_TOOLCHAIN_FILE=\${CMAKE_HOST_TOOLCHAIN} \\
+         -DLLVM_HOST_TRIPLE=\$MACHTYPE \\
+         -DLLVM_PARALLEL_LINK_JOBS=\$LLVM_PARALLEL_LINK_JOBS \\
+         -DCXX_STANDARD=c++17 \\
+         -DCLANG_DEFAULT_STD_CXX=cxx17 \\
+         \$CMAKE_EXTRA_OPTS \\
+         -DCMAKE_BUILD_TYPE=Release \\
+         -DCLING_CXX_PATH=g++ \\
+         -B NATIVE -S srcdir/root
+
+   ninja -C NATIVE -j\${njobs} rootcling_stage1 llvm-tblgen clang-tblgen llvm-config llvm-symbolizer
+
+   CMAKE_EXTRA_OPTS="\$CMAKE_EXTRA_OPTS -DNATIVE_BINARY_DIR=\$PWD/NATIVE \\
+      -DLLVM_TABLEGEN=\"\$PWD/NATIVE/interpreter/llvm-project/llvm/bin/llvm-tblgen\" \\
+      -DCLANG_TABLEGEN=\"\$PWD/NATIVE/interpreter/llvm-project/llvm/bin/clang-tblgen\" \\
+      -DLLVM_CONFIG_PATH=\"\$PWD/NATIVE/interpreter/llvm-project/llvm/bin/llvm-config\""
+fi
+
+# CPLUS_INCLUDE_PATH used to set system include path for rootcling in absence
+# of a -sysroot option. It should be transparent gcc and target build as it set the path 
+# to the value obtained from gcc itself before setting CPLUS_INCLUDE_PATH and using
+# the same sysroot option as for compilation.
+export CPLUS_INCLUDE_PATH="\$SYSTEM_INCLUDE_PATH"
+mkdir build
+cmake -GNinja \\
+      -DCMAKE_TOOLCHAIN_FILE=\${CMAKE_TARGET_TOOLCHAIN} \\
+      -DCMAKE_INSTALL_PREFIX=\$prefix \\
+      -DLLVM_HOST_TRIPLE=\$target \\
+      -DLLVM_PARALLEL_LINK_JOBS=\$LLVM_PARALLEL_LINK_JOBS \\
+      -DCXX_STANDARD=c++17 \\
+      -DCLANG_DEFAULT_STD_CXX=cxx17 \\
+      \$CMAKE_EXTRA_OPTS \\
+      -DCMAKE_BUILD_TYPE=\$BUILD_TYPE -DLLVM_BUILD_TYPE=\$BUILD_TYPE \\
+      -DCLING_CXX_PATH=g++ \\
+      -B build -S srcdir/root
+
+# Build the code
+cmake --build build -j\${njobs}
+
+# Install the binaries
+cmake --install build --prefix \$prefix
+cp -a build/core/rootcling_stage1/src/rootcling_stage1 \$prefix/bin/
+EOF
+bash -x -e script.sh
 """
 
 # These are the platforms we will build for by default, unless further
@@ -73,6 +147,9 @@ platforms = [
 
 # The products that we will ensure are always built
 products = Product[
+    ExecutableProduct("root", :root)
+    ExecutableProduct("rootcling", :rootcling)
+    ExecutableProduct("rootcling_stage1", :rootcling_stage1)
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -84,7 +161,6 @@ dependencies = [
     Dependency("Xorg_libXft_jll")
 
     #Optionnal dependencies (if absent, either a feature will be disabled or a built-in version will be compiled)
-    Dependency("oneTBB_jll")
     Dependency("VDT_jll")
     Dependency("XRootD_jll")
     Dependency(PackageSpec(name="LAPACK_jll", uuid="51474c39-65e3-53ba-86ba-03b1b862ec14"))
@@ -110,7 +186,8 @@ dependencies = [
     Dependency("Xorg_libxkbfile_jll")
     Dependency("Libglvnd_jll")
     Dependency("OpenBLAS_jll")
+    Dependency("oneTBB_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"11")
