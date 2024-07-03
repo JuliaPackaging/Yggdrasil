a1b89c4d61296e8e90c4e70394fbea9893af5641
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-07-03 14:03:20 -0400

    Add a platform for CUDSS

--- C/CUDA/CUDSS/build_tarballs.jl
+++ C/CUDA/CUDSS/build_tarballs.jl
@@ -44,6 +44,7 @@ products = [
 dependencies = [RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll"))]
 
 platforms = [Platform("x86_64", "linux"),
+             Platform("aarch64", "linux"; cuda_platform="jetson"),
              Platform("aarch64", "linux"; cuda_platform="sbsa"),
              Platform("x86_64", "windows")]
 
