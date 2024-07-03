c82b3dd872c383a1d462480b7ab9d78b99ce5220
Elliot Saba <staticfloat@gmail.com>
2024-07-03 15:39:35 +0000

    [Libtool] Use `/bin/bash` in `libtoolize`
    We were fixing the shell shebang in `libtool` previously with [0], but
    failing to set the shell shebang for `libtoolize` as well.
    
    [0] https://github.com/JuliaPackaging/Yggdrasil/pull/8923

--- L/Libtool/build_tarballs.jl
+++ L/Libtool/build_tarballs.jl
@@ -9,11 +9,18 @@ version = v"2.4.7"
 sources = [
     ArchiveSource("https://ftpmirror.gnu.org/libtool/libtool-$(version).tar.gz",
                   "04e96c2404ea70c590c546eba4202a4e12722c640016c12b9b2f1ce3d481e9a8"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/libtool-*
+cd ${WORKSPACE}/srcdir/libtool-*
+for patch in ${WORKSPACE}/srcdir/patches/*.patch; do
+    atomic_patch -p1 ${patch}
+done
+# Prevent `help2man` needing to be run because we patched `libtoolize`
+touch doc/libtoolize.1
+
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} SHELL=/bin/bash
 make
 make install
