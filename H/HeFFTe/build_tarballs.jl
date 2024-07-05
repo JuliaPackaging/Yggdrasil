a5a2ce8b7d68a9ace1e8ab2d3c5ac7db483897dd
Jukka Aho <jukka.aho@vtt.fi>
2024-07-05 14:47:23 +0300

    Link MPI

--- H/HeFFTe/build_tarballs.jl
+++ H/HeFFTe/build_tarballs.jl
@@ -2,8 +2,14 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
+
 name = "HeFFTe"
 version = v"2.4.0"
+
+OpenMPI_version="5.0"
+MPItrampoline_version="5.2.1"
 
 # Collection of sources required to complete build
 sources = [
@@ -14,17 +20,31 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/heffte
 cmake -B build \
-    -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DCMAKE_BUILD_TYPE=Release
-cmake --build build
+    -DCMAKE_FIND_ROOT_PATH='${prefix}/lib/mpich;${prefix}' \
+    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON \
+    -DMPI_C_COMPILER=$bindir/mpicc \
+    -DMPI_CXX_COMPILER=$bindir/mpicxx \
+    -DHeffte_ENABLE_FFTW=ON
+
+cmake --build build -j${nproc}
 cmake --install build
+"""
+
+augment_platform_block = """
+    using Base.BinaryPlatforms
+    $(MPI.augment)
+    augment_platform!(platform::Platform) = augment_mpi!(platform)
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = filter(Sys.islinux, supported_platforms())
 platforms = expand_cxxstring_abis(platforms)
+
+platforms, platform_dependencies = MPI.augment_platforms(platforms; MPItrampoline_compat=MPItrampoline_version, OpenMPI_compat=OpenMPI_version)
 
 # The products that we will ensure are always built
 products = [
@@ -34,8 +54,25 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
-    Dependency(PackageSpec(name="MPICH_jll", uuid="7cb0a576-ebde-5e09-9194-50597f1243b4"))
 ]
 
+# Avoid platforms where the MPI implementation isn't supported
+# OpenMPI
+platforms = filter(p -> !(p["mpi"] == "openmpi" && arch(p) == "armv6l" && libc(p) == "glibc"), platforms)
+
+# MPItrampoline
+platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && libc(p) == "musl"), platforms)
+platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && Sys.isfreebsd(p)), platforms)
+
+append!(dependencies, platform_dependencies)
+
+# Don't look for `mpiwrapper.so` when BinaryBuilder examines and
+# `dlopen`s the shared libraries. (MPItrampoline will skip its
+# automatic initialization.)
+ENV["MPITRAMPOLINE_DELAY_INIT"] = "1"
+
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"9")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               augment_platform_block,
+               julia_compat="1.6",
+               preferred_gcc_version = v"9")
