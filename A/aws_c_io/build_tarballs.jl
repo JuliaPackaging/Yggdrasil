0ae8ba27a5c3584642b45a7742e317763708a36b
quinnj2 <karbarcca2@gmail.com>
2024-07-12 09:51:39 -0600

    [aws_c_io] Update to version 0.14.11 (#9057)
    * [aws_c_io] Update to 0.14.11
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- A/aws_c_io/build_tarballs.jl
+++ A/aws_c_io/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_io"
-version = v"0.14.9"
+version = v"0.14.11"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/awslabs/aws-c-io.git", "878b4fa027bda4041493f06e0562d5e98bb3deb8"),
+    GitSource("https://github.com/awslabs/aws-c-io.git", "d04508d113851f1bc15630d93490b2aa09676137"),
     DirectorySource("./bundled"),
 ]
 
