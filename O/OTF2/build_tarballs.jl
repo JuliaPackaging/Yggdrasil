b278463249fb95c7c200c6f16f75658feb91303a
Mosè Giordano <giordano@users.noreply.github.com>
2024-07-13 17:19:57 +0200

    [OTF2] Build for more platforms (#9074)
    * [OTF2] Build for more platforms
    
    * [OTF2] Exclude Windows

--- O/OTF2/build_tarballs.jl
+++ O/OTF2/build_tarballs.jl
@@ -3,11 +3,23 @@ using BinaryBuilder
 name = "OTF2"
 version = v"3.0.3"
 sources = [
-    ArchiveSource("https://perftools.pages.jsc.fz-juelich.de/cicd/otf2/tags/otf2-$version/otf2-$version.tar.gz", "18a3905f7917340387e3edc8e5766f31ab1af41f4ecc5665da6c769ca21c4ee8"),
+    ArchiveSource("https://perftools.pages.jsc.fz-juelich.de/cicd/otf2/tags/otf2-$(version)/otf2-$(version).tar.gz",
+                  "18a3905f7917340387e3edc8e5766f31ab1af41f4ecc5665da6c769ca21c4ee8"),
 ]
 
 script = raw"""
 cd ${WORKSPACE}/srcdir/otf2-*
+
+# The build system of this package is completely broken, in cross-compilation mode it
+# confuses host and build platforms, so we set `{CC,CXX}_FOR_BUILD` to the target
+# cross-compilers because life is horrible.  We use the absolute path just to be extra sure
+# we're pointing to the intended compilers.  Also, below we need to set
+# `ac_scorep_cross_compiling=yes` explicitly because the broken build system can't even
+# detect cross-compilation mode sensibly.
+CC=$(realpath $(which ${CC}))
+CXX=$(realpath $(which ${CXX}))
+CC_FOR_BUILD=${CC}
+CXX_FOR_BUILD=${CXX}
 
 autoreconf -fvi
 ./configure \
@@ -21,17 +33,16 @@ autoreconf -fvi
     --disable-doxygen-doc \
     --disable-doxygen-dot \
     --disable-doxygen-html \
-    --disable-doxygen-rtf
+    --disable-doxygen-rtf \
+    --disable-silent-rules \
+    --target=${target} \
+    ac_scorep_cross_compiling=yes
 
 make -j${nproc}
 make install V=1
 """
 
-# NOTE cross-compilation is not working because `--host` is not getting propagated
-platforms = [
-    Platform("x86_64", "linux"; libc="glibc"),
-    Platform("x86_64", "linux"; libc="musl"),
-]
+platforms = expand_cxxstring_abis(supported_platforms(; exclude=Sys.iswindows))
 
 dependencies = Dependency[]
 
