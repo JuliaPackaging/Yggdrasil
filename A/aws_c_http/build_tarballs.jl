a3649ece56eb711f060ade7aa07eaa65a315ed23
quinnj2 <karbarcca2@gmail.com>
2024-07-12 23:19:09 -0600

    [aws_c_http] Update to 0.8.4 (#9066)
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Jacob Quinn <quinn.jacobd@gmail.com>

--- A/aws_c_http/build_tarballs.jl
+++ A/aws_c_http/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_http"
-version = v"0.8.3"
+version = v"0.8.4"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/awslabs/aws-c-http.git", "652e2febf2242d6b3562267dc0dd982375ed698e"),
+    GitSource("https://github.com/awslabs/aws-c-http.git", "2c6fd577dcd50192cb541658a658aebd7b395685"),
 ]
 
 # Bash recipe for building across all platforms
