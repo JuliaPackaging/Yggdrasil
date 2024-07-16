1cfa781b581dfeace1abd4091c8a8f1c523b9e3c
Rémi Garcia <47191601+remi-garcia@users.noreply.github.com>
2024-07-16 09:12:37 +0200

    Update C/Chuffed/build_tarballs.jl
    Co-authored-by: Oscar Dowson <odow@users.noreply.github.com>

--- C/Chuffed/build_tarballs.jl
+++ C/Chuffed/build_tarballs.jl
@@ -17,7 +17,6 @@ cmake -B build \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release
-#rm ../chuffed/flatzinc/parser.tab.h
 cmake --build build --parallel ${nproc}
 cmake --install build
 """
