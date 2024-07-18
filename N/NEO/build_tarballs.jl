d2af8dc7fb2f1dfe4c6a120ce180586d4959f7a2
Tim Besard <tim.besard@gmail.com>
2024-07-18 15:42:39 +0200

    oneAPI: Bump NEO to v24.22.29735.20.

--- N/NEO/build_tarballs.jl
+++ N/NEO/build_tarballs.jl
@@ -7,7 +7,7 @@ const YGGDRASIL_DIR = "../.."
 include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "NEO"
-version = v"24.13.29138"#.7
+version = v"24.22.29735"#.20
 
 # Collection of sources required to build this package.
 sources = [
@@ -87,9 +87,9 @@ products = [
 #       when using a non-public release, refer to the compiled manifest
 #       https://github.com/intel/compute-runtime/blob/master/manifests/manifest.yml.
 dependencies = [
-    Dependency("gmmlib_jll"; compat="=22.3.18"),
-    Dependency("libigc_jll"; compat="=1.0.16510"),
-    Dependency("oneAPI_Level_Zero_Headers_jll", v"1.9.2"; compat="1.7.8"),
+    Dependency("gmmlib_jll"; compat="=22.3.19"),
+    Dependency("libigc_jll"; compat="=1.0.16695"),
+    Dependency("oneAPI_Level_Zero_Headers_jll"; compat="=1.9.2"),
 ]
 
 augment_platform_block = raw"""
