e0edf4b16743a4336abaaaa166cfc12b3e85e0f5
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-07-19 12:10:56 -0400

    [CUTest] Upgrade to v2.2.0

--- C/CUTEst/build_tarballs.jl
+++ C/CUTEst/build_tarballs.jl
@@ -3,104 +3,42 @@
 using BinaryBuilder, Pkg
 
 name = "CUTEst"
-version = v"2.0.7"
+version = v"2.2.0"
 
 # Collection of sources required to build ThinASLBuilder
 sources = [
-    GitSource("https://github.com/ralna/ARCHDefs.git" ,"5ab94bbbe45e13c1d00acdc09b8b7df470b98c29"),
-    GitSource("https://github.com/ralna/SIFDecode.git","42d3241205dc56e1f943687293e95586755a3c10"),
-    GitSource("https://github.com/ralna/CUTEst.git"   ,"1d2954ef69cfd541d3ec2299d29da7302cb8b6a3"),
+    GitSource("https://github.com/ralna/CUTEst.git", "05822b4a5d57fa65270b8d6e63c066da6da373d1"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-echo "building for ${target}"
+# Update Ninja
+cp ${host_prefix}/bin/ninja /usr/bin/ninja
 
-# setup
-mkdir -p ${bindir}
-mkdir -p ${libdir}
+cd ${WORKSPACE}/srcdir/CUTEst
 
-cp -r ARCHDefs ${prefix}
-cp -r SIFDecode ${prefix}
-cp -r CUTEst ${prefix}
+meson setup builddir --cross-file=${MESON_TARGET_TOOLCHAIN%.*}_gcc.meson \
+                     --prefix=$prefix
 
-export ARCHDEFS=${prefix}/ARCHDefs
-export SIFDECODE=${prefix}/SIFDecode
-export CUTEST=${prefix}/CUTEst
+meson compile -C builddir
+meson install -C builddir
 
-# ARCHDefs requires tput
-apk update
-apk add ncurses
-
-# build SIFDecode
-cd $SIFDECODE
-if [[ "${target}" == *-linux* || "${target}" == *-freebsd* || "${target}" == *-mingw* ]]; then
-  echo "6" > sifdecode.opts   # PC64
-  echo "2" >> sifdecode.opts  # Linux
-  echo "6" >> sifdecode.opts  # gfortran
-elif [[ "${target}" == *-apple* ]]; then
-  echo "13" > sifdecode.opts  # macOS
-  echo "2" >> sifdecode.opts  # gfortran
-fi
-echo "nny" >> sifdecode.opts
-./install_sifdecode < sifdecode.opts
-
-# build CUTEst
-cd $CUTEST
-if [[ "${target}" == *-linux* || "${target}" == *-freebsd* || "${target}" == *-mingw* ]]; then
-  echo "6" > cutest.opts   # PC64
-  echo "2" >> cutest.opts  # Linux
-  echo "6" >> cutest.opts  # gfortran
-  echo "2" >> cutest.opts  # build all tools except Matlab
-  echo "8" >> cutest.opts  # gcc
-  export MYARCH=pc64.lnx.gfo
-elif [[ "${target}" == *-apple* ]]; then
-  echo "13" > cutest.opts  # macOS
-  echo "2" >> cutest.opts  # gfortran
-  echo "2" >> cutest.opts  # build all tools except Matlab
-  echo "5" >> cutest.opts  # gcc
-  export MYARCH=mac64.osx.gfo
-fi
-echo "nnydy" >> cutest.opts
-./install_cutest < cutest.opts
-
-# Static libraries
-cp $CUTEST/objects/$MYARCH/single/libcutest.a $prefix/lib/libcutest_single.a
-cp $CUTEST/objects/$MYARCH/double/libcutest.a $prefix/lib/libcutest_double.a
-
-# Shared libraries
-if [[ "${target}" != *mingw* ]]; then
-    extra=""
-    if [[ "${target}" == *-apple-* ]]; then
-        extra="-Wl,-undefined -Wl,dynamic_lookup -headerpad_max_install_names"
-    fi
-    cd $CUTEST/objects/$MYARCH/single
-    gfortran -fPIC -shared ${extra} $(flagon -Wl,--whole-archive) libcutest.a $(flagon -Wl,--no-whole-archive) -o ${libdir}/libcutest_single.${dlext}
-    cd $CUTEST/objects/$MYARCH/double
-    gfortran -fPIC -shared ${extra} $(flagon -Wl,--whole-archive) libcutest.a $(flagon -Wl,--no-whole-archive) -o ${libdir}/libcutest_double.${dlext}
-fi
-
-cp $ARCHDEFS/bin/helper_functions ${bindir}/helper_functions
-cp $SIFDECODE/bin/sifdecoder ${bindir}/sifdecoder
-install_license $CUTEST/lgpl-3.0.txt
+install_license lgpl-3.0.txt
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-# can't build shared libs on Windows, which imposes all symbols to be defined
-platforms = expand_gfortran_versions(supported_platforms())
-platforms = filter!(p -> !(os(p) == "freebsd" && libgfortran_version(p) == v"3"), platforms)
+# These are the platforms we will build for by default, unless further platforms are passed in on the command line
+platforms = supported_platforms()
+platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
 products = [
-    FileProduct("bin/helper_functions", :helper_functions),
-    FileProduct("bin/sifdecoder", :sifdecoder),
     FileProduct("lib/libcutest_single.a", :libcutest_single),
     FileProduct("lib/libcutest_double.a", :libcutest_double),
 ]
 
 dependencies = [
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+    HostBuildDependency(PackageSpec(name="Ninja_jll", uuid="76642167-d241-5cee-8c94-7a494e8cb7b7")),
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
