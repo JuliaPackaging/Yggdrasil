e77b3efb5ee4d2b0b6f61655779c0c8a10101d95
Alex Arslan <ararslan@comcast.net>
2024-07-22 16:17:32 -0700

    Fix compat
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- S/spdlog/build_tarballs.jl
+++ S/spdlog/build_tarballs.jl
@@ -25,7 +25,7 @@ platforms = expand_cxxstring_abis(supported_platforms())
 
 products = [LibraryProduct("libspdlog", :libspdlog)]
 
-dependencies = [Dependency("Fmt_jll"; compat="11")]
+dependencies = [Dependency("Fmt_jll"; compat="11.0.1")]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.6")
