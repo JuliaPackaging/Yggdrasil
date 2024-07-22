be420bbb7cb1db8e1dcc30e4b106cced616ebe5d
Elliot Saba <staticfloat@gmail.com>
2024-07-22 16:54:31 -0700

    Update common.jl

--- H/HarfBuzz/common.jl
+++ H/HarfBuzz/common.jl
@@ -81,5 +81,5 @@ fi
     end
 
     # Build the tarballs, and possibly a `build.jl` as well.
-    build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"5", julia_compat="1.6")
+    build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"5", julia_compat="1.6", clang_use_lld=false)
 end
