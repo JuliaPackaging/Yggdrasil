a0b13a00e11c3b8f40095f1369ca77d002f13fb9
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2024-07-23 18:28:37 +0200

    [mlir_jl_tblgen] Update to v0.0.10 (#9129)

--- M/mlir_jl_tblgen/build_tarballs.jl
+++ M/mlir_jl_tblgen/build_tarballs.jl
@@ -7,12 +7,12 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
 
 name = "mlir_jl_tblgen"
 repo = "https://github.com/JuliaLabs/MLIR.jl.git"
-version = v"0.0.9"
+version = v"0.0.10"
 
 llvm_versions = [v"14.0.6", v"15.0.7", v"16.0.6", v"17.0.6", v"18.1.7"]
 
 sources = [
-    GitSource(repo, "1d8004d4fe22dec9b70e7d0e12e5c729d6af5e0f"),
+    GitSource(repo, "1e5e8a2b7b43ec79ec2132cf7a90a5f96d97b4da"),
 ]
 
 # Bash recipe for building across all platforms
