3aaeafa7743e1fc4d7f3e8307495bc4adf479b76
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-08-01 20:17:47 -0400

    [MKL_Pardiso] Add a standalone library

---
+++ M/MKL_Pardiso/build_tarballs.jl
@@ -0,0 +1,38 @@
+using BinaryBuilder, Pkg
+
+name = "MKL_Pardiso"
+version = v"2024.2.0"
+
+sources = [
+    GitSource("https://github.com/amontoison/MKL_Pardiso.git", "beeb78f8c44af3adcdb1665dc265bac43229a91a")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/MKL_Pardiso
+$CC -shared -o $libdir/libmkl_pardiso_shim.${dlext} mkl_pardiso_shim.c -lmkl_rt
+$CC -shared -o $libdir/libmkl_pardiso.${dlext} mkl_pardiso.c -lmkl_pardiso_shim
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Platform("x86_64", "linux"; libc="glibc"),
+    Platform("i686", "linux"; libc="glibc"),
+    Platform("i686", "windows"),
+    Platform("x86_64", "windows"),
+]
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libmkl_pardiso", :libmkl_pardiso),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="MKL_jll", uuid="856f044c-d86e-5d09-b602-aeab76dc8ba7"); compat="=$version"),
+    BuildDependency(PackageSpec(name="MKL_Headers_jll", uuid="b2f2f022-7a59-54f4-945e-e9b78c3fd545"); compat="=$version"),
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
