75652d1e00f7f485766cedd074dd71ffae1b0520
Bertrand Rix <bertrand.rix@artelys.com>
2024-08-05 16:03:10 +0200

    Clean and checkout pypowsybl from GitSource instead of git sub module.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- P/Powsybl/build_tarballs.jl
+++ P/Powsybl/build_tarballs.jl
@@ -3,13 +3,13 @@ using BinaryBuilder, Pkg
 name = "Powsybl"
 version = v"0.1"
 sources = [
-    GitSource("https://github.com/powsybl/powsybl.jl.git", "554ae1c7804af8a32a8480c779fb1b0505d112a5")
+    GitSource("https://github.com/powsybl/powsybl.jl.git", "554ae1c7804af8a32a8480c779fb1b0505d112a5"),
+    GitSource("https://github.com/powsybl/pypowsybl.git", "vBinariesDeployment")
 ]
 
 julia_versions = [v"1.7", v"1.8", v"1.9", v"1.10"]
 
 script = raw"""
-
 cd $WORKSPACE/srcdir
 
 # Get binary for powsybl-java, generated with GraalVm
@@ -23,18 +23,14 @@ if [[ "${target}" == *-apple-* ]]; then
     wget https://github.com/powsybl/pypowsybl/releases/download/vBinariesDeployment/binaries-vBinariesDeployment-darwin.zip -O powsybl-java.zip
 fi
 unzip powsybl-java.zip -d $prefix
-cd powsybl.jl/
-git submodule init
-git submodule update
-mkdir build
-cd cpp/pypowsybl/
-mkdir build
-cd build
+
 # Build powsybl-cpp API
-cmake ${WORKSPACE}/srcdir/powsybl.jl/cpp/pypowsybl/cpp/powsybl-cpp -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DBUILD_PYPOWSYBL_JAVA=OFF -DPYPOWSYBL_JAVA_LIBRARY_DIR=$prefix/lib -DPYPOWSYBL_JAVA_INCLUDE_DIR=$prefix/include -DCMAKE_INSTALL_PREFIX=$prefix
+cd pypowsybl/cpp && mkdir build && cd build
+cmake ${WORKSPACE}/srcdir/pypowsybl/cpp/powsybl-cpp -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DBUILD_PYPOWSYBL_JAVA=OFF -DPYPOWSYBL_JAVA_LIBRARY_DIR=$prefix/lib -DPYPOWSYBL_JAVA_INCLUDE_DIR=$prefix/include -DCMAKE_INSTALL_PREFIX=$prefix
 cmake --build . --target install --config Release
+
 # Build julia wrapper
-cd ../../../build/
+cd $WORKSPACE/srcdir/powsybl.jl && mkdir build && cd build
 cmake -DCMAKE_BUILD_TYPE=Release ../cpp/powsybljl-cpp -DJulia_PREFIX=$prefix -DJlCxx_DIR=$prefix/lib/cmake/JlCxx -DCMAKE_FIND_ROOT_PATH=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_PREFIX_PATH=$prefix -DCMAKE_INSTALL_PREFIX=$prefix -DPOWSYBL_INSTALL_DIR=$prefix
 cmake --build . --target install --config Release
 install_license $WORKSPACE/srcdir/powsybl.jl/LICENSE.md
