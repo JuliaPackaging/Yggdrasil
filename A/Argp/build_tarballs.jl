b09a4c08614cf7270da31ef5327235503021fa70
laurentbartholdi <laurent.bartholdi@gmail.com>
2024-08-05 12:25:37 +0000

    needs -t in install to make directories

--- A/Argp/build_tarballs.jl
+++ A/Argp/build_tarballs.jl
@@ -22,8 +22,8 @@ done
 autoreconf -i
 ./configure --prefix=$prefix --build=${MACHTYPE} --host=${target}
 make CC=gcc
-install -Dvm 644 argp.h $includedir
-install -Dvm 644 libargp.a $libdir
+install -Dvm 644 argp.h -t $includedir
+install -Dvm 644 libargp.a -t $libdir
 
 install_license ${WORKSPACE}/srcdir/argp-standalone/README.md
 """
