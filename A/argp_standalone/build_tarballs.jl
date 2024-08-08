2bf176c2e99e9d69e05737ca9f10681308fe7c20
laurentbartholdi <laurent.bartholdi@gmail.com>
2024-08-08 12:25:33 +0200

    Update A/argp_standalone/build_tarballs.jl
    Co-authored-by: Mosè Giordano <giordano@users.noreply.github.com>

--- A/argp_standalone/build_tarballs.jl
+++ A/argp_standalone/build_tarballs.jl
@@ -30,7 +30,7 @@ install_license ${WORKSPACE}/srcdir/argp-standalone/README.md
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(;experimental=true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = Product[
