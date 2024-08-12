fcf804eac795f5f06cc2ba83a93cf92ef71c1cdc
Ian Butterworth <i.r.butterworth@gmail.com>
2024-08-12 15:51:54 +0200

    update LLVMLibUnwind

---
+++ L/LLVMLibUnwind/LLVMLibUnwind@18.1.1/build_tarballs.jl
@@ -0,0 +1,7 @@
+version = v"18.1.1"
+
+include("../common.jl")
+
+# Build the tarballs
+build_tarballs(ARGS, configure(version; experimental=true)...;
+               preferred_gcc_version=v"6.1.0", julia_compat="1.12")
