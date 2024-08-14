fa19d3e9b6a72ab08e32a6c13addfb49e97e79d1
Viral B. Shah <virals@gmail.com>
2024-08-13 20:08:42 -0400

    Update build_tarballs.jl

--- R/Rmath/build_tarballs.jl
+++ R/Rmath/build_tarballs.jl
@@ -22,4 +22,4 @@ products = [
 
 dependencies = Dependency[]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat = "1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat = "1.6", preferred_gcc_version=v"6")
