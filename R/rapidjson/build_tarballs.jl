a474342f9b37ae29972d7d1831597aa23da93df8
Marius Meyer <marius.meyer@uni-paderborn.de>
2024-08-16 12:17:41 +0200

    Use current master branch for building instead of outdated release

--- R/rapidjson/build_tarballs.jl
+++ R/rapidjson/build_tarballs.jl
@@ -5,13 +5,19 @@ using BinaryBuilder
 name = "rapidjson"
 version = v"1.1.0"
 sources = [
-    GitSource("https://github.com/Tencent/rapidjson.git", "f54b0e47a08782a6131cc3d60f94d038fa6e0a51")
+    GitSource("https://github.com/Tencent/rapidjson.git", "ab1842a2dae061284c0a62dca1cc6d5e7e37e346")
 ]
 
 script = raw"""
 cd ${WORKSPACE}/srcdir/rapidjson
-cmake -B build -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release \
-    -DRAPIDJSON_BUILD_DOC=No -DRAPIDJSON_BUILD_EXAMPLES=No -D RAPIDJSON_BUILD_TESTS=No -DRAPIDJSON_BUILD_CXX17=Yes -DRAPIDJSON_BUILD_CXX11=No 
+cmake -S . -B build \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DRAPIDJSON_BUILD_DOC=No \
+    -DRAPIDJSON_BUILD_EXAMPLES=No \
+    -DRAPIDJSON_BUILD_TESTS=No \
+    -DRAPIDJSON_BUILD_CXX17=Yes
 cmake --build build --parallel ${nproc}
 cmake --install build
 """
