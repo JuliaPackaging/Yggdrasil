dc507f0d87a29be4432f47e28fdf4f9c480e824e
Marius Meyer <marius.meyer@uni-paderborn.de>
2024-08-16 10:33:13 +0200

    Add OpenCL ICD boíuld script

---
+++ O/ocl_icd/build_tarballs.jl
@@ -0,0 +1,29 @@
+using BinaryBuilder
+
+name = "ocl_icd"
+version = v"2.3.2"
+sources = [
+    GitSource("https://github.com/OCL-dev/ocl-icd.git", "fdde6677b21329432db8b481e2637cd10f7d3cb2")
+]
+
+script = raw"""
+cd ${WORKSPACE}/srcdir/ocl-icd
+./bootstrap
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make -j all
+make install
+"""
+
+platforms = supported_platforms()
+
+products = [
+    LibraryProduct(["libOpenCL", "OpenCL"], :libocl_icd),
+    FileProduct("include/ocl_icd.h", :ocl_icd_h)
+]
+
+
+dependencies = [
+    BuildDependency("OpenCL_Headers_jll"),
+]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
