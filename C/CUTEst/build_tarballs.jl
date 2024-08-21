3d3a6122d455bca2318c5dfa5cd49f18f6ca9a3d
Alexis Montoison <alexis.montoison@polymtl.ca>
2024-08-21 01:29:36 -0400

    Drop old libgfortran

--- C/CUTEst/build_tarballs.jl
+++ C/CUTEst/build_tarballs.jl
@@ -50,6 +50,7 @@ fi
 # These are the platforms we will build for by default, unless further platforms are passed in on the command line
 platforms = supported_platforms()
 platforms = expand_gfortran_versions(platforms)
+platforms = filter(p -> libgfortran_version(p) != v"3", platforms)
 
 # The products that we will ensure are always built
 products = [
