a22823732576ec8d2a2a2b100ddfc98555979c5e
Marius Meyer <marius.meyer@uni-paderborn.de>
2024-08-21 15:04:08 +0200

    Add windows support

--- O/ocl_icd/build_tarballs.jl
+++ O/ocl_icd/build_tarballs.jl
@@ -3,15 +3,26 @@ using BinaryBuilder
 name = "ocl_icd"
 version = v"2.3.2"
 sources = [
-    GitSource("https://github.com/OCL-dev/ocl-icd.git", "fdde6677b21329432db8b481e2637cd10f7d3cb2")
+    GitSource("https://github.com/OCL-dev/ocl-icd.git", "fdde6677b21329432db8b481e2637cd10f7d3cb2"),
+    DirectorySource("$(pwd())/patches")
 ]
 
 script = raw"""
 apk add ruby
 
 cd ${WORKSPACE}/srcdir/ocl-icd
-./bootstrap
 
+# Copy License
+mkdir -p ${WORKSPACE}/destdir/share/licenses/ocl_icd
+cp COPYING ${WORKSPACE}/destdir/share/licenses/ocl_icd
+
+# Fix build for windows based on 
+# https://cygwin.com/cgit/cygwin-packages/ocl-icd/tree/ocl-icd-2.3.2-1.src.patch
+if [[ "${target}" == *-w64-* ]]; then
+    git apply ../cygwin.patch
+fi
+
+./bootstrap
 
 # Fix rpl_malloc error with musl
 if [[ "${target}" == *-musl* ]]; then
@@ -23,13 +34,15 @@ if [[ "${target}" == *-apple-* ]]; then
     sed -i '/AC_FUNC_REALLOC/d' ./configure.ac
 fi
 
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+./configure --prefix=${prefix} \
+    --build=${MACHTYPE} \
+    --host=${target}
 make -j all
 make install
 """
 
 platforms = supported_platforms()
-filter!(!Sys.iswindows, platforms)
+filter!(p -> !(Sys.iswindows(p) && arch(p) == "i686"), platforms)
 
 products = [
     LibraryProduct(["libOpenCL", "OpenCL"], :libocl_icd),
@@ -38,7 +51,7 @@ products = [
 
 
 dependencies = [
-    BuildDependency("OpenCL_Headers_jll"),
+    Dependency("dlfcn_win32_jll", platforms=filter(Sys.iswindows, platforms))
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
