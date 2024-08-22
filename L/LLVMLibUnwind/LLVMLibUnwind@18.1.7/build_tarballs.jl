60d79a903bf5e49c4fa8c504eac5ca431c2e104a
Alex Arslan <ararslan@comcast.net>
2024-08-22 16:10:34 -0700

    Guess we'll try it out

--- L/LLVMLibUnwind/LLVMLibUnwind@18.1.7/build_tarballs.jl
+++ L/LLVMLibUnwind/LLVMLibUnwind@18.1.7/build_tarballs.jl
@@ -4,4 +4,4 @@ include("../common.jl")
 
 # Build the tarballs
 build_tarballs(ARGS, configure(version; experimental=true)...;
-               preferred_gcc_version=v"6.1.0", julia_compat="1.12")
+               preferred_gcc_version=v"10", preferred_llvm_version=v"17", julia_compat="1.12")
