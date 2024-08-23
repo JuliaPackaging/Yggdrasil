cb1c46eccf23f553bded11da36878f80e35a529b
Jared Wahlstrand <jwahlstrand@gmail.com>
2024-08-23 17:44:57 -0400

    [Pango] rebuild for HarfBuzz update

--- P/Pango/build_tarballs.jl
+++ P/Pango/build_tarballs.jl
@@ -73,7 +73,7 @@ dependencies = [
     Dependency("FreeType2_jll"; compat="2.13.1"),
     Dependency("FriBidi_jll"; compat="1.0.10"),
     Dependency("Glib_jll"; compat="2.74.0"),
-    Dependency("HarfBuzz_jll"; compat="2.8.1"),
+    Dependency("HarfBuzz_jll"; compat="8.3.1"),
     BuildDependency("Xorg_xorgproto_jll"; platforms=filter(p->Sys.islinux(p)||Sys.isfreebsd(p), platforms)),
 ]
 
