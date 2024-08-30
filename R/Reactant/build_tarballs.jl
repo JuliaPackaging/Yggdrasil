b129eeaef0a9171782f8b56ffa17a90205577c0e
William Moses <gh@wsmoses.com>
2024-08-29 20:57:41 -0500

    Update build_tarballs.jl

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -215,9 +215,8 @@ if [[ "${bb_full_target}" == *darwin* ]]; then
 #	echo "-fuse-ld=lld" >> bazel-out/k8-$MODE/bin/libReactantExtra.so-2.params
 	cat bazel-out/k8-$MODE/bin/libReactantExtra.so-2.params
 	ls -all .
-	ls -all bazel-out
-	ls -all bazel-out/k8-$MODE/
-    @bazel-out/k8-$MODE/bin/libReactantExtra.so-2.params
+	ls -all bazel-bin
+    @bazel-bin/libReactantExtra.so-2.params
 else
     $BAZEL ${BAZEL_FLAGS[@]} build ${BAZEL_BUILD_FLAGS[@]} :libReactantExtra.so
 fi
