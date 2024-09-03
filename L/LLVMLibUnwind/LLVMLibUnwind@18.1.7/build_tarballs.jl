fee5d9f77b28e74e99503a6a18df2dded5347bce
Ian Butterworth <i.r.butterworth@gmail.com>
2024-09-03 16:41:31 -0400

    Update L/LLVMLibUnwind/LLVMLibUnwind@18.1.7/build_tarballs.jl

--- L/LLVMLibUnwind/LLVMLibUnwind@18.1.7/build_tarballs.jl
+++ L/LLVMLibUnwind/LLVMLibUnwind@18.1.7/build_tarballs.jl
@@ -4,4 +4,4 @@ include("../common.jl")
 
 # Build the tarballs
 build_tarballs(ARGS, configure(version; experimental=true)...;
-               preferred_gcc_version=v"10", preferred_llvm_version=v"17", julia_compat="1.12")
+               preferred_gcc_version=v"10", preferred_llvm_version=v"18", julia_compat="1.12")
