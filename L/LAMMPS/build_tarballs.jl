a56a3a3daf2a364f83ae11dfc2b7cfba9d23ad90
jmeziere <jmeziere@gmail.com>
2024-09-06 13:31:24 -0600

    try string

--- L/LAMMPS/build_tarballs.jl
+++ L/LAMMPS/build_tarballs.jl
@@ -48,13 +48,13 @@ else
 fi
 
 # The MPI enabled LAMMPS_jll doesn't load properly on windows
-if [[ "${target}" == *mingw* ]] || [[ "${target}" == *mpi+none* ]]; then
+if [[ "${target}" == *mingw* ]] || [[ "${target}" == *"mpi+none"* ]]; then
     MPI_OPTION="OFF"
 else
     MPI_OPTION="ON"
 fi
 
-if [[ "${target}" != *cuda+none* ]]; then
+if [[ "${target}" != *"cuda+none"* ]]; then
     ADD_CUDA="-DGPU_API=cuda"
 else
     ADD_CUDA=""
