03266584144ce9f813b84dbd8e9bb950268aa7b2
Boris Kaus <kaus@uni-mainz.de>
2024-09-08 19:11:16 +0200

    remove line

--- P/PhreeqcRM/build_tarballs.jl
+++ P/PhreeqcRM/build_tarballs.jl
@@ -81,8 +81,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
         Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
-
-        BuildDependency("LLVMCompilerRT_jll"; platforms=filter(p -> Sys.isapple(p) && arch(p) == "aarch64", platforms)),
+        BuildDependency("LLVMCompilerRT_jll"; platforms=filter(p -> Sys.isapple(p) && arch(p) == "aarch64", platforms))
     ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
