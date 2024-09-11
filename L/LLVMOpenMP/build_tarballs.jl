fe57dbe784058137b35e8adc16f1ee0d00386bfa
Jonas Krimmer <30567171+jkrimmer@users.noreply.github.com>
2024-09-11 12:31:43 +0200

    Use v18.1.7 instead

--- L/LLVMOpenMP/build_tarballs.jl
+++ L/LLVMOpenMP/build_tarballs.jl
@@ -3,18 +3,18 @@
 using BinaryBuilder, Pkg
 
 name = "LLVMOpenMP"
-version = v"18.1.8"
+version = v"18.1.7"
 
 sources = [
     ArchiveSource(
         "https://github.com/llvm/llvm-project/releases/download/llvmorg-$(version)/openmp-$(version).src.tar.xz",
-        "60ed57245e73894e4a2a89b15889f367bd906abfe6d3f92e1718223d4b496150"
+        "6523c898d754d466b77b64ddca8fd0185c5aeb7f24260ddb0fae5779eb31cee3"
     ),
     # we need a bunch of additional cmake files to build the subproject separately
     # see: https://github.com/llvm/llvm-project/issues/53281#issuecomment-1260187944
     ArchiveSource(
         "https://github.com/llvm/llvm-project/releases/download/llvmorg-$(version)/cmake-$(version).src.tar.xz",
-        "59badef592dd34893cd319d42b323aaa990b452d05c7180ff20f23ab1b41e837"
+        "f0b67599f51cddcdbe604c35b6de97f2d0a447e18b9c30df300c82bf1ee25bd7"
     ),
     DirectorySource("./bundled"),
 ]
