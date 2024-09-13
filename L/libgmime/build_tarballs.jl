cfb29b83d1cf9f8654f09aeb41982ca1bbec8998
AlexKlo <65765845+AlexKlo@users.noreply.github.com>
2024-09-13 11:39:38 +0300

    Update build_tarballs.jl
    Add all supported platforms

--- L/libgmime/build_tarballs.jl
+++ L/libgmime/build_tarballs.jl
@@ -36,9 +36,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("x86_64", "linux"; libc = "glibc")
-]
+platforms = supported_platforms()
 
 
 # The products that we will ensure are always built
