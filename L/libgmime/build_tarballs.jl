e835034b4a5fae8f7d67ccd8e40f3de5cf2c437f
AlexKlo <65765845+AlexKlo@users.noreply.github.com>
2024-09-14 12:49:27 +0300

    Update build_tarballs.jl
    Add lib name for macos

--- L/libgmime/build_tarballs.jl
+++ L/libgmime/build_tarballs.jl
@@ -41,7 +41,7 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libgmime-3.0", :libgmime)
+    LibraryProduct(["libgmime-3.0", "libgmime-3"], :libgmime)
 ]
 
 # Dependencies that must be installed before this package can be built
