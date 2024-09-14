f5edb05c5dbc34774f30a092d54f0e136fbe0894
Alex Arslan <ararslan@comcast.net>
2024-09-14 01:22:38 -0700

    [XZ] Poke CI

--- X/XZ/build_tarballs.jl
+++ X/XZ/build_tarballs.jl
@@ -86,6 +86,6 @@ products = [
 dependencies = Dependency[
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well
+# Build the tarballs, and possibly a `build.jl` as well!
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.6")
