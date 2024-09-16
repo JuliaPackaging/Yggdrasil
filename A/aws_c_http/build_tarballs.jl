6442da63dbdc68d4659cd76707c2341f5f19441e
quinnj2 <karbarcca2@gmail.com>
2024-09-16 05:25:57 -0600

    [aws_c_http] Update to version 0.8.10 (#9439)
    * [aws_c_http] Update to 0.8.10
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- A/aws_c_http/build_tarballs.jl
+++ A/aws_c_http/build_tarballs.jl
@@ -3,19 +3,16 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_http"
-version = v"0.8.8"
+version = v"0.8.10"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/awslabs/aws-c-http.git", "4e74ab1e3702763e0b87bd1752f5a37c2f0400ac"),
+    GitSource("https://github.com/awslabs/aws-c-http.git", "6068653e1d582bd8e7d1c9f81f86beaf10444e3d"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/aws-c-http
-
-# Patch for MinGW toolchain
-find . -type f -exec sed -i 's/WS2tcpip.h/ws2tcpip.h/g' '{}' \;
 
 mkdir build && cd build
 cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
@@ -32,6 +29,7 @@ cmake --build . -j${nproc} --target install
 # platforms are passed in on the command line
 platforms = supported_platforms()
 filter!(p -> !(Sys.iswindows(p) && arch(p) == "i686"), platforms)
+filter!(p -> !(Sys.isfreebsd(p) && arch(p) == "aarch64"), platforms)
 
 # The products that we will ensure are always built
 products = [
