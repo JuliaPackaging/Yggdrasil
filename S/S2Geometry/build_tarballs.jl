db52dd6fa58ec2a8b6790e1785a115995f7633ea
Anshul Singhvi <anshulsinghvi@gmail.com>
2024-09-18 04:36:31 -0700

    fix missing close paren

--- S/S2Geometry/build_tarballs.jl
+++ S/S2Geometry/build_tarballs.jl
@@ -32,7 +32,7 @@ platforms = supported_platforms()
 # Only 64-bit platforms supported
 filter!(p -> nbits(p) == 64, platforms)
 # We are missing some dependencies for aarch64-freebsd, can be re-enabled in the future when we have them
-filter!(p -> !(Sys.isfreebsd(p) && arch(p) == "aarch64", platforms)
+filter!(p -> !(Sys.isfreebsd(p) && arch(p) == "aarch64"), platforms)
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
