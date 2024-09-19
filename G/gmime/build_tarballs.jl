91dc2575366fbdc4ff44d94be646207c2fc334d2
AlexKlo <65765845+AlexKlo@users.noreply.github.com>
2024-09-19 22:50:05 +0300

    Update build_tarballs.jl

--- G/gmime/build_tarballs.jl
+++ G/gmime/build_tarballs.jl
@@ -7,7 +7,8 @@ version = v"3.2.15"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/jstedfast/gmime/releases/download/$(version)/gmime-$(version).tar.xz", "84cd2a481a27970ec39b5c95f72db026722904a2ccf3fdbd57b280cf2d02b5c4")
+    ArchiveSource("https://github.com/jstedfast/gmime/releases/download/$(version)/gmime-$(version).tar.xz", 
+                  "84cd2a481a27970ec39b5c95f72db026722904a2ccf3fdbd57b280cf2d02b5c4")
 ]
 
 # Bash recipe for building across all platforms
@@ -18,12 +19,16 @@ echo '#define ICONV_ISO_STR_FORMAT "iso-%u-%s"' >> iconv-detect.h
 echo '#define ICONV_SHIFT_JIS "shift-jis"' >> iconv-detect.h
 echo '#define ICONV_10646 "UCS-4BE"' >> iconv-detect.h
 
-export LDFLAGS="-liconv"
-export CMAKE_EXE_LINKER_FLAGS="-Wl,--copy-dt-needed-entries"
++if [[ "${target}" == *-apple-* ]]; then
++   # Help the linker prefer our libiconv over system's libiconv.
++   export LDFLAGS="-L${libdir}"
++fi
 
 ./configure --prefix=${prefix} \
             --build=${MACHTYPE} \
             --host=${target} \
+            --enable-static=no \
+            --with-libiconv=gnu \
             ac_cv_have_iconv_detect_h=yes
 make -j${nproc}
 make install
@@ -31,7 +36,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(exclude=[Platform("aarch64", "FreeBSD")])
+platforms = supported_platforms(; exclude=p -> Sys.isfreebsd(p) && arch(p) == "aarch64")
 
 
 # The products that we will ensure are always built
@@ -42,7 +47,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="Glib_jll", uuid="7746bdde-850d-59dc-9ae8-88ece973131d"); compat="2.68.1")
-    BuildDependency(PackageSpec(name="Libiconv_jll", uuid="94ce4f54-9a6c-5748-9c1c-f9c7231a4531"))
+    Dependency(PackageSpec(name="Libiconv_jll", uuid="94ce4f54-9a6c-5748-9c1c-f9c7231a4531"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
