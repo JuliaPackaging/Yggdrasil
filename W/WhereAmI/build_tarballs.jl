b80bb3b83f1f91ce19332367300f8f87cdcbb1cd
Alex Arslan <ararslan@comcast.net>
2024-09-21 15:28:38 -0700

    Add builder for whereami
    Cross-platform library for identifying where on the filesystem the
    current executable lives.

---
+++ W/WhereAmI/build_tarballs.jl
@@ -0,0 +1,27 @@
+using BinaryBuilder
+
+name = "WhereAmI"
+version = v"2024.8.6"  # no proper versions/releases, this is the date of the commit used
+
+sources = [
+    GitSource("https://github.com/gpakosz/whereami",
+              "dcb52a058dc14530ba9ae05e4339bd3ddfae0e0e"),
+]
+
+script = raw"""
+cd ${WORKSPACE}/srcdir/whereami/
+cc -Isrc -O2 -std=c99 -fPIC -ldl -shared -o "${libdir}/libwai.${dlext}"
+install -Dvm 644 src/whereami.h "${includedir}/whereami.h"
+install_license LICENSE.MIT
+"""
+
+platforms = supported_platforms()
+
+products = [
+    LibraryProduct("libwai", :libwai),
+]
+
+dependencies = Dependency[]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6")
