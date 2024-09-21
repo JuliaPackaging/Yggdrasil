cbc31b5a1ca59244429ea7ef77565f68e8c35356
Alex Arslan <ararslan@comcast.net>
2024-09-21 12:05:49 -0700

    [MbedTLS@2.28.6] Rebuild for FreeBSD AArch64 support

--- M/MbedTLS/MbedTLS@2.28.6/build_tarballs.jl
+++ M/MbedTLS/MbedTLS@2.28.6/build_tarballs.jl
@@ -3,3 +3,5 @@ include("../common.jl")
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.9", preferred_llvm_version=llvm_version)
+
+# Build trigger: 1
