9e013e38400a9a19cd653a5fbdddcb542de0d82f
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-09-22 23:38:32 -0400

    [ECharts] Add recipe for v5.5.1

---
+++ E/ECharts/build_tarballs.jl
@@ -0,0 +1,22 @@
+using BinaryBuilder
+
+name = "echarts"
+version = v"5.5.1"
+sources = GitSource[]
+
+script = "version=$(version)\n" * raw"""
+apk add --update npm
+cd ${prefix}
+npm install echarts@${version}
+"""
+
+platforms = [AnyPlatform()]
+
+products = [
+    FileProduct("node_modules/echarts/dist/src/echarts.js", :echarts),
+]
+
+dependencies = Dependency[]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
