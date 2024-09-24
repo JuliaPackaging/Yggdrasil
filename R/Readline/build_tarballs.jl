8c87c72b7700788af3af74d2c5bafd192f079af1
Erik Schnetter <schnetter@gmail.com>
2024-09-24 16:01:12 -0400

    Readline: Build for aarch64-unknown-freebsd

--- R/Readline/build_tarballs.jl
+++ R/Readline/build_tarballs.jl
@@ -39,7 +39,7 @@ install_license COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = Product[
