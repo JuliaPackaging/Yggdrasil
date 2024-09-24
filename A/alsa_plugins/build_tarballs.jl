0f09fe52564887b82ad97a6e99f98753f54bd411
Viral B. Shah <744411+ViralBShah@users.noreply.github.com>
2024-09-24 00:54:19 -0400

    Update build_tarballs.jl

--- A/alsa_plugins/build_tarballs.jl
+++ A/alsa_plugins/build_tarballs.jl
@@ -46,6 +46,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
     Dependency(PackageSpec(name="FFMPEG_jll", uuid="b22a6f82-2f65-5046-a5b2-351ab43fb4e5"); compat="6.1.1")
     Dependency(PackageSpec(name="alsa_jll", uuid="45378030-f8ea-5b20-a7c7-1a9d95efb90e"); compat="1.2.5")
     Dependency(PackageSpec(name="libsamplerate_jll", uuid="9427e74d-4e05-59c1-8ff3-7d74b6e52ac8"); compat="0.1.9")
