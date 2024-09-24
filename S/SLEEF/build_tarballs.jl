4595343ee26c9837177db9bc525b4fbd79981bee
Erik Schnetter <schnetter@gmail.com>
2024-09-24 15:30:07 -0400

    SLEEF: New version 3.7.0 (#9498)
    * SLEEF: New version 3.7.0
    
    * SLEEF: Remove outdated patches
    
    * SLEEF: Don't need "bundled"
    
    * SLEEF: Disable some arm32 optimizations again
    
    * SLEEF: Remove MPFR compat bound

--- S/SLEEF/build_tarballs.jl
+++ S/SLEEF/build_tarballs.jl
@@ -3,35 +3,34 @@
 using BinaryBuilder, Pkg
 
 name = "SLEEF"
-version = v"3.5.2" # This is an unreleased version later than v3.5.1 (the currently latest released version).
+version = v"3.7.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/shibatch/sleef.git", "e0a003ee838b75d11763aa9c3ef17bf71a725bff"),
-    DirectorySource("./bundled"),
+    GitSource("https://github.com/shibatch/sleef.git", "c5494730bf601599a55f4e77f357b51ba590585e"),
+    DirectorySource("bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/sleef
+if [[ $target == arm-* ]]; then
+    atomic_patch -p1 ../patches/arm-neon32vfpv4.patch
+fi
 mkdir build-native
 cd build-native
 cmake \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_HOST_TOOLCHAIN} \
     -G Ninja \
-    -DBUILD_TESTS=OFF \
-    -DBUILD_DFT=TRUE \
-    -DBUILD_QUAD=TRUE \
+    -DSLEEF_BUILD_DFT=TRUE \
+    -DSLEEF_BUILD_QUAD=TRUE \
+    -DSLEEF_BUILD_SCALAR_LIB=TRUE \
+    -DSLEEF_BUILD_SHARED_LIBS=TRUE \
+    -DSLEEF_BUILD_TESTS=OFF \
     ..
 ninja all
 
 cd $WORKSPACE/srcdir/sleef
-if [[ $target == *w64-mingw32* ]]; then
-    atomic_patch -p1 ../patches/lowercase-windows-include.patch
-    atomic_patch -p1 ../patches/cross-compile-mingw-on-unix.patch
-elif [[ $target == arm-* ]]; then
-    atomic_patch -p1 ../patches/arm-neon32vfpv4.patch
-fi
 mkdir build-cross
 cd build-cross
 cmake \
@@ -41,9 +40,11 @@ cmake \
     -G Ninja \
     -DNATIVE_BUILD_DIR=$WORKSPACE/srcdir/sleef/build-native \
     -DSLEEF_SHOW_CONFIG=1 \
-    -DBUILD_TESTS=OFF \
-    -DBUILD_DFT=TRUE \
-    -DBUILD_QUAD=TRUE \
+    -DSLEEF_BUILD_DFT=TRUE \
+    -DSLEEF_BUILD_QUAD=TRUE \
+    -DSLEEF_BUILD_SCALAR_LIB=TRUE \
+    -DSLEEF_BUILD_SHARED_LIBS=TRUE \
+    -DSLEEF_BUILD_TESTS=OFF \
     ..
 ninja all
 ninja install
@@ -57,14 +58,16 @@ filter!(p -> !(arch(p) == "i686" && Sys.iswindows(p)), platforms) # i686-windows
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libsleef", :libsleef),
+    LibraryProduct("libsleefdft", :libsleefdft),
     LibraryProduct("libsleefquad", :libsleefquad),
+    LibraryProduct("libsleefscalar", :libsleefscalar),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
     Dependency("MPFR_jll"),
-    Dependency("OpenSSL_jll"; compat="1.1.10"),
+    Dependency("OpenSSL_jll"; compat="3.0.15"), # we need 3.0.15 for aarch64-unknown-freebsd
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
