b87263ad752b8b0a2e31ae624a028024306d4cae
André Guedes <andre.guedes@relational.ai>
2024-09-26 15:43:01 -0300

    Upgrade object_store_ffi to v0.8.1

--- O/object_store_ffi/build_tarballs.jl
+++ O/object_store_ffi/build_tarballs.jl
@@ -1,11 +1,11 @@
 using BinaryBuilder
 
 name = "object_store_ffi"
-version = v"0.8.0"
+version = v"0.8.1"
 
 sources = [
-    # https://github.com/RelationalAI/object_store_ffi/commit/05fd11411ae1c6540d163739324459ea1fb7a848
-    GitSource("https://github.com/RelationalAI/object_store_ffi.git", "05fd11411ae1c6540d163739324459ea1fb7a848")
+    # https://github.com/RelationalAI/object_store_ffi/commit/8ab05166d5ecd5ee6c25d602483f35a458d4b19a
+    GitSource("https://github.com/RelationalAI/object_store_ffi.git", "8ab05166d5ecd5ee6c25d602483f35a458d4b19a")
 ]
 
 # Bash recipe for building across all platforms
