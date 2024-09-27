ba6e838fd76e9c0705a8eb65528df6de45c3a7ba
Maarten Pronk <git@evetion.nl>
2024-09-27 08:09:24 +0200

    Make cd a oneliner.

--- J/JSON_C/build_tarballs.jl
+++ J/JSON_C/build_tarballs.jl
@@ -12,8 +12,7 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd json-c-*
+cd $WORKSPACE/srcdir/json-c-*
 cmake -B build \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
