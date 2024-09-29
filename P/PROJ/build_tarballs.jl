b7830cee1a4d47061320f46ccfb5ff5eec0c38d3
Martijn Visser <mgvisser@gmail.com>
2024-09-29 14:22:49 +0000

    Disable aarch64 for freebsd for now

--- P/PROJ/build_tarballs.jl
+++ P/PROJ/build_tarballs.jl
@@ -61,6 +61,8 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
+# Disable until the dependencies are available for this platform
+filter!(p -> !(Sys.isfreebsd(p) && arch(p) == "aarch64"), platforms)
 
 # The products that we will ensure are always built
 products = [
