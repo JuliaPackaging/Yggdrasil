e2315a26149a8ed31e6517af3233e659bd07d31a
Billy Moses <gh@wsmoses.com>
2024-10-02 17:14:09 +0000

    fix

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -9,7 +9,7 @@ repo = "https://github.com/EnzymeAD/Reactant.jl.git"
 version = v"0.0.19"
 
 sources = [
-  GitSource(repo, "ffcfb29e0f5bbf5170973936ecca83502b2c50c1"),
+  GitSource(repo, "df6316384f420f889dcdff5d1489e272a6d0add3"),
   ArchiveSource("https://github.com/bazelbuild/bazel/releases/download/6.5.0/bazel-6.5.0-dist.zip",
                 "fc89da919415289f29e4ff18a5e01270ece9a6fe83cb60967218bac4a3bb3ed2"; unpack_target="bazel-dist"),
 ]
