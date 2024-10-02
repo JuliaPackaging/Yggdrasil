1672dc494558f7b2acc8f33dfc416311cdd10d0b
Erik Schnetter <schnetter@gmail.com>
2024-10-02 15:09:19 -0400

    AMReX: Trigger rebuild

--- A/AMReX/build_tarballs.jl
+++ A/AMReX/build_tarballs.jl
@@ -109,15 +109,18 @@ platforms = filter(p -> libgfortran_version(p).major ≥ 5, platforms)
 # We cannot build with musl since AMReX requires the `fegetexcept` GNU API
 platforms = filter(p -> libc(p) ≠ "musl", platforms)
 
-platforms, platform_dependencies = MPI.augment_platforms(platforms; MPItrampoline_compat="5.5.0", OpenMPI_compat="4.1.6, 5")
+platforms, platform_dependencies = MPI.augment_platforms(platforms;
+                                                         MPICH_compat="4.2.3",
+                                                         MPItrampoline_compat="5.5.0",
+                                                         OpenMPI_compat="4.1.6, 5")
 # Avoid platforms where the MPI implementation isn't supported
 # OpenMPI
 platforms = filter(p -> !(p["mpi"] == "openmpi" && arch(p) == "armv6l" && libc(p) == "glibc"), platforms)
 # MPItrampoline
 platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && (Sys.iswindows(p) || libc(p) == "musl")), platforms)
 
-# Give up on Windows
-hdf5_platforms = filter(!Sys.iswindows, platforms)
+# HDF5 has not yet been built for aarch64-unknown-freebsd. Re-enable once it's available.
+hdf5_platforms = filter(p -> !(Sys.iswindows(p) || (Sys.isfreebsd(p) && arch(p) == "aarch64")), platforms)
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
