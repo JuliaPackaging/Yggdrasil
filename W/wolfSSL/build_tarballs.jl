a25b04d0aeb880618a01bb2cc4ab54803ee8898f
Dan Taras <dan.taras@graystonetrading.com>
2024-10-08 15:29:59 +0200

    wolfSSL: Exclude failing platforms

--- W/wolfSSL/build_tarballs.jl
+++ W/wolfSSL/build_tarballs.jl
@@ -31,7 +31,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental = true)
+platforms = supported_platforms(; exclude = x -> Sys.iswindows(x) || Sys.isapple(x))
 
 
 # The products that we will ensure are always built
