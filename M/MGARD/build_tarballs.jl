cebcd7bebe4d2adf7520400b5f2d038f074782c9
Erik Schnetter <schnetter@gmail.com>
2024-10-08 13:23:25 -0400

    MGARD: Use GCC 8

--- M/MGARD/build_tarballs.jl
+++ M/MGARD/build_tarballs.jl
@@ -45,5 +45,6 @@ dependencies = [
     Dependency("protoc_jll"),
 ]
 
+# We need at least GCC 8 for C++17
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6")
+               julia_compat="1.6", preferred_gcc_version=v"8")
