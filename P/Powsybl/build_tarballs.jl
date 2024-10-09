c80d344f31f4c7234ebb2ed2954982eeab95669b
Bertrand Rix <bertrand.rix@artelys.com>
2024-10-09 09:45:14 +0200

    Switch to 1.7.0 and latest powsybl.jl.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- P/Powsybl/build_tarballs.jl
+++ P/Powsybl/build_tarballs.jl
@@ -3,8 +3,8 @@ using BinaryBuilder, Pkg
 name = "Powsybl"
 version = v"0.1"
 sources = [
-    GitSource("https://github.com/powsybl/powsybl.jl.git", "b1f9ec74fc703d85f5d34c8a5f7148a316852f1d"),
-    GitSource("https://github.com/powsybl/pypowsybl.git", "4b2c40531890760872ee85c1b37dc600d6bbfeae")
+    GitSource("https://github.com/powsybl/powsybl.jl.git", "f019cc3fb0ef19c41812780d068d302b7ef879bd"),
+    GitSource("https://github.com/powsybl/pypowsybl.git", "cd5fea41bbfb2897fd71a6e63b2d07a465055699")
 ]
 
 julia_versions = [v"1.7", v"1.8", v"1.9", v"1.10"]
@@ -14,13 +14,13 @@ cd $WORKSPACE/srcdir
 
 # Get binary for powsybl-java, generated with GraalVm
 if [[ "${target}" == *-mingw* ]]; then
-    wget https://github.com/powsybl/pypowsybl/releases/download/vBinariesDeployment/binaries-vBinariesDeployment-windows.zip -O powsybl-java.zip
+    wget https://github.com/powsybl/pypowsybl/releases/download/v1.7.0/binaries-v1.7.0-windows.zip -O powsybl-java.zip
 fi
 if [[ "${target}" == *-linux-* ]]; then
-    wget https://github.com/powsybl/pypowsybl/releases/download/vBinariesDeployment/binaries-vBinariesDeployment-linux.zip -O powsybl-java.zip
+    wget https://github.com/powsybl/pypowsybl/releases/download/v1.7.0/binaries-v1.7.0-linux.zip -O powsybl-java.zip
 fi
 if [[ "${target}" == *-apple-* ]]; then
-    wget https://github.com/powsybl/pypowsybl/releases/download/vBinariesDeployment/binaries-vBinariesDeployment-darwin.zip -O powsybl-java.zip
+    wget https://github.com/powsybl/pypowsybl/releases/download/v1.7.0/binaries-v1.7.0-darwin.zip -O powsybl-java.zip
 fi
 unzip powsybl-java.zip -d $prefix
 
@@ -41,8 +41,6 @@ platforms = vcat(libjulia_platforms.(julia_versions)...)
 
 filter!(p -> arch(p) == "x86_64" && os(p) ∈ ("windows", "linux", "macos"), platforms)
 platforms = expand_cxxstring_abis(platforms)
-
-@show platforms
 
 products = [
 LibraryProduct(["math", "libmath"], :libmath)
