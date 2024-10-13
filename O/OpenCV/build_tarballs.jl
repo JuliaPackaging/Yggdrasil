1e71d812ca79ff1225dc4b49e24341811b880b05
Viral B. Shah <744411+ViralBShah@users.noreply.github.com>
2024-10-13 10:43:20 -0400

    Update O/OpenCV/build_tarballs.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- O/OpenCV/build_tarballs.jl
+++ O/OpenCV/build_tarballs.jl
@@ -127,4 +127,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.10", preferred_gcc_version = v"9")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"9")
