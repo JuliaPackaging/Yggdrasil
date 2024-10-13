63910609e1d0c6e91cf9801bd0dfcf0975bb6594
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2024-10-13 16:53:19 -0500

    Update U/Uno/build_tarballs.jl

--- U/Uno/build_tarballs.jl
+++ U/Uno/build_tarballs.jl
@@ -22,7 +22,6 @@ cmake \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
-    -DBLA_VENDOR="libblastrampoline" \
     -DMUMPS_INCLUDE_DIR=${includedir} \
     -DMUMPS_LIBRARY=${libdir} \
     ..
