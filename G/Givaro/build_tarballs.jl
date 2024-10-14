955eb290a9612326854736598d4159480516db5a
laurentbartholdi <laurent.bartholdi@gmail.com>
2024-10-14 11:38:43 +0000

    push

--- G/Givaro/build_tarballs.jl
+++ G/Givaro/build_tarballs.jl
@@ -7,35 +7,38 @@ version = v"4.2.0"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/linbox-team/givaro/releases/download/v4.2.0/givaro-4.2.0.tar.gz", "865e228812feca971dfb6e776a7bc7ac959cf63ebd52b4f05492730a46e1f189"),
+    ArchiveSource("https://github.com/linbox-team/givaro/releases/download/v$version/givaro-$version.tar.gz", "865e228812feca971dfb6e776a7bc7ac959cf63ebd52b4f05492730a46e1f189"),
     DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd ${WORKSPACE}/srcdir/givaro
+cd ${WORKSPACE}/srcdir/givaro-4.2.0
 
 for f in ${WORKSPACE}/srcdir/patches/*.patch; do
     atomic_patch -p0 ${f}
 done
 
+autoreconf
 ./configure CCNAM=${CC} CPLUS_INCLUDE_PATH=$includedir --prefix=$prefix --build=${MACHTYPE} --host=${target}
 
 # really ugly! but I see no other solution for now.
-if [[ ${target} != aarch64-apple-darwin* ]]; then
-#    patch -p0 < ${WORKSPACE}/srcdir/patches/libtool-aarch64-apple-darwin.hack
+# this patch modifies libtool so that it doesn't use the not-yet-implemented "-r" flag for the linker, replacing it by -dynamiclib
+if [[ ${target} =~ aarch64-apple-darwin* ]]; then
+    echo "Patching libtool so it doesn't use ld -r"
+    patch -p0 < ${WORKSPACE}/srcdir/patches/libtool-aarch64-apple-darwin.hack
 fi
 
 make -j ${nproc}
 make install
 
-install_license LICENSE
+install_license Licence_CeCILL-B_V1-en.txt
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()                                                       
-filter!(!Sys.iswindows, platforms)
+platforms = [Platform("aarch64","macos"), Platform("x86_64","macos")]
+# building on x86_64-linux fails with error "undefined reference to `memcpy@GLIBC_2.14'" when linking gmp
 
 # The products that we will ensure are always built
 products = Product[
