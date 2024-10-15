3e515239b8cff4356172f4490bc205139271c683
Oscar Dowson <odow@users.noreply.github.com>
2024-10-15 15:40:27 +1300

    Update build_tarballs.jl

--- U/Uno/build_tarballs.jl
+++ U/Uno/build_tarballs.jl
@@ -19,13 +19,21 @@ mkdir -p build
 cd build
 
 if [[ "${target}" == *mingw* ]]; then
-    LBT=libblastrampoline-5
+    LBT=blastrampoline-5
 else
-    LBT=libblastrampoline
+    LBT=blastrampoline
 fi
+
+if [[ "${target}" == *apple* ]] || [[ "${target}" == *freebsd* ]]; then
+    OMP=omp
+else
+    OMP=gomp
+fi
+bob
 
 cmake \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_PREFIX_PATH=${libdir} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
     -Damplsolver=${libdir}/libasl.${dlext} \
@@ -36,36 +44,36 @@ cmake \
     -DMUMPS_COMMON_LIBRARY="${libdir}/libmumps_common.${dlext}" \
     -DMUMPS_PORD_LIBRARY="${libdir}/libpord.${dlext}" \
     -DMUMPS_MPISEQ_LIBRARY="${libdir}/libmpiseq.${dlext}" \
-    -DBLAS_LIBRARIES="${libdir}/${LBT}.${dlext}" \
-    -DLAPACK_LIBRARIES="${libdir}/${LBT}.${dlext}" \
+    -DBLAS_LIBRARIES="${libdir}/lib${LBT}.${dlext}" \
+    -DLAPACK_LIBRARIES="${libdir}/lib${LBT}.${dlext}" \
     ..
 
 make -j${nproc}
-install -Dvm 644 libuno.a -t ${prefix}/lib
-
-# We need -fPIC to create a shared library
-# $CXX -fPIC -shared $(flagon -Wl,--whole-archive) libuno.a $(flagon -Wl,--no-whole-archive) -o "${libdir}/libuno.${dlext}"
+cp libuno.a ${libdir}/libuno.a
+cp uno_ampl${exeext} ${bindir}/uno_ampl${exeext}
+# ${CXX} -shared $(flagon -Wl,--whole-archive) libuno.a $(flagon -Wl,--no-whole-archive) -o "${libdir}/libuno.${dlext}" -L${OMP} -l${LBT} -ldmumps -lmetis
 """
 
 platforms = supported_platforms()
 
 products = [
-    # ExecutableProduct("Uno", :amplexe),
+    ExecutableProduct("uno_ampl", :amplexe),
     FileProduct("lib/libuno.a", :libuno),
 ]
 
 dependencies = [
+    Dependency(PackageSpec(name="METIS_jll", uuid="d00139f3-1899-568f-a2f0-47f597d42d70")),
     Dependency(PackageSpec(name="ASL_jll", uuid="ae81ac8f-d209-56e5-92de-9978fef736f9"), compat="0.1.3"),
     Dependency(PackageSpec(name="MUMPS_seq_jll", uuid="d7ed1dd3-d0ae-5e8e-bfb4-87a502085b8d")),
     Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"), compat="5.4.0"),
-    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
-    # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
+    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD systems),
+    # and libgomp from `CompilerSupportLibraries_jll` everywhere else.
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
 
-    # We need at least 3.29 (Ygg version), or 3.30 upstream version
-    # for LBT support, so always pull the most recent CMake version.
-    HostBuildDependency(PackageSpec(; name="CMake_jll")),
+    # We need at least 3.29 (Ygg version), or 3.30 upstream version for LBT support,
+    # so always pull the most recent CMake version.
+    HostBuildDependency(PackageSpec(name="CMake_jll", uuid="3f4e10e2-61f2-5801-8945-23b9d642d0e6")),
 ]
 
 build_tarballs(
