0fecbbc5eb8d3ff93f2fe61cd86bc8105f10ef3a
notinaboat <73502641+notinaboat@users.noreply.github.com>
2024-10-16 13:44:57 +1100

    Update build_tarballs.jl

--- L/LVGL_SDL/build_tarballs.jl
+++ L/LVGL_SDL/build_tarballs.jl
@@ -3,82 +3,34 @@
 using BinaryBuilder, Pkg
 
 name = "LVGL_SDL"
-version = v"9.2.0"
+version = v"9.3.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/lvgl/lv_port_pc_eclipse.git", "ba45efb68b125aa93237e7c75c51d4cd65fc10cf")
+    GitSource("https://github.com/lvgl/lvgl.git", "4f086111a107718b719a45e2cc422f5b756d7edd"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd lv_port_pc_eclipse/
-git submodule update --init --recursive
-cd lvgl/
-patch -p1 << "EOF"
-diff --git a/env_support/cmake/custom.cmake b/env_support/cmake/custom.cmake
-index e4cd36376..0e1d4b9d0 100644
---- a/env_support/cmake/custom.cmake
-+++ b/env_support/cmake/custom.cmake
-@@ -26,6 +26,7 @@ file(GLOB_RECURSE THORVG_SOURCES ${LVGL_ROOT_DIR}/src/libs/thorvg/*.cpp ${LVGL_R
- # Build LVGL library
- add_library(lvgl ${SOURCES})
- add_library(lvgl::lvgl ALIAS lvgl)
-+target_link_libraries(lvgl lvgl_thorvg ${SDL2_LIBRARIES})
- 
- target_compile_definitions(
-   lvgl PUBLIC $<$<BOOL:${LV_LVGL_H_INCLUDE_SIMPLE}>:LV_LVGL_H_INCLUDE_SIMPLE>
-@@ -49,7 +50,7 @@ if(NOT LV_CONF_BUILD_DISABLE_THORVG_INTERNAL)
-     add_library(lvgl_thorvg ${THORVG_SOURCES})
-     add_library(lvgl::thorvg ALIAS lvgl_thorvg)
-     target_include_directories(lvgl_thorvg SYSTEM PUBLIC ${LVGL_ROOT_DIR}/src/libs/thorvg)
--    target_link_libraries(lvgl_thorvg PUBLIC lvgl)
-+    #    target_link_libraries(lvgl_thorvg PUBLIC lvgl)
- endif()
- 
- if(NOT (CMAKE_C_COMPILER_ID STREQUAL "MSVC"))
-EOF
+cd $WORKSPACE/srcdir/lvgl
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/*.patch
+if [[ "${target}" == "x86_64-apple-darwin14" ]]
+then
+    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/x86_64-apple-darwin14/*.patch
+fi
+cp lv_conf_template.h lv_conf.h
+cp lv_conf.h ..
 
-patch -p1 << "EOF"
-diff --git a/src/drivers/sdl/lv_sdl_window.c b/src/drivers/sdl/lv_sdl_window.c
-index df8a28e4e..40c5c0bcd 100644
---- a/src/drivers/sdl/lv_sdl_window.c
-+++ b/src/drivers/sdl/lv_sdl_window.c
-@@ -438,7 +438,12 @@ static void * sdl_draw_buf_realloc_aligned(void * ptr, size_t new_size)
-     /* Size must be multiple of align, See: https://en.cppreference.com/w/c/memory/aligned_alloc */
- 
- #define BUF_ALIGN (LV_DRAW_BUF_ALIGN < sizeof(void *) ? sizeof(void *) : LV_DRAW_BUF_ALIGN)
--    return aligned_alloc(BUF_ALIGN, LV_ALIGN_UP(new_size, BUF_ALIGN));
-+    void* result;
-+    if (posix_memalign(&result, BUF_ALIGN, LV_ALIGN_UP(new_size, BUF_ALIGN)) == 0)
-+    {
-+        return result;
-+    }
-+    return NULL;
- #else
-     return _aligned_malloc(LV_ALIGN_UP(new_size, LV_DRAW_BUF_ALIGN), LV_DRAW_BUF_ALIGN);
- #endif /* _WIN32 */
-EOF
-
-cd ..
-patch -p1 << "EOF"
-diff --git a/CMakeLists.txt b/CMakeLists.txt
-index 78eaf02..1753f9f 100644
---- a/CMakeLists.txt
-+++ b/CMakeLists.txt
-@@ -1,6 +1,8 @@
- cmake_minimum_required(VERSION 3.10)
- project(lvgl)
- 
-+option(BUILD_SHARED_LIBS "Build shared libraries" ON)
-+
- option(LV_USE_DRAW_SDL "Use SDL draw unit" OFF)
- option(LV_USE_LIBPNG "Use libpng to decode PNG" OFF)
- option(LV_USE_LIBJPEG_TURBO "Use libjpeg turbo to decode JPEG" OFF)
-EOF
-
-cmake -B build -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
+cmake -B build \
+      -DBUILD_SHARED_LIBS=ON \
+      -DLIB_INSTALL_DIR=${prefix}/lib \
+      -DLV_CONF_BUILD_DISABLE_DEMOS=1 \
+      -DLV_CONF_BUILD_DISABLE_EXAMPLES=1 \
+      -DLV_CONF_INCLUDE_SIMPLE=OFF \
+      -DCMAKE_INSTALL_PREFIX=${prefix} \
+      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+      -DCMAKE_BUILD_TYPE=Release
 cmake --build build --parallel ${nproc}
 cmake --install build
 """
