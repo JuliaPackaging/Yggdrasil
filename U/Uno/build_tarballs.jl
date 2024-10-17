a30c6b78ceb73811653f4fa2f9839629400d5ce8
Oscar Dowson <odow@users.noreply.github.com>
2024-10-18 12:08:38 +1300

    [Uno] bump gitsha without updating version (#9634)
    * [Uno] bump gitsha without updating version
    
    * Update U/Uno/build_tarballs.jl

--- U/Uno/build_tarballs.jl
+++ U/Uno/build_tarballs.jl
@@ -9,7 +9,7 @@ version = v"1.1.0"
 sources = [
     GitSource(
         "https://github.com/cvanaret/Uno.git",
-        "1f373ce25049e593088bee66b37a9057039ac19a",
+        "0ccd578d7c0b3ee9e3efd7c04aa1918e49867f77",
     ),
 ]
 
