71ad798f10701017bea0b3da1e616ab56b881684
Tim Besard <tim.besard@gmail.com>
2024-10-21 17:19:28 +0200

    Try without bumping glibc.

--- A/AMGX/build_tarballs.jl
+++ A/AMGX/build_tarballs.jl
@@ -8,19 +8,16 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "AMGX"
 version = v"2.4.0"
+
+# Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/NVIDIA/AMGX.git",
               "2b4762f02af2ed136134c7f0570646219753ab3e"),
     DirectorySource("./bundled")
 ]
 
+# Bash recipe for building across all platforms
 script = raw"""
-# check if we need to use a more recent glibc
-if [[ -f "$prefix/usr/include/sched.h" ]]; then
-    GLIBC_ARTIFACT_DIR=$(dirname $(dirname $(dirname $(realpath $prefix/usr/include/sched.h))))
-    rsync --archive ${GLIBC_ARTIFACT_DIR}/ /opt/${target}/${target}/sys-root/
-fi
-
 # nvcc writes to /tmp, which is a small tmpfs in our sandbox.
 # make it use the workspace instead
 export TMPDIR=${WORKSPACE}/tmpdir
@@ -53,41 +50,27 @@ make -j${nproc} install
 
 # clean-up
 ## unneeded static libraries
-rm ${libdir}/*.a ${libdir}/sublibs/*.a
+rm ${libdir}/*.a
 """
 
-augment_platform_block = CUDA.augment
-
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
 platforms = CUDA.supported_platforms()
 filter!(p -> arch(p) == "x86_64", platforms)
 
-# some platforms need a newer glibc, because the default one is too old
-glibc_platforms = filter(platforms) do p
-    libc(p) == "glibc" && proc_family(p) in ["intel", "power"]
-end
-
+# The products that we will ensure are always built
 products = [
     LibraryProduct("libamgxsh", :libamgxsh),
 ]
 
-# build AMGX for all supported CUDA toolkits
+# Build for all supported CUDA toolkits
 for platform in platforms
     should_build_platform(triplet(platform)) || continue
 
     dependencies = CUDA.required_dependencies(platform; static_sdk=true)
 
-    cuda_version = VersionNumber(platform["cuda"])
-    if cuda_version >= v"12"
-        # CUDA 12 requires glibc 2.17
-        # which isn't compatible with current Linux kernel headers,
-        # so use the next packaged version
-        push!(dependencies,
-              BuildDependency(PackageSpec(name = "Glibc_jll", version = v"2.19");
-                              platforms=glibc_platforms),)
-    end
-
     build_tarballs(ARGS, name, version, sources, script, [platform],
                    products, dependencies; lazy_artifacts=true,
-                   julia_compat="1.6", augment_platform_block,
+                   julia_compat="1.6", augment_platform_block=CUDA.augment,
                    dont_dlopen=true, preferred_gcc_version=v"9")
 end
