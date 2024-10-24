de89a238be0d57af41e58259be3cefad05cdb08a
Bart Janssens <bart@bartjanssens.org>
2024-10-24 07:44:56 +0200

    Switch to Qt6

--- O/OpenCV/build_tarballs.jl
+++ O/OpenCV/build_tarballs.jl
@@ -133,10 +133,10 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Qt5Base_jll", uuid="ea2cea3b-5b76-57ae-a6ef-0a8af62496e1"))
-    Dependency(PackageSpec(name="Libglvnd_jll", uuid="7e76a0d4-f3c7-5321-8279-8d96eeed0f29"))
-    BuildDependency(PackageSpec(name="libjulia_jll"))
-    Dependency(PackageSpec(name="libcxxwrap_julia_jll", uuid="3eaa8342-bff7-56a5-9981-c04077f7cee7"); compat="0.13")
+    Dependency("Qt6Base_jll"; compat="~6.7.1"),
+    Dependency(PackageSpec(name="Libglvnd_jll", uuid="7e76a0d4-f3c7-5321-8279-8d96eeed0f29")),
+    BuildDependency(PackageSpec(name="libjulia_jll")),
+    Dependency(PackageSpec(name="libcxxwrap_julia_jll", uuid="3eaa8342-bff7-56a5-9981-c04077f7cee7"); compat="0.13"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
