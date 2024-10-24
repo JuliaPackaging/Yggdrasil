1e757525a60208f1e67a76de94681f0af0bce572
kevmoor <kevmoor@sandia.gov>
2024-10-24 16:22:08 -0600

    Pull in Frederiks updates and try gcc 14

--- O/OWENSOpenFAST/build_tarballs.jl
+++ O/OWENSOpenFAST/build_tarballs.jl
@@ -7,8 +7,11 @@ version = v"1.0.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/andrew-platt/openfast.git", "24c05a744f9d93877a7f7ac32adef469b3cd8269"),
-    DirectorySource(joinpath(@__DIR__, "bundled"))
+    # These sources are for the initial build
+    # GitSource("https://github.com/andrew-platt/openfast.git", "24c05a744f9d93877a7f7ac32adef469b3cd8269"),
+    # DirectorySource("./bundled"),
+    # Try building upstream dev branch (23 Oct)
+    GitSource("https://github.com/OpenFAST/openfast.git", "c5cdcdd97e1b5db9ee5bbc146c57f69b51e089bd"),
 ]
 
 # Bash recipe for building across all platforms
@@ -24,49 +27,49 @@ mkdir build && cd build
 cmake .. \
 -DCMAKE_INSTALL_PREFIX=${prefix} \
 -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
--DCMAKE_BUILD_TYPE=Debug \
+-DCMAKE_BUILD_TYPE=Release \
 -DBUILD_SHARED_LIBS=ON \
 -DBUILD_FASTFARM=OFF \
 -DORCA_DLL_LOAD=OFF \
--DOPENMP=ON \
+-DOPENMP=OFF \
 -DBLAS_LIBRARIES="${libdir}/libopenblas.${dlext}" \
 -DLAPACK_LIBRARIES="${libdir}/libopenblas.${dlext}"
 
 #WARNING: compiling this locally can go crazy and lock up your machine, using only 2 jobs and Debug version to make it behave
-make -j2
+make -j${nproc}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 
-platforms = expand_gfortran_versions(supported_platforms(; experimental=true))
+platforms = expand_cxxstring_abis(expand_gfortran_versions(supported_platforms(; experimental = true)))
 
-#remove aarch64-linux-musl from platforms, this platform does not currently have IEEE_ARITHMETIC enabled for gfortran under the current configure set up
-#see also: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100662
-# filter!(p -> !(arch(p) == "aarch64" && Sys.islinux(p) && libc(p) == "musl"), platforms)
+# Filter out aarch64-linux-musl and aarch64-unknown-freebsd from platforms, this platform
+# does not currently have IEEE_ARITHMETIC enabled for gfortran under the current configure
+# set up see also: https://gcc.gnu.org/bugzilla/show_bug.cgi?id=100662
+filter!(p -> !(arch(p) == "aarch64" && Sys.islinux(p) && libc(p) == "musl"), platforms)
+filter!(p -> !(arch(p) == "aarch64" && Sys.isfreebsd(p)), platforms)
 
-#remove aarch-apple-darwin platforms, same issue as aarch64-linux-musl
-# filter!(p -> !(Sys.isapple(p) && arch(p) == "aarch64"), platforms)
+# Filter out armv6l and armv7l
+filter!(p -> arch(p) ∉ ("armv6l", "armv7l"), platforms)
 
-#filter arm platforms
-# filter!(p -> arch(p) ∉ ("armv6l", "armv7l"), platforms)
-
-#filter windows platforms - MinGW is not well supported relative to MSVC
-# filter!(p -> !Sys.iswindows(p), platforms)
+# # TODO: These platforms work, just filtering out while iterating on broken ones
+# filter!(!Sys.islinux, platforms)
+# filter!(!Sys.iswindows, platforms) # TODO: x86_64 builds uses avx2 instructions
+# filter!(p -> !(Sys.isfreebsd(p) && arch(p) == "x86_64"), platforms)
 
 # The products that we will ensure are always built
-products = [    
-    LibraryProduct("ifw_c_binding", :ifw_c_binding),
-    LibraryProduct("moordyn_c_binding", :libmoordynlib),
-    LibraryProduct("hydrodyn_c_binding", :aerodyn_inflow_c_binding),
-    LibraryProduct("aerodyn_inflow_c_binding", :aerodyn_inflow_c_binding),
-    ExecutableProduct("turbsim", :turbsim),  
-    ExecutableProduct("inflowwind_driver", :inflowwind_driver),  
-    ExecutableProduct("aerodyn_driver", :hydrodyn_driver),
-    ExecutableProduct("moordyn_driver", :hydrodyn_driver),
+products = [
+    LibraryProduct("libifw_c_binding", :libifw_c_binding),
+    LibraryProduct("libmoordyn_c_binding", :libmoordyn_c_binding),
+    LibraryProduct("libhydrodyn_c_binding", :libhydrodyn_c_binding),
+    LibraryProduct("libaerodyn_inflow_c_binding", :libaerodyn_inflow_c_binding),
+    ExecutableProduct("turbsim", :turbsim),
+    ExecutableProduct("inflowwind_driver", :inflowwind_driver),
+    ExecutableProduct("aerodyn_driver", :aerodyn_driver),
+    ExecutableProduct("moordyn_driver", :moordyn_driver),
     ExecutableProduct("hydrodyn_driver", :hydrodyn_driver),
-    ExecutableProduct("aerodyn_driver", :hydrodyn_driver),
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -78,4 +81,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"14", clang_use_lld = false)
