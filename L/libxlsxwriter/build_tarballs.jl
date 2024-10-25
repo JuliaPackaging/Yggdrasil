741d54fd2fb18bfa1daa2c942b8fe1966e67d2a5
AlexKlo <65765845+AlexKlo@users.noreply.github.com>
2024-10-25 14:52:34 +0400

    Exclude Windows i686

--- L/libxlsxwriter/build_tarballs.jl
+++ L/libxlsxwriter/build_tarballs.jl
@@ -21,25 +21,7 @@ cmake --install build
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("x86_64", "macos"; ),
-    Platform("aarch64", "macos"; ),
-    Platform("x86_64", "freebsd"; ),
-    Platform("aarch64", "freebsd"; ),
-    Platform("x86_64", "windows"; )
-]
-
+platforms = supported_platforms(; exclude=p -> Sys.iswindows(p) && arch(p) == "i686")
 
 # The products that we will ensure are always built
 products = [
