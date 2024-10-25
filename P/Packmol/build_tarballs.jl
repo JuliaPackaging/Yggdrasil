98ba01c78f3bfeb099fc55154dbce43f024a205d
Leandro Martínez <31046348+lmiq@users.noreply.github.com>
2024-10-25 12:24:00 -0300

    update packmol to 20.15.2

--- P/Packmol/build_tarballs.jl
+++ P/Packmol/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "Packmol"
-version = v"20.15.1"
+version = v"20.15.2"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/m3g/packmol", "d6cd4b790adb6c9c1377690c8da8af043c33554d"),
+    GitSource("https://github.com/m3g/packmol", "0f4e5797a7f94e0d906e2c2e45203849e29b3e91"),
 ]
 
 # Bash recipe for building across all platforms
