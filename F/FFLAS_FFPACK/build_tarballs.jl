634052aaf014485ee7d58097eeb173202f631140
laurentbartholdi <laurent.bartholdi@gmail.com>
2024-10-26 12:01:29 +0000

    Removed expand_cxxstring_abis

--- F/FFLAS_FFPACK/build_tarballs.jl
+++ F/FFLAS_FFPACK/build_tarballs.jl
@@ -30,7 +30,7 @@ install_license COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(exclude=Sys.iswindows) |> expand_cxxstring_abis
+platforms = supported_platforms(exclude=Sys.iswindows)
 
 # The products that we will ensure are always built
 products = [
