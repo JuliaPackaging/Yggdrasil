1912c2f0af5051cfece3719f851f74c0793c4a9c
Elliot Saba <staticfloat@gmail.com>
2024-10-27 18:38:30 -0700

    Update L/ldid/build_tarballs.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- L/ldid/build_tarballs.jl
+++ L/ldid/build_tarballs.jl
@@ -30,7 +30,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("libplist_jll"),
-    Dependency("OpenSSL_jll"; compat="3"),
+    Dependency("OpenSSL_jll"; compat="3.0.8"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
