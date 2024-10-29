7ac46ca992690f532c910b34d9e6c704b05b981c
laurentbartholdi <laurent.bartholdi@gmail.com>
2024-10-29 20:30:42 +0000

    first draft with packaging

--- S/Spasm/build_tarballs.jl
+++ S/Spasm/build_tarballs.jl
@@ -8,39 +8,19 @@ version = v"1.3.0"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/laurentbartholdi/spasm.git", "072719a40c837e447dfe4ae9e4941c60d9a28eda"),
-    GitSource("https://github.com/linbox-team/givaro.git", "fc6cac7820539c900dde332326c71461ba7b910b"),
-    GitSource("https://github.com/linbox-team/fflas-ffpack.git", "94aa88263f5c6032adb5c86bf806f007fec7aded"),
-    GitSource("https://github.com/ericonr/argp-standalone.git", "743004c68e7358fb9cd4737450f2d9a34076aadf"),
     DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd ${WORKSPACE}/srcdir
+cd ${WORKSPACE}/srcdir/spasm
 
 for f in ${WORKSPACE}/srcdir/patches/*.patch; do
-    atomic_patch -p0 ${f}
+    atomic_patch -p1 ${f}
 done
 
-cd ${WORKSPACE}/srcdir/argp-standalone
-autoreconf -i
-./configure --prefix=$prefix --build=${MACHTYPE} --host=${target}
-make
-install argp.h $includedir
-install libargp.a $libdir
-
-cd ${WORKSPACE}/srcdir/givaro
-./autogen.sh CCNAM=gcc CPLUS_INCLUDE_PATH=$includedir --prefix=$prefix --build=${MACHTYPE} --host=${target}
-make -j${nproc}
-make install
-
-cd ${WORKSPACE}/srcdir/fflas-ffpack
-./autogen.sh CPLUS_INCLUDE_PATH=$includedir --prefix=$prefix --build=${MACHTYPE} --host=${target}
-make install
-
-cd ${WORKSPACE}/srcdir/spasm
 cmake -B build -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
-cmake --build build --parallel ${nproc}
+cmake --build build
 cmake --install build
 
 install_license ${WORKSPACE}/srcdir/spasm/COPYING
@@ -48,19 +28,24 @@ install_license ${WORKSPACE}/srcdir/spasm/COPYING
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(exclude=Sys.iswindows)
 
 platforms = [Platform("x86_64", "linux")] # for testing, for now
 
 # The products that we will ensure are always built
 products = Product[
+    LibraryProduct("libspasm", :spasm)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    Dependency("Givaro_jll"; compat="4.2.0"),
+    Dependency("FFLAS_FFPACK_jll"; compat="2.5.0"),
+    Dependency("argp_standalone_jll"),
     Dependency("libblastrampoline_jll"; compat="5.4.0"),
     Dependency("GMP_jll"; compat="6.2.1")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.9")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+  julia_compat="1.9", clang_use_lld=false, referred_gcc_version=v"6"))
