bf841923629dfa938dcf75a542016eadd83e7a4a
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-10-30 13:13:33 +1300

    [libcellml] Update to version 0.6.2 (#9703)
    * Update build_tarballs.jl for release 0.6.2
    
    * Update build_tarballs.jl fix LibXml2 compatibility.

--- L/libcellml/build_tarballs.jl
+++ L/libcellml/build_tarballs.jl
@@ -1,12 +1,12 @@
 using BinaryBuilder, Pkg
 
 name = "libcellml"
-version = v"0.6.0"
+version = v"0.6.2"
 
 sources = [
     GitSource(
-        "https://github.com/hsorby/libcellml",
-        "438f64d536db5f15791b35ffa2e5dfa849f55322"),
+        "https://github.com/cellml/libcellml",
+        "13f7d6666ccba2c226cf2b13566169cb2f4328f4"),
     ArchiveSource(
         "https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
         "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
