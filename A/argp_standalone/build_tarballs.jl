7af3647aa764813bfe04561b1922e0b86de89162
laurentbartholdi <laurent.bartholdi@gmail.com>
2024-10-30 16:49:26 +0000

    First attempt

--- A/argp_standalone/build_tarballs.jl
+++ A/argp_standalone/build_tarballs.jl
@@ -9,12 +9,18 @@ version = v"1.3.1"
 sources = [
     ArchiveSource("http://www.lysator.liu.se/~nisse/misc/argp-standalone-$(version.major).$(version.minor).tar.gz",
                   "dec79694da1319acd2238ce95df57f3680fea2482096e483323fddf3d818d8be"),
-    FileSource("https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt", "8177f97513213526df2cf6184d8ff986c675afb514d4e68a404010521b880643"; filename="LICENSE"),
+    FileSource("https://www.gnu.org/licenses/old-licenses/gpl-2.0.txt", "edaef632cbb643e4e7a221717a6c441a4c1a7c918e6e4d56debc3d8739b233f6"; filename="LICENSE"),
+    DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/argp-*/
+
+for p in $WORKSPACE/srcdir/patches/*.patch; do
+    atomic_patch -p1 $p
+done
+
 CFLAGS="-fPIC" ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make -j${nproc}
 install_license $WORKSPACE/srcdir/LICENSE
@@ -23,7 +29,7 @@ install -D -m755 libargp.a ${libdir}/libargp.a
 """
 
 # Select Unix platforms
-platforms = filter(p->Sys.islinux(p) && libc(p) == "musl", supported_platforms(;experimental=true))
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
@@ -36,4 +42,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.9", preferred_gcc_version=v"6")
