219d9a9ea3042a68f73783fece06c229acb0d9e3
Hugh Sorby <h.sorby@auckland.ac.nz>
2024-10-30 13:06:30 +1300

    Update build_tarballs.jl fix LibXml2 compatibility.

--- L/libcellml/build_tarballs.jl
+++ L/libcellml/build_tarballs.jl
@@ -49,7 +49,7 @@ products = [
 dependencies = [
     # XML2 apparently had a breaking change, so it's important to specify the compat bound:
     # https://github.com/JuliaPackaging/Yggdrasil/pull/9673#issuecomment-2434514026
-    Dependency("XML2_jll"),
+    Dependency("XML2_jll"; compat="2.13.4"),
     Dependency("Zlib_jll"),
 ]
 
