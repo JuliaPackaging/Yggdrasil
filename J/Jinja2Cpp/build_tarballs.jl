d9c2f06ac6d1d526d531594f001c9a9b2171160b
AlexKlo <65765845+AlexKlo@users.noreply.github.com>
2024-10-30 15:42:21 +0400

    Update build_tarballs.jl

--- J/Jinja2Cpp/build_tarballs.jl
+++ J/Jinja2Cpp/build_tarballs.jl
@@ -8,12 +8,24 @@ version = v"1.3.2"
 # Collection of sources required to build CMake
 sources = [
     GitSource("https://github.com/jinja2cpp/Jinja2Cpp.git", "86dfb939b5c2beb7fabddae2df386be4e7fb9507"),
+    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
+    "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
     DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/Jinja2Cpp
+
+if [[ "${target}" == x86_64-apple-darwin* ]]; then
+    # Install a newer SDK which supports `shared_timed_mutex` and `std::filesystem`
+    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
+    rm -rf /opt/${target}/${target}/sys-root/System
+    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
+    cp -ra System "/opt/${target}/${target}/sys-root/."
+    export MACOSX_DEPLOYMENT_TARGET=10.15
+    popd
+fi
 
 apk del cmake
 
