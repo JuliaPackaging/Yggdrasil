77f058c52734eae9231dc655aa5d11c459096066
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2024-11-04 15:48:56 +0200

    rerun

--- O/openfhe_julia/build_tarballs.jl
+++ O/openfhe_julia/build_tarballs.jl
@@ -4,7 +4,7 @@ using BinaryBuilder, Pkg
 
 # See https://github.com/JuliaLang/Pkg.jl/issues/2942
 # Once this Pkg issue is resolved, this must be removed
-uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f") 
+uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "openfhe_julia"
