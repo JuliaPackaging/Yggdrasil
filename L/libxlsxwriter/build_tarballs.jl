db471bcb5df5a2e9a6105defc7564d86c6a571b1
AlexKlo <65765845+AlexKlo@users.noreply.github.com>
2024-11-04 19:11:27 +0400

    Update build_tarballs.jl

--- L/libxlsxwriter/build_tarballs.jl
+++ L/libxlsxwriter/build_tarballs.jl
@@ -12,9 +12,12 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd libxlsxwriter/
-cmake -B build -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON
+cd $WORKSPACE/srcdir/libxlsxwriter/
+cmake -B build \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DBUILD_SHARED_LIBS=ON
 cmake --build build --parallel ${nproc}
 cmake --install build
 """
