843cc394d594951fa94d35449ac6ab56e3b4ba4b
Viral B. Shah <viral@mayin.org>
2024-11-04 13:24:14 -0500

    Update build_tarballs.jl

--- O/Octave/build_tarballs.jl
+++ O/Octave/build_tarballs.jl
@@ -60,6 +60,7 @@ dependencies = [
     Dependency("Readline_jll"),
     Dependency("Libiconv_jll"),
     Dependency("Zlib_jll"),
+    Dependency("BZip2_jl"),
     Dependency("Arpack_jll"),
     Dependency("FFTW_jll"),
     Dependency("LibCURL_jll"),
