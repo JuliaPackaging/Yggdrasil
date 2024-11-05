953bfd5c33810cc7110db1031e9e1cbcfbbe6a25
Sergio Sánchez Ramírez <15837247+mofeing@users.noreply.github.com>
2024-11-05 19:57:28 +0100

    [Reactant] Update version to 0.0.24 (#9739)
    Small bugfix

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -6,10 +6,10 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "Reactant"
 repo = "https://github.com/EnzymeAD/Reactant.jl.git"
-version = v"0.0.23"
+version = v"0.0.24"
 
 sources = [
-  GitSource(repo, "c0e4de6db9b45486cee06b883612ba648a1ac583"),
+  GitSource(repo, "3b305334cc60ea2316d5166b2affbbb71750f05a"),
   FileSource("https://github.com/wsmoses/binaries/releases/download/v0.0.1/bazel-dev",
              "8b43ffdf519848d89d1c0574d38339dcb326b0a1f4015fceaa43d25107c3aade")
 ]
