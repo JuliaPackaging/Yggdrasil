86b3a68eb5a72915ecc7a540d033a6b7b1a8033f
Viral B. Shah <viral@mayin.org>
2024-11-11 17:26:56 +0000

    Revert "Try to enable windows build"
    This reverts commit cd13883fb782ad99f3e9a3dd59646ebfe7a0fd83.

--- I/iODBC/build_tarballs.jl
+++ I/iODBC/build_tarballs.jl
@@ -24,7 +24,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms(; exclude=Sys.iswindows)
 
 
 # The products that we will ensure are always built
