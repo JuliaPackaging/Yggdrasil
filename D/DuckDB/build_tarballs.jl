8050e12c7d7203a92219d8cba46f60a8d4c01730
KronosTheLate <densbal@live.no>
2024-11-13 19:26:17 +0100

    Add special case for windows

--- D/DuckDB/build_tarballs.jl
+++ D/DuckDB/build_tarballs.jl
@@ -1,5 +1,5 @@
-# Note that this script can accept some limited command-line arguments, 
-# run `julia build_tarballs.jl --help` to see a usage message.
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
 name = "DuckDB"
@@ -10,32 +10,40 @@ sources = [
     GitSource("https://github.com/duckdb/duckdb.git", "f680b7d08f56183391b581077d4baf589e1cc8bd"),
 ]
 
-# Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/duckdb/
 
-cmake -B build \
-      -DCMAKE_INSTALL_PREFIX=${prefix} \
-      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-      -DCMAKE_BUILD_TYPE=Release \
-      -DENABLE_SANITIZER=FALSE \
-      -DBUILD_EXTENSIONS='autocomplete;icu;parquet;json;fts;tpcds;tpch' \
-      -DENABLE_EXTENSION_AUTOLOADING=1 \
-      -DENABLE_EXTENSION_AUTOINSTALL=1 \
-      -DBUILD_UNITTESTS=FALSE \
-      -DBUILD_SHELL=TRUE \
-      -DDUCKDB_EXPLICIT_PLATFORM="${target}"
-cmake --build build --parallel ${nproc}
-cmake --install build
+if [[ "$target" == *-ming* ]]; then
+  build_command="cmake -B build -G \"Ninja\""
+  build_extensions="icu;parquet;json"
+else
+  build_command="cmake -B build"
+  build_extensions="autocomplete;icu;parquet;json;fts;tpcds;tpch"
+fi
 
-if [[ "${target}" == *-mingw32 ]]; then
-    install -Dvm 755 "build/src/libduckdb.${dlext}" -t "${libdir}"
+$build_command \
+       -DBUILD_EXTENSIONS="$build_extensions" \
+       -DBUILD_SHELL=TRUE \
+       -DBUILD_UNITTESTS=FALSE \
+       -DCMAKE_BUILD_TYPE=Release \
+       -DCMAKE_INSTALL_PREFIX=$prefix \
+       -DCMAKE_TOOLCHAIN_FILE=$CMAKE_TARGET_TOOLCHAIN \
+       -DDUCKDB_EXPLICIT_PLATFORM="$target" \
+       -DENABLE_EXTENSION_AUTOINSTALL=1 \
+       -DENABLE_EXTENSION_AUTOLOADING=1 \
+       -DENABLE_SANITIZER=FALSE
+ cmake --build build --parallel $nproc
+ cmake --install build
+
+if [[ "$target" == *-w64-mingw32 ]]; then
+  install -Dvm 755 "build/src/libduckdb.${dlext}" -t "${libdir}"
 fi
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
+
 # Building for PowerPC results in errors inside jemalloc:
 #     /tmp/ccmHnfhC.s: Assembler messages:
 #     /tmp/ccmHnfhC.s:7829: Error: unrecognized opcode: `pause'
