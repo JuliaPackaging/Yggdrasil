63e34c290856f7d827857507434f672bc3534bc9
KronosTheLate <densbal@live.no>
2024-11-13 19:25:03 +0100

    Trivial change to triger build

--- D/DuckDB/build_tarballs.jl
+++ D/DuckDB/build_tarballs.jl
@@ -1,5 +1,5 @@
-# Note that this script can accept some limited command-line arguments, run
-# `julia build_tarballs.jl --help` to see a usage message.
+# Note that this script can accept some limited command-line arguments, 
+# run `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
 name = "DuckDB"
