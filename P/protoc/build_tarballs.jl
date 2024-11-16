38dc06877eb214ee22f32ca4006071dda297b5fc
Jesper Stemann Andersen <jesper@sait.dk>
2024-11-16 14:44:05 +0100

    Added static libraries
    Compiled as position independent code, cf. https://github.com/onnx/onnx/blob/v1.11.0/README.md#linux

--- P/protoc/build_tarballs.jl
+++ P/protoc/build_tarballs.jl
@@ -19,6 +19,23 @@ cd $WORKSPACE/srcdir/protobuf
 sed -i -e 's!set(CMAKE_C_COMPILER.*!set(CMAKE_C_COMPILER '${WORKSPACE}/srcdir/files/ccsafe')!' ${CMAKE_TARGET_TOOLCHAIN}
 sed -i -e 's!set(CMAKE_CXX_COMPILER.*!set(CMAKE_CXX_COMPILER '${WORKSPACE}/srcdir/files/c++safe')!' ${CMAKE_TARGET_TOOLCHAIN}
 
+# Build static libraries
+cmake -B work -G Ninja \
+    -DBUILD_SHARED_LIBS=OFF \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=14 \
+    -DCMAKE_FIND_ROOT_PATH=${prefix} \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -Dprotobuf_BUILD_LIBPROTOC=ON \
+    -Dprotobuf_BUILD_TESTS=OFF \
+    cmake
+cmake --build work --parallel ${nproc}
+cmake --install work
+rm -rf work
+
+# Build dynamic libraries
 cmake -B work -G Ninja \
     -DBUILD_SHARED_LIBS=ON \
     -DCMAKE_BUILD_TYPE=Release \
@@ -40,9 +57,12 @@ platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libprotoc", :libprotoc),
+    FileProduct("lib/libprotobuf.a", :libprotobuf_static),
+    FileProduct("lib/libprotobuf-lite.a", :libprotobuf_lite_static),
+    FileProduct("lib/libprotoc.a", :libprotoc_static),
     LibraryProduct("libprotobuf", :libprotobuf),
     LibraryProduct("libprotobuf-lite", :libprotobuf_lite),
+    LibraryProduct("libprotoc", :libprotoc),
     ExecutableProduct("protoc", :protoc),
 ]
 
