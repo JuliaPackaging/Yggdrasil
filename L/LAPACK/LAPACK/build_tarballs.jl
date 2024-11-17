16e17eb1f0c63130c9a0e6981221efed7c3199ef
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-11-17 13:01:41 -0500

    Disable failing builds

--- L/LAPACK/LAPACK/build_tarballs.jl
+++ L/LAPACK/LAPACK/build_tarballs.jl
@@ -10,13 +10,13 @@ products = [
 
 # Building ILP64 LAPACK on aarch64 linux runs into internal compiler errors with
 # GCC ≤ 7 (=> libgfortran ≤ 4).
-#filter!(p -> !(arch(p) == "aarch64" && Sys.islinux(p) && libgfortran_version(p) ≤ v"4"), platforms)
+filter!(p -> !(arch(p) == "aarch64" && Sys.islinux(p) && libgfortran_version(p) ≤ v"4"), platforms)
 
 append!(dependencies,
         [Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"), compat="5.4.0")])
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               clang_use_lld=false, julia_compat="1.9", preferred_gcc_version=v"8")
+               clang_use_lld=false, julia_compat="1.9", preferred_gcc_version=v"6")
 
 # Build Trigger: 3
