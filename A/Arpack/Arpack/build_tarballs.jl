37e28264dd8cb1573df1a944d922d10fee39e027
Viral B. Shah <viral@mayin.org>
2024-11-17 09:43:55 -0500

    [ARPACK32] Build 32-bit version of Arpack (#9740)
    * [ARPACK32] New build for 32-bit ARPACK with OpenBLAS32
    * Bump gcc version to 6
    
    ---------
    
    Co-authored-by: Viral Shah <viralshah@Virals-MacBook-Pro.local>
    Co-authored-by: Viral B. Shah <ViralBShah@users.noreply.github.com>

---
+++ A/Arpack/Arpack/build_tarballs.jl
@@ -0,0 +1,21 @@
+using BinaryBuilder
+
+# Collection of sources required to build Arpack
+name = "Arpack"
+
+version = v"3.9.1"
+
+include("../common.jl")
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libarpack", :libarpack),
+]
+
+# Dependencies that must be installed before this package can be built
+append!(dependencies, [Dependency("libblastrampoline_jll")])
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, arpack_sources(version), build_script(build_32bit=false),
+               platforms, products, dependencies;
+	       julia_compat="1.8", clang_use_lld=false, preferred_gcc_version=v"6")
