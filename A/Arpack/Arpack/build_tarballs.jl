bb047bf8a1f6cc056e51d6ef8f87f1fa95f48e99
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-11-17 15:33:28 -0500

    [ARPACK] Rebuild Arpack32 to use LBT.

--- A/Arpack/Arpack/build_tarballs.jl
+++ A/Arpack/Arpack/build_tarballs.jl
@@ -12,10 +12,7 @@ products = [
     LibraryProduct("libarpack", :libarpack),
 ]
 
-# Dependencies that must be installed before this package can be built
-append!(dependencies, [Dependency("libblastrampoline_jll")])
-
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, arpack_sources(version), build_script(build_32bit=false),
                platforms, products, dependencies;
-	       julia_compat="1.8", clang_use_lld=false, preferred_gcc_version=v"6")
+	       julia_compat="1.9", clang_use_lld=false, preferred_gcc_version=v"6")
