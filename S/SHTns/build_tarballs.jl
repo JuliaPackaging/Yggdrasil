2660c9c95ce65c39a6b4b7e63719e218f7dd9759
Felix Gerick <felixgerick@gmail.com>
2024-11-18 12:50:38 +0000

    start cuda shtns

--- S/SHTns/build_tarballs.jl
+++ S/SHTns/build_tarballs.jl
@@ -2,15 +2,20 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-include(joinpath(@__DIR__, "..", "..", "platforms", "microarchitectures.jl"))
+#YGGDRASILPATH = joinpath(@__DIR__, "..", "..")
+YGGDRASILPATH = joinpath(@__DIR__, "..", "Yggdrasil")
+
+include(joinpath(YGGDRASILPATH, "platforms", "microarchitectures.jl"))
+include(joinpath(YGGDRASILPATH, "platforms", "cuda.jl"))
 
 name = "SHTns"
-version = v"3.6.6"
+version = v"3.7"
+version_string = version.patch == 0 ? string(version.major)*"."*string(version.minor) : string(version)
 
 # Collection of sources required to complete build (note to self: use `sha256sum` to generate the checksum from tarball) 
 sources = [
-    ArchiveSource("https://gricad-gitlab.univ-grenoble-alpes.fr/schaeffn/shtns/-/archive/v$(version)/shtns-v$(version).tar.gz",
-                  "f060757ed6914c837cc2b251d370078e4c92b6894fef7aac189a9a1f5f1521a2")
+    ArchiveSource("https://gricad-gitlab.univ-grenoble-alpes.fr/schaeffn/shtns/-/archive/v$(version_string)/shtns-v$(version_string).tar.gz",
+                  "6c727ccc4d15d3170c3e20ad2b8a721c8b1fd838b1944c7d7e515a4fce43f75c")
 ]
 
 # Bash recipe for building across all platforms
@@ -21,13 +26,11 @@ export CFLAGS="-fPIC -O3" #only -fPIC produces slow code on linux x86 and MacOS 
 #remove lfftw3_omp library references, as FFTW_jll does not provide it
 sed -i -e 's/lfftw3_omp/lfftw3/' configure
 
-./configure --prefix=${prefix} --host=${target} --enable-openmp --enable-kernel-compiler=cc
+./configure --prefix=${prefix} --host=${target} --enable-openmp --enable-cuda --enable-shared --enable-kernel-compiler=cc
 make -j${nproc}
-make install
 
 mkdir -p ${libdir}
-cc -fopenmp -shared -o "${libdir}/libshtns.${dlext}" *.o -lfftw3
-rm "${prefix}/lib/libshtns_omp.a"
+mv libshtns_cuda_omp.so ${libdir}/libshtns.${dltext}
 
 install_license LICENSE
 """
@@ -36,7 +39,7 @@ install_license LICENSE
 # platforms are passed in on the command line
 
 # Expand for microarchitectures on x86_64 (library doesn't have CPU dispatching)
-platforms = expand_microarchitectures(supported_platforms(), ["x86_64", "avx", "avx2", "avx512"])
+cpu_platforms = expand_microarchitectures(supported_platforms(), ["x86_64", "avx", "avx2", "avx512"])
 
 augment_platform_block = """
     $(MicroArchitectures.augment)
@@ -50,6 +53,9 @@ augment_platform_block = """
     end
     """
 
+cuda_platforms = expand_microarchitectures(CUDA.supported_platforms(), ["x86_64", "avx", "avx2", "avx512"])
+platforms = [cpu_platforms; cuda_platforms]
+
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libshtns", :LibSHTns)
@@ -59,13 +65,15 @@ products = [
 dependencies = [
     Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a")),
     # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
-    # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
+    # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else. 
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6",
-               preferred_gcc_version=v"10",
-               augment_platform_block)
+for platform in platforms
+    # Build the tarballs, and possibly a `build.jl` as well.
+    build_tarballs(ARGS, name, version, sources, script, [platform], products, [dependencies; CUDA.required_dependencies(platform)];
+                julia_compat="1.6",
+                preferred_gcc_version=v"10",
+                augment_platform_block)
+end
