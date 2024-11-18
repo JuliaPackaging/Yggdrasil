ca2f395d56a13c03ab2423364c52578c6858fb94
Viral B. Shah <ViralBShah@users.noreply.github.com>
2024-11-17 22:43:26 -0500

    add Pkg

--- Q/QRupdate_ng/build_tarballs.jl
+++ Q/QRupdate_ng/build_tarballs.jl
@@ -1,6 +1,6 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "QRupdate_ng"
 version = v"1.1.5"
