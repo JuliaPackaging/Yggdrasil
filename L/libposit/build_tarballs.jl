573e5f52a1c7059251a300c7347450ffe352b03a
Laslo Hunhold <hunhold@uni-koeln.de>
2024-11-20 19:02:29 +0100

    New Recipe: libposit v1.0.0 (#9817)
    Signed-off-by: Laslo Hunhold <hunhold@uni-koeln.de>

---
+++ L/libposit/build_tarballs.jl
@@ -0,0 +1,35 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "libposit"
+version = v"1.0.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/takum-arithmetic/libposit.git", "ad4e2daf29edd2b4fdfdd5d5188ef9b3c42fe19c")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/libposit
+./configure
+make PREFIX=${prefix} LDCONFIG= -j${nproc} install
+rm -f ${prefix}/lib/libposit.a ${prefix}/lib/libposit.lib ${prefix}/share/man/man3/posit*.3 ${prefix}/share/man/man7/libposit.7
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libposit", :libposit)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
