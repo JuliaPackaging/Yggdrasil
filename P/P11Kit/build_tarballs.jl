f97a3c60c0ecc7908a3d1e0db70d9295bcb2bbbb
Erik Schnetter <schnetter@gmail.com>
2024-11-22 11:39:44 -0500

    P11Kit: Do not use clang lld

--- P/P11Kit/build_tarballs.jl
+++ P/P11Kit/build_tarballs.jl
@@ -55,4 +55,4 @@ dependencies = [
 # Build the tarballs, and possibly a `build.jl` as well.
 # Prefer GCC 6 to define `memcpy@GLIBC_2.14'`
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"6")
+               clang_use_lld=false, julia_compat="1.6", preferred_gcc_version=v"6")
