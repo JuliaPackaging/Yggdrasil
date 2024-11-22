88ebbf2bc6fcdd69ff2b6f33ce73842dce3b562f
Erik Schnetter <schnetter@gmail.com>
2024-11-22 12:57:37 -0500

    wget: Enable Windows build

--- W/wget/build_tarballs.jl
+++ W/wget/build_tarballs.jl
@@ -24,8 +24,8 @@ make install
 # platforms are passed in on the command line
 platforms = supported_platforms()
 
-# Disable windows because GnuTLS_jll is not available there
-filter!(!Sys.iswindows, platforms)
+#TODO # Disable windows because GnuTLS_jll is not available there
+#TODO filter!(!Sys.iswindows, platforms)
 
 # The products that we will ensure are always built
 products = [
