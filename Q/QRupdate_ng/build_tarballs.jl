83c2ece3c53d11f795dc47afd684b78a80f7da14
Viral B. Shah <viral@mayin.org>
2024-11-24 13:26:35 -0500

    Update build_tarballs.jl

--- Q/QRupdate_ng/build_tarballs.jl
+++ Q/QRupdate_ng/build_tarballs.jl
@@ -53,4 +53,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.9", preferred_gcc_version=v"8")
+               julia_compat="1.9")
