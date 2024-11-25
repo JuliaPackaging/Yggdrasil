c2be5cead1ba1a4172ea0e1e67a854bc0401797c
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2024-11-25 11:06:06 +0100

    Revert "[Singular] ABI fixes for kMin_std (#9846)"
    This reverts commit b656960d7628bf10879e47982c4697f50bb032e8.

--- S/Singular/build_tarballs.jl
+++ S/Singular/build_tarballs.jl
@@ -29,7 +29,7 @@ import Pkg.Types: VersionSpec
 name = "Singular"
 
 upstream_version = v"4.4.0-7" # 4.4.0p7
-version_offset = v"0.0.9"
+version_offset = v"0.0.8"
 
 version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + version_offset.major,
                         upstream_version.patch * 100 + version_offset.minor,
@@ -37,7 +37,7 @@ version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + 
 
 # Collection of sources required to build normaliz
 sources = [
-    GitSource("https://github.com/Singular/Singular.git", "9f4b8c87374a5d13d0d7fa743e8f37d5435aa974"),
+    GitSource("https://github.com/Singular/Singular.git", "b3ba3db6b5a761767722d2e7bd42b5771924a5ec"),
     #ArchiveSource("https://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/$(upstream_version.major)-$(upstream_version.minor)-$(upstream_version.patch)/singular-$(upstream_version).tar.gz",
     #              "5b0f6c036b4a6f58bf620204b004ec6ca3a5007acc8352fec55eade2fc9d63f6"),
     #DirectorySource("./bundled")
