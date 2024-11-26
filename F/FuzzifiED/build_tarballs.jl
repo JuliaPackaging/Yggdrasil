56ff8f7ad24f2ac1bccf900fe9e4d6652a6a1c53
Zheng Zhou (周正) <122618104+mankai-chow@users.noreply.github.com>
2024-11-26 10:35:33 -0500

    Update build_tarballs.jl

--- F/FuzzifiED/build_tarballs.jl
+++ F/FuzzifiED/build_tarballs.jl
@@ -13,12 +13,19 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd FuzzifiED_Fortran/src/
-if [[ ${nbits} == 32 ]]; then     FFLAGS="-O3 -fPIC -fopenmp"; else     FFLAGS="-O3 -fdefault-integer-8 -fPIC -fopenmp"; fi
-for src in cfs.f90 bs.f90 op.f90 diag.f90 diag_re.f90 ent.f90; do     gfortran ${FFLAGS} -c ./${src}; done
+if [[ ${nbits} == 32 ]]; then
+    FFLAGS="-O3 -fPIC -fopenmp"
+else
+    FFLAGS="-O3 -fdefault-integer-8 -fPIC -fopenmp"
+fi
+for src in cfs.f90 bs.f90 op.f90 diag.f90 diag_re.f90 ent.f90; do
+    gfortran ${FFLAGS} -c ./${src}
+done
 gfortran ${FFLAGS} -shared -o ${libdir}/libfuzzified.$dlext ./*.o -L ${libdir} -larpack
 cd super
-for src in scfs.f90 sbs.f90 sop.f90; do     gfortran ${FFLAGS} -c ./${src}; done
+for src in scfs.f90 sbs.f90 sop.f90; do
+    gfortran ${FFLAGS} -c ./${src}
+done
 gfortran ${FFLAGS} -shared -o ${libdir}/libfuzzifino.$dlext ./*.o
 cd ..
 """
@@ -26,6 +33,7 @@ cd ..
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -35,8 +43,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
-    Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"))
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
+    Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="Arpack_jll", uuid="68821587-b530-5797-8361-c406ea357684"))
 ]
 
