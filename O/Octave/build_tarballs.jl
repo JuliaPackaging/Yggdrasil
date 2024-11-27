6d6d708acfba27a3f5fbc713b5a734dd0369b775
Viral B. Shah <viral@mayin.org>
2024-11-27 16:00:30 -0500

    [Octave] Retry the 9.2 build (#9869)

--- O/Octave/build_tarballs.jl
+++ O/Octave/build_tarballs.jl
@@ -17,6 +17,12 @@ export CPPFLAGS="-I${includedir}"
 export TMPDIR=${WORKSPACE}/tmpdir
 mkdir -p ${TMPDIR}
 
+if [[ "${target}" == *-mingw* ]]; then
+    LBT="blastrampoline-5"
+else
+    LBT="blastrampoline"
+fi
+
 # Base configure flags
 FLAGS=(
     --prefix="$prefix"
@@ -24,12 +30,12 @@ FLAGS=(
     --host="${target}"
     --enable-shared
     --disable-static
-    --with-blas="-L${libdir} -lopenblas"
-    --with-lapack="-L${libdir} -lopenblas"
+    --with-blas="-L${libdir} -l${LBT}"
+    --with-lapack="-L${libdir} -l${LBT}"
 )
 
 ./configure "${FLAGS[@]}"
-make -j${nproc} 
+make -j${nproc}
 make install
 """
 
@@ -47,12 +53,14 @@ products = [
 dependencies = [
     HostBuildDependency("flex_jll"),
     HostBuildDependency("Bison_jll"),
+    BuildDependency("SuiteSparse_jll"),
     Dependency("CompilerSupportLibraries_jll"),
-    Dependency("OpenBLAS32_jll"),
+    Dependency("libblastrampoline_jll"),
     Dependency("SuiteSparse32_jll"),
     Dependency("Arpack32_jll"),
     Dependency("Sundials32_jll"),
-    Dependency("CXSparse_jll"),
+    Dependency("QRupdate_ng_jll"),
+    Dependency("CXSparse_jll"; compat="400.400.100"),
     Dependency("PCRE2_jll"),
     Dependency("Readline_jll"),
     Dependency("Libiconv_jll"),
@@ -60,15 +68,16 @@ dependencies = [
     Dependency("Bzip2_jll"),
     Dependency("FFTW_jll"),
     Dependency("GLPK_jll"),
-    Dependency("GMP_jll", v"6.2.0"),
+    Dependency("GMP_jll"; compat="6.2"),
     Dependency("LibCURL_jll"),
     Dependency("Qhull_jll"),
     Dependency("HDF5_jll"),
     Dependency("rapidjson_jll"),
     Dependency("libsndfile_jll"),
     Dependency("GraphicsMagick_jll"),
+
 ]
 
 # Build the tarballs.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", clang_use_lld=false, preferred_gcc_version=v"10")
+               julia_compat="1.8", clang_use_lld=false, preferred_gcc_version=v"10")
