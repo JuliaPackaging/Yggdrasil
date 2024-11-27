8ff7e6e204112c7c678cf68c0bc741b044d64192
Daniel Guest <daniel_guest@urmc.rochester.edu>
2024-11-27 16:44:50 -0500

    Reverted product name to libzbc2014

--- Z/zbc2014/build_tarballs.jl
+++ Z/zbc2014/build_tarballs.jl
@@ -11,14 +11,14 @@ cd ${WORKSPACE}/srcdir/ZBC2014.jl_CSource
 ${CC} -c -fPIC complex.c -o complex.o
 ${CC} -c -fPIC model_IHC.c -o model_IHC.o
 ${CC} -c -fPIC model_Synapse.c -o model_Synapse.o
-${CC} -shared -o "zbc2014.${dlext}" model_IHC.o model_Synapse.o complex.o
-install -Dvm 755 "zbc2014.${dlext}" -t "${libdir}"
+${CC} -shared -o "libzbc2014.${dlext}" model_IHC.o model_Synapse.o complex.o
+install -Dvm 755 "libzbc2014.${dlext}" -t "${libdir}"
 """
 
 platforms = supported_platforms()
 
 products = [
-    LibraryProduct("zbc2014", :zbc2014)
+    LibraryProduct("libzbc2014", :libzbc2014)
 ]
 
 dependencies = Dependency[]
