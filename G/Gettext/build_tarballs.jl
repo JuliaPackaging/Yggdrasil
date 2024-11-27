dc7a64aa54a5122ca874d222ff74d6a6c61e2133
Erik Schnetter <schnetter@gmail.com>
2024-11-27 10:42:15 -0500

    Gettext: Build for aarch64-*-freebsd

--- G/Gettext/build_tarballs.jl
+++ G/Gettext/build_tarballs.jl
@@ -39,7 +39,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms(; experimental=true))
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
