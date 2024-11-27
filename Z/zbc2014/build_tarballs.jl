ae16af335599e4fd65973e00c41f8398da1c17f6
Daniel Guest <daniel_guest@urmc.rochester.edu>
2024-11-27 16:33:33 -0500

    Tweaked name  ->

--- Z/zbc2014/build_tarballs.jl
+++ Z/zbc2014/build_tarballs.jl
@@ -1,6 +1,6 @@
 using BinaryBuilder
 
-name = "libzbc2014"
+name = "zbc2014"
 version = v"0.3.0"
 sources = [
      GitSource("https://github.com/guestdaniel/ZBC2014.jl_CSource.git", "861db9fc377ae632ff7af0aab7ace8e815c445b6"),
@@ -11,14 +11,14 @@ cd ${WORKSPACE}/srcdir/ZBC2014.jl_CSource
 ${CC} -c -fPIC complex.c -o complex.o
 ${CC} -c -fPIC model_IHC.c -o model_IHC.o
 ${CC} -c -fPIC model_Synapse.c -o model_Synapse.o
-${CC} -shared -o "libzbc2014.${dlext}" model_IHC.o model_Synapse.o complex.o
-install -Dvm 755 "libzbc2014.${dlext}" -t "${libdir}"
+${CC} -shared -o "zbc2014.${dlext}" model_IHC.o model_Synapse.o complex.o
+install -Dvm 755 "zbc2014.${dlext}" -t "${libdir}"
 """
 
 platforms = supported_platforms()
 
 products = [
-    LibraryProduct("libzbc2014", :libzbc2014)
+    LibraryProduct("zbc2014", :zbc2014)
 ]
 
 dependencies = Dependency[]
