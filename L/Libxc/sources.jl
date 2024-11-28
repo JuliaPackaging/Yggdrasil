9c914b7a3492aa81c483e82ff35055ee32518e40
Tim Besard <tim.besard@gmail.com>
2024-11-28 15:09:24 +0100

    Update sources.jl

--- L/Libxc/sources.jl
+++ L/Libxc/sources.jl
@@ -1,5 +1,5 @@
 # Sources required for all builds
 sources = [
     ArchiveSource("https://gitlab.com/libxc/libxc/-/archive/$(version)/libxc-$(version).tar.gz",
-                  "f593745fa47ebfb9ddc467aaafdc2fa1275f0d7250c692ce9761389a90dd8eaf"),
+                  "9baf23501dca21b05fa22d8e2ffeb56f294abe19ba12584cb3f9b421ae719c5f"),
 ]
