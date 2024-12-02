1ab9fa8edb747783c311b461ce454f061cb36524
Zheng Zhou (周正) <122618104+mankai-chow@users.noreply.github.com>
2024-12-02 15:26:17 -0500

    Update build_tarballs.jl

--- F/FuzzifiED/build_tarballs.jl
+++ F/FuzzifiED/build_tarballs.jl
@@ -30,8 +30,7 @@ gfortran "${FFLAGS[@]}" -shared -o "${libdir}/libfuzzifino.${dlext}" ./*.o
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-platforms = expand_gfortran_versions(platforms)
+platforms = expand_gfortran_versions(supported_platforms(exclude = p -> arch(p) == "aarch64" && os(p) == "freebsd"))
 
 # The products that we will ensure are always built
 products = [
