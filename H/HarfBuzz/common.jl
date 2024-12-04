650862bca2c778e9aab8e4cd5ae50b08e80ee963
Erik Schnetter <schnetter@gmail.com>
2024-12-04 09:41:56 -0500

    HarfBuzz: Trigger CI

--- H/HarfBuzz/common.jl
+++ H/HarfBuzz/common.jl
@@ -56,7 +56,7 @@ fi
 
     # These are the platforms we will build for by default, unless further
     # platforms are passed in on the command line
-    platforms = filter!(p -> arch(p) != "armv6l", supported_platforms(; experimental=true))
+    platforms = filter!(p -> arch(p) != "armv6l", supported_platforms())
 
     # The products that we will ensure are always built
     products = if icu
