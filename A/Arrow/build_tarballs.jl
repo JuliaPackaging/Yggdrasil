9ceecedf7a87e387380452233f8714e6e70914d6
Erik Schnetter <schnetter@gmail.com>
2024-12-05 12:27:08 -0500

    Arrow: Disable aarch64-*-freebsd

--- A/Arrow/build_tarballs.jl
+++ A/Arrow/build_tarballs.jl
@@ -65,6 +65,7 @@ cmake --install build
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
+filter!(p -> !(Sys.isfreebsd(p) && arch(p) == "aarch64"), platforms)
 
 # The products that we will ensure are always built
 products = [
