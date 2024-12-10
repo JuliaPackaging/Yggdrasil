a6acbd5b143132c229426224c0d9c5a344ca4fd4
Brian Kropf <brian.kropf@gmail.com>
2024-12-09 23:17:21 -0500

    Update F/flecs/build_tarballs.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- F/flecs/build_tarballs.jl
+++ F/flecs/build_tarballs.jl
@@ -23,8 +23,7 @@ cmake --install build
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; exclude=Sys.isbsd)
-
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
