01d9ab53545099a7acdf08d46131bfc95f256093
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2024-12-10 13:46:49 +0100

    [libsingular_julia] update treehash

--- L/libsingular_julia/build_tarballs.jl
+++ L/libsingular_julia/build_tarballs.jl
@@ -9,11 +9,11 @@ uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "libsingular_julia"
-version = v"0.46.0"
+version = v"0.46.1"
 
 # Collection of sources required to build libsingular-julia
 sources = [
-    GitSource("https://github.com/oscar-system/Singular.jl.git", "14c4df209ce853183c43e5950c48ab829ee2fcfb"),
+    GitSource("https://github.com/oscar-system/Singular.jl.git", "9d9b9756549fd6594f1fd555039650d19c11652f"),
 ]
 
 # Bash recipe for building across all platforms
