38de6569cd3cbd6e2c1a6ed02bac63666459954b
Max Horn <max@quendi.de>
2024-12-10 08:38:02 +0100

    [GAP_pkg_float] rebuild with proper LDFLAGS

--- G/GAP_pkg/GAP_pkg_float/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_float/build_tarballs.jl
@@ -49,3 +49,5 @@ products = [
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.6", preferred_gcc_version=v"7")
 
+
+# rebuild trigger: 1
