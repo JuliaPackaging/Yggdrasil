1d91d9950c687a33b6124c48a13f6ca9445ed7f9
Jesper Stemann Andersen <jesper@sait.dk>
2024-12-11 08:25:55 +0100

    Updated for MLX C v0.1.0, matching MLX v0.21.0
    Enabling FreeBSD.

--- M/MLX_C/build_tarballs.jl
+++ M/MLX_C/build_tarballs.jl
@@ -3,28 +3,22 @@
 using BinaryBuilder, Pkg
 
 name = "MLX_C"
-version = v"0.0.11"
+version = v"0.1.0"
 
 sources = [
-    GitSource("https://github.com/ml-explore/mlx-c.git", "126253627b95a0803273f44655fdaf5e7be4fbe1"),
+    GitSource("https://github.com/ml-explore/mlx-c.git", "e8889ddf56b3bd734eb79d5e5b13586a39c01403"),
     ArchiveSource("https://github.com/roblabla/MacOSX-SDKs/releases/download/macosx14.0/MacOSX14.0.sdk.tar.xz",
                   "4a31565fd2644d1aec23da3829977f83632a20985561a2038e198681e7e7bf49"),
     DirectorySource("./bundled"),
 ]
 
 script = raw"""
-# MLX hack
-cd $includedir
-mv mlx/io.h mlx/io.h.bak
-cp mlx/io.h.bak mlx/io.h
-mv mlx/backend/metal/metal.h mlx/backend/metal/metal.h.bak
-cp mlx/backend/metal/metal.h.bak mlx/backend/metal/metal.h
-atomic_patch -p1 $WORKSPACE/srcdir/patches/mlx-std_unordered_map.patch
-
 if [[ "$target" == *-apple-darwin* ]]; then
     apple_sdk_root=$WORKSPACE/srcdir/MacOSX14.0.sdk
     sed -i "s!/opt/$bb_target/$bb_target/sys-root!$apple_sdk_root!" $CMAKE_TARGET_TOOLCHAIN
     sed -i "s!/opt/$bb_target/$bb_target/sys-root!$apple_sdk_root!" /opt/bin/$bb_full_target/$target-clang++
+
+    export MACOSX_DEPLOYMENT_TARGET=13.3
 fi
 
 cd $WORKSPACE/srcdir/mlx-c
@@ -34,8 +28,6 @@ atomic_patch -p1 ../patches/cmake-win32.patch
 if [[ "$target" == *-w64-mingw32* ]]; then
     atomic_patch -p1 ../patches/cmake-win32-io.patch
 fi
-
-export MACOSX_DEPLOYMENT_TARGET=13.3
 
 cmake \
     -B build \
@@ -49,17 +41,9 @@ cmake \
 cmake --build build --parallel $nproc
 cmake --install build
 install_license LICENSE
-
-# Restore MLX hack
-cd $includedir
-rm mlx/io.h
-mv mlx/io.h.bak mlx/io.h
-rm mlx/backend/metal/metal.h
-mv mlx/backend/metal/metal.h.bak mlx/backend/metal/metal.h
 """
 
 platforms = supported_platforms()
-filter!(!Sys.isfreebsd, platforms) # No MLX_jll artifact for FreeBSD
 platforms = expand_cxxstring_abis(platforms)
 
 products = Product[
@@ -69,10 +53,10 @@ products = Product[
 
 dependencies = [
     BuildDependency("dlfcn_win32_jll"; platforms = filter(Sys.iswindows, platforms)),
-    Dependency("MLX_jll"; compat = "0.20")
+    Dependency("MLX_jll"; compat = "0.21")
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
     julia_compat="1.9",
-    preferred_gcc_version = v"10", # arm_bf16.h
+    preferred_gcc_version = v"10", # Required for arm_bf16.h
 )
