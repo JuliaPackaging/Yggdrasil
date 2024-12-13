b8ba063e853e6188f475a661eee0b6b8d5fae41f
Sergio Sánchez Ramírez <sergio.sanchez.ramirez+git@bsc.es>
2024-12-13 18:47:42 +0100

    bump commit

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -9,7 +9,7 @@ repo = "https://github.com/EnzymeAD/Reactant.jl.git"
 version = v"0.0.26"
 
 sources = [
-  GitSource(repo, "c3170c9402441832098f83ecac54ce156b084fc2"),
+  GitSource(repo, "4761cce5187023e52f2a3b81cedfffd63683cecb"),
   FileSource("https://github.com/wsmoses/binaries/releases/download/v0.0.1/bazel-dev",
              "8b43ffdf519848d89d1c0574d38339dcb326b0a1f4015fceaa43d25107c3aade")
 ]
