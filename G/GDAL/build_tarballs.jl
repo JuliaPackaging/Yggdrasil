daf5981dcdb50e89202abc7359a5a254292b083b
Erik Schnetter <schnetter@gmail.com>
2024-12-16 10:43:13 -0500

    GDAL: Disable aarch64-*-freebsd

--- G/GDAL/build_tarballs.jl
+++ G/GDAL/build_tarballs.jl
@@ -121,6 +121,8 @@ cmake --install build
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
+# Disable until the dependencies are available for this platform
+filter!(p -> !(Sys.isfreebsd(p) && arch(p) == "aarch64"), platforms)
 
 # The products that we will ensure are always built
 products = [
