721f0dbbed3167668eeabc294844650605f866ec
Max Horn <max@quendi.de>
2024-12-17 00:38:46 +0100

    WIP

--- G/GAP_pkg/GAP_pkg_curlinterface/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_curlinterface/build_tarballs.jl
@@ -20,6 +20,7 @@ script = raw"""
 cd curlInterface*
 
 export CPPFLAGS="-I$includedir"  # workaround issue with clang on Linux with musl libc, see https://github.com/JuliaPackaging/Yggdrasil/pull/10000
+export LDFLAGS="-L$libdir"
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-gaproot=${prefix}/lib/gap --with-libcurl=${prefix}
 make -j${nproc}
 
