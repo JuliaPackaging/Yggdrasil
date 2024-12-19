49471c2538cf73ca151d4805b6fd602a0970c7ca
Mosè Giordano <765740+giordano@users.noreply.github.com>
2024-12-19 15:27:10 +0100

    Update S/snappy/build_tarballs.jl

--- S/snappy/build_tarballs.jl
+++ S/snappy/build_tarballs.jl
@@ -36,7 +36,7 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
+dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
