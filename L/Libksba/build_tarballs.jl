1f36ed9c2910f1b48ee7b8737601306536fb1ea9
Erik Schnetter <schnetter@gmail.com>
2024-12-19 13:07:40 -0500

    Libksba: New version 1.6.7

--- L/Libksba/build_tarballs.jl
+++ L/Libksba/build_tarballs.jl
@@ -3,13 +3,13 @@
 using BinaryBuilder
 
 name = "Libksba"
-version = v"1.5.1"
+version = v"1.6.7"
 
 # Collection of sources required to build libgcrypt
 sources = [
     ArchiveSource("https://gnupg.org/ftp/gcrypt/libksba/libksba-$(version).tar.bz2",
-                  "b0f4c65e4e447d9a2349f6b8c0e77a28be9531e4548ba02c545d1f46dc7bf921"),
-    DirectorySource("./bundled"),
+                  "cf72510b8ebb4eb6693eef765749d83677a03c79291a311040a5bfd79baab763"),
+    DirectorySource("bundled"),
 ]
 
 # Bash recipe for building across all platforms
@@ -41,10 +41,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    # Future versions of `Libgpg_error_jll` maybe can have a more lax compat,
-    # but the move 1.36 -> 1.42 changed the soname for FreeBSD and Windows.
-    Dependency("Libgpg_error_jll", v"1.36.0"; compat="=1.36.0"),
+    Dependency("Libgpg_error_jll"; compat="1.50.0"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6")
