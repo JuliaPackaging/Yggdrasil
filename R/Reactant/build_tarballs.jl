5c84e3d026c92273ebedd91cfaef110b45565b52
Billy Moses <gh@wsmoses.com>
2024-12-19 19:21:37 +0000

    Reactant: global kernel fix

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -6,10 +6,10 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "Reactant"
 repo = "https://github.com/EnzymeAD/Reactant.jl.git"
-version = v"0.0.27"
+version = v"0.0.28"
 
 sources = [
-  GitSource(repo, "5eaa114d88dc92d83ea9844fdd7b04267d225cae"),
+  GitSource(repo, "d87355a1f7d1c3501ea4382198799df87df60b97"),
   FileSource("https://github.com/wsmoses/binaries/releases/download/v0.0.1/bazel-dev",
              "8b43ffdf519848d89d1c0574d38339dcb326b0a1f4015fceaa43d25107c3aade")
 ]
