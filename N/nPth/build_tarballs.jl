cbb5199caacd9910c26c61a69c51419a85c4dac0
Erik Schnetter <schnetter@gmail.com>
2024-12-20 14:40:19 -0500

    nPth: Clean up version number handling

--- N/nPth/build_tarballs.jl
+++ N/nPth/build_tarballs.jl
@@ -3,11 +3,12 @@
 using BinaryBuilder
 
 name = "nPth"
-version = v"1.8"
+version_string = "1.8"
+version = VersionNumber(version_string)
 
 # Collection of sources required to build libgcrypt
 sources = [
-    ArchiveSource("https://gnupg.org/ftp/gcrypt/npth/npth-1.8.tar.bz2",
+    ArchiveSource("https://gnupg.org/ftp/gcrypt/npth/npth-$(version_string).tar.bz2",
                   "8bd24b4f23a3065d6e5b26e98aba9ce783ea4fd781069c1b35d149694e90ca3e"),
 ]
 
