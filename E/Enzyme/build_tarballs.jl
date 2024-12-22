7c14a24ee80c3b1ca2204d0573bdf37a9782a371
wsmoses <1260124+wsmoses@users.noreply.github.com>
2024-12-22 00:14:12 +0000

    Upgrade enzyme to refs/tags/v0.0.169

--- E/Enzyme/build_tarballs.jl
+++ E/Enzyme/build_tarballs.jl
@@ -8,14 +8,14 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "llvm.jl"))
 name = "Enzyme"
 repo = "https://github.com/EnzymeAD/Enzyme.git"
 
-auto_version = "refs/tags/v0.0.168"
+auto_version = "refs/tags/v0.0.169"
 version = VersionNumber(split(auto_version, "/")[end])
 
 llvm_versions = [v"15.0.7", v"16.0.6", v"17.0.6", v"18.1.7", v"19.1.1"]
 
 # Collection of sources required to build attr
 sources = [
-    GitSource(repo, "07e1826f2bc1090c3d8bc5bd30b9e2797e65a6dc"),
+    GitSource(repo, "a03c1d4008369c27536ab8450e808e34d4514a5f"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
                   "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f"),
 ]
