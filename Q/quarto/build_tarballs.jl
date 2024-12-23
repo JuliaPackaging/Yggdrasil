39f0a8ffaa95164b68d74e97def6358fe71536c4
Brian D. Klahn <briandk@virginia.edu>
2024-12-22 19:17:45 -0500

    forgot to update the quarto archive hashes

--- Q/quarto/build_tarballs.jl
+++ Q/quarto/build_tarballs.jl
@@ -7,11 +7,11 @@ version = VersionNumber(quarto_ver)
 
 url_prefix = "https://github.com/quarto-dev/quarto-cli/releases/download/v$(quarto_ver)/quarto-$(quarto_ver)"
 sources = [
-    ArchiveSource("$(url_prefix)-linux-amd64.tar.gz", "6418effd9f7c8a5f043197bcf1f30a4a2d588de399f23121ecec580ca5133296"; unpack_target = "x86_64-linux-gnu"),
-    ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "13225b149fa487457ab633a73e4e8fad892aa4d1dc8e44a20d06e5b51995cdd6"; unpack_target = "aarch64-linux-gnu"),
-    ArchiveSource("$(url_prefix)-macos.tar.gz", "0c90ca0ad03b4337213d71ec97b3fa5ed1e1fe450c4df64eb6c825808cc70e61"; unpack_target = "x86_64-apple-darwin14"),
-    ArchiveSource("$(url_prefix)-macos.tar.gz", "0c90ca0ad03b4337213d71ec97b3fa5ed1e1fe450c4df64eb6c825808cc70e61"; unpack_target = "aarch64-apple-darwin20"),
-    ArchiveSource("$(url_prefix)-win.zip", "7e8c55c2151f4f898230f53451eac199206cd7b018308ab203e9b3353cacdf19"; unpack_target = "x86_64-w64-mingw32"),
+    ArchiveSource("$(url_prefix)-linux-amd64.tar.gz", "d797c796713a57c14d8115f49a45d626a16478697096a0421f2d2d980e5f9d4a"; unpack_target="x86_64-linux-gnu"),
+    ArchiveSource("$(url_prefix)-linux-arm64.tar.gz", "6cade6c1fa7002459de2a44aba18ce13cc513f1d620cb51d79e4f1e2c5509b40"; unpack_target="aarch64-linux-gnu"),
+    ArchiveSource("$(url_prefix)-macos.tar.gz", "5b422f396756ee0ee268970ae851775fed7e1005aea98500d59b7f12cd9a4e16"; unpack_target="x86_64-apple-darwin14"),
+    ArchiveSource("$(url_prefix)-macos.tar.gz", "5b422f396756ee0ee268970ae851775fed7e1005aea98500d59b7f12cd9a4e16"; unpack_target="aarch64-apple-darwin20"),
+    ArchiveSource("$(url_prefix)-win.zip", "0fc0678222326ca5b2affc97df832dc23b71cb404dd9b17895db7a607e8e5a75"; unpack_target="x86_64-w64-mingw32"),
     FileSource("https://raw.githubusercontent.com/quarto-dev/quarto-cli/v$(quarto_ver)/COPYRIGHT", "490f3bfa035e325018ce9b0c8c2aec1f291c67ff55358a653d079488385af517"),
     FileSource("https://raw.githubusercontent.com/quarto-dev/quarto-cli/v$(quarto_ver)/COPYING.md", "54a55511991726b38e3867966ab14fd62919114670f2178654cced9394af78fd"),
 ]
@@ -48,4 +48,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
