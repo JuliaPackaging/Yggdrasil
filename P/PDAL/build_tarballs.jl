2cd95cc849ac539871896a80f015f09c9a6f02a6
Erik Schnetter <schnetter@gmail.com>
2024-12-24 11:13:28 -0500

    PDAL: Use GDAL 3.10

--- P/PDAL/build_tarballs.jl
+++ P/PDAL/build_tarballs.jl
@@ -82,7 +82,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="GDAL_jll", uuid="a7073274-a066-55f0-b90d-d619367d196c"); compat="301.902.300"),
+    Dependency(PackageSpec(name="GDAL_jll", uuid="a7073274-a066-55f0-b90d-d619367d196c"); compat="302.1000.0"),
     Dependency(PackageSpec(name="libgeotiff_jll", uuid="06c338fa-64ff-565b-ac2f-249532af990e"); compat="100.702.300"),
     # From GDAL recipe, for 32-bit platforms, when we need to link to OpenMP we need version 4,
     # because version 5 dropped support for these architectures
@@ -91,4 +91,5 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 # we require a compiler that supports C++ 17 and <filesystem>
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", preferred_gcc_version=v"9")
