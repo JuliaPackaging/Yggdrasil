005fd7a30e7154d660121d8c36ebe627b243381a
Erik Schnetter <schnetter@gmail.com>
2024-12-24 15:28:33 -0500

    PDAL: New version 2.8.2 (#9986)
    * PDAL: New version 2.8.2
    
    * PDAL: Disable aarch64-*-freebsd
    
    * PDAL: Build against GDAL 3.9
    
    * PDAL: Build against GDAL 3.9
    
    * PDAL: Use GDAL 3.10
    
    * PDAL: Install license
    
    * PDAL: Disable riscv64

--- P/PDAL/build_tarballs.jl
+++ P/PDAL/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "PDAL"
-version = v"2.6.3"
+version = v"2.8.2"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/PDAL/PDAL.git", "d37b077053116f4b76d360d379dbcaf890fd4a39"),
+    GitSource("https://github.com/PDAL/PDAL.git", "736fa0a66af4bed7105dff5fa152edf26bbb8a3a"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
                   "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
 ]
@@ -42,9 +42,8 @@ mkdir -p build/dimbuilder && cd build/dimbuilder
 
 #make sure we're back in source dir
 cd $WORKSPACE/srcdir/PDAL*
-cd build
 
-cmake .. -G Ninja \
+cmake -B build -G Ninja \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_LIBRARY_PATH:FILEPATH="${libdir}" \
@@ -64,11 +63,17 @@ cmake .. -G Ninja \
     -DWITH_ZLIB=ON \
     -DWITH_TESTS=OFF
 
-ninja -j${nproc}
-ninja install
+cmake --build build --parallel ${nproc}
+cmake --install build
+
+install_license LICENSE.txt
 """
 
 platforms = expand_cxxstring_abis(supported_platforms())
+
+# Some dependencies (e.g. PROJ_jll) are missing for aarch64-*-freebsd and riscv64-linux-*
+filter!(p -> !(Sys.isfreebsd(p) && arch(p) == "aarch64"), platforms)
+filter!(p -> !(Sys.islinux(p) && arch(p) == "riscv64"), platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -79,8 +84,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="GDAL_jll", uuid="a7073274-a066-55f0-b90d-d619367d196c")),
-    Dependency(PackageSpec(name="libgeotiff_jll", uuid="06c338fa-64ff-565b-ac2f-249532af990e")),
+    Dependency(PackageSpec(name="GDAL_jll", uuid="a7073274-a066-55f0-b90d-d619367d196c"); compat="302.1000.0"),
+    Dependency(PackageSpec(name="libgeotiff_jll", uuid="06c338fa-64ff-565b-ac2f-249532af990e"); compat="100.702.300"),
     # From GDAL recipe, for 32-bit platforms, when we need to link to OpenMP we need version 4,
     # because version 5 dropped support for these architectures
     BuildDependency(PackageSpec(; name="OpenMPI_jll", version=v"4.1.6"); platforms=filter(p -> nbits(p)==32, platforms)),
@@ -88,4 +93,5 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 # we require a compiler that supports C++ 17 and <filesystem>
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", preferred_gcc_version=v"9")
