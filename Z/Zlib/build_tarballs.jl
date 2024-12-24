528fcceba771299332bdfe28c52ec6760780d899
Mosè Giordano <765740+giordano@users.noreply.github.com>
2024-12-24 16:00:19 +0100

    [Zlib] Rebuild for RISC-V (#10085)

--- Z/Zlib/build_tarballs.jl
+++ Z/Zlib/build_tarballs.jl
@@ -50,3 +50,5 @@ dependencies = [
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.9", preferred_llvm_version=llvm_version)
+
+# build trigger: 1
