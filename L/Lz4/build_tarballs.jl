304fbf3943f65b2451eadc85020f5160b9ea4639
Erik Schnetter <schnetter@gmail.com>
2024-12-25 10:17:30 -0500

    Lz4: Build for riscv64

--- L/Lz4/build_tarballs.jl
+++ L/Lz4/build_tarballs.jl
@@ -48,4 +48,4 @@ dependencies = Dependency[
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
 
-# Build Trigger: 1
+# Build Trigger: 2
