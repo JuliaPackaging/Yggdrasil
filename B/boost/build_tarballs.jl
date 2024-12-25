acbb641b7de6140c6c62f7d4f5771ebf777951d5
Erik Schnetter <schnetter@gmail.com>
2024-12-25 11:21:39 -0500

    boost: Build for riscv64

--- B/boost/build_tarballs.jl
+++ B/boost/build_tarballs.jl
@@ -107,3 +107,5 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"7", julia_compat="1.6")
+
+# Build trigger: 1
