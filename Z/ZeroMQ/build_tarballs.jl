b081f069226aff252935223270f3a6e90bac9486
Ryan Mast <3969255+nightlark@users.noreply.github.com>
2024-12-26 13:19:25 -0800

    ZeroMQ: Build for riscv64 (#10140)

--- Z/ZeroMQ/build_tarballs.jl
+++ Z/ZeroMQ/build_tarballs.jl
@@ -51,3 +51,5 @@ dependencies = [
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                clang_use_lld=false, julia_compat="1.6")
+
+# Build trigger: 1
