28239dc1512ed17dc74cb147d302358f595232b1
Erik Schnetter <schnetter@gmail.com>
2024-12-26 10:45:08 -0500

    LibGit2: Build for risc64

--- L/LibGit2/build_tarballs.jl
+++ L/LibGit2/build_tarballs.jl
@@ -70,3 +70,5 @@ dependencies = [
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.9", preferred_llvm_version=llvm_version)
+
+# Build trigger: 1
