9f116e85e658d0c8466b19a7dd03a81be77479a6
Ian McInerney <i.mcinerney17@imperial.ac.uk>
2024-12-28 03:52:25 +0000

    Update S/spdlog/build_tarballs.jl

--- S/spdlog/build_tarballs.jl
+++ S/spdlog/build_tarballs.jl
@@ -41,4 +41,4 @@ dependencies = [
 ]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6")
+               julia_compat="1.6", preferred_gcc_version=v"5")
