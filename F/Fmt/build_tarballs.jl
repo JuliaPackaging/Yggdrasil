216e92fe78f36c291e9f0a18cb992a1736aa401c
Ian McInerney <i.mcinerney17@imperial.ac.uk>
2024-12-28 02:31:36 +0000

    [Fmt] Update to 11.1.1 (#10164)

--- F/Fmt/build_tarballs.jl
+++ F/Fmt/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "Fmt"
-version = v"11.0.2"
+version = v"11.1.1"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/fmtlib/fmt.git", "0c9fce2ffefecfdce794e1859584e25877b7b592")
+    GitSource("https://github.com/fmtlib/fmt.git", "e3ddede6c4ee818825c4e5a6dfa1d384860c27d9")
 ]
 
 # Bash recipe for building across all platforms
@@ -43,4 +43,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"5")
