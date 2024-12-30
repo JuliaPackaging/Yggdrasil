f5d5fa2932be45b0a21372873e20a0118cd2b396
Jake Rosoman <jkroso@gmail.com>
2024-12-31 09:36:49 +1100

    Update C/Clay/build_tarballs.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- C/Clay/build_tarballs.jl
+++ C/Clay/build_tarballs.jl
@@ -47,4 +47,4 @@ products = [
 dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"6")
