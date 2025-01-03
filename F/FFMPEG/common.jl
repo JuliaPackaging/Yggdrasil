4cc06ecc5b572865db5d6c8f50097b64ea080cc7
Erik Schnetter <schnetter@gmail.com>
2025-01-03 12:29:43 -0500

    FFMPEG: Update dependencies, disable some architectures

--- F/FFMPEG/common.jl
+++ F/FFMPEG/common.jl
@@ -135,7 +135,7 @@ end
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms())
+platforms = supported_platforms()
 # `libass_jll` is missing
 filter!(p -> arch(p) != "armv6l", platforms)
 # `libass_jll` is missing
