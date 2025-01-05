1355b59eafefd1051c83566aa82a356305adef97
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-01-05 18:17:07 +0000

    Apply suggestions from code review

--- L/LLVM/LLVM_full@19/build_tarballs.jl
+++ L/LLVM/LLVM_full@19/build_tarballs.jl
@@ -3,5 +3,5 @@ version = v"19.1.1"
 include("../common.jl")
 
 build_tarballs(ARGS, configure_build(ARGS, version; experimental_platforms=true)...;
-               preferred_gcc_version=v"13", preferred_llvm_version=v"18", julia_compat="1.6")
-# Build trigger: 5
+               preferred_gcc_version=v"10", preferred_llvm_version=v"18", julia_compat="1.6")
+# Build trigger: 6
