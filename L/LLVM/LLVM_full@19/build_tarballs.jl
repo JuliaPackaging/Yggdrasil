6830c9d5b162e370bff4848d5559726526b82857
Zentrik <llvm.zentrik@gmail.com>
2025-01-05 12:56:29 +0000

    [LLVM] Rebuild for riscv64 but change version number

--- L/LLVM/LLVM_full@19/build_tarballs.jl
+++ L/LLVM/LLVM_full@19/build_tarballs.jl
@@ -1,7 +1,7 @@
-version = v"19.1.1"
+version = v"19.2.1" # We had to bump the minor version number to change compat bounds for riscv64 support, but from next version we can go back to follow upstream version number
 
 include("../common.jl")
 
 build_tarballs(ARGS, configure_build(ARGS, version; experimental_platforms=true)...;
-               preferred_gcc_version=v"10", preferred_llvm_version=v"18", julia_compat="1.6")
+               preferred_gcc_version=v"13", preferred_llvm_version=v"18", julia_compat="1.6")
 # Build trigger: 5
