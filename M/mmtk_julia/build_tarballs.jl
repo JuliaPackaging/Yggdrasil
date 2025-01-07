3d6f196df9c2365e4c9564b9d6cfd85c9b294c46
Eduardo Souza <ledusou@gmail.com>
2025-01-07 12:03:55 +1100

    Update M/mmtk_julia/build_tarballs.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- M/mmtk_julia/build_tarballs.jl
+++ M/mmtk_julia/build_tarballs.jl
@@ -35,4 +35,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", compilers = [:rust, :c], preferred_gcc_version = v"13.2.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", compilers = [:rust, :c], preferred_gcc_version = v"10")
