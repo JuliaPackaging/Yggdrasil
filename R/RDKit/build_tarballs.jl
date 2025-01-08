b8bf1a126f40bdf42c207eff520623cf063939d8
thomvet <thvt@lundbeck.com>
2025-01-08 16:55:33 +0100

    back to basics

--- R/RDKit/build_tarballs.jl
+++ R/RDKit/build_tarballs.jl
@@ -12,7 +12,7 @@ script = raw"""
 cd ${WORKSPACE}/srcdir/rdkit
 
 # Windows build fails to link a test, despite the fact we don't want tests.
-# atomic_patch -p1 ../patches/do-not-build-cffi-test.patch
+#atomic_patch -p1 ../patches/do-not-build-cffi-test.patch
 
 FLAGS=()
 # if [[ "${target}" == *-mingw* ]]; then
@@ -21,45 +21,19 @@ FLAGS=()
 
 mkdir build
 cd build
-# cmake \
-#     -DCMAKE_BUILD_TYPE=Release \
-#     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-#     -DCMAKE_INSTALL_PREFIX=${prefix} \
-#     -DRDK_INSTALL_INTREE=OFF \
-#     -DRDK_BUILD_INCHI_SUPPORT=ON \
-#     -DRDK_BUILD_PYTHON_WRAPPERS=OFF \
-#     -DRDK_BUILD_CFFI_LIB=ON \
-#     -DRDK_BUILD_FREETYPE_SUPPORT=ON \
-#     -DRDK_BUILD_CPP_TESTS=OFF \
-#     -DRDK_BUILD_SLN_SUPPORT=OFF \
-#     -DRDK_TEST_MULTITHREADED=OFF \
-#     "${FLAGS[@]}" \
-
 cmake \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DRDK_INSTALL_INTREE=OFF \
-    -DRDK_INSTALL_STATIC_LIBS=OFF \
-    -DRDK_BUILD_CPP_TESTS=ON \
-    -DRDK_BUILD_COORDGEN_SUPPORT=ON \
-    -DRDK_BUILD_MAEPARSER_SUPPORT=ON \
-    -DRDK_OPTIMIZE_POPCNT=ON \
-    -DRDK_BUILD_TEST_GZIP=ON \
-    -DRDK_BUILD_FREESASA_SUPPORT=ON \
-    -DRDK_BUILD_AVALON_SUPPORT=ON \
     -DRDK_BUILD_INCHI_SUPPORT=ON \
-    -DRDK_BUILD_CFFI_LIB=ON \
-    -DRDK_CFFI_STATIC=OFF \
-    -DRDK_BUILD_THREADSAFE_SSS=ON \
-    -DRDK_BUILD_SWIG_WRAPPERS=OFF \
-    -DRDK_SWIG_STATIC=OFF \
-    -DRDK_BUILD_FREETYPE_SUPPORT=ON \
-    -DRDK_TEST_MULTITHREADED=ON \
-    -DRDK_INSTALL_DLLS_MSVC=ON \
     -DRDK_BUILD_PYTHON_WRAPPERS=OFF \
-    -DRDK_BUILD_PGSQL=OFF \
-    -DRDK_PGSQL_STATIC=OFF \
+    -DRDK_BUILD_CFFI_LIB=ON \
+    -DRDK_BUILD_FREETYPE_SUPPORT=ON \
+    -DRDK_BUILD_CPP_TESTS=OFF \
+    -DRDK_BUILD_SLN_SUPPORT=OFF \
+    -DRDK_TEST_MULTITHREADED=OFF \
+    "${FLAGS[@]}" \
     ..
 make -j${nproc}
 make install
