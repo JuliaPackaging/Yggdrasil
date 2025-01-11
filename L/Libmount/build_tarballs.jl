2b145c291a8be29879f5c06be1580dbd2d37a433
Mosè Giordano <mose@gnu.org>
2025-01-11 13:25:57 +0000

    [Libmount] Upgrade to v2.40.3

--- L/Libmount/build_tarballs.jl
+++ L/Libmount/build_tarballs.jl
@@ -3,13 +3,13 @@
 using BinaryBuilder
 
 name = "Libmount"
-version_string = "2.40.2"
+version_string = "2.40.3"
 version = VersionNumber(version_string)
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://mirrors.edge.kernel.org/pub/linux/utils/util-linux/v$(version.major).$(version.minor)/util-linux-$(version_string).tar.xz",
-                  "d78b37a66f5922d70edf3bdfb01a6b33d34ed3c3cafd6628203b2a2b67c8e8b3"),
+                  "0d6429d0cc474eafb972b0f4fee6b9c3d3f31c7bbada012bb3a1e255f00612c5"),
 ]
 
 # Bash recipe for building across all platforms
