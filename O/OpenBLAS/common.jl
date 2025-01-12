e0004efd8a03fb34519e2b4ba6125bb34eb9e75e
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-01-12 14:56:23 +0000

    [OpenBLAS] Restore dynamic arch build for riscv64

--- O/OpenBLAS/common.jl
+++ O/OpenBLAS/common.jl
@@ -183,10 +183,7 @@ function openblas_script(;num_64bit_threads::Integer=32, openblas32::Bool=false,
     elif [[ ${target} == powerpc64le-* ]]; then
         flags+=(TARGET=POWER8 DYNAMIC_ARCH=1)
     elif [[ ${target} == riscv64-* ]]; then
-        # We would like to enable DYNAMIC_ARCH=1, but we cannot:
-        # GCC 12 doesn't support RISC-V vector instructions
-        # GCC 13 does not know the type `vfloat32m4x2_t`
-        flags+=(TARGET=RISCV64_GENERIC)
+        flags+=(TARGET=RISCV64_GENERIC DYNAMIC_ARCH=1)
     fi
 
     # If we're building for x86_64 Windows gcc7+, we need to disable usage of
