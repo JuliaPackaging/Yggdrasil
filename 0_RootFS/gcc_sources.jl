76b7ef0f49d3af9becedab2d88ad228cc3ecaeb7
Erik Schnetter <schnetter@gmail.com>
2025-01-12 09:40:16 -0500

    0_Rootfs: Add GCC 14 (#10132)
    * 0_Rootfs: Add GCC 14
    
    * GCC 14: Remove outdated patches
    
    * GCC 14: Build with binutils 2.43.1
    
    * GCC 14: Update cctools
    
    * [GCC 14] Fix building newer cctools
    
    * [GCC 14] Add some version-based conditionals in the scripts
    
    * [GCC 14] Build newer libtapi
    
    * [GCC 14] Add patch to make building libgcc for macOS work
    
    * [GCC 14] Update libtapi to latest commit which includes our patch
    
    * [GCC 14] Simplify logic to set `LIB64` directory
    
    * [GCC 14] Fix glibc musl rejection patch
    
    * [GCC 14] Add comments about necessity to make backward-compatible changes
    
    * [GCC 14] Delete dummy `libc.so` in riscv64-linux-gnu build
    
    ---------
    
    Co-authored-by: Mosè Giordano <mose@gnu.org>

--- 0_RootFS/gcc_sources.jl
+++ 0_RootFS/gcc_sources.jl
@@ -106,7 +106,6 @@ const gcc_version_sources = Dict{VersionNumber,Vector}(
                         "fcf78dd9656c10eb8cf9fbd5f59a0b6b01386205fe1934b3b287a0a1898145c0"),
     ],
     v"13.2.0" => [
-
         ArchiveSource("https://mirrors.kernel.org/gnu/gcc/gcc-13.2.0/gcc-13.2.0.tar.xz",
                         "e275e76442a6067341a27f04c5c6b83d8613144004c0413528863dc6b5c743da"),
         ArchiveSource("https://mirrors.kernel.org/gnu/gmp/gmp-6.2.1.tar.xz",
@@ -117,6 +116,18 @@ const gcc_version_sources = Dict{VersionNumber,Vector}(
                         "17503d2c395dfcf106b622dc142683c1199431d095367c6aacba6eec30340459"),
         ArchiveSource("https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.24.tar.bz2",
                         "fcf78dd9656c10eb8cf9fbd5f59a0b6b01386205fe1934b3b287a0a1898145c0"),
+    ],
+    v"14.2.0" => [
+        ArchiveSource("https://mirrors.kernel.org/gnu/gcc/gcc-14.2.0/gcc-14.2.0.tar.xz",
+                      "a7b39bc69cbf9e25826c5a60ab26477001f7c08d85cec04bc0e29cabed6f3cc9"),
+        ArchiveSource("https://mirrors.kernel.org/gnu/gmp/gmp-6.2.1.tar.xz",
+                      "fd4829912cddd12f84181c3451cc752be224643e87fac497b69edddadc49b4f2"),
+        ArchiveSource("https://mirrors.kernel.org/gnu/mpfr/mpfr-4.1.0.tar.xz",
+                      "0c98a3f1732ff6ca4ea690552079da9c597872d30e96ec28414ee23c95558a7f"),
+        ArchiveSource("https://mirrors.kernel.org/gnu/mpc/mpc-1.2.1.tar.gz",
+                      "17503d2c395dfcf106b622dc142683c1199431d095367c6aacba6eec30340459"),
+        ArchiveSource("https://gcc.gnu.org/pub/gcc/infrastructure/isl-0.24.tar.bz2",
+                      "fcf78dd9656c10eb8cf9fbd5f59a0b6b01386205fe1934b3b287a0a1898145c0"),
     ],
 )
 
@@ -156,12 +167,21 @@ function gcc_sources(gcc_version::VersionNumber, compiler_target::Platform; kwar
                           "258e6cd51b3fbdfc185c716d55f82c08aff57df0c6fbd143cf6ed561267a1526"),
         ]
         # MacOS doesn't actually use binutils, it uses cctools
-        binutils_sources = [
-            GitSource("https://github.com/tpoechtrager/apple-libtapi.git",
-                      "a66284251b46d591ee4a0cb4cf561b92a0c138d8"),
-            GitSource("https://github.com/tpoechtrager/cctools-port.git",
-                      "634a084377ee2e2932c66459b0396edf76da2e9f"),
-        ]
+        if gcc_version < v"14"
+            binutils_sources = [
+                GitSource("https://github.com/tpoechtrager/apple-libtapi.git",
+                          "a66284251b46d591ee4a0cb4cf561b92a0c138d8"),
+                GitSource("https://github.com/tpoechtrager/cctools-port.git",
+                          "634a084377ee2e2932c66459b0396edf76da2e9f"),
+            ]
+        else
+            binutils_sources = [
+                GitSource("https://github.com/tpoechtrager/apple-libtapi.git",
+                          "54c9044082ba35bdb2b0edf282ba1a340096154c"),
+                GitSource("https://github.com/tpoechtrager/cctools-port.git",
+                          "81f205e8ca6bbf2fdbcb6948132454fd1f97839e"),
+            ]
+        end
     else
         # Different versions of GCC should be paired with different versions of Binutils
         binutils_gcc_version_mapping = Dict(
@@ -175,6 +195,7 @@ function gcc_sources(gcc_version::VersionNumber, compiler_target::Platform; kwar
             v"11.1.0" => v"2.36",
             v"12.1.0" => v"2.38",
             v"13.2.0" => v"2.41",
+            v"14.2.0" => v"2.43.1",
         )
 
         # Everyone else uses GNU Binutils, but we have to version carefully.
@@ -222,6 +243,10 @@ function gcc_sources(gcc_version::VersionNumber, compiler_target::Platform; kwar
             v"2.41" => [
                 ArchiveSource("https://ftp.gnu.org/gnu/binutils/binutils-2.41.tar.xz",
                               "ae9a5789e23459e59606e6714723f2d3ffc31c03174191ef0d015bdf06007450"),
+            ],
+            v"2.43.1" => [
+                ArchiveSource("https://ftp.gnu.org/gnu/binutils/binutils-2.43.1.tar.xz",
+                              "13f74202a3c4c51118b797a39ea4200d3f6cfbe224da6d1d95bb938480132dfd"),
             ],
         )
         binutils_version = binutils_gcc_version_mapping[gcc_version]
