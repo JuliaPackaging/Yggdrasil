725dff1e406b07fdb9cb691e4bce268df3d4fed9
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-01-13 07:32:02 -0600

    [MKL_Headers] Upgrade to v2025.0.1

--- M/MKL_Headers/build_tarballs.jl
+++ M/MKL_Headers/build_tarballs.jl
@@ -3,58 +3,27 @@
 using BinaryBuilder, Pkg
 
 name = "MKL_Headers"
-version = v"2024.2.0"
+version = v"2025.0.1"
 
 # Collection of sources required to complete build
 sources = [
-    # Archives for the headers
-    ArchiveSource(
-        "https://conda.anaconda.org/intel/win-32/mkl-include-2024.2.0-intel_661.tar.bz2",
-        "431feac62519a0d65c85e801d7329cb7caa66ced53a0b4d26f15420d06d1717d";
-        unpack_target = "mkl-include-i686-w64-mingw32"
-    ),
-    ArchiveSource(
-        "https://conda.anaconda.org/intel/win-64/mkl-include-2024.2.0-intel_661.tar.bz2",
-        "34f5cc20b6d2ab7c82f301b108fa2ac48e1f6c0acd8ad166897fb53184d5c93e";
-        unpack_target = "mkl-include-x86_64-w64-mingw32"
-    ),
-    ArchiveSource(
-        "https://conda.anaconda.org/intel/linux-32/mkl-include-2024.2.0-intel_663.tar.bz2",
-        "d97e655707590ba38d1240a4f9be3f60df2bc82f3ab5f7b16cf2735d4d9ba401";
-        unpack_target = "mkl-include-i686-linux-gnu"
-    ),
-    ArchiveSource(
-        "https://conda.anaconda.org/intel/linux-64/mkl-include-2024.2.0-intel_663.tar.bz2",
-        "2e29ca36f199bafed778230b054256593c2d572aeb050389fd87355ba0466d13";
-        unpack_target = "mkl-include-x86_64-linux-gnu"
-    ),
-
-    # Archives for the CMake/pkgconfig files
-    ArchiveSource(
-        "https://conda.anaconda.org/intel/win-32/mkl-devel-2024.2.0-intel_661.tar.bz2",
-        "db2bdd63f774edaca6cdc23677a5cc7ad390cf2bee362140b80238736483ae8f";
-        unpack_target = "mkl-devel-i686-w64-mingw32"
-    ),
-    ArchiveSource(
-        "https://conda.anaconda.org/intel/win-64/mkl-devel-2024.2.0-intel_661.tar.bz2",
-        "dd8758a3404d2bf6844463b16a3096820d7f7905bafdc057b9135eccf065e118";
-        unpack_target = "mkl-devel-x86_64-w64-mingw32"
-    ),
-    ArchiveSource(
-        "https://conda.anaconda.org/intel/linux-32/mkl-devel-2024.2.0-intel_663.tar.bz2",
-        "aabe1d37edc7d5d70891e25328d3bd2d8c9d7c5102cc5b400870164322df3a3c";
-        unpack_target = "mkl-devel-i686-linux-gnu"
-    ),
-    ArchiveSource(
-        "https://conda.anaconda.org/intel/linux-64/mkl-devel-2024.2.0-intel_663.tar.bz2",
-        "e3c37c75aa870aa8daa32e6cbfa6e34639f7e6fe6a67fc4b34fa2a94a497df15";
-        unpack_target = "mkl-devel-x86_64-linux-gnu"
-    ),
+    FileSource("https://files.pythonhosted.org/packages/cd/b7/092df7c41f57b3a41bb60392081564da0ea64ae300128fa9d44dd7acd84b/mkl_include-2025.0.1-py2.py3-none-win_amd64.whl",
+               "1149c1b34bc41166c0bb09c103e42ae251f0a9d190b886a99124b70632b3bc8d"; filename="mkl_include-x86_64-w64-mingw32.whl"),
+    FileSource("https://files.pythonhosted.org/packages/7b/42/74f3ed839c59e7bf3992804ce172590b4acac01dac4157f81daeb7774202/mkl_include-2025.0.1-py2.py3-none-manylinux_2_28_x86_64.whl",
+               "a70b90ce07f7a970a6fc8b324e416e0e978edec751b2d2e1b52d511f886fe506"; filename="mkl_include-x86_64-linux-gnu.whl"),
+    FileSource("https://files.pythonhosted.org/packages/1f/b1/03a2388889052dd018cebf8b915599015161519c6af89e42e4a31a62e2f0/mkl_devel-2025.0.1-py2.py3-none-win_amd64.whl",
+               "fc62df9c689722dadf282af6380538063dbd4fd3d328aa3c60eb72a521d06789"; filename="mkl_devel-x86_64-w64-mingw32.whl"),
+    FileSource("https://files.pythonhosted.org/packages/fd/e1/b13109f2d31fc6b1e753d0746317a62a119292ec127e196b409ad420587b/mkl_devel-2025.0.1-py2.py3-none-manylinux_2_28_x86_64.whl",
+               "18662a91ce12613622b7e002b7f5dcb790bf22bba83cef80179b9d2d3458ed2d"; filename="mkl_devel-x86_64-linux-gnu.whl"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/mkl-include-$target
+cd $WORKSPACE/srcdir
+unzip -d mkl_include-$target mkl_include-$target.whl
+unzip -d mkl_devel-$target mkl_devel-$target.whl
+
+cd $WORKSPACE/srcdir/mkl_include-$target
 if [[ $target == *-mingw* ]]; then
     rsync -av Library/include/ ${includedir}
 else
@@ -62,7 +31,7 @@ else
 fi
 install_license info/licenses/*.txt
 
-cd $WORKSPACE/srcdir/mkl-devel-$target
+cd $WORKSPACE/srcdir/mkl_devel-$target
 mkdir -p ${libdir}
 if [[ $target == *-mingw* ]]; then
     # These toolchain files must still go inside the lib folder, not the ${libdir} folder
@@ -76,8 +45,6 @@ fi
 # platforms are passed in on the command line
 platforms = [
     Platform("x86_64", "linux"; libc="glibc"),
-    Platform("i686", "linux"; libc="glibc"),
-    Platform("i686", "windows"),
     Platform("x86_64", "windows"),
 ]
 
@@ -87,8 +54,7 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
-]
+dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
