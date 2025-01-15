508125a795ca504d771dc98a6865fa8b09646868
Erik Schnetter <schnetter@gmail.com>
2025-01-15 09:17:43 -0500

    libsixel: New version 1.10.5

--- L/libsixel/build_tarballs.jl
+++ L/libsixel/build_tarballs.jl
@@ -3,19 +3,16 @@
 using BinaryBuilder, Pkg
 
 name = "libsixel"
-version = v"1.10.4"
+version = v"1.10.5"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/libsixel/libsixel.git", "cd9322ff7834b0ec301780b76f05a44fd285cb5b"),
-    DirectorySource("./bundled")
+    GitSource("https://github.com/libsixel/libsixel.git", "37026b01a0bd38634ae0a8c5017bd4671101fe08"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libsixel
-
-atomic_patch -p1 ../patches/01-headers.patch
 
 meson --cross-file="${MESON_TARGET_TOOLCHAIN}" build
 cd build
@@ -42,5 +39,3 @@ dependencies = [
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                clang_use_lld=false, julia_compat="1.6")
-
-# Build trigger: 1
