9fe43db4a6749e0113901b63ab37adb177ad3698
Max Horn <max@quendi.de>
2025-01-16 01:01:01 +0100

    [GAP_pkg_semigroups] new package

--- G/GAP_pkg/GAP_pkg_semigroups/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_semigroups/build_tarballs.jl
@@ -4,36 +4,38 @@ include("../common.jl")
 
 gap_version = v"400.1400.0"
 gap_lib_version = v"400.1400.0"
-name = "digraphs"
-upstream_version = "1.9.0" # when you increment this, reset offset to v"0.0.0"
+name = "semigroups"
+upstream_version = "5.4.0" # when you increment this, reset offset to v"0.0.0"
 offset = v"0.0.0" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
 version = offset_version(upstream_version, offset)
 
 # Collection of sources required to build this JLL
 sources = [
-    ArchiveSource("https://github.com/digraphs/Digraphs/releases/download/v$(upstream_version)/digraphs-$(upstream_version).tar.gz",
-                  "08dcc08be73a042f85ec1694111f91b1026b3241483f74f7100b308bbf9c4f11"),
+    ArchiveSource("https://github.com/semigroups/Semigroups/releases/download/v$(upstream_version)/semigroups-$(upstream_version).tar.gz",
+                  "9a22d6c6cd2a99392e286b6a4636258cdf50a308655dc11f444696aa4880d98e"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd digraphs*
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-gaproot=${prefix}/lib/gap
+cd semigroups*
+./configure --build=${MACHTYPE} --host=${target} --with-gaproot=${prefix}/lib/gap
 make -j${nproc}
 
 # copy the loadable module
 mkdir -p ${prefix}/lib/gap
 cp bin/*/*.so ${prefix}/lib/gap/
+cp bin/lib/*.* ${prefix}/lib/gap/
 
 install_license LICENSE
 """
 
 name = gap_pkg_name(name)
 platforms, dependencies = setup_gap_package(gap_version, gap_lib_version)
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
-    FileProduct("lib/gap/digraphs.so", :digraphs),
+    FileProduct("lib/gap/semigroups.so", :semigroups),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
