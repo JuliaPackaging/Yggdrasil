4f02cbe4c695cf2f6fd93de66abfb3e0c6dac20c
divital-coder <divital2004@gmail.com>
2025-01-18 02:50:42 +0530

    [ITK-v5.3.1] Removed redundant lines

--- I/ITK/build_tarballs.jl
+++ I/ITK/build_tarballs.jl
@@ -58,17 +58,16 @@ mkdir -pv ${prefix}/bin
 find $prefix/lib -name "*.dll" -exec mv -v {} ${prefix}/bin/ \;
 fi
 """
-platforms = [Platform("x86_64","Windows")]
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-#platforms = supported_platforms()
+platforms = supported_platforms()
 #sse2 disabled errors in ITK with open issues on github for i686 platforms [https://github.com/InsightSoftwareConsortium/ITK/issues/2529] [https://github.com/microsoft/vcpkg/issues/37574]
-#filter!(p -> !(arch(p) == "i686"), platforms)
+filter!(p -> !(arch(p) == "i686"), platforms)
 #CMAKE errors for _libcxx_run_result in cross compilation for freebsd and x86_64 linux musl
-#filter!(!Sys.isfreebsd, platforms)
-#filter!(p -> !(arch(p) == "x86_64" && libc(p) == "musl"), platforms)
-#filter!(p -> !(arch(p) == "riscv64"), platforms)
-#platforms = expand_cxxstring_abis(platforms)
+filter!(!Sys.isfreebsd, platforms)
+filter!(p -> !(arch(p) == "x86_64" && libc(p) == "musl"), platforms)
+filter!(p -> !(arch(p) == "riscv64"), platforms)
+platforms = expand_cxxstring_abis(platforms)
 # The products that we will ensure are always built
 products = [
     LibraryProduct(["libITKRegistrationMethodsv4", "libITKRegistrationMethodsv4-5.3", "libITKRegistrationMethodsv4-5"], :libITKRegistrationMethodsv4),
