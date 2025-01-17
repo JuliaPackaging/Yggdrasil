01b387048708cb734d3575c167b66ad954544090
Bertrand Rix <bertrand.rix@artelys.com>
2025-01-17 11:15:23 +0100

    Try pypowsybl 1.9.0 binaries.
    Signed-off-by: Bertrand Rix <bertrand.rix@artelys.com>

--- P/Powsybl/build_tarballs.jl
+++ P/Powsybl/build_tarballs.jl
@@ -8,26 +8,26 @@ version = v"0.2.0"
 uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
-pypowsybl_version = "BinariesDeployment"
+pypowsybl_version = "1.9.0"
 
 
 sources = [
     GitSource("https://github.com/powsybl/powsybl.jl.git", "0a9e3110c366808e7ac23cfd9e19f51793807392"),
     GitSource("https://github.com/powsybl/pypowsybl.git", "2bd6ed8f64b335a52483237e99b75a98d04e7cc4"),
     ArchiveSource("https://github.com/powsybl/pypowsybl/releases/download/v$(pypowsybl_version)/binaries-v$(pypowsybl_version)-windows.zip",
-                  "a4ddf94e91f785a456165f7732504526ac5b3761e5013771583e1cfbe44f26f1",
+                  "a3d1912e9ed213a4ca157ffa8470e4c38cbce84fa1529c646cb32ec4562f8c13",
                   "powsybl-java-x86_64-w64-mingw32"),
     ArchiveSource("https://github.com/powsybl/pypowsybl/releases/download/v$(pypowsybl_version)/binaries-v$(pypowsybl_version)-linux.zip",
-                  "070831654779a53d504c8c9396bbabc1ab209fb2ffb4e9bea12dc9e39c36ddf1",
+                  "98af7cdd909e7b9c0f4afea2788bbaf8671d59053f50dead52121241cba2d38e",
                   "powsybl-java-x86_64-linux-gnu"),
     ArchiveSource("https://github.com/powsybl/pypowsybl/releases/download/v$(pypowsybl_version)/binaries-v$(pypowsybl_version)-linux.zip",
-                  "070831654779a53d504c8c9396bbabc1ab209fb2ffb4e9bea12dc9e39c36ddf1",
+                  "98af7cdd909e7b9c0f4afea2788bbaf8671d59053f50dead52121241cba2d38e",
                   "powsybl-java-x86_64-linux-musl"), # linux package for gnu and musl
     ArchiveSource("https://github.com/powsybl/pypowsybl/releases/download/v$(pypowsybl_version)/binaries-v$(pypowsybl_version)-darwin.zip",
-                  "e27ec64eb1962ff9d6d04aa11da1071687eef00a546312e969deea3f52b23267",
+                  "1d1a2989ab7b7f9b3ca0605885c0d32c93331e48c5505094c23519bfd054d6ba",
                   "powsybl-java-x86_64-apple-darwin14"),
     ArchiveSource("https://github.com/powsybl/pypowsybl/releases/download/v$(pypowsybl_version)/binaries-v$(pypowsybl_version)-darwin-arm64.zip",
-                  "dbd4ffbffee5a00b9e0b71f510189a4043ae648973415f60f8631418ac9991d2",
+                  "3eac481b3da3c9224d3e76cfe2310d0ee620f99fcd8a78308eca79ed7447c948",
                   "powsybl-java-aarch64-apple-darwin20")
 ]
 
