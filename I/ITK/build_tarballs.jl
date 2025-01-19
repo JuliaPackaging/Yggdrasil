07bae003c094fdf56934102ca013ab167f3daf9c
divital-coder <divital2004@gmail.com>
2025-01-20 04:24:35 +0530

    [ITK-v5.3.1] Build was stuck, and interrupted so running again

--- I/ITK/build_tarballs.jl
+++ I/ITK/build_tarballs.jl
@@ -5,7 +5,7 @@ version = v"5.3.1"
 sources = [
     GitSource("https://github.com/InsightSoftwareConsortium/ITK.git", "1fc47c7bec4ee133318c1892b7b745763a17d411")
 ]
-# Bash recipe for building across all platforms
+# Bash recipe for building across all Platforms
 script = raw"""
 if [[ "${target}" == *x86_64-w64-mingw32* ]]; then
     CONFIG=msys2-64
