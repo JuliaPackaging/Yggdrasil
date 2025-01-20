3c4bfa145b53291a8aeb082b4f3937e1e0b04dc9
Tan, Kian-ting <yoxem@kianting.info>
2025-01-20 22:29:42 +0800

    Update L/libharu/build_tarballs.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- L/libharu/build_tarballs.jl
+++ L/libharu/build_tarballs.jl
@@ -12,8 +12,7 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cd libharu
+cd $WORKSPACE/srcdir/libharu
 mkdir build
 cmake -B build -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_FLAGS="-std=c99"
 cmake --build build --parallel ${nproc}
