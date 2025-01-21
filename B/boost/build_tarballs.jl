f04445bee7a6f844146d7a123310b05bf9f98556
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-01-21 12:05:11 +0100

    Fix source url

--- B/boost/build_tarballs.jl
+++ B/boost/build_tarballs.jl
@@ -8,8 +8,8 @@ version = v"1.87.0"
 # Collection of sources required to build boost
 sources = [
     ArchiveSource(
-        "https://boostorg.jfrog.io/artifactory/main/release/$(version)/source/boost_$(version.major)_$(version.minor)_$(version.patch).tar.bz2",
-        "475d589d51a7f8b3ba2ba4eda022b170e562ca3b760ee922c146b6c65856ef39"),
+        "https://archives.boost.io/release/1.87.0/source/boost_$(version.major)_$(version.minor)_$(version.patch).tar.bz2",
+        "af57be25cb4c4f4b413ed692fe378affb4352ea50fbe294a11ef548f4d527d89"),
 ]
 
 # Bash recipe for building across all platforms
