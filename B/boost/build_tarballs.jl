ba7cd93c16ddb833afc5d5b54133a47b55d6729a
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-01-22 23:19:44 +0100

    Update build_tarballs.jl

--- B/boost/build_tarballs.jl
+++ B/boost/build_tarballs.jl
@@ -46,7 +46,6 @@ if [[ $target == *apple* ]]; then
     fi
 elif [[ $target == *mingw* ]]; then
     targetos=windows
-    toolset=gcc-mingw
     echo "using gcc : 8.1 : $CXX : <cxxflags>\\"-DBOOST_USE_WINAPI_VERSION=0x0601 -D_WIN32_WINNT=0x0601 -DBOOST_STACKTRACE_USE_WINDBG=1\\" <linkflags>\\"-DBOOST_STACKTRACE_USE_WINDBG=1\\" ;" > project-config.jam
 
     if [[ $target == x86_64*mingw* ]]; then
@@ -89,7 +88,7 @@ products = [
     LibraryProduct("libboost_graph", :libboost_graph),
     LibraryProduct("libboost_iostreams", :libboost_iostreams),
     # boost_locale segfaults on windows, see https://github.com/benlorenz/boostBuilder/issues/2
-    #LibraryProduct("libboost_locale", :libboost_locale),
+    # LibraryProduct("libboost_locale", :libboost_locale),
     LibraryProduct("libboost_log", :libboost_log),
     LibraryProduct("libboost_log_setup", :libboost_log_setup),
     LibraryProduct("libboost_prg_exec_monitor", :libboost_prg_exec_monitor),
@@ -97,8 +96,8 @@ products = [
     LibraryProduct("libboost_random", :libboost_random),
     LibraryProduct("libboost_regex", :libboost_regex),
     LibraryProduct("libboost_serialization", :libboost_serialization),
-    # LibraryProduct("libboost_stacktrace_basic", :libboost_stacktrace_basic),
-    # LibraryProduct("libboost_stacktrace_noop", :libboost_stacktrace_noop),
+    LibraryProduct("libboost_stacktrace_basic", :libboost_stacktrace_basic),
+    LibraryProduct("libboost_stacktrace_noop", :libboost_stacktrace_noop),
     LibraryProduct("libboost_system", :libboost_system),
     LibraryProduct("libboost_thread", :libboost_thread),
     LibraryProduct("libboost_timer", :libboost_timer),
