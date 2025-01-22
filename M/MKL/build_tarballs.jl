a8430a3469a312f550f1809604f8d170c652c85a
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-01-22 14:55:02 -0600

    Don't overwrite the env variable target

--- M/MKL/build_tarballs.jl
+++ M/MKL/build_tarballs.jl
@@ -23,12 +23,11 @@ if [[ ${target} == *x86_64-linux-gnu* ]]; then
     install -Dvm 755 mkl-${target}/mkl-*.data/data/lib/* -t "${libdir}"
     cd mkl-${target}/mkl-*.data/data/lib
     for lib in *.so.2; do
-        target="${lib%.2}"
-        ln -s "${libdir}/$lib" "${libdir}/$target"
+        symlink="${lib%.2}"
+        ln -s "${libdir}/$lib" "${libdir}/$symlink"
     done
 fi
-cd $WORKSPACE/srcdir
-install_license mkl-${target}/mkl-2025.0.1.dist-info/LICENSE.txt
+install_license $WORKSPACE/srcdir/mkl-${target}/mkl-*.dist-info/LICENSE.txt
 """
 
 # These are the platforms we will build for by default, unless further
