c60ecabb9799cc9e8acb92a946fc43fa488a52c8
Jesper Stemann Andersen <jesper@sait.dk>
2025-01-26 13:20:13 +0100

    Bumped GCC requirement to v"10"

--- F/Faiss/Faiss/build_tarballs.jl
+++ F/Faiss/Faiss/build_tarballs.jl
@@ -10,5 +10,5 @@ include(joinpath(@__DIR__, "..", "common.jl"))
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
     julia_compat="1.6",
-    preferred_gcc_version=v"9", # Not using v"7", since OpenBLAS v0.3.29+ on PowerPC64LE requires libgfortran5
+    preferred_gcc_version=v"10", # Not using v"7", since OpenBLAS v0.3.29+ on PowerPC64LE requires libgfortran5, and not using v"8", and v"9" due to internal compiler errors on aarch64-linux-gnu
 )
