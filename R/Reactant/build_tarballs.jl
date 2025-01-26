588c4191a9b39c05c3b497972fd17eb84981b366
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-01-26 06:35:20 +0000

    [Reactant] Comment out ccache

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -64,9 +64,9 @@ BAZEL_FLAGS+=(--server_javabase=$JAVA_HOME)
 
 BAZEL_BUILD_FLAGS+=(--jobs ${nproc})
 
-# Use ccache to speedup re-builds
-BAZEL_BUILD_FLAGS+=(--action_env=USE_CCACHE=${USE_CCACHE})
-BAZEL_BUILD_FLAGS+=(--action_env=CCACHE_NOHASHDIR=yes)
+# # Use ccache to speedup re-builds
+# BAZEL_BUILD_FLAGS+=(--action_env=USE_CCACHE=${USE_CCACHE})
+# BAZEL_BUILD_FLAGS+=(--action_env=CCACHE_NOHASHDIR=yes)
 # Set `SUPER_VERBOSE` to a non empty string to make the compiler wrappers more
 # verbose. Useful for debugging.
 BAZEL_BUILD_FLAGS+=(--action_env=SUPER_VERBOSE=)
