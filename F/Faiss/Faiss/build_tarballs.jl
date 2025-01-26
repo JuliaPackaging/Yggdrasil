e402b5d64d08e0351b5802d12282f8752600365b
Jesper Stemann Andersen <jesper@sait.dk>
2025-01-26 12:39:07 +0100

    Bumped GCC requirement to v"8" to support PowerPC64LE

--- F/Faiss/Faiss/build_tarballs.jl
+++ F/Faiss/Faiss/build_tarballs.jl
@@ -8,4 +8,7 @@ version = v"1.9.0"
 include(joinpath(@__DIR__, "..", "common.jl"))
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"7")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+    julia_compat="1.6",
+    preferred_gcc_version=v"8", # Not using v"7", since OpenBLAS v0.3.29+ on PowerPC64LE requires libgfortran5
+)
