8a18f21e17105fd1106152b27a9de2f6d7eb76a3
Henrik Jakob <jakob@ibb.uni-stuttgart.de>
2025-01-27 07:50:55 +0100

    fix blas

--- M/muesli/build_tarballs.jl
+++ M/muesli/build_tarballs.jl
@@ -17,7 +17,16 @@ cd $WORKSPACE/srcdir/muesli
 
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/cmakesupport.patch
 
-cmake -B builddir -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release
+if [[ "${target}" == *mingw* ]]; then
+    BLAS=blastrampoline-5
+    LAPACK=blastrampoline-5
+else
+    BLAS=blastrampoline
+    LAPACK=blastrampoline
+fi
+
+cmake -B builddir -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release \
+                  -DBLAS_LIBRARIES="-l${BLAS}" -DLAPACK_LIBRARIES="-l${LAPACK}" 
 cmake --build builddir --parallel ${nprocs}
 cmake --install builddir
 
@@ -25,22 +34,20 @@ if [[ "${target}" == *-mingw* ]]; then
 #cmake install only grabs the .dll.a and leaves the actual .dll behind, manually move it 
 mv builddir/libmuesli.dll ${libdir}
 fi
-
 """
 
 platforms = supported_platforms()
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
-products =[
+products = Product[
     LibraryProduct("libmuesli", :libmuesli)
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("OpenBLAS_jll", v"0.3.10"),
-    Dependency("LAPACK_jll")
+    Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"), compat="5.4.0"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.9", preferred_gcc_version=v"8")
