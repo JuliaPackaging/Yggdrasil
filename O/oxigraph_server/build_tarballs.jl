d77a89d3778205c4275ebe11f88ceeea7aa9cc99
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-01-27 17:20:50 +0100

    fix hashes

--- O/oxigraph_server/build_tarballs.jl
+++ O/oxigraph_server/build_tarballs.jl
@@ -13,8 +13,8 @@ sources = [
     FileSource("$(url_prefix)_x86_64_apple", "3bc3c89d2ea1d3ad45e4e9d4c5c86ffc8be8266a930be767db98db0b5838ffc5"; filename = "oxigraph_server-x86_64-apple-darwin14"),
     FileSource("$(url_prefix)_aarch64_linux_gnu", "998ca76d3c2c25d02404fe8e808f8cab6119ee894653d48958fd8a01e8cfb5e7"; filename = "oxigraph_server-aarch64-linux-gnu"),
     FileSource("$(url_prefix)_x86_64_linux_gnu", "d34c1011339ad9337225f64943dbca8f439b495041ba38a1d40f440dd1d28a4b"; filename = "oxigraph_server-x86_64-linux-gnu"),
-    FileSource("$(url_prefix)_x86_64_windows_msvc.exe", "eb8499e1a510ae904511e585e233f2d81bbf5ee70a4949a75102e57901903e69"; filename = "oxigraph_server-x86_64-w64-mingw32"),
-    FileSource("https://raw.githubusercontent.com/oxigraph/oxigraph/v$version/LICENSE-MIT", "000d2aa6b11092d068edf2586542073f92584eb65e0018f59b611da4470f454b"; filename = "LICENSE.txt")
+    FileSource("$(url_prefix)_x86_64_windows_msvc.exe", "000d2aa6b11092d068edf2586542073f92584eb65e0018f59b611da4470f454b"; filename = "oxigraph_server-x86_64-w64-mingw32"),
+    FileSource("https://raw.githubusercontent.com/oxigraph/oxigraph/v$version/LICENSE-MIT", "1f4f6736adc52ebfda18bb84947e0ef492bd86a408c0e83872efb75ed5e02838"; filename = "LICENSE.txt")
 ]
 
 # Bash recipe for building across all platforms
