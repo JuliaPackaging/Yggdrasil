976ecfec0e091d4dbaf5c0fa94de408b5c687f9e
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2025-01-28 13:51:04 +0100

    [openfhe_julia] and [openfhe_julia_int128] update to version v0.3.9 (#10358)
    * [openfhe_julia] and [openfhe_julia_int128] update to version v0.3.7
    
    * remove temporary riscv64 support
    
    * [openfhe_julia] and [openfhe_julia_int128] update to version v0.3.8
    
    * [openfhe_julia] and [openfhe_int128_julia] update to version v0.2.5
    
    * filter julia versions
    
    * Update common.jl

--- O/openfhe_julia/common.jl
+++ O/openfhe_julia/common.jl
@@ -65,6 +65,8 @@ function prepare_openfhe_julia_build(name::String, git_hash::String)
 
     # These are the platforms we will build for by default, unless further
     # platforms are passed in on the command line
+    # Required Julia version is 1.10
+    filter!(v -> v >= v"1.10", julia_versions)
     platforms = vcat(libjulia_platforms.(julia_versions)...)
 
     # We cannot build with musl since OpenFHE requires the `execinfo.h` header for `backtrace`
