b189eda8b1cae04f619bdf935b646d60169079d8
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-01-28 18:17:41 +0000

    [Reactant] Update commit

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -9,7 +9,7 @@ repo = "https://github.com/EnzymeAD/Reactant.jl.git"
 version = v"0.0.55"
 
 sources = [
-  GitSource(repo, "81e1eeaa1ea3ec9cf49fe03b74ee6806e9300398"),
+  GitSource(repo, "69517088526a39f25fe6f9171f00dda39ca25691"),
   FileSource("https://github.com/wsmoses/binaries/releases/download/v0.0.1/bazel-dev",
              "8b43ffdf519848d89d1c0574d38339dcb326b0a1f4015fceaa43d25107c3aade")
 ]
