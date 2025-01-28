c03676c89e67b4ccf10b0ec3750cdcdebdb49201
Divyansh Goyal <divital2004@gmail.com>
2025-01-28 19:48:42 +0530

    removed windows until further notice.

--- I/ITKIOWrapper/build_tarballs.jl
+++ I/ITKIOWrapper/build_tarballs.jl
@@ -41,6 +41,7 @@ filter!(p -> !(arch(p) == "i686"), platforms)
 filter!(!Sys.isfreebsd, platforms)
 filter!(p -> !(arch(p) == "x86_64" && libc(p) == "musl"), platforms)
 filter!(p -> !(arch(p) == "riscv64"), platforms)
+filter!(!Sys.isWindows, platforms)
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
