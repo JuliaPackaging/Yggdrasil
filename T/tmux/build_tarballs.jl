75bd2ba9161f78d7779ca71eda3b2adb181e68b2
Erik Schnetter <schnetter@gmail.com>
2025-01-28 15:54:16 -0500

    tmux: Disable riscv64 (missing dependencies)

--- T/tmux/build_tarballs.jl
+++ T/tmux/build_tarballs.jl
@@ -28,6 +28,9 @@ make install
 # platforms are passed in on the command line
 platforms = supported_platforms(; exclude=Sys.iswindows)
 
+# libevent_jll is not available yet for riscv64
+filter!(arch(p) != "riscv64", platforms)
+
 # The products that we will ensure are always built
 products = [
     ExecutableProduct("tmux", :tmux)
