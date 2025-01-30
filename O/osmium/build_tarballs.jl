5c7503eaa10ac053ed0d916962f443aca812191f
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-01-30 15:12:41 +0100

    try more recent gcc

--- O/osmium/build_tarballs.jl
+++ O/osmium/build_tarballs.jl
@@ -66,4 +66,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"7")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"10")
