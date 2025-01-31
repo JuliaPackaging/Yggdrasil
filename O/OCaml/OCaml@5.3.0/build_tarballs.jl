1f927e5af8b67f374f1350c248b48110460631e4
Cody Tapscott <topolarity@tapscott.me>
2025-01-31 16:59:04 -0500

    Add `ocamlrun` build product
    This is needed for interpreting OCaml files.

--- O/OCaml/OCaml@5.3.0/build_tarballs.jl
+++ O/OCaml/OCaml@5.3.0/build_tarballs.jl
@@ -39,7 +39,8 @@ platforms = [
 # The products that we will ensure are always built
 products = [
     ExecutableProduct("ocamlopt.opt", :ocamlopt),
-    ExecutableProduct("ocamlc.opt", :ocamlc)
+    ExecutableProduct("ocamlc.opt", :ocamlc),
+    ExecutableProduct("ocamlrun", :ocamlrun),
 ]
 
 # Dependencies that must be installed before this package can be built
