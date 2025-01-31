63402be8e0696b42220ed37085fa9c597991eebd
Liu, Guoyuan <liuguoyuan01@outlook.com>
2025-01-31 18:54:39 +0100

    New Recipe: autossh v1.4.0-f (#10336)
    * New Recipe: autossh v1.4.0-f
    
    * Update A/autossh/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    
    * Update A/autossh/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    
    * add build for macos
    
    ---------
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

---
+++ A/autossh/build_tarballs.jl
@@ -0,0 +1,38 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "autossh"
+version = v"1.4.0" # autossh v1.4f, adapted to the BinaryBuilder requirements
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://github.com/Autossh/autossh/releases/download/v1.4f/autossh-1.4f.tgz", "0172e5e1bea40c642e0ef025334be3aadd4ff3b4d62c0b177ed88a8384e2f8f2")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/autossh*
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --with-ssh=${prefix}/bin \
+            ac_cv_func_malloc_0_nonnull=yes ac_cv_func_realloc_0_nonnull=yes
+make -j${nproc}
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+filter!(p -> arch(p) != "riscv64" && (Sys.islinux(p) || Sys.isapple(p)), platforms)
+
+# The products that we will ensure are always built
+products = [
+    ExecutableProduct("autossh", :autossh)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="OpenSSH_jll", uuid="9bd350c2-7e96-507f-8002-3f2e150b4e1b"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
