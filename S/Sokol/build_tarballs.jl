8d831a83a0640abfc86baa01e035fe3661f37dff
Chetan Vardhan <chetan.verdhan8@gmail.com>
2025-02-01 01:43:53 +0900

    separate files, correct date, fix macos error

--- S/Sokol/build_tarballs.jl
+++ S/Sokol/build_tarballs.jl
@@ -1,7 +1,10 @@
 using BinaryBuilder
+import Pkg: PackageSpec
+include("create_sokol_c.jl")
+include("create_cmakelists.jl")
 
 name = "Sokol"
-version = v"2024.1.1"  # Use the commit date or tag of Sokol you're targeting
+version = v"2025.28.1"  # Use the commit date or tag of Sokol you're targeting
 
 # Use the latest commit or a specific tag from the Sokol repository
 sources = [
@@ -12,88 +15,21 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/sokol*
 export CFLAGS="-I${includedir}"
-touch CMakeLists.txt
-touch sokol.c
-
-cat >> sokol.c <<EOF
-#define SOKOL_IMPL
-#if defined(_MSC_VER)
-#define SOKOL_D3D11
-#define SOKOL_LOG(str) OutputDebugStringA(str)
-#elif defined(__EMSCRIPTEN__)
-#define SOKOL_GLES2
-#elif defined(__APPLE__)
-// NOTE: on macOS, sokol.c is compiled explicitly as ObjC 
-#include <TargetConditionals.h>
-#define SOKOL_METAL
-#else
-#define SOKOL_GLCORE
-#endif
-
-#define SOKOL_WIN32_FORCE_MAIN
-#include "sokol_audio.h"
-#include "sokol_app.h"
-#include "sokol_gfx.h"
-#include "sokol_time.h"
-#include "sokol_glue.h"
-#include "sokol_fetch.h"
-#include "sokol_log.h"
-#include "sokol_args.h"
-EOF
-
-cat >> CMakeLists.txt <<EOF
-cmake_minimum_required(VERSION 3.0)
-project(game)
-set(CMAKE_C_STANDARD 11)
-if (CMAKE_SYSTEM_NAME STREQUAL Emscripten)
-    set(CMAKE_EXECUTABLE_SUFFIX ".html")
-endif()
-
-# Linux -pthread shenanigans
-if (CMAKE_SYSTEM_NAME STREQUAL Linux)
-    set(THREADS_PREFER_PTHREAD_FLAG ON)
-    find_package(Threads REQUIRED)
-endif()
-
-#=== LIBRARY: sokol
-# add headers to the the file list because they are useful to have in IDEs
-set(SOKOL_HEADERS
-    sokol_gfx.h
-    sokol_app.h
-    sokol_audio.h
-    sokol_time.h
-    sokol_glue.h
-    sokol_log.h
-    sokol_args.h
-    sokol_fetch.h
-    )
-add_library(sokol SHARED sokol.c ${SOKOL_HEADERS})
-if(CMAKE_SYSTEM_NAME STREQUAL Darwin)
-    set(exe_type MACOSX_BUNDLE)    
-    # compile sokol.c as Objective-C
-    target_compile_options(sokol PRIVATE -x objective-c)
-    target_link_libraries(sokol
-        "-framework QuartzCore"
-        "-framework Cocoa"
-        "-framework MetalKit"
-        "-framework Metal"
-        "-framework AudioToolbox")
-else()
-    if (CMAKE_SYSTEM_NAME STREQUAL Linux)
-        target_link_libraries(sokol INTERFACE X11 Xi Xcursor GL asound dl m)
-        target_link_libraries(sokol PUBLIC Threads::Threads)
-    endif()
-    if(CMAKE_SYSTEM_NAME STREQUAL Windows)
-        set(exe_type WIN32)
-    endif()
-endif()
-target_include_directories(sokol INTERFACE sokol)
-EOF
+$(create_sokol_c)
+$(create_cmakelists)
+if [[ ${target} == aarch64-apple-* ]]; then
+    # Linking FFMPEG requires the function `__divdc3`, which is implemented in
+    # `libclang_rt.osx.a` from LLVM compiler-rt.
+    FLAGS+=(-DCMAKE_SHARED_LINKER_FLAGS="-L${libdir}/darwin -lclang_rt.osx"
+            -DCMAKE_EXE_LINKER_FLAGS="-L${libdir}/darwin -lclang_rt.osx"
+            )
+fi
 
 cmake -B build \
     -DCMAKE_INSTALL_PREFIX=$prefix \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DCMAKE_BUILD_TYPE=Release
+    -DCMAKE_BUILD_TYPE=Release \
+    "${FLAGS[@]}"
 
 cmake --build build
 
@@ -109,7 +45,8 @@ install -Dm 755 "sokol_fetch.h" "${includedir}/sokol_fetch.h"
 """
 
 # Supported platforms
-platforms = supported_platforms(exclude=p->arch(p)=="armv6l"||Sys.isbsd(p)||arch(p)=="riscv64"||Sys.iswindows(p))
+platforms = supported_platforms(exclude=p->arch(p)=="armv6l"||Sys.isfreebsd(p)||arch(p)=="riscv64")
+llvm_version = v"13.0.1+1"
 
 # Platform-specific dependencies
 dependencies = [
@@ -122,7 +59,8 @@ dependencies = [
     Dependency("Libglvnd_jll"; platforms=filter(Sys.islinux, platforms)),
     Dependency("alsa_jll"; platforms=filter(Sys.islinux, platforms)),
     BuildDependency("Xorg_xorgproto_jll"; platforms=filter(Sys.islinux, platforms)),
-    Dependency("xkbcommon_jll"; platforms=filter(Sys.islinux, platforms))
+    BuildDependency(PackageSpec(name="LLVMCompilerRT_jll", uuid="4e17d02c-6bf5-513e-be62-445f41c75a11", version=llvm_version);
+    platforms=[Platform("aarch64", "macos")])
 ]
 
 # Library products
@@ -130,4 +68,4 @@ products = [
     LibraryProduct("libsokol", :libsokol)
 ]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"7", preferred_llvm_version=llvm_version)
