bad3219f32bb2516ff4f98b322159f46e82969cc
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-01-31 19:48:30 +0100

    Bump snappy

--- A/Arrow/build_tarballs.jl
+++ A/Arrow/build_tarballs.jl
@@ -89,7 +89,7 @@ dependencies = [
     Dependency("Zstd_jll"; compat="1.5.7"),
     Dependency("boost_jll"; compat="=1.87.0"),
     Dependency("brotli_jll"; compat="1.1.0"),
-    Dependency("snappy_jll"; compat="1.2.1"),
+    Dependency("snappy_jll"; compat="1.2.2"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
