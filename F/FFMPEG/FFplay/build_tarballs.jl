d504474a0842c2e32d72921ff5f0abb8ac5a26b9
Erik Schnetter <schnetter@gmail.com>
2025-02-01 11:53:51 -0500

    FFplay: Rebuild with newer dependencies

--- F/FFMPEG/FFplay/build_tarballs.jl
+++ F/FFMPEG/FFplay/build_tarballs.jl
@@ -24,3 +24,5 @@ dependencies = [
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script(; ffplay=true), platforms, products, dependencies;
                julia_compat="1.6", preferred_gcc_version, clang_use_lld=false)
+
+# Build trigger: 1
