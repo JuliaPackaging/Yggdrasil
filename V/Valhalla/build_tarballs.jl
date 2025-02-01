7feec532128aebe403baaa00b24f51cd54601ce0
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-01 23:08:07 +0000

    add protoc exec patch

--- V/Valhalla/build_tarballs.jl
+++ V/Valhalla/build_tarballs.jl
@@ -22,6 +22,8 @@ fi
 
 git submodule update --init --recursive
 
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/protoc-find-exec.patch
+
 if [[ "${target}" == *freebsd* ]]; then
     cd third_party/cpp-statsd-client
     atomic_patch -p1 ${WORKSPACE}/srcdir/patches/cpp-statsd-client.patch
