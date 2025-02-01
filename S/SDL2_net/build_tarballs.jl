dda2189145367aa9d652e4919c35a9c6823e86be
Kyle Conel <kyjordev@gmail.com>
2025-02-01 17:13:27 -0500

    Update build_tarballs.jl

--- S/SDL2_net/build_tarballs.jl
+++ S/SDL2_net/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "SDL2_net"
-version = v"2.0.1"
+version = v"2.2.0"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://libsdl.org/projects/SDL_net/release/SDL2_net-$(version).tar.gz",
-                  "15ce8a7e5a23dafe8177c8df6e6c79b6749a03fff1e8196742d3571657609d21"),
+                  "4e4a891988316271974ff4e9585ed1ef729a123d22c08bd473129179dc857feb"),
     DirectorySource("./bundled"),
 ]
 
