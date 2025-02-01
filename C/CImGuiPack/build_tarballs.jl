b1b9f3a0ed7f5b9190698261d24c87997938f517
JamesWrigley <james@puiterwijk.org>
2025-02-02 00:07:24 +0100

    fixup! [CImGuiPack] Bump version of wrappers

--- C/CImGuiPack/build_tarballs.jl
+++ C/CImGuiPack/build_tarballs.jl
@@ -81,4 +81,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.9", preferred_gcc_version=v"8")
