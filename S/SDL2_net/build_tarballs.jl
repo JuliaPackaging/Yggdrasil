16d34c1d6a29ca720ae6f1131aa673eb13d3a577
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-02-01 22:31:02 +0000

    Update build_tarballs.jl

--- S/SDL2_net/build_tarballs.jl
+++ S/SDL2_net/build_tarballs.jl
@@ -36,6 +36,8 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+# Missing dep at the moment
+filter!(p -> arch(p) != "riscv64", platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -48,4 +50,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", preferred_gcc_version=v"5")
