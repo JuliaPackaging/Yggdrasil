99d9172894ab555e9dfe58ba9f63a362c64078ae
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-02 23:33:45 +0100

    use clang for Mac

--- V/Valhalla/build_tarballs.jl
+++ V/Valhalla/build_tarballs.jl
@@ -34,7 +34,7 @@ mkdir build && cd build
 
 cmake .. \
     -DCMAKE_INSTALL_PREFIX=$prefix \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN%.*}_gcc.cmake \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=ON \
     -DENABLE_DATA_TOOLS=OFF \
