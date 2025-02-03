f8d94849c633a7a7cedef9b73fc1a73e80e2061f
Pere Mato <pere.mato@cern.ch>
2025-02-03 10:00:37 +0100

    Adapted to new way to generate wrapper code and new Minuit2 version

--- M/Minuit2_Julia_Wrapper/build_tarballs.jl
+++ M/Minuit2_Julia_Wrapper/build_tarballs.jl
@@ -1,45 +1,59 @@
-using BinaryBuilder
-using Pkg
-
-sources = [
-	GitSource("https://github.com/jstrube/Minuit2_Julia_Wrapper.git", "10bf344912baa777a940862d6d80631bea34a448")
-]
-
-julia_version = v"1.5.3"
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
 
 name = "Minuit2_Julia_Wrapper"
-version = v"0.1"
+version = v"0.2"
+
+sources = [
+	GitSource("https://github.com/JuliaHEP/Minuit2_Julia_Wrapper.git",
+              "de92f78ab964cc251d207b59f3b0e82e9b8892a0")
+]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd ${WORKSPACE}/srcdir/Minuit2_Julia_Wrapper/
-mkdir build && cd build
-cmake -DJulia_PREFIX=${prefix} -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DCMAKE_FIND_ROOT_PATH=${prefix} ..
-VERBOSE=ON cmake --build . --config Release --target install
-install_license $WORKSPACE/srcdir/Minuit2_Julia_Wrapper/LICENSE 
+
+cmake ${WORKSPACE}/srcdir/Minuit2_Julia_Wrapper -B build \
+    -DJulia_PREFIX=${prefix} \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_CXX_STANDARD=17
+
+VERBOSE=ON cmake --build build --config Release --target install -- -j${nproc}
+
+install_license Minuit2_Julia_Wrapper/LICENSE 
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 include("../../L/libjulia/common.jl")
-platforms = libjulia_platforms(julia_version)
-# filter!(!Sys.isfreebsd, platforms)
-# filter!(!Sys.iswindows, platforms)
-# filter!(p -> arch(p) != "armv7l", platforms)
 
-platforms = expand_cxxstring_abis(platforms)
+# platforms supported by libjulia
+platforms = vcat(libjulia_platforms.(julia_versions)...)
+
+# platforms supported by Minuit2
+platforms = filter(p -> libc(p) != "musl" && 
+                        os(p) != "freebsd" && 
+                        arch(p) != "armv6l" && 
+                        arch(p) != "armv7l" && 
+                        arch(p) != "i686" &&
+                        arch(p) != "riscv64", platforms) |> expand_cxxstring_abis
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libminuit2wrap", :minuit2wrap)
+    LibraryProduct("libMinuit2Wrap", :libMinuit2Wrap),
+    FileProduct("Minuit2-export.jl", :Minuit2_exports),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-        Dependency(PackageSpec(name="libcxxwrap_julia_jll")),
-        Dependency(PackageSpec(name="Minuit2_jll")),
-        BuildDependency(PackageSpec(name="libjulia_jll", version=julia_version))
+    BuildDependency("libjulia_jll"),
+    Dependency("libcxxwrap_julia_jll"; compat="0.13.2"),
+    Dependency("Minuit2_jll"; compat = "~6.34",)
 ]
 
+# Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
-    preferred_gcc_version=v"8", julia_compat = "^$(julia_version.major).$(julia_version.minor)")
+    preferred_gcc_version=v"9", 
+    julia_compat = "1.6")
