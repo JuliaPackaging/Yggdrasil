3b27fa2da294f4e37d381eb1daf1254873358304
divital-coder <divital2004@gmail.com>
2025-02-03 23:49:04 +0530

    fixing errors

--- I/ITKIOWrapper/build_tarballs.jl
+++ I/ITKIOWrapper/build_tarballs.jl
@@ -1,17 +1,14 @@
 # Note: this script will require BinaryBuilder.jl v0.3.0 or greater
 using BinaryBuilder, Pkg
 name = "ITKIOWrapper"
-version = v"1.0.0"  # Update this to your package version
-
-# The following lines should be updated to reflect the actual URL and revision of your ITKWrapper package
+version = v"1.0.0"
 
 # Collection of sources required to build ITKWrapper
 sources = [
     DirectorySource("./src"),
 ]
-# uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
+uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 # delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
-
 #needed for libjulia_platforms and julia_versions
 include("../../L/libjulia/common.jl")
 
@@ -20,30 +17,41 @@ script = raw"""
 export CXXFLAGS="-I${includedir}/julia $CXXFLAGS"
 export CFLAGS="-I${includedir}/julia $CFLAGS"
 mkdir -p build/
-cmake -B build -S . \
-    -DCMAKE_INSTALL_PREFIX=${prefix} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DBUILD_SHARED_LIBS:BOOL=ON
+
+# Different CMake configuration for macOS
+if [[ "${target}" == *-apple-* ]]; then
+    cmake -B build -S . \
+        -DCMAKE_INSTALL_PREFIX=${prefix} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+        -DBUILD_SHARED_LIBS:BOOL=ON \
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+        -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10
+else
+    cmake -B build -S . \
+        -DCMAKE_INSTALL_PREFIX=${prefix} \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+        -DBUILD_SHARED_LIBS:BOOL=ON \
+        -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+fi
+
 cmake --build build --parallel ${nproc}
 cmake --install build
 install_license /usr/share/licenses/MIT
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-
+# These are the platforms we will build for by default
 platforms = vcat(libjulia_platforms.(julia_versions)...)
-
-#Filtering all the platforms, that ITK filters https://github.com/JuliaPackaging/Yggdrasil/blob/master/I/ITK/build_tarballs.jl#L42-L53
+# Filter platforms
 filter!(p -> !(arch(p) == "i686"), platforms)
-filter!(!Sys.iswindows, platforms)
 filter!(!Sys.isfreebsd, platforms)
-filter!(p -> !(arch(p) == "riscv64"), platforms)
 filter!(p -> !(arch(p) == "x86_64" && libc(p) == "musl"), platforms)
+filter!(p -> !(arch(p) == "riscv64"), platforms)
+filter!(!Sys.iswindows, platforms)
 
+# Expand C++ string ABI platforms
 platforms = expand_cxxstring_abis(platforms)
-
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libITKIOWrapper", :libITKIOWrapper),
@@ -51,11 +59,12 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("ITK_jll"; compat="5.3.1"),
-    Dependency("libcxxwrap_julia_jll"; compat = "0.13.2"),
-    BuildDependency("libjulia_jll"),
-    BuildDependency("Eigen_jll")
+    BuildDependency(PackageSpec(name="Eigen_jll", uuid="bc6bbf8a-a594-5541-9c57-10b0d0312c70")),
+    Dependency(PackageSpec(name="ITK_jll", uuid="3324d3a8-621a-5aaa-97fa-c3bc8dfc0481"); compat="5.3.1"),
+    Dependency(PackageSpec(name="libcxxwrap_julia_jll", uuid="3eaa8342-bff7-56a5-9981-c04077f7cee7"); compat = "0.13.2"),
+    BuildDependency(PackageSpec(name="libjulia_jll", uuid="5ad3ddd2-0711-543a-b040-befd59781bbf"))
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"8.1.0")
+# Build the tarballs
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"8.1.0",
+preferred_llvm_version=v"13.0.1")
--- I/ITKIOWrapper/src/itklib.cpp
+++ I/ITKIOWrapper/src/itklib.cpp
@@ -1,45 +1,105 @@
+#include <memory>
+#include <type_traits>
 
-// #include "jlcxx/jlcxx.hpp"
+// Check if std::make_unique_for_overwrite is available
+#if !__cpp_lib_make_unique_for_overwrite
+
+// Define the custom implementation in the global namespace
+template<typename T>
+typename std::enable_if<std::is_array<T>::value, std::unique_ptr<T>>::type
+make_unique_for_overwrite(std::size_t size) {
+    return std::unique_ptr<T>(new typename std::remove_extent<T>::type[size]);
+}
+
+template<typename T, typename... Args>
+typename std::enable_if<!std::is_array<T>::value, std::unique_ptr<T>>::type
+make_unique_for_overwrite(Args&&... args) {
+    return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+}
+
+// Inject the custom implementation into the std namespace
+namespace std {
+    using ::make_unique_for_overwrite;
+}
+
+#endif // !__cpp_lib_make_unique_for_overwrite
+
+
 #include "jlcxx/jlcxx.hpp"
 #include "jlcxx/array.hpp"
 #include "jlcxx/tuple.hpp"
 #include "itkImageFileReader.h"
 #include "itkImageFileWriter.h"
-#include "itkImage.h"
+#include "itkImageSeriesReader.h"
+#include "itkImageSeriesWriter.h"
+#include "itkGDCMImageIO.h"
+#include "itkGDCMSeriesFileNames.h"
+#include "itkOrientImageFilter.h"
+#include "itkMetaDataObject.h"
 #include <vector>
 #include <memory>
 #include <stdexcept>
+#include <iostream>
 
 using ImageType = itk::Image<float, 3>;
-
 
 class ITKImageWrapper {
 private:
     ImageType::Pointer m_Image;
 
 public:
-    ITKImageWrapper(const std::string& filename) {
-        auto reader = itk::ImageFileReader<ImageType>::New();
-        reader->SetFileName(filename);
-        reader->Update();
-        m_Image = reader->GetOutput();
+    // Constructor to load a NIfTI file or DICOM series
+    ITKImageWrapper(const std::string& path, bool isDicom = false) {
+        if (isDicom) {
+            // Handle DICOM directory
+            auto namesGenerator = itk::GDCMSeriesFileNames::New();
+            namesGenerator->SetUseSeriesDetails(true);
+            namesGenerator->SetDirectory(path);
+
+            const auto& seriesUIDs = namesGenerator->GetSeriesUIDs();
+            if (seriesUIDs.empty()) {
+                throw std::runtime_error("No DICOM series found in the directory.");
+            }
+
+            // Read the first series
+            const std::string seriesIdentifier = seriesUIDs.begin()->c_str();
+            const auto& fileNames = namesGenerator->GetFileNames(seriesIdentifier);
+
+            // Read the DICOM series
+            auto reader = itk::ImageSeriesReader<ImageType>::New();
+            auto dicomIO = itk::GDCMImageIO::New();
+            reader->SetImageIO(dicomIO);
+            reader->SetFileNames(fileNames);
+            reader->Update();
+            m_Image = reader->GetOutput();
+        } else {
+            // Handle NIfTI file
+            auto reader = itk::ImageFileReader<ImageType>::New();
+            reader->SetFileName(path);
+            reader->Update();
+            m_Image = reader->GetOutput();
+        }
     }
 
+    // Get origin
     std::vector<double> getOrigin() const {
         auto origin = m_Image->GetOrigin();
         return {origin[0], origin[1], origin[2]};
     }
 
+    // Get spacing
     std::vector<double> getSpacing() const {
         auto spacing = m_Image->GetSpacing();
         return {spacing[0], spacing[1], spacing[2]};
     }
 
+    // Get size
     std::vector<std::size_t> getSize() const {
         auto size = m_Image->GetLargestPossibleRegion().GetSize();
         return {size[0], size[1], size[2]};
     }
 
+    // Get pixel data
     std::vector<float> getPixelData() const {
         std::vector<float> data;
         itk::ImageRegionConstIterator<ImageType> it(m_Image, m_Image->GetLargestPossibleRegion());
@@ -49,6 +109,7 @@ public:
         return data;
     }
 
+    // Get direction
     std::vector<double> getDirection() const {
         auto direction = m_Image->GetDirection();
         std::vector<double> direction_flat;
@@ -60,32 +121,53 @@ public:
         return direction_flat;
     }
 
-    void writeImage(const std::string& filename) const {
-        auto writer = itk::ImageFileWriter<ImageType>::New();
-        writer->SetFileName(filename);
-        writer->SetInput(m_Image);
-        writer->Update();
+    // Write image (NIfTI or DICOM)
+    void writeImage(const std::string& filename, bool isDicom = false) const {
+        if (isDicom) {
+            // Save as DICOM series
+            auto writer = itk::ImageSeriesWriter<ImageType, ImageType>::New();
+            auto dicomIO = itk::GDCMImageIO::New();
+            writer->SetImageIO(dicomIO);
+
+            // Generate output file names
+            std::vector<std::string> outputFileNames;
+            for (int i = 0; i < m_Image->GetLargestPossibleRegion().GetSize()[2]; ++i) {
+                outputFileNames.push_back(filename + "_" + std::to_string(i) + ".dcm");
+            }
+
+            writer->SetFileNames(outputFileNames);
+            writer->SetInput(m_Image);
+            writer->Update();
+        } else {
+            // Save as NIfTI
+            auto writer = itk::ImageFileWriter<ImageType>::New();
+            writer->SetFileName(filename);
+            writer->SetInput(m_Image);
+            writer->Update();
+        }
+    }
+
+    // Reorient image to LPS
+    void reorientToLPS() {
+        using OrientFilterType = itk::OrientImageFilter<ImageType, ImageType>;
+        auto orientFilter = OrientFilterType::New();
+        orientFilter->SetInput(m_Image);
+        orientFilter->SetDesiredCoordinateOrientation(itk::SpatialOrientation::ITK_COORDINATE_ORIENTATION_LPS);
+        orientFilter->Update();
+        m_Image = orientFilter->GetOutput();
     }
 };
-
-// ... existing code ...
 
 JLCXX_MODULE define_julia_module(jlcxx::Module& mod)
 {
     mod.add_type<ITKImageWrapper>("ITKImageWrapper")
-        .constructor<std::string>()
+        .constructor<std::string>()  // For NIfTI files
+        .constructor<std::string, bool>()  // For DICOM series
         .method("getOrigin", &ITKImageWrapper::getOrigin)
         .method("getSpacing", &ITKImageWrapper::getSpacing)
         .method("getSize", &ITKImageWrapper::getSize)
         .method("getPixelData", &ITKImageWrapper::getPixelData)
-        .method("getDirection", &ITKImageWrapper::getDirection)  // Add this line
-        .method("writeImage", &ITKImageWrapper::writeImage);
-
-    // Explicitly map methods to the allocated type
-    mod.method("getOrigin", [](ITKImageWrapper& w) { return w.getOrigin(); });
-    mod.method("getSpacing", [](ITKImageWrapper& w) { return w.getSpacing(); });
-    mod.method("getSize", [](ITKImageWrapper& w) { return w.getSize(); });
-    mod.method("getPixelData", [](ITKImageWrapper& w) { return w.getPixelData(); });
-    mod.method("getDirection", [](ITKImageWrapper& w) { return w.getDirection(); });  // Add this line
-    mod.method("writeImage", [](ITKImageWrapper& w, const std::string& filename) { w.writeImage(filename); });
+        .method("getDirection", &ITKImageWrapper::getDirection)
+        .method("writeImage", &ITKImageWrapper::writeImage)
+        .method("reorientToLPS", &ITKImageWrapper::reorientToLPS);
 }
