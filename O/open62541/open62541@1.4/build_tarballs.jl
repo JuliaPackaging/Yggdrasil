b7a70c04917e1ef3b3ab9cf628f7521e0ae8159f
Thomas Vetter <thvt@lundbeck.com>
2025-02-04 18:22:34 +0100

    [open62541] update to v1.4.10

--- O/open62541/open62541@1.4/build_tarballs.jl
+++ O/open62541/open62541@1.4/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "open62541"
-version = v"1.4.9"
+version = v"1.4.10"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/open62541/open62541.git",
-              "403f4628c4aa4cb87a789d1063c1c79338384bcc")
+              "abc76d3374fee1633c4afcafd72d710d88a6e604")
 ]
 
 # Bash recipe for building across all platforms
