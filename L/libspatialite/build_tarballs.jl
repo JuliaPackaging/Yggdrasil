73830a166264a88b36c1c3f9858f3385d1119a27
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-05 16:15:56 +0100

    try gcc 8 for better libxml handling

--- L/libspatialite/build_tarballs.jl
+++ L/libspatialite/build_tarballs.jl
@@ -97,4 +97,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 #GEOS uses preferred of 6, so match that here
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"8")
