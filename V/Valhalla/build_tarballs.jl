797bbc2e049daf384d0657cff3f0a3026488c806
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-05 15:19:31 +0100

    add libspatialite

--- V/Valhalla/build_tarballs.jl
+++ V/Valhalla/build_tarballs.jl
@@ -100,6 +100,10 @@ dependencies = [
     HostBuildDependency("protoc_jll")
     Dependency("Zlib_jll")
     Dependency("SQLite_jll")
+    # Until we have a new version of OpenSSL built for riscv64 we need to use the
+    # `get_addable_spec` hack.  From v3.0.16 we should be able to remove it here.
+    Dependency(get_addable_spec("OpenSSL_jll", v"3.0.15+2"); compat="3.0.15", platforms=filter(p -> !(Sys.iswindows(p) || Sys.isapple(p)), platforms)),
+    Dependency("libspatialite_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
