4362879e6a4175a836ff3f249ff76581cd0e4f40
Bart Janssens <bart@bartjanssens.org>
2025-02-06 15:09:29 +0100

    [Qt6ShaderTools] Version 6.8.2 (host build) (#10452)

--- Q/Qt6ShaderTools/build_tarballs.jl
+++ Q/Qt6ShaderTools/build_tarballs.jl
@@ -3,17 +3,17 @@
 using BinaryBuilder, Pkg
 
 name = "Qt6ShaderTools"
-version = v"6.7.1"
+version = v"6.8.2"
 
 # Set this to true first when updating the version. It will build only for the host (linux musl).
-# After that JLL is in the registyry, set this to false to build for the other platforms, using
+# After that JLL is in the registry, set this to false to build for the other platforms, using
 # this same package as host build dependency.
-const host_build = false
+const host_build = true
 
 # Collection of sources required to build qt6
 sources = [
     ArchiveSource("https://download.qt.io/official_releases/qt/$(version.major).$(version.minor)/$version/submodules/qtshadertools-everywhere-src-$version.tar.xz",
-                  "e585e3a985b2e2bad8191a84489a04e69c3defc6022a8e746aad22a1f17910c2"),
+                  "d1d5f90e8885fc70d63ac55a4ce4d9a2688562033a000bc4aff9320f5f551871"),
     ArchiveSource("https://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/mingw-w64-v10.0.0.tar.bz2",
                   "ba6b430aed72c63a3768531f6a3ffc2b0fde2c57a3b251450dcf489a894f0894")
 ]
@@ -65,24 +65,12 @@ cmake --build . --parallel ${nproc}
 cmake --install .
 install_license $WORKSPACE/srcdir/qt*-src-*/LICENSES/LGPL-3.0-only.txt
 """
-
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-if host_build
-    platforms = [Platform("x86_64", "linux",cxxstring_abi=:cxx11,libc="musl")]
-else
-    platforms = expand_cxxstring_abis(filter(!Sys.isapple, supported_platforms()))
-    filter!(p -> arch(p) != "armv6l", platforms) # No OpenGL on armv6
-    platforms_macos = [ Platform("x86_64", "macos"), Platform("aarch64", "macos") ]
-end
+# Get the common Qt platforms
+include("../Qt6Base/common.jl")
 
 # The products that we will ensure are always built
 products = [
     LibraryProduct(["Qt6ShaderTools", "libQt6ShaderTools", "QtShaderTools"], :libqt6shadertools),
-]
-
-products_macos = [
-    FrameworkProduct("QtShaderTools", :libqt6shadertools),
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -95,13 +83,5 @@ if !host_build
     push!(dependencies, HostBuildDependency("Qt6ShaderTools_jll"))
 end
 
-include("../../fancy_toys.jl")
-
-@static if !host_build
-    if any(should_build_platform.(triplet.(platforms_macos)))
-        build_tarballs(ARGS, name, version, sources, script, platforms_macos, products_macos, dependencies; preferred_gcc_version = v"10", julia_compat="1.6")
-    end
-end
-if any(should_build_platform.(triplet.(platforms)))
-    build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"10", julia_compat="1.6")
-end
+# From ../Qt6Base/common.jl to ensure consistency
+build_qt(name, version, sources, script, products, dependencies)
