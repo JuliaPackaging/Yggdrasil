c4408b4bde4a44adb41cbbe003a5dbcb6663cccf
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-02-07 20:25:17 +0100

    Update build_tarballs.jl

--- S/SCIP_PaPILO/build_tarballs.jl
+++ S/SCIP_PaPILO/build_tarballs.jl
@@ -119,7 +119,7 @@ build_tarballs(
     platforms,
     products,
     dependencies;
-    preferred_gcc_version=v"8",
+    preferred_gcc_version=v"9",
     julia_compat="1.6",
     clang_use_lld=false,
 )
