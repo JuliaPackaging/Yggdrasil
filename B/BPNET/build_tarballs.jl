90f031d5916cdfab7c13090246604fb9765554f1
Yuki Nagai <cometscome@gmail.com>
2025-02-07 20:34:15 +0900

    Update build_tarballs.jl

--- B/BPNET/build_tarballs.jl
+++ B/BPNET/build_tarballs.jl
@@ -29,6 +29,7 @@ cp lib/* $prefix/
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
 products = [
