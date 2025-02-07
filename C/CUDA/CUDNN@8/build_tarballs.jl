2f6df6bab17c32528c1003847e179cdd7a610db5
Jesper Stemann Andersen <jesper@sait.dk>
2025-02-07 21:10:45 +0100

    CUDA: Add CUDNN v8.9.7

--- C/CUDA/CUDNN@8/build_tarballs.jl
+++ C/CUDA/CUDNN@8/build_tarballs.jl
@@ -8,7 +8,7 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "CUDNN"
-version = v"8.9.6"
+version = v"8.9.7"
 
 script = raw"""
 mkdir -p ${libdir} ${prefix}/include
