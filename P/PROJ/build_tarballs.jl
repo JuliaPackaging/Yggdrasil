519ed5a657c4219bcf9e8ba404c724ef0443c6f6
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-07 11:56:25 +0100

    enable aarch64-unknown-freebsd

--- P/PROJ/build_tarballs.jl
+++ P/PROJ/build_tarballs.jl
@@ -61,8 +61,6 @@ make install
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
-# Disable until the dependencies are available for this platform
-filter!(p -> !(Sys.isfreebsd(p) && arch(p) == "aarch64"), platforms)
 
 # The products that we will ensure are always built
 products = [
