ba981e791d925df6713d3a79d54fa90b8b6d3f63
Mathieu Besançon <mathieu.besancon@gmail.com>
2025-02-07 20:32:26 +0100

    [SCIP] scip up to 9.2.1 (#10474)
    * scip up to 921
    
    * remove RISC
    
    * Update build_tarballs.jl
    
    ---------
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- S/SCIP/build_tarballs.jl
+++ S/SCIP/build_tarballs.jl
@@ -3,14 +3,14 @@
 using BinaryBuilder, Pkg
 
 name = "SCIP"
-upstream_version = v"9.2.0"
+upstream_version = v"9.2.1"
 version = VersionNumber(upstream_version.major * 100, upstream_version.minor * 100, upstream_version.patch * 100)
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource(
         "https://scipopt.org/download/release/scipoptsuite-$(upstream_version).tgz",
-        "a174cc58592d245c74c9c95c1d4819750d7ba2d467b4baae616a5aa336aac8d0"
+        "41b71a57af773403e9a6724f78c37d8396ac4b6b270a9bbf3716d67f1af12edf"
     ),
 ]
 
@@ -65,7 +65,7 @@ cp $WORKSPACE/srcdir/scipoptsuite*/papilo/COPYING ${prefix}/share/licenses/SCIP/
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
 filter!(platforms) do p
-    !(Sys.isfreebsd(p) && arch(p) == "aarch64")
+    !(Sys.isfreebsd(p) && arch(p) == "aarch64") && !occursin("riscv", arch(p))
 end
 
 # The products that we will ensure are always built
@@ -78,7 +78,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="boost_jll", uuid="28df3c45-c428-5900-9ff8-a3135698ca75"); compat="=1.79.0"),
-    Dependency(PackageSpec(name="Bzip2_jll", uuid="6e34b625-4abd-537c-b88f-471c36dfa7a0"); compat="1.0.8"),
+    Dependency(PackageSpec(name="Bzip2_jll", uuid="6e34b625-4abd-537c-b88f-471c36dfa7a0"); compat="1.0.9"),
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
     Dependency(PackageSpec(name="GMP_jll", uuid="781609d7-10c4-51f6-84f2-b8444358ff6d"), v"6.2.1"),
     Dependency(PackageSpec(name="Ipopt_jll", uuid="9cc047cb-c261-5740-88fc-0cf96f7bdcc7"); compat="300.1400.1400"),
