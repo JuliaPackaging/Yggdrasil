157b79abef7cc2760540d1b8c659c631627ce3d0
Viral B. Shah <viral@mayin.org>
2025-02-09 14:52:33 -0500

    [Libevent] Rebuild for riscv

--- L/libevent/build_tarballs.jl
+++ L/libevent/build_tarballs.jl
@@ -4,8 +4,8 @@ using BinaryBuilder, Pkg
 
 name = "libevent"
 libevent_version =v"2.1.12"
-# We update to 2.1.14 because we updated our dependencies
-version = v"2.1.14"
+# We update to 2.1.15 because we updated our dependencies
+version = v"2.1.15"
 
 # Collection of sources required to complete build
 sources = [
