332689dad13f1b6a0bf101f9445d954fd65e7111
Divyansh Goyal <divital2004@gmail.com>
2025-02-09 20:34:27 +0530

    [ITKIOWrapper-v2.0.0] Added support for mac OS (#10295)
    * [ITKIOWrapper-v1.0.0] Added support for mac OS
    
    * [ITKIOWrapper-v1.0.0] Added eigen as a build dep
    
    * [ITKIOWrapper-v1.0.0] ITKIOWrapper stuff
    
    * [ITKIOWrapper-1.0.0] Changed C++ std
    
    * [ITKIOWrapper-v1.0.0] Fixing darwin compilation
    
    * [ITKIOWrapper-v1.0.0] Fixing darwin compilation
    
    * [ITKIOWrapper-v1.0.0] Fixing darwin compilation
    
    * [ITKIOWrapper-v1.0.0] Fixing darwin compilation
    
    * [ITKIOWrapper-v1.0.0] Rerunning build
    
    * removed windows until further notice.
    
    * Update build_tarballs.jl
    
    * [ITKIOWrapper-v1.0.0] Removing windows platform until further notice
    
    * [ITKIOWrapper-v1.0.0] Removing windows platform until further notice
    
    * [ITKIOWrapper-v1.0.0] Fixing checks stuck in a loop by rerunning builds
    
    * [ITKIOWrapper-v1.0.0] Added UUIDs package usage
    
    * [ITKIOWrapper-v1.0.0] Added UUIDs package usage
    
    * [ITKIOWrapper-v1.0.0] fixing errors
    
    * [ITKIOWrapper-v1.0.0] fixing errors
    
    * Docs
    
    * fixing errors
    
    * bumped julia versoin
    
    * bumped package version
    
    * bumped package and julia version
    
    * Added julia compat 1.10
    
    * Added julia compat 1.10
    
    * changed deletion version for stdlibs uuid
    
    * changed deletion version for stdlibs uuid
    
    * changed deletion version for stdlibs uuid
    
    * Removed windows platforms
    
    * Added julia versions 12 and 13 back
    
    * Filtered julia version 1.13 due to Jlcxx package error in cmake
    
    * Reverted julia version 1.13 exclusion
    
    * Update I/ITKIOWrapper/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    
    * Removed explicit macOs SDK version
    
    * Update I/ITKIOWrapper/build_tarballs.jl
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    
    * Update I/ITKIOWrapper/src/CMakeLists.txt
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    
    * Added changes based on reviews
    
    * Added new line in cmake
    
    ---------
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- I/ITKIOWrapper/build_tarballs.jl
+++ I/ITKIOWrapper/build_tarballs.jl
@@ -1,19 +1,20 @@
 # Note: this script will require BinaryBuilder.jl v0.3.0 or greater
 using BinaryBuilder, Pkg
 name = "ITKIOWrapper"
-version = v"1.0.0"  # Update this to your package version
-
-# The following lines should be updated to reflect the actual URL and revision of your ITKWrapper package
-
+version = v"2.0.0"
 # Collection of sources required to build ITKWrapper
 sources = [
     DirectorySource("./src"),
 ]
-uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
-delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
+uuidopenssl = Base.UUID("458c3c95-2e84-50aa-8efc-19380b2a3a95")
+delete!(Pkg.Types.get_last_stdlibs(v"1.12.0"), uuidopenssl)
+delete!(Pkg.Types.get_last_stdlibs(v"1.13.0"), uuidopenssl)
 #needed for libjulia_platforms and julia_versions
 include("../../L/libjulia/common.jl")
+
+#filter julia versions to include only Julia >= 1.10 for LTS
+julia_versions = filter(v-> v >= v"1.10", julia_versions)
 
 # Bash recipe for building across all platforms
 script = raw"""
@@ -24,25 +25,25 @@ cmake -B build -S . \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DBUILD_SHARED_LIBS:BOOL=ON
+    -DBUILD_SHARED_LIBS:BOOL=ON \
+    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+
 cmake --build build --parallel ${nproc}
 cmake --install build
 install_license /usr/share/licenses/MIT
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-
+# These are the platforms we will build for by default
 platforms = vcat(libjulia_platforms.(julia_versions)...)
-
-#Filtering all the platforms, that ITK filters https://github.com/JuliaPackaging/Yggdrasil/blob/master/I/ITK/build_tarballs.jl#L42-L53
+# Filter platforms
 filter!(p -> !(arch(p) == "i686"), platforms)
-filter!(!Sys.isapple, platforms)
 filter!(!Sys.isfreebsd, platforms)
 filter!(p -> !(arch(p) == "x86_64" && libc(p) == "musl"), platforms)
 filter!(p -> !(arch(p) == "riscv64"), platforms)
-platforms = expand_cxxstring_abis(platforms)
+filter!(!Sys.iswindows, platforms)
 
+# Expand C++ string ABI platforms
+platforms = expand_cxxstring_abis(platforms)
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libITKIOWrapper", :libITKIOWrapper),
@@ -50,10 +51,12 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("ITK_jll"; compat="5.3.0"),
-    Dependency("libcxxwrap_julia_jll"; compat = "0.13.2"),
-    BuildDependency("libjulia_jll")
+    BuildDependency(PackageSpec(name="Eigen_jll", uuid="bc6bbf8a-a594-5541-9c57-10b0d0312c70")),
+    Dependency(PackageSpec(name="ITK_jll", uuid="3324d3a8-621a-5aaa-97fa-c3bc8dfc0481"); compat="5.3.1"),
+    Dependency(PackageSpec(name="libcxxwrap_julia_jll", uuid="3eaa8342-bff7-56a5-9981-c04077f7cee7"); compat = "0.13.4"),
+    BuildDependency(PackageSpec(name="libjulia_jll", uuid="5ad3ddd2-0711-543a-b040-befd59781bbf"))
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"8.1.0")
+# Build the tarballs
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.10", preferred_gcc_version=v"8.1.0")
+
