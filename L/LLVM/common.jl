ddd6b6771c57d137462491c3415d9a7c5a371225
Zentrik <llvm.zentrik@gmail.com>
2025-02-10 23:10:51 +0000

    Update link and filter right function

--- L/LLVM/common.jl
+++ L/LLVM/common.jl
@@ -608,7 +608,6 @@ function configure_build(ARGS, version; experimental_platforms=false, assert=fal
     ]
 
     platforms = expand_cxxstring_abis(supported_platforms(; experimental=experimental_platforms))
-    filter!(p->arch(p)!="riscv64", platforms) # Not supported yet, see https://github.com/JuliaPackaging/Yggdrasil/pull/10503#issuecomment-2649445132
     if version >= v"15"
         # We don't build LLVM 15 for i686-linux-musl, see
         # <https://github.com/JuliaPackaging/Yggdrasil/pull/5592#issuecomment-1430063957>:
@@ -758,6 +757,7 @@ function configure_extraction(ARGS, LLVM_full_version, name, libLLVM_version=not
     end
 
     platforms = supported_platforms(; experimental=experimental_platforms)
+    filter!(p->arch(p)!="riscv64", platforms) # Not supported yet, see https://github.com/JuliaPackaging/BinaryBuilder.jl/issues/1366
     push!(platforms, Platform("x86_64", "linux"; sanitize="memory"))
     if version >= v"15"
         # We don't build LLVM 15 for i686-linux-musl.
