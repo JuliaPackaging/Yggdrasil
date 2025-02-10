d6b480aa1e87617912e6054452c3793da067db14
Jesper Stemann Andersen <jesper@sait.dk>
2025-02-10 10:57:23 +0100

    Fixed URL for SBSA

--- T/TensorRT/build_cuda+12.jl
+++ T/TensorRT/build_cuda+12.jl
@@ -3,7 +3,7 @@ platforms_and_sources = Dict(
         ArchiveSource("https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.7.0/tars/TensorRT-10.7.0.23.l4t.aarch64-gnu.cuda-12.6.tar.gz",
                       "b3028a82818a9daf6296f43d0cdecfa51eaea4552ffb6fe6fad5e6e1aea44da6")],
     Platform("aarch64", "linux"; cuda_platform="sbsa") => [
-        ArchiveSource("https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.7.0/tars/TensorRT-10.7.0.23.Ubuntu-24.04.aarch64-gnu.cuda-12.6.tar.gz",
+        ArchiveSource("https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.7.0/tars/TensorRT-10.7.0.23.Linux.aarch64-gnu.cuda-12.6.tar.gz",
                       "6b304cf014f2977e845bd44fdb343f0e7af2d9cded997bc9cfea3949d9e84dcb")],
     Platform("x86_64", "linux") => [
         ArchiveSource("https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.7.0/tars/TensorRT-10.7.0.23.Linux.x86_64-gnu.cuda-12.6.tar.gz",
