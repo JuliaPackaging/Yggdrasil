5ad55132de6b0916aa07f2442229e5cde8de88ce
Jesper Stemann Andersen <jesper@sait.dk>
2025-02-10 11:23:59 +0100

    [TensorRT] Changed to package v10.7.0 (#10422)
    v10.8.0 bumps the minium glibc version for Linux x86_64 from 2.17 to 2.28: https://docs.nvidia.com/deeplearning/tensorrt/archives/tensorrt-1070/release-notes/index.html#rel-10-7-0

--- T/TensorRT/build_cuda+11.jl
+++ T/TensorRT/build_cuda+11.jl
@@ -1,8 +1,8 @@
 platforms_and_sources = Dict(
     Platform("x86_64", "linux") => [
-        ArchiveSource("https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.5.0/tars/TensorRT-10.5.0.18.Linux.x86_64-gnu.cuda-11.8.tar.gz",
-                      "ca37e6752e8182173aa18051a21c3082f8fdcb9995951e0ed0a8c2395f6f77b3")],
+        ArchiveSource("https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.7.0/tars/TensorRT-10.7.0.23.Linux.x86_64-gnu.cuda-11.8.tar.gz",
+                      "958e1c32b48e41d1c48bdc94363450e14f996ca9de0e205ccee65af319eea2c0")],
     Platform("x86_64", "windows") => [
-        ArchiveSource("https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.5.0/zip/TensorRT-10.5.0.18.Windows.win10.cuda-11.8.zip",
-                      "9e83c1c877ee9b36e916adaaf8af009c6bddc05529b7f8ad0e8a39f582d92f93")],
+        ArchiveSource("https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/10.7.0/zip/TensorRT-10.7.0.23.Windows.win10.cuda-11.8.zip",
+                      "fd6ec60f8fc48cdd050fbcc632473b42c28a217f0ec44e0177c4cc9a18c77af8")],
 )
