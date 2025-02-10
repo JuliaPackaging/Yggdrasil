445bc6e3fa26afd11a9a1831caa4aef6482bf70e
Zentrik <llvm.zentrik@gmail.com>
2025-02-10 10:04:42 +0000

    [LLVM] Bump to v19.1.7+0 (#10479)
    * [LLVM] Bump to v19.1.7+0
    
    Also switch clang back to v16 for compilation as that seems to be faster.
    
    * Update L/LLVM/common.jl
    
    * Bump LLVM to 3cb75bacf7f61380a4ef45fef519c2fb5940dd4a
    
    ---------
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- L/LLVM/LLVM_full@19/build_tarballs.jl
+++ L/LLVM/LLVM_full@19/build_tarballs.jl
@@ -1,7 +1,7 @@
-version = v"19.1.1"
+version = v"19.1.7"
 
 include("../common.jl")
 
 build_tarballs(ARGS, configure_build(ARGS, version; experimental_platforms=true)...;
-               preferred_gcc_version=v"10", preferred_llvm_version=v"18", julia_compat="1.6")
-# Build trigger: 7
+               preferred_gcc_version=v"10", preferred_llvm_version=v"16", julia_compat="1.6")
+# Build trigger: 8
