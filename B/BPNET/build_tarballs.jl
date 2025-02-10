15208b3079ec2fcb8f8e5d934edd847a1f7db732
cometscome <cometscome@gmail.com>
2025-02-10 12:25:08 +0900

    revised

--- B/BPNET/build_tarballs.jl
+++ B/BPNET/build_tarballs.jl
@@ -7,6 +7,21 @@ version = v"0.0.2"
 
 # Collection of sources required to complete build
 sources = [
+    GitSource("https://github.com/cometscome/BPNET.git", "207fd1d739b8cadcf243fc1bd7e1e8d7d28af392"),
+    DirectorySource("./bundled")
+]
+
+
+
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "BPNET"
+version = v"0.0.2"
+
+# Collection of sources required to complete build
+sources = [
 GitSource("https://github.com/cometscome/BPNET.git", "207fd1d739b8cadcf243fc1bd7e1e8d7d28af392"),
     DirectorySource("./bundled")
 ]
@@ -19,27 +34,26 @@ cd $WORKSPACE/srcdir
 for f in ${WORKSPACE}/srcdir/patches/*.patch; do
     atomic_patch -p1 ${f}
 done
-cd BPNET-0.0.2/
-mkdir build 
-cd build
+cd BPNET/
+mkdir build
+cd build/
 cmake ..
 make
-cp ./bin/* ${prefix}/
-cp ./lib/* ${prefix}/
+cp bin/*.x $prefix/
+cp lib/* $prefix/
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
 platforms = expand_gfortran_versions(platforms)
- 
 
 # The products that we will ensure are always built
 products = [
-    ExecutableProduct("trainbin2ASCII.x", :trainbin2ASCII),
-    ExecutableProduct("bpnet_predict.x", :BPNET_predict),
-    ExecutableProduct("nnASCII2bin.x", :nnASCII2bin),
-    ExecutableProduct("bpnet_generate.x", :BPNET_generate)
+    ExecutableProduct("trainbin2ASCII.x", :bpnet_trainbin2ASCII),
+    ExecutableProduct("bpnet_predict.x", :bpnet_predict),
+    ExecutableProduct("nnASCII2bin.x", :bpnet_nnASCII2bin),
+    ExecutableProduct("bpnet_generate.x", :bpnet_generate)
 ]
 
 # Dependencies that must be installed before this package can be built
@@ -48,3 +62,4 @@ dependencies = Dependency[
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"13.2.0")
+
