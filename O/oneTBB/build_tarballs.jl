0929aec4e6a21014f27c71542e980dcfb0938e79
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-13 13:04:25 +0100

    try gcc11

--- O/oneTBB/build_tarballs.jl
+++ O/oneTBB/build_tarballs.jl
@@ -61,4 +61,4 @@ dependencies = Dependency[
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"13")
+               julia_compat="1.6", preferred_gcc_version=v"11")
