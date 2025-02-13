19af4fa68e132eeb3103aaf6eba7e2c773a4af21
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-13 20:59:13 +0100

    try using point release source

--- R/Readline/build_tarballs.jl
+++ R/Readline/build_tarballs.jl
@@ -7,10 +7,8 @@ version = v"8.2.13"
 
 # Collection of sources required to build Readline
 sources = [
-    ArchiveSource("https://ftp.gnu.org/gnu/readline/readline-$(version.major).$(version.minor).tar.gz",
-                  "3feb7171f16a84ee82ca18a36d7b9be109a52c04f492a053331d7d1095007c35"),
-    FileSource("https://ftp.gnu.org/gnu/readline/readline-$(version.major).$(version.minor)-patches/readline82-001",
-               "bbf97f1ec40a929edab5aa81998c1e2ef435436c597754916e6a5868f273aff7"),
+    ArchiveSource("https://ftp.gnu.org/gnu/readline/readline-$(version).tar.gz",
+                  "0e5be4d2937e8bd9b7cd60d46721ce79f88a33415dd68c2d738fb5924638f656"),
     DirectorySource("./bundled"),
 ]
 
@@ -18,7 +16,6 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir/readline-*/
 
-atomic_patch -p0 ${WORKSPACE}/srcdir/readline82-013
 # Patch from https://aur.archlinux.org/cgit/aur.git/tree/readline-1-fixes.patch?h=mingw-w64-readline
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/readline-1-fixes.patch
 
