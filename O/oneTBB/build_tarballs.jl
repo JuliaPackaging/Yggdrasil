1335bdbfe7c0bd7a10112608a61a43c8385132c3
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-13 13:37:58 +0100

    try gcc13 and Julia 1.9

--- O/oneTBB/build_tarballs.jl
+++ O/oneTBB/build_tarballs.jl
@@ -62,4 +62,4 @@ dependencies = Dependency[
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"12")
+               julia_compat="1.9", preferred_gcc_version=v"13")
