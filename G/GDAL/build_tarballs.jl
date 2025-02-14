d556ae17e2bb7836b67ce1354ca705755a158eaa
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-14 15:10:21 +0100

    [GDAL] Bump v3.10.2

--- G/GDAL/build_tarballs.jl
+++ G/GDAL/build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "GDAL"
-upstream_version = v"3.10.1"
+upstream_version = v"3.10.2"
 # The version offset is used for two purposes:
 # - If we need to release multiple jll packages for the same GDAL
 #   library (usually for weird packaging reasons) then we increase the
@@ -19,7 +19,7 @@ version = VersionNumber(upstream_version.major * 100 + version_offset.major,
 # Collection of sources required to build GDAL
 sources = [
     GitSource("https://github.com/OSGeo/gdal.git",
-        "9b7a7c8ffa7b7aff696974c432d4254a809b3efe"),
+        "e31053b64d9db2e0dc6f8eec0982908a2087eedf"),
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
         "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
     DirectorySource("./bundled")
