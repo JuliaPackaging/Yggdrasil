2eaa37fbe664d6a5d39e1b2c758dc0b89db452e6
Zheng Zhou (周正) <122618104+mankai-chow@users.noreply.github.com>
2025-02-14 16:05:25 -0500

    Update build_tarballs.jl

--- F/FuzzifiED/build_tarballs.jl
+++ F/FuzzifiED/build_tarballs.jl
@@ -18,7 +18,7 @@ make DLEXT=${dlext} LIB_DIR=${libdir} NBITS=${nbits}
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_gfortran_versions(supported_platforms(exclude = p -> arch(p) == "aarch64" && os(p) == "freebsd"))
+platforms = expand_gfortran_versions(supported_platforms(exclude = p -> os(p) == "freebsd"))
 
 # The products that we will ensure are always built
 products = [
@@ -28,8 +28,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
-    Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
     Dependency(PackageSpec(name="Arpack_jll", uuid="68821587-b530-5797-8361-c406ea357684"); compat="~3.5.1")
 ]
 
