b3fb4147d95702d9367bb4ba7b2ddb03f8e6f945
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-14 16:27:21 +0100

    tweak packagespec

--- V/Valhalla/build_tarballs.jl
+++ V/Valhalla/build_tarballs.jl
@@ -94,7 +94,7 @@ products = Product[
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    HostBuildDependency(PackageSpec(name="protoc_jll", version="105.29.3"))
+    HostBuildDependency(PackageSpec(name="protoc_jll", version=v"105.29.3"))
     Dependency("boost_jll"; compat="=1.87.0")
     Dependency("GEOS_jll"; compat="3.13.1")
     Dependency("LibCURL_jll"; compat="7.73,8")
