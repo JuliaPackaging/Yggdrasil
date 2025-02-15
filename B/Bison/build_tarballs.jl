e06e45de9883c3ef3add9e2a0ee3d50a5af124e9
Jeremiah <4462211+jeremiahpslewis@users.noreply.github.com>
2025-02-15 11:44:08 +0100

    [Bison] Build for riscv

--- B/Bison/build_tarballs.jl
+++ B/Bison/build_tarballs.jl
@@ -3,6 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "Bison"
+ygg_version = v"3.8.3" # Bump to pick up riscv
 version = v"3.8.2"
 
 # Collection of sources required to complete build
@@ -13,14 +14,16 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/bison-*
+
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --enable-relocatable
+
 make -j${nproc}
 make install
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; experimental=true)
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
