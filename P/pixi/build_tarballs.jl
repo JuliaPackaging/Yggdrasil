16c97932e76f36a046347d3d666cef22736a3cbc
Christopher Doris <github.com/cjdoris>
2025-02-17 12:57:33 +0000

    re-add lock_microarchitecture arg

--- P/pixi/build_tarballs.jl
+++ P/pixi/build_tarballs.jl
@@ -50,4 +50,5 @@ dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               compilers=[:c, :rust], julia_compat="1.6", lazy_artifacts=true)
+               compilers=[:c, :rust], julia_compat="1.6", lock_microarchitecture=false,
+               lazy_artifacts=true)
