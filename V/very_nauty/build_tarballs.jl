d299f9351d793bba40b03a7c910229d8c155b308
Stefan Krastanov <github.acc@krastanov.org>
2025-02-21 14:02:14 -0500

    rename to avoid clash with already existing `nauty`

--- V/very_nauty/build_tarballs.jl
+++ V/very_nauty/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "nauty_graph"
+name = "very_nauty"
 version = v"1.1.1"
 
 # Collection of sources required to complete build
