22bd5d1412b57013431fce7abdcd2aac45497058
Viral B. Shah <ViralBShah@users.noreply.github.com>
2025-02-21 12:57:17 -0500

    fix

--- O/Octave/build_tarballs.jl
+++ O/Octave/build_tarballs.jl
@@ -58,7 +58,6 @@ products = [
 dependencies = [
     HostBuildDependency("flex_jll"),
     HostBuildDependency("Bison_jll"),
-    HostBuildDependency("Librsvg_jll"),
     HostBuildDependency("gperf_jll"),
     Dependency("CompilerSupportLibraries_jll"),
     Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"),
@@ -84,6 +83,7 @@ dependencies = [
     Dependency("rapidjson_jll"),
     Dependency("libsndfile_jll"),
     Dependency("GraphicsMagick_jll"),
+    Dependency("Librsvg_jll"),
 ]
 
 # Build the tarballs.
