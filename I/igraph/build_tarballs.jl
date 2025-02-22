c9f7819ceb2bb214db821b2855932a2ad4e76c99
Stefan Krastanov <github.acc@krastanov.org>
2025-02-22 18:05:48 -0500

    try gcc 14 again

--- I/igraph/build_tarballs.jl
+++ I/igraph/build_tarballs.jl
@@ -78,4 +78,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"8") # early gcc versions cause problems with std::isnan
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"14") # early gcc versions cause problems with std::isnan
