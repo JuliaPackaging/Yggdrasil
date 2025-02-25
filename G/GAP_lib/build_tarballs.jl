27b7d4c1604d56908a6a319b68b445581035ff46
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-02-25 17:42:36 +0100

    [GAP_lib] Trigger rebuild (#10618)

--- G/GAP_lib/build_tarballs.jl
+++ G/GAP_lib/build_tarballs.jl
@@ -78,3 +78,5 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+
+# Build trigger: 1
