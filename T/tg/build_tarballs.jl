02ded698e15afaf0125c0bb4e36dd00db0198344
Anshul Singhvi <asinghvi17@simons-rock.edu>
2025-02-27 12:38:52 -0500

    Apply formatting suggestions
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- T/tg/build_tarballs.jl
+++ T/tg/build_tarballs.jl
@@ -4,12 +4,11 @@ using BinaryBuilder
 
 name = "tg"
 upstream_version = v"0.7.2"
-version = v"0.7.2" # Different version number because we needed to change compat bound, in the future we can go back to follow upstream
 
 # Collection of sources required to build Libtiff
 sources = [
     GitSource("https://github.com/tidwall/tg.git",
-                  "8fcb9b2809ad696025b4618ad226140f9ddbc452"),
+              "8fcb9b2809ad696025b4618ad226140f9ddbc452"),
 ]
 
 # Bash recipe for building across all platforms
@@ -30,7 +29,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
-    ]
+]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
