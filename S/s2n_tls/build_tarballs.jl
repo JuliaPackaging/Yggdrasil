53949cc5425c143811e95f7ba1191de930a071cb
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-01 07:16:21 +0000

    Update build_tarballs.jl

--- S/s2n_tls/build_tarballs.jl
+++ S/s2n_tls/build_tarballs.jl
@@ -42,3 +42,5 @@ dependencies = [
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.6", preferred_gcc_version=v"7")
+
+# build trigger: 1
