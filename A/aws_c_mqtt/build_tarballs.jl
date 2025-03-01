7663050b2830fda43ca9e6afd913aa5ffbeb7ac7
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-01 10:45:30 +0000

    Update build_tarballs.jl

--- A/aws_c_mqtt/build_tarballs.jl
+++ A/aws_c_mqtt/build_tarballs.jl
@@ -36,7 +36,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("aws_c_http_jll"; compat="0.8.1"),
+    Dependency("aws_c_http_jll"; compat="0.9.3"),
     Dependency("aws_c_io_jll"; compat="0.17.0"),
     BuildDependency("aws_lc_jll"),
 ]
