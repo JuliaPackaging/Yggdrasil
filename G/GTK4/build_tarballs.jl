f82bc530e12e496a93b84a853022025575cc8333
Jared Wahlstrand <jwahlstrand@gmail.com>
2025-03-01 18:15:20 -0500

    update GTK4 to 4.16.7

--- G/GTK4/build_tarballs.jl
+++ G/GTK4/build_tarballs.jl
@@ -3,13 +3,13 @@
 using BinaryBuilder
 
 name = "GTK4"
-version = v"4.14.5"
+version = v"4.16.7"
 
 # Collection of sources required to build GTK
 sources = [
     # https://download.gnome.org/sources/gtk/
     ArchiveSource("https://download.gnome.org/sources/gtk/$(version.major).$(version.minor)/gtk-$(version).tar.xz",
-                  "5547f2b9f006b133993e070b87c17804e051efda3913feaca1108fa2be41e24d"),
+                  "5303c7624e15a48880591b9ddd43389afba3de4fb92a219719519bb1642ce3dc"),
     ArchiveSource("https://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/mingw-w64-v10.0.0.tar.bz2",
                   "ba6b430aed72c63a3768531f6a3ffc2b0fde2c57a3b251450dcf489a894f0894"),
 ]
@@ -53,15 +53,16 @@ fi
 cd $WORKSPACE/srcdir/gtk*/
 
 mkdir build-gtk && cd build-gtk
-meson .. \
+# PKG_CONFIG_SYSROOT_DIR='' copied from xkbcommon recipe, see discussion there
+PKG_CONFIG_SYSROOT_DIR='' meson .. \
     --buildtype=release \
     -Dmedia-gstreamer=disabled \
     -Dintrospection=disabled \
-    -Ddemos=false \
+    -Dbuild-demos=false \
     -Dbuild-examples=false \
     -Dbuild-tests=false \
     -Dbuild-testsuite=false \
-    -Dgtk_doc=false \
+    -Ddocumentation=false \
     -Dvulkan=disabled \
     "${FLAGS[@]}" \
     --cross-file="${MESON_TARGET_TOOLCHAIN}"
@@ -98,12 +99,12 @@ dependencies = [
     BuildDependency("Xorg_xorgproto_jll"; platforms=x11_platforms),
     # Build needs a header from but does not link to libdrm
     BuildDependency("libdrm_jll"; platforms=x11_platforms),
-    Dependency("Glib_jll"; compat="2.76.5"),
+    Dependency("Glib_jll"; compat="2.82.2"),
     Dependency("Graphene_jll"; compat="1.10.6"),
     Dependency("Cairo_jll"),
-    Dependency("Pango_jll"; compat="1.50.3"),
+    Dependency("Pango_jll"; compat="1.54.1"),
     Dependency("FriBidi_jll"),
-    Dependency("FreeType2_jll"; compat="2.10.4"),
+    Dependency("FreeType2_jll"; compat="2.13.3"),
     Dependency("gdk_pixbuf_jll"),
     Dependency("Libepoxy_jll"),
     Dependency("Libtiff_jll"; compat="4.5.1"),
@@ -111,7 +112,7 @@ dependencies = [
     Dependency("xkbcommon_jll"; platforms=x11_platforms),
     Dependency("iso_codes_jll"),
     Dependency("Wayland_jll"; platforms=x11_platforms),
-    Dependency("Wayland_protocols_jll"; compat="1.31", platforms=x11_platforms),
+    Dependency("Wayland_protocols_jll"; compat="1.36", platforms=x11_platforms),
     Dependency("Xorg_libXrandr_jll"; platforms=x11_platforms),
     Dependency("Xorg_libX11_jll"; platforms=x11_platforms),
     Dependency("Xorg_libXrender_jll"; platforms=x11_platforms),
