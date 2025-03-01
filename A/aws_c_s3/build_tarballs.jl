a564ad196da4782dc135e7e50e6417e275ef8fef
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-01 08:32:29 +0000

    Update build_tarballs.jl

--- A/aws_c_s3/build_tarballs.jl
+++ A/aws_c_s3/build_tarballs.jl
@@ -36,9 +36,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("aws_checksums_jll"; compat="0.1.17"),
-    Dependency("aws_c_http_jll"; compat="0.8.3"),
-    Dependency("aws_c_auth_jll"; compat="0.7.18"),
+    Dependency("aws_checksums_jll"; compat="0.2.3"),
+    Dependency("aws_c_http_jll"; compat="0.9.3"),
+    Dependency("aws_c_auth_jll"; compat="0.8.5"),
     BuildDependency("aws_lc_jll"),
 ]
 
