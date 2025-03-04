ccf48a42dbe331599a502cfdffadb9cb9de202a3
Erik Schnetter <schnetter@gmail.com>
2025-03-04 14:50:30 -0500

    platforms/mpi.jl: Update compat and supported architectures

--- platforms/mpi.jl
+++ platforms/mpi.jl
@@ -45,8 +45,9 @@ using BinaryBuilder, Pkg
 using Base.BinaryPlatforms
 
 mpi_abis = (
-    ("MPICH", PackageSpec(name="MPICH_jll"), "4.2.3 - 4", !Sys.iswindows) ,
-    ("OpenMPI", PackageSpec(name="OpenMPI_jll"), "", p -> !Sys.iswindows(p) && !(arch(p) == "armv6l" && libc(p) == "glibc")),
+    # Note: riscv64 is only disabled because BinaryBuilder doesn't support it yet, the respective MPI packages have been built.
+    ("MPICH", PackageSpec(name="MPICH_jll"), "4.3.0 - 4", p -> !Sys.iswindows(p) && !(arch(p) == "riscv64")),
+    ("OpenMPI", PackageSpec(name="OpenMPI_jll"), "4.1.8, 5", p -> !Sys.iswindows(p) && !(arch(p) == "riscv64") && !(arch(p) == "armv6l" && libc(p) == "glibc")),
     ("MicrosoftMPI", PackageSpec(name="MicrosoftMPI_jll"), "", Sys.iswindows),
     ("MPItrampoline", PackageSpec(name="MPItrampoline_jll"), "5.5.0 - 5", p -> !(Sys.iswindows(p) || libc(p) == "musl"))
 )
