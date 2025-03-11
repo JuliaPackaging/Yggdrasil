ea21d45953bb341751190597446cc50a7221caf5
Ian Butterworth <i.r.butterworth@gmail.com>
2025-03-11 13:02:34 -0400

    try removing platform filters

--- D/Darknet/Darknet/build_tarballs.jl
+++ D/Darknet/Darknet/build_tarballs.jl
@@ -9,11 +9,11 @@ include("../common.jl")
 # platforms are passed in on the command line
 platforms = expand_cxxstring_abis(supported_platforms())
 
-# skip CXX03 string ABI because of many incompatibl for loop declarations
-filter!(p -> cxxstring_abi(p) != "cxx03", platforms)
+# # skip CXX03 string ABI because of many incompatibilities for loop declarations
+# filter!(p -> cxxstring_abi(p) != "cxx03", platforms)
 
-# same for old intel macOS
-filter!(p -> !(Sys.isapple(p) && arch(p) == "x86_64"), platforms)
+# # same for old intel macOS
+# filter!(p -> !(Sys.isapple(p) && arch(p) == "x86_64"), platforms)
 
 
 version, sources, script, products, dependencies = gen_common(platforms; gpu=false)
