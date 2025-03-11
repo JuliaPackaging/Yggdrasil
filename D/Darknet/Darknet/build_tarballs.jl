3344622586d16dde6446c57516ed632b097970e3
Ian Butterworth <i.r.butterworth@gmail.com>
2025-03-11 13:22:15 -0400

    reenable filters

--- D/Darknet/Darknet/build_tarballs.jl
+++ D/Darknet/Darknet/build_tarballs.jl
@@ -10,10 +10,10 @@ include("../common.jl")
 platforms = expand_cxxstring_abis(supported_platforms())
 
 # # skip CXX03 string ABI because of many incompatibilities for loop declarations
-# filter!(p -> cxxstring_abi(p) != "cxx03", platforms)
+filter!(p -> cxxstring_abi(p) != "cxx03", platforms)
 
 # # same for old intel macOS
-# filter!(p -> !(Sys.isapple(p) && arch(p) == "x86_64"), platforms)
+filter!(p -> !(Sys.isapple(p) && arch(p) == "x86_64"), platforms)
 
 
 version, sources, script, products, dependencies = gen_common(platforms; gpu=false)
