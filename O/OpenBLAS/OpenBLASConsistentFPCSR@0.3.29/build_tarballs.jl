6f893071fd2848d9c5cf8d5498f9456503d3c924
Olivier Hénot <38465572+OlivierHnt@users.noreply.github.com>
2025-03-12 13:11:06 +0100

    Update O/OpenBLAS/OpenBLASConsistentFPCSR@0.3.29/build_tarballs.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- O/OpenBLAS/OpenBLASConsistentFPCSR@0.3.29/build_tarballs.jl
+++ O/OpenBLAS/OpenBLASConsistentFPCSR@0.3.29/build_tarballs.jl
@@ -11,8 +11,7 @@ script = openblas_script(;aarch64_ilp64=true, num_64bit_threads=512, bfloat16=tr
 platforms = expand_gfortran_versions(supported_platforms(; exclude=p -> (arch(p) != "x86_64" && arch(p) != "aarch64")))
 push!(platforms, Platform("x86_64", "linux"; sanitize="memory"))
 products = openblas_products()
-preferred_llvm_version = v"13.0.1"
-dependencies = openblas_dependencies(platforms; llvm_compilerrt_version=preferred_llvm_version)
+dependencies = openblas_dependencies(platforms)
 
 # Build the tarballs
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
