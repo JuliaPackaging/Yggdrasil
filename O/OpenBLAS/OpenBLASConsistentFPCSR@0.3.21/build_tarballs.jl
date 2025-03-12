99d3c4d11a7ffb9b2a5835575e9d5428a625c58c
Olivier Hénot <38465572+OlivierHnt@users.noreply.github.com>
2025-03-12 01:43:16 +0100

    Revert 5aab9b7

--- O/OpenBLAS/OpenBLASConsistentFPCSR@0.3.21/build_tarballs.jl
+++ O/OpenBLAS/OpenBLASConsistentFPCSR@0.3.21/build_tarballs.jl
@@ -4,11 +4,14 @@ include("../common.jl")
 
 # Collection of sources required to build OpenBLAS
 name = "OpenBLASConsistentFPCSR"
-version = v"0.3.22"
+version = v"0.3.21"
 
 sources = openblas_sources(version)
 script = openblas_script(;aarch64_ilp64=true, num_64bit_threads=512, consistent_fpcsr=true)
-platforms = expand_gfortran_versions(supported_platforms(; exclude=p -> (arch(p) != "x86_64" && arch(p) != "aarch64")))
+# CONSISTENT_FPCSR = 1 works only for for x86/x86_64
+# It should work for aarch64 but we have no aarch64 machine for debugging,
+# so we exclude it for the moment
+platforms = expand_gfortran_versions(supported_platforms(; exclude=p -> arch(p) != "x86_64"))
 # push!(platforms, Platform("x86_64", "linux"; sanitize="memory"))
 products = openblas_products()
 dependencies = openblas_dependencies(platforms)
