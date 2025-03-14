0d02fb876a993fc77fc9a75ebbf0e10058139e44
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-03-14 16:31:19 +0100

    [normaliz] Rebuild with FLINT 3.2.0

--- N/normaliz/build_tarballs.jl
+++ N/normaliz/build_tarballs.jl
@@ -22,14 +22,13 @@ import Pkg.Types: VersionSpec
 # to all components.
 
 name = "normaliz"
-version = v"300.1000.200"
+version = v"300.1000.201"
 upstream_version = v"3.10.2"
 
 # Collection of sources required to complete build
 sources = [
     #ArchiveSource("https://github.com/Normaliz/Normaliz/releases/download/v$(upstream_version)/normaliz-$(upstream_version).tar.gz",
     #              "365e1d1e2a338dc4df1947a440e606bb66dd261307e617905e8eca64eaafcf6e"),
-    # this is basically 3.10.1 + flint3 support
     GitSource("https://github.com/Normaliz/Normaliz.git",
               "3bc242209e82488886eada17006e372fd89aa032"),
 ]
@@ -71,7 +70,7 @@ products = [
 dependencies = [
     Dependency("GMP_jll", v"6.2.0"),
     Dependency("MPFR_jll", v"4.1.1"),
-    Dependency("FLINT_jll"; compat = "~300.100.300"),
+    Dependency("FLINT_jll"; compat = "~300.200.0"),
     Dependency("nauty_jll"; compat = "~2.6.13"),
     # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
     # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
