4285c00d244b51547a24baaee5b98a12b0ab603a
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-03-16 14:09:33 +0100

    Set gcc version to v6

--- N/nauty/build_tarballs.jl
+++ N/nauty/build_tarballs.jl
@@ -128,6 +128,8 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6",
+               preferred_gcc_version=v"6")
 
 # Build trigger: 1
