00f4b7a38cb15bcc1d248b9e9a1adc60ca0b50a0
Nicolas Riel <a.nicolas.riel@gmail.com>
2025-03-18 22:06:00 +0100

    re-added filter for aarch64 platform

--- M/MAGEMin/build_tarballs.jl
+++ M/MAGEMin/build_tarballs.jl
@@ -42,7 +42,7 @@ install_license LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = supported_platforms((; exclude=p->Sys.isfreebsd(p) && arch(p) == "aarch64"))
 platforms = expand_gfortran_versions(platforms)
 platforms = filter(p -> !(arch(p)  == "riscv64"), platforms)
 platforms = filter(p -> !( (libgfortran_version(p) == v"3" || libgfortran_version(p) == v"4") && arch(p)=="powerpc64le"), platforms)
