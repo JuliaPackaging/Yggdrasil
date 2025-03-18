2068497aaf9c6a39e14cb784bf105f3831ff47ec
Nicolas Riel <a.nicolas.riel@gmail.com>
2025-03-18 14:42:26 +0100

    trying to correct flag to deactivate MPI

--- M/MAGEMin/build_tarballs.jl
+++ M/MAGEMin/build_tarballs.jl
@@ -16,7 +16,9 @@ sources = [GitSource("https://github.com/ComputationalThermodynamics/MAGEMin",
 script = raw"""
 cd MAGEMin*
 
-CCFLAGS="-DUSE_MPI=0 -O3 -g -fPIC -std=c99"
+USE_MPI ?= 0
+
+CCFLAGS="-DUSE_MPI -O3 -g -fPIC -std=c99"
 
 if [[ "${target}" == *-apple* ]]; then 
     # Use Accelerate for Lapack dependencies
@@ -28,10 +30,10 @@ else
 fi
 
 # Compile library:
-make CC="${CC}" CCFLAGS="${CCFLAGS}" LIBS="${LIBS}" INC="${INC}" lib
+make USE_MPI=0 CC="${CC}" CCFLAGS="${CCFLAGS}" LIBS="${LIBS}" INC="${INC}" lib
 
 # Compile binary
-make EXE_NAME="MAGEMin${exeext}" CC="${CC}" CCFLAGS="${CCFLAGS}" LIBS="${LIBS}" INC="${INC}" all
+make USE_MPI=0 EXE_NAME="MAGEMin${exeext}" CC="${CC}" CCFLAGS="${CCFLAGS}" LIBS="${LIBS}" INC="${INC}" all
 
 install -Dvm 755 libMAGEMin.dylib "${libdir}/libMAGEMin.${dlext}"
 install -Dvm 755 MAGEMin${exeext} "${bindir}/MAGEMin${exeext}"
