13cecc010b72ebc569af257f52b7b11c362cdce1
Alexander Plavin <alexander@plav.in>
2025-03-18 23:56:02 -0700

    Update build_tarballs.jl

--- T/TDLib/build_tarballs.jl
+++ T/TDLib/build_tarballs.jl
@@ -46,4 +46,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"7", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"100", julia_compat="1.6")
