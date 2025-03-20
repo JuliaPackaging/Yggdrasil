72b8061b1fbed840b2fef7980dcfd5c59dc2b5de
AlexKlo <65765845+AlexKlo@users.noreply.github.com>
2025-03-20 17:51:00 +0100

    Update build_tarballs.jl

--- Q/quickfix/build_tarballs.jl
+++ Q/quickfix/build_tarballs.jl
@@ -8,6 +8,8 @@ version = v"1.16.0"
 # Collection of sources required to build CMake
 sources = [
     GitSource("https://github.com/quickfix/quickfix.git", "92c85ca63fc260d16e24e0ece419ecdec9ffe868"),
+    ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
+    "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
     DirectorySource("./bundled")
 ]
 
@@ -20,6 +22,16 @@ julia_versions = filter(v-> v >= v"1.8", julia_versions)
 # Bash recipe for building across all platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/quickfix
+
+if [[ "${target}" == x86_64-apple-darwin* ]]; then
+    # Install a newer SDK which supports `shared_timed_mutex` and `std::filesystem`
+    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
+    rm -rf /opt/${target}/${target}/sys-root/System
+    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
+    cp -ra System "/opt/${target}/${target}/sys-root/."
+    export MACOSX_DEPLOYMENT_TARGET=10.15
+    popd
+fi
 
 for f in ${WORKSPACE}/srcdir/patches/*.patch; do
     atomic_patch -p1 ${f}
@@ -42,7 +54,8 @@ install_license ${WORKSPACE}/srcdir/quickfix/LICENSE
 """
 
 # Build for all supported platforms.
-platforms = supported_platforms()
+platforms = vcat(libjulia_platforms.(julia_versions)...)
+platforms = filter(!Sys.iswindows, platforms)
 
 # The products that we will ensure are always built
 products = [
