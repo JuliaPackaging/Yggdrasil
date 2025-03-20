bec077554ee98ef54d5a6450033474f101ce02d2
AlexKlo <65765845+AlexKlo@users.noreply.github.com>
2025-03-20 16:39:28 +0100

    Update build_tarballs.jl

--- Q/quickfix/build_tarballs.jl
+++ Q/quickfix/build_tarballs.jl
@@ -55,4 +55,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.8", preferred_gcc_version=v"7")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.8", preferred_gcc_version=v"9")
