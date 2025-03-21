cac9e946cdbefa2c8fc9fec28aad74ebbd80bb28
ejmeitz <emeitz@andrew.cmu.edu>
2025-03-21 10:55:09 -0400

    run tests in build

--- T/TDEP/build_tarballs.jl
+++ T/TDEP/build_tarballs.jl
@@ -50,6 +50,15 @@ export LDFLAGS="-L${libdir} -L${WORKSPACE}srcdir/tdep/build/libolle"
 bash ${WORKSPACE}/srcdir/make_important_settings.sh
 bash build_things.sh --clean --nomanpage --nthreads_make ${nproc} --install
 
+pip3 install pytest
+cd ${WORKSPACE}/srcdir/tdep/tests
+pytest > test_results.txt
+RESULT=$?
+if [RESULT -eq 0 ]; then
+  echo "All tests passed!"
+else
+  echo "Tests failed with exit code $RESULT"
+fi
 
 install_license ${WORKSPACE}/srcdir/tdep/LICENSE.md ${WORKSPACE}/srcdir/tdep/CITATION.cff
 """
@@ -60,10 +69,7 @@ augment_platform_block = """
     augment_platform!(platform::Platform) = augment_mpi!(platform)
 """
 
-# platforms = [Platform("aarch64", "macos")]
 platforms = supported_platforms()
-# platforms = [Platform("x86_64", "linux", libc=:glibc), Platform("aarch64", "macos")]
-
 
 # TODO REMOVE i686-linux-musl
 # Do no support Windows (yet)
@@ -75,21 +81,16 @@ platforms = filter(p -> arch(p) != "riscv64", platforms)
 # No 32-bit support
 platforms = filter(p -> nbits(p) != 32, platforms)
 
-# This breaks things, I expect its cause I used GCC10
-platforms = expand_gfortran_versions(platforms) 
-
-# This library uses Fortran 2008 features which are only
+# TDEP uses Fortran 2008 features
 # libgfortran 3 & 4 always crash with syntax issues
+platforms = expand_gfortran_versions(platforms) 
 platforms = filter(p -> libgfortran_version(p) >= v"5.0.0", platforms)
 
-
-# We need to use the same compat bounds as HDF5
+# Need to use the same compat bounds as HDF5
 platforms, platform_dependencies = MPI.augment_platforms(platforms; MPItrampoline_compat="5.5.0", OpenMPI_compat="4.1.6, 5")
 
 # Avoid platforms where the MPI implementation isn't supported
 platforms = filter(p -> !(p["mpi"] == "openmpi" && Sys.isfreebsd(p)), platforms)
-# platforms = filter(p -> !(p["mpi"] == "mpitrampoline" && libc(p) == "musl"), platforms)
-
 
 # Only use OpenMPI or MPICH, add windows here later if desired
 platforms = filter(p -> (p["mpi"] == "mpich" || p["mpi"] == "openmpi"), platforms)
@@ -122,14 +123,7 @@ dependencies = [
 ]
 
 # This will add the MPI dependencies
-# This also adds Microsoft_JLL and MPITrampoline even though I don't need those
 append!(dependencies, platform_dependencies)
-# println("DEPS: $(dependencies)")
-
-# Don't look for `mpiwrapper.so` when BinaryBuilder examines and
-# `dlopen`s the shared libraries. (MPItrampoline will skip its
-# automatic initialization.)
-# ENV["MPITRAMPOLINE_DELAY_INIT"] = "1"
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                  augment_platform_block, julia_compat = "1.6", preferred_gcc_version = v"10")
