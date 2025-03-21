be2f7bec924bdf6e0df643c21c437fd7658eae5c
ejmeitz <emeitz@andrew.cmu.edu>
2025-03-20 21:58:54 -0400

    test upstream changes

--- T/TDEP/build_tarballs.jl
+++ T/TDEP/build_tarballs.jl
@@ -8,12 +8,16 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 name = "TDEP"
 version = v"24.09"
 sources = [
-    GitSource("https://github.com/tdep-developers/tdep.git", "8579c30cce7a3d78c2a88535cda247e0c00eb1a9"),
-    DirectorySource("./bundled") # contents will be copied to ${WORKSPACE}/srcdir
+    GitSource("https://github.com/ejmeitz/tdep.git", "8d36ad97bf974116c29fef8c3d3be9ffa7ebe554"),
+    DirectorySource("./bundled")
 ]
 
 # IF OPENMPI USE -lmpi -lmpi_mpifh
 # IF MPICH USE -lmpi -lmpifort
+
+# cp ../Makefile.shared ./src/libolle/SharedMakefile
+# cp ../Makefile.install ./src/libolle/InstallMakefile
+
 
 script = raw"""
 
@@ -40,18 +44,11 @@ if [[ ${target} == x86_64-linux-musl ]]; then
     rm /usr/lib/libnghttp2.*
 fi
 
+export LDFLAGS="-L${libdir} -L${WORKSPACE}srcdir/tdep/build/libolle"
+
 bash ${WORKSPACE}/srcdir/make_important_settings.sh
-bash build_things.sh --clean --nomanpage --nthreads_make ${nproc}
+bash build_things.sh --clean --nomanpage --nthreads_make ${nproc} --install
 
-
-export LDFLAGS="-L${libdir}"
-cp ../Makefile.shared ./src/libolle/SharedMakefile
-cp ../Makefile.install ./src/libolle/InstallMakefile
-
-cd ./src/libolle
-make -f SharedMakefile -j ${nproc}
-
-make -f InstallMakefile install BB_bindir=${bindir} BB_libdir=${libdir}
 
 install_license ${WORKSPACE}/srcdir/tdep/LICENSE.md ${WORKSPACE}/srcdir/tdep/CITATION.cff
 """
@@ -62,17 +59,27 @@ augment_platform_block = """
     augment_platform!(platform::Platform) = augment_mpi!(platform)
 """
 
+# platforms = [Platform("aarch64", "macos")]
 platforms = supported_platforms()
-# platforms = [Platform("x86_64", "linux", libc=:glibc)]
+# platforms = [Platform("x86_64", "linux", libc=:glibc), Platform("aarch64", "macos")]
 
-# This breaks things, I expect its cause I used GCC10
-# platforms = expand_gfortran_versions(platforms) 
 
 # TODO REMOVE i686-linux-musl
-# platforms = filter(p -> os(p) == "linux" || os(p) == "macos", platforms) # could support others, but lets get it working first
-platforms = filter(p -> os(p) == "linux", platforms) # could support others, but lets get it working first
-platforms = filter(p -> arch(p) != "riscv64", platforms) # Doesn't work with gcc10 it seems
-platforms = filter(p -> nbits(p) ≠ 32, platforms) # Only support 64-bit platforms
+# Do no support Windows (yet)
+platforms = filter(p -> os(p) == "linux" || os(p) == "macos", platforms)
+
+# Remove RiscV until thats something someone actually wants
+platforms = filter(p -> arch(p) != "riscv64", platforms)
+
+# No 32-bit support
+platforms = filter(p -> nbits(p) != 32, platforms)
+
+# This breaks things, I expect its cause I used GCC10
+platforms = expand_gfortran_versions(platforms) 
+
+# This library uses Fortran 2008 features which are only
+# supported by gfortran>=7 --> libgfortran 4,5
+platforms = filter(p -> libgfortran_version(p) >= v"4.0.0", platforms)
 
 
 # We need to use the same compat bounds as HDF5
@@ -86,17 +93,15 @@ platforms = filter(p -> !(p["mpi"] == "openmpi" && Sys.isfreebsd(p)), platforms)
 # Only use OpenMPI or MPICH, add windows here later if desired
 platforms = filter(p -> (p["mpi"] == "mpich" || p["mpi"] == "openmpi"), platforms)
 
-# println(platform_dependencies)
-# println(platforms)
 
 products = [
-    LibraryProduct("libolle", :libolle), #there is libolle.a and .so, this grabs the shared version I think
+    LibraryProduct("libolle", :libolle),
     ExecutableProduct("generate_structure", :generate_structure),
     ExecutableProduct("canonical_configuration", :canonical_configuration),
     ExecutableProduct("extract_forceconstants", :extract_forceconstants),
     ExecutableProduct("phonon_dispersion_relations", :phonon_dispersion_relations),
     ExecutableProduct("thermal_conductivity", :thermal_conductivity),
-    # ExecutableProduct("thermal_conductivity_2023", :thermal_conductivity_2023),
+    ExecutableProduct("thermal_conductivity_2023", :thermal_conductivity_2023),
     ExecutableProduct("lineshape", :lineshape),
     ExecutableProduct("anharmonic_free_energy", :anharmonic_free_energy),
     ExecutableProduct("atomic_distribution", :atomic_distribution),
@@ -105,22 +110,24 @@ products = [
     ExecutableProduct("dump_dynamical_matrices", :dump_dynamical_matrices),
     ExecutableProduct("crystal_structure_info", :crystal_structure_info),
     ExecutableProduct("refine_structure", :refine_structure),
-    ExecutableProduct("phasespace_surface", :phasespace_surface)
+    # ExecutableProduct("phasespace_surface", :phasespace_surface)
 ]
 
 dependencies = [
-    Dependency("HDF5_jll"), Dependency("FFTW_jll"), Dependency("OpenBLAS32_jll") # Dependency("LAPACK_jll")
+    Dependency("HDF5_jll"),
+    Dependency("FFTW_jll"),
+    Dependency("OpenBLAS32_jll")
 ]
 
 # This will add the MPI dependencies
 # This also adds Microsoft_JLL and MPITrampoline even though I don't need those
 append!(dependencies, platform_dependencies)
-println("DEPS: $(dependencies)")
+# println("DEPS: $(dependencies)")
 
 # Don't look for `mpiwrapper.so` when BinaryBuilder examines and
 # `dlopen`s the shared libraries. (MPItrampoline will skip its
 # automatic initialization.)
-ENV["MPITRAMPOLINE_DELAY_INIT"] = "1"
+# ENV["MPITRAMPOLINE_DELAY_INIT"] = "1"
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                  augment_platform_block, julia_compat = "1.6", preferred_gcc_version = v"10")
