cb0b1a2b75f6815966d4f6c4addd39a72c7dfb90
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-03-21 17:30:10 +0000

    [Reactant] Upgrade to v0.0.96 (#10796)
    * [Reactant] Upgrade to v0.0.96
    
    * Update build_tarballs.jl

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -6,10 +6,10 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "Reactant"
 repo = "https://github.com/EnzymeAD/Reactant.jl.git"
-version = v"0.0.95"
+version = v"0.0.96"
 
 sources = [
-  GitSource(repo, "66e4a54e81a135e31e84f6c45eff5c589c89e57e"),
+  GitSource(repo, "8bdc51d9c9658048d7ccdf50b4d3b7c6e20b3983"),
   FileSource("https://github.com/wsmoses/binaries/releases/download/v0.0.1/bazel-dev",
              "8b43ffdf519848d89d1c0574d38339dcb326b0a1f4015fceaa43d25107c3aade")
 ]
