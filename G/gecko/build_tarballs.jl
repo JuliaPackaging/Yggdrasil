faa6ad76f70a7bb4a3a54bfe6452e4f2d2bce2de
termi-official <termi-official@users.noreply.github.com>
2025-03-24 22:13:35 +0100

    Add julia compat back

--- G/gecko/build_tarballs.jl
+++ G/gecko/build_tarballs.jl
@@ -64,4 +64,4 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"8", julia_compat="1.6")
