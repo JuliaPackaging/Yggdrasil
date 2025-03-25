44d4dd82f58aceaa55d34ce83248e77915a3e51a
ArseniyKholod <119304909+ArseniyKholod@users.noreply.github.com>
2025-03-25 09:41:25 +0100

    [OpenFHE] and [OpenFHE_int128] update to 1.2.4

--- O/OpenFHE/common.jl
+++ O/OpenFHE/common.jl
@@ -39,7 +39,9 @@ function prepare_openfhe_build(name::String, git_hash::String)
     
     # These are the platforms we will build for by default, unless further
     # platforms are passed in on the command line
-    platforms = supported_platforms()
+    # Required Julia version is 1.10
+    filter!(v -> v >= v"1.10", julia_versions)
+    platforms = vcat(libjulia_platforms.(julia_versions)...)
 
     # We cannot build with musl since OpenFHE requires the `execinfo.h` header for `backtrace`
     platforms = filter(p -> libc(p) != "musl", platforms)
--- O/OpenFHE_int128/build_tarballs.jl
+++ O/OpenFHE_int128/build_tarballs.jl
@@ -5,11 +5,11 @@ include(joinpath(pwd(), "..", "OpenFHE", "common.jl"))
 # If you make changes in this file, e.g., to release a new version,
 # be sure to also release a new version of `OpenFHE` as well (see `../OpenFHE/build_tarballs.jl`)
 name = "OpenFHE_int128"
-version = v"1.2.3"
+version = v"1.2.4"
 
 git_hash = "7b8346f4eac27121543e36c17237b919e03ec058"
 
 sources, script, platforms, products, dependencies = prepare_openfhe_build(name, git_hash)
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"10.2.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.10", preferred_gcc_version = v"10.2.0")
