0843b49c60de304b3beff6e1aa3633402c422ff4
ejmeitz <emeitz@andrew.cmu.edu>
2025-03-25 14:36:05 -0400

    manually install python deps

--- L/legate/build_tarballs.jl
+++ L/legate/build_tarballs.jl
@@ -16,19 +16,34 @@ sources = [
 CUDA_VERSION = CUDA.full_version(v"12.2")
 # CUDA_VERSION = CUDA.full_version(v"11.8")
 
+# --build=${MACHTYPE} \
+# --host=${target} \
+
 script = raw"""
+
+export PATH=${host_bindir}/python3.10:$PATH
+
+cd ${WORKSPACE}/srcdir
+mkdir config_deps && cd config_deps
+pip3 download --no-deps rich
+pip3 download --no-deps typing_extensions
+RICH_WHEEL=$(ls rich-*-py3-none-any.whl)
+TYPE_EXT_WHEEL=$(ls typing_extensions-*-py3-none-any.whl)
+unzip -q "$RICH_WHEEL" -d config_deps
+unzip -q "$TYPE_EXT_WHEEL" -d config_deps
+
+export PY_DEPS_DIR=$(pwd)
+
+PYTHONPATH="${PY_DEPS_DIR}/config_deps" python3.10 -c "import rich, typing_extensions; print('Python deps installed and working!')"
+
 cd ${WORKSPACE}/srcdir/legate
 
 export CPPFLAGS="${CPPFLAGS} -I${prefix}/include"
 export CLFAGS="${CLFAGS} -I${prefix}/include"
 export LDFLAGS="${LDFLAGS} -L${prefix}/lib -L${prefix}/lib64"
 
-export PATH=${host_bindir}/python3.10:$PATH
-
-python3.10 configure \
+PYTHONPATH="${PY_DEPS_DIR}/config_deps" python3.10 configure \
     --prefix=$prefix \
-    --build=${MACHTYPE} \
-    --host=${target} \
     --build-march=${target%%-*} \
     --with-cuda \
     --with-cudac=${bindir}/cuda/nvcc \
@@ -67,6 +82,7 @@ dependencies = [
     Dependency("UCX_jll"),
     Dependency("Zlib_jll"),
     Dependency("CUDA_SDK_jll", CUDA_VERSION), #* PROBABLY BETTER TO DEFINE A COMPAT FOR >= 12.0
+    # HostBuildDependency(PackageSpec(; name = "Expat_jll")),
     HostBuildDependency(PackageSpec(; name = "Python_jll", version = v"3.10.16"))
 ]
 
@@ -80,6 +96,7 @@ for platform in platforms
     build_tarballs(ARGS, name, version, sources, script, [platform],
                     products, [dependencies; cuda_deps];
                     julia_compat = "1.10", preferred_gcc_version = v"13",
-                    augment_platform_block=CUDA.augment)
+                    augment_platform_block=CUDA.augment
+                )
 
 end
