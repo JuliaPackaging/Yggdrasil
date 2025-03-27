830cfbe06b12cbf1bd389b858e7b61b474661adc
quinnj2 <karbarcca2@gmail.com>
2025-03-27 15:35:33 -0600

    [aws_c_common] Update to 0.12.2 (#10831)
    Co-authored-by: github-actions <github-actions@github.com>

--- A/aws_c_common/build_tarballs.jl
+++ A/aws_c_common/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_common"
-version = v"0.12.1"
+version = v"0.12.2"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/awslabs/aws-c-common.git", "d80b00560f0ebb441538b3ab40192a242afeaa80"),
+    GitSource("https://github.com/awslabs/aws-c-common.git", "8ae8f48ebddb0ee2624d643952ac33afa5e8859e"),
 ]
 
 # Bash recipe for building across all platforms
