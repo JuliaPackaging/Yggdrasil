4d904deb88e14fba48459e502b7cf044cca7bd62
Avik Pal <avikpal@mit.edu>
2025-03-27 20:37:47 -0400

    Reactant_jll v0.0.106 (#10839)

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -6,10 +6,10 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "Reactant"
 repo = "https://github.com/EnzymeAD/Reactant.jl.git"
-version = v"0.0.105"
+version = v"0.0.106"
 
 sources = [
-  GitSource(repo, "fc986ff7970fd528365a8639a6cebc031a163399"),
+  GitSource(repo, "e97549e4d056772f3f72c84601cd90d91f597b94"),
   FileSource("https://github.com/wsmoses/binaries/releases/download/v0.0.1/bazel-dev",
              "8b43ffdf519848d89d1c0574d38339dcb326b0a1f4015fceaa43d25107c3aade")
 ]
