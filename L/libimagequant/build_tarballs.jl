eb13cdf33fbf0894639fc82bc1536be663f77f8b
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-03-29 01:15:09 +0100

    Update L/libimagequant/build_tarballs.jl

--- L/libimagequant/build_tarballs.jl
+++ L/libimagequant/build_tarballs.jl
@@ -38,7 +38,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = Dependency[
-    HostBuildDependency("OpenSSL_jll"; compat="3.0.16"),
+    HostBuildDependency("OpenSSL_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
