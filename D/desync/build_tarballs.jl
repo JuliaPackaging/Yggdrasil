b9c81d1c0c6592b3745698d91fa6615a7631ccfb
Elliot Saba <staticfloat@gmail.com>
2025-03-29 20:38:26 +0800

    Exclude freebsd

--- D/desync/build_tarballs.jl
+++ D/desync/build_tarballs.jl
@@ -19,6 +19,7 @@ install -Dvm 755 "desync${exeext}" -t "${bindir}"
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+filter!(p -> !Sys.isfreebsd(p), platforms)
 
 # The products that we will ensure are always built
 products = [
