3a5f975db86093f5ea5654aa7111af59ca54755e
Erik Schnetter <schnetter@gmail.com>
2025-04-02 13:44:17 -0400

    Graphene: Correct Apple build

--- G/Graphene/build_tarballs.jl
+++ G/Graphene/build_tarballs.jl
@@ -40,4 +40,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               clang_use_lld=false, julia_compat="1.6")
