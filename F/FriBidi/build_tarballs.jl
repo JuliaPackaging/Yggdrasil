733f9b8d80f401c1c7efde05484493e9d1994df7
Erik Schnetter <schnetter@gmail.com>
2025-04-02 13:39:42 -0400

    FriBidi: Correct Julia version number

--- F/FriBidi/build_tarballs.jl
+++ F/FriBidi/build_tarballs.jl
@@ -38,5 +38,5 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+build_tarballs(ARGS, name, ygg_version, sources, script, platforms, products, dependencies;
                julia_compat="1.6", clang_use_lld=false)
