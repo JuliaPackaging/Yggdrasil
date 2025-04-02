4ff7c438742562785e8e1e767a799555907d1aa0
Erik Schnetter <schnetter@gmail.com>
2025-04-02 17:55:10 -0400

    XZ: New version 5.8.0 (#10909)

--- X/XZ/build_tarballs.jl
+++ X/XZ/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder
 
 name = "XZ"
-version = v"5.6.4"
+version = v"5.8.0"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/tukaani-project/xz",
-              "ac50df0d89ce73f30430b8174e578071cbb4e056")
+              "db9258e828bc2cd96e3954f1ddcc9d3530589025")
 ]
 
 # Bash recipe for building across all platforms
