6f61ccd70242bbfebd3581c08622a418ca0c11a3
Erik Schnetter <schnetter@gmail.com>
2025-04-02 21:54:31 -0400

    Dbus: New version 1.16.2

--- D/Dbus/build_tarballs.jl
+++ D/Dbus/build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "Dbus"
-version = v"1.14.10"
+version = v"1.16.2"
 
 # NOTE: Next stable version 1.16 will require cmake for Windows and meson for everything
 # else. Check [here](https://cgit.freedesktop.org/dbus/dbus/tree/NEWS?h=master#n21).
@@ -11,24 +11,28 @@ version = v"1.14.10"
 # Collection of sources required to build Dbus
 sources = [
     ArchiveSource("https://dbus.freedesktop.org/releases/dbus/dbus-$(version).tar.xz",
-                  "ba1f21d2bd9d339da2d4aa8780c09df32fea87998b73da24f49ab9df1e36a50f"),
+                  "0ba2a1a4b16afe7bceb2c07e9ce99a8c2c3508e5dec290dbb643384bd6beb7e2"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/dbus-*
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} \
-    --with-dbus-user=messagebus \
-    --with-system-pid-file=/var/run/dbus.pid \
-    --disable-verbose-mode \
-    --disable-static \
-    --enable-inotify \
-    --disable-asserts \
-    --enable-user-session \
-    --with-session-socket-dir=/tmp \
-    --with-x=no
-make -j${nproc}
-make install
+
+meson setup builddir \
+    --buildtype=release \
+    --cross-file=${MESON_TARGET_TOOLCHAIN} \
+    --prefix=${prefix} \
+    -Ddbus_user=messagebus \
+    -Dsystem_pid_file=/var/run/dbus.pid \
+    -Dverbose_mode=false \
+    -Dinotify=auto \
+    -Dasserts=false \
+    -Duser_session=true \
+    -Dsession_socket_dir=/tmp \
+    -Dx11_autolaunch=disabled
+
+meson compile -C builddir
+meson install -C builddir
 """
 
 # These are the platforms we will build for by default, unless further
@@ -43,7 +47,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Expat_jll", uuid="2e619515-83b5-522b-bb60-26c02a35a201"); compat="2.2.10"),
+    Dependency(PackageSpec(name="Expat_jll", uuid="2e619515-83b5-522b-bb60-26c02a35a201"); compat="2.6.5"),
+    Dependency("Glib_jll"; compat="2.84.0"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
