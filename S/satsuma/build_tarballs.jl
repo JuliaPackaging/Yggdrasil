49c73cf82b2d59ef497ba8ebeadd2b64ed4d779d
laurentbartholdi <laurent.bartholdi@gmail.com>
2025-04-05 12:52:08 +0000

    Silly typo filtered

--- S/satsuma/build_tarballs.jl
+++ S/satsuma/build_tarballs.jl
@@ -29,7 +29,7 @@ install_license LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filtered(!Sys.iswindows, supported_platforms(; experimental=true))
+platforms = filter(!Sys.iswindows, supported_platforms(; experimental=true))
 
 # The products that we will ensure are always built
 products = [
