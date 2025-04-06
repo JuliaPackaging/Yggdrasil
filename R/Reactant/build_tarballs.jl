73be8af82a3a118d2922cedd51dc718c4200d921
William Moses <me@wsmoses.com>
2025-04-06 00:46:36 -0500

    Reactant 0.0.124 (#10932)
    * Reactant 0.0.124
    
    * Update build_tarballs.jl

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -6,10 +6,10 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "Reactant"
 repo = "https://github.com/EnzymeAD/Reactant.jl.git"
-version = v"0.0.123"
+version = v"0.0.124"
 
 sources = [
-  GitSource(repo, "10264c68fa177110534f60e08e395bc9dbe6f33d"),
+  GitSource(repo, "62a98c9e0f5d79ddd23d027d4944ef7be04b63b2"),
   FileSource("https://github.com/wsmoses/binaries/releases/download/v0.0.1/bazel-dev",
              "8b43ffdf519848d89d1c0574d38339dcb326b0a1f4015fceaa43d25107c3aade")
 ]
