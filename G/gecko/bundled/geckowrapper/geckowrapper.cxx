df904c57b103d9347684104c5f83867d13db497d
termi-official <termi-official@users.noreply.github.com>
2025-04-08 20:12:25 +0200

    Working wrapper

--- G/gecko/bundled/geckowrapper/geckowrapper.cxx
+++ G/gecko/bundled/geckowrapper/geckowrapper.cxx
@@ -1,20 +1,14 @@
 #include "jlcxx/jlcxx.hpp"
+#include "jlcxx/stl.hpp"
 #include <gecko.h>
 #include <gecko/graph.h>
-
-// struct JLProgress : public Progress {
-//     virtual void beginorder(const Graph* /*graph*/, Float /*cost*/) const {}
-//     virtual void endorder(const Graph* /*graph*/, Float /*cost*/) const {}
-//     virtual void beginiter(const Graph* /*graph*/, Gecko::uint /*iter*/, Gecko::uint /*maxiter*/, Gecko::uint /*window*/) const {}
-//     virtual void enditer(const Graph* /*graph*/, Float /*mincost*/, Float /*cost*/) const {}
-//     virtual void beginphase(const Graph* /*graph*/, std::string /*name*/) const {};
-//     virtual void endphase(const Graph* /*graph*/, bool /*show*/) const {};
-//     virtual bool quit() const { return false; }
-// };
+#include <gecko/progress.h>
 
 namespace jlcxx
 {
     template<> struct IsMirroredType<Gecko::Arc> : std::false_type { };
+
+    template<> struct SuperType<Gecko::WeightedSum> { typedef Gecko::WeightedValue type; };
 
     template<> struct SuperType<Gecko::FunctionalQuasiconvex> { typedef Gecko::Functional type; };
     template<> struct SuperType<Gecko::FunctionalHarmonic> { typedef Gecko::Functional type; };
@@ -29,6 +23,7 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& gecko)
 {
     auto wv_type =  gecko.add_type<Gecko::WeightedValue>("WeightedValue");
     wv_type.constructor<Gecko::Float, Gecko::Float>();
+
     auto ws_type = gecko.add_type<Gecko::WeightedSum>("WeightedSum", jlcxx::julia_base_type<Gecko::WeightedValue>());
     ws_type.constructor<Gecko::Float, Gecko::Float>();
 
@@ -36,6 +31,9 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& gecko)
 
     auto node_type = gecko.add_type<Gecko::Node>("Node");
     node_type.constructor<Gecko::Float, Gecko::Float, Gecko::Arc::Index, Gecko::Node::Index>();
+
+    auto progress_type =  gecko.add_type<Gecko::Progress>("Progress");
+    progress_type.constructor<>();
 
     // Functionals for the optimization
     auto functional_type = gecko.add_type<Gecko::Functional>("Functional");
@@ -55,8 +53,10 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& gecko)
     functional_type.method("mean", [](const Gecko::Functional* a, Gecko::WeightedSum& s)-> Gecko::Float {return a->mean(s); } );
     functional_type.method("bond", [](const Gecko::Functional& a, Gecko::Float f, Gecko::Float l, Gecko::uint k)-> Gecko::Float {return a.bond(f, l, k); } );
     functional_type.method("bond", [](const Gecko::Functional* a, Gecko::Float f, Gecko::Float l, Gecko::uint k)-> Gecko::Float {return a->bond(f, l, k); } );
-    functional_type.method("optimum", [](const Gecko::Functional& a, const std::vector<Gecko::WeightedValue>& s)-> Gecko::Float {return a.optimum(s); } );
-    functional_type.method("optimum", [](const Gecko::Functional* a, const std::vector<Gecko::WeightedValue>& s)-> Gecko::Float {return a->optimum(s); } );
+    // FIXME We need these three lines for custom functionals. However, I could not figure out how to make the wrap work, because the WeightedValue ctor requires inputs.
+    // //jlcxx::stl::apply_stl<Gecko::WeightedValue>(gecko);
+    // functional_type.method("optimum", [](const Gecko::Functional& a, const std::vector<Gecko::WeightedValue>& s)-> Gecko::Float {return a.optimum(s); } );
+    // functional_type.method("optimum", [](const Gecko::Functional* a, const std::vector<Gecko::WeightedValue>& s)-> Gecko::Float {return a->optimum(s); } );
 
     gecko.add_type<Gecko::FunctionalQuasiconvex>("FunctionalQuasiconvex", jlcxx::julia_base_type<Gecko::Functional>());
     gecko.add_type<Gecko::FunctionalHarmonic>("FunctionalHarmonic", jlcxx::julia_base_type<Gecko::Functional>());
@@ -93,14 +93,19 @@ JLCXX_MODULE define_julia_module(jlcxx::Module& gecko)
     // graph_type.method("cost", [](const Gecko::Graph& a, const std::vector<Gecko::Arc::Index>& subset, Gecko::Float pos)-> Gecko::WeightedSum {return a.cost(subset, pos); } );
     // graph_type.method("cost", [](const Gecko::Graph* a, const std::vector<Gecko::Arc::Index>& subset, Gecko::Float pos)-> Gecko::WeightedSum {return a->cost(subset, pos); } );
     graph_type.method("directed", &Gecko::Graph::directed);
+
+    progress_type.method("beginorder", [](const Gecko::Progress& a, const Gecko::Graph* s, Gecko::Float c)-> void {return a.beginorder(s, c); } );
+    progress_type.method("beginorder", [](const Gecko::Progress* a, const Gecko::Graph* s, Gecko::Float c)-> void {return a->beginorder(s, c); } );
+    progress_type.method("endorder", [](const Gecko::Progress& a, const Gecko::Graph* s, Gecko::Float c)-> void {return a.endorder(s, c); } );
+    progress_type.method("endorder", [](const Gecko::Progress* a, const Gecko::Graph* s, Gecko::Float c)-> void {return a->endorder(s, c); } );
+    progress_type.method("beginiter", [](const Gecko::Progress& a, const Gecko::Graph* s, uint c, uint c2, uint c3)-> void {return a.beginiter(s, c, c2, c3); } );
+    progress_type.method("beginiter", [](const Gecko::Progress* a, const Gecko::Graph* s, uint c, uint c2, uint c3)-> void {return a->beginiter(s, c, c2, c3); } );
+    progress_type.method("enditer", [](const Gecko::Progress& a, const Gecko::Graph* s, Gecko::Float c, Gecko::Float c2)-> void {return a.enditer(s, c, c2); } );
+    progress_type.method("enditer", [](const Gecko::Progress* a, const Gecko::Graph* s, Gecko::Float c, Gecko::Float c2)-> void {return a->enditer(s, c, c2); } );
+    progress_type.method("beginphase", [](const Gecko::Progress& a, const Gecko::Graph* s, std::string c)-> void {return a.beginphase(s, c); } );
+    progress_type.method("beginphase", [](const Gecko::Progress* a, const Gecko::Graph* s, std::string c)-> void {return a->beginphase(s, c); } );
+    progress_type.method("endphase", [](const Gecko::Progress& a, const Gecko::Graph* s, bool c)-> void {return a.endphase(s, c); } );
+    progress_type.method("endphase", [](const Gecko::Progress* a, const Gecko::Graph* s, bool c)-> void {return a->endphase(s, c); } );
+    progress_type.method("quit", [](const Gecko::Progress& a)-> bool {return a.quit(); } );
+    progress_type.method("endorder", [](const Gecko::Progress* a)-> bool {return a->quit(); } );
 }
-
-
-// module GeckoWrapper
-//   using CxxWrap
-//   @wrapmodule(() -> joinpath("lib","libgeckowrapper"))
-
-//   function __init__()
-//     @initcxx
-//   end
-// end
