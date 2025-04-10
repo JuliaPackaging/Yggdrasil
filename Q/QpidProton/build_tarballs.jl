57e2ad74fc9357b6f358d697449f8d1eb3110297
Simone Carlo Surace <51025924+simsurace@users.noreply.github.com>
2025-04-10 15:55:29 +0200

    Expand strings for platforms
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- Q/QpidProton/build_tarballs.jl
+++ Q/QpidProton/build_tarballs.jl
@@ -34,7 +34,7 @@ make install
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # Skip Windows for now due to build complexities
 filter!(p -> !Sys.iswindows(p), platforms)
