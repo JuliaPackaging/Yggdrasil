49a7a24253cdb43ca0156c4c4d64c21fea8e21f9
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-04-12 23:47:41 +0100

    Update R/Reactant/build_tarballs.jl
    Co-authored-by: Avik Pal <avikpal@mit.edu>

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -9,7 +9,7 @@ repo = "https://github.com/EnzymeAD/Reactant.jl.git"
 version = v"0.0.139"
 
 sources = [
-  GitSource(repo, "d1cfd44c5ccd6e022be607a03e475fb743ac448a"),
+  GitSource(repo, "df1503c8f5ae1c559ec163eab4cb0621fce94032"),
   FileSource("https://github.com/wsmoses/binaries/releases/download/v0.0.1/bazel-dev",
              "8b43ffdf519848d89d1c0574d38339dcb326b0a1f4015fceaa43d25107c3aade")
 ]
