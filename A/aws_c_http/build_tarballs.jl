180deb989b6b5bc980c552ee66f708d9b9e9981a
quinnj2 <karbarcca2@gmail.com>
2025-04-15 22:06:49 -0600

    [aws_c_http] Update to 0.9.7 (#11000)
    Co-authored-by: github-actions <github-actions@github.com>

--- A/aws_c_http/build_tarballs.jl
+++ A/aws_c_http/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_http"
-version = v"0.9.6"
+version = v"0.9.7"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/awslabs/aws-c-http.git", "e526ac338ca414c01d3fc037da1c418c935808bc"),
+    GitSource("https://github.com/awslabs/aws-c-http.git", "6586c80edc09a07d3e6db6bf82c4b53aefdfe895"),
 ]
 
 # Bash recipe for building across all platforms
