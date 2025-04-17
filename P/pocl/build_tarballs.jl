1f276a9c3acc66769cbb1525467432395e23ae9e
Tim Besard <tim.besard@gmail.com>
2025-04-17 11:26:24 +0200

    Bump the macOS SDK.

--- P/pocl/build_tarballs.jl
+++ P/pocl/build_tarballs.jl
@@ -10,7 +10,10 @@ version = v"7.0"
 sources = [
     DirectorySource("./bundled"),
     GitSource("https://github.com/pocl/pocl",
-              "6accdd750d8ff66dbcc60c499b5aca5004e61c0e")
+              "6accdd750d8ff66dbcc60c499b5aca5004e61c0e"),
+    ArchiveSource(
+        "https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.14.sdk.tar.xz",
+        "0f03869f72df8705b832910517b47dd5b79eb4e160512602f593ed243b28715f")
 ]
 
 #=
@@ -34,6 +37,16 @@ builds of LLVM we need:
 script = raw"""
 cd $WORKSPACE/srcdir/pocl/
 install_license LICENSE
+
+if [[ ("${target}" == x86_64-apple-darwin*) ]]; then
+    # LLVM 15+ requires macOS SDK 10.14
+    pushd $WORKSPACE/srcdir/MacOSX10.*.sdk
+    rm -rf /opt/${target}/${target}/sys-root/System
+    cp -ra usr/* "/opt/${target}/${target}/sys-root/usr/."
+    cp -ra System "/opt/${target}/${target}/sys-root/."
+    export MACOSX_DEPLOYMENT_TARGET=10.14
+    popd
+fi
 
 if [[ "${target}" == *freebsd* ]]; then
     atomic_patch -p1 -p1 $WORKSPACE/srcdir/patches/freebsd-stdlib.patch
