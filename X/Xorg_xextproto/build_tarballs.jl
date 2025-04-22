d8c54213363c2de304c829cafc410da1e2a9d7b6
Erik Schnetter <schnetter@gmail.com>
2025-04-22 15:42:28 -0400

    Xorg_xextproto: Build for riscv64 (#11042)

--- X/Xorg_xextproto/build_tarballs.jl
+++ X/Xorg_xextproto/build_tarballs.jl
@@ -4,20 +4,20 @@ using BinaryBuilder
 
 name = "Xorg_xextproto"
 version = v"7.3.0"
+# We bumped the version number because we built for riscv64
+ygg_version = v"7.3.1"
 
 # Collection of sources required to build xextproto
 sources = [
     ArchiveSource("https://www.x.org/archive/individual/proto/xextproto-$(version).tar.bz2",
-               "f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc92aa5bb0"),
+                  "f3f4b23ac8db9c3a9e0d8edb591713f3d70ef9c3b175970dd8823dfc92aa5bb0"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/xextproto-*/
-CPPFLAGS="-I${prefix}/include"
-# When compiling for things like ppc64le, we need newer `config.sub` files
+cd $WORKSPACE/srcdir/xextproto-*
 update_configure_scripts
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --enable-malloc0returnsnull=no
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make -j${nproc}
 make install
 """
@@ -34,4 +34,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
+build_tarballs(ARGS, name, ygg_version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6")
