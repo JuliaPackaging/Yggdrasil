6ffbc8c97fd75f274c3727060449f51d5cd3c0db
Erik Schnetter <schnetter@gmail.com>
2025-04-23 16:44:46 -0400

    Xorg_inputproto: Correct comment

--- X/Xorg_inputproto/build_tarballs.jl
+++ X/Xorg_inputproto/build_tarballs.jl
@@ -4,7 +4,9 @@ using BinaryBuilder
 
 name = "Xorg_inputproto"
 version = v"2.3.2"
-# We bumped the version number because we converted the package to AnyPlatform
+# We bumped the version number because we converted the package to
+# AnyPlatform, which required rebuilding the package, which implicitly
+# changed the compat bounds
 ygg_version = v"2.3.3"
 
 # Collection of sources required to build inputproto
