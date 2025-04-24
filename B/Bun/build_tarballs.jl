0c445a796abe34c65a76916e14f34bd5421d42bf
Alberto Mengali <disberd@gmail.com>
2025-04-24 21:56:30 +0200

    fix typo

--- B/Bun/build_tarballs.jl
+++ B/Bun/build_tarballs.jl
@@ -9,7 +9,7 @@ release_url = "https://github.com/oven-sh/bun/releases/download/bun-v$version"
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("$release_url/bun-linux-x64.zip", "68a154ff1be96851b4d1a87cc5197f027ef80ab79afa3d4587150fae5c34c36e"; unpack_target = "x86_64-linux-gnu"),
-    ArchiveSource("$release_arm_url/bun-linux-aarch64.zip", "54592fd0237e3ec91a2933dff015e15a78989d97234042e7d5334a4c0ad50603"; unpack_target = "aarch64-linux-gnu"),
+    ArchiveSource("$release_url/bun-linux-aarch64.zip", "54592fd0237e3ec91a2933dff015e15a78989d97234042e7d5334a4c0ad50603"; unpack_target = "aarch64-linux-gnu"),
     ArchiveSource("$release_url/bun-darwin-x64.zip", "3443df70c763665db70267c6e883d312c922ccb54a085a40645e7d5603ba9b59"; unpack_target = "x86_64-apple-darwin"),
     ArchiveSource("$release_url/bun-darwin-aarch64.zip", "07895ef0fb661249b86b0b723b65b1cc4790c7f3685b63cda90122b00299972c"; unpack_target="aarch64-apple-darwin"),
     ArchiveSource("$release_url/bun-windows-x64.zip", "e936fcfacb3cdd823f4fd60402920f52219cfe1dd970eb5650656f67a5ee9109"; unpack_target = "x86_64-w64-mingw32"),
