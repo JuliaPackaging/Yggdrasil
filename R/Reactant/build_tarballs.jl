fce7bb94f0cd33c7f6b27d5a9c50308d44dca1e2
William Moses <me@wsmoses.com>
2025-04-25 10:49:23 -0500

    Update build_tarballs.jl

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -9,7 +9,7 @@ repo = "https://github.com/EnzymeAD/Reactant.jl.git"
 version = v"0.0.152"
 
 sources = [
-  GitSource(repo, "346ab4c1f1087fdf2d3663557e7a5c99756ed801"),
+  GitSource(repo, "ee175df091d293e3e251024bfadc77d4c14943ee"),
   FileSource("https://github.com/wsmoses/binaries/releases/download/v0.0.1/bazel-dev",
              "8b43ffdf519848d89d1c0574d38339dcb326b0a1f4015fceaa43d25107c3aade")
 ]
