7122c059232ae5613f4fd7ea58e7e544ab09bf59
Alberto Mengali <disberd@gmail.com>
2025-04-26 08:57:32 +0200

    update based on review comments

--- B/Bun/build_tarballs.jl
+++ B/Bun/build_tarballs.jl
@@ -7,29 +7,17 @@ version = v"1.2.10"
 
 release_url = "https://github.com/oven-sh/bun/releases/download/bun-v$version"
 
-# We download the SHA file to extract the SHAs of the archives, taken from https://github.com/MichaelHatherly/BundledWebResources.jl
-sha256sums = let 
-    d = Dict{String,String}()
-    sha256sums_url = "$(release_url)/SHASUMS256.txt"
-    for line in eachline(download(sha256sums_url))
-        sha, file = strip.(split(line, ' '; limit = 2))
-        d[file] = sha
-    end
-    d
-end
-
-repo_SHA1 = let
-    response = read(download("https://api.github.com/repos/oven-sh/bun/git/refs/tags/bun-v$(version)"), String)
-    match(r"\"sha\": \"([0-9a-f]{40})\"", response).captures[1]
-end
+# The code for generating the data and source arrays below can be automatically computed based on bun version from the colab notebook at this link: https://colab.research.google.com/drive/1bicSGoDOkd7u1SeUrDjJIe2yAIR83vWu?usp=sharing
 
 # This will store filename, arch and platform for each supported platform
 data = [
-    (; filename = "bun-linux-x64.zip", sha = sha256sums["bun-linux-x64.zip"], target = "x86_64-linux-gnu", platform = Platform("x86_64", "linux"; libc = "glibc")),
-    (; filename = "bun-linux-aarch64.zip", sha = sha256sums["bun-linux-aarch64.zip"], target = "aarch64-linux-gnu", platform = Platform("aarch64", "linux"; libc = "glibc")),
-    (; filename = "bun-darwin-x64.zip", sha = sha256sums["bun-darwin-x64.zip"], target = "x86_64-apple-darwin14", platform = Platform("x86_64", "macos")),
-    (; filename = "bun-darwin-aarch64.zip", sha = sha256sums["bun-darwin-aarch64.zip"], target = "aarch64-apple-darwin20", platform = Platform("aarch64", "macos")),
-    (; filename = "bun-windows-x64.zip", sha = sha256sums["bun-windows-x64.zip"], target = "x86_64-w64-mingw32", platform = Platform("x86_64", "windows")),
+    (; filename = "bun-linux-x64.zip", sha = "68a154ff1be96851b4d1a87cc5197f027ef80ab79afa3d4587150fae5c34c36e", target = "x86_64-linux-gnu", platform = Platform("x86_64", "linux"; libc = "glibc")),
+    (; filename = "bun-linux-aarch64.zip", sha = "54592fd0237e3ec91a2933dff015e15a78989d97234042e7d5334a4c0ad50603", target = "aarch64-linux-gnu", platform = Platform("aarch64", "linux"; libc = "glibc")),
+    (; filename = "bun-linux-x64-musl.zip", sha = "68a154ff1be96851b4d1a87cc5197f027ef80ab79afa3d4587150fae5c34c36e", target = "x86_64-linux-musl", platform = Platform("x86_64", "linux"; libc = "musl")),
+    (; filename = "bun-linux-aarch64-musl.zip", sha = "54592fd0237e3ec91a2933dff015e15a78989d97234042e7d5334a4c0ad50603", target = "aarch64-linux-musl", platform = Platform("aarch64", "linux"; libc = "musl")),
+    (; filename = "bun-darwin-x64.zip", sha = "3443df70c763665db70267c6e883d312c922ccb54a085a40645e7d5603ba9b59", target = "x86_64-apple-darwin14", platform = Platform("x86_64", "macos")),
+    (; filename = "bun-darwin-aarch64.zip", sha = "07895ef0fb661249b86b0b723b65b1cc4790c7f3685b63cda90122b00299972c", target = "aarch64-apple-darwin20", platform = Platform("aarch64", "macos")),
+    (; filename = "bun-windows-x64.zip", sha = "e936fcfacb3cdd823f4fd60402920f52219cfe1dd970eb5650656f67a5ee9109", target = "x86_64-w64-mingw32", platform = Platform("x86_64", "windows")),
 ]
 
 
@@ -37,7 +25,7 @@ data = [
 # We start by putting the per-arch archive with the executable
 sources = [
     [ArchiveSource("$release_url/$(d.filename)", d.sha; unpack_target = d.target) for d in data]...,
-    GitSource("https://github.com/oven-sh/bun.git", String(repo_SHA1))
+    GitSource("https://github.com/oven-sh/bun.git", "db2e7d7f748dd3951ac0c983de73e75df51bb735")
 ]
 
 # Bash recipe for building across all platforms
@@ -45,7 +33,7 @@ script = raw"""
 cd ${WORKSPACE}/srcdir/
 install_license bun/LICENSE.md
 mkdir "${bindir}"
-install -m 755 "${target}/"*"/bun${exeext}" "${bindir}"
+install -m 755 $target/*/bun$exeext $bindir
 """
 
 # These are the platforms we will build for by default, unless further
