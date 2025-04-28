97b4bd057a6efb87fd9d869443db18e352fb86c0
William S. Moses <gh@wsmoses.com>
2025-04-28 18:09:40 -0500

    bump gcc

--- B/Bazel/build_tarballs.jl
+++ B/Bazel/build_tarballs.jl
@@ -74,4 +74,4 @@ products = [
 dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"7")
