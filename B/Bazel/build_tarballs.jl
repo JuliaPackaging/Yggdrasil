dc9c4cf9e767a14e1057cf9ebab5e27eac7accb1
William Moses <me@wsmoses.com>
2025-04-28 19:50:22 -0500

    Apply suggestions from code review
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- B/Bazel/build_tarballs.jl
+++ B/Bazel/build_tarballs.jl
@@ -4,7 +4,7 @@ name = "Bazel"
 version = v"7.6.1"
 sources = [
     ArchiveSource("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_alpine-linux_hotspot_21.0.7_6.tar.gz", "79ecc4b213d21ae5c389bea13c6ed23ca4804a45b7b076983356c28105580013"),
-    ArchiveSource("https://github.com/bazelbuild/bazel/releases/download/7.6.1/bazel-7.6.1-dist.zip", "c1106db93eb8a719a6e2e1e9327f41b003b6d7f7e9d04f206057990775a7760e"),
+    ArchiveSource("https://github.com/bazelbuild/bazel/releases/download/$(version)/bazel-$(version)-dist.zip", "c1106db93eb8a719a6e2e1e9327f41b003b6d7f7e9d04f206057990775a7760e"),
     
 ]
 
@@ -68,6 +68,7 @@ install_license LICENSE
 platforms = [
     Platform("x86_64", "linux"; libc="musl"),
 ]
+platforms = expand_cxxstring_abis(platforms)
 
 products = [
     ExecutableProduct("bazel", :bazel),
