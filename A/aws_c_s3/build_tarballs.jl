1f917ab066febf98c8aefef4d4cae17feef2adc0
quinnj2 <karbarcca2@gmail.com>
2025-04-29 10:57:33 -0600

    [aws_c_s3] Update to version 0.7.16 (#11092)
    * [aws_c_s3] Update to 0.7.16
    
    * Update build_tarballs.jl
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Jacob Quinn <quinn.jacobd@gmail.com>

--- A/aws_c_s3/build_tarballs.jl
+++ A/aws_c_s3/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_s3"
-version = v"0.7.15"
+version = v"0.7.16"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/awslabs/aws-c-s3.git", "0770d353ce2d04af0f2b1ee3680a8f827f28263f"),
+    GitSource("https://github.com/awslabs/aws-c-s3.git", "5eac79fb577b05cc83e39ae3b01412c3d66ab69e"),
 ]
 
 # Bash recipe for building across all platforms
@@ -36,7 +36,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("aws_checksums_jll"; compat="0.2.3"),
+    Dependency("aws_checksums_jll"; compat="0.2.7"),
     Dependency("aws_c_http_jll"; compat="0.9.3"),
     Dependency("aws_c_auth_jll"; compat="0.8.5"),
     BuildDependency("aws_lc_jll"),
