b3d789b79c79220357da717c9a6d9b21345d184e
Benoît Legat <benoit.legat@gmail.com>
2025-04-30 10:12:08 +0200

    [Tracy] Update to v0.11.1

--- T/TracyProfiler/build_tarballs.jl
+++ T/TracyProfiler/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder, Pkg
 
 name = "TracyProfiler"
-version = v"0.10.0"
+version = v"0.11.1"
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/wolfpld/tracy.git",
-              "37aff70dfa50cf6307b3fee6074d627dc2929143"), # v0.10
+              "5d542dc09f3d9378d005092a4ad446bd405f819a"), # v0.11.1
     ArchiveSource("https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.0.sdk.tar.xz",
                   "d3feee3ef9c6016b526e1901013f264467bb927865a03422a9cb925991cc9783"),
     DirectorySource("./bundled"),
