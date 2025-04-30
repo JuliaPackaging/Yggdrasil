05ca97d83f3263a20581a7d2047a779dafa14057
Aleksei Klochkov <aklochkov@bhft.com>
2025-04-30 18:33:50 +0200

    Update build_tarballs.jl

--- T/TDLib/build_tarballs.jl
+++ T/TDLib/build_tarballs.jl
@@ -3,35 +3,50 @@
 using BinaryBuilder, Pkg
 
 name = "TDLib"
-version = v"1.8.46"
+version = v"1.8.47"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/tdlib/td.git", "207f3be7b58b2a2b9f0a066b5b6ef18782b8b517")
+    GitSource("https://github.com/tdlib/td.git", "971684a3dcc7bdf99eec024e1c4f57ae729d6d53")
 ]
+
+include("../../L/libjulia/common.jl")
+julia_versions = [v"1.8", v"1.9", v"1.10", v"1.11"]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd td/
+cd ${WORKSPACE}/srcdir/td/
+
 install_license LICENSE_1_0.txt
-sed -i 's|TD_HAS_MMSG 1|TD_HAS_MMSG 0|' tdutils/td/utils/port/config.h  # otherwise not compatible with old glibc
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DZLIB_LIBRARY=$libdir -DZLIB_INCLUDE_DIR=$includedir ..
-for f in $(grep -l $(find . -name link.txt) -e 'opt/bin\S*/c++')
-do
-    # without this flag it cannot find libz
-    sed -i ' 1 s/$/ -lz/' $f
-done
-cmake --build . -j${nproc}
-cp libtdjson.${dlext} $libdir/libtdjson.${dlext}
+
+find /usr/share/cmake -name "._*" -delete
+
+mkdir build_native && cd build_native
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_HOST_TOOLCHAIN} \
+    -DZLIB_LIBRARY="${host_libdir}/libz.a" \
+    -DZLIB_INCLUDE_DIR="${host_includedir}" \
+    ..
+cmake --build . --target prepare_cross_compiling -j4
+
+cd ${WORKSPACE}/srcdir/td/
+mkdir build-cross && cd build-cross
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DNATIVE_BUILD_DIR=${WORKSPACE}/srcdir/td/build_native \
+    ..
+cmake --build . --target tdjson -j4
+cmake --build . --target tdjson_static -j4
+cmake --install .
 """
 
 # These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = filter(p -> Sys.islinux(p) && (libc(p) == "glibc" && arch(p) == "i686" || arch(p) == "x86_64"), supported_platforms())
+platforms = supported_platforms()
 platforms = expand_cxxstring_abis(platforms)
-
 
 # The products that we will ensure are always built
 products = [
@@ -40,10 +55,13 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-	HostBuildDependency("gperf_jll")
-	Dependency("OpenSSL_jll"; compat="1.1.10")
-	Dependency("Zlib_jll")
+	HostBuildDependency("gperf_jll"),
+	HostBuildDependency("OpenSSL_jll"),
+	HostBuildDependency("Zlib_jll"),
+
+	Dependency("OpenSSL_jll"; compat="3.0.16"),
+	Dependency("Zlib_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9", julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"9", julia_compat="1.8")
