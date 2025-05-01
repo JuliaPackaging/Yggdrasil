a101cbd05d75f7911a3ecf87e5a0dedf28770fa6
Jesper Stemann Andersen <jesper@sait.dk>
2025-05-02 01:19:25 +0200

    Align with TensorRT@10

--- T/TensorRT/TensorRT@8/build_tarballs.jl
+++ T/TensorRT/TensorRT@8/build_tarballs.jl
@@ -1,24 +1,33 @@
 using BinaryBuilder, Pkg
 using Base.BinaryPlatforms: arch, os
 
-include("../../fancy_toys.jl")
+const YGGDRASIL_DIR = "../../.."
+include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
+include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "TensorRT"
 version = v"8.2.1"
 
+cuda_versions = [
+    v"10.2",
+    v"11",
+]
+
+# Cf. https://docs.nvidia.com/deeplearning/tensorrt/archives/tensorrt-825/support-matrix/index.html
+cudnn_version = v"8.2.1"
+
 script = raw"""
+mkdir -p $bindir $includedir $libdir
 cd ${WORKSPACE}/srcdir
 
 if [[ ${bb_full_target} == aarch64-linux-gnu*cuda+10.2 ]]; then
     apk add dpkg
     ls *.deb | xargs -Ideb_file dpkg-deb -x deb_file tmp
-    mkdir -p $bindir $includedir $libdir
     install -Dv --mode=755 tmp/usr/src/tensorrt/bin/* $bindir
     install -Dv --mode=644 tmp/usr/include/$target/* $includedir
     install -Dv --mode=755 tmp/usr/lib/$target/*.so* $libdir
     install_license tmp/usr/share/doc/libnvinfer8/copyright
 else
-    mkdir -p ${bindir} ${libdir} ${includedir}
     cd TensorRT*
     mv bin/* ${bindir}
     mv include/* ${includedir}
@@ -41,21 +50,37 @@ lib_names = [
 ]
 
 products = vcat(
-    [LibraryProduct(["lib" * lib_name, lib_name], Symbol("lib" * lib_name); dont_dlopen=true) for lib_name in lib_names],
+    [LibraryProduct(["lib$lib_name", lib_name], Symbol("lib$lib_name"); dont_dlopen=true) for lib_name in lib_names],
     [ExecutableProduct("trtexec", :trtexec)]
 )
 
-cuda_versions = [v"10.2", v"11.0", v"11.1", v"11.2", v"11.3", v"11.4", v"11.5"]
+builds = []
 for cuda_version in cuda_versions
-    cuda_tag = "$(cuda_version.major).$(cuda_version.minor)"
-    include("build_$(cuda_tag).jl")
-    cudnn_build_version = cuda_version < v"11.4" ? v"8.2.1" : cuda_version < v"11.5" ? v"8.2.2" : v"8.3.1"
-    dependencies = [Dependency("CUDNN_jll", cudnn_build_version; compat="8.2")]
+    include("build_cuda+$(cuda_version.major).jl")
+    
     for (platform, sources) in platforms_and_sources
-        augmented_platform = Platform(arch(platform), os(platform); cuda=cuda_tag)
+        augmented_platform = deepcopy(platform)
+        augmented_platform["cuda"] = CUDA.platform(cuda_version)
         should_build_platform(triplet(augmented_platform)) || continue
-        arch(platform) != "aarch64" || cuda_version == v"10.2" || cuda_version == v"11.4" || continue # AArch64 only support CUDA v10.2 and v11.4
-        build_tarballs(ARGS, name, version, sources, script, [augmented_platform],
-                       products, dependencies; lazy_artifacts=true)
+
+        dependencies = [Dependency("CUDNN_jll", cudnn_version; compat="8.2")]
+
+        push!(builds, (; dependencies, platforms=[augmented_platform], sources))
     end
+end
+
+augment_platform_block = CUDA.augment
+
+# don't allow `build_tarballs` to override platform selection based on ARGS.
+# we handle that ourselves by calling `should_build_platform`
+non_platform_ARGS = filter(arg -> startswith(arg, "--"), ARGS)
+
+# `--register` should only be passed to the latest `build_tarballs` invocation
+non_reg_ARGS = filter(arg -> arg != "--register", non_platform_ARGS)
+
+for (i,build) in enumerate(builds)
+    build_tarballs(i == lastindex(builds) ? non_platform_ARGS : non_reg_ARGS,
+                   name, version, build.sources, script,
+                   build.platforms, products, build.dependencies;
+                   julia_compat="1.6", augment_platform_block, dont_dlopen=true)
 end
