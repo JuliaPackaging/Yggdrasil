4e93fd1adb220794fd2a7bed10fa05208a4147a9
Tim Besard <tim.besard@gmail.com>
2025-05-09 12:47:54 +0200

    CUDA: Bump cuTensor to 2.2.0. (#11148)

--- C/CUDA/CUTENSOR/build_tarballs.jl
+++ C/CUDA/CUTENSOR/build_tarballs.jl
@@ -8,7 +8,7 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "CUTENSOR"
-version = v"2.1.0"
+version = v"2.2.0"
 
 
 scripts = Dict()
