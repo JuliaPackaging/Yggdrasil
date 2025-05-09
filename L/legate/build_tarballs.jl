c7e81481bdfa215dba121d579c06d8180bf58bf6
ejmeitz <emeitz@andrew.cmu.edu>
2025-05-09 10:30:29 -0400

    try some other stuff

--- L/legate/build_tarballs.jl
+++ L/legate/build_tarballs.jl
@@ -11,62 +11,69 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 name = "legate"
 version = v"25.03"
 sources = [
-    GitSource("https://github.com/nv-legate/legate.git", "40d29633519eb449e4a928bdc29cf0bd029707f8"),
+    GitSource("https://github.com/nv-legate/legate.git","98e78b221f02bf3d04dbe22518e1bf3033bf1f8b")
+    # GitSource("https://github.com/nv-legate/legate.git", "1478fa09e7e3cb6b177bef0aae74cd04c3a43417"),
 ]
 
 MIN_CUDA_VERSION = v"12.0" #CUDA.full_version(v"12.0")
-
-# REMOVE LATER
-TEST_CUDA_VERSION = v"12.2"
-
-# --build=${MACHTYPE} \
-# --host=${target} \
-
+TEST_CUDA_VERSION = v"12.8" # REMOVE LATER
 
 # --build-march="${MARCH_FLAG}" \
-# --with-hdf5 \
-# --with-hdf5-dir=${prefix} \
 
+# HDF5 needs libcurl, and it needs to be the BinaryBuilder libcurl, not the system libcurl.
+#     rm /usr/lib/libcurl.*
 
+# --build-type="debug" \
 
 
 script = raw"""
 
 if [[ ${target} == x86_64-linux-musl ]]; then
-    # HDF5 needs libcurl, and it needs to be the BinaryBuilder libcurl, not the system libcurl.
     # MPI needs libevent, and it needs to be the BinaryBuilder libevent, not the system libevent.
-    rm /usr/lib/libcurl.*
     rm /usr/lib/libevent*
     rm /usr/lib/libnghttp2.*
 fi
 
+# Put new CMake first on path
 export PATH=${host_bindir}:$PATH
+
+### Install Python Dependencies in a Really Stupid Way
 
 cd ${WORKSPACE}/srcdir
 mkdir config_deps && cd config_deps
 pip3 download --no-deps rich
 pip3 download --no-deps typing_extensions
+pip3 download --no-deps packaging
 RICH_WHEEL=$(ls rich-*-py3-none-any.whl)
 TYPE_EXT_WHEEL=$(ls typing_extensions-*-py3-none-any.whl)
+PACKAGING_WHEEL=$(ls packaging-*-py3-none-any.whl)
 unzip -q "$RICH_WHEEL" -d config_deps
 unzip -q "$TYPE_EXT_WHEEL" -d config_deps
+unzip -q "$PACKAGING_WHEEL" -d config_deps
 
-export PY_DEPS_DIR=$(pwd)/config_deps
+pip3 download --no-deps git+https://github.com/nv-legate/aedifix@1.2.0
+unzip aedifix-*
 
-PYTHONPATH="${PY_DEPS_DIR}" python3.10 -c "import rich, typing_extensions; print('Python deps installed and working!')"
+export PY_DEPS_DIR=$(pwd)/config_deps:$(pwd)/config_deps/aedifix/src/aedifix
+
+PYTHONPATH="${PY_DEPS_DIR}" python3.10 -c "import rich, typing_extensions, packaging, aedifix; print('Python deps installed and working!')"
 
 cd ${WORKSPACE}/srcdir/legate
+
+### Set Up CUDA ENV Vars
 
 export CPPFLAGS="${CPPFLAGS} -I${prefix}/include"
 export CFLAGS="${CFLAGS} -I${prefix}/include"
 export LDFLAGS="${LDFLAGS} -L${prefix}/lib -L${prefix}/lib64"
 
-export CUDA_HOME=${WORKSPACE}/destdir/cuda;
+export CUDA_HOME=${prefix}/cuda;
 export PATH=$PATH:$CUDA_HOME/bin
 export CUDACXX=$CUDA_HOME/bin/nvcc
 
 # nvcc thinks the libraries are located inside lib64, but the SDK actually has them in lib
 ln -s ${CUDA_HOME}/lib ${CUDA_HOME}/lib64
+
+### Parse march flag
 
 MARCH_FLAG=""
 
@@ -82,21 +89,37 @@ case "${target%%-*}" in
     exit 1
 esac
 
-PYTHONPATH="${PY_DEPS_DIR}" python3.10 configure \
+export PYTHONPATH="${PY_DEPS_DIR}"
+
+sed -i "1s|#\!/usr/bin/env python3|#\!${host_bindir}/python3.10|" configure
+
+
+#--with-nccl-dir=${prefix} \
+# --with-cxx=${CXX} \
+# --with-cc=${CC} \
+# --with-cxx=clang++ \
+# --with-cc=clang \
+
+./configure \
     --prefix=${prefix} \
     --with-cudac=${CUDACXX} \
     --with-cuda-dir=${CUDA_HOME} \
-    --with-nccl-dir=${prefix} \
+    --with-nccl=0 \
     --with-mpiexec-executable=${bindir}/mpiexec \
     --with-mpi-dir=${prefix} \
     --with-zlib-dir=${prefix} \
-    --num-threads=${nproc} \
+    --with-hdf5=0 \
+    --with-hdf5-vfd-gds=0 \
+    --num-threads=1 \
     --with-cxx=${CXX} \
     --with-cc=${CC} \
     --CXXFLAGS="${CPPFLAGS}" \
     --CFLAGS="${CFLAGS}" \
+    --with-clean \
+    --cmake-executable=${host_bindir}/cmake \
+    -- "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}"
 
-make install PREFIX=${prefix}
+make install -j ${nproc} PREFIX=${prefix}
 """
 
 platforms = CUDA.supported_platforms()
@@ -107,24 +130,30 @@ platforms = filter!(p -> arch(p) == "x86_64", platforms) #* could also support a
 #* REMOVE LATER
 platforms = filter!(p -> VersionNumber(tags(p)["cuda"]) == TEST_CUDA_VERSION, platforms)
 
+print(platforms)
 
 #* TODO expand c-abi
-#* TODO expand MPI?
+#* TODO expand MPI abi
 
 products = [
     LibraryProduct("liblegate", :liblegate),
+    LibraryProduct("liblegion-legate", :liblegionlegate),
+    LibraryProduct("librealm-legate", :librealmlegate),
+    LibraryProduct("libcpptrace", :libcpptrace),
 ]
 
 # CUDA SDK added later, see cuda_deps
 dependencies = [
     Dependency("MPICH_jll"),
     Dependency("HDF5_jll"),
-    Dependency("NCCL_jll"),
+    # Dependency("NCCL_jll"),
     Dependency("UCX_jll"),
     Dependency("Zlib_jll"),
     Dependency("OpenSSL_jll"),
-    HostBuildDependency(PackageSpec(; name = "Python_jll", version = v"3.10.16")),
-    HostBuildDependency(PackageSpec(; name = "CMake_jll", version = v"3.30.2"))
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+    BuildDependency(PackageSpec(; name = "Clang_jll", version = v"v20.1.2")),
+    HostBuildDependency(PackageSpec(; name = "CMake_jll", version = v"3.30.2")),
+    HostBuildDependency(PackageSpec(; name = "Python_jll", version = v"3.10.16"))
 ]
 
 for platform in platforms
@@ -137,7 +166,7 @@ for platform in platforms
 
     build_tarballs(ARGS, name, version, sources, script, [platform],
                     products, [dependencies; cuda_deps];
-                    julia_compat = "1.10", preferred_gcc_version = v"12",
+                    julia_compat = "1.10", preferred_gcc_version = v"13",
                     augment_platform_block=CUDA.augment
                 )
 
