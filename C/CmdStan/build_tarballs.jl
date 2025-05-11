97bc228a415051befcac61608fc861ffad972360
Viral B. Shah <virals@gmail.com>
2025-05-10 23:56:46 -0400

    Update build_tarballs.jl

--- C/CmdStan/build_tarballs.jl
+++ C/CmdStan/build_tarballs.jl
@@ -3,19 +3,20 @@
 using BinaryBuilder, Pkg
 
 name = "CmdStan"
-version = v"2.26.1"
+version = v"2.36.0"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/stan-dev/cmdstan/releases/download/v$(version)/cmdstan-$(version).tar.gz", "d944d23ac7ed5ebf924d859f3a1f3052891161e2c70313503f21257ea13f0b0c"),
-    DirectorySource("./bundled")
+    GitSource("https://github.com/stan-dev/cmdstan.git", "61c4a75ca469617a2ec36b5329d0e518b67d6f36"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-
-cd $WORKSPACE/srcdir/cmdstan-*
+cd $WORKSPACE/srcdir/cmdstan*
 cp ../local make/
+git submodule update --init --recursive
+
 if [[ "${target}" == i686* ]]; then
     echo "BITS=32" >> make/local
 fi
@@ -31,12 +32,15 @@ fi
 
 make -j${nproc} build
 cp -R bin/ $prefix/bin/ 
-install_license ${WORKSPACE}/srcdir/cmdstan-*/LICENSE
+install_license ${WORKSPACE}/srcdir/cmdstan*/LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms())
+build_platforms = Platform[
+    Platform("x86_64", "Linux"),
+]
+platforms = expand_cxxstring_abis(build_platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -50,4 +54,5 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"5.2.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               preferred_gcc_version = v"9", julia_compat="1.6")
