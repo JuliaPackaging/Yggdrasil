1a754be076c59547dee632ff160e4f244c0e0b26
odow <o.dowson@gmail.com>
2025-05-16 16:34:19 +1200

    [SCIP|SCIP_PaPILO] update to v9.2.2

--- S/SCIP/build_tarballs.jl
+++ S/SCIP/build_tarballs.jl
@@ -3,15 +3,14 @@
 using BinaryBuilder, Pkg
 
 name = "SCIP"
-upstream_version = v"9.2.1"
-# TODO(odow): remove +1 when updating upstream
-version = VersionNumber(upstream_version.major * 100, upstream_version.minor * 100, upstream_version.patch * 100 + 1)
+upstream_version = v"9.2.2"
+version = VersionNumber(upstream_version.major * 100, upstream_version.minor * 100, upstream_version.patch * 100)
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource(
         "https://scipopt.org/download/release/scipoptsuite-$(upstream_version).tgz",
-        "41b71a57af773403e9a6724f78c37d8396ac4b6b270a9bbf3716d67f1af12edf"
+        "1a6d5b2bceb99faf1facbd6cd79e4a3eb8de60ed1d480281f12ae5c540d4a8a4"
     ),
 ]
 
