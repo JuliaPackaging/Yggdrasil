2ad6e0377b22a7bd7ae45d43181c8ea24bf4f673
ejmeitz <54505069+ejmeitz@users.noreply.github.com>
2025-05-17 15:52:23 -0400

    add make install

--- N/NCCL/build_tarballs.jl
+++ N/NCCL/build_tarballs.jl
@@ -9,35 +9,12 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 name = "NCCL"
 version = v"2.26.5"
 
-MIN_CUDA_VERSION = v"12.0"
+MIN_CUDA_VERSION = v"12.0" # should be able to do most of 11.x too
 
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/NVIDIA/nccl.git", "3000e3c797b4b236221188c07aa09c1f3a0170d4"),
 ]
-
-# Bash recipe for building across all platforms
-# old_script = raw"""
-# cd $WORKSPACE/srcdir
-
-# export TMPDIR=${WORKSPACE}/tmpdir # we need a lot of tmp space
-# export CUDA_HOME=${WORKSPACE}/destdir/cuda
-# export CUDA_LIB=${CUDA_HOME}/lib
-# export CXXFLAGS='-D__STDC_FORMAT_MACROS'
-# export CUDARTLIB=cudart # link against dynamic library
-
-# mkdir -p ${TMPDIR}
-
-# cd nccl
-
-# make -j pkg.txz.build
-# tar -xJf build/pkg/txz/*.txz -C ${WORKSPACE}/destdir --strip-components=1
-# rm ${WORKSPACE}/destdir/LICENSE.txt
-# rm ${WORKSPACE}/destdir/lib/libnccl_static.a  # remove static library: saves 230 MB
-
-# install_license ${WORKSPACE}/srcdir/nccl/LICENSE.txt
-# """
-
 
 script = raw"""
 cd $WORKSPACE/srcdir
@@ -58,7 +35,9 @@ ln -s ${CUDA_HOME}/lib ${CUDA_HOME}/lib64
 cd nccl
 make -j ${nproc} src.build CUDA_HOME=${CUDA_HOME} PREFIX=${prefix}
 
-rm ${WORKSPACE}/destdir/lib/libnccl_static.a  # remove static library: saves 230 MB
+make install PREFIX=${prefix}
+
+rm -f ${WORKSPACE}/srcdir/nccl/build/lib/libnccl_static.a
 
 install_license ${WORKSPACE}/srcdir/nccl/LICENSE.txt
 """
