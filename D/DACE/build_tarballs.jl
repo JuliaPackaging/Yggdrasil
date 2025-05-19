a1c3894b95975e606b797e0d1027893cbf74ebf6
Alberto Fossà <alberto.fossa18@gmail.com>
2025-05-19 09:30:13 +0200

    [DACE] bump dependency

--- D/DACE/build_tarballs.jl
+++ D/DACE/build_tarballs.jl
@@ -8,7 +8,7 @@ uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "DACE"
-version = v"0.7.0"
+version = v"0.7.1"
 
 # Collection of sources required to build DACE
 sources = [
@@ -51,7 +51,7 @@ products = [
 dependencies = [
     BuildDependency(PackageSpec(name="libjulia_jll")),
     BuildDependency("Eigen_jll"),
-    Dependency("libcxxwrap_julia_jll"; compat="~0.14.1"),
+    Dependency("libcxxwrap_julia_jll"; compat="~0.14.2"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
