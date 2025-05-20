1ba5cd5644ea8a004d52b0a3511065f2196bbc40
Matteo Seclì <secli.matteo@gmail.com>
2025-05-20 13:54:45 +0200

    MAGMA: try to fix aarch64 build

--- M/MAGMA/build_tarballs.jl
+++ M/MAGMA/build_tarballs.jl
@@ -9,6 +9,10 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 name = "MAGMA"
 version = v"2.9.0"
 
+# Note: Hopper should still build with CUDA v11.8
+# on x86_64, but aarch64 requires CUDA v12.0
+MIN_CUDA_VERSION = v"12"
+
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("http://icl.utk.edu/projectsfiles/magma/downloads/magma-$(version).tar.gz",
@@ -18,25 +22,54 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/magma*
+cd $WORKSPACE/srcdir
 
-export CUDADIR=${WORKSPACE}/destdir/cuda
-export PATH=${PATH}:${CUDADIR}
+export TMPDIR=${WORKSPACE}/tmpdir # we need a lot of tmp space
+mkdir -p ${TMPDIR}
+
+# Necessary operations to cross compile CUDA from x86_64 to aarch64
+if [[ "${target}" == aarch64-linux-* ]]; then
+
+   # Add /usr/lib/csl-musl-x86_64 to LD_LIBRARY_PATH to be able to use host nvcc
+   export LD_LIBRARY_PATH="/usr/lib/csl-musl-x86_64:/usr/lib/csl-glibc-x86_64:${LD_LIBRARY_PATH}"
+   
+   # Make sure we use host CUDA executable by copying from the x86_64 CUDA redist
+   NVCC_DIR=(/workspace/srcdir/cuda_nvcc-*-archive)
+   rm -rf ${prefix}/cuda/bin
+   cp -r ${NVCC_DIR}/bin ${prefix}/cuda/bin
+   
+   rm -rf ${prefix}/cuda/nvvm/bin
+   cp -r ${NVCC_DIR}/nvvm/bin ${prefix}/cuda/nvvm/bin
+
+   export NVCC_PREPEND_FLAGS="-ccbin='${CXX}'"
+fi
+
+export CUDADIR=${prefix}/cuda
+export PATH=${PATH}:${CUDADIR}/bin
+export CUDACXX=${CUDADIR}/bin/nvcc
+
+cd magma*
 cp ../make.inc .
+
 # Patch to _64_ suffixes
 atomic_patch -p1 ../0001-mangle-to-ILP64.patch
+
 # reduce parallelism since otherwise the builder may OOM.
 (( nproc=1+nproc/3 ))
+
 make -j${nproc} sparse-shared
 make install prefix=${prefix}
+
 install_license COPYRIGHT
+
+if [[ "${target}" == aarch64-linux-* ]]; then
+   # ensure products directory is clean
+   rm -rf ${CUDADIR}
+fi
 """
 
-augment_platform_block = CUDA.augment
-
-# Note: Hopper should still build with CUDA v11.8
-# on x86_64, but aarch64 requires CUDA v12.0
-platforms = CUDA.supported_platforms(min_version=v"12")
+platforms = CUDA.supported_platforms(min_version = MIN_CUDA_VERSION)
+filter!(p -> arch(p) == "x86_64" || arch(p) == "aarch64", platforms)
 platforms = expand_cxxstring_abis(platforms)
 
 
@@ -57,10 +90,18 @@ for platform in platforms
 
     cuda_deps = CUDA.required_dependencies(platform)
 
-    build_tarballs(ARGS, name, version, sources, script, [platform],
+    cuda_ver = platform["cuda"]
+
+    platform_sources = BinaryBuilder.AbstractSource[sources...]
+
+    if arch(platform) == "aarch64"
+        push!(platform_sources, CUDA.cuda_nvcc_redist_source(cuda_ver, "x86_64"))
+    end
+
+    build_tarballs(ARGS, name, version, platform_sources, script, [platform],
                    products, [dependencies; cuda_deps];
                    preferred_gcc_version=v"8",
                    julia_compat="1.8",
-                   augment_platform_block,
+                   augment_platform_block=CUDA.augment,
                    skip_audit=true, dont_dlopen=true)
 end
