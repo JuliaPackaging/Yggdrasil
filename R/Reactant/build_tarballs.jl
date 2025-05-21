e54a7223598b38f000687dc0623242e5a89ed0d0
William Moses <me@wsmoses.com>
2025-05-20 19:04:32 -0500

    Update build_tarballs.jl

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -9,7 +9,7 @@ repo = "https://github.com/EnzymeAD/Reactant.jl.git"
 version = v"0.0.187"
 
 sources = [
-   GitSource(repo, "beb145711535108543f9b0954dac428c2d2a13c6"),
+   GitSource(repo, "9bcc67680ead60d1bc205bf34d5e897bdbb0fcf4"),
    ArchiveSource("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_alpine-linux_hotspot_21.0.7_6.tar.gz", "79ecc4b213d21ae5c389bea13c6ed23ca4804a45b7b076983356c28105580013"),
    ArchiveSource("https://github.com/JuliaBinaryWrappers/Bazel_jll.jl/releases/download/Bazel-v7.6.1+0/Bazel.v7.6.1.x86_64-linux-musl-cxx03.tar.gz", "01ac6c083551796f1f070b0dc9c46248e6c49e01e21040b0c158f6e613733345")
 ]
