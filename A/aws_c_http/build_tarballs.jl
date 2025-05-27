047c84d855b3eee6fae150e5ef3e05118209f159
Jacob Quinn <quinn.jacobd@gmail.com>
2025-05-26 22:57:18 -0600

    Update aws_c_http deps and bump version

--- A/aws_c_http/build_tarballs.jl
+++ A/aws_c_http/build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_http"
-version = v"0.10.1"
+version = v"0.10.2"
 
 # Collection of sources required to complete build
 sources = [
@@ -38,7 +38,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("aws_c_compression_jll"; compat="0.3.1"),
-    Dependency("aws_c_io_jll"; compat="0.18.0"),
+    Dependency("aws_c_io_jll"; compat="0.19.1"),
     BuildDependency("aws_lc_jll"),
 ]
 
