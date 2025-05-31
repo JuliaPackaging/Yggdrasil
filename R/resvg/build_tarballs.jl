b0f51fb9bf8884b0be56b86777c065f3cdcdc2d7
Jerry Ling <proton@jling.dev>
2025-05-31 11:19:41 -0400

    add resvg

---
+++ R/resvg/build_tarballs.jl
@@ -0,0 +1,36 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "resvg"
+version = v"0.45.1"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/linebender/resvg.git", "1b6c2fddbcbeffa8135df4323b02aaae84890907")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/resvg*
+cargo build --release
+mkdir -p "${bindir}"
+cp "target/${rust_target}/release/resvg${exeext}" "${bindir}/."
+install_license LICENSE-MIT LICENSE-APACHE
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("resvg", :resvg)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", compilers = [:rust, :c])
