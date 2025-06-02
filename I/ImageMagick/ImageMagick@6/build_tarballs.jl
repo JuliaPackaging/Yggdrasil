5ca452b37b79572c4811ba228be5edf99d8eba7b
Albert Gnadt <20693045+gnadt@users.noreply.github.com>
2025-06-01 23:34:06 -0400

    Revert "Try adding LERC_jll & LittleCMS_jll as dependencies"
    This reverts commit 52f6326d9291614840181ee85e6cce6084b51da4.

--- I/ImageMagick/ImageMagick@6/build_tarballs.jl
+++ I/ImageMagick/ImageMagick@6/build_tarballs.jl
@@ -60,9 +60,7 @@ products = [
 dependencies = [
     Dependency("Ghostscript_jll"),
     Dependency("JpegTurbo_jll"),
-    Dependency("LERC_jll"),
     Dependency("Libtiff_jll"; compat="4.7.1"),
-    Dependency("LittleCMS_jll"),
     Dependency("OpenJpeg_jll"),
     Dependency("Zlib_jll"),
     Dependency("libpng_jll"),
