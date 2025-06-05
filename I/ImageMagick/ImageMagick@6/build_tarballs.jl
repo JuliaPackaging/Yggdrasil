e1a2d63c5cc62d67d7692b3935f85ec744173f9a
Albert Gnadt <20693045+gnadt@users.noreply.github.com>
2025-06-05 12:53:34 -0400

    Revert Libtiff_jll to ~4.5.1

--- I/ImageMagick/ImageMagick@6/build_tarballs.jl
+++ I/ImageMagick/ImageMagick@6/build_tarballs.jl
@@ -64,7 +64,7 @@ products = [
 dependencies = [
     Dependency("Ghostscript_jll"),
     Dependency("JpegTurbo_jll"),
-    Dependency("Libtiff_jll"; compat="4.7.1"),
+    Dependency("Libtiff_jll"; compat="~4.5.1"),
     Dependency("OpenJpeg_jll"),
     Dependency("Zlib_jll"; compat="1.2.12"),
     Dependency("libpng_jll"),
