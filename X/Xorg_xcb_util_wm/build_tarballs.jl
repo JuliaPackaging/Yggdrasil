7bf9202fe25707614eafe4fd1f3ae6fba2a1529b
Jānis Erdmanis <akels14@gmail.com>
2025-06-09 17:48:47 +0300

    Update build_tarballs.jl

--- X/Xorg_xcb_util_wm/build_tarballs.jl
+++ X/Xorg_xcb_util_wm/build_tarballs.jl
@@ -7,7 +7,7 @@ version = v"0.4.2"
 
 # Collection of sources required to build libxcb
 sources = [
-    ArchiveSource("https://xcb.freedesktop.org/dist/xcb-util-wm-$(version).tar.bz2",
+    ArchiveSource("https://xcb.freedesktop.org/dist/xcb-util-wm-$(version).tar.xz",
                   "28bf8179640eaa89276d2b0f1ce4285103d136be6c98262b6151aaee1d3c2a3f"),
 ]
 
