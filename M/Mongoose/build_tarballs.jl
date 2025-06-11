ec4a710c66790bc85999a30015a9b99974782faa
Abraham Jaimes <60442066+AbrJA@users.noreply.github.com>
2025-06-11 10:29:18 -0600

    Apply suggestions from code review
    Co-authored-by: Ian McInerney <mcianster@gmail.com>
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- M/Mongoose/build_tarballs.jl
+++ M/Mongoose/build_tarballs.jl
@@ -33,28 +33,12 @@ cmake -B build  \
     -S .
 cmake --build build --parallel ${nproc}
 cmake --install build
+install_license mongoose/LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("riscv64", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("x86_64", "macos"; ),
-    Platform("aarch64", "macos"; ),
-    Platform("x86_64", "freebsd"; ),
-    Platform("aarch64", "freebsd"; )
-]
+platforms = supported_platforms(; exclude=Sys.iswindows)
 
 
 # The products that we will ensure are always built
