ac19b27cd2411ce79c1d666971c8875d688df10b
Erik Schnetter <schnetter@gmail.com>
2025-06-13 15:13:08 -0400

    libwebm: New package

--- L/libwebm/build_tarballs.jl
+++ L/libwebm/build_tarballs.jl
@@ -2,10 +2,10 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 
-name = "libwebp"
+name = "libwebm"
 version = v"1.0.31"
 
-# Collection of sources required to build libwebp
+# Collection of sources required to build libwebm
 sources = [
     GitSource("https://github.com/webmproject/libwebm", "6745fd29e0245fc584b0bb9f65018ea2366fe7fb"),
 ]
@@ -21,19 +21,20 @@ cmake -B build \
     -DENABLE_SAMPLE_PROGRAMS=OFF
 cmake --build build --parallel ${njobs}
 cmake --install build
+# Executables are not installed by default
+for exe in webm2pes webm2ts webm_info; do
+    install -Dvm 755 build/${exe}${exeext} ${bindir}/${exe}${exeext}
+done
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
+platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libmkvmuxer", :libmkvmuxer),
-    LibraryProduct("libmkvparser", :libmkvparser),
     LibraryProduct("libwebm", :libwebm),
-    LibraryProduct("libwebmts", :libwebmts),
-    LibraryProduct("libwebvtt_common", :libwebvtt_common),
     ExecutableProduct("webm2pes", :webm2pes),
     ExecutableProduct("webm2ts", :webm2ts),
     ExecutableProduct("webm_info", :webm_info),
