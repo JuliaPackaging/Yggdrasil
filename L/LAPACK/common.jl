b0278f7c65b8af5fac4cd06aebce2e06a01a0b00
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-06-13 22:34:09 -0500

    Remove the platform riscv64 for LAPACK 3.12.1

--- L/LAPACK/common.jl
+++ L/LAPACK/common.jl
@@ -382,6 +382,9 @@ end
 # platforms are passed in on the command line
 platforms = expand_gfortran_versions(supported_platforms())
 
+# Require LBT ≥ 5.12.0
+filter!(p -> arch(p) != "riscv64", platforms)
+
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
