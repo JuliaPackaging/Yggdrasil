2e4e09ff72f83164b435950a4c15e63681e73770
nhz2 <nhz2@cornell.edu>
2025-06-19 16:12:58 -0400

    Update to 0.25.0 and add CompilerSupportLibraries_jll dep

--- G/GettextRuntime/build_tarballs.jl
+++ G/GettextRuntime/build_tarballs.jl
@@ -2,11 +2,11 @@ using BinaryBuilder
 
 # Collection of sources required to build GettextRuntime
 name = "GettextRuntime"
-version = v"0.24.0"
+version = v"0.25.0"
 
 sources = [
     ArchiveSource("https://ftp.gnu.org/pub/gnu/gettext/gettext-$(version.major).$(version.minor).tar.xz",
-                  "e1620d518b26d7d3b16ac570e5018206e8b0d725fb65c02d048397718b5cf318"),
+                  "05240b29f5b0f422e5a4ef8e9b5f76d8fa059cc057693d2723cdb76f36a88ab0"),
 ]
 
 # Bash recipe for building across all platforms
@@ -46,6 +46,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    Dependency("CompilerSupportLibraries_jll"),
     Dependency("Libiconv_jll"),
 ]
 
