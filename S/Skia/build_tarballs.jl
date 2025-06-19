9d94835550d494af8e685fdfd3c147757ac1cdfd
stensmo <141365386+stensmo@users.noreply.github.com>
2025-06-19 12:13:57 +0200

    Update S/Skia/build_tarballs.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- S/Skia/build_tarballs.jl
+++ S/Skia/build_tarballs.jl
@@ -71,7 +71,27 @@ elif [[ "${target}" == i686-* ]]; then
 fi
 
 
-bin/gn gen out/Static --args='target_cpu="'$target_cpu'" cc="clang" cxx="clang++" is_official_build=true skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_zlib=false skia_use_system_freetype2=false skia_use_fontconfig=true skia_enable_pdf=true skia_use_system_icu=false skia_use_system_expat=false skia_use_harfbuzz=false skia_use_vulkan=true skia_use_gl=true extra_cflags=["-fpic", "-fvisibility=default"]'
+ARGS="
+target_cpu=$target_cpu
+cc=clang
+cxx=clang++
+is_official_build=true
+skia_enable_pdf=true
+skia_use_fontconfig=true
+skia_use_gl=true
+skia_use_harfbuzz=false
+skia_use_system_expat=false
+skia_use_system_freetype2=false
+skia_use_system_icu=false
+skia_use_system_libjpeg_turbo=false
+skia_use_system_libpng=false
+skia_use_system_libwebp=false
+skia_use_system_zlib=false
+skia_use_vulkan=true
+extra_cflags=[\"-fpic\", \"-fvisibility=default\"]
+"
+
+bin/gn gen out/Static --args="$ARGS"
 
 ninja -j${nproc} -C out/Static
 
