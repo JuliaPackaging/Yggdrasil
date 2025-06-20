b0b290c998f790ef176edb0940cfd30c9ba2cd82
Eric <26843285+Agapanthus@users.noreply.github.com>
2025-06-20 16:11:54 +0200

    Update build_tarballs.jl
    the platforms must target specific julia versions, otherwise cxxwrap cannot always link the runtime

--- L/libczi_julia/build_tarballs.jl
+++ L/libczi_julia/build_tarballs.jl
@@ -7,16 +7,15 @@ version = v"0.1.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/Agapanthus/libczi_julia.git", 
-        "95bde92f18428e89a0e1542ff8257fd71710dc6a")
+	GitSource("https://github.com/Agapanthus/libczi_julia.git",
+		"1cd9791c8d4bab776a6420d104663123daba37da"),
 ]
 
 # Bash recipe for building across all platforms
 # disable NEON for now, as it causes issues on aarch64 platforms
 script = raw"""
-cd $WORKSPACE/srcdir
-cmake -S $WORKSPACE/srcdir/libczi_julia/src \
-	-B build \
+cd $WORKSPACE/srcdir/libczi_julia/src
+cmake . -B build \
 	-DJulia_PREFIX="$prefix" \
 	-DCMAKE_INSTALL_PREFIX="$prefix" \
 	-DCMAKE_FIND_ROOT_PATH="$prefix" \
@@ -24,9 +23,9 @@ cmake -S $WORKSPACE/srcdir/libczi_julia/src \
 	-D_UNALIGNED_ACCESS_RESULT=1 \
 	-DCMAKE_CXX_STANDARD=17 \
 	-DCMAKE_BUILD_TYPE=Release \
-    -DENABLE_NEON=OFF \
-    -D_NEON_INTRINSICS_RESULT_EXITCODE=0 \
-    -D_NEON_INTRINSICS_RESULT_EXITCODE__TRYRUN_OUTPUT=""
+	-DENABLE_NEON=OFF \
+	-D_NEON_INTRINSICS_RESULT_EXITCODE=0 \
+	-D_NEON_INTRINSICS_RESULT_EXITCODE__TRYRUN_OUTPUT=""
 cmake --build build --parallel ${nproc}
 cmake --install build
 install_license $WORKSPACE/srcdir/libczi_julia/COPYING
@@ -35,18 +34,23 @@ exit
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms(; exclude=(p -> (arch(p) == "i686" && libc(p) == "musl") || !(os(p) in ["linux", "windows"])))
+# we have to use the same Julia versions as in libjulia_jll - otherwise cxxwrap cannot link against libjulia
+include("../../L/libjulia/common.jl")
+platforms = vcat(libjulia_platforms.(julia_versions)...)
+filter!(p -> os(p) in ["linux", "windows"], platforms)
+filter!(p -> !(arch(p) == "i686" && libc(p) == "musl"), platforms)
+
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct("libczi_julia", :libczi_julia)
+	LibraryProduct("libczi_julia", :libczi_julia),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="libcxxwrap_julia_jll", uuid="3eaa8342-bff7-56a5-9981-c04077f7cee7"))
-    Dependency(PackageSpec(name="libjulia_jll", uuid="5ad3ddd2-0711-543a-b040-befd59781bbf"))
+	BuildDependency(PackageSpec(name = "libjulia_jll", version = v"1.10.16")),
+	Dependency("libcxxwrap_julia_jll"; compat = "~0.14.3"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"12.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat = "1.6", preferred_gcc_version = v"12.1.0")
