6fdd09a4da20c238d677662f1e52eb9adc0172ed
Avik Pal <avikpal@mit.edu>
2025-06-20 07:56:57 -0400

    Reactant_jll v0.0.209

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -6,10 +6,10 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "Reactant"
 repo = "https://github.com/EnzymeAD/Reactant.jl.git"
-version = v"0.0.208"
+version = v"0.0.209"
 
 sources = [
-   GitSource(repo, "436d6f32875cea1ffc6c0737c98121cb6b363ad0"),
+   GitSource(repo, "4eaf23f1cbb70853416a0091243b5cdf2884eb31"),
    ArchiveSource("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_alpine-linux_hotspot_21.0.7_6.tar.gz", "79ecc4b213d21ae5c389bea13c6ed23ca4804a45b7b076983356c28105580013"),
    ArchiveSource("https://github.com/JuliaBinaryWrappers/Bazel_jll.jl/releases/download/Bazel-v7.6.1+0/Bazel.v7.6.1.x86_64-linux-musl-cxx03.tar.gz", "01ac6c083551796f1f070b0dc9c46248e6c49e01e21040b0c158f6e613733345")
 ]
