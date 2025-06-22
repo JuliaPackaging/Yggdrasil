311a0627da520ec33e79e856199c1c8746239c14
brucala <brucala@gmail.com>
2025-06-22 18:26:23 +0200

    move to prebuilt binaries

--- V/VLConvert/build_tarballs.jl
+++ V/VLConvert/build_tarballs.jl
@@ -1,22 +1,36 @@
 using BinaryBuilder
 
 name = "VLConvert"
-version = v"1.1.0"
+version = v"1.8.0"
 
+url_prefix = "https://github.com/vega/vl-convert/releases/download/v$(version)/vl-convert"
 sources = [
-    GitSource("https://github.com/vega/vl-convert.git", "ec4940b42b54bda3d9828323cf0de0e3c71bdf39"),
+    ArchiveSource("$(url_prefix)-linux-aarch64.zip", "b5ac831f1898e2fbc07ce190baeb043b94f08db5fb981d345a018711933be5f5"; unpack_target = "linux-aarch64"),
+    ArchiveSource("$(url_prefix)-linux-x86.zip", "55155e33a7ea6a04fd36ec4b52403e752184edaa38d7208dbeaa78ff887550d9"; unpack_target = "linux-x86"),
+    ArchiveSource("$(url_prefix)-osx-64.zip", "e895eabce5d437c42161048da6b7168375751ce3ec3613f8e6f6a5a482675380"; unpack_target = "osx-64"),
+    ArchiveSource("$(url_prefix)-osx-arm64.zip", "a818cffe345958602c7ed999f6cbe17e33a071060b2edc678f2943e4b3ab75c3"; unpack_target = "osx-arm64"),
+    ArchiveSource("$(url_prefix)-win-64.zip", "1beba91b80e2236ddf954a73cd0bb84522965a7144995be5c7fe3ca7aacd21b9"; unpack_target = "win-64"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd ${WORKSPACE}/srcdir/vl-convert*/
-V8_FROM_SOURCE=1 cargo build --release
-install -D -m 755 "target/${rust_target}/release/vl-convert${exeext}" "${bindir}/vl-convert${exeext}"
+cd ${WORKSPACE}/srcdir/
+cp -r ${target}/* ${prefix}/.
+chmod +x ${bindir}/vl-convert
+LIC_DIR="${prefix}/share/licenses/${SRC_NAME}"
+mkdir -p "${LIC_DIR}"
+mv "${bindir}/LICENSE" "${LIC_DIR}/LICENSE.txt"
 """
 
-platforms = supported_platforms()
-# Our Rust toolchain for i686 Windows is unusable
-filter!(p -> !Sys.iswindows(p) || arch(p) != "i686", platforms)
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Platform("aarch64", "linux"),
+    Platform("x86_64", "linux"),
+    Platform("x86_64", "macos"),
+    Platform("aarch64", "macos"),
+    Platform("x86_64", "windows"),
+]
 
 # The products that we will ensure are always built
 products = [
@@ -27,8 +41,5 @@ products = [
 dependencies = Dependency[
 ]
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(
-    ARGS, name, version, sources, script, platforms, products, dependencies;
-    compilers=[:c, :rust], julia_compat="1.6", lock_microarchitecture=false,
-)
+# Build
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
