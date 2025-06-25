8b017f59ae209c0d3e3421d1aecbe83d22d87039
Erik Schnetter <schnetter@gmail.com>
2025-06-25 09:51:10 -0400

    GTK4: Update to 4.19.1

--- G/GTK4/build_tarballs.jl
+++ G/GTK4/build_tarballs.jl
@@ -3,31 +3,35 @@
 using BinaryBuilder
 
 name = "GTK4"
-version = v"4.14.5"
+version = v"4.19.1"
 
 # Collection of sources required to build GTK
 sources = [
     # https://download.gnome.org/sources/gtk/
     ArchiveSource("https://download.gnome.org/sources/gtk/$(version.major).$(version.minor)/gtk-$(version).tar.xz",
-                  "5547f2b9f006b133993e070b87c17804e051efda3913feaca1108fa2be41e24d"),
-    ArchiveSource("https://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/mingw-w64-v10.0.0.tar.bz2",
-                  "ba6b430aed72c63a3768531f6a3ffc2b0fde2c57a3b251450dcf489a894f0894"),
+                  "7e793899bc2a47fdf52b149b9262f5398a2dd1252cfe58536e3dbfb78848d4eb"),
+    FileSource("https://sourceforge.net/projects/mingw-w64/files/mingw-w64/mingw-w64-release/mingw-w64-v10.0.0.tar.bz2",
+               "ba6b430aed72c63a3768531f6a3ffc2b0fde2c57a3b251450dcf489a894f0894"),
+    DirectorySource("bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/gtk*/
+cd $WORKSPACE/srcdir/gtk*
 
 # We need to run some commands with a native Glib
 apk update
-apk add glib-dev
+apk add glib-dev py3-pip
+
+# we need a newer meson (>= 1.5.0)
+pip3 install -U meson
+
+atomic_patch -p1 ../patches/memfd.patch
 
 # This is awful, I know
 ln -sf /usr/bin/glib-compile-resources ${bindir}/glib-compile-resources
 ln -sf /usr/bin/glib-compile-schemas ${bindir}/glib-compile-schemas
 ln -sf /usr/bin/gdk-pixbuf-pixdata ${bindir}/gdk-pixbuf-pixdata
-# Remove gio-2.0 pkgconfig file so that it isn't picked up by post-install script.
-rm ${prefix}/lib/pkgconfig/gio-2.0.pc
 
 FLAGS=()
 if [[ "${target}" == *-apple-* ]]; then
@@ -35,11 +39,13 @@ if [[ "${target}" == *-apple-* ]]; then
 elif [[ "${target}" == *-freebsd* ]]; then
     FLAGS+=(-Dwayland-backend=false)
 elif [[ "${target}" == *-mingw* ]]; then
-    cd $WORKSPACE/srcdir/mingw*/mingw-w64-headers
+    cd $WORKSPACE/srcdir
+    tar xjf mingw-w64-v10.0.0.tar.bz2
+    cd mingw*/mingw-w64-headers
     ./configure --prefix=/opt/$target/$target/sys-root --enable-sdk=all --host=$target
     make install
 
-    cd ../mingw-w64-crt/
+    cd ../mingw-w64-crt
     if [ ${target} == "i686-w64-mingw32" ]; then
         _crt_configure_args="--disable-lib64 --enable-lib32"
     elif [ ${target} == "x86_64-w64-mingw32" ]; then
@@ -50,25 +56,23 @@ elif [[ "${target}" == *-mingw* ]]; then
     make install
 fi
 
-cd $WORKSPACE/srcdir/gtk*/
+cd $WORKSPACE/srcdir/gtk*
 
-mkdir build-gtk && cd build-gtk
-meson .. \
+PKG_CONFIG_SYSROOT_DIR='' meson setup builddir \
     --buildtype=release \
     -Dmedia-gstreamer=disabled \
     -Dintrospection=disabled \
-    -Ddemos=false \
+    -Dvulkan=disabled \
+    -Dbuild-demos=false \
     -Dbuild-examples=false \
     -Dbuild-tests=false \
     -Dbuild-testsuite=false \
-    -Dgtk_doc=false \
-    -Dvulkan=disabled \
     "${FLAGS[@]}" \
     --cross-file="${MESON_TARGET_TOOLCHAIN}"
-ninja -j${nproc}
-ninja install
+meson compile -C builddir
+meson install -C builddir
 
-install_license ../COPYING
+install_license COPYING
 
 # post-install script is disabled when cross-compiling
 glib-compile-schemas ${prefix}/share/glib-2.0/schemas
@@ -79,7 +83,7 @@ rm ${bindir}/gdk-pixbuf-pixdata ${bindir}/glib-compile-{resources,schemas}
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = filter!(p -> arch(p) != "armv6l", supported_platforms())
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
@@ -98,20 +102,20 @@ dependencies = [
     BuildDependency("Xorg_xorgproto_jll"; platforms=x11_platforms),
     # Build needs a header from but does not link to libdrm
     BuildDependency("libdrm_jll"; platforms=x11_platforms),
-    Dependency("Glib_jll"; compat="2.76.5"),
-    Dependency("Graphene_jll"; compat="1.10.6"),
-    Dependency("Cairo_jll"),
-    Dependency("Pango_jll"; compat="1.50.3"),
+    Dependency("Glib_jll"; compat="2.84.3"),
+    Dependency("Graphene_jll"; compat="1.10.8"),
+    Dependency("Cairo_jll"; compat="1.18.5"),
+    Dependency("Pango_jll"; compat="1.56.3"),
     Dependency("FriBidi_jll"),
-    Dependency("FreeType2_jll"; compat="2.10.4"),
+    Dependency("FreeType2_jll"; compat="2.13.4"),
     Dependency("gdk_pixbuf_jll"),
-    Dependency("Libepoxy_jll"),
-    Dependency("Libtiff_jll"; compat="4.5.1"),
+    Dependency("Libepoxy_jll"; compat="1.5.11"),
+    Dependency("Libtiff_jll"; compat="4.7.1"),
     Dependency("HarfBuzz_jll"),
     Dependency("xkbcommon_jll"; platforms=x11_platforms),
     Dependency("iso_codes_jll"),
     Dependency("Wayland_jll"; platforms=x11_platforms),
-    Dependency("Wayland_protocols_jll"; compat="1.31", platforms=x11_platforms),
+    Dependency("Wayland_protocols_jll"; compat="1.44", platforms=x11_platforms),
     Dependency("Xorg_libXrandr_jll"; platforms=x11_platforms),
     Dependency("Xorg_libX11_jll"; platforms=x11_platforms),
     Dependency("Xorg_libXrender_jll"; platforms=x11_platforms),
@@ -125,4 +129,6 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"5", clang_use_lld=false)
+# We need at least GCC 8 to support `__VA_OPT__`
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               clang_use_lld=false, julia_compat="1.6", preferred_gcc_version=v"8")
