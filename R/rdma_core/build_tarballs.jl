5ed03c1114c4e8e8a6cbb06e6952c490245478ae
david-macmahon <davidm@astro.berkeley.edu>
2025-06-25 16:00:19 -0700

    [rdma_core_jll] Update to v58.0 (#11532)
    * [rdma_core_jll] Fix #11531
    
    * [rdma_core_jll] Update to version 58.0

--- R/rdma_core/build_tarballs.jl
+++ R/rdma_core/build_tarballs.jl
@@ -3,17 +3,22 @@
 using BinaryBuilder, Pkg
 
 name = "rdma_core"
-version = v"57.0.0"
+version = v"58.0.0"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/linux-rdma/rdma-core/releases/download/v$(version.major).$(version.minor)/rdma-core-$(version.major).$(version.minor).tar.gz", "5f94c463c931e4a9273f366ca7cb446b54d8bd4732288ade04679886be06862d")
+    ArchiveSource("https://github.com/linux-rdma/rdma-core/releases/download/v$(version.major).$(version.minor)/rdma-core-$(version.major).$(version.minor).tar.gz", "88d67897b793f42d2004eec2629ab8464e425e058f22afabd29faac0a2f54ce4"),
+	DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd rdma-core-*
+# Apply patches
+for f in ${WORKSPACE}/srcdir/patches/*.patch; do
+	atomic_patch -p1 ${f}
+done
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DNO_MAN_PAGES=ON
 make -j
 make install
@@ -43,5 +48,11 @@ dependencies = [
     Dependency(PackageSpec(name="libnl_jll", uuid="7c700954-19d3-5208-81e2-8fa5fe7c0bd8"))
 ]
 
+init_block = raw"""
+if !haskey(ENV, "JULIA_IBV_CONFIG_DIR")
+        ENV["JULIA_IBV_CONFIG_DIR"] = joinpath(artifact_dir, "etc", "libibverbs.d")
+    end
+"""
+
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"5.2.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"5.2.0", init_block)
