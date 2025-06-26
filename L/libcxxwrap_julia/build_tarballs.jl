66ff1ef5315d77038bc39b8772355fbce1c9065c
Max Horn <max@quendi.de>
2025-06-26 21:06:06 +0200

    [libcxxwrap-julia] rebuild against latest libjulia_jll

--- L/libcxxwrap_julia/build_tarballs.jl
+++ L/libcxxwrap_julia/build_tarballs.jl
@@ -53,7 +53,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency("libjulia_jll"),
+    BuildDependency(PackageSpec(;name="libjulia_jll", version=v"1.10.18")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
