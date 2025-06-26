2e7ae424087297d1ab99202f3cb317a15daab5d1
Erik Schnetter <schnetter@gmail.com>
2025-06-26 10:59:24 -0400

    xkbcommon: New version 1.9.2

--- X/xkbcommon/build_tarballs.jl
+++ X/xkbcommon/build_tarballs.jl
@@ -3,20 +3,21 @@
 using BinaryBuilder
 
 name = "xkbcommon"
-version = v"1.8.1"
+version = v"1.9.2"
 
 # Collection of sources required to build xkbcommon
 sources = [
-    GitSource("https://github.com/xkbcommon/libxkbcommon", "b3465081878e80ca6c11fe35c81787ec374ec15a"),
+    GitSource("https://github.com/xkbcommon/libxkbcommon", "dd642359f8d43c09968e34ca7f1eb1121b2dfd70"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/libxkbcommon
-mv $bindir/wayland-scanner $bindir/wayland-scanner_
-ln -s `which wayland-scanner` $bindir
-mkdir build && cd build
-cp $prefix/libdata/pkgconfig/* $prefix/lib/pkgconfig || true
+
+#TODO mv $bindir/wayland-scanner $bindir/wayland-scanner_
+#TODO ln -s `which wayland-scanner` $bindir
+#TODO mkdir build && cd build
+#TODO cp $prefix/libdata/pkgconfig/* $prefix/lib/pkgconfig || true
 
 # Need to disable PKG_CONFIG_SYSROOT_DIR because this behaviour shows up in meson:
 #
@@ -33,19 +34,19 @@ cp $prefix/libdata/pkgconfig/* $prefix/lib/pkgconfig || true
 # - https://github.com/JuliaPackaging/Yggdrasil/pull/3193#discussion_r654943148
 # - https://github.com/pkgconf/pkgconf/issues/213
 
-PKG_CONFIG_SYSROOT_DIR='' meson .. --cross-file="${MESON_TARGET_TOOLCHAIN}" \
-    -Denable-docs=false
-ninja -j${nproc}
-ninja install
-rm $bindir/wayland-scanner
-mv $bindir/wayland-scanner_ $bindir/wayland-scanner
-rm -f $prefix/lib/pkgconfig/epoll-shim*.pc
+#TODO PKG_CONFIG_SYSROOT_DIR='' 
+meson setup builddir --buildtype=release --cross-file="${MESON_TARGET_TOOLCHAIN}" -Denable-tools=false -Denable-bash-completion=false 
+meson compile -C builddir
+meson install -C builddir
+
+#TODO rm $bindir/wayland-scanner
+#TODO mv $bindir/wayland-scanner_ $bindir/wayland-scanner
+#TODO rm -f $prefix/lib/pkgconfig/epoll-shim*.pc
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-# TODO: Enable armv6l once Wayland has been built for it
-platforms = filter(p -> arch(p) != "armv6l" && (Sys.islinux(p) || Sys.isfreebsd(p)), supported_platforms())
+platforms = filter(p -> Sys.islinux(p) || Sys.isfreebsd(p), supported_platforms())
 
 # The products that we will ensure are always built
 products = [
@@ -55,17 +56,17 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency("Xorg_xorgproto_jll"),
-    Dependency("Xorg_xkeyboard_config_jll"),
-    Dependency("Xorg_libxcb_jll"),
-    Dependency("Wayland_jll"),
-    Dependency("Wayland_protocols_jll"),
     HostBuildDependency("EpollShim_jll"),
     HostBuildDependency("Wayland_jll"),
     HostBuildDependency("Wayland_protocols_jll"),
+    BuildDependency("Xorg_xorgproto_jll"),
+    Dependency("Wayland_jll"),
+    Dependency("Wayland_protocols_jll"),
+    Dependency("Xorg_libxcb_jll"),
+    Dependency("Xorg_xkeyboard_config_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
+# We need GCC 8 because this links to XML2, which requires GCC 8
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               # We need GCC 8 because this links to XML2, which requires GCC 8
-               preferred_gcc_version=v"8", julia_compat="1.6")
+               julia_compat="1.6", preferred_gcc_version=v"8")
