7db22a383a25dcc7018cf2418e5ee9376dadc28d
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-06-27 19:25:48 +0200

    [libsingular_julia] Rebuild with recent libjulia_jll

--- L/libsingular_julia/build_tarballs.jl
+++ L/libsingular_julia/build_tarballs.jl
@@ -55,7 +55,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency(PackageSpec(;name="libjulia_jll", version=v"1.10.16")),
+    BuildDependency(PackageSpec(;name="libjulia_jll", version=v"1.10.18")),
     BuildDependency("GMP_jll"),
     BuildDependency("MPFR_jll"),
     Dependency("libcxxwrap_julia_jll"; compat = "~0.14.3"),
