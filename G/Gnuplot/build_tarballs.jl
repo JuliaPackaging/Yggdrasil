4ad41364e21f3d62dc2372e0a6441dcf05f62dab
t-bltg <tf.bltg@gmail.com>
2025-06-30 20:52:32 +0200

    [Gnuplot_jll] fix `Gnuplot` 5 build (#11571)
    * dummy
    
    * try to fix ming build
    
    * fix aarch64-apple-darwin
    
    * fix
    
    * cleanup
    
    * remove comment
    
    * move to gnuplot 5.4.6
    
    * use a different version numbering scheme
    
    * use different version scheme
    
    * revert version bump
    
    * revert sha256sum
    
    * spacing
    
    * damn phone editor
    
    * clarify version

--- G/Gnuplot/build_tarballs.jl
+++ G/Gnuplot/build_tarballs.jl
@@ -1,8 +1,9 @@
-
 using BinaryBuilder, Pkg
 
 name = "Gnuplot"
 version = v"5.4.5"
+build_number_jll = 0  # NOTE: increment on rebuild of the same version, reset on new gnuplot version
+version_jll = VersionNumber(version.major, version.minor, 1_000 * version.patch + build_number_jll)
 
 # Collection of sources required to complete build
 sources = [
@@ -13,23 +14,26 @@ sources = [
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/gnuplot-*/
-
+cd $WORKSPACE/srcdir/gnuplot-*
 
 if [[ "${target}" == "${MACHTYPE}" ]]; then
     # Delete system libexpat to avoid confusion
     rm /usr/lib/libexpat.so*
 elif [[ "${target}" == *-mingw* ]]; then
-    # This is needed because otherwise we get unusable binaries (error "The specified executable is not a valid application for this OS platform"). These come from CompilerSupportLibraries_jll:
-    rm $prefix/lib/libgcc* $prefix/lib/libmsvcrt*
     # Apply patch from https://github.com/msys2/MINGW-packages/blob/5dcff9fd637714972b113c6d3fbf6db17e9b707a/mingw-w64-gnuplot/01-gnuplot.patch
     atomic_patch -p1 ../patches/01-gnuplot.patch
     autoreconf -fiv
 fi
 
-export CPPFLAGS="$(pkg-config --cflags glib-2.0) $(pkg-config --cflags cairo) $(pkg-config --cflags pango) -I$(realpath term)"
-export LDFLAGS="-liconv"
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+# FIXME: no Qt Tools artifacts available for these platforms (missing either uic or lrelease)
+case "$target" in
+    aarch64-apple-darwin*) args+=(--with-qt=no);;
+esac
+
+export LIBS='-liconv'
+
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} ${args[@]}
+
 cd src
 make -j${nproc}
 make install
@@ -40,21 +44,21 @@ make install
 platforms = supported_platforms()
 
 # The products that we will ensure are always built
-products = [
-    ExecutableProduct("gnuplot", :gnuplot),
-    #ExecutableProduct("gnuplot_qt", :gnuplot_qt, "$libexecdir")
-]
+products = [ExecutableProduct("gnuplot", :gnuplot)]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
     BuildDependency(PackageSpec(name="Xorg_xorgproto_jll", uuid = "c4d99508-4286-5418-9131-c86396af500b")),
     Dependency(PackageSpec(name="Libcerf_jll", uuid="af83a40a-c4c4-57a0-81df-2309fbd279e3")),
     Dependency(PackageSpec(name="LibGD_jll", uuid="16339573-6216-525a-b38f-30b6f6b71b5f")),
-    BuildDependency(PackageSpec(name="Qt5Tools_jll", uuid="a9c6e4b1-b2fb-56d5-96a9-25f276f13840")),
-    Dependency(PackageSpec(name="Qt5Svg_jll", uuid="3af4ccab-a251-578e-a514-ea85a0ba79ee")),
     Dependency(PackageSpec(name="Cairo_jll", uuid="83423d85-b0ee-5818-9007-b63ccbeb887a")),
     Dependency(PackageSpec(name="Libiconv_jll", uuid="94ce4f54-9a6c-5748-9c1c-f9c7231a4531")),
+    BuildDependency(PackageSpec(name="Qt5Tools_jll", uuid="a9c6e4b1-b2fb-56d5-96a9-25f276f13840")),
+    Dependency(PackageSpec(name="Qt5Svg_jll", uuid="3af4ccab-a251-578e-a514-ea85a0ba79ee")),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"8")
+build_tarballs(
+    ARGS, name, version_jll, sources, script, platforms, products, dependencies;
+    julia_compat="1.6", preferred_gcc_version = v"8"
+)
