2d7f3f93fdb3dfe4609954447486f357f9c15cf6
t-bltg <tf.bltg@gmail.com>
2025-07-01 09:56:43 +0200

    [Gnuplot_jll] update `Gnuplot` to `6.0.3` (#11570)
    * update gnuplot to 6.0.3
    
    * comment help
    
    * debug
    
    * add Readline_jll
    
    * add Xorg_libX11_jll
    
    * update
    
    * update
    
    * update
    
    * fix musl and freebsd
    
    * up
    
    * restrict to src
    
    * try qt6
    
    * fix
    
    * fix cond
    
    * restore qt5
    
    * try
    
    * up
    
    * simplify script
    
    * restore CPPFLAGS
    
    * update CXXFLAGS
    
    * update
    
    * update
    
    * qtcore fix
    
    * fix
    
    * up
    
    * update
    
    * up
    
    * up
    
    * update
    
    * install
    
    * exclude platforms
    
    * disable armv6l-linux-gnueabihf
    
    * remove debugging code
    
    * debug
    
    * fix for arm-linux-gnueabihf
    
    * cleanup
    
    * try adding Pango_jll
    
    * add Libffi_jll
    
    * remove Pango_jll
    
    * fix builds
    
    * add comment
    
    * cleanup
    
    * final cleanup
    
    * more cleanup
    
    * remove comment
    
    * use a different version number scheme

--- G/Gnuplot/build_tarballs.jl
+++ G/Gnuplot/build_tarballs.jl
@@ -1,14 +1,14 @@
 using BinaryBuilder, Pkg
 
 name = "Gnuplot"
-version = v"5.4.5"
+version = v"6.0.3"
 build_number_jll = 0  # NOTE: increment on rebuild of the same version, reset on new gnuplot version
 version_jll = VersionNumber(version.major, version.minor, 1_000 * version.patch + build_number_jll)
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://downloads.sourceforge.net/project/gnuplot/gnuplot/$(version)/gnuplot-$(version).tar.gz",
-                  "66f679115dd30559e110498fc94d926949d4d370b4999a042e724b8e910ee478"),
+                  "ec52e3af8c4083d4538152b3f13db47f6d29929a3f6ecec5365c834e77f251ab"),
     DirectorySource("./bundled"),
 ]
 
@@ -25,18 +25,24 @@ elif [[ "${target}" == *-mingw* ]]; then
     autoreconf -fiv
 fi
 
+export LIBS='-liconv -lffi'
+if [[ ${target} == aarch64-apple-* ]]; then  # FIXES the undefined symbol: __divdc3 error
+    export LDFLAGS="-L${libdir}/darwin -lclang_rt.osx"
+fi
+
+unset args
+args+=(--disable-wxwidgets)
+
 # FIXME: no Qt Tools artifacts available for these platforms (missing either uic or lrelease)
 case "$target" in
-    aarch64-apple-darwin*) args+=(--with-qt=no);;
+    *-musl*|*-freebsd*|riscv64-linux-gnu*|aarch64-apple-darwin*|arm-linux-gnueabihf*)
+        args+=(--with-qt=no);;
 esac
-
-export LIBS='-liconv'
 
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} ${args[@]}
 
-cd src
-make -j${nproc}
-make install
+make -C src -j${nproc}
+make -C src install
 """
 
 # These are the platforms we will build for by default, unless further
@@ -48,13 +54,22 @@ products = [ExecutableProduct("gnuplot", :gnuplot)]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency(PackageSpec(name="Xorg_xorgproto_jll", uuid = "c4d99508-4286-5418-9131-c86396af500b")),
-    Dependency(PackageSpec(name="Libcerf_jll", uuid="af83a40a-c4c4-57a0-81df-2309fbd279e3")),
-    Dependency(PackageSpec(name="LibGD_jll", uuid="16339573-6216-525a-b38f-30b6f6b71b5f")),
-    Dependency(PackageSpec(name="Cairo_jll", uuid="83423d85-b0ee-5818-9007-b63ccbeb887a")),
-    Dependency(PackageSpec(name="Libiconv_jll", uuid="94ce4f54-9a6c-5748-9c1c-f9c7231a4531")),
-    BuildDependency(PackageSpec(name="Qt5Tools_jll", uuid="a9c6e4b1-b2fb-56d5-96a9-25f276f13840")),
-    Dependency(PackageSpec(name="Qt5Svg_jll", uuid="3af4ccab-a251-578e-a514-ea85a0ba79ee")),
+    # libclang_rt.osx.a is required on aarch64-macos to provide `__divdc3`.
+    BuildDependency("LLVMCompilerRT_jll"; platforms = filter(p -> Sys.isapple(p) && arch(p) == "aarch64", platforms)),
+    BuildDependency("Xorg_xorgproto_jll"),
+    Dependency("libwebp_jll"),
+    Dependency("Libcerf_jll"),
+    Dependency("LibGD_jll"),
+    Dependency("Cairo_jll"),
+    Dependency("Pango_jll"),
+    Dependency("Libffi_jll"),
+    Dependency("Libiconv_jll"),
+    Dependency("Readline_jll"),
+    BuildDependency("Qt5Tools_jll"),
+    Dependency("Qt5Svg_jll"),
+    # FIXME: build with Qt6 fails, must probably add a `Qt6Tools_jll` recipe
+    # Dependency("Qt6Base_jll"),
+    # Dependency("Qt6Svg_jll"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
