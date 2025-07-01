32513a512d2350ad7d1154240e61aaecfd8f0d6e
Feroz Ahmed Mian <feroz.ahmed.mian@gmail.com>
2025-07-01 20:41:44 +0500

    🛠️ add dist4-mri

---
+++ D/dist-m4ri/build_tarballs.jl
@@ -0,0 +1,26 @@
+using BinaryBuilder, Pkg
+
+name = "dist-m4ri"
+version = v"0.0.1"
+
+sources = [
+    GitSource("https://github.com/QEC-pages/dist-m4ri.git",
+              "0c540cb766f5538d9a89152dc875ae5a357ee6c6")
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir/dist-m4ri/
+make -j${nproc} CC=$CC CXX=$CXX
+mkdir -p ${bindir}
+cp dist-m4ri ${bindir}/
+"""
+
+platforms = filter(p -> Sys.islinux(p) || Sys.isapple(p), supported_platforms())
+
+products = [
+    ExecutableProduct("dist-m4ri", :dist_m4ri_exe)
+]
+
+dependencies = []
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
