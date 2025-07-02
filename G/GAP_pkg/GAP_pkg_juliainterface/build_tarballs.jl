f8eeea548090e18c8a0b6a3c1d145541e7f70147
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-07-02 11:28:09 +0200

    [GAP_pkg_juliainterface] Rebuild for GAP.jl 0.14.x release (#11582)

--- G/GAP_pkg/GAP_pkg_juliainterface/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_juliainterface/build_tarballs.jl
@@ -11,13 +11,13 @@ delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 gap_version = v"400.1400.005"
 name = "JuliaInterface"
-upstream_version = "0.13.1" # when you increment this, reset offset to v"0.0.0"
-offset = v"0.0.4" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
+upstream_version = "0.14.1" # when you increment this, reset offset to v"0.0.0"
+offset = v"0.0.0" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
 version = offset_version(upstream_version, offset)
 
 # Collection of sources required to build this JLL
 sources = [
-    GitSource("https://github.com/oscar-system/GAP.jl", "c33287b416ca30fcc386a6b192d67c1955fdae8f"),
+    GitSource("https://github.com/oscar-system/GAP.jl", "93b12dd37e581320232e892c18db9048f8c9ad83"),
 ]
 
 # Bash recipe for building across all platforms
