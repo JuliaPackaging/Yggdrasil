22c981bc056a36c2cb7acf8529c4e25ead366da3
t-bltg <tf.bltg@gmail.com>
2025-07-04 00:54:23 +0200

    add suffix

--- G/Gnuplot/build_tarballs.jl
+++ G/Gnuplot/build_tarballs.jl
@@ -45,10 +45,16 @@ esac
 
 make -C src -j${nproc}
 make -C src install
+
+if [[ "${target}" == *-mingw* ]]; then
+    suffix='.exe"
+else
+    suffix=''
+fi
 """ * """
 mkdir -p \$prefix/$libexec_path
-touch \$prefix/$libexec_path/gnuplot_fake
-chmod +x \$prefix/$libexec_path/gnuplot_fake
+touch \$prefix/$libexec_path/gnuplot_fake\$suffix
+chmod +x \$prefix/$libexec_path/gnuplot_fake\$suffix
 """
 
 # These are the platforms we will build for by default, unless further
