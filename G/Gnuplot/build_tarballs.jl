6f676efd4881d7e9163b1008591fdbbadac94bbc
t-bltg <tf.bltg@gmail.com>
2025-07-05 09:45:13 +0200

    revert gnuplot changes

--- G/Gnuplot/build_tarballs.jl
+++ G/Gnuplot/build_tarballs.jl
@@ -2,8 +2,8 @@ using BinaryBuilder, Pkg
 
 name = "Gnuplot"
 version = v"6.0.3"
-ygg_build = 1  # NOTE: increment on rebuild of the same version, reset on new gnuplot version
-ygg_version = VersionNumber(version.major, version.minor, version.patch * 1_000 + ygg_build)
+build_number_jll = 1  # NOTE: increment on rebuild of the same version, reset on new gnuplot version
+version_jll = VersionNumber(version.major, version.minor, 1_000 * version.patch + build_number_jll)
 
 # Collection of sources required to complete build
 sources = [
@@ -86,6 +86,6 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(
-    ARGS, name, ygg_version, sources, script, platforms, products, dependencies;
+    ARGS, name, version_jll, sources, script, platforms, products, dependencies;
     julia_compat="1.6", preferred_gcc_version = v"8"
 )
