83d4797e161c4a504cbb345c4ae8a491b5fc3ac5
quinnj2 <karbarcca2@gmail.com>
2025-07-15 02:55:55 -0600

    [aws_c_event_stream] Update to version 0.5.5 (#11558)
    * [aws_c_event_stream] Update to 0.5.5 with latest dependency versions
    
    * Update A/aws_c_event_stream/build_tarballs.jl
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- A/aws_c_event_stream/build_tarballs.jl
+++ A/aws_c_event_stream/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "aws_c_event_stream"
-version = v"0.5.4"
+version = v"0.5.5"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/awslabs/aws-c-event-stream.git", "9312b052583183b98526aaeb91e5c72ec3db9627"),
+    GitSource("https://github.com/awslabs/aws-c-event-stream.git", "8f8f599e78864188fe8547dafaa695a1d4855d6a"),
 ]
 
 # Bash recipe for building across all platforms
@@ -36,9 +36,9 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("aws_c_common_jll"; compat="0.11.1"),
-    Dependency("aws_c_io_jll"; compat="0.17.0"),
-    Dependency("aws_checksums_jll"; compat="0.2.3"),
+    Dependency("aws_c_common_jll"; compat="0.12.3"),
+    Dependency("aws_c_io_jll"; compat="0.21.0"),
+    Dependency("aws_checksums_jll"; compat="0.2.8"),
     BuildDependency("aws_lc_jll"),
 ]
 
