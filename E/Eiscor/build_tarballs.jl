e0891d2a92df8dabddc2aa4d557a737f9d6eda0a
Andreas Noack <andreas@noack.dk>
2025-07-19 20:44:43 +0000

    New Recipe: Eiscor v0.2.0

---
+++ E/Eiscor/build_tarballs.jl
@@ -0,0 +1,38 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "Eiscor"
+version = v"0.2.0"
+
+# Collection of sources required to complete build
+sources = [
+    ArchiveSource("https://github.com/eiscor/eiscor/archive/refs/tags/v$(version).tar.gz", "b82e725d8062db1eb5de70fc270acace48ca740d3f76715444d773fd6dbcc1f9")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd eiscor-0.2.0/
+export HOME=$libdir
+make "-j$nproc"
+make install
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libeiscor", :libeiscor, "lib/eiscor/lib")
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
