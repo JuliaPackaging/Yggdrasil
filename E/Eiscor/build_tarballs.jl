6b72799800b3883b524e1a027e73267e17ef431a
Andreas Noack <andreas@noack.dk>
2025-07-21 10:45:50 +0200

    macOS is a BSD

--- E/Eiscor/build_tarballs.jl
+++ E/Eiscor/build_tarballs.jl
@@ -24,7 +24,7 @@ platforms = supported_platforms()
 platforms = expand_gfortran_versions(platforms)
 
 # Filter out platforms that we don't care about
-filter!(!Sys.isbsd, platforms)
+filter!(t -> !Sys.isbsd(t) || Sys.isapple(t), platforms)
 
 # The products that we will ensure are always built
 products = [
