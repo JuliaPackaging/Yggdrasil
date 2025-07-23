ab0d427a5b685142613b6cf76aa9418311f35e34
Michael Schlottke-Lakemper <michael@sloede.com>
2025-07-23 09:24:39 +0200

    Undo version restriction

--- O/openfhe_julia/common.jl
+++ O/openfhe_julia/common.jl
@@ -70,8 +70,8 @@ function prepare_openfhe_julia_build(name::String, git_hash::String)
 
     # These are the platforms we will build for by default, unless further
     # platforms are passed in on the command line
-    # Required Julia version is v1.10, v1.12 is not supported yet
-    filter!(v -> v >= v"1.10" && v < v"1.12", julia_versions)
+    # Required Julia version is v1.10, v1.13 is not supported yet
+    filter!(v -> v >= v"1.10" && v < v"1.13", julia_versions)
     platforms = vcat(libjulia_platforms.(julia_versions)...)
 
     # We cannot build with musl since OpenFHE requires the `execinfo.h` header for `backtrace`
