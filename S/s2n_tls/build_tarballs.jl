4dba544a0fdd5637cc6cde4473b108129681040b
quinnj2 <karbarcca2@gmail.com>
2025-07-24 18:23:23 -0600

    [s2n_tls] Update to 1.5.23 with latest dependency versions (#11724)
    Co-authored-by: github-actions <github-actions@github.com>

--- S/s2n_tls/build_tarballs.jl
+++ S/s2n_tls/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "s2n_tls"
-version = v"1.5.22"
+version = v"1.5.23"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/aws/s2n-tls.git", "bb9c5991877c6a749981204131e6e47845adbe3d"),
+    GitSource("https://github.com/aws/s2n-tls.git", "30f6248b828091510dbbde0c3cddf2052c9f67d3"),
 ]
 
 # Bash recipe for building across all platforms
