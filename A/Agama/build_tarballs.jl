5aadf8dca29d5ddc008d8f513f33454ea7890d6a
Zach Langford <langfzac@sas.upenn.edu>
2025-07-25 10:04:30 -0400

    Add compat for GSL_jll

--- A/Agama/build_tarballs.jl
+++ A/Agama/build_tarballs.jl
@@ -16,7 +16,7 @@ products = [
 ]
 
 dependencies = [
-    Dependency("GSL_jll"),
+    Dependency("GSL_jll", compat="~2.8.1"),
     Dependency("LLVMOpenMP_jll"),
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))
 ]
