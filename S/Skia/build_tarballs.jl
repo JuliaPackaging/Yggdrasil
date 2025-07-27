13b484412d1102889cf31ace9d939cd3ee0520ec
stensmo <141365386+stensmo@users.noreply.github.com>
2025-07-27 20:45:55 +0200

    New version of C API

--- S/Skia/build_tarballs.jl
+++ S/Skia/build_tarballs.jl
@@ -8,7 +8,7 @@ version = v"0.40.0"
 # Collection of sources required to complete build
 sources = [
    GitSource("https://github.com/google/skia.git", "482de011c920d85fdbe21a81c45852655df6a809"),
-   GitSource("https://github.com/stensmo/cskia.git", "3438e6efd3a4f27f43457db675ceb33da30c60cf"),
+   GitSource("https://github.com/stensmo/cskia.git", "7790e8f426cec7ad620cd04a04f28f604751e8d1"),
    DirectorySource("./bundled"),
    # Missing header ft2build.h for freetype2
    GitSource("https://chromium.googlesource.com/chromium/src/third_party/freetype2.git","5d4e649f740c675426fbe4cdaffc53ee2a4cb954"),
