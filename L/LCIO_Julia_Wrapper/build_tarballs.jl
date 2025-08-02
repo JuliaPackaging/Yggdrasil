8d36fc089f6ee5d8e5d6f1a4ca4b3bf40011d538
Jerry Ling <proton@jling.dev>
2025-08-02 12:05:21 -0400

    Change repo for LCIO_Julia_Wrapper (#11764)
    * change repo
    
    * add libcxxwrap compat
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl
    
    * increase version number
    
    * Update build_tarballs.jl
    
    * Update build_tarballs.jl

--- L/LCIO_Julia_Wrapper/build_tarballs.jl
+++ L/LCIO_Julia_Wrapper/build_tarballs.jl
@@ -10,12 +10,12 @@ uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
 name = "LCIO_Julia_Wrapper"
-version = v"0.13.2"
+version = v"0.13.3"
 julia_versions = [v"1.6.3", v"1.7", v"1.8", v"1.9", v"1.10"]
 
 # Collection of sources required to build LCIOWrapBuilder
 sources = [
-	GitSource("https://github.com/jstrube/LCIO_Julia_Wrapper.git", "e28132bfdc0664faf9724a74b2ae33803c26dc5a")
+	GitSource("https://github.com/JuliaHEP/LCIO_Julia_Wrapper.git", "e28132bfdc0664faf9724a74b2ae33803c26dc5a")
 ]
 
 # Bash recipe for building across all platforms
@@ -35,7 +35,8 @@ include("../../L/libjulia/common.jl")
 platforms = expand_cxxstring_abis(vcat(libjulia_platforms.(julia_versions)...))
 filter!(!Sys.isfreebsd, platforms)
 filter!(!Sys.iswindows, platforms)
-filter!(p -> arch(p) ∉ ("armv6l", "armv7l") , platforms)
+filter!(p -> arch(p) ∉ ("armv6l", "armv7l", "i686") , platforms)
+filter!(p -> libc(p) != "musl" , platforms)
 	
 # The products that we will ensure are always built
 products = [
@@ -44,8 +45,8 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="libcxxwrap_julia_jll")),
-    Dependency(PackageSpec(name="LCIO_jll")),
+    Dependency(PackageSpec(name="libcxxwrap_julia_jll"), compat="0.13.4"),
+    Dependency(PackageSpec(name="LCIO_jll"), compat="2.22.6"),
     BuildDependency(PackageSpec(name="libjulia_jll"))
 ]
 
