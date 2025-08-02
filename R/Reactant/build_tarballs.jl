bcaa81b22d16883e3cf0b0ed75e73bdd13a4d661
William Moses <me@wsmoses.com>
2025-08-02 03:05:09 -0500

    Reactant 0.0.227 (#11763)

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -6,10 +6,10 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "Reactant"
 repo = "https://github.com/EnzymeAD/Reactant.jl.git"
-version = v"0.0.226"
+version = v"0.0.227"
 
 sources = [
-   GitSource(repo, "12896f896a4adef841a3261e9e004ce0ff051686"),
+   GitSource(repo, "aca4040b06eeee63466e1bfbdda6e97ba0a8460d"),
    ArchiveSource("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_alpine-linux_hotspot_21.0.7_6.tar.gz", "79ecc4b213d21ae5c389bea13c6ed23ca4804a45b7b076983356c28105580013"),
    ArchiveSource("https://github.com/JuliaBinaryWrappers/Bazel_jll.jl/releases/download/Bazel-v7.6.1+0/Bazel.v7.6.1.x86_64-linux-musl-cxx03.tar.gz", "01ac6c083551796f1f070b0dc9c46248e6c49e01e21040b0c158f6e613733345")
 ]
