8fcb6a8001094db7e9de40289890802a69643661
William S. Moses <gh@wsmoses.com>
2025-08-07 15:52:09 -0500

    fix

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -9,7 +9,7 @@ repo = "https://github.com/EnzymeAD/Reactant.jl.git"
 version = v"0.0.226"
 
 sources = [
-   GitSource(repo, "3fa56341d848922a676448f953ba7d2817dd2a21"),
+   GitSource(repo, "9cfe48a763ab7ed340df0be1c21c7a8413f82b44"),
    ArchiveSource("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_alpine-linux_hotspot_21.0.7_6.tar.gz", "79ecc4b213d21ae5c389bea13c6ed23ca4804a45b7b076983356c28105580013"),
    ArchiveSource("https://github.com/JuliaBinaryWrappers/Bazel_jll.jl/releases/download/Bazel-v7.6.1+0/Bazel.v7.6.1.x86_64-linux-musl-cxx03.tar.gz", "01ac6c083551796f1f070b0dc9c46248e6c49e01e21040b0c158f6e613733345")
 ]
@@ -110,9 +110,9 @@ elif [[ "${target}" == x86_64-linux-* ]]; then
    BAZEL_CPU=k8
 elif [[ "${target}" == aarch64-linux-* ]]; then
    BAZEL_CPU=aarch64
-elif [[ "${target}" == x86_64-w64-mingw32-* ]]; then
+elif [[ "${target}" == x86_64-w64-mingw32* ]]; then
    BAZEL_CPU=x64_windows
-elif [[ "${target}" == aarch64-mingw32-* ]]; then
+elif [[ "${target}" == aarch64-mingw32* ]]; then
    BAZEL_CPU=arm64_windows
 fi
 
