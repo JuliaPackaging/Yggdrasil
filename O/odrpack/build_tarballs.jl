813d22f67d8f2fcf3166e4193186c4825c16ed62
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-08-09 18:12:10 +0200

    back to meson, with gcc=12

--- O/odrpack/build_tarballs.jl
+++ O/odrpack/build_tarballs.jl
@@ -29,25 +29,25 @@ dependencies = [
     Dependency("OpenBLAS32_jll")
 ]
 
-# script = raw"""
-# cd $WORKSPACE/srcdir/odrpack95
-
-# mkdir build && cd build
-# meson setup .. --cross-file="${MESON_TARGET_TOOLCHAIN}" -Dbuild_shared=true
-# ninja -j${nproc}
-# ninja install
-# """
-
 script = raw"""
 cd $WORKSPACE/srcdir/odrpack95
+
 mkdir build && cd build
-cmake .. \
-      -DCMAKE_INSTALL_PREFIX=${prefix} \
-      -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-      -DBUILD_SHARED=ON
-cmake --build . --parallel ${nproc}
-cmake --install .
+meson setup .. --cross-file="${MESON_TARGET_TOOLCHAIN}" -Dbuild_shared=true
+ninja -j${nproc}
+ninja install
 """
+
+# script = raw"""
+# cd $WORKSPACE/srcdir/odrpack95
+# mkdir build && cd build
+# cmake .. \
+#       -DCMAKE_INSTALL_PREFIX=${prefix} \
+#       -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+#       -DBUILD_SHARED=ON
+# cmake --build . --parallel ${nproc}
+# cmake --install .
+# """
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
     julia_compat="1.6", preferred_gcc_version=v"12")
