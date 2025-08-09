51028a31e5577cc6941cd614456a43a12fffd74e
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-08-09 13:46:02 +0200

    all platforms; only libgfortran5

--- O/odrpack/build_tarballs.jl
+++ O/odrpack/build_tarballs.jl
@@ -7,19 +7,24 @@ sources = [
     GitSource("https://github.com/HugoMVale/odrpack95.git", "54e58ae7f56564e358fb097f2108e4112498fce9")
 ]
 
-platforms = [
-    Platform("i686",    "linux"; libc="glibc"),
-    Platform("aarch64", "linux"; libc="glibc"),
-    Platform("x86_64",  "linux"; libc="glibc"),
-    Platform("i686",    "windows"),
-    Platform("x86_64",  "windows"),
-    Platform("x86_64",  "macos"),
-    Platform("x86_64",  "freebsd")
-]
+# platforms = [
+#     Platform("i686",        "linux"; libc="glibc"),
+#     Platform("aarch64",     "linux"; libc="glibc"),
+#     Platform("x86_64",      "linux"; libc="glibc"),
+#     Platform("powerpc64le", "linux"; libc="glibc"),
+#     Platform("i686",        "windows"),
+#     Platform("x86_64",      "windows"),
+#     Platform("x86_64",      "macos"),
+#     Platform("aarch64",     "macos"),
+#     Platform("x86_64",      "freebsd"),
+#     Platform("aarch64",     "freebsd")
+# ]
 
-# platforms = expand_gfortran_versions(platforms)
-# Disable old libgfortran builds - only use libgfortran5
-# filter!(p -> !(any(libgfortran_version(p) .== (v"4.0.0", v"3.0.0"))), platforms)
+platforms = supported_platforms()
+
+platforms = filter(p -> !(libc(p) == "musl"), platforms)
+platforms = expand_gfortran_versions(platforms)
+platforms = filter(p -> libgfortran_version(p).major ≥ 5, platforms)
 
 products = [
     LibraryProduct("libodrpack95", :libodrpack95)
