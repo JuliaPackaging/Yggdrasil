f8a455fb0892f62f52b1d45efe5f84dcaecc3b8c
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-08-13 00:44:33 +0200

    [Bazel] Enable ccache (#11826)
    * [Bazel] Enable ccache
    
    * [Bazel] Trigger rebuild
    
    [skip ci]

--- B/Bazel/build_tarballs.jl
+++ B/Bazel/build_tarballs.jl
@@ -5,7 +5,7 @@ version = v"8.3.1"
 sources = [
     ArchiveSource("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_alpine-linux_hotspot_21.0.7_6.tar.gz", "79ecc4b213d21ae5c389bea13c6ed23ca4804a45b7b076983356c28105580013"),
     ArchiveSource("https://github.com/bazelbuild/bazel/releases/download/$(version)/bazel-$(version)-dist.zip", "79da863df05fa4de79a82c4f9d4e710766f040bc519fd8b184a4d4d51345d5ba"),
-    
+
 ]
 
 script = raw"""
@@ -45,7 +45,7 @@ if [ "${EMBED_LABEL-x}" = "x" ]; then
   EMBED_LABEL="$(get_last_version) (@${git_sha1:-non-git})"
 fi
 
-export EXTRA_BAZEL_ARGS="--tool_java_runtime_version=local_jdk --jobs ${nproc}" 
+export EXTRA_BAZEL_ARGS="--tool_java_runtime_version=local_jdk --jobs ${nproc}"
 set -o xtrace
 
 source scripts/bootstrap/bootstrap.sh
@@ -53,7 +53,10 @@ source scripts/bootstrap/bootstrap.sh
 bazel_build "src:bazel_nojdk${EXE_EXT}" \
   --action_env=PATH \
   --host_platform=@platforms//host \
-  --platforms=@platforms//host
+  --platforms=@platforms//host \
+  --action_env=USE_CCACHE \
+  --action_env=CCACHE_DIR \
+  --action_env=CCACHE_NOHASHDIR=yes
 
 bazel_bin_path="$(get_bazel_bin_path)/src/bazel_nojdk${EXE_EXT}"
 cp -f "$bazel_bin_path" "output/bazel${EXE_EXT}"
