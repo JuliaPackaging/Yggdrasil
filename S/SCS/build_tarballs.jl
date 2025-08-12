027aaafca402becdbf73cb11376aaedd98647cf2
araujoms <maltusan@gmail.com>
2025-08-12 18:16:20 +0200

    update openblas

--- S/SCS/build_tarballs.jl
+++ S/SCS/build_tarballs.jl
@@ -34,7 +34,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("OpenBLAS32_jll", v"0.3.10"),
+    Dependency("OpenBLAS32_jll"),
     # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
     # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae");
