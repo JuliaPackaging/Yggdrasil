28c36ce120a5117064d9eb88b80c203fff7d5587
William Moses <me@wsmoses.com>
2025-08-14 14:04:10 -0700

    Update build_tarballs.jl

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -9,7 +9,7 @@ repo = "https://github.com/EnzymeAD/Reactant.jl.git"
 version = v"0.0.230"
 
 sources = [
-   GitSource(repo, "de0c357a97d611ba5a3eb40f904e297bef3c9dc9"),
+   GitSource(repo, "6b0bcf9556b254f3d4514b8cef65495100d5527e"),
    ArchiveSource("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_alpine-linux_hotspot_21.0.7_6.tar.gz", "79ecc4b213d21ae5c389bea13c6ed23ca4804a45b7b076983356c28105580013"),
    ArchiveSource("https://github.com/JuliaBinaryWrappers/Bazel_jll.jl/releases/download/Bazel-v7.6.1+0/Bazel.v7.6.1.x86_64-linux-musl-cxx03.tar.gz", "01ac6c083551796f1f070b0dc9c46248e6c49e01e21040b0c158f6e613733345")
 ]
@@ -75,7 +75,6 @@ BAZEL_BUILD_FLAGS+=(--jobs ${nproc})
 # BAZEL_BUILD_FLAGS+=(--action_env=SUPER_VERBOSE=true)
 
 BAZEL_BUILD_FLAGS+=(--verbose_failures)
-BAZEL_BUILD_FLAGS+=(--strip=never)
 BAZEL_BUILD_FLAGS+=(--cxxopt=-std=c++17 --host_cxxopt=-std=c++17)
 BAZEL_BUILD_FLAGS+=(--cxxopt=-DTCP_USER_TIMEOUT=0)
 BAZEL_BUILD_FLAGS+=(--check_visibility=false)
