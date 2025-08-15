c08778db1871d019d6401c56a7e74a0b83c9176f
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-08-15 16:59:08 +0200

    [Singular] Revert "update to 4.4.1p1 (#11851)" (#11861)

--- S/Singular/build_tarballs.jl
+++ S/Singular/build_tarballs.jl
@@ -29,7 +29,7 @@ import Pkg.Types: VersionSpec
 name = "Singular"
 
 upstream_version = v"4.4.1-3" # 4.4.1
-version_offset = v"0.0.0"
+version_offset = v"0.0.1"
 
 version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + version_offset.major,
                         upstream_version.patch * 100 + version_offset.minor,
@@ -37,7 +37,7 @@ version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + 
 
 # Collection of sources required to build Singular
 sources = [
-    GitSource("https://github.com/Singular/Singular.git", "2bb69e4d59e9a3e99ece691ee53178d6391e758f"),
+    GitSource("https://github.com/Singular/Singular.git", "595d7167e6e019d45d9a4f1e18ae741df1f3c41d"),
     #ArchiveSource("https://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/$(upstream_version.major)-$(upstream_version.minor)-$(upstream_version.patch)/singular-$(upstream_version).tar.gz",
     #              "5b0f6c036b4a6f58bf620204b004ec6ca3a5007acc8352fec55eade2fc9d63f6"),
     #DirectorySource("./bundled")
