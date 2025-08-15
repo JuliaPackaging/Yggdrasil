5d8b60b848fd39335e576a194be1b8140706ddd2
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-08-15 14:29:23 +0200

    [GAP_pkg_*] Rebuild with julia compat 1.10 (part 5)

--- G/GAP_pkg/GAP_pkg_anupq/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_anupq/build_tarballs.jl
@@ -4,7 +4,7 @@ include("../common.jl")
 
 name = "anupq"
 upstream_version = "3.3.1" # when you increment this, reset offset to v"0.0.0"
-offset = v"0.0.0" # increment this when rebuilding with unchanged upstream_version
+offset = v"0.0.1" # increment this when rebuilding with unchanged upstream_version
 version = offset_version(upstream_version, offset)
 
 # This package only produces an executable and does not need GAP for this at all.
@@ -56,5 +56,5 @@ products = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"7")
+               julia_compat="1.10", preferred_gcc_version=v"7")
 
