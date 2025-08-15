3d989a93f936b2b051d83f23d94d0629b1516606
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2025-08-15 12:03:43 +0200

    [Singular] update to 4.4.1p1

--- S/Singular/build_tarballs.jl
+++ S/Singular/build_tarballs.jl
@@ -28,8 +28,8 @@ import Pkg.Types: VersionSpec
 #
 name = "Singular"
 
-upstream_version = v"4.4.1-1" # 4.4.1
-version_offset = v"0.0.9"
+upstream_version = v"4.4.1-3" # 4.4.1
+version_offset = v"0.0.10"
 
 version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + version_offset.major,
                         upstream_version.patch * 100 + version_offset.minor,
@@ -37,7 +37,7 @@ version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + 
 
 # Collection of sources required to build Singular
 sources = [
-    GitSource("https://github.com/Singular/Singular.git", "595d7167e6e019d45d9a4f1e18ae741df1f3c41d"),
+    GitSource("https://github.com/Singular/Singular.git", "2bb69e4d59e9a3e99ece691ee53178d6391e758f"),
     #ArchiveSource("https://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/$(upstream_version.major)-$(upstream_version.minor)-$(upstream_version.patch)/singular-$(upstream_version).tar.gz",
     #              "5b0f6c036b4a6f58bf620204b004ec6ca3a5007acc8352fec55eade2fc9d63f6"),
     #DirectorySource("./bundled")
