ebbbdfa26eaa6e230ac789594658d01d8620f1d5
Erik Schnetter <schnetter@gmail.com>
2025-08-19 15:00:48 -0400

    SLEEF: Make Apple work

--- S/SLEEF/build_tarballs.jl
+++ S/SLEEF/build_tarballs.jl
@@ -8,27 +8,24 @@ version = v"3.9.0"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/shibatch/sleef.git", "906ca7512ee483296780a81a21b9ca715d40dfe1"),
-    #TODO FileSource("https://github.com/phracker/MacOSX-SDKs/releases/download/11.3/MacOSX11.3.sdk.tar.xz",
-    #TODO            "cd4f08a75577145b8f05245a2975f7c81401d75e9535dcffbb879ee1deefcbf4"),
+    # We need C++20
+    FileSource("https://github.com/alexey-lysiuk/macos-sdk/releases/download/14.5/MacOSX14.5.tar.xz",
+               "f6acc6209db9d56b67fcaf91ec1defe48722e9eb13dc21fb91cfeceb1489e57e"),
     DirectorySource("bundled"),
 ]
-
-There are problems on 32-bit systems, on 64-bit Windows, and on MacOS.
-On MacOS we probably need the MacOSX12 SDK and we don't know how to download it.
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/sleef
 
-#TODO if [[ "${target}" == *-apple-darwin* ]]; then
-#TODO     rm -rf /opt/${target}/${target}/sys-root/System
-#TODO     tar --extract --file=${WORKSPACE}/srcdir/MacOSX11.3.sdk.tar.xz --directory="/opt/${target}/${target}/sys-root/." --strip-components=1 MacOSX11.3.sdk/System MacOSX11.3.sdk/usr
-#TODO     export MACOSX_DEPLOYMENT_TARGET=11.3
-#TODO fi
-
-if [[ $target == arm-* ]]; then
-    atomic_patch -p1 ../patches/arm-neon32vfpv4.patch
+if [[ "${target}" == *-apple-darwin* ]]; then
+    rm -rf /opt/${target}/${target}/sys-root/System /opt/${target}/${target}/sys-root/usr/include/libxml2
+    tar --extract --file=${WORKSPACE}/srcdir/MacOSX14.5.tar.xz --directory="/opt/${target}/${target}/sys-root/." --strip-components=1 MacOSX14.5.sdk/System MacOSX14.5.sdk/usr
+    export MACOSX_DEPLOYMENT_TARGET=14.5
 fi
+
+# The respective file does not exist any more on the master branch
+atomic_patch -p1 ../patches/windows.patch
 
 mkdir build-native
 cd build-native
@@ -61,12 +58,19 @@ cmake \
     ..
 ninja all
 ninja install
+
+install_license $WORKSPACE/srcdir/sleef/LICENSE.txt
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
-filter!(p -> !(arch(p) == "i686" && Sys.iswindows(p)), platforms) # i686-windows build fails
+# 32-bit platforms are not supported any more
+filter!(p -> arch(p) ∉ ("armv6l", "armv7l", "i686"), platforms)
+# On Windows there is a problem with exception handling. SLEEF wants
+# to use sjlj, but GCC doesn't provide the respective run-time
+# functions.
+filter!(!Sys.iswindows, platforms)
 
 # The products that we will ensure are always built
 products = [
@@ -78,10 +82,15 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+    # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD systems),
+    # and libgomp from `CompilerSupportLibraries_jll` everywhere else.
+    Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae");
+               platforms=filter(!Sys.isbsd, platforms)),
+    Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e");
+               platforms=filter(Sys.isbsd, platforms)),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-# SLEEF uses modern C++ features and requires at least GCC 11
+# SLEEF uses C++20 and requires at least GCC 11
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.6", preferred_gcc_version=v"11")
