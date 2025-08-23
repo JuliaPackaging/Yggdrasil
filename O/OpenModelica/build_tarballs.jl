07c7a7b8f3e088acc6902768c6339a0316b6be4b
Viral B. Shah <ViralBShah@users.noreply.github.com>
2025-08-22 21:57:01 -0400

    [OpenModelica] Check which platforms will build fully

--- O/OpenModelica/build_tarballs.jl
+++ O/OpenModelica/build_tarballs.jl
@@ -35,10 +35,11 @@ install_license OSMC-License.txt
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("x86_64", "linux"; libc="glibc"),
-]
-platforms = expand_cxxstring_abis(platforms)
+#platforms = [
+#    Platform("x86_64", "linux"; libc="glibc"),
+#]
+#platforms = expand_cxxstring_abis(platforms)
+platforms = expand_cxxstring_abis(supported_platforms())
 
 # The products that we will ensure are always built
 products = [
