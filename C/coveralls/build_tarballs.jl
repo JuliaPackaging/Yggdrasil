a214640cd9bf52bd37a18f45c06b6ae3de0544cc
Ian Butterworth <i.r.butterworth@gmail.com>
2025-08-24 08:09:09 +0100

    try linux only

--- C/coveralls/build_tarballs.jl
+++ C/coveralls/build_tarballs.jl
@@ -68,7 +68,7 @@ install_license LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+platforms = filter(p -> Sys.islinux(p), supported_platforms())
 # The products that we will ensure are always built
 products = [
     ExecutableProduct("coveralls", :coveralls),
