b4732250916ad50b5c6206f88d8f27db0db3cd03
William Moses <gh@wsmoses.com>
2025-08-29 11:33:59 -0500

    Update GitSource commit hash in build_tarballs.jl

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -9,7 +9,7 @@ repo = "https://github.com/EnzymeAD/Reactant.jl.git"
 version = v"0.0.237"
 
 sources = [
-   GitSource(repo, "230089d578d12aed131ab6f9d09622ce21bd7fc5"),
+   GitSource(repo, "d4674bbb5dad3fd106aa98dfb2b4f6973f14dc03"),
    ArchiveSource("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_alpine-linux_hotspot_21.0.7_6.tar.gz", "79ecc4b213d21ae5c389bea13c6ed23ca4804a45b7b076983356c28105580013"),
    ArchiveSource("https://github.com/JuliaBinaryWrappers/Bazel_jll.jl/releases/download/Bazel-v7.6.1+0/Bazel.v7.6.1.x86_64-linux-musl-cxx03.tar.gz", "01ac6c083551796f1f070b0dc9c46248e6c49e01e21040b0c158f6e613733345")
 ]
