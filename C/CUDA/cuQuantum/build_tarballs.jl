89169f6e0d7297e7f9cf2d40ce12951bf87b00df
Katharine Hyatt <kshyatt@users.noreply.github.com>
2025-09-01 12:34:25 +0200

    Update build_tarballs.jl
    New minor release of CUQUANTUM has dropped

--- C/CUDA/cuQuantum/build_tarballs.jl
+++ C/CUDA/cuQuantum/build_tarballs.jl
@@ -8,7 +8,7 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "cuQuantum"
-version_str = "25.03.0"
+version_str = "25.06.0"
 version = VersionNumber(version_str)
 
 # Bash recipe for building across all platforms
