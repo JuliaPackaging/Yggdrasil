98314f9fae81bfde88678af4ba54beae233b64be
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-09-01 16:06:22 +0200

    Specify gcc version
    without it, there are many compilation warnings

--- G/GAP_lib/build_tarballs.jl
+++ G/GAP_lib/build_tarballs.jl
@@ -78,6 +78,6 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.10")
+               preferred_gcc_version=v"7", julia_compat="1.10")
 
 # Build trigger: 1
