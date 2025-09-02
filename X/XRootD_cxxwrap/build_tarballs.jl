6cd21f3eee33f22feb32822c6886f1dc575a8b3a
Pere Mato <pere.mato@cern.ch>
2025-09-02 14:18:33 +0200

    openssl issue workaround

--- X/XRootD_cxxwrap/build_tarballs.jl
+++ X/XRootD_cxxwrap/build_tarballs.jl
@@ -7,6 +7,11 @@ using BinaryBuilder, Pkg
 uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
 delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
 
+# Workaround for the Pkg issue above, also remove openssl stdlib
+openssl = Base.UUID("458c3c95-2e84-50aa-8efc-19380b2a3a95")
+delete!(Pkg.Types.get_last_stdlibs(v"1.12.0"), openssl)
+delete!(Pkg.Types.get_last_stdlibs(v"1.13.0"), openssl  )
+
 name = "XRootD_cxxwrap"
 version = v"0.3.0"
 
