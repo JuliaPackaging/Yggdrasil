c523b9b282aebe759540062eccb9f6ece0f6fdf7
Mandar Chitre <mandar@nus.edu.sg>
2025-09-05 02:00:14 +0800

    Update version and patch ORCA for mode dumping
    Updated version number and added a patch for ORCA.

--- A/AcousticsToolbox/build_tarballs.jl
+++ A/AcousticsToolbox/build_tarballs.jl
@@ -4,13 +4,29 @@
 using BinaryBuilder, Pkg
 
 name = "AcousticsToolbox"
-version = VersionNumber("2025.06.18")
+version = VersionNumber("2025.09.05")
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("http://oalib.hlsresearch.com/AcousticsToolbox/at_2024_12_25.zip", "7b57e80bded7f71ea9536e541029615f3f430e390651d697a2212569cbafd85c")
     ArchiveSource("https://oalib-acoustics.org/website_resources/Modes/orca/mac_linux/ORCA_Mode_modelling_gfortran.zip", "4ac15c1374e08bedd0dd03fd5f79612a8f84899ebf529237e662d7efb1dfb10a")
 ]
+
+# patch ORCA to dump modes and TL (both are broken in this version)
+patch = """
+c: HACK: dump mode functions and acoustic pressure to a file
+               open(67,file='modes_tlc.bin',form='unformatted')
+               write(67) nzsr, nmode, nrec, nsrc
+               write(67) ((phi(j,jm), j=1,nzsr), jm=1,nmode)
+               write(67) ((tlc(jrec,jsrc), jrec=1,nrec), jsrc=1,nsrc)
+               close(67)
+"""
+orca = readlines("$WORKSPACE/srcdir/ORCA_Mode_modelling_gfortran/src/cw_modes.f")
+open("$WORKSPACE/srcdir/ORCA_Mode_modelling_gfortran/src/cw_modes.f", "w") do f
+    print(f, join(orca[1:107], '\n'))
+    print(f, patch)
+    print(f, join(orca[108:end], '\n'))
+end
 
 # Bash recipe for building across all platforms
 script = raw"""
