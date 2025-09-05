fd5360a28692ea202e8b04b7b285f50ec605bf96
Erik Schnetter <schnetter@gmail.com>
2025-09-05 14:42:47 -0400

    Sollya: Correct build errors

--- S/Sollya/build_tarballs.jl
+++ S/Sollya/build_tarballs.jl
@@ -6,11 +6,14 @@ version = v"8.0.0"
 sources = [
     ArchiveSource("https://www.sollya.org/releases/sollya-$(version.major).$(version.minor)/sollya-$(version.major).$(version.minor).tar.bz2",
                   "5f5af648424084c1dc075ff103912905ff9bd6bf97187a922d0374939964c1e9"),
+    DirectorySource("bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/sollya*
+
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/gettimeofday.patch
 
 options=(
     --prefix=${prefix}
@@ -34,6 +37,9 @@ make install
 # platforms are passed in on the command line
 platforms = supported_platforms()
 
+# fplll is not available on Windows
+filter!(!Sys.iswindows, platforms)
+
 # The products that we will ensure are always built
 products = [
     LibraryProduct("libsollya", :libsollya),
@@ -47,10 +53,11 @@ dependencies = [
     Dependency("MPFR_jll"; compat="4.2.0"),
     Dependency("XML2_jll"; compat="~2.13.6"),
     Dependency("Zlib_jll"; compat="1.2.12"),
+    Dependency("dlfcn_win32_jll"; compat="1.4.1", platforms=filter(Sys.iswindows, platforms)),
     Dependency("fplll_jll"; compat="5.5.0"),
 ]
 
 # Build the tarballs.
 # We need at least GCC 5 for newer C and C++ features.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"5")
+               clang_use_lld=false, julia_compat="1.6", preferred_gcc_version=v"5")
