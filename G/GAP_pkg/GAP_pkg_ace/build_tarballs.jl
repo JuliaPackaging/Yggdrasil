7bd85a91c927b3c1e94bc4dfdd2d5661a043360c
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-09-06 16:25:21 +0200

    Update `ace` to 5.7.0

--- G/GAP_pkg/GAP_pkg_ace/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_ace/build_tarballs.jl
@@ -3,8 +3,8 @@
 include("../common.jl")
 
 name = "ace"
-upstream_version = "5.6.2" # when you increment this, reset offset to v"0.0.0"
-offset = v"0.0.1" # increment this when rebuilding with unchanged upstream_version
+upstream_version = "5.7.0" # when you increment this, reset offset to v"0.0.0"
+offset = v"1.0.0" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
 version = offset_version(upstream_version, offset)
 
 # This package only produces an executable and does not need GAP for this at all.
@@ -12,7 +12,7 @@ version = offset_version(upstream_version, offset)
 # Collection of sources required to build this JLL
 sources = [
     ArchiveSource("https://github.com/gap-packages/ace/releases/download/v$(upstream_version)/ace-$(upstream_version).tar.gz",
-                  "9cc94cd6327147b8b538341ba7594e4252a80ca7c960ac6749224efa437c9b66"),
+                  "7123e4100f1340a791d20bca1a8d9e6d9f09fe74a1c5fb15f1723899a1bb4553"),
 ]
 
 # Bash recipe for building across all platforms
