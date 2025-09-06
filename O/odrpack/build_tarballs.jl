2d516d2f0708bc2b3ca0b0a47ce4f2b3729aae68
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-09-06 12:27:08 +0200

    [odrpack] update to 3.0.1; no closures (#12029)
    * update to 3.0.1; no closures
    
    * remove musl exclusion
    
    * preferred_gcc_version=v"12"

--- O/odrpack/build_tarballs.jl
+++ O/odrpack/build_tarballs.jl
@@ -1,16 +1,14 @@
 using BinaryBuilder
 
 name = "odrpack"
-version = v"2.1.0"
+version = v"3.0.1"
 
 sources = [
     GitSource("https://github.com/HugoMVale/odrpack95.git", 
-    "89f4b1432da8bf316faf8628b4fb2f9ad6499b6a") # tag=v2.1.0
+    "22d3c785fee429febb9c1905ddfcca33a95cdc66") # tag=v3.0.1
 ]
 
 platforms = supported_platforms()
-
-platforms = filter(p -> !(libc(p) == "musl"), platforms)
 platforms = expand_gfortran_versions(platforms)
 platforms = filter(p -> libgfortran_version(p).major ≥ 5, platforms)
 
@@ -32,4 +30,4 @@ ninja install
 """
 # gcc-14 fails with x86_64-apple
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", clang_use_lld=false, preferred_gcc_version=v"13")
+               julia_compat="1.6", clang_use_lld=false, preferred_gcc_version=v"12")
