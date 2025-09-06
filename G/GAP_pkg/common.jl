5daaca47df8f77a2e9d4ceb4334aaecf28bc9dfb
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-09-06 16:22:50 +0200

    Update jll version scheme

--- G/GAP_pkg/common.jl
+++ G/GAP_pkg/common.jl
@@ -6,6 +6,11 @@ using BinaryBuilder
 Compute a version that allows distinguishing between changes in the upstream
 version and changes to the JLL which retain the same upstream version.
 
+To match the gap code of a package with its kernel extension jll, we need to
+make sure that any change to the upstream version is reflected at least
+as a minor version change. Patch versions are just for changes in jll
+dependencies, updated gap version etc.
+
 When the `upstream` version is changed, `offset` version numbers should be reset
 to `v"0.0.0"` and incremented following semantic versioning.
 """
@@ -13,8 +18,8 @@ function offset_version(upstream_str, offset)
     upstream = VersionNumber(replace(upstream_str, "-" => "."))
     return VersionNumber(
         upstream.major * 100 + offset.major,
-        upstream.minor * 100 + offset.minor,
-        upstream.patch * 100 + offset.patch,
+        upstream.minor * 10000 + upstream.patch * 100 + offset.minor,
+        offset.patch,
     )
 end
 
