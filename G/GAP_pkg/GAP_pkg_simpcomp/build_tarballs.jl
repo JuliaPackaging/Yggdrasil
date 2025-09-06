b4e588dfd3fc4cc5eb6d35f8f035b7a4d89239be
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2025-09-06 16:40:47 +0200

    Touch `simpcomp` for new versioning scheme

--- G/GAP_pkg/GAP_pkg_simpcomp/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_simpcomp/build_tarballs.jl
@@ -4,7 +4,7 @@ include("../common.jl")
 
 name = "simpcomp"
 upstream_version = "2.1.14" # when you increment this, reset offset to v"0.0.0"
-offset = v"0.0.1" # increment this when rebuilding with unchanged upstream_version
+offset = v"1.0.0" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
 version = offset_version(upstream_version, offset)
 
 # This package only produces an executable and does not need GAP for this at all.
@@ -45,3 +45,4 @@ products = [
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                julia_compat="1.10", preferred_gcc_version=v"7")
 
+# rebuild trigger: 1
