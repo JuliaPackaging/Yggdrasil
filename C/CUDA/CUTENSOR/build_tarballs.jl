070226c45adc3d5abf03d364c7875eb3bef5a072
Katharine Hyatt <kslimes@gmail.com>
2025-09-08 10:41:07 +0200

    Bump CUTENSOR to 2.3.1

--- C/CUDA/CUTENSOR/build_tarballs.jl
+++ C/CUDA/CUTENSOR/build_tarballs.jl
@@ -8,7 +8,7 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "CUTENSOR"
-version = v"2.3.0"
+version = v"2.3.1"
 
 script = raw"""
 mkdir -p ${libdir} ${prefix}/include
