badce71278a4479682d86a4c320adf9340b8fee8
Katharine Hyatt <kshyatt@users.noreply.github.com>
2025-09-08 11:41:34 +0200

    [CUDA_Driver] Update init.jl
    Try to broadcast over readlines to unbreak things

--- C/CUDA/CUDA_Driver/init.jl
+++ C/CUDA/CUDA_Driver/init.jl
@@ -127,7 +127,7 @@ function inspect_driver(driver, deps=String[]; inspect_devices=false)
     out = Pipe()
     proc = run(pipeline(cmd, stdin=devnull, stdout=out), wait=false)
     close(out.in)
-    out_reader = Threads.@spawn String(readlines(out))
+    out_reader = Threads.@spawn String.(readlines(out))
     wait(proc)
     success(proc) || return nothing
 
