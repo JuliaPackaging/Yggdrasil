d1d5f3f2fbf591f5f7c83781fc5de5ff17c77bf9
Katharine Hyatt <kshyatt@users.noreply.github.com>
2025-09-09 21:14:29 +0200

    Update build_tarballs.jl

--- C/CUDA/cuQuantum/build_tarballs.jl
+++ C/CUDA/cuQuantum/build_tarballs.jl
@@ -26,7 +26,7 @@ augment_platform_block = CUDA.augment
 
 dependencies = [
     RuntimeDependency(PackageSpec(name="CUDA_Runtime_jll")),
-    RuntimeDependency(PackageSpec(name="CUTENSOR_jll"), compat="2.2")
+    RuntimeDependency(PackageSpec(name="CUTENSOR_jll"), compat="2.3.1)
 ]
 
 # The products that we will ensure are always built
@@ -37,12 +37,15 @@ products = [
 ]
 
 platforms = [Platform("x86_64", "linux"),
-    Platform("aarch64", "linux"; cuda_platform="sbsa")]
+             Platform("aarch64", "linux")]
 
 builds = []
 for cuda_version in [v"12", v"13"], platform in platforms
     augmented_platform = deepcopy(platform)
     augmented_platform["cuda"] = CUDA.platform(cuda_version)
+    if cuda_version <= v"12"
+        augmented_platform["cuda_platform"] = "sbsa"
+    end
     should_build_platform(triplet(augmented_platform)) || continue
 
     sources = get_sources("cuquantum", ["cuquantum"]; version=version_str, platform=augmented_platform,
