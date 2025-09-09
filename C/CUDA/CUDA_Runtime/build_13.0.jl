ec3941c7b00c739c22a642680ea0126945a6bda8
Tim Besard <tim.besard@gmail.com>
2025-09-09 12:42:03 +0200

    CUDA: Bump Runtime/Compiler/Driver JLLs for 13.0.1.

--- C/CUDA/CUDA_Runtime/build_13.0.jl
+++ C/CUDA/CUDA_Runtime/build_13.0.jl
@@ -8,7 +8,7 @@ function get_products(platform)
         LibraryProduct(["libcusolver", "cusolver64_12"], :libcusolver),
         LibraryProduct(["libcusolverMg", "cusolverMg64_12"], :libcusolverMg),
         LibraryProduct(["libcurand", "curand64_10"], :libcurand),
-        LibraryProduct(["libcupti", "cupti64_2025.3.0"], :libcupti),
+        LibraryProduct(["libcupti", "cupti64_2025.3.1"], :libcupti),
         LibraryProduct(["libnvperf_host", "nvperf_host"], :libnvperf_host),
         LibraryProduct(["libnvperf_target", "nvperf_target"], :libnvperf_target),
         LibraryProduct(["libnvrtc", "nvrtc64_130_0"], :libnvrtc),
