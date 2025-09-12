adf419c5813b9a7df6cc41f4827db8ab65dc0812
Junyuan Chen <norman.chen.git@gmail.com>
2025-09-12 14:44:06 -0700

    Fix the wrong patch file name

--- A/AOCL/AOCL_Utils/build_tarballs.jl
+++ A/AOCL/AOCL_Utils/build_tarballs.jl
@@ -7,7 +7,7 @@ version = v"5.1.0"
 # Collection of sources required to complete build
 sources = [
     GitSource("https://github.com/amd/aocl-utils.git", "aaa5a385032a05973745cef9e8d7349fb5ba6cda"),
-    DirectorySource("./bundled")
+    DirectorySource("../bundled")
 ]
 
 # Bash recipe for building across all platforms
@@ -20,7 +20,7 @@ export CXXFLAGS="-static-libstdc++ -static-libgcc"
 export LDFLAGS="-static-libstdc++ -static-libgcc"
 
 if [[ "${target}" == *"musl"* ]]; then
-    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/X86Cpu_linux-musl.patch
+    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/au_X86Cpu_linux-musl.patch
 fi
 
 cmake -B default -DCMAKE_INSTALL_PREFIX=${prefix} -DAU_BUILD_STATIC_LIBS=OFF
