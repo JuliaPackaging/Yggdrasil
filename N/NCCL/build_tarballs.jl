4baafd6aea9498af5958e748b7553e9b72718fe8
Avik Pal <avikpal@mit.edu>
2025-09-11 21:42:50 -0400

    fix: use redist on some platforms

--- N/NCCL/build_tarballs.jl
+++ N/NCCL/build_tarballs.jl
@@ -11,12 +11,12 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 name = "NCCL"
 version = v"2.28.3"
 
-sources = [
+git_sources = [
     GitSource("https://github.com/NVIDIA/nccl.git", "f1308997d0420148b1be1c24d63f19d902ae589b"),
     DirectorySource("./bundled/")
 ]
 
-script = raw"""
+build_script = raw"""
 cd $WORKSPACE/srcdir
 for f in ${WORKSPACE}/srcdir/patches/*.patch; do
     atomic_patch -p1 ${f}
@@ -66,6 +66,20 @@ if [[ "${target}" == aarch64-linux-* ]]; then
 fi
 """
 
+redist_script = raw"""
+mkdir -p ${libdir} ${prefix}/include
+
+cd ${WORKSPACE}/srcdir
+cd nccl*
+find .
+
+install_license LICENSE.txt
+
+mv lib/libnccl*.so* ${libdir}
+
+mv include/* ${prefix}/include
+"""
+
 products = [
     LibraryProduct("libnccl", :libnccl),
 ]
@@ -76,9 +90,43 @@ dependencies = [
 ]
 
 builds = []
-for platform in CUDA.supported_platforms(; min_version=v"12.9")
+
+for platform in CUDA.supported_platforms(; min_version=v"12", max_version=v"12.8")
     should_build_platform(triplet(platform)) || continue
-    push!(builds, (; platforms=[platform]))
+    push!(builds, (; platforms=[platform], sources=git_sources, script=build_script, req_deps=true))
+end
+
+# redist for sources that are available
+for cuda_version in [v"12.9", v"13.0"]
+    platforms = [
+        Platform("x86_64", "linux"),
+        Platform("aarch64", "linux")
+    ]
+    for platform in platforms
+        augmented_platform = deepcopy(platform)
+        augmented_platform["cuda"] = "13"
+        should_build_platform(triplet(augmented_platform)) || continue
+
+        if cuda_version == v"12.9"
+            if arch(platform) == "aarch64"
+                hash = "c51b970bb26a0d3afd676048923fc404ed1d1131441558a7d346940e93d6ab54"
+            elseif arch(platform) == "x86_64"
+                hash = "98f7abd2f505ba49f032052f3f36b14e28798a6e16ca783fe293e351e9376546"
+            end
+        else
+            if arch(platform) == "aarch64"
+                hash = "2b5961c4c4bcbc16148d8431c7b65525d00f386105ab1b9fa82051b7c05f6fd0"
+            elseif arch(platform) == "x86_64"
+                hash = "3117db0efe13e1336dbe32e8b98eab943ad5baa69518189918d4aca9e3ce3270"
+            end
+        end
+
+        sources = [
+            ArchiveSource("https://developer.download.nvidia.com/compute/redist/nccl/v$(version)/nccl_$(version)-1+cuda$(cuda_version.major).$(cuda_version.minor)_$(arch(platform)).txz", hash)
+        ]
+
+        push!(builds, (; platforms=[augmented_platform], sources, script=redist_script, req_deps=false))
+    end
 end
 
 # don't allow `build_tarballs` to override platform selection based on ARGS.
@@ -89,10 +137,15 @@ non_platform_ARGS = filter(arg -> startswith(arg, "--"), ARGS)
 non_reg_ARGS = filter(arg -> arg != "--register", non_platform_ARGS)
 
 for (i, build) in enumerate(builds)
+    if build.req_deps
+        deps = [dependencies; CUDA.required_dependencies(build.platforms[1])]
+    else
+        deps = dependencies
+    end
+
     build_tarballs(i == lastindex(builds) ? non_platform_ARGS : non_reg_ARGS,
-        name, version, sources, script,
-        build.platforms, products,
-        [dependencies; CUDA.required_dependencies(build.platforms[1])];
+        name, version, build.sources, build.script,
+        build.platforms, products, deps;
         julia_compat="1.10", augment_platform_block=CUDA.augment,
         preferred_gcc_version=v"10")
 end
