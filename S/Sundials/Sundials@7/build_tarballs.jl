cb7e797f956d09f390eb36b5236d9bd4b636bb8b
Viral B. Shah <ViralBShah@users.noreply.github.com>
2025-09-12 21:32:12 -0400

    [Sundials] Try to build for GPU

--- S/Sundials/Sundials@7/build_tarballs.jl
+++ S/Sundials/Sundials@7/build_tarballs.jl
@@ -4,6 +4,10 @@ name = "Sundials"
 version = v"7.4.0"
 ygg_version = v"7.4.1" # Fake version since we are changing deps
 
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
+include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
+
 # Collection of sources required to build Sundials
 sources = [
     GitSource("https://github.com/LLNL/sundials.git",
@@ -29,21 +33,22 @@ fi
 # Build
 cd $WORKSPACE/srcdir/sundials*
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" \
-    -DEXAMPLES_ENABLE_C=OFF \
-    -DENABLE_KLU=ON \
-    -DKLU_INCLUDE_DIR="${includedir}/suitesparse" \
-    -DKLU_LIBRARY_DIR="${libdir}" \
-    -DKLU_WORKS=ON \
-    -DENABLE_LAPACK=ON \
-    -DLAPACK_WORKS=ON \
-    -DBLA_VENDOR="OpenBLAS" \
-    ..
 
-cmake --build . --parallel ${nproc}
-cmake --install .
+CMAKE_FLAGS=(-DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" -DEXAMPLES_ENABLE_C=OFF -DENABLE_KLU=ON -DKLU_INCLUDE_DIR="${includedir}/suitesparse" -DKLU_LIBRARY_DIR="${libdir}" -DKLU_WORKS=ON -DENABLE_LAPACK=ON -DLAPACK_WORKS=ON -DBLA_VENDOR="OpenBLAS")
+
+if  [[ $bb_full_target == *-linux*cuda+1* ]]; then
+    # nvcc writes to /tmp, which is a small tmpfs in our sandbox.
+    # make it use the workspace instead
+    export TMPDIR=${WORKSPACE}/tmpdir
+    mkdir ${TMPDIR}
+
+    export CUDA_HOME=${WORKSPACE}/destdir/cuda
+    export PATH=$PATH:$CUDA_HOME/bin
+fi
+
+cmake "${CMAKE_FLAGS}" --build . --parallel ${nproc}
+cmake "${CMAKE_FLAGS}" --install .
+rm -f ${libdir}/*.a
 """
 
 # We attempt to build for all the platforms OpenBLAS32_jll is available for
@@ -85,6 +90,59 @@ dependencies = [
     Dependency("SuiteSparse32_jll"),
 ]
 
-# Build the tarballs
-build_tarballs(ARGS, name, ygg_version, sources, script, platforms, products, dependencies;
-               preferred_gcc_version = v"6", julia_compat="1.6")
+augment_platform_block = CUDA.augment
+
+versions_to_build = [
+    nothing,
+    v"11.4",
+    v"12.0",
+]
+
+cuda_preambles = Dict(
+    nothing => "",
+    v"11.4" => "CUDA_ARCHS=\"60;70;75;80\";",
+    v"12.0" => "CUDA_ARCHS=\"60;70;75;80;89;90\";",
+)
+
+for cuda_version in versions_to_build, platform in platforms
+
+    cuda_platform = (os(platform) == "linux") && (arch(platform) in ["x86_64"])
+    if !isnothing(cuda_version) && !cuda_platform
+        continue
+    end
+
+    # For platforms we can't create cuda builds on, we want to avoid adding cuda=none
+    # https://github.com/JuliaPackaging/Yggdrasil/issues/6911#issuecomment-1599350319
+    if cuda_platform
+        augmented_platform = Platform(arch(platform), os(platform);
+            cxxstring_abi = cxxstring_abi(platform),
+            cuda=isnothing(cuda_version) ? "none" : CUDA.platform(cuda_version)
+        )
+    else
+        augmented_platform = deepcopy(platform)
+    end
+    should_build_platform(triplet(augmented_platform)) || continue
+
+    append!(dependencies,
+            AbstractDependency[
+                # For OpenMP we use libomp from `LLVMOpenMP_jll` where we use LLVM as compiler (BSD
+                # systems), and libgomp from `CompilerSupportLibraries_jll` everywhere else.
+                Dependency("CompilerSupportLibraries_jll";
+                           platforms=filter(!Sys.isbsd, [augmented_platform])),
+                Dependency("LLVMOpenMP_jll";
+                           platforms=filter(Sys.isbsd, [augmented_platform])),
+            ]
+            )
+
+    if !isnothing(cuda_version)
+        push!(dependencies, BuildDependency("CUDA_full_jll", version=CUDA.full_version(cuda_version)))
+        push!(dependencies, RuntimeDependency("CUDA_Runtime_jll"))
+    end
+    preamble = cuda_preambles[cuda_version]
+
+    build_tarballs(ARGS, name, version, sources,
+                   preamble*script, [augmented_platform], products, dependencies;
+                   preferred_gcc_version=v"6",
+                   julia_compat="1.6",
+                   augment_platform_block)
+end
