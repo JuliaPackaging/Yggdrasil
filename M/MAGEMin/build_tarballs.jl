9aa34c76a7d6b41b5ebd81aa913654e1f6459360
Nicolas Riel <50235786+NicolasRiel@users.noreply.github.com>
2025-09-15 12:53:27 +0200

    MAGEMin v1.8.5 (#12072)

--- M/MAGEMin/build_tarballs.jl
+++ M/MAGEMin/build_tarballs.jl
@@ -4,11 +4,11 @@ using BinaryBuilder, Pkg
 using Base.BinaryPlatforms
 
 name = "MAGEMin"
-version = v"1.8.4"
+version = v"1.8.5"
 
 # Collection of sources required to complete build
 sources = [GitSource("https://github.com/ComputationalThermodynamics/MAGEMin", 
-                    "442b7b93ed40dcdcc5e05a8a3e005241a88f2047")                 ]
+                    "748d91d9877a98269f570502adfc14262bcb95d4")                 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
