538be3880c3e7ee1b5830e0b2dc7e8bd15ba1647
Maarten Pronk <git@evetion.nl>
2025-09-18 13:15:43 +0200

    Remove cxx expand.

--- N/nanoarrow/build_tarballs.jl
+++ N/nanoarrow/build_tarballs.jl
@@ -21,7 +21,7 @@ cmake --install build
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = expand_cxxstring_abis(supported_platforms())
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
@@ -29,7 +29,7 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = [
+dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
