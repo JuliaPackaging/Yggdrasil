5ae1e7ae49bc506390bce4c773e64a6574c873d7
Oscar Dowson <odow@users.noreply.github.com>
2025-09-24 19:06:17 +1200

    Enhance build_tarballs.jl for CUDA integration
    Refactor build script to include CUDA support and dependencies.

--- C/cuPDLPx/build_tarballs.jl
+++ C/cuPDLPx/build_tarballs.jl
@@ -1,48 +1,54 @@
-using BinaryBuilder
+using Pkg, BinaryBuilder
+
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
+include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "cuPDLPx"
-version = v"0.1.0"  # Update to your release version
-
-# Source tarball from GitHub
-# sources = [
-#     ArchiveSource("https://github.com/MIT-Lu-Lab/cuPDLPx/archive/refs/tags/v$(version).tar.gz",
-#                   "<sha256sum-of-release-tarball>"),
-# ]
+version = v"0.1.0"
 
 sources = [
-    GitSource("https://github.com/ZedongPeng/cuPDLPx.git","6c9c99472023aae268c2dae690eadd1a3d37733c"),
-]
-
-platforms = [
-    Platform("x86_64", "linux"; libc="glibc"),
-    # Platform("aarch64", "linux"; libc="glibc"),
-]
-
-
-# Dependencies: CUDA runtime and libraries
-dependencies = [
-    Dependency("CUDA_Runtime_jll"),
-    Dependency("CUDA_full_jll"),
-    # Dependency("CUBLAS_jll"),
-    # Dependency("CUSPARSE_jll"),
+    GitSource(
+        "https://github.com/ZedongPeng/cuPDLPx.git",
+        "6c9c99472023aae268c2dae690eadd1a3d37733c",
+    ),
 ]
 
 script = raw"""
-#!/bin/bash
-set -euxo pipefail
-
-# Ensure CUDA paths are visible
-export CUDA_HOME="${prefix}"
-export PATH="${CUDA_HOME}/bin:$PATH"
-export LD_LIBRARY_PATH="${CUDA_HOME}/lib64:$LD_LIBRARY_PATH"
-
-# Build and install using the provided Makefile
-make -C $WORKSPACE/srcdir/cuPDLPx PREFIX=$prefix install
+cd ${WORKSPACE}/srcdir/cuPDLPx
+export CUDA_HOME="${prefix}/cuda"
+export PATH=${PATH}:${CUDA_HOME}/bin
+ln -s ${CUDA_HOME}/lib ${CUDA_HOME}/lib64
+make install PREFIX=$prefix
 """
 
-# Installed products
 products = [
-    LibraryProduct("libcupdlpx", :libcupdlpx),  # shared library
+    LibraryProduct("libcupdlpx", :libcupdlpx),
 ]
 
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;julia_compat = "1.9")
+platforms = CUDA.supported_platforms(; min_version = v"12.4")
+filter!(p -> arch(p) == "x86_64", platforms)
+
+for platform in platforms
+    if !should_build_platform(triplet(platform))
+        continue
+    end
+    dependencies = [
+        Dependency("Zlib_jll"),
+        Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")),
+        CUDA.required_dependencies(platform; static_sdk = true)...,
+    ]
+    build_tarballs(
+        ARGS,
+        name,
+        version,
+        sources, 
+        script,
+        [platform],
+        products,
+        dependencies;
+        preferred_gcc_version = v"8",
+        julia_compat = "1.10",
+        augment_platform_block = CUDA.augment,
+    )
+end
