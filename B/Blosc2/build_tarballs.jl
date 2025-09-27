1523c9e92231988551f5b1edb21aa9f93d624e91
Erik Schnetter <schnetter@gmail.com>
2025-09-27 04:56:47 -0400

    Blosc2: Update to 2.21.3 (#12148)

--- B/Blosc2/build_tarballs.jl
+++ B/Blosc2/build_tarballs.jl
@@ -4,7 +4,7 @@ using BinaryBuilder, Pkg
 
 name = "Blosc2"
 
-upstream_version = v"2.21.2"
+upstream_version = v"2.21.3"
 # We add a version offset because:
 # - Blosc2 2.15 is not ABI-compatible with Blosc2 2.14
 #   (see the release notes <https://github.com/Blosc/c-blosc2/releases/tag/v2.15.0>)
@@ -17,7 +17,7 @@ version = VersionNumber(upstream_version.major * 100 + version_offset.major,
 
 # Collection of sources required to build Blosc2
 sources = [
-    GitSource("https://github.com/Blosc/c-blosc2.git", "82a85f9d285d7ae6d141c73e84c7574bfc24a134"),
+    GitSource("https://github.com/Blosc/c-blosc2.git", "0c853a639ba97997e33e29db9eed202459ebc6f0"),
     DirectorySource("bundled"),
 ]
 
