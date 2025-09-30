62fe29fce58ac1aca47fb131fa728fc584ebe9ef
Kirill Shilin <kshilin@bhft.com>
2025-09-30 16:16:07 +0200

    initial commit

--- L/libwebsockets/build_tarballs.jl
+++ L/libwebsockets/build_tarballs.jl
@@ -3,7 +3,7 @@
 using BinaryBuilder, Pkg
 
 name = "libwebsockets"
-version = v"4.3.4"
+version = v"4.4.0"
 
 # Collection of sources required to complete build
 sources = [
@@ -17,11 +17,22 @@ cmake -B build \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
+    -DGENCERTS=OFF \
+    -DOPENSSL_EXECUTABLE="$(command -v openssl || echo /usr/bin/openssl)" \
+    -DCMAKE_C_FLAGS="${CFLAGS} -Duv_poll_init_socket=uv_poll_init" \
     -DZLIB_LIBRARY=${libdir}/libz.${dlext} \
     -DZLIB_INCLUDE_DIR=${includedir} \
     -DLWS_WITH_ACCESS_LOG=ON \
     -DLWS_WITHOUT_EXTENSIONS=OFF \
     -DLWS_WITHOUT_TESTAPPS=ON \
+    -DLWS_WITH_LIBUV=ON \
+    -DLWS_WITH_SHARED=ON \
+    -DLWS_WITH_STATIC=OFF \
+    -DLWS_WITH_SSL=ON \
+    -DLWS_WITH_ZLIB=ON \
+    -DLWS_WITH_MINIMAL_EXAMPLES=OFF \
+    -DLWS_IPV6=ON \
+    -DLWS_WITH_HTTP2=ON \
     -DLWS_WITH_SOCKS5=ON
 cmake --build build --parallel ${nproc}
 cmake --install build
@@ -30,7 +41,6 @@ cmake --install build
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms(exclude = Sys.iswindows)
-
 # The products that we will ensure are always built
 products = [
     LibraryProduct(["libwebsockets"], :libwebsockets)
@@ -39,8 +49,9 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("OpenSSL_jll"; compat="3.0.16"),
-    Dependency("Zlib_jll")
+    Dependency("Zlib_jll"),
+    Dependency("LibUV_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"5.2.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
