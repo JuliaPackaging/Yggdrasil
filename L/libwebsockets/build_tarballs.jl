7e66c90d0acab14f7421090c962075301312150e
luigikav <90866467+luigikav@users.noreply.github.com>
2025-10-01 23:17:33 +0200

    Libwebsocket with libuv (#12197)
    * initial commit
    
    * review fixes
    
    * add \n
    
    * revert gcc version
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- L/libwebsockets/build_tarballs.jl
+++ L/libwebsockets/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "libwebsockets"
-version = v"4.3.4"
+version = v"4.4.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/warmcat/libwebsockets.git", "e7fbdac39154c7bdfd42dd73c5cf25e4fd2e190d"),
+    GitSource("https://github.com/warmcat/libwebsockets.git", "e636b7bd133c4c0bfbdbf0d77afebdb20e09a5a2"),
 ]
 
 # Bash recipe for building across all platforms
@@ -17,11 +17,21 @@ cmake -B build \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
+    -DGENCERTS=OFF \
+    -DCMAKE_C_FLAGS="${CFLAGS} -Duv_poll_init_socket=uv_poll_init" \
     -DZLIB_LIBRARY=${libdir}/libz.${dlext} \
     -DZLIB_INCLUDE_DIR=${includedir} \
     -DLWS_WITH_ACCESS_LOG=ON \
     -DLWS_WITHOUT_EXTENSIONS=OFF \
     -DLWS_WITHOUT_TESTAPPS=ON \
+    -DLWS_WITH_LIBUV=ON \
+    -DLWS_WITH_SHARED=ON \
+    -DLWS_WITH_STATIC=OFF \
+    -DLWS_WITH_SSL=ON \
+    -DLWS_WITH_ZLIB=ON \
+    -DLWS_WITH_MINIMAL_EXAMPLES=OFF \
+    -DLWS_IPV6=ON \
+    -DLWS_WITH_HTTP2=ON \
     -DLWS_WITH_SOCKS5=ON
 cmake --build build --parallel ${nproc}
 cmake --install build
@@ -30,7 +40,6 @@ cmake --install build
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms(exclude = Sys.iswindows)
-
 # The products that we will ensure are always built
 products = [
     LibraryProduct(["libwebsockets"], :libwebsockets)
@@ -39,7 +48,8 @@ products = [
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("OpenSSL_jll"; compat="3.0.16"),
-    Dependency("Zlib_jll")
+    Dependency("Zlib_jll"),
+    Dependency("LibUV_jll")
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
