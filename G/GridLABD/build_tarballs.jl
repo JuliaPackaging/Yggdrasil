21ced9e6e214ccb88a75c21e10467b730fcd8730
Pedro Maciel Xavier <mail@pedro.xn--qxam>
2025-10-07 18:40:36 -0400

    Add  Patch

--- G/GridLABD/build_tarballs.jl
+++ G/GridLABD/build_tarballs.jl
@@ -7,12 +7,15 @@ version = v"5.3.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/gridlab-d/gridlab-d.git", "1b2e463d3c149e999a2c5f04213477f545e01dc6")
+    GitSource("https://github.com/gridlab-d/gridlab-d.git", "1b2e463d3c149e999a2c5f04213477f545e01dc6"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/gridlab-d/
+
+atomic_patch ../patches/apple-soname.patch
 
 git submodule update --init
 
@@ -27,7 +30,27 @@ install_license ${WORKSPACE}/srcdir/gridlab-d/LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
+# platforms = supported_platforms()
+platforms = [
+    # Platform("i686", "Linux"; libc="glibc"),
+    # Platform("x86_64", "Linux"; libc="glibc"),
+    # Platform("aarch64", "Linux"; libc="glibc"),
+    # Platform("armv6l", "Linux"; call_abi="eabihf", libc="glibc"),
+    # Platform("armv7l", "Linux"; call_abi="eabihf", libc="glibc"),
+    # Platform("powerpc64le", "Linux"; libc="glibc"),
+    # Platform("riscv64", "Linux"; libc="glibc"),
+    Platform("i686", "Linux"; libc="musl"),
+    Platform("x86_64", "Linux"; libc="musl"),
+    Platform("aarch64", "Linux"; libc="musl"),
+    Platform("armv6l", "Linux"; call_abi="eabihf", libc="musl"),
+    Platform("armv7l", "Linux"; call_abi="eabihf", libc="musl"),
+    Platform("x86_64", "macOS"),
+    Platform("aarch64", "macOS"),
+    Platform("x86_64", "FreeBSD"),
+    Platform("aarch64", "FreeBSD"),
+    Platform("i686", "Windows"),
+    Platform("x86_64", "Windows"),
+]
 
 # The products that we will ensure are always built
 products = [
@@ -35,8 +58,7 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
-]
+dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"9.1.0")
