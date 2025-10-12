f6c0692661ecb9fe30be559512f98ecf5b96a7c6
Dan Stahlke <dan@stahlke.org>
2025-10-12 12:29:27 -0700

    Update C/cliquer/build_tarballs.jl
    Change to platforms suggested by reviewer.
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- C/cliquer/build_tarballs.jl
+++ C/cliquer/build_tarballs.jl
@@ -25,23 +25,7 @@ cp LICENSE $prefix/share/licenses/cliquer/
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("riscv64", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("x86_64", "freebsd"; ),
-    Platform("aarch64", "freebsd"; )
-]
-
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
