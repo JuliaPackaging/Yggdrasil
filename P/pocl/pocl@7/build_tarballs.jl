6ef23a6bb6473e3106cc09d86bd6a92b0c77337c
Simeon David Schaub <simeon@schaub.rocks>
2025-10-15 14:02:32 +0200

    PoCL: Bump to v7.1 and reenable Float16 support (#12219)
    Also contains the workaround proposed in https://github.com/pocl/pocl/discussions/1943#discussioncomment-14605412

--- P/pocl/pocl@7/build_tarballs.jl
+++ P/pocl/pocl@7/build_tarballs.jl
@@ -7,7 +7,7 @@ const YGGDRASIL_DIR = "../../.."
 include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "pocl"
-version = v"7.0.0"
+version = v"7.1.0"
 
 # Build
 
@@ -15,7 +15,7 @@ version = v"7.0.0"
 sources = [
     DirectorySource("./bundled"),
     GitSource("https://github.com/juliagpu/pocl",
-              "ebd70398005f9e2fd93b0627ad467e006f5ddacd")
+              "afa068265c1c63b8eb92bfabdf3e232491e6c3b6")
 ]
 
 #=
@@ -95,7 +95,7 @@ for platform in platforms
     preferred_gcc_version = if Sys.iswindows(platform)
         v"13"
     else
-        v"10"
+        v"12"
     end
 
     push!(builds, (; platform,
@@ -118,7 +118,7 @@ for (i,build) in enumerate(builds)
                    name, version, build.sources, build_script(),
                    [build.platform], products, build.dependencies;
                    build.preferred_gcc_version, preferred_llvm_version=v"20",
-                   julia_compat="1.6", init_block=init_block())
+                   julia_compat="1.6", init_block=init_block(), dont_dlopen=true)
 end
 
 # bump
