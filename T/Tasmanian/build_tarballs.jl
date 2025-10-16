e37d71bfdb3bfc2c2a2a049d7a54a8be758a39ad
MichelJuillard <michel.juillard@mjui.fr>
2025-10-16 15:28:04 +0200

    remove commented dependency

--- T/Tasmanian/build_tarballs.jl
+++ T/Tasmanian/build_tarballs.jl
@@ -66,7 +66,6 @@ dependencies = [
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
     Dependency("libblastrampoline_jll"; compat="5.4.0"),
-#    Dependency("OpenBLAS"),
     HostBuildDependency("CMake_jll"),
 ]
 
