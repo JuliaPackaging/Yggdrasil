7c5c239a8d008b6e718f3e0a9f8f61c78e99b481
Michel Schanen <michel.schanen@gmail.com>
2025-10-16 13:04:43 -0500

    [oneAPI] libigc v2.18.5 (#12307)

--- L/libigc/build_tarballs.jl
+++ L/libigc/build_tarballs.jl
@@ -7,7 +7,7 @@ const YGGDRASIL_DIR = "../.."
 include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 name = "libigc"
-version = v"2.16.0"
+version = v"2.18.5"
 
 # IGC depends on LLVM, a custom Clang, and a Khronos tool. Instead of building these pieces
 # separately, taking care to match versions and apply Intel-specific patches where needed
@@ -27,11 +27,11 @@ version = v"2.16.0"
 #       see https://github.com/intel/intel-graphics-compiler/blob/master/.github/workflows/build-IGC.yml
 #
 sources = [
-    GitSource("https://github.com/intel/intel-graphics-compiler.git", "420b632df9ea2d645bafbd258789baf7c34e6f55"),
+    GitSource("https://github.com/intel/intel-graphics-compiler.git", "bd67908e0b06013e83ee02c9f3ff10cf976ed96a"),
     GitSource("https://github.com/intel/opencl-clang.git", "7eef46576eca117685ae431735c2725ddb889260" #= branch ocl-open-150 =#),
-    GitSource("https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git", "6fd7ff4e76b344dd17cca735af60ee613f322440" #= branch llvm_release_150 =#),
-    GitSource("https://github.com/KhronosGroup/SPIRV-Tools.git", "f289d047f49fb60488301ec62bafab85573668cc" #= tag v2025.1.rc1 =#),
-    GitSource("https://github.com/KhronosGroup/SPIRV-Headers.git", "aa6cef192b8e693916eb713e7a9ccadf06062ceb"), #= main =#
+    GitSource("https://github.com/KhronosGroup/SPIRV-LLVM-Translator.git", "a077a090060f953ba7dd024208980ca837233d87" #= branch llvm_release_150 =#),
+    GitSource("https://github.com/KhronosGroup/SPIRV-Tools.git", "28a883ba4c67f58a9540fb0651c647bb02883622" #= tag v2025.1.rc1 =#),
+    GitSource("https://github.com/KhronosGroup/SPIRV-Headers.git", "04b76709bf40a7ce8df3382060ef3620f19de566"), #= main =#
     GitSource("https://github.com/intel/vc-intrinsics.git", "46286b96fb9eee9fa4fcf8b8ecf74a8c01af4c1a" #= tag v0.23.1 =#),
     GitSource("https://github.com/llvm/llvm-project.git", "8dfdcc7b7bf66834a761bd8de445840ef68e4d1a" #= tag llvmorg-15.0.7 =#),
     # patches
