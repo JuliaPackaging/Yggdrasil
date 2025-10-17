71224aba8030a14f0f1cd40c0e5dace88fcea049
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2025-10-16 22:29:34 -0500

    [skip ci] Add a compat entry on SCALAPACK32_jll.jl for dependents (#12288)

--- M/MUMPS/MUMPS@5/build_tarballs.jl
+++ M/MUMPS/MUMPS@5/build_tarballs.jl
@@ -140,8 +140,8 @@ dependencies = [
     Dependency(PackageSpec(name="PARMETIS_jll", uuid="b247a4be-ddc1-5759-8008-7e02fe3dbdaa")),
     Dependency(PackageSpec(name="SCOTCH_jll", uuid="a8d0f55d-b80e-548d-aff6-1a04c175f0f9"); compat="~7.0.6"),
     # Dependency(PackageSpec(name="PTSCOTCH_jll", uuid="b3ec0f5a-9838-5c9b-9e77-5f2c6a4b089f"); compat="~7.0.6"),
-    Dependency(PackageSpec(name="SCALAPACK32_jll", uuid="aabda75e-bfe4-5a37-92e3-ffe54af3c273")),
-    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2"))
+    Dependency(PackageSpec(name="SCALAPACK32_jll", uuid="aabda75e-bfe4-5a37-92e3-ffe54af3c273"); compat="2.1.0 - 2.2.1"),
+    Dependency(PackageSpec(name="OpenBLAS32_jll", uuid="656ef2d0-ae68-5445-9ca0-591084a874a2")),
 ]
 append!(dependencies, platform_dependencies)
 
