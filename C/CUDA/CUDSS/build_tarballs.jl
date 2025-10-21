d2795bab54bb8f8d89fbbc4ff5110bd8600e9cf4
Alexis Montoison <alexis.montoison@polymtl.ca>
2025-10-21 08:43:59 -0500

    [CUDSS] Upgrade to v0.7.1

--- C/CUDA/CUDSS/build_tarballs.jl
+++ C/CUDA/CUDSS/build_tarballs.jl
@@ -8,8 +8,8 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "CUDSS"
-version = v"0.7.0"
-full_version = "0.7.0.20"
+version = v"0.7.1"
+full_version = "0.7.1.4"
 
 script = raw"""
 mkdir -p ${libdir} ${prefix}/include
