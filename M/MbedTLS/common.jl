345f9b0d959801795fc7268c452ab00c4cad2c7f
Matt Bauman <mbauman@juliahub.com>
2025-10-24 15:20:40 -0400

    make failures non-fatal in msan builds

--- M/MbedTLS/common.jl
+++ M/MbedTLS/common.jl
@@ -96,7 +96,7 @@ cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
 make -j${nproc}
 
 if [[ "${BB_RUN_TESTS}" ]]; then
-    make test ARGS="--output-on-failure"
+    make test ARGS="--output-on-failure" || [[ "${target}" == *-sanitize+memory ]]
 fi
 
 make install
