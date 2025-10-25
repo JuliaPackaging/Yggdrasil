e46f02e5a71537bb1902ce58ece083b566c01c6f
Viral B. Shah <ViralBShah@users.noreply.github.com>
2025-10-25 12:00:03 -0400

    Bump compat to julia 1.10

--- D/Doom/build_tarballs.jl
+++ D/Doom/build_tarballs.jl
@@ -47,4 +47,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` file.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"5")
+               julia_compat="1.10", preferred_gcc_version=v"5")
