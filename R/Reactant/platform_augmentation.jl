d3a7f8407a72055e3becd137f98b343930059fcb
William Moses <me@wsmoses.com>
2025-10-26 14:15:18 -0500

    Update platform_augmentation.jl

--- R/Reactant/platform_augmentation.jl
+++ R/Reactant/platform_augmentation.jl
@@ -30,7 +30,7 @@ else
 end
 
 const cuda_version_preference = if haskey(preferences, "cuda_version")
-    expected = ("none", "12.8", "13.0")
+    expected = ("none", "12.9", "13.0")
     if isa(preferences["cuda_version"], String) && preferences["cuda_version"] in expected
         preferences["cuda_version"]
     else
@@ -94,8 +94,8 @@ function augment_platform!(platform::Platform)
 
             if cuda_version_tag == "none" && current_cuda_version isa VersionNumber
                 if v"12" <= current_cuda_version < v"13"
-                    cuda_version_tag = "12.8"
-                elseif v"13.0" <= current_cuda_version < v"14"
+                    cuda_version_tag = "12.9"
+                elseif v"13.0" <= current_cuda_version < v"14" && arch(platform) == "x86_64"
                     cuda_version_tag = "13.0"
                 else
                     @warn "CUDA version $(current_cuda_version) in $(path) not supported with this version of Reactant (min supported: 12)"
