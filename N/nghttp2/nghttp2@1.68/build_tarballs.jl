757b58942ad1833618c903e00bec1f24890ed6f5
Erik Schnetter <schnetter@gmail.com>
2025-10-27 12:36:31 -0400

    nghtt2: Build again, this time actually version 1.68.0

--- N/nghttp2/nghttp2@1.68/build_tarballs.jl
+++ N/nghttp2/nghttp2@1.68/build_tarballs.jl
@@ -5,4 +5,4 @@ include("../common.jl")
 build_tarballs(ARGS, configure_nghttp2_build(version)...;
                julia_compat="1.6", preferred_llvm_version=llvm_version)
 
-# Build trigger: 1
+# Build trigger: 2
