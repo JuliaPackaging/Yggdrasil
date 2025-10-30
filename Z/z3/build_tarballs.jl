1a7c2677ef2bf173127291147c493a4141e93540
Erik Schnetter <schnetter@gmail.com>
2025-10-30 10:25:35 -0400

    z3: Update to 4.15.4

--- Z/z3/build_tarballs.jl
+++ Z/z3/build_tarballs.jl
@@ -2,18 +2,13 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-# See https://github.com/JuliaLang/Pkg.jl/issues/2942
-# Once this Pkg issue is resolved, this must be removed
-uuid = Base.UUID("a83860b7-747b-57cf-bf1f-3e79990d037f")
-delete!(Pkg.Types.get_last_stdlibs(v"1.6.3"), uuid)
-
 name = "z3"
-version = v"4.15.3"
+version = v"4.15.4"
 
 # Collection of sources required to complete build
 sources = [
     ArchiveSource("https://github.com/Z3Prover/z3/releases/download/z3-$(version)/z3_solver-$(version).0.tar.gz",
-                  "78f69aebda5519bfd8af146a129f36cf4721a3c2667e80d9fe35cc9bb4d214a6"),
+                  "928c29b58c4eb62106da51c1914f6a4a55d0441f8f48a81b9da07950434a8946"),
     FileSource("https://github.com/phracker/MacOSX-SDKs/releases/download/10.15/MacOSX10.15.sdk.tar.xz",
                "2408d07df7f324d3beea818585a6d990ba99587c218a3969f924dfcc4de93b62"),
 ]
