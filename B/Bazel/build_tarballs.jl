955ff0b7db83dafee881dc66528ad3faf6c4d7f0
William Moses <me@wsmoses.com>
2025-10-30 17:00:18 -0500

    Update Bazel source checksum for version 7.7.0

--- B/Bazel/build_tarballs.jl
+++ B/Bazel/build_tarballs.jl
@@ -4,7 +4,7 @@ name = "Bazel"
 version = v"7.7.0"
 sources = [
     ArchiveSource("https://github.com/adoptium/temurin21-binaries/releases/download/jdk-21.0.7%2B6/OpenJDK21U-jdk_x64_alpine-linux_hotspot_21.0.7_6.tar.gz", "79ecc4b213d21ae5c389bea13c6ed23ca4804a45b7b076983356c28105580013"),
-    ArchiveSource("https://github.com/bazelbuild/bazel/releases/download/$(version)/bazel-$(version)-dist.zip", "79da863df05fa4de79a82c4f9d4e710766f040bc519fd8b184a4d4d51345d5ba"),
+    ArchiveSource("https://github.com/bazelbuild/bazel/releases/download/$(version)/bazel-$(version)-dist.zip", "277946818c77fff70be442864cecc41faac862b6f2d0d37033e2da0b1fee7e0f"),
 
 ]
 
