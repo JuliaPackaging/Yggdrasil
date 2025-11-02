ae3fdea9f80ffd8ee18e66bf8a29b380beb98d69
tz-lom <nuzhdin.urii@gmail.com>
2025-11-02 22:27:05 +0100

    Add ability to add new licenses

--- L/licensecheck/bundled/main.go
+++ L/licensecheck/bundled/main.go
@@ -5,12 +5,16 @@ import (
 	"github.com/google/licensecheck"
 
 	"unsafe"
+	"log"
 )
+
+var _licenses []licensecheck.License;
+var _scanner *licensecheck.Scanner = nil;
 
 //export License
 func License(msg *C.char) (**C.char, int, float64) {
 	bytes := []byte(C.GoString(msg))
-	cov := licensecheck.Scan(bytes)
+	cov := _scanner.Scan(bytes)
 
 	// https://stackoverflow.com/a/41493208
 	cArray := C.malloc(C.size_t(len(cov.Match)) * C.size_t(unsafe.Sizeof(uintptr(0))))
@@ -24,4 +28,44 @@ func License(msg *C.char) (**C.char, int, float64) {
 	return (**C.char)(cArray), len(cov.Match), cov.Percent
 }
 
-func main() {}
+//export ResetToBuiltinLicences
+func ResetToBuiltinLicences() {
+	_licenses = licensecheck.BuiltinLicenses()
+	rebuildScanner()
+}
+
+//export AddBuiltinLicense
+func AddBuiltinLicense(name *C.char) {
+	sname := C.GoString(name)
+
+	for _, license := range licensecheck.BuiltinLicenses() {
+        if license.ID == sname {
+            _licenses = append(_licenses, license)
+			break;
+        }
+    }
+	rebuildScanner()
+}
+
+//export AddLicense
+func AddLicense(cID *C.char, /*cType *C.char,*/ cLRE *C.char, cURL *C.char) {
+	// type, err := licensecheck.ParseType(C.GoString(cType))
+	// if err != nil {
+	// 	type = licensecheck.Unknown
+	// }
+	_licenses = append(_licenses, licensecheck.License{
+		C.GoString(cID), licensecheck.Unknown, C.GoString(cLRE), C.GoString(cURL)})
+	rebuildScanner()
+}
+
+func rebuildScanner() {
+	var err any;
+	_scanner, err = licensecheck.NewScanner(_licenses)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	ResetToBuiltinLicences()
+}
