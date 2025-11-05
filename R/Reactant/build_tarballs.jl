31d3e54250b6e36989ca3b9ca416ef71b258a592
William S. Moses <gh@wsmoses.com>
2025-11-04 23:42:06 -0600

    fix

--- R/Reactant/build_tarballs.jl
+++ R/Reactant/build_tarballs.jl
@@ -58,6 +58,7 @@ if [[ "${bb_full_target}" == *gpu+rocm* ]]; then
     apk add zlib-dev
     mv /workspace/srcdir/lib/libhiprtc-builtins.so.6.5.25281-42077334f /workspace/srcdir/lib/libhiprtc-builtins.so.6.5.25281
     mv /workspace/srcdir/lib/libhiprtc.so.6.5.25281-42077334f /workspace/srcdir/lib/libhiprtc.so.6.5.25281
+    mv $ROCM_PATH/lib/libamdhip64.so.6.5.25281-42077334f $ROCM_PATH/lib/libamdhip64.so.6.5.25281
     ln -s $ROCM_PATH/lib/llvm/amdgcn $ROCM_PATH/amdgcn
     mv $ROCM_PATH/bin/hipcc{,.real}
     cp `which clang` $ROCM_PATH/bin/hipcc
