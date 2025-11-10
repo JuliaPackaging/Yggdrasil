5c8a3c1b2e5b0c89b4b505a6fda436fad9390993
Abraham Jaimes <60442066+AbrJA@users.noreply.github.com>
2025-11-10 09:01:57 -0800

    Mongoose: Adding a patch and simplifying the build (#12500)
    * Adding a patch for the library
    
    * using LIBS to simply the logic
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>
    
    * Adding LIBS var
    
    * Bug fix for LIBS var
    
    ---------
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- M/Mongoose/build_tarballs.jl
+++ M/Mongoose/build_tarballs.jl
@@ -7,35 +7,20 @@ version = v"7.19.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/cesanta/mongoose.git", "739d78a45a8fa6595498df67d11cac3e39af8c0e")
+    GitSource("https://github.com/cesanta/mongoose.git", "739d78a45a8fa6595498df67d11cac3e39af8c0e"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cat > CMakeLists.txt <<EOF
-cmake_minimum_required(VERSION 3.10)
-project(mongoose C)
+mkdir -p ${libdir}
+LIBS=""
+if [[ "${target}" == *mingw* ]]; then
+    LIBS="-lws2_32"
+fi
+cc -fPIC -O2 -shared mongoose/mongoose.c -o ${libdir}/libmongoose.${dlext} ${LIBS}
 
-add_library(mongoose SHARED mongoose/mongoose.c)
-
-if(WIN32)
-    target_link_libraries(mongoose PRIVATE ws2_32)
-endif()
-
-install(TARGETS mongoose
-        LIBRARY DESTINATION $libdir
-        ARCHIVE DESTINATION $libdir
-        RUNTIME DESTINATION $bindir)
-EOF
-
-cmake -B build  \
-    -DCMAKE_INSTALL_PREFIX=${prefix}  \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}  \
-    -DCMAKE_BUILD_TYPE=Release  \
-    -S .
-cmake --build build --parallel ${nproc}
-cmake --install build
 install_license mongoose/LICENSE
 """
 
