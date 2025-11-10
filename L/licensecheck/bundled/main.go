723e0430d6a142b62203a9d3d87758f266fd8936
tz-lom <nuzhdin.urii@gmail.com>
2025-11-10 22:11:33 +0100

    Fix memory issues

--- L/licensecheck/bundled/main.go
+++ L/licensecheck/bundled/main.go
@@ -7,16 +7,21 @@ import "C"
 import (
 	"github.com/google/licensecheck"
 
-	"unsafe"
 	"log"
+	"sync"
+	"unsafe"
 )
 
-var _licenses []licensecheck.License;
-var _scanner *licensecheck.Scanner = nil;
-var _result unsafe.Pointer = nil;
+var (
+	_licenses []licensecheck.License
+	_scanner  *licensecheck.Scanner = nil
+	_mu       sync.Mutex
+)
 
 //export License
 func License(msg *C.char) (**C.char, int, float64) {
+	_mu.Lock()
+	defer _mu.Unlock()
 
 	if len(_licenses) == 0 {
 		// _scanner do now work if there are no licenses, work-around such case
@@ -26,56 +31,76 @@ func License(msg *C.char) (**C.char, int, float64) {
 	bytes := []byte(C.GoString(msg))
 	cov := _scanner.Scan(bytes)
 
-	// assuming Julia made a copy of the result, we can safely free prev. buffer and populate a new one
-	if _result != nil {
-		C.free(_result)
-	}
-	// https://stackoverflow.com/a/41493208
-	_result := C.malloc(C.size_t(len(cov.Match)) * C.size_t(unsafe.Sizeof(uintptr(0))))
-	// mimic https://github.com/docker/docker-credential-helpers/pull/61
-	a := (*[(1 << 29) - 1]*C.char)(_result)
+	// Collect IDs from each match into a slice of *C.char
+	cArray := C.malloc(C.size_t(len(cov.Match)) * C.size_t(unsafe.Sizeof(uintptr(0))))
 
-	for idx, m := range cov.Match {
-		a[idx] = C.CString(m.ID)
+	ids := (*[1<<30 - 1]*C.char)(cArray)[0:len(cov.Match)]
+	for i, m := range cov.Match {
+		ids[i] = C.CString(m.ID)
 	}
 
-	return (**C.char)(_result), len(cov.Match), cov.Percent
+	return (**C.char)(cArray), len(ids), cov.Percent
+}
+
+//export FreeLicenseResult
+func FreeLicenseResult(result **C.char, length C.int) {
+	if result == nil {
+		return
+	}
+	// Convert **C.char to a slice so we can index it
+	slice := (*[1<<30 - 1]*C.char)(unsafe.Pointer(result))[0:int(length)]
+	for i := 0; i < int(length); i++ {
+		C.free(unsafe.Pointer(slice[i]))
+	}
+	C.free(unsafe.Pointer(result))
 }
 
 //export ClearLicenseList
 func ClearLicenseList() {
-	_licenses = []licensecheck.License{};
+	_mu.Lock()
+	defer _mu.Unlock()
+
+	_licenses = []licensecheck.License{}
 	rebuildScanner()
 }
 
 //export ResetToBuiltinLicences
 func ResetToBuiltinLicences() {
+	_mu.Lock()
+	defer _mu.Unlock()
+
 	_licenses = licensecheck.BuiltinLicenses()
 	rebuildScanner()
 }
 
 //export AddBuiltinLicense
 func AddBuiltinLicense(name *C.char) {
+	_mu.Lock()
+	defer _mu.Unlock()
+
 	sname := C.GoString(name)
 
 	for _, license := range licensecheck.BuiltinLicenses() {
-        if license.ID == sname {
-            _licenses = append(_licenses, license)
-			break;
-        }
-    }
+		if license.ID == sname {
+			_licenses = append(_licenses, license)
+			break
+		}
+	}
 	rebuildScanner()
 }
 
 //export AddLicense
 func AddLicense(cID *C.char, cLRE *C.char) {
+	_mu.Lock()
+	defer _mu.Unlock()
+
 	_licenses = append(_licenses, licensecheck.License{
 		C.GoString(cID), licensecheck.Unknown, C.GoString(cLRE), ""})
 	rebuildScanner()
 }
 
 func rebuildScanner() {
-	var err any;
+	var err any
 	_scanner, err = licensecheck.NewScanner(_licenses)
 	if err != nil {
 		log.Fatal(err)
