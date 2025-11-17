52144e4026f5d8fa7ac6b00918abb547ad5b28e9
ejmeitz <emeitz@andrew.cmu.edu>
2025-11-17 17:04:15 -0600

    sigh

--- C/cunumeric_jl_wrapper/build_tarballs.jl
+++ C/cunumeric_jl_wrapper/build_tarballs.jl
@@ -9,7 +9,7 @@ include("make_script.jl")
 name = "cunumeric_jl_wrapper"
 version = v"25.10" 
 sources = [
-    GitSource("https://github.com/JuliaLegate/cunumeric_jl_wrapper.git","3c1f48f9ef64dc5c96cb89bfee87c1e1a062eeae"),
+    GitSource("https://github.com/JuliaLegate/cunumeric_jl_wrapper.git","01cbfd3116feaac20bbcec848520dd91571459cc"),
 ]
 
 MIN_JULIA_VERSION = v"1.10"
