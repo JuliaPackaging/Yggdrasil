d690f48e1f4d9396675a5e0ef27427dfa823d193
Jim Garrison <jim@garrison.cc>
2025-11-17 00:04:41 -0500

    Update build_tarballs.jl

--- Q/qiskit_ibm_runtime/build_tarballs.jl
+++ Q/qiskit_ibm_runtime/build_tarballs.jl
@@ -36,10 +36,10 @@ install_license LICENSE.txt
 platforms = [
     Platform("x86_64", "linux"; libc = "glibc"),
     Platform("x86_64", "linux"; libc = "musl"),
-#    Platform("aarch64", "linux"; libc = "glibc"),
-#    Platform("aarch64", "linux"; libc = "musl"),
-#    Platform("x86_64", "macos"),
-#    Platform("aarch64", "macos"),
+    Platform("aarch64", "linux"; libc = "glibc"),
+    Platform("aarch64", "linux"; libc = "musl"),
+    Platform("x86_64", "macos"),
+    Platform("aarch64", "macos"),
 ]
 
 
