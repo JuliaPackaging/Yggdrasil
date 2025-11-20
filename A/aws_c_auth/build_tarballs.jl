9f8143c3d7cda804d6b1a13b154ab1099c2af865
Mosè Giordano <765740+giordano@users.noreply.github.com>
2025-11-20 17:53:36 -0600

    Apply suggestion

--- A/aws_c_auth/build_tarballs.jl
+++ A/aws_c_auth/build_tarballs.jl
@@ -36,7 +36,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("aws_c_cal_jll"; compat="0.9.9"),
+    Dependency("aws_c_cal_jll"; compat="0.9.11"),
     Dependency("aws_c_http_jll"; compat="0.10.7"),
     Dependency("aws_c_sdkutils_jll"; compat="0.2.4"),
     BuildDependency("aws_lc_jll"),
