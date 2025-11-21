017659fa9d6ea79bcbab1e48f5789fd94daa012d
Hans Schoenemann <hannes@mathematik.uni-kl.de>
2025-11-21 12:11:49 +0100

    [Singular] fix for OSCAR issue 5571 (#12588)

--- S/Singular/build_tarballs.jl
+++ S/Singular/build_tarballs.jl
@@ -29,7 +29,7 @@ import Pkg.Types: VersionSpec
 name = "Singular"
 
 upstream_version = v"4.4.1-5" # 4.4.1p5
-version_offset = v"0.1.0"
+version_offset = v"0.1.1"
 
 version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + version_offset.major,
                         upstream_version.patch * 100 + version_offset.minor,
@@ -37,7 +37,7 @@ version = VersionNumber(upstream_version.major * 100 + upstream_version.minor + 
 
 # Collection of sources required to build Singular
 sources = [
-    GitSource("https://github.com/Singular/Singular.git", "7ab5247be80f25ae5ab5a4f4dae59fdd4d4e4782"),
+    GitSource("https://github.com/Singular/Singular.git", "bceb01c7426b42d1f0a176b6f3e2af31196ca070"),
     #ArchiveSource("https://www.mathematik.uni-kl.de/ftp/pub/Math/Singular/SOURCES/$(upstream_version.major)-$(upstream_version.minor)-$(upstream_version.patch)/singular-$(upstream_version).tar.gz",
     #              "5b0f6c036b4a6f58bf620204b004ec6ca3a5007acc8352fec55eade2fc9d63f6"),
     #DirectorySource("./bundled")
