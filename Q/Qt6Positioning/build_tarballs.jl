63d01a37fc235f4f55e70e5f9a06cfbe236cc442
Max Horn <max@quendi.de>
2025-11-24 22:44:04 +0100

    more

--- Q/Qt6Positioning/build_tarballs.jl
+++ Q/Qt6Positioning/build_tarballs.jl
@@ -46,21 +46,13 @@ install_license $WORKSPACE/srcdir/qt*-src-*/LICENSES/LGPL-3.0-only.txt
 
 sources, script = require_macos_sdk("14.0", sources, script; deployment_target="12")
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = expand_cxxstring_abis(filter(!Sys.isapple, supported_platforms()))
-filter!(p -> arch(p) != "armv6l", platforms) # No OpenGL on armv6
-platforms_macos = [ Platform("x86_64", "macos"), Platform("aarch64", "macos") ]
+# Get the common Qt platforms
+include("../Qt6Base/common.jl")
 
 # The products that we will ensure are always built
 products = [
     LibraryProduct(["Qt6Positioning", "libQt6Positioning", "QtPositioning"], :libqt6positioning),
     LibraryProduct(["Qt6PositioningQuick", "libQt6PositioningQuick", "QtPositioningQuick"], :libqt6positioningquick),
-]
-
-products_macos = [
-    FrameworkProduct("QtPositioning", :libqt6positioning),
-    FrameworkProduct("QtPositioningQuick", :libqt6positioningquick),
 ]
 
 # We must use the same version of LLVM for the build toolchain and LLVMCompilerRT_jll
@@ -69,19 +61,10 @@ llvm_version = v"16.0.6"
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    BuildDependency(PackageSpec(name="LLVM_full_jll", version=llvm_version)),
-    BuildDependency(PackageSpec(name="LLVMCompilerRT_jll", uuid="4e17d02c-6bf5-513e-be62-445f41c75a11", version=llvm_version);
-                    platforms=filter(p -> Sys.isapple(p), platforms_macos)),
     HostBuildDependency("Qt6Base_jll"),
     HostBuildDependency("Qt6Declarative_jll"),
     Dependency("Qt6Base_jll"; compat="="*string(version)),
     Dependency("Qt6Declarative_jll"; compat="="*string(version)),
 ]
 
-if any(should_build_platform.(triplet.(platforms_macos)))
-    build_tarballs(ARGS, name, version, sources, script, platforms_macos, products_macos, dependencies; preferred_gcc_version = v"10", julia_compat="1.6")
-end
-
-if any(should_build_platform.(triplet.(platforms)))
-    build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version = v"10", julia_compat="1.6")
-end
+build_qt(name, version, sources, script, products, dependencies)
