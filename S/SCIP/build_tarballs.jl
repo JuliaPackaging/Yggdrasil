c12b79c708d0cd49df45943a385a0fc9e703dcd6
matbesancon <mathieu.besancon@gmail.com>
2025-11-24 19:34:51 +0100

    include macos SDK

--- S/SCIP/build_tarballs.jl
+++ S/SCIP/build_tarballs.jl
@@ -2,6 +2,9 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "platforms", "macos_sdks.jl"))
+
 name = "SCIP"
 
 upstream_version = v"10.0.0"
