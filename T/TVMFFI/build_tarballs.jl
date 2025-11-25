03fc7b32053cde1fc47a083ee984f87745bef53f
Gabriel Wu <13583761+lucifer1004@users.noreply.github.com>
2025-11-25 11:44:18 +0800

    Simplify platform filtering for build process

--- T/TVMFFI/build_tarballs.jl
+++ T/TVMFFI/build_tarballs.jl
@@ -29,8 +29,7 @@ install_license LICENSE
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = supported_platforms()
-platforms = filter(!Sys.iswindows, platforms)
+platforms = supported_platforms(exclude=Sys.iswindows)
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
