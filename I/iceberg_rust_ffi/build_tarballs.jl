36c6b7896af6e6bbd30ba585f47913d41d7e64a7
Gerald Berger <gberger.work@gmail.com>
2025-11-25 09:24:57 +0100

    Bump version to 0.4.1

--- I/iceberg_rust_ffi/build_tarballs.jl
+++ I/iceberg_rust_ffi/build_tarballs.jl
@@ -1,10 +1,10 @@
 using BinaryBuilder
 
 name = "iceberg_rust_ffi"
-version = v"0.4.0"
+version = v"0.4.1"
 
 sources = [
-    GitSource("https://github.com/RelationalAI/RustyIceberg.jl.git", "4713dd08b36f511a04b235dc86f2584cb8e3427e"),
+    GitSource("https://github.com/RelationalAI/RustyIceberg.jl.git", "ca3ade070fd1a9be6a5f30ffb7c7afeb057367fc"),
 ]
 
 # Bash recipe for building across all platforms
