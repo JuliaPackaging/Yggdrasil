7d6e8c681bae1da2b29ce3fa72959a850bcd9d6b
Eric Hanson <5846501+ericphanson@users.noreply.github.com>
2025-11-28 15:40:25 +0100

    fix?

--- L/libimagequant/build_tarballs.jl
+++ L/libimagequant/build_tarballs.jl
@@ -22,7 +22,7 @@ export PATH="$CARGO_HOME/bin:$PATH"
 cd $WORKSPACE/srcdir/libimagequant/imagequant-sys
 
 # avoid the compiler wrappers so we can compile cargo-c for the host, not the target
-/opt/$(rust_host)/bin/cargo install --locked cargo-c
+/opt/x86_64-linux-musl/bin/cargo install --locked cargo-c
 cargo cinstall --destdir=${sysroot} --prefix=${prefix} --libdir=${libdir}
 install_license ./COPYRIGHT
 """
