85a4f49711e4b94b8cd903c7381d3c0cc4c92bdd
Jesper Stemann Andersen <jesper@sait.dk>
2025-11-30 14:40:06 +0100

    Replaced bash script with patch for only installing certain components

--- P/ProtocolBuffers/common.jl
+++ P/ProtocolBuffers/common.jl
@@ -7,13 +7,7 @@ sources = [
 
 binary_symbols = [:protoc]
 
-include_dirs = ["google/protobuf"]
-
 include_symbols = Dict{Symbol,String}()
-
-library_dirs = [
-    "cmake/protobuf",
-]
 
 protobuf_library_symbols = Dict(
     :libprotobuf => "protobuf",
@@ -65,24 +59,18 @@ products_map = Dict(
     ),
 )
 
-script = """
-BB_PROTOBUF_BIN_FILES=($(join(["bin/$bin" for bin in binary_symbols], " ")))
-BB_PROTOBUF_INCLUDE_DIRS=($(join(["include/$include_dir" for include_dir in include_dirs], " ")))
-BB_PROTOBUF_INCLUDE_FILES=($(join(["include/$include_file" for include_file in values(all_include_symbols)], " ")))
-BB_PROTOBUF_LIB_DIRS=($(join(["lib/$library_dir" for library_dir in library_dirs], " ")))
-BB_PROTOBUF_LIBRARIES=($(join(values(library_symbols), " ")))
-BB_PROTOBUF_ADDITIONAL_LIBRARIES=($(join(values(additional_library_symbols), " ")))
-BB_PROTOC_LIBRARIES=($(join(values(protoc_library_symbols), " ")))
-""" * raw"""
+script = raw"""
 cd $WORKSPACE/srcdir/protobuf
 
 # Avoid problems with `-march`, `-ffast-math` etc.
 sed -i -e 's!set(CMAKE_C_COMPILER.*!set(CMAKE_C_COMPILER '${WORKSPACE}/srcdir/files/ccsafe')!' ${CMAKE_TARGET_TOOLCHAIN}
 sed -i -e 's!set(CMAKE_CXX_COMPILER.*!set(CMAKE_CXX_COMPILER '${WORKSPACE}/srcdir/files/c++safe')!' ${CMAKE_TARGET_TOOLCHAIN}
 
+atomic_patch -p1 ../patches/protobuf-cmake-install-components.patch
+
 cmake_extra_args=()
 
-if [[ "$BB_PROTOBUF_PRODUCT" == "ProtocolBuffersSDK_static" ]]; then
+if [[ "$BB_PROTOBUF_PRODUCT" == ProtocolBuffersSDK_static ]]; then
     cmake_extra_args+=(
         -DBUILD_SHARED_LIBS=OFF
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON
@@ -93,13 +81,43 @@ else
     )
 fi
 
-if [[ "$BB_PROTOBUF_PRODUCT" == "ProtocolBuffersCompiler" ]]; then
+if [[ "$BB_PROTOBUF_PRODUCT" == ProtocolBuffersCompiler ]]; then
     cmake_extra_args+=(
         -Dprotobuf_BUILD_PROTOC_BINARIES=ON
     )
 else
     cmake_extra_args+=(
         -Dprotobuf_BUILD_PROTOC_BINARIES=OFF
+    )
+fi
+
+if [[ "$BB_PROTOBUF_PRODUCT" == ProtocolBuffers ]]; then
+    cmake_extra_args+=(
+        -Dprotobuf_INSTALL_PROTOBUF=ON
+    )
+else
+    cmake_extra_args+=(
+        -Dprotobuf_INSTALL_PROTOBUF=OFF
+    )
+fi
+
+if [[ "$BB_PROTOBUF_PRODUCT" == ProtocolBuffersLite ]]; then
+    cmake_extra_args+=(
+        -Dprotobuf_INSTALL_PROTOBUF_LITE=ON
+    )
+else
+    cmake_extra_args+=(
+        -Dprotobuf_INSTALL_PROTOBUF_LITE=OFF
+    )
+fi
+
+if [[ "$BB_PROTOBUF_PRODUCT" == ProtocolBuffersSDK* ]]; then
+    cmake_extra_args+=(
+        -Dprotobuf_INSTALL_SDK=ON
+    )
+else
+    cmake_extra_args+=(
+        -Dprotobuf_INSTALL_SDK=OFF
     )
 fi
 
@@ -116,90 +134,12 @@ cmake \
 cmake --build work --parallel $nproc
 cmake --install work
 install_license LICENSE
-
-# Ensure the proper files are in $prefix
-include_dirs_regex=$(IFS='|' ; echo "${BB_PROTOBUF_INCLUDE_DIRS[*]}")
-lib_dirs_regex=$(IFS='|' ; echo "${BB_PROTOBUF_LIB_DIRS[*]}")
-lib_ext_regex="(${dlext}.*|.+\.${dlext})"
-if [[ "$BB_PROTOBUF_PRODUCT" == "ProtocolBuffersSDK_static" ]]; then
-    lib_ext_regex=a
-fi
-
-cd $prefix
-
-if [[ "$BB_PROTOBUF_PRODUCT" == "ProtocolBuffersCompiler" ]]; then
-    bin_files=($(find bin -type f | sort))
-    for bin_file in "${bin_files[@]}"; do
-        match_found=false
-        for bin_name in "${BB_PROTOBUF_BIN_FILES[@]}"; do
-            if [[ "$bin_file" =~ ^${bin_name} ]]; then
-                match_found=true
-                break
-            fi
-        done
-        if ! $match_found; then
-            exit 1
-        fi
-    done
-else
-    [[ "$target" == *-w64-mingw32 ]] || [[ ! -d bin ]] || exit 1
-fi
-
-include_files=($(find include -type f | grep -E -v "^($include_dirs_regex)/" | sort))
-[[ "$(printf "%s\n" ${include_files[@]})" == "$(printf "%s\n" "${BB_PROTOBUF_INCLUDE_FILES[@]}" | sort)" ]] || exit 1
-
-lib_files=($(find lib -type f | grep -E -v "^($lib_dirs_regex)/" | sort))
-for lib_file in "${lib_files[@]}"; do
-    match_found=false
-    for lib_name in "${BB_PROTOBUF_LIBRARIES[@]}"; do
-        if $(echo "$lib_file" | grep -q -E "^lib/lib${lib_name}\.${lib_ext_regex}") || [[  "$lib_file" =~ ^lib/pkgconfig/${lib_name}.pc ]]; then
-            match_found=true
-            break
-        fi
-    done
-    for lib_name in "${BB_PROTOBUF_ADDITIONAL_LIBRARIES[@]}"; do
-        if [[ "$lib_file" =~ ^lib/lib${lib_name}.a ]]; then
-            match_found=true
-            break
-        fi
-    done
-    for lib_name in "${BB_PROTOC_LIBRARIES[@]}"; do
-        if $(echo "$lib_file" | grep -q -E "^lib/lib${lib_name}\.${lib_ext_regex}"); then
-            match_found=true
-            break
-        fi
-    done
-    if ! $match_found; then
-        exit 1
-    fi
-done
-
-# Ensure the proper files are in each tarball
-if [[ "$BB_PROTOBUF_PRODUCT" =~ ^ProtocolBuffers(Lite|Compiler)?$ ]]; then
-    rm -frv "${BB_PROTOBUF_INCLUDE_DIRS[@]}"
-    rm -fv "${BB_PROTOBUF_INCLUDE_FILES[@]}"
-    find include -type d | xargs rmdir --ignore-fail-on-non-empty -p
-
-    rm -frv "${BB_PROTOBUF_LIB_DIRS[@]}"
-    rm -fv $(for lib_name in "${BB_PROTOBUF_LIBRARIES[@]}"; do echo lib/pkgconfig/${lib_name}.pc; done)
-    rm -fv $(for lib_name in "${BB_PROTOBUF_ADDITIONAL_LIBRARIES[@]}"; do echo lib/lib${lib_name}.a; done)
-    find lib -type d | xargs rmdir --ignore-fail-on-non-empty -p
-elif [[ "$BB_PROTOBUF_PRODUCT" == "ProtocolBuffersSDK" ]]; then
-    rm -fv $(for lib_name in "${BB_PROTOBUF_LIBRARIES[@]}"; do echo lib/lib${lib_name}.${dlext}*; done)
-fi
-
-if [[ "$BB_PROTOBUF_PRODUCT" == "ProtocolBuffers" ]]; then
-    rm -fv lib/libprotobuf-lite.${dlext}*
-elif [[ "$BB_PROTOBUF_PRODUCT" == "ProtocolBuffersLite" ]]; then
-    rm -fv lib/libprotobuf.${dlext}*
-elif [[ "$BB_PROTOBUF_PRODUCT" == "ProtocolBuffersCompiler" ]]; then
-    rm -fv $(for lib_name in "${BB_PROTOBUF_LIBRARIES[@]}"; do echo lib/lib${lib_name}.${dlext}*; done)
-fi
 """
 
 platforms = expand_cxxstring_abis(supported_platforms())
 
 dependencies = [
+    Dependency("CompilerSupportLibraries_jll"),
     Dependency("Zlib_jll"),
 ]
 
