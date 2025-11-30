446f5b0af39575cee81ec1ec0e9400c1cdd519fc
Jesper Stemann Andersen <jesper@sait.dk>
2025-11-30 17:19:58 +0100

    Added ProtocolBuffers (#9794)
    Added ProtocolBuffers recipes - as a replacement for protoc.
    
    Packaging v3.16.0 used by ONNX v1.11.0 (and in turn, Torch v1.12.1).
    
    Runtime libraries:
    * ProtocolBuffers, i.e., `libprotobuf`.
    * ProtocolBuffersLite, i.e., `libprotobuf-lite`.
    
    Compiler:
    * ProtocolBuffersCompiler, i.e., `protoc` and `libprotoc`, includes `libprotobuf` on which `libprotoc` depends.
    
    SDKs (including both runtime libraries and the compiler):
    * ProtocolBuffersSDK - headers, png-config, cmake files
    * ProtocolBuffersSDK_static - static libraries as well as SDK
    
    * Added omitted Zlib_jll dependency
    
    * GCC 8 should be sufficient
    
    * Even safer ccsafe

---
+++ P/ProtocolBuffers/ProtocolBuffersSDK_static/build_tarballs.jl
@@ -0,0 +1,17 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+include(joinpath(@__DIR__, "..", "common.jl"))
+
+name = "ProtocolBuffersSDK_static"
+
+script = """
+export BB_PROTOBUF_PRODUCT=$name
+""" *
+script
+
+products = products_map[name]
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat, preferred_gcc_version)
