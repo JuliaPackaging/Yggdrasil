b0e8a6510bb890e2426c5a2152aac73e1b858419
Mosè Giordano <m.giordano@ucl.ac.uk>
2025-12-02 16:48:44 +0000

    [GnuTLS] Require GCC 11
    This is required for `__has_c_attribute` support in C23:
    https://en.cppreference.com/w/c/compiler_support.html

--- G/GnuTLS/build_tarballs.jl
+++ G/GnuTLS/build_tarballs.jl
@@ -65,4 +65,4 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               clang_use_lld=false, julia_compat="1.6", lock_microarchitecture=false, preferred_gcc_version=v"6")
+               clang_use_lld=false, julia_compat="1.6", lock_microarchitecture=false, preferred_gcc_version=v"11")
