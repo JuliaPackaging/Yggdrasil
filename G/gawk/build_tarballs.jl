fcb89325d05dc74f0059dc6c35b4b0b8b0814b35
Erik Schnetter <schnetter@gmail.com>
2025-12-05 12:21:00 -0500

    gawk: New version 5.3.2 (#12707)
    * gawk: New version 5.3.2
    
    * gawk: Use cache.julialang.org
    
    * gawk: Download from savannah
    
    * gawk: Use a GitSource
    
    * gawk: Correct macOS builds

--- G/gawk/build_tarballs.jl
+++ G/gawk/build_tarballs.jl
@@ -1,17 +1,22 @@
 using BinaryBuilder
 
 name = "gawk"
-version = v"5.3.1"
+version = v"5.3.2"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://ftp.gnu.org/gnu/gawk/gawk-$(version).tar.xz",
-                  "694db764812a6236423d4ff40ceb7b6c4c441301b72ad502bb5c27e00cd56f78")
+    GitSource("https://git.savannah.gnu.org/git/gawk.git", "027ebff56b83600d88fd303b98c36201116a7560"),
+    DirectorySource("bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/gawk*/
+
+# Add missing #include for `_NSGetExecutablePath`
+atomic_patch -p1 $WORKSPACE/srcdir/patches/gawk_nsgep.patch
+
+apk add texinfo
 
 CONFIGURE_ARGS=()
 if [[ ${target} == aarch64-apple-darwin* ]]; then
