d4ff6eb1178f90752512c4ce291f1eecbadfe127
Jesper Stemann Andersen <jesper@sait.dk>
2025-12-06 09:46:22 +0100

    windows/kineto fix

--- T/Torch/build_tarballs.jl
+++ T/Torch/build_tarballs.jl
@@ -129,7 +129,7 @@ else
     cmake_extra_args+=(-DUSE_TENSORPIPE=ON)
 fi
 
-if [[ $target == *-w64-mingw32-* # Fails to compile: third_party/kineto/libkineto/src/ThreadUtil.cpp:6:10: fatal error: sys/syscall.h: No such file or directory
+if [[ $target == *-w64-mingw32* # Fails to compile: third_party/kineto/libkineto/src/ThreadUtil.cpp:6:10: fatal error: sys/syscall.h: No such file or directory
     || $target == *-freebsd* # Fails to compile: third_party/kineto/libkineto/src/ThreadUtil.cpp:52:32: error: use of undeclared identifier 'SYS_gettid'
 ]]; then
     cmake_extra_args+=(-DUSE_KINETO=OFF)
