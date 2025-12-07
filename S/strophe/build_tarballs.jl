f238127fc0ee1b8bdd14e8f990e18954347d21e8
Hugo Levy-Falk <hugo@klafyvel.me>
2025-12-07 18:45:39 +0100

    [libstrophe] Add libstrophe 0.14.0 to Yggdrasil
    This excludes Windows platforms as cross-compilation using mingw fails. See: https://github.com/strophe/libstrophe/issues/264

---
+++ S/strophe/build_tarballs.jl
@@ -0,0 +1,37 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+
+name = "libstrophe"
+version = v"0.14.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/strophe/libstrophe.git", "b852c3e957b408cf857e920c8b3d854c6004a721"),
+]
+
+# Script template (structure is the same for all platforms, directory for make differs)
+# We need to do some plumbing with _MSC_VER because it is not defined when using mingw.
+# We also cherry-pick a commit that fixes Windows builds.
+script = raw"""
+cd $WORKSPACE/srcdir/libstrophe
+./bootstrap.sh
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-static --disable-examples
+make -j${nproc}
+make install
+"""
+
+platforms = [p for p in supported_platforms() if !(triplet(p) in ["x86_64-w64-mingw32", "i686-w64-mingw32"])]
+
+products = [
+    LibraryProduct("libstrophe", :libstrophe)
+]
+
+dependencies = [
+    Dependency("Expat_jll")
+    Dependency("OpenSSL_jll")
+    Dependency("Zlib_jll")
+]
+
+
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"6")
