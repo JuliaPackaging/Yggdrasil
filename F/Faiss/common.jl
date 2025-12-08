014ce210712c2ef8d720fad5bf2f0abadddb0e3f
Jesper Stemann Andersen <jesper@sait.dk>
2025-12-08 10:52:45 +0100

    Fixed PackageSpec version/String

--- F/Faiss/common.jl
+++ F/Faiss/common.jl
@@ -101,5 +101,5 @@ dependencies = [
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"); platforms=filter(!Sys.isbsd, platforms)),
     Dependency(PackageSpec(name="LLVMOpenMP_jll", uuid="1d63c593-3942-5779-bab2-d838dc0a180e"); platforms=filter(Sys.isbsd, platforms)),
     Dependency("libblastrampoline_jll"; compat="5.4"),
-    HostBuildDependency(PackageSpec("CMake_jll", v"3.30.2")),
+    HostBuildDependency(PackageSpec(name="CMake_jll", version="3.30.2")),
 ]
