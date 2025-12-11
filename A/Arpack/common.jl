d95c01dbb6eb35434c05e36e84036accccef7301
Viral B. Shah <viral@mayin.org>
2025-12-11 22:47:33 +0000

    fix riscv64

--- A/Arpack/common.jl
+++ A/Arpack/common.jl
@@ -99,5 +99,5 @@ platforms = expand_gfortran_versions(supported_platforms())
 # Dependencies that must be installed before this package can be built
 dependencies = [
     Dependency("CompilerSupportLibraries_jll"),
-    Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"), compat="5.11.0"),
+    Dependency(PackageSpec(name="libblastrampoline_jll", uuid="8e850b90-86db-534c-a0d3-1478176c7d93"), compat="5.12"),
 ]
