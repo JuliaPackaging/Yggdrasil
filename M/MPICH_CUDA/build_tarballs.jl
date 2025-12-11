8d96ed80628820d953904898318ff7709f6dcfa4
Erik Schnetter <schnetter@gmail.com>
2025-12-11 09:42:23 -0500

    MPICH_CUDA: New version 4.3.2 (#12766)

--- M/MPICH_CUDA/build_tarballs.jl
+++ M/MPICH_CUDA/build_tarballs.jl
@@ -6,12 +6,12 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 
 
 name = "MPICH_CUDA"
-version = v"4.3.1"
+version = v"4.3.2"
 
 sources = [
     ArchiveSource("https://www.mpich.org/static/downloads/$(version)/mpich-$(version).tar.gz",
-                  "acc11cb2bdc69678dc8bba747c24a28233c58596f81f03785bf2b7bb7a0ef7dc")
-        ]
+                  "47d774587a7156a53752218c811c852e70ac44db9c502dc3f399b4cb817e3818"),
+]
 
 script = raw"""
 ################################################################################
