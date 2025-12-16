896646fb7a6c8cccf51acef8db1f57af12971129
Tim Besard <tim.besard@gmail.com>
2025-12-16 14:51:13 +0100

    Fix platform mismatch.

--- C/CUDA/CUDA_SDK_static@13.0/build_tarballs.jl
+++ C/CUDA/CUDA_SDK_static@13.0/build_tarballs.jl
@@ -13,13 +13,6 @@ platforms = [Platform("x86_64", "linux"),
              Platform("aarch64", "linux"),
              Platform("x86_64", "windows")]
 
-full_platforms = Platform[]
-for platform in platforms
-    augmented_platform = deepcopy(platform)
-    augmented_platform["cuda"] = "$(version.major)"
-    push!(full_platforms, augmented_platform)
-end
-
-build_sdk(name, version, full_platforms; static=true)
+build_sdk(name, version, platforms; static=true)
 
 # bump
