cd6eef58c126206d7a337db2eead4b112c748c0c
Gabriel Wu <13583761+lucifer1004@users.noreply.github.com>
2025-12-28 23:01:11 +0800

    wigxjpf: update to 1.13

--- W/Wigxjpf/build_tarballs.jl
+++ W/Wigxjpf/build_tarballs.jl
@@ -3,44 +3,88 @@
 using BinaryBuilder, Pkg
 
 name = "Wigxjpf"
-version = v"1.11.0"
+version_string = "1.13"
+version = VersionNumber(version_string)
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("http://fy.chalmers.se/subatom/wigxjpf/wigxjpf-latest.tar.gz", "5d078bbbf87c917d0df3c5e2204cbd8a51041517630ac84bdca728611ea2f12f")
+    ArchiveSource(
+        "http://fy.chalmers.se/subatom/wigxjpf/wigxjpf-$version_string.tar.gz",
+        "90ab9bfd495978ad1fdcbb436e274d6f4586184ae290b99920e5c978d64b3e6a",
+    ),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/wigxjpf-1.11
+cd $WORKSPACE/srcdir/wigxjpf-*
 
-mkdir -p $WORKSPACE/destdir/lib
-mkdir -p $WORKSPACE/destdir/bin
-mkdir -p $WORKSPACE/destdir/shared/licenses
+# Fix and enhance CMakeLists.txt
+atomic_patch -p1 ../patches/cmake_build.patch
 
-if [ "$(uname)" == "Darwin" ]; then
-    make lib/libwigxjpf_shared.dylib
-    cp lib/libwigxjpf_shared.dylib $WORKSPACE/destdir/lib/  
-else
-    make lib/libwigxjpf_shared.so
-    cp lib/libwigxjpf_shared.so $WORKSPACE/destdir/lib/
-    cp lib/libwigxjpf_shared.so $WORKSPACE/destdir/bin/libwigxjpf_shared.dll
-fi
+# Build with CMake
+cmake -S . -B build -G Ninja \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DCMAKE_INSTALL_LIBDIR=${libdir} \
+    -DCMAKE_INSTALL_BINDIR=${bindir}
 
-cp README $WORKSPACE/destdir/shared/licenses/LICENSE
+cmake --build build
+cmake --install build
+install_license COPYING COPYING.LESSER
 """
 
-# These are the platforms we will build for by default, unless further
-# platforms are passed in on the command line
-platforms = supported_platforms(;experimental=true)
-
-# The products that we will ensure are always built
-products = [
-    LibraryProduct("libwigxjpf_shared", :wigxjpf)
+# Dependencies that must be installed before this package can be built
+dependencies = [
+    Dependency(
+        PackageSpec(
+            name = "CompilerSupportLibraries_jll",
+            uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae",
+        ),
+    ),
 ]
 
-# Dependencies that must be installed before this package can be built
-dependencies = [Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae"))]
+# Split platforms based on quadmath support
+platforms = supported_platforms(; experimental = true)
 
-# Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+# Platforms with quadmath support: x86 and x86_64
+platforms_with_quadmath = filter(p -> arch(p) in [:i686, :x86_64], platforms)
+
+# Platforms without quadmath support: all others
+platforms_without_quadmath = filter(p -> !(arch(p) in [:i686, :x86_64]), platforms)
+
+# Products for platforms WITH quadmath
+products_with_quadmath = [
+    LibraryProduct("libwigxjpf_shared", :libwigxjpf),
+    LibraryProduct("libwigxjpf_quadmath_shared", :libwigxjpf_quadmath),
+]
+
+# Products for platforms WITHOUT quadmath
+products_without_quadmath = [LibraryProduct("libwigxjpf_shared", :libwigxjpf)]
+
+# Build for x86/x86_64 platforms (with quadmath support)
+build_tarballs(
+    ARGS,
+    name,
+    version,
+    sources,
+    script,
+    platforms_with_quadmath,
+    products_with_quadmath,
+    dependencies;
+    julia_compat = "1.6",
+)
+
+# Build for other platforms (without quadmath)
+build_tarballs(
+    ARGS,
+    name,
+    version,
+    sources,
+    script,
+    platforms_without_quadmath,
+    products_without_quadmath,
+    dependencies;
+    julia_compat = "1.6",
+)
