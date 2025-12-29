d106ed3551174babcae9b323cfe80752da60466f
Chris Rackauckas - Beep Boop Edition <admin@chrisrackauckas.com>
2025-12-29 12:10:27 -0500

    Update D/DASKR/build_tarballs.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- D/DASKR/build_tarballs.jl
+++ D/DASKR/build_tarballs.jl
@@ -30,8 +30,6 @@ gfortran -shared -fPIC ${EXTRA_FFLAGS} -o $libdir/libdaskr.${dlext} solver/d*.f
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 platforms = supported_platforms()
-# Filter out riscv64 as gfortran support is limited
-filter!(p -> arch(p) != "riscv64", platforms)
 platforms = expand_gfortran_versions(platforms)
 
 # The products that we will ensure are always built
