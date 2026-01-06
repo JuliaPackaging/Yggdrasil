14600d263ecd88a73062130e9a621b3611e7df18
Erik Schnetter <schnetter@gmail.com>
2026-01-05 19:31:46 -0500

    HelloWorldMPIFortran: Allow OpenMPI to run as root

--- H/HelloWorldMPIFortran/build_tarballs.jl
+++ H/HelloWorldMPIFortran/build_tarballs.jl
@@ -32,6 +32,8 @@ install_license /usr/share/licenses/MIT
 
 # Run the executable if we built for the host platform
 if [[ ${rust_host} == ${rust_target} ]]; then
+    export OMPI_ALLOW_RUN_AS_ROOT=1
+    export OMPI_ALLOW_RUN_AS_ROOT_CONFIRM=1
     mpiexec -n 2 ${bindir}/hello_world${exeext}
 fi
 """
