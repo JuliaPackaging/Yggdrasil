8abdf26b2ef59ddd8218f90e16ffc7e48a1cad45
Paul Berg <9824244+Pangoraw@users.noreply.github.com>
2026-01-08 11:08:14 +0100

    use the other ts repository

--- T/tree_sitter_mlir/build_tarballs.jl
+++ T/tree_sitter_mlir/build_tarballs.jl
@@ -5,30 +5,18 @@ using BinaryBuilder, Pkg
 name = "tree_sitter_mlir"
 version = v"0.0.1"
 
-llvm_version = v"21.1.1"
-
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource(
-        "https://github.com/llvm/llvm-project/releases/download/llvmorg-$(llvm_version)/mlir-$(llvm_version).src.tar.xz",
-        "75086853150ffe559a747559644c5c4b619b93f6fefc2bb2bfc3b143c97ae1ea"
+    GitSource(
+        "https://github.com/artagnon/tree-sitter-mlir.git",
+        "c7eec06be8a9ddae688e1b03fca2eed79e9801c4"
     ),
     DirectorySource("./bundled")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-mv mlir-* mlir
-mv mlir/utils/tree-sitter-mlir tree-sitter
-
-# Install nodejs and npm
-apk add --update nodejs npm
-
-# Generate tree-sitter files
-cd tree-sitter
-npm ci
-npm run compile
-cd -
+mv tree-sitter-mlir tree-sitter
 
 # Build parser library
 BUILD_FLAGS=(-DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN})
@@ -37,12 +25,12 @@ cmake .. "${BUILD_FLAGS[@]}"
 make -j${nproc}
 make install LANGUAGE_NAME=libtreesitter_mlir
 
-install_license $WORKSPACE/srcdir/mlir/LICENSE.TXT
+install_license $WORKSPACE/srcdir/tree-sitter/LICENSE
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = Platform[Platform("x86_64", "linux"; libc="gnu")]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
