e3ba5db2899c110d0138b1476ce2d516a11a678c
Sebastian Schmitt <62754211+se-schmitt@users.noreply.github.com>
2026-01-09 11:11:40 +0000

    [RDKit] remove flag to set C++17 (doesn't fix it)

--- R/RDKit/build_tarballs.jl
+++ R/RDKit/build_tarballs.jl
@@ -29,7 +29,6 @@ fi
 mkdir build
 cd build
 cmake \
-    -DCMAKE_CXX_FLAGS="-std=c++17" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
