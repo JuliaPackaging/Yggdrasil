0da5badef4177dfd0c00614d17728bc322959e46
Viral B. Shah <virals@gmail.com>
2026-01-12 21:01:49 +0530

    Refactor build_tarballs.jl for clarity and structure

--- T/Thermopack/build_tarballs.jl
+++ T/Thermopack/build_tarballs.jl
@@ -26,13 +26,14 @@ mkdir build
 cd build
 
 # Configure with CMake
-cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} \
+cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=ON \
     -DBLA_VENDOR=OpenBLAS \
     -DBLAS_LIBRARIES="${libdir}/libopenblas.${dlext}" \
-    -DLAPACK_LIBRARIES="${libdir}/libopenblas.${dlext}"
+    -DLAPACK_LIBRARIES="${libdir}/libopenblas.${dlext}" \
+    ..
 
 # Build
 make -j${nproc}
@@ -65,3 +66,4 @@ dependencies = [
 # Build the tarballs
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                preferred_gcc_version=v"8", julia_compat="1.6")
+
