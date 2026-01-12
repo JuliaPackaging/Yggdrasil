0941f09a6afccb047793b0320f886fb639f227b6
Viral B. Shah <virals@gmail.com>
2026-01-12 16:26:55 +0530

    Refactor build_tarballs.jl for clarity and structure

--- T/Thermopack/build_tarballs.jl
+++ T/Thermopack/build_tarballs.jl
@@ -65,7 +65,3 @@ dependencies = [
 # Build the tarballs
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
                preferred_gcc_version=v"8", julia_compat="1.6")
-
-
-
-
