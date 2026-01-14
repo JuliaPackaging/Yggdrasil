f5cb8a58078f7b2ad24e9129edf3621523631a73
Maarten Pronk <git@evetion.nl>
2026-01-14 16:19:04 +0100

    [Thrift] Update gcc to align with boost dependency
    The current linked boost dependency https://github.com/JuliaPackaging/Yggdrasil/blob/05345d261dd96ec573100b03b1a1260e7c05ed12/B/boost/build_tarballs.jl#L138, has gcc v11.1.
    
    Follows advice from https://docs.binarybuilder.org/stable/troubleshooting/#Building-with-an-old-GCC-version-a-library-that-has-dependencies-built-with-newer-GCC-versions
    
    Might help with JuliaGeo/GDAL.jl/issues/146

--- T/Thrift/build_tarballs.jl
+++ T/Thrift/build_tarballs.jl
@@ -64,6 +64,6 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"7")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"11.1")
 
 # Build trigger: 1
