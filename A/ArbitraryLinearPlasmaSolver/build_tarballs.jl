19c25ff67e73ccf3def26ddcbe46e2f1177a15a2
Beforerr <zzj956959688@gmail.com>
2026-01-16 16:22:34 -0800

    fix: use ArbitraryLinearPlasmaSolver to avoid conflicts

--- A/ArbitraryLinearPlasmaSolver/build_tarballs.jl
+++ A/ArbitraryLinearPlasmaSolver/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "ALPS"
+name = "ArbitraryLinearPlasmaSolver"
 version = v"1.1.2"
 
 # Collection of sources required to complete build
