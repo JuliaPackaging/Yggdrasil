26241abc2ccd86f6346c104205d40f04796fbaa0
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2026-01-28 18:30:38 -0600

    Apply suggestion from @amontoison

--- C/Coin-OR/Clp/build_tarballs.jl
+++ C/Coin-OR/Clp/build_tarballs.jl
@@ -1,7 +1,7 @@
 # In addition to coin-or-common.jl, we need to modify this file to trigger a
 # rebuild.
 #
-# Last updated: 2026-01-26
+# Last updated: 2026-01-26 
 
 include("../coin-or-common.jl")
 
