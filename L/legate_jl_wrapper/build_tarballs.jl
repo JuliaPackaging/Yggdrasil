9f890904c387992d2cf61c46e21a3a0dfb3a0f02
krasow <krasow@u.northwestern.edu>
2026-01-30 19:03:44 -0600

    try Legate.jl? Why did this have legate_jl_wrapper.git which we archived?

--- L/legate_jl_wrapper/build_tarballs.jl
+++ L/legate_jl_wrapper/build_tarballs.jl
@@ -9,7 +9,7 @@ include("make_script.jl")
 name = "legate_jl_wrapper"
 version = v"25.10.3" # legate has 05, but Julia doesn't like that
 sources = [
-    GitSource("https://github.com/JuliaLegate/legate_jl_wrapper.git","42bd7f6b80b8eae53e4659915f414a4c11055bf8"),
+    GitSource("https://github.com/JuliaLegate/Legate.jl","42bd7f6b80b8eae53e4659915f414a4c11055bf8"),
 ]
 
 MIN_JULIA_VERSION = v"1.10"
