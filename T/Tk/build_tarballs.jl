ac93b6476e0548e15438988491fc850d35785936
Viral B. Shah <viral@mayin.org>
2026-02-05 23:06:08 +0000

    [Tk] Update to 9.0.3

--- T/Tk/build_tarballs.jl
+++ T/Tk/build_tarballs.jl
@@ -1,23 +1,31 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
+include("../../platforms/macos_sdks.jl")
 
 name = "Tk"
-version = v"8.6.12"
+version = v"9.0.3"
 
 # Collection of sources required to build Tk
 sources = [
-    ArchiveSource("https://downloads.sourceforge.net/sourceforge/tcl/tk$(version)-src.tar.gz",
-                  "12395c1f3fcb6bed2938689f797ea3cdf41ed5cb6c4766eec8ac949560310630"),
+    GitSource("https://github.com/tcltk/tk.git",
+              "9bfcbfb7a406321125298dcaa5ea4c2a446e2ec1"),
     DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 if [[ "${target}" == *-mingw* ]]; then
-    cd $WORKSPACE/srcdir/tk*/win/
+    cd $WORKSPACE/srcdir/tk/win/
 else
-    cd $WORKSPACE/srcdir/tk*/unix/
+    cd $WORKSPACE/srcdir/tk/unix/
+fi
+
+# musl needs bsd-compat-headers for sys/queue.h
+# Copy header to sysroot since cross-compiler doesn't see /usr/include
+if [[ "${target}" == *-musl* ]]; then
+    apk add bsd-compat-headers
+    cp /usr/include/sys/queue.h /opt/${target}/${target}/sys-root/usr/include/sys/
 fi
 
 export CFLAGS="-I${prefix}/include ${CFLAGS}"
@@ -40,6 +48,15 @@ if [[ "${target}" == *mingw* ]]; then
     # `windres` invocations don't get the proper tk include path; just hack it in
     atomic_patch -p2 "${WORKSPACE}/srcdir/patches/win_tk_rc_include.patch"
 fi
+# Enable Xft for TrueType font support on X11 platforms
+if [[ "${target}" == *-linux-* ]] || [[ "${target}" == *-freebsd* ]]; then
+    FLAGS+=(--enable-xft)
+fi
+# musl libc has a working strtod, so disable the fixstrtod workaround
+# that causes "multiple definition of fixstrtod" linker errors
+if [[ "${target}" == *-musl* ]]; then
+    export tcl_cv_strtod_buggy=ok
+fi
 
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} "${FLAGS[@]}"
 make -j${nproc}
@@ -47,8 +64,11 @@ make install
 make install-private-headers
 
 # Install license file
-install_license $WORKSPACE/srcdir/tk*/license.terms
+install_license $WORKSPACE/srcdir/tk/license.terms
 """
+
+# Tk 9.0.3 uses macOS 10.13+ APIs (window tabbing, dark mode)
+sources, script = require_macos_sdk("10.15", sources, script)
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
@@ -57,15 +77,15 @@ x11_platforms = filter(p->Sys.islinux(p) || Sys.isfreebsd(p), platforms)
 
 # The products that we will ensure are always built
 products = [
-    LibraryProduct(["libtk8.6", "libtk8", "tk86"], :libtk),
-    ExecutableProduct(["wish8.6", "wish86"], :wish),
+    LibraryProduct(["libtcl9tk9.0", "tcl9tk90"], :libtk),
+    ExecutableProduct(["wish9.0", "wish90"], :wish),
 ]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
     BuildDependency("Xorg_xorgproto_jll"; platforms=x11_platforms),
     Dependency("Tcl_jll"; compat="~"*string(version)),
-    Dependency("Xorg_libXft_jll"; platforms=x11_platforms)
+    Dependency("Xorg_libXft_jll"; platforms=x11_platforms),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
