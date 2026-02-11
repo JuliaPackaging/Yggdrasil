46bbbb15a94c4a1b9b6952d88ac366df845d202b
Erik Schnetter <schnetter@gmail.com>
2026-02-11 13:00:51 -0500

    HDF5: Enable native float16 support

--- H/HDF5/build_tarballs.jl
+++ H/HDF5/build_tarballs.jl
@@ -8,6 +8,8 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 
 name = "HDF5"
 version = v"2.0.0"
+# We bump the version number to add float16 support
+ygg_version = v"2.0.1"
 
 # Collection of sources required to complete build
 sources = [
@@ -57,6 +59,7 @@ cmake_options=(
     -DHDF5_ENABLE_HDFS=OFF             # would require Java
     -DHDF5_ENABLE_MAP_API=ON
     -DHDF5_ENABLE_MIRROR_VFD=ON
+    -DHDF5_ENABLE_NONSTANDARD_FEATURE_FLOAT16=ON
     -DHDF5_ENABLE_PARALLEL=${parallel}
     -DHDF5_ENABLE_ROS3_VFD=${ros3_vfd}
     -DHDF5_ENABLE_SUBFILING_VFD=ON
@@ -64,14 +67,10 @@ cmake_options=(
     -DHDF5_ENABLE_ZLIB_SUPPORT=ON
     -DHDF5_USE_PREGEN=ON
     -DMPI_HOME=${prefix}
+    -DH5_FLOAT16_CONVERSION_FUNCS_LINK=ON
+    -DH5_FLOAT16_CONVERSION_FUNCS_LINK_NO_FLAGS=ON
+    -DH5_LDOUBLE_TO_FLOAT16_CORRECT=ON
 )
-
-# We could enable this, but it would require more cross-compiling information:
-#     -DHDF5_ENABLE_NONSTANDARD_FEATURE_FLOAT16=ON
-# We would need to set these flags (and check that these conditions are true!): 
-#     -DH5_FLOAT16_CONVERSION_FUNCS_LINK=ON
-#     -DH5_FLOAT16_CONVERSION_FUNCS_LINK_NO_FLAGS=ON
-#     -DH5_LDOUBLE_TO_FLOAT16_CORRECT=ON
 
 # We have pregenerated the Fortran configurations for Linux.
 # (See <https://github.com/HDFGroup/hdf5/issues/6042>.)
@@ -231,13 +230,15 @@ cmake --install builddir
 install_license LICENSE
 """
 
-sources, script = require_macos_sdk("10.14", sources, script)
+# The HDF5 2.0.0 release note state that MacOS 13 does not correct
+# convert long double to float16, while MacOS 14 is fine.
+sources, script = require_macos_sdk("14.0", sources, script)
 
 augment_platform_block = """
     using Base.BinaryPlatforms
     $(MPI.augment)
     augment_platform!(platform::Platform) = augment_mpi!(platform)
-"""
+    """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
@@ -286,7 +287,7 @@ dependencies = [
     Dependency("LibCURL_jll"; compat="7.73, 8"),
     Dependency("OpenSSL_jll"; compat="3.0.16"),
     Dependency("Zlib_jll"; compat="1.2.12"),
-    Dependency("aws_c_s3_jll"; compat="0.9.2"),
+    Dependency("aws_c_s3_jll"; compat="0.11.2"),
     Dependency("dlfcn_win32_jll"; platforms=filter(Sys.iswindows, platforms)),
     Dependency("libaec_jll"; compat="1.1.4"), # This is the successor of szlib
 ]
@@ -297,5 +298,5 @@ append!(dependencies, platform_dependencies)
 ENV["MPITRAMPOLINE_DELAY_INIT"] = "1"
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               augment_platform_block, clang_use_lld=false, julia_compat="1.6", preferred_gcc_version=v"6")
+build_tarballs(ARGS, name, ygg_version, sources, script, platforms, products, dependencies;
+               augment_platform_block, clang_use_lld=false, julia_compat="1.10", preferred_gcc_version=v"12")
