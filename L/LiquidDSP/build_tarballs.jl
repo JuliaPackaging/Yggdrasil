ed44a5f634376a99b446624d29c5bf0952313e7e
Dia Laird <diadora26@yahoo.com>
2026-02-12 21:32:59 -0500

    working for all builds. set flags for aarch apple, patched CMakeLists for window

--- L/LiquidDSP/build_tarballs.jl
+++ L/LiquidDSP/build_tarballs.jl
@@ -1,4 +1,4 @@
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "LiquidDSP"
 version = v"1.7.0"
@@ -12,16 +12,30 @@ cd ${WORKSPACE}/srcdir/liquid-dsp
 
 #need to explicitly call patching
 atomic_patch -p1 ../patches/NoPrint.patch
+atomic_patch -p1 ../patches/FixWinCMAKE.patch
 
 mkdir build
 cd build 
 
-#for all linux build, disable simd and any/all testing. not ideal with respect
-#to simd but a complicated cmake. current linker error with windows and apple
-cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_BUILD_TYPE=Release \
-         -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-         -DFIND_SIMD=OFF -DENABLE_SIMD=OFF \
-         -DBUILD_EXAMPLES=OFF -DBUILD_AUTOTESTS=OFF -DBUILD_BENCHMARKS=OFF
+#for all builds, disable simd and any/all testing. 
+#not ideal with respect to simd but a complicated cmake otherwise. 
+#need to set flags for aarch64-apple. 
+
+if [[ "${target}" == *aarch64-apple* ]]; then 
+   cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+            -DCMAKE_EXE_LINKER_FLAGS="-L/opt/aarch64-apple-darwin20/aarch64-apple-darwin20/lib -lgcc_s.1.1" \
+            -DCMAKE_SHARED_LINKER_FLAGS="-L/opt/aarch64-apple-darwin20/aarch64-apple-darwin20/lib -lgcc_s.1.1" \
+            -DFIND_SIMD=OFF -DENABLE_SIMD=OFF \
+            -DBUILD_EXAMPLES=OFF -DBUILD_AUTOTESTS=OFF -DBUILD_BENCHMARKS=OFF
+else 
+   cmake .. -DCMAKE_INSTALL_PREFIX=${prefix} -DCMAKE_BUILD_TYPE=Release \
+            -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+            -DFIND_SIMD=OFF -DENABLE_SIMD=OFF \
+            -DBUILD_EXAMPLES=OFF -DBUILD_AUTOTESTS=OFF -DBUILD_BENCHMARKS=OFF \
+            -DCMAKE_C_STANDARD_LIBRARIES="" \
+            -DCMAKE_CXX_STANDARD_LIBRARIES=""
+fi
 
 make -j${nproc}
 make install
@@ -31,15 +45,16 @@ install_license ${WORKSPACE}/srcdir/liquid-dsp/LICENSE
 
 """
 
-#linker error with windows, compiler issue with apple
-platforms = supported_platforms( exclude=x->(Sys.isapple(x) || Sys.iswindows(x)) )
+#linker error with windows
+platforms = supported_platforms()
+#platforms = supported_platforms( exclude=x->(!Sys.isapple(x)) )
 
 products = [
     LibraryProduct("libliquid", :libliquid),
 ]
 
 dependencies = [
-    Dependency("FFTW_jll"),
+    Dependency(PackageSpec(name="FFTW_jll", uuid="f5851436-0d7a-5f13-b9de-f02708fd171a"))
 ]
 
 #requires gcc v8
