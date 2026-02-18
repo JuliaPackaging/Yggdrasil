7cc2fbee42dd8e25a52bb4e2e2f22dec1e9a24c6
Benoît Legat <benoit.legat@gmail.com>
2026-02-18 16:27:24 +0100

    Add asio and ssl

--- V/vroom/build_tarballs.jl
+++ V/vroom/build_tarballs.jl
@@ -5,13 +5,25 @@ version = v"1.14.0"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/VROOM-Project/vroom.git", "1fd711bc8c20326dd8e9538e2c7e4cb1ebd67bdb")
+    GitSource("https://github.com/VROOM-Project/vroom.git", "1fd711bc8c20326dd8e9538e2c7e4cb1ebd67bdb"),
+    GitSource("https://github.com/chriskohlhoff/asio.git", "asio-1-30-2"),
 ]
 
 # Bash recipe for building across all platforms
+# ASIO is expected at ../asio (sibling of vroom); add its include path to the makefile
 script = raw"""
 cd $WORKSPACE/srcdir
-cd vroom/src
+cd asio/asio
+./autogen.sh
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make
+make install
+cd ../..
+cd vroom
+git submodule init
+git submodule update
+cd src
+sed -i 's|-I\. |-I. -I../../asio/asio/include |' makefile
 make
 """
 
@@ -27,6 +39,7 @@ products = Product[
 dependencies = [
     Dependency(PackageSpec(name="GLPK_jll", uuid="e8aa6df9-e6ca-548a-97ff-1f85fc5b8b98"))
     Dependency(PackageSpec(name="jq_jll", uuid="f8f80db2-c0ba-59e9-a5c3-38d72e3c5ac2"))
+    Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"))
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
