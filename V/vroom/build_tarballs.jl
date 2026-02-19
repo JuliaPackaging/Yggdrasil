dc3ca6e7c85d88c67085471ee893b27b25eac58d
Benoît Legat <benoit.legat@gmail.com>
2026-02-19 09:27:42 +0100

    Bump GCC to v13

--- V/vroom/build_tarballs.jl
+++ V/vroom/build_tarballs.jl
@@ -42,5 +42,5 @@ dependencies = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-# Need GCC 11 for full C++20 support (e.g. `using enum`, which vroom uses)
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"11")
+# Need GCC 13+ for C++20 <format> and full C++20 support (e.g. `using enum`)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"13")
