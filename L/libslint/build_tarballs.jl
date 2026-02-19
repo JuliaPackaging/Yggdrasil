8a5adbb44e19aff85711127a095415e208978b7f
Oliver Heil <git@heilbit.de>
2026-02-19 11:43:30 +0100

    new Slint.jl release v.0.1.8

--- L/libslint/build_tarballs.jl
+++ L/libslint/build_tarballs.jl
@@ -3,42 +3,25 @@
 using BinaryBuilder
 
 name = "libslintwrapper"
-version = v"0.1.7"
+version = v"0.1.8"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/oheil/Slint.jl.git", "83ac1ad0db4bfd2a5b7c3869f51c4d3a798c8822")
+    GitSource("https://github.com/oheil/Slint.jl.git", "ff7d5fbc6057da9a4160f9963c0615e121a74465")
 ]
 
 # Adapted from the justfile of the repo
 script = raw"""
 install_license Slint.jl/LICENSE
 
+# need more space
 mkdir opt-x86_64-linux-musl
 mkdir opt-x86_64-linux-musl/registry
 ln -s /workspace/srcdir/opt-x86_64-linux-musl/registry /opt/x86_64-linux-musl/registry
 
+# need more space
 mv /tmp .
 ln -s /workspace/srcdir/tmp /tmp
-
-#apk add fontconfig-dev
-#echo '
-#prefix=/usr
-#exec_prefix=${prefix}
-#libdir=${prefix}/lib
-#includedir=${prefix}/include
-#sysconfdir=/etc
-#localstatedir=/var
-#PACKAGE=fontconfig
-#confdir=${sysconfdir}/fonts
-#cachedir=${localstatedir}/cache/${PACKAGE}
-#
-#Name: Fontconfig
-#Description: Font configuration and customization library
-#Version: 1.12.0
-#Libs: -L${libdir} -lfontconfig
-#Cflags: -I${includedir}
-#' > /usr/local/lib/pkgconfig/fontconfig.pc
 
 cd Slint.jl/deps/SlintWrapper
 cargo build --release 
@@ -65,9 +48,7 @@ products = [
 ]
 
 # Dependencies that must be installed before this package can be built
-dependencies = Dependency[
-    #HostBuildDependency("Fontconfig_jll"),
-    #Dependency("Fontconfig_jll"; compat="2.16.0"),
+dependencies = [
     Dependency("Fontconfig_jll"),
 ]
 
