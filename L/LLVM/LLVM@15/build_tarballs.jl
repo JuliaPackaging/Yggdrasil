d931847badf193fb94a915c0e891f74356877d1a
Christian Guinard <28689358+christiangnrd@users.noreply.github.com>
2026-02-25 17:23:58 -0400

    Bump remaining llvm 15 tools

--- L/LLVM/LLVM@15/build_tarballs.jl
+++ L/LLVM/LLVM@15/build_tarballs.jl
@@ -1,6 +1,6 @@
 name = "LLVM"
-llvm_full_version = v"15.0.7+11"
-libllvm_version = v"15.0.7+11"
+llvm_full_version = v"15.0.7+12"
+libllvm_version = v"15.0.7+12"
 
 using BinaryBuilder, Pkg
 using Base.BinaryPlatforms
