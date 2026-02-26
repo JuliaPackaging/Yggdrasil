76ef47781fd2548b3df94747ef489f76dbe1b038
Max Horn <max@quendi.de>
2026-02-26 20:00:38 +0100

    [Readline] update to 8.3.3 (#13215)

--- R/Readline/build_tarballs.jl
+++ R/Readline/build_tarballs.jl
@@ -3,19 +3,30 @@
 using BinaryBuilder
 
 name = "Readline"
-version_str = "8.3"
-version = VersionNumber(version_str)
+version = v"8.3.3"
+version_str = "$(version.major).$(version.minor)"
+version_str2 = "$(version.major)$(version.minor)"
 
 # Collection of sources required to build Readline
 sources = [
     ArchiveSource("https://ftpmirror.gnu.org/gnu/readline/readline-$(version_str).tar.gz",
                   "fe5383204467828cd495ee8d1d3c037a7eba1389c22bc6a041f627976f9061cc"),
+    FileSource("https://ftpmirror.gnu.org/gnu/readline/readline-$(version_str)-patches/readline83-001",
+               "21f0a03106dbe697337cd25c70eb0edbaa2bdb6d595b45f83285cdd35bac84de"),
+    FileSource("https://ftpmirror.gnu.org/gnu/readline/readline-$(version_str)-patches/readline83-002",
+               "e27364396ba9f6debf7cbaaf1a669e2b2854241ae07f7eca74ca8a8ba0c97472"),
+    FileSource("https://ftpmirror.gnu.org/gnu/readline/readline-$(version_str)-patches/readline83-003",
+               "72dee13601ce38f6746eb15239999a7c56f8e1ff5eb1ec8153a1f213e4acdb29"),
     DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/readline-*/
+
+atomic_patch -p0 ${WORKSPACE}/srcdir/readline83-001
+atomic_patch -p0 ${WORKSPACE}/srcdir/readline83-002
+atomic_patch -p0 ${WORKSPACE}/srcdir/readline83-003
 
 # Declare `struct winsize` on Windows. We'll never actually use it.
 atomic_patch -p1 ${WORKSPACE}/srcdir/patches/mingw-winsize.patch
