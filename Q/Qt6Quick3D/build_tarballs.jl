c0fc38370628664f88500f913011995548fca83e
Bart Janssens <bart@bartjanssens.org>
2026-02-27 08:05:35 +0100

    [Qt6Quick3D] Version 6.10.2 (host build)

--- Q/Qt6Quick3D/build_tarballs.jl
+++ Q/Qt6Quick3D/build_tarballs.jl
@@ -3,24 +3,30 @@
 using BinaryBuilder, Pkg
 
 name = "Qt6Quick3D"
-version = v"6.8.2"
+version = v"6.10.2"
 
-host_build = false
+host_build = true
 
 # Collection of sources required to build qt6
 sources = [
     ArchiveSource("https://download.qt.io/official_releases/qt/$(version.major).$(version.minor)/$version/submodules/qtquick3d-everywhere-src-$version.tar.xz",
-                  "084cebccb8c5b1c6bafb7756ab89b08ced23c20cd2e996ed54909a154a9f0b6d"),
+                  "b95439f31d1e580c379e9828b48b03b932b0bdade4ff09f4dd639eff9da2cd75"),
     ArchiveSource("https://github.com/roblabla/MacOSX-SDKs/releases/download/macosx14.0/MacOSX14.0.sdk.tar.xz",
                   "4a31565fd2644d1aec23da3829977f83632a20985561a2038e198681e7e7bf49"),
+    DirectorySource("./bundled"),
 ]
 
 script = raw"""
+# Need newer cmake from JLL
+apk del cmake
+
 cd $WORKSPACE/srcdir
 
 mkdir build
 cd build/
 qtsrcdir=`ls -d ../qtquick3d-*`
+
+atomic_patch -p1 -d "${qtsrcdir}" ../patches/assimp-6.patch
 
 if [[ $bb_full_target == *"musl"* ]]; then
     # secure_getenv is undefined on the musl platforms
@@ -98,14 +104,17 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
+    HostBuildDependency("CMake_jll"),
     HostBuildDependency("Qt6Base_jll"),
     HostBuildDependency("Qt6Declarative_jll"),
     HostBuildDependency("Qt6ShaderTools_jll"),
+    Dependency("assimp_jll"; compat="6.0.4"),
     Dependency("Qt6Base_jll"; compat="="*string(version)),
     Dependency("Qt6Declarative_jll"; compat="="*string(version)),
     Dependency("Qt6ShaderTools_jll"; compat="="*string(version)),
     Dependency("Qt6QuickTimeline_jll"; compat="="*string(version)),
     BuildDependency("Vulkan_Headers_jll"),
+    BuildDependency("Xorg_xproto_jll"),
 ]
 
 if !host_build
