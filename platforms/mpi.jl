6c76927a66630ad72ace7a1d5dae4f9dee930bd3
Erik Schnetter <schnetter@gmail.com>
2026-03-02 12:43:29 -0500

    platforms/mpi: Simplify code

--- platforms/mpi.jl
+++ platforms/mpi.jl
@@ -7,40 +7,39 @@ const augment = raw"""
     MPIPreferences_UUID = Base.UUID("3da0fdf6-3ccc-4f1b-acd9-58baa6c99267")
     const preferences = Base.get_preferences(MPIPreferences_UUID)
 
+    const binary_to_abi = Dict(
+        "MPIABI_jll" => "MPIABI",
+        "MPICH_jll" => "MPICH",
+        "MPICH_CUDA_jll" => "MPICH",
+        "MPItrampoline_jll" => "MPItrampoline",
+        "MicrosoftMPI_jll" => "MicrosoftMPI",
+        "OpenMPI_jll" => "OpenMPI",
+    )
+
     # Keep logic in sync with MPIPreferences.jl
     function augment_mpi!(platform)
         # Doesn't need to be `const` since we depend on MPIPreferences so we
         # invalidate the cache when it changes.
+
+        # Determine which jll binary provides our MPI implementation.
+        # Use MPIPreferences if set, otherwise use either MPICH or MicrosoftMPI (on Windows).
         # Note: MPIPreferences uses `Sys.iswindows()` without the `platform` argument.
         binary = get(preferences, "binary", Sys.iswindows(platform) ? "MicrosoftMPI_jll" : "MPICH_jll")
 
-        abi = if binary == "system"
-            let abi = get(preferences, "abi", nothing)
-                if abi === nothing
-                    error("MPIPreferences: Inconsistent state detected, binary set to system, but no ABI set.")
-                else
-                    abi
-                end
-            end
-        elseif binary == "MPIABI_jll"
-            "MPIABI"
-        elseif binary == "MPICH_jll"
-            "MPICH"
-        elseif binary == "MPICH_CUDA_jll"
-            "MPICH"
-        elseif binary == "MPItrampoline_jll"
-            "MPItrampoline"
-        elseif binary == "MicrosoftMPI_jll"
-            "MicrosoftMPI"
-        elseif binary == "OpenMPI_jll"
-            "OpenMPI"
+        # Convert the jll binary to an ABI
+        if binary == "system"
+            abi = get(preferences, "abi", nothing)
+            abi === nothing && error("MPIPreferences: Inconsistent state detected, `binary` is set to system, but `abi` is not set.")
         else
-            error("Unknown binary: $binary")
+            abi = get(binary_to_abi, binary, nothing)
+            abi == nothing && error("Unknown MPI binary: $binary")
         end
 
+        # Add an `mpi` entry to the platform if it's missing
         if !haskey(platform, "mpi")
             platform["mpi"] = abi
         end
+
         return platform
     end
     """
