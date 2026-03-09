7420c9adb5c2c3db166317299118509b5bbf9fb4
Benoît Legat <benoit.legat@gmail.com>
2026-03-09 22:48:42 +0100

    [asio] New package (#13174)
    * [asio] New package
    
    * Bump gcc to v5
    
    * Add header as product
    
    * Skip make for windows
    
    * Update A/asio/build_tarballs.jl
    
    * Update A/asio/build_tarballs.jl
    
    * Remove preferred GCC version from build_tarballs
    
    Remove preferred GCC version specification.
    
    * Simplify build script for header-only distribution
    
    Removed Windows-specific build steps and simplified the build script for header-only distribution.
    
    * Apply suggestion from @giordano
    
    * Apply suggestion from @giordano
    
    * Apply suggestion from @giordano
    
    ---------
    
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

---
+++ A/asio/build_tarballs.jl
@@ -0,0 +1,36 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "asio"
+version = v"1.36.0"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/chriskohlhoff/asio.git", "231cb29bab30f82712fcd54faaea42424cc6e710"),
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/asio/asio
+./autogen.sh
+./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
+make install
+"""
+
+# Because we are just producing a header, the same product is available for any platform
+platforms = [AnyPlatform()]
+
+# The products that we will ensure are always built (header-only: export main header so downstreams can discover include/)
+products = [
+    FileProduct("include/asio.hpp", :asio_hpp),
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(
+    ARGS, name, version, sources, script, platforms, products, dependencies;
+    julia_compat="1.6",
+)
