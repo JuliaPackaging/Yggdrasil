a317f57539465e21291c98b077962d84bc94366c
James Foster <James.Foster@csiro.au>
2026-03-10 00:47:34 +1100

    [gdx] New build script

---
+++ G/gdx/build_tarballs.jl
@@ -0,0 +1,56 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder, Pkg
+
+name = "gdx"
+version = v"7.11.19"
+
+# Collection of sources required to complete build
+sources = [
+    GitSource("https://github.com/GAMS-dev/gdx.git", "fd8c1292973885cb6f8b689208b81b33b1270f26"),
+    GitSource("https://github.com/madler/zlib.git", "216c70c020aa53f0c40920d155f808b6b59c9acb")
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir/gdx/
+rmdir zlib
+mv ../zlib/ .
+
+cmake -S . -B build --install-prefix ${prefix} --toolchain ${CMAKE_TARGET_TOOLCHAIN} -DCMAKE_BUILD_TYPE=Release -DNO_TESTS=ON -DNO_EXAMPLES=ON -DNO_TOOLS=ON
+cmake --build build --parallel ${nproc}
+cmake --install build
+
+mkdir -p ${libdir}
+if [[ "${target}" == *mingw* ]]; then
+    mv build/gdxcclib*.dll ${libdir};
+else
+    mv build/libgdxcclib*.* ${libdir};
+fi
+
+install_license ${WORKSPACE}/srcdir/gdx/LICENSE
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = [
+    Platform("i686", "linux"; libc = "glibc"),
+    Platform("x86_64", "linux"; libc = "glibc"),
+    Platform("aarch64", "linux"; libc = "glibc"),
+    Platform("x86_64", "macos"; ),
+    Platform("aarch64", "macos"; )
+]
+
+
+# The products that we will ensure are always built
+products = [
+    LibraryProduct("libgdxcclib64", :libgdx)
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = Dependency[
+]
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version = v"11.1.0")
+
