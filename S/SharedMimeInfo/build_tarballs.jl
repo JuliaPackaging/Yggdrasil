1770f75e2e5b10beb5331cb8d2ff906e6cd6b636
Erik Schnetter <schnetter@gmail.com>
2026-03-12 10:41:46 -0400

    SharedMimeInfo: Update to 2.4

--- S/SharedMimeInfo/build_tarballs.jl
+++ S/SharedMimeInfo/build_tarballs.jl
@@ -3,52 +3,26 @@
 using BinaryBuilder
 
 name = "SharedMimeInfo"
-version = v"1.15"
+version = v"2.4"
 
 sources = [
-    ArchiveSource("https://gitlab.freedesktop.org/xdg/shared-mime-info/uploads/b27eb88e4155d8fccb8bb3cd12025d5b/shared-mime-info-$(version.major).$(version.minor).tar.xz",
-                  "f482b027437c99e53b81037a9843fccd549243fd52145d016e9c7174a4f5db90"),
+    ArchiveSource("https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/$(version.major).$(version.minor)/shared-mime-info-$(version.major).$(version.minor).tar.bz2",
+                  "32dc32ae39ff1c1bf8434dd3b36770b48538a1772bc0298509d034f057005992"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
-cd $WORKSPACE/srcdir/shared-mime-info-*/
-apk add itstool
+cd $WORKSPACE/srcdir/shared-mime-info-*
+apk add gettext
 
-./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-update-mimedb
-make -j${nproc}
-make install
+meson setup builddir --cross-file="${MESON_TARGET_TOOLCHAIN}" -Dbuild-tests=false
+meson compile -C builddir
+meson install -C builddir
 """
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-# Experimental platforms cannot be used with Julia v1.5-.
-# Change `julia_compat` to require at least Julia v1.6
-# platforms = supported_platforms()
-# Remove this when we build a newer version for which we can target the former
-# experimental platforms
-platforms = [
-    # glibc Linuces
-    Platform("i686", "linux"),
-    Platform("x86_64", "linux"),
-    Platform("aarch64", "linux"),
-    Platform("armv7l", "linux"),
-    Platform("powerpc64le", "linux"),
-
-    # musl Linuces
-    Platform("i686", "linux"; libc="musl"),
-    Platform("x86_64", "linux"; libc="musl"),
-    Platform("aarch64", "linux"; libc="musl"),
-    Platform("armv7l", "linux"; libc="musl"),
-
-    # BSDs
-    Platform("x86_64", "macos"),
-    Platform("x86_64", "freebsd"),
-
-    # Windows
-    Platform("i686", "windows"),
-    Platform("x86_64", "windows"),
-]
+platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products = [
@@ -59,7 +33,7 @@ products = [
 # Dependencies that must be installed before this package can be built
 # Based on http://www.linuxfromscratch.org/blfs/view/8.3/general/shared-mime-info.html
 dependencies = [
-    Dependency("Glib_jll", v"2.59.0"; compat="2.59.0"),
+    Dependency("Glib_jll"; compat="2.86.3"),
     # We had to restrict compat with XML2 because of ABI breakage:
     # https://github.com/JuliaPackaging/Yggdrasil/pull/10965#issuecomment-2798501268
     # Updating to `compat="~2.14.1"` is likely possible without problems but requires rebuilding this package
@@ -68,4 +42,5 @@ dependencies = [
 
 # Build the tarballs, and possibly a `build.jl` as well.  We use GCC 8 because it is the only GCC version that links
 # properly on powerpc64le.  Shocking, I know, but this is the world we live in.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; preferred_gcc_version=v"8")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+               julia_compat="1.6", preferred_gcc_version=v"8")
