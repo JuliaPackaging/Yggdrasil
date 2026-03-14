d26c69c01b7434df030bd9cf7c71e4849c15dd6a
Benoît Legat <benoit.legat@gmail.com>
2026-03-14 11:50:50 +0100

    BuildDep

--- V/vroom/build_tarballs.jl
+++ V/vroom/build_tarballs.jl
@@ -58,7 +58,7 @@ products = Product[
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="asio_jll", uuid="adbc9c39-6bf4-5566-8295-c0623552eeca"); compat="1.18.1")
+    BuildDependency(PackageSpec(name="asio_jll", uuid="adbc9c39-6bf4-5566-8295-c0623552eeca"); compat="1.18.1")
     Dependency(PackageSpec(name="GLPK_jll", uuid="e8aa6df9-e6ca-548a-97ff-1f85fc5b8b98"))
     Dependency(PackageSpec(name="jq_jll", uuid="f8f80db2-c0ba-59e9-a5c3-38d72e3c5ac2"))
     Dependency(PackageSpec(name="OpenSSL_jll", uuid="458c3c95-2e84-50aa-8efc-19380b2a3a95"); compat="3.0.16")
