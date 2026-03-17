efb9fdcc25c340231bd3076f89ab5b6fd669eb83
Erik Schnetter <schnetter@gmail.com>
2026-03-17 10:25:08 -0400

    SpFFT: Clean up build script

--- S/SpFFT/build_tarballs.jl
+++ S/SpFFT/build_tarballs.jl
@@ -7,6 +7,7 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 
 name = "SpFFT"
 version = v"1.1.1"
+ygg_version = v"1.1.2"          # we bumped the version to update MPI compat bounds
 
 sources = [
    GitSource("https://github.com/eth-cscs/SpFFT/", "34391bc8c5e2b8add000e38fdc6ce68896184d10")
@@ -15,44 +16,22 @@ sources = [
 script = raw"""
 cd $WORKSPACE/srcdir
 
-mkdir build
-cd build
+CMAKE_ARGS=(
+    -DSPFFT_OMP=ON
+    -DSPFFT_MPI=ON
+    -DSPFFT_INSTALL=ON
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN}
+    -DCMAKE_FIND_ROOT_PATH=${prefix}
+    -DCMAKE_INSTALL_PREFIX=${prefix}
+    -DCMAKE_BUILD_TYPE=Release
+    -DBUILD_SHARED_LIBS=ON
+    -DMPI_C_COMPILER=${bindir}/mpicc
+    -DMPI_CXX_COMPILER=${bindir}/mpicxx
+)
 
-CMAKE_ARGS="-DSPFFT_OMP=ON \
-            -DSPFFT_MPI=ON \
-            -DSPFFT_INSTALL=ON \
-            -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-            -DCMAKE_FIND_ROOT_PATH='${prefix}/lib/mpich;${prefix}' \
-            -DCMAKE_INSTALL_PREFIX=$prefix \
-            -DCMAKE_BUILD_TYPE=Release \
-            -DBUILD_SHARED_LIBS=ON \
-            -DMPI_C_COMPILER=$bindir/mpicc \
-            -DMPI_CXX_COMPILER=$bindir/mpicxx"
-
-#TODO if [[ "${target}" == *-apple-mpich ]]; then
-#TODO   CMAKE_ARGS="${CMAKE_ARGS} \
-#TODO                -DMPI_C_LIB_NAMES='mpi;pmpi;hwloc' \
-#TODO                -DMPI_CXX_LIB_NAMES='mpicxx;mpi;pmpi;hwloc' \
-#TODO                -DMPI_mpicxx_LIBRARY=${libdir}/libmpicxx.dylib \
-#TODO                -DMPI_mpi_LIBRARY=${libdir}/libmpi.dylib \
-#TODO                -DMPI_pmpi_LIBRARY=${libdir}/libpmpi.dylib \
-#TODO                -DMPI_hwloc_LIBRARY=${libdir}/libhwloc.dylib"
-#TODO fi
-#TODO 
-#TODO if [[ "${target}" == *-apple-mpitrampoline ]]; then
-#TODO   CMAKE_ARGS="${CMAKE_ARGS} \
-#TODO                -DMPI_C_LIB_NAMES='mpi;pmpi;hwloc' \
-#TODO                -DMPI_CXX_LIB_NAMES='mpicxx;mpi;pmpi;hwloc' \
-#TODO                -DMPI_mpicxx_LIBRARY=${libdir}/mpich/lib.libmpicxx.a \
-#TODO                -DMPI_mpi_LIBRARY=${libdir}/mpich/lib/libmpi.a \
-#TODO                -DMPI_pmpi_LIBRARY=${libdir}/mpich/lib/libpmpi.a \
-#TODO                -DMPI_hwloc_LIBRARY=${libdir}/libhwloc.dylib"
-#TODO fi
-
-cmake .. ${CMAKE_ARGS}
-
-make -j${nproc} install
-
+cmake -B build ${CMAKE_ARGS[@]}
+cmake --build build --parallel ${nproc}
+cmake --install build
 """
 
 augment_platform_block = """
@@ -62,8 +41,6 @@ augment_platform_block = """
 """
 platforms = supported_platforms()                                                       
 filter!(!Sys.iswindows, platforms)
-#TODO filter!(!Sys.isfreebsd, platforms)
-#TODO filter!(p -> arch(p) != "riscv64", platforms)
 platforms = expand_cxxstring_abis(platforms)
 
 products = [
@@ -81,5 +58,5 @@ platforms, platform_dependencies = MPI.augment_platforms(platforms)
 append!(dependencies, platform_dependencies)
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               augment_platform_block, julia_compat="1.6", preferred_gcc_version = v"8")
+build_tarballs(ARGS, name, ygg_version, sources, script, platforms, products, dependencies;
+               augment_platform_block, julia_compat="1.6", preferred_gcc_version=v"8")
