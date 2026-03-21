d9d5b8b1b8ad57f2558eae9eb386ea45c45e9549
Jaron <jaronpackard7@outlook.com>
2026-03-21 16:57:07 +0000

    Fix Dependency Miscall

--- L/LSBoringSSL/build_tarballs.jl
+++ L/LSBoringSSL/build_tarballs.jl
@@ -38,7 +38,7 @@ products = [
    LibraryProduct("libssl", :libssl)
 ]
 
-dependencies = []
+dependencies = Dependency[]
 
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; 
                compilers=[:c, :go], julia_compat="1.6")
