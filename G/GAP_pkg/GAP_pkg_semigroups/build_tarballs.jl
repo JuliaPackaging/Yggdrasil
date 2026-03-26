1e0c1080daa11ca0eba049fc41481328a363d6dc
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-03-26 11:19:17 +0100

    [GAP_pkg_semigroups] Bump to v5.6.1

--- G/GAP_pkg/GAP_pkg_semigroups/build_tarballs.jl
+++ G/GAP_pkg/GAP_pkg_semigroups/build_tarballs.jl
@@ -4,14 +4,14 @@ include("../common.jl")
 
 gap_version = v"400.1500.0"
 name = "semigroups"
-upstream_version = "5.5.4" # when you increment this, reset offset to v"0.0.0"
-offset = v"1.0.1" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
+upstream_version = "5.6.1" # when you increment this, reset offset to v"0.0.0"
+offset = v"1.0.0" # increment this when rebuilding with unchanged upstream_version, e.g. gap_version changes
 version = offset_version(upstream_version, offset)
 
 # Collection of sources required to build this JLL
 sources = [
     ArchiveSource("https://github.com/semigroups/Semigroups/releases/download/v$(upstream_version)/semigroups-$(upstream_version).tar.gz",
-                  "df0488b14af106338009c0ae7dc8b7def9052fb3619a6ba53c31f60cc0731353"),
+                  "92123474c067710219f7813c0511fb418338a04e493d0d2dbfb0a88ad5c6dc9d"),
 ]
 
 # Bash recipe for building across all platforms
@@ -62,6 +62,6 @@ products = [
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.10", preferred_gcc_version=v"7")
+               julia_compat="1.10", preferred_gcc_version=v"10")
 
 # rebuild trigger: 1
