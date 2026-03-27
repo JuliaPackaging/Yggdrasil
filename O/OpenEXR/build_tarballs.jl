66add9030b7e726f599058c2cfdcb9a8a0891590
Erik Schnetter <schnetter@gmail.com>
2026-03-26 20:09:23 -0400

    OpenEXR: New version 3.4.8 (#13409)

--- O/OpenEXR/build_tarballs.jl
+++ O/OpenEXR/build_tarballs.jl
@@ -5,11 +5,11 @@ const YGGDRASIL_DIR = "../.."
 include(joinpath(YGGDRASIL_DIR, "platforms", "macos_sdks.jl"))
 
 name = "OpenEXR"
-version = v"3.4.4"
+version = v"3.4.8"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/AcademySoftwareFoundation/openexr.git", "741ecb82ccdb291ce5b04713fc6c03208753575e"),
+    GitSource("https://github.com/AcademySoftwareFoundation/openexr.git", "adbd2a588b47b1f6d02557571d8e36a91084da8a"),
     DirectorySource("bundled"),
 ]
 
@@ -17,15 +17,17 @@ sources = [
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/openexr*
-cmake -B build -G Ninja \
-    -DBUILD_TESTING=OFF \
-    -DOPENEXR_INSTALL_TOOLS=OFF \
-    -DOPENEXR_INSTALL_EXAMPLES=OFF \
-    -DCMAKE_INSTALL_PREFIX=$prefix \
-    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
-    -DCMAKE_BUILD_TYPE=Release
+
 # We are building with old kernel headers that do not define `HWCAP_SVE2`
 atomic_patch -p1 $WORKSPACE/srcdir/patches/sve2.patch
+
+cmake -B build -G Ninja \
+    -DBUILD_TESTING=OFF \
+    -DOPENEXR_BUILD_TOOLS=OFF \
+    -DOPENEXR_BUILD_EXAMPLES=OFF \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DCMAKE_BUILD_TYPE=Release
 cmake --build build --parallel ${nproc}
 cmake --install build
 install_license LICENSE.md
