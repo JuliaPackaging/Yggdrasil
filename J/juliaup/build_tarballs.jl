ce068204cd9ee230adc7e6ab819829fa30d84492
Dilum Aluthge <dilum@aluthge.com>
2026-03-29 23:12:42 -0400

    Juliaup: Update from 1.18.9 to 1.19.9

--- J/juliaup/build_tarballs.jl
+++ J/juliaup/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "juliaup"
-version = v"1.18.9"
+version = v"1.19.9"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/JuliaLang/juliaup.git", "a0841737fd01b13a06b56883bb852ecb6ca056d9")
+    GitSource("https://github.com/JuliaLang/juliaup.git", "f12e43abb626bbad9217e8941661fa67b35053d1")
 ]
 
 # Bash recipe for building across all platforms
