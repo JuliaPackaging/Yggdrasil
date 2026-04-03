b30ead8c42b9bd4adac394ba8621686e2bdeb8d3
Michael Myers <120071679+myersm0@users.noreply.github.com>
2026-04-03 13:14:54 -0500

    [Montre] Add Montre v0.2.0 (#13344)
    * [Montre] Add montre v0.2.0
    
    * Update build_tarballs.jl
    
    [skip build]
    
    * updated version to 0.3.0 and the corresponding sha256sum; also tried removing two of the platform exclusions to see if build works now
    
    * added the two rust 1.94-based platform exclusions back, after confirming that they indeed still are unable to unable to build for the same reason
    
    * Added note about continued failure of two platforms due to still lacking rust 1.94 toolchain
    
    [skip build]
    
    * bump montre to v0.4.0
    
    * bump montre to v0.5.0
    
    [skip build]
    
    * Bumped to v0.5.1 and tried fixing _Unwind linker error for i686-w64-mingw32
    
    * My attempted fix for i686-w64-mingw32 didnt work; added it back to platform exclusions
    
    * Update build_tarballs.jl

---
+++ M/Montre/build_tarballs.jl
@@ -0,0 +1,45 @@
+using BinaryBuilder, Pkg
+
+name = "Montre"
+version = v"0.6.0"
+
+sources = [
+	GitSource(
+		"https://github.com/myersm0/montre.git",
+		"1802089f6771c3b6d23f26bd05161c7e1986fe70",
+	),
+]
+
+script = raw"""
+cd $WORKSPACE/srcdir/montre*/
+cargo build --release --target $CARGO_BUILD_TARGET
+install -Dvm 755 \
+	target/$CARGO_BUILD_TARGET/release/*montre_ffi.${dlext} \
+	${libdir}/libmontre_ffi.${dlext}
+"""
+
+excluded = [
+	# Rust 1.94.0 toolchain not available for this platform target
+	# (confirmed still failing as of 2026-03-21, after Rust 1.94 was added to Yggdrasil)
+	"riscv64-linux-gnu",
+	"aarch64-unknown-freebsd",
+	# _Unwind linker errors:
+	"i686-w64-mingw32",
+]
+
+
+platforms = filter(supported_platforms()) do p
+	triplet(p) ∉ excluded && !occursin("musl", triplet(p))
+end
+
+products = [
+	LibraryProduct("libmontre_ffi", :libmontre),
+]
+
+dependencies = Dependency[]
+
+build_tarballs(
+	ARGS, name, version, sources, script, platforms, products, dependencies;
+	julia_compat = "1.10",
+	compilers = [:rust, :c],
+)
