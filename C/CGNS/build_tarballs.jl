2973a7d8197e8d1b98589876d8806856acf43c32
Erik Schnetter <schnetter@gmail.com>
2026-04-06 17:23:13 -0400

    CGNS: Require MPI

--- C/CGNS/build_tarballs.jl
+++ C/CGNS/build_tarballs.jl
@@ -1,4 +1,6 @@
 using BinaryBuilder, Pkg
+const YGGDRASIL_DIR = "../.."
+include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 
 name = "CGNS"
 version = v"4.5.1"
@@ -17,17 +19,31 @@ if [[ ${target} == x86_64-linux-musl ]]; then
     rm /usr/lib/libnghttp2.*
 fi
 
+# We need to build with MPI since our HDF5 uses MPI, and this makes
+# CGNS depend explicitly on MPI. Since we're doing that we might as
+# well enable the parallel API.
+
 cmake -Bbuild -GNinja \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE="${CMAKE_TARGET_TOOLCHAIN}" \
-    -DCGNS_ENABLE_SHARED_LIB=ON \
-    -DCGNS_ENABLE_STATIC_LIB=OFF
+    -DCGNS_BUILD_SHARED=ON \
+    -DCGNS_ENABLE_PARALLEL=ON
 cmake --build build --parallel ${nproc}
 cmake --install build
+
+# CGNS always builds a static library. Remove it.
+rm -f ${libdir}/libcgns.a
 """
 
+augment_platform_block = """
+    using Base.BinaryPlatforms
+    $(MPI.augment)
+    augment_platform!(platform::Platform) = augment_mpi!(platform)
+    """
+
 platforms = supported_platforms()
+platforms, platform_dependencies = MPI.augment_platforms(platforms)
 
 products = [
     LibraryProduct("libcgns", :libcgns),
@@ -40,13 +56,10 @@ products = [
 ]
 
 dependencies = [
-    # Without OpenMPI as build dependency the build fails on 32-bit platforms
-    BuildDependency(PackageSpec(; name="OpenMPI_jll", version="4.1.8"); platforms=filter(p -> nbits(p)==32, platforms)),
     Dependency("HDF5_jll"; compat="~2.1.1"),
 ]
+append!(dependencies, platform_dependencies)
 
 # We need at least GCC 5 for the HDF5 libraries
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", preferred_gcc_version=v"8")
-
-# build trigger: 1
+               augment_platform_block, julia_compat="1.6", preferred_gcc_version=v"8")
