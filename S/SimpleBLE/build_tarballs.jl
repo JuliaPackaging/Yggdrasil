43ec36b7d49776d2f192215f82013dfed84b23e3
Torfi Þorgrímsson <torfit@gmail.com>
2026-04-07 22:24:58 +0000

    Update S/SimpleBLE/build_tarballs.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- S/SimpleBLE/build_tarballs.jl
+++ S/SimpleBLE/build_tarballs.jl
@@ -46,24 +46,8 @@ fi
 
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
-platforms = [
-    Platform("i686", "linux"; libc = "glibc"),
-    Platform("x86_64", "linux"; libc = "glibc"),
-    Platform("aarch64", "linux"; libc = "glibc"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "glibc"),
-    Platform("powerpc64le", "linux"; libc = "glibc"),
-    Platform("riscv64", "linux"; libc = "glibc"),
-    Platform("i686", "linux"; libc = "musl"),
-    Platform("x86_64", "linux"; libc = "musl"),
-    Platform("aarch64", "linux"; libc = "musl"),
-    Platform("armv6l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("armv7l", "linux"; call_abi = "eabihf", libc = "musl"),
-    Platform("x86_64", "freebsd"; ),
-    Platform("aarch64", "freebsd"; ),
-    Platform("i686", "windows"; ),
-    Platform("x86_64", "windows"; )
-]
+platforms = supported_platforms()
+filter!(!Sys.isapple, platforms)
 platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
