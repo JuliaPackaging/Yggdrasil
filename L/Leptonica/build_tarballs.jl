4cdf88da0ff30ec561ea14cd997523ecb9b8512b
Erik Schnetter <schnetter@gmail.com>
2026-04-10 09:16:29 -0400

    Add compat ranges to all `Giflib_jll` users
    We currently ship Giflib 5. Giflib 6 is out and is not ABI compatible. Add compat ranges to all recipies so that they won't break when/if they are rebuilt.
    
    [skip build]

--- L/Leptonica/build_tarballs.jl
+++ L/Leptonica/build_tarballs.jl
@@ -43,7 +43,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="Giflib_jll", uuid="59f7168a-df46-5410-90c8-f2779963d0ec")),
+    Dependency(PackageSpec(name="Giflib_jll", uuid="59f7168a-df46-5410-90c8-f2779963d0ec"); compat="5.2.3"),
     Dependency(PackageSpec(name="JpegTurbo_jll", uuid="aacddb02-875f-59d6-b918-886e6ef4fbf8")),
     Dependency(PackageSpec(name="libpng_jll", uuid="b53b4c65-9356-5827-b1ea-8c7a1a84506f")),
     Dependency("Libtiff_jll"; compat="4.7"),
