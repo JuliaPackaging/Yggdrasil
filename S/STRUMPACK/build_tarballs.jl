9f153ea63997d78bc556622b3a541e06c53d837d
Jash Ambaliya <87864439+AJ0070@users.noreply.github.com>
2026-04-13 21:08:50 +0530

    Update build_tarballs.jl
    Co-authored-by: Lars Göttgens <lars.goettgens@gmail.com>

--- S/STRUMPACK/build_tarballs.jl
+++ S/STRUMPACK/build_tarballs.jl
@@ -18,7 +18,8 @@ cd $WORKSPACE/srcdir/STRUMPACK
 # Set LD_LIBRARY_PATH so CMake can find the libraries
 export LD_LIBRARY_PATH="${libdir}:${LD_LIBRARY_PATH}"
 
-cmake -DCMAKE_INSTALL_PREFIX=${prefix} \
+cmake -B build \
+    -DCMAKE_INSTALL_PREFIX=${prefix} \
     -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
     -DCMAKE_BUILD_TYPE=Release \
     -DBUILD_SHARED_LIBS=ON \
