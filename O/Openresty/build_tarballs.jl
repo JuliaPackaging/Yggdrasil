caf8f830c6c607c01ec3b135004c4e9de2b01203
Matt Bauman <mbauman@juliahub.com>
2026-04-20 15:26:25 -0400

    fix hashes and folder names

--- O/Openresty/build_tarballs.jl
+++ O/Openresty/build_tarballs.jl
@@ -9,7 +9,7 @@ upstream_version = "1.29.2.3"
 # Collection of sources required to complete build.
 # Openresty requires static linking of these libraries, hence sources are provided so that they are compiled in.
 sources = [
-    ArchiveSource("https://openresty.org/download/openresty-$(upstream_version).tar.gz", "79b071e27bdc143d5f401d0dbf504de4420070d867538c5edc2546d0351fd5c0"),
+    ArchiveSource("https://openresty.org/download/openresty-$(upstream_version).tar.gz", "315e49fa4568747fec4bdada9614d2ba287e7aed4b3430d7ea25685e24cc43ff"),
     ArchiveSource("https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.47/pcre2-10.47.tar.bz2", "47fe8c99461250d42f89e6e8fdaeba9da057855d06eb7fc08d9ca03fd08d7bc7"),
     ArchiveSource("https://github.com/openssl/openssl/releases/download/openssl-3.5.5/openssl-3.5.5.tar.gz", "b28c91532a8b65a1f983b4c28b7488174e4a01008e29ce8e69bd789f28bc2a89"),
     ArchiveSource("https://www.zlib.net/zlib-1.3.2.tar.gz", "bb329a0a2cd0274d05519d61c667c062e06990d72e125ee2dfa8de64f0119d16")
@@ -23,7 +23,7 @@ export SUPER_VERBOSE=1
     --with-cc=$CC \
     --with-zlib=$WORKSPACE/srcdir/zlib-1.3.2 \
     --with-openssl=$WORKSPACE/srcdir/openssl-3.5.5 \
-    --with-pcre=$WORKSPACE/srcdir/pcre-10.47 \
+    --with-pcre=$WORKSPACE/srcdir/pcre2-10.47 \
     --with-pcre-jit
 make
 make install
