3b368e9fefd9596b016d8bc6b5660cb30ed0f39b
Tom Finke <tom.finke@uni-a.de>
2026-04-20 17:02:30 +0200

    [OpenFHE] and [OpenFHE_int128] Bumped version to 1.5.1 (#13542)

--- O/OpenFHE_int128/build_tarballs.jl
+++ O/OpenFHE_int128/build_tarballs.jl
@@ -5,8 +5,8 @@ include(joinpath(pwd(), "..", "OpenFHE", "common.jl"))
 # If you make changes in this file, e.g., to release a new version,
 # be sure to also release a new version of `OpenFHE` as well (see `../OpenFHE/build_tarballs.jl`)
 name = "OpenFHE_int128"
-version = v"1.5.0"
-git_hash = "df495ba2e91739a6dc8f1de254fc5a41155ce504"
+version = v"1.5.1"
+git_hash = "1306d14f8c26bb6150d3e6ad54f28dfe1007689e"
 
 sources, script, platforms, products, dependencies = prepare_openfhe_build(name, git_hash)
 
