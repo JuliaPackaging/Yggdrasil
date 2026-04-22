a5eb0c63499455de6bb2428edb1367505c46c950
Tim Besard <tim.besard@gmail.com>
2026-04-22 11:00:18 +0200

    LLVM: Bump LLVMExtra. (#13554)
    Includes https://github.com/JuliaLLVM/LLVM.jl/pull/542

--- L/LLVMExtra/build_tarballs.jl
+++ L/LLVMExtra/build_tarballs.jl
@@ -8,12 +8,12 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "macos_sdks.jl"))
 
 name = "LLVMExtra"
 repo = "https://github.com/maleadt/LLVM.jl.git"
-version = v"0.0.38"
+version = v"0.0.39"
 
-llvm_versions = [v"15.0.7", v"16.0.6", v"18.1.7", v"20.1.8"]
+llvm_versions = [v"15.0.7+12", v"16.0.6+6", v"18.1.7+5", v"20.1.8+0"]
 
 sources = [
-    GitSource(repo, "4d55835dca597672dac00ef55ca555550acbf790"),
+    GitSource(repo, "e76b8e04d2ed33e96f72bd836ce88590c4f943fc"),
 ]
 
 # Bash recipe for building across all platforms
