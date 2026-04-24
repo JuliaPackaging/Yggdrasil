fe1570754700ba1caeae4eb1b83db651519c878f
Erik Schnetter <schnetter@gmail.com>
2026-04-24 09:12:37 -0400

    Update M/M4/build_tarballs.jl
    Co-authored-by: Max Horn <max@quendi.de>

--- M/M4/build_tarballs.jl
+++ M/M4/build_tarballs.jl
@@ -4,7 +4,7 @@ name = "M4"
 version = v"1.4.21"
 
 sources = [
-    ArchiveSource("https://ftp.gnu.org/gnu/m4/m4-$(version).tar.xz",
+    ArchiveSource("https://ftpmirror.gnu.org/m4/m4-$(version).tar.xz",
                   "f25c6ab51548a73a75558742fb031e0625d6485fe5f9155949d6486a2408ab66"),
 ]
 
