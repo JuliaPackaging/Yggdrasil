2fa0530898e6a9bd8a0b1c8d905fe5962def6c0a
Cody Tapscott <topolarity@tapscott.me>
2026-04-24 17:44:19 -0400

    [CompilerSupportLibraries] Package `libmingwex.a` and `libkernel32.a`

--- C/CompilerSupportLibraries/CompilerSupportLibraries@v1.5/build_tarballs.jl
+++ C/CompilerSupportLibraries/CompilerSupportLibraries@v1.5/build_tarballs.jl
@@ -1,5 +1,5 @@
 include("../common.jl")
 
-build_csl(ARGS, v"1.5.0"; preferred_gcc_version=v"15", unix_staticlibs=true, windows_staticlibs=true, julia_compat="1.12")
+build_csl(ARGS, v"1.5.1"; preferred_gcc_version=v"15", unix_staticlibs=true, windows_staticlibs=true, julia_compat="1.12")
 
 # Build trigger: 0
