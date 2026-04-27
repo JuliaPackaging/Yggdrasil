a5dce82489641df39e444bf2853db51ebc2cbc65
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-04-28 01:23:56 +0200

    [Expat] Update to 2.8.0 (#13613)

--- E/Expat/build_tarballs.jl
+++ E/Expat/build_tarballs.jl
@@ -3,12 +3,12 @@
 using BinaryBuilder
 
 name = "Expat"
-version = v"2.7.5"
+version = v"2.8.0"
 
 # Collection of sources required to build Expat
 sources = [
     ArchiveSource("https://github.com/libexpat/libexpat/releases/download/R_$(version.major)_$(version.minor)_$(version.patch)/expat-$(version).tar.xz",
-                  "1032dfef4ff17f70464827daa28369b20f6584d108bc36f17ab1676e1edd2f91"),
+                  "a37bfae0aa9775bd8521ebd85dc456d486f0ff31138f6c91fd902ea732624542"),
 ]
 
 # Bash recipe for building across all platforms
