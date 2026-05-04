e0f7bbc5e06257256cd6dbd24e66150579a21c4b
Evan Follman <evan.follman@gmail.com>
2026-05-04 12:28:18 -0500

    libsie_z: zig_jll dep fixes

--- L/libsie_z/build_tarballs.jl
+++ L/libsie_z/build_tarballs.jl
@@ -16,6 +16,9 @@ sources = [
 # Runs inside the BinaryBuilder sandbox. `${target}` is the BB GNU triple,
 # which `build.zig` translates to a Zig target via `-Dtriple=`.
 script = raw"""
+export PATH="${host_prefix}/bin/zig:$PATH"
+export ZIG_GLOBAL_CACHE_DIR=$WORKSPACE/.zig-cache
+
 cd $WORKSPACE/srcdir/libsie-z*
 
 zig build jll \
@@ -34,7 +37,6 @@ products = [
     LibraryProduct(["libsie", "sie"], :libsie_z),
 ]
 
-#waiting on zig_jll to have musl linux artifact available
 dependencies = BinaryBuilder.AbstractDependency[
     HostBuildDependency(
         PackageSpec(name = "zig_jll", version = zig_jll_version)
