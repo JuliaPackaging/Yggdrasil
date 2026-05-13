0f439d28cae2114d98b7a2b482d6c15d3acd624d
Tim Besard <tim.besard@gmail.com>
2026-05-13 16:41:19 +0200

    CUDA_Driver_jll: Various improvements. (#13710)
    - Make the driver inspection utility reusable
    - Also have it inspect the driver's full path (for include_dependency)
    - Don't have it inspect device capabilities, as that's unneeded
      (drivers are backwards compatible) and hurts load time.

--- C/CUDA/CUDA_Driver/build_tarballs.jl
+++ C/CUDA/CUDA_Driver/build_tarballs.jl
@@ -84,24 +84,16 @@ non_platform_ARGS = filter(arg -> startswith(arg, "--"), ARGS)
 # `--register` should only be passed to the latest `build_tarballs` invocation
 non_reg_ARGS = filter(arg -> arg != "--register", non_platform_ARGS)
 
+augment_platform_block = """
+augment_platform! = identity
+
+$(read(joinpath(@__DIR__, "inspect_driver.jl"), String))
+"""
+
 for (i,build) in enumerate(builds)
     build_tarballs(i == lastindex(builds) ? non_platform_ARGS : non_reg_ARGS,
                    name, cuda_version, build.sources, script,
                    build.platforms, products, dependencies;
                    skip_audit=true, init_block, julia_compat="1.10",
-                   augment_platform_block="""
-                   # Precompile the process-spawning and version-parsing hot path
-                   precompile(Tuple{typeof(Base.cmd_gen), Tuple{Tuple{Base.Cmd}, Tuple{String}, Tuple{Bool}, Tuple{Array{String, 1}}}})
-                   precompile(Tuple{typeof(Base.arg_gen), Bool})
-                   precompile(Tuple{typeof(Base.read), Base.Cmd, Type{String}})
-                   precompile(Tuple{typeof(Base.readlines), Base.IOBuffer})
-                   precompile(Tuple{typeof(Base.push!), Array{Base.VersionNumber, 1}, Base.VersionNumber})
-                   precompile(Tuple{typeof(Base.Iterators.enumerate), Array{Base.VersionNumber, 1}})
-                   precompile(Tuple{typeof(Base.iterate), Base.Iterators.Enumerate{Array{Base.VersionNumber, 1}}})
-                   precompile(Tuple{typeof(Base.iterate), Base.Iterators.Enumerate{Array{Base.VersionNumber, 1}}, Tuple{Int64, Int64}})
-                   precompile(Tuple{typeof(Base.indexed_iterate), Tuple{Int64, Base.VersionNumber}, Int64})
-                   precompile(Tuple{typeof(Base.indexed_iterate), Tuple{Int64, Base.VersionNumber}, Int64, Int64})
-
-                   augment_platform! = identity
-                   """)
+                   augment_platform_block)
 end
