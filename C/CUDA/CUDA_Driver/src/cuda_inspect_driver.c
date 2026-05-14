86223935bdca01ed5715ce70789230f966883a93
Tim Besard <tim.besard@gmail.com>
2026-05-14 17:53:56 +0200

    CUDA: Also build driver JLL on Windows.

--- C/CUDA/CUDA_Driver/src/cuda_inspect_driver.c
+++ C/CUDA/CUDA_Driver/src/cuda_inspect_driver.c
@@ -1,15 +1,54 @@
 /* Script to check the usability of CUDA drivers. */
 
-#define _GNU_SOURCE
-#include <dlfcn.h>
-#include <link.h>
+/* On glibc-based Linux, dlinfo/RTLD_DI_LINKMAP require _GNU_SOURCE. The build
+ * script passes -D_GNU_SOURCE in CFLAGS. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// These match the flags of Libdl.dlopen():
-// https://docs.julialang.org/en/v1/stdlib/Libdl/#Base.Libc.Libdl.dlopen
-#define DLOPEN_FLAGS (RTLD_LAZY | RTLD_DEEPBIND | RTLD_LOCAL)
+#ifdef _WIN32
+  #include <windows.h>
+
+  typedef HMODULE lib_handle_t;
+  static lib_handle_t lib_open(const char *path) {
+      return LoadLibraryA(path);
+  }
+  static void *lib_sym(lib_handle_t h, const char *name) {
+      return (void *)GetProcAddress(h, name);
+  }
+  static int lib_path(lib_handle_t h, char *out, size_t out_size) {
+      DWORD n = GetModuleFileNameA(h, out, (DWORD)out_size);
+      if (n == 0 || n >= out_size) {
+          return -1;
+      }
+      return 0;
+  }
+#else
+  #include <dlfcn.h>
+  #include <link.h>
+
+  /* These match the flags of Libdl.dlopen():
+   * https://docs.julialang.org/en/v1/stdlib/Libdl/#Base.Libc.Libdl.dlopen */
+  #define DLOPEN_FLAGS (RTLD_LAZY | RTLD_DEEPBIND | RTLD_LOCAL)
+
+  typedef void *lib_handle_t;
+  static lib_handle_t lib_open(const char *path) {
+      return dlopen(path, DLOPEN_FLAGS);
+  }
+  static void *lib_sym(lib_handle_t h, const char *name) {
+      return dlsym(h, name);
+  }
+  static int lib_path(lib_handle_t h, char *out, size_t out_size) {
+      struct link_map *lm = NULL;
+      if (dlinfo(h, RTLD_DI_LINKMAP, &lm) != 0 || lm == NULL || lm->l_name == NULL) {
+          return -1;
+      }
+      strncpy(out, lm->l_name, out_size - 1);
+      out[out_size - 1] = '\0';
+      return 0;
+  }
+#endif
 
 const int DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MAJOR = 75;
 const int DEVICE_ATTRIBUTE_COMPUTE_CAPABILITY_MINOR = 76;
@@ -31,12 +70,12 @@ int main(int argc, char *argv[]) {
     int inspect_devices = (strcmp(argv[2], "true") == 0 || strcmp(argv[2], "1") == 0);
 
     for (int i = 3; i < argc; i++) {
-        if (dlopen(argv[i], DLOPEN_FLAGS) == NULL) {
+        if (lib_open(argv[i]) == NULL) {
             return -1;
         }
     }
 
-    void *library_handle = dlopen(driver, DLOPEN_FLAGS);
+    lib_handle_t library_handle = lib_open(driver);
     if (library_handle == NULL) {
         return -1;
     }
@@ -44,13 +83,13 @@ int main(int argc, char *argv[]) {
     /* Report the resolved absolute path of the loaded driver, so that callers
      * can register it as a file dependency (e.g. for cache invalidation) without
      * having to dlopen the library themselves. */
-    struct link_map *lm = NULL;
-    if (dlinfo(library_handle, RTLD_DI_LINKMAP, &lm) != 0 || lm == NULL) {
+    char path_buf[4096];
+    if (lib_path(library_handle, path_buf, sizeof(path_buf)) != 0) {
         return -1;
     }
-    printf("%s\n", lm->l_name);
+    printf("%s\n", path_buf);
 
-    cuInit_t cuInit = (cuInit_t)dlsym(library_handle, "cuInit");
+    cuInit_t cuInit = (cuInit_t)lib_sym(library_handle, "cuInit");
     if (cuInit == NULL) {
         return -2;
     }
@@ -59,7 +98,7 @@ int main(int argc, char *argv[]) {
         return -2;
     }
 
-    cuDriverGetVersion_t cuDriverGetVersion = (cuDriverGetVersion_t)dlsym(library_handle, "cuDriverGetVersion");
+    cuDriverGetVersion_t cuDriverGetVersion = (cuDriverGetVersion_t)lib_sym(library_handle, "cuDriverGetVersion");
     if (cuDriverGetVersion == NULL) {
         return -3;
     }
@@ -75,7 +114,7 @@ int main(int argc, char *argv[]) {
     printf("%d.%d.%d\n", major, minor, patch);
 
     if (inspect_devices) {
-        cuDeviceGetCount_t cuDeviceGetCount = (cuDeviceGetCount_t)dlsym(library_handle, "cuDeviceGetCount");
+        cuDeviceGetCount_t cuDeviceGetCount = (cuDeviceGetCount_t)lib_sym(library_handle, "cuDeviceGetCount");
         if (cuDeviceGetCount == NULL) {
             return -4;
         }
@@ -85,8 +124,8 @@ int main(int argc, char *argv[]) {
             return -4;
         }
 
-        cuDeviceGet_t cuDeviceGet = (cuDeviceGet_t)dlsym(library_handle, "cuDeviceGet");
-        cuDeviceGetAttribute_t cuDeviceGetAttribute = (cuDeviceGetAttribute_t)dlsym(library_handle, "cuDeviceGetAttribute");
+        cuDeviceGet_t cuDeviceGet = (cuDeviceGet_t)lib_sym(library_handle, "cuDeviceGet");
+        cuDeviceGetAttribute_t cuDeviceGetAttribute = (cuDeviceGetAttribute_t)lib_sym(library_handle, "cuDeviceGetAttribute");
         if (cuDeviceGet == NULL || cuDeviceGetAttribute == NULL) {
             return -5;
         }
