73786ea7eefe699f5f0840249f0a5bc5f65feaf1
Christopher Rowley <cjdrwly@gmail.com>
2026-05-18 14:02:10 +0100

    Update P/pixi/build_tarballs.jl
    Co-authored-by: Mosè Giordano <765740+giordano@users.noreply.github.com>

--- P/pixi/build_tarballs.jl
+++ P/pixi/build_tarballs.jl
@@ -80,4 +80,4 @@ dependencies = Dependency[]
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               julia_compat="1.6", lock_microarchitecture=false, lazy_artifacts=true)
+               julia_compat="1.6", lazy_artifacts=true)
