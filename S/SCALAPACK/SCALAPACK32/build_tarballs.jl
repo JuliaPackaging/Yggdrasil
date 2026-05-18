79d16cfab9b0adb24aedbccc4b113adbbc525ed1
Viral B. Shah <viral@mayin.org>
2026-05-18 19:53:36 +0000

    SCALAPACK32: bump ygg_version to 2.2.4

--- S/SCALAPACK/SCALAPACK32/build_tarballs.jl
+++ S/SCALAPACK/SCALAPACK32/build_tarballs.jl
@@ -5,6 +5,7 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 
 name = "SCALAPACK32"
 version = v"2.2.3"
+ygg_version = v"2.2.4"
 
 sources = [
   GitSource("https://github.com/Reference-ScaLAPACK/scalapack", "3e0da655fb07de5f1d76d6afb43f16ae17ca98c4"),  # v2.2.3
@@ -129,5 +130,5 @@ dependencies = [
 append!(dependencies, platform_dependencies)
 
 # Build the tarballs.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
+build_tarballs(ARGS, name, ygg_version, sources, script, platforms, products, dependencies;
                augment_platform_block, julia_compat="1.9", preferred_gcc_version=v"9")
