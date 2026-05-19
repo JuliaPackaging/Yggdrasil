4d688ebfca03ccf727e2dacf02b14e55f7b3c721
Viral B. Shah <viral@mayin.org>
2026-05-19 00:51:49 +0000

    SCALAPACK: derive ygg_version from upstream patch via 100*patch+offset

--- S/SCALAPACK/SCALAPACK/build_tarballs.jl
+++ S/SCALAPACK/SCALAPACK/build_tarballs.jl
@@ -5,10 +5,12 @@ include(joinpath(YGGDRASIL_DIR, "platforms", "mpi.jl"))
 
 name = "SCALAPACK"
 version = v"2.2.3"
-ygg_version = v"2.2.4"
+# ygg_version.patch = 100 * version.patch + offset; bump `offset` for rebuilds.
+offset = 0
+ygg_version = VersionNumber(version.major, version.minor, 100 * version.patch + offset)
 
 sources = [
-  GitSource("https://github.com/Reference-ScaLAPACK/scalapack", "3e0da655fb07de5f1d76d6afb43f16ae17ca98c4"),  # v2.2.3
+  GitSource("https://github.com/Reference-ScaLAPACK/scalapack", "3e0da655fb07de5f1d76d6afb43f16ae17ca98c4"),
 ]
 
 # Bash recipe for building across all platforms
