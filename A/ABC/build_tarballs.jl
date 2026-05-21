6e130a30fe2275a74acbe44c20daf7f3d4bfb4cf
PasoStudio73 <paso.studio73@gmail.com>
2026-05-21 11:52:21 +0200

    dubug cflags

--- A/ABC/build_tarballs.jl
+++ A/ABC/build_tarballs.jl
@@ -10,8 +10,9 @@ sources = [
     GitSource(
         "https://github.com/berkeley-abc/abc.git",
         "f4d870e109938fd1a283ecceea950bd9cd616f67"
-    ),
-    DirectorySource("./bundled"),
+        # "https://github.com/PasoStudio73/abc.git",
+        # "b74950f11b4b6064d7d09e4479dee68a9cbba55e"
+    )
 ]
 
 # ABC is setup for native builds (the arch_flags program and running the
@@ -26,41 +27,31 @@ sources = [
 
 script = raw"""
 EXFLGS="\
+-fPIC \
 -Wall \
 -Wno-unused-function \
 -Wno-write-strings \
 -Wno-sign-compare \
--Wno-psabi \
--Wno-error=psabi \
+-Wno-unused-but-set-variable \
 -DSIZEOF_INT=4 \
 -DABC_USE_CUDD=1 \
 -DABC_USE_PTHREADS \
--Wno-unused-but-set-variable \
--fPIC \
 -DSIZEOF_VOID_P=8 \
 -DSIZEOF_LONG=8 \
 -DLIN64"
 
 cd ${WORKSPACE}/srcdir/abc
 
-if [[ "${target}" == *-apple-* ]]; then
-    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/clock_gettime_darwin.patch
-fi
-
-if [[ "${target}" == *-musl* ]]; then
-    atomic_patch -p1 ${WORKSPACE}/srcdir/patches/stdint_musl.patch
-fi
-
 # it appears necessary to have the file(s) for the FileProduct(s) live in the destdir
 cp abc.rc ${prefix}/.
 
-make -j${nproc} ABC_USE_NO_READLINE=1 CFLAGS+="${EXFLGS}" libabc.so
+make -j${nproc} CC=clang CXX=clang++ ABC_USE_NO_READLINE=1 CFLAGS+="${EXFLGS}" libabc.so
 # the abc Makefile always makes a .so  Fix that here.
 mv libabc.so libabc.${dlext}
 mkdir -p "${libdir}"
 cp libabc.${dlext} ${libdir}
 
-make -j${nproc} ABC_USE_NO_READLINE=1 CFLAGS+="${EXFLGS}"
+make -j${nproc} CC=clang CXX=clang++ ABC_USE_NO_READLINE=1 CFLAGS+="${EXFLGS}"
 mkdir -p "${bindir}"
 cp abc${exeext} ${bindir}
 chmod +x ${bindir}/*
@@ -71,9 +62,13 @@ exit
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line
 # core-math uses unsigned __int128 which is unavailable on 32-bit platforms
+# platforms = supported_platforms(exclude= x -> (
+#     Sys.iswindows(x) ||
+#     Sys.isfreebsd(x) ||
+#     nbits(x) == 32
+# ))
 platforms = supported_platforms(exclude= x -> (
-    Sys.iswindows(x) ||
-    Sys.isfreebsd(x) ||
+    !Sys.isapple(x) ||
     nbits(x) == 32
 ))
 platforms = expand_cxxstring_abis(platforms)
@@ -93,5 +88,6 @@ build_tarballs(
     ARGS, name, version, sources, script,
     platforms, products, dependencies;
     julia_compat="1.6",
-    preferred_gcc_version=v"8"
+    # preferred_gcc_version=v"8"
+    preferred_llvm_version=v"13"
 )
