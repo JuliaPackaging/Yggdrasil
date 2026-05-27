b1bcae5f943f8a4747449888d886eaadaec73713
Ludovic Räss <61313342+luraess@users.noreply.github.com>
2026-05-27 17:15:22 +0200

    Update
    Co-authored-by: Ian McInerney <mcianster@gmail.com>

--- R/ROCmDeviceLibs/ROCmDeviceLibs@6.4.1/build_tarballs.jl
+++ R/ROCmDeviceLibs/ROCmDeviceLibs@6.4.1/build_tarballs.jl
@@ -20,7 +20,7 @@ function configure_build(version)
 
     ar x *.deb
     tar xf data.tar.*
-    mv opt/rocm-6.4.1/lib/llvm/lib/clang/19/lib/amdgcn ${prefix}/amdgcn
+    mv -v opt/rocm-6.4.1/lib/llvm/lib/clang/19/lib/amdgcn ${prefix}/amdgcn
     install_license opt/rocm-6.4.1/share/doc/ROCm-Device-Libs/LICENSE.TXT
     """
 
