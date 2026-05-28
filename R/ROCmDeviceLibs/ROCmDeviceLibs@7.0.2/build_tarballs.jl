4c050ffc2d942a3c639e158b53392782fdc3a29f
Ludovic Raess <ludovic.rass@gmail.com>
2026-05-28 16:00:42 +0200

    Fixup

--- R/ROCmDeviceLibs/ROCmDeviceLibs@7.0.2/build_tarballs.jl
+++ R/ROCmDeviceLibs/ROCmDeviceLibs@7.0.2/build_tarballs.jl
@@ -9,8 +9,8 @@ const PRODUCTS = [FileProduct("amdgcn/bitcode/", :bitcode_path)]
 # TODO bundle multiple devlibs & select based on Julia LLVM.
 const URLS = Dict(
     v"7.0.2" => (
-                 "https://repo.radeon.com/rocm/apt/7.0.2/pool/main/r/rocm-device-libs7.0.2/rocm-device-libs7.0.2_1.0.0.70002-56~22.04_amd64.deb",
-                 "536f54c9073c1d88d7583c61e4ca6436d491dac850d7268c4185df51947bd01d",
+        "https://repo.radeon.com/rocm/apt/7.0.2/pool/main/r/rocm-device-libs7.0.2/rocm-device-libs7.0.2_1.0.0.70002-56~22.04_amd64.deb",
+        "536f54c9073c1d88d7583c61e4ca6436d491dac850d7268c4185df51947bd01d",
     )
 )
 
@@ -20,8 +20,7 @@ function configure_build(version)
 
     ar x *.deb
     tar xf data.tar.*
-    mkdir -p ${WORKSPACE}/destdir/amdgcn
-    install -Dv opt/rocm-7.0.2/lib/llvm/lib/clang/20/lib/amdgcn/* ${WORKSPACE}/destdir/amdgcn
+    cp -rv $(find opt -type d -name "amdgcn" | head -1) ${prefix}/amdgcn
     install_license opt/rocm-7.0.2/share/doc/ROCm-Device-Libs/LICENSE.TXT
     """
 
