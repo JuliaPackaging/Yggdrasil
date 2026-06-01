bcd303482b9c8f18a14e9eba06e2fc8ede903975
Tom Finke <tom.finke@webterra.de>
2026-06-01 11:05:10 +0200

    [OpenFHE_julia] and [OpenFHE_julia_int128] version 0.6.1 with lower and upper compatibility bounds (hpyhen specifiers)

--- O/openfhe_julia/build_tarballs.jl
+++ O/openfhe_julia/build_tarballs.jl
@@ -5,13 +5,13 @@ include("common.jl")
 # If you make changes in this file, e.g., to release a new version,
 # be sure to also release a new version of `openfhe_julia_int128` as well (see `../openfhe_julia_int128/build_tarballs.jl`)
 name = "openfhe_julia"
-version = v"0.6.0"
+version = v"0.6.1"
 
-git_hash = "66b903eca1c2742268c740581bfaae4a49cb444d"
+git_hash = "4ab65278eecee883b0211cde40210286341cb771"
 
 sources, script, platforms, products, dependencies = prepare_openfhe_julia_build(name, git_hash)
 
-push!(dependencies, Dependency(PackageSpec(name="OpenFHE_jll", uuid="a2687184-f17b-54bc-b2bb-b849352af807"); compat="1.5.0"))
+push!(dependencies, Dependency(PackageSpec(name="OpenFHE_jll", uuid="a2687184-f17b-54bc-b2bb-b849352af807"); compat="1.5.0 - 1.5.1"))
 
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
