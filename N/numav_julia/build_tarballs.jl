cdb93a243ffce85b540f9d4bf6732bc49375ed70
Matheus Machado Fiuza <matheusmachadofiuza@gmail.com>
2026-06-01 12:58:25 -0300

    [numav_julia] New package

---
+++ N/numav_julia/build_tarballs.jl
@@ -0,0 +1,83 @@
+# Copyright (c) 2026 Matheus Machado Fiuza <matheusmachadofiuza@gmail.com>
+
+using BinaryBuilder, Pkg
+
+name = "numav_julia"
+version = v"0.1.0"
+
+sources = [ 
+    GitSource(
+        "https://github.com/mmfiuza/numav.git", 
+        "f0f0b67e98ad47b2a0e9b451f9e97fffa7d6bc8a"
+    )
+]
+
+script = raw"""
+    # oneMKL is picked for the available platforms, otherwise Eigen is picked
+    if [[ ${target} == x86_64-linux-gnu* || ${target} == x86_64-w64* ]];
+    then
+        SOLVER=ONEMKL
+    else
+        SOLVER=EIGEN
+    fi
+    
+    cd ${WORKSPACE}/srcdir/numav && mkdir build && cd build
+    cmake \
+        -D CMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+        -D CMAKE_BUILD_TYPE=Release \
+        -D SOLVER=${SOLVER} \
+        -D BIND_JULIA=TRUE \
+        -D Julia_PREFIX=${prefix} \
+        -D JlCxx_DIR=${prefix}/lib/cmake/JlCxx \
+        -D CMAKE_FIND_ROOT_PATH=${prefix} \
+        -D CMAKE_INSTALL_PREFIX=${prefix} \
+        ..
+    cmake --build . --config Release --target install -- -j${nproc}
+"""
+
+julia_versions = [ "1.10", "1.11", "1.12", "1.13", "1.14" ]
+
+platforms = Platform[]
+for v in VersionNumber.(julia_versions)
+    append!(platforms,
+        [
+            Platform("x86_64" , "linux"  ; libc=:glibc, julia_version=v),
+            Platform("aarch64", "linux"  ; libc=:glibc, julia_version=v),
+            Platform("x86_64" , "linux"  ; libc=:musl , julia_version=v),
+            Platform("aarch64", "linux"  ; libc=:musl , julia_version=v),
+            Platform("x86_64" , "windows";              julia_version=v),
+            Platform("x86_64" , "macos"  ;              julia_version=v),
+            Platform("aarch64", "macos"  ;              julia_version=v),
+        ]
+    )
+end
+platforms = expand_cxxstring_abis(platforms)
+
+products = [ LibraryProduct("libnumav_julia", :libnumav_julia) ]
+
+mkl_platforms = filter(p ->
+    (arch(p) == "x86_64" && Sys.islinux(p) && libc(p) == "glibc") ||
+    (arch(p) == "x86_64" && Sys.iswindows(p)),
+    platforms
+)
+
+dependencies = [
+    Dependency("libcxxwrap_julia_jll", compat="0.14.10"),
+    Dependency("HDF5_jll", compat="2.1"),
+    Dependency("spdlog_jll", compat="1.15.0"),
+    BuildDependency(PackageSpec(name="libjulia_jll", version="1.11.0")),
+    BuildDependency(PackageSpec(name="Eigen_jll", version="5.0.1")),
+
+    # oneMKL for x86_64-linux-gnu and x86_64-w64
+    Dependency("MKL_jll", compat="=2025.2.0"; platforms=mkl_platforms),
+    BuildDependency(
+        PackageSpec(name="MKL_Headers_jll", version="2025.2.0");
+        platforms=mkl_platforms
+    ),
+]
+
+build_tarballs(
+    ARGS, name, version, sources, script, platforms, products, dependencies;
+    preferred_gcc_version=v"10",
+    julia_compat=join("~".*julia_versions, ", ")
+)
