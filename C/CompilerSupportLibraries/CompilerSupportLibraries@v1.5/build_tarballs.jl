c27c30a2d6f5625c7e7a2e45af7c971584027fb5
Keno Fischer <keno@juliacomputing.com>
2026-06-25 18:31:58 -0400

    [CSL] Rebuild again
    To pick up corrected mingw version

--- C/CompilerSupportLibraries/CompilerSupportLibraries@v1.5/build_tarballs.jl
+++ C/CompilerSupportLibraries/CompilerSupportLibraries@v1.5/build_tarballs.jl
@@ -2,4 +2,4 @@ include("../common.jl")
 
 build_csl(ARGS, v"1.5.5"; preferred_gcc_version=v"15", unix_staticlibs=true, windows_staticlibs=true, julia_compat="1.12")
 
-# Build trigger: 1
+# Build trigger: 2
