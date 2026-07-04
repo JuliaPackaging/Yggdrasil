ab6c9051cba6f2cecfcbcfe12132e5675dd5b766
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-07-04 12:17:33 +0200

    [msolve] Touch recipe to demonstrate build failures

--- M/msolve/build_tarballs.jl
+++ M/msolve/build_tarballs.jl
@@ -54,3 +54,5 @@ dependencies = [
 # Build the tarballs, and possibly a `build.jl` as well.
 build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
   julia_compat="1.6", preferred_gcc_version = v"8")
+
+# Trigger build: 1
