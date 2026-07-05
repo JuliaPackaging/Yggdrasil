a32e1fefe11007c7fecdde4b6f4380e3bc967cf8
Tim Besard <tim.besard@gmail.com>
2026-07-05 20:05:12 +0200

    PoCL: Bump for printf JIT fix.

--- P/pocl/pocl_next/build_tarballs.jl
+++ P/pocl/pocl_next/build_tarballs.jl
@@ -22,7 +22,7 @@ llvm_version = v"20.1.2"
 sources = [
     DirectorySource("./bundled"),
     GitSource("https://github.com/JuliaGPU/pocl",
-              "82c74c5c4732f7f0a8afcef86f2ccdb467e48582"),
+              "cfe9b5d3e6c67047ecf7c872dec5308e19d4a9a1"),
     # vendored SPIR-V translator, built as a static library against our LLVM (see
     # common.jl); this commit is the LLVM-20.1-compatible revision (matches
     # LLVM_full_jll 20.1.2).
