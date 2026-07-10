e1bff867df9e900326af7ae4011e64b922cff3db
Tim Besard <tim.besard@gmail.com>
2026-07-10 21:37:12 +0200

    NCCL: Upgrade to v2.30.7.

--- N/NCCL/build_tarballs.jl
+++ N/NCCL/build_tarballs.jl
@@ -9,10 +9,10 @@ include(joinpath(YGGDRASIL_DIR, "fancy_toys.jl"))
 include(joinpath(YGGDRASIL_DIR, "platforms", "cuda.jl"))
 
 name = "NCCL"
-version = v"2.28.9"
+version = v"2.30.7"
 
 git_sources = [
-    GitSource("https://github.com/NVIDIA/nccl.git", "dbc86fd06e8b0c4517b95d8958a09ccacf9520c9"),
+    GitSource("https://github.com/NVIDIA/nccl.git", "73cf112295c33aee2b895f329f592f2a9b4b0f97"),
     DirectorySource("./bundled/")
 ]
 
