df573a6270437aef84bb5cde538ab765fad2785b
Torfi Þorgrímsson <torfit@gmail.com>
2026-07-12 22:01:12 +0000

    Update build_tarballs.jl
    bug fix

--- S/SimpleBLE/build_tarballs.jl
+++ S/SimpleBLE/build_tarballs.jl
@@ -53,4 +53,4 @@ products = [
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products; julia_compat="1.6", preferred_gcc_version = v"7.1.0")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, []; julia_compat="1.6", preferred_gcc_version = v"7.1.0")
