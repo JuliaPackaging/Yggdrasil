b72e1e3f1907d909f8ad17b8d9978b104505f9a3
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-07-13 16:16:38 +0200

    Require newer gcc

--- L/libpcap/build_tarballs.jl
+++ L/libpcap/build_tarballs.jl
@@ -18,7 +18,6 @@ cd libpcap
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make
 make install
-exit
 """
 
 # These are the platforms we will build for by default, unless further
@@ -37,4 +36,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"5")
