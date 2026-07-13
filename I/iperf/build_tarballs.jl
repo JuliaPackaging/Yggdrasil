9bedaa16c8ba19473b51fe6f50ce2867f2421b50
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-07-13 17:29:01 +0200

    Patch musl failures

--- I/iperf/build_tarballs.jl
+++ I/iperf/build_tarballs.jl
@@ -1,18 +1,20 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
-using BinaryBuilder
+using BinaryBuilder, Pkg
 
 name = "iperf"
 version = v"3.21"
 
 # Collection of sources required to complete build
 sources = [
-    ArchiveSource("https://github.com/esnet/iperf/releases/download/$(version.major).$(version.minor)/iperf-$(version.major).$(version.minor).tar.gz", "656e4405ebd620121de7ceca3eaf43a88f79ea1b857d041a6a0b1314801acdd8")
+    ArchiveSource("https://github.com/esnet/iperf/releases/download/$(version.major).$(version.minor)/iperf-$(version.major).$(version.minor).tar.gz", "656e4405ebd620121de7ceca3eaf43a88f79ea1b857d041a6a0b1314801acdd8"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir/iperf-*
+atomic_patch -p1 $WORKSPACE/srcdir/patches/0001-use-uint64_t-for-atomic-fallback.patch # work around https://github.com/esnet/iperf/issues/2060
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target} --disable-profiling
 make -j${nproc}
 make install
