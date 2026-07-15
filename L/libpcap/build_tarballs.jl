47023515526be00f8b93a24c0ee94bb54b2230e6
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-07-15 11:21:44 +0200

    [libpcap] Bump to v1.10.5 (#14203)

--- L/libpcap/build_tarballs.jl
+++ L/libpcap/build_tarballs.jl
@@ -3,21 +3,21 @@
 using BinaryBuilder, Pkg
 
 name = "libpcap"
-version = v"1.10.4"
+version = v"1.10.5"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/the-tcpdump-group/libpcap.git", "104271ba4a14de6743e43bcf87536786d8fddea4")
+    GitSource("https://github.com/the-tcpdump-group/libpcap.git", "bbcbc9174df3298a854daee2b3e666a4b6e5383a")
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
 cd libpcap
+./autogen.sh
 ./configure --prefix=${prefix} --build=${MACHTYPE} --host=${target}
 make
 make install
-exit
 """
 
 # These are the platforms we will build for by default, unless further
@@ -36,4 +36,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6", preferred_gcc_version=v"5")
