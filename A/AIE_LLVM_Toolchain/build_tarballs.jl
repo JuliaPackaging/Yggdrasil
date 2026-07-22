a7aaf6bece6353882f59dafbfc2b9e626f3cd2f4
Simeon David Schaub <simeon@schaub.rocks>
2026-07-22 20:58:50 +0200

    rename to AIE_LLVM_Toolchain_jll

--- A/AIE_LLVM_Toolchain/build_tarballs.jl
+++ A/AIE_LLVM_Toolchain/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder, Pkg
 
-name = "AIE_LLVM_Backend"
+name = "AIE_LLVM_Toolchain"
 version = v"21.0.0"
 
 # Peano is the AMD/Xilinx fork of LLVM for compiling C/C++ to AIEngine processors.
