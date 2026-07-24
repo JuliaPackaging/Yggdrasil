37db0b323a8f419d45dd3e53df4207f3f1f0af88
Simeon David Schaub <simeon@schaub.rocks>
2026-07-24 11:26:09 +0200

    [xrt_cxxwrap] rebuild for xrt@2.26
    Also includes the gcc bump to avoid bad any_cast errors. I believe
    the newer gcc version is the reason we have to specify `dont_dlopen`

--- X/xrt_cxxwrap/xrt_cxxwrap@2.26/build_tarballs.jl
+++ X/xrt_cxxwrap/xrt_cxxwrap@2.26/build_tarballs.jl
@@ -1,4 +1,4 @@
 # Note that this script can accept some limited command-line arguments, run
 # `julia build_tarballs.jl --help` to see a usage message.
 include("../common.jl")
-build_xrt_cxxwrap(ARGS, v"2.23.0")
+build_xrt_cxxwrap(ARGS, v"2.26.0")
