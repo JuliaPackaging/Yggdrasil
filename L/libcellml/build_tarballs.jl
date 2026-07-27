343d15935fee805f90e9eb11f646c66364e5b37a
Hugh Sorby <h.sorby@auckland.ac.nz>
2026-07-27 19:44:14 +1200

    Add ZLIB-static.cmake touch logic for environments
    Added checks to touch ZLIB-static.cmake for specific environments.

--- L/libcellml/build_tarballs.jl
+++ L/libcellml/build_tarballs.jl
@@ -15,15 +15,17 @@ sources = [
 # https://libcellml.org/documentation/installation/build_from_source
 script = raw"""
 
+# ZLIB-static.cmake is missing from the following environments. We touch the file to allow the configuration to succeed.
+if [ -d /opt/aarch64-apple-darwin20/aarch64-apple-darwin20/sys-root/usr/local/lib/cmake/zlib/ ]; then
+  touch /opt/aarch64-apple-darwin20/aarch64-apple-darwin20/sys-root/usr/local/lib/cmake/zlib/ZLIB-static.cmake
+fi
 if [ -d /workspace/MacOSX10.15.sdk/usr/local/lib/cmake/zlib/ ]; then
-    ls /workspace/MacOSX10.15.sdk/usr/local/lib/cmake/zlib/
-    cat /workspace/MacOSX10.15.sdk/usr/local/lib/cmake/zlib/ZLIBConfig.cmake
     touch /workspace/MacOSX10.15.sdk/usr/local/lib/cmake/zlib/ZLIB-static.cmake
 fi
-
+if [ -d /opt/aarch64-unknown-freebsd14.1/aarch64-unknown-freebsd14.1/sys-root/usr/local/lib/cmake/zlib/ ]; then
+    touch /opt/aarch64-unknown-freebsd14.1/aarch64-unknown-freebsd14.1/sys-root/usr/local/lib/cmake/zlib/ZLIB-static.cmake
+fi
 if [ -d /opt/x86_64-unknown-freebsd13.4/x86_64-unknown-freebsd13.4/sys-root/usr/local/lib/cmake/zlib/ ]; then
-    ls /opt/x86_64-unknown-freebsd13.4/x86_64-unknown-freebsd13.4/sys-root/usr/local/lib/cmake/zlib/
-    cat /opt/x86_64-unknown-freebsd13.4/x86_64-unknown-freebsd13.4/sys-root/usr/local/lib/cmake/zlib/ZLIBConfig.cmake
     touch /opt/x86_64-unknown-freebsd13.4/x86_64-unknown-freebsd13.4/sys-root/usr/local/lib/cmake/zlib/ZLIB-static.cmake
 fi
 
