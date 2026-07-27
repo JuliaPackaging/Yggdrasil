3acbaf1bbcb249d8c896a452073823354691b210
Erik Schnetter <schnetter@gmail.com>
2026-07-27 14:57:28 +0200

    ImageMagick: New version 7.1.2-27 (#14286)

--- I/ImageMagick/ImageMagick@7/build_tarballs.jl
+++ I/ImageMagick/ImageMagick@7/build_tarballs.jl
@@ -2,7 +2,7 @@
 # `julia build_tarballs.jl --help` to see a usage message.
 using BinaryBuilder
 name = "ImageMagick"
-upstream_version = v"7.1.2-26"
+upstream_version = v"7.1.2-27"
 version = VersionNumber(
     upstream_version.major,
     upstream_version.minor,
@@ -12,7 +12,7 @@ version = VersionNumber(
 # Collection of sources required to build imagemagick
 sources = [
     GitSource("https://github.com/ImageMagick/ImageMagick",
-              "38ba210c5e5a660013d632a3d33c431e658befcc"),
+              "b661ac969aa3a0d326690f95830aab246569f090"),
     DirectorySource("./bundled"),
 ]
 
