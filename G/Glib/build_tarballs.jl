01fe229ad7e9c5f78b5bf3d0e44509021651891f
Erik Schnetter <schnetter@gmail.com>
2026-07-30 11:33:23 -0400

    Glib: Correct apple and mingw32 build errors

--- G/Glib/build_tarballs.jl
+++ G/Glib/build_tarballs.jl
@@ -4,12 +4,13 @@ const YGGDRASIL_DIR = "../.."
 include(joinpath(YGGDRASIL_DIR, "platforms", "macos_sdks.jl"))
 
 name = "Glib"
-version = v"2.88.1"
+# Odd minor versions are development versions; skip them
+version = v"2.88.3"
 
 # Collection of sources required to build Glib
 sources = [
     ArchiveSource("https://ftp.gnome.org/pub/gnome/sources/glib/$(version.major).$(version.minor)/glib-$(version).tar.xz",
-                  "51ab804c56f6eab3e5045c774d1290ac5e4c923d4f9a3d8e33123bee45c1840e"),
+                  "ab24d24e698dfa1e408b7bcdb508f4aafc906185a8b8ce72fdf79bbbdc9b383b"),
     DirectorySource("bundled"),
 ]
 
@@ -21,6 +22,22 @@ install_license COPYING
 # meson shouldn't be so opinionated (mesonbuild/meson#4542 is incomplete)
 sed -i '/Werror=unused-command-line-argument/d' /usr/lib/python3.9/site-packages/mesonbuild/compilers/mixins/clang.py
 
+# tell meson about the target system
+if [[ "${target}" == *-darwin* ]]; then
+    sed -i "/^\[host_machine\]/a subsystem = 'macos'" "$MESON_TARGET_TOOLCHAIN"
+
+    # GLib checks MAC_OS_X_VERSION_MIN_REQUIRED via AvailabilityMacros.h, which is
+    # only set correctly if -mmacosx-version-min is passed explicitly.
+    # require_macos_sdk() only ensures the SDK is mounted; it does not add this
+    # flag to the Meson cross file, so add it ourselves for every relevant
+    # compile/link args array.
+    MACOS_MIN_VERSION="10.13"   # keep in sync with require_macos_sdk() call below
+    for args in c_args cpp_args objc_args objcpp_args fortran_args \
+                c_link_args cpp_link_args objc_link_args objcpp_link_args fortran_link_args; do
+        sed -i "s/\(${args} = \[[^]]*\)\]/\1, '-mmacosx-version-min=${MACOS_MIN_VERSION}']/" "${MESON_TARGET_TOOLCHAIN}"
+    done
+fi
+
 if [[ "${target}" == *-freebsd* ]]; then
     # Adapt patch relative to `xattr` from
     # http://cvsweb.netbsd.org/bsdweb.cgi/pkgsrc/devel/glib2/patches/patch-meson.build?rev=1.2&content-type=text/x-cvsweb-markup.
@@ -28,6 +45,15 @@ if [[ "${target}" == *-freebsd* ]]; then
     #     Don't fail if getxattr is not available. The code is already ready
     #     for this case with some small configure changes.
     atomic_patch -p1 ../patches/freebsd-have_xattr.patch
+fi
+
+if [[ "${target}" == *-mingw32* ]]; then
+    # gwin32packageparser.c needs windows.applicationmodel.core.h (WinRT/UWP
+    # headers), which this mingw-w64 toolchain shard lacks. It's only used for
+    # UWP package-identity enumeration (gwin32appinfo.c's uwp_package_cb call),
+    # not core GLib functionality, so stub it out when the header is missing
+    # rather than failing the whole build.
+    cp ../files/gwin32packageparser.c gio/gwin32packageparser.c
 fi
 
 mkdir build_glib && cd build_glib
