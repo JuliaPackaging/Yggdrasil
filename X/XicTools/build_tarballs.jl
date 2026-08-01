007493b95a17c2c292934258fa05ff101d2bf101
Kevin P. O'Brien <kpobrien@mit.edu>
2026-08-01 18:23:49 -0400

    update to v4.3.23

--- X/XicTools/build_tarballs.jl
+++ X/XicTools/build_tarballs.jl
@@ -3,55 +3,47 @@
 using BinaryBuilder
 
 name = "XicTools"
-version = v"4.3.19"
+version = v"4.3.23"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/wrcad/xictools", "c7a50a5fcd71966730e45a5358b8507227ae098c"),
-    DirectorySource("./bundled")
+    GitSource("https://github.com/wrcad/xictools",
+              "a51c6b6386ad923b9da4e66d11513dc2e0fe3997"),
+    DirectorySource("./bundled"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd ${WORKSPACE}/srcdir/xictools/
 cp Makefile.sample Makefile
-atomic_patch -p0 ${WORKSPACE}/srcdir/patches/Makefile.patch
-atomic_patch -p0 ${WORKSPACE}/srcdir/patches/malloc-2.8.6.c.patch
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/malloc-2.8.6.c.patch
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/Makefile.patch
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/local_malloc-free-init.patch
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/vardb-stdc-format-macros.patch
+atomic_patch -p1 ${WORKSPACE}/srcdir/patches/hcimlib-no-x11.patch
 update_configure_scripts
 make config
 make all
-make prefix="${prefix}/usr" install
-install_license ${WORKSPACE}/srcdir/xictools/license/LICENSE-2.0.txt
-cd ${prefix}/usr/xictools
-FILES="wrspice.current/bin/mmjco
-wrspice.current/bin/multidec
-wrspice.current/bin/printtoraw
-wrspice.current/bin/proc2mod
-wrspice.current/bin/wrspice
-wrspice.current/bin/wrspiced
-bin/admsXml
-bin/busgen
-bin/capgen
-bin/cubegen
-bin/fastcap
-bin/fasthenry
-bin/fcpp
-bin/lstpack
-bin/lstunpack
-bin/mrouter
-bin/pipedgen
-bin/pyragen
-bin/vl
-xic.current/bin/wrencode
-xic.current/bin/wrdecode
-xic.current/bin/wrsetpass
-xic.current/bin/xic
-bin/zbuf"
-for file in $FILES; do
-    install -Dvm 755 "${file}" "${bindir}/$(basename ${file})"
-    rm "${file}"
-    ln -s "${bindir}/$(basename ${file})" "${file}"
+install -d ${prefix}/xictools/bin
+make INSTALL_PREFIX=${prefix} install
+install -m755 wrspice/bin/wrspice ${prefix}/xictools/wrspice.current/bin/wrspice
+rm -f ${prefix}/xictools/wrspice.current/bin/wrspice.sh
+rm -f ${prefix}/xictools/bin/wrspice
+mkdir -p ${bindir}
+cd ${prefix}
+for tool in wrspice wrspiced csvtoraw multidec printtoraw mmjco; do
+    mv xictools/wrspice.current/bin/${tool} ${bindir}/${tool}
+    ln -sfn ../../../bin/${tool} xictools/wrspice.current/bin/${tool}
 done
+for tool in admsXml vl; do
+    mv xictools/bin/${tool} ${bindir}/${tool}
+    ln -sfn ../../bin/${tool} xictools/bin/${tool}
+done
+for tool in wrspice wrspiced csvtoraw multidec printtoraw mmjco; do
+    ln -sfn ../wrspice.current/bin/${tool} xictools/bin/${tool}
+done
+ln -sfn wrspice.current xictools/wrspice
+install_license ${WORKSPACE}/srcdir/xictools/license/LICENSE-2.0.txt
 """
 
 
@@ -72,36 +64,18 @@ platforms = expand_cxxstring_abis(platforms)
 
 # The products that we will ensure are always built
 products = [
-	    ExecutableProduct("mmjco", :mmjco),
-	    ExecutableProduct("multidec", :multidec),
-	    ExecutableProduct("printtoraw", :printtoraw),
-	    ExecutableProduct("proc2mod", :proc2mod),
-	    ExecutableProduct("wrspice", :wrspice),
-	    ExecutableProduct("wrspiced", :wrspiced),
-	    ExecutableProduct("admsXml", :admsXml),
-	    ExecutableProduct("busgen", :busgen),
-	    ExecutableProduct("capgen", :capgen),
-	    ExecutableProduct("cubegen", :cubegen),
-	    ExecutableProduct("fastcap", :fastcap),
-	    ExecutableProduct("fasthenry", :fasthenry),
-	    ExecutableProduct("fcpp", :fcpp),
-	    ExecutableProduct("lstpack", :lstpack),
-	    ExecutableProduct("lstunpack", :lstunpack),
-	    ExecutableProduct("mrouter", :mrouter),
-	    ExecutableProduct("pipedgen", :pipedgen),
-	    ExecutableProduct("pyragen", :pyragen),
-	    ExecutableProduct("vl", :vl),
-	    ExecutableProduct("wrdecode", :wrdecode),
-	    ExecutableProduct("wrencode", :wrencode),
-	    ExecutableProduct("xic", :xic),
-	    ExecutableProduct("zbuf", :zbuf),
-	    ]
+    ExecutableProduct("wrspice", :wrspice),
+    ExecutableProduct("wrspiced", :wrspiced),
+    ExecutableProduct("csvtoraw", :csvtoraw),
+    ExecutableProduct("multidec", :multidec),
+    ExecutableProduct("printtoraw", :printtoraw),
+    ExecutableProduct("mmjco", :mmjco),
+    ExecutableProduct("admsXml", :admsXml),
+    ExecutableProduct("vl", :vl),
+]
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency("Libtiff_jll"; compat="4.5.1"),
-    Dependency("libpng_jll"),
-    Dependency("JpegTurbo_jll"),
     Dependency("GSL_jll"; compat="~2.7.2"),
     Dependency("CompilerSupportLibraries_jll"),
 ]
