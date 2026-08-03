a5267e523ba2c56366c09492225ee22ac103ff87
Matt Bauman <mbauman@juliahub.com>
2026-08-03 10:29:53 -0400

    [LibModbus] update version to 3.1.12
    https://github.com/stephane/libmodbus/commit/9af6c16074df566551bca0a7c37443e48f216289

--- L/LibModbus/build_tarballs.jl
+++ L/LibModbus/build_tarballs.jl
@@ -3,11 +3,11 @@
 using BinaryBuilder, Pkg
 
 name = "LibModbus"
-version = v"3.1.10"
+version = v"3.1.12"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/stephane/libmodbus.git", "b25629bfb508bdce7d519884c0fa9810b7d98d44")
+    GitSource("https://github.com/stephane/libmodbus.git", "9af6c16074df566551bca0a7c37443e48f216289")
 ]
 
 # Bash recipe for building across all platforms
