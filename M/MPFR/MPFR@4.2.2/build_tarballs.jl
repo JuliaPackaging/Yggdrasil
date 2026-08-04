5dfae61523ce00f1b6bc7c3e8862f8c5dbd8b448
Alex Arslan <ararslan@comcast.net>
2026-08-03 20:49:55 -0700

    [MPFR@4.2.2] Set preferred LLVM version to 17
    This fixes a miscompile with Clang on FreeBSD AArch64. Older Clang
    versions didn't model the AArch64 TLSDESC call as clobbering the NZCV
    condition flags, which for MPFR led to Julia issue 57121. This was fixed
    in LLVM 17, and I've confirmed locally that `libmpfr.so.6.2.2` built
    with `preferred_llvm_version=v"17"` resolves that Julia issue.

--- M/MPFR/MPFR@4.2.2/build_tarballs.jl
+++ M/MPFR/MPFR@4.2.2/build_tarballs.jl
@@ -12,7 +12,7 @@ script = mpfr_script()
 platforms = mpfr_platforms()
 products = mpfr_products()
 
-preferred_llvm_version = v"13.0.1"
+preferred_llvm_version = v"17"
 
 # Dependencies that must be installed before this package can be built
 dependencies = mpfr_dependencies(platforms; llvm_compilerrt_version=preferred_llvm_version)
