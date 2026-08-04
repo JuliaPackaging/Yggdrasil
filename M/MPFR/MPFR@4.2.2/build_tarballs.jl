2aae6a722005ccbe35adea2c07043c888b7b9abe
Alex Arslan <ararslan@comcast.net>
2026-08-03 22:58:14 -0700

    Use a similar pattern as the OpenBLAS builder

--- M/MPFR/MPFR@4.2.2/build_tarballs.jl
+++ M/MPFR/MPFR@4.2.2/build_tarballs.jl
@@ -13,11 +13,30 @@ platforms = mpfr_platforms()
 products = mpfr_products()
 
 preferred_llvm_version = v"17"
+msan_preferred_llvm_version = v"13.0.1+0"
 
 # Dependencies that must be installed before this package can be built
-dependencies = mpfr_dependencies(platforms; llvm_compilerrt_version=v"13.0.1+0")
+dependencies = mpfr_dependencies(platforms; llvm_compilerrt_version=msan_preferred_llvm_version)
 
-# Build the tarballs
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies;
-               preferred_gcc_version=v"5", preferred_llvm_version=preferred_llvm_version,
-               julia_compat="1.6")
+# Everything below is necessary only because we need to build msan platforms with a different LLVM version
+
+# Do we build all platforms, or those specified as arguments?
+platform_args = filter(!startswith("--"), ARGS)
+if !isempty(platform_args)
+    platforms = parse_platform.(split(only(platform_args), ','))
+end
+
+# The regular options, excluding the list of platforms
+option_args = filter(startswith("--"), ARGS)
+non_register_option_args = filter(!=("--register"), option_args)
+
+for (n, platform) in enumerate(platforms)
+    # We register the build products only after the last build
+    args = n == length(platforms) ? option_args : non_register_option_args
+
+    pref_llvm = sanitize(platform) == "memory" ? msan_preferred_llvm_version : preferred_llvm_version
+
+    build_tarballs(args, name, version, sources, script, [platform], products, dependencies;
+                   preferred_gcc_version=v"5", preferred_llvm_version=pref_llvm,
+                   julia_compat="1.6")
+end
