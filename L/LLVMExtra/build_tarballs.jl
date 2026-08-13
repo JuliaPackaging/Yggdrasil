4d7c21e5d76e5cf8bb31589ed17c079dbe4bd86b
Tim Besard <tim.besard@gmail.com>
2026-08-13 14:11:04 +0000

    Fix hash.

--- L/LLVMExtra/build_tarballs.jl
+++ L/LLVMExtra/build_tarballs.jl
@@ -13,7 +13,7 @@ version = v"0.0.45"
 llvm_versions = [v"15.0.7+12", v"16.0.6+6", v"18.1.7+5", v"20.1.8+0", v"21.1.8+0", v"22.1.8+0"]
 
 sources = [
-    GitSource(repo, "43fd81ddaf4c9060d9c99fa0fd3a7ea33b640138"),
+    GitSource(repo, "fc806c1e1a7c71e4b4260ad453c55ea74056e4dd"),
 ]
 
 # Bash recipe for building across all platforms
