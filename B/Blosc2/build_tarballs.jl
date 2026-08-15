bd5491cc91de344b4c2ba7d9208ba85bafa81418
Erik Schnetter <schnetter@gmail.com>
2026-08-15 12:02:18 -0400

    Blosc2: New version 3.3.2 (#14432)

--- B/Blosc2/build_tarballs.jl
+++ B/Blosc2/build_tarballs.jl
@@ -4,7 +4,7 @@ using BinaryBuilder, Pkg
 
 name = "Blosc2"
 
-upstream_version = v"3.2.3"
+upstream_version = v"3.3.2"
 # We add a version offset because:
 # - Blosc2 2.15 is not ABI-compatible with Blosc2 2.14
 #   (see the release notes <https://github.com/Blosc/c-blosc2/releases/tag/v2.15.0>)
@@ -12,7 +12,8 @@ upstream_version = v"3.2.3"
 #   (the shared library SOVERSION was increased)
 # - Blosc2 2.23.1 is not ABI-compatible with Blosc2 2.23.0
 #   (the shared library SOVERSION was increased)
-# - Blosc 3.2 is not ABI-compatible with Blosc2 3.1
+# - Blosc2 3.2 is not ABI-compatible with Blosc2 3.1
+# - Blosc2 3.3 IS ABI-compatible with Blosc2 3.2
 version_offset = v"1.0.0"
 version = VersionNumber(upstream_version.major * 100 + version_offset.major,
                         upstream_version.minor * 100 + version_offset.minor,
@@ -20,7 +21,7 @@ version = VersionNumber(upstream_version.major * 100 + version_offset.major,
 
 # Collection of sources required to build Blosc2
 sources = [
-    GitSource("https://github.com/Blosc/c-blosc2.git", "d52cbeec8afb35ada7ea62f04168e5d970d9c40b"),
+    GitSource("https://github.com/Blosc/c-blosc2.git", "d1905e9d7157e0f5317ca069c80dd5bacc0d9657"),
     DirectorySource("bundled"),
 ]
 
@@ -33,9 +34,6 @@ cd $WORKSPACE/srcdir/c-blosc2
 # functions have incompatible return types (although both are 64-bit
 # integers).
 atomic_patch -p1 ../patches/_xsetbv.patch
-
-# The build options hide `flock` on FreeBSD <https://github.com/Blosc/c-blosc2/issues/794>
-atomic_patch -p1 ../patches/flock.patch
 
 # Clang on Apple does not (yet?) properly support `__builtin_cpu_supports`.
 # The symbol `__cpu_model` is not provided by any standard library.
