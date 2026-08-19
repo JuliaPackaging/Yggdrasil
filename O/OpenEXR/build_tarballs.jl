f73068c2986c19b59e69d171a9bc5156dfc503e9
Erik Schnetter <schnetter@gmail.com>
2026-08-19 11:57:32 -0400

    OpenEXR: New version 3.4.14

--- O/OpenEXR/build_tarballs.jl
+++ O/OpenEXR/build_tarballs.jl
@@ -5,11 +5,11 @@ const YGGDRASIL_DIR = "../.."
 include(joinpath(YGGDRASIL_DIR, "platforms", "macos_sdks.jl"))
 
 name = "OpenEXR"
-version = v"3.4.13"
+version = v"3.4.14"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/AcademySoftwareFoundation/openexr.git", "c1194b2cb23a1bdf76fe5e756b22e8436b9a98c9"),
+    GitSource("https://github.com/AcademySoftwareFoundation/openexr.git", "777d231a179de1711d2a942810d9559216ab3f4c"),
     DirectorySource("bundled"),
 ]
 
