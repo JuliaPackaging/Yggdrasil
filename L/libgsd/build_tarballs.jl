19bd645d73bab3cee319f996fa9066bce699205b
ywitzky <134135819+ywitzky@users.noreply.github.com>
2026-08-27 10:15:14 +0200

    dont filter riscv

--- L/libgsd/build_tarballs.jl
+++ L/libgsd/build_tarballs.jl
@@ -37,4 +37,4 @@ dependencies = Dependency[
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
-build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6",skip_audit = any_riscv)
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies; julia_compat="1.6")
