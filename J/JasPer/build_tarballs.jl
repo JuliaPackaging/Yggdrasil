31b66ef0fcfaff82823df9e9444d801b1b453ba7
Erik Schnetter <schnetter@gmail.com>
2026-09-05 19:24:07 -0400

    JasPer: New version 4.2.9

--- J/JasPer/build_tarballs.jl
+++ J/JasPer/build_tarballs.jl
@@ -3,32 +3,32 @@
 using BinaryBuilder, Pkg
 
 name = "JasPer"
-version = v"2.0.33"
+version = v"4.2.9"
 
 # Collection of sources required to complete build
 sources = [
-    GitSource("https://github.com/jasper-software/jasper", "fe00207dc10db1d7cc6f2757961c5c6bdfd10973"),
+    GitSource("https://github.com/jasper-software/jasper", "63e106c80eb72af9fd4fa28772499ab0138b9994"),
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 
 cd $WORKSPACE/srcdir/jasper*
-mkdir jll_build
 
-cmake -H. \
--B./jll_build \
--DCMAKE_INSTALL_PREFIX=$prefix \
--DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
--DJAS_ENABLE_DOC=false \
--DJAS_ENABLE_OPENGL=false \
--DALLOW_IN_SOURCE_BUILD=true \
--DJAS_ENABLE_AUTOMATIC_DEPENDENCIES=false \
--DCMAKE_BUILD_TYPE=Release
+# Note: the build directory must not be named `build`. The JasPer source tree
+# contains a file `build/build`, which then collides with the generated
+# `${CMAKE_CURRENT_BINARY_DIR}/build/pkgconfig/jasper.pc`.
+cmake -Bjll_build \
+    -DCMAKE_INSTALL_PREFIX=$prefix \
+    -DCMAKE_TOOLCHAIN_FILE=${CMAKE_TARGET_TOOLCHAIN} \
+    -DJAS_ENABLE_DOC=false \
+    -DJAS_ENABLE_OPENGL=false \
+    -DALLOW_IN_SOURCE_BUILD=true \
+    -DJAS_STDC_VERSION=201112L \
+    -DCMAKE_BUILD_TYPE=Release
 
-cd jll_build/
-make clean all
-make install
+cmake --build jll_build --parallel ${nproc}
+cmake --install jll_build
 """
 
 # These are the platforms we will build for by default, unless further
@@ -46,7 +46,7 @@ products = [
 
 # Dependencies that must be installed before this package can be built
 dependencies = [
-    Dependency(PackageSpec(name="JpegTurbo_jll", uuid="aacddb02-875f-59d6-b918-886e6ef4fbf8"))
+    Dependency("JpegTurbo_jll"; compat="3.2.0"),
 ]
 
 # Build the tarballs, and possibly a `build.jl` as well.
