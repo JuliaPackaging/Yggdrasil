dc372a7a92073ea9df30df86e3950e463ab8e633
Tim Besard <tim.besard@gmail.com>
2026-09-14 22:18:02 +0200

    [LLVM_full] Rebuild v23.1.1 with AArch64 load latency fix (#14797)

--- L/LLVM/common.jl
+++ L/LLVM/common.jl
@@ -30,7 +30,7 @@ const llvm_tags = Dict(
     v"20.1.8" => "24bdfdd813f4117f0464fd0dac5b4bce43ed1388", # julia-20.1.8-2
     v"21.1.8" => "151034ef71856c7406f58c150dba7d419dbd063d", # julia-21.1.8-1
     v"22.1.8" => "4df0bb28e9e4d59f293433a1e325a46479da5174", # julia-22.1.8-1
-    v"23.1.1" => "5ab773a1ba20e4e289b8343283b62d7bd64b8642", # julia-23.1.1-1
+    v"23.1.1" => "fbe0dc11974bf1e4f6380eae95101d36fa0650b8", # julia-release/23.x (julia-23.1.1-1 + 2)
 )
 
 const buildscript = raw"""
