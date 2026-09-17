c521f062d544b47b547439bf36357c9aafa9ce05
Alex Arslan <ararslan@comcast.net>
2026-09-16 18:14:07 -0700

    Revert "[CHICKEN] Remove the static library artifact"
    This reverts commit 422e0324aba2fd2f0588b518f2dba5a5f3d18245.

--- C/CHICKEN/build_tarballs.jl
+++ C/CHICKEN/build_tarballs.jl
@@ -65,6 +65,7 @@ products = [
     ExecutableProduct("chicken-uninstall", :chicken_uninstall),
     ExecutableProduct("csc", :chicken_csc),
     ExecutableProduct("csi", :chicken_csi),
+    FileProduct("lib/libchicken.a", :libchicken_a),
     FileProduct("include/chicken/chicken.h", :chicken_h),
     FileProduct("include/chicken/chicken-config.h", :chicken_config_h),
     LibraryProduct("libchicken", :libchicken),
