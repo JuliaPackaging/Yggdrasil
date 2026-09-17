0091f0d02d07d142fbb8ed2d89dbcced8d070f3c
Alex Arslan <ararslan@comcast.net>
2026-09-16 18:14:58 -0700

    Fix static library maybe

--- C/CHICKEN/build_tarballs.jl
+++ C/CHICKEN/build_tarballs.jl
@@ -65,7 +65,7 @@ products = [
     ExecutableProduct("chicken-uninstall", :chicken_uninstall),
     ExecutableProduct("csc", :chicken_csc),
     ExecutableProduct("csi", :chicken_csi),
-    FileProduct("lib/libchicken.a", :libchicken_a),
+    FileProduct("lib/libchicken-static.a", :libchicken_static),
     FileProduct("include/chicken/chicken.h", :chicken_h),
     FileProduct("include/chicken/chicken-config.h", :chicken_config_h),
     LibraryProduct("libchicken", :libchicken),
