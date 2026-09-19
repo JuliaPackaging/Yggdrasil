ea4b6d458c58f194c53532a52c2aaf0e41f9ee08
ejmeitz <emeitz@andrew.cmu.edu>
2026-09-19 15:42:51 -0500

    forgot this needs to get bumped :/

--- C/cunumeric_jl_wrapper/build_tarballs.jl
+++ C/cunumeric_jl_wrapper/build_tarballs.jl
@@ -54,7 +54,7 @@ products = [
 dependencies = [
     Dependency("cupynumeric_jll"; compat = "~26.6"), # versioning is Year.Month
     Dependency("legate_jll"; compat = "~26.6"),
-    Dependency("libcxxwrap_julia_jll"; compat="0.14.3"),
+    Dependency("libcxxwrap_julia_jll"; compat="0.14.11"),
     BuildDependency("libjulia_jll"),
     HostBuildDependency(PackageSpec(; name = "CMake_jll", version = "3.31.9")),
     Dependency(PackageSpec(name="CompilerSupportLibraries_jll", uuid="e66e0078-7015-5450-92f7-15fbd957f2ae")) 
