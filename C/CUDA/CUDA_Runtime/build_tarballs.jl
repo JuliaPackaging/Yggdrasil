c6ed7df1a22c5df2208c679433cd6c86556cf35a
Tim Besard <tim.besard@gmail.com>
2026-09-22 16:29:07 +0200

    CUDA: Bump for 13.4 Update 1.

--- C/CUDA/CUDA_Runtime/build_tarballs.jl
+++ C/CUDA/CUDA_Runtime/build_tarballs.jl
@@ -194,5 +194,3 @@ for (i,build) in enumerate(builds)
                    julia_compat="1.10", augment_platform_block, init_block=build.init_block,
                    lazy_artifacts=true, dont_dlopen=true)
 end
-
-# bump
