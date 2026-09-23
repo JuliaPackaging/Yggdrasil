231b21b6967e365d1ab84025c22ffb99e14351fe
Lars Göttgens <lars.goettgens@rwth-aachen.de>
2026-09-23 11:41:05 +0200

    [GAP] Add a patch for julia nightly compatability

--- G/GAP/build_tarballs.jl
+++ G/GAP/build_tarballs.jl
@@ -22,7 +22,7 @@ using BinaryBuilder, Pkg
 
 name = "GAP"
 upstream_version = v"4.16.1"
-version = v"400.1600.101"
+version = v"400.1600.102"
 
 # Collection of sources required to complete build
 sources = [
