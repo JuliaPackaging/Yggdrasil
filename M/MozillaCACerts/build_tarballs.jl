f3961133a1a7af91bfbcc20b3958170b95cbc2e4
Erik Schnetter <schnetter@gmail.com>
2026-09-25 10:38:27 -0400

    MozillaCACerts: New version 2026-09-25 (#14918)

--- M/MozillaCACerts/build_tarballs.jl
+++ M/MozillaCACerts/build_tarballs.jl
@@ -4,13 +4,13 @@ using BinaryBuilder
 
 name = "MozillaCACerts"
 # Info and new versions here: https://curl.haxx.se/docs/caextract.html
-cacert_version = "2026-08-13"
+cacert_version = "2026-09-25"
 version = VersionNumber(replace(cacert_version, '-'=>'.'))
 
 # Collection of sources required to build MozillaCACerts
 sources = [
     FileSource("https://curl.haxx.se/ca/cacert-$cacert_version.pem", 
-               "f66dff1bdf8f96060b8177976f8b7d9254bc89bc4db933d769f7384d28480bc9",
+               "a41b5d356aea97a529fe27e0f7316d2f9d946d75927476cf9cf1b90637d00505",
                filename="cacert.pem"),
 ]
 
