d6c1bdf857e1c275e45e9e09b60dce9b08e8a5fa
Alexis Montoison <35051714+amontoison@users.noreply.github.com>
2026-09-25 08:30:50 -0400

    Apply suggestion from @amontoison

--- G/GALAHAD/build_tarballs.jl
+++ G/GALAHAD/build_tarballs.jl
@@ -130,7 +130,7 @@ platforms = filter(p -> libgfortran_version(p) != v"3", platforms)
 platforms = filter(p -> libgfortran_version(p) != v"4", platforms)
 platforms = filter(p -> libc(p) != "musl", platforms)
 platforms = filter(p -> nbits(p) != 32, platforms)
-platforms = filter(p -> p != "aarch64-unknown-freebsd-libgfortran5", platforms)
+platforms = filter(p -> !(arch(p) == "aarch64" && Sys.isfreebsd(p)), platforms)
 
 # The products that we will ensure are always built
 products = [
